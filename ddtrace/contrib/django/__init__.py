500bd8de38ff46bfac24f958c988f371f7a10e66
Matt Perpick <matthewperpick@gmail.com>
2016-06-29 17:34:19 +0000

    django: add user information

--- ddtrace/contrib/django/__init__.py
+++ ddtrace/contrib/django/__init__.py
@@ -11,6 +11,7 @@ from ...contrib import func_name
 
 # 3p
 from django.template import Template
+from django.apps import apps
 
 
 log = logging.getLogger(__name__)
@@ -52,7 +53,12 @@ class TraceMiddleware(object):
             span = _get_req_span(request)
             if span:
                 span.set_tag(http.STATUS_CODE, response.status_code)
+
+                if apps.is_installed("django.contrib.auth"):
+                    span = _set_auth_tags(span, request)
+
                 span.finish()
+
         except Exception:
             log.exception("error tracing request")
         finally:
@@ -93,10 +99,32 @@ def _patch_template(tracer):
 
     Template.render = TracedTemplate.render.__func__
 
-
 def _get_req_span(request):
+    """ Return the datadog span from the given request. """
     return getattr(request, '_datadog_request_span', None)
 
 def _set_req_span(request, span):
+    """ Set the datadog span on the given request. """
     return setattr(request, '_datadog_request_span', span)
+
+
+def _set_auth_tags(span, request):
+    """ Patch any available auth tags from the request onto the span. """
+    user = getattr(request, 'user', None)
+    if not user:
+        return
+
+    if hasattr(user, 'is_authenticated'):
+        span.set_tag('django.user.is_authenticated', user.is_authenticated())
+
+    uid = getattr(user, 'pk', None)
+    if uid:
+        span.set_tag('django.user.id', uid)
+
+    uname = getattr(user, 'username', None)
+    if uname:
+        span.set_tag('django.user.name', uname)
+
+    return span
+
 
