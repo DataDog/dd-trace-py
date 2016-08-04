96b1b76a6f0064bef2f6ccf9a25316ee9570a495
Matt Perpick <matthewperpick@gmail.com>
2016-08-04 20:20:40 +0000

    sqlalchemy: fix docs

---
+++ ddtrace/contrib/sqlalchemy/engine.py
@@ -0,0 +1,111 @@
+"""
+To trace sqlalchemy queries, add instrumentation to the engine class or
+instance you are using::
+
+    from ddtrace import tracer
+    from ddtrace.contrib.sqlalchemy import trace_engine
+    from sqlalchemy import create_engine
+
+    engine = create_engine('sqlite:///:memory:')
+    trace_engine(engine, tracer, "my-database")
+
+    engine.connect().execute("select count(*) from users")
+"""
+
+# 3p
+import sqlalchemy
+from sqlalchemy.event import listen
+
+# project
+import ddtrace
+from ddtrace.buffer import ThreadLocalSpanBuffer
+from ddtrace.ext import sql as sqlx
+from ddtrace.ext import errors as errorsx
+from ddtrace.ext import net as netx
+
+
+def trace_engine(engine, tracer=None, service=None):
+    """
+    Add tracing instrumentation to the given sqlalchemy engine or instance.
+
+    :param sqlalchemy.Engine engine: a SQLAlchemy engine class or instance
+    :param ddtrace.Tracer tracer: a tracer instance. will default to the global
+    :param str service: the name of the service to trace.
+    """
+    tracer = tracer or ddtrace.tracer # by default use the global tracing instance.
+    EngineTracer(tracer, service, engine)
+
+
+class EngineTracer(object):
+
+    def __init__(self, tracer, service, engine):
+        self.tracer = tracer
+        self.engine = engine
+        self.vendor = sqlx.normalize_vendor(engine.name)
+        self.service = service or self.vendor
+        self.name = "%s.query" % self.vendor
+
+        self._span_buffer = ThreadLocalSpanBuffer()
+
+        listen(engine, 'before_cursor_execute', self._before_cur_exec)
+        listen(engine, 'after_cursor_execute', self._after_cur_exec)
+        listen(engine, 'dbapi_error', self._dbapi_error)
+
+    def _before_cur_exec(self, conn, cursor, statement, parameters, context, executemany):
+        self._span_buffer.pop() # should always be empty
+
+        span = self.tracer.trace(
+            self.name,
+            service=self.service,
+            span_type=sqlx.TYPE,
+            resource=statement)
+
+        # keep the unnormalized query
+        span.set_tag(sqlx.QUERY, statement)
+
+        if not _set_tags_from_url(span, conn.engine.url):
+            _set_tags_from_cursor(span, self.vendor, cursor)
+
+        self._span_buffer.set(span)
+
+    def _after_cur_exec(self, conn, cursor, statement, parameters, context, executemany):
+        span = self._span_buffer.pop()
+        if not span:
+            return
+
+        try:
+            if cursor and cursor.rowcount >= 0:
+                span.set_tag(sqlx.ROWS, cursor.rowcount)
+        finally:
+            span.finish()
+
+
+    def _dbapi_error(self, conn, cursor, statement, parameters, context, exception):
+        span = self._span_buffer.pop()
+        if not span:
+            return
+
+        try:
+            span.set_traceback()
+        finally:
+            span.finish()
+
+def _set_tags_from_url(span, url):
+    """ set connection tags from the url. return true if successful. """
+    if url.host:        span.set_tag(netx.TARGET_HOST, url.host)
+    if url.port:        span.set_tag(netx.TARGET_PORT, url.port)
+    if url.database:    span.set_tag(sqlx.DB, url.database)
+
+    return bool(span.get_tag(netx.TARGET_HOST))
+
+def _set_tags_from_cursor(span, vendor, cursor):
+    """ attempt to set db connection tags by introspecting the cursor. """
+    if 'postgres' == vendor:
+        if hasattr(cursor, 'connection') and hasattr(cursor.connection, 'dsn'):
+            dsn = getattr(cursor.connection, 'dsn', None)
+            if dsn:
+                d = sqlx.parse_pg_dsn(dsn)
+                span.set_tag(sqlx.DB, d.get("dbname"))
+                span.set_tag(netx.TARGET_HOST, d.get("host"))
+                span.set_tag(netx.TARGET_PORT, d.get("port"))
+
