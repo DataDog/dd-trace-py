84396b641a74dc655232c2c91812b8c92f656146
Matt Perpick <matthewperpick@gmail.com>
2016-08-04 02:54:29 +0000

    db: use name sqlite instead of sqlite3
    just a bit nicer.

--- ddtrace/ext/sql.py
+++ ddtrace/ext/sql.py
@@ -5,3 +5,16 @@ TYPE = "sql"
 # tags
 QUERY = "sql.query"   # the query text
 ROWS = "sql.rows"     # number of rows returned by a query
+DB = "sql.db"         # the name of the database
+
+
+def normalize_vendor(vendor):
+    """ Return a canonical name for a type of database. """
+    if not vendor:
+        return "db"  # should this ever happen?
+    elif vendor == "sqlite":
+        return "sqlite3"  # for consistency with the sqlite3 integration
+    elif vendor == "postgresql":
+        return "postgres"
+    else:
+        return vendor
