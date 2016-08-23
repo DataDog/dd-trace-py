3050a564ed0a85d897572e7d7d0bf96ed5701e93
Christian Mauduit <christian.mauduit@datadoghq.com>
2016-08-23 14:54:27 +0200

    Tracing MySQLConnection and MySQLCursor objects.
    Made a subclass of MySQLConnection which, in turn, should creates
    subclasses of MySQLCursor, which actually trace the important stuff.

--- ddtrace/contrib/mysql/__init__.py
+++ ddtrace/contrib/mysql/__init__.py
@@ -3,29 +3,16 @@ The MySQL mysql.connector integration works by creating patched
 MySQL connection classes which will trace API calls. For basic usage::
 
     from ddtrace import tracer
-    from ddtrace.contrib.mysql import get_traced_mysql
+    from ddtrace.contrib.mysql import get_traced_mysql_connection
 
-    # Trace the redis.StrictRedis class ...
-    MySQL = get_traced_redis(tracer, service="my-redis-cache")
-    conn = MySQL.connect(user="alice", password="b0b", host="localhost", port=3306, database="test")
-    conn.set("key", "value")
-
-To use a specific connector, e.g. the C extension module
-_mysql_connector::
-
-    import _mysql_connector
-    from ddtrace import tracer
-    from ddtrace.contrib.mysql import get_traced_mysql_from
-
-    # Trace the _mysql_connector.MySQL class
-    MySQL = get_traced_redis_from(tracer, _mysql_connector.MySQL, service="my-mysql-server")
-    conn = MySQL.connect(user="alice", password="b0b", host="localhost", port=3306, database="test")
-    conn.query("SELECT foo FROM bar;")
+    # Trace the mysql.connector.connection.MySQLConnection class ...
+    MySQL = get_traced_mysql_connection(tracer, service="my-mysql-server")
+    conn = MySQL(user="alice", password="b0b", host="localhost", port=3306, database="test")
+    cursor = conn.execute("SELECT 6*7 AS the_answer;")
 
 Help on mysql.connector can be found on:
 https://dev.mysql.com/doc/connector-python/en/
 """
-
 
 from ..util import require_modules
 
@@ -33,7 +20,6 @@ required_modules = ['mysql.connector']
 
 with require_modules(required_modules) as missing_modules:
     if not missing_modules:
-        from .tracers import get_traced_mysql
-        from .tracers import get_traced_mysql_from
+        from .tracers import get_traced_mysql_connection
 
-        __all__ = ['connection_factory']
+        __all__ = ['get_traced_mysql_connection']
--- ddtrace/contrib/mysql/tracers.py
+++ ddtrace/contrib/mysql/tracers.py
@@ -4,6 +4,8 @@ tracers exposed publicly
 # stdlib
 import time
 
+from mysql.connector.connection import MySQLConnection
+
 # dogtrace
 from ...ext import sql as sqlx
 from ...ext import AppTypes
@@ -11,23 +13,22 @@ from ...ext import AppTypes
 
 DEFAULT_SERVICE = 'mysql'
 
+def get_traced_mysql_connection(ddtracer, service=DEFAULT_SERVICE, meta=None):
+    return _get_traced_mysql(ddtracer, MySQLConnection, service, meta)
 
-def get_traced_mysql(ddtracer, service=DEFAULT_SERVICE, meta=None):
-    return _get_traced_mysql(ddtracer, mysql.connector, service, meta)
-
-
-def get_traced_mysql_from(ddtracer, baseclass, service=DEFAULT_SERVICE, meta=None):
-    return _get_traced_mysql(ddtracer, baseclass, service, meta)
+# _mysql_connector unsupported for now
+# def get_traced_mysql_connection_from(ddtracer, baseclass, service=DEFAULT_SERVICE, meta=None):
+#    return _get_traced_mysql(ddtracer, baseclass, service, meta)
 
 # pylint: disable=protected-access
 def _get_traced_mysql(ddtracer, baseclass, service, meta):
     ddtracer.set_service_info(
         service=service,
-        app="mysql",
+        app='mysql',
         app_type=AppTypes.db,
     )
 
-    class TracedMysql(baseclass):
+    class TracedMySQLCursor(baseclass):
         _datadog_tracer = ddtracer
         _datadog_service = service
         _datadog_meta = meta
@@ -36,21 +37,42 @@ def _get_traced_mysql(ddtracer, baseclass, service, meta):
         def set_datadog_meta(cls, meta):
             cls._datadog_meta = meta
 
-        def connect(self, *args, **kwargs):
-            with self._datadog_tracer.trace('mysql.connect') as s:
+        def __init__(self, *args, **kwargs):
+            self._datadog_cursor_creation = time.time()
+            super(TracedMySQLCursor, self).__init__(*args, **kwargs)
+
+        def execute(self, operation, params=None):
+            with self._datadog_tracer.trace('mysql.execute') as s:
                 if s.sampled:
                     s.service = self._datadog_service
                     s.span_type = sqlx.TYPE
 
-                    # query = format_command_args(args)
-                    # s.resource = query
+                    # FIXME query = format_command_args(args)
+                    s.resource = operation
                     # non quantized version
-                    #s.set_tag(sqlx.RAWCMD, query)
-
-                    #s.set_tags(_extract_conn_tags(self.connection_pool.connection_kwargs))
+                    s.set_tag(sqlx.QUERY, operation)
+                    s.set_tag(sqlx.DB, 'mysql')
+                    result = super(TracedMysql, self).execute(self, operation, params)
                     s.set_tags(self._datadog_meta)
-                    #s.set_metric(sqlx.ARGS_LEN, len(args))
+                    s.set_metric(sqlx.ROWS, cursor.rowcount)
 
-                return super(TracedMysql, self).execute_command(*args, **options)
+                return super(TracedMysql, self).execute(self, operation, params)
 
-    return TracedMysql
+    class TracedMySQLConnection(baseclass):
+        _datadog_tracer = ddtracer
+        _datadog_service = service
+        _datadog_meta = meta
+
+        @classmethod
+        def set_datadog_meta(cls, meta):
+            cls._datadog_meta = meta
+
+        def __init__(self, *args, **kwargs):
+            self._datadog_connection_creation = time.time()
+            super(TracedMySQLConnection, self).__init__(*args, **kwargs)
+
+        def cursor(self, buffered=None, raw=None, cursor_class=None):
+            # todo...
+            return
+
+    return TracedMySQLConnection
