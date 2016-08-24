104e87234456b5818c3f94877fc8d79074f142be
Christian Mauduit <christian.mauduit@datadoghq.com>
2016-08-24 12:04:32 +0200

    Added a test that returns several rows from MySQL

--- tests/contrib/mysql/test_mysql.py
+++ tests/contrib/mysql/test_mysql.py
@@ -5,16 +5,16 @@ import unittest
 
 from ddtrace.contrib.mysql import missing_modules
 
-if missing_modules:
-    raise unittest.SkipTest("Missing dependencies %s" % missing_modules)
-
-from nose.tools import eq_, ok_
+from nose.tools import eq_
 
 from ddtrace.tracer import Tracer
 from ddtrace.contrib.mysql import get_traced_mysql_connection
 
 from tests.test_tracer import DummyWriter
 from tests.contrib.config import MYSQL_CONFIG
+
+if missing_modules:
+    raise unittest.SkipTest("Missing dependencies %s" % missing_modules)
 
 class MySQLTest(unittest.TestCase):
     SERVICE = 'test-db'
@@ -42,5 +42,20 @@ class MySQLTest(unittest.TestCase):
         MySQL = get_traced_mysql_connection(tracer, service=MySQLTest.SERVICE)
         conn = MySQL(**MYSQL_CONFIG)
         cursor = conn.cursor()
-        rows = cursor.execute("SELECT 1;")
+        cursor.execute("SELECT 1")
+        rows = cursor.fetchall()
+        eq_(len(rows), 1)
+        conn.close()
+
+    def test_query_with_several_rows(self):
+        writer = DummyWriter()
+        tracer = Tracer()
+        tracer.writer = writer
+
+        MySQL = get_traced_mysql_connection(tracer, service=MySQLTest.SERVICE)
+        conn = MySQL(**MYSQL_CONFIG)
+        cursor = conn.cursor()
+        cursor.execute("SELECT n FROM (SELECT 42 n UNION SELECT 421 UNION SELECT 4210) m")
+        rows = cursor.fetchall()
+        eq_(len(rows), 3)
         conn.close()
