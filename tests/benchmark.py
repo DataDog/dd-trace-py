7c6544b9fadf341648006e537df89aa4c27c28cb
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2016-09-06 11:05:45 +0200

    [tracer] add benchmark for the @wrapt decorator

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,6 +1,8 @@
-
 import time
+import timeit
+
 from ddtrace import Tracer
+
 from .test_tracer import DummyWriter
 
 
@@ -20,6 +22,7 @@ def trace_error(tracer):
         1 / 0
 
 def run():
+    print("## tracer.trace() benchmark ##")
     tracer = Tracer()
     tracer.writer = DummyWriter()
 
@@ -28,9 +31,48 @@ def run():
     for _ in range(10000):
         trace(tracer)
     dur = time.time() - start
-    print 'loops:%s duration:%.5fs' % (loops, dur)
+    print '- trace execution time -> loops:%s duration:%.5fs' % (loops, dur)
+
+
+def benchmark_tracer_wrap_with_classname():
+    repeat = 10
+    number = 10000
+    writer = DummyWriter()
+    tracer = Tracer()
+    tracer.writer = writer
+
+    # testcase
+    class Foo(object):
+        @staticmethod
+        @tracer.wrap()
+        def s():
+            return 0
+
+        @classmethod
+        @tracer.wrap()
+        def c(cls):
+            return 0
+
+        @tracer.wrap()
+        def m(self):
+            return 0
+
+    f = Foo()
+
+    # benchmark
+    print("## @wrapt benchmark ##")
+    timer = timeit.Timer(f.s)
+    result = timer.repeat(repeat=repeat, number=number)
+    print("- staticmethod execution time: {:8.6f}".format(min(result)))
+    timer = timeit.Timer(f.c)
+    result = timer.repeat(repeat=repeat, number=number)
+    print("- classmethod execution time: {:8.6f}".format(min(result)))
+    timer = timeit.Timer(f.m)
+    result = timer.repeat(repeat=repeat, number=number)
+    print("- method execution time: {:8.6f}".format(min(result)))
 
 
 # Run it with `python -m tests.benchmark`
 if __name__ == '__main__':
+    benchmark_tracer_wrap_with_classname()
     run()
