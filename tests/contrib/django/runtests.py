e8029e903f2cd95df71dde65e55d80e8fbad6daf
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2016-09-19 14:54:56 -0400

    [django] add django test runner for nosetests

---
+++ tests/contrib/django/runtests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import os
+import sys
+
+
+if __name__ == "__main__":
+    app_to_test = "tests/contrib/django"
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+    from django.core.management import execute_from_command_line
+    execute_from_command_line([sys.argv[0], "test", app_to_test])
---
+++ tests/contrib/django/settings.py
@@ -0,0 +1,51 @@
+"""
+Settings configuration for the Django web framework. Update this
+configuration if you need to change the default behavior of
+Django during tests
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:'
+    }
+}
+
+SITE_ID = 1
+SECRET_KEY = 'not_very_secret_in_tests'
+USE_I18N = True
+USE_L10N = True
+STATIC_URL = '/static/'
+ROOT_URLCONF = 'tests.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+)
