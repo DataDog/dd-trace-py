83f0c316f5ce731bd4cc3e42578bb603922833e4
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2016-10-21 10:31:01 +0200

    [trace] improved traces encoder so that it can process multiple traces at once

--- ddtrace/encoding.py
+++ ddtrace/encoding.py
@@ -1,13 +1,20 @@
-"""
-Serialization code.
-"""
-
-
 from .compat import json
 
 
-def encode_spans(spans):
-    return json.dumps([s.to_dict() for s in spans])
+def encode_spans(traces):
+    """
+    Encodes a list of traces. It expects a list of items where each items
+    is a list of spans.
+
+    :param traces: A list of traces that should be serialized
+    """
+    spans = [span.to_dict() for trace in traces for span in trace]
+    return json.dumps(spans)
 
 def encode_services(services):
+    """
+    Encodes a dictionary of services.
+
+    :param services: A dictionary that contains one or more services
+    """
     return json.dumps(services)
