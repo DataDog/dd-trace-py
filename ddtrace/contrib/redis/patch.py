eb53430d623c161012fe319b429198002c834e6e
Matt Perpick <matthewperpick@gmail.com>
2016-10-31 13:48:05 +0000

    patch/redis: add tests for monkeypatched command

--- ddtrace/contrib/redis/patch.py
+++ ddtrace/contrib/redis/patch.py
@@ -4,10 +4,14 @@ import wrapt
 import redis
 
 import ddtrace
+from .util import format_command_args, _extract_conn_tags
+from ...ext import redis as redisx
 
 import redis
 
+
 def patch():
+    """ patch will patch the redis library to add tracing. """
     patch_target(redis.Redis)
     patch_target(redis.StrictRedis)
 
@@ -30,6 +34,11 @@ def patch_target(target, service=None, tracer=None):
 def _execute_command(func, instance, args, kwargs):
     service = getattr(instance, 'datadog_service', None) or 'redis'
     tracer = getattr(instance, 'datadog_tracer', None) or ddtrace.tracer
-    with tracer.trace('redis.cmd'):
-        print 'trace'
+    with tracer.trace('redis.command', service=service, span_type='redis') as s:
+        query = format_command_args(args)
+        s.resource = query
+        # non quantized version
+        s.set_tag(redisx.RAWCMD, query)
+        s.set_tags(_extract_conn_tags(instance.connection_pool.connection_kwargs))
+        s.set_metric(redisx.ARGS_LEN, len(args))
         return func(*args, **kwargs)
