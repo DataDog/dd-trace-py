2117cb8713c33416a2c42b7130716e36ff69a613
Matt Perpick <matthewperpick@gmail.com>
2016-10-31 21:33:30 +0000

    patch/redis: add pipeline tracing

--- tests/contrib/redis/test.py
+++ tests/contrib/redis/test.py
@@ -11,6 +11,7 @@ from nose.tools import eq_, ok_
 
 from ddtrace.tracer import Tracer
 from ddtrace.contrib.redis import get_traced_redis, get_traced_redis_from
+from ddtrace.info import ServiceInfo
 
 from ..config import REDIS_CONFIG
 from ...test_tracer import DummyWriter
@@ -62,8 +63,6 @@ class RedisTest(unittest.TestCase):
         writer = DummyWriter()
         tracer = Tracer()
         tracer.writer = writer
-
-
         TracedRedisCache = get_traced_redis(tracer, service=self.SERVICE)
         r = TracedRedisCache(port=REDIS_CONFIG['port'])
         _assert_conn_traced(r, tracer, self.SERVICE)
@@ -90,36 +89,23 @@ class RedisTest(unittest.TestCase):
 
         TracedRedisCache = get_traced_redis(tracer, service=self.SERVICE)
         r = TracedRedisCache(port=REDIS_CONFIG['port'])
-
-        with r.pipeline() as p:
-            p.set('blah', 32)
-            p.rpush('foo', u'éé')
-            p.hgetall('xxx')
-
-            p.execute()
-
-        spans = writer.pop()
-        eq_(len(spans), 1)
-        span = spans[0]
-        eq_(span.service, self.SERVICE)
-        eq_(span.name, 'redis.pipeline')
-        eq_(span.resource, u'SET blah 32\nRPUSH foo éé\nHGETALL xxx')
-        eq_(span.span_type, 'redis')
-        eq_(span.error, 0)
-        eq_(span.get_tag('out.redis_db'), '0')
-        eq_(span.get_tag('out.host'), 'localhost')
-        eq_(span.get_tag('out.port'), self.TEST_PORT)
-        eq_(span.get_tag('redis.raw_command'), u'SET blah 32\nRPUSH foo éé\nHGETALL xxx')
-        ok_(span.get_metric('redis.pipeline_age') > 0)
-        eq_(span.get_metric('redis.pipeline_length'), 3)
+        _assert_pipeline_traced(r, tracer, self.SERVICE)
 
     def test_monkeypatch(self):
-        tracer = Tracer()
-        tracer.writer = DummyWriter()
-        r = redis.Redis(port=REDIS_CONFIG['port'])
         from ddtrace.contrib.redis import patch
-        patch.patch_target(r, service=self.SERVICE, tracer=tracer)
-        _assert_conn_traced(r, service=self.SERVICE, tracer=tracer)
+
+        suite = [
+            _assert_conn_traced,
+            _assert_pipeline_traced
+        ]
+
+        for f in suite:
+            tracer = Tracer()
+            tracer.writer = DummyWriter()
+            r = redis.Redis(port=REDIS_CONFIG['port'])
+            service_info = ServiceInfo(service=self.SERVICE, tracer=tracer)
+            patch.patch_target(r, service_info)
+            f(r, service=self.SERVICE, tracer=tracer)
 
     def test_custom_class(self):
         class MyCustomRedis(redis.Redis):
@@ -149,6 +135,28 @@ class RedisTest(unittest.TestCase):
         eq_(spans[1].name, 'redis.command')
         eq_(spans[1].resource, 'GET foo')
 
+def _assert_pipeline_traced(conn, tracer, service):
+    r = conn
+    writer = tracer.writer
+    with r.pipeline() as p:
+        p.set('blah', 32)
+        p.rpush('foo', u'éé')
+        p.hgetall('xxx')
+        p.execute()
+
+    spans = writer.pop()
+    eq_(len(spans), 1)
+    span = spans[0]
+    eq_(span.service, service)
+    eq_(span.name, 'redis.command')
+    eq_(span.resource, u'SET blah 32\nRPUSH foo éé\nHGETALL xxx')
+    eq_(span.span_type, 'redis')
+    eq_(span.error, 0)
+    eq_(span.get_tag('out.redis_db'), '0')
+    eq_(span.get_tag('out.host'), 'localhost')
+    eq_(span.get_tag('redis.raw_command'), u'SET blah 32\nRPUSH foo éé\nHGETALL xxx')
+    #ok_(span.get_metric('redis.pipeline_age') > 0)
+    eq_(span.get_metric('redis.pipeline_length'), 3)
 
 def _assert_conn_traced(conn, tracer, service):
     r = conn
@@ -161,14 +169,9 @@ def _assert_conn_traced(conn, tracer, service):
     eq_(span.name, 'redis.command')
     eq_(span.span_type, 'redis')
     eq_(span.error, 0)
-    meta = {
-        'redis.raw_command': u'GET cheese',
-        'out.host': u'localhost',
-        #'out.port': self.TEST_PORT,
-        'out.redis_db': u'0',
-    }
-    for k, v in meta.items():
-        assert span.get_tag(k) == v
+    eq_(span.get_tag('out.redis_db'), '0')
+    eq_(span.get_tag('out.host'), 'localhost')
+    eq_(span.get_tag('redis.raw_command'), u'GET cheese')
     eq_(span.get_metric('redis.args_length'), 2)
     eq_(span.resource, 'GET cheese')
 
