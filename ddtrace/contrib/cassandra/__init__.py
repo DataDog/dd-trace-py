97bf6de1512a120d1e0dd812681f752903f1ca88
Matt Perpick <matthewperpick@gmail.com>
2016-11-07 22:15:40 +0000

    remove unused import

--- ddtrace/contrib/cassandra/__init__.py
+++ ddtrace/contrib/cassandra/__init__.py
@@ -17,7 +17,7 @@ required_modules = ['cassandra.cluster']
 
 with require_modules(required_modules) as missing_modules:
     if not missing_modules:
-        from .session import get_traced_cassandra, patch, patch_cluster
+        from .session import get_traced_cassandra, patch
         __all__ = [
             'get_traced_cassandra',
             'patch',
