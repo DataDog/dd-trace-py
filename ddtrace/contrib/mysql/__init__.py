73c93a7d19ce573bc246e9920b32f8083942f353
Benjamin Fernandes <benjamin@datadoghq.com>
2016-11-21 18:53:28 +0100

    Consolidate mysql patching
    - Add tests for patching
    - Update documentation with patch()

--- ddtrace/contrib/mysql/__init__.py
+++ ddtrace/contrib/mysql/__init__.py
@@ -1,15 +1,18 @@
-"""
-The MySQL mysql.connector integration works by creating patched
-MySQL connection classes which will trace API calls. For basic usage::
+"""Instrumeent mysql to report MySQL queries.
 
-    from ddtrace import tracer
-    from ddtrace.contrib.mysql import get_traced_mysql_connection
+Patch your mysql connection to make it work.
 
-    # Trace the mysql.connector.connection.MySQLConnection class ...
-    MySQL = get_traced_mysql_connection(tracer, service="my-mysql-server")
-    conn = MySQL(user="alice", password="b0b", host="localhost", port=3306, database="test")
+    from ddtrace import Pin, patch
+    patch(mysql=True)
+    from mysql.connector import connect
+
+    # This will report a span with the default settings
+    conn = connect(user="alice", password="b0b", host="localhost", port=3306, database="test")
     cursor = conn.cursor()
     cursor.execute("SELECT 6*7 AS the_answer;")
+
+    # To customize one client instrumentation
+    Pin.get_from(conn).service = 'my-mysql'
 
 This package works for mysql.connector version 2.1.x.
 Only the default full-Python integration works. The binary C connector,
@@ -18,13 +21,13 @@ provided by _mysql_connector, is not supported yet.
 Help on mysql.connector can be found on:
 https://dev.mysql.com/doc/connector-python/en/
 """
-
 from ..util import require_modules
 
 required_modules = ['mysql.connector']
 
 with require_modules(required_modules) as missing_modules:
     if not missing_modules:
+        from .patch import patch
         from .tracers import get_traced_mysql_connection
 
-        __all__ = ['get_traced_mysql_connection']
+        __all__ = ['get_traced_mysql_connection', 'patch']
--- ddtrace/contrib/mysql/tracers.py
+++ ddtrace/contrib/mysql/tracers.py
@@ -1,61 +1,9 @@
-
-# stdlib
 import logging
 
-# 3p
-import wrapt
 import mysql.connector
 
-# project
-from ddtrace import Pin
-from ddtrace.contrib.dbapi import TracedConnection
-from ...ext import net, db
-
-
-CONN_ATTR_BY_TAG = {
-    net.TARGET_HOST : 'server_host',
-    net.TARGET_PORT : 'server_port',
-    db.USER: 'user',
-    db.NAME: 'database',
-}
-
-
-log = logging.getLogger(__name__)
-
-
-def patch():
-    """ Patch monkey patches psycopg's connection function
-        so that the connection's functions are traced.
-    """
-    wrapt.wrap_function_wrapper('mysql.connector', 'connect', _connect)
-    if hasattr(mysql.connector, 'Connect'):
-        mysql.connector.Connect = mysql.connector.connect
-
-def unpatch():
-    if isinstance(mysql.connector.connect, wrapt.ObjectProxy):
-        mysql.connector.connect = mysql.connector.connect.__wrapped__
-        if hasattr(mysql.connector, 'Connect'):
-            mysql.connector.Connect = mysql.connector.connect
-
-def patch_conn(conn, pin=None):
-    if not pin:
-        pin = Pin(service="mysql", app="mysql")
-
-    # grab the metadata from the conn
-    pin.tags = pin.tags or {}
-    for tag, attr in CONN_ATTR_BY_TAG.items():
-        pin.tags[tag] = getattr(conn, attr, '')
-
-    wrapped = TracedConnection(conn)
-    pin.onto(wrapped)
-    return wrapped
-
-
-def _connect(func, instance, args, kwargs):
-    conn = func(*args, **kwargs)
-    return patch_conn(conn)
 
 # deprecated
 def get_traced_mysql_connection(*args, **kwargs):
-    log.warn("get_traced_mysql_connection is deprecated")
+    logging.warn("get_traced_mysql_connection is deprecated")
     return mysql.connector.MySQLConnection
