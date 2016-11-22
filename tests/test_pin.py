3f53c06c36e2c19a1ce3421336d2a9aa740daa4a
Matt Perpick <matthewperpick@gmail.com>
2016-11-22 20:01:09 +0000

    pin: make immutable
    change the interface of the pin so we:
    
    - make it immutable (as much as python lets us)
    - make it clear in the api that if you change it, you're cloning
      and you must re-pin.
    
    this lets us attach default pins to classes that can be overridden
    in instances. see the attached redis example.

--- tests/test_pin.py
+++ tests/test_pin.py
@@ -1,12 +1,14 @@
 
 from ddtrace import Pin
+from nose.tools import eq_
+
 
 def test_pin():
     class A(object):
         pass
 
     a = A()
-    pin = Pin(service="abc")
+    pin = Pin.new(service="abc")
     pin.onto(a)
 
     got = Pin.get_from(a)
@@ -21,12 +23,30 @@ def test_cant_pin():
     t = Thing()
     t.t = 1
 
-    Pin(service="a").onto(t)
+    Pin.new(service="a").onto(t)
+
+def test_cant_modify():
+    p = Pin.new(service="abc")
+    try:
+        p.service = "other"
+    except AttributeError:
+        pass
+
+def test_copy():
+    p1 = Pin.new(service="a", app="app_type", tags={"a":"b"})
+    p2 = p1.clone(service="b")
+    assert p1.service == "a"
+    assert p2.service == "b"
+    assert p1.app == "app_type"
+    assert p2.app == "app_type"
+    eq_(p1.tags, p2.tags)
+    assert not (p1.tags is p2.tags)
+    assert p1.tracer is p2.tracer
 
 def test_none():
     assert None is Pin.get_from(None)
 
 def test_repr():
-    p = Pin(service="abc")
+    p = Pin.new(service="abc")
     assert p.service == "abc"
     assert 'abc' in str(p)
