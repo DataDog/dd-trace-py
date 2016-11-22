c8a622d5c2bd50cdfdb189bdd4aa97f79b63cc3c
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2016-11-22 11:19:45 +0100

    [encoder] provide Encoder class for JSON and msgpack

--- ddtrace/encoding.py
+++ ddtrace/encoding.py
@@ -1,21 +1,66 @@
-from .compat import json
+import json
+import msgpack
+import logging
+
+from .util import flatten_spans
 
 
-def encode_traces(traces):
+log = logging.getLogger(__name__)
+
+
+class Encoder(object):
     """
-    Encodes a list of traces, expecting a list of items where each items
-    is a list of spans. Before dump the string in a JSON format, the list
-    is flatten.
-
-    :param traces: A list of traces that should be serialized
+    Encoder interface that provides the logic to encode traces and service.
     """
-    flatten_spans = [span.to_dict() for trace in traces for span in trace]
-    return json.dumps(flatten_spans)
+    def __init__(self):
+        """
+        When extending the ``Encoder`` class, ``headers`` must be set because
+        they're returned by the encoding methods, so that the API transport doesn't
+        need to know what is the right header to suggest the decoding format to the
+        agent
+        """
+        self.headers = {}
 
-def encode_services(services):
-    """
-    Encodes a dictionary of services.
+    def encode_traces(self, traces):
+        """
+        Encodes a list of traces, expecting a list of items where each items
+        is a list of spans. Before dump the string in a serialized format, the list
+        is flatten.
 
-    :param services: A dictionary that contains one or more services
-    """
-    return json.dumps(services)
+        :param traces: A list of traces that should be serialized
+        """
+        spans = flatten_spans(traces)
+        return self._encode(spans)
+
+    def encode_services(self, services):
+        """
+        Encodes a dictionary of services.
+
+        :param services: A dictionary that contains one or more services
+        """
+        return self._encode(services)
+
+    def _encode(self, obj):
+        """
+        Defines the underlying format used during traces or services encoding.
+        This method must be implemented and should only be used by the internal functions.
+        """
+        raise NotImplementedError
+
+
+class JSONEncoder(Encoder):
+    def __init__(self):
+        self.headers = { 'Content-Type': 'application/json' }
+
+    def _encode(self, obj):
+        log.debug('using JSON encoder; application performance may be degraded')
+        return json.dumps(obj)
+
+
+class MsgpackEncoder(Encoder):
+    def __init__(self):
+        self.headers = { 'Content-Type': 'application/msgpack' }
+
+    def _encode(self, obj):
+        log.debug('using Msgpack encoder')
+        return msgpack.packb(obj, use_bin_type=True)
