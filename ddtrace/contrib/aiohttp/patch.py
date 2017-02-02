7fc977abd4b08003f1e00d66ca0a4e475487140d
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-02-02 14:23:44 +0100

    [pin] prevent from moving down the tracer reference for patch_all and patch methods

--- ddtrace/contrib/aiohttp/patch.py
+++ ddtrace/contrib/aiohttp/patch.py
@@ -1,14 +1,13 @@
 import wrapt
 import aiohttp_jinja2
 
+from ddtrace.contrib import asyncio
+
 from .template import _trace_template_rendering
 from ...pin import Pin
 
-# TODO: the tracer should be found in a different way
-from ddtrace.contrib.asyncio import tracer
 
-
-def patch():
+def patch(tracer=None):
     """
     Patch aiohttp third party modules
     """
@@ -16,6 +15,10 @@ def patch():
         return
     setattr(aiohttp_jinja2, '__datadog_patch', True)
 
+    # expect a tracer or use the asyncio default one
+    tracer = tracer or asyncio.tracer
+
+    # wrap the template engine and create the PIN object on the module
     _w = wrapt.wrap_function_wrapper
     _w('aiohttp_jinja2', 'render_template', _trace_template_rendering)
     Pin(app='aiohttp', service=None, app_type='web', tracer=tracer).onto(aiohttp_jinja2)
