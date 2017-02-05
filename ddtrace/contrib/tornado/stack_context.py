fb9b97d829cc22b386d355908e46988c985a2e86
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-02-05 19:16:53 +0100

    [docs] add some docstrings for Tornado; minor fixes for asyncio and aiohttp docs

--- ddtrace/contrib/tornado/stack_context.py
+++ ddtrace/contrib/tornado/stack_context.py
@@ -1,5 +1,4 @@
 import threading
-import tornado.stack_context
 
 from ...context import Context
 
@@ -7,11 +6,13 @@ from ...context import Context
 class ContextManager(object):
     """
     A context manager that manages Context instances in thread-local state.
-    Intended for use with the ddtrace StackContext and not alone because
-    it doesn't work in asynchronous environments.
+    It must be used with the Tornado ``StackContext`` and not alone, because
+    it doesn't work in asynchronous environments. To use it within a
+    ``StackContext``, simply::
 
-    TODO: the current implementation sets the active context in the ContextManager
-    class. This will not work without the StackContext.
+        with StackContext(lambda: ContextManager()):
+            ctx = ContextManager.current_context()
+            # use your context here
     """
 
     _state = threading.local()
@@ -20,7 +21,10 @@ class ContextManager(object):
     @classmethod
     def current_context(cls):
         """
-        Get the Context from the current execution unit.
+        Get the ``Context`` from the current execution flow. This method can be
+        used inside Tornado coroutines to retrieve and use the current context.
+        At the moment, the method cannot handle ``Context`` switching when
+        delayed callbacks are used.
         """
         return getattr(cls._state, 'context', None)
 
@@ -28,11 +32,17 @@ class ContextManager(object):
         self._context = Context()
 
     def __enter__(self):
+        """
+        Enable a new ``Context`` instance.
+        """
         self._prev_context = self.__class__.current_context()
         self.__class__._state.context = self._context
         return self._context
 
     def __exit__(self, *_):
+        """
+        Disable the current ``Context`` instance and activate the previous one.
+        """
         self.__class__._state.context = self._prev_context
         self._prev_context = None
         return False
