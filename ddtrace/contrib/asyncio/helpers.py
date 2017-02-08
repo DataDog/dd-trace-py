9a0ab96a26c27edf80ca580fc5dc42a275486c6e
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-02-08 18:37:43 +0100

    [asyncio] implement the run_in_executor helper for Context passing

--- ddtrace/contrib/asyncio/helpers.py
+++ ddtrace/contrib/asyncio/helpers.py
@@ -5,6 +5,8 @@ Context and Spans in instrumented ``asyncio`` code.
 """
 import asyncio
 
+from ddtrace.context import Context
+
 # TODO: we don't want to do this; it will be
 # from ddtrace import tracer
 from ddtrace.contrib.asyncio import tracer
@@ -23,17 +25,51 @@ def ensure_future(coro_or_future, *, loop=None):
     return task
 
 
-def run_in_executor():
+def run_in_executor(executor, func, *args, loop=None):
     """
-    This wrapper must be implemented.
-    The idea is that when you run synchronous code in a separated
-    executor, a copy of the context will be available in the new Thread.
-    After the thread has been executed, the Context can be merged back
-    if it has been used.
+    Wrapper for the loop.run_in_executor() function that
+    sets a context to the newly created Thread. If the current
+    task has a Context, it will be attached as an empty Context
+    with the current_span activated to inherit the ``trace_id``
+    and the ``parent_id``.
 
-    TODO: we're not providing this API at the moment and run_in_executor
-    will not work with the current asyncio tracing API. The implementation
-    is in the roadmap after frameworks instrumentation.
-    Probably this requires that Tracer is merged with AsyncTracer.
+    Because the separated thread does synchronous execution, the
+    ``AsyncioTracer`` fallbacks to the thread-local ``Context``
+    handler.
     """
-    pass
+    try:
+        loop = loop or asyncio.get_event_loop()
+    except RuntimeError:
+        # this exception means that the run_in_executor is run in the
+        # wrong loop; this should happen only in wrong call usage
+        # TODO: here we can't do something better; it's the same as
+        # calling:
+        #   loop = None
+        #   loop.run_in_executor(...)
+        raise
+
+    # because the Executor can run the Thread immediately or after the
+    # coroutine is executed, we may have two different scenarios:
+    # * the Context is copied in the new Thread and the trace is sent twice
+    # * the coroutine flushes the Context and when the Thread copies the
+    #   Context it is already empty (so it will be a root Span)
+    # because of that we create a new Context that knows only what was
+    # the latest active Span when the executor has been launched
+    ctx = Context()
+    current_ctx = tracer.get_call_context()
+    ctx._current_span = current_ctx._current_span
+
+    future = loop.run_in_executor(executor, _wrap_executor, func, args, ctx)
+    return future
+
+
+def _wrap_executor(fn, args, ctx):
+    """
+    This function is executed in the newly created Thread so the right
+    ``Context`` can be set in the thread-local storage. This operation
+    is safe because the ``Context`` class is thread-safe and can be
+    updated concurrently.
+    """
+    # set the given Context in the thread-local storage
+    tracer._context.set(ctx)
+    return fn(*args)
