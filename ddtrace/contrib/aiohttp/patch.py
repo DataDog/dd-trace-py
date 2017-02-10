4d86d0e824268760ae02c40455121826238d87c0
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-02-10 10:10:01 +0100

    [aiohttp] add unpatch() for aiohttp_jinja2 wrapper

--- ddtrace/contrib/aiohttp/patch.py
+++ ddtrace/contrib/aiohttp/patch.py
@@ -22,3 +22,15 @@ def patch(tracer=None):
     _w = wrapt.wrap_function_wrapper
     _w('aiohttp_jinja2', 'render_template', _trace_template_rendering)
     Pin(app='aiohttp', service=None, app_type='web', tracer=tracer).onto(aiohttp_jinja2)
+
+
+def unpatch():
+    if getattr(aiohttp_jinja2, '__datadog_patch', False):
+        setattr(aiohttp_jinja2, '__datadog_patch', False)
+        _unwrap(aiohttp_jinja2, 'render_template')
+
+
+def _unwrap(obj, attr):
+    f = getattr(obj, attr, None)
+    if f and isinstance(f, wrapt.ObjectProxy) and hasattr(f, '__wrapped__'):
+        setattr(obj, attr, f.__wrapped__)
