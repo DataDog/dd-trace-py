453dd791fb5acfaf0bced3184d8ab85aef99957c
talwai <aaditya.21@gmail.com>
2017-02-16 11:51:34 -0500

    tests: test django autopatching

---
+++ tests/contrib/django/test_autopatching.py
@@ -0,0 +1,13 @@
+from ddtrace.monkey import patch
+from .utils import DjangoTraceTestCase
+
+class DjangoAutopatchTest(DjangoTraceTestCase):
+    def test_autopatching(self):
+        patch(django=True)
+
+        import django
+        ok_(django._datadog_patch)
+
+        from django.conf import settings
+        ok_('ddtrace.contrib.django' in settings.INSTALLED_APS)
+        eq_(settings.MIDDLEWARE_CLASSES[0], 'ddtrace.contrib.django.TraceMiddleware'))
