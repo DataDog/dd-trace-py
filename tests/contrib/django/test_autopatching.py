75fc387e1c0c67463dc76ea5495c5d99166de3a8
talwai <aaditya.21@gmail.com>
2017-02-16 13:49:14 -0500

    django: fix import

--- tests/contrib/django/test_autopatching.py
+++ tests/contrib/django/test_autopatching.py
@@ -1,5 +1,6 @@
 from ddtrace.monkey import patch
 from .utils import DjangoTraceTestCase
+from nose.tools import eq_, ok_
 
 class DjangoAutopatchTest(DjangoTraceTestCase):
     def test_autopatching(self):
