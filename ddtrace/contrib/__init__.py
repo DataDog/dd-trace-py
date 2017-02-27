5917c70db13984c3272a649532e9f09beb966e65
Sabin Iacob <iacobs@m0n5t3r.info>
2017-02-27 16:06:19 +0200

    make func_name account for Django class-based views

--- ddtrace/contrib/__init__.py
+++ ddtrace/contrib/__init__.py
@@ -1,9 +1,7 @@
 
 def func_name(f):
     """ Return a human readable version of the function's name. """
-    if hasattr(f, '__name__'):
-        return "%s.%s" % (f.__module__, f.__name__)
-    return "%s.%s" % (f.__module__, f.__class__.__name__)
+    return "%s.%s" % (f.__module__, getattr(f, '__name__', f.__class__.__name__))
 
 def module_name(instance):
     return instance.__class__.__module__.split('.')[0]
