3e5fa76b9ea4c21332492609b142684fa989eb1d
Raphael Gavache <raphael.gavache@student.ecp.fr>
2017-02-28 18:22:14 -0500

    First commit, botocore contrib (tests working on local machine)

---
+++ ddtrace/contrib/botocore/patch.py
@@ -0,0 +1,59 @@
+"""
+Trace queries to aws api done via botocore client
+"""
+
+# project
+import ddtrace
+
+# 3p
+import wrapt
+import botocore.client
+
+from ...ext import AppTypes
+from ...ext import http
+
+# Original botocore client class
+_Botocore_client = botocore.client.BaseClient
+
+DEFAULT_SERVICE = "aws"
+SPAN_TYPE = "botocore"
+
+
+def patch():
+    wrapt.wrap_function_wrapper('botocore.client', 'BaseClient._make_api_call', patched_api_call)
+
+
+def patched_api_call(original_func, instance, args, kwargs):
+
+    tracer = getattr(instance, 'datadog_tracer', ddtrace.tracer)
+    tracer.set_service_info(service=DEFAULT_SERVICE, app=SPAN_TYPE, app_type=AppTypes.web)
+
+    if not tracer.enabled:
+        return original_func(*args, **kwargs)
+
+    with tracer.trace("aws.api.request") as span:
+        _endpoint = getattr(instance, "_endpoint", None)
+        endpoint_name = getattr(_endpoint, "_endpoint_prefix", "unknown")
+        _boto_meta = getattr(instance, "meta", None)
+        region_name = getattr(_boto_meta, "region_name", "unknown")
+        meta = {
+            'aws.agent': 'botocore',
+            'aws.operation': args,
+            'aws.endpoint': endpoint_name,
+            'aws.region': region_name,
+        }
+        meta = add_cleaned_api_params(meta, args, kwargs)
+        span.resource = '%s.%s.%s' % (args, endpoint_name, region_name)
+        span.set_tags(meta)
+
+        result = original_func(*args, **kwargs)
+        span.set_tag(http.STATUS_CODE, getattr(result, 'HTTPStatusCode', 500))
+        return result
+
+
+def add_cleaned_api_params(meta, operation_name, api_params):
+    """ Avoid sending sensitive information
+    """
+    if operation_name != 'AssumeRole':
+        meta['aws.api_params'] = str(api_params)
+    return meta
---
+++ tests/contrib/botocore/test.py
@@ -0,0 +1,60 @@
+
+# stdlib
+import unittest
+
+# 3p
+from nose.tools import eq_
+import botocore.session
+
+
+# project
+from ddtrace.contrib.botocore.patch import patch
+from ddtrace.ext import http
+
+# testing
+from ...test_tracer import get_dummy_tracer
+
+
+class BotocoreTest(unittest.TestCase):
+    """Botocore integration testsuite"""
+
+    def setUp(self):
+        patch()
+        self.session = botocore.session.get_session()
+
+    def test_traced_client(self):
+
+        client = self.session.create_client('ec2', region_name='us-west-2')
+        client.datadog_tracer = get_dummy_tracer()
+        writer = client.datadog_tracer.writer
+
+        # Trying describe instances command
+        client.describe_instances()
+
+        spans = writer.pop()
+        assert spans
+        span = spans[0]
+        eq_(span.get_tag('aws.agent'), "botocore")
+        eq_(span.get_tag('aws.region'), 'us-west-2')
+        eq_(span.get_tag('aws.endpoint'), 'ec2')
+        #eq_(span.get_tag(http.STATUS_CODE), 200)
+
+
+        # ec2 = session.create_client('ec2', region_name='us-west-2')
+        # eq_(len(spans), 1)
+
+        # s3 tracing test
+        # s3 = session.create_client('s3', region_name='us-west-2')
+
+    def test_s3_client(self):
+        client = self.session.create_client('s3', region_name='us-west-2')
+        client.datadog_tracer = get_dummy_tracer()
+        writer = client.datadog_tracer.writer
+
+        # Listing buckets
+        result = client.list_buckets()
+
+        spans = writer.pop()
+        assert spans
+        span = spans[0]
+        eq_(span.get_tag('aws.endpoint'), "s3")
