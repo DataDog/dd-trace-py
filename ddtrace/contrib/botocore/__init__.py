b91d6dafdf958f452a94fb290f45a8be4c096ff8
Raphael Gavache <raphael.gavache@student.ecp.fr>
2017-03-01 13:38:08 -0500

    should pass contrib test

--- ddtrace/contrib/botocore/__init__.py
+++ ddtrace/contrib/botocore/__init__.py
@@ -4,5 +4,5 @@ required_modules = ['botocore.client']
 
 with require_modules(required_modules) as missing_modules:
     if not missing_modules:
-        from .botopatch import patch  # noqa
+        from .patch import patch  # noqa
         __all__ = ['patch']
