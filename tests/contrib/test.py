90b2b7a85c3ef52a3506883e3bc3c3b04de80094
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2017-03-01 10:50:02 +0100

    [contrib] added tests for func_name when called on static or class methods

--- tests/contrib/test.py
+++ tests/contrib/test.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-import unittest
-
 from nose.tools import eq_
 
 from ddtrace.contrib import func_name
@@ -10,9 +8,22 @@ class SomethingCallable(object):
     """
     A dummy class that implements __call__().
     """
+
+    value = 42
+
     def __call__(self):
         return "something"
 
+    def me(self):
+        return self
+
+    @staticmethod
+    def add(a,b):
+        return a + b
+
+    @classmethod
+    def answer(cls):
+        return cls.value
 
 def SomeFunc():
     """
@@ -32,6 +43,14 @@ class TestContrib(object):
         """
         eq_("nothing", SomeFunc())
         eq_("tests.contrib.test.SomeFunc", func_name(SomeFunc))
+
         f = SomethingCallable()
         eq_("something", f())
         eq_("tests.contrib.test.SomethingCallable", func_name(f))
+
+        eq_(f, f.me())
+        eq_("tests.contrib.test.me", func_name(f.me))
+        eq_(3, f.add(1,2))
+        eq_("tests.contrib.test.add", func_name(f.add))
+        eq_(42, f.answer())
+        eq_("tests.contrib.test.answer", func_name(f.answer))
