f243c680096c51d4b0f10fa368a68c12ede69e54
Raphael Gavache <raphael.gavache@student.ecp.fr>
2017-03-01 13:28:17 -0500

    fixing contrib patching issue

--- ddtrace/contrib/botocore/__init__.py
+++ ddtrace/contrib/botocore/__init__.py
@@ -4,5 +4,5 @@ required_modules = ['botocore.client']
 
 with require_modules(required_modules) as missing_modules:
     if not missing_modules:
-        from .client import get_traced_botocore_client  # noqa
-        __all__ = ['get_traced_botocore_client']
+        from .botopatch import patch  # noqa
+        __all__ = ['patch']
