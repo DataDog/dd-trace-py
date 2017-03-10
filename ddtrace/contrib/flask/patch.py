709b0bdaef2d2a9b9999cb95dc622e772ecf1e75
talwai <aaditya.21@gmail.com>
2017-03-10 13:08:33 -0500

    wip

--- ddtrace/contrib/flask/patch.py
+++ ddtrace/contrib/flask/patch.py
@@ -14,6 +14,8 @@ def patch():
 
     setattr(flask, '_datadog_patch', True)
     setattr(flask, 'Flask', TracedFlask)
+    # _w = wrapt.wrap_function_wrapper
+    # _w('flask.Flask', 'full_dispatch_request', traced_full_dispatch_request)
 
 
 class TracedFlask(flask.Flask):
@@ -22,4 +24,43 @@ class TracedFlask(flask.Flask):
         super(TracedFlask, self).__init__(*args, **kwargs)
         service = os.environ.get("DATADOG_SERVICE_NAME") or "flask"
 
-        TraceMiddleware(self, tracer, service=service)
+        traced_app = TraceMiddleware(self, tracer, service=service)
+
+        from flask import signals
+
+        # traced_app.app has signals
+        assert len(list(signals.request_started.receivers_for(traced_app.app))) > 0
+
+        assert traced_app.app is self
+
+        assert len(list(signals.request_started.receivers_for(self))) > 0
+        # Signals are registered here
+
+
+# def traced_full_dispatch_request(wrapped, instance, args, kwargs):
+#     instance.try_trigger_before_first_request_functions()
+#     try:
+#         request_started.send(self)
+#         rv = instance.preprocess_request()
+#         if rv is None:
+#             rv = instance.dispatch_request()
+#     except Exception as e:
+#         rv = instance.handle_user_exception(e)
+#     return instance.finalize_request(rv)
+#
+#
+# def full_dispatch_request(self):
+#     """Dispatches the request and on top of that performs request
+#     pre and postprocessing as well as HTTP exception catching and
+#     error handling.
+#     .. versionadded:: 0.7
+#     """
+#     self.try_trigger_before_first_request_functions()
+#     try:
+#         request_started.send(self)
+#         rv = self.preprocess_request()
+#         if rv is None:
+#             rv = self.dispatch_request()
+#     except Exception as e:
+#         rv = self.handle_user_exception(e)
+#     return self.finalize_request(rv)
