7148840b169f5e7950792e8ac2524df29b15808b
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-05-07 16:06:46 +0200

    [sqlalchemy] convert SQLite tests into a SQLiteTestCase with different test cases

--- tests/contrib/sqlalchemy/test.py
+++ tests/contrib/sqlalchemy/test.py
@@ -4,9 +4,11 @@ import contextlib
 
 # 3rd party
 import psycopg2
-from nose.tools import eq_
+from unittest import TestCase
+from nose.tools import eq_, ok_, assert_raises
 from nose.plugins.attrib import attr
 
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (
@@ -25,23 +27,154 @@ from ddtrace.ext import net as netx
 
 # testing
 from ..config import POSTGRES_CONFIG
-from ...test_tracer import DummyWriter
+from ...test_tracer import get_dummy_tracer, DummyWriter
 
 
 Base = declarative_base()
 
 
 class Player(Base):
+    """Player entity used to test SQLAlchemy ORM"""
     __tablename__ = 'players'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
 
-def test_sqlite():
-    engine_args = {'url': 'sqlite:///:memory:'}
-    meta = {sqlx.DB: ":memory:"}
-    _test_create_engine(engine_args, "sqlite-foo", "sqlite", meta)
+class SQLiteTestCase(TestCase):
+    """Testing SQLite engine"""
+
+    def create_engine(self, engine_args, meta):
+        # create a SQLAlchemy engine
+        url = engine_args.pop('url')
+        return create_engine(url, **engine_args)
+
+    @contextlib.contextmanager
+    def connection(self):
+        # context manager that provides a connection
+        # to the underlying database
+        try:
+            conn = self.engine.connect()
+            yield conn
+        finally:
+            conn.close()
+
+    def setUp(self):
+        # TODO: move these at class level?
+        self.vendor = 'sqlite'
+        self.meta = {sqlx.DB: ':memory:'}
+        self.engine_args = {'url': 'sqlite:///:memory:'}
+        # create an engine
+        self.engine = self.create_engine(self.engine_args, self.meta)
+
+        # create the database / entities and prepare a session for the test
+        Base.metadata.create_all(self.engine)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+
+        # trace the engine
+        self.tracer = get_dummy_tracer()
+        trace_engine(self.engine, self.tracer, service='sqlite-foo')
+
+    def tearDown(self):
+        # clear the database and dispose the engine
+        Base.metadata.drop_all(bind=self.engine)
+        self.engine.dispose()
+
+    def test_orm_insert(self):
+        # ensures that the ORM session is traced
+        wayne = Player(id=1, name='wayne')
+        self.session.add(wayne)
+        self.session.commit()
+
+        traces = self.tracer.writer.pop_traces()
+        # trace composition
+        eq_(len(traces), 1)
+        eq_(len(traces[0]), 1)
+        span = traces[0][0]
+        # span fields
+        eq_(span.name, 'sqlite.query')
+        eq_(span.service, 'sqlite-foo')
+        eq_(span.resource, 'INSERT INTO players (id, name) VALUES (?, ?)')
+        eq_(span.get_tag('sql.db'), ':memory:')
+        eq_(span.get_tag('sql.rows'), '1')
+        eq_(span.span_type, 'sql')
+        eq_(span.error, 0)
+        ok_(span.duration > 0)
+
+    def test_session_query(self):
+        # ensures that the Session queries are traced
+        out = list(self.session.query(Player).filter_by(name='wayne'))
+        eq_(len(out), 0)
+
+        traces = self.tracer.writer.pop_traces()
+        # trace composition
+        eq_(len(traces), 1)
+        eq_(len(traces[0]), 1)
+        span = traces[0][0]
+        # span fields
+        eq_(span.name, 'sqlite.query')
+        eq_(span.service, 'sqlite-foo')
+        eq_(span.resource, 'SELECT players.id AS players_id, players.name AS players_name \nFROM players \nWHERE players.name = ?')
+        eq_(span.get_tag('sql.db'), ':memory:')
+        ok_(span.get_tag('sql.rows') is None)
+        eq_(span.span_type, 'sql')
+        eq_(span.error, 0)
+        ok_(span.duration > 0)
+
+    def test_engine_connect_execute(self):
+        # ensures that engine.connect() is properly traced
+        with self.connection() as conn:
+            rows = conn.execute('SELECT * FROM players').fetchall()
+            eq_(len(rows), 0)
+
+        traces = self.tracer.writer.pop_traces()
+        # trace composition
+        eq_(len(traces), 1)
+        eq_(len(traces[0]), 1)
+        span = traces[0][0]
+        # span fields
+        eq_(span.name, 'sqlite.query')
+        eq_(span.service, 'sqlite-foo')
+        eq_(span.resource, 'SELECT * FROM players')
+        eq_(span.get_tag('sql.db'), ':memory:')
+        ok_(span.get_tag('sql.rows') is None)
+        eq_(span.span_type, 'sql')
+        eq_(span.error, 0)
+        ok_(span.duration > 0)
+
+    def test_engine_execute_errors(self):
+        # ensures that SQL errors are reported
+        with assert_raises(OperationalError) as ex:
+            with self.connection() as conn:
+                conn.execute('SELECT * FROM a_wrong_table').fetchall()
+
+        traces = self.tracer.writer.pop_traces()
+        # trace composition
+        eq_(len(traces), 1)
+        eq_(len(traces[0]), 1)
+        span = traces[0][0]
+        # span fields
+        eq_(span.name, 'sqlite.query')
+        eq_(span.service, 'sqlite-foo')
+        eq_(span.resource, 'SELECT * FROM a_wrong_table')
+        eq_(span.get_tag('sql.db'), ':memory:')
+        ok_(span.get_tag('sql.rows') is None)
+        eq_(span.span_type, 'sql')
+        ok_(span.duration > 0)
+        # check the error
+        eq_(span.error, 1)
+        eq_(span.get_tag('error.msg'), 'no such table: a_wrong_table')
+        ok_('OperationalError' in span.get_tag('error.type'))
+        ok_('OperationalError: no such table: a_wrong_table' in span.get_tag('error.stack'))
+
+    def test_traced_service(self):
+        # ensures that the service is set as expected
+        services = self.tracer.writer.pop_services()
+        expected = {
+            'sqlite-foo': {'app': self.vendor, 'app_type': 'db'}
+        }
+        eq_(services, expected)
 
 
 @attr('postgres')
