ca5d4a65e7f970f8aca4f33ccd4e558bec2fac02
Michal Kuffa <michal@bynder.com>
2017-06-11 17:26:07 +0000

    Add few words on usage to __init__ doc

--- ddtrace/contrib/graphql/__init__.py
+++ ddtrace/contrib/graphql/__init__.py
@@ -1,4 +1,18 @@
 """
+To trace all GraphQL requests, patch the library like so::
+
+    from ddtrace.contrib.graphql import patch
+    patch()
+
+    from graphql import graphql
+    result = graphql(schema, query)
+
+
+If you do not want to monkeypatch ``graphql.graphql`` function or want to trace
+only certain calls you can use the ``traced_graphql`` function::
+
+    from ddtrace.contrib.graphql import traced_graphql
+    traced_graphql(schema, query)
 """
 
 
