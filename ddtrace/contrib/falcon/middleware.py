d91b22a29ff51543c6d296b0fa54e26d1cb060dc
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-06-23 02:15:38 +0200

    [falcon] extended support for Falcon 1.2; improve error handling

--- ddtrace/contrib/falcon/middleware.py
+++ ddtrace/contrib/falcon/middleware.py
@@ -1,4 +1,6 @@
-from ddtrace.ext import http as httpx, errors as errx
+import sys
+
+from ddtrace.ext import http as httpx
 
 
 class TraceMiddleware(object):
@@ -23,7 +25,9 @@ class TraceMiddleware(object):
             return  # unexpected
         span.resource = "%s %s" % (req.method, _name(resource))
 
-    def process_response(self, req, resp, resource):
+    def process_response(self, req, resp, resource, req_succeeded=None):
+        # req_succeded is not a kwarg in the API, but we need that to support
+        # Falcon 1.0 that doesn't provide this argument
         span = self.tracer.current_span()
         if not span:
             return  # unexpected
@@ -34,20 +38,42 @@ class TraceMiddleware(object):
         # to proper status codes, so we we have to try to infer them
         # here. See https://github.com/falconry/falcon/issues/606
         if resource is None:
-            span.resource = "%s 404" % req.method
             status = '404'
+            span.resource = "%s 404" % req.method
+            span.set_tag(httpx.STATUS_CODE, status)
+            span.finish()
+            return
 
-        # If we have an active unhandled error, treat it as a 500
-        span.set_traceback()
-        err_msg = span.get_tag(errx.ERROR_MSG)
-        if err_msg and not _is_404(err_msg):
-            status = '500'
+        err_type = sys.exc_info()[0]
+        if err_type is not None:
+            if req_succeeded is None:
+                # backward-compatibility with Falcon 1.0; any version
+                # greater than 1.0 has req_succeded in [True, False]
+                # TODO[manu]: drop the support at some point
+                status = _detect_and_set_status_error(err_type, span)
+            elif req_succeeded is False:
+                # Falcon 1.1+ provides that argument that is set to False
+                # if get an Exception (404 is still an exception)
+                status = _detect_and_set_status_error(err_type, span)
 
         span.set_tag(httpx.STATUS_CODE, status)
         span.finish()
 
-def _is_404(err_msg):
-    return 'HTTPNotFound' in err_msg
+
+def _is_404(err_type):
+    return 'HTTPNotFound' in err_type.__name__
+
+
+def _detect_and_set_status_error(err_type, span):
+    """Detect the HTTP status code from the current stacktrace and
+    set the traceback to the given Span
+    """
+    if not _is_404(err_type):
+        span.set_traceback()
+        return '500'
+    elif _is_404(err_type):
+        return '404'
+
 
 def _name(r):
     return "%s.%s" % (r.__module__, r.__class__.__name__)
--- tests/contrib/falcon/test.py
+++ tests/contrib/falcon/test.py
@@ -8,13 +8,13 @@ from wsgiref import simple_server
 # 3p
 import falcon
 import falcon.testing
-from nose.tools import eq_
+from nose.tools import eq_, ok_
 from nose.plugins.attrib import attr
 
 # project
 from ddtrace import Tracer
 from ddtrace.contrib.falcon import TraceMiddleware
-from ddtrace.ext import http as httpx
+from ddtrace.ext import errors as errx, http as httpx
 from tests.test_tracer import DummyWriter
 
 
@@ -28,11 +28,20 @@ class Resource200(object):
         # throw a handled exception here to ensure our use of
         # set_traceback doesn't affect 200s
         try:
-            1/0
+            1 / 0
         except Exception:
             pass
 
         resp.status = falcon.HTTP_200
+        resp.body = self.BODY
+
+
+class Resource201(object):
+    BODY = "Added"
+    ROUTE = "/201"
+
+    def on_post(self, req, resp, **kwargs):
+        resp.status = falcon.HTTP_201
         resp.body = self.BODY
 
 
@@ -54,6 +63,15 @@ class ResourceExc(object):
         raise Exception("argh")
 
 
+class ResourceNotFound(object):
+    ROUTE = "/not_found"
+
+    def on_get(self, req, resp, **kwargs):
+        # simulate that the endpoint is hit but raise a 404 because
+        # the object isn't found in the database
+        raise falcon.HTTPNotFound()
+
+
 class TestMiddleware(falcon.testing.TestCase):
 
     def setUp(self):
@@ -66,8 +84,10 @@ class TestMiddleware(falcon.testing.TestCase):
 
         resources = [
             Resource200,
+            Resource201,
             Resource500,
             ResourceExc,
+            ResourceNotFound,
         ]
         for r in resources:
             self.api.add_route(r.ROUTE, r())
@@ -84,7 +104,6 @@ class TestMiddleware(falcon.testing.TestCase):
         eq_(span.resource, "GET 404")
         eq_(span.get_tag(httpx.STATUS_CODE), '404')
         eq_(span.name, "falcon.request")
-
 
     def test_exception(self):
         try:
@@ -115,6 +134,19 @@ class TestMiddleware(falcon.testing.TestCase):
         eq_(span.get_tag(httpx.STATUS_CODE), '200')
         eq_(span.name, "falcon.request")
 
+    def test_201(self):
+        out = self.simulate_post(Resource201.ROUTE)
+        eq_(out.status_code, 201)
+        eq_(out.content.decode('utf-8'), Resource201.BODY)
+
+        spans = self._writer.pop()
+        eq_(len(spans), 1)
+        span = spans[0]
+        eq_(span.service, self._service)
+        eq_(span.resource, "POST tests.contrib.falcon.test.Resource201")
+        eq_(span.get_tag(httpx.STATUS_CODE), "201")
+        eq_(span.name, "falcon.request")
+
     def test_500(self):
         out = self.simulate_get(Resource500.ROUTE)
         eq_(out.status_code, 500)
@@ -127,6 +159,29 @@ class TestMiddleware(falcon.testing.TestCase):
         eq_(span.resource, "GET tests.contrib.falcon.test.Resource500")
         eq_(span.get_tag(httpx.STATUS_CODE), '500')
         eq_(span.name, "falcon.request")
+
+    def test_404_exception(self):
+        out = self.simulate_get(ResourceNotFound.ROUTE)
+        eq_(out.status_code, 404)
+
+        spans = self._writer.pop()
+        eq_(len(spans), 1)
+        span = spans[0]
+        eq_(span.service, self._service)
+        eq_(span.resource, "GET tests.contrib.falcon.test.ResourceNotFound")
+        eq_(span.get_tag(httpx.STATUS_CODE), "404")
+        eq_(span.name, "falcon.request")
+
+    def test_404_exception_no_stacktracer(self):
+        # it should not have the stacktrace when a 404 exception is raised
+        out = self.simulate_get(ResourceNotFound.ROUTE)
+        eq_(out.status_code, 404)
+
+        spans = self._writer.pop()
+        eq_(len(spans), 1)
+        span = spans[0]
+        eq_(span.get_tag(httpx.STATUS_CODE), "404")
+        ok_(span.get_tag(errx.ERROR_TYPE) is None)
 
 
 if __name__ == '__main__':
@@ -144,5 +199,5 @@ if __name__ == '__main__':
     port = 8000
     httpd = simple_server.make_server('127.0.0.1', port, app)
     routes = [r.ROUTE for r in resources]
-    print('running test app on %s. routes: %s'  % (port, ' '.join(routes)))
+    print('running test app on %s. routes: %s' % (port, ' '.join(routes)))
     httpd.serve_forever()
