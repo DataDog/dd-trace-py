50e716a7d24e1af0f47471c09d5b61f6468cb8e2
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-09-01 16:26:18 +0200

    [gevent] honors the Context Provider public API

--- ddtrace/contrib/gevent/provider.py
+++ ddtrace/contrib/gevent/provider.py
@@ -15,12 +15,19 @@ class GeventContextProvider(BaseContextProvider):
     in the ``gevent`` library. Framework instrumentation that uses the
     gevent WSGI server (or gevent in general), can use this provider.
     """
-    def __call__(self):
+    def activate(self, context):
+        """Sets the scoped ``Context`` for the current running ``Greenlet``.
+        """
+        current_g = gevent.getcurrent()
+        if current_g is not None:
+            setattr(current_g, CONTEXT_ATTR, context)
+            return context
+
+    def active(self):
         """
         Returns the scoped ``Context`` for this execution flow. The ``Context``
         uses the ``Greenlet`` class as a carrier, and everytime a greenlet
-        is created it receives the "parent" context. The main greenlet
-        will never have an attached ``Context``.
+        is created it receives the "parent" context.
         """
         current_g = gevent.getcurrent()
         ctx = getattr(current_g, CONTEXT_ATTR, None)
@@ -29,9 +36,14 @@ class GeventContextProvider(BaseContextProvider):
             return ctx
 
         # the Greenlet doesn't have a Context so it's created and attached
-        # unless it's the main greenlet; in that case we must be sure
-        # that no Context is generated
-        if current_g.parent:
+        # TODO: previous implementation avoided to add a Context to the main
+        # greenlet because it could have side-effects when switching back
+        # and forth between different executions. This results in issues such
+        # as: https://github.com/DataDog/dd-trace-py/issues/309
+        # and is required for Distributed Tracing when providing a new arbitrary
+        # Context. On the other hand, it's imperative to double check if there
+        # are side effects.
+        if current_g:
             ctx = Context()
             setattr(current_g, CONTEXT_ATTR, ctx)
             return ctx
