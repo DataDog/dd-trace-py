9f9b269df798b86bd6420f9009906ecdd1179130
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2017-09-27 18:56:50 +0200

    [distributed sampling] generalized downgrade mecanism

--- ddtrace/api.py
+++ ddtrace/api.py
@@ -12,6 +12,19 @@ log = logging.getLogger(__name__)
 
 TRACE_COUNT_HEADER = 'X-Datadog-Trace-Count'
 
+_VERSIONS = {'v0.4': {'traces': '/v0.4/traces',
+                      'services': '/v0.4/services',
+                      'compatibility_mode': False,
+                      'fallback': 'v0.3'},
+             'v0.3': {'traces': '/v0.3/traces',
+                     'services': '/v0.3/services',
+                      'compatibility_mode': False,
+                      'fallback': 'v0.2'},
+             'v0.2': {'traces': '/v0.2/traces',
+                     'services': '/v0.2/services',
+                      'compatibility_mode': True,
+                      'fallback': None}}
+
 class API(object):
     """
     Send data to the trace agent using the HTTP protocol and JSON format
@@ -19,24 +32,38 @@ class API(object):
     def __init__(self, hostname, port, headers=None, encoder=None, priority_sampling=False):
         self.hostname = hostname
         self.port = port
-        if priority_sampling:
-            self._traces = '/v0.4/traces'
-            self._services = '/v0.4/services'
-        else:
-            self._traces = '/v0.3/traces'
-            self._services = '/v0.3/services'
-        self._compatibility_mode = False
-        self._encoder = encoder or get_encoder()
 
-        # overwrite the Content-type with the one chosen in the Encoder
         self._headers = headers or {}
+        self._version = None
+
+        if priority_sampling:
+            self._set_version('v0.4', encoder=encoder)
+        else:
+            self._set_version('v0.3', encoder=encoder)
+
         self._headers.update({
-            'Content-Type': self._encoder.content_type,
             'Datadog-Meta-Lang': 'python',
             'Datadog-Meta-Lang-Version': PYTHON_VERSION,
             'Datadog-Meta-Lang-Interpreter': PYTHON_INTERPRETER,
             'Datadog-Meta-Tracer-Version': ddtrace.__version__,
         })
+
+    def _set_version(self, version, encoder=None):
+        if version not in _VERSIONS:
+            version = 'v0.2'
+        if version == self._version:
+            return
+        self._version = version
+        self._traces = _VERSIONS[version]['traces']
+        self._services = _VERSIONS[version]['services']
+        self._fallback = _VERSIONS[version]['fallback']
+        self._compatibility_mode = _VERSIONS[version]['compatibility_mode']
+        if self._compatibility_mode:
+            self._encoder = JSONEncoder()
+        else:
+            self._encoder = encoder or get_encoder()
+        # overwrite the Content-type with the one chosen in the Encoder
+        self._headers.update({'Content-Type': self._encoder.content_type})
 
     def _downgrade(self):
         """
@@ -45,11 +72,7 @@ class API(object):
         ensures that the compatibility mode is activated so that the downgrade will be
         executed only once.
         """
-        self._compatibility_mode = True
-        self._traces = '/v0.2/traces'
-        self._services = '/v0.2/services'
-        self._encoder = JSONEncoder()
-        self._headers.update({'Content-Type': self._encoder.content_type})
+        self._set_version(self._fallback)
 
     def send_traces(self, traces):
         if not traces:
