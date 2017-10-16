7edaaf2f25a1582944d889c78bfee8b8c3631c91
Bertrand Mermet <bertrand.mermet@datadoghq.com>
2017-10-16 15:14:37 +0200

    Renamed tween_list to tweens since it's not a python list

--- ddtrace/contrib/pyramid/patch.py
+++ ddtrace/contrib/pyramid/patch.py
@@ -45,16 +45,16 @@ def traced_init(wrapped, instance, args, kwargs):
 def insert_tween_if_needed(settings):
     if 'pyramid.tweens' not in settings:
         return
-    tweens_list = settings['pyramid.tweens']
+    tweens = settings['pyramid.tweens']
     # If the list is empty, pyramid does not consider the tweens have been
     # set explicitly so we will insert the tween with add_tween.
-    if not tweens_list.strip():
+    if not tweens.strip():
         return
     # If the our tween is already there, nothing to do
-    if DD_TWEEN_NAME in tweens_list:
+    if DD_TWEEN_NAME in tweens:
         return
-    idx = tweens_list.find(pyramid.tweens.EXCVIEW)
+    idx = tweens.find(pyramid.tweens.EXCVIEW)
     if idx is -1:
-        settings['pyramid.tweens'] = tweens_list + '\n' + DD_TWEEN_NAME
+        settings['pyramid.tweens'] = tweens + '\n' + DD_TWEEN_NAME
     else:
-        settings['pyramid.tweens'] = tweens_list[:idx] + DD_TWEEN_NAME + "\n" + tweens_list[idx:]
+        settings['pyramid.tweens'] = tweens[:idx] + DD_TWEEN_NAME + "\n" + tweens[idx:]
