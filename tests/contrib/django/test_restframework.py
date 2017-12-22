03c3901d9b3769f5d953c15b549f2783df9882a4
Gabin Marignier <gabin.contact@gmail.com>
2017-12-22 11:33:34 +0100

    Add new unit test

--- tests/contrib/django/test_restframework.py
+++ tests/contrib/django/test_restframework.py
@@ -27,12 +27,22 @@ class RestFrameworkTest(DjangoTraceTestCase):
         self.unpatch_rest_framework()
         ok_(not hasattr(self.APIView, self.ORIGINAL_HANDLE_EXCEPTION))
 
-    def test_tracer(self):
-        ok_(self.tracer.enabled)
-        eq_(self.tracer.writer.api.hostname, 'localhost')
-        eq_(self.tracer.writer.api.port, 8126)
-        eq_(self.tracer.tags, {'env': 'test'})
-    
+        response = self.client.get('/rest_framework/users/')
+
+        # Our custom exception handler is setting the status code to 500
+        eq_(response.status_code, 500)
+
+        # check for spans
+        spans = self.tracer.writer.pop()
+        eq_(len(spans), 1)
+        sp = spans[0]
+        eq_(sp.name, 'django.request')
+        eq_(sp.resource, 'tests.contrib.django.app.restframework.UserViewSet')
+        eq_(sp.error, 0)
+        eq_(sp.span_type, 'http')
+        eq_(sp.get_tag('http.status_code'), '500')
+        eq_(sp.get_tag('error.msg'), None)
+
     def test_trace_exceptions(self):
         response = self.client.get('/rest_framework/users/')
 
