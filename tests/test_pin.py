38f99ad8bd2edf3e42c16ab04423cd0504dbe0e5
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2018-03-07 12:01:46 +0100

    [core] refactoring Pin tests

--- tests/test_pin.py
+++ tests/test_pin.py
@@ -1,76 +1,95 @@
+from unittest import TestCase
 
 from ddtrace import Pin
-from nose.tools import eq_
+from nose.tools import eq_, ok_, assert_raises
 
 
-def test_pin():
-    class A(object):
-        pass
+class PinTestCase(TestCase):
+    """TestCase for the `Pin` object that is used when an object is wrapped
+    with our tracing functionalities.
+    """
+    def setUp(self):
+        # define a simple class object
+        class Obj(object):
+            pass
 
-    a = A()
-    pin = Pin(service="abc")
-    pin.onto(a)
+        self.Obj = Obj
 
-    got = Pin.get_from(a)
-    assert pin.service == got.service
-    assert pin is got
+    def test_pin(self):
+        # ensure a Pin can be attached to an instance
+        obj = self.Obj()
+        pin = Pin(service='metrics')
+        pin.onto(obj)
 
-def test_cant_pin():
+        got = Pin.get_from(obj)
+        eq_(got.service, pin.service)
+        ok_(got is pin)
 
-    class Thing(object):
-        __slots__ = ['t']
+    def test_cant_pin_with_slots(self):
+        # ensure a Pin can't be attached if the __slots__ is defined
+        class Obj(object):
+            __slots__ = ['value']
 
-    t = Thing()
-    t.t = 1
+        obj = Obj()
+        obj.value = 1
 
-    Pin(service="a").onto(t)
+        Pin(service='metrics').onto(obj)
+        got = Pin.get_from(obj)
+        ok_(got is None)
 
-def test_cant_modify():
-    p = Pin(service="abc")
-    try:
-        p.service = "other"
-    except AttributeError:
-        pass
+    def test_cant_modify(self):
+        # ensure a Pin is immutable once initialized
+        pin = Pin(service='metrics')
+        with assert_raises(AttributeError):
+            pin.service = 'intake'
 
-def test_copy():
-    p1 = Pin(service="a", app="app_type", tags={"a":"b"})
-    p2 = p1.clone(service="b")
-    assert p1.service == "a"
-    assert p2.service == "b"
-    assert p1.app == "app_type"
-    assert p2.app == "app_type"
-    eq_(p1.tags, p2.tags)
-    assert not (p1.tags is p2.tags)
-    assert p1.tracer is p2.tracer
+    def test_copy(self):
+        # ensure a Pin is copied when using the clone methods
+        p1 = Pin(service='metrics', app='flask', tags={'key': 'value'})
+        p2 = p1.clone(service='intake')
+        # values are the same
+        eq_(p1.service, 'metrics')
+        eq_(p2.service, 'intake')
+        eq_(p1.app, 'flask')
+        eq_(p2.app, 'flask')
+        # but it's a copy
+        ok_(p1.tags is not p2.tags)
+        # of almost everything
+        ok_(p1.tracer is p2.tracer)
 
-def test_none():
-    assert None is Pin.get_from(None)
+    def test_none(self):
+        # ensure get_from returns None if a Pin is not available
+        ok_(Pin.get_from(None) is None)
 
-def test_repr():
-    p = Pin(service="abc")
-    assert p.service == "abc"
-    assert 'abc' in str(p)
+    def test_repr(self):
+        # ensure the service name is in the string representation of the Pin
+        pin = Pin(service='metrics')
+        ok_('metrics' in str(pin))
 
-def test_override():
-    class A(object):
-        pass
+    def test_override(self):
+        # ensure Override works for an instance object
+        class A(object):
+            pass
 
-    Pin(service="foo", app="blah").onto(A)
-    a = A()
-    Pin.override(a, app="bar")
-    eq_(Pin.get_from(a).app, "bar")
-    eq_(Pin.get_from(a).service, "foo")
+        Pin(service='metrics', app='flask').onto(A)
+        a = A()
+        Pin.override(a, app='django')
+        eq_(Pin.get_from(a).app, 'django')
+        eq_(Pin.get_from(a).service, 'metrics')
 
-    b = A()
-    eq_(Pin.get_from(b).service, "foo")
-    eq_(Pin.get_from(b).app, "blah")
+        b = A()
+        eq_(Pin.get_from(b).app, 'flask')
+        eq_(Pin.get_from(b).service, 'metrics')
 
+    def test_override_missing(self):
+        # ensure overriding an instance doesn't override the Class
+        class A(object):
+            pass
 
-def test_overide_missing():
-    class A():
-        pass
+        a = A()
+        ok_(Pin.get_from(a) is None)
+        Pin.override(a, service='metrics')
+        eq_(Pin.get_from(a).service, 'metrics')
 
-    a = A()
-    assert not Pin.get_from(a)
-    Pin.override(a, service="foo")
-    assert Pin.get_from(a).service == "foo"
+        b = A()
+        ok_(Pin.get_from(b) is None)
