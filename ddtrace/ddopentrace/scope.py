bebf01262ef52b7f51f56ed2b8feae436c4ea1bb
kyle-verhoog <kyle@verhoog.ca>
2018-05-16 18:35:39 -0400

    [opentrace] add skeleton code

---
+++ ddtrace/ddopentrace/scope.py
@@ -0,0 +1,10 @@
+from opentracing import Scope as OpenTracingScope
+
+
+class Scope(OpenTracingScope):
+    """"""
+
+    def close(self):
+        """"""
+        pass
+
--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
     install_requires=[
         "wrapt",
         "msgpack-python",
+        "opentracing",
     ],
     # plugin tox
     tests_require=['tox', 'flake8'],
