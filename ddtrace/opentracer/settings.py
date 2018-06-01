b54c3a7ffb54e331e115a04397e7167e16096166
Kyle Verhoog <kyle@verhoog.ca>
2018-06-01 11:35:26 -0400

    [opentracer] tracer configuration/initialization (#470)
    * [opentracer] add default configuration
    
    * [opentracer] begin integrating ddtracer
    
    * [opentracer] fix linting
    
    * [opentracer] add config validation on debug
    
    * [opentracer] use namedtuple instead of class, fix string formatting
    
    * [opentracer] actually set the opentracer tests to run
    
    * [opentracer] attempt to trigger circleci build
    
    * [opentracer] use ConfigKeys directly, use opentracing v2.0.0
    
    * [opentracer] service_name can only be set through the config
    
    * [opentracer] add a reminder for setting default service name
    
    * [opentracer] make service name the first kwarg, remove from config
    
    * [opentracer] use immutable config, remove APP_KEY from config

---
+++ ddtrace/opentracer/settings.py
@@ -0,0 +1,34 @@
+from collections import namedtuple
+
+
+CONFIG_KEY_NAMES = [
+    'AGENT_HOSTNAME',
+    'AGENT_PORT',
+    'DEBUG',
+    'ENABLED',
+    'GLOBAL_TAGS',
+    'CONTEXT_PROVIDER',
+    'SAMPLER',
+    'PRIORITY_SAMPLING',
+    'SETTINGS',
+]
+
+# Keys used for the configuration dict
+ConfigKeyNames = namedtuple('ConfigKeyNames', CONFIG_KEY_NAMES)
+
+ConfigKeys = ConfigKeyNames(
+    AGENT_HOSTNAME='agent_hostname',
+    AGENT_PORT='agent_port',
+    DEBUG='debug',
+    ENABLED='enabled',
+    GLOBAL_TAGS='global_tags',
+    CONTEXT_PROVIDER='context_provider',
+    SAMPLER='sampler',
+    PRIORITY_SAMPLING='priority_sampling',
+    SETTINGS='settings',
+)
+
+
+def config_invalid_keys(config):
+    """Returns a list of keys that exist in *config* and not in KEYS."""
+    return [key for key in config.keys() if key not in ConfigKeys]
--- ddtrace/opentracer/tracer.py
+++ ddtrace/opentracer/tracer.py
@@ -1,13 +1,70 @@
 import opentracing
+import logging
+
+from ddtrace import Tracer as DatadogTracer
+from ddtrace.constants import FILTERS_KEY
+from ddtrace.ext import AppTypes
+from ddtrace.settings import ConfigException
+
+from .settings import ConfigKeys as keys, config_invalid_keys
+
+from .util import merge_dicts
+
+
+log = logging.getLogger(__name__)
+
+DEFAULT_CONFIG = {
+    keys.AGENT_HOSTNAME: 'localhost',
+    keys.AGENT_PORT: 8126,
+    keys.DEBUG: False,
+    keys.ENABLED: True,
+    keys.GLOBAL_TAGS: {},
+    keys.SAMPLER: None,
+    keys.CONTEXT_PROVIDER: None,
+    keys.PRIORITY_SAMPLING: None,
+    keys.SETTINGS: {
+        FILTERS_KEY: [],
+    },
+}
 
 
 class Tracer(opentracing.Tracer):
-    """"""
+    """A wrapper providing an OpenTracing API for the Datadog tracer."""
 
-    __slots__ = []
+    __slots__ = ['_enabled', '_debug', '_service_name', '_tracer']
 
-    def __init__(self, scope_manager=None):
-        pass
+    def __init__(self, service_name=None, config=None, scope_manager=None):
+        # Merge the given config with the default into a new dict
+        config = config or {}
+        self._config = merge_dicts(DEFAULT_CONFIG, config)
+
+        # Pull out commonly used properties for performance
+        self._service_name = service_name
+        self._enabled = self._config.get(keys.ENABLED)
+        self._debug = self._config.get(keys.DEBUG)
+
+        if self._debug:
+            # Ensure there are no typos in any of the keys
+            invalid_keys = config_invalid_keys(self._config)
+            if invalid_keys:
+                str_invalid_keys = ','.join(invalid_keys)
+                raise ConfigException('invalid key(s) given (%s)'.format(str_invalid_keys))
+
+        # TODO: we should set a default reasonable `service_name` (__name__) or
+        # similar.
+        if not self._service_name:
+            raise ConfigException('a service_name is required')
+
+        self._tracer = DatadogTracer()
+
+        self._tracer.configure(enabled=self._enabled,
+                               hostname=self._config.get(keys.AGENT_HOSTNAME),
+                               port=self._config.get(keys.AGENT_PORT),
+                               sampler=self._config.get(keys.SAMPLER),
+                               settings=self._config.get(keys.SETTINGS),
+                               context_provider=self._config.get(keys.CONTEXT_PROVIDER),
+                               priority_sampling=self._config.get(keys.PRIORITY_SAMPLING),
+                               )
 
     @property
     def scope_manager(self):
@@ -37,3 +94,7 @@ class Tracer(opentracing.Tracer):
     def extract(self, span_context, format, carrier):
         """"""
         pass
+
+def set_global_tracer(tracer):
+    """Sets the global opentracer to the given tracer."""
+    opentracing.tracer = tracer
