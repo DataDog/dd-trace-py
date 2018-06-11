5c2459b66eeb75946b4ebbde08d1c47fe33f96ba
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2018-06-11 12:30:12 +0200

    [celery] patch TaskRegistry to support old-style task with `ddtrace-run`

--- ddtrace/contrib/celery/patch.py
+++ ddtrace/contrib/celery/patch.py
@@ -1,15 +1,21 @@
-# Third party
 import celery
 
-# Project
+from wrapt import wrap_function_wrapper as _w
+
 from .app import patch_app, unpatch_app
+from .registry import _wrap_register
+from ...utils.wrappers import unwrap as _u
 
 
 def patch():
-    """ patch will add all available tracing to the celery library """
+    """Instrument Celery base application and the `TaskRegistry` so
+    that any new registered task is automatically instrumented
+    """
     setattr(celery, 'Celery', patch_app(celery.Celery))
+    _w('celery.app.registry', 'TaskRegistry.register', _wrap_register)
 
 
 def unpatch():
-    """ unpatch will remove tracing from the celery library """
+    """Removes instrumentation from Celery"""
     setattr(celery, 'Celery', unpatch_app(celery.Celery))
+    _u(celery.app.registry.TaskRegistry, 'register')
