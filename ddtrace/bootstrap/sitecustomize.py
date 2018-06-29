64e818bd1406c1e77cf2773155fa82308cb45ab9
kyle-verhoog <kyle@verhoog.ca>
2018-06-29 05:56:24 -0400

    move ddopentrace-run logic to ddtrace-run

--- ddtrace/bootstrap/sitecustomize.py
+++ ddtrace/bootstrap/sitecustomize.py
@@ -42,62 +42,99 @@ def update_patched_modules():
 
         EXTRA_PATCHED_MODULES.update({module: should_patch.lower() == 'true'})
 
-
-try:
-    from ddtrace import tracer
-    patch = True
-
-    # Respect DATADOG_* environment variables in global tracer configuration
-    # TODO: these variables are deprecated; use utils method and update our documentation
-    # correct prefix should be DD_*
-    enabled = os.environ.get("DATADOG_TRACE_ENABLED")
-    hostname = os.environ.get("DATADOG_TRACE_AGENT_HOSTNAME")
-    port = os.environ.get("DATADOG_TRACE_AGENT_PORT")
-    priority_sampling = os.environ.get("DATADOG_PRIORITY_SAMPLING")
-
-    opts = {}
-
-    if enabled and enabled.lower() == "false":
-        opts["enabled"] = False
-        patch = False
-    if hostname:
-        opts["hostname"] = hostname
-    if port:
-        opts["port"] = int(port)
-    if priority_sampling:
-        opts["priority_sampling"] = asbool(priority_sampling)
-
-    if opts:
-        tracer.configure(**opts)
-
-    if patch:
-        update_patched_modules()
-        from ddtrace import patch_all; patch_all(**EXTRA_PATCHED_MODULES) # noqa
-
-    debug = os.environ.get("DATADOG_TRACE_DEBUG")
-    if debug and debug.lower() == "true":
-        tracer.debug_logging = True
-
-    if 'DATADOG_ENV' in os.environ:
-        tracer.set_tags({"env": os.environ["DATADOG_ENV"]})
-
-    # Ensure sitecustomize.py is properly called if available in application directories:
-    # * exclude `bootstrap_dir` from the search
-    # * find a user `sitecustomize.py` module
-    # * import that module via `imp`
-    bootstrap_dir = os.path.dirname(__file__)
-    path = list(sys.path)
-    path.remove(bootstrap_dir)
-
+def opentracing_patch():
     try:
-        (f, path, description) = imp.find_module('sitecustomize', path)
+        import opentracing
+        from ddtrace.opentracer import Tracer, set_global_tracer
+        from ddtrace.opentracer.settings import ConfigKeys
+        enabled = os.environ.get("DATADOG_TRACE_ENABLED")
+        hostname = os.environ.get("DATADOG_TRACE_AGENT_HOSTNAME")
+        port = os.environ.get("DATADOG_TRACE_AGENT_PORT")
+        priority_sampling = os.environ.get("DATADOG_PRIORITY_SAMPLING")
+        service_name = os.environ.get("DATADOG_SERVICE_NAME")
+        debug = os.environ.get("DATADOG_TRACE_DEBUG")
+
+        config = {}
+
+        if enabled and enabled.lower() == "false":
+            config[ConfigKeys.ENABLED] = False
+        if debug and debug.lower() == "true":
+            config[ConfigKeys.DEBUG] = True
+        if hostname:
+            config[ConfigKeys.AGENT_HOSTNAME] = hostname
+        if port:
+            config[ConfigKeys.AGENT_PORT] = int(port)
+        if priority_sampling:
+            config[ConfigKeys.PRIORITY_SAMPLING] = asbool(priority_sampling)
+
+        tracer = Tracer(service_name=service_name, config=config)
+        set_global_tracer(tracer)
+        return True
     except ImportError:
-        pass
-    else:
-        # `sitecustomize.py` found, load it
-        log.debug('sitecustomize from user found in: %s', path)
-        imp.load_module('sitecustomize', f, path, description)
+        log.info("opentracing patching failed", exc_info=True)
+        return False
+
+def datadog_patch():
+    try:
+        from ddtrace import tracer
+        patch = True
+
+        # Respect DATADOG_* environment variables in global tracer configuration
+        # TODO: these variables are deprecated; use utils method and update our documentation
+        # correct prefix should be DD_*
+        enabled = os.environ.get("DATADOG_TRACE_ENABLED")
+        hostname = os.environ.get("DATADOG_TRACE_AGENT_HOSTNAME")
+        port = os.environ.get("DATADOG_TRACE_AGENT_PORT")
+        priority_sampling = os.environ.get("DATADOG_PRIORITY_SAMPLING")
+
+        opts = {}
+
+        if enabled and enabled.lower() == "false":
+            opts["enabled"] = False
+            patch = False
+        if hostname:
+            opts["hostname"] = hostname
+        if port:
+            opts["port"] = int(port)
+        if priority_sampling:
+            opts["priority_sampling"] = asbool(priority_sampling)
+
+        if opts:
+            tracer.configure(**opts)
+
+        if patch:
+            update_patched_modules()
+            from ddtrace import patch_all; patch_all(**EXTRA_PATCHED_MODULES) # noqa
+
+        debug = os.environ.get("DATADOG_TRACE_DEBUG")
+        if debug and debug.lower() == "true":
+            tracer.debug_logging = True
+
+        if 'DATADOG_ENV' in os.environ:
+            tracer.set_tags({"env": os.environ["DATADOG_ENV"]})
+
+        # Ensure sitecustomize.py is properly called if available in application directories:
+        # * exclude `bootstrap_dir` from the search
+        # * find a user `sitecustomize.py` module
+        # * import that module via `imp`
+        bootstrap_dir = os.path.dirname(__file__)
+        path = list(sys.path)
+        path.remove(bootstrap_dir)
+
+        try:
+            (f, path, description) = imp.find_module('sitecustomize', path)
+        except ImportError:
+            pass
+        else:
+            # `sitecustomize.py` found, load it
+            log.debug('sitecustomize from user found in: %s', path)
+            imp.load_module('sitecustomize', f, path, description)
+
+    except Exception as e:
+        log.warn("error configuring Datadog tracing", exc_info=True)
 
 
-except Exception as e:
-    log.warn("error configuring Datadog tracing", exc_info=True)
+# first try to patch if OpenTracing is installed
+if not opentracing_patch():
+    # if this fails then do our usual patching
+    datadog_patch()
