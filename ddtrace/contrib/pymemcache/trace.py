269438a5764ae2afb8dd56677c0c0f4e4208801e
kyle-verhoog <kyle@verhoog.ca>
2018-07-30 23:30:42 -0400

    [pymemcache] clean up unneeded indirection

--- ddtrace/contrib/pymemcache/trace.py
+++ ddtrace/contrib/pymemcache/trace.py
@@ -24,75 +24,79 @@ class WrappedClient(wrapt.ObjectProxy):
         c = _Client(*args, **kwargs)
         super(WrappedClient, self).__init__(c)
 
+        # attach the pin onto this instance
+        p = Pin(service=memcachedx.SERVICE, app_type=memcachedx.TYPE)
+        p.onto(self)
+
     def set(self, *args, **kwargs):
-        return self._cmd("set", *args, **kwargs)
+        return self._traced_cmd("set", *args, **kwargs)
 
     def set_many(self, *args, **kwargs):
-        return self._cmd("set_many", *args, **kwargs)
+        return self._traced_cmd("set_many", *args, **kwargs)
 
     def add(self, *args, **kwargs):
-        return self._cmd("add", *args, **kwargs)
+        return self._traced_cmd("add", *args, **kwargs)
 
     def replace(self, *args, **kwargs):
-        return self._cmd("replace", *args, **kwargs)
+        return self._traced_cmd("replace", *args, **kwargs)
 
     def append(self, *args, **kwargs):
-        return self._cmd("append", *args, **kwargs)
+        return self._traced_cmd("append", *args, **kwargs)
 
     def prepend(self, *args, **kwargs):
-        return self._cmd("prepend", *args, **kwargs)
+        return self._traced_cmd("prepend", *args, **kwargs)
 
     def cas(self, *args, **kwargs):
-        return self._cmd("cas", *args, **kwargs)
+        return self._traced_cmd("cas", *args, **kwargs)
 
     def get(self, *args, **kwargs):
-        return self._cmd("get", *args, **kwargs)
+        return self._traced_cmd("get", *args, **kwargs)
 
     def get_many(self, *args, **kwargs):
-        return self._cmd("get_many", *args, **kwargs)
+        return self._traced_cmd("get_many", *args, **kwargs)
 
     def gets(self, *args, **kwargs):
-        return self._cmd("gets", *args, **kwargs)
+        return self._traced_cmd("gets", *args, **kwargs)
 
     def gets_many(self, *args, **kwargs):
-        return self._cmd("gets_many", *args, **kwargs)
+        return self._traced_cmd("gets_many", *args, **kwargs)
 
     def delete(self, *args, **kwargs):
-        return self._cmd("delete", *args, **kwargs)
+        return self._traced_cmd("delete", *args, **kwargs)
 
     def delete_many(self, *args, **kwargs):
-        return self._cmd("delete_many", *args, **kwargs)
+        return self._traced_cmd("delete_many", *args, **kwargs)
 
     def incr(self, *args, **kwargs):
-        return self._cmd("incr", *args, **kwargs)
+        return self._traced_cmd("incr", *args, **kwargs)
 
     def decr(self, *args, **kwargs):
-        return self._cmd("decr", *args, **kwargs)
+        return self._traced_cmd("decr", *args, **kwargs)
 
     def touch(self, *args, **kwargs):
-        return self._cmd("touch", *args, **kwargs)
+        return self._traced_cmd("touch", *args, **kwargs)
 
     def stats(self, *args, **kwargs):
-        return self._cmd("stats", *args, **kwargs)
+        return self._traced_cmd("stats", *args, **kwargs)
 
     def version(self, *args, **kwargs):
-        return self._cmd("version", *args, **kwargs)
+        return self._traced_cmd("version", *args, **kwargs)
 
     def flush_all(self, *args, **kwargs):
-        return self._cmd("flush_all", *args, **kwargs)
+        return self._traced_cmd("flush_all", *args, **kwargs)
 
     def quit(self, *args, **kwargs):
-        return self._cmd("quit", *args, **kwargs)
+        return self._traced_cmd("quit", *args, **kwargs)
 
     def set_multi(self, *args, **kwargs):
         """set_multi is an alias for set_many"""
-        return self._cmd("set_many", *args, **kwargs)
+        return self._traced_cmd("set_many", *args, **kwargs)
 
     def get_multi(self, *args, **kwargs):
         """set_multi is an alias for set_many"""
-        return self._cmd("get_many", *args, **kwargs)
+        return self._traced_cmd("get_many", *args, **kwargs)
 
-    def _cmd(self, method_name, *args, **kwargs):
+    def _traced_cmd(self, method_name, *args, **kwargs):
         """Run and trace the given command.
 
         Any pymemcache exception is caught and span error information is
@@ -100,7 +104,13 @@ class WrappedClient(wrapt.ObjectProxy):
         appropriately.
         """
         method = getattr(self.__wrapped__, method_name)
-        with self._span(method_name) as span:
+        p = Pin.get_from(self)
+        with p.tracer.trace(
+            memcachedx.CMD,
+            service=memcachedx.SERVICE,
+            resource=method_name,
+            span_type=memcachedx.TYPE,
+        ) as span:
             try:
                 return method(*args, **kwargs)
             except (
@@ -113,20 +123,3 @@ class WrappedClient(wrapt.ObjectProxy):
                 (typ, val, tb) = sys.exc_info()
                 span.set_exc_info(typ, val, tb)
                 reraise(typ, val, tb)
-
-    def _span(self, cmd_name):
-        """ Return a newly created span for the given command. """
-        p = self._get_pin()
-        return p.tracer.trace(
-            memcachedx.CMD,
-            service=memcachedx.SERVICE,
-            resource=cmd_name,
-            span_type=memcachedx.TYPE,
-        )
-
-    def _get_pin(self):
-        p = Pin.get_from(self)
-        if not p:
-            p = Pin(service=memcachedx.SERVICE, app_type=memcachedx.TYPE)
-            p.onto(self)
-        return p
