351590972c5d78f5f865739b436698a2d515df6f
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2018-08-03 14:07:37 +0200

    [celery] add patch_task and unpatch_task as a backward compatibility
    This reverts commit 0440d39f801f842e91249cddf6e02c823adf2735.

--- ddtrace/contrib/celery/patch.py
+++ ddtrace/contrib/celery/patch.py
@@ -1,16 +1,6 @@
 import celery
 
-from celery import signals
-
 from .app import patch_app, unpatch_app
-
-from .signals import (
-    trace_prerun,
-    trace_postrun,
-    trace_before_publish,
-    trace_after_publish,
-    trace_failure,
-)
 
 
 def patch():
@@ -20,18 +10,8 @@ def patch():
     must be instrumented because Django doesn't use the Celery registry.
     """
     patch_app(celery.Celery)
-    signals.task_prerun.connect(trace_prerun)
-    signals.task_postrun.connect(trace_postrun)
-    signals.before_task_publish.connect(trace_before_publish)
-    signals.after_task_publish.connect(trace_after_publish)
-    signals.task_failure.connect(trace_failure)
 
 
 def unpatch():
     """Disconnect all signals and remove Tracing capabilities"""
     unpatch_app(celery.Celery)
-    signals.task_prerun.disconnect(trace_prerun)
-    signals.task_postrun.disconnect(trace_postrun)
-    signals.before_task_publish.disconnect(trace_before_publish)
-    signals.after_task_publish.disconnect(trace_after_publish)
-    signals.task_failure.disconnect(trace_failure)
