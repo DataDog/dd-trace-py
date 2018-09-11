6177e8f49dbb77553f79de132e6689b560774f40
Luca Abbati <luca.abbati@gmail.com>
2018-09-11 13:54:37 +0200

    Update falcon versions in tests

--- tests/contrib/falcon/test_autopatch.py
+++ tests/contrib/falcon/test_autopatch.py
@@ -2,12 +2,20 @@ from ddtrace import tracer
 from tests.test_tracer import DummyWriter
 
 from falcon import testing
+from falcon.testing.client import TestClient
 
 from .app import get_app
 from .test_suite import FalconTestCase
 
 
 class AutoPatchTestCase(testing.TestCase, FalconTestCase):
+
+    def __init__(self, methodName='runTest'):
+        super(AutoPatchTestCase, self).__init__(methodName=methodName)
+        # Required as in falcon 1.3+ some required test properties are initialized
+        # in the __init__ method
+        TestClient.__init__(self, None)
+
     def setUp(self):
         self._service = 'my-falcon'
         self.tracer = tracer
