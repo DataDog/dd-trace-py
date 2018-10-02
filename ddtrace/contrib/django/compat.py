b252bb2a7feb3aa6b90f6165113633867fafa3ce
Bruno Alla <alla.brunoo@gmail.com>
2018-10-02 13:02:42 +0100

    Add flake8 exception as the CallableBool cannot be compared using `is`

--- ddtrace/contrib/django/compat.py
+++ ddtrace/contrib/django/compat.py
@@ -5,7 +5,7 @@ if django.VERSION >= (1, 10, 1):
     def user_is_authenticated(user):
         # Explicit comparision due to the following bug
         # https://code.djangoproject.com/ticket/26988
-        return user.is_authenticated == True
+        return user.is_authenticated == True  # noqa E712
 else:
     def user_is_authenticated(user):
         return user.is_authenticated()
