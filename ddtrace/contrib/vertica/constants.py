fbe4797b4c580cc11209706a36b8bc6df049553d
kyle-verhoog <kyle@verhoog.ca>
2018-10-09 11:46:51 +0200

    [vertica] code clean-up and formatting

--- ddtrace/contrib/vertica/constants.py
+++ ddtrace/contrib/vertica/constants.py
@@ -1,4 +1,2 @@
-
-
 # Service info
 APP = "vertica"
--- ddtrace/contrib/vertica/patch.py
+++ ddtrace/contrib/vertica/patch.py
@@ -19,17 +19,22 @@ provide.
 
 _PATCHED = False
 
-def copy_before(instance, span, conf, *args, **kwargs):
+
+def copy_span_start(instance, span, conf, *args, **kwargs):
     span.set_tag("query", args[0])
 
-def execute_before(instance, span, conf, *args, **kwargs):
+
+def execute_span_start(instance, span, conf, *args, **kwargs):
     span.set_tag("query", args[0])
 
-def execute_after(result, instance, span, conf, *args, **kwargs):
+
+def execute_span_end(result, instance, span, conf, *args, **kwargs):
     span.set_metric(dbx.ROWCOUNT, instance.rowcount)
 
-def fetch_after(result, instance, span, conf, *args, **kwargs):
+
+def fetch_span_end(result, instance, span, conf, *args, **kwargs):
     span.set_metric(dbx.ROWCOUNT, instance.rowcount)
+
 
 def cursor_after(cursor, instance, span, conf, *args, **kwargs):
     tags = {}
@@ -45,7 +50,7 @@ def cursor_after(cursor, instance, span, conf, *args, **kwargs):
         app=APP,
         app_type=AppTypes.db,
         tags=tags,
-        _config=config.vertica["patch"]["vertica_python.vertica.cursor.Cursor"]
+        _config=config.vertica["patch"]["vertica_python.vertica.cursor.Cursor"],
     )
     pin.onto(cursor)
 
@@ -60,45 +65,43 @@ config._add(
         "patch": {
             "vertica_python.vertica.connection.Connection": {
                 "routines": {
-                    "cursor": {
-                        "trace_enabled": False,
-                        "on_after": cursor_after,
-                    }
-                },
+                    "cursor": {"trace_enabled": False, "span_end": cursor_after}
+                }
             },
             "vertica_python.vertica.cursor.Cursor": {
                 "routines": {
                     "execute": {
                         "operation_name": "vertica.query",
                         "span_type": "vertica",
-                        "on_before": execute_before,
-                        "on_after": execute_after,
+                        "span_start": execute_span_start,
+                        "span_end": execute_span_end,
                     },
                     "copy": {
                         "operation_name": "vertica.copy",
                         "span_type": "vertica",
-                        "on_before": copy_before,
+                        "span_start": copy_span_start,
                     },
                     "fetchone": {
                         "operation_name": "vertica.fetchone",
                         "span_type": "vertica",
-                        "on_after": fetch_after,
+                        "span_end": fetch_span_end,
                     },
                     "fetchall": {
                         "operation_name": "vertica.fetchall",
                         "span_type": "vertica",
-                        "on_after": fetch_after,
+                        "span_end": fetch_span_end,
                     },
                     "nextset": {
                         "operation_name": "vertica.nextset",
                         "span_type": "vertica",
-                        "on_after": fetch_after,
+                        "span_end": fetch_span_end,
                     },
-                },
+                }
             },
         },
     },
 )
+
 
 def patch():
     global _PATCHED
@@ -115,43 +118,46 @@ def unpatch():
         _uninstall(config.vertica)
         _PATCHED = False
 
+
 def _uninstall(config):
     for patch_class_path in config["patch"]:
-        patch_mod = '.'.join(patch_class_path.split('.')[0:-1])
-        patch_class = patch_class_path.split('.')[-1]
+        patch_mod, _, patch_class = patch_class_path.rpartition(".")
 
         for patch_routine in config["patch"][patch_class_path]["routines"]:
             mod = importlib.import_module(patch_mod)
             cls = getattr(mod, patch_class)
             unwrap(cls, patch_routine)
 
+
 def _install(config):
     for patch_class_path in config["patch"]:
-        patch_mod = '.'.join(patch_class_path.split('.')[0:-1])
-        patch_class = patch_class_path.split('.')[-1]
+        patch_mod, _, patch_class = patch_class_path.rpartition(".")
+
+        def wrap_init():
+            _patch_item = patch_class_path
+            patch_class_routine = "{}.{}".format(patch_class, "__init__")
+
+            # patch the __init__ of the class with a Pin instance containing the defaults
+            @wrapt.patch_function_wrapper(patch_mod, patch_class_routine)
+            def init_wrapper(wrapped, instance, args, kwargs):
+                wrapped(*args, **kwargs)
+
+                # create and attach a pin with the defaults
+                Pin(
+                    service=config["service_name"],
+                    app=config["app"],
+                    app_type=config["app_type"],
+                    tags=config.get("tags", {}),
+                    tracer=config.get("tracer", ddtrace.tracer),
+                    _config=config["patch"][_patch_item],
+                ).onto(instance)
+
+        wrap_init()
 
         for patch_routine in config["patch"][patch_class_path]["routines"]:
-            def _wrap():
-                # _wrap is needed to provide data to the wrappers which can
-                # only be provided from a function closure.
 
+            def wrap_routine():
                 _patch_routine = patch_routine
-                _patch_item = patch_class_path
-
-                # patch the __init__ of the class with a Pin instance containing the defaults
-                @wrapt.patch_function_wrapper(patch_mod, "{}.{}".format(patch_class, "__init__"))
-                def init_wrapper(wrapped, instance, args, kwargs):
-                    wrapped(*args, **kwargs)
-
-                    # create and attach a pin with the defaults
-                    Pin(
-                        service=config["service_name"],
-                        app=config["app"],
-                        app_type=config["app_type"],
-                        tags=config.get("tags", {}),
-                        tracer=config.get("tracer", ddtrace.tracer),
-                        _config=config["patch"][_patch_item],
-                    ).onto(instance)
 
                 def _find_config(instance, routine_name):
                     bases = instance.__class__.__bases__
@@ -160,11 +166,17 @@ def _install(config):
                         newbases = []
                         for base in bases:
                             full_name = "{}.{}".format(base.__module__, base.__name__)
-                            if full_name in config["patch"] and routine_name in config["patch"][full_name]["routines"]:
-                                return config["patch"][full_name]["routines"][routine_name]
+                            config_routines = config["patch"][full_name]["routines"]
+                            if (
+                                full_name in config["patch"]
+                                and routine_name in config_routines
+                            ):
+                                return config_routines[routine_name]
                             newbases += base.__class__.__bases__
 
-                @wrapt.patch_function_wrapper(patch_mod, "{}.{}".format(patch_class, patch_routine))
+                patch_class_routine = "{}.{}".format(patch_class, patch_routine)
+
+                @wrapt.patch_function_wrapper(patch_mod, patch_class_routine)
                 def wrapper(wrapped, instance, args, kwargs):
                     # TODO?: remove Pin dependence
                     pin = Pin.get_from(instance)
@@ -228,8 +240,10 @@ def _install(config):
                             if "span_type" in conf:
                                 span.span_type = conf["span_type"]
 
-                            if "on_before" in conf:
-                                conf["on_before"](instance, span, conf, *args, **kwargs)
+                            if "span_start" in conf:
+                                conf["span_start"](
+                                    instance, span, conf, *args, **kwargs
+                                )
 
                             result = wrapped(*args, **kwargs)
                             return result
@@ -241,6 +255,9 @@ def _install(config):
                         # if an exception is raised result will not exist
                         if "result" not in locals():
                             result = None
-                        if "on_after" in conf:
-                            conf["on_after"](result, instance, span, conf, *args, **kwargs)
-            _wrap()
+                        if "span_end" in conf:
+                            conf["span_end"](
+                                result, instance, span, conf, *args, **kwargs
+                            )
+
+            wrap_routine()
