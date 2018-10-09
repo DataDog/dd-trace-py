377163d9038ecacb42ce34ba066deaaa9e5c1c7b
kyle-verhoog <kyle@verhoog.ca>
2018-10-09 15:08:19 +0200

    [vertica] clean-up

--- ddtrace/ext/db.py
+++ ddtrace/ext/db.py
@@ -1,5 +1,4 @@
-
 # tags
 NAME = "db.name"     # the database name (eg: dbname for pgsql)
 USER = "db.user"     # the user connecting to the db
-ROWCOUNT = "db.rowcount" # the rowcount of a query
+ROWCOUNT = "db.rowcount"  # the rowcount of a query
