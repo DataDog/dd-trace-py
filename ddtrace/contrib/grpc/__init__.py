594352f91d91fecd148c2eb9ee8780e6b76a1b4e
Pierre-Hugues Jeanneret <ph.jeanneret@datadoghq.com>
2018-10-10 17:56:25 +0200

    Remove emacs file and verify module presence

--- ddtrace/contrib/grpc/__init__.py
+++ ddtrace/contrib/grpc/__init__.py
@@ -1,2 +1,9 @@
-from .patch import patch, unpatch
-__all__ = ['patch', 'unpatch']
+from ...utils.importlib import require_modules
+
+required_modules = ["grpc"]
+
+with require_modules(required_modules) as missing_modules:
+    if not missing_modules:
+        from .patch import patch, unpatch
+
+        __all__ = ['patch', 'unpatch']
