ae44fa9a8bda544c8fc1b091ed8b2d5dc7691ce3
kyle-verhoog <kyle@verhoog.ca>
2018-10-10 15:40:41 +0200

    [vertica] clean up code

--- ddtrace/contrib/vertica/patch.py
+++ ddtrace/contrib/vertica/patch.py
@@ -122,10 +122,10 @@ def unpatch():
 def _uninstall(config):
     for patch_class_path in config["patch"]:
         patch_mod, _, patch_class = patch_class_path.rpartition(".")
+        mod = importlib.import_module(patch_mod)
+        cls = getattr(mod, patch_class)
 
         for patch_routine in config["patch"][patch_class_path]["routines"]:
-            mod = importlib.import_module(patch_mod)
-            cls = getattr(mod, patch_class)
             unwrap(cls, patch_routine)
 
 
@@ -141,125 +141,117 @@ def _find_routine_config(config, instance, routine_name):
 
         config_routines = config["patch"][full_name]["routines"]
 
-        if full_name in config["patch"] and routine_name in config_routines:
+        if routine_name in config_routines:
             return config_routines[routine_name]
     return {}
+
+
+def _install_init(patch_item, patch_class, patch_mod, config):
+    patch_class_routine = "{}.{}".format(patch_class, "__init__")
+
+    # patch the __init__ of the class with a Pin instance containing the defaults
+    @wrapt.patch_function_wrapper(patch_mod, patch_class_routine)
+    def init_wrapper(wrapped, instance, args, kwargs):
+        wrapped(*args, **kwargs)
+
+        # create and attach a pin with the defaults
+        Pin(
+            service=config["service_name"],
+            app=config["app"],
+            app_type=config["app_type"],
+            tags=config.get("tags", {}),
+            tracer=config.get("tracer", ddtrace.tracer),
+            _config=config["patch"][patch_item],
+        ).onto(instance)
+
+
+def _install_routine(patch_routine, patch_class, patch_mod, config):
+    patch_class_routine = "{}.{}".format(patch_class, patch_routine)
+
+    @wrapt.patch_function_wrapper(patch_mod, patch_class_routine)
+    def wrapper(wrapped, instance, args, kwargs):
+        # TODO?: remove Pin dependence
+        pin = Pin.get_from(instance)
+
+        # TODO: inheritance problem with pins:
+        # Say we have a base class B and child class C:
+        # class B:
+        #   def my_method(self):
+        #     pass
+        # class C(B):
+        #   pass
+        #
+        # with tracing config:
+        #  'patch': {
+        #     'B': {
+        #       'routines': {
+        #         'my_method: {...}
+        #       }
+        #     },
+        #     'C': {}  # does not contain 'my_method'
+        #  }
+        # and an instance `c`:
+        # c = C()
+        # c.my_method()
+        #
+        # An instance of either will have a pin attached so that
+        # the user can override specific configuration for that
+        # instance.
+        #
+        # The pins will contain the config for the instance which
+        # they are attached:
+        #   PinB._config == { 'routines: { 'my_method: { ... } } }
+        #   PinC._config == { }
+        #
+        # The problem here is that at this point in the wrapper
+        # when c.my_method() is called we have an instance of C and
+        # a pin from C, but require the config from B.
+        #
+        # Possible solution: follow inheritance and look for config
+        # on a parent. Can we use normal python inheritance somehow?
+        if patch_routine in pin._config["routines"]:
+            conf = pin._config["routines"][patch_routine]
+        else:
+            conf = _find_routine_config(config, instance, patch_routine)
+
+        enabled = conf.get("trace_enabled", True)
+
+        span = None
+
+        try:
+            # shortcut if not enabled
+            if not enabled:
+                result = wrapped(*args, **kwargs)
+                return result
+
+            operation_name = conf["operation_name"]
+            tracer = pin.tracer
+            with tracer.trace(operation_name, service=pin.service) as span:
+                span.set_tags(pin.tags)
+                if "span_type" in conf:
+                    span.span_type = conf["span_type"]
+
+                if "span_start" in conf:
+                    conf["span_start"](instance, span, conf, *args, **kwargs)
+
+                result = wrapped(*args, **kwargs)
+                return result
+        except Exception as err:
+            if "on_error" in conf:
+                conf["on_error"](instance, err, span, conf, *args, **kwargs)
+            raise
+        finally:
+            # if an exception is raised result will not exist
+            if "result" not in locals():
+                result = None
+            if "span_end" in conf:
+                conf["span_end"](instance, result, span, conf, *args, **kwargs)
 
 
 def _install(config):
     for patch_class_path in config["patch"]:
         patch_mod, _, patch_class = patch_class_path.rpartition(".")
-
-        def wrap_init():
-            _patch_item = patch_class_path
-            patch_class_routine = "{}.{}".format(patch_class, "__init__")
-
-            # patch the __init__ of the class with a Pin instance containing the defaults
-            @wrapt.patch_function_wrapper(patch_mod, patch_class_routine)
-            def init_wrapper(wrapped, instance, args, kwargs):
-                wrapped(*args, **kwargs)
-
-                # create and attach a pin with the defaults
-                Pin(
-                    service=config["service_name"],
-                    app=config["app"],
-                    app_type=config["app_type"],
-                    tags=config.get("tags", {}),
-                    tracer=config.get("tracer", ddtrace.tracer),
-                    _config=config["patch"][_patch_item],
-                ).onto(instance)
-
-        wrap_init()
+        _install_init(patch_class_path, patch_class, patch_mod, config)
 
         for patch_routine in config["patch"][patch_class_path]["routines"]:
-
-            def wrap_routine():
-                _patch_routine = patch_routine
-
-                patch_class_routine = "{}.{}".format(patch_class, patch_routine)
-
-                @wrapt.patch_function_wrapper(patch_mod, patch_class_routine)
-                def wrapper(wrapped, instance, args, kwargs):
-                    # TODO?: remove Pin dependence
-                    pin = Pin.get_from(instance)
-
-                    # TODO: inheritance problem with pins:
-                    # Say we have a base class B and child class C:
-                    # class B:
-                    #   def my_method(self):
-                    #     pass
-                    #
-                    # class C(B):
-                    #   pass
-                    #
-                    # with tracing config:
-                    #  'patch': {
-                    #     'B': {
-                    #       'routines': {
-                    #         'my_method: {...}
-                    #       }
-                    #     },
-                    #     'C': {}  # does not contain 'my_method'
-                    #  }
-                    # and an instance `c`:
-                    # c = C()
-                    # c.my_method()
-                    #
-                    # An instance of either will have a pin attached so that
-                    # the user can override specific configuration for that
-                    # instance.
-                    #
-                    # The pins will contain the config for the instance which
-                    # they are attached:
-                    #   PinB._config == { 'routines: { 'my_method: { ... } } }
-                    #   PinC._config == { }
-                    #
-                    # The problem here is that at this point in the wrapper
-                    # when c.my_method() is called we have an instance of C and
-                    # a pin from C, but require the config from B.
-                    #
-                    # Possible solution: follow inheritance and look for config
-                    # on a parent. Can we use normal python inheritance somehow?
-                    if _patch_routine in pin._config["routines"]:
-                        conf = pin._config["routines"][_patch_routine]
-                    else:
-                        conf = _find_routine_config(config, instance, _patch_routine)
-
-                    enabled = conf.get("trace_enabled", True)
-
-                    span = None
-
-                    try:
-                        # shortcut if not enabled
-                        if not enabled:
-                            result = wrapped(*args, **kwargs)
-                            return result
-
-                        operation_name = conf["operation_name"]
-                        tracer = pin.tracer
-                        with tracer.trace(operation_name, service=pin.service) as span:
-                            span.set_tags(pin.tags)
-                            if "span_type" in conf:
-                                span.span_type = conf["span_type"]
-
-                            if "span_start" in conf:
-                                conf["span_start"](
-                                    instance, span, conf, *args, **kwargs
-                                )
-
-                            result = wrapped(*args, **kwargs)
-                            return result
-                    except Exception as err:
-                        if "on_error" in conf:
-                            conf["on_error"](instance, err, span, conf, *args, **kwargs)
-                        raise
-                    finally:
-                        # if an exception is raised result will not exist
-                        if "result" not in locals():
-                            result = None
-                        if "span_end" in conf:
-                            conf["span_end"](
-                                instance, result, span, conf, *args, **kwargs
-                            )
-
-            wrap_routine()
+            _install_routine(patch_routine, patch_class, patch_mod, config)
