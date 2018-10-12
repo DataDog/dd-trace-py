2a113608db2aefa0d21892446642d24735dad7df
Luca Abbati <luca.abbati@datadoghq.com>
2018-10-12 15:16:23 +0200

    [falcon] Add http headers to the span

--- ddtrace/contrib/falcon/middleware.py
+++ ddtrace/contrib/falcon/middleware.py
@@ -1,6 +1,8 @@
 import sys
 
+from ddtrace import config
 from ddtrace.ext import http as httpx
+from ddtrace.http import store_request_headers, store_response_headers
 from ddtrace.propagation.http import HTTPPropagator
 from ...compat import iteritems
 from ...ext import AppTypes
@@ -22,6 +24,8 @@ class TraceMiddleware(object):
             service=service,
         )
 
+        self.traced_headers = config.http.get_integration_traced_headers('falcon')
+
     def process_request(self, req, resp):
         if self._distributed_tracing:
             # Falcon uppercases all header names.
@@ -39,6 +43,10 @@ class TraceMiddleware(object):
         span.set_tag(httpx.METHOD, req.method)
         span.set_tag(httpx.URL, req.url)
 
+        # Note: any request header set after this line will not be stored in the span
+        if self.traced_headers:
+            store_request_headers(req.headers, span, self.traced_headers)
+
     def process_resource(self, req, resp, resource, params):
         span = self.tracer.current_span()
         if not span:
@@ -53,6 +61,10 @@ class TraceMiddleware(object):
             return  # unexpected
 
         status = httpx.normalize_status_code(resp.status)
+
+        # Note: any response header set after this line will not be stored in the span
+        if self.traced_headers:
+            store_response_headers(resp._headers, span, self.traced_headers)
 
         # FIXME[matt] falcon does not map errors or unmatched routes
         # to proper status codes, so we we have to try to infer them
