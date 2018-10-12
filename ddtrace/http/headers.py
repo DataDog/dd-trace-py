31d00c513f6da02b6542f633379f00650c6fcc4a
Luca Abbati <luca.abbati@datadoghq.com>
2018-10-12 12:13:01 +0200

    [tests] wildcards in headers whitelisting

--- ddtrace/http/headers.py
+++ ddtrace/http/headers.py
@@ -5,38 +5,42 @@ REQUEST = 'request'
 RESPONSE = 'response'
 
 
-def store_request_headers(headers_provider, span):
+def store_request_headers(headers, span, white_list):
     """
     Store request headers as a span's tags
-    :param headers_provider: A callback that provides http headers to be stored in the span
+    :param headers: All the request's http headers, will be filtered through the whitelist
+    :dict headers: dict
     :param span: The Span instance where tags will be store
     :type span: ddtrace.Span
     """
-    _store_headers(headers_provider, span, REQUEST)
+    _store_headers(headers, span, white_list, REQUEST)
 
 
-def store_response_headers(headers_provider, span):
+def store_response_headers(headers, span, white_list):
     """
-    Store response headers as a span's tags
-    :param headers_provider: A callback that provides http headers to be stored in the span
+    Store request headers as a span's tags
+    :param headers: All the response's http headers, will be filtered through the whitelist
+    :dict headers: dict
     :param span: The Span instance where tags will be store
     :type span: ddtrace.Span
     """
-    _store_headers(headers_provider, span, RESPONSE)
+    _store_headers(headers, span, white_list, RESPONSE)
 
 
-def _store_headers(headers_provider, span, request_or_response):
+def _store_headers(headers, span, white_list, request_or_response):
     """
-    :param headers_provider: A callback that provides http headers to be stored in the span
+    :param headers: A callback that provides http headers to be stored in the span
     :param span: The Span instance where tags will be store
     :type span: ddtrace.span.Span
     :param request_or_response: The context of the headers: request|response
     """
-    headers = headers_provider()  # type: dict
+    headers = headers  # type: dict
     if not isinstance(headers, dict):
         return
 
     for header_name, header_value in headers.items():
+        if not _is_white_listed(header_name, white_list):
+            continue
         tag_name = _normalize_tag_name(request_or_response, header_name)
         span.set_tag(tag_name, header_value)
 
@@ -63,3 +67,11 @@ def _normalize_tag_name(request_or_response, header_name):
     #   - any block of any length of different ASCII chars is converted to a single underscore '_'
     normalized_name = re.sub(r'([^a-z0-9])+', '_',  header_name.strip().lower())
     return 'http.{}.headers.{}'.format(request_or_response, normalized_name)
+
+
+def _is_white_listed(header_name, white_list):
+    for white_list_entry in white_list:
+        # White list can use basic * substitution
+        if re.match(white_list_entry.strip().lower().replace('*', '.*'), header_name.strip().lower()):
+            return True
+    return False
--- tests/unit/http/test_headers.py
+++ tests/unit/http/test_headers.py
@@ -10,41 +10,71 @@ class TestHeaders(object):
         yield Span(tracer, 'some_span')
 
     def test_store_multiple_request_headers_as_dict(self, span):
-        store_request_headers(lambda: {
+        store_request_headers({
             'Content-Type': 'some;value',
             'Max-Age': 'some;other;value',
-        }, span)
+        }, span, ['*'])
         assert span.get_tag('http.request.headers.content_type') == 'some;value'
         assert span.get_tag('http.request.headers.max_age') == 'some;other;value'
 
     def test_store_multiple_response_headers_as_dict(self, span):
-        store_response_headers(lambda: {
+        store_response_headers({
             'Content-Type': 'some;value',
             'Max-Age': 'some;other;value',
-        }, span)
+        }, span, ['*'])
         assert span.get_tag('http.response.headers.content_type') == 'some;value'
         assert span.get_tag('http.response.headers.max_age') == 'some;other;value'
 
     def test_numbers_in_headers_names_are_allowed(self, span):
-        store_response_headers(lambda: {
+        store_response_headers({
             'Content-Type123': 'some;value',
-        }, span)
+        }, span, ['*'])
         assert span.get_tag('http.response.headers.content_type123') == 'some;value'
 
     def test_blocs_of_non_letters_and_digits_to_underscore(self, span):
-        store_response_headers(lambda: {
+        store_response_headers({
             'Content----T_%%y&%$pe': 'some;value',
-        }, span)
+        }, span, ['*'])
         assert span.get_tag('http.response.headers.content_t_y_pe') == 'some;value'
 
     def test_key_trim_leading_trailing_spaced(self, span):
-        store_response_headers(lambda: {
+        store_response_headers({
             '   Content-Type   ': 'some;value',
-        }, span)
+        }, span, ['*'])
         assert span.get_tag('http.response.headers.content_type') == 'some;value'
 
     def test_value_not_trim_leading_trailing_spaced(self, span):
-        store_response_headers(lambda: {
+        store_response_headers({
             'Content-Type': '   some;value   ',
-        }, span)
+        }, span, ['*'])
         assert span.get_tag('http.response.headers.content_type') == '   some;value   '
+
+    def test_no_whitelist(self, span):
+        store_response_headers({
+            'Content-Type': 'some;value',
+        }, span, [])
+        assert span.get_tag('http.response.headers.content_type') is None
+
+    def test_whitelist_all(self, span):
+        store_response_headers({
+            'Content-Type': 'some;value',
+        }, span, ['*'])
+        assert span.get_tag('http.response.headers.content_type') == 'some;value'
+
+    def test_whitelist_exact(self, span):
+        store_response_headers({
+            'Content-Type': 'some;value',
+        }, span, ['content-type'])
+        assert span.get_tag('http.response.headers.content_type') == 'some;value'
+
+    def test_whitelist_match(self, span):
+        store_response_headers({
+            'Content-Type': 'some;value',
+        }, span, ['conten*ype'])
+        assert span.get_tag('http.response.headers.content_type') == 'some;value'
+
+    def test_whitelist_case_insensitive(self, span):
+        store_response_headers({
+            'ContENt-Type': 'some;value',
+        }, span, ['conTen*ypE'])
+        assert span.get_tag('http.response.headers.content_type') == 'some;value'
