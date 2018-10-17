332b9473d6f9ca1683f9107305e2a64c543a78c4
Mickaël Guérin <mickael.guerin@people-doc.com>
2018-10-17 15:58:29 +0200

    Merge _wrap_render and _wrap_generate
    and fix single quotes

--- ddtrace/contrib/jinja2/patch.py
+++ ddtrace/contrib/jinja2/patch.py
@@ -18,30 +18,30 @@ config._add('jinja2',{
 
 
 def patch():
-    if getattr(jinja2, "__datadog_patch", False):
+    if getattr(jinja2, '__datadog_patch', False):
         # already patched
         return
-    setattr(jinja2, "__datadog_patch", True)
+    setattr(jinja2, '__datadog_patch', True)
     Pin(
         service=config.jinja2['service_name'],
         app='jinja2',
         app_type=http.TEMPLATE,
         _config=config.jinja2,
     ).onto(jinja2.environment.Environment)
-    _w(jinja2, "environment.Template.render", _wrap_render)
-    _w(jinja2, "environment.Template.generate", _wrap_generate)
-    _w(jinja2, "environment.Environment.compile", _wrap_compile)
-    _w(jinja2, "environment.Environment._load_template", _wrap_load_template)
+    _w(jinja2, 'environment.Template.render', _get_render_wrapper('jinja2.render'))
+    _w(jinja2, 'environment.Template.generate', _get_render_wrapper('jinja2.generate'))
+    _w(jinja2, 'environment.Environment.compile', _wrap_compile)
+    _w(jinja2, 'environment.Environment._load_template', _wrap_load_template)
 
 
 def unpatch():
-    if not getattr(jinja2, "__datadog_patch", False):
+    if not getattr(jinja2, '__datadog_patch', False):
         return
-    setattr(jinja2, "__datadog_patch", False)
-    _u(jinja2.Template, "render")
-    _u(jinja2.Template, "generate")
-    _u(jinja2.Environment, "compile")
-    _u(jinja2.Environment, "_load_template")
+    setattr(jinja2, '__datadog_patch', False)
+    _u(jinja2.Template, 'render')
+    _u(jinja2.Template, 'generate')
+    _u(jinja2.Environment, 'compile')
+    _u(jinja2.Environment, '_load_template')
 
 
 def _get_service_name(environment, span):
@@ -56,39 +56,24 @@ def _get_service_name(environment, span):
     return cfg['service_name']
 
 
-def _wrap_generate(wrapped, instance, args, kwargs):
-    pin = Pin.get_from(instance.environment)
-    if not pin or not pin.enabled():
-        for s in wrapped(*args, **kwargs):
-            yield s
-
-    template_name = instance.name or DEFAULT_TEMPLATE_NAME
-    with pin.tracer.trace("jinja2.generate", span_type=http.TEMPLATE) as span:
-        # update the span service name before doing any action
-        span.service = _get_service_name(instance.environment, span)
-        try:
-            for s in wrapped(*args, **kwargs):
-                yield s
-        finally:
-            span.resource = template_name
-            span.set_tag("jinja2.template_name", template_name)
-
-
-def _wrap_render(wrapped, instance, args, kwargs):
-    pin = Pin.get_from(instance.environment)
-    if not pin or not pin.enabled():
-        return wrapped(*args, **kwargs)
-
-    template_name = instance.name or DEFAULT_TEMPLATE_NAME
-    print("render tracer", id(pin.tracer))
-    with pin.tracer.trace("jinja2.render", span_type=http.TEMPLATE) as span:
-        # update the span service name before doing any action
-        span.service = _get_service_name(instance.environment, span)
-        try:
+def _get_render_wrapper(operation):
+    def _wrap_render(wrapped, instance, args, kwargs):
+        """Wrap `Template.render()` or `Template.generate()`
+        """
+        pin = Pin.get_from(instance.environment)
+        if not pin or not pin.enabled():
             return wrapped(*args, **kwargs)
-        finally:
-            span.resource = template_name
-            span.set_tag("jinja2.template_name", template_name)
+
+        template_name = instance.name or DEFAULT_TEMPLATE_NAME
+        with pin.tracer.trace(operation, span_type=http.TEMPLATE) as span:
+            # update the span service name before doing any action
+            span.service = _get_service_name(instance.environment, span)
+            try:
+                return wrapped(*args, **kwargs)
+            finally:
+                span.resource = template_name
+                span.set_tag('jinja2.template_name', template_name)
+    return _wrap_render
 
 
 def _wrap_compile(wrapped, instance, args, kwargs):
@@ -99,16 +84,16 @@ def _wrap_compile(wrapped, instance, args, kwargs):
     if len(args) > 1:
         template_name = args[1]
     else:
-        template_name = kwargs.get("name", DEFAULT_TEMPLATE_NAME)
+        template_name = kwargs.get('name', DEFAULT_TEMPLATE_NAME)
 
-    with pin.tracer.trace("jinja2.compile", span_type=http.TEMPLATE) as span:
+    with pin.tracer.trace('jinja2.compile', span_type=http.TEMPLATE) as span:
         # update the span service name before doing any action
         span.service = _get_service_name(instance, span)
         try:
             return wrapped(*args, **kwargs)
         finally:
             span.resource = template_name
-            span.set_tag("jinja2.template_name", template_name)
+            span.set_tag('jinja2.template_name', template_name)
 
 
 def _wrap_load_template(wrapped, instance, args, kwargs):
@@ -116,8 +101,8 @@ def _wrap_load_template(wrapped, instance, args, kwargs):
     if not pin or not pin.enabled():
         return wrapped(*args, **kwargs)
 
-    template_name = kwargs.get("name", args[0])
-    with pin.tracer.trace("jinja2.load", span_type=http.TEMPLATE) as span:
+    template_name = kwargs.get('name', args[0])
+    with pin.tracer.trace('jinja2.load', span_type=http.TEMPLATE) as span:
         # update the span service name before doing any action
         span.service = _get_service_name(instance, span)
         template = None
@@ -126,6 +111,6 @@ def _wrap_load_template(wrapped, instance, args, kwargs):
             return template
         finally:
             span.resource = template_name
-            span.set_tag("jinja2.template_name", template_name)
+            span.set_tag('jinja2.template_name', template_name)
             if template:
-                span.set_tag("jinja2.template_path", template.filename)
+                span.set_tag('jinja2.template_path', template.filename)
