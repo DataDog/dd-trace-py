673524a68d9e898cca9bc914315117c47153afbe
Kyle Verhoog <kyle.verhoog@datadoghq.com>
2018-10-19 17:41:28 +0200

    [falcon] enable by default, add idempotence check (#660)

--- ddtrace/contrib/falcon/patch.py
+++ ddtrace/contrib/falcon/patch.py
@@ -2,7 +2,7 @@ import os
 import wrapt
 import falcon
 
-from ddtrace import tracer
+import ddtrace
 
 from .middleware import TraceMiddleware
 from ...utils.formats import asbool
@@ -24,8 +24,22 @@ def traced_init(wrapped, instance, args, kwargs):
     service = os.environ.get('DATADOG_SERVICE_NAME') or 'falcon'
     distributed_tracing = asbool(os.environ.get(
         'DATADOG_FALCON_DISTRIBUTED_TRACING')) or False
-
-    mw.insert(0, TraceMiddleware(tracer, service, distributed_tracing))
+    # It appears that the only way to install middleware is via the initializer
+    # so it's sufficient to check the middleware for our middleware to
+    # ensure idempotence.
+    # If the tracing middleware is already included then defer to that
+    if not _find_dd_middleware(mw):
+        mw.insert(0, TraceMiddleware(ddtrace.tracer, service, distributed_tracing))
     kwargs['middleware'] = mw
-
     wrapped(*args, **kwargs)
+
+
+def _find_dd_middleware(middlewares):
+    """
+    Returns whether or not the Datadog middleware is in a list of falcon
+    middlewares.
+    """
+    for mw in middlewares:
+        if isinstance(mw, TraceMiddleware):
+            return True
+    return False
