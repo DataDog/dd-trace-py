d4bc9e388917386c72178ff6cd7a48f8fff5e740
brettlangdon <brett.langdon@datadoghq.com>
2018-10-25 14:22:47 -0400

    [core] add Tracer.on and Tracer.emit for span hooks

--- ddtrace/contrib/falcon/middleware.py
+++ ddtrace/contrib/falcon/middleware.py
@@ -76,6 +76,12 @@ class TraceMiddleware(object):
                 status = _detect_and_set_status_error(err_type, span)
 
         span.set_tag(httpx.STATUS_CODE, status)
+
+        # Emit span hook for this response
+        # DEV: Emit before closing so they can overwrite `span.resource` if they want
+        self.tracer.emit(span, req, resp)
+
+        # Close the span
         span.finish()
 
 
