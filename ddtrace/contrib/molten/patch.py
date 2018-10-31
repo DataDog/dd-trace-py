cdac752eed3f7ac0ada3101c2c65981303b866dd
Tahir H. Butt <tahir.butt@datadoghq.com>
2018-10-30 21:49:19 -0400

    Patching components

--- ddtrace/contrib/molten/patch.py
+++ ddtrace/contrib/molten/patch.py
@@ -1,14 +1,17 @@
-import molten
-from ddtrace import config, Pin
-from ddtrace.utils.formats import get_env
-from ...ext import AppTypes
 import inspect
 
 import wrapt
 
+import molten
+from ddtrace import Pin, config
+from ddtrace.utils.formats import get_env
+from ddtrace.utils.importlib import func_name
+
+from ...ext import AppTypes
+
 # Configure default configuration
 config._add('molten', dict(
-    service_name=get_env('molten', 'service_name'),
+    service_name=get_env('molten', 'service_name', 'molten'),
     app='molten',
     app_type=AppTypes.web,
     distributed_tracing_enabled=False,
@@ -16,39 +19,42 @@ config._add('molten', dict(
 
 def patch():
     """Patch the instrumented methods
-
-    This duplicated doesn't look nice. The nicer alternative is to use an ObjectProxy on top
-    of Redis and StrictRedis. However, it means that any "import redis.Redis" won't be instrumented.
     """
     if getattr(molten, '_datadog_patch', False):
         return
     setattr(molten, '_datadog_patch', True)
 
-    Pin(
+    pin = Pin(
         service=config.molten['service_name'],
         app=config.molten['app'],
         app_type=config.molten['app_type'],
-    ).onto(molten.App)
+    )
+
+    # add pin to module since many classes use __slots__
+    pin.onto(molten)
 
     _w = wrapt.wrap_function_wrapper
-    _w('molten', 'App.__init__', trace_app_init)
-    _w('molten', 'App.__call__', trace_app_call)
+    _w('molten', 'App.__init__', patch_app_init)
+    _w('molten', 'App.__call__', patch_app_call)
+    _w('molten', 'Router.add_route', patch_add_route)
 
-def trace_app_call(wrapped, instance, args, kwargs):
-    pin = Pin.get_from(instance)
-    if not pin or not pin.enabled():
-        return wrapped(*args, **kwargs)
-
-    with pin.tracer.trace('molten.request', service='molten'):
-        return wrapped(*args, **kwargs)
-
-def wrap_middleware(instance, middleware):
-    # we want to trace both the __call__ as well as the function that is returned
-
+def trace_func(resource):
     @wrapt.function_wrapper
-    def trace_middleware(wrapped, _instance, args, kwargs):
+    def _trace_func(wrapped, instance, args, kwargs):
+        pin = Pin.get_from(molten)
 
-        pin = Pin.get_from(instance)
+        if not pin or not pin.enabled():
+            return wrapped(*args, **kwargs)
+
+        with pin.tracer.trace(func_name(wrapped), service=pin.service, resource=resource):
+            return wrapped(*args, **kwargs)
+
+    return _trace_func
+
+def trace_middleware(middleware):
+    @wrapt.function_wrapper
+    def _trace_middleware(wrapped, instance, args, kwargs):
+        pin = Pin.get_from(molten)
         if not pin or not pin.enabled():
             return wrapped(*args, **kwargs)
 
@@ -58,23 +64,63 @@ def wrap_middleware(instance, middleware):
         else:
             name = type(wrapped).__name__
 
-        with pin.tracer.trace(name, service='molten'):
+        with pin.tracer.trace(name, service=pin.service):
+            return trace_func(name)(wrapped(*args, **kwargs))
+
+    return _trace_middleware(middleware)
+
+
+def patch_add_route(wrapped, instance, args, kwargs):
+    pin = Pin.get_from(molten)
+
+    def _wrap(route_like, prefix="", namespace=None):
+        # avoid patching non-Route, e.g. Include
+        if not isinstance(route_like, molten.Route):
             return wrapped(*args, **kwargs)
 
-    return trace_middleware(middleware, instance)
+        resource = '{} {}'.format(
+            route_like.method,
+            route_like.name or route_like.template
+        )
 
-def trace_app_init(wrapped, instance, args, kwargs):
+        # patch handler for route
+        route_like.handler = trace_func(resource)(route_like.handler)
+
+        return wrapped(route_like, prefix=prefix, namespace=namespace)
+
+    return _wrap(*args, **kwargs)
+
+
+def patch_app_call(wrapped, instance, args, kwargs):
+    pin = Pin.get_from(molten)
+    if not pin or not pin.enabled():
+        return wrapped(*args, **kwargs)
+
+    # trace call
+    with pin.tracer.trace(func_name(wrapped), service=pin.service):
+        return wrapped(*args, **kwargs)
+
+
+def patch_app_init(wrapped, instance, args, kwargs):
+    # intercept components and patch methods
+    if 'components' in kwargs:
+        components = kwargs['components']
+        for component in components:
+            component.can_handle_parameter = trace_func(component.__class__.__name__)(component.can_handle_parameter)
+            component.resolve = trace_func(component.__class__.__name__)(component.resolve)
+        kwargs['components'] = components
+
     # allow instance to be initialized with middleware
     wrapped(*args, **kwargs)
 
     # add Pin to instance
-    pin = Pin.get_from(instance)
+    pin = Pin.get_from(molten)
+
     if not pin or not pin.enabled():
         return
 
-    # wrap each middleware in instance
+    # trace middleware in instance
     instance.middleware = [
-        wrap_middleware(instance, mw)
+        trace_middleware(mw)
         for mw in instance.middleware
     ]
-    pass
