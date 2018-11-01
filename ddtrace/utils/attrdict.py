917c2e14aff511d09432201b962c0afc51507a77
brettlangdon <brett.langdon@datadoghq.com>
2018-11-01 10:33:23 -0400

    [core] AttrDict Add documentation

--- ddtrace/utils/attrdict.py
+++ ddtrace/utils/attrdict.py
@@ -1,4 +1,21 @@
 class AttrDict(dict):
+    """
+    dict implementation that allows for item attribute access
+
+
+    Example::
+
+       data = AttrDict()
+       data['key'] = 'value'
+       print(data['key'])
+
+       data.key = 'new-value'
+       print(data.key)
+
+       # Convert an existing `dict`
+       data = AttrDict(dict(key='value'))
+       print(data.key)
+    """
     def __getattr__(self, key):
         if key in self:
             return self[key]
