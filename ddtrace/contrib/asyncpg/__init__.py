6b160d8291da9b7fa5829a918abc77cb230cbbcc
Alexander Mohr <thehesiod@hotmail.com>
2018-11-06 14:27:31 -0800

    fix warning

--- ddtrace/contrib/asyncpg/__init__.py
+++ ddtrace/contrib/asyncpg/__init__.py
@@ -14,7 +14,7 @@ Instrument `asyncpg` to report a span for each executed Postgres queries::
     # Use a pin to specify metadata related to this connection
     Pin.override(db, service='postgres-users')
 """
-from ..util import require_modules
+from ...utils.importlib import require_modules
 
 
 required_modules = ['asyncpg']
