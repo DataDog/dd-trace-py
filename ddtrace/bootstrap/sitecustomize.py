0230d5d22f619492ed32a735f45e6bf0d969c98e
Tahir H. Butt <tahir@tahirbutt.com>
2018-11-14 10:07:46 -0500

    [core] Add global tracer tags (#702)
    * Initial attempt at global tags
    
    * Use patch module list approach for global trace tags
    
    * Add sdist flag to testrunner start
    
    * remove environment variable
    
    * [ddtracerun] Fix tests for global tags
    
    * Remove type casting for global tags
    
    * Use partition and fix test
    
    * Rename to `DD_TRACE_GLOBAL_TAGS`
    
    * Prefer single quotes

--- ddtrace/bootstrap/sitecustomize.py
+++ ddtrace/bootstrap/sitecustomize.py
@@ -43,6 +43,18 @@ def update_patched_modules():
         EXTRA_PATCHED_MODULES.update({module: should_patch.lower() == 'true'})
 
 
+def add_global_tags(tracer):
+    tags = {}
+    for tag in os.environ.get('DD_TRACE_GLOBAL_TAGS', '').split(','):
+        tag_name, _, tag_value = tag.partition(':')
+        if not tag_name or not tag_value:
+            log.debug('skipping malformed tracer tag')
+            continue
+
+        tags[tag_name] = tag_value
+    tracer.set_tags(tags)
+
+
 try:
     from ddtrace import tracer
     patch = True
@@ -80,6 +92,9 @@ try:
 
     if 'DATADOG_ENV' in os.environ:
         tracer.set_tags({"env": os.environ["DATADOG_ENV"]})
+
+    if 'DD_TRACE_GLOBAL_TAGS' in os.environ:
+        add_global_tags(tracer)
 
     # Ensure sitecustomize.py is properly called if available in application directories:
     # * exclude `bootstrap_dir` from the search
