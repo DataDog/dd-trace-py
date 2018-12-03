7d708057fb4bcb33fc2fbc988c8c27446b7838b7
brettlangdon <brett.langdon@datadoghq.com>
2018-12-03 15:20:18 -0500

    [core] vendor wrapt==1.10.11

--- ddtrace/vendor/__init__.py
+++ ddtrace/vendor/__init__.py
@@ -13,4 +13,18 @@ License: MIT
 Notes:
   `six/__init__.py` is just the source code's `six.py`
   `curl https://raw.githubusercontent.com/benjaminp/six/1.11.0/six.py > ddtrace/vendor/six/__init__.py`
+
+
+wrapt
+-----
+
+Website: https://wrapt.readthedocs.io/en/latest/
+Source: https://github.com/GrahamDumpleton/wrapt/
+Version: 1.10.11
+License: BSD 2-Clause "Simplified" License
+
+Notes:
+  `wrapt/__init__.py` was updated to include a copy of `wrapt`'s license: https://github.com/GrahamDumpleton/wrapt/blob/1.10.11/LICENSE
+
+  `setup.py` will attempt to build the `wrapt/_wrappers.c` C module
 """
