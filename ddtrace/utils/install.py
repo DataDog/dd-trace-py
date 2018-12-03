58145dd301aa95a45349b9591907278d2d7e15c2
kyle-verhoog <kyle@verhoog.ca>
2018-12-03 19:06:58 +0100

    [core] add install logic

---
+++ ddtrace/utils/install.py
@@ -0,0 +1,82 @@
+import sys
+
+from wrapt.importer import (
+    register_post_import_hook,
+    _post_import_hooks,
+    _post_import_hooks_lock,
+)
+from wrapt.decorators import synchronized
+
+
+def install_module_import_hook(modulename, modulehook):
+    """Installs a module import hook for a given module.
+
+    A flag is also stored and checked on the module to so that the module is
+    not instrumented more than once.
+    """
+    # wrap the module hook with an idempotence check
+    def check_patched_hook(module):
+        if module_patched(module):
+            return
+        _mark_module_patched(module)
+        modulehook(module)
+    setattr(check_patched_hook, '_datadog_hook', True)
+    register_post_import_hook(check_patched_hook, modulename)
+
+
+def _hook_matcher(hook):
+    """Matches functions that are hooks."""
+    return hasattr(hook, '_datadog_hook')
+
+
+def uninstall_module_import_hook(modulename):
+    """Uninstalls a module import hook for a module given its name.
+
+    Note: any patching done on the module will still apply, this function
+    simply removed/disables the import hook.
+    """
+    if modulename in sys.modules:
+        module = sys.modules[modulename]
+        _mark_module_unpatched(module)
+    _deregister_post_import_hook(modulename, _hook_matcher)
+
+
+@synchronized(_post_import_hooks_lock)
+def _deregister_post_import_hook(modulename, matcher):
+    """
+    Deregisters post import hooks for a module given the module name and a
+    matcher function. All hooks matching the matcher function will be removed.
+    """
+    hooks = _post_import_hooks.get(modulename, []) or []
+    hooks = list(filter(lambda h: not matcher(h), hooks))
+
+    # Work around for wrapt since wrapt assumes that if
+    # _post_import_hooks.get(modulename) is not None then the module must have
+    # been imported.
+    if not len(hooks):
+        hooks = None
+    _post_import_hooks[modulename] = hooks
+
+
+def _mark_module_patched(module):
+    """
+    Marks a module as being patched.
+    """
+    setattr(module, '_datadog_patch', True)
+
+
+def module_patched(module):
+    """
+    Returns whether a given module is patched.
+    """
+    return getattr(module, '_datadog_patch', False)
+
+
+def _mark_module_unpatched(module):
+    """
+    Marks a module as being unpatched.
+    """
+    if not getattr(module, '_datadog_patch', False):
+        return
+
+    setattr(module, '_datadog_patch', False)
---
+++ tests/utils/test_install.py
@@ -0,0 +1,100 @@
+import mock
+import sys
+import types
+
+from tests.subprocesstest import SubprocessTestCase, run_in_subprocess
+
+from ddtrace.utils.install import (
+    install_module_import_hook,
+    uninstall_module_import_hook,
+    _mark_module_patched,
+    _mark_module_unpatched,
+    module_patched,
+)
+
+
+@run_in_subprocess
+class TestInstallUtils(SubprocessTestCase):
+    def test_mark_module_patched(self):
+        module = types.ModuleType('module')
+        _mark_module_patched(module)
+        self.assertTrue(module_patched(module))
+
+    def test_mark_module_unpatched(self):
+        module = types.ModuleType('module')
+        _mark_module_unpatched(module)
+        self.assertFalse(module_patched(module))
+
+    def test_mark_module_unpatched_after_patch(self):
+        module = types.ModuleType('module')
+        _mark_module_patched(module)
+        _mark_module_unpatched(module)
+        self.assertFalse(module_patched(module))
+
+    def test_install_module_import_hook_on_import(self):
+        """
+        Test that import hooks are called when the module is imported for the
+        first time.
+        """
+        test_hook = mock.MagicMock()
+        install_module_import_hook('tests.utils.test_module', test_hook)
+        self.assertEqual(test_hook.called, 0, 'test_hook should not be called until module import')
+
+        import tests.utils.test_module  # noqa
+        test_hook.assert_called_once()
+
+    def test_install_module_import_hook_reload(self):
+        """
+        Import hooks should only be called once if the hooked module is imported
+        twice.
+        """
+        def hook(mod):
+            def patch_fn(self):
+                return 2
+            mod.A.fn = patch_fn
+        install_module_import_hook('tests.utils.test_module', hook)
+
+        import tests.utils.test_module  # noqa
+        self.assertEqual(tests.utils.test_module.A().fn(), 2)
+
+        # TODO: the assert below should be
+        # >>> assert tests.utils.test_module.A().fn() == 2
+        # but wrapt uninstalls the import hooks after they are fired
+        # https://github.com/GrahamDumpleton/wrapt/blob/4bcd190457c89e993ffcfec6dad9e9969c033e9e/src/wrapt/importer.py#L127-L136
+
+        # remove the module from sys.modules to force a reimport
+        del sys.modules['tests.utils.test_module']
+        import tests.utils.test_module  # noqa
+        self.assertEqual(tests.utils.test_module.A().fn(), 1)
+
+    def test_install_module_import_hook_already_imported(self):
+        """
+        Test that import hooks are fired when the module is already imported.
+        """
+        module = types.ModuleType('somewhere')
+        sys.modules['some.module.somewhere'] = module
+        test_hook = mock.MagicMock()
+        install_module_import_hook('some.module.somewhere', test_hook)
+        test_hook.assert_called_once()
+
+    def test_uninstall_module_import_hook(self):
+        """
+        Test that a module import hook can be uninstalled.
+        """
+        test_hook = mock.MagicMock()
+        install_module_import_hook('tests.utils.test_module', test_hook)
+        uninstall_module_import_hook('tests.utils.test_module')
+        import tests.utils.test_module  # noqa
+        self.assertEqual(test_hook.called, 0, 'test_hook should not be called')
+
+    def test_install_uninstall_install(self):
+        """
+        Test that a module import hook can be installed, uninstalled and
+        reinstalled.
+        """
+        test_hook = mock.MagicMock()
+        install_module_import_hook('tests.utils.test_module', test_hook)
+        uninstall_module_import_hook('tests.utils.test_module')
+        install_module_import_hook('tests.utils.test_module', test_hook)
+        import tests.utils.test_module  # noqa
+        test_hook.assert_called_once()
