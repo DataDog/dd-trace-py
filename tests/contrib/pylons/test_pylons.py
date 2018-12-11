0dec01efcbe21c754e8eb6800c3ab83e14a20072
brettlangdon <brett.langdon@datadoghq.com>
2018-12-10 21:04:48 -0500

    fix pylons test

--- tests/contrib/pylons/test_pylons.py
+++ tests/contrib/pylons/test_pylons.py
@@ -289,7 +289,7 @@ class PylonsTestCase(TestCase):
 
         ok_(span.trace_id != 100)
         ok_(span.parent_id != 42)
-        eq_(span.get_metric(SAMPLING_PRIORITY_KEY), None)
+        ok_(span.get_metric(SAMPLING_PRIORITY_KEY) != 2)
 
     def test_distributed_tracing_enabled(self):
         # ensure distributed tracing propagator is working
