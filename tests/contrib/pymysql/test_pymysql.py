f19b105a3bfa17ee8213452f0b00f65e7401088d
brettlangdon <brett.langdon@datadoghq.com>
2018-12-20 11:51:30 -0500

    fix pymysql tests

--- tests/contrib/pymysql/test_pymysql.py
+++ tests/contrib/pymysql/test_pymysql.py
@@ -1,7 +1,6 @@
 # 3p
 import pymysql
 
-from unittest import TestCase
 from nose.tools import eq_
 
 # project
@@ -12,8 +11,8 @@ from ddtrace.contrib.pymysql.patch import patch, unpatch
 
 # testing
 from tests.opentracer.utils import init_tracer
+from ...base import BaseTracerTestCase
 from ...util import assert_dict_issuperset
-from ...test_tracer import get_dummy_tracer
 from ...contrib.config import MYSQL_CONFIG
 
 
@@ -38,9 +37,11 @@ class PyMySQLCore(object):
         })
 
     def setUp(self):
+        super(PyMySQLCore, self).setUp()
         patch()
 
     def tearDown(self):
+        super(PyMySQLCore, self).tearDown()
         if self.conn and not self.conn._closed:
             self.conn.close()
         unpatch()
@@ -57,7 +58,7 @@ class PyMySQLCore(object):
         rows = cursor.fetchall()
         eq_(len(rows), 1)
         spans = writer.pop()
-        eq_(len(spans), 2)
+        eq_(len(spans), 1)
 
         span = spans[0]
         eq_(span.service, self.TEST_SERVICE)
@@ -68,8 +69,28 @@ class PyMySQLCore(object):
         meta.update(self.DB_INFO)
         assert_dict_issuperset(span.meta, meta)
 
-        fetch_span = spans[1]
-        eq_(fetch_span.name, 'pymysql.query.fetchall')
+    def test_simple_query_fetchall(self):
+        with self.override_config('dbapi2', dict(trace_fetch_methods=True)):
+            conn, tracer = self._get_conn_tracer()
+            writer = tracer.writer
+            cursor = conn.cursor()
+            cursor.execute('SELECT 1')
+            rows = cursor.fetchall()
+            eq_(len(rows), 1)
+            spans = writer.pop()
+            eq_(len(spans), 2)
+
+            span = spans[0]
+            eq_(span.service, self.TEST_SERVICE)
+            eq_(span.name, 'pymysql.query')
+            eq_(span.span_type, 'sql')
+            eq_(span.error, 0)
+            meta = {}
+            meta.update(self.DB_INFO)
+            assert_dict_issuperset(span.meta, meta)
+
+            fetch_span = spans[1]
+            eq_(fetch_span.name, 'pymysql.query.fetchall')
 
     def test_query_with_several_rows(self):
         conn, tracer = self._get_conn_tracer()
@@ -80,10 +101,23 @@ class PyMySQLCore(object):
         rows = cursor.fetchall()
         eq_(len(rows), 3)
         spans = writer.pop()
-        eq_(len(spans), 2)
+        eq_(len(spans), 1)
+        self.assertEqual(spans[0].name, 'pymysql.query')
 
-        fetch_span = spans[1]
-        eq_(fetch_span.name, 'pymysql.query.fetchall')
+    def test_query_with_several_rows_fetchall(self):
+        with self.override_config('dbapi2', dict(trace_fetch_methods=True)):
+            conn, tracer = self._get_conn_tracer()
+            writer = tracer.writer
+            cursor = conn.cursor()
+            query = 'SELECT n FROM (SELECT 42 n UNION SELECT 421 UNION SELECT 4210) m'
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            eq_(len(rows), 3)
+            spans = writer.pop()
+            eq_(len(spans), 2)
+
+            fetch_span = spans[1]
+            eq_(fetch_span.name, 'pymysql.query.fetchall')
 
     def test_query_many(self):
         # tests that the executemany method is correctly wrapped.
@@ -112,11 +146,42 @@ class PyMySQLCore(object):
         eq_(rows[1][1], "this is foo")
 
         spans = writer.pop()
-        eq_(len(spans), 3)
+        eq_(len(spans), 2)
         cursor.execute("drop table if exists dummy")
 
-        fetch_span = spans[2]
-        eq_(fetch_span.name, 'pymysql.query.fetchall')
+    def test_query_many_fetchall(self):
+        with self.override_config('dbapi2', dict(trace_fetch_methods=True)):
+            # tests that the executemany method is correctly wrapped.
+            conn, tracer = self._get_conn_tracer()
+            writer = tracer.writer
+            tracer.enabled = False
+            cursor = conn.cursor()
+
+            cursor.execute("""
+                create table if not exists dummy (
+                    dummy_key VARCHAR(32) PRIMARY KEY,
+                    dummy_value TEXT NOT NULL)""")
+            tracer.enabled = True
+
+            stmt = "INSERT INTO dummy (dummy_key, dummy_value) VALUES (%s, %s)"
+            data = [("foo", "this is foo"),
+                    ("bar", "this is bar")]
+            cursor.executemany(stmt, data)
+            query = "SELECT dummy_key, dummy_value FROM dummy ORDER BY dummy_key"
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            eq_(len(rows), 2)
+            eq_(rows[0][0], "bar")
+            eq_(rows[0][1], "this is bar")
+            eq_(rows[1][0], "foo")
+            eq_(rows[1][1], "this is foo")
+
+            spans = writer.pop()
+            eq_(len(spans), 3)
+            cursor.execute("drop table if exists dummy")
+
+            fetch_span = spans[2]
+            eq_(fetch_span.name, 'pymysql.query.fetchall')
 
     def test_query_proc(self):
         conn, tracer = self._get_conn_tracer()
@@ -174,8 +239,8 @@ class PyMySQLCore(object):
             eq_(len(rows), 1)
 
         spans = writer.pop()
-        eq_(len(spans), 3)
-        ot_span, dd_span, fetch_span = spans
+        eq_(len(spans), 2)
+        ot_span, dd_span = spans
 
         # confirm parenting
         eq_(ot_span.parent_id, None)
@@ -192,7 +257,38 @@ class PyMySQLCore(object):
         meta.update(self.DB_INFO)
         assert_dict_issuperset(dd_span.meta, meta)
 
-        eq_(fetch_span.name, 'pymysql.query.fetchall')
+    def test_simple_query_ot_fetchall(self):
+        """OpenTracing version of test_simple_query."""
+        with self.override_config('dbapi2', dict(trace_fetch_methods=True)):
+            conn, tracer = self._get_conn_tracer()
+            writer = tracer.writer
+            ot_tracer = init_tracer('mysql_svc', tracer)
+            with ot_tracer.start_active_span('mysql_op'):
+                cursor = conn.cursor()
+                cursor.execute("SELECT 1")
+                rows = cursor.fetchall()
+                eq_(len(rows), 1)
+
+            spans = writer.pop()
+            eq_(len(spans), 3)
+            ot_span, dd_span, fetch_span = spans
+
+            # confirm parenting
+            eq_(ot_span.parent_id, None)
+            eq_(dd_span.parent_id, ot_span.span_id)
+
+            eq_(ot_span.service, 'mysql_svc')
+            eq_(ot_span.name, 'mysql_op')
+
+            eq_(dd_span.service, self.TEST_SERVICE)
+            eq_(dd_span.name, 'pymysql.query')
+            eq_(dd_span.span_type, 'sql')
+            eq_(dd_span.error, 0)
+            meta = {}
+            meta.update(self.DB_INFO)
+            assert_dict_issuperset(dd_span.meta, meta)
+
+            eq_(fetch_span.name, 'pymysql.query.fetchall')
 
     def test_commit(self):
         conn, tracer = self._get_conn_tracer()
@@ -215,10 +311,9 @@ class PyMySQLCore(object):
         eq_(span.name, 'pymysql.connection.rollback')
 
 
-class TestPyMysqlPatch(PyMySQLCore, TestCase):
+class TestPyMysqlPatch(PyMySQLCore, BaseTracerTestCase):
     def _get_conn_tracer(self):
         if not self.conn:
-            tracer = get_dummy_tracer()
             self.conn = pymysql.connect(**MYSQL_CONFIG)
             assert not self.conn._closed
             # Ensure that the default pin is there, with its default value
@@ -227,9 +322,9 @@ class TestPyMysqlPatch(PyMySQLCore, TestCase):
             assert pin.service == 'pymysql'
             # Customize the service
             # we have to apply it on the existing one since new one won't inherit `app`
-            pin.clone(service=self.TEST_SERVICE, tracer=tracer).onto(self.conn)
+            pin.clone(service=self.TEST_SERVICE, tracer=self.tracer).onto(self.conn)
 
-            return self.conn, tracer
+            return self.conn, self.tracer
 
     def test_patch_unpatch(self):
         unpatch()
@@ -240,12 +335,11 @@ class TestPyMysqlPatch(PyMySQLCore, TestCase):
 
         patch()
         try:
-            tracer = get_dummy_tracer()
-            writer = tracer.writer
+            writer = self.tracer.writer
             conn = pymysql.connect(**MYSQL_CONFIG)
             pin = Pin.get_from(conn)
             assert pin
-            pin.clone(service=self.TEST_SERVICE, tracer=tracer).onto(conn)
+            pin.clone(service=self.TEST_SERVICE, tracer=self.tracer).onto(conn)
             assert not conn._closed
 
             cursor = conn.cursor()
@@ -253,7 +347,7 @@ class TestPyMysqlPatch(PyMySQLCore, TestCase):
             rows = cursor.fetchall()
             eq_(len(rows), 1)
             spans = writer.pop()
-            eq_(len(spans), 2)
+            eq_(len(spans), 1)
 
             span = spans[0]
             eq_(span.service, self.TEST_SERVICE)
@@ -264,10 +358,6 @@ class TestPyMysqlPatch(PyMySQLCore, TestCase):
             meta = {}
             meta.update(self.DB_INFO)
             assert_dict_issuperset(span.meta, meta)
-
-            fetch_span = spans[1]
-            eq_(fetch_span.name, 'pymysql.query.fetchall')
-
         finally:
             unpatch()
 
