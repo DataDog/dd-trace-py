da664ecf1eae0c1690924734d8c9fdf3f6acf8df
Tahir H. Butt <tahir.butt@datadoghq.com>
2019-01-04 11:47:08 -0500

    Move logging doc to module

--- ddtrace/contrib/logging/__init__.py
+++ ddtrace/contrib/logging/__init__.py
@@ -1,10 +1,47 @@
 """
-The patching of standard library logging to inject tracing information
+Datadog APM traces can be integrated with Logs by first having the tracing
+library patch the standard library ``logging`` module and updating the log
+formatter used by an application. This feature enables you to inject the current
+trace information into a log entry.
 
-Example::
+Before the trace information can be injected into logs, the formatter has to be
+updated to include ``dd.trace_id`` and ``dd.span_id`` attributes from the log
+record. The integration with Logs occurs as long as the log entry includes
+``trace_id=%(dd.trace_id)s`` and ``span_id=%(dd.span_id)s``.
+
+ddtrace-run
+^^^^^^^^^^^
+
+When using ``ddtrace-run``, enable patching by setting the environment variable
+``DD_LOGS_INJECTION=true`` then update the log formatter as in the following
+example::
+
     import logging
     from ddtrace import tracer
+
+    logging.basicConfig(
+        format=('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] - %(message)s'
+                ' - dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s')
+    )
+    log = logging.getLogger()
+    log.level = logging.INFO
+
+
+    @tracer.wrap()
+    def foo():
+        log.info('Hello!')
+
+    foo()
+
+Manual Instrumentation
+^^^^^^^^^^^^^^^^^^^^^^
+
+If you prefer to instrument manually, patch the logging library then update the
+log formatter as in the following example::
+
     from ddtrace import patch_all; patch_all(logging=True)
+    import logging
+    from ddtrace import tracer
 
     logging.basicConfig(
         format=('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] - %(message)s'
