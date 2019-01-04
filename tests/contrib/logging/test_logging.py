d704c93e08dec60236582ea474340e782e5e1fd0
Brett Langdon <me@brett.is>
2019-01-04 15:11:15 -0500

    Update tests/contrib/logging/test_logging.py
    Co-Authored-By: majorgreys <tahir@tahirbutt.com>

--- tests/contrib/logging/test_logging.py
+++ tests/contrib/logging/test_logging.py
@@ -87,7 +87,7 @@ class LoggingTestCase(BaseTracerTestCase):
             # with format string for trace info
             output, _ = capture_function_log(
                 func,
-                fmt='%(message)s - dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s'
+                fmt='%(message)s - dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s',
             )
             self.assertEqual(
                 output,
