c29f31ccd3942d21a252a1509c092a0902046ade
kyle-verhoog <kyle@verhoog.ca>
2019-02-02 00:12:27 -0500

    [metrics] initial implementation
    - add data structures with tests
    - add collectors for psutil and platform modules
    - send metrics to agent statsd

---
+++ ddtrace/runtime_metrics.py
@@ -0,0 +1,239 @@
+import os
+import platform
+import time
+import logging
+import importlib
+from ddtrace import __version__
+
+from ddtrace.settings import config
+
+DEFAULT_AGENT_HOST = '127.0.0.1'
+DEFAULT_METRIC_AGENT_PORT = 8125
+
+logging.basicConfig(level=logging.DEBUG)
+
+# TODO: look at gc
+# TODO: forking/multi-process app
+
+log = logging.getLogger(__name__)
+
+
+def process_id():
+    """Returns the current process id of this python process.
+    """
+    return os.getpid()
+
+
+def interpreter_implementation(modules, keys):
+    """Returns the Python interpreter implementation.
+
+    For CPython this is 'CPython'.
+    For Pypy this is 'PyPy'.
+    For Jython this is 'Jython'.
+    """
+    return {
+        'datadog.tracer.lang_interpreter': platform.python_implementation()
+    }
+
+
+def interpreter_version(modules, keys):
+    """Returns the interpreter version as a string.
+
+    >>>> interpreter_version()
+    '2.7.13'
+    """
+    if 'datadog.tracer.lang_version' not in keys:
+        return {}
+    return {
+        'datadog.tracer.lang_version': platform.python_version()
+    }
+
+def tracer_version(modules, keys):
+    """Returns the ddtrace version."""
+    if 'datadog.tracer.version' not in keys:
+        return {}
+    return {
+        'datadog.tracer.version': __version__
+    }
+
+
+# Default metrics to collect
+DEFAULT_ENABLED_METRICS = set([
+    'datadog.tracer.runtime.thread_count',
+    'datadog.tracer.runtime.mem.rss',
+])
+
+# Default tags to apply to metrics
+DEFAULT_ENABLED_TAGS = set([
+    'datadog.tracer.lang',
+    'datadog.tracer.lang_interpreter',
+    'datadog.tracer.lang_version',
+    'datadog.tracer.version',
+])
+
+
+class ValueCollector(object):
+    """A basic state machine useful for collecting, caching and updating data
+    obtained from different Python modules.
+
+    The two primary use-cases are
+    1) data loaded once (like tagging information)
+    2) periodically updating data sources (like thread count)
+
+    Functionality is provided for requiring modules which may or may not be
+    installed.
+    """
+    def __init__(self, collect_fn=None, enabled=True, periodic=False, required_modules=None):
+        self._collect_fn = collect_fn
+        self.enabled = enabled
+        self.periodic = periodic
+        self.required_modules = required_modules or []
+
+        self.value = None
+        self.value_loaded = False
+
+        self.modules = self._load_modules()
+
+    def _load_modules(self):
+        modules = {}
+        try:
+            for module in self.required_modules:
+                modules[module] = importlib.import_module(module)
+        except ImportError:
+            # DEV: disable collector if we cannot load any of the required modules
+            self.enabled = False
+            log.warn('Could not import module "{}" for {}'.format(module, self))
+            return None
+        return modules
+
+    def collect_fn(self, modules, keys):
+        """Returns metrics given a set of keys and provided modules.
+        :param modules: modules loaded from `required_modules`
+        :param keys: set of keys to collect
+        :return: collected metrics as a dict
+        """
+        if not self._collect_fn:
+            raise NotImplementedError('A collect function must be implemented')
+        return self._collect_fn(modules, keys)
+
+    def collect(self, keys=None):
+        """Returns metrics as collected by `collect_fn`.
+
+        :param keys: The keys of the metrics to collect.
+        """
+        if not self.enabled:
+            return self.value
+
+        keys = keys or set()
+
+        if not self.periodic and self.value_loaded:
+            return self.value
+
+        self.value = self.collect_fn(self.modules, keys)
+        self.value_loaded = True
+        return self.value
+
+    def __repr__(self):
+        return '<Collector(enabled={},periodic={},required_modules={})>'.format(self.enabled, self.periodic, self.required_modules)
+
+
+class PSUtilRuntimeMetricCollector(ValueCollector):
+    """Collector for psutil metrics.
+
+    Performs batched operations via proc.oneshot() to optimize the calls.
+    See https://psutil.readthedocs.io/en/latest/#psutil.Process.oneshot
+    for more information.
+    """
+    def __init__(self, *args, **kwargs):
+        kwargs['required_modules'] = ['psutil']
+        self.proc = None
+        super(PSUtilRuntimeMetricCollector, self).__init__(*args, **kwargs)
+
+    def collect_fn(self, modules, keys):
+        if not self.proc:
+            self.proc = self.modules['psutil'].Process(os.getpid())
+
+        metrics = {}
+        with self.proc.oneshot():
+            if 'datadog.tracer.runtime.thread_count' in keys:
+                metrics['datadog.tracer.runtime.thread_count'] = self.proc.num_threads()
+            if 'datadog.tracer.runtime.mem.rss' in keys:
+                metrics['datadog.tracer.runtime.mem.rss'] = self.proc.memory_info().rss
+        return metrics
+
+
+class RuntimeMetricTagCollector(ValueCollector):
+    pass
+
+
+class RuntimeMetricCollector(ValueCollector):
+    pass
+
+
+class RuntimeMetrics(object):
+    """
+    TODO: configuration
+    """
+
+    METRIC_COLLECTORS = [
+        PSUtilRuntimeMetricCollector(periodic=True),
+    ]
+
+    TAG_COLLECTORS = [
+        RuntimeMetricTagCollector(collect_fn=interpreter_version),
+        RuntimeMetricTagCollector(collect_fn=interpreter_implementation),
+        RuntimeMetricTagCollector(collect_fn=tracer_version),
+    ]
+
+    def __init__(self, enabled_metrics=DEFAULT_ENABLED_METRICS, enabled_tags=DEFAULT_ENABLED_TAGS):
+        self.agent_host = DEFAULT_AGENT_HOST
+        self.agent_metric_port = DEFAULT_METRIC_AGENT_PORT
+        self.enabled_metrics = enabled_metrics
+        self.enabled_tags = enabled_tags
+        self.statsd = None
+
+        self._init_statsd()
+
+    def _collect_constant_tags(self):
+        """Collects tags to be sent to ddstatsd.
+
+        Note: ddstatsd expects tags in the form ['key1:value1', 'key2:value2', ...]
+        :return:
+        """
+        tags = []
+        for tag_collector in self.TAG_COLLECTORS:
+            collected_tags = tag_collector.collect(self.enabled_tags)
+            tags += ['{}:{}'.format(k, v) for k, v in collected_tags.items()]
+        log.info('Reporting constant tags {}'.format(tags))
+        return tags
+
+    def _collect_metrics(self):
+        metrics = {}
+        for metric_collector in self.METRIC_COLLECTORS:
+            collector_metrics = metric_collector.collect(self.enabled_metrics)
+            metrics.update(collector_metrics)
+        return metrics
+
+    def _init_statsd(self):
+        try:
+            from datadog import DogStatsd
+            tags = self._collect_constant_tags()
+            self.statsd = DogStatsd(host=self.agent_host, port=self.agent_metric_port, constant_tags=tags)
+        except ImportError:
+            log.info('Install the `datadog` package to enable runtime metrics.')
+        except Exception:
+            log.warn('Could not initialize ddstatsd')
+
+    def flush(self):
+        """Collects and flushes enabled metrics to the Datadog Agent.
+        """
+        if not self.statsd:
+            log.warn('Attempted flush with uninitialized or failed statsd client')
+            return
+
+        metrics = self._collect_metrics()
+
+        for metric_key, metric_value in metrics.items():
+            log.info('Flushing metric {}:{}'.format(metric_key, metric_value))
+            self.statsd.gauge(metric_key, metric_value)
+
---
+++ tests/test_metrics.py
@@ -0,0 +1,122 @@
+import unittest
+import sys
+import mock
+from ddtrace.runtime_metrics import ValueCollector
+
+
+class TestValueCollector(unittest.TestCase):
+    def test_default_usage(self):
+        mock_collect = mock.MagicMock()
+        mock_collect.side_effect = lambda m, k: {
+            'key1': 'value1',
+            'key2': 'value2',
+        }
+
+        vc = ValueCollector(collect_fn=mock_collect)
+        self.assertEqual(vc.collect(keys=set(['key1'])), {
+            'key1': 'value1',
+            'key2': 'value2',
+        })
+        mock_collect.assert_called_once()
+        mock_collect.assert_called_with({}, set(['key1']))
+
+        self.assertEqual(mock_collect.call_count, 1, "Collector is not periodic by default")
+
+    def test_enabled(self):
+        collect = mock.MagicMock()
+        vc = ValueCollector(collect_fn=collect, enabled=False)
+        collect.assert_not_called()
+        vc.collect()
+        collect.assert_not_called()
+
+    def test_periodic(self):
+        collect = mock.MagicMock()
+        vc = ValueCollector(collect_fn=collect, periodic=True)
+        vc.collect()
+        self.assertEqual(collect.call_count, 1)
+        vc.collect()
+        self.assertEqual(collect.call_count, 2)
+
+    def test_not_periodic(self):
+        collect = mock.MagicMock()
+        vc = ValueCollector(collect_fn=collect)
+        collect.assert_not_called()
+        vc.collect()
+        self.assertEqual(collect.call_count, 1)
+        vc.collect()
+        self.assertEqual(collect.call_count, 1)
+        vc.collect()
+        self.assertEqual(collect.call_count, 1)
+
+    def test_required_module(self):
+        mock_module = mock.MagicMock()
+        mock_module.fn.side_effect = lambda: 'test'
+        # TODO: use a context manager to insert and delete from sys modules
+        sys.modules['A'] = mock_module
+
+        def collect(modules, keys):
+            self.assertIn('A', modules)
+            a = modules.get('A')
+            a.fn()
+
+        vc = ValueCollector(collect_fn=collect, required_modules=['A'])
+        vc.collect()
+        mock_module.fn.assert_called_once()
+        del sys.modules['A']
+
+    def test_required_module_not_installed(self):
+        collect = mock.MagicMock()
+        with mock.patch('ddtrace.runtime_metrics.log') as log_mock:
+            # Should log a warning (tested below)
+            vc = ValueCollector(collect_fn=collect, required_modules=['moduleshouldnotexist'])
+
+            # Collect should not be called as the collector should be disabled.
+            collect.assert_not_called()
+            vc.collect()
+            collect.assert_not_called()
+
+        calls = [
+            mock.call(
+                'Could not import module "moduleshouldnotexist" for <Collector(enabled=False,periodic=False,required_modules=[\'moduleshouldnotexist\'])>'
+            )
+        ]
+        log_mock.warn.assert_has_calls(calls)
+
+    def test_collected_values(self):
+        class V(object):
+            i = 0
+
+        def collect(modules, keys):
+            V.i += 1
+            return {'i': V.i}
+
+        vc = ValueCollector(collect_fn=collect)
+        self.assertEqual(vc.collect(), {'i': 1})
+        self.assertEqual(vc.collect(), {'i': 1})
+        self.assertEqual(vc.collect(), {'i': 1})
+
+    def test_collected_values_periodic(self):
+        class V(object):
+            i = 0
+
+        def collect(modules, keys):
+            V.i += 1
+            return {'i': V.i}
+
+        vc = ValueCollector(collect_fn=collect, periodic=True)
+        self.assertEqual(vc.collect(), {'i': 1})
+        self.assertEqual(vc.collect(), {'i': 2})
+        self.assertEqual(vc.collect(), {'i': 3})
+
+
+class TestMetrics(unittest.TestCase):
+    def test_manual(self):
+        from ddtrace.runtime_metrics import RuntimeMetrics
+        from time import sleep
+
+        rtm = RuntimeMetrics()
+        l = []
+        while True:
+            rtm.flush()
+            l += [i for i in range(1000)]
+            sleep(1)
