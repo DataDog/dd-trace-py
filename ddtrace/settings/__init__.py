bddfd18ee7dd4a1c6ed509aedce24d7cc678695f
brettlangdon <brett.langdon@datadoghq.com>
2019-02-04 16:25:52 -0500

    Fix import name

--- ddtrace/settings/__init__.py
+++ ddtrace/settings/__init__.py
@@ -2,7 +2,7 @@ from .config import Config
 from .exceptions import ConfigException
 from .http import HttpConfig
 from .hooks import Hooks
-from .integrations import IntegrationConfig
+from .integration import IntegrationConfig
 
 # Default global config
 config = Config()
