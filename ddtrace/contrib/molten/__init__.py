d72608934efbf717486d50e9fcdb2adf898fd5b3
brettlangdon <brett.langdon@datadoghq.com>
2019-02-08 15:50:49 -0500

    [molten] Ennable distributed tracing by default

--- ddtrace/contrib/molten/__init__.py
+++ ddtrace/contrib/molten/__init__.py
@@ -13,8 +13,8 @@ You may also enable molten tracing automatically via ``ddtrace-run``::
 
     ddtrace-run python app.py
 
-To enable distributed tracing when using autopatching, set the
-``DD_MOLTEN_DISTRIBUTED_TRACING`` environment variable to ``True``.
+To disable distributed tracing when using autopatching, set the
+``DD_MOLTEN_DISTRIBUTED_TRACING`` environment variable to ``False``.
 """
 from ...utils.importlib import require_modules
 
