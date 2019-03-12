5b3d09f8fc850a06ec3ec35a23a87d776c44830d
Tahir H. Butt <tahir.butt@datadoghq.com>
2019-03-12 15:43:53 -0400

    Update trace search for kombu

--- tests/contrib/kombu/test.py
+++ tests/contrib/kombu/test.py
@@ -2,6 +2,7 @@
 import kombu
 
 from ddtrace import Pin
+from ddtrace.constants import ANALYTICS_SAMPLE_RATE_KEY
 from ddtrace.contrib.kombu.patch import patch, unpatch
 from ddtrace.contrib.kombu import utils
 from ddtrace.ext import kombu as kombux
@@ -19,6 +20,12 @@ class TestKombuPatch(BaseTracerTestCase):
 
         conn = kombu.Connection("amqp://guest:guest@127.0.0.1:{p}//".format(p=self.TEST_PORT))
         conn.connect()
+        producer = conn.Producer()
+        Pin.override(producer, service=self.TEST_SERVICE, tracer=self.tracer)
+
+        self.conn = conn
+        self.producer = producer
+
         patch()
 
     def tearDown(self):
@@ -27,25 +34,15 @@ class TestKombuPatch(BaseTracerTestCase):
         super(TestKombuPatch, self).tearDown()
 
     def test_basics(self):
-        conn, producer, tracer = self.get_kombu_and_tracer()
-        self._assert_conn_traced(conn, producer, tracer, self.TEST_SERVICE)
-
-    def get_kombu_and_tracer(self):
-        conn = kombu.Connection("amqp://guest:guest@127.0.0.1:{p}//".format(p=self.TEST_PORT))
-        conn.connect()
-        producer = conn.Producer()
-        Pin.override(producer, service=self.TEST_SERVICE, tracer=self.tracer)
-        return conn, producer, self.tracer
+        self._publish_consume()
+        self._assert_spans()
 
     def test_extract_conn_tags(self):
-        conn, _, _ = self.get_kombu_and_tracer()
-        result = utils.extract_conn_tags(conn)
+        result = utils.extract_conn_tags(self.conn)
         assert result['out.host'] == '127.0.0.1'
         assert result['out.port'] == str(self.TEST_PORT)
 
-
-    def _assert_conn_traced(self, conn, producer, tracer, service):
-        """Tests both producer and consumer tracing"""
+    def _publish_consume(self):
         results = []
 
         def process_message(body, message):
@@ -54,20 +51,23 @@ class TestKombuPatch(BaseTracerTestCase):
 
         task_queue = kombu.Queue('tasks', kombu.Exchange('tasks'), routing_key='tasks')
         to_publish = {'hello': 'world'}
-        producer.publish(to_publish,
+        self.producer.publish(to_publish,
                         exchange=task_queue.exchange,
                         routing_key=task_queue.routing_key,
                         declare=[task_queue])
 
-        with kombu.Consumer(conn, [task_queue], accept=['json'], callbacks=[process_message]) as consumer:
-            Pin.override(consumer, service='kombu-patch', tracer=tracer)
-            conn.drain_events(timeout=2)
+        with kombu.Consumer(self.conn, [task_queue], accept=['json'], callbacks=[process_message]) as consumer:
+            Pin.override(consumer, service='kombu-patch', tracer=self.tracer)
+            self.conn.drain_events(timeout=2)
 
         self.assertEqual(results[0], to_publish)
-        spans = tracer.writer.pop()
+
+    def _assert_spans(self):
+        """Tests both producer and consumer tracing"""
+        spans = self.get_spans()
         self.assertEqual(len(spans), 2)
         consumer_span = spans[0]
-        self.assertEqual(consumer_span.service, service)
+        self.assertEqual(consumer_span.service, self.TEST_SERVICE)
         self.assertEqual(consumer_span.name, kombux.PUBLISH_NAME)
         self.assertEqual(consumer_span.span_type, 'kombu')
         self.assertEqual(consumer_span.error, 0)
@@ -79,9 +79,38 @@ class TestKombuPatch(BaseTracerTestCase):
         self.assertEqual(consumer_span.resource, 'tasks')
 
         producer_span = spans[1]
-        self.assertEqual(producer_span.service, service)
+        self.assertEqual(producer_span.service, self.TEST_SERVICE)
         self.assertEqual(producer_span.name, kombux.RECEIVE_NAME)
         self.assertEqual(producer_span.span_type, 'kombu')
         self.assertEqual(producer_span.error, 0)
         self.assertEqual(producer_span.get_tag('kombu.exchange'), u'tasks')
         self.assertEqual(producer_span.get_tag('kombu.routing_key'), u'tasks')
+
+    def test_analytics_default(self):
+        self._publish_consume()
+
+        spans = self.get_spans()
+        self.assertEqual(len(spans), 2)
+        self.assertIsNone(spans[0].get_metric(ANALYTICS_SAMPLE_RATE_KEY))
+
+    def test_analytics_with_rate(self):
+        with self.override_config(
+            'kombu',
+            dict(analytics_enabled=True, analytics_sample_rate=0.5)
+        ):
+            self._publish_consume()
+
+        spans = self.get_spans()
+        self.assertEqual(len(spans), 2)
+        self.assertEqual(spans[0].get_metric(ANALYTICS_SAMPLE_RATE_KEY), 0.5)
+
+    def test_analytics_without_rate(self):
+        with self.override_config(
+            'kombu',
+            dict(analytics_enabled=True)
+        ):
+            self._publish_consume()
+
+        spans = self.get_spans()
+        self.assertEqual(len(spans), 2)
+        self.assertEqual(spans[0].get_metric(ANALYTICS_SAMPLE_RATE_KEY), 1.0)
