e3440854f99f13cf198a451763eaf7eb14ccde08
Tahir H. Butt <tahir.butt@datadoghq.com>
2019-04-10 11:14:36 -0400

    Fix cumulative metrics

--- ddtrace/internal/runtime/metric_collectors.py
+++ ddtrace/internal/runtime/metric_collectors.py
@@ -48,19 +48,44 @@ class PSUtilRuntimeMetricCollector(RuntimeMetricCollector):
     for more information.
     """
     required_modules = ['psutil']
+    stored_value = dict(
+        CPU_TIME_SYS_TOTAL=0,
+        CPU_TIME_USER_TOTAL=0,
+        CTX_SWITCH_VOLUNTARY_TOTAL=0,
+        CTX_SWITCH_INVOLUNTARY_TOTAL=0,
+    )
 
     def _on_modules_load(self):
         self.proc = self.modules['psutil'].Process(os.getpid())
 
     def collect_fn(self, keys):
         with self.proc.oneshot():
+            # only return time deltas
+            # TODO[tahir]: better abstraction for metrics based on last value
+            cpu_time_sys_total = self.proc.cpu_times().system
+            cpu_time_user_total = self.proc.cpu_times().user
+            cpu_time_sys = cpu_time_sys_total - self.stored_value['CPU_TIME_SYS_TOTAL']
+            cpu_time_user = cpu_time_user_total - self.stored_value['CPU_TIME_USER_TOTAL']
+
+            ctx_switch_voluntary_total = self.proc.num_ctx_switches().voluntary
+            ctx_switch_involuntary_total = self.proc.num_ctx_switches().involuntary
+            ctx_switch_voluntary = ctx_switch_voluntary_total - self.stored_value['CTX_SWITCH_VOLUNTARY_TOTAL']
+            ctx_switch_involuntary = ctx_switch_involuntary_total - self.stored_value['CTX_SWITCH_INVOLUNTARY_TOTAL']
+
+            self.stored_value = dict(
+                CPU_TIME_SYS_TOTAL=cpu_time_sys_total,
+                CPU_TIME_USER_TOTAL=cpu_time_user_total,
+                CTX_SWITCH_VOLUNTARY_TOTAL=ctx_switch_voluntary_total,
+                CTX_SWITCH_INVOLUNTARY_TOTAL=ctx_switch_involuntary_total,
+            )
+
             metrics = [
                 (THREAD_COUNT, self.proc.num_threads()),
                 (MEM_RSS, self.proc.memory_info().rss),
-                (CTX_SWITCH_VOLUNTARY, self.proc.num_ctx_switches().voluntary),
-                (CTX_SWITCH_INVOLUNTARY, self.proc.num_ctx_switches().involuntary),
-                (CPU_TIME_SYS, self.proc.cpu_times().system),
-                (CPU_TIME_USER, self.proc.cpu_times().user),
+                (CTX_SWITCH_VOLUNTARY, ctx_switch_voluntary),
+                (CTX_SWITCH_INVOLUNTARY, ctx_switch_involuntary),
+                (CPU_TIME_SYS, cpu_time_sys),
+                (CPU_TIME_USER, cpu_time_user),
                 (CPU_PERCENT, self.proc.cpu_percent()),
             ]
 
