657061b04cd268eb588a344030a60c03e561f1d8
Tahir H. Butt <tahir.butt@datadoghq.com>
2019-04-10 14:01:13 -0400

    Flag check unnecessary

--- ddtrace/bootstrap/sitecustomize.py
+++ ddtrace/bootstrap/sitecustomize.py
@@ -85,7 +85,7 @@ try:
     hostname = os.environ.get('DD_AGENT_HOST', os.environ.get('DATADOG_TRACE_AGENT_HOSTNAME'))
     port = os.environ.get("DATADOG_TRACE_AGENT_PORT")
     priority_sampling = os.environ.get("DATADOG_PRIORITY_SAMPLING")
-    runtime_metrics_enabled = get_env('runtime_metrics', 'enabled')
+    runtime_metrics_enabled = asbool(get_env('runtime_metrics', 'enabled'))
 
     opts = {}
 
@@ -98,8 +98,6 @@ try:
         opts["port"] = int(port)
     if priority_sampling:
         opts["priority_sampling"] = asbool(priority_sampling)
-    if runtime_metrics_enabled:
-        opts["collect_metrics"] = asbool(runtime_metrics_enabled)
 
     if opts:
         tracer.configure(**opts)
