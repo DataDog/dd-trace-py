077cad98153c26cf1fda52f4ce2abb3237fe1b28
Brett Langdon <me@brett.is>
2019-04-11 13:59:32 -0400

    Update ddtrace/bootstrap/sitecustomize.py
    Co-Authored-By: majorgreys <tahir@tahirbutt.com>

--- ddtrace/bootstrap/sitecustomize.py
+++ ddtrace/bootstrap/sitecustomize.py
@@ -98,7 +98,7 @@ try:
     if priority_sampling:
         opts["priority_sampling"] = asbool(priority_sampling)
 
-    opts["collect_metrics"] = asbool(get_env('runtime_metrics', 'enabled'))
+    opts['collect_metrics'] = asbool(get_env('runtime_metrics', 'enabled'))
 
     if opts:
         tracer.configure(**opts)
