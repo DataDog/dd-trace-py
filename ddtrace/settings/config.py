51ff5b43583524ab7ae31e6849d839d4e3ea0d5c
Julien Danjou <julien@danjou.info>
2019-04-26 10:52:50 +0200

    [config] declare get_from as static
    This does not change much, but it indicates that this method as actually
    nothing to do with this class.

--- ddtrace/settings/config.py
+++ ddtrace/settings/config.py
@@ -33,7 +33,8 @@ class Config(object):
     def __getattr__(self, name):
         return self._config[name]
 
-    def get_from(self, obj):
+    @staticmethod
+    def get_from(obj):
         """Retrieves the configuration for the given object.
         Any object that has an attached `Pin` must have a configuration
         and if a wrong object is given, an empty `dict` is returned
