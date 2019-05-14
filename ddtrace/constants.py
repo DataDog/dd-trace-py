3d06a98393356dcee9a8531b2081588c86285287
brettlangdon <brett.langdon@datadoghq.com>
2019-05-14 12:23:50 -0400

    [core] Add config to set hostname tag on trace root span

--- ddtrace/constants.py
+++ ddtrace/constants.py
@@ -3,6 +3,7 @@ SAMPLE_RATE_METRIC_KEY = '_sample_rate'
 SAMPLING_PRIORITY_KEY = '_sampling_priority_v1'
 ANALYTICS_SAMPLE_RATE_KEY = '_dd1.sr.eausr'
 ORIGIN_KEY = '_dd.origin'
+HOSTNAME_KEY = '_dd.hostname'
 
 NUMERIC_TAGS = (ANALYTICS_SAMPLE_RATE_KEY, )
 
