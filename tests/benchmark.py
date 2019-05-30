77f44608dc7b65ea2481f2fdc75ba1e8b02f087d
Tahir H. Butt <tahir.butt@datadoghq.com>
2019-05-30 12:39:35 -0400

    Add tests for more tracer functionality

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -13,13 +13,8 @@ def tracer():
 
 def test_tracer_context(benchmark, tracer):
     def func(tracer):
-        with tracer.trace('a', service='s', resource='r', span_type='t') as s:
-            s.set_tag('a', 'b')
-            s.set_tag('b', 1)
-            with tracer.trace('another.thing'):
-                pass
-            with tracer.trace('another.thing'):
-                pass
+        with tracer.trace('a', service='s', resource='r', span_type='t'):
+            pass
 
     benchmark(func, tracer)
 
@@ -54,3 +49,44 @@ def test_tracer_wrap_instancemethod(benchmark, tracer):
 
     f = Foo()
     benchmark(f.func)
+
+
+def test_tracer_start_span(benchmark, tracer):
+    benchmark(tracer.start_span, 'benchmark')
+
+
+def test_tracer_start_finish_span(benchmark, tracer):
+    def func(tracer):
+        s = tracer.start_span('benchmark')
+        s.finish()
+
+    benchmark(func, tracer)
+
+
+def test_trace_simple_trace(benchmark, tracer):
+    def func(tracer):
+        with tracer.trace('parent'):
+            for i in range(5):
+                with tracer.trace('child') as c:
+                    c.set_tag('i', i)
+
+    benchmark(func, tracer)
+
+
+def test_tracer_large_trace(benchmark, tracer):
+    import random
+
+    # generate trace with 1024 spans
+    @tracer.wrap()
+    def func(tracer, level=0):
+        span = tracer.current_span()
+
+        # do some work
+        num = random.randint(1, 10)
+        span.set_tag('num', num)
+
+        if level < 10:
+            func(tracer, level+1)
+            func(tracer, level+1)
+
+    benchmark(func, tracer)
