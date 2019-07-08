4ec9b78254a9f169b415113cc36a5207a44ecd75
Tahir H. Butt <tahir.butt@datadoghq.com>
2019-07-08 11:28:20 -0400

    Update ddtrace to 0.27.0

--- ddtrace/__init__.py
+++ ddtrace/__init__.py
@@ -4,7 +4,7 @@ from .span import Span
 from .tracer import Tracer
 from .settings import config
 
-__version__ = '0.26.0'
+__version__ = '0.27.0'
 
 # a global tracer instance with integration settings
 tracer = Tracer()
