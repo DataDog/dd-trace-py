2d6cbc41799531375b20bd29b477fad841d4cebb
brettlangdon <brett.langdon@datadoghq.com>
2019-08-16 18:39:52 -0400

    fix quotes

--- tests/contrib/django/testapp_1/wsgi.py
+++ tests/contrib/django/testapp_1/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testapp_1.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testapp_1.settings')
 
 application = get_wsgi_application()
