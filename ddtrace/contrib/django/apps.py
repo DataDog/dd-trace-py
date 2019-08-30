f0f50c5ce95b7f0af83955706bd94689a3971c97
Joseph Kahn <josephbkahn@gmail.com>
2019-08-30 07:33:23 -0400

    [django] call connections.all in ready (#1019)

--- ddtrace/contrib/django/apps.py
+++ ddtrace/contrib/django/apps.py
@@ -2,15 +2,7 @@
 from django.apps import AppConfig, apps
 
 # project
-from .db import patch_db
-from .conf import settings
-from .cache import patch_cache
-from .templates import patch_template
-from .middleware import insert_exception_middleware, insert_trace_middleware
-
-from ...internal.logger import get_logger
-
-log = get_logger(__name__)
+from .patch import apply_django_patches
 
 
 class TracerConfig(AppConfig):
@@ -23,46 +15,5 @@ class TracerConfig(AppConfig):
         Tracing capabilities must be enabled in this function so that
         all Django internals are properly configured.
         """
-        tracer = settings.TRACER
-
-        if settings.TAGS:
-            tracer.set_tags(settings.TAGS)
-
-        # configure the tracer instance
-        # TODO[manu]: we may use configure() but because it creates a new
-        # AgentWriter, it breaks all tests. The configure() behavior must
-        # be changed to use it in this integration
-        tracer.enabled = settings.ENABLED
-        tracer.writer.api.hostname = settings.AGENT_HOSTNAME
-        tracer.writer.api.port = settings.AGENT_PORT
-
-        if settings.AUTO_INSTRUMENT:
-            # trace Django internals
-            insert_trace_middleware()
-            insert_exception_middleware()
-
-            if settings.INSTRUMENT_TEMPLATE:
-                try:
-                    patch_template(tracer)
-                except Exception:
-                    log.exception('error patching Django template rendering')
-
-            if settings.INSTRUMENT_DATABASE:
-                try:
-                    patch_db(tracer)
-                except Exception:
-                    log.exception('error patching Django database connections')
-
-            if settings.INSTRUMENT_CACHE:
-                try:
-                    patch_cache(tracer)
-                except Exception:
-                    log.exception('error patching Django cache')
-
-            # Instrument rest_framework app to trace custom exception handling.
-            if apps.is_installed('rest_framework'):
-                try:
-                    from .restframework import patch_restframework
-                    patch_restframework(tracer)
-                except Exception:
-                    log.exception('error patching rest_framework app')
+        rest_framework_is_installed = apps.is_installed('rest_framework')
+        apply_django_patches(patch_rest_framework=rest_framework_is_installed)
