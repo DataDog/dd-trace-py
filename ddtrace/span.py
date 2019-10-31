f758857dd880f4ca832082185eaf2ef648481e79
brettlangdon <brett.langdon@datadoghq.com>
2019-10-31 10:11:24 -0400

    update tests

--- ddtrace/span.py
+++ ddtrace/span.py
@@ -4,7 +4,7 @@ import sys
 import traceback
 
 from .compat import StringIO, stringify, iteritems, numeric_types, time_ns
-from .constants import NUMERIC_TAGS, MANUAL_DROP_KEY, MANUAL_KEEP_KEY, ANALYTICS_SAMPLE_RATE_KEY
+from .constants import NUMERIC_TAGS, MANUAL_DROP_KEY, MANUAL_KEEP_KEY
 from .ext import errors, priority
 from .internal.logger import get_logger
 
