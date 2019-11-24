97fc4be3468bb593361be9532e55020e60328cb2
brettlangdon <brett.langdon@datadoghq.com>
2019-11-23 19:04:02 -0500

    add Counter.decrement support

--- ddtrace/utils/counter.py
+++ ddtrace/utils/counter.py
@@ -17,27 +17,30 @@ class Counter(object):
         for _ in range(10):
             counter.increment()
         counter.value()  # 11
+
+        counter.decrement()
+        counter.value()  # 10
     """
     def __init__(self):
         self._read_count = 0
-        self._counter = itertools.count()
+        self._increment = itertools.count()
+        self._decrement = itertools.count()
         self._read_lock = threading.Lock()
 
-    def increment(self, step=1):
-        """
-        Increment the counter
+    def increment(self):
+        """Increment the counter"""
+        next(self._increment)
 
-        :param step: The number of times to increment the counter (default: ``1``)
-        :type step: int
-        """
-        for _ in range(step):
-            next(self._counter)
+    def decrement(self):
+        """Increment the counter"""
+        next(self._decrement)
 
     def _value(self):
         """Return the current value of the counter"""
-        val = next(self._counter) - self._read_count
+        inc = next(self._increment) - self._read_count
+        dec = next(self._decrement) - self._read_count
         self._read_count += 1
-        return val
+        return inc - dec
 
     def value(self, no_lock=False):
         """
