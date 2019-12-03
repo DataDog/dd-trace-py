05fd4db3fed4b0ffd8d3801748a4103f93999290
Tahir H. Butt <tahir.butt@datadoghq.com>
2019-12-03 09:50:48 -0500

    fix flake8

--- docs/conf.py
+++ docs/conf.py
@@ -190,7 +190,7 @@ html_theme_options = {
 
 # Custom sidebar templates, maps document names to template names.
 #
-html_sidebars = {"**": ["about.html", "nav.html", "relations.html", "searchbox.html",]}
+html_sidebars = {"**": ["about.html", "nav.html", "relations.html", "searchbox.html", ]}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
