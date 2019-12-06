010c484bfb794a2b4f4c6bde9251a2a2c3cd8054
Matt Perpick <matthewperpick@gmail.com>
2019-12-06 14:03:34 -0500

    responder: black

--- ddtrace/contrib/responder/patch.py
+++ ddtrace/contrib/responder/patch.py
@@ -1,4 +1,3 @@
-
 # stdlib
 
 # project
@@ -11,6 +10,7 @@ from ddtrace.propagation.http import extract_context_from_http_headers
 
 # 3p
 import responder
+
 patches = []  # will be overriden when patched funcs are defined.
 
 
@@ -29,8 +29,8 @@ def unpatch():
     _set_patched(responder, False)
 
     for obj, _, prop, _ in patches:
-        if '.' in prop:
-            attr, _, prop = prop.partition('.')
+        if "." in prop:
+            attr, _, prop = prop.partition(".")
             obj = getattr(obj, attr, object())
         _u(obj, prop)
 
@@ -41,20 +41,20 @@ async def _api_call(wrapped, instance, args, kwargs):
     scope, receive, send = args  # FIXME: what if this is partially using kwargs?
 
     # check for propagated traced ids in the http heaers.
-    headers = {k.decode(): v for k, v in scope.get('headers', ())}
+    headers = {k.decode(): v for k, v in scope.get("headers", ())}
     ctx = extract_context_from_http_headers(headers)
     if ctx.trace_id:
         tracer.context_provider.activate(ctx)
 
     with tracer.trace("responder.request", service="responder") as span:
         span.span_type = SpanTypes.WEB.value
-        span.set_tag(http.METHOD, scope.get('method'))
+        span.set_tag(http.METHOD, scope.get("method"))
 
         traced_send = _trace_asgi_send_func(send, span)
         await wrapped(scope, receive, traced_send)
 
         # the router patching will mark the matched route on the request scope.
-        span.resource = scope.get('__dd_route', '404')
+        span.resource = scope.get("__dd_route", "404")
 
 
 def _api_template(wrapped, instance, args, kwargs):
@@ -67,16 +67,16 @@ async def _route_call(wrapped, instance, args, kwargs):
     # patch the route on the scope for use in top-level patching
     if args:
         scope = args[0]
-        scope['__dd_route'] = getattr(instance, 'route')
+        scope["__dd_route"] = getattr(instance, "route")
 
     await wrapped(*args, **kwargs)
 
 
 patches = [
-    (responder.api, 'responder.api', 'API.__call__', _api_call),
-    (responder.api, 'responder.api', 'API.template', _api_template),
-    (responder.api, 'responder.api', 'API.template_string', _api_template),
-    (responder.routes, 'responder.routes', 'Route.__call__', _route_call)
+    (responder.api, "responder.api", "API.__call__", _api_call),
+    (responder.api, "responder.api", "API.template", _api_template),
+    (responder.api, "responder.api", "API.template_string", _api_template),
+    (responder.routes, "responder.routes", "Route.__call__", _route_call),
 ]
 
 
@@ -86,6 +86,7 @@ def _trace_asgi_send_func(send, span):
         if event_type == "http.response.start":
             span.set_tag(http.STATUS_CODE, event.get("status"))
         await send(event)
+
     return _traced_send
 
 
@@ -97,8 +98,8 @@ def _get_tracer(instance):
 
 
 def _set_patched(obj, state):
-    setattr(obj, '__datadog_patch', state)
+    setattr(obj, "__datadog_patch", state)
 
 
 def _is_patched(obj):
-    return getattr(obj, '__datadog_patch', False)
+    return getattr(obj, "__datadog_patch", False)
