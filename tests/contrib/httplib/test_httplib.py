2bbc2e3b452508da7d351a77877f6950e205634c
brettlangdon <brett.langdon@datadoghq.com>
2019-12-19 19:21:53 -0500

    fix up httplib

--- tests/contrib/httplib/test_httplib.py
+++ tests/contrib/httplib/test_httplib.py
@@ -17,7 +17,7 @@ from ddtrace.pin import Pin
 from tests.opentracer.utils import init_tracer
 
 from ...base import BaseTracerTestCase
-from ...util import assert_dict_issuperset, override_global_tracer
+from ...util import override_global_tracer
 
 if PY2:
     from urllib2 import urlopen, build_opener, Request
@@ -152,14 +152,9 @@ class HTTPLibTestCase(HTTPLibBaseMixin, BaseTracerTestCase):
         self.assertIsNone(span.service)
         self.assertEqual(span.name, self.SPAN_NAME)
         self.assertEqual(span.error, 0)
-        assert_dict_issuperset(
-            span.meta,
-            {
-                'http.method': 'GET',
-                'http.status_code': '200',
-                'http.url': URL_200,
-            }
-        )
+        assert span.get_tag('http.method') == 'GET'
+        assert span.get_tag('http.url') == URL_200
+        assert span.get_metric('http.status_code') == 200
         if config.httplib.trace_query_string:
             assert span.get_tag(http.QUERY_STRING) == query_string
         else:
@@ -194,14 +189,9 @@ class HTTPLibTestCase(HTTPLibBaseMixin, BaseTracerTestCase):
         self.assertIsNone(span.service)
         self.assertEqual(span.name, self.SPAN_NAME)
         self.assertEqual(span.error, 0)
-        assert_dict_issuperset(
-            span.meta,
-            {
-                'http.method': 'GET',
-                'http.status_code': '200',
-                'http.url': 'https://httpbin.org/status/200',
-            }
-        )
+        assert span.get_tag('http.method') == 'GET'
+        assert span.get_metric('http.status_code') == 200
+        assert span.get_tag('http.url') == 'https://httpbin.org/status/200'
 
     def test_httplib_request_post_request(self):
         """
@@ -223,14 +213,9 @@ class HTTPLibTestCase(HTTPLibBaseMixin, BaseTracerTestCase):
         self.assertIsNone(span.service)
         self.assertEqual(span.name, self.SPAN_NAME)
         self.assertEqual(span.error, 0)
-        assert_dict_issuperset(
-            span.meta,
-            {
-                'http.method': 'POST',
-                'http.status_code': '200',
-                'http.url': URL_200,
-            }
-        )
+        assert span.get_tag('http.method' == 'POST')
+        assert span.get_metric('http.status_code' == 200)
+        assert span.get_tag('http.url' == URL_200)
 
     def test_httplib_request_get_request_query_string(self):
         """
@@ -251,15 +236,9 @@ class HTTPLibTestCase(HTTPLibBaseMixin, BaseTracerTestCase):
         self.assertIsNone(span.service)
         self.assertEqual(span.name, self.SPAN_NAME)
         self.assertEqual(span.error, 0)
-        assert_dict_issuperset(
-            span.meta,
-            {
-                'http.method': 'GET',
-                'http.status_code': '200',
-                # check url metadata lacks query string
-                'http.url': '{}'.format(URL_200),
-            }
-        )
+        assert span.get_tag('http.method') == 'GET'
+        assert span.get_tag('http.status_code') == 200
+        assert span.get_tag('http.url') == URL_200
 
     def test_httplib_request_500_request(self):
         """
@@ -502,14 +481,9 @@ class HTTPLibTestCase(HTTPLibBaseMixin, BaseTracerTestCase):
         self.assertEqual(dd_span.span_type, 'http')
         self.assertEqual(dd_span.name, self.SPAN_NAME)
         self.assertEqual(dd_span.error, 0)
-        assert_dict_issuperset(
-            dd_span.meta,
-            {
-                'http.method': 'GET',
-                'http.status_code': '200',
-                'http.url': URL_200,
-            }
-        )
+        assert span.get_tag('http.method') == 'GET'
+        assert span.get_metric('http.status_code') == 200
+        assert span.get_tag('http.url') == URL_200
 
     def test_analytics_default(self):
         conn = self.get_http_connection(SOCKET)
