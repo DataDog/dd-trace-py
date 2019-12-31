ca231894d44aead03ab5c45c6044382bdb937334
brettlangdon <brett.langdon@datadoghq.com>
2019-12-31 10:14:57 -0500

    add counter value tests

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -1,3 +1,4 @@
+import mock
 import pytest
 
 from ddtrace.utils.counter import Counter
@@ -27,3 +28,27 @@ def test_counter_decrement(counter):
         assert counter.value() == i
 
     assert counter.value() == -1000
+
+
+def test_counter_value_lock(counter):
+    lock = mock.MagicMock(wraps=counter._read_lock)
+    counter._read_lock = lock
+
+    counter.increment()
+
+    # Fetch the value using the built-in read lock
+    assert counter.value() == 1
+
+    # Assert that anything was called on "lock"
+    # DEV: We could assert that `__enter__` was called, but
+    #      we really just want to know "was this used at all"
+    assert lock.mock_calls
+
+    # Reset for the next validation
+    lock.reset_mock()
+
+    # Fetch the value without using the built-in read lock
+    assert counter.value(no_lock=True) == 1
+
+    # Assert nothing was called on the "lock"
+    assert not lock.mock_calls
