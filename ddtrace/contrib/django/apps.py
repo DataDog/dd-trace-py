6747e12f0d971df6d2ef6722e25eca1bd7f5ee6b
brettlangdon <brett.langdon@datadoghq.com>
2020-01-23 13:50:15 -0500

    manually call patch() when using old settings without calling patch

--- ddtrace/contrib/django/apps.py
+++ ddtrace/contrib/django/apps.py
@@ -2,11 +2,18 @@ from django.apps import AppConfig
 
 from ddtrace.vendor.debtcollector import removals
 from ...internal.logger import get_logger
+from .patch import patch
 
 log = get_logger(__name__)
 
 
-@removals.removed_class("TracerConfig")
+@removals.removed_class(
+    "TracerConfig",
+    message="""Adding ddtrace.contrib.django to settings.py is no longer required, instead do:
+    import ddtrace
+    ddtrace.patch_all()
+    """,
+)
 class TracerConfig(AppConfig):
     name = "ddtrace.contrib.django"
     label = "datadog_django"
@@ -15,4 +22,5 @@ class TracerConfig(AppConfig):
         """
         Ready formerly was used to initiate patching of Django and DRF.
         """
-        pass
+        # Make sure our app is configured
+        patch()
