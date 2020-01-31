1e02c49ddcfd3491e2dbfa3e9de591d5f268fc31
brettlangdon <brett.langdon@datadoghq.com>
2020-01-31 14:40:09 -0500

    black: ddtrace/utils/

--- ddtrace/utils/config.py
+++ ddtrace/utils/config.py
@@ -4,7 +4,7 @@ import os
 
 def get_application_name():
     """Attempts to find the application name using system arguments."""
-    if hasattr(sys, 'argv') and sys.argv[0]:
+    if hasattr(sys, "argv") and sys.argv[0]:
         app_name = os.path.basename(sys.argv[0])
     else:
         app_name = None
