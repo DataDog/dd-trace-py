68e1bbb295ce90a299cc544e6bc75ac3d2a25e68
brettlangdon <brett.langdon@datadoghq.com>
2020-02-10 11:50:05 -0500

    fix double dot

--- ddtrace/contrib/dbapi/__init__.py
+++ ddtrace/contrib/dbapi/__init__.py
@@ -43,7 +43,7 @@ class TracedCursor(wrapt.ObjectProxy):
         if not pin or not pin.enabled():
             return method(*args, **kwargs)
         service = pin.service
-        measured = name == self.._self_datadog_name
+        measured = name == self._self_datadog_name
         with pin.tracer.trace(name, service=service, resource=resource,
                               span_type=SpanTypes.SQL, _measured=measured) as s:
             # No reason to tag the query since it is set as the resource by the agent. See:
