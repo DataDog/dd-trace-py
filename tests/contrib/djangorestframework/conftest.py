d617e821c810edb1c9a4d0f2843efd6685e51470
Tahir H. Butt <tahir.butt@datadoghq.com>
2020-02-10 14:31:33 -0500

    fix drf tests

--- tests/contrib/djangorestframework/conftest.py
+++ tests/contrib/djangorestframework/conftest.py
@@ -1,15 +1,15 @@
-__all__ = ["pytest_configure", "test_spans"]
+__all__ = ["pytest_configure", "test_spans", "tracer", "patch_django"]
 
 import os
 import django
 from django.conf import settings
 
 from ddtrace.contrib.django import patch
-from ..django.conftest import test_spans
+from ..django.conftest import test_spans, tracer, patch_django
 
 # We manually designate which settings we will be using in an environment variable
 # This is similar to what occurs in the `manage.py`
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.contrib.djangorestframework.app.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "tests.contrib.djangorestframework.app.settings"
 
 
 # `pytest` automatically calls this function once when tests are run.
