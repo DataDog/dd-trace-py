ca6f548c34f92bdf55f44fa81f65b649d6b6e0ea
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2020-03-11 15:56:43 -0400

    Write new version of json encoder

--- tests/test_encoders.py
+++ tests/test_encoders.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 
 from ddtrace.span import Span
 from ddtrace.compat import msgpack_type, string_type
-from ddtrace.encoding import JSONEncoder, MsgpackEncoder
+from ddtrace.encoding import JSONEncoder, JSONEncoderV2, MsgpackEncoder
 
 
 class TestEncoders(TestCase):
@@ -58,6 +58,70 @@ class TestEncoders(TestCase):
         for i in range(2):
             for j in range(2):
                 assert "client.testing" == items[i][j]["name"]
+<<<<<<< HEAD
+=======
+
+    def test_encode_traces_json_v2(self):
+        # test encoding for JSON format
+        traces = []
+        traces.append(
+            [
+                Span(name="client.testing", tracer=None, span_id=0xAAAAAA),
+                Span(name="client.testing", tracer=None, span_id=0xAAAAAA),
+            ]
+        )
+        traces.append(
+            [
+                Span(name="client.testing", tracer=None, span_id=0xAAAAAA),
+                Span(name="client.testing", tracer=None, span_id=0xAAAAAA),
+            ]
+        )
+
+        encoder = JSONEncoderV2()
+        spans = encoder.encode_traces(traces)
+        items = json.loads(spans)["traces"]
+
+        # test the encoded output that should be a string
+        # and the output must be flatten
+        assert isinstance(spans, string_type)
+        assert len(items) == 2
+        assert len(items[0]) == 2
+        assert len(items[1]) == 2
+        for i in range(2):
+            for j in range(2):
+                assert "client.testing" == items[i][j]["name"]
+                assert isinstance(items["span_id"], string_type)
+                assert items["span_id"] == "0xAAAAAA"
+
+    def test_join_encoded_json_v2(self):
+        # test encoding for JSON format
+        traces = []
+        traces.append([Span(name="client.testing", tracer=None), Span(name="client.testing", tracer=None)])
+        traces.append([Span(name="client.testing", tracer=None), Span(name="client.testing", tracer=None)])
+
+        encoder = JSONEncoderV2()
+
+        # Encode each trace on it's own
+        encoded_traces = [encoder.encode_trace(trace) for trace in traces]
+
+        # Join the encoded traces together
+        data = encoder.join_encoded(encoded_traces)["traces"]
+
+        # Parse the resulting data
+        items = json.loads(data)
+
+        # test the encoded output that should be a string
+        # and the output must be flatten
+        assert isinstance(data, string_type)
+        assert len(items) == 2
+        assert len(items[0]) == 2
+        assert len(items[1]) == 2
+        for i in range(2):
+            for j in range(2):
+                assert "client.testing" == items[i][j]["name"]
+                assert isinstance(items["span_id"], string_type)
+                assert items["span_id"] == "0xAAAAAA"
+>>>>>>> Write new version of json encoder
 
     def test_encode_traces_msgpack(self):
         # test encoding for MsgPack format
