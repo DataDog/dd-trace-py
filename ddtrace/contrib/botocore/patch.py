9d73007a89cf8d1f58185db81a9ca45798f5051f
Zach Hammer <zhammer@seatgeek.com>
2020-03-12 08:43:13 -0400

    guard against missing botocore response metadata (#1264)
    * guard against missing botocore response metadata
    
    as is done for request_id
    
    * avoid omitting false-y values

--- ddtrace/contrib/botocore/patch.py
+++ ddtrace/contrib/botocore/patch.py
@@ -70,12 +70,15 @@ def patched_api_call(original_func, instance, args, kwargs):
         result = original_func(*args, **kwargs)
 
         response_meta = result['ResponseMetadata']
-        span.set_tag(http.STATUS_CODE, response_meta['HTTPStatusCode'])
-        span.set_tag('retry_attempts', response_meta['RetryAttempts'])
 
-        request_id = response_meta.get('RequestId')
-        if request_id:
-            span.set_tag('aws.requestid', request_id)
+        if 'HTTPStatusCode' in response_meta:
+            span.set_tag(http.STATUS_CODE, response_meta['HTTPStatusCode'])
+
+        if 'RetryAttempts' in response_meta:
+            span.set_tag('retry_attempts', response_meta['RetryAttempts'])
+
+        if 'RequestId' in response_meta:
+            span.set_tag('aws.requestid', response_meta['RequestId'])
 
         # set analytics sample rate
         span.set_tag(
