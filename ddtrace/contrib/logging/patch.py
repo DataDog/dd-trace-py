440b3dd8212a95c08257a7651e4e0b0dd4239ced
brettlangdon <brett.langdon@datadoghq.com>
2020-03-12 18:58:55 -0400

    inject dd.service, dd.env, and dd.version into logs

--- ddtrace/contrib/logging/patch.py
+++ ddtrace/contrib/logging/patch.py
@@ -1,31 +1,74 @@
 import logging
 
-from ddtrace import config
+import ddtrace
 
-from ...helpers import get_correlation_ids
+from ...constants import ENV_KEY, VERSION_KEY
 from ...utils.wrappers import unwrap as _u
 from ...vendor.wrapt import wrap_function_wrapper as _w
 
-RECORD_ATTR_TRACE_ID = 'dd.trace_id'
-RECORD_ATTR_SPAN_ID = 'dd.span_id'
-RECORD_ATTR_VALUE_NULL = 0
+RECORD_ATTR_TRACE_ID = "dd.trace_id"
+RECORD_ATTR_SPAN_ID = "dd.span_id"
+RECORD_ATTR_ENV = "dd.env"
+RECORD_ATTR_VERSION = "dd.version"
+RECORD_ATTR_SERVICE = "dd.service"
+RECORD_ATTR_VALUE_ZERO = 0
+RECORD_ATTR_VALUE_EMPTY = ""
 
-config._add('logging', dict(
-    tracer=None,  # by default, override here for custom tracer
-))
+ddtrace.config._add("logging", dict(tracer=None,))  # by default, override here for custom tracer
+
+
+def _get_current_span(tracer=None):
+    """Helper to get the currently active span"""
+    if not tracer:
+        tracer = ddtrace.tracer
+
+    if not tracer.enabled:
+        return None
+
+    return tracer.current_span()
 
 
 def _w_makeRecord(func, instance, args, kwargs):
+    # Get the LogRecord instance for this log
     record = func(*args, **kwargs)
 
-    # add correlation identifiers to LogRecord
-    trace_id, span_id = get_correlation_ids(tracer=config.logging.tracer)
-    if trace_id and span_id:
-        setattr(record, RECORD_ATTR_TRACE_ID, trace_id)
-        setattr(record, RECORD_ATTR_SPAN_ID, span_id)
+    # Get the currently active span, if there is one
+    span = _get_current_span(tracer=ddtrace.config.logging.tracer)
+
+    # Inject `dd.version`
+    # Order of precedence:
+    #   - `version` tag on the currentl active span
+    #   - `config.version` config (`DD_VERSION` env)
+    #   - empty string
+    version = None
+    if span:
+        version = span.get_tag(VERSION_KEY)
+    version = version or ddtrace.config.version or RECORD_ATTR_VALUE_EMPTY
+    setattr(record, RECORD_ATTR_VERSION, version)
+
+    # Inject `dd.env`
+    # Order of precedence:
+    #   - `env` tag on the currentl active span
+    #   - `config.env` config (`DD_ENV` env)
+    #   - empty string
+    env = None
+    if span:
+        env = span.get_tag(ENV_KEY)
+    env = env or ddtrace.config.env or RECORD_ATTR_VALUE_EMPTY
+    setattr(record, RECORD_ATTR_ENV, env)
+
+    service = ""
+    if span:
+        service = span.service or RECORD_ATTR_VALUE_EMPTY
+    setattr(record, RECORD_ATTR_SERVICE, service)
+
+    # Inject `dd.trace_id` and `dd.span_id`
+    if span:
+        setattr(record, RECORD_ATTR_TRACE_ID, span.trace_id)
+        setattr(record, RECORD_ATTR_SPAN_ID, span.span_id)
     else:
-        setattr(record, RECORD_ATTR_TRACE_ID, RECORD_ATTR_VALUE_NULL)
-        setattr(record, RECORD_ATTR_SPAN_ID, RECORD_ATTR_VALUE_NULL)
+        setattr(record, RECORD_ATTR_TRACE_ID, RECORD_ATTR_VALUE_ZERO)
+        setattr(record, RECORD_ATTR_SPAN_ID, RECORD_ATTR_VALUE_ZERO)
 
     return record
 
@@ -35,15 +78,15 @@ def patch():
     Patch ``logging`` module in the Python Standard Library for injection of
     tracer information by wrapping the base factory method ``Logger.makeRecord``
     """
-    if getattr(logging, '_datadog_patch', False):
+    if getattr(logging, "_datadog_patch", False):
         return
-    setattr(logging, '_datadog_patch', True)
+    setattr(logging, "_datadog_patch", True)
 
-    _w(logging.Logger, 'makeRecord', _w_makeRecord)
+    _w(logging.Logger, "makeRecord", _w_makeRecord)
 
 
 def unpatch():
-    if getattr(logging, '_datadog_patch', False):
-        setattr(logging, '_datadog_patch', False)
+    if getattr(logging, "_datadog_patch", False):
+        setattr(logging, "_datadog_patch", False)
 
-        _u(logging.Logger, 'makeRecord')
+        _u(logging.Logger, "makeRecord")
--- tests/contrib/logging/test_logging.py
+++ tests/contrib/logging/test_logging.py
@@ -1,6 +1,7 @@
 import logging
 
-from ddtrace.helpers import get_correlation_ids
+import ddtrace
+from ddtrace.constants import ENV_KEY, VERSION_KEY
 from ddtrace.compat import StringIO
 from ddtrace.contrib.logging import patch, unpatch
 from ddtrace.vendor import wrapt
@@ -11,8 +12,19 @@ from ...base import BaseTracerTestCase
 logger = logging.getLogger()
 logger.level = logging.INFO
 
+DEFAULT_FORMAT = (
+    "%(message)s - dd.service=%(dd.service)s dd.version=%(dd.version)s dd.env=%(dd.env)s"
+    " dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s"
+)
 
-def capture_function_log(func, fmt):
+
+def current_span(tracer=None):
+    if not tracer:
+        tracer = ddtrace.tracer
+    return tracer.current_span()
+
+
+def capture_function_log(func, fmt=DEFAULT_FORMAT):
     # add stream handler to capture output
     out = StringIO()
     sh = logging.StreamHandler(out)
@@ -41,55 +53,107 @@ class LoggingTestCase(BaseTracerTestCase):
         """
         Confirm patching was successful
         """
-        patch()
         log = logging.getLogger()
         self.assertTrue(isinstance(log.makeRecord, wrapt.BoundFunctionWrapper))
+
+        unpatch()
+        log = logging.getLogger()
+        self.assertFalse(isinstance(log.makeRecord, wrapt.BoundFunctionWrapper))
+
+    def _test_logging(self, create_span, version="", env=""):
+        def func():
+            span = create_span()
+            logger.info("Hello!")
+            if span:
+                span.finish()
+            return span
+
+        with self.override_config("logging", dict(tracer=self.tracer)):
+            # with format string for trace info
+            output, span = capture_function_log(func)
+            trace_id = 0
+            span_id = 0
+            service = ""
+            if span:
+                trace_id = span.trace_id
+                span_id = span.span_id
+                service = span.service or ""
+
+            assert output == "Hello! - dd.service={} dd.version={} dd.env={} dd.trace_id={} dd.span_id={}".format(
+                service, version, env, trace_id, span_id
+            )
+
+            # without format string
+            output, _ = capture_function_log(func, fmt="%(message)s")
+            assert output == "Hello!"
 
     def test_log_trace(self):
         """
         Check logging patched and formatter including trace info
         """
-        @self.tracer.wrap()
-        def func():
-            logger.info('Hello!')
-            return get_correlation_ids(tracer=self.tracer)
 
-        with self.override_config('logging', dict(tracer=self.tracer)):
-            # with format string for trace info
-            output, result = capture_function_log(
-                func,
-                fmt='%(message)s - dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s',
-            )
-            self.assertEqual(
-                output,
-                'Hello! - dd.trace_id={} dd.span_id={}'.format(*result),
-            )
+        def create_span():
+            return self.tracer.trace("test.logging")
 
-            # without format string
-            output, _ = capture_function_log(
-                func,
-                fmt='%(message)s',
-            )
-            self.assertEqual(
-                output,
-                'Hello!',
-            )
+        self._test_logging(create_span=create_span)
+
+        with self.override_global_config(dict(version="global.version", env="global.env")):
+            self._test_logging(create_span=create_span, version="global.version", env="global.env")
+
+    def test_log_trace_service(self):
+        """
+        Check logging patched and formatter including trace info
+        """
+
+        def create_span():
+            return self.tracer.trace("test.logging", service="logging")
+
+        self._test_logging(create_span=create_span)
+
+        with self.override_global_config(dict(version="global.version", env="global.env")):
+            self._test_logging(create_span=create_span, version="global.version", env="global.env")
+
+    def test_log_trace_version(self):
+        """
+        Check logging patched and formatter including trace info
+        """
+
+        def create_span():
+            span = self.tracer.trace("test.logging")
+            span.set_tag(VERSION_KEY, "manual.version")
+            return span
+
+        self._test_logging(create_span=create_span, version="manual.version")
+
+        # Setting global config version and overriding with span specific value
+        with self.override_global_config(dict(version="global.version", env="global.env")):
+            self._test_logging(create_span=create_span, version="manual.version", env="global.env")
+
+    def test_log_trace_env(self):
+        """
+        Check logging patched and formatter including trace info
+        """
+
+        def create_span():
+            span = self.tracer.trace("test.logging")
+            span.set_tag(ENV_KEY, "manual.env")
+            return span
+
+        self._test_logging(create_span=create_span, env="manual.env")
+
+        # Setting global config env and overriding with span specific value
+        with self.override_global_config(dict(version="global.version", env="global.env")):
+            self._test_logging(create_span=create_span, version="global.version", env="manual.env")
 
     def test_log_no_trace(self):
         """
         Check traced funclogging patched and formatter not including trace info
         """
-        def func():
-            logger.info('Hello!')
-            return get_correlation_ids()
 
-        with self.override_config('logging', dict(tracer=self.tracer)):
-            # with format string for trace info
-            output, _ = capture_function_log(
-                func,
-                fmt='%(message)s - dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s',
-            )
-            self.assertEqual(
-                output,
-                'Hello! - dd.trace_id=0 dd.span_id=0',
-            )
+        def create_span():
+            return None
+
+        self._test_logging(create_span=create_span)
+
+        with self.override_global_config(dict(version="global.version", env="global.env")):
+            self._test_logging(create_span=create_span, version="global.version", env="global.env")
