ec70850f324fdec4931975b39054173359e9eae9
Kyle Verhoog <kyle.verhoog@datadoghq.com>
2020-03-20 14:59:39 -0400

    molten: prioritize DD_SERVICE

--- ddtrace/contrib/molten/patch.py
+++ ddtrace/contrib/molten/patch.py
@@ -17,7 +17,7 @@ MOLTEN_VERSION = tuple(map(int, molten.__version__.split()[0].split('.')))
 
 # Configure default configuration
 config._add('molten', dict(
-    service_name=get_env('molten', 'service_name', default='molten'),
+    service_name=config.get_service(default=get_env('molten', 'service_name', default='molten')),
     app='molten',
     distributed_tracing=asbool(get_env('molten', 'distributed_tracing', default=True)),
 ))
