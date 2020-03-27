2e608fe8c98b887ca875f286345a0d9d000fed5b
Kyle Verhoog <kyle.verhoog@datadoghq.com>
2020-03-27 17:31:44 -0400

    celery: black fmt tests

--- tests/contrib/celery/test_integration.py
+++ tests/contrib/celery/test_integration.py
@@ -105,13 +105,13 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
 
         assert_is_measured(span)
         assert span.error == 0
-        assert span.name == 'celery.run'
-        assert span.resource == 'tests.contrib.celery.test_integration.fn_task'
-        assert span.service == 'celery-worker'
-        assert span.span_type == 'worker'
-        assert span.get_tag('celery.id') == t.task_id
-        assert span.get_tag('celery.action') == 'run'
-        assert span.get_tag('celery.state') == 'SUCCESS'
+        assert span.name == "celery.run"
+        assert span.resource == "tests.contrib.celery.test_integration.fn_task"
+        assert span.service == "celery-worker"
+        assert span.span_type == "worker"
+        assert span.get_tag("celery.id") == t.task_id
+        assert span.get_tag("celery.action") == "run"
+        assert span.get_tag("celery.state") == "SUCCESS"
 
     def test_fn_task_apply_bind(self):
         # it should execute a traced task with a returning value
@@ -121,7 +121,7 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
 
         t = fn_task.apply()
         assert t.successful()
-        assert 'fn_task' in t.result.name
+        assert "fn_task" in t.result.name
 
         traces = self.tracer.writer.pop_traces()
         assert 1 == len(traces)
@@ -130,12 +130,12 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
 
         assert_is_measured(span)
         assert span.error == 0
-        assert span.name == 'celery.run'
-        assert span.resource == 'tests.contrib.celery.test_integration.fn_task'
-        assert span.service == 'celery-worker'
-        assert span.get_tag('celery.id') == t.task_id
-        assert span.get_tag('celery.action') == 'run'
-        assert span.get_tag('celery.state') == 'SUCCESS'
+        assert span.name == "celery.run"
+        assert span.resource == "tests.contrib.celery.test_integration.fn_task"
+        assert span.service == "celery-worker"
+        assert span.get_tag("celery.id") == t.task_id
+        assert span.get_tag("celery.action") == "run"
+        assert span.get_tag("celery.state") == "SUCCESS"
 
     def test_fn_task_apply_async(self):
         # it should execute a traced async task that has parameters
@@ -143,8 +143,8 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
         def fn_task_parameters(user, force_logout=False):
             return (user, force_logout)
 
-        t = fn_task_parameters.apply_async(args=['user'], kwargs={'force_logout': True})
-        assert 'PENDING' == t.status
+        t = fn_task_parameters.apply_async(args=["user"], kwargs={"force_logout": True})
+        assert "PENDING" == t.status
 
         traces = self.tracer.writer.pop_traces()
         assert 1 == len(traces)
@@ -153,12 +153,12 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
 
         assert_is_measured(span)
         assert span.error == 0
-        assert span.name == 'celery.apply'
-        assert span.resource == 'tests.contrib.celery.test_integration.fn_task_parameters'
-        assert span.service == 'celery-producer'
-        assert span.get_tag('celery.id') == t.task_id
-        assert span.get_tag('celery.action') == 'apply_async'
-        assert span.get_tag('celery.routing_key') == 'celery'
+        assert span.name == "celery.apply"
+        assert span.resource == "tests.contrib.celery.test_integration.fn_task_parameters"
+        assert span.service == "celery-producer"
+        assert span.get_tag("celery.id") == t.task_id
+        assert span.get_tag("celery.action") == "apply_async"
+        assert span.get_tag("celery.routing_key") == "celery"
 
     def test_fn_task_delay(self):
         # using delay shorthand must preserve arguments
@@ -166,8 +166,8 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
         def fn_task_parameters(user, force_logout=False):
             return (user, force_logout)
 
-        t = fn_task_parameters.delay('user', force_logout=True)
-        assert 'PENDING' == t.status
+        t = fn_task_parameters.delay("user", force_logout=True)
+        assert "PENDING" == t.status
 
         traces = self.tracer.writer.pop_traces()
         assert 1 == len(traces)
@@ -176,22 +176,22 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
 
         assert_is_measured(span)
         assert span.error == 0
-        assert span.name == 'celery.apply'
-        assert span.resource == 'tests.contrib.celery.test_integration.fn_task_parameters'
-        assert span.service == 'celery-producer'
-        assert span.get_tag('celery.id') == t.task_id
-        assert span.get_tag('celery.action') == 'apply_async'
-        assert span.get_tag('celery.routing_key') == 'celery'
+        assert span.name == "celery.apply"
+        assert span.resource == "tests.contrib.celery.test_integration.fn_task_parameters"
+        assert span.service == "celery-producer"
+        assert span.get_tag("celery.id") == t.task_id
+        assert span.get_tag("celery.action") == "apply_async"
+        assert span.get_tag("celery.routing_key") == "celery"
 
     def test_fn_exception(self):
         # it should catch exceptions in task functions
         @self.app.task
         def fn_exception():
-            raise Exception('Task class is failing')
+            raise Exception("Task class is failing")
 
         t = fn_exception.apply()
         assert t.failed()
-        assert 'Task class is failing' in t.traceback
+        assert "Task class is failing" in t.traceback
 
         traces = self.tracer.writer.pop_traces()
         assert 1 == len(traces)
@@ -199,26 +199,26 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
         span = traces[0][0]
 
         assert_is_measured(span)
-        assert span.name == 'celery.run'
-        assert span.resource == 'tests.contrib.celery.test_integration.fn_exception'
-        assert span.service == 'celery-worker'
-        assert span.get_tag('celery.id') == t.task_id
-        assert span.get_tag('celery.action') == 'run'
-        assert span.get_tag('celery.state') == 'FAILURE'
+        assert span.name == "celery.run"
+        assert span.resource == "tests.contrib.celery.test_integration.fn_exception"
+        assert span.service == "celery-worker"
+        assert span.get_tag("celery.id") == t.task_id
+        assert span.get_tag("celery.action") == "run"
+        assert span.get_tag("celery.state") == "FAILURE"
         assert span.error == 1
-        assert span.get_tag('error.msg') == 'Task class is failing'
-        assert 'Traceback (most recent call last)' in span.get_tag('error.stack')
-        assert 'Task class is failing' in span.get_tag('error.stack')
+        assert span.get_tag("error.msg") == "Task class is failing"
+        assert "Traceback (most recent call last)" in span.get_tag("error.stack")
+        assert "Task class is failing" in span.get_tag("error.stack")
 
     def test_fn_exception_expected(self):
         # it should catch exceptions in task functions
         @self.app.task(throws=(MyException,))
         def fn_exception():
-            raise MyException('Task class is failing')
+            raise MyException("Task class is failing")
 
         t = fn_exception.apply()
         assert t.failed()
-        assert 'Task class is failing' in t.traceback
+        assert "Task class is failing" in t.traceback
 
         traces = self.tracer.writer.pop_traces()
         assert 1 == len(traces)
@@ -226,23 +226,23 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
         span = traces[0][0]
 
         assert_is_measured(span)
-        assert span.name == 'celery.run'
-        assert span.resource == 'tests.contrib.celery.test_integration.fn_exception'
-        assert span.service == 'celery-worker'
-        assert span.get_tag('celery.id') == t.task_id
-        assert span.get_tag('celery.action') == 'run'
-        assert span.get_tag('celery.state') == 'FAILURE'
+        assert span.name == "celery.run"
+        assert span.resource == "tests.contrib.celery.test_integration.fn_exception"
+        assert span.service == "celery-worker"
+        assert span.get_tag("celery.id") == t.task_id
+        assert span.get_tag("celery.action") == "run"
+        assert span.get_tag("celery.state") == "FAILURE"
         assert span.error == 0
 
     def test_fn_retry_exception(self):
         # it should not catch retry exceptions in task functions
         @self.app.task
         def fn_exception():
-            raise Retry('Task class is being retried')
+            raise Retry("Task class is being retried")
 
         t = fn_exception.apply()
         assert not t.failed()
-        assert 'Task class is being retried' in t.traceback
+        assert "Task class is being retried" in t.traceback
 
         traces = self.tracer.writer.pop_traces()
         assert 1 == len(traces)
@@ -250,18 +250,18 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
         span = traces[0][0]
 
         assert_is_measured(span)
-        assert span.name == 'celery.run'
-        assert span.resource == 'tests.contrib.celery.test_integration.fn_exception'
-        assert span.service == 'celery-worker'
-        assert span.get_tag('celery.id') == t.task_id
-        assert span.get_tag('celery.action') == 'run'
-        assert span.get_tag('celery.state') == 'RETRY'
-        assert span.get_tag('celery.retry.reason') == 'Task class is being retried'
+        assert span.name == "celery.run"
+        assert span.resource == "tests.contrib.celery.test_integration.fn_exception"
+        assert span.service == "celery-worker"
+        assert span.get_tag("celery.id") == t.task_id
+        assert span.get_tag("celery.action") == "run"
+        assert span.get_tag("celery.state") == "RETRY"
+        assert span.get_tag("celery.retry.reason") == "Task class is being retried"
 
         # This type of retrying should not be marked as an exception
         assert span.error == 0
-        assert not span.get_tag('error.msg')
-        assert not span.get_tag('error.stack')
+        assert not span.get_tag("error.msg")
+        assert not span.get_tag("error.stack")
 
     def test_class_task(self):
         # it should execute class based tasks with a returning value
@@ -271,7 +271,7 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
 
         t = BaseTask()
         # register the Task class if it's available (required in Celery 4.0+)
-        register_task = getattr(self.app, 'register_task', None)
+        register_task = getattr(self.app, "register_task", None)
         if register_task is not None:
             register_task(t)
 
@@ -286,28 +286,28 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
 
         assert_is_measured(span)
         assert span.error == 0
-        assert span.name == 'celery.run'
-        assert span.resource == 'tests.contrib.celery.test_integration.BaseTask'
-        assert span.service == 'celery-worker'
-        assert span.get_tag('celery.id') == r.task_id
-        assert span.get_tag('celery.action') == 'run'
-        assert span.get_tag('celery.state') == 'SUCCESS'
+        assert span.name == "celery.run"
+        assert span.resource == "tests.contrib.celery.test_integration.BaseTask"
+        assert span.service == "celery-worker"
+        assert span.get_tag("celery.id") == r.task_id
+        assert span.get_tag("celery.action") == "run"
+        assert span.get_tag("celery.state") == "SUCCESS"
 
     def test_class_task_exception(self):
         # it should catch exceptions in class based tasks
         class BaseTask(self.app.Task):
             def run(self):
-                raise Exception('Task class is failing')
+                raise Exception("Task class is failing")
 
         t = BaseTask()
         # register the Task class if it's available (required in Celery 4.0+)
-        register_task = getattr(self.app, 'register_task', None)
+        register_task = getattr(self.app, "register_task", None)
         if register_task is not None:
             register_task(t)
 
         r = t.apply()
         assert r.failed()
-        assert 'Task class is failing' in r.traceback
+        assert "Task class is failing" in r.traceback
 
         traces = self.tracer.writer.pop_traces()
         assert 1 == len(traces)
@@ -315,16 +315,16 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
         span = traces[0][0]
 
         assert_is_measured(span)
-        assert span.name == 'celery.run'
-        assert span.resource == 'tests.contrib.celery.test_integration.BaseTask'
-        assert span.service == 'celery-worker'
-        assert span.get_tag('celery.id') == r.task_id
-        assert span.get_tag('celery.action') == 'run'
-        assert span.get_tag('celery.state') == 'FAILURE'
+        assert span.name == "celery.run"
+        assert span.resource == "tests.contrib.celery.test_integration.BaseTask"
+        assert span.service == "celery-worker"
+        assert span.get_tag("celery.id") == r.task_id
+        assert span.get_tag("celery.action") == "run"
+        assert span.get_tag("celery.state") == "FAILURE"
         assert span.error == 1
-        assert span.get_tag('error.msg') == 'Task class is failing'
-        assert 'Traceback (most recent call last)' in span.get_tag('error.stack')
-        assert 'Task class is failing' in span.get_tag('error.stack')
+        assert span.get_tag("error.msg") == "Task class is failing"
+        assert "Traceback (most recent call last)" in span.get_tag("error.stack")
+        assert "Task class is failing" in span.get_tag("error.stack")
 
     def test_class_task_exception_expected(self):
         # it should catch exceptions in class based tasks
@@ -332,17 +332,17 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
             throws = (MyException,)
 
             def run(self):
-                raise MyException('Task class is failing')
+                raise MyException("Task class is failing")
 
         t = BaseTask()
         # register the Task class if it's available (required in Celery 4.0+)
-        register_task = getattr(self.app, 'register_task', None)
+        register_task = getattr(self.app, "register_task", None)
         if register_task is not None:
             register_task(t)
 
         r = t.apply()
         assert r.failed()
-        assert 'Task class is failing' in r.traceback
+        assert "Task class is failing" in r.traceback
 
         traces = self.tracer.writer.pop_traces()
         assert 1 == len(traces)
@@ -350,12 +350,12 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
         span = traces[0][0]
 
         assert_is_measured(span)
-        assert span.name == 'celery.run'
-        assert span.resource == 'tests.contrib.celery.test_integration.BaseTask'
-        assert span.service == 'celery-worker'
-        assert span.get_tag('celery.id') == r.task_id
-        assert span.get_tag('celery.action') == 'run'
-        assert span.get_tag('celery.state') == 'FAILURE'
+        assert span.name == "celery.run"
+        assert span.resource == "tests.contrib.celery.test_integration.BaseTask"
+        assert span.service == "celery-worker"
+        assert span.get_tag("celery.id") == r.task_id
+        assert span.get_tag("celery.action") == "run"
+        assert span.get_tag("celery.state") == "FAILURE"
         assert span.error == 0
 
     def test_shared_task(self):
@@ -374,13 +374,13 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
 
         assert_is_measured(span)
         assert span.error == 0
-        assert span.name == 'celery.run'
-        assert span.service == 'celery-worker'
-        assert span.resource == 'tests.contrib.celery.test_integration.add'
+        assert span.name == "celery.run"
+        assert span.service == "celery-worker"
+        assert span.resource == "tests.contrib.celery.test_integration.add"
         assert span.parent_id is None
-        assert span.get_tag('celery.id') == res.task_id
-        assert span.get_tag('celery.action') == 'run'
-        assert span.get_tag('celery.state') == 'SUCCESS'
+        assert span.get_tag("celery.id") == res.task_id
+        assert span.get_tag("celery.action") == "run"
+        assert span.get_tag("celery.state") == "SUCCESS"
 
     def test_worker_service_name(self):
         @self.app.task
@@ -389,7 +389,7 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
 
         # Ensure worker service name can be changed via
         # configuration object
-        with self.override_config('celery', dict(worker_service_name='worker-notify')):
+        with self.override_config("celery", dict(worker_service_name="worker-notify")):
             t = fn_task.apply()
             self.assertTrue(t.successful())
             self.assertEqual(42, t.result)
@@ -398,7 +398,7 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
             self.assertEqual(1, len(traces))
             self.assertEqual(1, len(traces[0]))
             span = traces[0][0]
-            self.assertEqual(span.service, 'worker-notify')
+            self.assertEqual(span.service, "worker-notify")
 
     def test_producer_service_name(self):
         @self.app.task
@@ -407,28 +407,28 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
 
         # Ensure producer service name can be changed via
         # configuration object
-        with self.override_config('celery', dict(producer_service_name='task-queue')):
+        with self.override_config("celery", dict(producer_service_name="task-queue")):
             t = fn_task.delay()
-            self.assertEqual('PENDING', t.status)
+            self.assertEqual("PENDING", t.status)
 
             traces = self.tracer.writer.pop_traces()
             self.assertEqual(1, len(traces))
             self.assertEqual(1, len(traces[0]))
             span = traces[0][0]
-            self.assertEqual(span.service, 'task-queue')
+            self.assertEqual(span.service, "task-queue")
 
     def test_fn_task_apply_async_ot(self):
         """OpenTracing version of test_fn_task_apply_async."""
-        ot_tracer = init_tracer('celery_svc', self.tracer)
+        ot_tracer = init_tracer("celery_svc", self.tracer)
 
         # it should execute a traced async task that has parameters
         @self.app.task
         def fn_task_parameters(user, force_logout=False):
             return (user, force_logout)
 
-        with ot_tracer.start_active_span('celery_op'):
-            t = fn_task_parameters.apply_async(args=['user'], kwargs={'force_logout': True})
-            assert 'PENDING' == t.status
+        with ot_tracer.start_active_span("celery_op"):
+            t = fn_task_parameters.apply_async(args=["user"], kwargs={"force_logout": True})
+            assert "PENDING" == t.status
 
         traces = self.tracer.writer.pop_traces()
         assert 1 == len(traces)
@@ -439,14 +439,14 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
         assert ot_span.parent_id is None
         assert dd_span.parent_id == ot_span.span_id
 
-        assert ot_span.name == 'celery_op'
-        assert ot_span.service == 'celery_svc'
+        assert ot_span.name == "celery_op"
+        assert ot_span.service == "celery_svc"
 
         assert_is_measured(dd_span)
         assert dd_span.error == 0
-        assert dd_span.name == 'celery.apply'
-        assert dd_span.resource == 'tests.contrib.celery.test_integration.fn_task_parameters'
-        assert dd_span.service == 'celery-producer'
-        assert dd_span.get_tag('celery.id') == t.task_id
-        assert dd_span.get_tag('celery.action') == 'apply_async'
-        assert dd_span.get_tag('celery.routing_key') == 'celery'
+        assert dd_span.name == "celery.apply"
+        assert dd_span.resource == "tests.contrib.celery.test_integration.fn_task_parameters"
+        assert dd_span.service == "celery-producer"
+        assert dd_span.get_tag("celery.id") == t.task_id
+        assert dd_span.get_tag("celery.action") == "apply_async"
+        assert dd_span.get_tag("celery.routing_key") == "celery"
