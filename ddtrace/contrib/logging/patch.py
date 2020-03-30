392e0fd325bc2e8d0d2d2a14c953083cef77c147
Kyle Verhoog <kyle@verhoog.ca>
2020-03-30 16:06:31 -0400

    logging: only use global setting for env, service and version (#1317)

--- ddtrace/contrib/logging/patch.py
+++ ddtrace/contrib/logging/patch.py
@@ -2,7 +2,6 @@ import logging
 
 import ddtrace
 
-from ...constants import ENV_KEY, VERSION_KEY
 from ...utils.wrappers import unwrap as _u
 from ...vendor.wrapt import wrap_function_wrapper as _w
 
@@ -32,37 +31,11 @@ def _w_makeRecord(func, instance, args, kwargs):
     # Get the LogRecord instance for this log
     record = func(*args, **kwargs)
 
-    # Get the currently active span, if there is one
+    setattr(record, RECORD_ATTR_VERSION, ddtrace.config.version or "")
+    setattr(record, RECORD_ATTR_ENV, ddtrace.config.env or "")
+    setattr(record, RECORD_ATTR_SERVICE, ddtrace.config.service or "")
+
     span = _get_current_span(tracer=ddtrace.config.logging.tracer)
-
-    # Inject `dd.version`
-    # Order of precedence:
-    #   - `version` tag on the currently active span
-    #   - `config.version` config (`DD_VERSION` env)
-    #   - empty string
-    version = None
-    if span:
-        version = span.get_tag(VERSION_KEY)
-    version = version or ddtrace.config.version or RECORD_ATTR_VALUE_EMPTY
-    setattr(record, RECORD_ATTR_VERSION, version)
-
-    # Inject `dd.env`
-    # Order of precedence:
-    #   - `env` tag on the currently active span
-    #   - `config.env` config (`DD_ENV` env)
-    #   - empty string
-    env = None
-    if span:
-        env = span.get_tag(ENV_KEY)
-    env = env or ddtrace.config.env or RECORD_ATTR_VALUE_EMPTY
-    setattr(record, RECORD_ATTR_ENV, env)
-
-    service = ""
-    if span:
-        service = span.service or RECORD_ATTR_VALUE_EMPTY
-    setattr(record, RECORD_ATTR_SERVICE, service)
-
-    # Inject `dd.trace_id` and `dd.span_id`
     if span:
         setattr(record, RECORD_ATTR_TRACE_ID, span.trace_id)
         setattr(record, RECORD_ATTR_SPAN_ID, span.span_id)
