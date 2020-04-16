da57e1e0fdff067a3019e09b75ac161562bc5136
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2020-04-16 14:31:07 -0700

    revert to master

--- tests/contrib/asyncio/test_tracer.py
+++ tests/contrib/asyncio/test_tracer.py
@@ -1,20 +1,19 @@
-# flake8: noqa
-# DEV: Skip linting, we lint with Python 2, we'll get SyntaxErrors from `yield from`
 import asyncio
+import pytest
+import time
 
-from asyncio import BaseEventLoop
 
 from ddtrace.context import Context
+from ddtrace.internal.context_manager import CONTEXTVARS_IS_AVAILABLE
 from ddtrace.provider import DefaultContextProvider
 from ddtrace.contrib.asyncio.patch import patch, unpatch
 from ddtrace.contrib.asyncio.helpers import set_call_context
 
-from nose.tools import eq_, ok_
 from tests.opentracer.utils import init_tracer
 from .utils import AsyncioTestCase, mark_asyncio
 
 
-_orig_create_task = BaseEventLoop.create_task
+_orig_create_task = asyncio.BaseEventLoop.create_task
 
 
 class TestAsyncioTracer(AsyncioTestCase):
@@ -22,21 +21,25 @@ class TestAsyncioTracer(AsyncioTestCase):
     the same ``IOLoop``.
     """
     @mark_asyncio
+    @pytest.mark.skipif(
+        CONTEXTVARS_IS_AVAILABLE,
+        reason='only applicable to legacy asyncio provider'
+    )
     def test_get_call_context(self):
         # it should return the context attached to the current Task
         # or create a new one
         task = asyncio.Task.current_task()
         ctx = getattr(task, '__datadog_context', None)
-        ok_(ctx is None)
+        assert ctx is None
         # get the context from the loop creates a new one that
         # is attached to the Task object
         ctx = self.tracer.get_call_context()
-        eq_(ctx, getattr(task, '__datadog_context', None))
+        assert ctx == getattr(task, '__datadog_context', None)
 
     @mark_asyncio
     def test_get_call_context_twice(self):
         # it should return the same Context if called twice
-        eq_(self.tracer.get_call_context(), self.tracer.get_call_context())
+        assert self.tracer.get_call_context() == self.tracer.get_call_context()
 
     @mark_asyncio
     def test_trace_coroutine(self):
@@ -45,10 +48,10 @@ class TestAsyncioTracer(AsyncioTestCase):
             span.resource = 'base'
 
         traces = self.tracer.writer.pop_traces()
-        eq_(1, len(traces))
-        eq_(1, len(traces[0]))
-        eq_('coroutine', traces[0][0].name)
-        eq_('base', traces[0][0].resource)
+        assert 1 == len(traces)
+        assert 1 == len(traces[0])
+        assert 'coroutine' == traces[0][0].name
+        assert 'base' == traces[0][0].resource
 
     @mark_asyncio
     def test_trace_multiple_coroutines(self):
@@ -64,23 +67,23 @@ class TestAsyncioTracer(AsyncioTestCase):
             value = yield from coro()
 
         # the coroutine has been called correctly
-        eq_(42, value)
+        assert 42 == value
         # a single trace has been properly reported
         traces = self.tracer.writer.pop_traces()
-        eq_(1, len(traces))
-        eq_(2, len(traces[0]))
-        eq_('coroutine_1', traces[0][0].name)
-        eq_('coroutine_2', traces[0][1].name)
+        assert 1 == len(traces)
+        assert 2 == len(traces[0])
+        assert 'coroutine_1' == traces[0][0].name
+        assert 'coroutine_2' == traces[0][1].name
         # the parenting is correct
-        eq_(traces[0][0], traces[0][1]._parent)
-        eq_(traces[0][0].trace_id, traces[0][1].trace_id)
+        assert traces[0][0] == traces[0][1]._parent
+        assert traces[0][0].trace_id == traces[0][1].trace_id
 
     @mark_asyncio
     def test_event_loop_exception(self):
         # it should handle a loop exception
         asyncio.set_event_loop(None)
         ctx = self.tracer.get_call_context()
-        ok_(ctx is not None)
+        assert ctx is not None
 
     def test_context_task_none(self):
         # it should handle the case where a Task is not available
@@ -88,10 +91,10 @@ class TestAsyncioTracer(AsyncioTestCase):
         # without a Task
         task = asyncio.Task.current_task()
         # the task is not available
-        ok_(task is None)
+        assert task is None
         # but a new Context is still created making the operation safe
         ctx = self.tracer.get_call_context()
-        ok_(ctx is not None)
+        assert ctx is not None
 
     @mark_asyncio
     def test_exception(self):
@@ -103,13 +106,13 @@ class TestAsyncioTracer(AsyncioTestCase):
         with self.assertRaises(Exception):
             yield from f1()
         traces = self.tracer.writer.pop_traces()
-        eq_(1, len(traces))
+        assert 1 == len(traces)
         spans = traces[0]
-        eq_(1, len(spans))
+        assert 1 == len(spans)
         span = spans[0]
-        eq_(1, span.error)
-        eq_('f1 error', span.get_tag('error.msg'))
-        ok_('Exception: f1 error' in span.get_tag('error.stack'))
+        assert 1 == span.error
+        assert 'f1 error' == span.get_tag('error.msg')
+        assert 'Exception: f1 error' in span.get_tag('error.stack')
 
     @mark_asyncio
     def test_nested_exceptions(self):
@@ -127,19 +130,19 @@ class TestAsyncioTracer(AsyncioTestCase):
             yield from f2()
 
         traces = self.tracer.writer.pop_traces()
-        eq_(1, len(traces))
+        assert 1 == len(traces)
         spans = traces[0]
-        eq_(2, len(spans))
+        assert 2 == len(spans)
         span = spans[0]
-        eq_('f2', span.name)
-        eq_(1, span.error) # f2 did not catch the exception
-        eq_('f1 error', span.get_tag('error.msg'))
-        ok_('Exception: f1 error' in span.get_tag('error.stack'))
+        assert 'f2' == span.name
+        assert 1 == span.error  # f2 did not catch the exception
+        assert 'f1 error' == span.get_tag('error.msg')
+        assert 'Exception: f1 error' in span.get_tag('error.stack')
         span = spans[1]
-        eq_('f1', span.name)
-        eq_(1, span.error)
-        eq_('f1 error', span.get_tag('error.msg'))
-        ok_('Exception: f1 error' in span.get_tag('error.stack'))
+        assert 'f1' == span.name
+        assert 1 == span.error
+        assert 'f1 error' == span.get_tag('error.msg')
+        assert 'Exception: f1 error' in span.get_tag('error.stack')
 
     @mark_asyncio
     def test_handled_nested_exceptions(self):
@@ -159,17 +162,17 @@ class TestAsyncioTracer(AsyncioTestCase):
         yield from f2()
 
         traces = self.tracer.writer.pop_traces()
-        eq_(1, len(traces))
+        assert 1 == len(traces)
         spans = traces[0]
-        eq_(2, len(spans))
+        assert 2 == len(spans)
         span = spans[0]
-        eq_('f2', span.name)
-        eq_(0, span.error) # f2 caught the exception
+        assert 'f2' == span.name
+        assert 0 == span.error  # f2 caught the exception
         span = spans[1]
-        eq_('f1', span.name)
-        eq_(1, span.error)
-        eq_('f1 error', span.get_tag('error.msg'))
-        ok_('Exception: f1 error' in span.get_tag('error.stack'))
+        assert 'f1' == span.name
+        assert 1 == span.error
+        assert 'f1 error' == span.get_tag('error.msg')
+        assert 'Exception: f1 error' in span.get_tag('error.stack')
 
     @mark_asyncio
     def test_trace_multiple_calls(self):
@@ -186,9 +189,9 @@ class TestAsyncioTracer(AsyncioTestCase):
             yield from future
 
         traces = self.tracer.writer.pop_traces()
-        eq_(10, len(traces))
-        eq_(1, len(traces[0]))
-        eq_('coroutine', traces[0][0].name)
+        assert 10 == len(traces)
+        assert 1 == len(traces[0])
+        assert 'coroutine' == traces[0][0].name
 
     @mark_asyncio
     def test_wrapped_coroutine(self):
@@ -200,11 +203,11 @@ class TestAsyncioTracer(AsyncioTestCase):
         yield from f1()
 
         traces = self.tracer.writer.pop_traces()
-        eq_(1, len(traces))
+        assert 1 == len(traces)
         spans = traces[0]
-        eq_(1, len(spans))
+        assert 1 == len(spans)
         span = spans[0]
-        ok_(span.duration > 0.25, msg='span.duration={}'.format(span.duration))
+        assert span.duration > 0.25, 'span.duration={}'.format(span.duration)
 
 
 class TestAsyncioPropagation(AsyncioTestCase):
@@ -222,11 +225,6 @@ class TestAsyncioPropagation(AsyncioTestCase):
     @mark_asyncio
     def test_tasks_chaining(self):
         # ensures that the context is propagated between different tasks
-        service_name = 'foobar'
-        ctx = Context(_service=service_name)
-        task = asyncio.Task.current_task()
-        set_call_context(task, ctx)
-
         @self.tracer.wrap('spawn_task')
         @asyncio.coroutine
         def coro_3():
@@ -237,32 +235,30 @@ class TestAsyncioPropagation(AsyncioTestCase):
             # This will have a new context, first run will test that the
             # new context works correctly, second run will test if when we
             # pop off the last span on the context if it is still parented
-            # corrrectly
+            # correctly
             yield from coro_3()
             yield from coro_3()
 
         @self.tracer.wrap('main_task')
         @asyncio.coroutine
         def coro_1():
-            ctx = self.tracer.get_call_context()
-            eq_(ctx._service, service_name)
             yield from asyncio.ensure_future(coro_2())
 
         yield from coro_1()
 
         traces = self.tracer.writer.pop_traces()
-        eq_(len(traces), 3)
-        eq_(len(traces[0]), 1)
-        eq_(len(traces[1]), 1)
+        assert len(traces) == 3
+        assert len(traces[0]) == 1
+        assert len(traces[1]) == 1
         main_task = traces[-1][0]
         spawn_task1 = traces[0][0]
         spawn_task2 = traces[1][0]
         # check if the context has been correctly propagated
-        eq_(spawn_task1.trace_id, main_task.trace_id)
-        eq_(spawn_task1.parent_id, main_task.span_id)
+        assert spawn_task1.trace_id == main_task.trace_id
+        assert spawn_task1.parent_id == main_task.span_id
 
-        eq_(spawn_task2.trace_id, main_task.trace_id)
-        eq_(spawn_task2.parent_id, main_task.span_id)
+        assert spawn_task2.trace_id == main_task.trace_id
+        assert spawn_task2.parent_id == main_task.span_id
 
     @mark_asyncio
     def test_concurrent_chaining(self):
@@ -280,21 +276,32 @@ class TestAsyncioPropagation(AsyncioTestCase):
 
         with self.tracer.trace('main_task'):
             yield from asyncio.gather(f1(), f2())
+            # do additional synchronous work to confirm main context is
+            # correctly handled
+            with self.tracer.trace('main_task_child'):
+                time.sleep(0.01)
 
         traces = self.tracer.writer.pop_traces()
-        eq_(len(traces), 3)
-        eq_(len(traces[0]), 1)
-        eq_(len(traces[1]), 1)
-        eq_(len(traces[2]), 1)
+        assert len(traces) == 3
+        assert len(traces[0]) == 1
+        assert len(traces[1]) == 1
+        assert len(traces[2]) == 2
         child_1 = traces[0][0]
         child_2 = traces[1][0]
         main_task = traces[2][0]
+        main_task_child = traces[2][1]
         # check if the context has been correctly propagated
-        eq_(child_1.trace_id, main_task.trace_id)
-        eq_(child_1.parent_id, main_task.span_id)
-        eq_(child_2.trace_id, main_task.trace_id)
-        eq_(child_2.parent_id, main_task.span_id)
+        assert child_1.trace_id == main_task.trace_id
+        assert child_1.parent_id == main_task.span_id
+        assert child_2.trace_id == main_task.trace_id
+        assert child_2.parent_id == main_task.span_id
+        assert main_task_child.trace_id == main_task.trace_id
+        assert main_task_child.parent_id == main_task.span_id
 
+    @pytest.mark.skipif(
+        CONTEXTVARS_IS_AVAILABLE,
+        reason='only applicable to legacy asyncio provider'
+    )
     @mark_asyncio
     def test_propagation_with_set_call_context(self):
         # ensures that if a new Context is attached to the current
@@ -307,11 +314,11 @@ class TestAsyncioPropagation(AsyncioTestCase):
             yield from asyncio.sleep(0.01)
 
         traces = self.tracer.writer.pop_traces()
-        eq_(len(traces), 1)
-        eq_(len(traces[0]), 1)
+        assert len(traces) == 1
+        assert len(traces[0]) == 1
         span = traces[0][0]
-        eq_(span.trace_id, 100)
-        eq_(span.parent_id, 101)
+        assert span.trace_id == 100
+        assert span.parent_id == 101
 
     @mark_asyncio
     def test_propagation_with_new_context(self):
@@ -324,18 +331,18 @@ class TestAsyncioPropagation(AsyncioTestCase):
             yield from asyncio.sleep(0.01)
 
         traces = self.tracer.writer.pop_traces()
-        eq_(len(traces), 1)
-        eq_(len(traces[0]), 1)
+        assert len(traces) == 1
+        assert len(traces[0]) == 1
         span = traces[0][0]
-        eq_(span.trace_id, 100)
-        eq_(span.parent_id, 101)
+        assert span.trace_id == 100
+        assert span.parent_id == 101
 
     @mark_asyncio
     def test_event_loop_unpatch(self):
         # ensures that the event loop can be unpatched
         unpatch()
-        ok_(isinstance(self.tracer._context_provider, DefaultContextProvider))
-        ok_(BaseEventLoop.create_task == _orig_create_task)
+        assert isinstance(self.tracer._context_provider, DefaultContextProvider)
+        assert asyncio.BaseEventLoop.create_task == _orig_create_task
 
     def test_event_loop_double_patch(self):
         # ensures that double patching will not double instrument
@@ -359,16 +366,16 @@ class TestAsyncioPropagation(AsyncioTestCase):
             value = yield from coro()
 
         # the coroutine has been called correctly
-        eq_(42, value)
+        assert 42 == value
         # a single trace has been properly reported
         traces = self.tracer.writer.pop_traces()
-        eq_(1, len(traces))
-        eq_(2, len(traces[0]))
-        eq_('coroutine_1', traces[0][0].name)
-        eq_('coroutine_2', traces[0][1].name)
+        assert 1 == len(traces)
+        assert 2 == len(traces[0])
+        assert 'coroutine_1' == traces[0][0].name
+        assert 'coroutine_2' == traces[0][1].name
         # the parenting is correct
-        eq_(traces[0][0], traces[0][1]._parent)
-        eq_(traces[0][0].trace_id, traces[0][1].trace_id)
+        assert traces[0][0] == traces[0][1]._parent
+        assert traces[0][0].trace_id == traces[0][1].trace_id
 
     @mark_asyncio
     def test_trace_multiple_coroutines_ot_inner(self):
@@ -386,13 +393,13 @@ class TestAsyncioPropagation(AsyncioTestCase):
             value = yield from coro()
 
         # the coroutine has been called correctly
-        eq_(42, value)
+        assert 42 == value
         # a single trace has been properly reported
         traces = self.tracer.writer.pop_traces()
-        eq_(1, len(traces))
-        eq_(2, len(traces[0]))
-        eq_('coroutine_1', traces[0][0].name)
-        eq_('coroutine_2', traces[0][1].name)
+        assert 1 == len(traces)
+        assert 2 == len(traces[0])
+        assert 'coroutine_1' == traces[0][0].name
+        assert 'coroutine_2' == traces[0][1].name
         # the parenting is correct
-        eq_(traces[0][0], traces[0][1]._parent)
-        eq_(traces[0][0].trace_id, traces[0][1].trace_id)
+        assert traces[0][0] == traces[0][1]._parent
+        assert traces[0][0].trace_id == traces[0][1].trace_id
