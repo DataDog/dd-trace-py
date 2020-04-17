4d1a06cb7ac6135e97410574ac2ef30cc38a8d79
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2020-04-17 02:57:46 -0700

    bring in changes from asyncpg branch

--- tests/contrib/asyncpg/py35/test_asyncpg.py
+++ tests/contrib/asyncpg/py35/test_asyncpg.py
@@ -1,9 +1,10 @@
+# flake8: noqa
+# DEV: Skip linting, we lint with Python 2, we'll get SyntaxErrors from `async`
 # stdlib
 import time
 
 # 3p
 import asyncpg.pool
-from nose.tools import eq_
 
 # project
 from ddtrace.contrib.asyncpg.patch import patch, unpatch
@@ -12,7 +13,7 @@ from ddtrace import Pin
 # testing
 from tests.contrib.config import POSTGRES_CONFIG
 from tests.test_tracer import get_dummy_tracer
-from tests.contrib.asyncio.utils import AsyncioTestCase, mark_sync
+from tests.contrib.asyncio.utils import AsyncioTestCase, mark_asyncio
 
 # Update to asyncpg way
 POSTGRES_CONFIG = dict(POSTGRES_CONFIG)  # make copy
@@ -28,20 +29,24 @@ class TestPsycopgPatch(AsyncioTestCase):
 
     def setUp(self):
         super().setUp()
-        self._conn = None
         patch()
 
     def tearDown(self):
-        if self._conn and not self._conn.is_closed():
-            self.loop.run_until_complete(self._conn.close())
-
         super().tearDown()
         unpatch()
 
-    async def _get_conn_and_tracer(self, service=None, tracer=None):
+    def _get_conn_ctx(self, service=None, tracer=None):
         Pin(service, tracer=tracer or self.tracer).onto(asyncpg)
-        conn = self._conn = await asyncpg.connect(**POSTGRES_CONFIG)
-        return conn, self.tracer
+
+        class _ConnCtx:
+            async def __aenter__(self):
+                self._conn = await asyncpg.connect(**POSTGRES_CONFIG)
+                return self._conn
+
+            async def __aexit__(self, exc_type, exc_val, exc_tb):
+                await self._conn.close()
+
+        return _ConnCtx()
 
     async def assert_conn_is_traced(self, tracer, db, service):
 
@@ -62,30 +67,30 @@ class TestPsycopgPatch(AsyncioTestCase):
         start = time.time()
         rows = await db.fetch(q, timeout=5)
         end = time.time()
-        eq_(rows, [("foobarblah",)])
+        assert rows == [("foobarblah",)]
         assert rows
         spans = writer.pop()
         assert spans
-        eq_(len(spans), 2)
+        assert len(spans) == 2
 
         # prepare span
         span = spans[0]
-        eq_(span.name, "postgres.prepare")
-        eq_(span.resource, q)
-        eq_(span.service, service)
-        eq_(span.error, 0)
-        eq_(span.span_type, "sql")
+        assert span.name == "postgres.prepare"
+        assert span.resource == q
+        assert span.service == service
+        assert span.error == 0
+        assert span.span_type == "sql"
         assert start <= span.start <= end
         assert span.duration <= end - start
 
         # execute span
         span = spans[1]
-        eq_(span.name, "postgres.bind_execute")
-        eq_(span.resource, q)
-        eq_(span.service, service)
-        eq_(span.error, 0)
-        eq_(span.span_type, "sql")
-        eq_(span.metrics["db.rowcount"], 1)
+        assert span.name == "postgres.bind_execute"
+        assert span.resource == q
+        assert span.service == service
+        assert span.error == 0
+        assert span.span_type == "sql"
+        assert span.metrics["db.rowcount"] == 1
         assert start <= span.start <= end
         assert span.duration <= end - start
 
@@ -100,17 +105,17 @@ class TestPsycopgPatch(AsyncioTestCase):
 
         spans = writer.pop()
         assert spans, spans
-        eq_(len(spans), 1)
+        assert len(spans) == 1
         span = spans[0]
-        eq_(span.name, "postgres.prepare")
-        eq_(span.resource, q)
-        eq_(span.service, service)
-        eq_(span.error, 1)
-        eq_(span.meta["out.host"], "localhost")
-        eq_(span.meta["out.port"], TEST_PORT)
-        eq_(span.span_type, "sql")
+        assert span.name == "postgres.prepare"
+        assert span.resource == q
+        assert span.service == service
+        assert span.error == 1
+        assert span.meta["out.host"] == "127.0.0.1"
+        assert span.metrics["out.port"] == int(TEST_PORT)
+        assert span.span_type == "sql"
 
-    @mark_sync
+    @mark_asyncio
     async def test_pool_dsn(self):
         Pin(None, tracer=self.tracer).onto(asyncpg)
         dsn = "postgresql://%(user)s:%(password)s@%(host)s:%(port)s/%(database)s" % POSTGRES_CONFIG
@@ -118,25 +123,24 @@ class TestPsycopgPatch(AsyncioTestCase):
             async with pool.acquire() as conn:
                 await conn.execute("select 1;")
 
-    @mark_sync
+    @mark_asyncio
     async def test_copy_from(self):
         # This test is here to ensure we don't break the params
-        Pin(None, tracer=self.tracer).onto(asyncpg)
-        conn, tracer = await self._get_conn_and_tracer()
+        async with self._get_conn_ctx() as conn:
 
-        async def consumer(input):
-            pass
+            async def consumer(input):
+                pass
 
-        await conn.execute("""CREATE TABLE IF NOT EXISTS mytable (a int);""")
+            await conn.execute("""CREATE TABLE IF NOT EXISTS mytable (a int);""")
 
-        try:
-            await conn.execute("""INSERT INTO mytable (a) VALUES (100), (200), (300);""")
+            try:
+                await conn.execute("""INSERT INTO mytable (a) VALUES (100), (200), (300);""")
 
-            result = await conn.copy_from_query("SELECT * FROM mytable WHERE a > $1", 10, output=consumer, format="csv")
-        finally:
-            await conn.execute("DROP TABLE IF EXISTS mytable")
+                await conn.copy_from_query("SELECT * FROM mytable WHERE a > $1", 10, output=consumer, format="csv")
+            finally:
+                await conn.execute("DROP TABLE IF EXISTS mytable")
 
-    @mark_sync
+    @mark_asyncio
     async def test_pool(self):
         Pin(None, tracer=self.tracer).onto(asyncpg)
 
@@ -146,47 +150,47 @@ class TestPsycopgPatch(AsyncioTestCase):
                     await conn.execute("select 1;")
 
             spans = self.tracer.writer.pop()
-            eq_(len(spans), 6)
+            assert len(spans) == 6
 
             if min_size == 0:
-                eq_(spans[0].name, "postgres.pool.acquire")
-                eq_(spans[1].name, "postgres.connect")
+                assert spans[0].name == "postgres.pool.acquire"
+                assert spans[1].name == "postgres.connect"
             else:
-                eq_(spans[0].name, "postgres.connect")
-                eq_(spans[1].name, "postgres.pool.acquire")
-            eq_(spans[2].name, "postgres.query")
-            eq_(spans[3].name, "postgres.query")
-            eq_(spans[4].name, "postgres.pool.release")
-            eq_(spans[5].name, "postgres.close")
+                assert spans[0].name == "postgres.connect"
+                assert spans[1].name == "postgres.pool.acquire"
+            assert spans[2].name == "postgres.query"
 
-    @mark_sync
+            # the ordering changes between python3.5 and later
+            assert {spans[3].name, spans[4].name, spans[5].name} == {
+                "postgres.pool.release",
+                "postgres.query",
+                "postgres.close",
+            }
+
+    @mark_asyncio
     async def test_disabled_execute(self):
         self.tracer.enabled = False
-        conn, tracer = await self._get_conn_and_tracer()
-        # these calls were crashing with a previous version of the code.
-        await conn.execute("select 'blah'")
-        await conn.execute("select 'blah'")
-        assert not tracer.writer.pop()
+        async with self._get_conn_ctx() as conn:
+            # these calls were crashing with a previous version of the code.
+            await conn.execute("select 'blah'")
+            await conn.execute("select 'blah'")
+            assert not self.tracer.writer.pop()
 
-    @mark_sync
+    @mark_asyncio
     async def test_connect_factory(self):
         tracer = get_dummy_tracer()
 
         services = ["db", "another"]
         for service in services:
-            conn, _ = await self._get_conn_and_tracer(service, tracer)
-            await self.assert_conn_is_traced(tracer, conn, service)
-            await conn.close()
+            async with self._get_conn_ctx(service, tracer) as conn:
+                await self.assert_conn_is_traced(tracer, conn, service)
 
         # ensure we have the service types
         service_meta = tracer.writer.pop_services()
-        expected = {
-            "db": {"app": "postgres", "app_type": "db"},
-            "another": {"app": "postgres", "app_type": "db"},
-        }
-        eq_(service_meta, expected)
+        expected = {}
+        assert service_meta == expected
 
-    @mark_sync
+    @mark_asyncio
     async def test_patch_unpatch(self):
         tracer = get_dummy_tracer()
         writer = tracer.writer
@@ -204,7 +208,7 @@ class TestPsycopgPatch(AsyncioTestCase):
 
         spans = writer.pop()
         assert spans, spans
-        eq_(len(spans), 3)
+        assert len(spans) == 3
 
         # Test unpatch
         unpatch()
@@ -224,4 +228,4 @@ class TestPsycopgPatch(AsyncioTestCase):
 
         spans = writer.pop()
         assert spans, spans
-        eq_(len(spans), 3)
+        assert len(spans) == 3
