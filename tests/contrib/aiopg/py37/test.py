a7320a2782017bef823f8d2372f50056d3ce09e6
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2020-04-17 12:17:01 -0700

    add support for aiopg1.0

--- tests/contrib/aiopg/py37/test.py
+++ tests/contrib/aiopg/py37/test.py
@@ -1,13 +1,10 @@
-# 3p
-import aiopg
-
 # project
 from ddtrace.contrib.aiopg.patch import patch, unpatch
-from ddtrace import Pin
 
 # testing
 from tests.contrib.config import POSTGRES_CONFIG
 from tests.contrib.asyncio.utils import AsyncioTestCase, mark_asyncio
+from ..test import ConnCtx, CursorCtx
 
 
 TEST_PORT = str(POSTGRES_CONFIG['port'])
@@ -19,34 +16,26 @@ class AiopgTestCase(AsyncioTestCase):
 
     def setUp(self):
         super().setUp()
-        self._conn = None
         patch()
 
     def tearDown(self):
         super().tearDown()
-        if self._conn and not self._conn.closed:
-            self._conn.close()
-
         unpatch()
 
-    async def _get_conn_and_tracer(self):
-        conn = self._conn = await aiopg.connect(**POSTGRES_CONFIG)
-        Pin.get_from(conn).clone(tracer=self.tracer).onto(conn)
-
-        return conn, self.tracer
+    def _get_conn(self):
+        return ConnCtx(self.tracer)
 
     @mark_asyncio
     async def test_async_generator(self):
-        conn, tracer = await self._get_conn_and_tracer()
-        cursor = await conn.cursor()
-        q = 'select \'foobarblah\''
-        await cursor.execute(q)
-        rows = []
-        async for row in cursor:
-            rows.append(row)
+        async with self._get_conn() as conn, CursorCtx(conn) as cursor:
+            q = 'select \'foobarblah\''
+            await cursor.execute(q)
+            rows = []
+            async for row in cursor:
+                rows.append(row)
 
         assert rows == [('foobarblah',)]
-        spans = tracer.writer.pop()
+        spans = self.tracer.writer.pop()
         assert len(spans) == 1
         span = spans[0]
         assert span.name == 'postgres.execute'
