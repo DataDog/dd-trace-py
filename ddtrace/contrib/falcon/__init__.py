fa2ccee73e289f0f0690f80196ad380ce8fecef0
Kyle Verhoog <kyle.verhoog@datadoghq.com>
2020-05-04 15:28:20 -0400

    update web integrations

--- ddtrace/contrib/falcon/__init__.py
+++ ddtrace/contrib/falcon/__init__.py
@@ -20,7 +20,7 @@ You can also use the autopatching functionality::
 To disable distributed tracing when using autopatching, set the
 ``DATADOG_FALCON_DISTRIBUTED_TRACING`` environment variable to ``False``.
 
-To enable generating APM events for Trace Search & Analytics, set the
+To enable analyzed spans for App Analytics, set the
 ``DD_FALCON_ANALYTICS_ENABLED`` environment variable to ``True``.
 
 **Supported span hooks**
