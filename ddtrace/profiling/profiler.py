a750c3c904f4de35802c736d20ef6b9c993e8e9d
Julien Danjou <julien@danjou.info>
2020-05-29 13:50:22 +0200

    feat(profiling): allow to pass service_name to the Profiler object (#1440)
    Co-authored-by: Kyle Verhoog <kyle@verhoog.ca>

--- ddtrace/profiling/profiler.py
+++ ddtrace/profiling/profiler.py
@@ -17,10 +17,10 @@ from ddtrace.profiling.exporter import http
 LOG = logging.getLogger(__name__)
 
 
-def _build_default_exporters():
+def _build_default_exporters(service):
     exporters = []
     if "DD_PROFILING_API_KEY" in os.environ or "DD_API_KEY" in os.environ:
-        exporters.append(http.PprofHTTPExporter())
+        exporters.append(http.PprofHTTPExporter(service=service))
 
     _OUTPUT_PPROF = os.environ.get("DD_PROFILING_OUTPUT_PPROF")
     if _OUTPUT_PPROF:
@@ -30,6 +30,13 @@ def _build_default_exporters():
         LOG.warning("No exporters are configured, no profile will be output")
 
     return exporters
+
+
+def _get_service_name():
+    for service_name_var in ("DD_SERVICE", "DD_SERVICE_NAME", "DATADOG_SERVICE_NAME"):
+        service_name = os.environ.get(service_name_var)
+        if service_name is not None:
+            return service_name
 
 
 def _build_default_collectors():
@@ -69,12 +76,16 @@ class Profiler(object):
 
     """
 
+    service = attr.ib(factory=_get_service_name)
     collectors = attr.ib(factory=_build_default_collectors)
-    exporters = attr.ib(factory=_build_default_exporters)
+    exporters = attr.ib(default=None)
     schedulers = attr.ib(init=False, factory=list)
     status = attr.ib(init=False, type=ProfilerStatus, default=ProfilerStatus.STOPPED)
 
     def __attrs_post_init__(self):
+        if self.exporters is None:
+            self.exporters = _build_default_exporters(self.service)
+
         if self.exporters:
             for rec in self.recorders:
                 self.schedulers.append(scheduler.Scheduler(recorder=rec, exporters=self.exporters))
