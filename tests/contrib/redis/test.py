4d8b58369e685cb4e60c939536337da325396bce
Kyle Verhoog <kyle.verhoog@datadoghq.com>
2020-06-26 16:06:33 -0400

    add snapshot agent

--- tests/contrib/redis/test.py
+++ tests/contrib/redis/test.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 import redis
 
+import ddtrace
 from ddtrace import Pin, compat
 from ddtrace.constants import ANALYTICS_SAMPLE_RATE_KEY
 from ddtrace.contrib.redis import get_traced_redis
 from ddtrace.contrib.redis.patch import patch, unpatch
 
 from tests.opentracer.utils import init_tracer
+from tests.util import snapshot
 from ..config import REDIS_CONFIG
 from ...test_tracer import get_dummy_tracer
 from ...base import BaseTracerTestCase
@@ -32,77 +34,32 @@ class TestRedisPatch(BaseTracerTestCase):
         super(TestRedisPatch, self).setUp()
         patch()
         r = redis.Redis(port=self.TEST_PORT)
-        r.flushall()
-        Pin.override(r, tracer=self.tracer)
         self.r = r
 
     def tearDown(self):
         unpatch()
         super(TestRedisPatch, self).tearDown()
+        self.r.flushall()
 
+    @snapshot()
     def test_long_command(self):
         self.r.mget(*range(1000))
 
-        spans = self.get_spans()
-        assert len(spans) == 1
-        span = spans[0]
-
-        assert_is_measured(span)
-        assert span.service == "redis"
-        assert span.name == "redis.command"
-        assert span.span_type == "redis"
-        assert span.error == 0
-        meta = {
-            "out.host": u"localhost",
-        }
-        metrics = {
-            "out.port": self.TEST_PORT,
-            "out.redis_db": 0,
-        }
-        for k, v in meta.items():
-            assert span.get_tag(k) == v
-        for k, v in metrics.items():
-            assert span.get_metric(k) == v
-
-        assert span.get_tag("redis.raw_command").startswith(u"MGET 0 1 2 3")
-        assert span.get_tag("redis.raw_command").endswith(u"...")
-
+    @snapshot()
     def test_basics(self):
         us = self.r.get("cheese")
-        assert us is None
-        spans = self.get_spans()
-        assert len(spans) == 1
-        span = spans[0]
-        assert_is_measured(span)
-        assert span.service == "redis"
-        assert span.name == "redis.command"
-        assert span.span_type == "redis"
-        assert span.error == 0
-        assert span.get_metric("out.redis_db") == 0
-        assert span.get_tag("out.host") == "localhost"
-        assert span.get_tag("redis.raw_command") == u"GET cheese"
-        assert span.get_metric("redis.args_length") == 2
-        assert span.resource == "GET cheese"
-        assert span.get_metric(ANALYTICS_SAMPLE_RATE_KEY) is None
+
 
     def test_analytics_without_rate(self):
         with self.override_config("redis", dict(analytics_enabled=True)):
             us = self.r.get("cheese")
-            assert us is None
-            spans = self.get_spans()
-            assert len(spans) == 1
-            span = spans[0]
-            assert span.get_metric(ANALYTICS_SAMPLE_RATE_KEY) == 1.0
 
+    @snapshot()
     def test_analytics_with_rate(self):
         with self.override_config("redis", dict(analytics_enabled=True, analytics_sample_rate=0.5)):
             us = self.r.get("cheese")
-            assert us is None
-            spans = self.get_spans()
-            assert len(spans) == 1
-            span = spans[0]
-            assert span.get_metric(ANALYTICS_SAMPLE_RATE_KEY) == 0.5
 
+    @snapshot()
     def test_pipeline_traced(self):
         with self.r.pipeline(transaction=False) as p:
             p.set("blah", 32)
@@ -110,40 +67,14 @@ class TestRedisPatch(BaseTracerTestCase):
             p.hgetall("xxx")
             p.execute()
 
-        spans = self.get_spans()
-        assert len(spans) == 1
-        span = spans[0]
-        assert_is_measured(span)
-        assert span.service == "redis"
-        assert span.name == "redis.command"
-        assert span.resource == u"SET blah 32\nRPUSH foo éé\nHGETALL xxx"
-        assert span.span_type == "redis"
-        assert span.error == 0
-        assert span.get_metric("out.redis_db") == 0
-        assert span.get_tag("out.host") == "localhost"
-        assert span.get_tag("redis.raw_command") == u"SET blah 32\nRPUSH foo éé\nHGETALL xxx"
-        assert span.get_metric("redis.pipeline_length") == 3
-        assert span.get_metric("redis.pipeline_length") == 3
-        assert span.get_metric(ANALYTICS_SAMPLE_RATE_KEY) is None
-
+    @snapshot()
     def test_pipeline_immediate(self):
         with self.r.pipeline() as p:
             p.set("a", 1)
             p.immediate_execute_command("SET", "a", 1)
             p.execute()
 
-        spans = self.get_spans()
-        assert len(spans) == 2
-        span = spans[0]
-        assert_is_measured(span)
-        assert span.service == "redis"
-        assert span.name == "redis.command"
-        assert span.resource == u"SET a 1"
-        assert span.span_type == "redis"
-        assert span.error == 0
-        assert span.get_metric("out.redis_db") == 0
-        assert span.get_tag("out.host") == "localhost"
-
+    @snapshot()
     def test_meta_override(self):
         r = self.r
         pin = Pin.get_from(r)
@@ -151,11 +82,6 @@ class TestRedisPatch(BaseTracerTestCase):
             pin.clone(tags={"cheese": "camembert"}).onto(r)
 
         r.get("cheese")
-        spans = self.get_spans()
-        assert len(spans) == 1
-        span = spans[0]
-        assert span.service == "redis"
-        assert "cheese" in span.meta and span.meta["cheese"] == "camembert"
 
     def test_patch_unpatch(self):
         tracer = get_dummy_tracer()
@@ -193,81 +119,46 @@ class TestRedisPatch(BaseTracerTestCase):
         assert spans, spans
         assert len(spans) == 1
 
+    @snapshot()
     def test_opentracing(self):
         """Ensure OpenTracing works with redis."""
-        ot_tracer = init_tracer("redis_svc", self.tracer)
+        ot_tracer = init_tracer("redis_svc", ddtrace.tracer)
 
         with ot_tracer.start_active_span("redis_get"):
             us = self.r.get("cheese")
             assert us is None
 
-        spans = self.get_spans()
-        assert len(spans) == 2
-        ot_span, dd_span = spans
-
-        # confirm the parenting
-        assert ot_span.parent_id is None
-        assert dd_span.parent_id == ot_span.span_id
-
-        assert ot_span.name == "redis_get"
-        assert ot_span.service == "redis_svc"
-
-        assert_is_measured(dd_span)
-        assert dd_span.service == "redis"
-        assert dd_span.name == "redis.command"
-        assert dd_span.span_type == "redis"
-        assert dd_span.error == 0
-        assert dd_span.get_metric("out.redis_db") == 0
-        assert dd_span.get_tag("out.host") == "localhost"
-        assert dd_span.get_tag("redis.raw_command") == u"GET cheese"
-        assert dd_span.get_metric("redis.args_length") == 2
-        assert dd_span.resource == "GET cheese"
-
+    @snapshot()
     @BaseTracerTestCase.run_in_subprocess(env_overrides=dict(DD_SERVICE="mysvc"))
     def test_user_specified_service(self):
         from ddtrace import config
 
-        assert config.service == "mysvc"
-
         self.r.get("cheese")
-        span = self.get_spans()[0]
-        assert span.service == "redis"
 
+    @snapshot()
     @BaseTracerTestCase.run_in_subprocess(env_overrides=dict(DD_REDIS_SERVICE="myredis"))
     def test_env_user_specified_redis_service(self):
         self.r.get("cheese")
-        span = self.get_spans()[0]
-        assert span.service == "myredis", span.service
 
         self.reset()
 
         # Global config
         with self.override_config("redis", dict(service="cfg-redis")):
-            from ddtrace import config
-
-            print(config.redis.service)
             self.r.get("cheese")
-            span = self.get_spans()[0]
-            assert span.service == "cfg-redis", span.service
 
         self.reset()
 
         # Manual override
         Pin.override(self.r, service="mysvc", tracer=self.tracer)
         self.r.get("cheese")
-        span = self.get_spans()[0]
-        assert span.service == "mysvc", span.service
 
+    @snapshot()
     @BaseTracerTestCase.run_in_subprocess(env_overrides=dict(DD_SERVICE="app-svc", DD_REDIS_SERVICE="env-redis"))
     def test_service_precedence(self):
         self.r.get("cheese")
-        span = self.get_spans()[0]
-        assert span.service == "env-redis", span.service
 
         self.reset()
 
         # Do a manual override
         Pin.override(self.r, service="override-redis", tracer=self.tracer)
         self.r.get("cheese")
-        span = self.get_spans()[0]
-        assert span.service == "override-redis", span.service
--- tests/util.py
+++ tests/util.py
@@ -1,5 +1,9 @@
-import ddtrace
 from contextlib import contextmanager
+import inspect
+import pytest
+
+import ddtrace
+from ddtrace.compat import httplib
 
 
 def assert_dict_issuperset(a, b):
@@ -18,3 +22,43 @@ def override_global_tracer(tracer):
     ddtrace.tracer = tracer
     yield
     ddtrace.tracer = original_tracer
+
+
+def snapshot(ignores=None, file=None, dir=None, tracer=ddtrace.tracer):
+    ignores = ignores or []
+
+    def dec(f):
+
+        def wrapper(*args, **kwargs):
+            if len(args) > 1:
+                self = args[0]
+                clsname = self.__class__.__name__
+            else:
+                clsname = ""
+
+            module = inspect.getmodule(f)
+            token = "{}{}{}.{}".format(module.__name__, "." if clsname else "", clsname, f.__name__)
+
+            try:
+                conn = httplib.HTTPConnection(tracer.writer.api.hostname, tracer.writer.api.port)
+                conn.request("GET", "/test/start?token=%s" % token)
+                r = conn.getresponse()
+                if r.status != 200:
+                    raise ValueError("", r.read().decode())
+
+                ret = f(*args, **kwargs)
+                tracer.writer.flush_queue()
+
+                ignoresqs = ",".join(ignores)
+                conn = httplib.HTTPConnection(tracer.writer.api.hostname, tracer.writer.api.port)
+                conn.request("GET", "/test/snapshot?ignores=%s&token=%s" % (ignoresqs, token))
+                r = conn.getresponse()
+                if r.status != 200:
+                    raise ValueError("", r.read().decode())
+                return ret
+            except ValueError as e:
+                pytest.fail(e.args[1], pytrace=False)
+            finally:
+                conn.close()
+        return wrapper
+    return dec
