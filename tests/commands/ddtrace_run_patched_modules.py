6f5cb65f71489e304347e7c41c37a9fa605a0852
Julien Danjou <julien@danjou.info>
2020-07-02 03:13:20 +0200

    feat(ddtrace-run): allow to enable profiling (#1537)
    * feat(ddtrace-run): allow to enable profiling
    
    This adds an environment variable DD_PROFILING_ENABLED (default to false). If
    set to `true`, this will make the profiler run in the wrapped program.
    
    * chore: black tests/commands
    
    Co-authored-by: Kyle Verhoog <kyle@verhoog.ca>

--- tests/commands/ddtrace_run_patched_modules.py
+++ tests/commands/ddtrace_run_patched_modules.py
@@ -1,5 +1,5 @@
 from ddtrace import monkey
 
-if __name__ == '__main__':
-    assert 'redis' in monkey.get_patched_modules()
-    print('Test success')
+if __name__ == "__main__":
+    assert "redis" in monkey.get_patched_modules()
+    print("Test success")
