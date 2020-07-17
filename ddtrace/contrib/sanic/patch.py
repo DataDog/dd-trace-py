29e00809128d96dc75b478bbf91b890b3e958a0e
Tahir H. Butt <tahir.butt@datadoghq.com>
2020-07-16 21:21:43 -0400

    fix patching of handle_request

--- ddtrace/contrib/sanic/patch.py
+++ ddtrace/contrib/sanic/patch.py
@@ -1,29 +1,46 @@
-from sanic import Sanic
-
-from ddtrace import config
-from ddtrace import tracer as global_tracer
+from ddtrace import Pin, config
+from ddtrace import tracer
+from ddtrace.ext import SpanTypes, http
+from ddtrace.propagation.http import HTTPPropagator
 from ddtrace.vendor.wrapt import wrap_function_wrapper as _w
+from ddtrace.utils.wrappers import unwrap as _u, iswrapped
+from ddtrace.utils.importlib import func_name
 
-from ...utils.formats import asbool, get_env
+import sanic
+
 from ...internal.logger import get_logger
-
+from ...utils.formats import asbool, get_env
 
 log = get_logger(__name__)
+
+config._add("sanic", dict(service=config._get_service(default="sanic"), distributed_tracing=True))
+
 
 def patch():
     """Patch the instrumented methods.
     """
-    if getattr(Sanic, '__datadog_patch', 'False'):
+    if getattr(sanic, "__datadog_patch", False):
         return
-    setattr(Sanic, '__datadog_patch', True)
-    _w('sanic', 'Sanic.handle_request', patch_handle_request)
+    setattr(sanic, "__datadog_patch", True)
+    _w("sanic", "Sanic.handle_request", patch_handle_request)
+    _w("sanic", "Sanic.register_middleware", patch_register_middleware)
+
+
+def unpatch():
+    """Unpatch the instrumented methods.
+    """
+    _u(sanic.Sanic, "handle_request")
 
 
 def patch_handle_request(wrapped, instance, args, kwargs):
     """Wrapper for Sanic.handle_request"""
-    def _wrap(request, write_callback, stream_callback):
-        # import pdb; pdb.set_trace()
-        log.debug("I have intercepted!")
-        return wrapped(request, write_callback, stream_callback, **kwargs)
+    request = args[0]
+    resource = "{} {}".format(request.method, request.path)
 
-    return _wrap(**args, **kwargs)
+    with tracer.trace("sanic.request", service=config.sanic.service, resource=resource, span_type=SpanTypes.WEB):
+        return wrapped(*args, **kwargs)
+
+
+def patch_register_middleware(wrapped, instance, args, kwargs):
+    # TODO: wrap middleware
+    wrapped(*args, **kwargs)
