f3eb85959086f4d7c12999edd976fd4560070bac
sadipgiri <sadipgiri@bennington.edu>
2020-07-17 16:33:49 -0400

    add sanic span tags

--- ddtrace/contrib/sanic/patch.py
+++ ddtrace/contrib/sanic/patch.py
@@ -1,6 +1,8 @@
 from ddtrace import Pin, config
 from ddtrace import tracer
+from ddtrace.constants import ANALYTICS_SAMPLE_RATE_KEY
 from ddtrace.ext import SpanTypes, http
+from ddtrace.http import store_request_headers, store_response_headers
 from ddtrace.propagation.http import HTTPPropagator
 from ddtrace.vendor.wrapt import wrap_function_wrapper as _w
 from ddtrace.utils.wrappers import unwrap as _u, iswrapped
@@ -14,6 +16,35 @@ from ...utils.formats import asbool, get_env
 log = get_logger(__name__)
 
 config._add("sanic", dict(service=config._get_service(default="sanic"), distributed_tracing=True))
+
+
+def _extract_tags_from_request(request):
+    tags = {}
+
+    http_method = request.method
+    if http_method:
+        tags[http.METHOD] = http_method
+    
+    http_url = request.url
+    if http_url:
+        tags[http.URL] = http_url
+
+    http_version = request.version
+    if http_version:
+        tags[http.VERSION] = http_version
+
+    # query_string = None
+    # if config.sanic.trace_query_string:
+    #     query_string = request.query_string
+    #     if isinstance(query_string, bytes):
+    #         query_string = query_string.decode()
+    #     tags[http.QUERY_STRING] = query_string
+
+    query_string = request.query_string
+    if query_string:
+        tags[http.QUERY_STRING] = query_string
+
+    return tags
 
 
 def patch():
@@ -30,17 +61,38 @@ def unpatch():
     """Unpatch the instrumented methods.
     """
     _u(sanic.Sanic, "handle_request")
+    -u(sanic.Sanic, "register_middleware")
 
 
 def patch_handle_request(wrapped, instance, args, kwargs):
     """Wrapper for Sanic.handle_request"""
+
     request = args[0]
     resource = "{} {}".format(request.method, request.path)
 
-    with tracer.trace("sanic.request", service=config.sanic.service, resource=resource, span_type=SpanTypes.WEB):
+    headers = {k: v for (k, v) in request.headers.items()}
+
+    if config.sanic.distributed_tracing:
+        propagator = HTTPPropagator()
+        context = propagator.extract(headers)
+        if context.trace_id:
+            tracer.context_provider.activate(context)
+
+    with tracer.trace("sanic.request", service=config.sanic.service, resource=resource, span_type=SpanTypes.WEB) as span:
+        sample_rate = config.sanic.get_analytics_sample_rate(use_global_config=True)
+        if sample_rate is not None:
+            span.set_tag(ANALYTICS_SAMPLE_RATE_KEY, sample_rate)
+
+        tags = _extract_tags_from_request(request=request)
+        span.set_tags(tags)
+
+        store_request_headers(headers, span, config.sanic)
+
         return wrapped(*args, **kwargs)
 
 
 def patch_register_middleware(wrapped, instance, args, kwargs):
+    """Wrrapper for Sanic.register_middleware"""
+
     # TODO: wrap middleware
     wrapped(*args, **kwargs)
