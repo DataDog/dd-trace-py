57d805003854eff4635ffee301b899c26f2d6cdc
Tahir H. Butt <tahir.butt@datadoghq.com>
2020-08-06 12:24:20 -0400

    add tests for service override

--- ddtrace/contrib/sanic/patch.py
+++ ddtrace/contrib/sanic/patch.py
@@ -1,7 +1,6 @@
 import asyncio
 import ddtrace
 import sanic
-from ddtrace import config
 from ddtrace.constants import ANALYTICS_SAMPLE_RATE_KEY
 from ddtrace.ext import SpanTypes, http
 from ddtrace.http import store_request_headers, store_response_headers
@@ -14,8 +13,6 @@ from ...internal.logger import get_logger
 
 log = get_logger(__name__)
 
-config._add("sanic", dict(service=config._get_service(default="sanic"), distributed_tracing=True))
-
 
 def _extract_tags_from_request(request):
     tags = {}
@@ -25,7 +22,7 @@ def _extract_tags_from_request(request):
     tags[http.URL] = url
 
     query_string = None
-    if config.sanic.trace_query_string:
+    if ddtrace.config.sanic.trace_query_string:
         query_string = request.query_string
         if isinstance(query_string, bytes):
             query_string = query_string.decode()
@@ -42,7 +39,7 @@ def _wrap_response_callback(span, callback):
         span.set_tag(http.STATUS_CODE, response.status)
         if 500 <= response.status < 600:
             span.error = 1
-        store_response_headers(response.headers, span, config.sanic)
+        store_response_headers(response.headers, span, ddtrace.config.sanic)
 
     @wrapt.function_wrapper
     def wrap_sync(wrapped, instance, args, kwargs):
@@ -70,6 +67,7 @@ def patch():
     if getattr(sanic, "__datadog_patch", False):
         return
     setattr(sanic, "__datadog_patch", True)
+    ddtrace.config._add("sanic", dict(service=ddtrace.config._get_service(default="sanic"), distributed_tracing=True))
     _w("sanic", "Sanic.handle_request", patch_handle_request)
 
 
@@ -92,23 +90,23 @@ def patch_handle_request(wrapped, instance, args, kwargs):
 
     headers = request.headers.copy()
 
-    if config.sanic.distributed_tracing:
+    if ddtrace.config.sanic.distributed_tracing:
         propagator = HTTPPropagator()
         context = propagator.extract(headers)
         if context.trace_id:
             ddtrace.tracer.context_provider.activate(context)
 
     with ddtrace.tracer.trace(
-        "sanic.request", service=config.sanic.service, resource=resource, span_type=SpanTypes.WEB
+        "sanic.request", service=ddtrace.config.sanic.service, resource=resource, span_type=SpanTypes.WEB
     ) as span:
-        sample_rate = config.sanic.get_analytics_sample_rate(use_global_config=True)
+        sample_rate = ddtrace.config.sanic.get_analytics_sample_rate(use_global_config=True)
         if sample_rate is not None:
             span.set_tag(ANALYTICS_SAMPLE_RATE_KEY, sample_rate)
 
         tags = _extract_tags_from_request(request=request)
         span.set_tags(tags)
 
-        store_request_headers(headers, span, config.sanic)
+        store_request_headers(headers, span, ddtrace.config.sanic)
 
         if write_callback is not None:
             write_callback = _wrap_response_callback(span, write_callback)
