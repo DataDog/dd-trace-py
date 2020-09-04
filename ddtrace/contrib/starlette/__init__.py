51a518ed77a71d23f7f8769248ddf0daa5801b1e
Tahir H. Butt <tahir.butt@datadoghq.com>
2020-09-04 17:12:50 -0400

    full example for py36

--- ddtrace/contrib/starlette/__init__.py
+++ ddtrace/contrib/starlette/__init__.py
@@ -11,9 +11,15 @@ To configure tracing manually::
 If using Python 3.6, the legacy ``AsyncioContextProvider`` will have to be
 enabled before using the middleware::
 
+    from ddtrace.contrib.asgi import TraceMiddleware
     from ddtrace.contrib.asyncio.provider import AsyncioContextProvider
     from ddtrace import tracer  # Or whichever tracer instance you plan to use
+
+
     tracer.configure(context_provider=AsyncioContextProvider())
+
+    app = Starlette()
+    app.add_middleware(TraceMiddleware, tracer=tracer)
 
 
 Configuration
