192d919ed1bf9634b5ce8970261e12e4fab4fdbc
isabella-pham <isabella.aivy.pham@gmail.com>
2020-09-10 17:00:05 -0400

    modified tests so that it is compatible with Falcon 2.0. All tests but 1 pass which is the Distributed tracing disabled test. I believe it is a problem with ghet get_app function which I'll look more into tomorrow.

--- tests/contrib/falcon/test_distributed_tracing.py
+++ tests/contrib/falcon/test_distributed_tracing.py
@@ -1,4 +1,5 @@
 from falcon import testing
+import falcon as falcon
 from tests.tracer.test_tracer import get_dummy_tracer
 
 from .app import get_app
@@ -14,14 +15,21 @@ class DistributedTracingTestCase(testing.TestCase):
         self._service = 'falcon'
         self.tracer = get_dummy_tracer()
         self.api = get_app(tracer=self.tracer)
+        self.version = falcon.__version__
+        if(self.version[0] != '1'):
+            self.client = testing.TestClient(self.api)
 
     def test_distributred_tracing(self):
         headers = {
             'x-datadog-trace-id': '100',
             'x-datadog-parent-id': '42',
         }
-        out = self.simulate_get('/200', headers=headers)
-        assert out.status_code == 200
+        if(self.version[0] == '1'):
+            out = self.simulate_get('/200', headers=headers)
+            assert out.status_code == 200
+        else:
+            out = self.client.simulate_get('/200', headers=headers)
+            assert out.status[:3] == "200"
         assert out.content.decode('utf-8') == 'Success'
 
         traces = self.tracer.writer.pop_traces()
@@ -39,8 +47,12 @@ class DistributedTracingTestCase(testing.TestCase):
             'x-datadog-trace-id': '100',
             'x-datadog-parent-id': '42',
         }
-        out = self.simulate_get('/200', headers=headers)
-        assert out.status_code == 200
+        if(self.version[0] == '1'):
+            out = self.simulate_get('/200', headers=headers)
+            assert out.status_code == 200
+        else:
+            out = self.client.simulate_get('/200', headers=headers)
+            assert out.status[:3] == "200"
         assert out.content.decode('utf-8') == 'Success'
 
         traces = self.tracer.writer.pop_traces()
