2d70e040f9d2fd0cb3b1afac75b74dfd0df7bda4
Kyle Verhoog <kyle.verhoog@datadoghq.com>
2020-09-21 11:38:45 -0400

    remove ThreadLocalContext

--- ddtrace/internal/context_manager.py
+++ ddtrace/internal/context_manager.py
@@ -32,45 +32,6 @@ class BaseContextManager(six.with_metaclass(abc.ABCMeta)):
         pass
 
 
-@debtcollector.removals.removed_class("ThreadLocalContext")
-class ThreadLocalContext(BaseContextManager):
-    """
-    ThreadLocalContext can be used as a tracer global reference to create
-    a different ``Context`` for each thread. In synchronous tracer, this
-    is required to prevent multiple threads sharing the same ``Context``
-    in different executions.
-    """
-
-    def __init__(self, reset=True):
-        # always initialize a new thread-local context holder
-        super(ThreadLocalContext, self).__init__(reset=True)
-
-    def _has_active_context(self):
-        """
-        Determine whether we have a currently active context for this thread
-
-        :returns: Whether an active context exists
-        :rtype: bool
-        """
-        ctx = getattr(self._locals, "context", None)
-        return ctx is not None
-
-    def set(self, ctx):
-        setattr(self._locals, "context", ctx)
-
-    def get(self):
-        ctx = getattr(self._locals, "context", None)
-        if not ctx:
-            # create a new Context if it's not available
-            ctx = Context()
-            self._locals.context = ctx
-
-        return ctx
-
-    def reset(self):
-        self._locals = threading.local()
-
-
 class ContextVarContextManager(BaseContextManager):
     """
     _ContextVarContext can be used in place of the ThreadLocalContext for Python
