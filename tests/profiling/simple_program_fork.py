31433045fa9542cf9ae166d350445e6992e5e0ec
Julien Danjou <julien@danjou.info>
2020-09-22 14:37:34 +0200

    test: improve fork test speed
    This removes the memory testing with is not fork-dependent and make sure the
    program exits as fast as possible.

--- tests/profiling/simple_program_fork.py
+++ tests/profiling/simple_program_fork.py
@@ -5,23 +5,15 @@ import threading
 import ddtrace.profiling.auto
 import ddtrace.profiling.bootstrap
 import ddtrace.profiling.profiler
-from ddtrace.profiling.collector import memory
 from ddtrace.profiling.collector import stack
 from ddtrace.profiling.collector import threading as cthreading
 
 
-def _allocate_mem():
-    # Do some serious memory allocations!
-    for x in range(5000000):
-        object()
-
-
 lock = threading.Lock()
 lock.acquire()
-test_lock_name = "simple_program_fork.py:19"
+test_lock_name = "simple_program_fork.py:12"
 
 
-_ = _allocate_mem()
 assert ddtrace.profiling.bootstrap.profiler.status == ddtrace.profiling.profiler.ProfilerStatus.RUNNING
 
 parent_recorder = list(ddtrace.profiling.bootstrap.profiler.recorders)[0]
@@ -45,7 +37,7 @@ if child_pid == 0:
 
     # We track this one though
     lock = threading.Lock()
-    test_lock_name = "simple_program_fork.py:47"
+    test_lock_name = "simple_program_fork.py:39"
     assert test_lock_name not in set(e.lock_name for e in recorder.events[cthreading.LockAcquireEvent])
     lock.acquire()
     assert test_lock_name in set(e.lock_name for e in recorder.events[cthreading.LockAcquireEvent])
@@ -57,10 +49,9 @@ if child_pid == 0:
     assert test_lock_name not in set(e.lock_name for e in parent_recorder.events[cthreading.LockAcquireEvent])
     assert test_lock_name not in set(e.lock_name for e in parent_recorder.events[cthreading.LockReleaseEvent])
 
-    _ = _allocate_mem()
-    if sys.version_info[0] >= 3:
-        assert recorder.events[memory.MemorySampleEvent]
-    assert recorder.events[stack.StackSampleEvent]
+    # This can run forever if anything is broken!
+    while not recorder.events[stack.StackSampleEvent]:
+        pass
     assert recorder.events[cthreading.LockAcquireEvent]
 else:
     recorder = list(ddtrace.profiling.bootstrap.profiler.recorders)[0]
