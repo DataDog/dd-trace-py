3e9e7c001f3c15663912628141b3622e50f2a4ea
Kyle Verhoog <kyle@verhoog.ca>
2020-09-30 20:08:01 -0400

    fix(asgi): tag 500-level spans as errors (#1699)
    Description
    500-level responses from ASGI apps were not being tagged as erroneous.
    If an ASGI app handled an error and returned a 500-level response.
    
    Fix
    500-level responses are now tagged as errors. An entry is added to the
    integration checklist to prevent oversight in the future.

--- ddtrace/contrib/asgi/middleware.py
+++ ddtrace/contrib/asgi/middleware.py
@@ -119,6 +119,9 @@ class TraceMiddleware:
                 if "status" in message:
                     status_code = message["status"]
                     span.set_tag(http.STATUS_CODE, status_code)
+                    if 500 <= int(status_code) < 600:
+                        span.error = 1
+
                 if "headers" in message:
                     store_response_headers(message["headers"], span, config.asgi)
 
