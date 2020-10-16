d0587f5dec64f080d8930e12768215c905a5b4e0
Kyle Verhoog <kyle.verhoog@datadoghq.com>
2020-10-16 16:18:20 -0400

    update more tests

--- tests/tracer/test_writer.py
+++ tests/tracer/test_writer.py
@@ -81,9 +81,7 @@ class FailingAPI(object):
 class AgentWriterTests(BaseTestCase):
     N_TRACES = 11
 
-    def create_worker(
-        self, filters=None, api_class=DummyAPI, enable_stats=False, num_traces=N_TRACES, num_spans=MAX_NUM_SPANS
-    ):
+    def create_worker(self, api_class=DummyAPI, enable_stats=False, num_traces=N_TRACES, num_spans=MAX_NUM_SPANS):
         with self.override_global_config(dict(health_metrics_enabled=enable_stats)):
             self.dogstatsd = mock.Mock()
             worker = AgentWriter(dogstatsd=self.dogstatsd, filters=filters)
@@ -112,35 +110,6 @@ class AgentWriterTests(BaseTestCase):
         assert worker._send_stats is False
         with self.override_global_config(dict(health_metrics_enabled=True)):
             assert worker._send_stats is False
-
-    def test_filters_keep_all(self):
-        filtr = KeepAllFilter()
-        self.create_worker([filtr])
-        self.assertEqual(len(self.api.traces), self.N_TRACES)
-        self.assertEqual(filtr.filtered_traces, self.N_TRACES)
-
-    def test_filters_remove_all(self):
-        filtr = RemoveAllFilter()
-        self.create_worker([filtr])
-        self.assertEqual(len(self.api.traces), 0)
-        self.assertEqual(filtr.filtered_traces, self.N_TRACES)
-
-    def test_filters_add_tag(self):
-        tag_name = "Tag"
-        filtr = AddTagFilter(tag_name)
-        self.create_worker([filtr])
-        self.assertEqual(len(self.api.traces), self.N_TRACES)
-        self.assertEqual(filtr.filtered_traces, self.N_TRACES)
-        for trace in self.api.traces:
-            for span in trace:
-                self.assertIsNotNone(span.get_tag(tag_name))
-
-    def test_filters_short_circuit(self):
-        filtr = KeepAllFilter()
-        filters = [RemoveAllFilter(), filtr]
-        self.create_worker(filters)
-        self.assertEqual(len(self.api.traces), 0)
-        self.assertEqual(filtr.filtered_traces, 0)
 
     def test_no_dogstats(self):
         worker = self.create_worker()
@@ -256,23 +225,11 @@ class AgentWriterTests(BaseTestCase):
 class LogWriterTests(BaseTestCase):
     N_TRACES = 11
 
-    def create_writer(self, filters=None):
+    def create_writer(self):
         self.output = DummyOutput()
-        writer = LogWriter(out=self.output, filters=filters)
+        writer = LogWriter(out=self.output)
         for i in range(self.N_TRACES):
             writer.write(
                 [Span(tracer=None, name="name", trace_id=i, span_id=j, parent_id=j - 1 or None) for j in range(7)]
             )
         return writer
-
-    def test_filters_keep_all(self):
-        filtr = KeepAllFilter()
-        self.create_writer([filtr])
-        self.assertEqual(len(self.output.entries), self.N_TRACES)
-        self.assertEqual(filtr.filtered_traces, self.N_TRACES)
-
-    def test_filters_remove_all(self):
-        filtr = RemoveAllFilter()
-        self.create_writer([filtr])
-        self.assertEqual(len(self.output.entries), 0)
-        self.assertEqual(filtr.filtered_traces, self.N_TRACES)
