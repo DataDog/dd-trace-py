9d749095cb3b8430033e3056178f22ffad3f6ff6
Jiri Kuncar <jiri.kuncar@gmail.com>
2020-10-22 10:39:05 +0200

    add simple docstring

--- ddtrace/contrib/pytest/__init__.py
+++ ddtrace/contrib/pytest/__init__.py
@@ -1,3 +1,13 @@
+"""
+Enable traced execution of tests using ``pytest`` runner by
+running ``pytest --ddtrace`` or by modifying any configuration
+file read by Pytest (``pytest.ini``, ``setup.cfg``, ...)::
+
+    [pytest]
+    ddtrace = 1
+
+"""
+
 from ddtrace import config
 
 # pytest default settings
