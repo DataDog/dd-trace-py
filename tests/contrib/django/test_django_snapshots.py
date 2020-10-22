fcc582933761d28d9210e8483d46ceedd3af369f
Tahir H. Butt <tahir.butt@datadoghq.com>
2020-10-22 13:26:26 -0400

    separate tests for django18

--- tests/contrib/django/test_django_snapshots.py
+++ tests/contrib/django/test_django_snapshots.py
@@ -24,7 +24,14 @@ def test_urlpatterns_include(client):
     assert client.get("/include/test/").status_code == 200
 
 
-@pytest.mark.skipif(django.VERSION > (1, 12), reason="")
+@pytest.mark.skipif(django.VERSION >= (1, 9), reason="")
+@snapshot()
+def test_middleware_trace_callable_view_18x(client):
+    # ensures that the internals are properly traced when using callable views
+    assert client.get("/feed-view/").status_code == 200
+
+
+@pytest.mark.skipif(not (django.VERSION >= (1, 9) and django.VERSION < (1, 12)), reason="")
 @snapshot()
 def test_middleware_trace_callable_view_111x(client):
     # ensures that the internals are properly traced when using callable views
@@ -45,7 +52,14 @@ def test_middleware_trace_callable_view(client):
     assert client.get("/feed-view/").status_code == 200
 
 
-@pytest.mark.skipif(django.VERSION > (1, 12), reason="")
+@pytest.mark.skipif(django.VERSION >= (1, 9), reason="")
+@snapshot()
+def test_middleware_trace_partial_based_view_18x(client):
+    # ensures that the internals are properly traced when using a function views
+    assert client.get("/partial-view/").status_code == 200
+
+
+@pytest.mark.skipif(not (django.VERSION >= (1, 9) and django.VERSION < (1, 12)), reason="")
 @snapshot()
 def test_middleware_trace_partial_based_view_111x(client):
     # ensures that the internals are properly traced when using a function views
