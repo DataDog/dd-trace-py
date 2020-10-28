6ebdec54d842ba49cd79b58b638b93e50aecb07a
Tahir H. Butt <tahir.butt@datadoghq.com>
2020-10-27 22:42:46 -0400

    fix(core): fix encoding of loose primitive types (#1752)
    * fix(django): add test for encoding of SafeString
    
    * add subtype encoder tests
    
    * fix(core): don't use strict type checks for data
    
    * add release note
    
    * add snap
    
    * update snaps
    
    * remove comment
    
    * use snapshot variants
    
    Co-authored-by: Kyle Verhoog <kyle.verhoog@datadoghq.com>
    Co-authored-by: Brett Langdon <brett.langdon@datadoghq.com>
    Co-authored-by: Kyle Verhoog <kyle@verhoog.ca>

--- tests/contrib/django/test_django_snapshots.py
+++ tests/contrib/django/test_django_snapshots.py
@@ -38,3 +38,16 @@ def test_middleware_trace_callable_view(client):
 def test_middleware_trace_partial_based_view(client):
     # ensures that the internals are properly traced when using a function views
     assert client.get("/partial-view/").status_code == 200
+
+
+@pytest.mark.django_db
+@snapshot(
+    variants={
+        "18x": django.VERSION < (1, 9),
+        "111x": (1, 9) <= django.VERSION < (1, 12),
+        "21x": (1, 12) < django.VERSION < (2, 2),
+        "": django.VERSION >= (2, 2),
+    }
+)
+def test_safe_string_encoding(client):
+    assert client.get("/safe-template/").status_code == 200
