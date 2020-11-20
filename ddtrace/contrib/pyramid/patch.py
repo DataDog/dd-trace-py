005846f1ce83d5edcc821ae8717e44c6077f146c
Isabella Pham <isabella.aivy.pham@gmail.com>
2020-11-20 13:11:57 -0500

    feat: http helper function (#1795)
    Create an http helper function so that there is a standardized way of setting http request data in spans and to ensure that nothing is forgotten.
    
    Co-authored-by: Kyle Verhoog <kyle@verhoog.ca>

--- ddtrace/contrib/pyramid/patch.py
+++ ddtrace/contrib/pyramid/patch.py
@@ -2,8 +2,10 @@ import os
 
 from .trace import trace_pyramid, DD_TWEEN_NAME
 from .constants import (
-    SETTINGS_SERVICE, SETTINGS_DISTRIBUTED_TRACING,
-    SETTINGS_ANALYTICS_ENABLED, SETTINGS_ANALYTICS_SAMPLE_RATE,
+    SETTINGS_SERVICE,
+    SETTINGS_DISTRIBUTED_TRACING,
+    SETTINGS_ANALYTICS_ENABLED,
+    SETTINGS_ANALYTICS_SAMPLE_RATE,
 )
 from ...utils.formats import asbool, get_env
 
@@ -13,7 +15,7 @@ from pyramid.path import caller_package
 from ddtrace import config
 from ddtrace.vendor import wrapt
 
-DD_PATCH = '_datadog_patch'
+DD_PATCH = "_datadog_patch"
 
 
 def patch():
@@ -25,21 +27,21 @@ def patch():
 
     setattr(pyramid.config, DD_PATCH, True)
     _w = wrapt.wrap_function_wrapper
-    _w('pyramid.config', 'Configurator.__init__', traced_init)
+    _w("pyramid.config", "Configurator.__init__", traced_init)
 
 
 def traced_init(wrapped, instance, args, kwargs):
-    settings = kwargs.pop('settings', {})
+    settings = kwargs.pop("settings", {})
     service = config._get_service(default="pyramid")
-    distributed_tracing = asbool(get_env('pyramid', 'distributed_tracing', default=True))
+    distributed_tracing = asbool(get_env("pyramid", "distributed_tracing", default=True))
     # DEV: integration-specific analytics flag can be not set but still enabled
     # globally for web frameworks
-    old_analytics_enabled = get_env('pyramid', 'analytics_enabled')
+    old_analytics_enabled = get_env("pyramid", "analytics_enabled")
     analytics_enabled = os.environ.get("DD_TRACE_PYRAMID_ANALYTICS_ENABLED", old_analytics_enabled)
     if analytics_enabled is not None:
         analytics_enabled = asbool(analytics_enabled)
     # TODO: why is analytics sample rate a string or a bool here?
-    old_analytics_sample_rate = get_env('pyramid', 'analytics_sample_rate', default=True)
+    old_analytics_sample_rate = get_env("pyramid", "analytics_sample_rate", default=True)
     analytics_sample_rate = os.environ.get("DD_TRACE_PYRAMID_ANALYTICS_SAMPLE_RATE", old_analytics_sample_rate)
     trace_settings = {
         SETTINGS_SERVICE: service,
@@ -54,25 +56,25 @@ def traced_init(wrapped, instance, args, kwargs):
     # If the tweens are explicitly set with 'pyramid.tweens', we need to
     # explicitly set our tween too since `add_tween` will be ignored.
     insert_tween_if_needed(trace_settings)
-    kwargs['settings'] = trace_settings
+    kwargs["settings"] = trace_settings
 
     # `caller_package` works by walking a fixed amount of frames up the stack
     # to find the calling package. So if we let the original `__init__`
     # function call it, our wrapper will mess things up.
-    if not kwargs.get('package', None):
+    if not kwargs.get("package", None):
         # Get the packge for the third frame up from this one.
         #   - ddtrace.contrib.pyramid.path
         #   - ddtrace.vendor.wrapt
         #   - (this is the frame we want)
         # DEV: Default is `level=2` which will give us the package from `wrapt`
-        kwargs['package'] = caller_package(level=3)
+        kwargs["package"] = caller_package(level=3)
 
     wrapped(*args, **kwargs)
     trace_pyramid(instance)
 
 
 def insert_tween_if_needed(settings):
-    tweens = settings.get('pyramid.tweens')
+    tweens = settings.get("pyramid.tweens")
     # If the list is empty, pyramid does not consider the tweens have been
     # set explicitly.
     # And if our tween is already there, nothing to do
@@ -83,6 +85,6 @@ def insert_tween_if_needed(settings):
     # exceptions will be caught before they reach our tween.
     idx = tweens.find(pyramid.tweens.EXCVIEW)
     if idx == -1:
-        settings['pyramid.tweens'] = tweens + '\n' + DD_TWEEN_NAME
+        settings["pyramid.tweens"] = tweens + "\n" + DD_TWEEN_NAME
     else:
-        settings['pyramid.tweens'] = tweens[:idx] + DD_TWEEN_NAME + '\n' + tweens[idx:]
+        settings["pyramid.tweens"] = tweens[:idx] + DD_TWEEN_NAME + "\n" + tweens[idx:]
