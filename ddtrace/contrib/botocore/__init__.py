2b4acc229787ce2711a53168325a40ce27510d7c
Sergio Prada <chechopradaster@gmail.com>
2020-11-23 09:19:52 -0500

    fix format

--- ddtrace/contrib/botocore/__init__.py
+++ ddtrace/contrib/botocore/__init__.py
@@ -4,23 +4,32 @@ library. Libraries like Boto3 that use Botocore will also be patched.
 
 Enabling
 ~~~~~~~~
+
 The botocore integration is enabled automatically when using
 :ref:`ddtrace-run<ddtracerun>` or :ref:`patch_all()<patch_all>`.
+
 Or use :ref:`patch()<patch>` to manually enable the integration::
+
     from ddtrace import patch
     patch(botocore=True)
 
 Configuration
 ~~~~~~~~~~~~~
+
 .. py:data:: ddtrace.config.botocore['distributed_tracing']
+
    Whether to inject distributed tracing data to requests in SQS and Lambda.
+
    Default: ``True``
 
+
 Example::
+
     from ddtrace import config
+
     # Enable distributed tracing
     config.botocore['distributed_tracing'] = True
-    # Override service name
+
 """
 
 
