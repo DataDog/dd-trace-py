bc0c95c908a85d56245c599f618a695b374291b5
isabella-pham <isabella.aivy.pham@gmail.com>
2020-11-25 12:12:04 -0500

    reformatted alogliasearch and consul using black and removed from pyproject

--- tests/contrib/consul/test.py
+++ tests/contrib/consul/test.py
@@ -11,14 +11,14 @@ from ... import TracerTestCase, assert_is_measured
 
 class TestConsulPatch(TracerTestCase):
 
-    TEST_SERVICE = 'test-consul'
+    TEST_SERVICE = "test-consul"
 
     def setUp(self):
         super(TestConsulPatch, self).setUp()
         patch()
         c = consul.Consul(
-            host=CONSUL_CONFIG['host'],
-            port=CONSUL_CONFIG['port'],
+            host=CONSUL_CONFIG["host"],
+            port=CONSUL_CONFIG["port"],
         )
         Pin.override(consul.Consul, service=self.TEST_SERVICE, tracer=self.tracer)
         Pin.override(consul.Consul.KV, service=self.TEST_SERVICE, tracer=self.tracer)
@@ -29,8 +29,8 @@ class TestConsulPatch(TracerTestCase):
         super(TestConsulPatch, self).tearDown()
 
     def test_put(self):
-        key = 'test/put/consul'
-        value = 'test_value'
+        key = "test/put/consul"
+        value = "test_value"
 
         self.c.kv.put(key, value)
 
@@ -39,20 +39,20 @@ class TestConsulPatch(TracerTestCase):
         span = spans[0]
 
         assert_is_measured(span)
-        assert span.span_type == 'http'
+        assert span.span_type == "http"
         assert span.service == self.TEST_SERVICE
         assert span.name == consulx.CMD
-        assert span.resource == 'PUT'
+        assert span.resource == "PUT"
         assert span.error == 0
         tags = {
             consulx.KEY: key,
-            consulx.CMD: 'PUT',
+            consulx.CMD: "PUT",
         }
         for k, v in tags.items():
             assert span.get_tag(k) == v
 
     def test_get(self):
-        key = 'test/get/consul'
+        key = "test/get/consul"
 
         self.c.kv.get(key)
 
@@ -61,20 +61,20 @@ class TestConsulPatch(TracerTestCase):
         span = spans[0]
 
         assert_is_measured(span)
-        assert span.span_type == 'http'
+        assert span.span_type == "http"
         assert span.service == self.TEST_SERVICE
         assert span.name == consulx.CMD
-        assert span.resource == 'GET'
+        assert span.resource == "GET"
         assert span.error == 0
         tags = {
             consulx.KEY: key,
-            consulx.CMD: 'GET',
+            consulx.CMD: "GET",
         }
         for k, v in tags.items():
             assert span.get_tag(k) == v
 
     def test_delete(self):
-        key = 'test/delete/consul'
+        key = "test/delete/consul"
 
         self.c.kv.delete(key)
 
@@ -83,21 +83,21 @@ class TestConsulPatch(TracerTestCase):
         span = spans[0]
 
         assert_is_measured(span)
-        assert span.span_type == 'http'
+        assert span.span_type == "http"
         assert span.service == self.TEST_SERVICE
         assert span.name == consulx.CMD
-        assert span.resource == 'DELETE'
+        assert span.resource == "DELETE"
         assert span.error == 0
         tags = {
             consulx.KEY: key,
-            consulx.CMD: 'DELETE',
+            consulx.CMD: "DELETE",
         }
         for k, v in tags.items():
             assert span.get_tag(k) == v
 
     def test_kwargs(self):
-        key = 'test/kwargs/consul'
-        value = 'test_value'
+        key = "test/kwargs/consul"
+        value = "test_value"
 
         self.c.kv.put(key=key, value=value)
 
@@ -106,10 +106,10 @@ class TestConsulPatch(TracerTestCase):
         span = spans[0]
 
         assert_is_measured(span)
-        assert span.span_type == 'http'
+        assert span.span_type == "http"
         assert span.service == self.TEST_SERVICE
         assert span.name == consulx.CMD
-        assert span.resource == 'PUT'
+        assert span.resource == "PUT"
         assert span.error == 0
         tags = {
             consulx.KEY: key,
@@ -118,7 +118,7 @@ class TestConsulPatch(TracerTestCase):
             assert span.get_tag(k) == v
 
     def test_patch_idempotence(self):
-        key = 'test/patch/idempotence'
+        key = "test/patch/idempotence"
 
         patch()
         patch()
@@ -135,20 +135,20 @@ class TestConsulPatch(TracerTestCase):
         assert not isinstance(self.c.kv.get, BoundFunctionWrapper)
 
     def test_patch_preserves_functionality(self):
-        key = 'test/functionality'
-        value = b'test_value'
+        key = "test/functionality"
+        value = b"test_value"
 
         self.c.kv.put(key, value)
         _, data = self.c.kv.get(key)
-        assert data['Value'] == value
+        assert data["Value"] == value
         self.c.kv.delete(key)
         _, data = self.c.kv.get(key)
         assert data is None
 
     def test_analytics_without_rate(self):
-        with self.override_config('consul', {'analytics_enabled': True}):
-            key = 'test/kwargs/consul'
-            value = 'test_value'
+        with self.override_config("consul", {"analytics_enabled": True}):
+            key = "test/kwargs/consul"
+            value = "test_value"
 
             self.c.kv.put(key=key, value=value)
 
@@ -158,9 +158,9 @@ class TestConsulPatch(TracerTestCase):
             assert span.get_metric(ANALYTICS_SAMPLE_RATE_KEY) == 1.0
 
     def test_analytics_with_rate(self):
-        with self.override_config('consul', {'analytics_enabled': True, 'analytics_sample_rate': 0.5}):
-            key = 'test/kwargs/consul'
-            value = 'test_value'
+        with self.override_config("consul", {"analytics_enabled": True, "analytics_sample_rate": 0.5}):
+            key = "test/kwargs/consul"
+            value = "test_value"
 
             self.c.kv.put(key=key, value=value)
 
@@ -170,9 +170,9 @@ class TestConsulPatch(TracerTestCase):
             assert span.get_metric(ANALYTICS_SAMPLE_RATE_KEY) == 0.5
 
     def test_analytics_disabled(self):
-        with self.override_config('consul', {'analytics_enabled': False}):
-            key = 'test/kwargs/consul'
-            value = 'test_value'
+        with self.override_config("consul", {"analytics_enabled": False}):
+            key = "test/kwargs/consul"
+            value = "test_value"
 
             self.c.kv.put(key=key, value=value)
 
