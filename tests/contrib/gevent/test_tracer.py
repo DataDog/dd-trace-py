adcdda0058070e3ab66ead9965df0b94433b1ca4
Kyle Verhoog <kyle@verhoog.ca>
2021-02-02 06:39:43 -0500

    format gevent (#1975)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- tests/contrib/gevent/test_tracer.py
+++ tests/contrib/gevent/test_tracer.py
@@ -23,6 +23,7 @@ class TestGeventTracer(TracerTestCase):
     Ensures that greenlets are properly traced when using
     the default Tracer.
     """
+
     def setUp(self):
         # use a dummy tracer
         self.tracer = get_dummy_tracer()
@@ -42,14 +43,14 @@ class TestGeventTracer(TracerTestCase):
     def test_main_greenlet(self):
         # the main greenlet must not be affected by the tracer
         main_greenlet = gevent.getcurrent()
-        ctx = getattr(main_greenlet, '__datadog_context', None)
+        ctx = getattr(main_greenlet, "__datadog_context", None)
         assert ctx is None
 
     def test_main_greenlet_context(self):
         # the main greenlet must have a ``Context`` if called
         ctx_tracer = self.tracer.get_call_context()
         main_greenlet = gevent.getcurrent()
-        ctx_greenlet = getattr(main_greenlet, '__datadog_context', None)
+        ctx_greenlet = getattr(main_greenlet, "__datadog_context", None)
         assert ctx_tracer is ctx_greenlet
         assert len(ctx_tracer._trace) == 0
 
@@ -61,7 +62,7 @@ class TestGeventTracer(TracerTestCase):
         g = gevent.spawn(greenlet)
         g.join()
         ctx = g.value
-        stored_ctx = getattr(g, '__datadog_context', None)
+        stored_ctx = getattr(g, "__datadog_context", None)
         assert stored_ctx is not None
         assert ctx == stored_ctx
 
@@ -82,7 +83,7 @@ class TestGeventTracer(TracerTestCase):
 
         g = gevent.spawn(greenlet)
         g.join()
-        ctx = getattr(g, '__datadog_context', None)
+        ctx = getattr(g, "__datadog_context", None)
         assert ctx is None
 
     def test_spawn_greenlet(self):
@@ -92,7 +93,7 @@ class TestGeventTracer(TracerTestCase):
 
         g = gevent.spawn(greenlet)
         g.join()
-        ctx = getattr(g, '__datadog_context', None)
+        ctx = getattr(g, "__datadog_context", None)
         assert ctx is not None
         assert 0 == len(ctx._trace)
 
@@ -104,27 +105,27 @@ class TestGeventTracer(TracerTestCase):
 
         g = gevent.spawn_later(0.01, greenlet)
         g.join()
-        ctx = getattr(g, '__datadog_context', None)
+        ctx = getattr(g, "__datadog_context", None)
         assert ctx is not None
         assert 0 == len(ctx._trace)
 
     def test_trace_greenlet(self):
         # a greenlet can be traced using the trace API
         def greenlet():
-            with self.tracer.trace('greenlet') as span:
-                span.resource = 'base'
+            with self.tracer.trace("greenlet") as span:
+                span.resource = "base"
 
         gevent.spawn(greenlet).join()
         traces = self.tracer.writer.pop_traces()
         assert 1 == len(traces)
         assert 1 == len(traces[0])
-        assert 'greenlet' == traces[0][0].name
-        assert 'base' == traces[0][0].resource
+        assert "greenlet" == traces[0][0].name
+        assert "base" == traces[0][0].resource
 
     def test_trace_map_greenlet(self):
         # a greenlet can be traced using the trace API
         def greenlet(_):
-            with self.tracer.trace('greenlet', resource='base'):
+            with self.tracer.trace("greenlet", resource="base"):
                 gevent.sleep(0.01)
 
         funcs = [
@@ -136,7 +137,7 @@ class TestGeventTracer(TracerTestCase):
             gevent.pool.Pool(2).imap_unordered,
         ]
         for func in funcs:
-            with self.tracer.trace('outer', resource='base') as span:
+            with self.tracer.trace("outer", resource="base") as span:
                 # Use a list to force evaluation
                 list(func(greenlet, [0, 1, 2]))
             traces = self.tracer.writer.pop_traces()
@@ -148,48 +149,48 @@ class TestGeventTracer(TracerTestCase):
                 assert 1 == len(t)
                 span = t[0]
                 spans.append(span)
-                if span.name == 'outer':
+                if span.name == "outer":
                     outer_span = span
 
             assert outer_span is not None
-            assert 'base' == outer_span.resource
+            assert "base" == outer_span.resource
             inner_spans = [s for s in spans if s is not outer_span]
             for s in inner_spans:
-                assert 'greenlet' == s.name
-                assert 'base' == s.resource
+                assert "greenlet" == s.name
+                assert "base" == s.resource
                 assert outer_span.trace_id == s.trace_id
                 assert outer_span.span_id == s.parent_id
 
     def test_trace_later_greenlet(self):
         # a greenlet can be traced using the trace API
         def greenlet():
-            with self.tracer.trace('greenlet') as span:
-                span.resource = 'base'
+            with self.tracer.trace("greenlet") as span:
+                span.resource = "base"
 
         gevent.spawn_later(0.01, greenlet).join()
         traces = self.tracer.writer.pop_traces()
         assert 1 == len(traces)
         assert 1 == len(traces[0])
-        assert 'greenlet' == traces[0][0].name
-        assert 'base' == traces[0][0].resource
+        assert "greenlet" == traces[0][0].name
+        assert "base" == traces[0][0].resource
 
     def test_trace_sampling_priority_spawn_multiple_greenlets_multiple_traces(self):
         # multiple greenlets must be part of the same trace
         def entrypoint():
-            with self.tracer.trace('greenlet.main') as span:
+            with self.tracer.trace("greenlet.main") as span:
                 span.context.sampling_priority = USER_KEEP
-                span.resource = 'base'
+                span.resource = "base"
                 jobs = [gevent.spawn(green_1), gevent.spawn(green_2)]
                 gevent.joinall(jobs)
 
         def green_1():
-            with self.tracer.trace('greenlet.worker') as span:
-                span.set_tag('worker_id', '1')
+            with self.tracer.trace("greenlet.worker") as span:
+                span.set_tag("worker_id", "1")
                 gevent.sleep(0.01)
 
         def green_2():
-            with self.tracer.trace('greenlet.worker') as span:
-                span.set_tag('worker_id', '2')
+            with self.tracer.trace("greenlet.worker") as span:
+                span.set_tag("worker_id", "2")
                 gevent.sleep(0.01)
 
         gevent.spawn(entrypoint).join()
@@ -207,19 +208,19 @@ class TestGeventTracer(TracerTestCase):
     def test_trace_spawn_multiple_greenlets_multiple_traces(self):
         # multiple greenlets must be part of the same trace
         def entrypoint():
-            with self.tracer.trace('greenlet.main') as span:
-                span.resource = 'base'
+            with self.tracer.trace("greenlet.main") as span:
+                span.resource = "base"
                 jobs = [gevent.spawn(green_1), gevent.spawn(green_2)]
                 gevent.joinall(jobs)
 
         def green_1():
-            with self.tracer.trace('greenlet.worker') as span:
-                span.set_tag('worker_id', '1')
+            with self.tracer.trace("greenlet.worker") as span:
+                span.set_tag("worker_id", "1")
                 gevent.sleep(0.01)
 
         def green_2():
-            with self.tracer.trace('greenlet.worker') as span:
-                span.set_tag('worker_id', '2')
+            with self.tracer.trace("greenlet.worker") as span:
+                span.set_tag("worker_id", "2")
                 gevent.sleep(0.01)
 
         gevent.spawn(entrypoint).join()
@@ -230,33 +231,33 @@ class TestGeventTracer(TracerTestCase):
         worker_1 = traces[0][0]
         worker_2 = traces[1][0]
         # check spans data and hierarchy
-        assert parent_span.name == 'greenlet.main'
-        assert parent_span.resource == 'base'
-        assert worker_1.get_tag('worker_id') == '1'
-        assert worker_1.name == 'greenlet.worker'
-        assert worker_1.resource == 'greenlet.worker'
+        assert parent_span.name == "greenlet.main"
+        assert parent_span.resource == "base"
+        assert worker_1.get_tag("worker_id") == "1"
+        assert worker_1.name == "greenlet.worker"
+        assert worker_1.resource == "greenlet.worker"
         assert worker_1.parent_id == parent_span.span_id
-        assert worker_2.get_tag('worker_id') == '2'
-        assert worker_2.name == 'greenlet.worker'
-        assert worker_2.resource == 'greenlet.worker'
+        assert worker_2.get_tag("worker_id") == "2"
+        assert worker_2.name == "greenlet.worker"
+        assert worker_2.resource == "greenlet.worker"
         assert worker_2.parent_id == parent_span.span_id
 
     def test_trace_spawn_later_multiple_greenlets_multiple_traces(self):
         # multiple greenlets must be part of the same trace
         def entrypoint():
-            with self.tracer.trace('greenlet.main') as span:
-                span.resource = 'base'
+            with self.tracer.trace("greenlet.main") as span:
+                span.resource = "base"
                 jobs = [gevent.spawn_later(0.01, green_1), gevent.spawn_later(0.01, green_2)]
                 gevent.joinall(jobs)
 
         def green_1():
-            with self.tracer.trace('greenlet.worker') as span:
-                span.set_tag('worker_id', '1')
+            with self.tracer.trace("greenlet.worker") as span:
+                span.set_tag("worker_id", "1")
                 gevent.sleep(0.01)
 
         def green_2():
-            with self.tracer.trace('greenlet.worker') as span:
-                span.set_tag('worker_id', '2')
+            with self.tracer.trace("greenlet.worker") as span:
+                span.set_tag("worker_id", "2")
                 gevent.sleep(0.01)
 
         gevent.spawn(entrypoint).join()
@@ -267,22 +268,22 @@ class TestGeventTracer(TracerTestCase):
         worker_1 = traces[0][0]
         worker_2 = traces[1][0]
         # check spans data and hierarchy
-        assert parent_span.name == 'greenlet.main'
-        assert parent_span.resource == 'base'
-        assert worker_1.get_tag('worker_id') == '1'
-        assert worker_1.name == 'greenlet.worker'
-        assert worker_1.resource == 'greenlet.worker'
+        assert parent_span.name == "greenlet.main"
+        assert parent_span.resource == "base"
+        assert worker_1.get_tag("worker_id") == "1"
+        assert worker_1.name == "greenlet.worker"
+        assert worker_1.resource == "greenlet.worker"
         assert worker_1.parent_id == parent_span.span_id
-        assert worker_2.get_tag('worker_id') == '2'
-        assert worker_2.name == 'greenlet.worker'
-        assert worker_2.resource == 'greenlet.worker'
+        assert worker_2.get_tag("worker_id") == "2"
+        assert worker_2.name == "greenlet.worker"
+        assert worker_2.resource == "greenlet.worker"
         assert worker_2.parent_id == parent_span.span_id
 
     def test_trace_concurrent_calls(self):
         # create multiple futures so that we expect multiple
         # traces instead of a single one
         def greenlet():
-            with self.tracer.trace('greenlet'):
+            with self.tracer.trace("greenlet"):
                 gevent.sleep(0.01)
 
         jobs = [gevent.spawn(greenlet) for x in range(100)]
@@ -291,7 +292,7 @@ class TestGeventTracer(TracerTestCase):
         traces = self.tracer.writer.pop_traces()
         assert 100 == len(traces)
         assert 1 == len(traces[0])
-        assert 'greenlet' == traces[0][0].name
+        assert "greenlet" == traces[0][0].name
 
     def test_propagation_with_new_context(self):
         # create multiple futures so that we expect multiple
@@ -300,7 +301,7 @@ class TestGeventTracer(TracerTestCase):
         self.tracer.context_provider.activate(ctx)
 
         def greenlet():
-            with self.tracer.trace('greenlet'):
+            with self.tracer.trace("greenlet"):
                 gevent.sleep(0.01)
 
         jobs = [gevent.spawn(greenlet) for x in range(1)]
@@ -317,7 +318,7 @@ class TestGeventTracer(TracerTestCase):
         # traces instead of a single one, even if greenlets
         # are delayed
         def greenlet():
-            with self.tracer.trace('greenlet'):
+            with self.tracer.trace("greenlet"):
                 gevent.sleep(0.01)
 
         jobs = [gevent.spawn_later(0.01, greenlet) for x in range(100)]
@@ -326,14 +327,14 @@ class TestGeventTracer(TracerTestCase):
         traces = self.tracer.writer.pop_traces()
         assert 100 == len(traces)
         assert 1 == len(traces[0])
-        assert 'greenlet' == traces[0][0].name
+        assert "greenlet" == traces[0][0].name
 
     @silence_errors
     def test_exception(self):
         # it should catch the exception like usual
         def greenlet():
-            with self.tracer.trace('greenlet'):
-                raise Exception('Custom exception')
+            with self.tracer.trace("greenlet"):
+                raise Exception("Custom exception")
 
         g = gevent.spawn(greenlet)
         g.join()
@@ -344,8 +345,8 @@ class TestGeventTracer(TracerTestCase):
         assert 1 == len(traces[0])
         span = traces[0][0]
         assert 1 == span.error
-        assert 'Custom exception' == span.get_tag('error.msg')
-        assert 'Traceback (most recent call last)' in span.get_tag('error.stack')
+        assert "Custom exception" == span.get_tag("error.msg")
+        assert "Traceback (most recent call last)" in span.get_tag("error.stack")
 
     def _assert_spawn_multiple_greenlets(self, spans):
         """A helper to assert the parenting of a trace when greenlets are
@@ -365,11 +366,11 @@ class TestGeventTracer(TracerTestCase):
         worker_2 = None
         # get the spans since they can be in any order
         for span in spans:
-            if span.name == 'greenlet.main':
+            if span.name == "greenlet.main":
                 parent = span
-            if span.name == 'greenlet.worker1':
+            if span.name == "greenlet.worker1":
                 worker_1 = span
-            if span.name == 'greenlet.worker2':
+            if span.name == "greenlet.worker2":
                 worker_2 = span
         assert parent
         assert worker_1
@@ -380,32 +381,33 @@ class TestGeventTracer(TracerTestCase):
         assert worker_2.parent_id == parent.span_id
 
         # check spans data and hierarchy
-        assert parent.name == 'greenlet.main'
-        assert worker_1.get_tag('worker_id') == '1'
-        assert worker_1.name == 'greenlet.worker1'
-        assert worker_1.resource == 'greenlet.worker1'
-        assert worker_2.get_tag('worker_id') == '2'
-        assert worker_2.name == 'greenlet.worker2'
-        assert worker_2.resource == 'greenlet.worker2'
+        assert parent.name == "greenlet.main"
+        assert worker_1.get_tag("worker_id") == "1"
+        assert worker_1.name == "greenlet.worker1"
+        assert worker_1.resource == "greenlet.worker1"
+        assert worker_2.get_tag("worker_id") == "2"
+        assert worker_2.name == "greenlet.worker2"
+        assert worker_2.resource == "greenlet.worker2"
 
     def test_trace_spawn_multiple_greenlets_multiple_traces_dd(self):
         """Datadog version of the same test."""
+
         def entrypoint():
-            with self.tracer.trace('greenlet.main') as span:
-                span.resource = 'base'
+            with self.tracer.trace("greenlet.main") as span:
+                span.resource = "base"
                 jobs = [gevent.spawn(green_1), gevent.spawn(green_2)]
                 gevent.joinall(jobs)
 
         def green_1():
-            with self.tracer.trace('greenlet.worker1') as span:
-                span.set_tag('worker_id', '1')
+            with self.tracer.trace("greenlet.worker1") as span:
+                span.set_tag("worker_id", "1")
                 gevent.sleep(0.01)
 
         # note that replacing the `tracer.trace` call here with the
         # OpenTracing equivalent will cause the checks to fail
         def green_2():
-            with self.tracer.trace('greenlet.worker2') as span:
-                span.set_tag('worker_id', '2')
+            with self.tracer.trace("greenlet.worker2") as span:
+                span.set_tag("worker_id", "2")
                 gevent.sleep(0.01)
 
         gevent.spawn(entrypoint).join()
@@ -415,24 +417,24 @@ class TestGeventTracer(TracerTestCase):
     def test_trace_spawn_multiple_greenlets_multiple_traces_ot(self):
         """OpenTracing version of the same test."""
 
-        ot_tracer = init_tracer('my_svc', self.tracer, scope_manager=GeventScopeManager())
+        ot_tracer = init_tracer("my_svc", self.tracer, scope_manager=GeventScopeManager())
 
         def entrypoint():
-            with ot_tracer.start_active_span('greenlet.main') as span:
-                span.resource = 'base'
+            with ot_tracer.start_active_span("greenlet.main") as span:
+                span.resource = "base"
                 jobs = [gevent.spawn(green_1), gevent.spawn(green_2)]
                 gevent.joinall(jobs)
 
         def green_1():
-            with self.tracer.trace('greenlet.worker1') as span:
-                span.set_tag('worker_id', '1')
+            with self.tracer.trace("greenlet.worker1") as span:
+                span.set_tag("worker_id", "1")
                 gevent.sleep(0.01)
 
         # note that replacing the `tracer.trace` call here with the
         # OpenTracing equivalent will cause the checks to fail
         def green_2():
-            with ot_tracer.start_active_span('greenlet.worker2') as scope:
-                scope.span.set_tag('worker_id', '2')
+            with ot_tracer.start_active_span("greenlet.worker2") as scope:
+                scope.span.set_tag("worker_id", "2")
                 gevent.sleep(0.01)
 
         gevent.spawn(entrypoint).join()
