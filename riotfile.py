29cd2f618a8fc481638fe7f2789b91574680c9a3
Brett Langdon <brett.langdon@datadoghq.com>
2021-02-08 15:39:54 -0500

    ci: move grpc to riot (#1995)
    Co-authored-by: Julien Danjou <julien.danjou@datadoghq.com>

--- riotfile.py
+++ riotfile.py
@@ -325,5 +325,42 @@ venv = Venv(
                 ),
             ],
         ),
+        Venv(
+            name="grpc",
+            command="pytest {cmdargs} tests/contrib/grpc",
+            pkgs={
+                "googleapis-common-protos": latest,
+            },
+            venvs=[
+                Venv(
+                    pys=select_pys(max_version=3.6),
+                    pkgs={
+                        "grpcio": ["~=1.12.0", "~=1.14.0", "~=1.18.0", "~=1.20.0", "~=1.21.0", "~=1.22.0"],
+                    },
+                ),
+                Venv(
+                    pys=["3.7"],
+                    pkgs={
+                        "grpcio": [
+                            "~=1.14.0",
+                            "~=1.18.0",
+                            "~=1.20.0",
+                            "~=1.21.0",
+                            "~=1.22.0",
+                            "~=1.24.0",
+                            "~=1.26.0",
+                            "~=1.28.0",
+                            latest,
+                        ],
+                    },
+                ),
+                Venv(
+                    pys=select_pys(min_version=3.8),
+                    pkgs={
+                        "grpcio": ["~=1.24.0", "~=1.26.0", "~=1.28.0", latest],
+                    },
+                ),
+            ],
+        ),
     ],
 )
