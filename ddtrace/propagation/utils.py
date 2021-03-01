61c68196161da0ba790c909de561feadd9ea783e
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2021-03-01 18:05:02 +0000

    perf(propagation): Cache WSGI header conversion
    Cache the result of the conversion of a WSGI compliant HTTP header to
    the original one

--- ddtrace/propagation/utils.py
+++ ddtrace/propagation/utils.py
@@ -6,17 +6,27 @@ def get_wsgi_header(header):
     return "HTTP_{}".format(header.upper().replace("-", "_"))
 
 
+_cached_headers = {}
+
+
 def from_wsgi_header(header):
     """Convert a WSGI compliant HTTP header into the original header.
     See https://www.python.org/dev/peps/pep-3333/#environ-variables for
     information from the spec.
     """
-    HTTP_PREFIX = "HTTP_"
-    # PEP 333 gives two headers which aren't prepended with HTTP_.
-    UNPREFIXED_HEADERS = {"CONTENT_TYPE", "CONTENT_LENGTH"}
+    try:
+        return _cached_headers[header]
+    except KeyError:
+        HTTP_PREFIX = "HTTP_"
+        # PEP 333 gives two headers which aren't prepended with HTTP_.
+        UNPREFIXED_HEADERS = {"CONTENT_TYPE", "CONTENT_LENGTH"}
 
-    if header.startswith(HTTP_PREFIX):
-        header = header[len(HTTP_PREFIX):]
-    elif header not in UNPREFIXED_HEADERS:
-        return None
-    return header.replace("_", "-").title()
+        original_header = header
+        if header.startswith(HTTP_PREFIX):
+            header = header[len(HTTP_PREFIX):]
+        elif header not in UNPREFIXED_HEADERS:
+            _cached_headers[header] = None
+            return None
+        result = header.replace("_", "-").title()
+        _cached_headers[original_header] = result
+        return result
