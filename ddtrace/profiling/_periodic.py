f1dc0d691b9d9671b0a799d3a494ee4004d98362
Julien Danjou <julien@danjou.info>
2021-03-15 14:57:01 +0100

    refactor(profiling): add some typing on periodic

--- ddtrace/profiling/_periodic.py
+++ ddtrace/profiling/_periodic.py
@@ -2,6 +2,7 @@
 import sys
 import threading
 import time
+import typing
 
 from ddtrace.profiling import _nogevent
 from ddtrace.profiling import _service
@@ -18,7 +19,14 @@ class PeriodicThread(threading.Thread):
 
     _ddtrace_profiling_ignore = True
 
-    def __init__(self, interval, target, name=None, on_shutdown=None):
+    def __init__(
+        self,
+        interval,  # type: float
+        target,  # type: typing.Callable[[], typing.Any]
+        name=None,  # type: typing.Optional[str]
+        on_shutdown=None,  # type: typing.Callable[[], typing.Any]
+    ):
+        # type: (...) -> None
         """Create a periodic thread.
 
         :param interval: The interval in seconds to wait between execution of the periodic function.
