72a2a71e81c7d6d66debd68bdb3b53b8f68dd098
Julien Danjou <julien@danjou.info>
2021-03-24 14:53:06 +0100

    refactor(forksafe): simplify code
    This simplifies the code to use a banal decorator approach rather than a weird
    lambda-passed-as-argument-based 😉
    
    It also adds typing and an `unregister` function to allow for… unregistering a function!

--- tests/tracer/test_forksafe.py
+++ tests/tracer/test_forksafe.py
@@ -6,7 +6,10 @@ from ddtrace.internal import forksafe
 def test_forksafe():
     state = []
 
-    @forksafe.register(after_in_child=lambda: state.append(1))
+    @forksafe.register
+    def after_in_child():
+        state.append(1)
+
     def my_func():
         return state
 
@@ -27,15 +30,24 @@ def test_forksafe():
 def test_registry():
     state = []
 
-    @forksafe.register(after_in_child=lambda: state.append(1))
+    @forksafe.register
+    def after_in_child_1():
+        state.append(1)
+
     def f1():
         pass
 
-    @forksafe.register(after_in_child=lambda: state.append(2))
+    @forksafe.register
+    def after_in_child_2():
+        state.append(2)
+
     def f2():
         pass
 
-    @forksafe.register(after_in_child=lambda: state.append(3))
+    @forksafe.register
+    def after_in_child_3():
+        state.append(3)
+
     def f3():
         pass
 
@@ -46,18 +58,16 @@ def test_registry():
 def test_duplicates():
     state = []
 
+    @forksafe.register
     def hook():
         state.append(1)
 
-    @forksafe.register(after_in_child=hook)
     def f1():
         return state
 
-    @forksafe.register(after_in_child=hook)
     def f2():
         return state
 
-    @forksafe.register(after_in_child=hook)
     def f3():
         return state
 
@@ -79,7 +89,7 @@ def test_method_usage():
     class A:
         def __init__(self):
             self.state = 0
-            self.method = forksafe.register(after_in_child=self.after_fork)(self.method)
+            forksafe.register(self.after_fork)
 
         def after_fork(self):
             self.state = 1
@@ -104,18 +114,24 @@ def test_method_usage():
 def test_hook_exception():
     state = []
 
+    @forksafe.register
     def after_in_child():
         raise ValueError
 
-    @forksafe.register(after_in_child=lambda: state.append(1))
+    @forksafe.register
+    def state_append_1():
+        forksafe.register(state.append(1))
+
+    @forksafe.register
+    def state_append_3():
+        forksafe.register(state.append(3))
+
     def f1():
         return state
 
-    @forksafe.register(after_in_child=after_in_child)
     def f2():
         return state
 
-    @forksafe.register(after_in_child=lambda: state.append(3))
     def f3():
         return state
 
