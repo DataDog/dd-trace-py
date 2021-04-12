45185e5355769a2df76893f1f4573e2372a52c7c
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2021-04-12 13:34:20 +0100

    reuse caching code for methods

--- ddtrace/utils/cache.py
+++ ddtrace/utils/cache.py
@@ -58,47 +58,6 @@ def cached(maxsize=256):
     return cached_wrapper
 
 
-class CachedMethod(object):
-    def __init__(self, method, maxsize=256):
-        # type: (F, int) -> None
-        self._method = method
-        self._cache = {}  # type: Dict[Any, Tuple[Any, int]]
-        self._lock = RLock()
-        self._maxsize = maxsize
-        _CACHED_OBJECTS.append(self)
-
-    def invalidate(self):
-        # type: () -> None
-        self._cache.clear()
-
-    def __call__(self, key):
-        # type: (T) -> S
-        cache = self._cache
-        maxsize = self._maxsize
-        if len(self._cache) >= maxsize:
-            for _, h in zip(range(maxsize >> 1), sorted(cache, key=lambda h: cache[h][1])):
-                del cache[h]
-
-        _ = cache.get(key, miss)
-        if _ is not miss:
-            value, count = _
-            cache[key] = (value, count + 1)
-            return value
-
-        with self._lock:
-            _ = cache.get(key, miss)
-            if _ is not miss:
-                value, count = _
-                cache[key] = (value, count + 1)
-                return value
-
-            result = self._method(key)
-
-            cache[key] = (result, 1)
-
-            return result
-
-
 class CachedMethodDescriptor(object):
     def __init__(self, method, maxsize):
         # type: (M, int) -> None
@@ -106,8 +65,8 @@ class CachedMethodDescriptor(object):
         self._maxsize = maxsize
 
     def __get__(self, obj, objtype=None):
-        # type: (Any, Optional[Type]) -> CachedMethod
-        cached_method = CachedMethod(self._method.__get__(obj, objtype), self._maxsize)  # type: ignore[attr-defined]
+        # type: (Any, Optional[Type]) -> F
+        cached_method = cached(self._maxsize)(self._method.__get__(obj, objtype))  # type: ignore[attr-defined]
         setattr(obj, self._method.__name__, cached_method)
         return cached_method
 
