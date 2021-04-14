49875eae91ae7e6058d3824dd3514ba004ccefd5
brettlangdon <brett.langdon@datadoghq.com>
2021-04-14 14:53:03 -0400

    fix flake8 issue

--- ddtrace/contrib/grpc/client_interceptor.py
+++ ddtrace/contrib/grpc/client_interceptor.py
@@ -11,7 +11,8 @@ from ddtrace.vendor import wrapt
 
 from . import constants
 from .. import trace_utils
-from ...constants import ANALYTICS_SAMPLE_RATE_KEY, SPAN_MEASURED_KEY
+from ...constants import ANALYTICS_SAMPLE_RATE_KEY
+from ...constants import SPAN_MEASURED_KEY
 from ...internal.logger import get_logger
 from ...propagation.http import HTTPPropagator
 from .utils import parse_method_path
