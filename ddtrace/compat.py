46ca7263c305e7ec21eedc3b979c7a0484aa2d5f
Yun Kim <tykimx99@gmail.com>
2021-04-15 13:41:47 -0400

    Removed noqa comments, modified __all__ in compat

--- ddtrace/compat.py
+++ ddtrace/compat.py
@@ -1,51 +1,76 @@
 from ddtrace.utils.deprecation import deprecation
 
-from .internal.compat import CONTEXTVARS_IS_AVAILABLE  # noqa: F401
-from .internal.compat import NumericType  # noqa: F401
+from .internal.compat import CONTEXTVARS_IS_AVAILABLE
+from .internal.compat import NumericType
 from .internal.compat import PY2
-from .internal.compat import PY3  # noqa: F401
-from .internal.compat import PYTHON_INTERPRETER  # noqa: F401
-from .internal.compat import PYTHON_VERSION  # noqa: F401
-from .internal.compat import PYTHON_VERSION_INFO  # noqa: F401
+from .internal.compat import PY3
+from .internal.compat import PYTHON_INTERPRETER
+from .internal.compat import PYTHON_VERSION
+from .internal.compat import PYTHON_VERSION_INFO
 from .internal.compat import Queue
 from .internal.compat import StringIO
-from .internal.compat import binary_type  # noqa: F401
-from .internal.compat import contextvars  # noqa: F401
-from .internal.compat import ensure_text  # noqa: F401
-from .internal.compat import get_connection_response  # noqa: F401
-from .internal.compat import getrandbits  # noqa: F401
+from .internal.compat import binary_type
+from .internal.compat import contextvars
+from .internal.compat import ensure_text
+from .internal.compat import get_connection_response
+from .internal.compat import getrandbits
 from .internal.compat import httplib
-from .internal.compat import is_integer  # noqa: F401
-from .internal.compat import iscoroutinefunction  # noqa: F401
+from .internal.compat import is_integer
+from .internal.compat import iscoroutinefunction
 from .internal.compat import iteritems
-from .internal.compat import main_thread  # noqa: F401
-from .internal.compat import make_async_decorator  # noqa: F401
-from .internal.compat import monotonic  # noqa: F401
-from .internal.compat import monotonic_ns  # noqa: F401
-from .internal.compat import msgpack_type  # noqa: F401
-from .internal.compat import numeric_types  # noqa: F401
+from .internal.compat import main_thread
+from .internal.compat import make_async_decorator
+from .internal.compat import monotonic
+from .internal.compat import monotonic_ns
+from .internal.compat import msgpack_type
+from .internal.compat import numeric_types
 from .internal.compat import parse
-from .internal.compat import pattern_type  # noqa: F401
-from .internal.compat import process_time_ns  # noqa: F401
-from .internal.compat import reload_module  # noqa: F401
+from .internal.compat import pattern_type
+from .internal.compat import process_time_ns
+from .internal.compat import reload_module
 from .internal.compat import reraise
-from .internal.compat import string_type  # noqa: F401
+from .internal.compat import string_type
 from .internal.compat import stringify
-from .internal.compat import time_ns  # noqa: F401
-from .internal.compat import to_unicode  # noqa: F401
+from .internal.compat import time_ns
+from .internal.compat import to_unicode
 from .internal.compat import urlencode
 
 
 __all__ = [
-    "httplib",
-    "iteritems",
+    "CONTEXTVARS_IS_AVAILABLE",
+    "NumericType",
     "PY2",
+    "PY3",
+    "PYTHON_INTERPRETER",
+    "PYTHON_VERSION",
+    "PYTHON_VERSION_INFO",
     "Queue",
-    "stringify",
     "StringIO",
-    "urlencode",
+    "binary_type",
+    "contextvars",
+    "ensure_text",
+    "get_connection_response",
+    "getrandbits",
+    "httplib",
+    "is_integer",
+    "iscoroutinefunction",
+    "iteritems",
+    "main_thread",
+    "make_async_decorator",
+    "monotonic",
+    "monotonic_ns",
+    "msgpack_type",
+    "numeric_types",
     "parse",
+    "pattern_type",
+    "process_time_ns",
+    "reload_module",
     "reraise",
+    "string_type",
+    "stringify",
+    "time_ns",
+    "to_unicode",
+    "urlencode",
 ]
 
 deprecation(
