ea0c4472d67eb2a3dd3474fa67fc92c0e6d3f1f3
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2021-05-06 10:33:08 +0100

    fix(monkey): use correct integration name when poi
    The fix affects elasticsearch in particular. The wrong integration name
    was being used when patching-on-import a module that was already loaded.

--- ddtrace/monkey.py
+++ ddtrace/monkey.py
@@ -178,7 +178,8 @@ def patch(raise_errors=True, **patch_modules):
             for m in modules_to_poi:
                 # If the module has already been imported then patch immediately
                 if m in sys.modules:
-                    patch_module(m, raise_errors=raise_errors)
+                    patch_module(module, raise_errors=raise_errors)
+                    break
                 # Otherwise, add a hook to patch when it is imported for the first time
                 else:
                     # Use factory to create handler to close over `module` and `raise_errors` values from this loop
