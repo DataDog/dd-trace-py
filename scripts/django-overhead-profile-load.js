e70a5b056b18a71a3383ab55559407d361a26347
Tahir H. Butt <tahir.butt@datadoghq.com>
2021-05-25 09:13:48 -0400

    add k6 load script

---
+++ scripts/django-overhead-profile-load.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    duration: '2m',
+    vus: 50,
+};
+
+export default function () {
+    const res = http.get('http://127.0.0.1:8000/accounts/signup/');
+    sleep(1);
+}
