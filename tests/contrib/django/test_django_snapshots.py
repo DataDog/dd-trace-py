0113475dc32b2be024b1b293d4f4c41f35d5d938
Tahir H. Butt <tahir.butt@datadoghq.com>
2021-06-03 17:39:28 -0400

    fix(django): handle composed queries in postgres

--- tests/contrib/django/test_django_snapshots.py
+++ tests/contrib/django/test_django_snapshots.py
@@ -68,3 +68,23 @@ def test_safe_string_encoding(client):
 )
 def test_404_exceptions(client):
     assert client.get("/404-view/").status_code == 404
+
+
+@snapshot(ignores=["meta.out.host"])
+@pytest.mark.django_db
+def test_psycopg_query_default(client):
+    """Execute a psycopg2 query on a Django database wrapper"""
+    from django.db import connections
+    from psycopg2.sql import SQL
+
+    from ddtrace.contrib.psycopg.patch import Psycopg2TracedCursor
+
+    query = SQL("""select 'one' as x""")
+    conn = connections["postgres"]
+    with conn.cursor() as cur:
+        # ensure django db instrumentation patches cursor with psycopg tracer
+        assert isinstance(cur, Psycopg2TracedCursor)
+        cur.execute(query)
+        rows = cur.fetchall()
+        assert len(rows) == 1, rows
+        assert rows[0][0] == "one"
