30ac8288d58a31ddc247d6dbf50fc571d67a4c0f
Brett Langdon <brett.langdon@datadoghq.com>
2021-06-18 16:46:02 -0400

    fix(runtime metrics): Fix manually starting runtime metrics (#2564)
    * fix(runtime metrics): Fix manually starting runtime metrics
    
    * Add release note
    
    * fix release note
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    (cherry picked from commit 8e1de8338e58842bdf815e2b293a58e15aa33568)

--- tests/runtime/test_runtime_metrics_api.py
+++ tests/runtime/test_runtime_metrics_api.py
@@ -22,3 +22,24 @@ def test_runtime_metrics_api_idempotency():
     RuntimeMetrics.disable()
     assert RuntimeWorker._instance is None
     RuntimeMetrics.disable()
+
+
+def test_manually_start_runtime_metrics(run_python_code_in_subprocess):
+    """
+    When importing and manually starting runtime metrics
+        There are no errors
+    """
+    out, err, status, pid = run_python_code_in_subprocess(
+        """
+from ddtrace.runtime import RuntimeMetrics
+RuntimeMetrics.enable()
+
+from ddtrace.internal.runtime.runtime_metrics import RuntimeWorker
+assert RuntimeWorker._instance is not None
+
+RuntimeMetrics.disable()
+assert RuntimeWorker._instance is None
+""",
+    )
+
+    assert status == 0
