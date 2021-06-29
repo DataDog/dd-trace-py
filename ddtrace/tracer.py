74d5e361e59a378da8706ef5665dfb0b51fd3d75
Kyle Verhoog <kyle@verhoog.ca>
2021-06-29 02:20:16 -0400

    Handle possibly none context
    The span._context can possibly be None but only when not using the main
    tracer APIs. Add a safeguard in case the main APIs are not used.

--- ddtrace/tracer.py
+++ ddtrace/tracer.py
@@ -490,7 +490,7 @@ class Tracer(object):
             if isinstance(child_of, Context):
                 context = child_of
             else:
-                context = child_of._context
+                context = child_of._context or Context()
                 parent = child_of
         else:
             context = Context()
