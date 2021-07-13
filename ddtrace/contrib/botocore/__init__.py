4cbfa8646c153cfc949b33ad964b30b8ddd49a22
chris.agocs <chris.agocs@datadoghq.com>
2021-07-13 13:53:32 -0400

    Document the config variable

--- ddtrace/contrib/botocore/__init__.py
+++ ddtrace/contrib/botocore/__init__.py
@@ -24,6 +24,19 @@ Configuration
 
    Default: ``True``
 
+.. py:data:: ddtrace.config.botocore['clientcontext_custom_add_datadog_object']
+
+    When a tracing a directly invoked lambda function, the trace context is added to the context.clientContext.custom field.
+    The new method is to add the trace context directly to the custom field, i.e. ``custom{"datadog-trace-id"="12345"}``.
+    When set to True, an object named "_datadog" containing the trace context is added to the context.clientContext.custom field. 
+    This is to preserve legacy behavior, and is not compatible with directly invoking Go or Java Lambda functions.
+
+    Please note: setting this to True is required for propagating traces to Lambda functions instrumented with datadog-lambda-python < v41 or datadog-lambda-js < v3.57.0.
+
+    Can also be enabled with the ``DD_BOTOCORE_CLIENTCONTEXT_CUSTOM_ADD_DATADOG_OBJECT`` environment variable.
+
+    Defaut: ``False``
+
 
 Example::
 
