501f62f64706f6d92eff476497c9cb667a8fc911
Kyle Verhoog <kyle@verhoog.ca>
2021-07-15 14:53:30 -0400

    Typecheck context equality method (#2650)
    * Typecheck context equality method
    
    This allows the method to be used to compare context objects against
    different types without raising.
    
    * Add testcases

--- tests/tracer/test_context.py
+++ tests/tracer/test_context.py
@@ -1,6 +1,7 @@
 import pytest
 
 from ddtrace.context import Context
+from ddtrace.span import Span
 
 
 @pytest.mark.parametrize(
@@ -21,6 +22,12 @@ def test_eq(ctx1, ctx2):
 @pytest.mark.parametrize(
     "ctx1,ctx2",
     [
+        (Context(), Span(None, "")),
+        (Context(), None),
+        (Context(), object()),
+        (None, Context()),
+        (Context(), 5),
+        (5, Context()),
         (
             Context(trace_id=123, span_id=321, dd_origin="synthetics", sampling_priority=2),
             Context(trace_id=1234, span_id=321, dd_origin="synthetics", sampling_priority=2),
