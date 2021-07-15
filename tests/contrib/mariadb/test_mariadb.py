6ddcde4830f9679a4a88b7c82f47ae32595e3961
Zachary Groves <32471391+ZStriker19@users.noreply.github.com>
2021-07-15 12:49:36 -0600

    Update tests/contrib/mariadb/test_mariadb.py
    Co-authored-by: Kyle Verhoog <kyle@verhoog.ca>

--- tests/contrib/mariadb/test_mariadb.py
+++ tests/contrib/mariadb/test_mariadb.py
@@ -65,7 +65,6 @@ def test_simple_query(connection, tracer):
 
 
 def test_query_executemany(connection, tracer):
-    # tests that the executemany method is correctly wrapped.
     tracer.enabled = False
     cursor = connection.cursor()
 
