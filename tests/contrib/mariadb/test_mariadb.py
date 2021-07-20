3cd48936774e3e5376de322d9407cdc70801b6b5
ZStriker19 <zgroves19@gmail.com>
2021-07-20 09:48:24 -0600

    updating tests so connection closes on each

--- tests/contrib/mariadb/test_mariadb.py
+++ tests/contrib/mariadb/test_mariadb.py
@@ -171,45 +171,34 @@ tracer.shutdown()
 
 @snapshot(include_tracer=True)
 def test_simple_query_snapshot(tracer):
-    connection = get_connection(tracer)
-    cursor = connection.cursor()
-    cursor.execute("SELECT 1")
-    rows = cursor.fetchall()
-    assert len(rows) == 1
-
-
-@snapshot(include_tracer=True, ignores=["meta.error.stack"])
-def test_simple_malformed_query_snapshot(tracer):
-    connection = get_connection(tracer)
-    cursor = connection.cursor()
-    with pytest.raises(mariadb.ProgrammingError):
-        cursor.execute("SELEC 1")
-
-
-@snapshot(include_tracer=True)
-def test_simple_query_fetchall_snapshot(tracer):
-    with override_config("mariadb", dict(trace_fetch_methods=True)):
-        connection = get_connection(tracer)
+    with get_connection(tracer) as connection:
         cursor = connection.cursor()
         cursor.execute("SELECT 1")
         rows = cursor.fetchall()
         assert len(rows) == 1
 
 
+@snapshot(include_tracer=True, ignores=["meta.error.stack"])
+def test_simple_malformed_query_snapshot(tracer):
+    with get_connection(tracer) as connection:
+        cursor = connection.cursor()
+        with pytest.raises(mariadb.ProgrammingError):
+            cursor.execute("SELEC 1")
+
+
+@snapshot(include_tracer=True)
+def test_simple_query_fetchall_snapshot(tracer):
+    with override_config("mariadb", dict(trace_fetch_methods=True)):
+        with get_connection(tracer) as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT 1")
+            rows = cursor.fetchall()
+            assert len(rows) == 1
+
+
 @snapshot(include_tracer=True)
 def test_query_with_several_rows_snapshot(tracer):
-    connection = get_connection(tracer)
-    cursor = connection.cursor()
-    query = "SELECT n FROM (SELECT 42 n UNION SELECT 421 UNION SELECT 4210) m"
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    assert len(rows) == 3
-
-
-@snapshot(include_tracer=True)
-def test_query_with_several_rows_fetchall_snapshot(tracer):
-    with override_config("mariadb", dict(trace_fetch_methods=True)):
-        connection = get_connection(tracer)
+    with get_connection(tracer) as connection:
         cursor = connection.cursor()
         query = "SELECT n FROM (SELECT 42 n UNION SELECT 421 UNION SELECT 4210) m"
         cursor.execute(query)
@@ -218,76 +207,87 @@ def test_query_with_several_rows_fetchall_snapshot(tracer):
 
 
 @snapshot(include_tracer=True)
+def test_query_with_several_rows_fetchall_snapshot(tracer):
+    with override_config("mariadb", dict(trace_fetch_methods=True)):
+        with get_connection(tracer) as connection:
+            cursor = connection.cursor()
+            query = "SELECT n FROM (SELECT 42 n UNION SELECT 421 UNION SELECT 4210) m"
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            assert len(rows) == 3
+
+
+@snapshot(include_tracer=True)
 def test_query_many_fetchall_snapshot(tracer):
     with override_config("mariadb", dict(trace_fetch_methods=True)):
-        connection = get_connection(tracer)
+        with get_connection(tracer) as connection:
 
         # tests that the executemany method is correctly wrapped.
-        tracer.enabled = False
-        cursor = connection.cursor()
+            tracer.enabled = False
+            cursor = connection.cursor()
 
-        cursor.execute(
-            """
-            create table if not exists dummy (
-                dummy_key VARCHAR(32) PRIMARY KEY,
-                dummy_value TEXT NOT NULL)"""
-        )
-        tracer.enabled = True
+            cursor.execute(
+                """
+                create table if not exists dummy (
+                    dummy_key VARCHAR(32) PRIMARY KEY,
+                    dummy_value TEXT NOT NULL)"""
+            )
+            tracer.enabled = True
 
-        stmt = "INSERT INTO dummy (dummy_key, dummy_value) VALUES (%s, %s)"
-        data = [
-            ("foo", "this is foo"),
-            ("bar", "this is bar"),
-        ]
-        cursor.executemany(stmt, data)
-        query = "SELECT dummy_key, dummy_value FROM dummy ORDER BY dummy_key"
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        assert len(rows) == 2
+            stmt = "INSERT INTO dummy (dummy_key, dummy_value) VALUES (%s, %s)"
+            data = [
+                ("foo", "this is foo"),
+                ("bar", "this is bar"),
+            ]
+            cursor.executemany(stmt, data)
+            query = "SELECT dummy_key, dummy_value FROM dummy ORDER BY dummy_key"
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            assert len(rows) == 2
 
 
 @snapshot(include_tracer=True)
 def test_commit_snapshot(tracer):
-    connection = get_connection(tracer)
-    connection.commit()
+    with get_connection(tracer) as connection:
+        connection.commit()
 
 
 @snapshot(include_tracer=True)
 def test_query_proc_snapshot(tracer):
-    connection = get_connection(tracer)
-    # create a procedure
-    tracer.enabled = False
-    cursor = connection.cursor()
-    cursor.execute("DROP PROCEDURE IF EXISTS sp_sum")
-    cursor.execute(
-        """
-        CREATE PROCEDURE sp_sum (IN p1 INTEGER, IN p2 INTEGER, OUT p3 INTEGER)
-        BEGIN
-            SET p3 := p1 + p2;
-        END;"""
-    )
+   with get_connection(tracer) as connection:
+        # create a procedure
+        tracer.enabled = False
+        cursor = connection.cursor()
+        cursor.execute("DROP PROCEDURE IF EXISTS sp_sum")
+        cursor.execute(
+            """
+            CREATE PROCEDURE sp_sum (IN p1 INTEGER, IN p2 INTEGER, OUT p3 INTEGER)
+            BEGIN
+                SET p3 := p1 + p2;
+            END;"""
+        )
 
-    tracer.enabled = True
-    proc = "sp_sum"
-    data = (40, 2, None)
-    cursor.callproc(proc, data)
+        tracer.enabled = True
+        proc = "sp_sum"
+        data = (40, 2, None)
+        cursor.callproc(proc, data)
 
 
 @snapshot(include_tracer=True)
 def test_analytics_with_rate_snapshot(tracer):
     with override_config("mariadb", dict(analytics_enabled=True, analytics_sample_rate=0.5)):
-        connection = get_connection(tracer)
-        cursor = connection.cursor()
-        cursor.execute("SELECT 1")
-        rows = cursor.fetchall()
-        assert len(rows) == 1
+        with get_connection(tracer) as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT 1")
+            rows = cursor.fetchall()
+            assert len(rows) == 1
 
 
 @snapshot(include_tracer=True)
 def test_analytics_without_rate_snapshot(tracer):
     with override_config("mariadb", dict(analytics_enabled=True)):
-        connection = get_connection(tracer)
-        cursor = connection.cursor()
-        cursor.execute("SELECT 1")
-        rows = cursor.fetchall()
-        assert len(rows) == 1
+        with get_connection(tracer) as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT 1")
+            rows = cursor.fetchall()
+            assert len(rows) == 1
