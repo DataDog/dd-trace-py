22edf8d3529cb830fe1a16b1f3da9c9c26a25fe3
Kyle Verhoog <kyle.verhoog@datadoghq.com>
2021-07-28 17:55:31 -0400

    fix(asgi): handle invalid headers
    The ASGI specification on headers:
    
      headers: These are byte strings of the exact byte sequences sent by the client/to be sent by the server. While modern HTTP standards say that headers should be ASCII, older ones did not and allowed a wider range of characters. Frameworks/applications should decode headers as they deem appropriate.
    
    This patch adds error handling for when headers cannot be decoded.
    
    Fixes #2103

--- tests/contrib/asgi/test_asgi.py
+++ tests/contrib/asgi/test_asgi.py
@@ -336,3 +336,20 @@ async def test_multiple_requests(tracer, test_spans):
     assert r2_span.get_tag("http.method") == "GET"
     assert r2_span.get_tag("http.url") == "http://testserver/"
     assert r2_span.get_tag("http.query.string") == "sleep=true"
+
+
+@pytest.mark.asyncio
+async def test_bad_headers(tracer, test_spans):
+    class BadMiddleware(object):
+        def __init__(self, app):
+            self.app = app
+
+        async def __call__(self, scope, receive, send):
+            scope["headers"] += [(bytes.fromhex("c0"), "test")]
+            return await self.app(scope, receive, send)
+
+    app = BadMiddleware(TraceMiddleware(basic_app, tracer=tracer))
+    async with httpx.AsyncClient(app=app) as client:
+        response = await client.get("http://testserver/")
+
+    assert response.status_code == 200
