80b7b4b439cc6807cb91814f4e17acd4f4e8ceed
Tahir H. Butt <tahir.butt@datadoghq.com>
2021-08-06 08:55:56 -0400

    refactor(perf): scenario framework

---
+++ benchmarks/encoder/run.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+
+import yaml
+
+
+def read_variants(path):
+    with open(path, "r") as fp:
+        return yaml.load(fp)
+
+
+def run(name, spec, output):
+    cmd = ["python", "scenario.py", "--copy-env", "--variant", name, "--append", output]
+    for (arg, val) in spec.items():
+        cmd.append("--{}".format(arg))
+        cmd.append(str(val))
+    proc = subprocess.Popen(cmd)
+    proc.wait()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        sys.exit(1)
+
+    output = os.path.join(sys.argv[2], "results.json")
+    print("Saving results to {}".format(output))
+    variants = read_variants(sys.argv[1])
+    for (name, spec) in variants.items():
+        run(name, spec, output)
