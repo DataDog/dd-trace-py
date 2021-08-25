35b8ecd5a4f47187ca23deb801426dd96cc346b3
maxday <maxime.david@datadoghq.com>
2021-08-25 18:06:24 -0400

    move tests

---
+++ tests/tracer/test_version.py
@@ -0,0 +1,39 @@
+import sys
+from unittest import mock
+
+from ddtrace.version import get_version
+from tests.tracer import _version  # noqa: F401 -> we need to import it so that it can be swapped with the test module
+
+
+def test_get_version_from_version_file(self):
+    with mock.patch.dict(sys.modules, {"ddtrace._version": sys.modules["tests.tracer._version"]}):
+        assert get_version() == "my_test_version_from_generated_file"
+
+
+def test_get_version_from_pkg_resources(self):
+    with mock.patch.dict(sys.modules, {"ddtrace._version": None}):
+        with mock.patch("pkg_resources.get_distribution") as mock_get_distribution:
+            mock_get_distribution.return_value = FakeDistribution()
+            assert get_version() == "my_test_version_from_pkg_resources"
+            mock_get_distribution.assert_called_with("ddtrace.version")
+
+
+def test_get_version_dev_fallback(self):
+    with mock.patch.dict(sys.modules, {"ddtrace._version": None}):
+        with mock.patch.dict(sys.modules, {"pkg_resources": None}):
+            assert get_version() == "dev"
+
+
+class FakeDistributionIterator:
+    def __init__(self, distribution):
+        pass
+
+    def __next__(self):
+        raise StopIteration
+
+
+class FakeDistribution:
+    version = "my_test_version_from_pkg_resources"
+
+    def __iter__(self):
+        return FakeDistributionIterator(self)
