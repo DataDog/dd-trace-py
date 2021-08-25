346a887d267726fdcf28f146ee8bdcdc0a68ab85
maxday <maxime.david@datadoghq.com>
2021-08-25 13:27:39 -0400

    lint

--- ddtrace/version.py
+++ ddtrace/version.py
@@ -1,13 +1,14 @@
 def get_version():
     try:
         from ._version import version
+
         return version
     except Exception:
         try:
             # something went wrong while creating _version.py, let's fallback to pkg_resources
             import pkg_resources
+
             return pkg_resources.get_distribution(__name__).version
         except Exception:
             # package is not installed
             return "dev"
-
--- tests/version/test_version.py
+++ tests/version/test_version.py
@@ -4,31 +4,34 @@ from unittest import mock
 import sys
 
 
-class TestVersion():
+class TestVersion:
     def test_get_version_from_version_file(self):
-        with mock.patch.dict(sys.modules, {'ddtrace._version':sys.modules['tests.version._version']}):
-            assert get_version() == 'my_test_version_from_generated_file'
+        with mock.patch.dict(sys.modules, {"ddtrace._version": sys.modules["tests.version._version"]}):
+            assert get_version() == "my_test_version_from_generated_file"
 
     def test_get_version_from_pkg_resources(self):
-        with mock.patch.dict(sys.modules, {'ddtrace._version':None}):
+        with mock.patch.dict(sys.modules, {"ddtrace._version": None}):
             with mock.patch("pkg_resources.get_distribution") as mock_get_distribution:
                 mock_get_distribution.return_value = FakeDistribution()
-                assert get_version() == 'my_test_version_from_pkg_resources'
-                mock_get_distribution.assert_called_with('ddtrace.version')
-                
+                assert get_version() == "my_test_version_from_pkg_resources"
+                mock_get_distribution.assert_called_with("ddtrace.version")
+
     def test_get_version_dev_fallback(self):
-        with mock.patch.dict(sys.modules, {'ddtrace._version':None}):
-            with mock.patch.dict(sys.modules, {'pkg_resources':None}):
+        with mock.patch.dict(sys.modules, {"ddtrace._version": None}):
+            with mock.patch.dict(sys.modules, {"pkg_resources": None}):
                 assert get_version() == "dev"
 
+
 class FakeDistributionIterator:
-   def __init__(self, distribution):
-       pass
-   def __next__(self):
-       raise StopIteration
+    def __init__(self, distribution):
+        pass
+
+    def __next__(self):
+        raise StopIteration
+
 
 class FakeDistribution:
-    version = 'my_test_version_from_pkg_resources'
+    version = "my_test_version_from_pkg_resources"
 
     def __iter__(self):
         return FakeDistributionIterator(self)
