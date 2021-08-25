bc1de5c4384e51d6b086e46d3699cc63c92dcf06
maxday <maxime.david@datadoghq.com>
2021-08-25 13:18:57 -0400

    change version detection mecanism

---
+++ tests/version/test_version.py
@@ -0,0 +1,34 @@
+from ddtrace.version import get_version
+from tests.version import _version
+from unittest import mock
+import sys
+
+
+class TestVersion():
+    def test_get_version_from_version_file(self):
+        with mock.patch.dict(sys.modules, {'ddtrace._version':sys.modules['tests.version._version']}):
+            assert get_version() == 'my_test_version_from_generated_file'
+
+    def test_get_version_from_pkg_resources(self):
+        with mock.patch.dict(sys.modules, {'ddtrace._version':None}):
+            with mock.patch("pkg_resources.get_distribution") as mock_get_distribution:
+                mock_get_distribution.return_value = FakeDistribution()
+                assert get_version() == 'my_test_version_from_pkg_resources'
+                mock_get_distribution.assert_called_with('ddtrace.version')
+                
+    def test_get_version_dev_fallback(self):
+        with mock.patch.dict(sys.modules, {'ddtrace._version':None}):
+            with mock.patch.dict(sys.modules, {'pkg_resources':None}):
+                assert get_version() == "dev"
+
+class FakeDistributionIterator:
+   def __init__(self, distribution):
+       pass
+   def __next__(self):
+       raise StopIteration
+
+class FakeDistribution:
+    version = 'my_test_version_from_pkg_resources'
+
+    def __iter__(self):
+        return FakeDistributionIterator(self)
