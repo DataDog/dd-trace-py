6cc56f69519a08511cd0d11d3838c4a06d7f4756
brettlangdon <brett.langdon@datadoghq.com>
2021-09-01 15:16:12 -0400

    rename env variable to _DD_CYTHON_ANNOTATE=1

--- setup.py
+++ setup.py
@@ -255,7 +255,7 @@ setup(
             "PY_MICRO_VERSION": sys.version_info.micro,
         },
         force=True,
-        annotate=os.getenv("CYTHON_ANNOTATE") == "1",
+        annotate=os.getenv("_DD_CYTHON_ANNOTATE") == "1",
     )
     + get_exts_for("wrapt")
     + get_exts_for("psutil"),
