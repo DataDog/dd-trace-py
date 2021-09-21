9e0a83ff3b0c7f68ff44444c7a4de15af2fd4e35
Kyle Verhoog <kyle.verhoog@datadoghq.com>
2021-09-21 16:53:03 -0400

    Fix formatting

--- templates/integration/__init__.py
+++ templates/integration/__init__.py
@@ -42,10 +42,12 @@ To configure the foo integration on an per-instance basis use the
 """
 from ...utils.importlib import require_modules
 
+
 required_modules = ["foo"]
 
 with require_modules(required_modules) as missing_modules:
     if not missing_modules:
-        from .patch import patch, unpatch
+        from .patch import patch
+        from .patch import unpatch
 
         __all__ = ["patch", "unpatch"]
