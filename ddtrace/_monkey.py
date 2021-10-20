7c5c13af2d8bbae5b86f4b79409163d4abe0bae7
mtoffl01 <mikayla.toffler@datadoghq.com>
2021-10-20 11:59:46 -0400

    changed import back to absolute import

--- ddtrace/_monkey.py
+++ ddtrace/_monkey.py
@@ -10,7 +10,7 @@ from .internal.logger import get_logger
 from .settings import _config as config
 from .utils import formats
 from .utils.deprecation import deprecated
-from .vendor.wrapt.importer import when_imported
+from ddtrace.vendor.wrapt.importer import when_imported
 
 
 log = get_logger(__name__)
