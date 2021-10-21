7230da52ec7277093e3a3cd515ebed6ea6fc1541
Kyle Verhoog <kyle@verhoog.ca>
2021-10-21 15:15:17 -0400

    <!-- Defaults to the title of the PR. Replace if desired. -->
    Disable, mark snowflake integration as beta
    
    <!-- Briefly describe the change and why it was required. -->
    We haven't yet been able to verify snowflake with a real snowflake
    instance. Until a time in which we can, the integration should be
    disabled by default.
    
    (cherry picked from commit 32c0d960eef525c20a5b7f72d328ef00aa03899c)

--- ddtrace/monkey.py
+++ ddtrace/monkey.py
@@ -53,7 +53,7 @@ PATCH_MODULES = {
     "requests": True,
     "rq": True,
     "sanic": True,
-    "snowflake": True,
+    "snowflake": False,
     "sqlalchemy": False,  # Prefer DB client instrumentation
     "sqlite3": True,
     "aiohttp": True,  # requires asyncio (Python 3.4+)
