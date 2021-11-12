4bbc24a7809a3c949a32c145d73c5432d287030e
Nicolas Vivet <nicolas.vivet@datadoghq.com>
2021-11-12 19:13:57 +0100

    Convert Python obj to ddwaf obj

--- ddtrace/appsec/_ddwaf.pyx
+++ ddtrace/appsec/_ddwaf.pyx
@@ -3,11 +3,75 @@
 # distutils: libraries = ddwaf
 
 import typing
-cimport _libddwaf
+from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
+
+from _libddwaf cimport (
+    ddwaf_object,
+    ddwaf_object_array,
+    ddwaf_object_array_add,
+    ddwaf_object_invalid,
+    ddwaf_object_map,
+    ddwaf_object_map_addl,
+    ddwaf_object_stringl,
+    ddwaf_object_free,
+    ddwaf_version,
+    ddwaf_get_version
+)
 
 
 def version():
     # type: () -> typing.Tuple[int, int, int]
-    cdef _libddwaf.ddwaf_version version
-    _libddwaf.ddwaf_get_version(&version)
+    cdef ddwaf_version version
+    ddwaf_get_version(&version)
     return (version.major, version.minor, version.patch)
+
+
+cdef ddwaf_object* _alloc():
+    return <ddwaf_object *> PyMem_Malloc(sizeof(ddwaf_object))
+
+cdef void _dealloc(ddwaf_object *obj):
+    if obj != NULL:
+        ddwaf_object_free(obj)
+        PyMem_Free(obj)
+
+# TODO unicode
+cdef ddwaf_object* _convert(value):
+    obj = _alloc()
+    if obj == NULL:
+        return NULL
+    if isinstance(value, unicode):
+        value = value.encode("utf-8", errors="surrogatepass")
+    if isinstance(value, bytes):
+        ddwaf_object_stringl(obj, value, len(value))
+    elif isinstance(value, (list, tuple)):
+        ddwaf_object_array(obj);
+        for item in value:
+            item_obj = _convert(item)
+            ret = ddwaf_object_array_add(obj, item_obj)
+            if ret is False:
+                _dealloc(item_obj)
+    elif isinstance(value, dict):
+        ddwaf_object_map(obj)
+        for k, v in value.items():
+            item_obj = _convert(v)
+            if isinstance(k, unicode):
+                k = k.encode("utf-8", errors="surrogatepass")
+            if isinstance(k, bytes):
+                ret = ddwaf_object_map_addl(obj, k, len(k), item_obj)
+                if ret is False:
+                    _dealloc(item_obj)
+    else:
+        ddwaf_object_invalid(obj)
+    return obj
+
+cdef class _Wrapper:
+    cdef ddwaf_object *_ptr
+
+    def __cinit__(self, value):
+        self._ptr = _convert(value)
+        if self._ptr == NULL:
+            raise MemoryError
+
+    def __dealloc__(self):
+        _dealloc(self._ptr)
+        self._ptr = NULL
--- ddtrace/appsec/_libddwaf.pxd
+++ ddtrace/appsec/_libddwaf.pxd
@@ -1,4 +1,5 @@
-from libc.stdint cimport uint16_t
+from libcpp cimport bool
+from libc.stdint cimport uint16_t, int64_t, uint64_t
 
 
 cdef extern from "include/ddwaf.h":
@@ -8,3 +9,22 @@ cdef extern from "include/ddwaf.h":
         uint16_t patch
 
     void ddwaf_get_version(ddwaf_version *version)
+
+    ctypedef struct ddwaf_object:
+        pass
+
+    ddwaf_object* ddwaf_object_invalid(ddwaf_object *obj);
+    ddwaf_object* ddwaf_object_string(ddwaf_object *obj, const char *val);
+    ddwaf_object* ddwaf_object_stringl(ddwaf_object *obj, const char *val, size_t length);
+    ddwaf_object* ddwaf_object_stringl_nc(ddwaf_object *obj, const char *val, size_t length);
+    ddwaf_object* ddwaf_object_signed(ddwaf_object *obj, int64_t val);
+    ddwaf_object* ddwaf_object_unsigned(ddwaf_object *obj, uint64_t val);
+    ddwaf_object* ddwaf_object_signed_force(ddwaf_object *obj, int64_t val);
+    ddwaf_object* ddwaf_object_unsigned_force(ddwaf_object *obj, uint64_t val);
+    ddwaf_object* ddwaf_object_array(ddwaf_object *obj);
+    bool ddwaf_object_array_add(ddwaf_object *array, ddwaf_object *obj);
+    ddwaf_object* ddwaf_object_map(ddwaf_object *obj);
+    bool ddwaf_object_map_add(ddwaf_object *map, const char *key, ddwaf_object *obj);
+    bool ddwaf_object_map_addl(ddwaf_object *map, const char *key, size_t length, ddwaf_object *obj);
+    bool ddwaf_object_map_addl_nc(ddwaf_object *map, const char *key, size_t length, ddwaf_object *obj);
+    void ddwaf_object_free(ddwaf_object *obj);
