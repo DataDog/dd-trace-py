1b6cec4c53c7bb5d46781b63dcb866ac64108610
Brett Langdon <brett.langdon@datadoghq.com>
2021-11-15 16:47:06 -0500

    fix(internal) cdef Python exceptions being ignored (#3008)
    * fix(internal) cdef Python exceptions being ignored
    
    In a cdef function `raise` statements will create a Python
    exception, add it to the exception stack, and then immediately
    return. However, the caller will not explicitly check to see if
    any exception has occurred unless an exception return value has
    been provided.
    
    Until now these cdef functions when they encountered a `raise`
    would push the Python exception, and then return the default
    `int` value of `0`. Which based on how we use the return values
    `0` means success.
    
    This means that anytime we encounter an exception in our encoder
    we potentially ignore any exceptions and still append the
    partially encoded trace into the buffer.
    
    With this change we are standardizing around `except? -1` which
    tells the caller that their _might_ be a Python exception if the
    return value is `-1`. We do this because the `msgpack_*`
    functions we call will also return `-1` on error, but will not
    create a Python exception.
    
    * add release note
    
    * Update integration tests
    
    * remove unused import
    
    * set file encoding
    
    * Update releasenotes/notes/fix-encoding-exceptions-c12900b38741d2bf.yaml
    
    Co-authored-by: Tahir H. Butt <tahir.butt@datadoghq.com>
    
    Co-authored-by: Tahir H. Butt <tahir.butt@datadoghq.com>

--- tests/integration/test_encoding.py
+++ tests/integration/test_encoding.py
@@ -5,7 +5,6 @@ import mock
 import pytest
 
 from ddtrace import Tracer
-from ddtrace.internal import agent
 
 
 AGENT_VERSION = os.environ.get("AGENT_VERSION")
@@ -64,68 +63,3 @@ class TestTraceAcceptedByAgent:
             tracer.shutdown()
         log.warning.assert_not_called()
         log.error.assert_not_called()
-
-
-@pytest.mark.skipif(AGENT_VERSION == "testagent", reason="Test agent doesn't return 400 response for bad trace")
-class TestTraceRejectedByAgent:
-    def _assert_bad_trace_refused_by_agent(self, mock_log):
-        """Assert that agent refused a bad trace via log call."""
-        calls = [
-            mock.call(
-                "failed to send traces to Datadog Agent at %s: HTTP error status %s, reason %s",
-                "{}/{}/traces".format(agent.get_trace_url(), "v0.4"),
-                400,
-                "Bad Request",
-            )
-        ]
-        mock_log.error.assert_has_calls(calls)
-
-    def test_wrong_span_name_type_refused_by_agent(self):
-        """Span names should be a text type."""
-        tracer = Tracer()
-        with mock.patch("ddtrace.internal.writer.log") as log:
-            with tracer.trace(123):
-                pass
-            tracer.shutdown()
-
-        self._assert_bad_trace_refused_by_agent(log)
-
-    @pytest.mark.parametrize(
-        "meta",
-        [
-            ({"env": "my-env", "tag1": "some_str_1", "tag2": "some_str_2", "tag3": [1, 2, 3]}),
-            ({"env": "test-env", b"tag1": {"wrong_type": True}, b"tag2": "some_str_2", b"tag3": "some_str_3"}),
-            ({"env": "my-test-env", u"😐": "some_str_1", b"tag2": "some_str_2", "unicode": 12345}),
-        ],
-    )
-    def test_trace_with_wrong_meta_types_refused_by_agent(self, meta):
-        tracer = Tracer()
-        with mock.patch("ddtrace.internal.writer.log") as log:
-            with tracer.trace("root") as root:
-                root.meta = meta
-                for _ in range(499):
-                    with tracer.trace("child") as child:
-                        child.meta = meta
-            tracer.shutdown()
-
-        self._assert_bad_trace_refused_by_agent(log)
-
-    @pytest.mark.parametrize(
-        "metrics",
-        [
-            ({"num1": 12345, "num2": 53421, "num3": 1, "num4": "not-a-number"}),
-            ({b"num1": 123.45, b"num2": [1, 2, 3], b"num3": 11.0, b"num4": 1.20}),
-            ({u"😐": "123.45", b"num2": "1", "num3": {"is_number": False}, "num4": "12345"}),
-        ],
-    )
-    def test_trace_with_wrong_metrics_types_refused_by_agent(self, metrics):
-        tracer = Tracer()
-        with mock.patch("ddtrace.internal.writer.log") as log:
-            with tracer.trace("root") as root:
-                root.metrics = metrics
-                for _ in range(499):
-                    with tracer.trace("child") as child:
-                        child.metrics = metrics
-            tracer.shutdown()
-
-        self._assert_bad_trace_refused_by_agent(log)
