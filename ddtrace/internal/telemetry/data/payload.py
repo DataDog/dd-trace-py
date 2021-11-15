a330ee1c2abadf0bb697cdf7a27da5eaf1c09c1c
Munir Abdinur <munir.abdinur@datadoghq.com>
2021-11-15 11:37:38 -0500

    add telemetry data classes

---
+++ ddtrace/internal/telemetry/data/payload.py
@@ -0,0 +1,84 @@
+from typing import Dict
+from typing import List
+
+from ....version import get_version
+from .dependency import Dependency
+from .dependency import create_dependency
+from .integration import Integration
+from .metrics import Series
+
+
+class Payload:
+    REQUEST_TYPE = "not-defined"  # type: str
+
+    def to_dict(self):
+        # type: () -> Dict
+        return {}
+
+
+class AppIntegrationsChangedPayload(Payload):
+    REQUEST_TYPE = "app-integrations-changed"  # type: str
+
+    def __init__(self, integrations):
+        # type: (List[Integration]) -> None
+        super().__init__()
+        self.integrations = integrations  # type: List[Integration]
+
+    def to_dict(self):
+        # type: () -> Dict
+        return {"integrations": self.integrations}
+
+
+class AppStartedPayload(Payload):
+    REQUEST_TYPE = "app-started"  # type: str
+
+    def __init__(self, configurations={}, additional_payload={}):
+        # type: (Dict, Dict) -> None
+        super().__init__()
+        self.configuration = configurations  # type: Dict[str, str]
+        self.additional_payload = additional_payload  # type: Dict[str, str]
+        self.dependencies = self.get_dependencies()
+
+    def get_dependencies(self):
+        # type: () -> List[Dependency]
+        import pkg_resources
+
+        dependencies = []
+        for pkg in pkg_resources.working_set:
+            dependency = create_dependency(pkg.project_name, pkg.version)  # type: Dependency
+            dependencies.append(dependency)
+
+        return dependencies
+
+    def to_dict(self):
+        # type: () -> Dict
+        return {
+            "configuration": self.configuration,
+            "additional_payload": self.additional_payload,
+            "dependencies": self.dependencies,
+        }
+
+
+class AppGenerateMetricsPayload(Payload):
+    REQUEST_TYPE = "app-generate-metrics"  # type: str
+
+    def __init__(self, series):
+        # type: (List[Series]) -> None
+        super().__init__()
+        self.namespace = "tracers"  # type: str
+        self.lib_language = "python"  # type: str
+        self.lib_version = get_version()  # type: str
+        self.series = series  # type: List[Series]
+
+    def to_dict(self):
+        # type: () -> Dict
+        return {
+            "namespace": self.namespace,
+            "lib_language": self.lib_language,
+            "lib_version": self.lib_version,
+            "series": [s.to_dict() for s in self.series],
+        }
+
+
+class AppClosedPayload(Payload):
+    REQUEST_TYPE = "app-closing"  # type: str
---
+++ ddtrace/internal/telemetry/data/telemetry_request.py
@@ -0,0 +1,59 @@
+import os
+import time
+from typing import Any
+from typing import Dict
+from typing import TypedDict
+
+from ...runtime import get_runtime_id
+from .application import APPLICATION
+from .application import Application
+from .host import HOST
+from .host import Host
+from .payload import Payload
+
+
+RequestBody = TypedDict(
+    "RequestBody",
+    {
+        "tracer_time": int,
+        "runtime_id": str,
+        "api_version": str,
+        "seq_id": int,
+        "application": Application,
+        "host": Host,
+        "payload": Dict[Any, Any],
+        "request_type": str,
+    },
+)
+
+TelemetryRequest = TypedDict(
+    "TelemetryRequest",
+    {
+        "headers": Dict[str, str],
+        "body": RequestBody,
+    },
+)
+
+
+def create_telemetry_request(payload):
+    # type: (Payload) -> TelemetryRequest
+    return {
+        "headers": {
+            "Content-type": "application/json",
+            "DD-API-KEY": os.environ.get(
+                "DD_API_KEY", default=""
+            ),  # get_api_key() will be removed when agent proxy is live
+            "DD-Telemetry-Request-Type": payload.REQUEST_TYPE,
+            "DD-Telemetry-API-Version": "v1",
+        },
+        "body": {
+            "tracer_time": int(time.time()),
+            "runtime_id": get_runtime_id(),
+            "api_version": "v1",
+            "seq_id": 0,
+            "application": APPLICATION,
+            "host": HOST,
+            "payload": payload.to_dict(),
+            "request_type": payload.REQUEST_TYPE,
+        },
+    }
