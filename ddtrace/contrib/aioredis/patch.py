b9cd6708a80ec78d666d81f7bd3b501565d66131
Munir Abdinur <munir.abdinur@datadoghq.com>
2021-12-07 18:16:46 -0500

    helpful comment about activate=False

--- ddtrace/contrib/aioredis/patch.py
+++ ddtrace/contrib/aioredis/patch.py
@@ -108,6 +108,8 @@ def traced_13_execute_command(func, instance, args, kwargs):
         return func(*args, **kwargs)
 
     decoded_args = [arg.decode() if isinstance(arg, bytes) else arg for arg in args]
+    # This span is unactive in the context because it should not have a child span
+    # Each consectuive execute command should create a sister span
     span = pin.tracer.start_span(
         redisx.CMD, service=trace_utils.ext_service(pin, config.aioredis), span_type=SpanTypes.REDIS, activate=False
     )
