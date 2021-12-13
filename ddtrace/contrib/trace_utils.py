3bf82a66aee3ad49e11b6bfc760fa3df02e9c060
ZStriker19 <zgroves19@gmail.com>
2021-12-12 20:31:40 -0800

    move propagation/utils methods to trace_utils.py

--- ddtrace/contrib/trace_utils.py
+++ ddtrace/contrib/trace_utils.py
@@ -321,3 +321,31 @@ def set_flattened_tags(
     for prefix, value in items:
         for tag, v in _flatten(value, sep, prefix, exclude_policy):
             span.set_tag(tag, processor(v) if processor is not None else v)
+
+
+@cached()
+def get_wsgi_header(header):
+    # type: (str) -> str
+    """Returns a WSGI compliant HTTP header.
+    See https://www.python.org/dev/peps/pep-3333/#environ-variables for
+    information from the spec.
+    """
+    return "HTTP_{}".format(header.upper().replace("-", "_"))
+
+
+@cached()
+def from_wsgi_header(header):
+    # type: (str) -> Optional[str]
+    """Convert a WSGI compliant HTTP header into the original header.
+    See https://www.python.org/dev/peps/pep-3333/#environ-variables for
+    information from the spec.
+    """
+    HTTP_PREFIX = "HTTP_"
+    # PEP 333 gives two headers which aren't prepended with HTTP_.
+    UNPREFIXED_HEADERS = {"CONTENT_TYPE", "CONTENT_LENGTH"}
+
+    if header.startswith(HTTP_PREFIX):
+        header = header[len(HTTP_PREFIX) :]
+    elif header not in UNPREFIXED_HEADERS:
+        return None
+    return header.replace("_", "-").title()
