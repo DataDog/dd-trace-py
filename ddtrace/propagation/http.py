9c2a9d83c9652bc91491973367e438bf0ea502fa
ZStriker19 <zgroves19@gmail.com>
2021-12-14 16:34:20 -0800

    update references

--- ddtrace/propagation/http.py
+++ ddtrace/propagation/http.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 from ..context import Context
 from ..internal.logger import get_logger
-from .utils import get_wsgi_header
+from ._utils import get_wsgi_header
 
 
 log = get_logger(__name__)
