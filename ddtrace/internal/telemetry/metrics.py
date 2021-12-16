5ebef95d6dd25d9d8aacbf6666b26c00ee3c0eaa
Munir Abdinur <munir.abdinur@datadoghq.com>
2021-12-15 19:53:18 -0500

    Update metrics.py

--- ddtrace/internal/telemetry/metrics.py
+++ ddtrace/internal/telemetry/metrics.py
@@ -14,8 +14,9 @@ MetricType = Literal["count", "gauge", "rate"]
 
 class Series:
     """stores metrics which will be sent to the Telemetry Intake metrics to the Datadog Instrumentation Telemetry Org"""
-
-    def __init__(self, metric, metric_type="count", common=False, interval=None):
+    HOST_NAME = get_hostname()
+    
+    def __init__(self, metric, metric_type="count", common=False)
         # type: (str, MetricType, bool, Optional[int]) -> None
         """
         metric: metric name
@@ -25,41 +26,43 @@ class Series:
         """
         self.metric = metric
         self.type = metric_type
-        self.interval = interval
         self.common = common
-        self.points = []  # type: List[Tuple[int, int]]
-        self.tags = {}  # type: Dict[str, str]
-        self.host = get_hostname()
+        self._interval = None
+        self._points = []  # type: List[Tuple[int, int]]
+        self._tags = {}  # type: Dict[str, str]
 
     def add_point(self, value):
         # type: (int) -> None
         """adds timestamped data point associated with a metric"""
         timestamp = int(time.time())  # type: int
-        self.points.append((timestamp, value))
+        self._points.append((timestamp, value))
 
     def set_tag(self, name, value):
         # type: (str, str) -> None
         """sets a metrics tag"""
-        self.tags[name] = value
+        self._tags[name] = value
+    
+    def set_interval(self, name, interval):
+        self._interval = interval
 
     def combine(self, other_series):
         # type: (Series) -> None
         if self.type != other_series.type:
             raise ValueError("Metrics with the same name should have the same type")
-        if self.interval != other_series.interval:
+        if self._interval != other_series._interval:
             raise ValueError("Metrics with the same name should have the same interval")
-        self.tags.update(other_series.tags)
-        self.points.extend(other_series.points)
+        self._tags.update(other_series._tags)
+        self._points.extend(other_series._points)
 
     def to_dict(self):
         # type: () -> Dict
         """returns a dictionary containing the metrics fields expected by the telemetry intake service"""
         return {
             "metric": self.metric,
-            "points": self.points,
-            "tags": self.tags,
-            "type": self.type.value,
+            "points": self._points,
+            "tags": self._tags,
+            "type": self.type,
             "common": self.common,
-            "interval": self.interval,
-            "host": self.host,
+            "interval": self._interval,
+            "host": HOST_NAME,
         }
