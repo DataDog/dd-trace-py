ba17e056264e34ea8760c1ff18e92bb2911682d1
Vladimir de Turckheim <vdeturckheim@users.noreply.github.com>
2022-01-04 14:50:37 +0100

    added some AppSec standard logs added _dd.runtime_family tag

--- ddtrace/appsec/__init__.py
+++ ddtrace/appsec/__init__.py
@@ -19,7 +19,12 @@ except ImportError as e:
 
 def enable(*args, **kwargs):
     if AppSecProcessor is None:
-        log.warning("AppSec was enabled but couldn't start due to the following error:\n%s", _appsec_import_error)
+        # DDAS-001-01
+        log.error(
+            "AppSec could not start because of an unexpected error. No security activities will be collected. "
+            "Please contact support at https://docs.datadoghq.com/help/ for help.Error details: \n%s",
+            _appsec_import_error,
+        )
         return
     AppSecProcessor.enable(*args, **kwargs)
 
