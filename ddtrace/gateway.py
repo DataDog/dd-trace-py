a560f3746f24f6d0339835b0fe2aeba74c4d49c4
Vladimir de Turckheim <vdeturckheim@users.noreply.github.com>
2022-01-05 17:20:11 +0100

    examples of message bus

---
+++ ddtrace/gateway.py
@@ -0,0 +1,40 @@
+from typing import Any
+
+
+class Subscription(object):
+
+    def __init__(self, addresses):
+        self.addresses = addresses
+
+    def run(self, store):
+        raise NotImplementedError("Please implement a proper subscription")
+
+class Gateway(object):
+
+    def __init__(self):
+        self.shortcuts = {}
+
+    def subscribe(self, subscription):
+        # type: (Subscription) -> None
+        for address in subscription.addresses:
+            if address not in self.shortcuts:
+                self.shortcuts[address] = []
+            self.shortcuts[address].append(subscription)
+
+
+    def propagate(self, store, data):
+        # type: (dict, dict) -> map[Any]
+        store.update(data)
+        new_keys = data.keys()
+        all_keys = set(store.keys())
+        todo = set()
+        for key in new_keys:
+            if key not in self.shortcuts:
+                continue
+            for subscription in self.shortcuts[key]:
+                if subscription in todo:
+                    continue
+                if all_keys < subscription.addresses:
+                    todo.add(subscription)
+        results = map(lambda sub: sub.run(store), todo)
+        return results
---
+++ ddtrace/gateway2.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class EVENTS(Enum):
+    INCOMING_REQUEST_START = 0
+    INCOMING_REQUEST_UPDATE = 1
+    INCOMING_REQUEST_END = 2
+
+
+class Gateway2(object):
+
+    def __init__(self):
+        self.listeners = {}
+
+    def on(self, event, callback):
+        if event not in self.listeners:
+            self.listeners[event] = set()
+        self.listeners[event].add(callback)
+
+
+    def emit(self, event, data):
+        if event not in self.listeners:
+            return
+        return map(lambda cb: cb(data), self.listeners[event])
