5215f8b01bc913c57c02856f56217331a3abcb96
chris.agocs <chris.agocs@datadoghq.com>
2022-01-13 17:16:50 -0500

    Add another entry

--- tests/contrib/botocore/test.py
+++ tests/contrib/botocore/test.py
@@ -713,18 +713,42 @@ class BotocoreTest(TracerTestCase):
                 "DetailType": "some-event-detail-type",
                 "Detail": '{"foo":"bar"}',
                 "EventBusName": "a-test-bus",
-            }
+            },
+            {
+                "Source": "another-event-source",
+                "DetailType": "a-different-event-detail-type",
+                "Detail": '{"foo":"baz"}',
+                "EventBusName": "a-test-bus",
+            },
         ]
         bridge.put_events(Entries=entries)
 
         spans = self.get_spans()
         assert spans
-        self.assertEqual(len(spans), 1)
+        self.assertEqual(len(spans), 2)
         span = spans[0]
         print("AGOCS! Here's the span:")
         print(span)
         print("Here is the meta")
         print(span.meta)
+
+        entries = json.loads(span.get_tag("Entries"))
+        self.assertTrue("Detail" in entries)
+        detail = json.loads(entries["Detail"])
+        self.assertTrue(HTTP_HEADER_PARENT_ID in detail)
+        self.assertTrue(HTTP_HEADER_TRACE_ID in detail)
+        self.assertEqual(detail[HTTP_HEADER_TRACE_ID], str(span.trace_id))
+        self.assertEqual(detail[HTTP_HEADER_PARENT_ID], str(span.span_id))
+
+        span = spans[1]
+
+        entries = json.loads(span.get_tag("Entries"))
+        self.assertTrue("Detail" in entries)
+        detail = json.loads(entries["Detail"])
+        self.assertTrue(HTTP_HEADER_PARENT_ID in detail)
+        self.assertTrue(HTTP_HEADER_TRACE_ID in detail)
+        self.assertEqual(detail[HTTP_HEADER_TRACE_ID], str(span.trace_id))
+        self.assertEqual(detail[HTTP_HEADER_PARENT_ID], str(span.span_id))
 
         bridge.delete_event_bus(Name="a-test-bus")
         assert False
