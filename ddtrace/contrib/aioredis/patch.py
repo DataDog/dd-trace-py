e27eb6ed84e97c9ce96fb75e9c5ccfef5037a46f
Jameel Al-Aziz <me@jalaziz.io>
2022-01-20 06:18:51 -0800

    fix formatting

--- ddtrace/contrib/aioredis/patch.py
+++ ddtrace/contrib/aioredis/patch.py
@@ -112,7 +112,11 @@ def traced_13_execute_command(func, instance, args, kwargs):
     # (we don't want this span to be the parent of all other spans created before the future is resolved)
     parent = pin.tracer.current_span()
     span = pin.tracer.start_span(
-        redisx.CMD, service=trace_utils.ext_service(pin, config.aioredis), span_type=SpanTypes.REDIS, activate=False, child_of=parent
+        redisx.CMD,
+        service=trace_utils.ext_service(pin, config.aioredis),
+        span_type=SpanTypes.REDIS,
+        activate=False,
+        child_of=parent,
     )
 
     span.set_tag(SPAN_MEASURED_KEY)
