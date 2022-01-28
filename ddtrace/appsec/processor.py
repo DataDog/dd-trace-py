bf84959ecbf2ad59ff6cac785516ba2cb3f08e58
Vladimir de Turckheim <vdeturckheim@users.noreply.github.com>
2022-01-28 13:50:10 +0100

    add log codes everywhere

--- ddtrace/appsec/processor.py
+++ ddtrace/appsec/processor.py
@@ -47,26 +47,27 @@ class AppSecSpanProcessor(SpanProcessor):
                 with open(self.rules, "r") as f:
                     rules = json.load(f)
             except EnvironmentError as err:
-                # DDAS-0001-03
                 if err.errno == errno.ENOENT:
-                    log.error("AppSec could not read the rule file %s. Reason: file does not exist", self.rules)
+                    log.error("[DDAS-0001-03] "
+                              "AppSec could not read the rule file %s. Reason: file does not exist", self.rules)
                 else:
-                    # DDAS-0001-03 - TODO: try to log reasons
-                    log.error("AppSec could not read the rule file %s.", self.rules)
+                    # TODO: try to log reasons
+                    log.error("[DDAS-0001-03] AppSec could not read the rule file %s.", self.rules)
                 raise
             except json.decoder.JSONDecodeError:
-                # DDAS-0001-03
-                log.error("AppSec could not read the rule file %s. Reason: invalid JSON file", self.rules)
+                log.error("[DDAS-0001-03] "
+                          "AppSec could not read the rule file %s. Reason: invalid JSON file", self.rules)
                 raise
             except Exception:
-                # DDAS-0001-03 - TODO: try to log reasons
-                log.error("AppSec could not read the rule file %s.", self.rules)
+                # TODO: try to log reasons
+                log.error("[DDAS-0001-03] "
+                          "AppSec could not read the rule file %s.", self.rules)
                 raise
             try:
                 self._ddwaf = DDWaf(rules)
             except ValueError:
                 # Partial of DDAS-0005-00
-                log.warning("WAF initialization failed")
+                log.warning("[DDAS-0005-00] WAF initialization failed")
                 raise
 
     def on_span_start(self, span):
@@ -84,12 +85,11 @@ class AppSecSpanProcessor(SpanProcessor):
                 "server.request.uri.raw": span.get_tag(ddtrace.ext.http.URL),
                 "server.response.status": span.get_tag(ddtrace.ext.http.STATUS_CODE),
             }
-            # DDAS-001-00
-            log.debug("Executing AppSec In-App WAF with parameters: %s", data)
+            log.debug("[DDAS-001-00] Executing AppSec In-App WAF with parameters: %s", data)
             res = self._ddwaf.run(data)  # res is a serialized json
             if res is not None:
                 # Partial DDAS-011-00
-                log.debug("AppSec In-App WAF returned: %s", res)
+                log.debug("[DDAS-011-00] AppSec In-App WAF returned: %s", res)
                 span.meta["appsec.event"] = "true"
                 span.meta["_dd.appsec.json"] = '{"triggers":%s}' % (res,)
                 span.set_tag(MANUAL_KEEP_KEY)
