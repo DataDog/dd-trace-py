2a488172f00c831bf4a0f2b856210d9ef3ec0e67
Harvinder Ghotra <ghotra.harvinder@gmail.com>
2022-02-07 11:54:06 -0500

    Update ddtrace/contrib/botocore/patch.py
    Co-authored-by: Kyle Verhoog <kyle@verhoog.ca>

--- ddtrace/contrib/botocore/patch.py
+++ ddtrace/contrib/botocore/patch.py
@@ -144,7 +144,7 @@ def inject_trace_to_eventbridge_detail(args, span):
         detail_size = len(detail_json)
         if detail_size >= MAX_EVENTBRIDGE_DETAIL_SIZE:
             msg = "Detail with trace injection ({}) exceeds ({})".format(detail_size, MAX_EVENTBRIDGE_DETAIL_SIZE)
-            log.debug(msg)
+            log.debug("Detail with trace injection (%s) exceeds limit (%s)", detail_size, MAX_EVENTBRIDGE_DETAIL_SIZE)
             continue
 
         entry["Detail"] = detail_json
