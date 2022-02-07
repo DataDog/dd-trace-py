e22b1118f661e0efd12133c5a4f905fbcd06cdb9
Harvinder Ghotra <ghotra.harvinder@gmail.com>
2022-02-07 12:11:11 -0500

    Upgrade msg to warning

--- ddtrace/contrib/botocore/patch.py
+++ ddtrace/contrib/botocore/patch.py
@@ -143,7 +143,7 @@ def inject_trace_to_eventbridge_detail(args, span):
         # check if detail size will exceed max size with headers
         detail_size = len(detail_json)
         if detail_size >= MAX_EVENTBRIDGE_DETAIL_SIZE:
-            log.debug("Detail with trace injection (%s) exceeds limit (%s)", detail_size, MAX_EVENTBRIDGE_DETAIL_SIZE)
+            log.warning("Detail with trace injection (%s) exceeds limit (%s)", detail_size, MAX_EVENTBRIDGE_DETAIL_SIZE)
             continue
 
         entry["Detail"] = detail_json
