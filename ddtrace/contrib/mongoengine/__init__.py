104610a532e8795196e84ad7bc89ce870e10f88c
Tahir H. Butt <tahir.butt@datadoghq.com>
2022-02-09 15:38:26 -0500

    chore(mongoengine)!: remove legacy tracing

--- ddtrace/contrib/mongoengine/__init__.py
+++ ddtrace/contrib/mongoengine/__init__.py
@@ -25,6 +25,5 @@ required_modules = ["mongoengine"]
 with require_modules(required_modules) as missing_modules:
     if not missing_modules:
         from .patch import patch
-        from .patch import trace_mongoengine
 
-        __all__ = ["patch", "trace_mongoengine"]
+        __all__ = ["patch"]
