62d4314e1c3f2a699e5f16c7414b4b2c3a0f3608
Tahir H. Butt <tahir.butt@datadoghq.com>
2022-02-11 10:18:44 -0500

    chore: ignore dependabot for pr-name

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  ignores: [
+    /*
+     * dependabot cannot be configured to match commitlint configuration
+     * https://github.com/dependabot/dependabot-core/issues/1666
+     */
+    (commit) => commit.includes("chore(deps): Bump"),
+  ],
+};
