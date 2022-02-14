fdf23cdd9a9d88a61ae7f4820c53ce1ab11f01fb
Tahir H. Butt <tahir.butt@datadoghq.com>
2022-02-14 09:49:00 -0500

    chore!: remove deprecations (#3236)
    This PR removes deprecation for the 1.0 release. The motivation for batching multiple different commits is to avoid the time it would take to get 20+ PRs through CI.
    
    * chore(cassandra)!: remove get_traced_cassandra
    * chore(celery)! remove patch_task
    * chore(mongoengine)!: remove trace_mongoengine
    * chore(mysql)!: remove legacy traced connection
    * chore(psycopg)!: remove connection_factory
    * chore(requests)!: remove legacy config
    * chore(sqlite3)!: remove connection_factory
    * chore!: remove ddtrace.Tracer deprecations
    * chore!: remove safe_patch
    * chore!: remove ddtrace.contrib.util
    * chore!: remove pprint and meta from ddtrace.Span
    * chore!: remove ddtrace.ext.priority
    * chore!: remove ddtrace.ext.errors
    * chore!: remove ddtrace.encoding
    * chore!: remove ddtrace.utils
    * chore!: remove ddtrace.propagation.utils
    * chore!: remove deprecated ddtrace.http
    * chore!: remove deprecated ddtrace.monkey
    * chore!: remove deprecated ddtrace.compat
    * chore!: remove deprecated service name
    * chore!: remove deprecated ddtrace.Sampler.default_sampler

--- tests/commands/ddtrace_run_service.py
+++ tests/commands/ddtrace_run_service.py
@@ -2,5 +2,5 @@ import os
 
 
 if __name__ == "__main__":
-    assert os.getenv("DATADOG_SERVICE_NAME") == "my_test_service" or os.getenv("DD_SERVICE") == "my_test_service"
+    assert os.getenv("DD_SERVICE") == "my_test_service"
     print("Test success")
