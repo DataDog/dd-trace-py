1c402d1b8f31e631159b232acb1bc82f8cd8cd8b
Munir Abdinur <munir.abdinur@datadoghq.com>
2022-02-17 17:50:43 -0500

    chore(tracing/integrations)!: remove deprecated manual instrumentation helpers (#3286)
    Removes:
    
        ddtrace.contrib.flask.middleware.py:TraceMiddleware()
        ddtrace.contrib.pymongo.patch.py:pytrace_mongo_client()
        ddtrace.contrib.pymysql.tracers.py:get_traced_pymysql_connection()
        ddtrace.contrib.redis.tracers.py:get_traced_redis()
        ddtrace.contrib.redis.tracers.py:get_traced_redis_from()

--- ddtrace/contrib/redis/__init__.py
+++ ddtrace/contrib/redis/__init__.py
@@ -53,7 +53,5 @@ required_modules = ["redis", "redis.client"]
 with require_modules(required_modules) as missing_modules:
     if not missing_modules:
         from .patch import patch
-        from .tracers import get_traced_redis
-        from .tracers import get_traced_redis_from
 
-        __all__ = ["get_traced_redis", "get_traced_redis_from", "patch"]
+        __all__ = ["patch"]
