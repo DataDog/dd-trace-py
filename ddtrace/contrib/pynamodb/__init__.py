6161d4bae49f7d37dda6e4443787c043aa80b0cb
Tahir H. Butt <tahir.butt@datadoghq.com>
2022-02-18 16:49:39 -0500

    docs: add release notes for 1.0.0

--- ddtrace/contrib/pynamodb/__init__.py
+++ ddtrace/contrib/pynamodb/__init__.py
@@ -6,9 +6,9 @@ Enabling
 ~~~~~~~~
 
 The PynamoDB integration is enabled automatically when using
-:ref:`ddtrace-run<ddtracerun>` or :ref:`patch_all()<patch_all>`.
+:ref:`ddtrace-run<ddtracerun>` or :func:`patch_all()<ddtrace.patch_all>`.
 
-Or use :ref:`patch()<patch>` to manually enable the integration::
+Or use :func:`patch()<ddtrace.patch>` to manually enable the integration::
 
     import pynamodb
     from ddtrace import patch, config
