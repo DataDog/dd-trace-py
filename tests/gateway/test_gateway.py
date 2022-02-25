6ac23f3de912f26843c62bc04df6a432d4cf4d29
Vladimir de Turckheim <vlad2t@hotmail.com>
2022-02-25 14:21:50 +0100

    fixing gateway import

--- tests/gateway/test_gateway.py
+++ tests/gateway/test_gateway.py
@@ -1,28 +1,28 @@
-from ddtrace.gateway import Addresses
-from ddtrace.span import RequestStore
+from ddtrace.gateway import _Addresses
+from ddtrace.span import _RequestStore
 
 
 def test_gateway_flow(gateway):
-    gateway.mark_needed(Addresses.SERVER_RESPONSE_STATUS.value)
-    assert not gateway.is_needed(Addresses.SERVER_REQUEST_HEADERS_NO_COOKIES.value)
+    gateway.mark_needed(_Addresses.SERVER_RESPONSE_STATUS.value)
+    assert not gateway.is_needed(_Addresses.SERVER_REQUEST_HEADERS_NO_COOKIES.value)
     assert gateway.needed_address_count == 1
-    assert gateway.is_needed(Addresses.SERVER_RESPONSE_STATUS.value)
-    store = RequestStore()
-    data = {Addresses.SERVER_RESPONSE_STATUS.value: "404"}
+    assert gateway.is_needed(_Addresses.SERVER_RESPONSE_STATUS.value)
+    store = _RequestStore()
+    data = {_Addresses.SERVER_RESPONSE_STATUS.value: "404"}
     gateway.propagate(store, data)
-    assert store.kept_addresses[Addresses.SERVER_RESPONSE_STATUS.value] == "404"
+    assert store.kept_addresses[_Addresses.SERVER_RESPONSE_STATUS.value] == "404"
 
 
 def test_gateway_clear(gateway):
-    gateway.mark_needed(Addresses.SERVER_RESPONSE_STATUS.value)
-    store = RequestStore()
-    data = {Addresses.SERVER_RESPONSE_STATUS.value: "404"}
+    gateway.mark_needed(_Addresses.SERVER_RESPONSE_STATUS.value)
+    store = _RequestStore()
+    data = {_Addresses.SERVER_RESPONSE_STATUS.value: "404"}
     gateway.propagate(store, data)
-    assert store.kept_addresses[Addresses.SERVER_RESPONSE_STATUS.value] == "404"
+    assert store.kept_addresses[_Addresses.SERVER_RESPONSE_STATUS.value] == "404"
 
     gateway.clear()
 
-    store = RequestStore()
-    data = {Addresses.SERVER_RESPONSE_STATUS.value: "404"}
+    store = _RequestStore()
+    data = {_Addresses.SERVER_RESPONSE_STATUS.value: "404"}
     gateway.propagate(store, data)
-    assert Addresses.SERVER_RESPONSE_STATUS.value not in store.kept_addresses
+    assert _Addresses.SERVER_RESPONSE_STATUS.value not in store.kept_addresses
