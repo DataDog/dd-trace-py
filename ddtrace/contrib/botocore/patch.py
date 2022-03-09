a3e1f48240bbb9108b72dd2d9fec2326d2f589a5
Andrew Rodriguez <andrewrod003@gmail.com>
2022-03-09 17:53:11 -0500

    inject trace based on event_source

--- ddtrace/contrib/botocore/patch.py
+++ ddtrace/contrib/botocore/patch.py
@@ -69,13 +69,21 @@ def inject_trace_data_to_message_attributes(trace_data, entry):
 
     Inject trace headers into the an SQS or SNS record's MessageAttributes
     """
-    if "MessageAttributes" not in entry:
-        entry["MessageAttributes"] = {}
-    # An Amazon SQS message can contain up to 10 metadata attributes.
-    if len(entry["MessageAttributes"]) < 10:
+    event_source = entry.get("eventSource", None)
+    if event_source == "aws:sqs":
+        entry["MessageAttributes"]["_datadog"] = {"DataType": "String", "StringValue": json.dumps(trace_data)}
+    elif event_source == "aws:sns":
         entry["MessageAttributes"]["_datadog"] = {"DataType": "Binary", "BinaryValue": json.dumps(trace_data)}
     else:
-        log.warning("skipping trace injection, max number (10) of MessageAttributes exceeded")
+        log.warning("skipping trace injection, eventSource is not sns or sqs")
+
+    # if "MessageAttributes" not in entry:
+    #     entry["MessageAttributes"] = {}
+    # # An Amazon SQS message can contain up to 10 metadata attributes.
+    # if len(entry["MessageAttributes"]) < 10:
+    #     entry["MessageAttributes"]["_datadog"] = {"DataType": "Binary", "BinaryValue": json.dumps(trace_data)}
+    # else:
+    #     log.warning("skipping trace injection, max number (10) of MessageAttributes exceeded")
 
 
 def inject_trace_to_sqs_or_sns_batch_message(params, span):
