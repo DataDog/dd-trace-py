db48d734c45f6ef1f55f1ac311b39b09b315e5e5
Kyle Verhoog <kyle@verhoog.ca>
2022-03-09 17:00:22 -0500

    Fix docs conflict

--- ddtrace/contrib/aiohttp_jinja2/__init__.py
+++ ddtrace/contrib/aiohttp_jinja2/__init__.py
@@ -6,9 +6,9 @@ Enabling
 ~~~~~~~~
 
 The integration is enabled automatically when using
-:ref:`ddtrace-run<ddtracerun>` or :func:`patch_all()<ddtrace.patch_all>`.
+:ref:`ddtrace-run<ddtracerun>` or :ref:`patch_all()<ddtrace.patch_all>`.
 
-Or use :func:`patch()<ddtrace.patch>` to manually enable the integration::
+Or use :ref:`patch()<ddtrace.patch>` to manually enable the integration::
 
     from ddtrace import patch
     patch(aiohttp_jinja2=True)
