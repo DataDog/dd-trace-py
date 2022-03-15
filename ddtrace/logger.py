68f31ce1f4abb3d16e47ae40a1310e2517240a44
wan <wan.tsui@datadoghq.com>
2022-03-15 18:05:22 -0400

    Adjust for mypy and add tests for ddtrace-run.

--- ddtrace/logger.py
+++ ddtrace/logger.py
@@ -15,7 +15,6 @@ def configure_ddtrace_logger():
     num_backup = 1
     log_format = "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] {}- %(message)s"
     log_formatter = logging.Formatter(log_format)
-    ddtrace_handler = logging.Handler
 
     ddtrace_logger = logging.getLogger("ddtrace")
     ddtrace_logger.propagate = False
@@ -26,12 +25,12 @@ def configure_ddtrace_logger():
         ddtrace_logger.setLevel(logging.DEBUG)
 
     if log_path is not None:
-        ddtrace_handler = RotatingFileHandler(filename=log_path, mode="a", maxBytes=max_file_bytes, backupCount=num_backup)
-        ddtrace_handler.setFormatter(log_formatter)
-        ddtrace_logger.addHandler(ddtrace_handler)
+        ddtrace_file_handler = RotatingFileHandler(filename=log_path, mode="a", maxBytes=max_file_bytes, backupCount=num_backup)
+        ddtrace_file_handler.setFormatter(log_formatter)
+        ddtrace_logger.addHandler(ddtrace_file_handler)
         ddtrace_logger.debug("Debug mode has been enabled with debug logs logging to %s", log_path)
     else:
-        ddtrace_handler = logging.StreamHandler()
-        ddtrace_handler.setFormatter(log_formatter)
-        ddtrace_logger.addHandler(ddtrace_handler)
+        ddtrace_stream_handler = logging.StreamHandler()
+        ddtrace_stream_handler.setFormatter(log_formatter)
+        ddtrace_logger.addHandler(ddtrace_stream_handler)
         ddtrace_logger.debug("Debug mode has been enabled with debug logs logging to stderr")
--- tests/tracer/test_logging.py
+++ tests/tracer/test_logging.py
@@ -1,7 +1,7 @@
 import os
 
 
-def test_unrelated_logger_loaded_first_patch_all(run_python_code_in_subprocess):
+def test_unrelated_logger_in_debug_with_patch_all(run_python_code_in_subprocess):
     """
     When importing the tracer after the custom log,
                 the ddtrace logger does not override any custom logs settings
@@ -107,7 +107,7 @@ s.finish()
 
 def test_debug_logs_go_to_stderr_ddtrace_run(ddtrace_run_python_code_in_subprocess):
     """
-    When setting up the default logger with ddtrace, it automatically logs to stderr.
+    When setting up the default logger with ddtrace-run, it automatically logs to stderr.
     """
     env = os.environ.copy()
     env["DD_TRACE_DEBUG"] = "true"
@@ -124,3 +124,79 @@ s.finish()
     out, err, status, pid = ddtrace_run_python_code_in_subprocess(code, env=env)
     assert status == 0, err
     assert err != b""
+
+
+def test_unrelated_logger_in_debug_with_ddtrace_run(ddtrace_run_python_code_in_subprocess):
+    """
+    When using ddtrace-run and a custom logger with debug mode enabled,
+                the ddtrace logger does not override any custom logs settings and
+                child loggers continue to inherit from the parent.
+                Debug logs from the python tracer are logged.
+    """
+    env = os.environ.copy()
+    env["DD_TRACE_DEBUG"] = "true"
+    code = """
+import logging
+custom_logger = logging.getLogger('custom')
+custom_logger.setLevel(logging.WARN)
+
+assert custom_logger.parent.name == 'root'
+assert custom_logger.level == logging.WARN
+
+child_logger = logging.getLogger('ddtrace.child')
+assert child_logger.parent.name == 'ddtrace'
+assert child_logger.parent.getEffectiveLevel() == child_logger.getEffectiveLevel()
+assert child_logger.getEffectiveLevel() == logging.DEBUG
+assert child_logger.handlers == []
+
+"""
+
+    out, err, status, pid = ddtrace_run_python_code_in_subprocess(code, env=env)
+    assert status == 0, err
+    assert str(err).find("DEBUG [ddtrace] [logger.py") != -1
+
+
+def test_unrelated_logger_in_no_debug_with_ddtrace_run(ddtrace_run_python_code_in_subprocess):
+    """
+    When using ddtrace-run and a custom logger without debug mode enabled,
+                the ddtrace logger does not override any custom logs settings and
+                child loggers continue to inherit from the parent.
+                No debug logs from the tracer are created.
+                The custom logger logs continue to be emitted.
+    """
+    env = os.environ.copy()
+    env["DD_TRACE_DEBUG"] = "false"
+    code = """
+import logging
+import ddtrace
+custom_logger = logging.getLogger('custom')
+stream_handler = logging.StreamHandler()
+custom_logger.addHandler(stream_handler)
+custom_logger.setLevel(logging.INFO)
+
+assert custom_logger.parent.name == 'root'
+assert custom_logger.level == logging.INFO
+
+child_logger = logging.getLogger('ddtrace.child')
+assert child_logger.parent.name == 'ddtrace'
+assert child_logger.parent.getEffectiveLevel() == child_logger.getEffectiveLevel()
+assert child_logger.getEffectiveLevel() == logging.WARN
+assert child_logger.handlers == []
+
+s = ddtrace.tracer.trace("custom_span", resource="resource_name")
+s.set_tag(None)
+s.finish()
+
+custom_logger.info('custom info log')
+custom_logger.debug('custom debug log')
+custom_logger.warn('custom warn log')
+
+"""
+
+    out, err, status, pid = ddtrace_run_python_code_in_subprocess(code, env=env)
+    assert status == 0, err
+    assert str(err).find("WARNING [ddtrace.span]") != -1
+    assert str(err).find("DEBUG [ddtrace] [logger.py") == -1
+    assert str(err).find("custom debug log") == -1
+    assert str(err).find("custom warn log") != -1
+    assert str(err).find("custom info log") != -1
