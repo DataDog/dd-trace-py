ef919b82225625180d79895e4855008c0f57b258
Munir Abdinur <munir.abdinur@datadoghq.com>
2022-03-23 11:38:40 -0400

    find a better home for telemetry

--- ddtrace/__init__.py
+++ ddtrace/__init__.py
@@ -1,5 +1,9 @@
+import os
+
 from ._monkey import patch  # noqa: E402
 from ._monkey import patch_all
+from .internal.utils.formats import asbool  # noqa
+from .internal.telemetry import telemetry_writer
 from .pin import Pin  # noqa: E402
 from .settings import _config as config  # noqa: E402
 from .span import Span  # noqa: E402
@@ -11,6 +15,11 @@ __version__ = get_version()
 
 # a global tracer instance with integration settings
 tracer = Tracer()
+
+# instrumentation telemetry writer should be enabled/started after the global tracer and configs
+# are initialized
+if asbool(os.getenv("DD_INSTRUMENTATION_TELEMETRY_ENABLED")):
+    telemetry_writer.enable()
 
 __all__ = [
     "patch",
