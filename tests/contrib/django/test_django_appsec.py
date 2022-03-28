aded0365bac31a615116e1f61753487b8e9e656a
Vladimir de Turckheim <vlad2t@hotmail.com>
2022-03-28 16:41:18 +0200

    fix appsec update

--- tests/contrib/django/test_django_appsec.py
+++ tests/contrib/django/test_django_appsec.py
@@ -2,7 +2,9 @@ import json
 
 
 def test_django_simple_attack(client, test_spans, tracer):
-    tracer._initialize_span_processors(appsec_enabled=True)
+    tracer._appsec_enabled = True
+    # Hack: need to pass an argument to configure so that the processors are recreated
+    tracer.configure(api_version="v0.4")
     assert client.get("/.git?q=1").status_code == 404
     root_span = test_spans.spans[0]
     assert "triggers" in json.loads(root_span.get_tag("_dd.appsec.json"))
