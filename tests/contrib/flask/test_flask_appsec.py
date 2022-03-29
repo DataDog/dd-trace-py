172102f6d7b3b2927150d3a55674d878389a5f24
Vladimir de Turckheim <vlad2t@hotmail.com>
2022-03-29 09:16:08 +0200

    fix appsec update

--- tests/contrib/flask/test_flask_appsec.py
+++ tests/contrib/flask/test_flask_appsec.py
@@ -5,7 +5,9 @@ from tests.contrib.flask import BaseFlaskTestCase
 
 class FlaskAppSecTestCase(BaseFlaskTestCase):
     def test_flask_simple_attack(self):
-        self.tracer._initialize_span_processors(appsec_enabled=True)
+        self.tracer._appsec_enabled = True
+        # Hack: need to pass an argument to configure so that the processors are recreated
+        self.tracer.configure(api_version="v0.4")
         resp = self.client.get("/.git?q=1")
         self.assertEqual(resp.status_code, 404)
         spans = self.pop_spans()
