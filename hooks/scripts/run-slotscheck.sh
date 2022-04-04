67b79b1406fa7f5ae2542ea4567e1786c56009b6
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2022-04-04 12:27:38 +0100

    chore: run slotscheck only on staged files
    Reduce the check run time by only scanning staged files

--- hooks/scripts/run-slotscheck.sh
+++ hooks/scripts/run-slotscheck.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-riot -v run slotscheck
+staged_files=$(git diff --staged --name-only HEAD --diff-filter=ACMR | grep -E '\.py$')
+if [ -n "$staged_files" ]; then
+    riot -v run -s slotscheck $staged_files
+else
+    echo 'Run slotscheck skipped: No Python files were found in git diff --staged'
+fi
