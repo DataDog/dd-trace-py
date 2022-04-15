DataDog/dd-trace-py

commit 6b36e67e28189197b8c66ef01038105839dfe036

perf(tracer): reuse HTTP connections for requests (#2999)
* Use http keep alive for agent connections

* Move import; connection logic to stopwatch

* Add feature flag

* Add testcase

* Update tests/tracer/test_writer.py

Co-authored-by: Tahir H. Butt <tahir.butt@datadoghq.com>

Co-authored-by: Brett Langdon <brett.langdon@datadoghq.com>
Co-authored-by: Tahir H. Butt <tahir.butt@datadoghq.com>
Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

ddtrace/internal/writer.py
@@ -5,6 +5,7 @@ from json import loads
 import logging
 import os
 import sys
+from typing import Dict
 from typing import List
 from typing import Optional
 from typing import TYPE_CHECKING
@@ -39,6 +40,8 @@ from .sma import SimpleMovingAverage
 if TYPE_CHECKING:
     from ddtrace import Span
 
+    from .agent import ConnectionType
+
 
 log = get_logger(__name__)
 
@@ -53,6 +56,7 @@ DEFAULT_SMA_WINDOW = 10
 DEFAULT_BUFFER_SIZE = 8 << 20  # 8 MB
 DEFAULT_MAX_PAYLOAD_SIZE = 8 << 20  # 8 MB
 DEFAULT_PROCESSING_INTERVAL = 1.0
+DEFAULT_REUSE_CONNECTIONS = True
 
 
 def get_writer_buffer_size():
@@ -68,6 +72,11 @@ def get_writer_max_payload_size():
 def get_writer_interval_seconds():
     # type: () -> float
     return float(os.getenv("DD_TRACE_WRITER_INTERVAL_SECONDS", default=DEFAULT_PROCESSING_INTERVAL))
+
+
+def get_writer_reuse_connections():
+    # type: () -> bool
+    return asbool(os.getenv("DD_TRACE_WRITER_REUSE_CONNECTIONS", DEFAULT_REUSE_CONNECTIONS))
 
 
 def _human_size(nbytes):
@@ -241,6 +250,7 @@ class AgentWriter(periodic.PeriodicService, TraceWriter):
         report_metrics=False,  # type: bool
         sync_mode=False,  # type: bool
         api_version=None,  # type: Optional[str]
+        reuse_connections=None,  # type: Optional[bool]
     ):
         # type: (...) -> None
         # Pre-conditions:
@@ -296,6 +306,7 @@ class AgentWriter(periodic.PeriodicService, TraceWriter):
         self._metrics_reset()
         self._drop_sma = SimpleMovingAverage(DEFAULT_SMA_WINDOW)
         self._sync_mode = sync_mode
+        self._conn = None  # type: Optional[ConnectionType]
         self._retry_upload = tenacity.Retrying(
             # Retry RETRY_ATTEMPTS times within the first half of the processing
             # interval, using a Fibonacci policy with jitter
@@ -306,6 +317,7 @@ class AgentWriter(periodic.PeriodicService, TraceWriter):
             retry=tenacity.retry_if_exception_type((compat.httplib.HTTPException, OSError, IOError)),
         )
         self._log_error_payloads = asbool(os.environ.get("_DD_TRACE_WRITER_LOG_ERROR_PAYLOADS", False))
+        self._reuse_connections = get_writer_reuse_connections() if reuse_connections is None else reuse_connections
 
     @property
     def _agent_endpoint(self):
@@ -358,13 +370,21 @@ class AgentWriter(periodic.PeriodicService, TraceWriter):
             api_version=self._api_version,
         )
 
-    def _put(self, data, headers):
-        conn = get_connection(self.agent_url, self._timeout)
+    def _reset_connection(self):
+        # type: () -> None
+        if self._conn:
+            self._conn.close()
+            self._conn = None
 
+    def _put(self, data, headers):
+        # type: (bytes, Dict[str, str]) -> Response
         with StopWatch() as sw:
+            if self._conn is None:
+                log.debug("creating new agent connection to %s with timeout %d", self.agent_url, self._timeout)
+                self._conn = get_connection(self.agent_url, self._timeout)
             try:
-                conn.request("PUT", self._endpoint, data, headers)
-                resp = compat.get_connection_response(conn)
+                self._conn.request("PUT", self._endpoint, data, headers)
+                resp = compat.get_connection_response(self._conn)
                 t = sw.elapsed()
                 if t >= self.interval:
                     log_level = logging.WARNING
@@ -372,8 +392,12 @@ class AgentWriter(periodic.PeriodicService, TraceWriter):
                     log_level = logging.DEBUG
                 log.log(log_level, "sent %s in %.5fs to %s", _human_size(len(data)), t, self._agent_endpoint)
                 return Response.from_http_response(resp)
+            except Exception:
+                self._reset_connection()
+                raise
             finally:
-                conn.close()
+                if self._conn and not self._reuse_connections:
+                    self._reset_connection()
 
     def _downgrade(self, payload, response):
         if self._endpoint == "v0.5/traces":
@@ -552,4 +576,8 @@ class AgentWriter(periodic.PeriodicService, TraceWriter):
         super(AgentWriter, self)._stop_service()
         self.join(timeout=timeout)
 
-    on_shutdown = periodic
+    def on_shutdown(self):
+        try:
+            self.periodic()
+        finally:
+            self._reset_connection()

releasenotes/notes/keep-alive-b5ec5febb435daad.yaml
@@ -1,0 +1,4 @@
+---
+features:
+  - |
+    HTTP connections for trace payloads to the agent will be reused when possible.

tests/tracer/test_writer.py
@@ -550,3 +550,34 @@ def test_writer_recreate_api_version(init_api_version, api_version, endpoint, en
     assert writer._api_version == api_version
     assert writer._endpoint == endpoint
     assert isinstance(writer._encoder, encoder_cls)
+
+
+def test_writer_reuse_connections_envvar(monkeypatch):
+    monkeypatch.setenv("DD_TRACE_WRITER_REUSE_CONNECTIONS", "false")
+    writer = AgentWriter(agent_url="http://localhost:9126")
+    assert not writer._reuse_connections
+
+    monkeypatch.setenv("DD_TRACE_WRITER_REUSE_CONNECTIONS", "true")
+    writer = AgentWriter(agent_url="http://localhost:9126")
+    assert writer._reuse_connections
+
+
+def test_writer_reuse_connections():
+    # Ensure connection is not reused
+    writer = AgentWriter(agent_url="http://localhost:9126", reuse_connections=True)
+    # Do an initial flush to get a connection
+    writer.flush_queue()
+    assert writer._conn is None
+    writer.flush_queue()
+    assert writer._conn is None
+
+
+def test_writer_reuse_connections_false():
+    # Ensure connection is reused
+    writer = AgentWriter(agent_url="http://localhost:9126", reuse_connections=False)
+    # Do an initial flush to get a connection
+    writer.flush_queue()
+    conn = writer._conn
+    # And another to potentially have it reset
+    writer.flush_queue()
+    assert writer._conn is conn

commit 78959eeeb2c256a83e42ee8229c3c43147cb7204

fix(tracer/writer): lock the connection
There existed a critical section with the connection object on writer as
the `periodic` logic of the writer could be invoked from the periodic
writer thread as well as from other threads calling `periodic`
(`tracer.flush()` or `tracer.shutdown()`). The critical section would
result in corrupted http connections. This bug was introduced in #2999.

The solution is to lock the connection when used. I don't suspect that
this would lead to any performance degradation for the vast majority of
users since there is little overhead added to the writer. A degradation
would occur if contending threads were calling `periodic()` as now the
connection is a shared resource.

ddtrace/internal/writer.py
@@ -5,6 +5,7 @@ from json import loads
 import logging
 import os
 import sys
+import threading
 from typing import Dict
 from typing import List
 from typing import Optional
@@ -310,6 +311,7 @@ class AgentWriter(periodic.PeriodicService, TraceWriter):
         self._drop_sma = SimpleMovingAverage(DEFAULT_SMA_WINDOW)
         self._sync_mode = sync_mode
         self._conn = None  # type: Optional[ConnectionType]
+        self._conn_lck = threading.RLock()  # type: threading.RLock
         self._retry_upload = tenacity.Retrying(
             # Retry RETRY_ATTEMPTS times within the first half of the processing
             # interval, using a Fibonacci policy with jitter
@@ -375,13 +377,16 @@ class AgentWriter(periodic.PeriodicService, TraceWriter):
 
     def _reset_connection(self):
         # type: () -> None
-        if self._conn:
-            self._conn.close()
-            self._conn = None
+        with self._conn_lck:
+            if self._conn:
+                self._conn.close()
+                self._conn = None
 
     def _put(self, data, headers):
         # type: (bytes, Dict[str, str]) -> Response
-        with StopWatch() as sw:
+        sw = StopWatch()
+        sw.start()
+        with self._conn_lck:
             if self._conn is None:
                 log.debug("creating new agent connection to %s with timeout %d", self.agent_url, self._timeout)
                 self._conn = get_connection(self.agent_url, self._timeout)
@@ -394,12 +399,15 @@ class AgentWriter(periodic.PeriodicService, TraceWriter):
                 else:
                     log_level = logging.DEBUG
                 log.log(log_level, "sent %s in %.5fs to %s", _human_size(len(data)), t, self._agent_endpoint)
-                return Response.from_http_response(resp)
             except Exception:
+                # Always reset the connection when an exception occurs
                 self._reset_connection()
                 raise
+            else:
+                return Response.from_http_response(resp)
             finally:
-                if self._conn and not self._reuse_connections:
+                # Reset the connection if reusing connections is disabled.
+                if not self._reuse_connections:
                     self._reset_connection()
 
     def _downgrade(self, payload, response):