898678b223a41ef2c3f37a16a8dda0621e39cd16
Munir Abdinur <munir.abdinur@datadoghq.com>
2022-04-18 10:19:13 -0400

    feat: add runtime metrics benchmark

--- benchmarks/startup/scenario.py
+++ benchmarks/startup/scenario.py
@@ -7,18 +7,19 @@ import bm
 
 class Startup(bm.Scenario):
     telemetry_enabled = bm.var_bool()
+    runtime_metrics_enabled = bm.var_bool()
     use_ddtrace_run = bm.var_bool()
     with_trace = bm.var_bool()
 
     def run(self):
-        code = "from ddtrace import tracer"
+        code = "import ddtrace"
         if self.with_trace:
             # queue and encode trace but don't send to the agent
             # requests to the agent will raise an exception in a benchmark test
             code += """
 import mock
-tracer._writer._send_payload = mock.Mock()
-with tracer.trace('test-x', service='bench-test'):
+ddtrace.tracer._writer._send_payload = mock.Mock()
+with ddtrace.tracer.trace('test-x', service='bench-test'):
     pass
 """
 
@@ -28,6 +29,7 @@ with tracer.trace('test-x', service='bench-test'):
 
         env = os.environ.copy()
         env["DD_INSTRUMENTATION_TELEMETRY_ENABLED"] = str(self.telemetry_enabled)
+        env["DD_RUNTIME_METRICS_ENABLED"] = str(self.runtime_metrics_enabled)
 
         def _(loops):
             for _ in range(loops):
