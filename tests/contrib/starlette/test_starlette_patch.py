e6f3ed70074d186110bf7baad929c2775b9773fc
ZStriker19 <zach.groves@datadoghq.com>
2022-04-25 16:06:14 -0700

    correct starlette patch test

--- tests/contrib/starlette/test_starlette_patch.py
+++ tests/contrib/starlette/test_starlette_patch.py
@@ -1,4 +1,4 @@
-from ddtrace.contrib.starlette import patch
+from ddtrace.contrib.starlette import patch, unpatch
 from tests.contrib.patch import PatchTestCase
 
 
@@ -6,13 +6,13 @@ class TestStarlettePatch(PatchTestCase.Base):
     __integration_name__ = "starlette"
     __module_name__ = "starlette"
     __patch_func__ = patch
-    __unpatch_func__ = None
+    __unpatch_func__ = unpatch
 
     def assert_module_patched(self, starlette):
         self.assert_wrapped(starlette.applications.Starlette.__init__)
 
     def assert_not_module_patched(self, starlette):
-        self.assert_wrapped(starlette.applications.Starlette.__init__)
+        self.assert_not_wrapped(starlette.applications.Starlette.__init__)
 
     def assert_not_module_double_patched(self, starlette):
-        self.assert_wrapped(starlette.applications.Starlette.__init__)
+        self.assert_not_double_wrapped(starlette.applications.Starlette.__init__)
