13700af2a14db09f735719dc29d9de61009b24f1
Vladimir de Turckheim <vlad2t@hotmail.com>
2022-04-25 10:58:51 +0200

    add more querystrings tests

--- tests/contrib/django/test_django_appsec.py
+++ tests/contrib/django/test_django_appsec.py
@@ -13,3 +13,19 @@ def test_django_simple_attack(client, test_spans, tracer):
     assert _context.get_item("http.request.uri", span=root_span) == "http://testserver/.git?q=1"
     assert _context.get_item("http.request.headers", span=root_span) is not None
     assert _context.get_item("http.request.query", span=root_span)["q"] == "1"
+
+
+def test_django_querystrings(client, test_spans, tracer):
+    tracer._appsec_enabled = True
+    # Hack: need to pass an argument to configure so that the processors are recreated
+    tracer.configure(api_version="v0.4")
+    client.get("/?a=1&b&c=d")
+    root_span = test_spans.spans[0]
+    assert _context.get_item("http.request.query", span=root_span)["a"] == "1"
+    assert _context.get_item("http.request.query", span=root_span)["b"] == ""
+    assert _context.get_item("http.request.query", span=root_span)["c"] == "d"
+    assert "d" not in _context.get_item("http.request.query", span=root_span)
+    self.client.get("/")
+    spans = self.pop_spans()
+    root_span = spans[0]
+    assert len(_context.get_item("http.request.query", span=root_span)) == 0
--- tests/contrib/flask/test_flask_appsec.py
+++ tests/contrib/flask/test_flask_appsec.py
@@ -17,3 +17,19 @@ class FlaskAppSecTestCase(BaseFlaskTestCase):
         assert "triggers" in json.loads(root_span.get_tag("_dd.appsec.json"))
         assert _context.get_item("http.request.uri", span=root_span) == "http://localhost/.git?q=1"
         assert _context.get_item("http.request.query", span=root_span)["q"] == "1"
+
+    def test_flask_querystrings(self):
+        self.tracer._appsec_enabled = True
+        # Hack: need to pass an argument to configure so that the processors are recreated
+        self.tracer.configure(api_version="v0.4")
+        self.client.get("/?a=1&b&c=d")
+        spans = self.pop_spans()
+        root_span = spans[0]
+        assert _context.get_item("http.request.query", span=root_span)["a"] == "1"
+        assert _context.get_item("http.request.query", span=root_span)["b"] == ""
+        assert _context.get_item("http.request.query", span=root_span)["c"] == "d"
+        assert "d" not in _context.get_item("http.request.query", span=root_span)
+        self.client.get("/")
+        spans = self.pop_spans()
+        root_span = spans[0]
+        assert len(_context.get_item("http.request.query", span=root_span)) == 0
