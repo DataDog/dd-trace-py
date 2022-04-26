a834f7577075d77a10a84d141578b0fa7dc48acf
Vladimir de Turckheim <vlad2t@hotmail.com>
2022-04-26 09:13:31 +0200

    update tests

--- tests/contrib/flask/test_flask_appsec.py
+++ tests/contrib/flask/test_flask_appsec.py
@@ -16,7 +16,7 @@ class FlaskAppSecTestCase(BaseFlaskTestCase):
 
         assert "triggers" in json.loads(root_span.get_tag("_dd.appsec.json"))
         assert _context.get_item("http.request.uri", span=root_span) == "http://localhost/.git?q=1"
-        assert _context.get_item("http.request.query", span=root_span)["q"] == {"q": "1"}
+        assert dict(_context.get_item("http.request.query", span=root_span)) == {"q": "1"}
 
     def test_flask_querystrings(self):
         self.tracer._appsec_enabled = True
@@ -25,10 +25,7 @@ class FlaskAppSecTestCase(BaseFlaskTestCase):
         self.client.get("/?a=1&b&c=d")
         spans = self.pop_spans()
         root_span = spans[0]
-        assert _context.get_item("http.request.query", span=root_span)["a"] == "1"
-        assert _context.get_item("http.request.query", span=root_span)["b"] == ""
-        assert _context.get_item("http.request.query", span=root_span)["c"] == "d"
-        assert "d" not in _context.get_item("http.request.query", span=root_span)
+        assert dict(_context.get_item("http.request.query", span=root_span)) == {"a": "1", "b": "", "c": "d"}
         self.client.get("/")
         spans = self.pop_spans()
         root_span = spans[0]
