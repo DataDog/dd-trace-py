d5879f8a06e25592a84167299a639ae7d566b40d
Munir Abdinur <munir.abdinur@datadoghq.com>
2022-04-27 14:47:16 -0400

    Apply suggestions from code review
    Co-authored-by: Tahir H. Butt <tahir.butt@datadoghq.com>

--- benchmarks/ddtrace_run/scenario.py
+++ benchmarks/ddtrace_run/scenario.py
@@ -6,28 +6,28 @@ import bm
 
 
 class DDtraceRun(bm.Scenario):
-    with_ddtrace_run = bm.var_bool()
-    runtime_metrics_enabled = bm.var_bool()
-    profiling_enabled = bm.var_bool()
-    appsec_enabled = bm.var_bool()
-    with_httpretty = bm.var_bool()
-    telemetry_enabled = bm.var_bool()
-    with_trace = bm.var_bool()
+    ddtrace_run = bm.var_bool()
+    http = bm.var_bool()
+    runtimemetrics = bm.var_bool()
+    telemetry = bm.var_bool()
+    profiling = bm.var_bool()
+    appsec = bm.var_bool()
+    tracing = bm.var_bool()
 
     def run(self):
         # setup subprocess environment variables
         env = os.environ.copy()
-        env["DD_RUNTIME_METRICS_ENABLED"] = str(self.runtime_metrics_enabled)
-        env["DD_APPSEC_ENABLED"] = str(self.appsec_enabled)
+        env["DD_RUNTIME_METRICS_ENABLED"] = str(self.runtimemetrics)
+        env["DD_APPSEC_ENABLED"] = str(self.appsec)
 
         # initialize subprocess args
         subp_cmd = []
         code = "import ddtrace; ddtrace.patch_all()\n"
-        if self.with_ddtrace_run:
+        if self.ddtrace_run:
             subp_cmd = ["ddtrace-run"]
             code = ""
 
-        if self.with_httpretty:
+        if self.http:
             # mock requests to the trace agent
             env["DD_TRACE_API_VERSION"] = "v0.4"
             code += """
@@ -38,16 +38,17 @@ from ddtrace.internal.telemetry import telemetry_writer
 httpretty.enable(allow_net_connect=False)
 httpretty.register_uri(httpretty.PUT, '%s/%s' % (tracer.agent_trace_url, 'v0.4/traces'))
 httpretty.register_uri(httpretty.POST, '%s/%s' % (tracer.agent_trace_url, telemetry_writer.ENDPOINT))
+# profiler will collect snapshot during shutdown
 httpretty.register_uri(httpretty.POST, '%s/%s' % (tracer.agent_trace_url, 'profiling/v1/input'))
 """
 
-        if self.telemetry_enabled:
+        if self.telemetry:
             code += "telemetry_writer.enable()\n"
 
-        if self.with_trace:
+        if self.tracing:
             code += "span = tracer.trace('test-x', service='bench-test'); span.finish()\n"
 
-        if self.profiling_enabled:
+        if self.profiling:
             code += "import ddtrace.profiling.auto\n"
 
         # stage code for execution in a subprocess
