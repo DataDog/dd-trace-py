92af0aa983c9473ea22f0b7015e5b9b5d2d8059c
Kyle Verhoog <kyle@verhoog.ca>
2022-05-06 13:19:42 -0400

    perf(tracer): don't call configure if no options

--- ddtrace/bootstrap/sitecustomize.py
+++ ddtrace/bootstrap/sitecustomize.py
@@ -107,7 +107,8 @@ try:
     if priority_sampling:
         opts["priority_sampling"] = asbool(priority_sampling)
 
-    tracer.configure(**opts)
+    if not opts:
+        tracer.configure(**opts)
 
     if trace_enabled:
         update_patched_modules()
