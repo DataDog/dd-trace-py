a358014866315800c870765cadad3dcef658b0b0
ZStriker19 <zach.groves@datadoghq.com>
2022-05-20 16:22:28 +0200

    Add fastapi and starlette patching tests

--- tests/contrib/fastapi/test_fastapi_patch.py
+++ tests/contrib/fastapi/test_fastapi_patch.py
@@ -1,8 +1,11 @@
+import starlette
+
 from ddtrace.contrib.fastapi import patch
 from ddtrace.contrib.fastapi import unpatch
 from tests.contrib.patch import PatchTestCase
 
 
+# Patching FastAPI also patches Starlette, so we test the Starlette methods as well
 class TestFastapiPatch(PatchTestCase.Base):
     __integration_name__ = "fastapi"
     __module_name__ = "fastapi"
@@ -13,10 +16,22 @@ class TestFastapiPatch(PatchTestCase.Base):
         self.assert_wrapped(fastapi.applications.FastAPI.__init__)
         self.assert_wrapped(fastapi.routing.serialize_response)
 
+        self.assert_wrapped(starlette.applications.Starlette.__init__)
+        self.assert_wrapped(starlette.routing.Mount.handle)
+        self.assert_wrapped(starlette.routing.Route.handle)
+
     def assert_not_module_patched(self, fastapi):
         self.assert_not_wrapped(fastapi.applications.FastAPI.__init__)
         self.assert_not_wrapped(fastapi.routing.serialize_response)
 
+        self.assert_not_wrapped(starlette.applications.Starlette.__init__)
+        self.assert_not_wrapped(starlette.routing.Mount.handle)
+        self.assert_not_wrapped(starlette.routing.Route.handle)
+
     def assert_not_module_double_patched(self, fastapi):
         self.assert_not_double_wrapped(fastapi.applications.FastAPI.__init__)
         self.assert_not_double_wrapped(fastapi.routing.serialize_response)
+
+        self.assert_not_double_wrapped(starlette.applications.Starlette.__init__)
+        self.assert_not_double_wrapped(starlette.routing.Mount.handle)
+        self.assert_not_double_wrapped(starlette.routing.Route.handle)
