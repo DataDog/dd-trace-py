641b80c74530daea13ea4b02d9f073d188cb1504
Munir Abdinur <munir.abdinur@datadoghq.com>
2022-05-24 15:17:32 -0400

    fix(telemetry): only send app-start and app-closed events in a parent process
    application start and application closed events should be generated for the "main" process (the process that initializes the TelemetryWriter). These events will not be generated when a process forks.
    
    Co-authored-by: Kyle Verhoog <kyle@verhoog.ca>

--- tests/tracer/telemetry/test_writer.py
+++ tests/tracer/telemetry/test_writer.py
@@ -1,3 +1,5 @@
+import os
+
 import httpretty
 import mock
 import pkg_resources
@@ -79,7 +81,7 @@ def test_add_event_disabled_writer(mock_send_request, telemetry_writer_disabled)
     assert len(httpretty.latest_requests()) == 0
 
 
-def test_add_app_started_event(mock_time, mock_send_request, telemetry_writer):
+def test_app_started_event(mock_time, mock_send_request, telemetry_writer):
     """asserts that app_started_event() queues a valid telemetry request which is then sent by periodic()"""
     # queue integrations
     telemetry_writer.add_integration("integration-t", True)
@@ -120,7 +122,20 @@ def test_add_app_started_event(mock_time, mock_send_request, telemetry_writer):
     assert httpretty.last_request().parsed_body == _get_request_body(payload, "app-started")
 
 
-def test_add_app_closing_event(mock_time, mock_send_request, telemetry_writer):
+def test_app_started_event_on_fork(telemetry_writer):
+    """asserts that app_started_event() is not sent after a fork"""
+    if os.fork() == 0:
+        # queue an app started event
+        telemetry_writer.app_started_event()
+        # send all queued events to the agent
+        telemetry_writer.periodic()
+        # ensure an app_started event was not sent
+        assert len(httpretty.latest_requests()) == 0
+        # Kill the process so it doesn't continue running the rest of the test suite
+        os._exit(0)
+
+
+def test_app_closing_event(mock_time, mock_send_request, telemetry_writer):
     """asserts that on_shutdown() queues and sends an app-closing telemetry request"""
     # send app closed event
     telemetry_writer.on_shutdown()
@@ -133,6 +148,17 @@ def test_add_app_closing_event(mock_time, mock_send_request, telemetry_writer):
     # ensure a valid request body was sent
     assert len(httpretty.latest_requests()) == 1
     assert httpretty.last_request().parsed_body == _get_request_body({}, "app-closing")
+
+
+def test_app_closing_on_fork(telemetry_writer):
+    """asserts that on_shutdown() does not send an app-closing telemetry request after a fork"""
+    if os.fork() == 0:
+        # send app closed event
+        telemetry_writer.on_shutdown()
+        # assert that no requests were sent
+        assert len(httpretty.latest_requests()) == 0
+        # Kill the process so it doesn't continue running the rest of the test suite
+        os._exit(0)
 
 
 def test_add_integration(mock_time, mock_send_request, telemetry_writer):
@@ -246,7 +272,8 @@ def test_send_request_exception():
 def test_telemetry_graceful_shutdown(mock_time, mock_send_request, telemetry_writer):
     telemetry_writer.start()
     telemetry_writer.stop()
-    assert httpretty.last_request().parsed_body == _get_request_body({}, "app-closing")
+    assert len(httpretty.latest_requests()) == 2
+    assert httpretty.last_request().parsed_body == _get_request_body({}, "app-closing", 2)
 
 
 def _get_request_body(payload, payload_type, seq_id=1):
