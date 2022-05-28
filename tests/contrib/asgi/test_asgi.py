7f1746ecdb9543950a734cf43553e2f097810137
ZStriker19 <zach.groves@datadoghq.com>
2022-05-28 12:43:34 +0200

    make multi-request span test case more realistic

--- tests/contrib/asgi/test_asgi.py
+++ tests/contrib/asgi/test_asgi.py
@@ -7,7 +7,6 @@ from asgiref.testing import ApplicationCommunicator
 import httpx
 import pytest
 
-from ddtrace import Span
 from ddtrace.contrib.asgi import TraceMiddleware
 from ddtrace.contrib.asgi import span_from_scope
 from ddtrace.propagation import http as http_propagation
@@ -391,18 +390,29 @@ async def test_get_asgi_span(tracer, test_spans):
     async def test_app_that_generates_multiple_request_spans(scope, receive, send):
         message = await receive()
         if message.get("type") == "http.request":
-            # Put an extra span in the request_spans dict to make sure that span_from_scope() can handle
-            span = Span("extra.span")
-            scope["datadog"]["request_spans"].append(span)
             assert len(scope["datadog"]["request_spans"]) == 2
             asgi_span = span_from_scope(scope)
             assert asgi_span is not None
             assert asgi_span.name == "asgi.request"
             assert asgi_span == scope["datadog"]["request_spans"][0]
+            assert asgi_span.resource == "span 1 resource"
+            assert scope["datadog"]["request_spans"][1].resource == "span 2 resource"
             await send({"type": "http.response.start", "status": 200, "headers": [[b"Content-Type", b"text/plain"]]})
             await send({"type": "http.response.body", "body": b""})
 
-    app = TraceMiddleware(test_app_that_generates_multiple_request_spans, tracer=tracer)
+    def span1_modifier(span, scope):
+        span.resource = "span 1 resource"
+
+    def span2_modifier(span, scope):
+        span.resource = "span 2 resource"
+
+    # We double wrap this app so that it generates multiple request spans per hit
+    app = TraceMiddleware(
+        TraceMiddleware(test_app_that_generates_multiple_request_spans, tracer=tracer, span_modifier=span2_modifier),
+        tracer=tracer,
+        span_modifier=span1_modifier,
+    )
+
     async with httpx.AsyncClient(app=app) as client:
         response = await client.get("http://testserver/")
         assert response.status_code == 200
