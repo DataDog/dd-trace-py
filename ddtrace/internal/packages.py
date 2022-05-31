a9a4fd9c6a03699f1a352e721ba22ba4701537a7
Munir Abdinur <munir.abdinur@datadoghq.com>
2022-05-31 13:54:40 -0400

    test: parse name and version then stop reading file

--- ddtrace/internal/packages.py
+++ ddtrace/internal/packages.py
@@ -6,6 +6,28 @@ Distribution = typing.NamedTuple("Distribution", [("name", str), ("version", str
 _DISTRIBUTIONS = None
 
 
+def _get_dist_metadata(dist):
+    for fname in ("METADATA", "PKG-INFO"):
+        file_path = dist._path / fname
+        if not file_path.exists():
+            continue
+
+        name = None
+        version = None
+        with file_path.open() as fp:
+            for line in fp:
+                if line.startswith("Name:"):
+                    _, _, name = line.partition(":")
+                    name = name.strip()
+                elif line.startswith("Version:"):
+                    _, _, version = line.partition(":")
+                    version = version.strip()
+
+                if name and version:
+                    return name, version
+    return None, None
+
+
 def get_distributions():
     # type: () -> typing.List[Distribution]
     global _DISTRIBUTIONS
@@ -20,9 +42,7 @@ def get_distributions():
     pkgs = []
     pkg_names = set()
     for dist in importlib_metadata.distributions():
-        metadata = dist.metadata
-        name = metadata["name"]
-        version = metadata["version"]
+        name, version = _get_dist_metadata(dist)
         if name and version and name not in pkg_names:
             pkgs.append(Distribution(name, version))
             # Avoids adding duplicates
