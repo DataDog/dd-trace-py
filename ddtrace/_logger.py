8215fbbf943a25bbd0c6f1e7f6cbf38e546fedbf
wan <wan.tsui@datadoghq.com>
2022-06-02 14:07:20 -0400

    Add new environment variable to control log level setting when logs are written.

--- ddtrace/_logger.py
+++ ddtrace/_logger.py
@@ -7,16 +7,19 @@ from ddtrace.internal.utils.formats import asbool
 
 DEFAULT_FILE_SIZE_BYTES = 15 << 20  # 15 MB
 
-
 def configure_ddtrace_logger():
     # type: () -> None
     """Configures ddtrace log levels and file paths.
-    By default, when debug hasn't been enabled and a file hasn't been specified:
-        - Inherit from the root logger in the logging module
 
-    In debug mode:
-        - Propagate logs up so logs show up in the application logs when a file path wasn't provided
-        - Logs are routed to a file when DD_TRACE_LOG_FILE is specified
+    Customization is possible with the environment variables:
+        ``DD_TRACE_DEBUG``, ``DD_TRACE_LOG_FILE_LEVEL``, and ``DD_TRACE_LOG_FILE``
+
+    By default, when none of the settings have been changed, ddtrace loggers
+        inherit from the root logger in the logging module and no logs are written to a file.
+
+    When DD_TRACE_DEBUG has been enabled:
+        - Propagate logs up so logs show up in the application logs if a file path wasn't provided
+        - Logs are routed to a file when DD_TRACE_LOG_FILE is specified, using the log level in DD_TRACE_LOG_FILE_LEVEL.
         - Child loggers inherit from the parent ddtrace logger
 
     Note: The ddtrace-run logs under commands/ddtrace-run do not follow DD_TRACE_LOG_FILE
@@ -24,6 +27,7 @@ def configure_ddtrace_logger():
 
     """
     debug_enabled = asbool(os.environ.get("DD_TRACE_DEBUG", "false"))
+    log_file_level = os.environ.get("DD_TRACE_LOG_FILE_LEVEL", "DEBUG")
     log_path = os.environ.get("DD_TRACE_LOG_FILE")
     max_file_bytes = int(os.environ.get("DD_TRACE_FILE_SIZE_BYTES", DEFAULT_FILE_SIZE_BYTES))
     num_backup = 1
@@ -35,16 +39,20 @@ def configure_ddtrace_logger():
     if debug_enabled is True:
         ddtrace_logger.setLevel(logging.DEBUG)
         ddtrace_logger.debug("Debug mode has been enabled for the ddtrace logger.")
+        file_log_level_value = logging.getLevelName("DEBUG")
+
+    if 'Level' not in str(logging.getLevelName(log_file_level)): # Result starts with 'Level' if no matches
+        file_log_level_value = logging.getLevelName(log_file_level)
+    else:
+        file_log_level_value = logging.getLevelName("DEBUG")
 
     if log_path is not None:
         log_path = os.path.abspath(log_path)
         ddtrace_file_handler = RotatingFileHandler(
             filename=log_path, mode="a", maxBytes=max_file_bytes, backupCount=num_backup
         )
-        if debug_enabled is False:
-            ddtrace_file_handler.setLevel(logging.WARN)
-        else:
-            ddtrace_file_handler.setLevel(logging.DEBUG)
+
+        ddtrace_file_handler.setLevel(file_log_level_value)
         ddtrace_file_handler.setFormatter(log_formatter)
         ddtrace_logger.addHandler(ddtrace_file_handler)
-        ddtrace_logger.debug("Debug mode has been enabled to route logs to %s", log_path)
+        ddtrace_logger.debug("ddtrace logs will be routed to %s", log_path)
--- tests/tracer/test_logging.py
+++ tests/tracer/test_logging.py
@@ -8,9 +8,10 @@ LOG_PATTERN = r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3} \w{1,} \[\S{1,}\] \[\
 
 
 @pytest.mark.parametrize("dd_trace_debug", ["true", "false", None])
+@pytest.mark.parametrize("dd_trace_log_file_level", ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", None])
 @pytest.mark.parametrize("dd_trace_log_file", ["example.log", None])
 def test_unrelated_logger_loaded_first_in_debug(
-    dd_trace_debug, dd_trace_log_file, run_python_code_in_subprocess, tmpdir
+    dd_trace_debug, dd_trace_log_file_level, dd_trace_log_file, run_python_code_in_subprocess, tmpdir
 ):
     """
     When the tracer is imported after logging has been configured in debug mode,
@@ -19,6 +20,9 @@ def test_unrelated_logger_loaded_first_in_debug(
     env = os.environ.copy()
     if dd_trace_debug is not None:
         env["DD_TRACE_DEBUG"] = dd_trace_debug
+
+    if dd_trace_log_file_level is not None:
+        env["DD_TRACE_LOG_FILE_LEVEL"] = dd_trace_log_file_level
 
     if dd_trace_log_file is not None:
         env["DD_TRACE_LOG_FILE"] = tmpdir + dd_trace_log_file
@@ -36,6 +40,7 @@ assert custom_logger.parent.name == 'root'
 assert custom_logger.level == logging.WARN
 
 ddtrace_logger = logging.getLogger('ddtrace')
+ddtrace_logger.critical('ddtrace critical log')
 ddtrace_logger.warning('ddtrace warning log')
 """
     out, err, status, pid = run_python_code_in_subprocess(code, env=env)
@@ -54,9 +59,10 @@ ddtrace_logger.warning('ddtrace warning log')
 
 
 @pytest.mark.parametrize("dd_trace_debug", ["true", "false", None])
+@pytest.mark.parametrize("dd_trace_log_file_level", ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", None])
 @pytest.mark.parametrize("dd_trace_log_file", ["example.log", None])
 def test_unrelated_logger_loaded_last_in_debug(
-    dd_trace_debug, dd_trace_log_file, run_python_code_in_subprocess, tmpdir
+    dd_trace_debug, dd_trace_log_file_level, dd_trace_log_file, run_python_code_in_subprocess, tmpdir
 ):
     """
     When the tracer is imported before logging has been configured in debug mode,
@@ -65,6 +71,9 @@ def test_unrelated_logger_loaded_last_in_debug(
     env = os.environ.copy()
     if dd_trace_debug is not None:
         env["DD_TRACE_DEBUG"] = dd_trace_debug
+
+    if dd_trace_log_file_level is not None:
+        env["DD_TRACE_LOG_FILE_LEVEL"] = dd_trace_log_file_level
 
     if dd_trace_log_file is not None:
         env["DD_TRACE_LOG_FILE"] = tmpdir + dd_trace_log_file
@@ -79,6 +88,7 @@ assert custom_logger.parent.name == 'root'
 assert custom_logger.level == logging.WARN
 
 ddtrace_logger = logging.getLogger('ddtrace')
+ddtrace_logger.critical('ddtrace critical log')
 ddtrace_logger.warning('ddtrace warning log')
 """
 
@@ -131,11 +141,11 @@ assert child_logger.handlers == []
         assert len(first_line) > 0
         assert re.search(LOG_PATTERN, first_line) is not None
 
-
 @pytest.mark.parametrize("dd_trace_debug", ["true", "false", None])
+@pytest.mark.parametrize("dd_trace_log_file_level", ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", None])
 @pytest.mark.parametrize("dd_trace_log_file", ["example.log", None])
 def test_unrelated_logger_in_debug_with_ddtrace_run(
-    dd_trace_debug, dd_trace_log_file, ddtrace_run_python_code_in_subprocess, tmpdir
+    dd_trace_debug, dd_trace_log_file_level, dd_trace_log_file, ddtrace_run_python_code_in_subprocess, tmpdir
 ):
     """
     When using ddtrace-run with a custom logger,
@@ -144,6 +154,9 @@ def test_unrelated_logger_in_debug_with_ddtrace_run(
     env = os.environ.copy()
     if dd_trace_debug is not None:
         env["DD_TRACE_DEBUG"] = dd_trace_debug
+
+    if dd_trace_log_file_level is not None:
+        env["DD_TRACE_LOG_FILE_LEVEL"] = dd_trace_log_file_level
 
     if dd_trace_log_file is not None:
         env["DD_TRACE_LOG_FILE"] = tmpdir + dd_trace_log_file
@@ -156,6 +169,7 @@ assert custom_logger.parent.name == 'root'
 assert custom_logger.level == logging.WARN
 
 ddtrace_logger = logging.getLogger('ddtrace')
+ddtrace_logger.critical('ddtrace critical log')
 ddtrace_logger.warning('ddtrace warning log')
 """
 
@@ -231,7 +245,8 @@ ddtrace_logger.debug('debug log')
 
 def test_warn_logs_can_go_to_file(run_python_code_in_subprocess, ddtrace_run_python_code_in_subprocess, tmpdir):
     """
-    When DD_TRACE_DEBUG is false, warn logs are emitted to the path defined in DD_TRACE_LOG_FILE.
+    When DD_TRACE_DEBUG is false and DD_TRACE_LOG_FILE_LEVEL hasn't been configured, 
+        warn logs are emitted to the path defined in DD_TRACE_LOG_FILE.
     """
     env = os.environ.copy()
     log_file = tmpdir + "/testlog.log"
@@ -285,13 +300,17 @@ ddtrace_logger.warning('warning log')
         assert re.search(LOG_PATTERN, first_line) is not None
 
 
-def test_debug_logs_streamhandler_default(run_python_code_in_subprocess, ddtrace_run_python_code_in_subprocess):
+@pytest.mark.parametrize("dd_trace_log_file_level", ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", None])
+def test_debug_logs_streamhandler_default(dd_trace_log_file_level, run_python_code_in_subprocess, ddtrace_run_python_code_in_subprocess):
     """
     When DD_TRACE_DEBUG is true, debug logs are emitted to StreamHandler
         following a configured logging.basicConfig and its defined format.
         Note: When running ddtrace-run, the ddtrace-run logs still emit to stderr.
+        DD_TRACE_LOG_FILE_LEVEL does not affect this setting.
     """
     env = os.environ.copy()
+    if dd_trace_log_file_level is not None:
+        env["DD_TRACE_LOG_FILE_LEVEL"] = dd_trace_log_file_level    
     env["DD_TRACE_DEBUG"] = "true"
     code = """
 import logging
@@ -336,15 +355,19 @@ ddtrace_logger.debug('debug log')
     assert out == b""
 
 
+@pytest.mark.parametrize("dd_trace_log_file_level", ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", None])
 def test_debug_logs_can_go_to_file_backup_count(
-    run_python_code_in_subprocess, ddtrace_run_python_code_in_subprocess, tmpdir
+    dd_trace_log_file_level, run_python_code_in_subprocess, ddtrace_run_python_code_in_subprocess, tmpdir
 ):
     """
     When DD_TRACE_DEBUG is true and DD_TRACE_LOG_FILE has been specified, debug logs are
-        written to a file at the expected backup count.
+        written to a file at the expected backup count, based on the DD_TRACE_LOG_FILE_LEVEL setting.
         Note: When running ddtrace-run, the ddtrace-run logs still emit to stderr.
     """
     env = os.environ.copy()
+    if dd_trace_log_file_level is not None:
+        env["DD_TRACE_LOG_FILE_LEVEL"] = dd_trace_log_file_level
+
     log_file = tmpdir + "/testlog.log"
     env["DD_TRACE_LOG_FILE"] = log_file
     env["DD_TRACE_DEBUG"] = "true"
@@ -364,6 +387,7 @@ ddtrace_logger = logging.getLogger('ddtrace')
 
 for attempt in range(100):
     ddtrace_logger.debug('ddtrace multiple debug log')
+    ddtrace_logger.critical('ddtrace multiple debug log')
 """
 
     out, err, status, pid = run_python_code_in_subprocess(code, env=env)
@@ -373,7 +397,6 @@ for attempt in range(100):
 
     testfiles = os.listdir(tmpdir)
     log_files = [filename for filename in testfiles if "testlog.log" in filename]
-
     assert log_files == ["testlog.log", "testlog.log.1"]
 
     with open(log_file) as file:
@@ -393,6 +416,7 @@ assert ddtrace_logger.handlers[0].backupCount == 1
 
 for attempt in range(100):
     ddtrace_logger.debug('ddtrace multiple debug log')
+    ddtrace_logger.critical('ddtrace multiple debug log')
 """
 
     out, err, status, pid = ddtrace_run_python_code_in_subprocess(code, env=env)
@@ -407,5 +431,5 @@ for attempt in range(100):
 
     with open(log_file) as file:
         content = file.read()
-        assert len(content) > 0
+        assert len(content) > 0     
         assert re.search(LOG_PATTERN, content) is not None
