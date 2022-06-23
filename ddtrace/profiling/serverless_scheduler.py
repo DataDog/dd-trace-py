80c71330afda1f6665d9f3cd83be4ef1ca31fa3a
AJ Stuyvenberg <astuyve@gmail.com>
2022-06-23 13:11:04 -0400

    feat: Fix double flushing issue by comparing last export time as well as counting number of profiled seconds

--- ddtrace/profiling/serverless_scheduler.py
+++ ddtrace/profiling/serverless_scheduler.py
@@ -16,11 +16,12 @@ class ServerlessScheduler(scheduler.Scheduler):
     _total_profiled_seconds = attr.ib(default=0)
 
     def periodic(self):
-        if self._total_profiled_seconds >= 60:
+        now = compat.time_ns()
+        if now - self._last_export >= 60 * 1e9 and self._total_profiled_seconds >= 60:
+            self._total_profiled_seconds = 0
             start_time = compat.monotonic()
             try:
                 self.flush()
-                self._total_profiled_seconds = 0
             finally:
                 self.interval = max(0, self._configured_interval - (compat.monotonic() - start_time))
         else:
