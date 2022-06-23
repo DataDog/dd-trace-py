d83c2ef4b8bbc84bae61817733018cd1e0536182
Brett Langdon <brett.langdon@datadoghq.com>
2022-06-23 07:34:15 -0400

    Update ddtrace/internal/utils/deprecations.py

--- ddtrace/internal/utils/deprecations.py
+++ ddtrace/internal/utils/deprecations.py
@@ -1,9 +1,6 @@
 class DDTraceDeprecationWarning(DeprecationWarning):
-    pass
-
-
-# Override module to simplify adding warning filters by querying for
-# ddtrace.DDTraceDeprecationWarning but not have to expose this in the
-# public API. This also allows us to avoid circular imports that would occur if
-# it was contained in the top-level ddtrace package.
-DDTraceDeprecationWarning.__module__ = "ddtrace"
+    # Override module to simplify adding warning filters by querying for
+    # ddtrace.DDTraceDeprecationWarning but not have to expose this in the
+    # public API. This also allows us to avoid circular imports that would occur if
+    # it was contained in the top-level ddtrace package.
+    __module__ = "ddtrace"
