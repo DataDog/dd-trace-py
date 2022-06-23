12c2242753968567165f716c8c41336688c77f82
Brett Langdon <brett.langdon@datadoghq.com>
2022-06-23 10:05:34 -0400

    Update ddtrace/__init__.py

--- ddtrace/__init__.py
+++ ddtrace/__init__.py
@@ -1,10 +1,6 @@
 from ._monkey import patch  # noqa: E402
-<<<<<<< HEAD
-from ._monkey import patch_all
-=======
 from ._monkey import patch_all  # noqa: E402
 from .internal.utils.deprecations import DDTraceDeprecationWarning  # noqa: E402
->>>>>>> cf87cf91 (fix(starlette/fastapi): add back removed public functions (#3850))
 from .pin import Pin  # noqa: E402
 from .settings import _config as config  # noqa: E402
 from .span import Span  # noqa: E402
