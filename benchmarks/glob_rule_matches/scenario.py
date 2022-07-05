7f7eafa9ceedd244c01c6df990711e6be891f5ad
ZStriker19 <zach.groves@datadoghq.com>
2022-07-05 11:49:24 -0700

    update bm scenario with RE comparison

--- benchmarks/glob_rule_matches/scenario.py
+++ benchmarks/glob_rule_matches/scenario.py
@@ -1,4 +1,5 @@
 import random
+import re
 import string
 
 import bm
@@ -22,8 +23,10 @@ def iter_n(items, n):
 
 
 class GlobMatchingRules(bm.Scenario):
+
     num_iterations = bm.var(type=int)
     num_sub = bm.var(type=int)
+    re_matcher = bm.var(type=bool)
 
     def run(self):
         # Generate random strings for the counts we requested
@@ -31,11 +34,24 @@ class GlobMatchingRules(bm.Scenario):
 
         # Create a single pattern to use for all matches
         # Pick a random pattern
-        rule = GlobMatcher(random.choice(subjects))
+        pattern = random.choice(subjects)
+        rule = GlobMatcher(pattern)
+
+        # Regular expression function to test against
+        def re_match(self, sub):
+            if re.match(pattern, sub):
+                return True
 
         def _(loops):
-            for _ in range(loops):
-                for subject in iter_n(subjects, n=self.num_iterations):
-                    rule.match(subject)
+            # Test the regular expression matching if that's the scenario specified
+            if self.re_matcher:
+                for _ in range(loops):
+                    for subject in iter_n(subjects, n=self.num_iterations):
+                        re_match(subject)
+            # Test the custom glob matchng if that's the scenario specified
+            else:
+                for _ in range(loops):
+                    for subject in iter_n(subjects, n=self.num_iterations):
+                        rule.match(subject)
 
         yield _
