c750eb802ed87d4a73ada0f6b007ad25f15ae885
Zachary Groves <32471391+ZStriker19@users.noreply.github.com>
2022-07-15 14:24:23 -0400

    feat(internal): add span sampling rule api (#3918)
    ## Description
    The span sampling rule api is part of the single span sampling initiative. It takes in service and name patterns, sampling_rate, and a rate limit. From there it uses those to evaluate whether or not we should sample a span if it's not already being sampled by the normal trace sampler. If we should sample it, then we give it unique tags, which the trace-agent will recognize and keep the single span with those tags.
    
    ## Motivation
    This is required in order for us to then apply the rules to the spans in a span processor.
    
    ## Design 
    Described above.
    
    ## Testing strategy
    Tested that the rule adds tags correctly. Although could use assistance if we want to tag that a certain percent of spans are tagged at a given sampling rate or something along those lines. Also tested that the rate limiter works when set to 0 (don't allow any spans through), but have not tested how exactly it reduces those spans. I assume that to some degree, since both the sampler and the rate limiter have their own tests, this may be unnecessary.  
    
    
    ## Checklist
    - [ ] Title must conform to [conventional commit](https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional).
    - [ ] Add additional sections for `feat` and `fix` pull requests.
    - [ ] Ensure tests are passing for affected code.
    - [ ] [Library documentation](https://github.com/DataDog/dd-trace-py/tree/1.x/docs) and/or [Datadog's documentation site](https://github.com/DataDog/documentation/) is updated. Link to doc PR in description.
    
    
    ## Reviewer Checklist
    - [x] Title is accurate.
    - [x] Description motivates each change.
    - [x] No unnecessary changes were introduced in this PR.
    - [x] PR cannot be broken up into smaller PRs.
    - [x] Avoid breaking [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces) changes unless absolutely necessary.
    - [x] Tests provided or description of manual testing performed is included in the code or PR.
    - [x] Release note has been added for fixes and features, or else `changelog/no-changelog` label added.
    - [x] All relevant GitHub issues are correctly linked.
    - [x] Backports are identified and tagged with Mergifyio.
    - [ ] Add to milestone.

--- ddtrace/internal/sampling.py
+++ ddtrace/internal/sampling.py
@@ -1,17 +1,29 @@
 import re
+from typing import Optional
 from typing import TYPE_CHECKING
 
+from ddtrace.constants import _SINGLE_SPAN_SAMPLING_MAX_PER_SEC
+from ddtrace.constants import _SINGLE_SPAN_SAMPLING_MECHANISM
+from ddtrace.constants import _SINGLE_SPAN_SAMPLING_RATE
+from ddtrace.internal.glob_matching import GlobMatcher
 from ddtrace.internal.logger import get_logger
+
+from .rate_limiter import RateLimiter
 
 
 log = get_logger(__name__)
 
 if TYPE_CHECKING:
     from typing import Dict
-    from typing import Optional
     from typing import Text
 
     from ddtrace.context import Context
+
+    from ..span import Span
+
+# Big prime number to make hashing better distributed
+KNUTH_FACTOR = 1111111111111111111
+MAX_SPAN_ID = 2 ** 64
 
 
 class SamplingMechanism(object):
@@ -23,6 +35,7 @@ class SamplingMechanism(object):
     APPSEC = 5
     REMOTE_RATE_USER = 6
     REMOTE_RATE_DATADOG = 7
+    SPAN_SAMPLING_RULE = 8
 
 
 SAMPLING_DECISION_TRACE_TAG_KEY = "_dd.p.dm"
@@ -82,3 +95,74 @@ def update_sampling_decision(
         return _set_trace_tag(context, sampling_mechanism)
     else:
         return _unset_trace_tag(context)
+
+
+class SpanSamplingRule:
+    """A span sampling rule to evaluate and potentially tag each span upon finish."""
+
+    __slots__ = (
+        "_service_matcher",
+        "_name_matcher",
+        "_sample_rate",
+        "_max_per_second",
+        "_sampling_id_threshold",
+        "_limiter",
+        "_matcher",
+    )
+
+    def __init__(
+        self,
+        service=None,  # type: Optional[str]
+        name=None,  # type: Optional[str]
+        sample_rate=1.0,  # type: Optional[float]
+        max_per_second=None,  # type: Optional[int]
+    ):
+        self.set_sample_rate(sample_rate)
+        self._max_per_second = max_per_second
+        # If no max_per_second specified then there is no limit
+        if max_per_second is None:
+            self._limiter = RateLimiter(-1)
+        else:
+            self._limiter = RateLimiter(max_per_second)
+
+        # we need to create matchers for the service and/or name pattern provided
+        if service is None:
+            self._service_matcher = GlobMatcher("*")
+        else:
+            self._service_matcher = GlobMatcher(service)
+
+        if name is None:
+            self._name_matcher = GlobMatcher("*")
+        else:
+            self._name_matcher = GlobMatcher(name)
+
+    def sample(self, span):
+        # type: (Span) -> bool
+        if self.match(span) and self._sample(span):
+            if self._limiter.is_allowed(span.start_ns):
+                self.apply_span_sampling_tags(span)
+                return True
+        return False
+
+    def _sample(self, span):
+        # type: (Span) -> bool
+        if self._sample_rate == 1:
+            return True
+        elif self._sample_rate == 0:
+            return False
+
+        return ((span.span_id * KNUTH_FACTOR) % MAX_SPAN_ID) <= self._sampling_id_threshold
+
+    def match(self, span):
+        """Determines if the span's service and name match the configured patterns"""
+        return self._service_matcher.match(span.service) and self._name_matcher.match(span.name)
+
+    def set_sample_rate(self, sample_rate=1.0):
+        self._sample_rate = float(sample_rate)
+        self._sampling_id_threshold = self._sample_rate * MAX_SPAN_ID
+
+    def apply_span_sampling_tags(self, span):
+        span.set_metric(_SINGLE_SPAN_SAMPLING_MECHANISM, SamplingMechanism.SPAN_SAMPLING_RULE)
+        span.set_metric(_SINGLE_SPAN_SAMPLING_RATE, self._sample_rate)
+        if self._max_per_second:
+            span.set_metric(_SINGLE_SPAN_SAMPLING_MAX_PER_SEC, self._max_per_second)
