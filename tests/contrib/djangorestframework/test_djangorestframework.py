a8cec0a63599e748f640f1db84679befa866fdab
Juanjo Alvarez Martinez <juanjo.alvarezmartinez@datadoghq.com>
2022-07-29 13:58:52 +0200

    feat(asm): detect attack on django body (#3993)
    ## Description
    Detect attacks on Django and Django Rest Framework body.
    
    ## Checklist
    - [ ] Title must conform to [conventional commit](https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional).
    - [ ] Add additional sections for `feat` and `fix` pull requests.
    - [ ] Ensure tests are passing for affected code.
    - [ ] [Library documentation](https://github.com/DataDog/dd-trace-py/tree/1.x/docs) and/or [Datadog's documentation site](https://github.com/DataDog/documentation/) is updated. Link to doc PR in description.
    
    
    ## Reviewer Checklist
    - [ ] Title is accurate.
    - [ ] Description motivates each change.
    - [ ] No unnecessary changes were introduced in this PR.
    - [ ] PR cannot be broken up into smaller PRs.
    - [ ] Avoid breaking [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces) changes unless absolutely necessary.
    - [ ] Tests provided or description of manual testing performed is included in the code or PR.
    - [ ] Release note has been added for fixes and features, or else `changelog/no-changelog` label added.
    - [ ] All relevant GitHub issues are correctly linked.
    - [ ] Backports are identified and tagged with Mergifyio.
    - [ ] Add to milestone.

--- tests/contrib/djangorestframework/test_djangorestframework.py
+++ tests/contrib/djangorestframework/test_djangorestframework.py
@@ -1,7 +1,10 @@
 import django
 import pytest
 
+from ddtrace.internal import _context
+from ddtrace.internal.compat import urlencode
 from tests.utils import assert_span_http_status_code
+from tests.utils import override_global_config
 
 
 @pytest.mark.skipif(django.VERSION < (1, 10), reason="requires django version >= 1.10")
@@ -31,3 +34,18 @@ def test_trace_exceptions(client, test_spans):  # noqa flake8 complains about sh
     assert err_span.error == 1
     assert err_span.get_tag("error.msg") == "Authentication credentials were not provided."
     assert "NotAuthenticated" in err_span.get_tag("error.stack")
+
+
+@pytest.mark.skipif(django.VERSION < (1, 10), reason="requires django version >= 1.10")
+def test_djangorest_request_body_urlencoded(client, test_spans, tracer):
+    with override_global_config(dict(_appsec_enabled=True)):
+        tracer._appsec_enabled = True
+        # Hack: need to pass an argument to configure so that the processors are recreated
+        tracer.configure(api_version="v0.4")
+        payload = urlencode({"mytestingbody_key": "mytestingbody_value"})
+        client.post("/users/", payload, content_type="application/x-www-form-urlencoded")
+        root_span = test_spans.spans[0]
+        query = dict(_context.get_item("http.request.body", span=root_span))
+
+        assert root_span.get_tag("_dd.appsec.json") is None
+        assert query == {"mytestingbody_key": "mytestingbody_value"}
