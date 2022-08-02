412e2be77f9d0150780d1934432911f2aece79da
Alberto Vara <alberto.vara@datadoghq.com>
2022-08-02 10:20:51 +0200

    fix(tests/django): ignore key in django snapshot test to avoid random error

--- tests/contrib/django/test_django_snapshots.py
+++ tests/contrib/django/test_django_snapshots.py
@@ -99,13 +99,13 @@ def test_middleware_trace_partial_based_view(client):
 
 @pytest.mark.django_db
 @snapshot(
-    ignores=["sqlite.query"],
+    ignores=["sqlite.query", "postgres.query"],
     variants={
         "18x": django.VERSION < (1, 9),
         "111x": (1, 9) <= django.VERSION < (1, 12),
         "21x": (1, 12) < django.VERSION < (2, 2),
         "": django.VERSION >= (2, 2),
-    }
+    },
 )
 def test_safe_string_encoding(client):
     assert client.get("/safe-template/").status_code == 200
