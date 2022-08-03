528291676571d752c31162118a2d9c61090429bc
AJ Stuyvenberg <astuyve@gmail.com>
2022-08-03 18:38:24 -0400

    feat: riot fmt

--- tests/profiling/test_serverless_scheduler.py
+++ tests/profiling/test_serverless_scheduler.py
@@ -3,8 +3,8 @@ import mock
 
 from ddtrace.internal import compat
 from ddtrace.profiling import exporter
-from ddtrace.profiling import recorder
 from ddtrace.profiling import external_scheduler
+from ddtrace.profiling import recorder
 
 
 @mock.patch("ddtrace.profiling.scheduler.Scheduler.periodic")
