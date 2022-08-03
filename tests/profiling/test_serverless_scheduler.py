77c133bf509b0797305db2a88ab66731f1329065
AJ Stuyvenberg <astuyve@gmail.com>
2022-08-03 19:14:53 -0400

    feat: fix bad rename

--- tests/profiling/test_serverless_scheduler.py
+++ tests/profiling/test_serverless_scheduler.py
@@ -3,14 +3,14 @@ import mock
 
 from ddtrace.internal import compat
 from ddtrace.profiling import exporter
-from ddtrace.profiling import external_scheduler
+from ddtrace.profiling import serverless_scheduler
 from ddtrace.profiling import recorder
 
 
 @mock.patch("ddtrace.profiling.scheduler.Scheduler.periodic")
 def test_periodic(mock_periodic):
     r = recorder.Recorder()
-    s = external_scheduler.ServerlessScheduler(r, [exporter.NullExporter()])
+    s = serverless_scheduler.ServerlessScheduler(r, [exporter.NullExporter()])
     s._last_export = compat.time_ns() - 65 * 1e9
     s._profiled_intervals = 65
     s.periodic()
