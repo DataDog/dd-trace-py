aededd57eb463f21e85f6dbf50fc8e9b1ddfd00c
AJ Stuyvenberg <astuyve@gmail.com>
2022-08-04 10:39:35 -0400

    feat: Override reset interval after flushing

--- ddtrace/profiling/serverless_scheduler.py
+++ ddtrace/profiling/serverless_scheduler.py
@@ -14,14 +14,12 @@ LOG = logging.getLogger(__name__)
 class ServerlessScheduler(scheduler.Scheduler):
     _interval = attr.ib(default=1.0, type=float)
     _profiled_intervals = attr.ib(default=0)
-    _init_time = compat.time_ns()
 
     def periodic(self):
         now = compat.time_ns()
-        # Guard against _last_export not being set
-        last_export = self._last_export or self._init_time
-        if (now - last_export >= int(self._interval) * 1e9) and (self._profiled_intervals >= 60):
+        if (now - self._last_export) >= 60 * 1e9 and self._profiled_intervals >= 60:
             self._profiled_intervals = 0
             super(ServerlessScheduler, self).periodic()
+            self.interval = 1.0
         else:
             self._profiled_intervals += 1
