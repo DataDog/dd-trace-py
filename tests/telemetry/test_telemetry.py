de5380e3c161d83d126ca8f4aad5807500f44b21
Brett Langdon <brett.langdon@datadoghq.com>
2022-08-05 08:06:48 -0400

    chore(ci): refactor telemetry tests to use the test agent (#3982)
    ## Description
    This PR introduces two changes:
    
    1. Adding helpers and usage of the test agent in order to send real telemetry payloads, capture, and validate them in the tests rather than mocking
    2. Fix a found bug with forking the TelemetryWriter after it has been enabled
    
    ## Checklist
    - [ ] Title must conform to [conventional commit](https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional).
    - [ ] Add additional sections for `feat` and `fix` pull requests.
    - [ ] Ensure tests are passing for affected code.
    - [ ] [Library documentation](https://github.com/DataDog/dd-trace-py/tree/1.x/docs) and/or [Datadog's documentation site](https://github.com/DataDog/documentation/) is updated. Link to doc PR in description.
    
    ## Relevant issue(s)
    
    Depends on: https://github.com/DataDog/dd-apm-test-agent/pull/91
    
    ## Testing strategy
    
    The new setup for these tests is to add a helper to enable sending telemetry requests to the test agent, and then being able to retrieve the raw requests to validate.
    
    We require a change to be merged and released from the test agent first before we can fully test this.
    
    The provided tests should be close to equivalent to what is there, but it should be easier now to expand upon the current tests to cover more cases (e.g. `ddtrace-run` usage).
    
    ## Reviewer Checklist
    - [x] Title is accurate.
    - [x] Description motivates each change.
    - [x] No unnecessary changes were introduced in this PR.
    - [ ] PR cannot be broken up into smaller PRs.
    - [ ] Avoid breaking [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces) changes unless absolutely necessary.
    - [ ] Tests provided or description of manual testing performed is included in the code or PR.
    - [ ] Release note has been added for fixes and features, or else `changelog/no-changelog` label added.
    - [ ] All relevant GitHub issues are correctly linked.
    - [ ] Backports are identified and tagged with Mergifyio.
    - [ ] Add to milestone.
    
    (cherry picked from commit a60322c76535b04189efe4f980dac500e0ef727d)

--- tests/telemetry/test_telemetry.py
+++ tests/telemetry/test_telemetry.py
@@ -1,73 +1,66 @@
+def test_enable(test_agent_session, run_python_code_in_subprocess):
+    code = """
+from ddtrace.internal.telemetry import telemetry_writer
+telemetry_writer.enable()
+"""
+
+    stdout, stderr, status, _ = run_python_code_in_subprocess(code)
+
+    assert status == 0, stderr
+    assert stdout == b"", stderr
+    assert stderr == b""
+
+    events = test_agent_session.get_events()
+    assert len(events) == 2
+
+    # Same runtime id is used
+    assert events[0]["runtime_id"] == events[1]["runtime_id"]
+    assert events[0]["request_type"] == "app-closing"
+    assert events[1]["request_type"] == "app-started"
+
+
+def test_enable_fork(test_agent_session, run_python_code_in_subprocess):
+    """assert app-started/app-closing events are only sent in parent process"""
+    code = """
 import os
 
-import httpretty
-import pytest
-
-from ddtrace.internal import telemetry
-from ddtrace.internal.service import ServiceStatus
+from ddtrace.internal.runtime import get_runtime_id
+from ddtrace.internal.telemetry import telemetry_writer
 
 
-@pytest.fixture
-def telemetry_writer():
-    yield telemetry.telemetry_writer
+# We have to start before forking since fork hooks are not enabled until after enabling
+telemetry_writer.enable()
+
+if os.fork() == 0:
+    # Send multiple started events to confirm none get sent
+    telemetry_writer.app_started_event()
+    telemetry_writer.app_started_event()
+    telemetry_writer.app_started_event()
+else:
+    # Print the parent process runtime id for validation
+    print(get_runtime_id())
+    """
+
+    stdout, stderr, status, _ = run_python_code_in_subprocess(code)
+    assert status == 0, stderr
+    assert stderr == b""
+
+    runtime_id = stdout.strip().decode("utf-8")
+
+    requests = test_agent_session.get_requests()
+
+    # We expect 2 events from the parent process to get sent, but none from the child process
+    assert len(requests) == 2
+    # Validate that the runtime id sent for every event is the parent processes runtime id
+    assert requests[0]["body"]["runtime_id"] == runtime_id
+    assert requests[0]["body"]["request_type"] == "app-closing"
+    assert requests[1]["body"]["runtime_id"] == runtime_id
+    assert requests[1]["body"]["request_type"] == "app-started"
 
 
-@pytest.fixture(autouse=True)
-def mock_send_request(telemetry_writer):
-    with httpretty.enabled():
-        httpretty.register_uri(httpretty.POST, telemetry_writer.url, status=200)
-        yield
-
-
-def test_enable(telemetry_writer):
-    telemetry_writer.enable()
-    assert telemetry_writer.status == ServiceStatus.RUNNING
-
-    # assert that calling enable twice does not raise an exception
-    telemetry_writer.enable()
-    assert telemetry_writer.status == ServiceStatus.RUNNING
-
-    # send queued events
-    telemetry_writer.periodic()
-    # ensure an app-started telemetry request is sent
-    assert len(httpretty.latest_requests()) == 1
-    headers = httpretty.last_request().headers
-    assert "DD-Telemetry-Request-Type" in headers
-    assert headers["DD-Telemetry-Request-Type"] == "app-started"
-
-
-def test_disable(telemetry_writer):
-    telemetry_writer.disable()
-    assert telemetry_writer.status == ServiceStatus.STOPPED
-
-    # assert that calling disable twice does not raise an exception
-    telemetry_writer.disable()
-    assert telemetry_writer.status == ServiceStatus.STOPPED
-
-    # send queued events
-    telemetry_writer.periodic()
-    # ensure no events were sent
-    assert len(httpretty.latest_requests()) == 0
-
-
-def test_fork():
-    telemetry.telemetry_writer.enable()
-
-    pid = os.fork()
-    if pid > 0:
-        assert telemetry.telemetry_writer.status == ServiceStatus.RUNNING
-    else:
-        assert telemetry.telemetry_writer._forked is True
-        assert telemetry.telemetry_writer._integrations_queue == []
-        assert telemetry.telemetry_writer._events_queue == []
-        assert telemetry.telemetry_writer.status == ServiceStatus.RUNNING
-        # Kill the process so it doesn't continue running the rest of the test suite
-        os._exit(0)
-
-
-def test_logs_after_fork(ddtrace_run_python_code_in_subprocess):
+def test_logs_after_fork(run_python_code_in_subprocess):
     # Regression test: telemetry writer should not log an error when a process forks
-    _, err, status, _ = ddtrace_run_python_code_in_subprocess(
+    _, err, status, _ = run_python_code_in_subprocess(
         """
 import ddtrace
 import logging
