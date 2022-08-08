1076aec0b74c870be0d2b5077d0fe3e4fa40724f
Brett Langdon <brett.langdon@datadoghq.com>
2022-08-08 15:53:47 -0400

    chore(telemetry): add support for app-heartbeat telemetry events (#3979)
    ## Description
    Queue an app-heartbeat event once every 60 seconds in the main process.
    
    This event has no payload data and is used to indicate that the app is still
    up and running.
    
    We will queue the event no faster than once every 60 seconds, but at least once
    every flush interval for telemetry data.
    
    ## Checklist
    - [x] Title must conform to [conventional commit](https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional).
    - [x] Add additional sections for `feat` and `fix` pull requests.
    - [x] Ensure tests are passing for affected code.
    - [x] [Library documentation](https://github.com/DataDog/dd-trace-py/tree/1.x/docs) and/or [Datadog's documentation site](https://github.com/DataDog/documentation/) is updated. Link to doc PR in description.
    
    ## Reviewer Checklist
    - [ ] Title is accurate.
    - [ ] Description motivates each change.
    - [ ] No unnecessary changes were introduced in this PR.
    - [ ] PR cannot be broken up into smaller PRs.
    - [ ] Avoid breaking [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces) changes unless absolutely necessary.
    - [ ] Tests provided or description of manual testing performed is included in the code or PR.
    - [ ] Release note has been added for fixes and features, or else `changelog/no-changelog` label added.
    - [ ] All relevant GitHub issues are correctly linked.
    - [ ] Backports are identified and tagged with Mergifyio.
    - [ ] Add to milestone.
    
    (cherry picked from commit 6f3d2cfce03f0b4840bd108ca80a04df40d9a2a5)
    
    # Conflicts:
    #	tests/telemetry/test_writer.py

--- tests/telemetry/test_writer.py
+++ tests/telemetry/test_writer.py
@@ -1,4 +1,10 @@
 import os
+<<<<<<< HEAD
+=======
+import time
+from typing import Any
+from typing import Dict
+>>>>>>> 6f3d2cfc (chore(telemetry): add support for app-heartbeat telemetry events (#3979))
 
 import httpretty
 import mock
@@ -11,6 +17,7 @@ from ddtrace.internal.telemetry.writer import TelemetryWriter
 from ddtrace.internal.telemetry.writer import get_runtime_id
 from ddtrace.settings import _config as config
 
+<<<<<<< HEAD
 
 AGENT_URL = "http://localhost:8126"
 TEST_URL = "%s/%s" % (AGENT_URL, TelemetryWriter.ENDPOINT)
@@ -19,6 +26,9 @@ TEST_URL = "%s/%s" % (AGENT_URL, TelemetryWriter.ENDPOINT)
 @pytest.fixture
 def mock_status():
     return 202
+=======
+from .conftest import TelemetryTestSession
+>>>>>>> 6f3d2cfc (chore(telemetry): add support for app-heartbeat telemetry events (#3979))
 
 
 @pytest.fixture
@@ -32,6 +42,7 @@ def mock_send_request(mock_status):
 def mock_time():
     with mock.patch("time.time") as mt:
         mt.return_value = 1642544540
+<<<<<<< HEAD
         yield
 
 
@@ -51,6 +62,9 @@ def telemetry_writer_disabled():
     assert telemetry_writer._enabled is None
     telemetry_writer._enabled = False
     yield telemetry_writer
+=======
+        yield mt
+>>>>>>> 6f3d2cfc (chore(telemetry): add support for app-heartbeat telemetry events (#3979))
 
 
 def test_add_event(mock_time, mock_send_request, telemetry_writer):
@@ -228,20 +242,23 @@ def test_periodic(mock_send_request, telemetry_writer):
 
 
 @pytest.mark.parametrize("mock_status", [300, 400, 401, 403, 500])
-def test_send_failing_request(mock_status, mock_send_request, telemetry_writer):
+def test_send_failing_request(mock_status, telemetry_writer):
     """asserts that a warning is logged when an unsuccessful response is returned by the http client"""
-    with mock.patch("ddtrace.internal.telemetry.writer.log") as log:
-        # sends failing app-closing event
-        telemetry_writer.on_shutdown()
-        # asserts unsuccessful status code was logged
-        log.debug.assert_called_with(
-            "failed to send telemetry to the Datadog Agent at %s/%s. response: %s",
-            telemetry_writer._agent_url,
-            telemetry_writer.ENDPOINT,
-            mock_status,
-        )
-    # ensure one failing request was sent
-    assert len(httpretty.latest_requests()) == 1
+
+    with httpretty.enabled():
+        httpretty.register_uri(httpretty.POST, telemetry_writer.url, status=mock_status)
+        with mock.patch("ddtrace.internal.telemetry.writer.log") as log:
+            # sends failing app-closing event
+            telemetry_writer.on_shutdown()
+            # asserts unsuccessful status code was logged
+            log.debug.assert_called_with(
+                "failed to send telemetry to the Datadog Agent at %s/%s. response: %s",
+                telemetry_writer._agent_url,
+                telemetry_writer.ENDPOINT,
+                mock_status,
+            )
+        # ensure one failing request was sent
+        assert len(httpretty.latest_requests()) == 1
 
 
 def test_send_request_exception():
@@ -270,10 +287,121 @@ def test_telemetry_graceful_shutdown(mock_time, mock_send_request, telemetry_wri
     assert httpretty.last_request().parsed_body == _get_request_body({}, "app-closing", 2)
 
 
+def test_app_heartbeat_event_periodic(mock_time, telemetry_writer, test_agent_session):
+    # type: (mock.Mock, Any, TelemetryWriter) -> None
+    """asserts that we queue/send app-heartbeat event every 60 seconds when periodc() is called"""
+
+    # Assert clean slate
+    events = test_agent_session.get_events()
+    assert len(events) == 0
+
+    # Assert first flush does not queue any events
+    events = test_agent_session.get_events()
+    assert len(events) == 0
+
+    # Advance time 60 seconds
+    mock_time.return_value += 60
+
+    # Assert next flush contains app-heartbeat event
+    telemetry_writer.periodic()
+    _assert_app_heartbeat_event(1, test_agent_session)
+
+    # Advance time 59 seconds
+    mock_time.return_value += 59
+
+    # Assert next flush does not contain an app-heartbeat event
+    telemetry_writer.periodic()
+    events = test_agent_session.get_events()
+    assert len(events) == 1
+
+    # Advance time 1 second
+    mock_time.return_value += 1
+
+    # Assert next flush contains app-heartbeat event
+    telemetry_writer.periodic()
+    _assert_app_heartbeat_event(2, test_agent_session)
+
+    # Advance time 120 seconds (2 intervals)
+    mock_time.return_value += 120
+
+    # Assert next flush contains only one app-heartbeat event
+    telemetry_writer.periodic()
+    _assert_app_heartbeat_event(3, test_agent_session)
+
+
+def test_app_heartbeat_event(mock_time, telemetry_writer, test_agent_session):
+    # type: (mock.Mock, Any, TelemetryWriter) -> None
+    """asserts that we queue/send app-heartbeat event every 60 seconds when app_heartbeat_event() is called"""
+
+    # Assert clean slate
+    events = test_agent_session.get_events()
+    assert len(events) == 0
+
+    # Within 60 seconds of telemetry writer creation, no event is queued
+    telemetry_writer.app_heartbeat_event()
+    telemetry_writer.periodic()
+    events = test_agent_session.get_events()
+    assert len(events) == 0
+
+    # Advance time 60 seconds
+    mock_time.return_value += 60
+
+    # Manual queueing and calling periodic will only queue and flush 1 event
+    telemetry_writer.app_heartbeat_event()
+    telemetry_writer.periodic()
+    _assert_app_heartbeat_event(1, test_agent_session)
+
+    # Advance time 120 seconds (2 intervals)
+    mock_time.return_value += 120
+
+    # Calling multiple times will only enqueue one event
+    telemetry_writer.app_heartbeat_event()
+    telemetry_writer.app_heartbeat_event()
+    telemetry_writer.app_heartbeat_event()
+    telemetry_writer.app_heartbeat_event()
+    telemetry_writer.periodic()
+    _assert_app_heartbeat_event(2, test_agent_session)
+
+
+def test_app_heartbeat_event_fork(mock_time, telemetry_writer, test_agent_session):
+    # type: (mock.Mock, TelemetryWriter) -> None
+    """asserts that we do not queue/send app-heartbeat events on forks"""
+    if os.fork() == 0:
+        # Advance time 120 seconds (2 intervals)
+        mock_time.return_value += 120
+
+        telemetry_writer.app_heartbeat_event()
+        telemetry_writer.app_heartbeat_event()
+        telemetry_writer.app_heartbeat_event()
+        telemetry_writer.app_heartbeat_event()
+        telemetry_writer.periodic()
+        # Kill the process so it doesn't continue running the rest of the test suite
+        events = test_agent_session.get_events()
+        assert len(events) == 0
+        os._exit(0)
+
+
+def _assert_app_heartbeat_event(seq_id, test_agent_session):
+    # type: (int, TelemetryWriter, TelemetryTestSession) -> None
+    """used to test heartbeat events received by the testagent"""
+    events = test_agent_session.get_events()
+    assert len(events) == seq_id
+    # The test_agent returns telemetry events in reverse chronological order
+    # The first event in the list is last event sent by the Telemetry Client
+    last_event = events[0]
+    assert last_event["request_type"] == "app-heartbeat"
+    assert last_event == _get_request_body({}, "app-heartbeat", seq_id=seq_id)
+
+
 def _get_request_body(payload, payload_type, seq_id=1):
-    """used to test the body of requests intercepted by httpretty"""
+    # type: (Dict, str, int) -> Dict
+    """used to test the body of requests received by the testagent"""
     return {
+<<<<<<< HEAD
         "tracer_time": 1642544540,
+=======
+        "tracer_time": time.time(),
+>>>>>>> 6f3d2cfc (chore(telemetry): add support for app-heartbeat telemetry events (#3979))
         "runtime_id": get_runtime_id(),
         "api_version": "v1",
         "seq_id": seq_id,
