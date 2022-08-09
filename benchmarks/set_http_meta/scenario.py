ed8a676704d36567439343901365f4820a009ae7
Alberto Vara <alberto.vara@datadoghq.com>
2022-08-09 18:29:11 +0200

    feat: rollback extra loops

--- benchmarks/set_http_meta/scenario.py
+++ benchmarks/set_http_meta/scenario.py
@@ -124,7 +124,6 @@ class SetHttpMeta(bm.Scenario):
 
         def bm(loops):
             for _ in range(loops):
-                for _ in range(10000):
-                    set_http_meta(span, config, **data)
+                set_http_meta(span, config, **data)
 
         yield bm
