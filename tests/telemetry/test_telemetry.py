e166ebf528231420300c45270d5a1b6f9b0ea0f2
Munir Abdinur <munir.abdinur@datadoghq.com>
2022-09-01 20:50:25 -0400

    chore(telemetry): enable telemetry by default (#4003)
    ## Description
    
    Enables telemetry collection when an application is run with `ddtrace-run`.
    
    ## Checklist
    - [x] Title must conform to [conventional commit](https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional).
    - [x] Add additional sections for `feat` and `fix` pull requests.
    - [x] Ensure tests are passing for affected code.
    - [x] [Library documentation](https://github.com/DataDog/dd-trace-py/tree/1.x/docs) and/or [Datadog's documentation site](https://github.com/DataDog/documentation/) is updated. Link to doc PR in description.
    
    ## Motivation
    
    Collect tracer configurations.
    
    ## Design
    
    Starts a telemetry writer which sends a payload of tracer configuration to the datadog agent. As of now one event is sent on application startup and another is sent when an application terminates.
    
    ## Testing strategy
    
    Ensure telemetry test suite passes in ci.
    
    ## Relevant issue(s)
    
    ## Testing strategy
    
    ## Reviewer Checklist
    - [ ] Title is accurate.
    - [ ] Description motivates each change.
    - [ ] No unnecessary changes were introduced in this PR.
    - [ ] PR cannot be broken up into smaller PRs.
    - [ ] Avoid breaking [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces) changes unless absolutely necessary.
    - [ ] Tests provided or description of manual testing performed is included in the code or PR.
    - [ ] Release note has been added for fixes and features, or else `changelog/no-changelog` label added.
    - [ ] All relevant GitHub issues are correctly linked.
    - [ ] Backports are identified and tagged with Mergifyio.
    - [ ] Add to milestone.
    
    (cherry picked from commit fffb0ef2830f87827439c5ccc54f69ffadcc7f96)
    
    # Conflicts:
    #	tests/telemetry/test_telemetry.py

--- tests/telemetry/test_telemetry.py
+++ tests/telemetry/test_telemetry.py
@@ -1,3 +1,55 @@
+<<<<<<< HEAD
+=======
+import pytest
+
+
+def test_enable(test_agent_session, run_python_code_in_subprocess):
+    code = """
+from ddtrace.internal.telemetry import telemetry_writer
+telemetry_writer.enable()
+"""
+
+    stdout, stderr, status, _ = run_python_code_in_subprocess(code)
+
+    assert status == 0, stderr
+    assert stdout == b"", stderr
+    assert stderr == b""
+
+    events = test_agent_session.get_events()
+    assert len(events) == 2
+
+    # Same runtime id is used
+    assert events[0]["runtime_id"] == events[1]["runtime_id"]
+    assert events[0]["request_type"] == "app-closing"
+    assert events[1]["request_type"] == "app-started"
+
+
+@pytest.mark.snapshot
+def test_telemetry_enabled_on_first_tracer_flush(test_agent_session, ddtrace_run_python_code_in_subprocess):
+    """assert telemetry events are generated after the first trace is flushed to the agent"""
+    # Using ddtrace-run and/or importing ddtrace alone should not enable telemetry
+    # Telemetry data should only be sent after the first trace to the agent
+    _, stderr, status, _ = ddtrace_run_python_code_in_subprocess("import ddtrace")
+    assert status == 0, stderr
+    # No trace and No Telemetry
+    assert len(test_agent_session.get_events()) == 0
+
+    # Submit a trace to the agent in a subprocess
+    code = 'from ddtrace import tracer; span = tracer.trace("test-telemetry"); span.finish()'
+    _, stderr, status, _ = ddtrace_run_python_code_in_subprocess(code)
+    assert status == 0, stderr
+    assert stderr == b""
+    # Ensure telemetry events were sent to the agent (snapshot ensures one trace was generated)
+    events = test_agent_session.get_events()
+    assert len(events) == 2
+    assert events[0]["request_type"] == "app-closing"
+    assert events[1]["request_type"] == "app-started"
+
+
+def test_enable_fork(test_agent_session, run_python_code_in_subprocess):
+    """assert app-started/app-closing events are only sent in parent process"""
+    code = """
+>>>>>>> fffb0ef2 (chore(telemetry): enable telemetry by default (#4003))
 import os
 
 import httpretty
