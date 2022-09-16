57aaaace054f46a774814c352c42e98d7bb62e7b
Simone Chiorazzo <simone.chiorazzo@cloudacademy.com>
2022-09-16 18:21:34 +0200

    fix(django): Append url_name of resolver match to differentiate different endpoints under the same ViewSet

--- ddtrace/contrib/django/utils.py
+++ ddtrace/contrib/django/utils.py
@@ -185,6 +185,11 @@ def _set_resolver_tags(pin, span, request):
             resolver_match = resolver.resolve(request.path_info)
         handler = func_name(resolver_match[0])
 
+        # Append url name in order to distinguish different routes of the same ViewSet
+        url_name = resolver_match.url_name
+        if url_name:
+            handler = ".".join([handler, url_name])
+
         if config.django.use_handler_resource_format:
             resource = " ".join((request.method, handler))
         elif config.django.use_legacy_resource_format:
