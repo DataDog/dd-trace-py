bb2b07bd4ede4eb263c0956dc97b20b2b24772d5
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2022-09-27 10:31:32 +0100

    docs(debugging): add dynamic instrumentation API
    This change adds the dynamic instrumentation public API in the API
    section of the docs.

--- ddtrace/debugging/_debugger.py
+++ ddtrace/debugging/_debugger.py
@@ -151,7 +151,11 @@ class Debugger(Service):
     @classmethod
     def enable(cls, run_module=False):
         # type: (bool) -> None
-        """Enable the debugger (idempotent)."""
+        """Enable dynamic instrumentation
+
+        This class method is idempotent. Dynamic instrumentation will be
+        disabled automatically at exit.
+        """
         if cls._instance is not None:
             log.debug("%s already enabled", cls.__name__)
             return
@@ -175,7 +179,11 @@ class Debugger(Service):
     @classmethod
     def disable(cls):
         # type: () -> None
-        """Disable the debugger (idempotent)."""
+        """Disable dynamic instrumentation.
+
+        This class method is idempotent. Called automatically at exit, if
+        dynamic instrumentation was enabled.
+        """
         if cls._instance is None:
             log.debug("%s not enabled", cls.__name__)
             return
@@ -206,7 +214,7 @@ class Debugger(Service):
                 Snapshot: SnapshotJsonEncoder(service_name),
                 str: str,
             },
-            on_full=self.on_encoder_buffer_full,
+            on_full=self._on_encoder_buffer_full,
         )
         self._probe_registry = ProbeRegistry(self.__logger__(service_name, self._encoder))
         self._uploader = self.__uploader__(self._encoder)
@@ -228,7 +236,7 @@ class Debugger(Service):
 
         log.debug("%s initialized (service name: %s)", self.__class__.__name__, service_name)
 
-    def on_encoder_buffer_full(self, item, encoded):
+    def _on_encoder_buffer_full(self, item, encoded):
         # type (Any, bytes) -> None
         # Send upload request
         self._uploader.upload()
