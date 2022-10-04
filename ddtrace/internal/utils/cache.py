ecebbae984e6a70b4c37d1e18a7ef241b929a642
Yun Kim <yun.kim@datadoghq.com>
2022-10-04 11:27:05 -0400

    Resolve merge conflicts with 0.61

--- ddtrace/internal/utils/cache.py
+++ ddtrace/internal/utils/cache.py
@@ -25,46 +25,18 @@ def cached(maxsize=256):
     the requested size is O(log(size)).
     """
 
-<<<<<<< HEAD
     def cached_wrapper(f):
         # type: (F) -> F
         cache = {}  # type: Dict[Any, Tuple[Any, int]]
         lock = RLock()
 
         def cached_f(key):
-            # type: (T) -> S
+            # type: (T) -> Any
             if len(cache) >= maxsize:
                 for _, h in zip(range(maxsize >> 1), sorted(cache, key=lambda h: cache[h][1])):
                     del cache[h]
 
             _ = cache.get(key, miss)
-=======
-    def __init__(self, maxsize=256):
-        # type: (int) -> None
-        self.maxsize = maxsize
-        self.lock = RLock()
-
-    def get(self, key, f):  # type: ignore[override]
-        # type: (T, F) -> Any
-        """Get a value from the cache.
-
-        If the value with the given key is not in the cache, the expensive
-        function ``f`` is called on the key to generate it. The return value is
-        then stored in the cache and returned to the caller.
-        """
-        if len(self) >= self.maxsize:
-            for _, h in zip(range(self.maxsize >> 1), sorted(self, key=lambda h: self[h][1])):
-                del self[h]
-
-        _ = super(LFUCache, self).get(key, miss)
-        if _ is not miss:
-            value, count = _
-            self[key] = (value, count + 1)
-            return value
-
-        with self.lock:
-            _ = super(LFUCache, self).get(key, miss)
->>>>>>> 026d6144 (fix(typing): update types to be compatible with latest mypy (backport #4234) (#4263))
             if _ is not miss:
                 value, count = _
                 cache[key] = (value, count + 1)
@@ -81,22 +53,7 @@ def cached(maxsize=256):
 
                 cache[key] = (result, 1)
 
-<<<<<<< HEAD
                 return result
-=======
-
-def cached(maxsize=256):
-    # type: (int) -> Callable[[F], F]
-    """Decorator for memoizing functions of a single argument (LFU policy)."""
-
-    def cached_wrapper(f):
-        # type: (F) -> F
-        cache = LFUCache(maxsize)
-
-        def cached_f(key):
-            # type: (T) -> Any
-            return cache.get(key, f)
->>>>>>> 026d6144 (fix(typing): update types to be compatible with latest mypy (backport #4234) (#4263))
 
         cached_f.invalidate = cache.clear  # type: ignore[attr-defined]
 
--- ddtrace/utils/cache.py
+++ ddtrace/utils/cache.py
@@ -3,7 +3,6 @@ from ddtrace.internal.utils.deprecations import DDTraceDeprecationWarning
 from ..internal.utils.cache import CachedMethodDescriptor  # noqa
 from ..internal.utils.cache import F  # noqa
 from ..internal.utils.cache import M  # noqa
-from ..internal.utils.cache import S  # noqa
 from ..internal.utils.cache import T  # noqa
 from ..internal.utils.cache import cached  # noqa
 from ..internal.utils.cache import cachedmethod  # noqa
