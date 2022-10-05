e3e003705ceeb979afcad7c09927ba2a3ecbef4f
Yun Kim <35776586+Yun-Kim@users.noreply.github.com>
2022-10-05 09:56:13 -0400

    ci(sanic): update test suite to run with sanic v22.9 (#4262)
    * Separate sanic test suites to match with sanic-testing dependency versions
    
    * update app and snapshots to be compatible with sanic 22.9.0
    
    * add sanic v22.9 in riot, update snapshot name
    
    * kill child processes
    
    * set span name and remove redundant snapshot
    
    * add comment
    
    Co-authored-by: Brett Langdon <brett.langdon@datadoghq.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Munir Abdinur <munir_abdinur@hotmail.com>
    (cherry picked from commit 91c427833441f33370a4e0eb94544e8b061f084f)
    
    # Conflicts:
    #	riotfile.py

--- riotfile.py
+++ riotfile.py
@@ -1796,8 +1796,33 @@ venv = Venv(
                         "sanic": [
                             "~=21.9.0",
                             "~=21.12.0",
+<<<<<<< HEAD
                             latest,
                         ],
+=======
+                        ],
+                        "sanic-testing": "~=0.8.3",
+                    },
+                ),
+                Venv(
+                    pys=select_pys(min_version="3.7"),
+                    pkgs={
+                        "sanic": "~=22.3.0",
+                        "sanic-testing": "~=22.3.0",
+                    },
+                ),
+                Venv(
+                    pys=select_pys(min_version="3.7"),
+                    pkgs={
+                        "sanic": "~=22.9.0",
+                        "sanic-testing": "~=22.9.0",
+                    },
+                ),
+                Venv(
+                    pys=select_pys(min_version="3.7"),
+                    pkgs={
+                        "sanic": latest,
+>>>>>>> 91c42783 (ci(sanic): update test suite to run with sanic v22.9 (#4262))
                         "sanic-testing": latest,
                     },
                 ),
