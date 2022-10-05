f3ce4b82131a75b63bc1f93d07576e4af52f8185
Munir Abdinur <munir_abdinur@hotmail.com>
2022-10-05 10:35:11 -0400

    fix conflicts

--- riotfile.py
+++ riotfile.py
@@ -1552,10 +1552,6 @@ venv = Venv(
                         "sanic": [
                             "~=21.9.0",
                             "~=21.12.0",
-<<<<<<< HEAD
-                            latest,
-                        ],
-=======
                         ],
                         "sanic-testing": "~=0.8.3",
                     },
@@ -1578,7 +1574,6 @@ venv = Venv(
                     pys=select_pys(min_version="3.7"),
                     pkgs={
                         "sanic": latest,
->>>>>>> 91c42783 (ci(sanic): update test suite to run with sanic v22.9 (#4262))
                         "sanic-testing": latest,
                     },
                 ),
