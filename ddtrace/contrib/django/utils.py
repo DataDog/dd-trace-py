912dd758f1c61ee3591d8c6b1bbab385347be665
Christophe Papazian <christophe.papazian@datadoghq.com>
2022-10-07 14:56:21 +0200

    Chore ASM: http.ROUTE

--- ddtrace/contrib/django/utils.py
+++ ddtrace/contrib/django/utils.py
@@ -359,6 +359,10 @@ def _after_request_tags(pin, span, request, response):
 
             headers_case_sensitive = django.VERSION < (2, 2)
 
+            route = None
+            if django.VERSION >= (2, 2) and request is not None and request.resolver_match is not None:
+                route = request.resolver_match.route
+
             trace_utils.set_http_meta(
                 span,
                 config.django,
@@ -375,7 +379,7 @@ def _after_request_tags(pin, span, request, response):
                 request_body=_extract_body(request),
                 peer_ip=request.META.get("REMOTE_ADDR"),
                 headers_are_case_sensitive=headers_case_sensitive,
-                route=request.resolver_match.route if django.VERSION >= (2, 2) and request is not None and request.resolver_match is not None else None,
+                route=route,
             )
     finally:
         if span.resource == REQUEST_DEFAULT_RESOURCE:
