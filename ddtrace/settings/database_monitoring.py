ff380ec344e3134ab7439a53de1f186aad267b48
Munir Abdinur <munir_abdinur@hotmail.com>
2022-10-14 15:46:56 -0400

    chore(dbm): generate comments

--- ddtrace/settings/database_monitoring.py
+++ ddtrace/settings/database_monitoring.py
@@ -1,5 +1,21 @@
+from typing import TYPE_CHECKING
+
 from envier import En
 from envier import validators
+
+from ddtrace.vendor.sqlcommenter import generate_sql_comment as _generate_sql_comment
+
+from . import _config as dd_config
+
+
+if TYPE_CHECKING:
+    from ddtrace import Span
+
+DBM_PARENT_SERVICE_NAME_KEY = "ddps"
+DBM_DATABASE_SERVICE_NAME_KEY = "dddbs"
+DBM_ENVIRONMENT_KEY = "dde"
+DBM_VERSION_KEY = "ddpv"
+DBM_TRACE_PARENT_KEY = "traceparent"
 
 
 class DatabaseMonitoringConfig(En):
@@ -14,4 +30,23 @@ class DatabaseMonitoringConfig(En):
     )
 
 
-config = DatabaseMonitoringConfig()
+dbm_config = DatabaseMonitoringConfig()
+
+
+def get_dbm_comment(db_span):
+    # type: (Span) -> str
+    if dbm_config.injection_mode == "disabled":
+        return ""
+
+    dbm_tags = {
+        DBM_PARENT_SERVICE_NAME_KEY: dd_config.service,
+        DBM_ENVIRONMENT_KEY: dd_config.env,
+        DBM_VERSION_KEY: dd_config.version,
+        DBM_DATABASE_SERVICE_NAME_KEY: db_span.service,
+    }
+
+    if dbm_config.injection_mode == "full":
+        # TODO: add _dd.dbm_trace_injected tag to db_span
+        dbm_tags[DBM_TRACE_PARENT_KEY] = db_span.context.traceparent
+
+    return _generate_sql_comment(**dbm_tags)
--- tests/internal/test_database_monitoring.py
+++ tests/internal/test_database_monitoring.py
@@ -7,7 +7,7 @@ from tests.utils import override_env
 def test_injection_mode_configuration():
     # Ensure Database Monitoring support is disabled by default
     # TODO: Enable DBM support by default
-    assert database_monitoring.config.injection_mode == "disabled"
+    assert database_monitoring.dbm_config.injection_mode == "disabled"
 
     # Ensure service is a valid injection mode
     with override_env(dict(DD_TRACE_SQL_COMMENT_INJECTION_MODE="service")):
@@ -27,3 +27,64 @@ def test_injection_mode_configuration():
         excinfo.value.args[0] == "Invalid value for environment variable DD_TRACE_SQL_COMMENT_INJECTION_MODE: "
         "value must be one of ['disabled', 'full', 'service']"
     )
+
+
+@pytest.mark.subprocess(env=dict(DD_TRACE_SQL_COMMENT_INJECTION_MODE="disabled"))
+def test_get_dbm_comment_disabled_mode():
+    from ddtrace import tracer
+    from ddtrace.settings import database_monitoring
+
+    dbspan = tracer.trace("dbspan", service="orders-db")
+    sqlcomment = database_monitoring.get_dbm_comment(dbspan)
+    assert sqlcomment == ""
+
+
+@pytest.mark.subprocess(
+    env=dict(
+        DD_TRACE_SQL_COMMENT_INJECTION_MODE="service",
+        DD_SERVICE="orders-app",
+        DD_ENV="staging",
+        DD_VERSION="v7343437-d7ac743",
+    )
+)
+def test_get_dbm_comment_service_mode():
+    from ddtrace import tracer
+    from ddtrace.settings import database_monitoring
+
+    dbspan = tracer.trace("dbname", service="orders-db")
+
+    sqlcomment = database_monitoring.get_dbm_comment(dbspan)
+    # tags in sqlcomments are comma delimited
+    # assert 4 tags are present in the sqlcomment
+    assert len(sqlcomment.split(",")) == 4
+    # assert tags sqlcomment contains the correct value
+    assert "dddbs='orders-db'" in sqlcomment
+    assert "dde='staging'" in sqlcomment
+    assert "ddps='orders-app'" in sqlcomment
+    assert "ddpv='v7343437-d7ac743'" in sqlcomment
+
+
+@pytest.mark.subprocess(
+    env=dict(
+        DD_TRACE_SQL_COMMENT_INJECTION_MODE="full",
+        DD_SERVICE="orders-app",
+        DD_ENV="staging",
+        DD_VERSION="v7343437-d7ac743",
+    )
+)
+def test_get_dbm_comment_full_mode():
+    from ddtrace import tracer
+    from ddtrace.settings import database_monitoring
+
+    dbspan = tracer.trace("dbname", service="orders-db")
+
+    sqlcomment = database_monitoring.get_dbm_comment(dbspan)
+    # tags in sqlcomments are comma delimited
+    # assert 4 tags are present in the sqlcomment
+    assert len(sqlcomment.split(",")) == 5
+    # assert tags sqlcomment contains the correct value
+    assert "dddbs='orders-db'" in sqlcomment
+    assert "dde='staging'" in sqlcomment
+    assert "ddps='orders-app'" in sqlcomment
+    assert "ddpv='v7343437-d7ac743'" in sqlcomment
+    assert "traceparent='%s'" % (dbspan.context.traceparent,) in sqlcomment
