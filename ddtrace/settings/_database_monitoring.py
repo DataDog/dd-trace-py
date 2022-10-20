612d5edc0af32f023fc0eb02a74b0c95ac0ed466
Munir Abdinur <munir_abdinur@hotmail.com>
2022-10-20 13:56:18 -0400

    chore(dbm): use DD_TRACE_SQL_COMMENT_PROPAGATION_MODE

--- ddtrace/settings/_database_monitoring.py
+++ ddtrace/settings/_database_monitoring.py
@@ -23,7 +23,7 @@ class DatabaseMonitoringConfig(En):
 
     injection_mode = En.v(
         str,
-        "sql_comment_injection_mode",
+        "sql_comment_propagation_mode",
         default="disabled",
         help="Valid Injection Modes: disabled, service, and full",
         validator=validators.choice(["disabled", "full", "service"]),
