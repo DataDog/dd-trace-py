c33a562c6ded110ed349bc77135d0f5d3b269ef3
Alberto Vara <alberto.vara@datadoghq.com>
2022-10-20 16:36:02 +0200

    refactor(iast): improve performance of get_info_frame

--- ddtrace/appsec/iast/_stacktrace.c
+++ ddtrace/appsec/iast/_stacktrace.c
@@ -2,13 +2,12 @@
 #include <frameobject.h>
 
 #ifdef _WIN32
-    #define DD_TRACE_INSTALLED_PREFIX "\\ddtrace\\"
-    #define TESTS_PREFIX "\\tests\\"
+#define DD_TRACE_INSTALLED_PREFIX "\\ddtrace\\"
+#define TESTS_PREFIX "\\tests\\"
 #else
-    #define DD_TRACE_INSTALLED_PREFIX "/ddtrace/"
-    #define TESTS_PREFIX "/tests/"
+#define DD_TRACE_INSTALLED_PREFIX "/ddtrace/"
+#define TESTS_PREFIX "/tests/"
 #endif
-
 
 /**
  * get_file_and_line
@@ -17,14 +16,17 @@
  *
  * @return Tuple, string and integer.
  **/
-static PyObject* get_file_and_line(PyObject* self, PyObject* const* args, Py_ssize_t nargs) {
-    PyThreadState *tstate = PyThreadState_GET();
+static PyObject*
+get_file_and_line(PyObject* self, PyObject* const* args, Py_ssize_t nargs)
+{
+    PyThreadState* tstate = PyThreadState_GET();
     PyFrameObject* frame;
     if (NULL != tstate && NULL != tstate->frame) {
         frame = tstate->frame;
         while (NULL != frame) {
-            char *filename = PyBytes_AsString(PyUnicode_AsEncodedString(frame->f_code->co_filename, "utf-8", "surrogatepass"));
-            if (strstr(filename, DD_TRACE_INSTALLED_PREFIX) != NULL && strstr(filename, TESTS_PREFIX) == NULL){
+            char* filename =
+              PyBytes_AsString(PyUnicode_AsEncodedString(frame->f_code->co_filename, "utf-8", "surrogatepass"));
+            if (strstr(filename, DD_TRACE_INSTALLED_PREFIX) != NULL && strstr(filename, TESTS_PREFIX) == NULL) {
                 frame = frame->f_back;
                 continue;
             }
@@ -34,17 +36,21 @@ static PyObject* get_file_and_line(PyObject* self, PyObject* const* args, Py_ssi
             */
             int line = PyCode_Addr2Line(frame->f_code, frame->f_lasti);
             return PyTuple_Pack(2, frame->f_code->co_filename, Py_BuildValue("i", line));
-
         }
     }
     return PyTuple_Pack(2, Py_None, Py_None);
 }
 
-static PyMethodDef StacktraceMethods[] = {{"get_info_frame", ((PyCFunction) get_file_and_line), METH_FASTCALL, "stacktrace functions"},
-                                     {NULL, NULL, 0, NULL}};
+static PyMethodDef StacktraceMethods[] = {
+    { "get_info_frame", ((PyCFunction)get_file_and_line), METH_FASTCALL, "stacktrace functions" },
+    { NULL, NULL, 0, NULL }
+};
 
-static struct PyModuleDef stacktrace = {PyModuleDef_HEAD_INIT, "ddtrace.appsec.iast._stacktrace", "stacktrace module", -1,
-                                         StacktraceMethods};
+static struct PyModuleDef stacktrace = { PyModuleDef_HEAD_INIT,
+                                         "ddtrace.appsec.iast._stacktrace",
+                                         "stacktrace module",
+                                         -1,
+                                         StacktraceMethods };
 
 PyMODINIT_FUNC
 PyInit__stacktrace(void)
