559ca5445f3a56bd5c1dc449ea18b01fbf834b39
Yun Kim <yun.kim@datadoghq.com>
2022-10-25 19:24:58 -0400

    Pin cpuinfo instead of pytest-benchmark

--- riotfile.py
+++ riotfile.py
@@ -217,9 +217,10 @@ venv = Venv(
         Venv(
             pys=select_pys(),
             pkgs={
-                # pytest-benchmark>=4.x dropped support for Python<=3.6, keep pinned to 3.4.1
-                # See https://pytest-benchmark.readthedocs.io/en/latest/changelog.html#id1
-                "pytest-benchmark": "~=3.4.1",
+                # pytest-benchmark depends on cpuinfo which dropped support for Python<=3.6 in 9.0
+                # See https://github.com/workhorsy/py-cpuinfo/issues/177
+                "pytest-benchmark": latest,
+                "py-cpuinfo": "~=8.0.0",
                 "msgpack": latest,
                 # TODO: remove py dependency once https://github.com/ionelmc/pytest-benchmark/pull/227 is released
                 "py": latest,
