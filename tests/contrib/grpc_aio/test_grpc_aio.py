e4649c046bbe1608c57bbb282763044fa6f884b4
Yun Kim <yun.kim@datadoghq.com>
2022-10-25 12:55:57 -0400

    Fix skip in grpc_aio with Python 3.11

--- tests/contrib/grpc_aio/test_grpc_aio.py
+++ tests/contrib/grpc_aio/test_grpc_aio.py
@@ -397,7 +397,7 @@ async def test_analytics_without_rate(server_info, tracer):
 
 
 @pytest.mark.skipif(
-    sys.version_info == (3, 11, 0), reason="Segfaults in Python 3.11, see https://github.com/grpc/grpc/issues/31441"
+    sys.version_info >= (3, 11, 0), reason="Segfaults in Python 3.11, see https://github.com/grpc/grpc/issues/31441"
 )
 @pytest.mark.asyncio
 async def test_unary_exception(server_info, tracer):
@@ -433,7 +433,7 @@ async def test_unary_exception(server_info, tracer):
 
 
 @pytest.mark.skipif(
-    sys.version_info == (3, 11, 0), reason="Segfaults in Python 3.11, see https://github.com/grpc/grpc/issues/31441"
+    sys.version_info >= (3, 11, 0), reason="Segfaults in Python 3.11, see https://github.com/grpc/grpc/issues/31441"
 )
 @pytest.mark.asyncio
 async def test_unary_cancellation(server_info, tracer):
