bb739181004ac0875a677239d97ccfd6387f350a
Alberto Vara <alberto.vara@datadoghq.com>
2022-10-26 18:23:35 +0200

    feat(asm): send to the agent 1-click activation capabilities

--- ddtrace/appsec/utils.py
+++ ddtrace/appsec/utils.py
@@ -11,6 +11,13 @@ def _appsec_rc_features_is_enabled():
 
 
 def _appsec_rc_capabilities():
+    """return the bit of the composed capabilities in base64
+    bit 0: Reserved
+    bit 1: ASM Activation
+    bit 2: ASM Ip blocking
+
+    TODO: refactor to compose the string and encode it
+    """
     if _appsec_rc_features_is_enabled():
         return "Ag=="
     return ""
