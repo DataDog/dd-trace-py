335fb4236d8c012eb9beeb69d90f51bf16047dc2
Christophe Papazian <christophe.papazian@datadoghq.com>
2022-10-28 11:14:57 +0200

    chore(asm): fix smoke_test.py for the new ddwaf python module, try 2

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -5,6 +5,6 @@ import ddtrace.bootstrap.sitecustomize as module
 
 
 if __name__ == "__main__":
-    ddtrace.appsec._ddwaf.version()
+    ddtrace.appsec.ddwaf.version()
 
     sys.exit(0 if module.loaded else 1)
