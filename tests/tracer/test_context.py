5d6c7e7feee96f83fe4821c7788538fb792ce6cd
Munir Abdinur <munir.abdinur@datadoghq.com>
2022-11-01 12:55:41 -0400

    merge conflict: remove traceparent from context

--- tests/tracer/test_context.py
+++ tests/tracer/test_context.py
@@ -52,33 +52,6 @@ def test_not_eq(ctx1, ctx2):
     assert ctx1 != ctx2
 
 
-def test_traceparent():
-    def validate_traceparent(context, sampled_expected):
-        version_hex, traceid_hex, spanid_hex, sampled_hex = context._traceparent.split("-")
-        assert version_hex == "00"
-
-        assert len(traceid_hex) == 32
-        assert traceid_hex == "{:032x}".format(context.trace_id)
-
-        assert len(spanid_hex) == 16
-        assert spanid_hex == "{:016x}".format(context.span_id)
-
-        assert len(sampled_hex) == 2
-        assert sampled_hex == sampled_expected
-
-    span = Span("span_a")
-    span.context.sampling_priority = -1
-    validate_traceparent(span.context, "00")
-
-    span = Span("span_b")
-    span.context.sampling_priority = 0
-    validate_traceparent(span.context, "00")
-
-    span = Span("span_c")
-    span.context.sampling_priority = 1
-    validate_traceparent(span.context, "01")
-
-
 @pytest.mark.parametrize(
     "context",
     [
