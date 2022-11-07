dd14281e27177d5f08d0464f71802806b8827f33
Munir Abdinur <munir.abdinur@datadoghq.com>
2022-11-07 11:43:43 -0500

    ci(pytest-bdd): pin pytest-bdd<6.1 (#4486)
    (cherry picked from commit c5c8821a7e5805583a368e843c511bbf529473f0)

--- riotfile.py
+++ riotfile.py
@@ -1502,7 +1502,8 @@ venv = Venv(
                             pkgs={
                                 "pytest-bdd": [
                                     ">=4.0,<5.0",
-                                    ">=6.0,<7.0",
+                                    # FIXME: add support for v6.1
+                                    ">=6.0,<6.1",
                                 ]
                             },
                         ),
@@ -1511,8 +1512,8 @@ venv = Venv(
                             pkgs={
                                 "pytest-bdd": [
                                     ">=4.0,<5.0",
-                                    ">=6.0,<7.0",
-                                    latest,
+                                    # FIXME: add support for v6.1
+                                    ">=6.0,<6.1",
                                 ]
                             },
                         ),
