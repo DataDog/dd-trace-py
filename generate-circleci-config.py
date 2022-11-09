ab5191b2f83f05aca908a81a6d23274a1b085127
Christophe Papazian <christophe.papazian@datadoghq.com>
2022-11-09 16:15:37 +0100

    two workflows defined together, one with approval

--- generate-circleci-config.py
+++ generate-circleci-config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from argparse import ArgumentParser
 import collections
+from copy import deepcopy
 import sys
 
 import yaml
@@ -25,18 +25,19 @@ def get_jobs_from_riot(venv: Venv) -> dict:
     return result_dict
 
 
-def generate_main_workflow(latest: bool) -> None:
+def generate_main_workflow() -> None:
     defined_jobs = get_jobs_from_riot(venv)
     circleci_config["jobs"].update(defined_jobs)
 
     BASE_JOBS = {"pre_check", "ccheck", "build_base_venvs"}
     NO_COVERAGE = BASE_JOBS | {"coverage_report", "graphene", "build_docs", "internal"}
     CHECKONLY_JOBS = ["build_docs"] + [f"profile-windows-3{i}" for i in (5, 6, 8, 9, 10)]
+    BASE_REQUIREMENTS = []
     DEFAULT_REQUIREMENTS = ["pre_check", "ccheck", "build_base_venvs"]
     CHECK_REQUIREMENTS = ["pre_check", "ccheck"]
     COVERAGE_REQUIREMENTS = [job for job in circleci_config["jobs"] if job not in NO_COVERAGE]
 
-    test_workflow = "latest_test" if latest else "test"
+    test_workflow = "test"
 
     # fast tests to check everything's ok quickly
     KEEP_TESTS = NO_COVERAGE | {"aredis", "pylons", "jinja2"}
@@ -51,32 +52,12 @@ def generate_main_workflow(latest: bool) -> None:
     # requirements and coverage reports are after all other tests.
     requirements = collections.defaultdict(lambda: DEFAULT_REQUIREMENTS)
     for jobs, reqs in [
-        (BASE_JOBS, []),
+        (BASE_JOBS, BASE_REQUIREMENTS),
         (CHECKONLY_JOBS, CHECK_REQUIREMENTS),
         (["coverage_report"], COVERAGE_REQUIREMENTS),
     ]:
         for job in jobs:
             requirements[job] = reqs
-
-    # if not latest:
-    #     # build the latest continuance
-    #     circleci_config["jobs"]["setup_latest"] = {
-    #         "executor": "continuation/default",
-    #         "steps": [
-    #             "checkout",
-    #             {"run": "sudo apt-get install python3-pip"},
-    #             {"run": "python3 -m pip install pyYAML riot"},
-    #             {
-    #                 "run": {
-    #                     "name": "Generate config for latest workflow",
-    #                     "command": "./generate-circleci-config.py --latest > generated_config_latest.yml",
-    #                     "environment": {"DD_USE_LATEST_VERSIONS": "true"},
-    #                 }
-    #             },
-    #             {"continuation/continue": {"configuration_path": "generated_config_latest.yml"}},
-    #         ],
-    #     }
-    #     requirements["setup_latest"] = ["coverage_report"]
 
     # Populating the jobs of tests with the appropriate requirements and environment
     circleci_config["workflows"][test_workflow] = {"jobs": []}
@@ -84,28 +65,49 @@ def generate_main_workflow(latest: bool) -> None:
         circleci_config["workflows"][test_workflow]["jobs"].append({name: {"requires": requirements[name]}})
         if "environment" not in circleci_config["jobs"][name]:
             circleci_config["jobs"][name]["environment"] = []
-        circleci_config["jobs"][name]["environment"].append({"DD_USE_LATEST_VERSIONS": "true" if latest else "false"})
-        if latest and name == "pre_check":
-            circleci_config["workflows"][test_workflow]["jobs"][-1][name]["type"] = "approval"
+        circleci_config["jobs"][name]["environment"].append({"DD_USE_LATEST_VERSIONS": "false"})
+        # if latest and name == "pre_check":
+        #     circleci_config["workflows"][test_workflow]["jobs"][-1][name]["type"] = "approval"
     # if latest:
     #     # patch tests to use latest version of packages
     #     run_test = circleci_config["commands"]["run_test"]["steps"][3]["when"]["steps"][2]["run"]
     #     run_test["environment"]["DD_USE_LATEST_VERSIONS"] = "true"
     # else:
-    if not latest:
-        # nightly tests are the same as tests but with specific triggers
-        circleci_config["workflows"]["test_nightly"] = {
-            "triggers": [{"schedule": {"cron": "0 0 * * *", "filters": {"branches": {"only": ["0.x", "1.x"]}}}}],
-        }
-        circleci_config["workflows"]["test_nightly"]["jobs"] = circleci_config["workflows"]["test"]["jobs"]
 
+    # nightly tests are the same as tests but with specific triggers
+    circleci_config["workflows"]["test_nightly"] = {
+        "triggers": [{"schedule": {"cron": "0 0 * * *", "filters": {"branches": {"only": ["0.x", "1.x"]}}}}],
+    }
+    circleci_config["workflows"]["test_nightly"]["jobs"] = circleci_config["workflows"]["test"]["jobs"]
+
+    def latest_name(name):
+        return name + "_latest"
+
+    def latest_requirements(name, cache_req=[], cache_req_latest=[]):
+        initial_req = requirements[name]
+        for req, req_l in zip(cache_req, cache_req_latest):
+            if initial_req is req:
+                return req_l
+        cache_req.append(initial_req)
+        res = [latest_name(n) for n in initial_req]
+        if not res:
+            res.append("wait_for_approval")
+        cache_req_latest.append(res)
+        return res
+
+    # Build latest_workflow
+    test_latest = latest_name(test_workflow)
+    circleci_config["workflows"][test_latest] = {"jobs": []}
+    #
+    for name in list(circleci_config["jobs"]):
+        lname = latest_name(name)
+        circleci_config["workflows"][test_latest]["jobs"].append({lname: {"requires": latest_requirements(name)}})
+        circleci_config["jobs"][lname] = deepcopy(circleci_config["jobs"][name])
+        circleci_config["jobs"][lname]["environment"][-1]["DD_USE_LATEST_VERSIONS"] = "true"
+    circleci_config["jobs"]["wait_for_approval"] = {"type": "approval"}
+    circleci_config["workflows"][test_latest]["jobs"].append("wait_for_approval")
     yaml.dump(circleci_config, sys.stdout, default_flow_style=False)
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(
-        prog="generate-circleci-config", description="Automatically generate CircleCI configuration yaml file"
-    )
-    parser.add_argument("--latest", action="store_true")
-    args = parser.parse_args()
-    generate_main_workflow(args.latest)
+    generate_main_workflow()
