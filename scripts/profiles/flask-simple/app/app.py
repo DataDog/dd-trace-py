b157b89c2e48da87fbe3287a16489de65b8aa812
Yun Kim <yun.kim@datadoghq.com>
2022-11-11 17:42:21 -0500

    Update release note, keep v0.3 as worst case endpoint

--- scripts/profiles/flask-simple/app/app.py
+++ scripts/profiles/flask-simple/app/app.py
@@ -1,3 +1,5 @@
+from os import environ
+
 from flask import Flask
 
 
@@ -6,10 +8,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def entry_point():
-    from ddtrace import tracer
+    if environ.get("MULTIPLE_TAGS_SCENARIO", None):
+        from ddtrace import tracer
 
-    span = tracer.current_span()
-    if span:
-        for _ in range(100):
-            span.set_tag("asd", "a" * 100)
+        span = tracer.current_span()
+        if span:
+            for _ in range(100):
+                span.set_tag("asd", "a" * 100)
     return "Hello World!"
