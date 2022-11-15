a94c3546ad11ef5ed90164e1c020e1b8bf6b8862
roberto montero <roberto.montero@datadoghq.com>
2022-11-15 08:31:18 +0100

    python lib injection

--- lib-injection/copy-lib.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# This script is used by the admission controller to install the library from the
-# init container into the application container.
-cp sitecustomize.py "$1/sitecustomize.py"
