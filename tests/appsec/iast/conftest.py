239f9154c6f5bc82694eb297ea6826ad8445f0ad
Federico Mon <federico.mon@datadoghq.com>
2022-11-22 12:01:46 +0100

    feat(iast): add env var DD_IAST_WEAK_HASH_ALGORITHMS (#4558)
    ## Description
    The list of weak hashing algorithms to report is configurable through the env var `DD_IAST_WEAK_HASH_ALGORITHMS`
    
    ## Motivation
    This allows customers to configure which algorithms they want reported.
    
    ## Design 
    `DD_IAST_WEAK_HASH_ALGORITHMS` is a comma-separated list of weak hashing algorithms, if unset, it will default to the ones that we detect currently: MD5 and SHA1
    
    ## Testing strategy
    Unit tests with the env var missing, configured to each single one of the algorithms, both and other invalid algorithms.
    
    ## Reviewer Checklist
    - [ ] Title is accurate.
    - [ ] Description motivates each change.
    - [ ] No unnecessary changes were introduced in this PR.
    - [ ] Avoid breaking [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces) changes unless absolutely necessary.
    - [ ] Tests provided or description of manual testing performed is included in the code or PR.
    - [ ] Release note has been added for fixes and features, or else `changelog/no-changelog` label added.
    - [ ] All relevant GitHub issues are correctly linked.
    - [ ] Backports are identified and tagged with Mergifyio.

--- tests/appsec/iast/conftest.py
+++ tests/appsec/iast/conftest.py
@@ -1,16 +1,46 @@
 import pytest
 
-from ddtrace._monkey import IAST_PATCH
-from ddtrace._monkey import patch_iast
 from ddtrace.appsec.iast import oce
+from ddtrace.appsec.iast.taint_sinks.weak_hash import patch
+from ddtrace.appsec.iast.taint_sinks.weak_hash import unpatch_iast
 from tests.utils import override_env
 
 
-@pytest.fixture
-def iast_span(tracer):
-    with override_env(dict(DD_IAST_ENABLED="true")):
+def iast_span(tracer, env):
+    with override_env(env):
         with tracer.trace("test") as span:
-            patch_iast(**IAST_PATCH)
+            patch()
             oce.acquire_request()
             yield span
             oce.release_request()
+            unpatch_iast()
+
+
+@pytest.fixture
+def iast_span_defaults(tracer):
+    for t in iast_span(tracer, dict(DD_IAST_ENABLED="true")):
+        yield t
+
+
+@pytest.fixture
+def iast_span_md5_and_sha1_configured(tracer):
+    for t in iast_span(tracer, dict(DD_IAST_ENABLED="true", DD_IAST_WEAK_HASH_ALGORITHMS="MD5, SHA1")):
+        yield t
+
+
+@pytest.fixture
+def iast_span_only_md4(tracer):
+    for t in iast_span(tracer, dict(DD_IAST_ENABLED="true", DD_IAST_WEAK_HASH_ALGORITHMS="MD4")):
+        yield t
+
+
+@pytest.fixture
+def iast_span_only_md5(tracer):
+    for t in iast_span(tracer, dict(DD_IAST_ENABLED="true", DD_IAST_WEAK_HASH_ALGORITHMS="MD5")):
+        yield t
+
+
+@pytest.fixture
+def iast_span_only_sha1(tracer):
+    for t in iast_span(tracer, dict(DD_IAST_ENABLED="true", DD_IAST_WEAK_HASH_ALGORITHMS="SHA1")):
+        yield t
