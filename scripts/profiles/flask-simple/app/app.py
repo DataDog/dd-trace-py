45077d5a708cda6e7b088299d9714dc5a239ef56
Yun Kim <yun.kim@datadoghq.com>
2022-11-22 11:01:59 -0500

    Add worst-case unique tags scenario to flask app

--- scripts/profiles/flask-simple/app/app.py
+++ scripts/profiles/flask-simple/app/app.py
@@ -1,4 +1,6 @@
 from os import environ
+import random
+import string
 
 from flask import Flask
 
@@ -8,11 +10,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def entry_point():
-    if environ.get("MULTIPLE_TAGS_SCENARIO", None):
+    if environ.get("DUPLICATE_TAGS_SCENARIO", None):
         from ddtrace import tracer
 
         span = tracer.current_span()
         if span:
             for _ in range(100):
-                span.set_tag("asd", "a" * 100)
+                span.set_tag(_, "a" * 100)
+    elif environ.get("UNIQUE_TAGS_SCENARIO", None):
+        from ddtrace import tracer
+
+        span = tracer.current_span()
+        if span:
+            for num in range(100):
+                span.set_tag(str(num), "".join(random.choices(string.ascii_letters, k=100)))
+
     return "Hello World!"
