f2f388bab04a837bbc6d3f95a1a14a30d2d7ad25
Yun Kim <yun.kim@datadoghq.com>
2022-12-02 18:16:45 -0500

    replace set_tag usage with set_tag_str

--- ddtrace/contrib/aiohttp_jinja2/patch.py
+++ ddtrace/contrib/aiohttp_jinja2/patch.py
@@ -30,7 +30,7 @@ def traced_render_template(aiohttp_jinja2, pin, func, instance, args, kwargs):
     template_meta = "%s/%s" % (template_prefix, template_name)
 
     with pin.tracer.trace("aiohttp.template", span_type=SpanTypes.TEMPLATE) as span:
-        span.set_tag("aiohttp.template", template_meta)
+        span.set_tag_str("aiohttp.template", template_meta)
         return func(*args, **kwargs)
 
 
