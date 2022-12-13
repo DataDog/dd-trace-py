a38189c58df0e87cf0d028afce6489f6661425ee
Munir Abdinur <munir_abdinur@hotmail.com>
2022-12-13 17:36:42 -0500

    also override inject

--- tests/tracer/test_propagation.py
+++ tests/tracer/test_propagation.py
@@ -69,7 +69,9 @@ def test_inject_tags_unicode(tracer):
 def test_inject_tags_bytes(tracer):
     """We properly encode when the meta key as long as it is just ascii characters"""
     # Context._meta allows str and bytes for keys
-    with override_global_config({"_propagation_style_extract": PROPAGATION_STYLE_ALL}):
+    with override_global_config(
+        {"_propagation_style_extract": PROPAGATION_STYLE_ALL, "_propagation_style_inject": PROPAGATION_STYLE_ALL}
+    ):
         meta = {u"_dd.p.test": b"bytes"}
         ctx = Context(trace_id=1234, sampling_priority=2, dd_origin="synthetics", meta=meta)
         tracer.context_provider.activate(ctx)
