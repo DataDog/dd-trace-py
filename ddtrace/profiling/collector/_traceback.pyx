3bd4a59da9ca016ec4c2524b53dc6782d87c439b
Yun Kim <yun.kim@datadoghq.com>
2023-01-12 14:39:14 -0500

    Use getter functions to access frame members

--- ddtrace/profiling/collector/_traceback.pyx
+++ ddtrace/profiling/collector/_traceback.pyx
@@ -4,19 +4,30 @@ cpdef _extract_class_name(frame):
 
     :param frame: The frame object.
     """
-    if frame.f_code.co_varnames:
-        argname = frame.f_code.co_varnames[0]
-        try:
-            value = frame.f_locals[argname]
-        except KeyError:
-            return ""
-        try:
-            if argname == "self":
-                return object.__getattribute__(type(value), "__name__")  # use type() and object.__getattribute__ to avoid side-effects
-            if argname == "cls":
-                return object.__getattribute__(value, "__name__")
-        except AttributeError:
-            return ""
+    # Python 3.11 moved PyFrameObject to internal C API and cannot be directly accessed from tstate
+    IF PY_MAJOR_VERSION >= 3 and PY_MINOR_VERSION >= 11:
+        co_varnames = PyFrame_GetCode(frame).co_varnames
+        if co_varnames:
+            argname = co_varnames[0]
+            try:
+                value = PyFrame_GetLocals(frame)[argname]
+            except KeyError:
+                return ""
+    ELSE:
+        co_varnames = frame.f_code.co_varnames
+        if co_varnames:
+            argname = co_varnames[0]
+            try:
+                value = frame.f_locals[argname]
+            except KeyError:
+                return ""
+    try:
+        if argname == "self":
+            return object.__getattribute__(type(value), "__name__")  # use type() and object.__getattribute__ to avoid side-effects
+        if argname == "cls":
+            return object.__getattribute__(value, "__name__")
+    except AttributeError:
+        return ""
     return ""
 
 
@@ -33,8 +44,14 @@ cpdef traceback_to_frames(traceback, max_nframes):
     while tb is not None:
         if nframes < max_nframes:
             frame = tb.tb_frame
-            code = frame.f_code
-            lineno = 0 if frame.f_lineno is None else frame.f_lineno
+            # Python 3.11 moved PyFrameObject to internal C API and cannot be directly accessed from tstate
+            IF PY_MAJOR_VERSION >= 3 and PY_MINOR_VERSION >= 11:
+                code = PyFrame_GetCode(frame)
+                lineno = PyFrame_GetLineNumber(frame)
+                lineno = 0 if lineno is None else lineno
+            ELSE:
+                code = frame.f_code
+                lineno = 0 if frame.f_lineno is None else frame.f_lineno
             frames.insert(0, (code.co_filename, lineno, code.co_name, _extract_class_name(frame)))
         nframes += 1
         tb = tb.tb_next
@@ -51,9 +68,18 @@ cpdef pyframe_to_frames(frame, max_nframes):
     nframes = 0
     while frame is not None:
         nframes += 1
-        if len(frames) < max_nframes:
-            code = frame.f_code
-            lineno = 0 if frame.f_lineno is None else frame.f_lineno
-            frames.append((code.co_filename, lineno, code.co_name, _extract_class_name(frame)))
-        frame = frame.f_back
+        # Python 3.11 moved PyFrameObject to internal C API and cannot be directly accessed from tstate
+        IF PY_MAJOR_VERSION >= 3 and PY_MINOR_VERSION >= 11:
+            if len(frames) < max_nframes:
+                code = PyFrame_GetCode(frame)
+                lineno = PyFrame_GetLineNumber(frame)
+                lineno = 0 if lineno is None else lineno
+                frames.append((code.co_filename, lineno, code.co_name, _extract_class_name(frame)))
+            frame = PyFrame_GetBack(frame)
+        ELSE:
+            if len(frames) < max_nframes:
+                code = frame.f_code
+                lineno = 0 if frame.f_lineno is None else frame.f_lineno
+                frames.append((code.co_filename, lineno, code.co_name, _extract_class_name(frame)))
+            frame = frame.f_back
     return frames, nframes
