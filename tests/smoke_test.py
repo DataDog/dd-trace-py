8ac477968440db91d7256e3cd2b65a87c58ee39f
Federico Mon <federico.mon@datadoghq.com>
2023-01-12 15:06:25 +0100

    chore(asm): improve smoke test

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -7,4 +7,5 @@ import ddtrace.bootstrap.sitecustomize as module
 if __name__ == "__main__":
     ddtrace.appsec.ddwaf.version()
 
-    sys.exit(0 if module.loaded else 1)
+    ddwaf_loaded = ddtrace.appsec.ddwaf._DDWAF_LOADED
+    sys.exit(0 if module.loaded and ddwaf_loaded else 1)
