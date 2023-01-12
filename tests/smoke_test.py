e7ffecd52e238f1a7b0e350b331de0b043638942
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-01-12 18:58:24 +0100

    fix precheck

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -7,7 +7,7 @@ import ddtrace.bootstrap.sitecustomize as module
 
 if __name__ == "__main__":
     if system() == "Linux":
-        if not sys.maxsize > 2**32:
+        if not sys.maxsize > 2 ** 32:
             # 32-bit linux DDWAF not ready yet.
             sys.exit(0)
     ddtrace.appsec.ddwaf.version()
