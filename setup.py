1b4b495a39ec878a283e21e97e9a4b9ba9270ceb
Federico Mon <federico.mon@datadoghq.com>
2023-01-16 11:41:31 +0100

    chore(asm): include suggested edit

--- setup.py
+++ setup.py
@@ -155,10 +155,7 @@ class LibDDWaf_Download(BuildPyCommand):
             with tarfile.open(filename, "r|gz", errorlevel=2) as tar:
                 print("extracting files:", [c.name for c in dynfiles])
                 tar.extractall(members=dynfiles, path=HERE)
-
                 os.rename(os.path.join(HERE, ddwaf_archive_dir), arch_dir)
-                # cleaning unwanted files
-                tar.close()
 
             # Rename ddwaf.xxx to libddwaf.xxx so the filename is the same for every OS
             original_file = os.path.join(arch_dir, "lib", "ddwaf" + SUFFIX)
