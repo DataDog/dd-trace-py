dc55e49c9037754cb9c1740af60cd681b59f8848
Federico Mon <federico.mon@datadoghq.com>
2023-01-16 10:38:48 +0100

    chore(asm): suggested edits

--- setup.py
+++ setup.py
@@ -155,10 +155,7 @@ class LibDDWaf_Download(BuildPyCommand):
             with tarfile.open(filename, "r|gz", errorlevel=2) as tar:
                 print("extracting files:", [c.name for c in dynfiles])
                 tar.extractall(members=dynfiles, path=HERE)
-
                 os.rename(os.path.join(HERE, ddwaf_archive_dir), arch_dir)
-                # cleaning unwanted files
-                tar.close()
 
             # Rename ddwaf.xxx to libddwaf.xxx so the filename is the same for every OS
             original_file = os.path.join(arch_dir, "lib", "ddwaf" + SUFFIX)
@@ -176,8 +173,7 @@ class LibDDWaf_Download(BuildPyCommand):
 class CleanLibraries(CleanCommand):
     @staticmethod
     def remove_dynamic_library():
-        dst = os.path.join(HERE, os.path.join("ddtrace", "appsec", "ddwaf", "libddwaf"))
-        shutil.rmtree(dst, True)
+        shutil.rmtree(LIBDDWAF_DOWNLOAD_DIR, True)
 
     def run(self):
         CleanLibraries.remove_dynamic_library()
