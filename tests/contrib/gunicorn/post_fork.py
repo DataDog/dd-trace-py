9f0940d80e2609995681ed02ee359b5532b40f3a
Emmett Butler <723615+emmettbutler@users.noreply.github.com>
2023-01-27 13:54:07 -0800

    more futureproof version check
    Co-authored-by: Munir Abdinur <munir.abdinur@datadoghq.com>

--- tests/contrib/gunicorn/post_fork.py
+++ tests/contrib/gunicorn/post_fork.py
@@ -14,7 +14,7 @@ monkeypatch_happened = gevent.monkey.is_module_patched("threading")
 # enabling DI here allows test cases to exercise the code paths that handle
 # gevent monkeypatching of running threads
 # post_fork is called before gevent.monkey.patch_all()
-if sys.version_info[1] < 11:
+if sys.version_info < (3, 11):
     DynamicInstrumentation.enable()
 
 RemoteConfig._was_enabled_after_gevent_monkeypatch = monkeypatch_happened
