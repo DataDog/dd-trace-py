397a5d3456d63ce30ba0192d36c056731e57e6d3
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2023-01-31 12:33:08 +0000

    ci(debugging): expose tests folder as submodule to exploration tests
    This change ensures that the tests folder is exposed to the exploration
    tests as a module so that it can be used during the test runs. This is
    now necessary because we now require the use of some test helpers that
    have been introduced as part of the probe model refactor #4959.

--- tests/debugging/exploration/preload.py
+++ tests/debugging/exploration/preload.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
+
+# Make the tests/debugging folder available as a module. We don't want to
+# go as further back as tests/ because we are likely to override a folder with
+# a similar name from the target project.
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+
 import _coverage  # noqa
 import _profiler  # noqa
 
