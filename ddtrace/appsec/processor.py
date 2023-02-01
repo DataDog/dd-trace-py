02d3c0d08eca5b10ea6008098d34d20c5024ee16
Juanjo Alvarez Martinez <juanjo.alvarezmartinez@datadoghq.com>
2023-02-01 15:34:27 +0100

    Update ddtrace/appsec/processor.py
    Co-authored-by: Brett Langdon <brett.langdon@datadoghq.com>

--- ddtrace/appsec/processor.py
+++ ddtrace/appsec/processor.py
@@ -387,7 +387,7 @@ class AppSecSpanProcessor(SpanProcessor):
                 log.debug("[DDAS-011-00] AppSec In-App WAF returned: %s", ddwaf_result.data)
                 span.set_tag_str(APPSEC_JSON, '{"triggers":%s}' % (ddwaf_result.data,))
             else:
-                span.set_tag(APPSEC_JSON, _context.get_item("http.request.waf_json", span=span))
+                span.set_tag_str(APPSEC_JSON, _context.get_item("http.request.waf_json", span=span))
                 span.set_tag("appsec.blocked", True)
 
             # Partial DDAS-011-00
