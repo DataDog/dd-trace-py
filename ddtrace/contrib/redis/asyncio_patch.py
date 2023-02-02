49a095622910495941c06a151e714a4ee43a0f45
wconti27 <william.conti@datadoghq.com>
2023-02-02 16:34:35 -0500

    initial commit adding row count for redis

--- ddtrace/contrib/redis/asyncio_patch.py
+++ ddtrace/contrib/redis/asyncio_patch.py
@@ -2,6 +2,7 @@ from ddtrace import config
 
 from ...internal.utils.formats import stringify_cache_args
 from ...pin import Pin
+from .util import _run_redis_command_async
 from .util import _trace_redis_cmd
 from .util import _trace_redis_execute_pipeline
 
@@ -14,8 +15,8 @@ async def traced_async_execute_command(func, instance, args, kwargs):
     if not pin or not pin.enabled():
         return await func(*args, **kwargs)
 
-    with _trace_redis_cmd(pin, config.redis, instance, args):
-        return await func(*args, **kwargs)
+    with _trace_redis_cmd(pin, config.redis, instance, args) as span:
+        return await _run_redis_command_async(span=span, func=func, args=args, kwargs=kwargs)
 
 
 async def traced_async_execute_pipeline(func, instance, args, kwargs):
