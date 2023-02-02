a2163e5daf3fbc7a80712eb3dc002214717c79d8
Munir Abdinur <munir_abdinur@hotmail.com>
2023-02-02 12:01:56 -0500

    internalize methods, update documentation, and remove locks

--- ddtrace/internal/telemetry/writer.py
+++ ddtrace/internal/telemetry/writer.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 import time
 from typing import Dict
@@ -17,7 +18,6 @@ from ..periodic import PeriodicService
 from ..runtime import get_runtime_id
 from ..service import ServiceStatus
 from ..utils.formats import asbool
-from ..utils.formats import parse_tags_str
 from ..utils.time import StopWatch
 from .data import get_application
 from .data import get_dependencies
@@ -32,25 +32,63 @@ def _get_interval_or_default():
     return float(os.getenv("DD_TELEMETRY_HEARTBEAT_INTERVAL", default=60))
 
 
+class _TelemetryClient:
+    def __init__(self, endpoint, headers):
+        # type: (str, Dict) -> None
+
+        self._agent_url = get_trace_url()
+        self._endpoint = endpoint
+        self._encoder = JSONEncoderV2()
+        self._headers = headers.copy()
+
+    def url(self):
+        return "%s/%s" % (self._agent_url, self._endpoint)
+
+    def send_event(self, request):
+        # type: (Dict) -> Optional[httplib.HTTPResponse]
+        """Sends a telemetry request to the trace agent"""
+        resp = None
+        try:
+            rb_json = self._encoder.encode(request)
+            headers = self._create_headers(request["request_type"])
+            with StopWatch() as sw:
+                conn = get_connection(self._agent_url)
+                conn.request("POST", self._endpoint, rb_json, headers)
+                resp = get_connection_response(conn)
+            if resp.status < 300:
+                log.debug("sent %d in %.5fs to %s. response: %s", len(rb_json), sw.elapsed(), self.url, resp.status)
+            else:
+                log.debug("failed to send telemetry to the Datadog Agent at %s. response: %s", self.url, resp.status)
+        except Exception:
+            log.debug("failed to send telemetry to the Datadog Agent at %s.", self.url, exc_info=True)
+        finally:
+            conn.close()
+        return resp
+
+    def _create_headers(self, payload_type):
+        # type: (str) -> Dict
+        """Creates request headers"""
+        headers = self._headers.copy()
+        headers["DD-Telemetry-Request-Type"] = payload_type
+        return headers
+
+
 class TelemetryWriter(PeriodicService):
     """
-    Periodic service which sends Telemetry request payloads to the agent.
-    Supports version one of the instrumentation telemetry api
+    Submits Instrumentation Telemetry events to the datadog agent.
+    Supports v1 of the instrumentation telemetry api
     """
 
     # telemetry endpoint uses events platform v2 api
-    ENDPOINT = "telemetry/proxy/api/v2/apmtelemetry"
+    ENDPOINT_V1 = "telemetry/proxy/api/v2/apmtelemetry"
 
-    def __init__(self, agent_url=None):
-        # type: (Optional[str]) -> None
+    def __init__(self):
+        # type: () -> None
         super(TelemetryWriter, self).__init__(interval=_get_interval_or_default())
 
         # _enabled is None at startup, and is only set to true or false
         # after the config has been processed
         self._enabled = None  # type: Optional[bool]
-        self._agent_url = agent_url or get_trace_url()
-
-        self._encoder = JSONEncoderV2()
         self._events_queue = []  # type: List[Dict]
         self._integrations_queue = []  # type: List[Dict]
         self._lock = forksafe.Lock()  # type: forksafe.ResetObject
@@ -59,47 +97,18 @@ class TelemetryWriter(PeriodicService):
         self._debug = asbool(os.environ.get("DD_TELEMETRY_DEBUG", "false"))
         forksafe.register(self._fork_writer)
 
-        self._headers = {
+        # counter representing the number of events sent by the writer
+        self._sequence = itertools.count(1)
+        headers = {
             "Content-type": "application/json",
             "DD-Telemetry-API-Version": "v1",
             "DD-Telemetry-Debug-Enabled": str(self._debug).lower(),
         }  # type: Dict[str, str]
-        additional_header_str = os.environ.get("_DD_TELEMETRY_WRITER_ADDITIONAL_HEADERS")
-        if additional_header_str is not None:
-            self._headers.update(parse_tags_str(additional_header_str))
-
-        # _sequence is a counter representing the number of requests sent by the writer
-        self._sequence = 1  # type: int
-
-    @property
-    def url(self):
-        return "%s/%s" % (self._agent_url, self.ENDPOINT)
-
-    def _send_request(self, request):
-        # type: (Dict) -> httplib.HTTPResponse
-        """Sends a telemetry request to the trace agent"""
-        with StopWatch() as sw:
-            try:
-                conn = get_connection(self._agent_url)
-                rb_json = self._encoder.encode(request)
-                conn.request("POST", self.ENDPOINT, rb_json, self._create_headers(request["request_type"]))
-
-                resp = get_connection_response(conn)
-                log.debug(
-                    "sent %d in %.5fs to %s/%s. response: %s",
-                    len(rb_json),
-                    sw.elapsed(),
-                    self._agent_url,
-                    self.ENDPOINT,
-                    resp.status,
-                )
-                return resp
-            finally:
-                conn.close()
+        self._client = _TelemetryClient(self.ENDPOINT_V1, headers)
 
     def _flush_integrations_queue(self):
         # type: () -> List[Dict]
-        """Returns a list of all integrations queued by add_integration"""
+        """Flushes and returns a list of all queued integrations"""
         with self._lock:
             integrations = self._integrations_queue
             self._integrations_queue = []
@@ -107,17 +116,16 @@ class TelemetryWriter(PeriodicService):
 
     def _flush_events_queue(self):
         # type: () -> List[Dict]
-        """Returns a list of all integrations queued by classmethods"""
+        """Flushes and returns a list of all telemtery event"""
         with self._lock:
-            requests = self._events_queue
+            events = self._events_queue
             self._events_queue = []
-        return requests
+        return events
 
     def reset_queues(self):
         # type: () -> None
-        with self._lock:
-            self._integrations_queue = []
-            self._events_queue = []
+        self._integrations_queue = []
+        self._events_queue = []
 
     def periodic(self):
         integrations = self._flush_integrations_queue()
@@ -126,31 +134,16 @@ class TelemetryWriter(PeriodicService):
 
         if not self._events_queue:
             # Optimization: only queue heartbeat if no other events are queued
-            self.app_heartbeat_event()
+            self._app_heartbeat_event()
 
-        telemetry_requests = self._flush_events_queue()
+        telemetry_events = self._flush_events_queue()
 
-        for telemetry_request in telemetry_requests:
-            try:
-                resp = self._send_request(telemetry_request)
-                if resp.status >= 300:
-                    log.debug(
-                        "failed to send telemetry to the Datadog Agent at %s/%s. response: %s",
-                        self._agent_url,
-                        self.ENDPOINT,
-                        resp.status,
-                    )
-            except Exception:
-                log.debug(
-                    "failed to send telemetry to the Datadog Agent at %s/%s.",
-                    self._agent_url,
-                    self.ENDPOINT,
-                    exc_info=True,
-                )
+        for telemetry_event in telemetry_events:
+            self._client.send_event(telemetry_event)
 
     def _start_service(self, *args, **kwargs):
         # type: (...) -> None
-        self.app_started_event()
+        self._app_started_event()
         return super(TelemetryWriter, self)._start_service(*args, **kwargs)
 
     def on_shutdown(self):
@@ -165,19 +158,27 @@ class TelemetryWriter(PeriodicService):
     def add_event(self, payload, payload_type):
         # type: (Dict, str) -> None
         """
-        Adds a Telemetry Request to the TelemetryWriter request buffer
+        Adds a Telemetry event to the TelemetryWriter event buffer
 
-        :param Dict payload: stores a formatted telemetry request
+        :param Dict payload: stores a formatted telemetry event
         :param str payload_type: The payload_type denotes the type of telmetery request.
             Payload types accepted by telemetry/proxy v1: app-started, app-closing, app-integrations-change
         """
-        with self._lock:
-            if not self._enabled:
-                return
+        if not self._enabled:
+            return
 
-            request = self._create_telemetry_request(payload, payload_type, self._sequence)
-            self._sequence += 1
-            self._events_queue.append(request)
+        event = {
+            "tracer_time": int(time.time()),
+            "runtime_id": get_runtime_id(),
+            "api_version": "v1",
+            "seq_id": next(self._sequence),
+            "debug": self._debug,
+            "application": get_application(config.service, config.version, config.env),
+            "host": get_host_info(),
+            "payload": payload,
+            "request_type": payload_type,
+        }
+        self._events_queue.append(event)
 
     def add_integration(self, integration_name, auto_enabled):
         # type: (str, bool) -> None
@@ -187,21 +188,20 @@ class TelemetryWriter(PeriodicService):
         :param str integration_name: name of patched module
         :param bool auto_enabled: True if module is enabled in _monkey.PATCH_MODULES
         """
-        with self._lock:
-            if self._enabled is not None and not self._enabled:
-                return
+        if self._enabled is not None and not self._enabled:
+            return
 
-            integration = {
-                "name": integration_name,
-                "version": "",
-                "enabled": True,
-                "auto_enabled": auto_enabled,
-                "compatible": True,
-                "error": "",
-            }
-            self._integrations_queue.append(integration)
+        integration = {
+            "name": integration_name,
+            "version": "",
+            "enabled": True,
+            "auto_enabled": auto_enabled,
+            "compatible": True,
+            "error": "",
+        }
+        self._integrations_queue.append(integration)
 
-    def app_started_event(self):
+    def _app_started_event(self):
         # type: () -> None
         """Sent when TelemetryWriter is enabled or forks"""
         if self._forked:
@@ -214,7 +214,7 @@ class TelemetryWriter(PeriodicService):
         }
         self.add_event(payload, "app-started")
 
-    def app_heartbeat_event(self):
+    def _app_heartbeat_event(self):
         # type: () -> None
         if self._forked:
             # TODO: Enable app-heartbeat on forks
@@ -228,7 +228,7 @@ class TelemetryWriter(PeriodicService):
 
     def _app_closing_event(self):
         # type: () -> None
-        """Adds a Telemetry request which notifies the agent that an application instance has terminated"""
+        """Adds a Telemetry event which notifies the agent that an application instance has terminated"""
         if self._forked:
             # app-closing event should only be sent by the main process
             return
@@ -237,33 +237,11 @@ class TelemetryWriter(PeriodicService):
 
     def _app_integrations_changed_event(self, integrations):
         # type: (List[Dict]) -> None
-        """Adds a Telemetry request which sends a list of configured integrations to the agent"""
+        """Adds a Telemetry event which sends a list of configured integrations to the agent"""
         payload = {
             "integrations": integrations,
         }
         self.add_event(payload, "app-integrations-change")
-
-    def _create_headers(self, payload_type):
-        # type: (str) -> Dict
-        """Creates request headers"""
-        headers = self._headers.copy()
-        headers["DD-Telemetry-Request-Type"] = payload_type
-        return headers
-
-    def _create_telemetry_request(self, payload, payload_type, sequence_id):
-        # type: (Dict, str, int) -> Dict
-        """Initializes the required fields for a generic Telemetry Intake Request"""
-        return {
-            "tracer_time": int(time.time()),
-            "runtime_id": get_runtime_id(),
-            "api_version": "v1",
-            "seq_id": sequence_id,
-            "debug": self._debug,
-            "application": get_application(config.service, config.version, config.env),
-            "host": get_host_info(),
-            "payload": payload,
-            "request_type": payload_type,
-        }
 
     def _fork_writer(self):
         # type: () -> None
