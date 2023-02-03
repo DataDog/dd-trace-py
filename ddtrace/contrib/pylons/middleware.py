e79c1015a828b7b55bdd8431d82035fbd1528121
Federico Mon <federico.mon@datadoghq.com>
2023-02-03 16:38:20 +0100

    chore(asm): add errors=ignore back

--- ddtrace/contrib/pylons/middleware.py
+++ ddtrace/contrib/pylons/middleware.py
@@ -107,11 +107,11 @@ class PylonsTraceMiddleware(object):
                         if hasattr(request, "json"):
                             req_body = request.json
                         else:
-                            req_body = json.loads(request.body.decode(request.charset))
+                            req_body = json.loads(request.body.decode(request.charset, errors="ignore"))
                     elif content_type in ("application/xml", "text/xml"):
-                        req_body = xmltodict.parse(request.body.decode(request.charset))
+                        req_body = xmltodict.parse(request.body.decode(request.charset, errors="ignore"))
                     else:  # text/plain, xml, others: take them as strings
-                        req_body = request.body.decode(request.charset)
+                        req_body = request.body.decode(request.charset, errors="ignore")
 
                 except (
                     AttributeError,
