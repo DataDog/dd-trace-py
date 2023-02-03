1d9b537c5dc19ed65ce1e58bc3ee4f5206fcdab7
wconti27 <william.conti@datadoghq.com>
2023-02-03 15:15:54 -0500

    fix syntax error

--- ddtrace/contrib/django/patch.py
+++ ddtrace/contrib/django/patch.py
@@ -152,7 +152,7 @@ def traced_cache(django, pin, func, instance, args, kwargs):
 
         result = func(*args, **kwargs)
         print("-" * 50)
-        print("kwargs", *kwargs)
+        print("kwargs", **kwargs)
         print("args", *args)
         print("func", func)
         print("command", command_name)
