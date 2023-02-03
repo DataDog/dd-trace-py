fb4cc26a8fe70610f16eff46da1e261dcaa46b5b
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-02-03 10:55:16 +0100

    fix(apm): UnicodeDecodeError in pylons

--- ddtrace/contrib/pylons/middleware.py
+++ ddtrace/contrib/pylons/middleware.py
@@ -107,11 +107,11 @@ class PylonsTraceMiddleware(object):
                         if hasattr(request, "json"):
                             req_body = request.json
                         else:
-                            req_body = json.loads(request.body.decode("UTF-8"))
+                            req_body = json.loads(request.body.decode("UTF-8", "ignore"))
                     elif content_type in ("application/xml", "text/xml"):
-                        req_body = xmltodict.parse(request.body.decode("UTF-8"))
+                        req_body = xmltodict.parse(request.body.decode("UTF-8", "ignore"))
                     else:  # text/plain, xml, others: take them as strings
-                        req_body = request.body.decode("UTF-8")
+                        req_body = request.body.decode("UTF-8", "ignore")
 
                 except (
                     AttributeError,
