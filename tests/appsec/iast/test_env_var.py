88f7950f10922e65dac4d7a3f2fb86176cd2e1e6
Federico Mon <federico.mon@datadoghq.com>
2023-02-03 08:43:57 +0100

    chore(asm): fix tests

--- tests/appsec/iast/test_env_var.py
+++ tests/appsec/iast/test_env_var.py
@@ -6,7 +6,7 @@ import sys
 import pytest
 
 
-def _run_python_file(*args, env=None):
+def _run_python_file(*args, **kwargs):
     current_dir = os.path.dirname(__file__)
     cmd = [
         "ddtrace-run",
@@ -14,14 +14,14 @@ def _run_python_file(*args, env=None):
         "python",
         os.path.join(current_dir, "fixtures", "integration", "main.py"),
     ] + list(args)
-    if env:
-        ret = subprocess.run(cmd, cwd=current_dir, env=env)
+    if "env" in kwargs:
+        ret = subprocess.run(cmd, cwd=current_dir, env=kwargs["env"])
     else:
         ret = subprocess.run(cmd, cwd=current_dir)
     assert ret.returncode == 0
 
 
-@pytest.mark.skipif(sys.version_info[0] < 3, reason="Not testing Python 2")
+@pytest.mark.skipif((3, 6, 0) < sys.version_info < (3, 11, 0), reason="IAST compatible versions")
 def test_env_var_iast_enabled(capfd):
     # type: (...) -> None
     env = os.environ.copy()
