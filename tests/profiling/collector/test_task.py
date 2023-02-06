7b8bcbcbfa5c79ab69c8cdf3dfe0bb1e6650c863
Emmett Butler <emmett.butler321@gmail.com>
2023-02-06 11:43:15 -0800

    put another gevent test in a subprocess

--- tests/profiling/collector/test_task.py
+++ tests/profiling/collector/test_task.py
@@ -1,70 +1,68 @@
-import os
+import threading
+
+import pytest
+
+from ddtrace.internal import compat
+from ddtrace.profiling.collector import _task
 
 
-# from ddtrace.internal import compat
+def test_get_task_main():
+    # type: (...) -> None
+    if _task._gevent_tracer is None:
+        assert _task.get_task(threading.main_thread().ident) == (None, None, None)
+    else:
+        assert _task.get_task(threading.main_thread().ident) == (compat.main_thread.ident, "MainThread", None)
 
 
-# import threading
-
-# import pytest
-
-
-# from ddtrace.profiling.collector import _task
+def test_list_tasks_nogevent():
+    assert _task.list_tasks(threading.main_thread().ident) == []
 
 
-TESTING_GEVENT = os.getenv("DD_PROFILE_TEST_GEVENT", False)
+@pytest.mark.subprocess
+def test_list_tasks_gevent():
+    import gevent.monkey
 
+    gevent.monkey.patch_all()
+    import threading
 
-# def test_get_task_main():
-#     # type: (...) -> None
-#     if _task._gevent_tracer is None:
-#         assert _task.get_task(nogevent.main_thread_id) == (None, None, None)
-#     else:
-#         assert _task.get_task(nogevent.main_thread_id) == (compat.main_thread.ident, "MainThread", None)
+    from ddtrace.internal import compat
+    from ddtrace.profiling.collector import _task
 
+    l1 = threading.Lock()
+    l1.acquire()
 
-# @pytest.mark.skipif(TESTING_GEVENT, reason="only works without gevent")
-# def test_list_tasks_nogevent():
-#     assert _task.list_tasks(nogevent.main_thread_id) == []
+    def wait():
+        l1.acquire()
+        l1.release()
 
+    def nothing():
+        pass
 
-# @pytest.mark.skipif(not TESTING_GEVENT, reason="only works with gevent")
-# def test_list_tasks_gevent():
-#     l1 = threading.Lock()
-#     l1.acquire()
+    t1 = threading.Thread(target=wait, name="t1")
+    t1.start()
 
-#     def wait():
-#         l1.acquire()
-#         l1.release()
+    tasks = _task.list_tasks(threading.main_thread().ident)
+    # can't check == 2 because there are left over from other tests
+    assert len(tasks) >= 2
 
-#     def nothing():
-#         pass
+    main_thread_found = False
+    t1_found = False
+    for task in tasks:
+        assert len(task) == 3
+        # main thread
+        if task[0] == compat.main_thread.ident:
+            assert task[1] == "MainThread"
+            assert task[2] is None
+            main_thread_found = True
+        # t1
+        elif task[0] == t1.ident:
+            assert task[1] == "t1"
+            assert task[2] is not None
+            t1_found = True
 
-#     t1 = threading.Thread(target=wait, name="t1")
-#     t1.start()
+    l1.release()
 
-#     tasks = _task.list_tasks(nogevent.main_thread_id)
-#     # can't check == 2 because there are left over from other tests
-#     assert len(tasks) >= 2
+    t1.join()
 
-#     main_thread_found = False
-#     t1_found = False
-#     for task in tasks:
-#         assert len(task) == 3
-#         # main thread
-#         if task[0] == compat.main_thread.ident:
-#             assert task[1] == "MainThread"
-#             assert task[2] is None
-#             main_thread_found = True
-#         # t1
-#         elif task[0] == t1.ident:
-#             assert task[1] == "t1"
-#             assert task[2] is not None
-#             t1_found = True
-
-#     l1.release()
-
-#     t1.join()
-
-#     assert t1_found
-#     assert main_thread_found
+    assert t1_found
+    assert main_thread_found
