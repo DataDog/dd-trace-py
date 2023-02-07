d7a7037708bd0a44e41e5c8172ad7fe203306efc
Emmett Butler <emmett.butler321@gmail.com>
2023-02-07 07:34:48 -0800

    add ddtrace-patch to working configs in test

--- tests/contrib/gunicorn/test_gunicorn.py
+++ tests/contrib/gunicorn/test_gunicorn.py
@@ -192,6 +192,7 @@ SETTINGS_GEVENT_POSTWORKERIMPORT_PATCH_POSTWORKERSERVICE = _gunicorn_settings_fa
     [
         SETTINGS_GEVENT_APPIMPORT_PATCH_POSTWORKERSERVICE,
         SETTINGS_GEVENT_POSTWORKERIMPORT_PATCH_POSTWORKERSERVICE,
+        SETTINGS_GEVENT_DDTRACERUN_PATCH,
     ],
 )
 def test_no_known_errors_occur(gunicorn_server_settings, tmp_path):
@@ -199,20 +200,4 @@ def test_no_known_errors_occur(gunicorn_server_settings, tmp_path):
         server_process, client = context
         r = client.get("/")
     assert_no_profiler_error(server_process)
-    assert_remoteconfig_started_successfully(r)
-
-
-@pytest.mark.parametrize(
-    "gunicorn_server_settings",
-    [
-        SETTINGS_GEVENT_DDTRACERUN_PATCH,
-    ],
-)
-def test_profiler_error_occurs_under_gevent_worker(gunicorn_server_settings, tmp_path):
-    with gunicorn_server(gunicorn_server_settings, tmp_path) as context:
-        server_process, client = context
-        r = client.get("/")
-    # this particular error does not manifest in 3.8 and older
-    if sys.version_info[1] > 8:
-        assert MOST_DIRECT_KNOWN_GUNICORN_RELATED_PROFILER_ERROR_SIGNAL in server_process.stderr.read()
     assert_remoteconfig_started_successfully(r)
