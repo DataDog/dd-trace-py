390321b2ffba3f16087a6e12bd0749a2da158e74
Emmett Butler <emmett.butler321@gmail.com>
2023-02-07 09:23:13 -0800

    undo changes in compat.py since they are unrelated to gevent compatibility

--- ddtrace/internal/compat.py
+++ ddtrace/internal/compat.py
@@ -87,7 +87,14 @@ else:
     pattern_type = re._pattern_type  # type: ignore[misc,attr-defined]
 
 try:
-    from inspect import getfullargspec
+    from inspect import getargspec as getfullargspec
+
+    def is_not_void_function(f, argspec):
+        return argspec.args or argspec.varargs or argspec.keywords or argspec.defaults or isgeneratorfunction(f)
+
+
+except ImportError:
+    from inspect import getfullargspec  # type: ignore[assignment]  # noqa: F401
 
     def is_not_void_function(f, argspec):
         return (
@@ -99,13 +106,6 @@ try:
             or argspec.kwonlydefaults
             or isgeneratorfunction(f)
         )
-
-
-except ImportError:
-    from inspect import getargspec as getfullargspec  # type: ignore[assignment]  # noqa: F401
-
-    def is_not_void_function(f, argspec):
-        return argspec.args or argspec.varargs or argspec.keywords or argspec.defaults or isgeneratorfunction(f)
 
 
 def is_integer(obj):
