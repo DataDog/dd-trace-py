8d65850806d6ccf67cd4ddd294c4d18a215d8913
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2023-02-09 17:41:28 +0100

    chore(rcm): add regression test
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

---
+++ tests/internal/remoteconfig/stuck_test_app.py
@@ -0,0 +1,20 @@
+import os
+import time
+
+from flask import Flask
+
+app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
+def start():
+    pid1 = os.fork()
+    if pid1 == 0:
+        os.setsid()
+        time.sleep(0.2)
+    else:
+        os.waitpid(pid1, 0)
+
+@app.route('/')
+def index():
+    start()
+    return "OK"
