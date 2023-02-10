48781f49b89087595376d7b5be65a42cc3a32193
Emmett Butler <emmett.butler321@gmail.com>
2023-02-10 11:12:00 -0800

    swap import attempt order

--- ddtrace/internal/compat.py
+++ ddtrace/internal/compat.py
@@ -1,4 +1,3 @@
-from inspect import getfullargspec
 from inspect import isgeneratorfunction
 import ipaddress
 import platform
@@ -87,17 +86,25 @@ if PYTHON_VERSION_INFO >= (3, 7):
 else:
     pattern_type = re._pattern_type  # type: ignore[misc,attr-defined]
 
+try:
+    from inspect import getfullargspec  # type: ignore[assignment]  # noqa: F401
 
-def is_not_void_function(f, argspec):
-    return (
-        argspec.args
-        or argspec.varargs
-        or argspec.varkw
-        or argspec.defaults
-        or argspec.kwonlyargs
-        or argspec.kwonlydefaults
-        or isgeneratorfunction(f)
-    )
+    def is_not_void_function(f, argspec):
+        return (
+            argspec.args
+            or argspec.varargs
+            or argspec.varkw
+            or argspec.defaults
+            or argspec.kwonlyargs
+            or argspec.kwonlydefaults
+            or isgeneratorfunction(f)
+        )
+
+except ImportError:
+    from inspect import getargspec as getfullargspec  # type: ignore[assignment]  # noqa: F401
+
+    def is_not_void_function(f, argspec):
+        return argspec.args or argspec.varargs or argspec.keywords or argspec.defaults or isgeneratorfunction(f)
 
 
 def is_integer(obj):
