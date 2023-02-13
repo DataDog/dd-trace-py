f33a4ae6db641e62428ed724d8a1d04e1badd048
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-02-13 11:53:48 +0100

    first try at operator patching

--- ddtrace/appsec/iast/_ast/aspects/__init__.py
+++ ddtrace/appsec/iast/_ast/aspects/__init__.py
@@ -5,3 +5,7 @@ from builtins import str as builtin_str
 
 def str_aspect(*args, **kwargs):
     return builtin_str(*args, **kwargs)
+
+
+def add_aspect(op1, op2):
+    return op1.__class__.__add__(op2)
