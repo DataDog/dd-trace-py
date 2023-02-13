a3d73a14e4a01c6d766516824776090887e82ce9
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-02-13 14:41:26 +0100

    operator + semantic

--- ddtrace/appsec/iast/_ast/aspects/__init__.py
+++ ddtrace/appsec/iast/_ast/aspects/__init__.py
@@ -7,5 +7,16 @@ def str_aspect(*args, **kwargs):
     return builtin_str(*args, **kwargs)
 
 
+def _not_implemented(_):
+    return NotImplemented
+
+
 def add_aspect(op1, op2):
-    return op1.__class__.__add__(op2)
+    res = getattr(op1.__class__, "__add__", _not_implemented)(op2)
+    if res is NotImplemented and op2.__class__ is not op1.__class__:
+        res = getattr(op2.__class__, "__radd__", _not_implemented)(op1)
+    if res is NotImplemented:
+        raise TypeError(
+            "unsupported operand type(s) for +: '%s' and '%s'" % (op1.__class__.__name__, op2.__class__.__name__)
+        )
+    return res
