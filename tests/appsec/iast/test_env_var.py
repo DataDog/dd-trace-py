c110664b96c0f7c0e6ab8a24196d99911b481dc9
Federico Mon <federico.mon@datadoghq.com>
2023-02-14 11:46:13 +0100

    chore(asm): Add gevent-specific tests

--- tests/appsec/iast/test_env_var.py
+++ tests/appsec/iast/test_env_var.py
@@ -14,7 +14,7 @@ def _run_python_file(*args, **kwargs):
         "ddtrace-run",
         "-d",
         "python",
-        os.path.join(current_dir, "fixtures", "integration", "main.py"),
+        os.path.join(current_dir, "fixtures", "integration", kwargs.get("filename", "main.py")),
     ] + list(args)
     if "env" in kwargs:
         ret = subprocess.run(cmd, cwd=current_dir, env=kwargs["env"])
@@ -52,3 +52,42 @@ def test_env_var_iast_unset(monkeypatch, capfd):
     captured = capfd.readouterr()
     assert "hi" in captured.out
     assert "IAST enabled" not in captured.err
+
+
+@pytest.mark.skipif(not _is_python_version_supported(), reason="IAST compatible versions")
+@pytest.mark.xfail(reason="IAST now working with Gevent yet")
+def test_env_var_iast_enabled_gevent_unload_modules_true(capfd):
+    # type: (...) -> None
+    env = os.environ.copy()
+    env["DD_IAST_ENABLED"] = "true"
+    env["DD_UNLOAD_MODULES_FROM_SITECUSTOMIZE"] = "true"
+    _run_python_file(filename="main_gevent.py", env=env)
+    captured = capfd.readouterr()
+    assert "IAST enabled" in captured.err
+    assert "hi" in captured.out
+
+
+@pytest.mark.skipif(not _is_python_version_supported(), reason="IAST compatible versions")
+@pytest.mark.xfail(reason="IAST now working with Gevent yet")
+def test_env_var_iast_enabled_gevent_unload_modules_false(capfd):
+    # type: (...) -> None
+    env = os.environ.copy()
+    env["DD_IAST_ENABLED"] = "true"
+    env["DD_UNLOAD_MODULES_FROM_SITECUSTOMIZE"] = "false"
+    _run_python_file(filename="main_gevent.py", env=env)
+    captured = capfd.readouterr()
+    assert "IAST enabled" in captured.err
+    assert "hi" in captured.out
+
+
+@pytest.mark.skipif(not _is_python_version_supported(), reason="IAST compatible versions")
+@pytest.mark.xfail(reason="IAST now working with Gevent yet")
+def test_env_var_iast_enabled_gevent_patch_all_true(capfd):
+    # type: (...) -> None
+    env = os.environ.copy()
+    env["DD_IAST_ENABLED"] = "true"
+    env["DD_GEVENT_PATCH_ALL"] = "true"
+    _run_python_file(filename="main_gevent.py", env=env)
+    captured = capfd.readouterr()
+    assert "IAST enabled" in captured.err
+    assert "hi" in captured.out
