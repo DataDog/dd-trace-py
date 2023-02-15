e4d1594d94202997edc5e803d7a2bd8e29e02d83
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-02-15 17:03:04 +0100

    first commit, WIP

--- ddtrace/appsec/iast/_ast/aspects/__init__.py
+++ ddtrace/appsec/iast/_ast/aspects/__init__.py
@@ -4,12 +4,13 @@ from builtins import str as builtin_str
 
 from ddtrace.appsec.iast._taint_tracking import is_pyobject_tainted
 from ddtrace.appsec.iast._taint_tracking import taint_pyobject
+from ddtrace.appsec.iast.input_info import Input_info
 
 
 def str_aspect(*args, **kwargs):
     result = builtin_str(*args, **kwargs)
     if isinstance(args[0], (str, bytes, bytearray)) and is_pyobject_tainted(args[0]):
-        taint_pyobject(result)
+        taint_pyobject(result, Input_info("str_aspect", result, 0))
 
     return result
 
--- ddtrace/appsec/iast/_taint_tracking.cpp
+++ ddtrace/appsec/iast/_taint_tracking.cpp
@@ -1,24 +1,63 @@
 #include <Python.h>
-#include <unordered_set>
+#include <unordered_map>
+#include <vector>
+#include <tuple>
 
-std::unordered_set<PyObject *> TaintMapping{};
+#define IS_TAINTED(obj) (TaintMapping.count(obj) && TaintMapping[obj].size())
+
+typedef PyObject *input_info;
+typedef std::tuple<input_info, Py_ssize_t, Py_ssize_t> tainted_range;
+typedef std::vector<tainted_range> tainted_range_list;
+
+std::unordered_map<PyObject *, tainted_range_list> TaintMapping{};
 
 static PyObject *clear_taint_mapping(PyObject *Py_UNUSED(module),
-                                     PyObject *Py_UNUSED(args)) {
-
+                                     PyObject *Py_UNUSED(args)) 
+{
   TaintMapping.clear();
   Py_RETURN_NONE;
 }
 
 static PyObject *taint_pyobject(PyObject *Py_UNUSED(module),
-                                PyObject *py_object) {
-  TaintMapping.insert(py_object);
+                                PyObject *args)
+{
+  PyObject *tainted_object;
+  input_info input_info;
+  PyArg_UnpackTuple(args, "ref", 2, 2, &tainted_object, &input_info);
+  // DEV: could use PyUnicode_GET_LENGTH if we are only using unicode string
+  TaintMapping[tainted_object] = {{input_info, 0, PyObject_Length(tainted_object)}}; 
   Py_RETURN_NONE;
 }
 
+static PyObject *add_taint_pyobject(PyObject *Py_UNUSED(module),
+                                PyObject *args)
+{
+  PyObject *tainted_object;
+  PyObject *op1;
+  PyObject *op2;
+  PyArg_UnpackTuple(args, "ref", 3, 3, &tainted_object, &op1, &op2);
+  // if both operand are untainted, do not taint
+  if (!(IS_TAINTED(op1) || IS_TAINTED(op2)))
+    Py_RETURN_FALSE;
+  if IS_TAINTED(op1)
+    TaintMapping[tainted_object] = TaintMapping[op1];
+  else
+    TaintMapping[tainted_object] = {};
+  if IS_TAINTED(op2)
+  {
+    Py_ssize_t offset = PyObject_Length(op1);
+    for(auto [input_info, start, size]: TaintMapping[op2])
+    {
+      TaintMapping[tainted_object].emplace_back(tainted_range(input_info, start+offset, size));
+    }
+  }
+  Py_RETURN_TRUE;
+}
+
 static PyObject *is_pyobject_tainted(PyObject *Py_UNUSED(module),
-                                     PyObject *py_object) {
-  if (TaintMapping.count(py_object))
+                                     PyObject *py_object)
+{
+  if IS_TAINTED(py_object)
     Py_RETURN_TRUE;
   Py_RETURN_FALSE;
 }
@@ -26,7 +65,10 @@ static PyObject *is_pyobject_tainted(PyObject *Py_UNUSED(module),
 static PyMethodDef TaintTrackingMethods[] = {
     {"clear_taint_mapping", (PyCFunction)clear_taint_mapping, METH_NOARGS,
      "clear taint mappings"},
-    {"taint_pyobject", (PyCFunction)taint_pyobject, METH_O, "taint pyobject"},
+    // We are using  METH_VARARGS because we need compatibility with python 3.5, 3.6.
+    // but METH_FASTCALL could be used instead for python >= 3.7
+    {"taint_pyobject", (PyCFunction)taint_pyobject, METH_VARARGS, "taint pyobject"},
+    {"add_taint_pyobject", (PyCFunction)add_taint_pyobject, METH_VARARGS, "taint pyobject obtained from +"},
     {"is_pyobject_tainted", (PyCFunction)is_pyobject_tainted, METH_O,
      "is pyobject tainted"},
     {NULL, NULL, 0, NULL}};
@@ -35,7 +77,8 @@ static struct PyModuleDef taint_tracking = {
     PyModuleDef_HEAD_INIT, "ddtrace.appsec.iast._taint_tracking",
     "taint tracking module", -1, TaintTrackingMethods};
 
-PyMODINIT_FUNC PyInit__taint_tracking(void) {
+PyMODINIT_FUNC PyInit__taint_tracking(void)
+{
   PyObject *m;
   m = PyModule_Create(&taint_tracking);
   if (m == NULL)
