cca3e8791aba7a8766f0043a9abb8a011c6f959c
Federico Mon <federico.mon@datadoghq.com>
2023-02-15 16:00:08 +0100

    chore(asm): remove debugging option from extra_compile_args

--- setup.py
+++ setup.py
@@ -298,7 +298,7 @@ if sys.version_info[:2] >= (3, 4) and not IS_PYSTON:
                 sources=[
                     "ddtrace/appsec/iast/_taint_tracking.cpp",
                 ],
-                extra_compile_args=debug_compile_args + ["-g", "-std=c++17"],
+                extra_compile_args=debug_compile_args + ["-std=c++17"],
             )
         )
 else:
