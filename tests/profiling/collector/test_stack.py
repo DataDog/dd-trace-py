61b3910727ab95f8fb71fe32ecced35aea2563b3
Munir Abdinur <munir_abdinur@hotmail.com>
2023-02-21 15:12:29 -0500

    update collector frames

--- tests/profiling/collector/test_stack.py
+++ tests/profiling/collector/test_stack.py
@@ -418,7 +418,7 @@ def test_exception_collection():
     assert e.sampling_period > 0
     assert e.thread_id == nogevent.thread_get_ident()
     assert e.thread_name == "MainThread"
-    assert e.frames == [(__file__, 411, "test_exception_collection", "")]
+    assert e.frames == [(__file__, 412, "test_exception_collection", "")]
     assert e.nframes == 1
     assert e.exc_type == ValueError
 
@@ -450,7 +450,7 @@ def test_exception_collection_trace(
     assert e.sampling_period > 0
     assert e.thread_id == nogevent.thread_get_ident()
     assert e.thread_name == "MainThread"
-    assert e.frames == [(__file__, 438, "test_exception_collection_trace", "")]
+    assert e.frames == [(__file__, 439, "test_exception_collection_trace", "")]
     assert e.nframes == 1
     assert e.exc_type == ValueError
     assert e.span_id == span.span_id
