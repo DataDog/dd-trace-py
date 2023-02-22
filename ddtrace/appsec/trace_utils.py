766761e029f380c518f1e94e476cf0042f74b2e7
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2023-02-22 18:01:22 +0100

    chore: merge
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/appsec/trace_utils.py
+++ ddtrace/appsec/trace_utils.py
@@ -177,5 +177,6 @@ def block_request_if_user_blocked(tracer, userid):  # type: (Tracer, str) -> Non
 
     if should_block_user(tracer, userid):
         span = tracer.current_root_span()
-        span.set_tag_str(user.ID, str(userid))
+        if span:
+            span.set_tag_str(user.ID, str(userid))
         _asm_request_context.block_request()
