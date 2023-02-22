d7fc648c5b47a97715c7b2aa2157ce07793d2713
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-02-22 16:56:45 +0100

    SRB integration with IP blocking

--- ddtrace/contrib/django/patch.py
+++ ddtrace/contrib/django/patch.py
@@ -378,56 +378,55 @@ def traced_get_response(django, pin, func, instance, args, kwargs):
 
             response = None
 
-            if config._appsec_enabled:
-                # set context information for waf with uri, params and query
-                query = request.META.get("QUERY_STRING", "")
-                uri = utils.get_request_uri(request)
-                if query:
-                    uri += "?" + query
-                resolver = get_resolver(getattr(request, "urlconf", None))
-                if resolver:
-                    try:
-                        path = resolver.resolve(request.path_info).kwargs
-                        log.debug("resolver.pattern %s", path)
-                    except Exception:
-                        path = None
-                parsed_query = request.GET
-                body = utils._extract_body(request)
-                trace_utils.set_http_meta(
-                    span,
-                    config.django,
-                    method=request.method,
-                    query=query,
-                    raw_uri=uri,
-                    request_path_params=path,
-                    parsed_query=parsed_query,
-                    request_body=body,
-                    request_cookies=request.COOKIES,
-                )
-                log.debug("first Django WAF call")
-                _asm_request_context.call_waf_callback()
-                if _context.get_item("http.request.blocked", span=span):
-                    return HttpResponseForbidden(appsec_utils._get_blocked_template(request_headers.get("Accept")))
             try:
-                if config._appsec_enabled and _context.get_item("http.request.blocked", span=span):
-                    response = HttpResponseForbidden(
-                        appsec_utils._get_blocked_template(request_headers.get("Accept")),
+                if config._appsec_enabled:
+                    # [IP Blocking]
+                    if _context.get_item("http.request.blocked", span=span):
+                        return HttpResponseForbidden(appsec_utils._get_blocked_template(request_headers.get("Accept")))
+
+                    # set context information for [Suspicious Request Blocking]
+                    query = request.META.get("QUERY_STRING", "")
+                    uri = utils.get_request_uri(request)
+                    if query:
+                        uri += "?" + query
+                    resolver = get_resolver(getattr(request, "urlconf", None))
+                    if resolver:
+                        try:
+                            path = resolver.resolve(request.path_info).kwargs
+                            log.debug("resolver.pattern %s", path)
+                        except Exception:
+                            path = None
+                    parsed_query = request.GET
+                    body = utils._extract_body(request)
+                    trace_utils.set_http_meta(
+                        span,
+                        config.django,
+                        method=request.method,
+                        query=query,
+                        raw_uri=uri,
+                        request_path_params=path,
+                        parsed_query=parsed_query,
+                        request_body=body,
+                        request_cookies=request.COOKIES,
                     )
-                else:
-                    response = func(*args, **kwargs)
-                    if isinstance(response, HttpResponseForbidden):
-                        # Add our custom block template
-                        response.content = appsec_utils._get_blocked_template(request_headers.get("Accept"))
-                return response
+                    log.debug("Django WAF call for Suspicious Request Blocking on request")
+                    _asm_request_context.call_waf_callback()
+                    # [Suspicious Request Blocking on request]
+                    if _context.get_item("http.request.blocked", span=span):
+                        return HttpResponseForbidden(appsec_utils._get_blocked_template(request_headers.get("Accept")))
+                response = func(*args, **kwargs)
+                if config._appsec_enabled:
+                    # [Blocking by client code]
+                    if _context.get_item("http.request.blocked", span=span):
+                        return HttpResponseForbidden(appsec_utils._get_blocked_template(request_headers.get("Accept")))
+                    log.debug("Django WAF call for Suspicious Request Blocking on response")
+                    _asm_request_context.call_waf_callback()
+                    # [Suspicious Request Blocking on response]
+                    if _context.get_item("http.request.blocked", span=span):
+                        return HttpResponseForbidden(appsec_utils._get_blocked_template(request_headers.get("Accept")))
             finally:
                 # DEV: Always set these tags, this is where `span.resource` is set
                 utils._after_request_tags(pin, span, request, response)
-                # calling the waf again if the request was not blocked at first with additional information (response)
-                if config._appsec_enabled:
-                    log.debug("second Django WAF call")
-                    _asm_request_context.call_waf_callback()
-                    if _context.get_item("http.request.blocked", span=span):
-                        return HttpResponseForbidden(appsec_utils._get_blocked_template(request_headers.get("Accept")))
             return response
 
 
