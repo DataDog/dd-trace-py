a51c0e4b0acdab293e1ddc9c5d169b0421f5d156
Federico Mon <federico.mon@datadoghq.com>
2023-02-22 12:11:38 +0100

    chore(asm): bad merge conflict resolution

--- ddtrace/appsec/iast/_taint_tracking/_taint_tracking.cpp
+++ ddtrace/appsec/iast/_taint_tracking/_taint_tracking.cpp
@@ -101,30 +101,6 @@ static PyObject *add_taint_pyobject(PyObject *Py_UNUSED(module),
   return tainted_object;
 }
 
-static PyObject *add_taint_pyobject(PyObject *Py_UNUSED(module),
-                                    PyObject *args) {
-  PyObject *tainted_object;
-  PyObject *op1;
-  PyObject *op2;
-  PyArg_ParseTuple(args, "OOO", &tainted_object, &op1, &op2);
-  // if both operand are untainted, do not taint
-  if (!(IS_TAINTED(op1) || IS_TAINTED(op2)))
-    Py_RETURN_FALSE;
-  if IS_TAINTED (op1)
-    TaintMapping[tainted_object] = TaintMapping[op1];
-  else
-    TaintMapping[tainted_object] = {};
-  if IS_TAINTED (op2) {
-    Py_ssize_t offset = PyObject_Length(op1);
-    for (auto [input_info, start, size] : TaintMapping[op2]) {
-      Py_INCREF(input_info);
-      TaintMapping[tainted_object].emplace_back(
-          tainted_range(input_info, start + offset, size));
-    }
-  }
-  Py_RETURN_TRUE;
-}
-
 static PyObject *is_pyobject_tainted(PyObject *Py_UNUSED(module),
                                      PyObject *py_object) {
   if IS_TAINTED (py_object)
