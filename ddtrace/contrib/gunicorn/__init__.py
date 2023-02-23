ebeef33d0de542768af5abb5292e9219d1c2a17d
Emmett Butler <emmett.butler321@gmail.com>
2023-02-23 14:21:08 -0800

    include important note in gunicorn documentation

--- ddtrace/contrib/gunicorn/__init__.py
+++ ddtrace/contrib/gunicorn/__init__.py
@@ -1,3 +1,13 @@
+"""
+ddtrace works with Gunicorn.
+
+.. note::
+    If your Gunicorn server does not run inside of ``ddtrace-run``,
+    be sure to ``import ddtrace.auto`` as early as possible in your application's
+    lifecycle.
+"""
+
+
 def patch():
     pass
 
