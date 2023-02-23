a8adc6a67085287cb15b314f2e6b9ad8c8acc834
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2023-02-23 16:47:40 +0100

    chore(asm): update the list of IP headers to the latest RFC
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- tests/contrib/flask/test_flask_appsec.py
+++ tests/contrib/flask/test_flask_appsec.py
@@ -134,7 +134,7 @@ class FlaskAppSecTestCase(BaseFlaskTestCase):
 
     def test_flask_client_ip_header_set_by_env_var_valid(self):
         with override_global_config(dict(_appsec_enabled=True, client_ip_header="X-Use-This")):
-            self.client.get("/?a=1&b&c=d", headers={"HTTP_CLIENT_IP": "8.8.8.8", "X-Use-This": "4.4.4.4"})
+            self.client.get("/?a=1&b&c=d", headers={"HTTP_X_CLIENT_IP": "8.8.8.8", "X-Use-This": "4.4.4.4"})
             spans = self.pop_spans()
             root_span = spans[0]
             assert root_span.get_tag(http.CLIENT_IP) == "4.4.4.4"
