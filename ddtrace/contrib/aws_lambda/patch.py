eb317123496dda86caa29f26b884784fac51aa15
Jordan Gonzalez <30836115+duncanista@users.noreply.github.com>
2023-02-23 17:04:43 -0500

    update signals handler to avoid wrapping the same signal multiple times

--- ddtrace/contrib/aws_lambda/patch.py
+++ ddtrace/contrib/aws_lambda/patch.py
@@ -32,21 +32,25 @@ def _crash_flush(_, __):
 
 def _handle_signal(sig, f):
     """
-    Wraps the given signal with a previously defined, if exists.
+    Wraps the given signal with a previously defined signal, if
+    it's callable and is different than the function `f` to handle.
+    Else, it doesn't wrap and just sets the function to handle as `f`.
     This to avoid our signals overriding existing ones.
 
-    Returns the handler of the wrapped signal.
+    Returns a signal of type `sig` with function `_f`
     """
-    old_signal = None
-    if callable(signal.getsignal(sig)):
-        old_signal = signal.getsignal(sig)
+    old_signal = signal.getsignal(sig)
+    if not callable(old_signal) or old_signal == f:
+        old_signal = None
 
     def wrap_signals(*args, **kwargs):
         if old_signal is not None:
             old_signal(*args, **kwargs)
         f(*args, **kwargs)
 
-    return signal.signal(sig, wrap_signals)
+    _f = wrap_signals if old_signal else f
+
+    return signal.signal(sig, _f)
 
 
 def _check_timeout(context):
