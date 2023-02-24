8f156b4cde01c5034f38419ad510313c35373488
Emmett Butler <emmett.butler321@gmail.com>
2023-02-24 06:51:50 -0800

    avoid attempting to decode sql

--- ddtrace/propagation/_database_monitoring.py
+++ ddtrace/propagation/_database_monitoring.py
@@ -30,10 +30,7 @@ def default_sql_injector(dbm_comment, sql_statement):
     # type: (str, Union[str, bytes]) -> Union[str, bytes]
     try:
         if isinstance(sql_statement, bytes):
-            # raises a unicode error when sql statements with non utf-8 encodings
-            # are concatenated. This operation will log a warning.
-            sql_statement = sql_statement.decode("utf-8", errors="strict")
-            return bytes(dbm_comment + sql_statement, "utf-8")
+            dbm_comment = dbm_comment.decode("utf-8", errors="strict")
         return dbm_comment + sql_statement
     except (TypeError, UnicodeDecodeError, UnicodeEncodeError):
         log.warning(
