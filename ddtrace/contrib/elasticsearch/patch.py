2a87fa2160e23d6444947bd2a17d6881848118a2
Kyle Verhoog <kyle@verhoog.ca>
2023-03-01 16:53:29 -0500

    fix(elasticsearch): omit large body tag values (#5229)
    The elasticsearch body tag value in practice can be very large. Since
    the value is not truncated, it can easily result in traces exceeding the
    max size limit which results in dropped traces.
    
    The proposed solution is to omit the value. This is because obfuscation
    logic lives in the Agent and so the body cannot be truncated as
    sensitive data would be leaked. No body tag is better than no trace.
    
    Ideally the obfuscation logic should exist in the client so that the
    body can be truncated.

--- ddtrace/contrib/elasticsearch/patch.py
+++ ddtrace/contrib/elasticsearch/patch.py
@@ -1,6 +1,7 @@
 from importlib import import_module
 
 from ddtrace import config
+from ddtrace._tracing import _limits
 from ddtrace.contrib.trace_utils import ext_service
 from ddtrace.internal.constants import COMPONENT
 from ddtrace.vendor.wrapt import wrap_function_wrapper as _w
@@ -95,7 +96,19 @@ def _get_perform_request(elasticsearch):
                 span.set_tag_str(http.QUERY_STRING, encoded_params)
 
             if method in ["GET", "POST"]:
-                span.set_tag_str(metadata.BODY, instance.serializer.dumps(body))
+                ser_body = instance.serializer.dumps(body)
+                # Elasticsearch request bodies can be very large resulting in traces being too large
+                # to send.
+                # When this occurs, drop the value.
+                # Ideally the body should be truncated, however we cannot truncate as the obfuscation
+                # logic for the body lives in the agent and truncating would make the body undecodable.
+                if len(ser_body) <= _limits.MAX_SPAN_META_VALUE_LEN:
+                    span.set_tag_str(metadata.BODY, ser_body)
+                else:
+                    span.set_tag_str(
+                        metadata.BODY,
+                        "<body size %s exceeds limit of %s>" % (len(ser_body), _limits.MAX_SPAN_META_VALUE_LEN),
+                    )
             status = None
 
             # set analytics sample rate
