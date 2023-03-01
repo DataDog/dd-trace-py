51f524eb62d47b1dc12a40d0e8ac83ecc60a7bdf
ZStriker19 <zach.groves@datadoghq.com>
2023-03-01 16:01:08 -0500

    creates release draft and notebook

--- scripts/release_script_1.py
+++ scripts/release_script_1.py
@@ -1,45 +1,110 @@
 import os
 
-#  Figure out the version of the library that you’re working on releasing grabbed with VERSION envar
-version = os.getenv("VERSION")
-if version is None:
-    print("need to specify the version with envar e.g. VERSION=1.8.0")
 
-# setup slack webclient
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
-# https://api.slack.com/apps
-# https://api.slack.com/authentication/basics
-slack_token = os.getenv("SLACK_TOKEN")
-client = WebClient(token=slack_token)
+def release_it():
 
+    #  Figure out the version of the library that you’re working on releasing grabbed with VERSION envar
+    version = os.getenv("VERSION")
+    dd_api_key = os.getenv("DD_API_KEY")
+    dd_app_key = os.getenv("DD_APP_KEY")
+    gh_token = os.getenv("GH_TOKEN")
+    slack_token = os.getenv("SLACK_TOKEN")
 
-try:
-    response = client.chat_postMessage(
-        channel="zachg-test",
-        text="Hello from your app! :tada:"
+    if version is None:
+        print("need to specify the version with envar e.g. VERSION=1.8.0")
+
+    # setup slack webclient
+    from slack_sdk import WebClient
+    from slack_sdk.errors import SlackApiError
+
+    # https://api.slack.com/apps
+    # https://api.slack.com/authentication/basics
+    client = WebClient(token=slack_token)
+
+    try:
+        response = client.chat_postMessage(channel="zachg-test", text="Hello from your app! :tada:")
+    except SlackApiError as e:
+        # You will get a SlackApiError if "ok" is False
+        assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+
+    # get release notes
+    import subprocess
+
+    rn_raw = subprocess.check_output(
+        "reno report --no-show-source | \
+    pandoc -f rst -t gfm --wrap=none",
+        shell=True,
+        cwd=os.pardir,
     )
-except SlackApiError as e:
-    # You will get a SlackApiError if "ok" is False
-    assert e.response["error"]    # str like 'invalid_auth', 'channel_not_found'
+    rn = rn_raw.decode().split("## v")[0].replace("\n## Unreleased\n", "", 1).replace("# Release Notes\n", "", 1)
+    print(rn)
 
-# get release notes
-import subprocess
-# this works but commenting out for now for speed
-rn_raw = subprocess.check_output("reno report --no-show-source | \
-pandoc -f rst -t gfm --wrap=none", shell=True, cwd = os.pardir)
-rn = rn_raw.decode().split("## v")[0].replace("\n## Unreleased\n", "", 1)
-print(rn) 
+    # Create our git release https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html?highlight=release#github.Repository.Repository.create_git_release
+    from github import Github
 
-# Create our git release https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html?highlight=release#github.Repository.Repository.create_git_release
-from github import Github
-g = Github(os.getenv("GH_TOKEN"))
-# dd_repo = g.get_repo(full_name_or_id="DataDog/dd-trace-py")
-# main_branch = dd_repo.get_branch(branch="1.x")
+    g = Github(gh_token)
+    # dd_repo = g.get_repo(full_name_or_id="DataDog/dd-trace-py")
+    # main_branch = dd_repo.get_branch(branch="1.x")
+    dd_repo = g.get_repo(full_name_or_id="Kyle-Verhoog/dd-trace-py-test")
+    main_branch = dd_repo.get_branch(branch="master")
 
-dd_repo = g.get_repo(full_name_or_id="Kyle-Verhoog/dd-trace-py-test")
-main_branch = dd_repo.get_branch(branch="master")
+    commit_id = main_branch.commit
+    # dd_repo.create_git_release(name=version, tag=version, prerelease=True, draft=True, target_commitish=main_branch, message=rn)
 
-dd_repo.create_git_release(name=version, tag=version, prerelease=True, draft=True, target_commitish=main_branch, message=rn)
-    
-print("ok")
+    # edit release notes to be put inside notebook
+    rn_notebook = rn.replace("-", "- [ ]")
+
+    # create the review notebook and add the release notes formatted for testing
+    import json
+
+    # get notebook template
+    template_notebook = subprocess.check_output(
+        'curl -X GET "https://api.datadoghq.com/api/v1/notebooks/4888117" \
+    -H "Accept: application/json" \
+    -H "DD-API-KEY: {dd_api_key}" \
+    -H "DD-APPLICATION-KEY: {dd_app_key}"'.format(
+            dd_api_key=dd_api_key, dd_app_key=dd_app_key
+        ),
+        shell=True,
+        cwd=os.pardir,
+    )
+
+    data = template_notebook.decode("utf8")
+    data = json.loads(data)
+    # change the value inside of our template to include release notes
+    data["data"]["attributes"]["cells"][1]["attributes"]["definition"]["text"] = (
+        "#  Release notes to test\n- %s\n<Tester> \n<PR>\n\n\n## Release Notes that will not be tested\n- <any release notes for PRs that don't need manual testing>\n\n\n"
+        % (rn_notebook)
+    )
+    # grab the cells out to be transferred into a new notebook
+    cells = data["data"]["attributes"]["cells"]
+
+    interpolated_notebook = {
+        "data": {
+            "attributes": {
+                "cells": cells,
+                "name": "ddtrace-py testing %s commit %s" % (version, commit_id.sha),
+                "time": {"live_span": "1d"},
+            },
+            "type": "notebooks",
+        }
+    }
+
+    notebook_json = json.dumps(interpolated_notebook, indent=4, sort_keys=True)
+
+    import requests
+
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "DD-API-KEY": dd_api_key,
+        "DD-APPLICATION-KEY": dd_app_key,
+    }
+    # create new release notebook
+    # resp_create = requests.post("https://api.datadoghq.com/api/v1/notebooks", data=notebook_json, headers=headers)
+
+    print("ok")
+
+
+if __name__ == "__main__":
+    release_it()
