d1f09e2677f8b0d73d4a81d3466b44385b0bc02d
Munir Abdinur <munir_abdinur@hotmail.com>
2023-03-08 10:29:02 -0500

    check if PY2

--- ddtrace/_monkey.py
+++ ddtrace/_monkey.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING
 from ddtrace.vendor.wrapt.importer import when_imported
 
 from .constants import IAST_ENV
+from .internal.compat import PY2
 from .internal.logger import get_logger
 from .internal.telemetry import telemetry_writer
 from .internal.utils import formats
@@ -118,8 +119,7 @@ _MODULES_FOR_CONTRIB = {
     "futures": ("concurrent.futures",),
     "vertica": ("vertica_python",),
     "aws_lambda": ("datadog_lambda",),
-    # patch httplib for PY2, http.client for PY3
-    "httplib": ("httplib", "http.client"),
+    "httplib": ("httplib" if PY2 else "http.client",),
 }
 
 IAST_PATCH = {
