5d323249b6e02a4aa6007eed35a79fe7916ff207
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2023-03-13 17:19:30 -0400

    fix(aws_lambda): update how `DD_APM_FLUSH_DEADLINE_MILLISECONDS` is used  (#5176)
    ## Description
    <!-- Briefly describe the change and why it was required. -->
    Update how `DD_APM_FLUSH_DEADLINE_MILLISECONDS` is used.
    Previously, we would set the deadline as the environment variable value,
    if set. Now, we take the deadline as the remaining time in an AWS
    Lambda, and the value in the environment variable is used to subtract
    from the deadline. The default is still 100ms.
    
    
    
    ## Checklist
    - [x] Followed the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/contributing.html#Release-Note-Guidelines)
    when writing a release note.
    - [x] Add additional sections for `feat` and `fix` pull requests.
    - [x] [Library
    documentation](https://github.com/DataDog/dd-trace-py/tree/1.x/docs)
    and/or [Datadog's documentation
    site](https://github.com/DataDog/documentation/) is updated. Link to doc
    PR in description.
    
    ## Motivation
    Miscommunication in how this environment variable is used.
    
    ## Testing strategy
    Added unit tests when using the environment variable.
    
    ## Risk
    This is not a breaking change, since `datadog-lambda-python` will not
    update its tracer version until this PR is merged and released.
    
    ## Checklist
    
    - [x] Change(s) are motivated and described in the PR description.
    - [x] Testing strategy is described if automated tests are not included
    in the PR.
    - [x] Risk is outlined (performance impact, potential for breakage,
    maintainability, etc).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] [Library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/contributing.html#Release-Note-Guidelines)
    are followed.
    - [x] Documentation is included (in-code, generated user docs, [public
    corp docs](https://github.com/DataDog/documentation/)).
    - [x] Author is aware of the performance implications of this PR as
    reported in the benchmarks PR comment.
    
    ## Reviewer Checklist
    
    - [x] Title is accurate.
    - [x] No unnecessary changes are introduced.
    - [x] Description motivates each change.
    - [x] Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes unless absolutely necessary.
    - [x] Testing strategy adequately addresses listed risk(s).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] Release note makes sense to a user of the library.
    - [x] Reviewer is aware of, and discussed the performance implications
    of this PR as reported in the benchmarks PR comment.
    
    ---------
    
    Co-authored-by: Munir Abdinur <munir.abdinur@datadoghq.com>

--- tests/contrib/aws_lambda/test_aws_lambda.py
+++ tests/contrib/aws_lambda/test_aws_lambda.py
@@ -44,14 +44,14 @@ def setup():
     unpatch()
 
 
-@pytest.mark.parametrize("customApmFlushDeadline", [("100"), ("200")])
+@pytest.mark.parametrize("customApmFlushDeadline", [("-100"), ("10"), ("100"), ("200")])
 @pytest.mark.snapshot()
 def test_timeout_traces(context, customApmFlushDeadline):
     os.environ.update(
         {
             "AWS_LAMBDA_FUNCTION_NAME": "timeout_handler",
             "DD_LAMBDA_HANDLER": "tests.contrib.aws_lambda.handlers.timeout_handler",
-            "DD_APM_FLUSH_DEADLINE": customApmFlushDeadline,
+            "DD_APM_FLUSH_DEADLINE_MILLISECONDS": customApmFlushDeadline,
         }
     )
 
