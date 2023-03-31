3fdad3430c31986009a56912e8527554d56f5046
Munir Abdinur <munir.abdinur@datadoghq.com>
2023-03-31 14:38:40 -0400

    nit comment

--- ddtrace/propagation/http.py
+++ ddtrace/propagation/http.py
@@ -122,7 +122,8 @@ def _dd_id_to_b3_id(dd_id):
     # type: (int) -> str
     """Helper to convert Datadog trace/span int ids into lower case hex values"""
     if dd_id > _MAX_UINT_64BITS:
-        # b3 trace ids can be 64bit or 128bit: https://github.com/openzipkin/b3-propagation#traceid
+        # b3 trace ids can have the length of 16 or 32 hex values:
+        # https://github.com/openzipkin/b3-propagation#traceid
         return "{:032x}".format(dd_id)
     return "{:016x}".format(dd_id)
 
