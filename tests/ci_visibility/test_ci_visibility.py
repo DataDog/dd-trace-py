a4631ed5758dc676eded9b02f8ea5460b34817dc
Yun Kim <yun.kim@datadoghq.com>
2023-04-03 13:12:03 -0400

    Fix failing tests, move filter tests into ci_visibility test suite

--- tests/ci_visibility/test_ci_visibility.py
+++ tests/ci_visibility/test_ci_visibility.py
@@ -1,10 +1,37 @@
 import mock
 import pytest
 
+import ddtrace
+from ddtrace.constants import AUTO_KEEP
+from ddtrace.ext import ci
 from ddtrace.internal.ci_visibility import CIVisibility
 from ddtrace.internal.ci_visibility.filters import TraceCiVisibilityFilter
 from ddtrace.internal.ci_visibility.recorder import _extract_repository_name_from_url
+from ddtrace.span import Span
 from tests.utils import DummyTracer
+
+
+def test_filters_test_spans():
+    trace_filter = TraceCiVisibilityFilter(tags={"hello": "world"}, service="test-service")
+    root_test_span = Span(name="span1", span_type="test")
+    root_test_span._local_root = root_test_span
+    # Root span in trace is a test
+    trace = [root_test_span]
+    assert trace_filter.process_trace(trace) == trace
+    assert root_test_span.service == "test-service"
+    assert root_test_span.get_tag(ci.LIBRARY_VERSION) == ddtrace.__version__
+    assert root_test_span.get_tag("hello") == "world"
+    assert root_test_span.context.dd_origin == ci.CI_APP_TEST_ORIGIN
+    assert root_test_span.context.sampling_priority == AUTO_KEEP
+
+
+def test_filters_non_test_spans():
+    trace_filter = TraceCiVisibilityFilter(tags={"hello": "world"}, service="test-service")
+    root_span = Span(name="span1")
+    root_span._local_root = root_span
+    # Root span in trace is not a test
+    trace = [root_span]
+    assert trace_filter.process_trace(trace) is None
 
 
 def test_ci_visibility_service_enable():
