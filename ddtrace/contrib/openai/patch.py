14b754b4a331b58f02f50aaf1bf38b01beb7947c
lievan <evan.li@datadoghq.com>
2023-04-06 10:29:13 -0400

    trace completion request and response

--- ddtrace/contrib/openai/patch.py
+++ ddtrace/contrib/openai/patch.py
@@ -1,6 +1,7 @@
 import openai
 
 from ddtrace import config
+from ddtrace.contrib.trace_utils import set_flattened_tags
 from ddtrace.internal.constants import COMPONENT
 from ddtrace.vendor import wrapt
 
@@ -16,6 +17,10 @@ config._add(
     },
 )
 
+REQUEST_TAG_PREFIX="request"
+RESPONSE_TAG_PREFIX="response"
+ERROR_TAG_PREFIX="error"
+ENGINE="engine"
 
 def patch():
     # Do monkey patching here
@@ -24,62 +29,42 @@ def patch():
     setattr(openai, "__datadog_patch", True)
     _w = wrapt.wrap_function_wrapper
     _w("openai", "api_resources.abstract.engine_api_resource.EngineAPIResource.create", patched_create)
-    # _w("openai", "Completion.create", traced_create)
-    # _w("openai", "OpenAIObject.__init__", traced_init)
-    # _w("openai", "OpenAIObject.construct_from", traced_init)
-
+    Pin().onto(openai)
 
 def unpatch():
     # Undo the monkey patching that patch() did here
     if getattr(openai, "__datadog_patch", False):
         setattr(openai, "__datadog_patch", False)
         unwrap(openai.api_resources.abstract.engine_api_resource.EngineAPIResource, "create")
-        # _u(openai.OpenAIObject, "__init__")
-        # _u(openai.OpenAIObject, "construct_from")
 
+def append_tag_prefixes(key_prefixes: list[str], data: dict):
+    prefix = ".".join(key_prefixes) + "."
+    return [(prefix + str(k), v) for k, v in data.items()]
+
+def process_response(engine, resp):
+    # alter the response tag value based on the `engine` 
+    # (completions, chat.completions, embeddings)
+    if engine == openai.Completion.OBJECT_NAME:
+        resp["choices"] = {str(i):dict(completion) for i, completion in enumerate(resp.choices)}
+        return resp
+    return {}
 
 def patched_create(func, instance, args, kwargs):
     pin = Pin.get_from(openai)
     if not pin or not pin.enabled():
         return func(*args, **kwargs)
-
-    with pin.tracer.trace(instance.OBJECT_NAME) as span:
+    # resource name is set to the model being used -- if that name is not found, use the engine name
+    sname = kwargs.get("model") if kwargs.get("model") is not None else instance.OBJECT_NAME
+    with pin.tracer.trace(sname) as span:
         span.set_tag_str(COMPONENT, config.openai.integration_name)
-        for k, v in args:
-            span.set_tag_str(k, v)
-        for k, v in kwargs.items():
-            span.set_tag_str(k, v)
+        span.set_tag_str(ENGINE, instance.OBJECT_NAME)
+        set_flattened_tags(span, append_tag_prefixes([REQUEST_TAG_PREFIX], {**kwargs, **{k:v for k, v in args}}))
+        resp = {}
         try:
             resp = func(*args, **kwargs)
+            set_flattened_tags(span, append_tag_prefixes([RESPONSE_TAG_PREFIX], process_response(instance.OBJECT_NAME, resp)))
             return resp
         except openai.error.OpenAIError as err:
-            span.set_tag("error", str(err))
+            set_flattened_tags(span, append_tag_prefixes([RESPONSE_TAG_PREFIX, ERROR_TAG_PREFIX], {"code": err.code, "message": str(err)}))
             span.finish()
             raise err
-
-
-# OpenAI Object tracing?
-
-# def traced_init(func, instance, args, kwargs):
-#     func(*args, **kwargs)
-
-#     tags = {}
-#     for arg in ["organization", "api_version", "api_type", "reponse_ms", "api_base", "engine"]:
-#         tags[arg] = args.get(arg)
-#     # set tracing meta-data for this model
-#     pin = Pin(service=args.get("organization"), tags=tags)
-#     pin.onto(instance)
-
-# def traced_construct(func, instance, args, kwargs):
-#     tags = {}
-#     for arg in ["organization", "api_version", "api_type", "reponse_ms", "api_base", "engine"]:
-#         tags[arg] = args.get(arg)
-#     obj = func(*args, **kwargs)
-#     # set tracing meta-data for this model
-#     pin = Pin(service=args.get("organization"))
-#     pin.onto(obj)
-#     return obj
-
-# def traced_refresh(func, instance, args, kwargs):
-#     # get the pin from the object first
-#     pass
--- tests/contrib/openai/test_openai.py
+++ tests/contrib/openai/test_openai.py
@@ -1,4 +1,5 @@
 import openai
+import os
 
 from ddtrace.contrib.openai.patch import patch
 from ddtrace.contrib.openai.patch import unpatch
@@ -16,13 +17,11 @@ class TestOpenAI(TracerTestCase):
         super(TestOpenAI, self).tearDown()
         unpatch()
 
-    def test_create(self):
-        # create a completion
+    def test_completion(self):
+        # create a completion (expected to fail since api key is not set)
         openai.Completion.create(model="ada", prompt="Hello world")
         spans = self.pop_spans()
         assert len(spans) > 0
         tags = spans[0].get_tags()
         assert tags["model"] == "ada"
         assert tags["prompt"] == "Hello world"
-        print(tags)
-        exit(1)
