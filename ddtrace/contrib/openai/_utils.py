b4f8350d351c677e595c2913c610c1f30f9ba44d
lievan <evan.li@datadoghq.com>
2023-04-07 17:11:19 -0400

    check for supported engines since it differs based on openai version

--- ddtrace/contrib/openai/_utils.py
+++ ddtrace/contrib/openai/_utils.py
@@ -5,6 +5,10 @@ if typing.TYPE_CHECKING:
     from typing import Dict
     from typing import List
 
+# SUPPORTED_ENGINES specifies the engines we support custom span formatting
+# for request/response data.
+SUPPORTED_ENGINES = {"ChatCompletion": "chat.completions", "Completions": "completions", "Embeddings": "embeddings"}
+
 
 def append_tag_prefixes(key_prefixes, data):
     # type: (List[str], Dict[str, str]) -> Dict[str, str]
@@ -18,34 +22,61 @@ def expand(data):
     return data
 
 
+def update_engine_names(openai):
+    if hasattr(openai, "ChatCompletion") and hasattr(openai.ChatCompletion, "OBJECT_NAME"):
+        SUPPORTED_ENGINES["ChatCompletion"] == openai.ChatCompletion.OBJECT_NAME
+    if hasattr(openai, "Completion") and hasattr(openai.Completion, "OBJECT_NAME"):
+        SUPPORTED_ENGINES["Completions"] == openai.Completion.OBJECT_NAME
+    if hasattr(openai, "Embeddings") and hasattr(openai.Embeddings, "OBJECT_NAME"):
+        SUPPORTED_ENGINES["Embeddings"] == openai.Embeddings.OBJECT_NAME
+
+
+def process_text(text):
+    if isinstance(text, str):
+        text = text.replace("\n", "\\n")
+    return text
+
+
+def infer_object_name(kwargs):
+    if kwargs.get("messages") is not None:
+        return "chat.completions"
+    elif kwargs.get("input") is not None:
+        return "embeddings"
+    elif kwargs.get("prompt") is not None:
+        return "completions"
+    return "<default>"
+
+
 def process_response(openai, engine, resp):
     # alter the response tag value based on the `engine`
     # (completions, chat.completions, embeddings)
     try:
+        update_engine_names(openai)
         resp = openai.util.convert_to_dict(resp)
-        if engine in (openai.ChatCompletion.OBJECT_NAME, openai.Completion.OBJECT_NAME):
+        if engine in (SUPPORTED_ENGINES["ChatCompletion"], SUPPORTED_ENGINES["Completions"]):
             resp["choices"] = expand(resp["choices"])
             return resp
-        elif engine == openai.Embedding.OBJECT_NAME:
+        elif engine == SUPPORTED_ENGINES["Embeddings"]:
             if "data" in resp:
                 resp["data"] = {
                     "num-embeddings": len(resp["data"]),
                     "embedding-length": len(resp["data"][0]["embedding"]),
                 }
                 return resp
+        else:
+            return resp
     except KeyError or IndexError:
         return {}
 
 
 def process_request(openai, engine, args, kwargs):
     try:
-        if engine in [openai.Completion.OBJECT_NAME, openai.Embedding.OBJECT_NAME]:
-            return {**kwargs, **{k: v for k, v in args}}
-        elif engine == openai.ChatCompletion.OBJECT_NAME:
+        update_engine_names(openai)
+        if engine == SUPPORTED_ENGINES["ChatCompletion"]:
             kwargs = kwargs.copy()
             messages = kwargs.get("messages")
             # messages are a series of messages each defined by a `role` and `content`
             kwargs["messages"] = expand(messages)
-            return {**kwargs, **{k: v for k, v in args}}
+        return {**kwargs, **{k: v for k, v in args}}
     except KeyError or IndexError:
         return {}
--- ddtrace/contrib/openai/patch.py
+++ ddtrace/contrib/openai/patch.py
@@ -7,8 +7,10 @@ from .. import trace_utils
 from ...pin import Pin
 from ..trace_utils import unwrap
 from ._utils import append_tag_prefixes
+from ._utils import infer_object_name
 from ._utils import process_request
 from ._utils import process_response
+from ._utils import process_text
 
 
 config._add(
@@ -49,18 +51,25 @@ def unpatch():
 @trace_utils.with_traced_module
 def patched_create(openai, pin, func, instance, args, kwargs):
     # resource name is set to the model being used -- if that name is not found, use the engine name
+    if not hasattr(instance, "OBJECT_NAME"):
+        setattr(instance, "OBJECT_NAME", infer_object_name(kwargs))
     resource = kwargs.get("model") if kwargs.get("model") is not None else instance.OBJECT_NAME
-
-    with pin.tracer.trace("openai.request", resource=resource, service=trace_utils.ext_service(pin, config.openai)) as span:
+    with pin.tracer.trace(
+        "openai.request", resource=resource, service=trace_utils.ext_service(pin, config.openai)
+    ) as span:
         span.set_tag_str(COMPONENT, config.openai.integration_name)
         span.set_tag_str(ENGINE, instance.OBJECT_NAME)
         set_flattened_tags(
-            span, append_tag_prefixes([REQUEST_TAG_PREFIX], process_request(openai, instance.OBJECT_NAME, args, kwargs))
+            span,
+            append_tag_prefixes([REQUEST_TAG_PREFIX], process_request(openai, instance.OBJECT_NAME, args, kwargs)),
+            processor=process_text,
         )
         try:
             resp = func(*args, **kwargs)
             set_flattened_tags(
-                span, append_tag_prefixes([RESPONSE_TAG_PREFIX], process_response(openai, instance.OBJECT_NAME, resp))
+                span,
+                append_tag_prefixes([RESPONSE_TAG_PREFIX], process_response(openai, instance.OBJECT_NAME, resp)),
+                processor=process_text,
             )
             return resp
         except openai.error.OpenAIError as err:
