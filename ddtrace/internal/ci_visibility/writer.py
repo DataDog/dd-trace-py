26116bc6a9a7c7355d4423238d79faccadb8b145
Emmett Butler <emmett.butler321@gmail.com>
2023-04-18 13:49:12 -0700

    make sync_mdoe=False the default to improve default performance

--- ddtrace/internal/ci_visibility/writer.py
+++ ddtrace/internal/ci_visibility/writer.py
@@ -46,7 +46,7 @@ class CIVisibilityWriter(HTTPWriter):
         processing_interval=get_writer_interval_seconds(),  # type: float
         timeout=agent.get_trace_agent_timeout(),  # type: float
         dogstatsd=None,  # type: Optional[DogStatsd]
-        sync_mode=True,  # type: bool
+        sync_mode=False,  # type: bool
         report_metrics=False,  # type: bool
         api_version=None,  # type: Optional[str]
         reuse_connections=None,  # type: Optional[bool]
