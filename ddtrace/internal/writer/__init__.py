5b9474d2b40bd4d34794bf31c3786ba56800d377
Emmett Butler <emmett.butler321@gmail.com>
2023-04-18 11:48:08 -0700

    broken import

--- ddtrace/internal/writer/__init__.py
+++ ddtrace/internal/writer/__init__.py
@@ -4,6 +4,6 @@ from .writer import HTTPWriter  # noqa
 from .writer import LogWriter  # noqa
 from .writer import Response  # noqa
 from .writer import TraceWriter  # noqa
-from .writer import _human_size  # noqa
 from .writer import get_writer_interval_seconds  # noqa
+from .writer import human_size  # noqa
 from .writer_client import WriterClientBase  # noqa
