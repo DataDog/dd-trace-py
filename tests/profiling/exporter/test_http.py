c5644c5e622836bc3f5a24de5595bfd3fc793feb
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2023-04-24 17:31:42 +0100

    refactor(profiling): use envier for configuration

--- tests/profiling/exporter/test_http.py
+++ tests/profiling/exporter/test_http.py
@@ -455,14 +455,20 @@ def test_get_tags_override(monkeypatch):
     assert tags["mytag"] == "123"
 
 
-def test_get_tags_legacy(monkeypatch):
-    monkeypatch.setenv("DD_PROFILING_TAGS", "mytag:baz")
+@pytest.mark.subprocess(env=dict(DD_PROFILING_TAGS="mytag:baz"))
+def test_get_tags_legacy():
+    from ddtrace.internal.utils.formats import parse_tags_str
+    from ddtrace.profiling.exporter import http
+
     tags = parse_tags_str(http.PprofHTTPExporter(endpoint="")._get_tags("foobar"))
     assert tags["mytag"] == "baz"
 
-    # precedence
-    monkeypatch.setenv("DD_TAGS", "mytag:val1,ddtag:hi")
-    monkeypatch.setenv("DD_PROFILING_TAGS", "mytag:val2,ddptag:lo")
+
+@pytest.mark.subprocess(env=dict(DD_PROFILING_TAGS="mytag:val2,ddptag:lo", DD_TAGS="mytag:val1,ddtag:hi"))
+def test_get_tags_precedence():
+    from ddtrace.internal.utils.formats import parse_tags_str
+    from ddtrace.profiling.exporter import http
+
     tags = parse_tags_str(http.PprofHTTPExporter(endpoint="")._get_tags("foobar"))
     assert tags["mytag"] == "val2"
     assert tags["ddtag"] == "hi"
