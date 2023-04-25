7a324b81b14b659622d8c8bc1fa9e35f34f8dd70
lievan <evan.li@datadoghq.com>
2023-04-25 14:29:10 -0400

    change deb to openai[embeddings]

--- riotfile.py
+++ riotfile.py
@@ -2424,21 +2424,13 @@ venv = Venv(
         Venv(
             name="openai",
             command="pytest {cmdargs} tests/contrib/openai",
+            pys=select_pys(min_version="3.8"),
             pkgs={
-                "openai": ["==0.26.5", "==0.27.2", "==0.27.3", "==0.27.4", latest],
+                "openai[embeddings]": ["==0.26.5", "==0.27.2", "==0.27.3", "==0.27.4", latest],
                 "vcrpy": "==4.2.1",
                 "packaging": latest,
                 "pytest-asyncio": latest,
             },
-            venvs=[
-                Venv(
-                    pys=select_pys(min_version="3.8", max_version="3.10"),
-                    pkgs={
-                        "numpy": ["==1.21.1"],
-                    },
-                ),
-                Venv(pys=select_pys(min_version="3.11"), pkgs={"numpy": ["==1.24.2"]}),
-            ],
         ),
         Venv(
             name="opentracer",
