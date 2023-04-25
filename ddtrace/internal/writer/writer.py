671d635221bbaaf08f1bf5de8a419541bb415dc1
AJ Stuyvenberg <astuyve@gmail.com>
2023-04-25 10:49:44 -0400

    Update ddtrace/internal/writer/writer.py
    Co-authored-by: Munir Abdinur <munir.abdinur@datadoghq.com>

--- ddtrace/internal/writer/writer.py
+++ ddtrace/internal/writer/writer.py
@@ -721,7 +721,8 @@ class AgentWriter(HTTPWriter):
 
             # appsec remote config should be enabled/started after the global tracer and configs
             # are initialized
-            if should_start_remoteconfig:
+            if config._appsec_enabled or asbool(os.environ.get("DD_REMOTE_CONFIGURATION_ENABLED", "true")):
+                from ddtrace.appsec._remoteconfiguration import enable_appsec_rc
                 enable_appsec_rc()
         except service.ServiceStatusError:
             pass
