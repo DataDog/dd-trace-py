b7bed0e0d87485814723359bf2540bf4c7f3ed4f
Teague Bick <teague.bick@datadoghq.com>
2023-04-26 18:43:12 -0400

    s/storage/database/g

--- ddtrace/internal/schema/__init__.py
+++ ddtrace/internal/schema/__init__.py
@@ -22,12 +22,12 @@ _validate_schema(__schema_version)
 
 DEFAULT_SPAN_SERVICE_NAME = _DEFAULT_SPAN_SERVICE_NAMES[__schema_version]
 schematize_service_name = _SPAN_ATTRIBUTE_TO_FUNCTION[__schema_version]["service_name"]
-schematize_storage_operation = _SPAN_ATTRIBUTE_TO_FUNCTION[__schema_version]["storage_operation"]
+schematize_database_operation = _SPAN_ATTRIBUTE_TO_FUNCTION[__schema_version]["database_operation"]
 schematize_cache_operation = _SPAN_ATTRIBUTE_TO_FUNCTION[__schema_version]["cache_operation"]
 
 __all__ = [
     "DEFAULT_SPAN_SERVICE_NAME",
     "schematize_service_name",
-    "schematize_storage_operation",
+    "schematize_database_operation",
     "schematize_cache_operation",
 ]
--- ddtrace/internal/schema/span_attribute_schema.py
+++ ddtrace/internal/schema/span_attribute_schema.py
@@ -8,11 +8,11 @@ def service_name_v1(*_, **__):
     return dd_config.service
 
 
-def storage_operation_v0(v0_operation, database_provider=None):
+def database_operation_v0(v0_operation, database_provider=None):
     return v0_operation
 
 
-def storage_operation_v1(v0_operation, database_provider=None):
+def database_operation_v1(v0_operation, database_provider=None):
     operation = "query"
     assert database_provider is not None, "You must specify a database provider, not 'None'"
     return "{}.{}".format(database_provider, operation)
@@ -31,12 +31,12 @@ def cache_operation_v1(v0_operation, cache_provider=None):
 _SPAN_ATTRIBUTE_TO_FUNCTION = {
     "v0": {
         "service_name": service_name_v0,
-        "storage_operation": storage_operation_v0,
+        "database_operation": database_operation_v0,
         "cache_operation": cache_operation_v0,
     },
     "v1": {
         "service_name": service_name_v1,
-        "storage_operation": storage_operation_v1,
+        "database_operation": database_operation_v1,
         "cache_operation": cache_operation_v1,
     },
 }
