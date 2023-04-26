c8eac858c02114754f621b05e8c9a0d656e62c89
Emmett Butler <723615+emmettbutler@users.noreply.github.com>
2023-04-26 09:07:10 -0700

    feat(pytest): coverage reporting (#5522)
    This change adds code coverage reporting to the pytest integration.
    Coverage information is calculated with the `coverage` package, stored
    at runtime as a span tag, and transmitted to the intake via the
    `CIVisibilityWriter` and `CIVisibilityCoverageClient`. Off by default,
    coverage tracking is enabled when the runtime is Python 3, the
    `coverage` package is installed, and the
    `DD_CIVISIBILITY_CODE_COVERAGE_ENABLED` flag is set.
    
    [Benchmark
    results](https://gist.github.com/emmettbutler/0d183fb3818e261bc516ee3715c27494):
    agentless is slower than baseline because the encoder is implemented in
    pure Python. Coverage is about 50% slower than basic agentless.
    
    I've punted on Python 2 support for the moment, because the latest
    version of Coverage doesn't support Python 2 and the private API on
    which this implementation depends has changed since the last version
    that supported Python 2. Python 3 support seems to me to be a solid beta
    implementation of this feature given those constraints. Let me know what
    you think.
    
    ### Changes
    
    * Under Py3, runs traced pytest code in a `coverage` context controlled
    by the new environment variable `DD_CIVISIBILITY_CODE_COVERAGE_ENABLED`
    * Adds `ci_visibility/coverage.py` implementing the `cover` context
    manager, which uses the `coverage` library to attach code coverage
    information to spans as a tag
    * Adds `CIVisibilityCoverageEncoderV02` implementing the wire format
    expected by the `citestcov` intake, and test
    * Adds `CIVisibilityCoverageClient` to the `CIVisibilityWriter`'s list
    of clients
    * Depends on already-present `coverage` installation. If the coverage
    envvar is set but the library is not available, generates a helpful
    error message
    * Adds a test verifying that the pytest integration generates coverage
    tags
    * Adds a test verifying that the appropriate data are sent to each of
    the `CIVisibilityWriter`'s multiple endpoints
    
    Co-authored-by: Yun Kim <35776586+Yun-Kim@users.noreply.github.com>

--- ddtrace/internal/ci_visibility/encoder.py
+++ ddtrace/internal/ci_visibility/encoder.py
@@ -1,3 +1,4 @@
+import json
 import threading
 from typing import Any
 from typing import Dict
@@ -6,6 +7,8 @@ from typing import TYPE_CHECKING
 from .._encoding import BufferedEncoder
 from .._encoding import packb as msgpack_packb
 from ..encoding import JSONEncoderV2
+from ..writer.writer import NoEncodableSpansError
+from .constants import COVERAGE_TAG_NAME
 
 
 if TYPE_CHECKING:  # pragma: no cover
@@ -21,6 +24,7 @@ class CIVisibilityEncoderV01(BufferedEncoder):
     def __init__(self, *args):
         super(CIVisibilityEncoderV01, self).__init__()
         self._lock = threading.RLock()
+        self._metadata = {}
         self._init_buffer()
         self._metadata = {}
 
@@ -72,6 +76,8 @@ class CIVisibilityEncoderV01(BufferedEncoder):
         sp["span_id"] = int(sp.get("span_id") or "1")
         sp["test_suite_id"] = 1  # TODO: populate with real ID
         sp["test_session_id"] = 1  # TODO: populate with real ID
+        if COVERAGE_TAG_NAME in sp["meta"]:
+            del sp["meta"][COVERAGE_TAG_NAME]
         if dd_origin is not None:
             sp["meta"].update({"_dd.origin": dd_origin})
         if span.span_type == "test":
@@ -79,3 +85,35 @@ class CIVisibilityEncoderV01(BufferedEncoder):
         else:
             event_type = "span"
         return {"version": CIVisibilityEncoderV01.TEST_EVENT_VERSION, "type": event_type, "content": sp}
+
+
+class CIVisibilityCoverageEncoderV02(CIVisibilityEncoderV01):
+    PAYLOAD_FORMAT_VERSION = 2
+
+    def put(self, spans):
+        spans_with_coverage = [span for span in spans if COVERAGE_TAG_NAME in span.get_tags()]
+        if not spans_with_coverage:
+            raise NoEncodableSpansError()
+        return super(CIVisibilityCoverageEncoderV02, self).put(spans_with_coverage)
+
+    def _build_payload(self, traces):
+        normalized_covs = [
+            CIVisibilityCoverageEncoderV02._convert_span(span, "")
+            for trace in traces
+            for span in trace
+            if COVERAGE_TAG_NAME in span.get_tags()
+        ]
+        if not normalized_covs:
+            return
+        # TODO: Split the events in several payloads as needed to avoid hitting the intake's maximum payload size.
+        return msgpack_packb({"version": self.PAYLOAD_FORMAT_VERSION, "coverages": normalized_covs})
+
+    @staticmethod
+    def _convert_span(span, dd_origin):
+        # type: (Span, str) -> Dict[str, Any]
+        return {
+            "span_id": span.span_id,
+            "test_session_id": 1,  # TODO: populate with real IDs
+            "test_suite_id": 1,
+            "files": json.loads(span.get_tag(COVERAGE_TAG_NAME))["files"],
+        }
