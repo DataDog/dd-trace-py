fbba5809fec6e2bf17aecfea75377dde40a47e26
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-04-27 11:10:25 +0200

    add regression test into djangorestframework

--- tests/contrib/djangorestframework/app/views.py
+++ tests/contrib/djangorestframework/app/views.py
@@ -1,9 +1,18 @@
+import json
+
 import django
 from django.conf.urls import include
 from django.contrib.auth.models import User
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import routers
 from rest_framework import serializers
 from rest_framework import viewsets
+from rest_framework.decorators import authentication_classes
+from rest_framework.decorators import permission_classes
+from rest_framework.parsers import MultiPartParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 # django.conf.urls.url was deprecated in django 3 and removed in django 4
@@ -31,9 +40,42 @@ class UserViewSet(viewsets.ModelViewSet):
 router = routers.DefaultRouter()
 router.register(r"users", UserViewSet)
 
+
+# ASM
+
+
+class CustomParser(MultiPartParser):
+    def parse(self, stream, media_type=None, parser_context=None):
+        """
+        Return a dataframe representing mds
+        """
+        parsed_files = super().parse(stream, media_type, parser_context)
+        value = json.loads(parsed_files.data.get("json", "")).get("value")
+        return {value: parsed_files.files.get("file1").read().decode("utf-8", errors="ignore")}
+
+
+@authentication_classes([])
+@permission_classes([])
+class ASM_View(APIView):
+    """
+    MDS generation through CSV file export — DEPRECATED
+    This mechanism will be replaced by generation_from_dispatch below once
+    it is adopted.
+    """
+
+    parser_classes = (CustomParser,)
+
+    @csrf_exempt
+    def post(self, request):
+        return Response({"received data form": request.data})
+
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
+
+
 urlpatterns = [
+    path(r"asm/", ASM_View.as_view()),
     handler(r"^", include(router.urls)),
     handler(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
