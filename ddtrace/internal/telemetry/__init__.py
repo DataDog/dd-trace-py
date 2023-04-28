ec00f42b02ec56d6c52f7640e057f45fef3bdf0f
Munir Abdinur <munir_abdinur@hotmail.com>
2023-04-28 17:57:23 -0400

    move install_exception hook to telemetry module

--- ddtrace/internal/telemetry/__init__.py
+++ ddtrace/internal/telemetry/__init__.py
@@ -6,6 +6,8 @@ To start the service manually, invoke the ``enable`` method::
     from ddtrace.internal.telemetry import telemetry_writer
     telemetry_writer.enable()
 """
+import sys
+
 from .writer import TelemetryLogsMetricsWriter
 from .writer import TelemetryWriter
 
@@ -14,3 +16,22 @@ telemetry_metrics_writer = TelemetryLogsMetricsWriter()
 telemetry_writer = TelemetryWriter()
 
 __all__ = ["telemetry_writer"]
+
+
+_ORIGINAL_EXCEPTHOOK = sys.excepthook
+
+
+def _excepthook(tp, value, traceback):
+    telemetry_writer.add_error(1, repr(value))
+    telemetry_writer.enable()
+    return _ORIGINAL_EXCEPTHOOK(tp, value, traceback)
+
+
+def install_excepthook():
+    """Install a hook that intercepts unhandled exception and send metrics about them."""
+    sys.excepthook = _excepthook
+
+
+def uninstall_excepthook():
+    """Uninstall the global tracer except hook."""
+    sys.excepthook = _ORIGINAL_EXCEPTHOOK
