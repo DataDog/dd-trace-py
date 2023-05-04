383389444c76453212e826715a6d22749b5876f5
David Lee <david.lee@datadoghq.com>
2023-05-04 12:09:32 -0700

    make the linter happy with shorter lines

--- ddtrace/internal/serverless/mini_agent.py
+++ ddtrace/internal/serverless/mini_agent.py
@@ -14,7 +14,15 @@ def maybe_start_serverless_mini_agent():
         return
 
     try:
-        rust_binary_path = os.getenv("DD_MINI_AGENT_PATH", "/layers/google.python.pip/pip/lib/python%d.%d/site-packages/datadog-serverless-agent-linux-amd64/datadog-serverless-trace-mini-agent" % PYTHON_VERSION_INFO[:2])
+        python_folder_name = "python%d.%d" % PYTHON_VERSION_INFO[:2]
+
+        rust_binary_path_default = (
+            "/layers/google.python.pip/pip/lib/%s/site-packages/"
+            "datadog-serverless-agent-linux-amd64/datadog-serverless-trace-mini-agent"
+        ) % python_folder_name
+
+        rust_binary_path = os.getenv("DD_MINI_AGENT_PATH", rust_binary_path_default)
+
         log.debug("rust_binary_path: %s", rust_binary_path)
         Popen(rust_binary_path)
     except Exception as e:
