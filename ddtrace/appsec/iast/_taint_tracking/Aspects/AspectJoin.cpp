48c87aa6cddb0c8fef7d33e113b3d5a0cc61dcef
Alberto Vara <alberto.vara@datadoghq.com>
2023-05-24 17:26:29 +0200

    update ast visitor

--- ddtrace/appsec/iast/_taint_tracking/Aspects/AspectJoin.cpp
+++ ddtrace/appsec/iast/_taint_tracking/Aspects/AspectJoin.cpp
@@ -13,57 +13,57 @@ PyObject* aspect_join(const PyObject* sep,
 
     const auto& to_joiner = get_tainted_object(sep, tx_taint_map);
 
-//    for (size_t i = 0; i < len_tuple; i++) {
-//        PyObject* element = PyTuple_GetItem(iterable_tuple, i);
-//        if (!element) {
-//            break;
-//        }
-//
-//        // The reason of this validation is the behavior of Python2:
-//        // u"a".join(b"c", b"d") -> unicode
-//        // b"a".join(u"c", u"d") -> unicode
-//        // b"a".join(u"c", b"d") -> unicode
-//
-//        const size_t& element_len = PyUnicode_GET_LENGTH(element);
-//        if (element_len > 0) {
-//            const auto& to_element = get_tainted_object(element, tx_taint_map);
-//            if (to_element) {
-//                if (current_pos == 0 and !first_tainted_to) {
-//                    first_tainted_to = to_element;
-//                } else {
-//                    if (!result_to) {
-//                        // If first_tainted_to is null, it's ranges won't be copied
-//                        result_to = initializer->allocate_tainted_object_copy(first_tainted_to);
-//                        first_tainted_to = nullptr;
-//                    }
-//                    result_to->add_ranges_shifted(to_element, current_pos);
-//                }
-//            }
-//
-//            current_pos += element_len;
-//        }
-//        if (len_sep > 0 and i < len_tuple - 1 and to_joiner) {
-//            if (!result_to) {
-//                // If first_tainted_to is null, it's ranges won't be copied
-//                result_to = initializer->allocate_tainted_object_copy(first_tainted_to);
-//                first_tainted_to = nullptr;
-//            }
-//            result_to->add_ranges_shifted(to_joiner, current_pos);
-//        }
-//        current_pos += len_sep;
-//    }
+    for (size_t i = 0; i < len_tuple; i++) {
+        PyObject* element = PyTuple_GetItem(iterable_tuple, i);
+        if (!element) {
+            break;
+        }
 
-//    if (!result_to) {
-//        if (first_tainted_to) {
-//            result_to = initializer->allocate_tainted_object_copy(first_tainted_to);
-//        } else {
-//            // No taints at all
-//            return result;
-//        }
-//    }
-//
-//    PyObject* new_result{copy_string_new_str_id(result)};
-//    set_tainted_object(new_result, result_to, tx_taint_map);
+        // The reason of this validation is the behavior of Python2:
+        // u"a".join(b"c", b"d") -> unicode
+        // b"a".join(u"c", u"d") -> unicode
+        // b"a".join(u"c", b"d") -> unicode
+
+        const size_t& element_len = PyUnicode_GET_LENGTH(element);
+        if (element_len > 0) {
+            const auto& to_element = get_tainted_object(element, tx_taint_map);
+            if (to_element) {
+                if (current_pos == 0 and !first_tainted_to) {
+                    first_tainted_to = to_element;
+                } else {
+                    if (!result_to) {
+                        // If first_tainted_to is null, it's ranges won't be copied
+                        result_to = initializer->allocate_tainted_object_copy(first_tainted_to);
+                        first_tainted_to = nullptr;
+                    }
+                    result_to->add_ranges_shifted(to_element, current_pos);
+                }
+            }
+
+            current_pos += element_len;
+        }
+        if (len_sep > 0 and i < len_tuple - 1 and to_joiner) {
+            if (!result_to) {
+                // If first_tainted_to is null, it's ranges won't be copied
+                result_to = initializer->allocate_tainted_object_copy(first_tainted_to);
+                first_tainted_to = nullptr;
+            }
+            result_to->add_ranges_shifted(to_joiner, current_pos);
+        }
+        current_pos += len_sep;
+    }
+
+    if (!result_to) {
+        if (first_tainted_to) {
+            result_to = initializer->allocate_tainted_object_copy(first_tainted_to);
+        } else {
+            // No taints at all
+            return result;
+        }
+    }
+
+    PyObject* new_result{new_pyobject_id(result, PyUnicode_GET_LENGTH(result))};
+    //set_tainted_object(new_result, result_to, tx_taint_map);
 
     return result;
 }
