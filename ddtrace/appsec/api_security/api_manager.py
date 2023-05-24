57886d0ccc741cce4ecf778599acf3e250128b30
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-05-24 15:29:29 +0200

    api security with custom tags

---
+++ ddtrace/appsec/api_security/api_manager.py
@@ -0,0 +1,37 @@
+import os
+
+from ddtrace.appsec._asm_request_context import add_context_callback
+from ddtrace.appsec._asm_request_context import remove_context_callback
+from ddtrace.appsec._constants import SPAN_DATA_NAMES
+
+from .schema import get_json_schema
+
+
+COLLECTED = [
+    SPAN_DATA_NAMES.REQUEST_HEADERS_NO_COOKIES,
+    SPAN_DATA_NAMES.REQUEST_QUERY,
+    SPAN_DATA_NAMES.REQUEST_PATH_PARAMS,
+    SPAN_DATA_NAMES.REQUEST_BODY,
+    SPAN_DATA_NAMES.RESPONSE_HEADERS_NO_COOKIES,
+    SPAN_DATA_NAMES.RESPONSE_BODY,
+]
+
+
+def flask_api_schema_callback(env):
+    waf_content = env.waf_addresses
+    api_content = {}
+    for addresses in COLLECTED:
+        value = waf_content.get(addresses, None)
+        if value:
+            api_content[addresses] = get_json_schema(value)
+    env.span._meta["api_security"] = api_content
+
+
+def enable_api_security():
+    if "_DD_API_SECURITY" in os.environ:
+        add_context_callback(flask_api_schema_callback, global_callback=True)
+
+
+def disable_api_security():
+    if "_DD_API_SECURITY" in os.environ:
+        remove_context_callback(flask_api_schema_callback, global_callback=True)
