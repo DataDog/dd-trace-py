dd0ba2a809fc48e28bf4149ce078a712f603ff62
Teague Bick <teague.bick@datadoghq.com>
2023-05-31 09:48:00 -0400

    chore(service naming): multiple contribs - sqlalchemy
    == Special Note ==
    These changes are gated behind a feature flag and are not expected to be
    stable until a release note is generated.  Existing customers should be
    unaffected.
    
    == Service Naming Summary ==
    Datadog is updating its method of determining service names!
    
    Service naming introduces a few concepts:
    
    * Versioned service name "schemas":
      * v0: The existing mechanism for determining service names
    * v1: Replacing the 'default' contrib-specific service name with the env
    var $DD_SERVICE
    
    These changes are being in conjunction with changes to peer.service
    updates to bring a better service-naming experience to customers. In
    particular, these changes will help:
    
    * Remove the creation of fake services, such as grpc.client
    * Standardize service names across and within tracers
    * Remove the need to create as many fake services to take advantage of
    Datadog features, such as metrics
    * Make more sense to humans
    
    == Operation Naming Summary ==
    The service name changes mean that generated spans can no longer rely on
    `span.service` to differentiate between processes. A subclass of spans
    now have a new problem - differentiating between inbound and outbound
    requests (for the purposes of metrics). For instance, spans with names
    `http.request` used to be differentiated by the service they belonged to
    (incoming requests under `span.service=$DD_SERVICE` and outgoing under
    `span.service=$DD_CONTRIB_SERVICE_NAME`). These are now no longer
    differentiated.
    
    To remedy these issues, operation names are also being changed as part
    of the service naming effort. In the above example, `http.request` turns
    into `http.server.request` (incoming) and `http.client.request`
    (outgoing).
    
    == This Change ==
    This change updates the following contribs:
    * sqlalchemy

--- tests/contrib/sqlalchemy/test_mysql.py
+++ tests/contrib/sqlalchemy/test_mysql.py
@@ -54,3 +54,73 @@ class MysqlConnectorTestCase(SQLAlchemyTestMixin, TracerTestCase):
         self.assertEqual(span.get_tag("error.type"), "mysql.connector.errors.ProgrammingError")
         self.assertTrue("Table 'test.a_wrong_table' doesn't exist" in span.get_tag(ERROR_MSG))
         self.assertTrue("Table 'test.a_wrong_table' doesn't exist" in span.get_tag("error.stack"))
+
+
+class TestSchematization(SQLAlchemyTestMixin, TracerTestCase):
+    """TestCase for mysql-connector engine"""
+
+    ENGINE_ARGS = {"url": "mysql+mysqlconnector://%(user)s:%(password)s@%(host)s:%(port)s/%(database)s" % MYSQL_CONFIG}
+
+    def setUp(self):
+        super(TestSchematization, self).setUp()
+
+    def tearDown(self):
+        super(TestSchematization, self).tearDown()
+
+    def _generate_span(self):
+        with pytest.raises(ProgrammingError):
+            with self.connection() as conn:
+                conn.execute("SELECT * FROM a_wrong_table").fetchall()
+
+        traces = self.pop_traces()
+        span = traces[0][0]
+
+        return span
+
+    @TracerTestCase.run_in_subprocess(env_overrides=dict(DD_SERVICE="mysvc"))
+    def test_schematized_service_name_default(self):
+        span = self._generate_span()
+
+        self.assertEqual(span.service, "mysql")
+
+    @TracerTestCase.run_in_subprocess(env_overrides=dict(DD_SERVICE="mysvc", DD_TRACE_SPAN_ATTRIBUTE_SCHEMA="v0"))
+    def test_schematized_service_name_v0(self):
+        span = self._generate_span()
+
+        self.assertEqual(span.service, "mysql")
+
+    @TracerTestCase.run_in_subprocess(env_overrides=dict(DD_SERVICE="mysvc", DD_TRACE_SPAN_ATTRIBUTE_SCHEMA="v1"))
+    def test_schematized_service_name_v1(self):
+        span = self._generate_span()
+
+        self.assertEqual(span.service, "mysvc")
+
+    @TracerTestCase.run_in_subprocess(env_overrides=dict())
+    def test_schematized_unspecified_service_name_default(self):
+        span = self._generate_span()
+
+        self.assertEqual(span.service, "mysql")
+
+    @TracerTestCase.run_in_subprocess(env_overrides=dict(DD_TRACE_SPAN_ATTRIBUTE_SCHEMA="v0"))
+    def test_schematized_unspecified_service_name_v0(self):
+        span = self._generate_span()
+
+        self.assertEqual(span.service, "mysql")
+
+    @TracerTestCase.run_in_subprocess(env_overrides=dict(DD_TRACE_SPAN_ATTRIBUTE_SCHEMA="v1"))
+    def test_schematized_unspecified_service_name_v1(self):
+        span = self._generate_span()
+
+        self.assertEqual(span.service, "unnamed-python-service")
+
+    @TracerTestCase.run_in_subprocess(env_overrides=dict(DD_TRACE_SPAN_ATTRIBUTE_SCHEMA="v0"))
+    def test_schematized_operation_name_v0(self):
+        span = self._generate_span()
+
+        self.assertEqual(span.name, "mysql.query")
+
+    @TracerTestCase.run_in_subprocess(env_overrides=dict(DD_TRACE_SPAN_ATTRIBUTE_SCHEMA="v1"))
+    def test_schematized_operation_name_v1(self):
+        span = self._generate_span()
+
+        self.assertEqual(span.name, "mysql.query")
