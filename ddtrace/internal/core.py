e4533f40cb38a879b72b2b6beec1a4bd4bf5be63
Emmett Butler <emmett.butler321@gmail.com>
2023-06-01 15:04:19 -0700

    early sketches of the core api

--- ddtrace/internal/core.py
+++ ddtrace/internal/core.py
@@ -1,16 +1,22 @@
 from collections import defaultdict
 from contextlib import contextmanager
+from typing import Any
+from typing import Callable
 from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Tuple
 
 
 class ExecutionContext:
-    def __init__(self, identifier=None, parent=None, **kwargs):
-        self.identifier = identifier or ExecutionContext.make_identifier()
+    def __init__(self, identifier: str, parent=None, **kwargs):
+        self.identifier = identifier
         self._parents = []
         self._children = []
         if parent is not None:
             self.addParent(parent)
-        self._data = self._build_data(kwargs)
+        self._data = {}
+        self._update_data(kwargs)
 
     @property
     def parents(self):
@@ -20,22 +26,21 @@ class ExecutionContext:
     def children(self):
         return self._children
 
-    def _build_data(self, dotted_attrs: Dict):
-        attr_data = dict()
+    def _update_data(self, dotted_attrs: Dict):
         for key, value in dotted_attrs.items():
-            current_parent_attr = attr_data
+            current_parent_attr = self._data
             key_parts = key.split(".")
             for key_part in key_parts[:-1]:
-                child_attr = dict()
-                current_parent_attr[key_part] = child_attr
-                current_parent_attr = child_attr
+                if key_part not in current_parent_attr:
+                    current_parent_attr[key_part] = dict()
+                current_parent_attr = current_parent_attr[key_part]
             current_parent_attr[key_parts[-1]] = value
 
     def end(self):
-        dispatch("context.%s.ended" % self.identifier)
+        return dispatch("context.ended.%s" % self.identifier, [])
 
     def addParent(self, context):
-        if context is root_context:
+        if self is root_context:
             raise ValueError("Cannot add parent to root context")
         self._parents.append(context)
 
@@ -44,34 +49,40 @@ class ExecutionContext:
 
     @classmethod
     @contextmanager
-    def context_with_data(cls, parent=None, **kwargs):
-        new_context = cls(parent=parent, **kwargs)
+    def context_with_data(cls, identifier, parent=None, **kwargs):
+        global current_context
+        new_context = cls(identifier, parent=parent, **kwargs)
+        prior_context = current_context
+        current_context = new_context
         try:
             yield new_context
         finally:
             new_context.end()
+            current_context = prior_context
 
-    @classmethod
-    def make_identifier(cls):
-        return "foobar.banana"
+    def get_item(self, data_key: str) -> Optional[Any]:
+        key_parts = data_key.split(".")
+        current_parent_attr = self._data
+        for key_part in key_parts:
+            if key_part not in current_parent_attr:
+                return
+            current_parent_attr = current_parent_attr[key_part]
+        return current_parent_attr
+
+    def set_item(self, data_key: str, data_value: Optional[Any]):
+        self._update_data({data_key, data_value})
 
 
-root_context = ExecutionContext()
+root_context = ExecutionContext("root")
 current_context = root_context
 
 
-def context_with_data(parent=None, **kwargs):
-    if parent is None:
-        parent = current_context
-    return ExecutionContext.context_with_data(parent=parent, **kwargs)
+def context_with_data(identifier: str, parent=None, **kwargs):
+    return ExecutionContext.context_with_data(identifier, parent=(parent or current_context), **kwargs)
 
 
 def get_item(data_key):
-    context = current_context
-    result = None
-    while result is None:
-        result = context._get_item(data_key)
-    return result
+    return current_context.get_item(data_key)
 
 
 class EventHub:
@@ -84,6 +95,20 @@ class EventHub:
     def on(self, event_id, callback):
         self._listeners[event_id].append(callback)
 
+    def dispatch(self, event_id: str, args: List[Any]):
+        results = []
+        exceptions = []
+        for listener in self._listeners.get(event_id, []):
+            result = None
+            exception = None
+            try:
+                result = listener(*args)
+            except Exception as exc:
+                exception = exc
+            results.append(result)
+            exceptions.append(exception)
+        return results, exceptions
+
 
 _event_hub = EventHub()
 
@@ -92,9 +117,9 @@ def has_listeners(event_id):
     return _event_hub.has_listeners(event_id)
 
 
-def on(event_id, callback):
+def on(event_id: str, callback: Callable):
     return _event_hub.on(event_id, callback)
 
 
-def dispatch(event_id):
-    pass
+def dispatch(event_id: str, args: List[Any]) -> Tuple[List[Optional[Any]], List[Optional[Exception]]]:
+    return _event_hub.dispatch(event_id, args)
--- tests/internal/test_context_events_api.py
+++ tests/internal/test_context_events_api.py
@@ -1,66 +1,94 @@
+import unittest
+
 from ddtrace.internal import core
 
 
-def test_core_get_execution_context():
-    context = core.ExecutionContext()
-    assert context.parents == []
-    assert context.children == []
-    context.addParent(core.ExecutionContext())
-    context.addChild(core.ExecutionContext())
-    assert len(context.parents) == 1
-    assert len(context.children) == 1
+class TestContextEventsApi(unittest.TestCase):
+    def setUp(self):
+        core._event_hub = core.EventHub()
 
+    def test_core_get_execution_context(self):
+        context = core.ExecutionContext("foo")
+        assert context.parents == []
+        assert context.children == []
+        context.addParent(core.ExecutionContext("bar"))
+        context.addChild(core.ExecutionContext("baz"))
+        assert len(context.parents) == 1
+        assert len(context.children) == 1
 
-def test_core_has_listeners():
-    event_name = "my.cool.event"
-    has_listeners = core.has_listeners(event_name)
-    assert not has_listeners
-    core.on(event_name, lambda *args: print("banana"))
-    has_listeners = core.has_listeners(event_name)
-    assert has_listeners
+    def test_core_has_listeners(self):
+        event_name = "my.cool.event"
+        has_listeners = core.has_listeners(event_name)
+        assert not has_listeners
+        core.on(event_name, lambda *args: True)
+        has_listeners = core.has_listeners(event_name)
+        assert has_listeners
 
+    def test_core_dispatch(self):
+        event_name = "my.cool.event"
+        dynamic_value = 42
+        handler_return = "from.event.{}"
+        core.on(event_name, lambda magic_number: handler_return.format(magic_number))
+        result = core.dispatch(event_name, [dynamic_value])[0][0]
+        assert result == handler_return.format(dynamic_value)
 
-def test_core_root_context():
-    assert isinstance(core.root_context, core.ExecutionContext)
-    assert len(core.root_context.parents) == 0
-    assert len(core.root_context.children) == 0
+    def test_core_dispatch_multiple_listeners(self):
+        event_name = "my.cool.event"
+        dynamic_value = 42
+        handler_return = "from.event.{}"
+        core.on(event_name, lambda magic_number: handler_return.format(magic_number))
+        core.on(event_name, lambda another_magic_number: handler_return + str(another_magic_number) + "!")
+        results, _ = core.dispatch(event_name, [dynamic_value])
+        assert results[0] == handler_return.format(dynamic_value)
+        assert results[1] == handler_return + str(dynamic_value) + "!"
 
+    def test_core_dispatch_multiple_listeners_multiple_threads(self):
+        assert False
 
-def test_core_current_context():
-    assert core.current_context is core.root_context
-    with core.context_with_data() as context:
-        assert core.current_context is context
-        assert context.parents[0] == core.root_context
-    assert core.current_context is core.root_context
+    def test_core_dispatch_context_ended(self):
+        context_id = "my.cool.context"
+        event_name = "context.ended.%s" % context_id
+        was_called = False
 
+        def handler():
+            global was_called
+            was_called = True
 
-def test_core_context_with_data():
-    data_key = "my.cool.data"
-    data_value = "ban.ana"
-    with core.context_with_data(**{data_key: data_value}):
-        assert core.get_item(data_key) == data_value
+        core.on(event_name, handler)
+        with core.context_with_data(context_id):
+            pass
+        assert was_called
 
+    def test_core_root_context(self):
+        assert isinstance(core.root_context, core.ExecutionContext)
+        assert len(core.root_context.parents) == 0
+        assert len(core.root_context.children) == 0
 
-def test_core_context_with_data_inheritance():
-    data_key = "my.cool.data"
-    original_data_value = "ban.ana"
-    with core.context_with_data(**{data_key: original_data_value}):
-        with core.context_with_data():
+    def test_core_current_context(self):
+        assert core.current_context is core.root_context
+        with core.context_with_data("foobar") as context:
+            assert core.current_context is context
+            assert context.parents[0] == core.root_context
+        assert core.current_context is core.root_context
+
+    def test_core_context_with_data(self):
+        data_key = "my.cool.data"
+        data_value = "ban.ana"
+        with core.context_with_data("foobar", **{data_key: data_value}):
+            assert core.get_item(data_key) == data_value
+        assert core.get_item(data_key) is None
+
+    def test_core_context_relationship_across_threads(self):
+        assert False
+
+    def test_core_context_with_data_inheritance(self):
+        data_key = "my.cool.data"
+        original_data_value = "ban.ana"
+        with core.context_with_data("foobar", **{data_key: original_data_value}):
+            with core.context_with_data("baz"):
+                assert core.get_item(data_key) is None
+
+            new_data_value = "baz.inga"
+            with core.context_with_data("foobaz", **{data_key: new_data_value}):
+                assert core.get_item(data_key) == new_data_value
             assert core.get_item(data_key) == original_data_value
-
-        new_data_value = "baz.inga"
-        with core.context_with_data(**{data_key: new_data_value}):
-            assert core.get_item(data_key) == new_data_value
-        assert core.get_item(data_key) == original_data_value
-
-
-def test_core_get_item():
-    pass
-
-
-def test_core_on():
-    pass
-
-
-def test_core_dispatch():
-    pass
