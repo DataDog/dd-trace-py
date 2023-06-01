cd2df17535e24c77ee0e8e89990969d2b6e88c10
Tahir H. Butt <tahir.butt@datadoghq.com>
2023-06-01 12:35:25 -0400

    chore(test): molten test fix [backport #5990 to 1.14] (#5996)
    Backport of #5990 to 1.14
    
    Pinning cattrs dependency version to avoid a breaking change affecting
    the molten test in our CI.
    
    ## Checklist
    
    - [x] Change(s) are motivated and described in the PR description.
    - [x] Testing strategy is described if automated tests are not included
    in the PR.
    - [x] Risk is outlined (performance impact, potential for breakage,
    maintainability, etc).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] [Library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/contributing.html#Release-Note-Guidelines)
    are followed.
    - [x] Documentation is included (in-code, generated user docs, [public
    corp docs](https://github.com/DataDog/documentation/)).
    
    ## Reviewer Checklist
    
    - [x] Title is accurate.
    - [x] No unnecessary changes are introduced.
    - [x] Description motivates each change.
    - [x] Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes unless absolutely necessary.
    - [x] Testing strategy adequately addresses listed risk(s).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] Release note makes sense to a user of the library.
    - [x] Reviewer has explicitly acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment.
    
    Co-authored-by: Christophe Papazian <114495376+christophe-papazian@users.noreply.github.com>

--- riotfile.py
+++ riotfile.py
@@ -2729,6 +2729,7 @@ venv = Venv(
             command="pytest {cmdargs} tests/contrib/molten",
             pys=select_pys(min_version="3.6"),
             pkgs={
+                "cattrs": ["<23.1.1"],
                 "molten": [">=1.0,<1.1", latest],
             },
         ),
