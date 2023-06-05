a46425c8f991d9215518953c08458066c83b9b67
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2023-06-05 12:24:11 +0200

    fmt
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/contrib/subprocess/__init__.py
+++ ddtrace/contrib/subprocess/__init__.py
@@ -15,7 +15,8 @@ Configuration
    wildcards will be scrubbed and replaced by a "?".
 
    Default: ``None`` for the config value but note that there are some wildcards always
-   enabled in this integration that you can check on ```ddtrace.contrib.subprocess.constants.SENSITIVE_WORDS_WILDCARDS```.
+   enabled in this integration that you can check on
+   ```ddtrace.contrib.subprocess.constants.SENSITIVE_WORDS_WILDCARDS```.
 """
 
 from ...internal.utils.importlib import require_modules
