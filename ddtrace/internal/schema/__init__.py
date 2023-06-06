8cdac0742ab6c8092a2d56ac58af579d895cd474
Teague Bick <teague.bick@datadoghq.com>
2023-06-06 16:28:50 -0400

    chore(service.naming) - add flag for v0
    This change adds the DD_TRACE_REMOVE_CLIENT_SERVICE_NAMES_ENABLED flag
    to the service.naming feature set. This is being done to allow toggling
    of service-name changes **only** when running in "v0".
    
    Prior to this change, one could either toggle ALL service name and
    operation updates to either the "v0" or "v1" versions.  After this
    change, the "v0" schema version can toggle service names and operations
    names separately. The new flag has no affect if the schema version is
    set to "v1".
    
    |SCHEMA_VERSION|CLIENT_SERVICE_NAMES_ENABLED|service_name|operation_name|
    |--|--|--|--|
    |v0|false|v0|v0|
    |v0|true|v1|v0|
    |v1|false|v1|v1|
    |v1|true|v1|v1|

--- ddtrace/internal/schema/__init__.py
+++ ddtrace/internal/schema/__init__.py
@@ -1,5 +1,7 @@
 import os
 
+from ddtrace.internal.utils.formats import asbool
+
 from .span_attribute_schema import _DEFAULT_SPAN_SERVICE_NAMES
 from .span_attribute_schema import _SPAN_ATTRIBUTE_TO_FUNCTION
 
@@ -18,10 +20,13 @@ def _validate_schema(version):
 
 
 __schema_version = os.getenv("DD_TRACE_SPAN_ATTRIBUTE_SCHEMA", default="v0")
+__remove_client_service_names = asbool(os.getenv("DD_TRACE_REMOVE_CLIENT_SERVICE_NAMES_ENABLED", default=False))
 _validate_schema(__schema_version)
 
-DEFAULT_SPAN_SERVICE_NAME = _DEFAULT_SPAN_SERVICE_NAMES[__schema_version]
-schematize_service_name = _SPAN_ATTRIBUTE_TO_FUNCTION[__schema_version]["service_name"]
+__service_name_schema_version = "v0" if __schema_version == "v0" and not __remove_client_service_names else "v1"
+
+DEFAULT_SPAN_SERVICE_NAME = _DEFAULT_SPAN_SERVICE_NAMES[__service_name_schema_version]
+schematize_service_name = _SPAN_ATTRIBUTE_TO_FUNCTION[__service_name_schema_version]["service_name"]
 schematize_database_operation = _SPAN_ATTRIBUTE_TO_FUNCTION[__schema_version]["database_operation"]
 schematize_cache_operation = _SPAN_ATTRIBUTE_TO_FUNCTION[__schema_version]["cache_operation"]
 schematize_cloud_api_operation = _SPAN_ATTRIBUTE_TO_FUNCTION[__schema_version]["cloud_api_operation"]
