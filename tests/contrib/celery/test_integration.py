4023d57cecda4e8e328c7ce47a927c36477ad1bc
Zarir Hamza <zarir.hamza@datadoghq.com>
2023-06-06 14:40:13 -0400

    uses local hostname to check tag values

--- tests/contrib/celery/test_integration.py
+++ tests/contrib/celery/test_integration.py
@@ -1,3 +1,4 @@
+import socket
 from collections import Counter
 import os
 import subprocess
@@ -191,7 +192,7 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
             assert async_span.get_tag("celery.routing_key") == "celery"
             assert async_span.get_tag("component") == "celery"
             assert async_span.get_tag("span.kind") == "producer"
-            assert async_span.get_tag("out.host") == "docker-desktop"
+            assert async_span.get_tag("out.host") == socket.gethostname()
         else:
             assert 1 == len(traces)
             assert 1 == len(traces[0])
@@ -235,7 +236,7 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
             assert async_span.get_tag("celery.routing_key") == "celery"
             assert async_span.get_tag("component") == "celery"
             assert async_span.get_tag("span.kind") == "producer"
-            assert async_span.get_tag("out.host") == "docker-desktop"
+            assert async_span.get_tag("out.host") == socket.gethostname()
         else:
             assert 1 == len(traces)
             assert 1 == len(traces[0])
@@ -684,7 +685,7 @@ class CeleryIntegrationTask(CeleryBaseTestCase):
             assert async_span.get_tag("celery.routing_key") == "celery"
             assert async_span.get_tag("component") == "celery"
             assert async_span.get_tag("span.kind") == "producer"
-            assert async_span.get_tag("out.host") == "docker-desktop"
+            assert async_span.get_tag("out.host") == socket.gethostname()
 
         run_span = self.find_span(name="celery.run")
         assert run_span.name == "celery.run"
