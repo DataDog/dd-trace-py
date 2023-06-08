633e6818b8b102816aa29e4a5bd5ad1e3c2a0c34
Emmett Butler <emmett.butler321@gmail.com>
2023-06-08 14:05:23 -0700

    checkpoint

--- ddtrace/contrib/pymemcache/client.py
+++ ddtrace/contrib/pymemcache/client.py
@@ -3,6 +3,7 @@ from typing import Iterable
 
 import pymemcache
 from pymemcache.client.base import Client
+from pymemcache.client.base import PooledClient
 from pymemcache.client.hash import HashClient
 from pymemcache.exceptions import MemcacheClientError
 from pymemcache.exceptions import MemcacheIllegalInputError
@@ -175,70 +176,38 @@ class WrappedHashClient(wrapt.ObjectProxy):
     def set(self, key, *args, **kwargs):
         return self._traced_cmd("set", key, False, *args, **kwargs)
 
-    def set_many(self, *args, **kwargs):
-        return self._traced_cmd("set_many", *args, **kwargs)
+    def add(self, key, *args, **kwargs):
+        return self._traced_cmd("add", key, False, *args, **kwargs)
 
-    def add(self, *args, **kwargs):
-        return self._traced_cmd("add", *args, **kwargs)
+    def replace(self, key, *args, **kwargs):
+        return self._traced_cmd("replace", key, False, *args, **kwargs)
 
-    def replace(self, *args, **kwargs):
-        return self._traced_cmd("replace", *args, **kwargs)
+    def append(self, key, *args, **kwargs):
+        return self._traced_cmd("append", key, False, *args, **kwargs)
 
-    def append(self, *args, **kwargs):
-        return self._traced_cmd("append", *args, **kwargs)
+    def prepend(self, key, *args, **kwargs):
+        return self._traced_cmd("prepend", key, False, *args, **kwargs)
 
-    def prepend(self, *args, **kwargs):
-        return self._traced_cmd("prepend", *args, **kwargs)
+    def cas(self, key, *args, **kwargs):
+        return self._traced_cmd("cas", key, False, *args, **kwargs)
 
-    def cas(self, *args, **kwargs):
-        return self._traced_cmd("cas", *args, **kwargs)
+    def get(self, key, *args, **kwargs):
+        return self._traced_cmd("get", key, None, *args, **kwargs)
 
-    def get(self, *args, **kwargs):
-        return self._traced_cmd("get", *args, **kwargs)
+    def gets(self, key, *args, **kwargs):
+        return self._traced_cmd("gets", key, None, *args, **kwargs)
 
-    def get_many(self, *args, **kwargs):
-        return self._traced_cmd("get_many", *args, **kwargs)
+    def delete(self, key, *args, **kwargs):
+        return self._traced_cmd("delete", key, False, *args, **kwargs)
 
-    def gets(self, *args, **kwargs):
-        return self._traced_cmd("gets", *args, **kwargs)
+    def incr(self, key, *args, **kwargs):
+        return self._traced_cmd("incr", key, False, *args, **kwargs)
 
-    def gets_many(self, *args, **kwargs):
-        return self._traced_cmd("gets_many", *args, **kwargs)
+    def decr(self, key, *args, **kwargs):
+        return self._traced_cmd("decr", key, False, *args, **kwargs)
 
-    def delete(self, *args, **kwargs):
-        return self._traced_cmd("delete", *args, **kwargs)
-
-    def delete_many(self, *args, **kwargs):
-        return self._traced_cmd("delete_many", *args, **kwargs)
-
-    def incr(self, *args, **kwargs):
-        return self._traced_cmd("incr", *args, **kwargs)
-
-    def decr(self, *args, **kwargs):
-        return self._traced_cmd("decr", *args, **kwargs)
-
-    def touch(self, *args, **kwargs):
-        return self._traced_cmd("touch", *args, **kwargs)
-
-    def stats(self, *args, **kwargs):
-        return self._traced_cmd("stats", *args, **kwargs)
-
-    def version(self, *args, **kwargs):
-        return self._traced_cmd("version", *args, **kwargs)
-
-    def flush_all(self, *args, **kwargs):
-        return self._traced_cmd("flush_all", *args, **kwargs)
-
-    def quit(self, *args, **kwargs):
-        return self._traced_cmd("quit", *args, **kwargs)
-
-    def set_multi(self, *args, **kwargs):
-        """set_multi is an alias for set_many"""
-        return self._traced_cmd("set_many", *args, **kwargs)
-
-    def get_multi(self, *args, **kwargs):
-        """set_multi is an alias for set_many"""
-        return self._traced_cmd("get_many", *args, **kwargs)
+    def touch(self, key, *args, **kwargs):
+        return self._traced_cmd("touch", key, False, *args, **kwargs)
 
     def _traced_cmd(self, cmd, key, default_val, *args, **kwargs):
         client = self._get_client(key)
@@ -259,6 +228,9 @@ class WrappedHashClient(wrapt.ObjectProxy):
             return _do_run()
 
         return _trace(_do_run, p, cmd, *args, **kwargs)
+
+
+_HashClient.client_class = WrappedClient
 
 
 def _get_address_tags(*args, **kwargs):
