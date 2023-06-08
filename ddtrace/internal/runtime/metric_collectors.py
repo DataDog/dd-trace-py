ee181a3c387003d78482c7068df22a754f9afe26
sanchda <838104+sanchda@users.noreply.github.com>
2023-06-08 11:17:55 -0500

    Formatting

--- ddtrace/internal/runtime/metric_collectors.py
+++ ddtrace/internal/runtime/metric_collectors.py
@@ -51,13 +51,13 @@ class PSUtilRuntimeMetricCollector(RuntimeMetricCollector):
 
     required_modules = ["ddtrace.vendor.psutil"]
     metric_funs = {
-        CPU_TIME_SYS:lambda p: p.cpu_times().system,
-        CPU_TIME_USER:lambda p: p.cpu_times().user,
-        CPU_PERCENT:lambda p: p.cpu_percent(),
-        CTX_SWITCH_VOLUNTARY:lambda p: p.num_ctx_switches().voluntary,
-        CTX_SWITCH_INVOLUNTARY:lambda p: p.num_ctx_switches().involuntary,
-        THREAD_COUNT:lambda p: p.num_threads(),
-        MEM_RSS:lambda p: p.memory_info().rss,
+        CPU_TIME_SYS: lambda p: p.cpu_times().system,
+        CPU_TIME_USER: lambda p: p.cpu_times().user,
+        CPU_PERCENT: lambda p: p.cpu_percent(),
+        CTX_SWITCH_VOLUNTARY: lambda p: p.num_ctx_switches().voluntary,
+        CTX_SWITCH_INVOLUNTARY: lambda p: p.num_ctx_switches().involuntary,
+        THREAD_COUNT: lambda p: p.num_threads(),
+        MEM_RSS: lambda p: p.memory_info().rss,
     }
 
     def _on_modules_load(self):
