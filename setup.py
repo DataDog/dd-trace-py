be34db26ed7fed43d702fbc30a889a3dbe9fbddc
Alberto Vara <alberto.vara@datadoghq.com>
2023-06-14 18:44:26 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -325,10 +325,11 @@ class CMakeBuild(build_ext):
                     if CURRENT_OS == "Darwin" and sys.version_info >= (3, 8, 0):
                         # Cross-compile support for macOS - respect ARCHFLAGS if set
                         # Darwin Universal2 should bundle both architectures
-                        default_platforms = (
-                            "-arch x86_64 -arch arm64" if os.getenv("PLAT").endswith("universal2") else ""
-                        )
-                        archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", default_platforms))
+                        # default_platforms = (
+                        #     "-arch x86_64 -arch arm64" if os.getenv("PLAT").endswith("universal2") else ""
+                        # )
+                        # archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", default_platforms))
+                        archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
                         if archs:
                             cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
 
