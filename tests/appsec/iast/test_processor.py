49d0456898a6bde951809e63f2f21e2d85964737
Alberto Vara <alberto.vara@datadoghq.com>
2023-06-14 09:00:27 +0200

    fix processor

--- tests/appsec/iast/test_processor.py
+++ tests/appsec/iast/test_processor.py
@@ -5,6 +5,7 @@ import pytest
 from ddtrace._monkey import IAST_PATCH
 from ddtrace._monkey import patch_iast
 from ddtrace.appsec._constants import IAST
+from ddtrace.appsec.iast._util import _is_python_version_supported
 from ddtrace.constants import SAMPLING_PRIORITY_KEY
 from ddtrace.constants import USER_KEEP
 from ddtrace.ext import SpanTypes
@@ -27,6 +28,7 @@ def traced_function(tracer):
     return span
 
 
+@pytest.mark.skipif(_is_python_version_supported(), reason="Python version not supported by IAST")
 def test_appsec_iast_processor():
     with override_global_config(dict(_iast_enabled=True)):
         patch_iast(**IAST_PATCH)
@@ -43,6 +45,7 @@ def test_appsec_iast_processor():
         assert len(json.loads(result)["vulnerabilities"]) == 1
 
 
+@pytest.mark.skipif(_is_python_version_supported(), reason="Python version not supported by IAST")
 @pytest.mark.parametrize("sampling_rate", ["0.0", "0.5", "1.0"])
 def test_appsec_iast_processor_ensure_span_is_manual_keep(sampling_rate):
     with override_env(dict(DD_TRACE_SAMPLE_RATE=sampling_rate)), override_global_config(dict(_iast_enabled=True)):
