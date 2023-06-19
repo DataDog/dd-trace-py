04792f4077fc019063ccbafeb0c1eaba8d4e7b6b
Alberto Vara <alberto.vara@datadoghq.com>
2023-06-19 18:12:49 +0200

    tmp setup

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -10,7 +10,11 @@ if __name__ == "__main__":
         if not sys.maxsize > 2 ** 32:
             # 32-bit linux DDWAF not ready yet.
             sys.exit(0)
-    ddtrace.appsec.ddwaf.version()
 
+    if sys.version_info >= (3, 6, 0):
+        from ddtrace.appsec.iast._taint_tracking._native.taint_tracking import OriginType
+        assert OriginType
+
+    ddtrace.appsec.ddwaf.version()
     assert ddtrace.appsec.ddwaf._DDWAF_LOADED
     assert module.loaded
