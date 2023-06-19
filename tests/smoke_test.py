c6c2b35beb2fa7f3f1669f5a2c23e590dad73203
Alberto Vara <alberto.vara@datadoghq.com>
2023-06-19 18:18:55 +0200

    tmp setup

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -6,14 +6,17 @@ import ddtrace.bootstrap.sitecustomize as module
 
 
 if __name__ == "__main__":
+    # ASM IAST smoke test
+    if sys.version_info >= (3, 6, 0):
+        from ddtrace.appsec.iast._taint_tracking._native.taint_tracking import OriginType
+        
+        assert OriginType
+
+    # ASM WAF smoke test
     if system() == "Linux":
         if not sys.maxsize > 2 ** 32:
             # 32-bit linux DDWAF not ready yet.
             sys.exit(0)
-
-    if sys.version_info >= (3, 6, 0):
-        from ddtrace.appsec.iast._taint_tracking._native.taint_tracking import OriginType
-        assert OriginType
 
     ddtrace.appsec.ddwaf.version()
     assert ddtrace.appsec.ddwaf._DDWAF_LOADED
