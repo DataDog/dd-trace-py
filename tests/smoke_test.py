f9bfd2b24783309925a1c94e645126249ffcbd56
Alberto Vara <alberto.vara@datadoghq.com>
2023-06-20 15:34:22 +0200

    tmp setup

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -7,7 +7,7 @@ import ddtrace.bootstrap.sitecustomize as module
 
 if __name__ == "__main__":
     # ASM IAST smoke test
-    if sys.version_info >= (3, 6, 0):
+    if sys.version_info >= (3, 6, 0) and system() != "Windows":
         from ddtrace.appsec.iast._taint_tracking._native.taint_tracking import OriginType
 
         assert OriginType
