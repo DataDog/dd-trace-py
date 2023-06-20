2cf0e6f01a008a753ed95a44eadf832f234e3248
Idan Shatz <idan.shatz@datadoghq.com>
2023-06-20 10:31:52 -0600

    adding tests for rc client id and runtime id on fork

--- tests/internal/remoteconfig/test_remoteconfig.py
+++ tests/internal/remoteconfig/test_remoteconfig.py
@@ -158,10 +158,15 @@ def test_remote_config_forksafe():
 
         parent_worker = remoteconfig_poller
         assert parent_worker.status == ServiceStatus.RUNNING
+        client_id = remoteconfig_poller._client.id
+        runtime_id = remoteconfig_poller._client._client_tracer["runtime_id"]
 
         if os.fork() == 0:
             assert remoteconfig_poller.status == ServiceStatus.RUNNING
             assert remoteconfig_poller._worker is not parent_worker
+
+            assert client_id != remoteconfig_poller._client.id
+            assert runtime_id != remoteconfig_poller._client._client_tracer["runtime_id"]
             exit(0)
 
 
