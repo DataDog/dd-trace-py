33e7f6fdc3ae124d648103357deae03ac417169b
Federico Mon <federico.mon@datadoghq.com>
2023-06-23 17:37:03 +0200

    test: temporarily use full-path suites and module prefixed suites

--- ddtrace/internal/ci_visibility/recorder.py
+++ ddtrace/internal/ci_visibility/recorder.py
@@ -304,8 +304,11 @@ class CIVisibility(Service):
                     module = item["attributes"].get("configurations", {}).get("test.bundle", None)
                     self._tests_to_skip[(item["attributes"]["suite"], module)].append(item["attributes"]["name"])
                 else:
-                    module = item["attributes"].get("configurations", {}).get("test.bundle", None).replace(".", "/")
-                    self._test_suites_to_skip.append("/".join((module, item["attributes"]["suite"])))
+                    if "/" in item["attributes"]["suite"]:
+                        self._test_suites_to_skip.append(item["attributes"]["suite"])
+                    else:
+                        module = item["attributes"].get("configurations", {}).get("test.bundle", None).replace(".", "/")
+                        self._test_suites_to_skip.append("/".join((module, item["attributes"]["suite"])))
 
     def _get_tests_to_skip(self, suite, module):
         if TEST_LEVEL == "test":
