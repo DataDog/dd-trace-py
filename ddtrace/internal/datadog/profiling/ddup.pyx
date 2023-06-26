7e65a44cc96d26ccc012fa066236231bce1378fe
sanchda <sanchda@users.noreply.github.com>
2023-06-26 15:39:32 +0000

    Use correct variant for six

--- ddtrace/internal/datadog/profiling/ddup.pyx
+++ ddtrace/internal/datadog/profiling/ddup.pyx
@@ -4,7 +4,7 @@ from typing import Optional
 
 import ddtrace
 from ddtrace.internal import runtime
-from ddtrace.internal.compat import ensure_str
+from ddtrace.internal.compat import ensure_binary
 from ddtrace.internal.constants import DEFAULT_SERVICE_NAME
 from ddtrace.span import Span
 
@@ -74,17 +74,17 @@ IF UNAME_SYSNAME == "Linux" and UNAME_MACHINE == "x86_64":
         # If otherwise no values are provided, the uploader will omit the fields
         # and they will be auto-populated in the backend
         if env:
-            ddup_config_env(ensure_str(env))
+            ddup_config_env(ensure_binary(env))
         if version:
-            ddup_config_version(ensure_str(version))
+            ddup_config_version(ensure_binary(version))
 
         if url:
-            ddup_config_url(ensure_str(url))
+            ddup_config_url(ensure_binary(url))
 
         # Inherited
-        ddup_config_runtime(ensure_str(platform.python_implementation()))
-        ddup_config_runtime_version(ensure_str(platform.python_version()))
-        ddup_config_profiler_version(ensure_str(ddtrace.__version__))
+        ddup_config_runtime(ensure_binary(platform.python_implementation()))
+        ddup_config_runtime_version(ensure_binary(platform.python_version()))
+        ddup_config_profiler_version(ensure_binary(ddtrace.__version__))
         ddup_config_max_nframes(max_nframes)
         if tags is not None:
             for key, val in tags.items():
@@ -113,17 +113,17 @@ IF UNAME_SYSNAME == "Linux" and UNAME_MACHINE == "x86_64":
         ddup_push_heap(value)
 
     def push_lock_name(lock_name: str) -> None:
-        ddup_push_lock_name(ensure_str(lock_name))
+        ddup_push_lock_name(ensure_binary(lock_name))
 
     def push_frame(name: str, filename: str, address: int, line: int) -> None:
         if name is None and filename is None:
             ddup_push_frame(name, filename, address, line)
         elif filename is None:
-            ddup_push_frame(ensure_str(name), filename, address, line)
+            ddup_push_frame(ensure_binary(name), filename, address, line)
         elif name is None:
-            ddup_push_frame(name, ensure_str(filename), address, line)
+            ddup_push_frame(name, ensure_binary(filename), address, line)
         else:
-            ddup_push_frame(ensure_str(name), ensure_str(filename), address, line)
+            ddup_push_frame(ensure_binary(name), ensure_binary(filename), address, line)
 
     def push_threadinfo(thread_id: int, thread_native_id: int, thread_name: Optional[str]) -> None:
         if thread_id is None:
@@ -132,25 +132,25 @@ IF UNAME_SYSNAME == "Linux" and UNAME_MACHINE == "x86_64":
             thread_native_id = 0
 
         if thread_name is None:
-            ddup_push_threadinfo(thread_id, thread_native_id, ensure_str(""))
+            ddup_push_threadinfo(thread_id, thread_native_id, ensure_binary(""))
         else:
-            ddup_push_threadinfo(thread_id, thread_native_id, ensure_str(thread_name))
+            ddup_push_threadinfo(thread_id, thread_native_id, ensure_binary(thread_name))
 
     def push_task_id(task_id: int) -> None:
         ddup_push_task_id(task_id)
 
     def push_task_name(task_name: str) -> None:
         if task_name:
-            ddup_push_task_name(ensure_str(task_name))
+            ddup_push_task_name(ensure_binary(task_name))
 
     def push_exceptioninfo(exc_type: type, count: int) -> None:
         if exc_type is not None:
             exc_name = exc_type.__module__ + "." + exc_type.__name__
-            ddup_push_exceptioninfo(ensure_str(exc_name), count)
+            ddup_push_exceptioninfo(ensure_binary(exc_name), count)
 
     def push_class_name(class_name: str) -> None:
         if class_name is not None:
-            ddup_push_class_name(ensure_str(class_name))
+            ddup_push_class_name(ensure_binary(class_name))
 
     def push_span(span: typing.Optional[Span], endpoint_collection_enabled: bool) -> None:
         if span:
@@ -165,6 +165,6 @@ IF UNAME_SYSNAME == "Linux" and UNAME_MACHINE == "x86_64":
         ddup_flush_sample()
 
     def upload() -> None:
-        runtime_id = ensure_str(runtime.get_runtime_id())
+        runtime_id = ensure_binary(runtime.get_runtime_id())
         ddup_set_runtime_id(runtime_id, len(runtime_id))
         ddup_upload()
