cff9194c96a39c57c884521328c1a41b026d6dc3
Romain Komorn <romain.komorn@datadoghq.com>
2023-06-27 16:35:28 +0100

    Reintroduce old method of getting parameters.

--- ddtrace/contrib/pytest_bdd/plugin.py
+++ ddtrace/contrib/pytest_bdd/plugin.py
@@ -25,6 +25,40 @@ def _store_span(item, span):
     setattr(item, "_datadog_span", span)
 
 
+def _extract_step_func_args(step, step_func, step_func_args):
+    """Backwards-compatible get arguments from step_func or step_func_args
+    """
+    if not (hasattr(step_func, "parser") or hasattr(step_func, "_pytest_bdd_parsers")):
+        return step_func_args
+
+    # store parsed step arguments
+    try:
+        parsers = [step_func.parser]
+    except AttributeError:
+        try:
+            # pytest-bdd >= 6.0.0
+            parsers = step_func._pytest_bdd_parsers
+        except AttributeError:
+            parsers = []
+    for parser in parsers:
+        if parser is not None:
+            converters = getattr(step_func, "converters", {})
+            parameters = {}
+            try:
+                for arg, value in parser.parse_arguments(step.name).items():
+                    try:
+                        if arg in converters:
+                            value = converters[arg](value)
+                    except Exception:
+                        # Ignore invalid converters
+                        pass
+                    parameters[arg] = value
+            except Exception:
+                pass
+
+    return parameters or None
+
+
 def pytest_configure(config):
     if config.pluginmanager.hasplugin("pytest-bdd"):
         config.pluginmanager.register(_PytestBddPlugin(), "_datadog-pytest-bdd")
@@ -80,6 +114,7 @@ class _PytestBddPlugin:
     def pytest_bdd_after_step(request, feature, scenario, step, step_func, step_func_args):
         span = _extract_span(step_func)
         if span is not None:
+            step_func_args = _extract_step_func_args(step, step_func, step_func_args)
             if step_func_args:
                 span.set_tag(test.PARAMETERS, json.dumps(step_func_args))
             span.finish()
@@ -93,6 +128,7 @@ class _PytestBddPlugin:
             else:
                 # PY2 compatibility workaround
                 _, _, tb = sys.exc_info()
+            step_func_args = _extract_step_func_args(step, step_func, step_func_args)
             if step_func_args:
                 span.set_tag(test.PARAMETERS, json.dumps(step_func_args))
             span.set_exc_info(type(exception), exception, tb)
