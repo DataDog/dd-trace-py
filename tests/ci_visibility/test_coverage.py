29b6547c43d7132fc167615acc6d7c46627acb98
Federico Mon <federico.mon@datadoghq.com>
2023-07-04 18:13:11 +0200

    test: add uid and gid to ddtest

--- tests/ci_visibility/test_coverage.py
+++ tests/ci_visibility/test_coverage.py
@@ -13,32 +13,9 @@ from ddtrace.internal.ci_visibility.coverage import segments
 from ddtrace.internal.ci_visibility.recorder import CITracer
 
 
-EXPECTED_COVERED_FILES = (
+SOME_EXPECTED_COVERED_FILES = (
     "ddtrace/internal/module.py",
     "ddtrace/contrib/pytest/plugin.py",
-    "ddtrace/span.py",
-    "ddtrace/tracer.py",
-    "ddtrace/provider.py",
-    "ddtrace/_hooks.py",
-    "ddtrace/internal/processor/trace.py",
-    "ddtrace/internal/processor/endpoint_call_counter.py",
-    "ddtrace/internal/ci_visibility/recorder.py",
-    "ddtrace/internal/service.py",
-    "ddtrace/internal/processor/__init__.py",
-    "ddtrace/internal/telemetry/writer.py",
-    "ddtrace/internal/periodic.py",
-    "ddtrace/internal/forksafe.py",
-    "ddtrace/internal/telemetry/metrics_namespaces.py",
-    "ddtrace/internal/telemetry/metrics.py",
-    "ddtrace/internal/runtime/__init__.py",
-    "ddtrace/internal/telemetry/data.py",
-    "ddtrace/internal/utils/cache.py",
-    "ddtrace/internal/runtime/container.py",
-    "ddtrace/internal/utils/time.py",
-    "ddtrace/internal/utils/http.py",
-    "ddtrace/internal/http.py",
-    "ddtrace/internal/compat.py",
-    "ddtrace/internal/ci_visibility/writer.py",
     "ddtrace/internal/ci_visibility/coverage.py",
 )
 
@@ -73,5 +50,5 @@ def test_cover():
     assert len(res["files"]) >= 27
 
     covered_files = [x["filename"] for x in res["files"]]
-    for filename in EXPECTED_COVERED_FILES:
+    for filename in SOME_EXPECTED_COVERED_FILES:
         assert filename in covered_files
