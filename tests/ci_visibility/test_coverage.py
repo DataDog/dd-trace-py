4e8ceb007d6d3d2d8ac0099e744950e9155b6e05
Federico Mon <federico.mon@datadoghq.com>
2023-07-04 14:20:32 +0200

    test: fix test_cover

--- tests/ci_visibility/test_coverage.py
+++ tests/ci_visibility/test_coverage.py
@@ -13,6 +13,37 @@ from ddtrace.internal.ci_visibility.coverage import segments
 from ddtrace.internal.ci_visibility.recorder import CITracer
 
 
+EXPECTED_COVERED_FILES = (
+    "ddtrace/internal/module.py",
+    "ddtrace/contrib/pytest/plugin.py",
+    "ddtrace/span.py",
+    "ddtrace/tracer.py",
+    "ddtrace/provider.py",
+    "ddtrace/_hooks.py",
+    "ddtrace/internal/processor/trace.py",
+    "ddtrace/internal/processor/endpoint_call_counter.py",
+    "ddtrace/internal/ci_visibility/recorder.py",
+    "ddtrace/internal/service.py",
+    "ddtrace/internal/processor/__init__.py",
+    "ddtrace/internal/telemetry/writer.py",
+    "ddtrace/internal/periodic.py",
+    "ddtrace/internal/forksafe.py",
+    "ddtrace/internal/telemetry/metrics_namespaces.py",
+    "ddtrace/internal/telemetry/metrics.py",
+    "ddtrace/internal/runtime/__init__.py",
+    "ddtrace/internal/telemetry/data.py",
+    "ddtrace/internal/utils/cache.py",
+    "ddtrace/internal/hostname.py",
+    "ddtrace/internal/runtime/container.py",
+    "ddtrace/internal/utils/time.py",
+    "ddtrace/internal/utils/http.py",
+    "ddtrace/internal/http.py",
+    "ddtrace/internal/compat.py",
+    "ddtrace/internal/ci_visibility/writer.py",
+    "ddtrace/internal/ci_visibility/coverage.py",
+)
+
+
 @pytest.mark.parametrize(
     "lines,expected_segments",
     [
@@ -40,37 +71,8 @@ def test_cover():
     res = literal_eval(span.get_tag(COVERAGE_TAG_NAME))
 
     assert "files" in res
-    assert len(res["files"]) == 27
+    assert len(res["files"]) >= 27
 
-    for filename in [x["filename"] for x in res["files"]]:
-        assert filename.endswith(
-            (
-                "ddtrace/internal/module.py",
-                "ddtrace/contrib/pytest/plugin.py",
-                "ddtrace/span.py",
-                "ddtrace/tracer.py",
-                "ddtrace/provider.py",
-                "ddtrace/_hooks.py",
-                "ddtrace/internal/processor/trace.py",
-                "ddtrace/internal/processor/endpoint_call_counter.py",
-                "ddtrace/internal/ci_visibility/recorder.py",
-                "ddtrace/internal/service.py",
-                "ddtrace/internal/processor/__init__.py",
-                "ddtrace/internal/telemetry/writer.py",
-                "ddtrace/internal/periodic.py",
-                "ddtrace/internal/forksafe.py",
-                "ddtrace/internal/telemetry/metrics_namespaces.py",
-                "ddtrace/internal/telemetry/metrics.py",
-                "ddtrace/internal/runtime/__init__.py",
-                "ddtrace/internal/telemetry/data.py",
-                "ddtrace/internal/utils/cache.py",
-                "ddtrace/internal/hostname.py",
-                "ddtrace/internal/runtime/container.py",
-                "ddtrace/internal/utils/time.py",
-                "ddtrace/internal/utils/http.py",
-                "ddtrace/internal/http.py",
-                "ddtrace/internal/compat.py",
-                "ddtrace/internal/ci_visibility/writer.py",
-                "ddtrace/internal/ci_visibility/coverage.py",
-            )
-        )
+    covered_files = [x["filename"] for x in res["files"]]
+    for filename in EXPECTED_COVERED_FILES:
+        assert filename in covered_files
