84a032b539db33029891eea73ed639812862c3f4
rhoboro <rhoboro@gmail.com>
2023-07-06 13:44:31 +0900

    feat(redis): add redis.asyncio.ClusterPipeline integration

--- ddtrace/contrib/redis/asyncio_patch.py
+++ ddtrace/contrib/redis/asyncio_patch.py
@@ -4,6 +4,7 @@ from ...internal.utils.formats import stringify_cache_args
 from ...pin import Pin
 from ..trace_utils_redis import _trace_redis_cmd
 from ..trace_utils_redis import _trace_redis_execute_pipeline
+from ..trace_utils_redis import _trace_redis_execute_async_cluster_pipeline
 
 
 #
@@ -26,4 +27,15 @@ async def traced_async_execute_pipeline(func, instance, args, kwargs):
     cmds = [stringify_cache_args(c, cmd_max_len=config.redis.cmd_max_length) for c, _ in instance.command_stack]
     resource = "\n".join(cmds)
     with _trace_redis_execute_pipeline(pin, config.redis, resource, instance):
+        return await func(*args, **kwargs)
+
+
+async def traced_async_execute_cluster_pipeline(func, instance, args, kwargs):
+    pin = Pin.get_from(instance)
+    if not pin or not pin.enabled():
+        return await func(*args, **kwargs)
+
+    cmds = [stringify_cache_args(c.args, cmd_max_len=config.redis.cmd_max_length) for c in instance._command_stack]
+    resource = "\n".join(cmds)
+    with _trace_redis_execute_async_cluster_pipeline(pin, config.redis, resource, instance):
         return await func(*args, **kwargs)
