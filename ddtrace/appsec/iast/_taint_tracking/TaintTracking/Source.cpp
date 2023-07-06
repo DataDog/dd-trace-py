8587529626bc152e2d7f31848851d9bf27eb61c7
Alberto Vara <alberto.vara@datadoghq.com>
2023-07-06 16:15:45 +0200

    fix: apline C++ error

--- ddtrace/appsec/iast/_taint_tracking/TaintTracking/Source.cpp
+++ ddtrace/appsec/iast/_taint_tracking/TaintTracking/Source.cpp
@@ -38,8 +38,7 @@ Source::operator std::string() const
 
 // Note: don't use size_t or long, if the hash is bigger than an int, Python
 // will re-hash it!
-uint
-Source::get_hash() const
+int Source::get_hash() const
 {
     return std::hash<size_t>()(std::hash<string>()(name) ^ (long)origin ^ std::hash<string>()(value));
 };
