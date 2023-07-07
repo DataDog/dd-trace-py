6b7dd2b1dfb2eb3b8c8a807f59f80e8b53804894
Alberto Vara <alberto.vara@datadoghq.com>
2023-07-07 16:00:39 +0200

    fix CLion autogenerated docs

--- ddtrace/appsec/iast/_taint_tracking/Constants.h
+++ ddtrace/appsec/iast/_taint_tracking/Constants.h
@@ -1,8 +1,2 @@
-//
-// Created by alberto.vara on 16/05/23.
-//
-
-#ifndef _TAINT_TRACKING_CONSTANTS_H
-#define _TAINT_TRACKING_CONSTANTS_H
+#pragma once
 #define PY_MODULE_NAME "ddtrace.appsec.iast._taint_tracking._native"
-#endif //_TAINT_TRACKING_CONSTANTS_H
