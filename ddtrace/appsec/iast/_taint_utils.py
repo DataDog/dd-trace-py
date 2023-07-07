ab578b88871e0432ffa3675cc64d25f020d7bbdb
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-07-07 14:08:47 +0200

    TaintLazy as capsules

--- ddtrace/appsec/iast/_taint_utils.py
+++ ddtrace/appsec/iast/_taint_utils.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+from collections import abc
+from typing import Any
+
 from ddtrace.appsec.iast._taint_tracking import is_pyobject_tainted
 from ddtrace.appsec.iast._taint_tracking import taint_pyobject
 from ddtrace.internal.logger import get_logger
@@ -10,31 +13,22 @@ DBAPI_PREFIXES = ("django-",)
 log = get_logger(__name__)
 
 
-class LazyTaintList(list):
-    def __init__(self, *args, origins=(0, 0), override_pyobject_tainted=False, source_name="[]"):
+class LazyTaintList:
+    def __init__(self, original_list, origins=(0, 0), override_pyobject_tainted=False, source_name="[]"):
+        self.obj = original_list
         self.origins = origins
         self.origin_value = origins[1]
         self.override_pyobject_tainted = override_pyobject_tainted
         self.source_name = source_name
-        super(LazyTaintList, self).__init__(*args)
 
     def __getitem__(self, key):
-        value = super(LazyTaintList, self).__getitem__(key)
+        value = self.obj[key]
         if value:
-            if isinstance(value, dict) and not isinstance(value, LazyTaintDict):
+            if isinstance(value, abc.Mapping) and not isinstance(value, LazyTaintDict):
                 value = LazyTaintDict(
                     value, origins=self.origins, override_pyobject_tainted=self.override_pyobject_tainted
                 )
-                self[key] = value
-            elif isinstance(value, list) and not isinstance(value, LazyTaintList):
-                value = LazyTaintList(
-                    value,
-                    origins=self.origins,
-                    override_pyobject_tainted=self.override_pyobject_tainted,
-                    source_name=self.source_name,
-                )
-                if isinstance(key, int):
-                    self[key] = value
+                self.obj[key] = value
             elif isinstance(value, (str, bytes, bytearray)):
                 if not is_pyobject_tainted(value) or self.override_pyobject_tainted:
                     try:
@@ -51,36 +45,40 @@ class LazyTaintList(list):
                         log.debug("SystemError while tainting value: %s with key: %s", value, key, exc_info=True)
                     except Exception:
                         log.debug("Unexpected exception while tainting value", exc_info=True)
-        return value
-
-    def __iter__(self):
-        return (self[i] for i in range(len(self)))
-
-
-class LazyTaintDict(dict):
-    def __init__(self, *args, origins=(0, 0), override_pyobject_tainted=False):
-        self.origins = origins
-        self.origin_key = origins[0]
-        self.origin_value = origins[1]
-        self.override_pyobject_tainted = override_pyobject_tainted
-        super(LazyTaintDict, self).__init__(*args)
-
-    def __getitem__(self, key):
-        value = super(LazyTaintDict, self).__getitem__(key)
-        if value:
-            if isinstance(value, dict) and not isinstance(value, LazyTaintDict):
-                value = LazyTaintDict(
-                    value, origins=self.origins, override_pyobject_tainted=self.override_pyobject_tainted
-                )
-                self[key] = value
-            elif isinstance(value, list) and not isinstance(value, LazyTaintList):
+            elif isinstance(value, abc.Mapping) and not isinstance(value, LazyTaintList):
                 value = LazyTaintList(
                     value,
                     origins=self.origins,
                     override_pyobject_tainted=self.override_pyobject_tainted,
-                    source_name=key,
+                    source_name=self.source_name,
                 )
-                self[key] = value
+                if isinstance(key, int):
+                    self.obj[key] = value
+        return value
+
+    def __iter__(self):
+        return (self.obj[i] for i in range(len(self)))
+
+    def __getattr__(self, name):
+        return getattr(self.obj, name)
+
+
+class LazyTaintDict:
+    def __init__(self, original_dict, origins=(0, 0), override_pyobject_tainted=False):
+        self.obj = original_dict
+        self.origins = origins
+        self.origin_key = origins[0]
+        self.origin_value = origins[1]
+        self.override_pyobject_tainted = override_pyobject_tainted
+
+    def __getitem__(self, key):
+        value = self.obj[key]
+        if value:
+            if isinstance(value, abc.Mapping) and not isinstance(value, LazyTaintDict):
+                value = LazyTaintDict(
+                    value, origins=self.origins, override_pyobject_tainted=self.override_pyobject_tainted
+                )
+                self.obj[key] = value
             elif isinstance(value, (str, bytes, bytearray)):
                 if not is_pyobject_tainted(value) or self.override_pyobject_tainted:
                     try:
@@ -94,6 +92,14 @@ class LazyTaintDict(dict):
                         log.debug("SystemError while tainting value: %s with key: %s", value, key, exc_info=True)
                     except Exception:
                         log.debug("Unexpected exception while tainting value", exc_info=True)
+            elif isinstance(value, abc.Sequence) and not isinstance(value, LazyTaintList):
+                value = LazyTaintList(
+                    value,
+                    origins=self.origins,
+                    override_pyobject_tainted=self.override_pyobject_tainted,
+                    source_name=key,
+                )
+                self.obj[key] = value
         return value
 
     def get(self, key, default=None):
@@ -108,7 +114,7 @@ class LazyTaintDict(dict):
             yield (k, self[k])
 
     def keys(self):
-        for k in super(LazyTaintDict, self).keys():
+        for k in self.obj.keys():
             if (
                 k
                 and isinstance(k, (str, bytes, bytearray))
@@ -123,6 +129,10 @@ class LazyTaintDict(dict):
     def values(self):
         for _, v in self.items():
             yield v
+
+    def __getattr__(self, name):
+        # type: (str) -> Any
+        return getattr(self.obj, name)
 
 
 def supported_dbapi_integration(integration_name):
