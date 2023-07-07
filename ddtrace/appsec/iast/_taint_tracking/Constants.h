a4b09ed0784446226540c6b15a120247b2b151cd
Alberto Vara <alberto.vara@datadoghq.com>
2023-07-07 15:42:26 +0200

    Update ddtrace/appsec/iast/_taint_tracking/Constants.h
    Co-authored-by: Gabriele N. Tornetta <P403n1x87@users.noreply.github.com>

--- ddtrace/appsec/iast/_taint_tracking/Constants.h
+++ ddtrace/appsec/iast/_taint_tracking/Constants.h
@@ -2,7 +2,6 @@
 // Created by alberto.vara on 16/05/23.
 //
 
-#ifndef _TAINT_TRACKING_CONSTANTS_H
-#define _TAINT_TRACKING_CONSTANTS_H
+#pragma once
 #define PY_MODULE_NAME "ddtrace.appsec.iast._taint_tracking._native"
 #endif //_TAINT_TRACKING_CONSTANTS_H
