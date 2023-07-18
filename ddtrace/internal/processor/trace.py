f98c9307ad0580564b101a15f23ba86d1ccf6eb6
ZStriker19 <zach.groves@datadoghq.com>
2023-07-18 14:32:35 -0400

    ignore typing for lock

--- ddtrace/internal/processor/trace.py
+++ ddtrace/internal/processor/trace.py
@@ -182,9 +182,9 @@ class SpanAggregator(SpanProcessor):
         repr=False,
     )
     if config._span_aggregator_rlock:
-        _lock = attr.ib(init=False, factory=threading.RLock, repr=False, type=contextmanager)
+        _lock = attr.ib(init=False, factory=threading.RLock, repr=False) # type: ignore
     else:
-        _lock = attr.ib(init=False, factory=threading.Lock, repr=False, type=contextmanager)
+        _lock = attr.ib(init=False, factory=threading.Lock, repr=False) # type: ignore
     # Tracks the number of spans created and tags each count with the api that was used
     # ex: otel api, opentracing api, datadog api
     _span_metrics = attr.ib(
