92c6c2091a67f15a98e1fe69cb0d0b5698f2b71b
ZStriker19 <zach.groves@datadoghq.com>
2023-07-18 15:06:45 -0400

    fix typing attempt

--- ddtrace/internal/processor/trace.py
+++ ddtrace/internal/processor/trace.py
@@ -1,6 +1,7 @@
 import abc
 from collections import defaultdict
 import threading
+from typing import Any
 from typing import Dict
 from typing import Iterable
 from typing import List
@@ -181,9 +182,9 @@ class SpanAggregator(SpanProcessor):
         repr=False,
     )
     if config._span_aggregator_rlock:
-        _lock = attr.ib(init=False, factory=threading.RLock, repr=False)
+        _lock = attr.ib(init=False, factory=threading.RLock, repr=False, type=Any)
     else:
-        _lock = attr.ib(init=False, factory=threading.Lock, repr=False)
+        _lock = attr.ib(init=False, factory=threading.Lock, repr=False, type=Any)
     # Tracks the number of spans created and tags each count with the api that was used
     # ex: otel api, opentracing api, datadog api
     _span_metrics = attr.ib(
