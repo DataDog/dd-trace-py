51b69c56288efa8407142f242f44015dc081b62c
Alberto Vara <alberto.vara@datadoghq.com>
2023-07-24 12:20:00 +0200

    feat: rollback flask and django

--- tests/contrib/django_celery/test_django_celery.py
+++ tests/contrib/django_celery/test_django_celery.py
@@ -12,6 +12,7 @@ def test_django_celery_gevent_startup():
     likely that we might see lazy objects, like settings, being created before
     time. This would cause Celery to trigger exceptions, causing the application
     to fail to start.
+
     In this particular instance we test that the application starts correctly
     (albeit with no message broker running) and that we don't get any errors
     about Django settings.
