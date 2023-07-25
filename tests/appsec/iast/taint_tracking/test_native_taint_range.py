89827a32347c70459df7364bc6ea34f71d0fb099
Alberto Vara <alberto.vara@datadoghq.com>
2023-07-25 12:54:48 +0200

    chore: enable iast tests

--- tests/appsec/iast/taint_tracking/test_native_taint_range.py
+++ tests/appsec/iast/taint_tracking/test_native_taint_range.py
@@ -60,7 +60,6 @@ _RANGE1 = TaintRange(0, 2, _SOURCE1)
 _RANGE2 = TaintRange(1, 3, _SOURCE2)
 
 
-@pytest.mark.skipif(sys.version_info[:2] in [(3, 9), (3, 10)], reason="flaky test on Python3.9")
 def test_unicode_fast_tainting():
     s = "somestr" * 4000
     s_check = "somestr" * 4000
