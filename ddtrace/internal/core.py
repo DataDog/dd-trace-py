acd6501a33f2cdaf7ff245b54c6a74fbd0b24b0a
Emmett Butler <emmett.butler321@gmail.com>
2023-07-27 08:06:28 -0700

    bandit

--- ddtrace/internal/core.py
+++ ddtrace/internal/core.py
@@ -52,11 +52,12 @@ class EventHub:
         if not isinstance(args, list):
             args = [args] + list(other_args)
         else:
-            assert not other_args, (
-                "When the first argument expected by the event handler is a list, all arguments "
-                "must be passed in a list. For example, use dispatch('foo', [[l1, l2], arg2]) "
-                "instead of dispatch('foo', [l1, l2], arg2)."
-            )
+            if other_args:
+                raise TypeError(
+                    "When the first argument expected by the event handler is a list, all arguments "
+                    "must be passed in a list. For example, use dispatch('foo', [[l1, l2], arg2]) "
+                    "instead of dispatch('foo', [l1, l2], arg2)."
+                )
         log.debug("Dispatching event %s", event_id)
         results = []
         exceptions = []
