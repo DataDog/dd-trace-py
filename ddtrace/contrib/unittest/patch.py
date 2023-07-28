eede45539c6fa9f1cdf21816934328d96fce7a10
eric.navarro <eric.navarro@datadoghq.com>
2023-07-28 15:04:38 +0200

    Refactor tags

--- ddtrace/contrib/unittest/patch.py
+++ ddtrace/contrib/unittest/patch.py
@@ -29,6 +29,18 @@ def _extract_span(item):
     """Extract span from `unittest` instance."""
     return getattr(item, "_datadog_span", None)
 
+def _extract_test_method_name(item):
+    """Extract test method name from `unittest` instance."""
+    return getattr(item, "_testMethodName", None)
+
+def _extract_suite_name_from_test_method(item):
+    item_type = type(item)
+    return getattr(item_type, "__name__", None)
+
+
+def _extract_module_name_from_test_method(item):
+    return getattr(item, "__module__", None)
+
 def patch():
     """
     Patched the instrumented methods from unittest
@@ -43,17 +55,19 @@ def patch():
 
     _w = wrapt.wrap_function_wrapper
 
-    _w(unittest, "TextTestResult.addSuccess", add_sucess_test_wrapper)
+    _w(unittest, "TextTestResult.addSuccess", add_success_test_wrapper)
     _w(unittest, "TextTestResult.addFailure", add_failure_test_wrapper)
+    _w(unittest, "TextTestResult.addError", add_error_test_wrapper)
+    _w(unittest, "TextTestResult.addSkip", add_skip_test_wrapper)
+
     _w(unittest, "TestCase.run", start_test_wrapper)
 
 
-def add_sucess_test_wrapper(func, instance, args, kwargs):
+def add_success_test_wrapper(func, instance, args, kwargs):
     if instance and type(instance) == unittest.runner.TextTestResult and args:
         test_item = args[0]
-        if test_item._outcome.success:
-            span = _extract_span(test_item)
-            span.set_tag_str(test.STATUS, test.Status.PASS.value)
+        span = _extract_span(test_item)
+        span.set_tag_str(test.STATUS, test.Status.PASS.value)
 
     return func(*args, **kwargs)
 
@@ -61,12 +75,26 @@ def add_sucess_test_wrapper(func, instance, args, kwargs):
 def add_failure_test_wrapper(func, instance, args, kwargs):
     if instance and type(instance) == unittest.runner.TextTestResult and args:
         test_item = args[0]
-        if test_item._outcome.success:
-            span = _extract_span(test_item)
-            span.set_tag_str(test.STATUS, test.Status.FAIL.value)
+        span = _extract_span(test_item)
+        span.set_tag_str(test.STATUS, test.Status.FAIL.value)
 
     return func(*args, **kwargs)
 
+def add_error_test_wrapper(func, instance, args, kwargs):
+    if instance and type(instance) == unittest.runner.TextTestResult and args:
+        test_item = args[0]
+        span = _extract_span(test_item)
+        span.set_tag_str(test.STATUS, test.Status.ERROR.value)
+
+    return func(*args, **kwargs)
+
+def add_skip_test_wrapper(func, instance, args, kwargs):
+    if instance and type(instance) == unittest.runner.TextTestResult and args:
+        test_item = args[0]
+        span = _extract_span(test_item)
+        span.set_tag_str(test.STATUS, test.Status.FAIL.value)
+
+    return func(*args, **kwargs)
 
 def start_test_wrapper(func, instance, args, kwargs):
     with _CIVisibility._instance.tracer._start_span(
@@ -75,28 +103,21 @@ def start_test_wrapper(func, instance, args, kwargs):
         resource="unittest.test",
         span_type=SpanTypes.TEST
     ) as span:
-        test_method_name = instance._testMethodName
+        span.set_tag_str(_EVENT_TYPE, SpanTypes.TEST)
+
         span.set_tag_str(COMPONENT, COMPONENT_VALUE)
         span.set_tag_str(SPAN_KIND, KIND)
+
         span.set_tag_str(test.FRAMEWORK, FRAMEWORK)
-        span.set_tag_str(_EVENT_TYPE, SpanTypes.TEST)
-        span.set_tag_str(test.NAME, test_method_name)
-        span.set_tag_str(test.COMMAND, "unittest")
-        #span.set_tag_str(_SESSION_ID, str(test_session_span.span_id))
-
-        #span.set_tag_str(_MODULE_ID, str(test_module_span.span_id))
-        span.set_tag_str(test.MODULE, "main")
-        span.set_tag_str(test.MODULE_PATH, "main.py")
-
-        #span.set_tag_str(_SUITE_ID, str(test_suite_span.span_id))
         span.set_tag_str(test.TYPE, SpanTypes.TEST)
-        span.set_tag_str(test.FRAMEWORK_VERSION, "unittest")
+
+        span.set_tag_str(test.NAME, _extract_test_method_name(instance))
+        span.set_tag_str(test.SUITE, _extract_suite_name_from_test_method(instance))
+        span.set_tag_str(test.MODULE, _extract_module_name_from_test_method(instance))
         """
         if item.location and item.location[0]:
             _CIVisibility.set_codeowners_of(item.location[0], span=span)
         """
-        # We preemptively set FAIL as a status, because if pytest_runtest_makereport is not called
-        # (where the actual test status is set), it means there was a pytest error
         span.set_tag_str(test.STATUS, test.Status.FAIL.value)
         _store_span(instance, span)
         result = func(*args, **kwargs)
