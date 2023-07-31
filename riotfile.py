8bb39cb0531244f971a8f64df754702c975ada70
Yun Kim <yun.kim@datadoghq.com>
2023-07-31 13:16:42 -0400

    Add subprocess test suite to circleci, riot

--- riotfile.py
+++ riotfile.py
@@ -2878,6 +2878,11 @@ venv = Venv(
             pkgs={"msgpack": latest, "coverage": latest},
         ),
         Venv(
+            name="subprocess",
+            command="pytest {cmdargs} tests/contrib/subprocess",
+            pys=select_pys(),
+        ),
+        Venv(
             name="profile",
             pkgs={
                 "gunicorn": latest,
