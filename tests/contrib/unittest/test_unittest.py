3fd1581219bacccdcc81fd802e8cf77e387c9290
eric.navarro <eric.navarro@datadoghq.com>
2023-08-02 11:43:50 +0200

    Add more unittest cases to test file

--- tests/contrib/unittest/test_unittest.py
+++ tests/contrib/unittest/test_unittest.py
@@ -13,7 +13,7 @@ class UnittestTestCase(TracerTestCase):
         self.monkeypatch = monkeypatch
         self.git_repo = git_repo
 
-    def test_unittest_pass(self):
+    def test_unittest_pass_single(self):
         """Test with a `unittest` test which should pass."""
         import unittest
         from ddtrace import patch
@@ -21,10 +21,172 @@ class UnittestTestCase(TracerTestCase):
         patch(unittest=True)
 
         class UnittestExampleTestCase(unittest.TestCase):
-            def test_will_pass(self):
+            def test_will_pass_first(self):
                 self.assertTrue(2 == 2)
+                self.assertTrue('test string' == 'test string')
+                self.assertFalse('not equal to' == 'this')
 
         suite = unittest.TestLoader().loadTestsFromTestCase(UnittestExampleTestCase)
         unittest.TextTestRunner(verbosity=0).run(suite)
 
-        print(self)
+    def test_unittest_pass_multiple(self):
+        """Tests with`unittest` tests which should pass."""
+        import unittest
+        from ddtrace import patch
+
+        patch(unittest=True)
+
+        class UnittestExampleTestCase(unittest.TestCase):
+            def test_will_pass_first(self):
+                self.assertTrue(2 == 2)
+                self.assertTrue('test string' == 'test string')
+                self.assertFalse('not equal to' == 'this')
+
+            def test_will_pass_second(self):
+                self.assertTrue(3 == 3)
+                self.assertTrue(4 != 5)
+
+        suite = unittest.TestLoader().loadTestsFromTestCase(UnittestExampleTestCase)
+        unittest.TextTestRunner(verbosity=0).run(suite)
+
+    def test_unittest_skip_single(self):
+        """Tests with a `unittest` test which should be skipped."""
+        import unittest
+        from ddtrace import patch
+
+        patch(unittest=True)
+
+        class UnittestExampleTestCase(unittest.TestCase):
+            def test_will_be_skipped(self):
+                self.assertTrue(2 == 2)
+                self.assertTrue('test string' == 'test string')
+                self.assertFalse('not equal to' == 'this')
+
+        suite = unittest.TestLoader().loadTestsFromTestCase(UnittestExampleTestCase)
+        unittest.TextTestRunner(verbosity=0).run(suite)
+
+    def test_unittest_skip_single_reason(self):
+        """Tests with a `unittest` test which should be skipped."""
+        import unittest
+        from ddtrace import patch
+
+        patch(unittest=True)
+
+        class UnittestExampleTestCase(unittest.TestCase):
+            @unittest.skip("demonstrating skipping with a reason")
+            def test_will_be_skipped(self):
+                self.assertTrue(2 == 2)
+                self.assertTrue('test string' == 'test string')
+                self.assertFalse('not equal to' == 'this')
+
+        suite = unittest.TestLoader().loadTestsFromTestCase(UnittestExampleTestCase)
+        unittest.TextTestRunner(verbosity=0).run(suite)
+
+    def test_unittest_skip_multiple_reason(self):
+        """Test with `unittest` tests which should be skipped."""
+        import unittest
+        from ddtrace import patch
+
+        patch(unittest=True)
+
+        class UnittestExampleTestCase(unittest.TestCase):
+            @unittest.skip("demonstrating skipping with a rason")
+            def test_will_be_skipped_first(self):
+                self.assertTrue(2 == 2)
+                self.assertTrue('test string' == 'test string')
+                self.assertFalse('not equal to' == 'this')
+
+            @unittest.skip("demonstrating skipping with a rason")
+            def test_will_be_skipped_second(self):
+                self.assertTrue(2 == 2)
+                self.assertTrue('test string' == 'test string')
+                self.assertFalse('not equal to' == 'this')
+
+        suite = unittest.TestLoader().loadTestsFromTestCase(UnittestExampleTestCase)
+        unittest.TextTestRunner(verbosity=0).run(suite)
+
+    def test_unittest_skip_combined(self):
+        """Test with `unittest` tests which should be skipped."""
+        import unittest
+        from ddtrace import patch
+
+        patch(unittest=True)
+
+        class UnittestExampleTestCase(unittest.TestCase):
+            @pytest.skip()
+            def test_will_be_skipped(self):
+                self.assertTrue(2 == 2)
+                self.assertTrue('test string' == 'test string')
+                self.assertFalse('not equal to' == 'this')
+
+            def test_wont_be_skipped(self):
+                self.assertTrue(2 == 2)
+                self.assertTrue('test string' == 'test string')
+                self.assertFalse('not equal to' == 'this')
+
+        suite = unittest.TestLoader().loadTestsFromTestCase(UnittestExampleTestCase)
+        unittest.TextTestRunner(verbosity=0).run(suite)
+
+    def test_unittest_fail_single(self):
+        """Test with `unittest` tests which should fail."""
+        import unittest
+        from ddtrace import patch
+
+        patch(unittest=True)
+
+        class UnittestExampleTestCase(unittest.TestCase):
+            def test_will_fail(self):
+                self.assertTrue(2 != 2)
+                self.assertTrue('test string' == 'test string')
+                self.assertFalse('not equal to' == 'this')
+
+        suite = unittest.TestLoader().loadTestsFromTestCase(UnittestExampleTestCase)
+        unittest.TextTestRunner(verbosity=0).run(suite)
+
+    def test_unittest_fail_multiple(self):
+        """Test with `unittest` tests which should fail."""
+        import unittest
+        from ddtrace import patch
+
+        patch(unittest=True)
+
+        class UnittestExampleTestCase(unittest.TestCase):
+            def test_will_fail_first(self):
+                self.assertTrue(2 != 2)
+                self.assertTrue('test string' == 'test string')
+                self.assertFalse('not equal to' == 'this')
+
+            def test_will_fail_second(self):
+                self.assertTrue(2 != 2)
+                self.assertTrue('test string' == 'test string')
+                self.assertFalse('not equal to' == 'this')
+
+        suite = unittest.TestLoader().loadTestsFromTestCase(UnittestExampleTestCase)
+        unittest.TextTestRunner(verbosity=0).run(suite)
+
+    def test_unittest_combined(self):
+        """Test with `unittest` tests which pass, get skipped and fail combined."""
+        import unittest
+        from ddtrace import patch
+
+        patch(unittest=True)
+
+        class UnittestExampleTestCase(unittest.TestCase):
+            def test_will_pass_first(self):
+                self.assertTrue(2 == 2)
+                self.assertTrue('test string' == 'test string')
+                self.assertFalse('not equal to' == 'this')
+
+            def test_will_fail_first(self):
+                self.assertTrue(2 != 2)
+                self.assertTrue('test string' == 'test string')
+                self.assertFalse('not equal to' == 'this')
+
+            @pytest.skip()
+            def test_will_be_skipped(self):
+                self.assertTrue(2 == 2)
+                self.assertTrue('test string' == 'test string')
+                self.assertFalse('not equal to' == 'this')
+
+        suite = unittest.TestLoader().loadTestsFromTestCase(UnittestExampleTestCase)
+        unittest.TextTestRunner(verbosity=0).run(suite)
