809d5440ecca74029cf948c09131ea88106259f9
Alberto Vara <alberto.vara@datadoghq.com>
2023-08-02 17:21:55 +0200

    fix(iast): missing source

--- ddtrace/appsec/iast/taint_sinks/command_injection.py
+++ ddtrace/appsec/iast/taint_sinks/command_injection.py
@@ -8,3 +8,9 @@ from ddtrace.appsec.iast.taint_sinks._base import VulnerabilityBase
 class CommandInjection(VulnerabilityBase):
     vulnerability_type = VULN_CMDI
     evidence_type = EVIDENCE_CMDI
+
+    @classmethod
+    def report(cls, evidence_value=None, sources=None):
+        if isinstance(evidence_value, (str, bytes, bytearray)):
+            evidence_value, sources = evidence_value(evidence_value)
+        super(CommandInjection, cls).report(evidence_value=evidence_value, sources=sources)
