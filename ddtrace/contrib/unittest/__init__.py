99029cf48abafa604caed129ea720ef67420ca79
eric.navarro <eric.navarro@datadoghq.com>
2023-08-02 14:58:54 +0200

    Add feature flag and release note

--- ddtrace/contrib/unittest/__init__.py
+++ ddtrace/contrib/unittest/__init__.py
@@ -5,13 +5,14 @@ from .patch import patch
 
 __all__ = ["patch"]
 
-# unittest default settings
-config._add(
-    "unittest",
-    dict(
-        _default_service="unittest",
-        operation_name=os.getenv("DD_UNITTEST_OPERATION_NAME", default="unittest.test"),
-    ),
-)
+if config._ci_visibility_unittest_enabled:
+    # unittest default settings
+    config._add(
+        "unittest",
+        dict(
+            _default_service="unittest",
+            operation_name=os.getenv("DD_UNITTEST_OPERATION_NAME", default="unittest.test"),
+        ),
+    )
 
-patch()
+    patch()
