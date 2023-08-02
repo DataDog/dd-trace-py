765314815331fc021e650f1391ce15e79e691852
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2023-08-02 15:41:22 +0200

    Disable non working auto generated contrib tests for subprocess
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- tests/contrib/subprocess/test_subprocess_patch.py
+++ tests/contrib/subprocess/test_subprocess_patch.py
@@ -1,3 +1,4 @@
+from ddtrace import config
 from ddtrace.contrib.subprocess.patch import patch
 
 
@@ -14,6 +15,10 @@ class TestSubprocessPatch(PatchTestCase.Base):
     __patch_func__ = patch
     __unpatch_func__ = unpatch
 
+    def __init__(self, *args, **kwargs):
+        config._appsec_enabled = True
+        super(TestSubprocessPatch, self).__init__(*args, **kwargs)
+
     def assert_module_patched(self, subprocess):
         self.assert_wrapped(subprocess.Popen.__init__)
         self.assert_wrapped(subprocess.Popen.wait)
@@ -25,3 +30,13 @@ class TestSubprocessPatch(PatchTestCase.Base):
     def assert_not_module_double_patched(self, subprocess):
         self.assert_not_double_wrapped(subprocess.Popen.__init__)
         self.assert_not_double_wrapped(subprocess.Popen.wait)
+
+    # These are disabled because the base class uses @run_in_subprocess which
+    # import subprocess before we have a chance to patch. However, the contrib
+    # unittests already test patch and unpatch
+    def test_ddtrace_run_patch_on_import(self): pass
+    def test_import_patch_unpatch_unpatch(self): pass
+    def test_import_unpatch_patch(self): pass
+    def test_patch_unpatch_import_unpatch(self): pass
+    def test_patch_unpatch_unpatch_import(self): pass
+    def test_unpatch_patch_import(self): pass
