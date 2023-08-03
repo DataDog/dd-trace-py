1e226ace29a6efad956e6c182a9f9e6172412c9d
eric.navarro <eric.navarro@datadoghq.com>
2023-08-03 15:46:12 +0200

    Add docs

--- ddtrace/contrib/unittest/__init__.py
+++ ddtrace/contrib/unittest/__init__.py
@@ -1,3 +1,19 @@
+"""
+The unittest integration traces test executions.
+
+
+Enabling
+~~~~~~~~
+
+The aredis integration is enabled automatically when using
+:ref:`ddtrace-run<ddtracerun>` or :func:`patch_all()<ddtrace.patch_all>`.
+
+Or use :func:`patch()<ddtrace.patch>` to manually enable the integration::
+
+    from ddtrace import patch
+    patch(unittest=True)
+"""
+
 from ddtrace import config
 from .patch import patch, unpatch
 
