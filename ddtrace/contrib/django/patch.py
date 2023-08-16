eae2a552db5b53375e8f26ca3d78f6e15fb09c32
Teague Bick <teague.bick@datadoghq.com>
2023-08-16 15:27:21 -0400

    fix django

--- ddtrace/contrib/django/patch.py
+++ ddtrace/contrib/django/patch.py
@@ -788,7 +788,7 @@ def traced_login(django, pin, func, instance, args, kwargs):
                         session_id=session_key,
                         propagate=True,
                         login_events_mode=mode,
-                        **user_extra,
+                        **user_extra
                     )
                     return
                 else:
