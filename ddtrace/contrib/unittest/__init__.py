fe84111bf855703139187136bba5ddf310a9cdaa
eric.navarro <eric.navarro@datadoghq.com>
2023-08-17 15:11:41 +0200

    Remove unnecessary config import

--- ddtrace/contrib/unittest/__init__.py
+++ ddtrace/contrib/unittest/__init__.py
@@ -13,7 +13,6 @@ Or use :func:`patch()<ddtrace.patch>` to manually enable the integration::
     from ddtrace import patch
     patch(unittest=True)
 """
-from ddtrace import config
 from .patch import patch, unpatch
 
 __all__ = ["patch", "unpatch"]
