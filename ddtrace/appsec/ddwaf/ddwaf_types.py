8f5fe0938950d731a243da595027d51d93f120c0
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-08-21 14:24:24 +0200

    necessary changes of the WAF API for 1.13

--- ddtrace/appsec/ddwaf/ddwaf_types.py
+++ ddtrace/appsec/ddwaf/ddwaf_types.py
@@ -76,6 +76,10 @@ class DDWAF_OBJ_TYPE(IntEnum):
     DDWAF_OBJ_MAP = 1 << 4
     # Value shall be decode as bool
     DDWAF_OBJ_BOOL = 1 << 5
+    # 64-bit float (or double) type
+    DDWAF_OBJ_FLOAT = 1 << 6
+    # Null type, only used for its semantical value
+    DDWAF_OBJ_NULL = 1 << 7
 
 
 class DDWAF_RET_CODE(IntEnum):
@@ -237,6 +241,7 @@ class ddwaf_value(ctypes.Union):
         ("intValue", ctypes.c_longlong),
         ("array", ddwaf_object_p),
         ("boolean", ctypes.c_bool),
+        ("f64", ctypes.c_double),
     ]
 
 
@@ -254,6 +259,7 @@ class ddwaf_result(ctypes.Structure):
         ("timeout", ctypes.c_bool),
         ("events", ddwaf_object),
         ("actions", ddwaf_object),
+        ("ddwaf_object derivatives", ddwaf_object),
         ("total_runtime", ctypes.c_uint64),
     ]
 
@@ -470,6 +476,22 @@ ddwaf_object_string = ctypes.CFUNCTYPE(ddwaf_object_p, ddwaf_object_p, ctypes.c_
 
 # object_string variants not used
 
+ddwaf_object_string_from_unsigned = ctypes.CFUNCTYPE(ddwaf_object_p, ddwaf_object_p, ctypes.c_uint64)(
+    ("ddwaf_object_string_from_unsigned", ddwaf),
+    (
+        (3, "object"),
+        (1, "value"),
+    ),
+)
+
+ddwaf_object_string_from_signed = ctypes.CFUNCTYPE(ddwaf_object_p, ddwaf_object_p, ctypes.c_int64)(
+    ("ddwaf_object_string_from_signed", ddwaf),
+    (
+        (3, "object"),
+        (1, "value"),
+    ),
+)
+
 ddwaf_object_unsigned = ctypes.CFUNCTYPE(ddwaf_object_p, ddwaf_object_p, ctypes.c_uint64)(
     ("ddwaf_object_unsigned", ddwaf),
     (
@@ -494,6 +516,20 @@ ddwaf_object_bool = ctypes.CFUNCTYPE(ddwaf_object_p, ddwaf_object_p, ctypes.c_bo
         (3, "object"),
         (1, "value"),
     ),
+)
+
+
+ddwaf_object_float = ctypes.CFUNCTYPE(ddwaf_object_p, ddwaf_object_p, ctypes.c_double)(
+    ("ddwaf_object_float", ddwaf),
+    (
+        (3, "object"),
+        (1, "value"),
+    ),
+)
+
+ddwaf_object_null = ctypes.CFUNCTYPE(ddwaf_object_p, ddwaf_object_p)(
+    ("ddwaf_object_null", ddwaf),
+    ((3, "object"),),
 )
 
 ddwaf_object_array = ctypes.CFUNCTYPE(ddwaf_object_p, ddwaf_object_p)(
@@ -533,6 +569,7 @@ ddwaf_object_map_add = ctypes.CFUNCTYPE(ctypes.c_bool, ddwaf_object_p, ctypes.c_
 # ddwaf_object_get_signed
 # ddwaf_object_get_index
 # ddwaf_object_get_bool https://github.com/DataDog/libddwaf/commit/7dc68dacd972ae2e2a3c03a69116909c98dbd9cb
+# ddwaf_object_get_float
 
 
 ddwaf_get_version = ctypes.CFUNCTYPE(ctypes.c_char_p)(
