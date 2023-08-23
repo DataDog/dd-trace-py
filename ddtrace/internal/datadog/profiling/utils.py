bf9a7545fef927414cd6ed5a0c7dde9dee11afed
sanchda <sanchda@users.noreply.github.com>
2023-08-23 17:11:12 +0000

    Change type annotations to comments

--- ddtrace/internal/datadog/profiling/utils.py
+++ ddtrace/internal/datadog/profiling/utils.py
@@ -3,7 +3,8 @@ from typing import Any
 
 
 # 3.11 and above
-def _sanitize_string_check(value: Any) -> str:
+def _sanitize_string_check(value):
+    # type: (Any) -> str
     from ddtrace.internal.logger import get_logger
 
     LOG = get_logger(__name__)
@@ -14,11 +15,10 @@ def _sanitize_string_check(value: Any) -> str:
 
     return value
 
-
 # 3.10 and below (the noop version)
-def _sanitize_string_identity(value: Any) -> str:
+def _sanitize_string_identity(value):
+    # type: (Any) -> str
     return value
-
 
 # Assign based on version
 sanitize_string = _sanitize_string_check if version_info[:2] > (3, 10) else _sanitize_string_identity
