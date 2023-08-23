047519a6de7dc0ff2459b7b1648957e35f175a03
sanchda <sanchda@users.noreply.github.com>
2023-08-23 16:10:37 +0000

    Harmonize sanitize_string

--- ddtrace/internal/datadog/profiling/utils.py
+++ ddtrace/internal/datadog/profiling/utils.py
@@ -1,12 +1,22 @@
 from typing import Any
+from sys import version_info
 
+# 3.11 and above
+def _sanitize_string_check(value: Any) -> str:
+    from ddtrace.internal.logger import get_logger
+    LOG = get_logger(__name__)
 
-def sanitize_string(value):
-    # type: (Any) -> str
-    try:
-        return six.ensure_str(value)
-    except Exception:
-        try:
-            return str(value)
-        except Exception:
-            return "[error]({})".format(value.__class__.__name__)
+    if not isinstance(value, str):
+        LOG.warning(
+            "Got object of type '%s' instead of str during profile serialization", type(value).__name__
+        )
+        return "[invalid string]%s" % type(value).__name__
+
+    return value
+
+# 3.10 and below (the noop version)
+def _sanitize_string_identity(value: Any) -> str:
+    return value
+
+# Assign based on version
+sanitize_string = _sanitize_string_check if version_info[:2] > (3, 10) else _sanitize_string_identity
