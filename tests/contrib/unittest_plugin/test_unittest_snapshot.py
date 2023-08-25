255c8fba26c0d1742de644a7d319994c6befa67c
eric.navarro <eric.navarro@datadoghq.com>
2023-08-25 17:31:20 +0200

    add snapshot tests

---
+++ tests/contrib/unittest_plugin/test_unittest_snapshot.py
@@ -0,0 +1,46 @@
+import unittest
+import pytest
+import sys
+import unittest
+
+import pytest
+
+from ddtrace.constants import ERROR_MSG
+from ddtrace.constants import ERROR_TYPE
+from ddtrace.constants import SPAN_KIND
+from ddtrace.contrib.unittest.constants import COMPONENT_VALUE
+from ddtrace.contrib.unittest.constants import FRAMEWORK
+from ddtrace.contrib.unittest.constants import KIND
+from ddtrace.contrib.unittest.patch import _set_tracer
+from ddtrace.contrib.unittest.patch import patch
+from tests.utils import TracerTestCase
+from tests.utils import override_env
+
+
+class UnittestTestCase(TracerTestCase):
+    @pytest.fixture(autouse=True)
+    def fixtures(self, testdir, monkeypatch, git_repo):
+        self.testdir = testdir
+        self.monkeypatch = monkeypatch
+        self.git_repo = git_repo
+
+    with override_env(dict(DD_API_KEY="foobar.baz")):
+        patch()
+
+        @pytest.mark.snapshot
+        def test_unittest_run(self):
+            _set_tracer(self.tracer)
+
+            class MyTestSuite(unittest.TestCase):
+                def test_will_pass(self):
+                    self.assertTrue(1 == 1)
+
+                def test_will_fail(self):
+                    self.assertTrue(1 == 2)
+
+                @unittest.skip(reason="we will need to skip this!")
+                def test_will_be_skipped(self):
+                    self.assertTrue(1 == 1)
+
+            suite = unittest.TestLoader().loadTestsFromTestCase(MyTestSuite)
+            unittest.TextTestRunner(verbosity=0).run(suite)
