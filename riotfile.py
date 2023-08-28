d2967aecd575f034f663a9cee9abcf4d3c9be6c6
eric.navarro <eric.navarro@datadoghq.com>
2023-08-28 16:19:50 +0200

    fix riotfile

--- riotfile.py
+++ riotfile.py
@@ -1738,8 +1738,12 @@ venv = Venv(
             name="unittest-snapshot",
             command="pytest --no-ddtrace {cmdargs} tests/contrib/unittest_plugin/snapshot/",
             pkgs={"msgpack": latest},
-            env={"DD_CIVISIBILITY_UNITTEST_ENABLED": "1"},
-            pys=select_pys(min_version="2.7", max_version="3.11"),
+            env={
+                "DD_CIVISIBILITY_UNITTEST_ENABLED": "1",
+                "DD_TRACE_AGENT_URL": "http://localhost:9126",
+                "AGENT_VERSION": "testagent",
+            },
+            pys="3.7",
         ),
         Venv(
             name="grpc",
