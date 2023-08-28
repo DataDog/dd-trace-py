0e05b3e31096a64bddb83bd8e4c7bd1b2b78cc62
Alberto Vara <alberto.vara@datadoghq.com>
2023-08-28 17:27:28 +0200

    fix(iast): command injection skipped (#6771)
    - CMDi isn't reported if that command with the same parameters are
    cached by the scrubbing system.
    - Move CMDi code from contrib folder to iast folder
    - Update test with a regression test
    
    ## Checklist
    
    - [x] Change(s) are motivated and described in the PR description.
    - [x] Testing strategy is described if automated tests are not included
    in the PR.
    - [x] Risk is outlined (performance impact, potential for breakage,
    maintainability, etc).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] [Library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    are followed. If no release note is required, add label
    `changelog/no-changelog`.
    - [x] Documentation is included (in-code, generated user docs, [public
    corp docs](https://github.com/DataDog/documentation/)).
    - [x] Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    
    - [x] Title is accurate.
    - [x] No unnecessary changes are introduced.
    - [x] Description motivates each change.
    - [x] Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes unless absolutely necessary.
    - [x] Testing strategy adequately addresses listed risk(s).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] Release note makes sense to a user of the library.
    - [x] Reviewer has explicitly acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment.
    - [x] Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    - [x] If this PR touches code that signs or publishes builds or
    packages, or handles credentials of any kind, I've requested a review
    from `@DataDog/security-design-and-guidance`.
    - [x] This PR doesn't touch any of that.
    
    ---------
    
    Co-authored-by: Christophe Papazian <114495376+christophe-papazian@users.noreply.github.com>

--- ddtrace/appsec/iast/taint_sinks/command_injection.py
+++ ddtrace/appsec/iast/taint_sinks/command_injection.py
@@ -1,3 +1,6 @@
+from typing import List
+from typing import Union
+
 from ddtrace.appsec.iast import oce
 from ddtrace.appsec.iast.constants import EVIDENCE_CMDI
 from ddtrace.appsec.iast.constants import VULN_CMDI
@@ -16,3 +19,21 @@ class CommandInjection(VulnerabilityBase):
 
             evidence_value, sources = taint_ranges_as_evidence_info(evidence_value)
         super(CommandInjection, cls).report(evidence_value=evidence_value, sources=sources)
+
+
+def _iast_report_cmdi(shell_args):
+    # type: (Union[str, List[str]]) -> None
+    report_cmdi = ""
+    from ddtrace.appsec.iast._taint_tracking import get_tainted_ranges
+    from ddtrace.appsec.iast._taint_tracking.aspects import join_aspect
+
+    if isinstance(shell_args, (list, tuple)):
+        for arg in shell_args:
+            if get_tainted_ranges(arg):
+                report_cmdi = join_aspect(" ", shell_args)
+                break
+    elif get_tainted_ranges(shell_args):
+        report_cmdi = shell_args
+
+    if report_cmdi:
+        CommandInjection.report(evidence_value=report_cmdi)
