f41cfaa21752447254f09fa027714d2aa50aa44c
eric.navarro <eric.navarro@datadoghq.com>
2023-08-28 12:52:03 +0200

    fix lint

--- riotfile.py
+++ riotfile.py
@@ -1728,7 +1728,8 @@ venv = Venv(
         ),
         Venv(
             name="unittest",
-            command="pytest --no-ddtrace {cmdargs} tests/contrib/unittest_plugin/ --ignore=tests/contrib/unittest_plugin/snapshot/",
+            command="pytest --no-ddtrace {cmdargs} tests/contrib/unittest_plugin/"
+            + " --ignore=tests/contrib/unittest_plugin/snapshot/",
             pkgs={"msgpack": latest},
             env={"DD_CIVISIBILITY_UNITTEST_ENABLED": "1"},
             pys=select_pys(min_version="2.7", max_version="3.11"),
