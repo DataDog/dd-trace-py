591feec9c6401eac4378ec4c10ff4562444b8e98
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2023-08-30 18:00:50 +0200

    Support kwargs on new taint_range pseudo constructor
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/appsec/iast/_taint_tracking/TaintTracking/TaintRange.cpp
+++ ddtrace/appsec/iast/_taint_tracking/TaintTracking/TaintRange.cpp
@@ -320,7 +320,7 @@ pyexport_taintrange(py::module& m)
     // Fake constructor, used to force calling allocate_taint_range for performance reasons
     m.def("taint_range", [](int start, int length, Source source) {
         return initializer->allocate_taint_range(start, length, std::move(source));
-    });
+    }, "start"_a, "length"_a, "source"_a);
 
     py::class_<TaintRange, shared_ptr<TaintRange>>(m, "TaintRange_")
       // Normal constructor disabled on the Python side, see above
