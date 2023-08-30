51d90dc42efd5b9790f274a7127146e6d583813f
Tahir H. Butt <tahir.butt@datadoghq.com>
2023-08-30 16:25:40 -0400

    disable dynamic instrumentation

--- tests/debugging/test_expressions.py
+++ tests/debugging/test_expressions.py
@@ -1,9 +1,14 @@
 from dis import dis
+import sys
 
 import pytest
 
 from ddtrace.debugging._expressions import dd_compile
 from ddtrace.internal.safety import SafeObjectProxy
+
+
+if sys.version_info[:2] == (3, 12):
+    pytest.skip("Dynamic instrumentation is not supported with Python 3.12", allow_module_level=True)
 
 
 class SideEffect(Exception):
