c621837677c3cfef7ea4bd699bcdfdb3177e62d8
Romain Komorn <romain.komorn@datadoghq.com>
2023-08-31 12:18:54 +0100

    Force unicode for all string keys and values in Python 2.7

--- ddtrace/internal/ci_visibility/encoder.py
+++ ddtrace/internal/ci_visibility/encoder.py
@@ -15,6 +15,8 @@ from ddtrace.internal.ci_visibility.constants import SESSION_ID
 from ddtrace.internal.ci_visibility.constants import SESSION_TYPE
 from ddtrace.internal.ci_visibility.constants import SUITE_ID
 from ddtrace.internal.ci_visibility.constants import SUITE_TYPE
+from ddtrace.internal.compat import PY2
+from ddtrace.internal.compat import ensure_text
 from ddtrace.internal.encoding import JSONEncoderV2
 from ddtrace.internal.writer.writer import NoEncodableSpansError
 
@@ -74,9 +76,28 @@ class CIVisibilityEncoderV01(BufferedEncoder):
         normalized_spans = [self._convert_span(span, trace[0].context.dd_origin) for trace in traces for span in trace]
         self._metadata = {k: v for k, v in self._metadata.items() if k in self.ALLOWED_METADATA_KEYS}
         # TODO: Split the events in several payloads as needed to avoid hitting the intake's maximum payload size.
-        return msgpack_packb(
+        return self._pack_payload(
             {"version": self.PAYLOAD_FORMAT_VERSION, "metadata": {"*": self._metadata}, "events": normalized_spans}
         )
+
+    def _pack_payload(self, payload):
+        def _py2_payload_force_unicode_strings(payload):
+            def _ensure_text_strings(o):
+                if type(o) == str:
+                    return ensure_text(o)
+                return o
+
+            if type(payload) == list:
+                return [_py2_payload_force_unicode_strings(item) for item in payload]
+            if type(payload) == dict:
+                return {_ensure_text_strings(k): _py2_payload_force_unicode_strings(v) for k, v in payload.items()}
+
+            return _ensure_text_strings(payload)
+
+        if PY2:
+            payload = _py2_payload_force_unicode_strings(payload)
+
+        return msgpack_packb(payload)
 
     def _convert_span(self, span, dd_origin):
         # type: (Span, str) -> Dict[str, Any]
