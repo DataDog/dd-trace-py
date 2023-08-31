3f9b6a5268f382b49a4d342c4e26060cba7c4ed7
Tahir H. Butt <tahir.butt@datadoghq.com>
2023-08-31 14:04:08 -0400

    fix telemetry writer after fork

--- ddtrace/internal/telemetry/writer.py
+++ ddtrace/internal/telemetry/writer.py
@@ -614,6 +614,11 @@ class TelemetryWriter(PeriodicService):
 
         self.stop(join=False)
 
+        # Enable writer service in child process to avoid interpeter shutdown
+        # error in Python 3.12
+        if sys.version_info >= (3, 12):
+            self.enable()
+
     def _restart_sequence(self):
         self._sequence = itertools.count(1)
 
