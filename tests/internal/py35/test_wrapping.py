80da0502b0872eab150a831ac1af43775e2b0215
Yun Kim <yun.kim@datadoghq.com>
2023-09-05 13:24:43 -0400

    re-add removed pyXY files as they are still applicable to python X.Y and above

---
+++ tests/internal/py35/test_wrapping.py
@@ -0,0 +1,79 @@
+from types import CoroutineType
+
+import pytest
+
+from ddtrace.internal.wrapping import wrap
+from tests.internal.py35.asyncstuff import async_func as asyncfoo
+
+
+def test_wrap_generator_yield_from():
+    channel = []
+
+    def wrapper(f, args, kwargs):
+        channel[:] = []
+        for _ in f(*args, **kwargs):
+            channel.append(_)
+            yield _
+
+    def g():
+        yield from range(10)
+
+    wrap(g, wrapper)
+
+    assert list(g()) == list(range(10)) == channel
+
+
+@pytest.mark.asyncio
+async def test_wrap_coroutine():
+    channel = []
+
+    def wrapper(f, args, kwargs):
+        async def _handle_coroutine(c):
+            retval = await c
+            channel.append(retval)
+            return retval
+
+        channel[:] = []
+        retval = f(*args, **kwargs)
+        if isinstance(retval, CoroutineType):
+            return _handle_coroutine(retval)
+        else:
+            channel.append(retval)
+            return retval
+
+    async def c():
+        return await asyncfoo()
+
+    wrap(c, wrapper)
+
+    assert await c() == 42
+
+    assert channel == [42]
+
+
+def test_wrap_args_kwarg():
+    def f(*args, path=None):
+        return (args, path)
+
+    def wrapper(f, args, kwargs):
+        return f(*args, **kwargs)
+
+    wrap(f, wrapper)
+
+    assert f(1, 2) == ((1, 2), None)
+
+
+def test_wrap_arg_args_kwarg_kwargs():
+    def f(posarg, *args, path=None, **kwargs):
+        return (posarg, args, path, kwargs)
+
+    def wrapper(f, args, kwargs):
+        return f(*args, **kwargs)
+
+    wrap(f, wrapper)
+
+    assert f(1, 2) == (1, (2,), None, {})
+    assert f(1, 2, 3, foo="bar") == (1, (2, 3), None, {"foo": "bar"})
+    assert f(1, 2, 3, path="bar") == (1, (2, 3), "bar", {})
+    assert f(1, 2, 3, 4, path="bar", foo="baz") == (1, (2, 3, 4), "bar", {"foo": "baz"})
+    assert f(1, path="bar", foo="baz") == (1, (), "bar", {"foo": "baz"})
