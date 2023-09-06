aa7ea55b2f93f6180d4f37eae507e4d09f60266c
Piotr Wolski <piotr.wolski42@gmail.com>
2023-09-06 13:19:34 -0400

    [data streams] Mock tracer

--- tests/datastreams/test_public_api.py
+++ tests/datastreams/test_public_api.py
@@ -1,30 +1,22 @@
-import pytest
+import mock
 
-from ddtrace import Tracer
 from ddtrace.data_streams import set_consume_checkpoint
 from ddtrace.data_streams import set_produce_checkpoint
 from ddtrace.internal.datastreams.processor import DataStreamsCtx
+from ddtrace.internal.datastreams.processor import DataStreamsProcessor
 
 
-@pytest.fixture
-def tracer():
-    t = Tracer()
-    t.configure()
-    try:
-        yield t
-    finally:
-        pass
+class MockedTracer:
+    def __init__(self):
+        self.data_streams_processor = DataStreamsProcessor("http://localhost:8126")
 
 
-def test_public_api(tracer):
+def test_public_api():
     headers = {}
-    try:
-        del tracer.data_streams_processor._current_context.value
-    except AttributeError:
-        pass
-    set_produce_checkpoint("kinesis", "stream-123", headers.setdefault)
-    got = set_consume_checkpoint("kinesis", "stream-123", headers.get)
-    ctx = DataStreamsCtx(tracer.data_streams_processor, 0, 0, 0)
-    parent_hash = ctx._compute_hash(sorted(["direction:out", "type:kinesis", "topic:stream-123"]), 0)
-    expected = ctx._compute_hash(sorted(["direction:in", "type:kinesis", "topic:stream-123"]), parent_hash)
-    assert got.hash == expected
+    with mock.patch("ddtrace.tracer", new=MockedTracer()):
+        set_produce_checkpoint("kinesis", "stream-123", headers.setdefault)
+        got = set_consume_checkpoint("kinesis", "stream-123", headers.get)
+        ctx = DataStreamsCtx(MockedTracer().data_streams_processor, 0, 0, 0)
+        parent_hash = ctx._compute_hash(sorted(["direction:out", "type:kinesis", "topic:stream-123"]), 0)
+        expected = ctx._compute_hash(sorted(["direction:in", "type:kinesis", "topic:stream-123"]), parent_hash)
+        assert got.hash == expected
