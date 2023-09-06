6d98458c265265ae73ca6bc6eb69a30b0f5e6488
Piotr Wolski <piotr.wolski42@gmail.com>
2023-09-06 15:39:10 -0400

    update tag

--- tests/datastreams/test_public_api.py
+++ tests/datastreams/test_public_api.py
@@ -24,9 +24,9 @@ def test_public_api():
             got = set_consume_checkpoint("kinesis", "stream-123", headers.get)
             ctx = DataStreamsCtx(MockedTracer().data_streams_processor, 0, 0, 0)
             parent_hash = ctx._compute_hash(
-                sorted(["direction:out", "custom_instrumentation:true", "type:kinesis", "topic:stream-123"]), 0
+                sorted(["direction:out", "manual_checkpoint:true", "type:kinesis", "topic:stream-123"]), 0
             )
             expected = ctx._compute_hash(
-                sorted(["direction:in", "custom_instrumentation:true", "type:kinesis", "topic:stream-123"]), parent_hash
+                sorted(["direction:in", "manual_checkpoint:true", "type:kinesis", "topic:stream-123"]), parent_hash
             )
             assert got.hash == expected
