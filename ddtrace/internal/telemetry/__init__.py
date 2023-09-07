678abc98cf7d4569287b3c5a1e64f0cc960fd74d
Munir Abdinur <munir_abdinur@hotmail.com>
2023-09-07 18:40:25 -0400

    Revert "fix: start telemetry writer early"
    This reverts commit 9b9e5eafdc9eb556c80f2466bab64773322e6595.

--- ddtrace/internal/telemetry/__init__.py
+++ ddtrace/internal/telemetry/__init__.py
@@ -49,6 +49,9 @@ def _excepthook(tp, value, root_traceback):
                 error_msg = "{}:{} {}".format(filename, lineno, str(value))
                 telemetry_writer.add_integration(integration_name, True, error_msg=error_msg)
 
+        telemetry_writer.app_shutdown()
+        telemetry_writer.disable()
+
     return _ORIGINAL_EXCEPTHOOK(tp, value, root_traceback)
 
 
