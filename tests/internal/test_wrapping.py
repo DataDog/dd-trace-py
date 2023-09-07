423620ee71f75537e7fc5a914ed3fb036a6ab035
Tahir H. Butt <tahir.butt@datadoghq.com>
2023-09-07 16:37:26 -0400

    disable for bytecode

--- tests/internal/test_wrapping.py
+++ tests/internal/test_wrapping.py
@@ -9,6 +9,11 @@ from ddtrace.internal.wrapping import unwrap
 from ddtrace.internal.wrapping import wrap
 
 
+# bytecode does not support Python 3.12 yet
+if sys.version_info[:2] >= (3, 12):
+    pytestmark = pytest.mark.skip
+
+
 async def async_func():
     return 42
 
