56309f05ef021c005bf75bd0071c63c0aec63c87
Alberto Vara <alberto.vara@datadoghq.com>
2023-09-07 14:46:52 +0200

    chore(iast): refactor cpp code

--- tests/appsec/iast/aspects/conftest.py
+++ tests/appsec/iast/aspects/conftest.py
@@ -4,13 +4,6 @@ from ddtrace.appsec.iast import oce
 from ddtrace.appsec.iast._ast.ast_patching import astpatch_module
 
 
-try:
-    from ddtrace.appsec.iast._taint_tracking import create_context
-    from ddtrace.appsec.iast._taint_tracking import reset_context
-except (ImportError, AttributeError):
-    pytest.skip("IAST not supported for this Python version", allow_module_level=True)
-
-
 def _iast_patched_module(module_name):
     module = __import__(module_name, fromlist=[None])
     module_path, patched_source = astpatch_module(module)
@@ -25,11 +18,3 @@ def _enable_oce():
     oce._enabled = True
     yield
     oce._enabled = False
-
-
-@pytest.fixture(autouse=True)
-def reset_iast_context():
-    oce._enabled = True
-    yield
-    reset_context()
-    _ = create_context()
