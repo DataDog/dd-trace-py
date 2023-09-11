b7c5db4f6d67a89c037ef6ae79cfb48536e084f6
Emmett Butler <emmett.butler321@gmail.com>
2023-09-11 14:40:56 -0700

    py2 skip

--- tests/contrib/django/test_django_wsgi.py
+++ tests/contrib/django/test_django_wsgi.py
@@ -11,6 +11,7 @@ from django.urls import path
 import pytest
 
 from ddtrace.contrib.wsgi import DDWSGIMiddleware
+from ddtrace.internal.compat import PY2
 from tests.webclient import Client
 
 
@@ -39,7 +40,9 @@ urlpatterns = [path("", handler)]
 app = DDWSGIMiddleware(get_wsgi_application(), app_is_iterator=True)
 
 
-@pytest.mark.skipif(django.VERSION < (3, 0, 0), reason="Older Django versions don't work with this use of django-admin")
+@pytest.mark.skipif(
+    django.VERSION < (3, 0, 0) or PY2, reason="Older Django versions don't work with this use of django-admin"
+)
 def test_django_app_receives_request_finished_signal_when_app_is_ddwsgimiddleware():
     env = os.environ.copy()
     env.update(
