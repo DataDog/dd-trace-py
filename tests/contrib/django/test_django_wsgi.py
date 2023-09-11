72a564260a13f073afcc8e4b9ef59f9abcbc6173
Emmett Butler <emmett.butler321@gmail.com>
2023-09-11 14:09:55 -0700

    reason

--- tests/contrib/django/test_django_wsgi.py
+++ tests/contrib/django/test_django_wsgi.py
@@ -39,7 +39,7 @@ urlpatterns = [path("", handler)]
 app = DDWSGIMiddleware(get_wsgi_application(), app_is_iterator=True)
 
 
-@pytest.mark.skipif(django.VERSION < (3, 0, 0), reason="")
+@pytest.mark.skipif(django.VERSION < (3, 0, 0), reason="Older Django versions don't work with this use of django-admin")
 def test_django_app_receives_request_finished_signal_when_app_is_ddwsgimiddleware():
     env = os.environ.copy()
     env.update(
