55c7b844e78191299e110741b3bc58a42c957245
Kyle Verhoog <kyle@verhoog.ca>
2023-09-13 15:36:03 -0400

    fix(lib-injection): handle unsupported Python versions
    When the injection is applied to an unsupported Python interpreter the
    following error is received:
    
    ```
    ERROR:datadog.autoinstrumentation(pid: 28271): failed to load ddtrace module: No module named 'ddtrace'
    Error in sitecustomize; set PYTHONVERBOSE for traceback:
    ModuleNotFoundError: No module named 'ddtrace'
    ```
    
    The application still runs but this adds implementation-detail noise to
    the application logs.
    
    A regression test is added for an unsupported Python interpreter just to
    ensure that the application still runs.

---
+++ tests/lib-injection/dd-lib-python-init-test-django-unsupported-python/django_app.py
@@ -0,0 +1,30 @@
+import os
+
+from django.http import HttpResponse
+from django.urls import path
+
+
+filepath, extension = os.path.splitext(__file__)
+ROOT_URLCONF = os.path.basename(filepath)
+DEBUG = False
+SECRET_KEY = "fdsfdasfa"
+ALLOWED_HOSTS = ["*"]
+
+
+def index(request):
+    import ddtrace
+
+    if ddtrace.__version__ != "1.12.0":
+        print(
+            "Assertion failure: unexpected ddtrace version received. Got %r when expecting '1.12.0'"
+            % ddtrace.__version__
+        )
+        # Hard exit so traces aren't flushed and the test will fail.
+        os._exit(1)
+
+    return HttpResponse("test")
+
+
+urlpatterns = [
+    path("", index),
+]
