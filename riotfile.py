60ffb57b8b246afebe79bc364971a414fdf0d634
Eyal Halpern Shalev <eyal.shalev@granulate.io>
2023-09-14 23:38:23 +0300

    clickhouse-driver ddtrace integration

--- riotfile.py
+++ riotfile.py
@@ -1142,6 +1142,16 @@ venv = Venv(
             ],
         ),
         Venv(
+            name="clickhouse-driver",
+            command="pytest {cmdargs} tests/contrib/clickhouse_driver",
+            venvs=[
+                Venv(
+                    pys=select_pys(min_version="3.7", max_version="3.11"),
+                    pkgs={"clickhouse-driver": ["==0.2.6", latest]},
+                ),
+            ],
+        ),
+        Venv(
             name="psycopg2",
             command="pytest {cmdargs} tests/contrib/psycopg2",
             venvs=[
