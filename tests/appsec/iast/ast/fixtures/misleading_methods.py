d22a7b031f8066a00d6347c179a55a76dfbe56f9
Federico Mon <federico.mon@datadoghq.com>
2023-09-20 18:35:17 +0200

    chore(iast): wip tests

--- tests/appsec/iast/ast/fixtures/misleading_methods.py
+++ tests/appsec/iast/ast/fixtures/misleading_methods.py
@@ -2,8 +2,23 @@
 
 
 class FakeStr(str):
-    def join(self, *args):
-        return args[0] + args[1]
+    def format_map(self, *args):
+        # This string is not propagating so it should not be tainted
+        return "not_tainted"
 
-    def call_join(self, *args):
+    def call_format_map(self, _, *args):
+        return self.format_map(*args)
+
+    def ljust(self, *args):
+        # This string is not propagating so it should not be tainted
+        return "not_tainted"
+
+    def call_ljust(self, candidate_text, *args):
+        return self.ljust(10, args[0], candidate_text)
+
+    def join(self, *args):
+        # This string is not propagating so it should not be tainted
+        return "not_tainted"
+
+    def call_join(self, _, *args):
         return self.join(*args)
