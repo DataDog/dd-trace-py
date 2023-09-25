ba04debaa9262c3b30715d6f59dd324e46dc4dd1
Alberto Vara <alberto.vara@datadoghq.com>
2023-09-25 17:20:29 +0200

    fix(iast): add aspect ranges error (#7024)
    Fix errors on `aspect_add` when:
    ```python
    a = b + b
    get_ranges(a) # ranges from b, not b + b
    ```
    
    ```python
    a = b + b
    a = b + b
    get_ranges(a) # empty ranges
    ```
    
    ## Checklist
    
    - [x] Change(s) are motivated and described in the PR description.
    - [x] Testing strategy is described if automated tests are not included
    in the PR.
    - [x] Risk is outlined (performance impact, potential for breakage,
    maintainability, etc).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] [Library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    are followed. If no release note is required, add label
    `changelog/no-changelog`.
    - [x] Documentation is included (in-code, generated user docs, [public
    corp docs](https://github.com/DataDog/documentation/)).
    - [x] Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    
    - [x] Title is accurate.
    - [x] No unnecessary changes are introduced.
    - [x] Description motivates each change.
    - [x] Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes unless absolutely necessary.
    - [x] Testing strategy adequately addresses listed risk(s).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] Release note makes sense to a user of the library.
    - [x] Reviewer has explicitly acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment.
    - [x] Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    - [x] If this PR touches code that signs or publishes builds or
    packages, or handles credentials of any kind, I've requested a review
    from `@DataDog/security-design-and-guidance`.
    - [x] This PR doesn't touch any of that.

--- ddtrace/appsec/_iast/_taint_tracking/Initializer/Initializer.h
+++ ddtrace/appsec/_iast/_taint_tracking/Initializer/Initializer.h
@@ -25,66 +25,125 @@ class Initializer
     unordered_set<TaintRangeMapType*> active_map_addreses;
 
   public:
+    /**
+     * Constructor for the Initializer class.
+     */
     Initializer();
 
+    /**
+     * Creates a new taint range map.
+     *
+     * @return A pointer to the created taint range map.
+     */
     TaintRangeMapType* create_tainting_map();
 
+    /**
+     * Frees a taint range map.
+     *
+     * @param tx_map The taint range map to be freed.
+     */
     void free_tainting_map(TaintRangeMapType* tx_map);
 
+    /**
+     * Gets the current taint range map.
+     *
+     * @return A pointer to the current taint range map.
+     */
     static TaintRangeMapType* get_tainting_map();
 
+    /**
+     * Clears all active taint maps.
+     */
     void clear_tainting_maps();
 
+    /**
+     * Gets the number of tainted objects.
+     *
+     * @return The number of tainted objects.
+     */
     static int num_objects_tainted();
 
+    /**
+     * Gets the size of the Initializer object.
+     *
+     * @return The size of the Initializer object.
+     */
     int initializer_size();
 
+    /**
+     * Gets the size of active map addresses.
+     *
+     * @return The size of active map addresses.
+     */
     int active_map_addreses_size();
 
+    /**
+     * Creates a new taint tracking context.
+     */
     void create_context();
 
+    /**
+     * Destroys the current taint tracking context.
+     */
     void destroy_context();
 
+    /**
+     * Resets the current taint tracking context.
+     */
     void reset_context();
 
+    /**
+     * Gets the ID of the current taint tracking context.
+     *
+     * @return The ID of the current taint tracking context.
+     */
     static size_t context_id();
 
-    // FIXME: these should be static functions of TaintedObject
+    /**
+     * Allocates a new tainted object.
 
-    // IMPORTANT: if the returned object is not assigned to the map, you have
-    // responsibility of calling release_tainted_object on it or you'll have a
-    // leak.
+     * IMPORTANT: if the returned object is not assigned to the map, you have responsibility of calling
+     * release_tainted_object on it or you'll have a leak.
+     *
+     * IMPORTANT2: allocate_ranges_into_taint_object moves the owner of the ranges, if you know the ranges of the
+     original
+     * tainted object should be used more times, use instead allocate_ranges_into_taint_object_copy.
+     *
+     * @return A pointer to the allocated tainted object.
+     */
     TaintedObjectPtr allocate_tainted_object();
 
-    TaintedObjectPtr allocate_tainted_object(TaintRangeRefs ranges)
-    {
-        auto toptr = allocate_tainted_object();
-        toptr->set_values(std::move(ranges));
-        return toptr;
-    }
+    /**
+     * Allocates a new tainted object as a copy of an existing object.
+     *
+     * @param from The existing tainted object to copy.
+     * @return A pointer to the allocated tainted object.
+     */
+    TaintedObjectPtr allocate_tainted_object(TaintedObjectPtr from);
 
-    TaintedObjectPtr allocate_tainted_object_copy(const TaintRangeRefs& ranges)
-    {
-        auto toptr = allocate_tainted_object();
-        toptr->copy_values(ranges);
-        return toptr;
-    }
+    /**
+     * Allocates taint ranges into new tainted object.
+     *
+     * @param ranges The taint ranges to assign to the allocated object.
+     * @return A pointer to the allocated tainted object.
+     */
+    TaintedObjectPtr allocate_ranges_into_taint_object(TaintRangeRefs ranges);
 
-    TaintedObjectPtr allocate_tainted_object(TaintedObjectPtr from)
-    {
-        if (!from) {
-            return allocate_tainted_object();
-        }
-        return allocate_tainted_object(std::move(from->ranges_));
-    }
+    /**
+     * Allocates and copy taint ranges into new tainted object.
+     *
+     * @param ranges The taint ranges to assign to the allocated object.
+     * @return A pointer to the allocated tainted object.
+     */
+    TaintedObjectPtr allocate_ranges_into_taint_object_copy(const TaintRangeRefs& ranges);
 
-    TaintedObjectPtr allocate_tainted_object_copy(const TaintedObjectPtr& from)
-    {
-        if (!from) {
-            return allocate_tainted_object();
-        }
-        return allocate_tainted_object_copy(from->ranges_);
-    }
+    /**
+     * Allocates and copy taint ranges from a Tainted Object into new tainted object.
+     *
+     * @param from The existing tainted object to copy.
+     * @return A pointer to the allocated tainted object.
+     */
+    TaintedObjectPtr allocate_tainted_object_copy(const TaintedObjectPtr& from);
 
     void release_tainted_object(TaintedObjectPtr tobj);
 
