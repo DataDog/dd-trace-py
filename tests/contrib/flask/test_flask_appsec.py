26d3041c7876e3a6932dc01a3d733f1aea0d60e2
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-09-26 11:06:36 +0200

    test update

--- tests/contrib/flask/test_flask_appsec.py
+++ tests/contrib/flask/test_flask_appsec.py
@@ -940,4 +940,4 @@ class FlaskAppSecTestCase(BaseFlaskTestCase):
         resp = self.client.get("/new_service/awesome_test")
         assert resp.status_code == 200
         assert get_response_body(resp) == "Ok awesome_test"
-        assert "awesome_test" in ddtrace.config.get_extra_services()
+        assert "awesome_test" in ddtrace.config._get_extra_services()
