e952c3df038a9f51e5584e38ac0336a645a97b5b
Alberto Vara <alberto.vara@datadoghq.com>
2023-09-26 14:29:09 +0200

    fix codestyle

--- tests/appsec/iast/aspects/test_index_aspect_fixtures.py
+++ tests/appsec/iast/aspects/test_index_aspect_fixtures.py
@@ -1,12 +1,14 @@
 # -*- encoding: utf-8 -*-
 import pytest
 
-from ddtrace.appsec._iast._taint_tracking import OriginType
-from ddtrace.appsec._iast._taint_tracking import get_tainted_ranges
-from ddtrace.appsec._iast._taint_tracking import taint_pyobject
 from ddtrace.appsec._iast._utils import _is_python_version_supported as python_supported_by_iast
 from tests.appsec.iast.aspects.conftest import _iast_patched_module
 
+
+if python_supported_by_iast():
+    from ddtrace.appsec._iast._taint_tracking import OriginType
+    from ddtrace.appsec._iast._taint_tracking import get_tainted_ranges
+    from ddtrace.appsec._iast._taint_tracking import taint_pyobject
 
 mod = _iast_patched_module("tests.appsec.iast.fixtures.aspects.str_methods")
 
