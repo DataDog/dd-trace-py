d2eda00b40d27f485aa01eb10ca384e7d2b71a44
Alberto Vara <alberto.vara@datadoghq.com>
2023-09-28 16:11:39 +0200

    feat(iast): slice propagation

--- ddtrace/appsec/_iast/_taint_tracking/Aspects/AspectSlice.h
+++ ddtrace/appsec/_iast/_taint_tracking/Aspects/AspectSlice.h
@@ -3,4 +3,4 @@
 #include "TaintedOps/TaintedOps.h"
 
 PyObject*
-        api_slice_aspect(PyObject* self, PyObject* const* args, Py_ssize_t nargs);
+api_slice_aspect(PyObject* self, PyObject* const* args, Py_ssize_t nargs);
