30c3f49ceab31cd29a728f2b7e9005420b711e5a
Hoki Tam <hoki.tam@datadoghq.com>
2023-09-29 10:39:22 -0400

    Fix agent URL issue with DSM

--- ddtrace/internal/datastreams/__init__.py
+++ ddtrace/internal/datastreams/__init__.py
@@ -1,3 +1,5 @@
+from ddtrace.internal.agent import get_trace_url
+
 from . import kafka  # noqa:F401
 
 
@@ -9,6 +11,6 @@ def data_streams_processor():
 
     global _processor
     if not _processor:
-        _processor = processor.DataStreamsProcessor()
+        _processor = processor.DataStreamsProcessor(agent_url=get_trace_url())
 
     return _processor
