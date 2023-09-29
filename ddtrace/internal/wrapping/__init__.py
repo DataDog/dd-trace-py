0bf839da9e20306981359022c5ad4961ce708d9b
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2023-09-29 12:19:43 +0100

    chore: introduce assembly language support
    We make a simple assembly language parser as a higher-level API on
    top of the bytecode package to allow writing bytecode in a more
    readable way.

--- ddtrace/internal/wrapping/__init__.py
+++ ddtrace/internal/wrapping/__init__.py
@@ -14,10 +14,9 @@ try:
 except ImportError:
     from typing_extensions import Protocol  # type: ignore[assignment]
 
-from bytecode import Bytecode
-from bytecode import CompilerFlags
-from bytecode import Instr
+import bytecode as bc
 
+from ddtrace.internal.assembly import Assembly
 from ddtrace.internal.compat import PYTHON_VERSION_INFO as PY
 from ddtrace.internal.wrapping.asyncs import wrap_async
 from ddtrace.internal.wrapping.generators import wrap_generator
@@ -38,67 +37,78 @@ Wrapper = Callable[[FunctionType, Tuple[Any], Dict[str, Any]], Any]
 
 def _add(lineno):
     if PY >= (3, 11):
-        from bytecode import BinaryOp
+        return bc.Instr("BINARY_OP", bc.BinaryOp.ADD, lineno=lineno)
 
-        return Instr("BINARY_OP", BinaryOp.ADD, lineno=lineno)
-
-    return Instr("INPLACE_ADD", lineno=lineno)
+    return bc.Instr("INPLACE_ADD", lineno=lineno)
 
 
-def _update_map_block(varkwargsname, lineno):
-    if PY >= (3, 12):
-        return [
-            Instr("COPY", 1, lineno=lineno),
-            Instr("LOAD_ATTR", (True, "update"), lineno=lineno),
-            Instr("LOAD_FAST", varkwargsname, lineno=lineno),
-            Instr("CALL", 1, lineno=lineno),
-            Instr("POP_TOP", lineno=lineno),
-        ]
+UPDATE_MAP = Assembly()
+if PY >= (3, 12):
+    UPDATE_MAP.parse(
+        r"""
+            copy                1
+            load_method         $update
+            load_fast           {varkwargsname}
+            call                1
+            pop_top
+        """
+    )
 
-    if PY >= (3, 11):
-        return [
-            Instr("COPY", 1, lineno=lineno),
-            Instr("LOAD_METHOD", "update", lineno=lineno),
-            Instr("LOAD_FAST", varkwargsname, lineno=lineno),
-            Instr("PRECALL", 1, lineno=lineno),
-            Instr("CALL", 1, lineno=lineno),
-            Instr("POP_TOP", lineno=lineno),
-        ]
+elif PY >= (3, 11):
+    UPDATE_MAP.parse(
+        r"""
+            copy                1
+            load_method         $update
+            load_fast           {varkwargsname}
+            precall             1
+            call                1
+            pop_top
+        """
+    )
+else:
+    UPDATE_MAP.parse(
+        r"""
+            dup_top
+            load_attr           $update
+            load_fast           {varkwargsname}
+            call_function       1
+            pop_top
+        """
+    )
 
-    return [
-        Instr("DUP_TOP", lineno=lineno),
-        Instr("LOAD_ATTR", "update", lineno=lineno),
-        Instr("LOAD_FAST", varkwargsname, lineno=lineno),
-        Instr("CALL_FUNCTION", 1, lineno=lineno),
-        Instr("POP_TOP", lineno=lineno),
-    ]
 
+CALL_RETURN = Assembly()
+if PY >= (3, 12):
+    CALL_RETURN.parse(
+        r"""
+            call                {arg}
+            return_value
+        """
+    )
 
-def _call_return_block(arg, lineno):
-    if PY >= (3, 12):
-        return [
-            Instr("CALL", arg, lineno=lineno),
-            Instr("RETURN_VALUE", lineno=lineno),
-        ]
+elif PY >= (3, 11):
+    CALL_RETURN.parse(
+        r"""
+            precall             {arg}
+            call                {arg}
+            return_value
+        """
+    )
 
-    if PY >= (3, 11):
-        return [
-            Instr("PRECALL", arg, lineno=lineno),
-            Instr("CALL", arg, lineno=lineno),
-            Instr("RETURN_VALUE", lineno=lineno),
-        ]
-
-    return [
-        Instr("CALL_FUNCTION", arg, lineno=lineno),
-        Instr("RETURN_VALUE", lineno=lineno),
-    ]
+else:
+    CALL_RETURN.parse(
+        r"""
+            call_function       {arg}
+            return_value
+        """
+    )
 
 
 FIRSTLINENO_OFFSET = int(PY >= (3, 11))
 
 
 def wrap_bytecode(wrapper, wrapped):
-    # type: (Wrapper, FunctionType) -> Bytecode
+    # type: (Wrapper, FunctionType) -> bc.Bytecode
     """Wrap a function with a wrapper function.
 
     The wrapper function expects the wrapped function as the first argument,
@@ -111,8 +121,8 @@ def wrap_bytecode(wrapper, wrapped):
 
     code = wrapped.__code__
     lineno = code.co_firstlineno + FIRSTLINENO_OFFSET
-    varargs = bool(code.co_flags & CompilerFlags.VARARGS)
-    varkwargs = bool(code.co_flags & CompilerFlags.VARKEYWORDS)
+    varargs = bool(code.co_flags & bc.CompilerFlags.VARARGS)
+    varkwargs = bool(code.co_flags & bc.CompilerFlags.VARKEYWORDS)
     nargs = code.co_argcount
     argnames = code.co_varnames[:nargs]
     try:
@@ -126,78 +136,76 @@ def wrap_bytecode(wrapper, wrapped):
     # Push the wrapper function that is to be called and the wrapped function to
     # be passed as first argument.
     instrs = [
-        Instr("LOAD_CONST", wrapper, lineno=lineno),
-        Instr("LOAD_CONST", wrapped, lineno=lineno),
+        bc.Instr("LOAD_CONST", wrapper, lineno=lineno),
+        bc.Instr("LOAD_CONST", wrapped, lineno=lineno),
     ]
     if PY >= (3, 11):
         # From insert_prefix_instructions
         instrs[0:0] = [
-            Instr("RESUME", 0, lineno=lineno - 1),
-            Instr("PUSH_NULL", lineno=lineno),
+            bc.Instr("RESUME", 0, lineno=lineno - 1),
+            bc.Instr("PUSH_NULL", lineno=lineno),
         ]
 
         if code.co_cellvars:
-            from bytecode import CellVar
-
-            instrs[0:0] = [Instr("MAKE_CELL", CellVar(_), lineno=lineno) for _ in code.co_cellvars]
+            instrs[0:0] = [bc.Instr("MAKE_CELL", bc.CellVar(_), lineno=lineno) for _ in code.co_cellvars]
 
         if code.co_freevars:
-            instrs.insert(0, Instr("COPY_FREE_VARS", len(code.co_freevars), lineno=lineno))
+            instrs.insert(0, bc.Instr("COPY_FREE_VARS", len(code.co_freevars), lineno=lineno))
 
     # Build the tuple of all the positional arguments
     if nargs:
         instrs.extend(
             [
-                Instr("LOAD_DEREF", CellVar(argname), lineno=lineno)
+                bc.Instr("LOAD_DEREF", bc.CellVar(argname), lineno=lineno)
                 if PY >= (3, 11) and argname in code.co_cellvars
-                else Instr("LOAD_FAST", argname, lineno=lineno)
+                else bc.Instr("LOAD_FAST", argname, lineno=lineno)
                 for argname in argnames
             ]
         )
-        instrs.append(Instr("BUILD_TUPLE", nargs, lineno=lineno))
+        instrs.append(bc.Instr("BUILD_TUPLE", nargs, lineno=lineno))
         if varargs:
             instrs.extend(
                 [
-                    Instr("LOAD_FAST", varargsname, lineno=lineno),
+                    bc.Instr("LOAD_FAST", varargsname, lineno=lineno),
                     _add(lineno),
                 ]
             )
     elif varargs:
-        instrs.append(Instr("LOAD_FAST", varargsname, lineno=lineno))
+        instrs.append(bc.Instr("LOAD_FAST", varargsname, lineno=lineno))
     else:
-        instrs.append(Instr("BUILD_TUPLE", 0, lineno=lineno))
+        instrs.append(bc.Instr("BUILD_TUPLE", 0, lineno=lineno))
 
     # Prepare the keyword arguments
     if kwonlyargs:
         for arg in kwonlyargnames:
             instrs.extend(
                 [
-                    Instr("LOAD_CONST", arg, lineno=lineno),
-                    Instr("LOAD_FAST", arg, lineno=lineno),
+                    bc.Instr("LOAD_CONST", arg, lineno=lineno),
+                    bc.Instr("LOAD_FAST", arg, lineno=lineno),
                 ]
             )
-        instrs.append(Instr("BUILD_MAP", kwonlyargs, lineno=lineno))
+        instrs.append(bc.Instr("BUILD_MAP", kwonlyargs, lineno=lineno))
         if varkwargs:
-            instrs.extend(_update_map_block(varkwargsname, lineno))
+            instrs.extend(UPDATE_MAP.bind({"varkwargsname": varkwargsname}, lineno=lineno))
 
     elif varkwargs:
-        instrs.append(Instr("LOAD_FAST", varkwargsname, lineno=lineno))
+        instrs.append(bc.Instr("LOAD_FAST", varkwargsname, lineno=lineno))
 
     else:
-        instrs.append(Instr("BUILD_MAP", 0, lineno=lineno))
+        instrs.append(bc.Instr("BUILD_MAP", 0, lineno=lineno))
 
     # Call the wrapper function with the wrapped function, the positional and
     # keyword arguments, and return the result.
-    instrs.extend(_call_return_block(3, lineno))
+    instrs.extend(CALL_RETURN.bind({"arg": 3}, lineno=lineno))
 
     # If the function has special flags set, like the generator, async generator
     # or coroutine, inject unraveling code before the return opcode.
-    if CompilerFlags.GENERATOR & code.co_flags and not (CompilerFlags.COROUTINE & code.co_flags):
+    if bc.CompilerFlags.GENERATOR & code.co_flags and not (bc.CompilerFlags.COROUTINE & code.co_flags):
         wrap_generator(instrs, code, lineno)
     elif PY3:
         wrap_async(instrs, code, lineno)
 
-    return Bytecode(instrs)
+    return bc.Bytecode(instrs)
 
 
 def wrap(f, wrapper):
@@ -245,7 +253,7 @@ def wrap(f, wrapper):
         nargs += code.kwonlyargcount
     except AttributeError:
         pass
-    nargs += bool(code.flags & CompilerFlags.VARARGS) + bool(code.flags & CompilerFlags.VARKEYWORDS)
+    nargs += bool(code.flags & bc.CompilerFlags.VARARGS) + bool(code.flags & bc.CompilerFlags.VARKEYWORDS)
     code.argnames = f.__code__.co_varnames[:nargs]
 
     f.__code__ = code.to_code()
