397bf7488139b6996176f06b8ad9a32a48e40ccb
Emmett Butler <emmett.butler321@gmail.com>
2023-10-02 14:16:09 -0700

    test user

--- tests/contrib/pyodbc/test_pyodbc.py
+++ tests/contrib/pyodbc/test_pyodbc.py
@@ -283,6 +283,7 @@ class TestPyODBCPatch(PyODBCTest, TracerTestCase):
             assert span.name == "pyodbc.query"
             assert span.span_type == "sql"
             assert span.get_tag("db.system") == "pyodbc"
+            assert span.get_tag("db.user") == ""
             assert span.error == 0
         finally:
             unpatch()
