de24258860341484f7d2017ac9db58f742fcab1d
Federico Mon <federico.mon@datadoghq.com>
2023-10-02 15:48:09 +0200

    chore(iast): fix imports in tests

--- tests/appsec/iast/aspects/test_replace_aspect.py
+++ tests/appsec/iast/aspects/test_replace_aspect.py
@@ -4,19 +4,15 @@ import sys
 
 import pytest
 
-# try:
-#     from ddtrace.appsec.iast._taint_tracking import OriginType
-#     from ddtrace.appsec.iast._taint_tracking import get_tainted_ranges
-#     from ddtrace.appsec.iast._taint_tracking import is_pyobject_tainted
-#     from ddtrace.appsec.iast._taint_tracking import taint_pyobject
-#     import ddtrace.appsec.iast._taint_tracking.aspects as ddtrace_aspects
-# except (ImportError, AttributeError):
-#     pytest.skip("IAST not supported for this Python version", allow_module_level=True)
-from ddtrace.appsec.iast._taint_tracking import OriginType
-from ddtrace.appsec.iast._taint_tracking import get_tainted_ranges
-from ddtrace.appsec.iast._taint_tracking import is_pyobject_tainted
-from ddtrace.appsec.iast._taint_tracking import taint_pyobject
-import ddtrace.appsec.iast._taint_tracking.aspects as ddtrace_aspects
+
+try:
+    from ddtrace.appsec._iast._taint_tracking import OriginType
+    from ddtrace.appsec._iast._taint_tracking import get_tainted_ranges
+    from ddtrace.appsec._iast._taint_tracking import is_pyobject_tainted
+    from ddtrace.appsec._iast._taint_tracking import taint_pyobject
+    import ddtrace.appsec._iast._taint_tracking.aspects as ddtrace_aspects
+except (ImportError, AttributeError):
+    pytest.skip("IAST not supported for this Python version", allow_module_level=True)
 
 
 @pytest.mark.parametrize(
