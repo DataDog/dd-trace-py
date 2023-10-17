d1d6b43493843ce507788d60e21ecc67d0bc42bd
Emmett Butler <emmett.butler321@gmail.com>
2023-10-17 15:43:19 +0200

    try the mark

--- tests/contrib/snowflake/test_snowflake.py
+++ tests/contrib/snowflake/test_snowflake.py
@@ -11,6 +11,7 @@ from ddtrace import tracer
 from ddtrace.contrib.snowflake import patch
 from ddtrace.contrib.snowflake import unpatch
 from tests.opentracer.utils import init_tracer
+from tests.utils import flaky
 from tests.utils import override_config
 from tests.utils import snapshot
 
@@ -175,6 +176,7 @@ def test_snowflake_service_env():
                 assert cur.fetchone() == ("4.30.2",)
 
 
+@flaky()
 @snapshot()
 @req_mock.activate
 def test_snowflake_pin_override(client):
