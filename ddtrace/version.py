44740487bab9a1e0fea8305ad5dad99200139d7f
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-10-18 14:19:21 +0200

    first iteration

--- ddtrace/version.py
+++ ddtrace/version.py
@@ -1,16 +1,20 @@
+MAJOR = 2
+MINOR = 2
+PATCH = 0
+
+# must be empty for regular
+# LABELS = ""
+
+# use rcN for release candidate
+# LABELS = "rc1"
+
+# use pre.dev0 for trunk branch
+LABELS = "pre.dev0"
+
+
+VERSION = f"{MAJOR}.{MINOR}.{PATCH}{LABELS}"
+
+
 def get_version():
     # type: () -> str
-    try:
-        from ._version import version
-
-        return version
-    except ImportError:
-        import pkg_resources
-
-        try:
-            # something went wrong while creating _version.py, let's fallback to pkg_resources
-
-            return pkg_resources.get_distribution(__name__).version
-        except pkg_resources.DistributionNotFound:
-            # package is not installed
-            return "dev"
+    return VERSION
