c007af02b3ee26e8d30885866b84abdb92f9ed1f
ZStriker19 <zach.groves@datadoghq.com>
2023-10-18 15:41:45 +0200

    remove prints

--- tests/contrib/httplib/test_httplib_distributed.py
+++ tests/contrib/httplib/test_httplib_distributed.py
@@ -31,8 +31,6 @@ class TestHTTPLibDistributed(HTTPLibBaseMixin, TracerTestCase):
         # we need to pull the trace_id values out of the response.message str
         x_datadog_trace_id = re.search(r"x-datadog-trace-id: (.*?)(\n|\r)", httplib_request_str).group(1)
         _dd_p_tid = re.search(r"_dd.p.tid=(.*?)(;|\n|$|\r)", httplib_request_str).group(1)
-        print("come here")
-        print({"x-datadog-trace-id": x_datadog_trace_id, "x-datadog-tags": f"_dd.p.tid={_dd_p_tid}"})
         header_t_id = get_128_bit_trace_id_from_headers(
             {"x-datadog-trace-id": x_datadog_trace_id, "x-datadog-tags": f"_dd.p.tid={_dd_p_tid}"}
         )
