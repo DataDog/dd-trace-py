18afda39b2874456542fd1fad30a16ea2fd2b86a
Alberto Vara <alberto.vara@datadoghq.com>
2023-10-20 14:43:34 +0200

    chore(ci): moto package drops and delete all versions <= 1.0 (#7302)
    Dropping some tests that are using `moto` because that package drops all
    version older than 1.0 (https://pypi.org/project/moto/#history). With
    that, this PR fix CI error on backports from 1.17 and olders
    
    - [x] Change(s) are motivated and described in the PR description.
    - [x] Testing strategy is described if automated tests are not included
    in the PR.
    - [x] Risk is outlined (performance impact, potential for breakage,
    maintainability, etc).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] [Library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    are followed. If no release note is required, add label
    `changelog/no-changelog`.
    - [x] Documentation is included (in-code, generated user docs, [public
    corp docs](https://github.com/DataDog/documentation/)).
    - [x] Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    - [x] Title is accurate.
    - [x] No unnecessary changes are introduced.
    - [x] Description motivates each change.
    - [x] Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes unless absolutely necessary.
    - [x] Testing strategy adequately addresses listed risk(s).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] Release note makes sense to a user of the library.
    - [x] Reviewer has explicitly acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment.
    - [x] Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    - [x] If this PR touches code that signs or publishes builds or
    packages, or handles credentials of any kind, I've requested a review
    from `@DataDog/security-design-and-guidance`.
    - [x] This PR doesn't touch any of that.
    
    (cherry picked from commit 9e62cc35029b5ce3aaf848f3c3fbd53ab6337b27)

--- riotfile.py
+++ riotfile.py
@@ -1298,15 +1298,6 @@ venv = Venv(
             command="pytest {cmdargs} tests/contrib/pynamodb",
             venvs=[
                 Venv(
-                    # pynamodb dropped support for Python 2.7/3.5 in 4.4
-                    pys=select_pys(max_version="3.5"),
-                    pkgs={
-                        "pynamodb": ["~=4.3.0"],
-                        "moto": ">=0.0,<1.0",
-                        "rsa": "<4.7.1",
-                    },
-                ),
-                Venv(
                     pys=select_pys(min_version="3.6"),
                     pkgs={
                         "pynamodb": ["~=5.0", "~=5.3", latest],
@@ -1465,24 +1456,10 @@ venv = Venv(
             ],
         ),
         Venv(
-            name="boto",
-            command="pytest {cmdargs} tests/contrib/boto",
-            venvs=[Venv(pys=select_pys(max_version="3.6"), pkgs={"boto": latest, "moto": "<1.0.0"})],
-        ),
-        Venv(
             name="botocore",
             command="pytest {cmdargs} tests/contrib/botocore",
             venvs=[
                 Venv(pys=select_pys(min_version="3.8"), pkgs={"moto[all]": latest, "botocore": latest}),
-                Venv(
-                    pys=["2.7"],
-                    pkgs={
-                        "moto": "~=1.0",
-                        "botocore": "~=1.20.0",
-                        "python-jose[cryptography]": "==3.1.0",
-                        "rsa": "<4.7.1",
-                    },
-                ),
                 Venv(
                     pkgs={
                         "cffi": "==1.14.0",
@@ -1491,12 +1468,6 @@ venv = Venv(
                         "python-jose[cryptography]": "==3.1.0",
                     },
                     venvs=[
-                        Venv(
-                            pys=["3.5"],
-                            pkgs={
-                                "moto[all]": "~=1.0",
-                            },
-                        ),
                         Venv(
                             pys=["3.6"],
                             pkgs={
