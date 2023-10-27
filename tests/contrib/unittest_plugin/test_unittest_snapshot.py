a40f02e1a3a9c89e2dd31855b6ccc12e2a400ffd
eric.navarro <eric.navarro@datadoghq.com>
2023-10-27 17:04:41 +0200

    add custom tests test

--- tests/contrib/unittest_plugin/test_unittest_snapshot.py
+++ tests/contrib/unittest_plugin/test_unittest_snapshot.py
@@ -358,3 +358,48 @@ class UnittestSnapshotTestCase(TracerTestCase):
         self.testdir.chdir()
         with override_env(dict(DD_API_KEY="foobar.baz", DD_CIVISIBILITY_ITR_ENABLED="1")):
             subprocess.run(["ddtrace-run", "python", "-m", "unittest"])
+
+    @snapshot(ignores=SNAPSHOT_IGNORES)
+    def test_unittest_will_include_custom_tests(self):
+        ret_false = """
+        def ret_false():
+            return False
+        """
+        self.testdir.makepyfile(ret_false=ret_false)
+        lib_fn = """
+        def lib_fn():
+            return True
+        """
+        self.testdir.makepyfile(lib_fn=lib_fn)
+        test_my_coverage = """
+        import unittest
+        import ddtrace
+        from ddtrace.internal.ci_visibility import CIVisibility as _CIVisibility
+        from unittest import mock
+        from unittest.mock import Mock
+        ddtrace.internal.ci_visibility.recorder.CIVisibility._check_enabled_features = Mock(
+            return_value=(True, True)
+        )
+        ddtrace.internal.ci_visibility.recorder.CIVisibility._fetch_tests_to_skip = Mock()
+        ddtrace.internal.ci_visibility.recorder.CIVisibility._tests_to_skip = {
+            "test_my_coverage/CoverageTestCase": ["test_cov"],
+        }
+        class CoverageTestCase(unittest.TestCase):
+            @unittest.skipIf(False, reason="datadog_itr_unskippable")
+            def test_cov(self):
+                from lib_fn import lib_fn
+                assert lib_fn()
+            def test_second(self):
+                from ret_false import ret_false
+                assert not ret_false()
+        """
+        self.testdir.makepyfile(test_my_coverage=test_my_coverage)
+        self.testdir.chdir()
+        with override_env(
+            dict(
+                DD_API_KEY="foobar.baz",
+                DD_CIVISIBILITY_ITR_ENABLED="1",
+                DD_TAGS="test.configuration.custom_key:some_value",
+            )
+        ):
+            subprocess.run(["ddtrace-run", "python", "-m", "unittest"])
