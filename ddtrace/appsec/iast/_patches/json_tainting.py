d02c468d43092e996ed15baa07eb7246d6d50f81
Federico Mon <federico.mon@datadoghq.com>
2023-11-01 10:48:50 +0100

    fix(iast): patch simplejson.encoder to handle LazyTaintDict and LazyTaintList properly (#7428)
    IAST: SimpleJSON encoder should handle LazyTaintDict and LazyTaintList
    by encoding their inner Dict and List, without this change, it would
    throw an exception because they are not serializable objects.
    
    - [x] Change(s) are motivated and described in the PR description.
    - [x] Testing strategy is described if automated tests are not included
    in the PR.
    - [x] Risk is outlined (performance impact, potential for breakage,
    maintainability, etc).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] [Library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    are followed. If no release note is required, add label
    `changelog/no-changelog`.
    - [x] Documentation is included (in-code, generated user docs, [public
    corp docs](https://github.com/DataDog/documentation/)).
    - [x] Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    - [x] Title is accurate.
    - [x] No unnecessary changes are introduced.
    - [x] Description motivates each change.
    - [x] Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes unless absolutely necessary.
    - [x] Testing strategy adequately addresses listed risk(s).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] Release note makes sense to a user of the library.
    - [x] Reviewer has explicitly acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment.
    - [x] Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    - [x] If this PR touches code that signs or publishes builds or
    packages, or handles credentials of any kind, I've requested a review
    from `@DataDog/security-design-and-guidance`.
    - [x] This PR doesn't touch any of that.
    
    ---------
    
    Co-authored-by: Alberto Vara <alberto.vara@datadoghq.com>
    (cherry picked from commit 306a8908107c33cc15e955d8fe773646d9870df8)

--- ddtrace/appsec/iast/_patches/json_tainting.py
+++ ddtrace/appsec/iast/_patches/json_tainting.py
@@ -18,6 +18,8 @@ def unpatch_iast():
     # type: () -> None
     set_module_unpatched("json", default_attr=_DEFAULT_ATTR)
     try_unwrap("json", "loads")
+    try_unwrap("json.encoder", "JSONEncoder.default")
+    try_unwrap("simplejson.encoder", "JSONEncoder.default")
 
 
 def patch():
@@ -27,6 +29,7 @@ def patch():
         return
     try_wrap_function_wrapper("json", "loads", wrapped_loads)
     try_wrap_function_wrapper("json.encoder", "JSONEncoder.default", patched_json_encoder_default)
+    try_wrap_function_wrapper("simplejson.encoder", "JSONEncoder.default", patched_json_encoder_default)
 
 
 def wrapped_loads(wrapped, instance, args, kwargs):
