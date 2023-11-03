d8d0c759a3c88e3153b9a46b5063a4abf934911b
Munir Abdinur <munir_abdinur@hotmail.com>
2023-11-03 15:23:46 -0400

    fix failing version tests

--- ddtrace/version.py
+++ ddtrace/version.py
@@ -6,15 +6,12 @@ def get_version():
         return version
     except ImportError:
         try:
-            # something went wrong while creating _version.py, let's fallback to importlib
-            from importlib.metadata import PackageNotFoundError
             from importlib.metadata import version as ilm_version
         except ImportError:
             # required for python3.7
-            from importlib_metadata import PackageNotFoundError  # type: ignore[no-redef]  # noqa
             from importlib_metadata import version as ilm_version  # type: ignore[no-redef]
         try:
-            return ilm_version(__name__)
-        except PackageNotFoundError:
+            return ilm_version("ddtrace")
+        except ModuleNotFoundError:
             # package is not installed
             return "dev"
