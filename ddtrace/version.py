4bc3421187ed96c760ea9d330d827f905d558a2a
Emmett Butler <emmett.butler321@gmail.com>
2023-11-03 09:40:05 -0700

    update get_version to use importlib_metadata

--- ddtrace/version.py
+++ ddtrace/version.py
@@ -5,5 +5,16 @@ def get_version():
 
         return version
     except ImportError:
-        # package is not installed
-        return "dev"
+        try:
+            # something went wrong while creating _version.py, let's fallback to importlib
+            from importlib.metadata import PackageNotFoundError
+            from importlib.metadata import version
+        except ImportError:
+            # required for python3.7
+            from importlib_metadata import PackageNotFoundError  # noqa
+            from importlib_metadata import version  # type: ignore[no-redef]
+        try:
+            version(__name__)
+        except PackageNotFoundError:
+            # package is not installed
+            return "dev"
