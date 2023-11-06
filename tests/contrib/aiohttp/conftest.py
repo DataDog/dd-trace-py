b006fe2985b9b3de47a995dd1e1a0a90e8f49e44
Emmett Butler <emmett.butler321@gmail.com>
2023-11-06 15:37:00 -0800

    style

--- tests/contrib/aiohttp/conftest.py
+++ tests/contrib/aiohttp/conftest.py
@@ -1,11 +1,12 @@
 import aiohttp
-
 import pytest
+
 from ddtrace.contrib.aiohttp.middlewares import trace_app
 from ddtrace.contrib.aiohttp.patch import unpatch
 from ddtrace.internal.utils import version
 
 from .app.web import setup_app
+
 
 @pytest.fixture
 async def app_tracer(tracer, loop):
@@ -13,11 +14,13 @@ async def app_tracer(tracer, loop):
     trace_app(app, tracer)
     return app, tracer
 
+
 @pytest.fixture
 async def patched_app_tracer(app_tracer):
     app, tracer = app_tracer
     yield app, tracer
     unpatch()
+
 
 @pytest.fixture
 async def untraced_app_tracer(tracer, loop):
