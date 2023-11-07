e0d92edf0a2326c5a04b0e64f3445977c11bdc08
ZStriker19 <zach.groves@datadoghq.com>
2023-11-07 13:40:42 -0500

    fix flask, logging, and telemetry tests

--- tests/contrib/logging/test_logging.py
+++ tests/contrib/logging/test_logging.py
@@ -102,7 +102,7 @@ class LoggingTestCase(TracerTestCase):
             else:
                 assert not isinstance(logging.StrFormatStyle.format, wrapt.BoundFunctionWrapper)
 
-    def _test_logging(self, create_span, service="", version="", env=""):
+    def _test_logging(self, create_span, service="", version="", env="", bit_128_logging_enabled=False):
         def func():
             span = create_span()
             logger.info("Hello!")
@@ -116,7 +116,7 @@ class LoggingTestCase(TracerTestCase):
             trace_id = 0
             span_id = 0
             if span:
-                trace_id = span.trace_id
+                trace_id = span._trace_id_64bits if not bit_128_logging_enabled else span.trace_id
                 span_id = span.span_id
 
             assert output.startswith(
@@ -164,7 +164,8 @@ class LoggingTestCase(TracerTestCase):
             return span
 
         with self.override_global_config(dict(version="v1.666", env="test")):
-            self._test_logging(create_span=create_span, version="v1.666", env="test")
+            self._test_logging(create_span=create_span, version="v1.666", env="test", bit_128_logging_enabled=True)
+            # makes sense that this fails because _test_logging looks for the 64 bit trace id
 
     @TracerTestCase.run_in_subprocess(
         env_overrides=dict(
@@ -267,7 +268,7 @@ class LoggingTestCase(TracerTestCase):
             lines = output.splitlines()
             assert (
                 "Hello! [dd.service= dd.env= dd.version= dd.trace_id={} dd.span_id={}]".format(
-                    span.trace_id, span.span_id
+                    span._trace_id_64bits, span.span_id
                 )
                 == lines[0]
             )
@@ -278,7 +279,7 @@ class LoggingTestCase(TracerTestCase):
                 lines = output.splitlines()
                 expected = (
                     "Hello! [dd.service=my.service dd.env=my.env dd.version=my.version dd.trace_id={} dd.span_id={}]"
-                ).format(span.trace_id, span.span_id)
+                ).format(span._trace_id_64bits, span.span_id)
                 assert expected == lines[0]
 
     @pytest.mark.skipif(six.PY2, reason="logging.StrFormatStyle does not exist on Python 2.7")
@@ -296,10 +297,10 @@ class LoggingTestCase(TracerTestCase):
                 record = logger.makeRecord("name", "INFO", "func", 534, "Manual log record", (), None)
                 log = formatter.format(record)
                 expected = "Manual log record [dd.service= dd.env= dd.version= dd.trace_id={} dd.span_id={}]".format(
-                    span.trace_id, span.span_id
+                    span._trace_id_64bits, span.span_id
                 )
                 assert log == expected
 
                 assert not hasattr(record, "dd")
-                assert getattr(record, RECORD_ATTR_TRACE_ID) == str(span.trace_id)
+                assert getattr(record, RECORD_ATTR_TRACE_ID) == str(span._trace_id_64bits)
                 assert getattr(record, RECORD_ATTR_SPAN_ID) == str(span.span_id)
