127e6b04814b92794077f4955f85c8c65da53e6a
Emmett Butler <emmett.butler321@gmail.com>
2023-11-07 09:24:24 -0800

    ddtrace.auto docstring

--- ddtrace/auto.py
+++ ddtrace/auto.py
@@ -1,1 +1,21 @@
+"""
+ddtrace.auto
+============
+
+Importing this module installs Datadog instrumentation in the runtime. It should be used
+when :ref:`ddtrace-run<ddtrace-run>` is not an option. Using it with :ref:`ddtrace-run<ddtrace-run>`
+is unsupported and may lead to undefined behavior.
+
+Example
+-------
+    # myapp.py
+
+    import ddtrace.auto  # install instrumentation as early as possible
+    import mystuff
+
+    def main():
+        pass
+
+    main()
+"""
 import ddtrace.bootstrap.sitecustomize  # noqa
