b7efbe77e1be4e19714fa22e59aea8e78db6188d
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2023-11-08 18:57:39 +0100

    Do a safer read on wsgi buffer
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- tests/appsec/integrations/test_handlers.py
+++ tests/appsec/integrations/test_handlers.py
@@ -100,7 +100,6 @@ def test_when_appsec_reads_empty_body_no_hang(appsec_enabled, tracer_enabled, se
         assert response.content == b"OK_test-body-hang"
 
 
-@pytest.mark.skip(reason="We're still finding a solution to this corner case. It hangs in CI")
 @pytest.mark.parametrize("appsec_enabled", ("true", "false"))
 @pytest.mark.parametrize("tracer_enabled", ("true", "false"))
 @pytest.mark.parametrize("server", ((gunicorn_server,)))
