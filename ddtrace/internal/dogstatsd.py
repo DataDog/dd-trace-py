76d7aaf6b385543fd3763ab65a9c1163e6bdd2b6
Emmett Butler <emmett.butler321@gmail.com>
2023-11-13 11:46:55 -0800

    lint

--- ddtrace/internal/dogstatsd.py
+++ ddtrace/internal/dogstatsd.py
@@ -6,9 +6,7 @@ from ddtrace.vendor.dogstatsd import DogStatsd
 from ddtrace.vendor.dogstatsd import base
 
 
-def get_dogstatsd_client(
-    url: str, namespace: Optional[str] = None, tags: Optional[List[str]] = None
-) -> DogStatsd:
+def get_dogstatsd_client(url: str, namespace: Optional[str] = None, tags: Optional[List[str]] = None) -> DogStatsd:
     # url can be either of the form `udp://<host>:<port>` or `unix://<path>`
     # also support without url scheme included
     if url.startswith("/"):
@@ -19,9 +17,7 @@ def get_dogstatsd_client(
     parsed = parse.urlparse(url)
 
     if parsed.scheme == "unix":
-        return DogStatsd(
-            socket_path=parsed.path, namespace=namespace, constant_tags=tags
-        )
+        return DogStatsd(socket_path=parsed.path, namespace=namespace, constant_tags=tags)
     elif parsed.scheme == "udp":
         return DogStatsd(
             host=parsed.hostname or "",
