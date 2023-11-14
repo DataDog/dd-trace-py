ac3a77776926eb1d2cb3a32b3ee258015064bf64
Brett Langdon <brett.langdon@datadoghq.com>
2023-11-14 16:11:08 -0500

    chore(core): make ddtrace-run and import ddtrace.run compatible with no side effects

--- tests/commands/test_runner.py
+++ tests/commands/test_runner.py
@@ -524,3 +524,45 @@ def test_ddtrace_re_module():
             (r"[\s,<>]", None),
         )
     )
+
+
+@pytest.mark.subprocess(ddtrace_run=True, err=None)
+def test_ddtrace_run_sitecustomize():
+    """When using ddtrace-run we ensure ddtrace.bootstrap.sitecustomize is in sys.module cache"""
+    import sys
+
+    assert "ddtrace.bootstrap.sitecustomize" in sys.modules
+
+    assert sys.modules["ddtrace.bootstrap.sitecustomize"].loaded
+
+
+@pytest.mark.subprocess(ddtrace_run=False, err=None)
+def test_ddtrace_auto_sitecustomize():
+    """When import ddtrace.auto we ensure ddtrace.bootstrap.sitecustomize is in sys.module cache"""
+    import sys
+
+    starting_modules = set(sys.modules.keys())
+
+    import ddtrace.auto  # noqa: F401
+
+    assert "ddtrace.bootstrap.sitecustomize" in sys.modules
+
+    assert sys.modules["ddtrace.bootstrap.sitecustomize"].loaded
+
+    # Compare the list of imported modules before/after ddtrace.auto to show it is a no-op with
+    # no additional modules imported / side-effects
+    final_modules = set(sys.modules.keys())
+    assert final_modules - starting_modules == set(["ddtrace.auto"])
+
+
+@pytest.mark.subprocess(ddtrace_run=True, err=None)
+def test_ddtrace_run_and_auto_sitecustomize():
+    """When using ddtrace-run and import ddtrace.auto we don't double import sitecustomize"""
+    assert sys.modules["ddtrace.bootstrap.sitecustomize"].loaded
+
+    # Setting this to false, and confirming that importing auto doesn't set it to True (sitecustomize code ran)
+    sys.modules["ddtrace.bootstrap.sitecustomize"].loaded = False
+
+    import ddtrace.auto  # noqa: F401
+
+    assert sys.modules["ddtrace.bootstrap.sitecustomize"].loaded is False
