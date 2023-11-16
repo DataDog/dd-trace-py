b4db9d258bae29a1a0c30acc6991a221a5431946
William Conti <william.conti@datadoghq.com>
2023-11-16 10:01:40 -0500

    fix according to reviewer comments

--- ddtrace/contrib/kafka/patch.py
+++ ddtrace/contrib/kafka/patch.py
@@ -20,6 +20,7 @@ from ddtrace.internal.schema import schematize_service_name
 from ddtrace.internal.schema.span_attribute_schema import SpanDirection
 from ddtrace.internal.utils import ArgumentError
 from ddtrace.internal.utils import get_argument_value
+from ddtrace.internal.utils import set_argument_value
 from ddtrace.internal.utils.formats import asbool
 from ddtrace.pin import Pin
 from ddtrace.propagation.http import HTTPPropagator as Propagator
@@ -165,16 +166,14 @@ def traced_produce(func, instance, args, kwargs):
         if rate is not None:
             span.set_tag(ANALYTICS_SAMPLE_RATE_KEY, rate)
 
+        # inject headers with Datadog tags if trace propagation is enabled
         if config.kafka.distributed_tracing_enabled:
             # inject headers with Datadog tags:
-            try:
-                Propagator.inject(span.context, kwargs["headers"])
-            except KeyError:
-                try:
-                    Propagator.inject(span.context, args[6])
-                except IndexError:
-                    kwargs["headers"] = {}
-                    Propagator.inject(span.context, kwargs["headers"])
+            headers = get_argument_value(args, kwargs, 6, "headers", True)
+            if headers is None:
+                headers = {}
+            Propagator.inject(span.context, headers)
+            args, kwargs = set_argument_value(args, kwargs, 6, "headers", headers)
         return func(*args, **kwargs)
 
 
