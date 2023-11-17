a5d1c04dd58d59ca1b8a4bed308014f04400802d
Yun Kim <yun.kim@datadoghq.com>
2023-11-17 14:57:16 -0500

    Separate openai integration changes

--- ddtrace/contrib/openai/_endpoint_hooks.py
+++ ddtrace/contrib/openai/_endpoint_hooks.py
@@ -224,8 +224,6 @@ class _CompletionHook(_BaseCompletionHook):
                 "sampled %s" % self.OPERATION_ID,
                 attrs={"prompt": prompt, "choices": log_choices},
             )
-        if integration.is_pc_sampled_llmobs(span):
-            integration.generate_completion_llm_records(resp, span, args, kwargs)
         return self._handle_response(pin, span, integration, resp)
 
 
@@ -327,8 +325,6 @@ class _ChatCompletionHook(_BaseCompletionHook):
                     "completion": log_choices,
                 },
             )
-        if integration.is_pc_sampled_llmobs(span):
-            integration.generate_chat_llm_records(resp, span, args, kwargs)
         return self._handle_response(pin, span, integration, resp)
 
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-
 import ast
 import base64
 import contextlib
