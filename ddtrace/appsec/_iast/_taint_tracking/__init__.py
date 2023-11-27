1c6fdaca6b109607455e46f67b54129ada2875c8
Alberto Vara <alberto.vara@datadoghq.com>
2023-11-27 16:36:32 +0100

    fix: source_name type error

--- ddtrace/appsec/_iast/_taint_tracking/__init__.py
+++ ddtrace/appsec/_iast/_taint_tracking/__init__.py
@@ -92,6 +92,9 @@ def taint_pyobject(pyobject, source_name, source_value, source_origin=None):
 
     if isinstance(source_name, (bytes, bytearray)):
         source_name = str(source_name, encoding="utf8")
+    if isinstance(source_name, OriginType):
+        source_name = origin_to_str(source_name)
+
     if isinstance(source_value, (bytes, bytearray)):
         source_value = str(source_value, encoding="utf8")
     if source_origin is None:
