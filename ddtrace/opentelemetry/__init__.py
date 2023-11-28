1b56ae4387c08e8fb1cdfcfdcce610f40ce69de9
Munir Abdinur <munir.abdinur@datadoghq.com>
2023-11-28 14:58:28 -0500

    ws

--- ddtrace/opentelemetry/__init__.py
+++ ddtrace/opentelemetry/__init__.py
@@ -73,7 +73,7 @@ The OpenTelemetry API support implementation maps OpenTelemetry spans to Datadog
       - ``spanID``
       -
     * - ``trace_state``
-      - ``meta["tracestate"]`` 
+      - ``meta["tracestate"]``
       - Datadog vendor-specific data is set in trace state using the ``dd=`` prefix
     * - ``parent_span_id``
       - ``parentID``
