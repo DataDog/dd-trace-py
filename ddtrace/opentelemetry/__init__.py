89b1a2fd5e2f9ec802ddd755725f3fd03f8136db
Wan Tsui <wan.tsui@datadoghq.com>
2023-11-28 11:56:13 -0500

    trace_state mapping

--- ddtrace/opentelemetry/__init__.py
+++ ddtrace/opentelemetry/__init__.py
@@ -73,7 +73,7 @@ The OpenTelemetry API support implementation maps OpenTelemetry spans to Datadog
       - ``spanID``
       -
     * - ``trace_state``
-      - ``meta["_sampling_priority_v1"]``, ``meta["_dd.origin"]``, ...
+      - ``meta["tracestate"]["_sampling_priority_v1"]``, ``meta["_dd.origin"]``, ...
       - Datadog vendor-specific data is set in trace state using the ``dd=`` prefix
     * - ``parent_span_id``
       - ``parentID``
