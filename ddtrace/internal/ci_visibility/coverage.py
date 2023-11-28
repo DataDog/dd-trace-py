2010a59e79532b7887e6a27a2b3fa48c4125629e
eric.navarro <eric.navarro@datadoghq.com>
2023-11-28 14:37:51 +0100

    modify riot-file

--- ddtrace/internal/ci_visibility/coverage.py
+++ ddtrace/internal/ci_visibility/coverage.py
@@ -59,17 +59,16 @@ def _stop_coverage(module):
         del module._coverage
 
 
-def _coverage_has_valid_data(coverage_data: Coverage, silent=False) -> bool:
+def _coverage_has_valid_data(coverage_data: Coverage) -> bool:
     if not coverage_data._collector or len(coverage_data._collector.data) == 0:
-        if not silent:
-            log.warning("No coverage collector or data found for item")
+        log.warning("No coverage collector or data found for item")
         return False
     return True
 
 
 def _switch_coverage_context(coverage_data: Coverage, unique_test_name: str):
     coverage_data.switch_context(unique_test_name)
-    if not _coverage_has_valid_data(coverage_data, silent=True):
+    if not _coverage_has_valid_data(coverage_data):
         return
     coverage_data._collector.data.clear()  # type: ignore[union-attr]
 
