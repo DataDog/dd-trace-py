21c5a816b68b2a13f6b9fd1ee2ac45cf5e207d73
eric.navarro <eric.navarro@datadoghq.com>
2023-11-28 15:27:15 +0100

    Revert "modify riot-file"
    This reverts commit 2010a59e79532b7887e6a27a2b3fa48c4125629e.

--- ddtrace/internal/ci_visibility/coverage.py
+++ ddtrace/internal/ci_visibility/coverage.py
@@ -59,16 +59,17 @@ def _stop_coverage(module):
         del module._coverage
 
 
-def _coverage_has_valid_data(coverage_data: Coverage) -> bool:
+def _coverage_has_valid_data(coverage_data: Coverage, silent=False) -> bool:
     if not coverage_data._collector or len(coverage_data._collector.data) == 0:
-        log.warning("No coverage collector or data found for item")
+        if not silent:
+            log.warning("No coverage collector or data found for item")
         return False
     return True
 
 
 def _switch_coverage_context(coverage_data: Coverage, unique_test_name: str):
     coverage_data.switch_context(unique_test_name)
-    if not _coverage_has_valid_data(coverage_data):
+    if not _coverage_has_valid_data(coverage_data, silent=True):
         return
     coverage_data._collector.data.clear()  # type: ignore[union-attr]
 
