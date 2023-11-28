DataDog/dd-trace-py

commit ee1a0437f4ca81751b0b4ea5f3a8a95dd48e6474

chore(django): remove some appsec code from django integration (#6922)
This change uses the Core API abstraction layer to remove some code
related to Application Security Monitoring from the Django integration
module, clarifying the separation of concerns and ownership between the
two areas.

## Checklist

- [x] Change(s) are motivated and described in the PR description.
- [x] Testing strategy is described if automated tests are not included
in the PR.
- [x] Risk is outlined (performance impact, potential for breakage,
maintainability, etc).
- [x] Change is maintainable (easy to change, telemetry, documentation).
- [x] [Library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
are followed. If no release note is required, add label
`changelog/no-changelog`.
- [x] Documentation is included (in-code, generated user docs, [public
corp docs](https://github.com/DataDog/documentation/)).
- [x] Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))

## Reviewer Checklist

- [x] Title is accurate.
- [x] No unnecessary changes are introduced.
- [x] Description motivates each change.
- [x] Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes unless absolutely necessary.
- [x] Testing strategy adequately addresses listed risk(s).
- [x] Change is maintainable (easy to change, telemetry, documentation).
- [x] Release note makes sense to a user of the library.
- [x] Reviewer has explicitly acknowledged and discussed the performance
implications of this PR as reported in the benchmarks PR comment.
- [x] Backport labels are set in a manner that is consistent with the
[release branch maintenance
policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
- [x] If this PR touches code that signs or publishes builds or

ddtrace/contrib/django/utils.py
@@ -18,6 +18,8 @@ from ddtrace.constants import SPAN_MEASURED_KEY
 from ddtrace.contrib import func_name
 from ddtrace.ext import SpanTypes
 from ddtrace.ext import user as _user
+from ddtrace.internal.utils.http import parse_form_multipart
+from ddtrace.internal.utils.http import parse_form_params
 from ddtrace.propagation._utils import from_wsgi_header
 
 from .. import trace_utils
@@ -263,17 +265,16 @@ def _before_request_tags(pin, span, request):
 
 def _extract_body(request):
     # DEV: Do not use request.POST or request.data, this could prevent custom parser to be used after
-    if config._appsec_enabled and request.method in _BODY_METHODS:
-        from ddtrace.appsec._utils import parse_form_multipart
-        from ddtrace.appsec._utils import parse_form_params
-
+    if request.method in _BODY_METHODS:
         req_body = None
         content_type = request.content_type if hasattr(request, "content_type") else request.META.get("CONTENT_TYPE")
+        results = core.dispatch("django.extract_body", [])[0]
+        headers = results[0] if results else None
         try:
             if content_type == "application/x-www-form-urlencoded":
                 req_body = parse_form_params(request.body.decode("UTF-8", errors="ignore"))
             elif content_type == "multipart/form-data":
-                req_body = parse_form_multipart(request.body.decode("UTF-8", errors="ignore"))
+                req_body = parse_form_multipart(request.body.decode("UTF-8", errors="ignore"), headers)
             elif content_type in ("application/json", "text/json"):
                 req_body = json.loads(request.body.decode("UTF-8", errors="ignore"))
             elif content_type in ("application/xml", "text/xml"):
@@ -282,7 +283,6 @@ def _extract_body(request):
                 req_body = None
         except BaseException:
             log.debug("Failed to parse request body", exc_info=True)
-            # req_body is None
         return req_body
 
 
@@ -365,14 +365,10 @@ def _after_request_tags(pin, span, request, response):
 
             url = get_request_uri(request)
 
-            request_headers = None
-            if config._appsec_enabled:
-                from ddtrace.appsec import _asm_request_context
-
-                request_headers = _asm_request_context.get_headers()
+            results = core.dispatch("django.after_request_headers", [])[0]
+            request_headers = results[0] if results else None
 
             if not request_headers:
-                # did not go through AppSecProcessor.on_span_start
                 request_headers = _get_request_headers(request)
 
             response_headers = dict(response.items()) if response else {}
@@ -404,8 +400,7 @@ def _after_request_tags(pin, span, request, response):
                 headers_are_case_sensitive=core.get_item("http.request.headers_case_sensitive", span=span),
                 response_cookies=response_cookies,
             )
-            if config._appsec_enabled and config._api_security_enabled:
-                _asm_request_context.set_body_response(response.content)
+            core.dispatch("django.after_request_headers.post", response.content, None)
     finally:
         if span.resource == REQUEST_DEFAULT_RESOURCE:
             span.resource = request.method

ddtrace/appsec/_asm_request_context.py
@@ -374,6 +374,9 @@ def _on_context_ended(ctx):
 
 core.on("context.started.wsgi.__call__", _on_context_started)
 core.on("context.ended.wsgi.__call__", _on_context_ended)
+core.on("context.started.django.traced_get_response", _on_context_started)
+core.on("context.ended.django.traced_get_response", _on_context_ended)
+core.on("django.traced_get_response.pre", set_block_request_callable)
 
 
 def _on_wrapped_view(kwargs):
@@ -428,15 +431,17 @@ def _on_pre_tracedrequest(ctx):
             block_request()
 
 
-def _on_post_finalizerequest(rv):
-    if config._api_security_enabled and config._appsec_enabled and getattr(rv, "is_sequence", False):
-        # start_response was not called yet, set the HTTP response headers earlier
-        set_headers_response(list(rv.headers))
-        set_body_response(rv.response)
+def _set_headers_and_response(response, headers):
+    if config._api_security_enabled and config._appsec_enabled:
+        if headers:
+            # start_response was not called yet, set the HTTP response headers earlier
+            set_headers_response(list(headers))
+        if response:
+            set_body_response(response)
 
 
-def _on_start_response():
-    log.debug("Flask WAF call for Suspicious Request Blocking on response")
+def _call_waf(integration):
+    log.debug("%s WAF call for Suspicious Request Blocking on response", integration)
     call_waf_callback()
     return get_headers().get("Accept", "").lower()
 
@@ -445,10 +450,20 @@ def _on_block_decided(callback):
     set_value(_CALLBACKS, "flask_block", callback)
 
 
+def _get_headers_if_appsec():
+    if config._appsec_enabled:
+        return get_headers()
+
+
 def listen_context_handlers():
-    core.on("flask.finalize_request.post", _on_post_finalizerequest)
+    core.on("flask.finalize_request.post", _set_headers_and_response)
     core.on("flask.wrapped_view", _on_wrapped_view)
     core.on("context.started.flask._patched_request", _on_pre_tracedrequest)
     core.on("wsgi.block_decided", _on_block_decided)
-    core.on("flask.start_response", _on_start_response)
+    core.on("flask.start_response", _call_waf)
+    core.on("django.start_response", _call_waf)
+    core.on("django.finalize_response", _call_waf)
+    core.on("django.after_request_headers", _get_headers_if_appsec)
+    core.on("django.extract_body", _get_headers_if_appsec)
+    core.on("django.after_request_headers.post", _set_headers_and_response)
     core.on("flask.set_request_tags", _on_set_request_tags)

ddtrace/appsec/_trace_utils.py
@@ -9,6 +9,7 @@ from ddtrace.appsec._constants import APPSEC
 from ddtrace.appsec._constants import LOGIN_EVENTS_MODE
 from ddtrace.appsec._constants import WAF_CONTEXT_NAMES
 from ddtrace.contrib.trace_utils import set_user
+from ddtrace.ext import SpanTypes
 from ddtrace.ext import user
 from ddtrace.internal import core
 from ddtrace.internal.logger import get_logger
@@ -254,3 +255,74 @@ def block_request_if_user_blocked(tracer: Tracer, userid: str) -> None:
         if span:
             span.set_tag_str(user.ID, str(userid))
         _asm_request_context.block_request()
+
+
+def _on_django_login(
+    pin,
+    request,
+    user,
+    mode,
+    info_retriever,
+):
+    if not config._appsec_enabled:
+        return
+
+    if user:
+        if str(user) != "AnonymousUser":
+            user_id, user_extra = info_retriever.get_user_info()
+            if not user_id:
+                log.debug(
+                    "Automatic Login Events Tracking: " "Could not determine user id field user for the %s user Model",
+                    type(user),
+                )
+                return
+
+            with pin.tracer.trace("django.contrib.auth.login", span_type=SpanTypes.AUTH):
+                from ddtrace.contrib.django.compat import user_is_authenticated
+
+                if user_is_authenticated(user):
+                    session_key = getattr(request, "session_key", None)
+                    track_user_login_success_event(
+                        pin.tracer,
+                        user_id=user_id,
+                        session_id=session_key,
+                        propagate=True,
+                        login_events_mode=mode,
+                        **user_extra
+                    )
+                else:
+                    # Login failed but the user exists
+                    track_user_login_failure_event(pin.tracer, user_id=user_id, exists=True, login_events_mode=mode)
+        else:
+            # Login failed and the user is unknown
+            if mode == "extended":
+                user_id = info_retriever.get_username()
+            else:  # safe mode
+                user_id = info_retriever.get_userid()
+            if not user_id:
+                user_id = "AnonymousUser"
+
+            track_user_login_failure_event(pin.tracer, user_id=user_id, exists=False, login_events_mode=mode)
+
+
+def _on_django_auth(result_user, mode, kwargs, pin, info_retriever):
+    if not config._appsec_enabled:
+        return True, result_user
+
+    userid_list = info_retriever.possible_user_id_fields if mode == "safe" else info_retriever.possible_login_fields
+
+    for possible_key in userid_list:
+        if possible_key in kwargs:
+            user_id = kwargs[possible_key]
+            break
+    else:
+        user_id = "missing"
+
+    if not result_user:
+        with pin.tracer.trace("django.contrib.auth.login", span_type=SpanTypes.AUTH):
+            track_user_login_failure_event(pin.tracer, user_id=user_id, exists=False, login_events_mode=mode)
+    return False, None
+
+
+core.on("django.login", _on_django_login)
+core.on("django.auth", _on_django_auth)

ddtrace/appsec/_utils.py
@@ -1,72 +1,16 @@
 import os
-from typing import Any
-from typing import Dict
-from typing import List
-from typing import Union
-from urllib import parse
 
 from ddtrace.appsec import _asm_request_context
 from ddtrace.appsec._constants import API_SECURITY
 from ddtrace.constants import APPSEC_ENV
 from ddtrace.internal.logger import get_logger
 from ddtrace.internal.utils.http import _get_blocked_template  # noqa
+from ddtrace.internal.utils.http import parse_form_multipart  # noqa
+from ddtrace.internal.utils.http import parse_form_params  # noqa
 from ddtrace.settings import _config as config
 
 
 log = get_logger(__name__)
-
-
-def parse_form_params(body: str) -> Dict[str, Union[str, List[str]]]:
-    """Return a dict of form data after HTTP form parsing"""
-    body_params = body.replace("+", " ")
-    req_body: Dict[str, Union[str, List[str]]] = dict()
-    for item in body_params.split("&"):
-        key, equal, val = item.partition("=")
-        if equal:
-            key = parse.unquote(key)
-            val = parse.unquote(val)
-            prev_value = req_body.get(key, None)
-            if prev_value is None:
-                req_body[key] = val
-            elif isinstance(prev_value, list):
-                prev_value.append(val)
-            else:
-                req_body[key] = [prev_value, val]
-    return req_body
-
-
-def parse_form_multipart(body: str) -> Dict[str, Any]:
-    """Return a dict of form data after HTTP form parsing"""
-    import email
-    import json
-
-    import xmltodict
-
-    def parse_message(msg):
-        if msg.is_multipart():
-            res = {
-                part.get_param("name", failobj=part.get_filename(), header="content-disposition"): parse_message(part)
-                for part in msg.get_payload()
-            }
-        else:
-            content_type = msg.get("Content-Type")
-            if content_type in ("application/json", "text/json"):
-                res = json.loads(msg.get_payload())
-            elif content_type in ("application/xml", "text/xml"):
-                res = xmltodict.parse(msg.get_payload())
-            elif content_type in ("text/plain", None):
-                res = msg.get_payload()
-            else:
-                res = ""
-
-        return res
-
-    headers = _asm_request_context.get_headers()
-    if headers is not None:
-        content_type = headers.get("Content-Type")
-        msg = email.message_from_string("MIME-Version: 1.0\nContent-Type: %s\n%s" % (content_type, body))
-        return parse_message(msg)
-    return {}
 
 
 def parse_response_body(raw_body):

commit fb188abde621ebd05a4d2eecde6fe6dc44325336

fix(django): support streamed responses (#7633)
Resolves:  https://github.com/DataDog/dd-trace-py/issues/7646
Introduced in tracing by:
https://github.com/DataDog/dd-trace-py/pull/6922 (v2.2.0)

## Checklist

- [x] Change(s) are motivated and described in the PR description.
- [x] Testing strategy is described if automated tests are not included
in the PR.
- [x] Risk is outlined (performance impact, potential for breakage,
maintainability, etc).
- [x] Change is maintainable (easy to change, telemetry, documentation).
- [x] [Library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
are followed. If no release note is required, add label
`changelog/no-changelog`.
- [x] Documentation is included (in-code, generated user docs, [public
corp docs](https://github.com/DataDog/documentation/)).
- [x] Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))

## Reviewer Checklist

- [x] Title is accurate.
- [x] No unnecessary changes are introduced.
- [x] Description motivates each change.
- [x] Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes unless absolutely necessary.
- [x] Testing strategy adequately addresses listed risk(s).
- [x] Change is maintainable (easy to change, telemetry, documentation).
- [x] Release note makes sense to a user of the library.
- [x] Reviewer has explicitly acknowledged and discussed the performance
implications of this PR as reported in the benchmarks PR comment.
- [x] Backport labels are set in a manner that is consistent with the
[release branch maintenance
policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
- [x] If this PR touches code that signs or publishes builds or
packages, or handles credentials of any kind, I've requested a review
from `@DataDog/security-design-and-guidance`.
- [x] This PR doesn't touch any of that.

ddtrace/contrib/django/utils.py
@@ -395,7 +395,10 @@ def _after_request_tags(pin, span: Span, request, response):
                 status,
                 response_cookies,
             )
-            core.dispatch("django.after_request_headers.finalize", response.content, None)
+            content = getattr(response, "content", None)
+            if content is None:
+                content = getattr(response, "streaming_content", None)
+            core.dispatch("django.after_request_headers.finalize", content, None)
     finally:
         if span.resource == REQUEST_DEFAULT_RESOURCE:
             span.resource = request.method

releasenotes/notes/fix-stream-response-django-cd92556cc2a082bb.yaml
@@ -1,0 +1,4 @@
+---
+fixes:
+  - |
+    django: Resolves ``AttributeError`` raised by traced ``StreamingHttpResponse``.

tests/contrib/django/django_app/urls.py
@@ -1,6 +1,9 @@
+import tempfile
+
 import django
 from django.contrib.auth import login
 from django.contrib.auth.models import User
+from django.http import FileResponse
 from django.http import HttpResponse
 from django.urls import include
 from django.urls import path
@@ -26,6 +29,12 @@ def repath_view(request):
 
 def path_view(request):
     return HttpResponse(status=200)
+
+
+def send_file(request):
+    f = tempfile.NamedTemporaryFile()
+    f.write(b"Stream Hello World!" * 100)
+    return FileResponse(f, content_type="text/plain")
 
 
 def authenticated_view(request):
@@ -65,6 +74,7 @@ urlpatterns = [
     handler(r"^template-view/$", views.template_view, name="template-view"),
     handler(r"^template-simple-view/$", views.template_simple_view, name="template-simple-view"),
     handler(r"^template-list-view/$", views.template_list_view, name="template-list-view"),
+    handler(r"^stream-file/$", send_file, name="stream-file"),
     re_path(r"re-path.*/", repath_view),
     path("path/", path_view),
     path("include/", include("tests.contrib.django.django_app.extra_urls")),

tests/contrib/django/test_django_snapshots.py
@@ -299,8 +299,13 @@ def test_templates_disabled():
         assert resp.content == b"some content\n"
 
 
+@snapshot(ignores=SNAPSHOT_IGNORES)
+def test_streamed_file(client):
+    assert client.get("/stream-file/").status_code == 200
+
+
 @snapshot(ignores=SNAPSHOT_IGNORES + ["meta.http.useragent"])
-@pytest.mark.skipif(django.VERSION < (3, 0, 0), reason="ASGI not supported in django<3")
+@pytest.mark.skipif(django.VERSION > (3, 0, 0), reason="ASGI not supported in django<3")
 def test_django_resource_handler():
     # regression test for: DataDog/dd-trace-py/issues/5711
     with daphne_client("application", additional_env={"DD_DJANGO_USE_HANDLER_RESOURCE_FORMAT": "true"}) as client: