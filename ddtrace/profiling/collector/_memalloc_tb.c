a88a9a6c52e2a9c551a8d39a2506b90dc4e6fdc4
sanchda <sanchda@users.noreply.github.com>
2023-11-29 20:25:19 +0000

    Nags

--- ddtrace/profiling/collector/_memalloc_tb.c
+++ ddtrace/profiling/collector/_memalloc_tb.c
@@ -95,42 +95,44 @@ traceback_free(traceback_t* tb)
 }
 
 /* Helper function for extracting the classname from a PyFrameObject */
-static const char *get_class_name(PyFrameObject *frame) {
-    const char *result = "";
-    PyCodeObject *code = PyFrame_GetCode(frame);
+static const char*
+get_class_name(PyFrameObject* frame)
+{
+    const char* result = "";
+    PyCodeObject* code = PyFrame_GetCode(frame);
     if (!code)
         goto final;
 
-    PyObject *varnames = PyCode_GetVarnames(code);
+    PyObject* varnames = PyCode_GetVarnames(code);
     if (!varnames || !PyTuple_Check(varnames) || PyTuple_Size(varnames) <= 0)
         goto final;
 
-    PyObject *name = PyTuple_GetItem(varnames, 0);  // Borrowed reference, no need to DECREF.
+    PyObject* name = PyTuple_GetItem(varnames, 0); // Borrowed reference, no need to DECREF.
     if (!name)
         goto final;
 
-    PyObject *locals = PyFrame_GetLocals(frame);
+    PyObject* locals = PyFrame_GetLocals(frame);
     if (!locals)
         goto final;
 
-    PyObject *value = PyDict_GetItem(locals, name);  // Borrowed reference. No need to DECREF.
+    PyObject* value = PyDict_GetItem(locals, name); // Borrowed reference. No need to DECREF.
     if (!value)
         goto final;
 
     if (PyUnicode_CompareWithASCIIString(name, "self") == 0) {
-        value = PyObject_Type(value);  // Gets the type of 'self'
+        value = PyObject_Type(value);
         if (!value)
             goto final;
     } else if (PyUnicode_CompareWithASCIIString(name, "cls") != 0) {
         goto final;
     }
 
-    PyObject *clsname = PyObject_GetAttrString(value, "__name__");
+    PyObject* clsname = PyObject_GetAttrString(value, "__name__");
     if (!clsname)
         goto final;
 
     result = PyUnicode_AsUTF8(clsname);
-    Py_DECREF(clsname);  // DECREF after using.
+    Py_DECREF(clsname);
 
 final:
     return result ? result : "";
