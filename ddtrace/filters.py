b4a0ebd74f89e65ec718a2428a7867e9f4b7b315
Emmett Butler <723615+emmettbutler@users.noreply.github.com>
2023-11-30 08:11:55 -0800

    Update filters.py

--- ddtrace/filters.py
+++ ddtrace/filters.py
@@ -5,9 +5,11 @@ from typing import List  # noqa:F401
 from typing import Optional  # noqa:F401
 from typing import Union  # noqa:F401
 
-from ddtrace import Span  # noqa:F401
 from ddtrace.ext import http
 from ddtrace.internal.processor.trace import TraceProcessor
+
+if TYPE_CHECKING:  # pragma: no cover
+    from ddtrace import Span  # noqa:F401
 
 
 class TraceFilter(TraceProcessor):
