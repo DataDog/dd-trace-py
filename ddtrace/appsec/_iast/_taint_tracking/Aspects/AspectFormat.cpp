DataDog/dd-trace-py

commit 211c07c4f9ccacf0de229cff05bbe2c4d24f253a

fix(iast): iast leak for python 3.11 (#7772)
- Fix Pybind11 leak error when it casts a list to `TaintRangeRefs` in
`format_aspect`

```
ranges_orig, candidate_text_ranges = are_all_text_all_ranges(candidate_text, params)
[....]
new_result = _convert_escaped_text_to_tainted_text(
            new_template.format(*new_args, **new_kwargs),
            ranges_orig=ranges_orig,
        )
```

This PR continues researches that we did in this PRs:
https://github.com/DataDog/dd-trace-py/pull/7757
https://github.com/DataDog/dd-trace-py/pull/7753
https://github.com/DataDog/dd-trace-py/pull/7707
https://github.com/DataDog/dd-trace-py/pull/7702
https://github.com/DataDog/dd-trace-py/pull/7630
https://github.com/DataDog/dd-trace-py/pull/7112


## Checklist

- [x] Change(s) are motivated and described in the PR description.
- [x] Testing strategy is described if automated tests are not included
in the PR.
- [x] Risk is outlined (performance impact, potential for breakage,
maintainability, etc).
- [x] Change is maintainable (easy to change, telemetry, documentation).
- [x] [Library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
are followed. If no release note is required, add label
`changelog/no-changelog`.
- [x] Documentation is included (in-code, generated user docs, [public
corp docs](https://github.com/DataDog/documentation/)).
- [x] Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))

## Reviewer Checklist

- [x] Title is accurate.
- [x] No unnecessary changes are introduced.
- [x] Description motivates each change.
- [x] Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes unless absolutely necessary.
- [x] Testing strategy adequately addresses listed risk(s).
- [x] Change is maintainable (easy to change, telemetry, documentation).

ddtrace/appsec/_iast/_taint_tracking/Aspects/AspectFormat.cpp
@@ -1,0 +1,50 @@
+#pragma once
+#include "Aspects/AspectFormat.h"
+
+template<class StrType>
+StrType
+api_format_aspect(StrType& candidate_text,
+                  const py::tuple& parameter_list,
+                  const py::args& args,
+                  const py::kwargs& kwargs)
+{
+    auto [ranges_orig, candidate_text_ranges] = are_all_text_all_ranges(candidate_text.ptr(), parameter_list);
+
+    if (!ranges_orig.empty() or !candidate_text_ranges.empty()) {
+        auto new_template =
+          _int_as_formatted_evidence<StrType>(candidate_text, candidate_text_ranges, TagMappingMode::Mapper);
+
+        py::list new_args;
+        py::dict new_kwargs;
+        for (const auto arg : args) {
+            auto str_arg = py::cast<py::str>(arg);
+            auto n_arg = _all_as_formatted_evidence<py::str>(str_arg, TagMappingMode::Mapper);
+            new_args.append(n_arg);
+        }
+        for (auto [key, value] : new_kwargs) {
+            auto str_value = py::cast<py::str>(value);
+            auto n_value = _all_as_formatted_evidence<py::str>(str_value, TagMappingMode::Mapper);
+            new_kwargs[key] = n_value;
+        }
+
+        StrType new_template_format =
+          py::getattr(new_template, "format")(*(py::cast<py::tuple>(new_args)), **new_kwargs);
+        std::tuple result = _convert_escaped_text_to_taint_text<StrType>(new_template_format, ranges_orig);
+        StrType result_text = get<0>(result);
+        TaintRangeRefs result_ranges = get<1>(result);
+        StrType result_new_id = copy_string_new_id(result_text);
+        set_ranges(result_new_id.ptr(), result_ranges);
+        return result_new_id;
+    }
+    return py::getattr(candidate_text, "format")(*args, **kwargs);
+}
+
+void
+pyexport_format_aspect(py::module& m)
+{
+    m.def("_format_aspect",
+          &api_format_aspect<py::str>,
+          "candidate_text"_a,
+          "parameter_list"_a,
+          py::return_value_policy::move);
+}

tests/appsec/iast/aspects/test_format_aspect_fixtures.py
@@ -109,6 +109,7 @@ class TestOperatorFormatReplacement(BaseReplacement):
             escaped_expected_result=":+-<input1>template⚠️<input1>-+: " ":+-<input2>parameter⚠️<input2>-+:",
         )
 
+    @pytest.mark.skip(reason="Migrating format_aspect to C++ breaks this test")
     def test_format_when_tainted_template_range_no_brackets_and_param_not_str_then_tainted(self):
         # type: () -> None
         self._assert_format_result(
@@ -118,6 +119,7 @@ class TestOperatorFormatReplacement(BaseReplacement):
             escaped_expected_result=":+-<input1>template<input1>-+: 3.14",
         )
 
+    @pytest.mark.skip(reason="Migrating format_aspect to C++ breaks this test")
     def test_format_when_tainted_template_range_with_brackets_and_param_not_str_then_tainted(self):
         # type: () -> None
         self._assert_format_result(

ddtrace/appsec/_iast/_taint_tracking/Aspects/AspectFormat.h
@@ -1,0 +1,18 @@
+#pragma once
+#include "Aspects/Helpers.h"
+#include "Initializer/Initializer.h"
+#include "TaintTracking/Source.h"
+#include "TaintTracking/TaintRange.h"
+#include "TaintTracking/TaintedObject.h"
+#include <Python.h>
+#include <pybind11/pybind11.h>
+
+template<class StrType>
+StrType
+api_format_aspect(StrType& candidate_text,
+                  const py::tuple& parameter_list,
+                  const py::args& args,
+                  const py::kwargs& kwargs);
+
+void
+pyexport_format_aspect(py::module& m);

ddtrace/appsec/_iast/_taint_tracking/Aspects/Helpers.cpp
@@ -94,25 +94,37 @@ range_sort(const TaintRangePtr& t1, const TaintRangePtr& t2)
     return t1->start < t2->start;
 }
 
+template<class StrType>
+StrType
+_all_as_formatted_evidence(StrType& text, TagMappingMode tag_mapping_mode)
+{
+    TaintRangeRefs text_ranges = api_get_ranges(text);
+    return AsFormattedEvidence<StrType>(text, text_ranges, tag_mapping_mode, nullopt);
+}
+
+template<class StrType>
+StrType
+_int_as_formatted_evidence(StrType& text, TaintRangeRefs text_ranges, TagMappingMode tag_mapping_mode)
+{
+    return AsFormattedEvidence<StrType>(text, text_ranges, tag_mapping_mode, nullopt);
+}
+
 // TODO OPTIMIZATION: Remove py::types once this isn't used in Python
 template<class StrType>
 StrType
-as_formatted_evidence(StrType& text,
-                      optional<TaintRangeRefs>& text_ranges,
-                      const optional<TagMappingMode>& tag_mapping_mode,
-                      const optional<const py::dict>& new_ranges)
+AsFormattedEvidence(StrType& text,
+                    TaintRangeRefs& text_ranges,
+                    const optional<TagMappingMode>& tag_mapping_mode,
+                    const optional<const py::dict>& new_ranges)
 {
-    if (!text_ranges) {
-        text_ranges = api_get_ranges(text);
-    }
-    if (text_ranges->empty()) {
+    if (text_ranges.empty()) {
         return text;
     }
     vector<StrType> res_vector;
     long index = 0;
 
-    sort(text_ranges->begin(), text_ranges->end(), &range_sort);
-    for (const auto& taint_range : *text_ranges) {
+    sort(text_ranges.begin(), text_ranges.end(), &range_sort);
+    for (const auto& taint_range : text_ranges) {
         py::object content;
         if (!tag_mapping_mode) {
             content = get_default_content(taint_range);
@@ -128,7 +140,7 @@ as_formatted_evidence(StrType& text,
                     // Nothing
                 }
             }
-        StrType tag = get_tag<StrType>(content);
+        auto tag = get_tag<StrType>(content);
 
         auto range_end = taint_range->start + taint_range->length;
 
@@ -143,6 +155,22 @@ as_formatted_evidence(StrType& text,
     }
     res_vector.push_back(text[py::slice(py::int_(index), nullptr, nullptr)]);
     return StrType(EVIDENCE_MARKS::BLANK).attr("join")(res_vector);
+}
+
+template<class StrType>
+StrType
+ApiAsFormattedEvidence(StrType& text,
+                       optional<TaintRangeRefs>& text_ranges,
+                       const optional<TagMappingMode>& tag_mapping_mode,
+                       const optional<const py::dict>& new_ranges)
+{
+    TaintRangeRefs _ranges;
+    if (!text_ranges) {
+        _ranges = api_get_ranges(text);
+    } else {
+        _ranges = text_ranges.value();
+    }
+    return AsFormattedEvidence<StrType>(text, _ranges, tag_mapping_mode, new_ranges);
 }
 
 vector<string>
@@ -165,7 +193,7 @@ api_convert_escaped_text_to_taint_text_ba(const py::bytearray& taint_escaped_tex
 {
     py::bytes bytes_text = py::bytes() + taint_escaped_text;
 
-    std::tuple result = _convert_escaped_text_to_taint_text<py::bytes>(bytes_text, ranges_orig);
+    std::tuple result = _convert_escaped_text_to_taint_text<py::bytes>(bytes_text, std::move(ranges_orig));
     py::bytearray result_new_id = copy_string_new_id(py::bytearray() + get<0>(result));
     set_ranges(result_new_id.ptr(), get<1>(result));
     return result_new_id;
@@ -315,35 +343,52 @@ pyexport_aspect_helpers(py::module& m)
     m.def("common_replace", &common_replace<py::bytes>, "string_method"_a, "candidate_text"_a);
     m.def("common_replace", &common_replace<py::str>, "string_method"_a, "candidate_text"_a);
     m.def("common_replace", &common_replace<py::bytearray>, "string_method"_a, "candidate_text"_a);
-    m.def("as_formatted_evidence",
-          &as_formatted_evidence<py::bytes>,
+    m.def("_all_as_formatted_evidence",
+          &_all_as_formatted_evidence<py::str>,
+          "text"_a,
+          "tag_mapping_function"_a = nullopt,
+          py::return_value_policy::move);
+    m.def("_int_as_formatted_evidence",
+          &_int_as_formatted_evidence<py::str>,
           "text"_a,
           "text_ranges"_a = nullopt,
           "tag_mapping_function"_a = nullopt,
-          "new_ranges"_a = nullopt);
+          py::return_value_policy::move);
     m.def("as_formatted_evidence",
-          &as_formatted_evidence<py::str>,
+          &ApiAsFormattedEvidence<py::bytes>,
           "text"_a,
           "text_ranges"_a = nullopt,
           "tag_mapping_function"_a = nullopt,
-          "new_ranges"_a = nullopt);
+          "new_ranges"_a = nullopt,
+          py::return_value_policy::move);
     m.def("as_formatted_evidence",
-          &as_formatted_evidence<py::bytearray>,
+          &ApiAsFormattedEvidence<py::str>,
           "text"_a,
           "text_ranges"_a = nullopt,
           "tag_mapping_function"_a = nullopt,
-          "new_ranges"_a = nullopt);
+          "new_ranges"_a = nullopt,
+          py::return_value_policy::move);
+    m.def("as_formatted_evidence",
+          &ApiAsFormattedEvidence<py::bytearray>,
+          "text"_a,
+          "text_ranges"_a = nullopt,
+          "tag_mapping_function"_a = nullopt,
+          "new_ranges"_a = nullopt,
+          py::return_value_policy::move);
     m.def("_convert_escaped_text_to_tainted_text",
           &api_convert_escaped_text_to_taint_text<py::bytes>,
           "taint_escaped_text"_a,
-          "ranges_orig"_a);
+          "ranges_orig"_a,
+          py::return_value_policy::move);
     m.def("_convert_escaped_text_to_tainted_text",
           &api_convert_escaped_text_to_taint_text<py::str>,
           "taint_escaped_text"_a,
-          "ranges_orig"_a);
+          "ranges_orig"_a,
+          py::return_value_policy::move);
     m.def("_convert_escaped_text_to_tainted_text",
           &api_convert_escaped_text_to_taint_text_ba,
           "taint_escaped_text"_a,
-          "ranges_orig"_a);
+          "ranges_orig"_a,
+          py::return_value_policy::move);
     m.def("parse_params", &parse_params);
 }

ddtrace/appsec/_iast/_taint_tracking/Aspects/Helpers.h
@@ -24,10 +24,25 @@ common_replace(const py::str& string_method,
 
 template<class StrType>
 StrType
-as_formatted_evidence(StrType& text,
-                      optional<TaintRangeRefs>& text_ranges,
-                      const optional<TagMappingMode>& tag_mapping_mode,
-                      const optional<const py::dict>& new_ranges);
+_all_as_formatted_evidence(StrType& text, TagMappingMode tag_mapping_mode);
+
+template<class StrType>
+StrType
+_int_as_formatted_evidence(StrType& text, TaintRangeRefs text_ranges, TagMappingMode tag_mapping_mode);
+
+template<class StrType>
+StrType
+AsFormattedEvidence(StrType& text,
+                    optional<TaintRangeRefs>& text_ranges,
+                    const TagMappingMode& tag_mapping_mode,
+                    const optional<const py::dict>& new_ranges);
+
+template<class StrType>
+StrType
+ApiAsFormattedEvidence(StrType& text,
+                       optional<TaintRangeRefs>& text_ranges,
+                       const optional<TagMappingMode>& tag_mapping_mode,
+                       const optional<const py::dict>& new_ranges);
 
 py::bytearray
 api_convert_escaped_text_to_taint_text_ba(const py::bytearray& taint_escaped_text, TaintRangeRefs ranges_orig);

ddtrace/appsec/_iast/_taint_tracking/Aspects/_aspects_exports.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "AspectFormat.h"
 #include "Helpers.h"
 #include <pybind11/pybind11.h>
 
@@ -7,4 +8,6 @@ pyexport_m_aspect_helpers(py::module& m)
 {
     py::module m_aspect_helpers = m.def_submodule("aspect_helpers", "Aspect Helpers");
     pyexport_aspect_helpers(m_aspect_helpers);
+    py::module m_aspect_format = m.def_submodule("aspect_format", "Aspect Format");
+    pyexport_format_aspect(m_aspect_format);
 }

ddtrace/appsec/_iast/_taint_tracking/__init__.py
@@ -12,6 +12,7 @@ if _is_python_version_supported():
     from ._native.aspect_helpers import _convert_escaped_text_to_tainted_text
     from ._native.aspect_helpers import as_formatted_evidence
     from ._native.aspect_helpers import common_replace
+    from ._native.aspect_format import _format_aspect
     from ._native.aspect_helpers import parse_params
     from ._native.initializer import active_map_addreses_size
     from ._native.initializer import create_context
@@ -74,6 +75,7 @@ __all__ = [
     "str_to_origin",
     "origin_to_str",
     "common_replace",
+    "_format_aspect",
     "as_formatted_evidence",
     "parse_params",
     "num_objects_tainted",

ddtrace/appsec/_iast/_taint_tracking/_native.cpp
@@ -13,7 +13,7 @@
 #include "Aspects/AspectJoin.h"
 #include "Aspects/AspectOperatorAdd.h"
 #include "Aspects/AspectSlice.h"
-#include "Aspects/_aspects_helpers.h"
+#include "Aspects/_aspects_exports.h"
 #include "Constants.h"
 #include "Initializer/_initializer.h"
 #include "TaintTracking/_taint_tracking.h"

commit 4626db49d7c1de4a78e148c887994deb786c2f55

fix(iast): format aspect propagation error (#7884)
IAST: Fix propagation error on ``.format`` string method.

Error partially introduced in:
https://github.com/DataDog/dd-trace-py/pull/7772
## Checklist

- [x] Change(s) are motivated and described in the PR description.
- [x] Testing strategy is described if automated tests are not included
in the PR.
- [x] Risk is outlined (performance impact, potential for breakage,
maintainability, etc).
- [x] Change is maintainable (easy to change, telemetry, documentation).
- [x] [Library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
are followed. If no release note is required, add label
`changelog/no-changelog`.
- [x] Documentation is included (in-code, generated user docs, [public
corp docs](https://github.com/DataDog/documentation/)).
- [x] Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))

## Reviewer Checklist

- [x] Title is accurate.
- [x] No unnecessary changes are introduced.
- [x] Description motivates each change.
- [x] Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes unless absolutely necessary.
- [x] Testing strategy adequately addresses listed risk(s).
- [x] Change is maintainable (easy to change, telemetry, documentation).
- [x] Release note makes sense to a user of the library.
- [x] Reviewer has explicitly acknowledged and discussed the performance
implications of this PR as reported in the benchmarks PR comment.
- [x] Backport labels are set in a manner that is consistent with the
[release branch maintenance
policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
- [x] If this PR touches code that signs or publishes builds or
packages, or handles credentials of any kind, I've requested a review
from `@DataDog/security-design-and-guidance`.
- [x] This PR doesn't touch any of that.

ddtrace/appsec/_iast/_taint_tracking/Aspects/AspectFormat.cpp
@@ -17,16 +17,23 @@ api_format_aspect(StrType& candidate_text,
         py::list new_args;
         py::dict new_kwargs;
         for (const auto arg : args) {
-            auto str_arg = py::cast<py::str>(arg);
-            auto n_arg = _all_as_formatted_evidence<py::str>(str_arg, TagMappingMode::Mapper);
-            new_args.append(n_arg);
+            if (is_text(arg.ptr())) {
+                auto str_arg = py::cast<py::str>(arg);
+                auto n_arg = _all_as_formatted_evidence<py::str>(str_arg, TagMappingMode::Mapper);
+                new_args.append(n_arg);
+            } else {
+                new_args.append(arg);
+            }
         }
-        for (auto [key, value] : new_kwargs) {
-            auto str_value = py::cast<py::str>(value);
-            auto n_value = _all_as_formatted_evidence<py::str>(str_value, TagMappingMode::Mapper);
-            new_kwargs[key] = n_value;
+        for (auto [key, value] : kwargs) {
+            if (is_text(value.ptr())) {
+                auto str_value = py::cast<py::str>(value);
+                auto n_value = _all_as_formatted_evidence<py::str>(str_value, TagMappingMode::Mapper);
+                new_kwargs[key] = n_value;
+            } else {
+                new_kwargs[key] = value;
+            }
         }
-
         StrType new_template_format =
           py::getattr(new_template, "format")(*(py::cast<py::tuple>(new_args)), **new_kwargs);
         std::tuple result = _convert_escaped_text_to_taint_text<StrType>(new_template_format, ranges_orig);

releasenotes/notes/iast-fix-propagation-error-b59b1b0db93c0f23.yaml
@@ -1,0 +1,4 @@
+---
+fixes:
+  - |
+    Vulnerability Management for Code-level (IAST): Fix propagation error on ``.format`` string method.

tests/appsec/iast/aspects/test_format_aspect_fixtures.py
@@ -109,7 +109,6 @@ class TestOperatorFormatReplacement(BaseReplacement):
             escaped_expected_result=":+-<input1>template⚠️<input1>-+: " ":+-<input2>parameter⚠️<input2>-+:",
         )
 
-    @pytest.mark.skip(reason="Migrating format_aspect to C++ breaks this test")
     def test_format_when_tainted_template_range_no_brackets_and_param_not_str_then_tainted(self):
         # type: () -> None
         self._assert_format_result(
@@ -119,7 +118,6 @@ class TestOperatorFormatReplacement(BaseReplacement):
             escaped_expected_result=":+-<input1>template<input1>-+: 3.14",
         )
 
-    @pytest.mark.skip(reason="Migrating format_aspect to C++ breaks this test")
     def test_format_when_tainted_template_range_with_brackets_and_param_not_str_then_tainted(self):
         # type: () -> None
         self._assert_format_result(
@@ -159,9 +157,8 @@ class TestOperatorFormatReplacement(BaseReplacement):
         assert as_formatted_evidence(res) == "-1234 6 1"
 
         string_input = create_taint_range_with_format(":+--12-+:34 {} {test_var}")
-        mod.do_args_kwargs_1(string_input, 6, test_var=1)  # pylint: disable=no-member
-        # TODO format with params doesn't work correctly
-        #  assert as_formatted_evidence(res) == ":+--12-+:34 6 1"
+        res = mod.do_args_kwargs_1(string_input, 6, test_var=1)  # pylint: disable=no-member
+        assert as_formatted_evidence(res) == ":+--12-+:34 6 1"
 
     def test_format_with_one_argument_args_and_kwargs(self):  # type: () -> None
         string_input = "-1234 {} {} {test_var}"
@@ -173,9 +170,8 @@ class TestOperatorFormatReplacement(BaseReplacement):
         assert as_formatted_evidence(res) == "-1234 1 6 1"
 
         string_input = create_taint_range_with_format(":+--12-+:34 {} {} {test_var}")
-        mod.do_args_kwargs_2(string_input, 6, test_var=1)  # pylint: disable=no-member
-        # TODO format with params doesn't work correctly
-        #  as_formatted_evidence(res) == ":+--12-+:34 1 6 1"
+        res = mod.do_args_kwargs_2(string_input, 6, test_var=1)  # pylint: disable=no-member
+        assert as_formatted_evidence(res) == ":+--12-+:34 1 6 1"
 
     def test_format_with_two_argument_args_and_kwargs(self):  # type: () -> None
         string_input = "-1234 {} {} {} {test_var}"
@@ -187,9 +183,8 @@ class TestOperatorFormatReplacement(BaseReplacement):
         assert as_formatted_evidence(res) == "-1234 1 2 6 1"
 
         string_input = create_taint_range_with_format(":+--12-+:34 {} {} {} {test_var}")
-        mod.do_args_kwargs_3(string_input, 6, test_var=1)  # pylint: disable=no-member
-        # TODO format with params doesn't work correctly
-        #  as_formatted_evidence(res) == ":+--12-+:34 1 2 6 1"
+        res = mod.do_args_kwargs_3(string_input, 6, test_var=1)  # pylint: disable=no-member
+        assert as_formatted_evidence(res) == ":+--12-+:34 1 2 6 1"
 
     def test_format_with_two_argument_two_keywordargument_args_kwargs(self):  # type: () -> None
         string_input = "-1234 {} {} {} {test_kwarg} {test_var}"
@@ -201,9 +196,8 @@ class TestOperatorFormatReplacement(BaseReplacement):
         assert as_formatted_evidence(res) == "-1234 1 2 6 3 1"
 
         string_input = create_taint_range_with_format(":+--12-+:34 {} {} {} {test_kwarg} {test_var}")
-        mod.do_args_kwargs_4(string_input, 6, test_var=1)  # pylint: disable=no-member
-        # TODO format with params doesn't work correctly
-        #  as_formatted_evidence(res) == ":+--12-+:34 1 2 6 3 1"
+        res = mod.do_args_kwargs_4(string_input, 6, test_var=1)  # pylint: disable=no-member
+        assert as_formatted_evidence(res) == ":+--12-+:34 1 2 6 3 1"
 
     def test_format_when_tainted_template_range_special_then_tainted_result(self):  # type: () -> None
         self._assert_format_result(

tests/appsec/iast/aspects/test_format_map_aspect_fixtures.py
@@ -33,23 +33,19 @@ class TestOperatorFormatMapReplacement(BaseReplacement):
 
         assert as_formatted_evidence(result, tag_mapping_function=None) == escaped_expected_result
 
-    @pytest.mark.skipif(not hasattr("", "format_map"), reason="Method format_map not supported")
     def test_format_map_when_template_is_none_then_raises_attribute_error(self):  # type: () -> None
         with pytest.raises(AttributeError):
             mod.do_format_map(None, {})
 
-    @pytest.mark.skipif(not hasattr("", "format_map"), reason="Method format_map not supported")
     def test_format_map_when_parameter_is_none_then_raises_type_error(self):  # type: () -> None
         with pytest.raises(TypeError):
             assert mod.do_format_map("{key}", None) == "None"
 
-    @pytest.mark.skipif(not hasattr("", "format_map"), reason="Method format_map not supported")
     def test_format_map_when_no_tainted_strings_then_no_tainted_result(self):  # type: () -> None
         result = mod.do_format_map("template {key}", {"key": "parameter"})
         assert result, "template parameter"
         assert not get_ranges(result)
 
-    @pytest.mark.skipif(not hasattr("", "format_map"), reason="Method format_map not supported")
     def test_format_map_when_tainted_parameter_then_tainted_result(self):  # type: () -> None
         self._assert_format_map_result(
             taint_escaped_template="template {key}",
@@ -58,7 +54,6 @@ class TestOperatorFormatMapReplacement(BaseReplacement):
             escaped_expected_result="template :+-<input1>parameter<input1>-+:",
         )
 
-    @pytest.mark.skipif(not hasattr("", "format_map"), reason="Method format_map not supported")
     def test_format_map_when_tainted_template_range_no_brackets_then_tainted_result(self):  # type: () -> None
         self._assert_format_map_result(
             taint_escaped_template=":+-<input1>template<input1>-+: {key}",
@@ -67,7 +62,6 @@ class TestOperatorFormatMapReplacement(BaseReplacement):
             escaped_expected_result=":+-<input1>template<input1>-+: parameter",
         )
 
-    @pytest.mark.skipif(not hasattr("", "format_map"), reason="Method format_map not supported")
     def test_format_map_when_tainted_template_range_with_brackets_then_tainted_result(self):  # type: () -> None
         self._assert_format_map_result(
             taint_escaped_template="template :+-<input1>{key}<input1>-+:",
@@ -76,7 +70,6 @@ class TestOperatorFormatMapReplacement(BaseReplacement):
             escaped_expected_result="template :+-<input1>parameter<input1>-+:",
         )
 
-    @pytest.mark.skipif(not hasattr("", "format_map"), reason="Method format_map not supported")
     def test_format_map_when_tainted_template_range_no_brackets_and_tainted_param_then_tainted(
         self,
     ):  # type: () -> None
@@ -87,7 +80,6 @@ class TestOperatorFormatMapReplacement(BaseReplacement):
             escaped_expected_result=":+-<input1>template<input1>-+: " ":+-<input2>parameter<input2>-+:",
         )
 
-    @pytest.mark.skipif(not hasattr("", "format_map"), reason="Method format_map not supported")
     def test_format_map_when_tainted_template_range_with_brackets_and_tainted_param_then_tainted(
         self,
     ):  # type: () -> None
@@ -98,7 +90,6 @@ class TestOperatorFormatMapReplacement(BaseReplacement):
             escaped_expected_result=":+-<input1>template <input1>-+:" ":+-<input2>parameter<input2>-+:",
         )
 
-    @pytest.mark.skipif(not hasattr("", "format_map"), reason="Method format_map not supported")
     def test_format_map_when_ranges_overlap_then_give_preference_to_ranges_from_parameter(self):  # type: () -> None
         self._assert_format_map_result(
             taint_escaped_template=":+-<input1>template {key} range overlapping<input1>-+:",
@@ -109,7 +100,6 @@ class TestOperatorFormatMapReplacement(BaseReplacement):
             ":+-<input1> range overlapping<input1>-+:",
         )
 
-    @pytest.mark.skipif(not hasattr("", "format_map"), reason="Method format_map not supported")
     def test_format_map_when_tainted_str_emoji_strings_then_tainted_result(self):  # type: () -> None
         self._assert_format_map_result(
             taint_escaped_template=":+-<input1>template⚠️<input1>-+: {key}",
@@ -118,7 +108,6 @@ class TestOperatorFormatMapReplacement(BaseReplacement):
             escaped_expected_result=":+-<input1>template⚠️<input1>-+: " ":+-<input2>parameter⚠️<input2>-+:",
         )
 
-    @pytest.mark.skipif(not hasattr("", "format_map"), reason="Method format_map not supported")
     def test_format_map_when_tainted_template_range_no_brackets_and_param_not_str_then_tainted(
         self,
     ):  # type: () -> None
@@ -129,7 +118,6 @@ class TestOperatorFormatMapReplacement(BaseReplacement):
             escaped_expected_result=":+-<input1>template<input1>-+: 3.14",
         )
 
-    @pytest.mark.skipif(not hasattr("", "format_map"), reason="Method format_map not supported")
     def test_format_map_when_tainted_template_range_with_brackets_and_param_not_str_then_tainted(
         self,
     ):  # type: () -> None
@@ -140,7 +128,6 @@ class TestOperatorFormatMapReplacement(BaseReplacement):
             escaped_expected_result=":+-<input1>template 3.14<input1>-+:",
         )
 
-    @pytest.mark.skipif(not hasattr("", "format_map"), reason="Method format_map not supported")
     def test_format_map_when_texts_tainted_and_contain_escape_sequences_then_result_uncorrupted(
         self,
     ):  # type: () -> None
@@ -153,7 +140,6 @@ class TestOperatorFormatMapReplacement(BaseReplacement):
             ":+-<0>my_code<0>-+:",
         )
 
-    @pytest.mark.skipif(not hasattr("", "format_map"), reason="Method format_map not supported")
     def test_format_map_when_parameter_value_already_present_in_template_then_range_is_correct(
         self,
     ):  # type: () -> None