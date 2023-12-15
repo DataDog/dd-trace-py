44ce14bfaed7b3e612aa9ebfbaa0df769b100652
Alberto Vara <alberto.vara@datadoghq.com>
2023-12-15 18:48:20 +0100

    chore(iast): packages tests (#7911)
    - Add IAST package tests to CI. This new step will take potentially many
    time
    - Add new package to test [idna](https://github.com/kjd/idna)
    
    Related PR: https://github.com/DataDog/dd-trace-py/pull/7856
    
    ## Checklist
    
    - [x] Change(s) are motivated and described in the PR description.
    - [x] Testing strategy is described if automated tests are not included
    in the PR.
    - [x] Risk is outlined (performance impact, potential for breakage,
    maintainability, etc).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] [Library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    are followed. If no release note is required, add label
    `changelog/no-changelog`.
    - [x] Documentation is included (in-code, generated user docs, [public
    corp docs](https://github.com/DataDog/documentation/)).
    - [x] Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    
    - [x] Title is accurate.
    - [x] No unnecessary changes are introduced.
    - [x] Description motivates each change.
    - [x] Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes unless absolutely necessary.
    - [x] Testing strategy adequately addresses listed risk(s).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] Release note makes sense to a user of the library.
    - [x] Reviewer has explicitly acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment.
    - [x] Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    - [x] If this PR touches code that signs or publishes builds or
    packages, or handles credentials of any kind, I've requested a review
    from `@DataDog/security-design-and-guidance`.
    - [x] This PR doesn't touch any of that.

--- tests/appsec/iast_packages/test_packages.py
+++ tests/appsec/iast_packages/test_packages.py
@@ -1,5 +1,6 @@
 import json
 
+import pip
 import pytest
 
 from tests.appsec.appsec_utils import flask_server
@@ -10,10 +11,19 @@ class PackageForTesting:
     package_version = ""
     url_to_test = ""
     expected_param = "test1234"
+    expected_result1 = ""
+    expected_result2 = ""
     xfail = False
 
-    def __init__(self, name):
+    def __init__(self, name, version, expected_param, expected_result1, expected_result2):
         self.package_name = name
+        self.package_version = version
+        if expected_param:
+            self.expected_param = expected_param
+        if expected_result1:
+            self.expected_result1 = expected_result1
+        if expected_result2:
+            self.expected_result2 = expected_result2
 
     @property
     def url(self):
@@ -22,9 +32,17 @@ class PackageForTesting:
     def __repr__(self):
         return f"{self.package_name}: {self.url_to_test}"
 
+    def install(self):
+        package_version = self.package_name + "==" + self.package_version
+        if hasattr(pip, "main"):
+            pip.main(["install", package_version])
+        else:
+            pip._internal.main(["install", package_version])
+
 
 PACKAGES = [
-    PackageForTesting("requests"),
+    PackageForTesting("requests", "2.31.0", "", "", ""),
+    PackageForTesting("idna", "3.6", "xn--eckwd4c7c.xn--zckzah", "ドメイン.テスト", "xn--eckwd4c7c.xn--zckzah"),
 ]
 
 
@@ -33,6 +51,7 @@ def setup():
         with flask_server(
             iast_enabled="false", tracer_enabled="true", remote_configuration_enabled="false", token=None
         ) as context:
+            package.install()
             _, client, pid = context
 
             try:
@@ -41,9 +60,12 @@ def setup():
                 assert response.status_code == 200
                 content = json.loads(response.content)
                 assert content["param"] == package.expected_param
+                assert content["result1"] == package.expected_result1
+                assert content["result2"] == package.expected_result2
                 assert content["params_are_tainted"] is False
-            except Exception:
+            except Exception as e:
                 package.xfail = True
+                print(e)
 
 
 @pytest.mark.parametrize("package", PACKAGES)
