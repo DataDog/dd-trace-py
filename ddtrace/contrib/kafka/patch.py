cc8c926d24b3a9b0b8e7d7737dfb5d66bcba003e
William Conti <58711692+wconti27@users.noreply.github.com>
2023-12-18 16:46:09 -0500

    feat(kafka): trace propagation for kafka consumer/producer spans (#7434)
    ## Description
    This PR adds trace context propagation for Kafka. The behavior is hidden
    behind a feature flag and is disabled by default to prevent any
    unexpected changes to users' kafka tracing
    
    
    ## Checklist
    
    - [x] Change(s) are motivated and described in the PR description.
    - [x] Testing strategy is described if automated tests are not included
    in the PR.
    - [x] Risk is outlined (performance impact, potential for breakage,
    maintainability, etc).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] [Library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    are followed. If no release note is required, add label
    `changelog/no-changelog`.
    - [x] Documentation is included (in-code, generated user docs, [public
    corp docs](https://github.com/DataDog/documentation/)).
    - [x] Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    
    - [x] Title is accurate.
    - [x] No unnecessary changes are introduced.
    - [x] Description motivates each change.
    - [x] Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes unless absolutely necessary.
    - [x] Testing strategy adequately addresses listed risk(s).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] Release note makes sense to a user of the library.
    - [x] Reviewer has explicitly acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment.
    - [x] Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    - [x] If this PR touches code that signs or publishes builds or
    packages, or handles credentials of any kind, I've requested a review
    from `@DataDog/security-design-and-guidance`.
    - [x] This PR doesn't touch any of that.
    
    ---------
    
    Co-authored-by: Munir Abdinur <munir.abdinur@datadoghq.com>

--- ddtrace/contrib/kafka/patch.py
+++ ddtrace/contrib/kafka/patch.py
@@ -1,3 +1,5 @@
+import os
+
 import confluent_kafka
 
 from ddtrace import config
@@ -9,6 +11,7 @@ from ddtrace.ext import SpanKind
 from ddtrace.ext import SpanTypes
 from ddtrace.ext import kafka as kafkax
 from ddtrace.internal import core
+from ddtrace.internal.compat import time_ns
 from ddtrace.internal.constants import COMPONENT
 from ddtrace.internal.constants import MESSAGING_SYSTEM
 from ddtrace.internal.logger import get_logger
@@ -17,9 +20,11 @@ from ddtrace.internal.schema import schematize_service_name
 from ddtrace.internal.schema.span_attribute_schema import SpanDirection
 from ddtrace.internal.utils import ArgumentError
 from ddtrace.internal.utils import get_argument_value
+from ddtrace.internal.utils import set_argument_value
 from ddtrace.internal.utils.formats import asbool
 from ddtrace.internal.utils.version import parse_version
 from ddtrace.pin import Pin
+from ddtrace.propagation.http import HTTPPropagator as Propagator
 
 
 _Producer = confluent_kafka.Producer
@@ -37,6 +42,8 @@ config._add(
     "kafka",
     dict(
         _default_service=schematize_service_name("kafka"),
+        distributed_tracing_enabled=asbool(os.getenv("DD_KAFKA_PROPAGATION_ENABLED", default=False)),
+        trace_empty_poll_enabled=asbool(os.getenv("DD_KAFKA_EMPTY_POLL_ENABLED", default=True)),
     ),
 )
 
@@ -177,6 +184,13 @@ def traced_produce(func, instance, args, kwargs):
         rate = config.kafka.get_analytics_sample_rate()
         if rate is not None:
             span.set_tag(ANALYTICS_SAMPLE_RATE_KEY, rate)
+
+        # inject headers with Datadog tags if trace propagation is enabled
+        if config.kafka.distributed_tracing_enabled:
+            # inject headers with Datadog tags:
+            headers = get_argument_value(args, kwargs, 6, "headers", True) or {}
+            Propagator.inject(span.context, headers)
+            args, kwargs = set_argument_value(args, kwargs, 6, "headers", headers)
         return func(*args, **kwargs)
 
 
@@ -185,41 +199,67 @@ def traced_poll(func, instance, args, kwargs):
     if not pin or not pin.enabled():
         return func(*args, **kwargs)
 
-    with pin.tracer.trace(
-        schematize_messaging_operation(kafkax.CONSUME, provider="kafka", direction=SpanDirection.PROCESSING),
-        service=trace_utils.ext_service(pin, config.kafka),
-        span_type=SpanTypes.WORKER,
-    ) as span:
+    # we must get start time now since execute before starting a span in order to get distributed context
+    # if it exists
+    start_ns = time_ns()
+    # wrap in a try catch and raise exception after span is started
+    err = None
+    try:
         message = func(*args, **kwargs)
-        span.set_tag_str(MESSAGING_SYSTEM, kafkax.SERVICE)
-        span.set_tag_str(COMPONENT, config.kafka.integration_name)
-        span.set_tag_str(SPAN_KIND, SpanKind.CONSUMER)
-        span.set_tag_str(kafkax.RECEIVED_MESSAGE, str(message is not None))
-        span.set_tag_str(kafkax.GROUP_ID, instance._group_id)
-        if message is not None:
-            core.set_item("kafka_topic", message.topic())
-            core.dispatch("kafka.consume.start", (instance, message, span))
+    except Exception as e:
+        err = e
+    ctx = None
+    if message and config.kafka.distributed_tracing_enabled and message.headers():
+        ctx = Propagator.extract(dict(message.headers()))
+    if message or config.kafka.trace_empty_poll_enabled:
+        with pin.tracer.start_span(
+            name=schematize_messaging_operation(kafkax.CONSUME, provider="kafka", direction=SpanDirection.PROCESSING),
+            service=trace_utils.ext_service(pin, config.kafka),
+            span_type=SpanTypes.WORKER,
+            child_of=ctx if ctx is not None else pin.tracer.context_provider.active(),
+            activate=True,
+        ) as span:
+            # reset span start time to before function call
+            span.start_ns = start_ns
 
-            message_key = message.key() or ""
-            message_offset = message.offset() or -1
-            span.set_tag_str(kafkax.TOPIC, message.topic())
+            span.set_tag_str(MESSAGING_SYSTEM, kafkax.SERVICE)
+            span.set_tag_str(COMPONENT, config.kafka.integration_name)
+            span.set_tag_str(SPAN_KIND, SpanKind.CONSUMER)
+            span.set_tag_str(kafkax.RECEIVED_MESSAGE, str(message is not None))
+            span.set_tag_str(kafkax.GROUP_ID, instance._group_id)
+            if message is not None:
+                core.set_item("kafka_topic", message.topic())
+                core.dispatch("kafka.consume.start", (instance, message, span))
 
-            # If this is a deserializing consumer, do not set the key as a tag since we
-            # do not have the serialization function
-            if (
-                (_DeserializingConsumer is not None and not isinstance(instance, _DeserializingConsumer))
-                or isinstance(message_key, str)
-                or isinstance(message_key, bytes)
-            ):
-                span.set_tag_str(kafkax.MESSAGE_KEY, message_key)
-            span.set_tag(kafkax.PARTITION, message.partition())
-            span.set_tag_str(kafkax.TOMBSTONE, str(len(message) == 0))
-            span.set_tag(kafkax.MESSAGE_OFFSET, message_offset)
-        span.set_tag(SPAN_MEASURED_KEY)
-        rate = config.kafka.get_analytics_sample_rate()
-        if rate is not None:
-            span.set_tag(ANALYTICS_SAMPLE_RATE_KEY, rate)
-        return message
+                message_key = message.key() or ""
+                message_offset = message.offset() or -1
+                span.set_tag_str(kafkax.TOPIC, message.topic())
+
+                # If this is a deserializing consumer, do not set the key as a tag since we
+                # do not have the serialization function
+                if (
+                    (_DeserializingConsumer is not None and not isinstance(instance, _DeserializingConsumer))
+                    or isinstance(message_key, str)
+                    or isinstance(message_key, bytes)
+                ):
+                    span.set_tag_str(kafkax.MESSAGE_KEY, message_key)
+                span.set_tag(kafkax.PARTITION, message.partition())
+                span.set_tag_str(kafkax.TOMBSTONE, str(len(message) == 0))
+                span.set_tag(kafkax.MESSAGE_OFFSET, message_offset)
+            span.set_tag(SPAN_MEASURED_KEY)
+            rate = config.kafka.get_analytics_sample_rate()
+            if rate is not None:
+                span.set_tag(ANALYTICS_SAMPLE_RATE_KEY, rate)
+
+            # raise exception if one was encountered
+            if err is not None:
+                raise err
+            return message
+    else:
+        if err is not None:
+            raise err
+        else:
+            return message
 
 
 def traced_commit(func, instance, args, kwargs):
