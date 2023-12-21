4f5a7274778b2c3e2322a4272740cd8441467417
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-12-21 11:37:20 +0100

    rename to Either

--- tests/appsec/utils.py
+++ tests/appsec/utils.py
@@ -1,12 +1,12 @@
 import sys
 
 
-class Mock_Alternative:
+class Either:
     def __init__(self, *possibilities):
         self.possibilities = possibilities
 
     def __eq__(self, other):
         if other not in self.possibilities:
-            print(f"Mock_Alternative: Expected {other} to be in {self.possibilities}", file=sys.stderr, flush=True)
+            print(f"Either: Expected {other} to be in {self.possibilities}", file=sys.stderr, flush=True)
             return False
         return True
