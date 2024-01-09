38b47d257abc73f6d8792e3b78b333b1e654757e
Munir Abdinur <munir.abdinur@datadoghq.com>
2024-01-09 16:18:45 -0500

    Update ddtrace/contrib/rq/__init__.py
    Co-authored-by: Emmett Butler <723615+emmettbutler@users.noreply.github.com>

--- ddtrace/contrib/rq/__init__.py
+++ ddtrace/contrib/rq/__init__.py
@@ -194,7 +194,7 @@ def traced_perform_job(rq, pin, func, instance, args, kwargs):
             try:
                 return func(*args, **kwargs)
             finally:
-                # Job status is returns None when ttl=0
+                # get_status() returns None when ttl=0
                 span.set_tag_str("job.status", job.get_status() or "None")
                 span.set_tag_str("job.origin", job.origin)
                 if job.is_failed:
