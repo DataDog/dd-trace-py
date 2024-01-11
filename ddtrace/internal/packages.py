84918573df1c48178085a9c128062671075c154f
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-01-11 11:28:49 +0100

    Handle possible valueerror in importlib
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/internal/packages.py
+++ ddtrace/internal/packages.py
@@ -98,16 +98,19 @@ def _package_file_mapping():
         mapping = {}
 
         for ilmd_d in il_md.distributions():
-            if ilmd_d is not None and ilmd_d.files is not None:
-                d = Distribution(name=ilmd_d.metadata["name"], version=ilmd_d.version, path=None)
-                for f in ilmd_d.files:
-                    if _is_python_source_file(f):
-                        # mapping[fspath(f.locate())] = d
-                        _path = fspath(f.locate())
-                        mapping[_path] = d
-                        _realp = os.path.realpath(_path)
-                        if _realp != _path:
-                            mapping[_realp] = d
+            try:
+                if ilmd_d is not None and ilmd_d.files is not None:
+                    d = Distribution(name=ilmd_d.metadata["name"], version=ilmd_d.version, path=None)
+                    for f in ilmd_d.files:
+                        if _is_python_source_file(f):
+                            # mapping[fspath(f.locate())] = d
+                            _path = fspath(f.locate())
+                            mapping[_path] = d
+                            _realp = os.path.realpath(_path)
+                            if _realp != _path:
+                                mapping[_realp] = d
+            except ValueError:
+                continue
 
         return mapping
 
