c4184e8d66a5124eb9a0b5d43f631f738d4343e4
Emmett Butler <723615+emmettbutler@users.noreply.github.com>
2024-01-18 09:36:07 -0800

    fix(tracing): enable settings to be unset (#8126) [backport 2.5] (#8142)
    The remote configuration backend omits settings when they are unset.
    This fix ensures that if settings are not set via remote config then we
    unset the previously set remote configuration values.
    
    Regression tests are added by matching the backend behaviour of not
    including the configuration item at all in the payload.
    
    - [x] Change(s) are motivated and described in the PR description.
    - [x] Testing strategy is described if automated tests are not included
    in the PR.
    - [x] Risk is outlined (performance impact, potential for breakage,
    maintainability, etc).
    - [x] Change is maintainable (easy to change, telemetry, documentation).
    - [x] [Library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    are followed. If no release note is required, add label
    `changelog/no-changelog`.
    - [x] Documentation is included (in-code, generated user docs, [public
    corp docs](https://github.com/DataDog/documentation/)).
    - [x] Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    Co-authored-by: Kyle Verhoog <kyle@verhoog.ca>

--- tests/internal/test_settings.py
+++ tests/internal/test_settings.py
@@ -12,22 +12,13 @@ def config():
 
 
 def _base_rc_config(cfg):
-    lib_config = {
-        "tracing_sampling_rate": None,
-        "log_injection_enabled": None,
-        "tracing_header_tags": None,
-        "tracing_tags": None,
-        "data_streams_enabled": None,
-    }
-    lib_config.update(cfg)
-
     return {
         "metadata": [],
         "config": [
             {
                 "action": "enable",
                 "service_target": {"service": None, "env": None},
-                "lib_config": lib_config,
+                "lib_config": cfg,
             }
         ],
     }
@@ -117,7 +108,7 @@ def _deleted_rc_config():
         },
     ],
 )
-def test_settings(testcase, config, monkeypatch):
+def test_settings_asdf(testcase, config, monkeypatch):
     for env_name, env_value in testcase.get("env", {}).items():
         monkeypatch.setenv(env_name, env_value)
         config._reset()
@@ -125,7 +116,7 @@ def test_settings(testcase, config, monkeypatch):
     for code_name, code_value in testcase.get("code", {}).items():
         setattr(config, code_name, code_value)
 
-    rc_items = testcase.get("rc", {}).items()
+    rc_items = testcase.get("rc", {})
     if rc_items:
         config._handle_remoteconfig(_base_rc_config(rc_items), None)
 
@@ -162,7 +153,7 @@ with tracer.trace("test") as span:
     pass
 assert span.get_metric("_dd.rule_psr") == 0.2
 
-config._handle_remoteconfig(_base_rc_config({"tracing_sampling_rate": None}))
+config._handle_remoteconfig(_base_rc_config({}))
 with tracer.trace("test") as span:
     pass
 assert span.get_metric("_dd.rule_psr") == 0.1
@@ -178,7 +169,7 @@ with tracer.trace("test") as span:
     pass
 assert span.get_metric("_dd.rule_psr") == 0.4
 
-config._handle_remoteconfig(_base_rc_config({"tracing_sampling_rate": None}))
+config._handle_remoteconfig(_base_rc_config({}))
 with tracer.trace("test") as span:
     pass
 assert span.get_metric("_dd.rule_psr") == 0.3
@@ -216,7 +207,7 @@ with tracer.trace("test") as span:
     pass
 assert span.get_tag("team") == "onboarding", span._meta
 
-config._handle_remoteconfig(_base_rc_config({"tracing_tags": None}))
+config._handle_remoteconfig(_base_rc_config({}))
 with tracer.trace("test") as span:
     pass
 assert span.get_tag("team") == "apm"
