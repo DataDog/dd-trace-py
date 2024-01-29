82e4f78b336d95c7c1b2254b092188b757087f8c
Romain Komorn <romain.komorn@datadoghq.com>
2024-01-29 10:30:31 +0000

    don't use CIVisibility if API key causes 403

--- tests/contrib/pytest/test_pytest_snapshot.py
+++ tests/contrib/pytest/test_pytest_snapshot.py
@@ -1,7 +1,9 @@
 import subprocess
+from unittest import mock
 
 import pytest
 
+from ddtrace.internal.ci_visibility.recorder import _CIVisibilitySettings
 from tests.utils import TracerTestCase
 from tests.utils import override_env
 from tests.utils import snapshot
@@ -71,7 +73,11 @@ class PytestSnapshotTestCase(TracerTestCase):
                 DD_PATCH_MODULES="sqlite3:false",
             )
         ):
-            subprocess.run(["ddtrace-run", "coverage", "run", "--include=tools.py", "-m", "pytest", "--ddtrace"])
+            with mock.patch(
+                "ddtrace.internal.ci_visibility.recorder.CIVisiblity._check_settings_api",
+                return_value=_CIVisibilitySettings(False, False, False, False),
+            ):
+                subprocess.run(["ddtrace-run", "coverage", "run", "--include=tools.py", "-m", "pytest", "--ddtrace"])
 
     @snapshot(ignores=SNAPSHOT_IGNORES)
     def test_pytest_wont_include_lines_pct_if_report_empty(self):
@@ -107,4 +113,8 @@ class PytestSnapshotTestCase(TracerTestCase):
                 DD_PATCH_MODULES="sqlite3:false",
             )
         ):
-            subprocess.run(["ddtrace-run", "coverage", "run", "--include=nothing.py", "-m", "pytest", "--ddtrace"])
+            with mock.patch(
+                "ddtrace.internal.ci_visibility.recorder.CIVisiblity._check_settings_api",
+                return_value=_CIVisibilitySettings(False, False, False, False),
+            ):
+                subprocess.run(["ddtrace-run", "coverage", "run", "--include=nothing.py", "-m", "pytest", "--ddtrace"])
