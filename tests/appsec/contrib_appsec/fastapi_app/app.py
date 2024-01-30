30d90413fdc82fe0a9a443e9cbdb48dcf78891d3
Christophe Papazian <114495376+christophe-papazian@users.noreply.github.com>
2024-01-30 10:09:56 +0100

    chore(asm): move threats tests to appsec and use hatch (#8155)
    ## Motivation
    - ASM and APM have many tests dependant of each other. It would be
    better to make ASM tests more standalone.
    - ASM tests are different for each supported framework, it would be
    better to have each test applied to each supported framework.
    
    ## This PR
    
    - add 3 new test suites for asm features (for Django, Flask and FastAPI)
    `appsec_threats_*`
    - use hatch to manage the suits, allowing multiple versions of each
    framework to be tested for several python versions
    - use CircleCI parallelism to keep tests fast (currently 6 minutes
    average on CircleCI)
    - all tests are factorized in `tests/appsec/contrib_appsec/utils.py` and
    each framework provides the same interface to be able to pass the tests.
    It will also help support more frameworks later for ASM.
    - 9 tests were added, starting to cover some threats asm features.
    - pip freeze output is added to each test at start to be able to easily
    investigate for errors caused by dependencies.
    
    ## What this PR does not do
    - delete obsolete tests covered by the new tests suite in each
    /tests/contrib suite
    - cover all asm threats features 
    This will eventually be done in a subsequent PR.
    
    ## Checklist
    
    - [x] Change(s) are motivated and described in the PR description
    - [x] Testing strategy is described if automated tests are not included
    in the PR
    - [x] Risks are described (performance impact, potential for breakage,
    maintainability)
    - [x] Change is maintainable (easy to change, telemetry, documentation)
    - [x] [Library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    are followed or label `changelog/no-changelog` is set
    - [x] Documentation is included (in-code, generated user docs, [public
    corp docs](https://github.com/DataDog/documentation/))
    - [x] Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    - [x] If this PR changes the public interface, I've notified
    `@DataDog/apm-tees`.
    - [x] If change touches code that signs or publishes builds or packages,
    or handles credentials of any kind, I've requested a review from
    `@DataDog/security-design-and-guidance`.
    
    ## Reviewer Checklist
    
    - [x] Title is accurate
    - [x] All changes are related to the pull request's stated goal
    - [x] Description motivates each change
    - [x] Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - [x] Testing strategy adequately addresses listed risks
    - [x] Change is maintainable (easy to change, telemetry, documentation)
    - [x] Release note makes sense to a user of the library
    - [x] Author has acknowledged and discussed the performance implications
    of this PR as reported in the benchmarks PR comment
    - [x] Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

---
+++ tests/appsec/contrib_appsec/fastapi_app/app.py
@@ -0,0 +1,67 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+
+fake_secret_token = "DataDog"
+
+fake_db = {
+    "foo": {"id": "foo", "name": "Foo", "description": "This item's description is foo."},
+    "bar": {"id": "bar", "name": "Bar", "description": "The bartenders"},
+    "testUserID": {"userid": "testUserID", "name": "Test User"},
+}
+
+
+class Item(BaseModel):
+    id: str
+    name: str
+    description: Optional[str] = None
+
+
+class User(BaseModel):
+    userid: int
+    name: str
+
+
+def get_app():
+    app = FastAPI()
+
+    @app.get("/")
+    async def read_homepage():  # noqa: B008
+        return HTMLResponse("ok ASM", 200)
+
+    @app.get("/asm/{param_int:int}/{param_str:str}/")
+    @app.post("/asm/{param_int:int}/{param_str:str}/")
+    async def multi_view(param_int: int, param_str: str, request: Request):  # noqa: B008
+        query_params = dict(request.query_params)
+        body = {
+            "path_params": {"param_int": param_int, "param_str": param_str},
+            "query_params": query_params,
+            "headers": dict(request.headers),
+            "cookies": dict(request.cookies),
+            "body": (await request.body()).decode("utf-8"),
+            "method": request.method,
+        }
+        status = int(query_params.get("status", "200"))
+        return JSONResponse(body, status_code=status)
+
+    @app.get("/asm/")
+    @app.post("/asm/")
+    async def multi_view_no_param(request: Request):  # noqa: B008
+        query_params = dict(request.query_params)
+        body = {
+            "path_params": {"param_int": 0, "param_str": ""},
+            "query_params": query_params,
+            "headers": dict(request.headers),
+            "cookies": dict(request.cookies),
+            "body": (await request.body()).decode("utf-8"),
+            "method": request.method,
+        }
+        status = int(query_params.get("status", "200"))
+        return JSONResponse(body, status_code=status)
+
+    return app
