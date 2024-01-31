ed7bd322dd3dfcd7e5604eb6f1fc52a53e856bbe
sanchda <sanchda@users.noreply.github.com>
2024-01-31 16:36:58 +0000

    Style

--- setup.py
+++ setup.py
@@ -452,7 +452,7 @@ if not IS_PYSTON:
             )
         )
 
-    if platform.system() is "Linux" and is_64_bit_python():
+    if platform.system() == "Linux" and is_64_bit_python():
         ext_modules.append(
             CMakeExtension(
                 "ddtrace.internal.datadog.profiling._ddup",
