03641444c01baad4676d470823265d4241ed6f74
Yun Kim <yun.kim@datadoghq.com>
2024-02-06 18:32:56 -0500

    revert enabling in ddtrace-run

--- ddtrace/bootstrap/preload.py
+++ ddtrace/bootstrap/preload.py
@@ -87,9 +87,6 @@ if config._otel_enabled:
 
         set_tracer_provider(TracerProvider())
 
-if config._llmobs_enabled:
-    from ddtrace.llmobs import LLMObs
-    LLMObs.enable()
 
 if asbool(os.getenv("DD_TRACE_ENABLED", default=True)):
     from ddtrace import patch_all
