ef0ee55da227d64ccf9baa79c2a439a1dce1cea1
David Sanchez <838104+sanchda@users.noreply.github.com>
2024-02-06 14:05:04 -0600

    Revert "chore(profiling): make ddup native code robust against forking (#8271)"
    This reverts commit 7ee7db3aaa6fd1656784c941c725c0b7485aff1a.

--- ddtrace/internal/datadog/profiling/cmake/FindCppcheck.cmake
+++
@@ -1,8 +0,0 @@
-include(ExternalProject)
-
-ExternalProject_Add(cppcheck_project
-  GIT_REPOSITORY https://github.com/danmar/cppcheck.git
-  GIT_TAG "2.13.3"
-  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/cppcheck
-)
-set(CPPCHECK_EXECUTABLE ${CMAKE_BINARY_DIR}/cppcheck/bin/cppcheck)
