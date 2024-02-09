c17103ac06336c702e38b248d359b1a8838a7033
Federico Mon <federico.mon@datadoghq.com>
2024-02-09 18:48:35 +0100

    refactor(iast): WIP: avoid converting type annotations

--- ddtrace/appsec/_iast/_ast/ast_patching.py
+++ ddtrace/appsec/_iast/_ast/ast_patching.py
@@ -23,8 +23,10 @@ from .visitor import AstVisitor
 
 # Prefixes for modules where IAST patching is allowed
 IAST_ALLOWLIST = ("tests.appsec.iast", "sqlalchemy")  # type: tuple[str, ...]
-IAST_DENYLIST = ("ddtrace", "pkg_resources", "sqlalchemy.orm.attributes")  # type: tuple[str, ...]
-
+IAST_DENYLIST = (
+    "ddtrace",
+    "pkg_resources",
+)  # type: tuple[str, ...]
 
 if IAST.PATCH_MODULES in os.environ:
     IAST_ALLOWLIST += tuple(os.environ[IAST.PATCH_MODULES].split(IAST.SEP_MODULES))
