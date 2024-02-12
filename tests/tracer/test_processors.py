e88f5221bf8f70dcf56200b072511952b6cb2163
Munir Abdinur <munir_abdinur@hotmail.com>
2024-02-12 13:05:52 -0500

    regression test

--- tests/tracer/test_processors.py
+++ tests/tracer/test_processors.py
@@ -372,6 +372,21 @@ def test_span_creation_metrics():
         )
 
 
+def test_span_creation_metrics_disabled_telemetry():
+    """Test that telemetry metrics are not queued when telemetry is disabled"""
+    aggr = SpanAggregator(
+        partial_flush_enabled=False, partial_flush_min_spans=0, trace_processors=[], writer=DummyWriter()
+    )
+
+    with override_global_config(dict(_telemetry_enabled=False)):
+        with mock.patch("ddtrace.internal.telemetry.telemetry_writer.add_count_metric") as mock_tm:
+            for _ in range(300):
+                span = Span("span", on_finish=[aggr.on_span_finish])
+                aggr.on_span_start(span)
+                span.finish()
+        mock_tm.assert_not_called()
+
+
 def test_single_span_sampling_processor():
     """Test that single span sampling tags are applied to spans that should get sampled"""
 
