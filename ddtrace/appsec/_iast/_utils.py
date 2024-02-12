dbe50fa7486bf55c50f7723ec170b3199897d99d
Federico Mon <federico.mon@datadoghq.com>
2024-02-12 14:16:22 +0100

    fix(iast): avoid converting most type annotations

--- ddtrace/appsec/_iast/_utils.py
+++ ddtrace/appsec/_iast/_utils.py
@@ -105,3 +105,31 @@ def _iast_report_to_str(data):
             return json.JSONEncoder.default(self, obj)
 
     return json.dumps(attr.asdict(data, filter=lambda attr, x: x is not None), cls=OriginTypeEncoder)
+
+
+def _print_patched_code(module_path, module_name):  # type: (str, str) -> str
+    """
+    Print the patched code to stdout, for debugging purposes.
+    """
+    import astunparse
+
+    from ddtrace.appsec._iast._ast.ast_patching import get_encoding
+    from ddtrace.appsec._iast._ast.ast_patching import visit_ast
+
+    with open(module_path, "r", encoding=get_encoding(module_path)) as source_file:
+        source_text = source_file.read()
+
+        new_source = visit_ast(
+            source_text,
+            module_path,
+            module_name=module_name,
+        )
+
+        new_code = astunparse.unparse(new_source)
+        return new_code
+
+
+if __name__ == "__main__":
+    module_path = sys.argv[1]
+    module_name = sys.argv[2]
+    print(_print_patched_code(module_path, module_name))
