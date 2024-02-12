1e96fa51c500fbce2c36a1d48e26616c44a8e71f
Federico Mon <federico.mon@datadoghq.com>
2024-02-12 17:54:06 +0100

    chore(iast): improve test

--- tests/appsec/iast/_ast/fixtures/annotated_code.py
+++ tests/appsec/iast/_ast/fixtures/annotated_code.py
@@ -13,6 +13,9 @@ _T_co = TypeVar("_T_co", bound=Any, covariant=True)
 
 foo: Optional[int] = 42
 
+MySequenceType = Sequence[int]
+# bar = my_list[0]
+
 
 # function with positional-only argument
 def bar(arg: Dict[str, Sequence[Any]], /) -> Tuple[str, ...]:
