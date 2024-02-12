5cca6ccff7cb8415d2d4ad86ac8280dfe69968cf
Federico Mon <federico.mon@datadoghq.com>
2024-02-12 17:32:52 +0100

    chore(iast): improve test

--- tests/appsec/iast/_ast/test_ast_patching_type_hints.py
+++ tests/appsec/iast/_ast/test_ast_patching_type_hints.py
@@ -8,4 +8,9 @@ def test_no_index_aspects():
     """
     patched_code = _get_patched_code("tests/appsec/iast/_ast/fixtures/annotated_code.py", "annotated_code")
 
+    # Makes sure no subscripts are patched:
     assert "index_aspect" not in patched_code
+
+    # Makes sure all add operators are patched:
+    assert "ddtrace_aspects.add_aspect" in patched_code
+    assert "+" not in patched_code
