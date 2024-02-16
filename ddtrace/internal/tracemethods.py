60f2eb87c9a2afcd4061fefaf964a4220c957540
erikayasuda <erika.yasuda@datadoghq.com>
2024-02-16 10:04:01 -0500

    updated trace methods to accept both legacy and updated dd_trace_methods syntax

--- ddtrace/internal/tracemethods.py
+++ ddtrace/internal/tracemethods.py
@@ -1,19 +1,57 @@
-import typing  # noqa:F401
+from typing import List
 
 import wrapt
 
 
-def _parse_trace_methods(raw_dd_trace_methods):
-    # type: (str) -> typing.List[str]
-    """Return the methods to trace based on the specification of DD_TRACE_METHODS.
+def _parse_trace_methods(raw_dd_trace_methods: str) -> List[(str, str)]:
+    """Return a list of the module,methodname tuples to trace based on the
+    specification of DD_TRACE_METHODS.
 
-    DD_TRACE_METHODS is specified to be FullyQualifiedClassOrModuleName[comma-separated-methods]
+    DD_TRACE_METHODS is specified to be FullyQualifiedModuleName:comma-separated-methods;...
+    The square bracket notation will be deprecated in favor of this new syntax
 
     Note that support for wildcard methods ([*]) is not implemented.
-    """
-    if not raw_dd_trace_methods:
-        return []
+    """ 
+    dd_trace_methods = []
+    for qualified_methods in raw_dd_trace_methods.split(";"):
+        # Validate that methods are specified
+        if ":" not in qualified_methods:
+            raise ValueError(
+                (
+                    "Invalid DD_TRACE_METHODS: %s. "
+                    "Methods must be specified after a colon following the fully qualified module."
+                )
+                % qualified_methods
+            )
 
+        # Store the prefix and the methods  (eg. for "foo.bar.baz:qux,quux",
+        # this is "foo.bar.baz" for the prefix and "qux,quux" for the methods)
+        qualified_method_prefix, methods = qualified_methods.split(":")
+
+        if qualified_method_prefix == "__main__":
+            # __main__ cannot be used since the __main__ that exists now is not the same as the __main__ that the user
+            # application will have. __main__ when sitecustomize module is run is the builtin __main__.
+            raise ValueError(
+                "Invalid DD_TRACE_METHODS: %s. Methods cannot be traced on the __main__ module." % qualified_methods
+            )
+
+        # Add the methods to the list of methods to trace
+        for method in methods.split(","):
+            if not str.isidentifier(method):
+                raise ValueError(
+                    "Invalid method name: %r. %s"
+                    % (
+                        method,
+                        "You might have a trailing comma."
+                        if method == ""
+                        else "Method names must be valid Python identifiers.",
+                    )
+                )
+            dd_trace_methods.append((qualified_method_prefix, method))
+    return dd_trace_methods    
+
+
+def _parse_legacy_trace_methods(raw_dd_trace_methods: str) -> List[str]:
     dd_trace_methods = []
     for qualified_methods in raw_dd_trace_methods.split(";"):
         # Validate that methods are specified
@@ -56,34 +94,38 @@ def _parse_trace_methods(raw_dd_trace_methods):
                     )
                 )
             dd_trace_methods.append("%s.%s" % (qualified_method_prefix, method))
-
     return dd_trace_methods
 
 
-def _install_trace_methods(raw_dd_trace_methods):
-    # type: (str) -> None
+def _install_trace_methods(raw_dd_trace_methods: str) -> None:
     """Install tracing on the given methods."""
-    for qualified_method in _parse_trace_methods(raw_dd_trace_methods):
-        # We don't know if the method is a class method or a module method, so we need to assume it's a module
-        # and if the import fails then go a level up and try again.
-        base_module_guess = ".".join(qualified_method.split(".")[:-1])
-        method_name = qualified_method.split(".")[-1]
-        module = None
+    if "[" in raw_dd_trace_methods:
+        # Using legacy syntax
+        for qualified_method in _parse_legacy_trace_methods(raw_dd_trace_methods):
+            # We don't know if the method is a class method or a module method, so we need to assume it's a module
+            # and if the import fails then go a level up and try again.
+            base_module_guess = ".".join(qualified_method.split(".")[:-1])
+            method_name = qualified_method.split(".")[-1]
+            module = None
 
-        while base_module_guess:
-            try:
-                module = __import__(base_module_guess)
-            except ImportError:
-                # Add the class to the method name
-                method_name = "%s.%s" % (base_module_guess.split(".")[-1], method_name)
-                base_module_guess = ".".join(base_module_guess.split(".")[:-1])
-            else:
-                break
+            while base_module_guess:
+                try:
+                    module = __import__(base_module_guess)
+                except ImportError:
+                    # Add the class to the method name
+                    method_name = "%s.%s" % (base_module_guess.split(".")[-1], method_name)
+                    base_module_guess = ".".join(base_module_guess.split(".")[:-1])
+                else:
+                    break
 
-        if module is None:
-            raise ImportError("Could not import module for %r" % qualified_method)
+            if module is None:
+                raise ImportError("Could not import module for %r" % qualified_method)
 
-        trace_method(base_module_guess, method_name)
+            trace_method(base_module_guess, method_name)
+    else:
+        # Using updated syntax, no need to try to import
+        for module, method_name in _parse_trace_methods(raw_dd_trace_methods):
+            trace_method(module, method_name)
 
 
 def trace_method(module, method_name):
