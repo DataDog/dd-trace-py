22edb7a73a1fc004b59811fdece56bb2d268465a
Emmett Butler <723615+emmettbutler@users.noreply.github.com>
2024-02-20 13:55:35 -0800

    Update __init__.py

--- ddtrace/tracing/__init__.py
+++ ddtrace/tracing/__init__.py
@@ -1,5 +1,5 @@
 from ddtrace._trace import trace_handlers  # noqa: F401
-from ddtrace._trace._span_links import SpanLink  # noqa: F401
+from ddtrace._trace._span_link import SpanLink  # noqa: F401
 from ddtrace.internal.utils.deprecations import DDTraceDeprecationWarning
 from ddtrace.vendor.debtcollector import deprecate
 
