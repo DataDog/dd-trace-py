d83d7ef65fbeb7ba862205b3657a02fcbd12e90f
Christophe Papazian <christophe.papazian@datadoghq.com>
2024-02-21 15:57:30 +0100

    ensure writer is recreated when appsec_enabled status is explicitely set

--- ddtrace/_trace/tracer.py
+++ ddtrace/_trace/tracer.py
@@ -452,7 +452,7 @@ class Tracer(object):
 
         if writer is not None:
             self._writer = writer
-        elif any(x is not None for x in [new_url, api_version, sampler, dogstatsd_url]):
+        elif any(x is not None for x in [new_url, api_version, sampler, dogstatsd_url, appsec_enabled]):
             if self._asm_enabled:
                 api_version = "v0.4"
             self._writer = AgentWriter(
