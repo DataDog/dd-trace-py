e778fb35fe1e8f62b60753e7f564a3100bcb839b
Yun Kim <yun.kim@datadoghq.com>
2024-02-21 15:51:03 -0500

    Add inline annotations + tagging, tests

--- ddtrace/llmobs/_llmobs.py
+++ ddtrace/llmobs/_llmobs.py
@@ -4,6 +4,7 @@ from typing import Any
 from typing import Dict
 from typing import List
 from typing import Optional
+from typing import Union
 
 import ddtrace
 from ddtrace import Span
@@ -75,6 +76,158 @@ class LLMObs(Service):
         cls.enabled = False
         log.debug("%s disabled", cls.__name__)
 
+    def _start_span(self, operation_kind: str, name: str = None, **kwargs) -> Span:
+        if name is None:
+            name = operation_kind
+        span = self.tracer.trace(name, resource=operation_kind, span_type=SpanTypes.LLM)
+        span_meta = {}
+        for k, v in kwargs.items():
+            span_meta.update({k: v})
+        span_meta["span.kind"] = operation_kind
+        span.set_tag_str("ml_obs.meta", json.dumps(span_meta))
+        return span
+
+    @classmethod
+    def llm(
+        cls,
+        model: str,
+        name: Optional[str] = None,
+        model_provider: Optional[str] = None,
+        session_id: Optional[str] = None,
+    ) -> Span:
+        if cls.enabled is False:
+            raise Exception("LLMObs.llm() cannot be used while LLMObs is disabled.")
+        if not model:
+            raise ValueError("model must be the specified name of the invoked model.")
+        if model_provider is None:
+            model_provider = "custom"
+        return cls._instance._start_span(
+            "llm", name, model_name=model, model_provider=model_provider, session_id=session_id
+        )
+
+    @classmethod
+    def tool(cls, name: Optional[str] = None, session_id: Optional[str] = None) -> Span:
+        if cls.enabled is False:
+            raise Exception("LLMObs.tool() cannot be used while LLMObs is disabled.")
+        return cls._instance._start_span("tool", name=name, session_id=session_id)
+
+    @classmethod
+    def task(cls, name: Optional[str] = None, session_id: Optional[str] = None) -> Span:
+        if cls.enabled is False:
+            raise Exception("LLMObs.task() cannot be used while LLMObs is disabled.")
+        return cls._instance._start_span("task", name=name, session_id=session_id)
+
+    @classmethod
+    def agent(cls, name: Optional[str] = None, session_id: Optional[str] = None) -> Span:
+        if cls.enabled is False:
+            raise Exception("LLMObs.agent() cannot be used while LLMObs is disabled.")
+        return cls._instance._start_span("agent", name=name, session_id=session_id)
+
+    @classmethod
+    def workflow(cls, name: Optional[str] = None, session_id: Optional[str] = None) -> Span:
+        if cls.enabled is False:
+            raise Exception("LLMObs.workflow() cannot be used while LLMObs is disabled.")
+        return cls._instance._start_span("workflow", name=name, session_id=session_id)
+
+    @classmethod
+    def tag(
+        cls,
+        span: Optional[Span] = None,
+        parameters: Optional[Dict[str, Any]] = None,
+        input_tags: Optional[Union[List[Dict[str, Any]], str]] = None,
+        output_tags: Optional[Union[List[Dict[str, Any]], str]] = None,
+    ):
+        """
+        Sets the parameter, input, and output tags for a given LLMObs span.
+        span: Span to annotate. If no span is provided, the current active span will be used.
+         Must be an LLMObs-type span.
+        parameters: Dictionary of input parameter key-value pairs such as max_tokens/temperature.
+         Will be mapped to span's meta.input.parameters.* fields.
+        input_tags: A single input string, or a list of dictionaries following {"content": "...", "role": "..."}.
+         Will be mapped to `meta.input.value` or `meta.input.messages.*`, respectively.
+         For llm span types, string inputs will be wrapped in a message dictionary.
+        output_tags: A single output string, or a list of dictionaries following {"content": "...", "role": "..."}.
+         Will be mapped to `meta.output.value` or `meta.output.messages.*`, respectively.
+         For llm span types, string outputs will be wrapped in a message dictionary.
+        """
+        if cls.enabled is False:
+            raise Exception("LLMObs.tag() cannot be used while LLMObs is disabled.")
+        if span is None:
+            span = cls._instance.tracer.current_span()
+            if span is None:
+                raise ValueError("No span provided and no active span found.")
+        if span.span_type != SpanTypes.LLM:
+            raise ValueError("span must be an LLM-type span.")
+        if span.finished:
+            log.warning("Cannot annotate a finished span.")
+            return
+        try:
+            span_meta = json.loads(span.get_tag("ml_obs.meta") or "{}")
+        except json.JSONDecodeError:
+            raise ValueError("Failed to deserialize existing ml_obs.meta tag.")
+        if parameters is not None:
+            span_meta = cls._tag_params(span_meta, parameters)
+        if input_tags is not None:
+            span_meta = cls._tag_span_input_output("input", span_meta, input_tags)
+        if output_tags is not None:
+            span_meta = cls._tag_span_input_output("output", span_meta, output_tags)
+        try:
+            span.set_tag_str("ml_obs.meta", json.dumps(span_meta))
+        except json.JSONDecodeError:
+            raise ValueError("Failed to serialize span meta.")
+
+    @staticmethod
+    def _tag_params(meta: Dict[str, Any], params: Dict[str, Any]) -> Dict[str, Any]:
+        if not isinstance(params, dict):
+            raise ValueError("parameters must be a dictionary of key-value pairs.")
+        if "input" not in meta:
+            meta["input"] = {"parameters": params}
+        elif "parameters" not in meta["input"]:
+            meta["input"]["parameters"] = params
+        elif "parameters" in meta["input"]:
+            meta["input"]["parameters"].update(params)
+        return meta
+
+    @staticmethod
+    def _tag_span_input_output(
+        io_type: str, meta: Dict[str, Any], io_tags: Union[List[Dict[str, Any]], str]
+    ) -> Dict[str, Any]:
+        """
+        Tags input/output for a given LLMObs span.
+        io_type: "input" or "output".
+        meta: Span's meta dictionary.
+        io_value: Input or output dictionary of key-value pairs to tag.
+        """
+        if io_type not in ("input", "output"):
+            raise ValueError("io_type must be either 'input' or 'output'.")
+        if not isinstance(io_tags, (str, list)):
+            raise TypeError(
+                "Invalid type, must be either a raw string or list of dictionaries with the format {'content': '...'}."
+            )
+        if isinstance(io_tags, list) and not all(isinstance(item, dict) for item in io_tags):
+            raise TypeError(
+                "Invalid list item type, must be a list of dictionaries with the format {'content': '...'}."
+            )
+        span_kind = meta.get("span.kind")
+        tags = {}
+        if not span_kind:
+            raise ValueError("Cannot tag input/output without a span.kind tag.")
+        if span_kind == "llm":
+            if isinstance(io_tags, str):
+                tags = {"messages": [{"content": io_tags}]}
+            elif isinstance(io_tags, list) and io_tags and isinstance(io_tags[0], dict):
+                tags = {"messages": io_tags}
+        else:
+            if isinstance(io_tags, str):
+                tags = {"value": io_tags}
+            else:
+                raise ValueError("Invalid input/output type for non-llm span. Must be a raw string.")
+        if io_type not in meta:
+            meta[io_type] = tags
+        else:
+            meta[io_type].update(tags)
+        return meta
+
 
 class LLMObsTraceProcessor(TraceProcessor):
     """
@@ -111,6 +264,8 @@ class LLMObsTraceProcessor(TraceProcessor):
     def submit_llmobs_span(self, span: Span) -> None:
         """Generate and submit an LLMObs span event to be sent to LLMObs."""
         meta = json.loads(span._meta.pop("ml_obs.meta", "{}"))
+        if "span.kind" not in meta:
+            meta["span.kind"] = span.resource
         if span.error:
             meta["error.message"] = span.get_tag("error.message")
         metrics = json.loads(span._meta.pop("ml_obs.metrics", "{}"))
@@ -120,11 +275,12 @@ class LLMObsTraceProcessor(TraceProcessor):
 
     def _llmobs_span_event(self, span: Span, meta: Dict[str, Any], metrics: Dict[str, Any]) -> Dict[str, Any]:
         """Span event object structure."""
+        session_id = meta.pop("session_id") or "{:x}".format(span.trace_id)
         return {
             "trace_id": "{:x}".format(span.trace_id),
             "span_id": str(span.span_id),
             "parent_id": str(self._get_llmobs_parent_id(span) or ""),
-            "session_id": "{:x}".format(span.trace_id),
+            "session_id": session_id,
             "tags": self._llmobs_tags(span, meta),
             "name": span.name,
             "error": span.error,
--- tests/llmobs/test_llmobs_service.py
+++ tests/llmobs/test_llmobs_service.py
@@ -1,53 +1,431 @@
+import json
+
 import mock
 import pytest
 
-from ddtrace.llmobs import LLMObs
+from ddtrace.llmobs import LLMObs as llmobs_service
 from ddtrace.llmobs._llmobs import LLMObsTraceProcessor
 from tests.utils import DummyTracer
 from tests.utils import override_global_config
 
 
+@pytest.fixture
+def mock_logs():
+    with mock.patch("ddtrace.llmobs._llmobs.log") as mock_logs:
+        yield mock_logs
+
+
+@pytest.fixture
+def mock_llmobs_writer():
+    patcher = mock.patch("ddtrace.llmobs._llmobs.LLMObsWriter")
+    LLMObsWriterMock = patcher.start()
+    m = mock.MagicMock()
+    LLMObsWriterMock.return_value = m
+    yield m
+    patcher.stop()
+
+
+@pytest.fixture
+def LLMObs(mock_llmobs_writer):
+    dummy_tracer = DummyTracer()
+    llmobs_service.enable(tracer=dummy_tracer)
+    yield llmobs_service
+    llmobs_service.disable()
+
+
 def test_llmobs_service_enable():
     with override_global_config(dict(_dd_api_key="<not-a-real-api-key>")):
         dummy_tracer = DummyTracer()
-        LLMObs.enable(tracer=dummy_tracer)
-        llmobs_instance = LLMObs._instance
+        llmobs_service.enable(tracer=dummy_tracer)
+        llmobs_instance = llmobs_service._instance
         assert llmobs_instance is not None
-        assert LLMObs.enabled
+        assert llmobs_service.enabled
         assert llmobs_instance.tracer == dummy_tracer
         assert any(isinstance(tracer_filter, LLMObsTraceProcessor) for tracer_filter in dummy_tracer._filters)
-        LLMObs.disable()
+        llmobs_service.disable()
 
 
 def test_llmobs_service_disable():
     with override_global_config(dict(_dd_api_key="<not-a-real-api-key>")):
         dummy_tracer = DummyTracer()
-        LLMObs.enable(tracer=dummy_tracer)
-        LLMObs.disable()
-        assert LLMObs._instance is None
-        assert LLMObs.enabled is False
+        llmobs_service.enable(tracer=dummy_tracer)
+        llmobs_service.disable()
+        assert llmobs_service._instance is None
+        assert llmobs_service.enabled is False
 
 
 def test_llmobs_service_enable_no_api_key():
     with override_global_config(dict(_dd_api_key="")):
         dummy_tracer = DummyTracer()
         with pytest.raises(ValueError):
-            LLMObs.enable(tracer=dummy_tracer)
-        llmobs_instance = LLMObs._instance
+            llmobs_service.enable(tracer=dummy_tracer)
+        llmobs_instance = llmobs_service._instance
         assert llmobs_instance is None
-        assert LLMObs.enabled is False
+        assert llmobs_service.enabled is False
 
 
-def test_llmobs_service_enable_already_enabled():
+def test_llmobs_service_enable_already_enabled(mock_logs):
     with override_global_config(dict(_dd_api_key="<not-a-real-api-key>")):
-        with mock.patch("ddtrace.llmobs._llmobs.log") as mock_log:
-            dummy_tracer = DummyTracer()
-            LLMObs.enable(tracer=dummy_tracer)
-            LLMObs.enable(tracer=dummy_tracer)
-        llmobs_instance = LLMObs._instance
+        dummy_tracer = DummyTracer()
+        llmobs_service.enable(tracer=dummy_tracer)
+        llmobs_service.enable(tracer=dummy_tracer)
+        llmobs_instance = llmobs_service._instance
         assert llmobs_instance is not None
-        assert LLMObs.enabled
+        assert llmobs_service.enabled
         assert llmobs_instance.tracer == dummy_tracer
         assert any(isinstance(tracer_filter, LLMObsTraceProcessor) for tracer_filter in dummy_tracer._filters)
-        LLMObs.disable()
-        mock_log.debug.assert_has_calls([mock.call("%s already enabled", "LLMObs")])
+        llmobs_service.disable()
+        mock_logs.debug.assert_has_calls([mock.call("%s already enabled", "LLMObs")])
+
+
+def test_llmobs_start_span_while_disabled_raises_error(LLMObs):
+    LLMObs.disable()
+    with pytest.raises(Exception, match="cannot be used while LLMObs is disabled."):
+        with LLMObs.llm(model="test_model", name="test_llm_call", model_provider="test_provider"):
+            pass
+    with pytest.raises(Exception, match="cannot be used while LLMObs is disabled."):
+        with LLMObs.tool(name="test_tool"):
+            pass
+    with pytest.raises(Exception, match="cannot be used while LLMObs is disabled."):
+        with LLMObs.task(name="test_task"):
+            pass
+    with pytest.raises(Exception, match="cannot be used while LLMObs is disabled."):
+        with LLMObs.workflow(name="test_workflow"):
+            pass
+    with pytest.raises(Exception, match="cannot be used while LLMObs is disabled."):
+        with LLMObs.agent(name="test_agent"):
+            pass
+
+
+def test_llmobs_start_span_uses_kind_as_default_name(LLMObs):
+    with LLMObs.llm(model="test_model", model_provider="test_provider") as span:
+        assert span.name == "llm"
+    with LLMObs.tool() as span:
+        assert span.name == "tool"
+    with LLMObs.task() as span:
+        assert span.name == "task"
+    with LLMObs.workflow() as span:
+        assert span.name == "workflow"
+    with LLMObs.agent() as span:
+        assert span.name == "agent"
+
+
+def test_llmobs_start_span_with_session_id(LLMObs):
+    with LLMObs.llm(model="test_model", session_id="test_session_id") as span:
+        span_meta = json.loads(span.get_tag("ml_obs.meta") or "{}")
+        assert span_meta["session_id"] == "test_session_id"
+    with LLMObs.tool(session_id="test_session_id") as span:
+        span_meta = json.loads(span.get_tag("ml_obs.meta") or "{}")
+        assert span_meta["session_id"] == "test_session_id"
+    with LLMObs.task(session_id="test_session_id") as span:
+        span_meta = json.loads(span.get_tag("ml_obs.meta") or "{}")
+        assert span_meta["session_id"] == "test_session_id"
+    with LLMObs.workflow(session_id="test_session_id") as span:
+        span_meta = json.loads(span.get_tag("ml_obs.meta") or "{}")
+        assert span_meta["session_id"] == "test_session_id"
+    with LLMObs.agent(session_id="test_session_id") as span:
+        span_meta = json.loads(span.get_tag("ml_obs.meta") or "{}")
+        assert span_meta["session_id"] == "test_session_id"
+
+
+def test_llmobs_session_id_becomes_top_level_field(LLMObs, mock_llmobs_writer):
+    session_id = "test_session_id"
+    with LLMObs.task(session_id=session_id) as span:
+        pass
+    mock_llmobs_writer.enqueue.assert_called_with(
+        {
+            "span_id": str(span.span_id),
+            "trace_id": "{:x}".format(span.trace_id),
+            "parent_id": "",
+            "session_id": session_id,
+            "name": span.name,
+            "tags": mock.ANY,
+            "start_ns": span.start_ns,
+            "duration": span.duration_ns,
+            "error": 0,
+            "meta": {"span.kind": "task"},
+            "metrics": mock.ANY,
+        },
+    )
+
+
+def test_llmobs_llm_span(LLMObs, mock_llmobs_writer):
+    with LLMObs.llm(model="test_model", name="test_llm_call", model_provider="test_provider") as span:
+        assert span.name == "test_llm_call"
+        assert span.resource == "llm"
+        assert span.span_type == "llm"
+        assert json.loads(span.get_tag("ml_obs.meta")) == {
+            "span.kind": "llm",
+            "model_name": "test_model",
+            "model_provider": "test_provider",
+            "session_id": None,
+        }
+    mock_llmobs_writer.enqueue.assert_called_with(
+        {
+            "span_id": str(span.span_id),
+            "trace_id": "{:x}".format(span.trace_id),
+            "parent_id": "",
+            "session_id": "{:x}".format(span.trace_id),
+            "name": span.name,
+            "tags": mock.ANY,
+            "start_ns": span.start_ns,
+            "duration": span.duration_ns,
+            "error": 0,
+            "meta": {"span.kind": "llm", "model_name": "test_model", "model_provider": "test_provider"},
+            "metrics": mock.ANY,
+        },
+    )
+
+
+def test_llmobs_llm_span_no_model_raises_error(LLMObs):
+    with pytest.raises(TypeError):
+        with LLMObs.llm(name="test_llm_call", model_provider="test_provider"):
+            pass
+    with pytest.raises(ValueError, match="model must be the specified name of the invoked model."):
+        with LLMObs.llm(model="", name="test_llm_call", model_provider="test_provider"):
+            pass
+
+
+def test_llmobs_default_model_provider_set_to_custom(LLMObs):
+    with LLMObs.llm(model="test_model", name="test_llm_call") as span:
+        assert span.name == "test_llm_call"
+        assert span.resource == "llm"
+        assert span.span_type == "llm"
+        assert json.loads(span.get_tag("ml_obs.meta")) == {
+            "span.kind": "llm",
+            "model_name": "test_model",
+            "model_provider": "custom",
+            "session_id": None,
+        }
+
+
+def test_llmobs_tool_span(LLMObs, mock_llmobs_writer):
+    with LLMObs.tool(name="test_tool") as span:
+        assert span.name == "test_tool"
+        assert span.resource == "tool"
+        assert span.span_type == "llm"
+        assert json.loads(span.get_tag("ml_obs.meta")) == {"span.kind": "tool", "session_id": None}
+    mock_llmobs_writer.enqueue.assert_called_with(
+        {
+            "span_id": str(span.span_id),
+            "trace_id": "{:x}".format(span.trace_id),
+            "parent_id": "",
+            "session_id": "{:x}".format(span.trace_id),
+            "name": span.name,
+            "tags": mock.ANY,
+            "start_ns": span.start_ns,
+            "duration": span.duration_ns,
+            "error": 0,
+            "meta": {"span.kind": "tool"},
+            "metrics": mock.ANY,
+        },
+    )
+
+
+def test_llmobs_task_span(LLMObs, mock_llmobs_writer):
+    with LLMObs.task(name="test_task") as span:
+        assert span.name == "test_task"
+        assert span.resource == "task"
+        assert span.span_type == "llm"
+        assert json.loads(span.get_tag("ml_obs.meta")) == {"span.kind": "task", "session_id": None}
+    mock_llmobs_writer.enqueue.assert_called_with(
+        {
+            "span_id": str(span.span_id),
+            "trace_id": "{:x}".format(span.trace_id),
+            "parent_id": "",
+            "session_id": "{:x}".format(span.trace_id),
+            "name": span.name,
+            "tags": mock.ANY,
+            "start_ns": span.start_ns,
+            "duration": span.duration_ns,
+            "error": 0,
+            "meta": {"span.kind": "task"},
+            "metrics": mock.ANY,
+        },
+    )
+
+
+def test_llmobs_workflow_span(LLMObs, mock_llmobs_writer):
+    with LLMObs.workflow(name="test_workflow") as span:
+        assert span.name == "test_workflow"
+        assert span.resource == "workflow"
+        assert span.span_type == "llm"
+        assert json.loads(span.get_tag("ml_obs.meta")) == {"span.kind": "workflow", "session_id": None}
+    mock_llmobs_writer.enqueue.assert_called_with(
+        {
+            "span_id": str(span.span_id),
+            "trace_id": "{:x}".format(span.trace_id),
+            "parent_id": "",
+            "session_id": "{:x}".format(span.trace_id),
+            "name": span.name,
+            "tags": mock.ANY,
+            "start_ns": span.start_ns,
+            "duration": span.duration_ns,
+            "error": 0,
+            "meta": {"span.kind": "workflow"},
+            "metrics": mock.ANY,
+        },
+    )
+
+
+def test_llmobs_agent_span(LLMObs, mock_llmobs_writer):
+    with LLMObs.agent(name="test_agent") as span:
+        assert span.name == "test_agent"
+        assert span.resource == "agent"
+        assert span.span_type == "llm"
+        assert json.loads(span.get_tag("ml_obs.meta")) == {"span.kind": "agent", "session_id": None}
+    mock_llmobs_writer.enqueue.assert_called_with(
+        {
+            "span_id": str(span.span_id),
+            "trace_id": "{:x}".format(span.trace_id),
+            "parent_id": "",
+            "session_id": "{:x}".format(span.trace_id),
+            "name": span.name,
+            "tags": mock.ANY,
+            "start_ns": span.start_ns,
+            "duration": span.duration_ns,
+            "error": 0,
+            "meta": {"span.kind": "agent"},
+            "metrics": mock.ANY,
+        },
+    )
+
+
+def test_llmobs_tag_while_disabled_raises_error(LLMObs):
+    LLMObs.disable()
+    with pytest.raises(Exception, match="cannot be used while LLMObs is disabled."):
+        LLMObs.tag(parameters={"test": "test"})
+
+
+def test_llmobs_tag_no_active_span_raises_error(LLMObs):
+    with pytest.raises(Exception, match="No span provided and no active span found."):
+        LLMObs.tag(parameters={"test": "test"})
+
+
+def test_llmobs_tag_non_llm_span_raises_error(LLMObs):
+    dummy_tracer = DummyTracer()
+    with dummy_tracer.trace("root") as non_llmobs_span:
+        with pytest.raises(ValueError, match="span must be an LLM-type span."):
+            LLMObs.tag(span=non_llmobs_span, parameters={"test": "test"})
+
+
+def test_llmobs_tag_finished_span_does_nothing(LLMObs, mock_logs):
+    with LLMObs.llm(model="test_model", name="test_llm_call", model_provider="test_provider") as span:
+        pass
+    LLMObs.tag(span=span, parameters={"test": "test"})
+    mock_logs.warning.assert_called_once_with("Cannot annotate a finished span.")
+
+
+def test_llmobs_tag_meta_deserialize_error_raises(LLMObs):
+    with LLMObs.task() as span:
+        span.set_tag_str("ml_obs.meta", "{")
+        with pytest.raises(ValueError, match="Failed to deserialize existing ml_obs.meta tag."):
+            LLMObs.tag(span=span, parameters={"test": "test"})
+
+
+def test_llmobs_tag_parameters(LLMObs):
+    with LLMObs.llm(model="test_model", name="test_llm_call", model_provider="test_provider") as span:
+        LLMObs.tag(span=span, parameters={"temperature": 0.9, "max_tokens": 50})
+        span_meta = json.loads(span.get_tag("ml_obs.meta"))
+        assert span_meta["input"]["parameters"] == {"temperature": 0.9, "max_tokens": 50}
+        LLMObs.tag(span=span, parameters={"n": 1})
+        span_meta = json.loads(span.get_tag("ml_obs.meta"))
+        assert span_meta["input"]["parameters"] == {"temperature": 0.9, "max_tokens": 50, "n": 1}
+
+
+def test_llmobs_tag_input_string(LLMObs):
+    with LLMObs.llm(model="test_model") as llm_span:
+        LLMObs.tag(span=llm_span, input_tags="test_input")
+        span_meta = json.loads(llm_span.get_tag("ml_obs.meta"))
+        assert span_meta["input"]["messages"] == [{"content": "test_input"}]
+    with LLMObs.task() as task_span:
+        LLMObs.tag(span=task_span, input_tags="test_input")
+        span_meta = json.loads(task_span.get_tag("ml_obs.meta"))
+        assert span_meta["input"]["value"] == "test_input"
+    with LLMObs.tool() as tool_span:
+        LLMObs.tag(span=tool_span, input_tags="test_input")
+        span_meta = json.loads(tool_span.get_tag("ml_obs.meta"))
+        assert span_meta["input"]["value"] == "test_input"
+    with LLMObs.workflow() as workflow_span:
+        LLMObs.tag(span=workflow_span, input_tags="test_input")
+        span_meta = json.loads(workflow_span.get_tag("ml_obs.meta"))
+        assert span_meta["input"]["value"] == "test_input"
+    with LLMObs.agent() as agent_span:
+        LLMObs.tag(span=agent_span, input_tags="test_input")
+        span_meta = json.loads(agent_span.get_tag("ml_obs.meta"))
+        assert span_meta["input"]["value"] == "test_input"
+
+
+def test_llmobs_tag_input_llm_message(LLMObs):
+    with LLMObs.llm(model="test_model") as llm_span:
+        LLMObs.tag(span=llm_span, input_tags=[{"content": "test_input", "role": "human"}])
+        span_meta = json.loads(llm_span.get_tag("ml_obs.meta"))
+        assert span_meta["input"]["messages"] == [{"content": "test_input", "role": "human"}]
+
+
+def test_llmobs_tag_non_llm_span_message_input_raises_error(LLMObs):
+    with LLMObs.task() as span:
+        with pytest.raises(ValueError, match="Invalid input/output type for non-llm span. Must be a raw string."):
+            LLMObs.tag(span=span, input_tags=[{"content": "test_input"}])
+
+
+def test_llmobs_tag_output_string(LLMObs):
+    with LLMObs.llm(model="test_model") as llm_span:
+        LLMObs.tag(span=llm_span, output_tags="test_output")
+        span_meta = json.loads(llm_span.get_tag("ml_obs.meta"))
+        assert span_meta["output"]["messages"] == [{"content": "test_output"}]
+    with LLMObs.task() as task_span:
+        LLMObs.tag(span=task_span, output_tags="test_output")
+        span_meta = json.loads(task_span.get_tag("ml_obs.meta"))
+        assert span_meta["output"]["value"] == "test_output"
+    with LLMObs.tool() as tool_span:
+        LLMObs.tag(span=tool_span, output_tags="test_output")
+        span_meta = json.loads(tool_span.get_tag("ml_obs.meta"))
+        assert span_meta["output"]["value"] == "test_output"
+    with LLMObs.workflow() as workflow_span:
+        LLMObs.tag(span=workflow_span, output_tags="test_output")
+        span_meta = json.loads(workflow_span.get_tag("ml_obs.meta"))
+        assert span_meta["output"]["value"] == "test_output"
+    with LLMObs.agent() as agent_span:
+        LLMObs.tag(span=agent_span, output_tags="test_output")
+        span_meta = json.loads(agent_span.get_tag("ml_obs.meta"))
+        assert span_meta["output"]["value"] == "test_output"
+
+
+def test_llmobs_tag_output_llm_message(LLMObs):
+    with LLMObs.llm(model="test_model") as llm_span:
+        LLMObs.tag(span=llm_span, output_tags=[{"content": "test_output", "role": "human"}])
+        span_meta = json.loads(llm_span.get_tag("ml_obs.meta"))
+        assert span_meta["output"]["messages"] == [{"content": "test_output", "role": "human"}]
+
+
+def test_llmobs_tag_non_llm_span_message_output_raises_error(LLMObs):
+    with LLMObs.task() as span:
+        with pytest.raises(ValueError, match="Invalid input/output type for non-llm span. Must be a raw string."):
+            LLMObs.tag(span=span, output_tags=[{"content": "test_input"}])
+
+
+def test_llmobs_span_error_sets_error_tag(LLMObs, mock_llmobs_writer):
+    with pytest.raises(ValueError):
+        with LLMObs.llm(model="test_model", model_provider="test_model_provider") as span:
+            raise ValueError("test error message")
+    mock_llmobs_writer.enqueue.assert_called_with(
+        {
+            "span_id": str(span.span_id),
+            "trace_id": "{:x}".format(span.trace_id),
+            "parent_id": "",
+            "session_id": "{:x}".format(span.trace_id),
+            "name": span.name,
+            "tags": mock.ANY,
+            "start_ns": span.start_ns,
+            "duration": span.duration_ns,
+            "error": 1,
+            "meta": {
+                "span.kind": "llm",
+                "model_name": "test_model",
+                "model_provider": "test_model_provider",
+                "error.message": "test error message",
+            },
+            "metrics": mock.ANY,
+        },
+    )
