99b1d84dffdaef8e9eeb8c7f5c135e4c9e539d2f
Munir Abdinur <munir.abdinur@datadoghq.com>
2024-02-21 11:15:45 -0500

    chore(tracer): downgrade to v0.4 only if asm is enabled (#8466)
    Follow up to: https://github.com/DataDog/dd-trace-py/pull/8441/files
    
    api_version `v0.5` is a more performant trace api and should be used
    whenever possible. We should only downgrade to `v0.4` if ASM is enabled.
    
    Note from ASM: with the new support for meta_struct in v0.4, v0.4 will
    be more efficient when ASM is enabled than v0.5 anyway, as soon as ASM
    moves its data from meta to meta_struct.
    
    ## Checklist
    
    - [x] Change(s) are motivated and described in the PR description
    - [x] Testing strategy is described if automated tests are not included
    in the PR
    - [x] Risks are described (performance impact, potential for breakage,
    maintainability)
    - [x] Change is maintainable (easy to change, telemetry, documentation)
    - [x] [Library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    are followed or label `changelog/no-changelog` is set
    - [x] Documentation is included (in-code, generated user docs, [public
    corp docs](https://github.com/DataDog/documentation/))
    - [x] Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    - [x] If this PR changes the public interface, I've notified
    `@DataDog/apm-tees`.
    - [x] If change touches code that signs or publishes builds or packages,
    or handles credentials of any kind, I've requested a review from
    `@DataDog/security-design-and-guidance`.
    
    ## Reviewer Checklist
    
    - [x] Title is accurate
    - [x] All changes are related to the pull request's stated goal
    - [x] Description motivates each change
    - [x] Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - [x] Testing strategy adequately addresses listed risks
    - [x] Change is maintainable (easy to change, telemetry, documentation)
    - [x] Release note makes sense to a user of the library
    - [x] Author has acknowledged and discussed the performance implications
    of this PR as reported in the benchmarks PR comment
    - [x] Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    
    ---------
    
    Co-authored-by: Christophe Papazian <christophe.papazian@datadoghq.com>
    Co-authored-by: Christophe Papazian <114495376+christophe-papazian@users.noreply.github.com>

--- ddtrace/_trace/span.py
+++ ddtrace/_trace/span.py
@@ -382,9 +382,9 @@ class Span(object):
         """
         self._meta_struct[key] = value
 
-    def get_struct_tag(self, key: str) -> Dict[str, Any]:
-        """Set a tag key/value pair on the span meta_struct"""
-        return self._meta_struct[key]
+    def get_struct_tag(self, key: str) -> Optional[Dict[str, Any]]:
+        """Return the given struct or None if it doesn't exist."""
+        return self._meta_struct.get(key, None)
 
     def set_tag_str(self, key: _TagNameType, value: Text) -> None:
         """Set a value for a tag. Values are coerced to unicode in Python 2 and
