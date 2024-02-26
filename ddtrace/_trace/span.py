568a5fd2c56cbfc59db289477d8391ad895cb26d
Zachary Groves <32471391+ZStriker19@users.noreply.github.com>
2024-02-26 14:56:53 -0500

    Update ddtrace/_trace/span.py

--- ddtrace/_trace/span.py
+++ ddtrace/_trace/span.py
@@ -277,7 +277,7 @@ class Span(object):
     def sampled(self, value):
         deprecate(
             "span.sampled is deprecated and will be removed in a future version of the tracer.",
-            message="This is a no-op setter. Please use span.set_tag(MANUAL_KEEP/MANUAL_DROP) to keep or drop spans.",
+            message="This is a no-op setter. Please use span.set_tag("manual.keep"/"manual.drop") to keep or drop spans.",
             category=DDTraceDeprecationWarning,
         )
 
