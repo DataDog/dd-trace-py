f1114d96e166bce2ea49fb3644081147343cafea
David Sanchez <838104+sanchda@users.noreply.github.com>
2024-02-28 20:35:37 -0600

    Update ddtrace/internal/datadog/profiling/stack_v2/include/util/cast_to_pyfunc.hpp
    Co-authored-by: Gabriele N. Tornetta <P403n1x87@users.noreply.github.com>

--- ddtrace/internal/datadog/profiling/stack_v2/include/util/cast_to_pyfunc.hpp
+++ ddtrace/internal/datadog/profiling/stack_v2/include/util/cast_to_pyfunc.hpp
@@ -3,7 +3,7 @@
 
 // Abiding by the contract required in the Python interface requires abandoning certain checks in one's main project.
 // Rather than abandoning discipline, we can include this header in a way that bypasses these checks.
-inline PyCFunction
+inline static PyCFunction
 cast_to_pycfunction(PyObject* (*func)(PyObject*, PyObject*, PyObject*))
 {
     // Direct C-style cast
