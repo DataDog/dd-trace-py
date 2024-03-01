cef8843f6d12d2e3951c1d8bf4b0d47d24398662
sanchda <sanchda@users.noreply.github.com>
2024-03-01 04:34:51 +0000

    Fix silly package_data bug

--- setup.py
+++ setup.py
@@ -518,6 +518,7 @@ setup(
         "ddtrace.appsec": ["rules.json"],
         "ddtrace.appsec._ddwaf": [str(Path("libddwaf") / "*" / "lib" / "libddwaf.*")],
         "ddtrace.appsec._iast._taint_tracking": ["CMakeLists.txt"],
+        "ddtrace.internal.datadog.profiling": ["libdd_wrapper.*"],
     },
     python_requires=">=3.7",
     zip_safe=False,
