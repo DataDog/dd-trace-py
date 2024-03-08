7a6063b813e31a15b7c4c4dd8fefa147f0f9153b
Christophe Papazian <114495376+christophe-papazian@users.noreply.github.com>
2024-03-08 16:31:32 +0100

    Update _asm_request_context.py
    - move active guard at start to limit concurrency risks
    - don't use len to test emptyness (faster)
    - ensure execution_context is ended even without callbacks (callback list is never empty but if it happens, we must still end the context)

--- ddtrace/appsec/_asm_request_context.py
+++ ddtrace/appsec/_asm_request_context.py
@@ -147,17 +147,17 @@ class _DataHandler:
 
     def finalise(self):
         if self.active:
+            self.active = False
             env = self.execution_context.get_item("asm_env")
             if env is not None:
                 callbacks = GLOBAL_CALLBACKS.get(_CONTEXT_CALL, []) if env.must_call_globals else []
                 env.must_call_globals = False
                 if env.callbacks is not None and env.callbacks.get(_CONTEXT_CALL):
                     callbacks = callbacks + env.callbacks.get(_CONTEXT_CALL)
-                if len(callbacks) > 0:
+                if callbacks:
                     for function in callbacks:
                         function(env)
-                    self.execution_context.end()
-            self.active = False
+                self.execution_context.end()
 
 
 def set_value(category: str, address: str, value: Any) -> None:
