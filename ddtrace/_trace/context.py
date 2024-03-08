DataDog/dd-trace-py

commit 90a3e3fed47763abf64cb4c04c6a32f5c64076cb

fix(tracing): ensure p is on the tracestate of active spans (#8569)
Currently last datadog parent id is added to the tracestate in
`Htttpropagator.inject`.

We should only set the `p` tracestate field if a span is active.

This PR simplifies this feature by ensure `dd=p:..........;....` is
always set in the tracestate of otel spans

## Checklist

- [x] Change(s) are motivated and described in the PR description
- [x] Testing strategy is described if automated tests are not included
in the PR
- [x] Risks are described (performance impact, potential for breakage,
maintainability)
- [x] Change is maintainable (easy to change, telemetry, documentation)
- [x] [Library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
are followed or label `changelog/no-changelog` is set
- [x] Documentation is included (in-code, generated user docs, [public
corp docs](https://github.com/DataDog/documentation/))
- [x] Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
- [x] If this PR changes the public interface, I've notified
`@DataDog/apm-tees`.
- [x] If change touches code that signs or publishes builds or packages,
or handles credentials of any kind, I've requested a review from
`@DataDog/security-design-and-guidance`.

## Reviewer Checklist

- [x] Title is accurate
- [x] All changes are related to the pull request's stated goal
- [x] Description motivates each change
- [x] Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes
- [x] Testing strategy adequately addresses listed risks
- [x] Change is maintainable (easy to change, telemetry, documentation)
- [x] Release note makes sense to a user of the library
- [x] Author has acknowledged and discussed the performance implications
of this PR as reported in the benchmarks PR comment
- [x] Backport labels are set in a manner that is consistent with the

ddtrace/_trace/context.py
@@ -30,8 +30,9 @@ if TYPE_CHECKING:  # pragma: no cover
         Optional[int],  # span_id
         _MetaDictType,  # _meta
         _MetricDictType,  # _metrics
-        list[SpanLink],
-        dict[str, Any],
+        list[SpanLink],  #  span_links
+        dict[str, Any],  # baggage
+        bool,  # is_remote
     ]
 
 
@@ -45,7 +46,7 @@ class Context(object):
     boundaries.
     """
 
-    __slots__ = ["trace_id", "span_id", "_lock", "_meta", "_metrics", "_span_links", "_baggage"]
+    __slots__ = ["trace_id", "span_id", "_lock", "_meta", "_metrics", "_span_links", "_baggage", "_is_remote"]
 
     def __init__(
         self,
@@ -58,6 +59,7 @@ class Context(object):
         lock=None,  # type: Optional[threading.RLock]
         span_links=None,  # type: Optional[list[SpanLink]]
         baggage=None,  # type: Optional[dict[str, Any]]
+        is_remote=True,  # type: bool
     ):
         self._meta = meta if meta is not None else {}  # type: _MetaDictType
         self._metrics = metrics if metrics is not None else {}  # type: _MetricDictType
@@ -65,6 +67,7 @@ class Context(object):
 
         self.trace_id = trace_id  # type: Optional[int]
         self.span_id = span_id  # type: Optional[int]
+        self._is_remote = is_remote  # type: bool
 
         if dd_origin is not None and _DD_ORIGIN_INVALID_CHARS_REGEX.search(dd_origin) is None:
             self._meta[ORIGIN_KEY] = dd_origin
@@ -91,13 +94,14 @@ class Context(object):
             self._meta,
             self._metrics,
             self._span_links,
-            self._baggage
+            self._baggage,
+            self._is_remote,
             # Note: self._lock is not serializable
         )
 
     def __setstate__(self, state):
         # type: (_ContextState) -> None
-        self.trace_id, self.span_id, self._meta, self._metrics, self._span_links, self._baggage = state
+        self.trace_id, self.span_id, self._meta, self._metrics, self._span_links, self._baggage, self._is_remote = state
         # We cannot serialize and lock, so we must recreate it unless we already have one
         self._lock = threading.RLock()
 
@@ -111,6 +115,8 @@ class Context(object):
             metrics=self._metrics,
             lock=self._lock,
             baggage=self._baggage,
+            span_links=self._span_links,
+            is_remote=self._is_remote,
         )
 
     def _update_tags(self, span):
@@ -251,18 +257,20 @@ class Context(object):
                     and self._metrics == other._metrics
                     and self._span_links == other._span_links
                     and self._baggage == other._baggage
+                    and self._is_remote == other._is_remote
                 )
         return False
 
     def __repr__(self):
         # type: () -> str
-        return "Context(trace_id=%s, span_id=%s, _meta=%s, _metrics=%s, _span_links=%s, _baggage=%s)" % (
+        return "Context(trace_id=%s, span_id=%s, _meta=%s, _metrics=%s, _span_links=%s, _baggage=%s, _is_remote=%s)" % (
             self.trace_id,
             self.span_id,
             self._meta,
             self._metrics,
             self._span_links,
             self._baggage,
+            self._is_remote,
         )
 
     __str__ = __repr__

tests/tracer/test_propagation.py
@@ -6,11 +6,13 @@ import pickle
 
 import pytest
 
+from ddtrace import tracer as ddtracer
 from ddtrace._trace._span_link import SpanLink
 from ddtrace._trace.context import Context
 from ddtrace._trace.span import _get_64_lowest_order_bits_as_int
 from ddtrace.internal.constants import _PROPAGATION_STYLE_NONE
 from ddtrace.internal.constants import _PROPAGATION_STYLE_W3C_TRACECONTEXT
+from ddtrace.internal.constants import LAST_DD_PARENT_ID_KEY
 from ddtrace.internal.constants import PROPAGATION_STYLE_B3_MULTI
 from ddtrace.internal.constants import PROPAGATION_STYLE_B3_SINGLE
 from ddtrace.internal.constants import PROPAGATION_STYLE_DATADOG
@@ -428,6 +430,38 @@ def test_extract_128bit_trace_ids_tracecontext():
             assert span.parent_id == span_id
             with tracer.trace("child_span") as child_span:
                 assert child_span.trace_id == trace_id
+
+
+def test_last_dd_span_id():
+    non_dd_remote_context = HTTPPropagator.extract(
+        {
+            "traceparent": "00-70f198ee56343ba864fe8b2a57d4eff7-34f067aa0ba902b9-01",
+            "tracestate": "dd=p:123067aa0ba902a6;s:2;o:rum",
+        }
+    )
+
+    with ddtracer.start_span("local-root", child_of=non_dd_remote_context) as local_root:
+        with ddtracer.start_span("child1", child_of=local_root) as child1:
+            pass
+
+    with ddtracer.start_span("child2", child_of=local_root) as chunk_root:
+        pass
+
+    with ddtracer.start_span("root", child_of=None) as root:
+        pass
+
+    # The last parent span_id tag should be set ONLY on the local root spans
+    assert local_root.get_tag(LAST_DD_PARENT_ID_KEY) == "123067aa0ba902a6"
+    for span in (root, child1, chunk_root):
+        assert span.get_tag(LAST_DD_PARENT_ID_KEY) is None
+    # `p` value in tracestate is set using the current active datadog span
+    headers = {}
+    HTTPPropagator.inject(root.context, headers)
+    assert "p:{:016x}".format(root.span_id) in headers["tracestate"]
+    # `p` value is set to the last datadog span in the trace (via non_dd_remote_context's tracestate)
+    headers = {}
+    HTTPPropagator.inject(local_root.context, headers)
+    assert "p:123067aa0ba902a6" in headers["tracestate"]
 
 
 def test_extract_unicode(tracer):  # noqa: F811
@@ -997,7 +1031,7 @@ def test_extract_tracestate(caplog, ts_string, expected_tuple, expected_logging,
                     "_dd.p.dm": "-4",
                     "_dd.p.usr.id": "baz64",
                     "_dd.origin": "rum",
-                    "_dd.parent_id": "0000000000000000",
+                    LAST_DD_PARENT_ID_KEY: "0000000000000000",
                     "traceparent": TRACECONTEXT_HEADERS_VALID[_HTTP_HEADER_TRACEPARENT],
                 },
                 "metrics": {"_sampling_priority_v1": 2},
@@ -1010,7 +1044,7 @@ def test_extract_tracestate(caplog, ts_string, expected_tuple, expected_logging,
                 "span_id": 67667974448284343,
                 "meta": {
                     "tracestate": "dd=p:00f067aa0ba902b7;s:2;o:rum",
-                    "_dd.parent_id": "00f067aa0ba902b7",
+                    LAST_DD_PARENT_ID_KEY: "00f067aa0ba902b7",
                     "_dd.origin": "rum",
                     "traceparent": TRACECONTEXT_HEADERS_VALID_BASIC[_HTTP_HEADER_TRACEPARENT],
                 },
@@ -1753,7 +1787,7 @@ EXTRACT_FIXTURES_ENV_ONLY = [
             "meta": {
                 "tracestate": "dd=p:00f067aa0ba902b7;s:2;o:rum",
                 "traceparent": TRACECONTEXT_HEADERS_VALID[_HTTP_HEADER_TRACEPARENT],
-                "_dd.parent_id": "00f067aa0ba902b7",
+                LAST_DD_PARENT_ID_KEY: "00f067aa0ba902b7",
             },
         },
     ),
@@ -1768,7 +1802,7 @@ EXTRACT_FIXTURES_ENV_ONLY = [
             "meta": {
                 "tracestate": "dd=o:rum",
                 "traceparent": TRACECONTEXT_HEADERS_VALID[_HTTP_HEADER_TRACEPARENT],
-                "_dd.parent_id": "0000000000000000",
+                LAST_DD_PARENT_ID_KEY: "0000000000000000",
             },
         },
     ),
@@ -1922,7 +1956,7 @@ FULL_CONTEXT_EXTRACT_FIXTURES = [
                 "_dd.p.dm": "-4",
                 "_dd.p.usr.id": "baz64",
                 "_dd.origin": "rum",
-                "_dd.parent_id": "0000000000000000",
+                LAST_DD_PARENT_ID_KEY: "0000000000000000",
             },
             metrics={"_sampling_priority_v1": 2},
             span_links=[],
@@ -1949,7 +1983,7 @@ FULL_CONTEXT_EXTRACT_FIXTURES = [
                 "_dd.p.dm": "-4",
                 "_dd.p.usr.id": "baz64",
                 "_dd.origin": "rum",
-                "_dd.parent_id": "0000000000000000",
+                LAST_DD_PARENT_ID_KEY: "0000000000000000",
             },
             metrics={"_sampling_priority_v1": 2},
             span_links=[
@@ -1983,7 +2017,7 @@ FULL_CONTEXT_EXTRACT_FIXTURES = [
                 "_dd.p.dm": "-4",
                 "_dd.p.usr.id": "baz64",
                 "_dd.origin": "rum",
-                "_dd.parent_id": "0000000000000000",
+                LAST_DD_PARENT_ID_KEY: "0000000000000000",
             },
             metrics={"_sampling_priority_v1": 2},
             span_links=[
@@ -2161,7 +2195,7 @@ INJECT_FIXTURES = [
             HTTP_HEADER_PARENT_ID: "8185124618007618416",
             HTTP_HEADER_SAMPLING_PRIORITY: "1",
             HTTP_HEADER_ORIGIN: "synthetics",
-            _HTTP_HEADER_TRACESTATE: "dd=p:7197677932a62370;s:1;o:synthetics",
+            _HTTP_HEADER_TRACESTATE: "dd=s:1;o:synthetics",
             _HTTP_HEADER_TRACEPARENT: "00-0000000000000000b5a2814f70060771-7197677932a62370-01",
         },
     ),
@@ -2173,7 +2207,7 @@ INJECT_FIXTURES = [
             HTTP_HEADER_TRACE_ID: "13088165645273925489",
             HTTP_HEADER_PARENT_ID: "8185124618007618416",
             HTTP_HEADER_SAMPLING_PRIORITY: "2",
-            _HTTP_HEADER_TRACESTATE: "dd=p:7197677932a62370;s:2",
+            _HTTP_HEADER_TRACESTATE: "dd=s:2",
             _HTTP_HEADER_TRACEPARENT: "00-0000000000000000b5a2814f70060771-7197677932a62370-01",
         },
     ),
@@ -2185,7 +2219,7 @@ INJECT_FIXTURES = [
             HTTP_HEADER_TRACE_ID: "13088165645273925489",
             HTTP_HEADER_PARENT_ID: "8185124618007618416",
             HTTP_HEADER_SAMPLING_PRIORITY: "0",
-            _HTTP_HEADER_TRACESTATE: "dd=p:7197677932a62370;s:0",
+            _HTTP_HEADER_TRACESTATE: "dd=s:0",
             _HTTP_HEADER_TRACEPARENT: "00-0000000000000000b5a2814f70060771-7197677932a62370-00",
         },
     ),
@@ -2348,6 +2382,7 @@ INJECT_FIXTURES = [
             "meta": {
                 "tracestate": "dd=s:2;o:rum",
                 "_dd.origin": "rum",
+                LAST_DD_PARENT_ID_KEY: "00f067aa0ba902b7",
                 "traceparent": TRACECONTEXT_HEADERS_VALID_BASIC[_HTTP_HEADER_TRACEPARENT],
             },
             "metrics": {"_sampling_priority_v1": 2},
@@ -2366,7 +2401,7 @@ INJECT_FIXTURES = [
         },
         {
             _HTTP_HEADER_TRACEPARENT: "00-%s-00f067aa0ba902b7-00" % (TRACE_ID_HEX,),
-            _HTTP_HEADER_TRACESTATE: "dd=p:00f067aa0ba902b7",
+            _HTTP_HEADER_TRACESTATE: "",
         },
     ),
     (
@@ -2396,7 +2431,7 @@ INJECT_FIXTURES = [
         },
         {
             _HTTP_HEADER_TRACEPARENT: "00-%s-00f067aa0ba902b7-01" % (TRACE_ID_HEX,),
-            _HTTP_HEADER_TRACESTATE: "dd=p:00f067aa0ba902b7;s:2;o:rum",
+            _HTTP_HEADER_TRACESTATE: "dd=s:2;o:rum",
         },
     ),
     (
@@ -2413,7 +2448,7 @@ INJECT_FIXTURES = [
         },
         {
             _HTTP_HEADER_TRACEPARENT: "00-%s-00f067aa0ba902b7-01" % (TRACE_ID_HEX,),
-            _HTTP_HEADER_TRACESTATE: "dd=p:00f067aa0ba902b7;s:2;o:rum,congo=baz123",
+            _HTTP_HEADER_TRACESTATE: "dd=s:2;o:rum,congo=baz123",
         },
     ),
     # All styles
@@ -2436,7 +2471,7 @@ INJECT_FIXTURES = [
             _HTTP_HEADER_B3_SAMPLED: "1",
             _HTTP_HEADER_B3_SINGLE: "b5a2814f70060771-7197677932a62370-1",
             _HTTP_HEADER_TRACEPARENT: "00-0000000000000000b5a2814f70060771-7197677932a62370-01",
-            _HTTP_HEADER_TRACESTATE: "dd=p:7197677932a62370;s:1;o:synthetics",
+            _HTTP_HEADER_TRACESTATE: "dd=s:1;o:synthetics",
         },
     ),
     (
@@ -2457,7 +2492,7 @@ INJECT_FIXTURES = [
             _HTTP_HEADER_B3_FLAGS: "1",
             _HTTP_HEADER_B3_SINGLE: "b5a2814f70060771-7197677932a62370-d",
             _HTTP_HEADER_TRACEPARENT: "00-0000000000000000b5a2814f70060771-7197677932a62370-01",
-            _HTTP_HEADER_TRACESTATE: "dd=p:7197677932a62370;s:2",
+            _HTTP_HEADER_TRACESTATE: "dd=s:2",
         },
     ),
     (
@@ -2478,7 +2513,7 @@ INJECT_FIXTURES = [
             _HTTP_HEADER_B3_SAMPLED: "0",
             _HTTP_HEADER_B3_SINGLE: "b5a2814f70060771-7197677932a62370-0",
             _HTTP_HEADER_TRACEPARENT: "00-0000000000000000b5a2814f70060771-7197677932a62370-00",
-            _HTTP_HEADER_TRACESTATE: "dd=p:7197677932a62370;s:0",
+            _HTTP_HEADER_TRACESTATE: "dd=s:0",
         },
     ),
     (
@@ -2500,7 +2535,7 @@ INJECT_FIXTURES = [
             _HTTP_HEADER_B3_SPAN_ID: "7197677932a62370",
             _HTTP_HEADER_B3_SINGLE: "b5a2814f70060771-7197677932a62370",
             _HTTP_HEADER_TRACEPARENT: "00-0000000000000000b5a2814f70060771-7197677932a62370-00",
-            _HTTP_HEADER_TRACESTATE: "dd=p:7197677932a62370",
+            _HTTP_HEADER_TRACESTATE: "",
         },
     ),
 ]

tests/opentelemetry/test_context.py
@@ -89,7 +89,7 @@ def _subprocess_task(parent_span_context, errors):
         ot_tracer._tracer.flush()
 
 
-@pytest.mark.snapshot
+@pytest.mark.snapshot(ignores=["meta.tracestate"])
 def test_otel_trace_across_fork(oteltracer):
     errors = multiprocessing.Queue()
     with oteltracer.start_as_current_span("root") as root:
@@ -102,7 +102,7 @@ def test_otel_trace_across_fork(oteltracer):
     assert errors.empty(), errors.get()
 
 
-@pytest.mark.snapshot(wait_for_num_traces=1)
+@pytest.mark.snapshot(wait_for_num_traces=1, ignores=["meta.tracestate"])
 @pytest.mark.parametrize("decision", [MANUAL_KEEP_KEY, MANUAL_DROP_KEY], ids=["manual.keep", "manual.drop"])
 def test_sampling_decisions_across_processes(oteltracer, decision):
     # sampling decision in the subprocess task should be the same as the parent

ddtrace/_trace/processor/__init__.py
@@ -18,6 +18,7 @@ from ddtrace.constants import SAMPLING_PRIORITY_KEY
 from ddtrace.constants import USER_KEEP
 from ddtrace.internal import gitmetadata
 from ddtrace.internal.constants import HIGHER_ORDER_TRACE_ID_BITS
+from ddtrace.internal.constants import LAST_DD_PARENT_ID_KEY
 from ddtrace.internal.constants import MAX_UINT_64BITS
 from ddtrace.internal.logger import get_logger
 from ddtrace.internal.sampling import SpanSamplingRule
@@ -218,6 +219,10 @@ class TraceTagsProcessor(TraceProcessor):
         if chunk_root.trace_id > MAX_UINT_64BITS:
             trace_id_hob = _get_64_highest_order_bits_as_hex(chunk_root.trace_id)
             chunk_root.set_tag_str(HIGHER_ORDER_TRACE_ID_BITS, trace_id_hob)
+
+        if LAST_DD_PARENT_ID_KEY in chunk_root._meta and chunk_root._parent is not None:
+            # we should only set the last parent id on local root spans
+            del chunk_root._meta[LAST_DD_PARENT_ID_KEY]
         return trace
 
 

ddtrace/_trace/span.py
@@ -568,7 +568,7 @@ class Span(object):
         # type: () -> Context
         """Return the trace context for this span."""
         if self._context is None:
-            self._context = Context(trace_id=self.trace_id, span_id=self.span_id)
+            self._context = Context(trace_id=self.trace_id, span_id=self.span_id, is_remote=False)
         return self._context
 
     def link_span(self, context, attributes=None):

ddtrace/_trace/tracer.py
@@ -641,6 +641,7 @@ class Tracer(object):
                     sampling_priority=child_of.context.sampling_priority,
                     span_id=child_of.span_id,
                     trace_id=child_of.trace_id,
+                    is_remote=False,
                 )
 
                 # If the child_of span was active then activate the new context
@@ -658,7 +659,7 @@ class Tracer(object):
                 context = child_of.context
                 parent = child_of
         else:
-            context = Context()
+            context = Context(is_remote=False)
 
         trace_id = context.trace_id
         parent_id = context.span_id

ddtrace/internal/constants.py
@@ -19,10 +19,12 @@ PROPAGATION_STYLE_ALL = (
 )
 W3C_TRACESTATE_KEY = "tracestate"
 W3C_TRACEPARENT_KEY = "traceparent"
+W3C_TRACESTATE_PARENT_ID_KEY = "p"
 W3C_TRACESTATE_ORIGIN_KEY = "o"
 W3C_TRACESTATE_SAMPLING_PRIORITY_KEY = "s"
 DEFAULT_SAMPLING_RATE_LIMIT = 100
 SAMPLING_DECISION_TRACE_TAG_KEY = "_dd.p.dm"
+LAST_DD_PARENT_ID_KEY = "_dd.parent_id"
 DEFAULT_SERVICE_NAME = "unnamed-python-service"
 # Used to set the name of an integration on a span
 COMPONENT = "component"

ddtrace/internal/utils/http.py
@@ -25,6 +25,7 @@ from ddtrace.internal.constants import BLOCKED_RESPONSE_JSON
 from ddtrace.internal.constants import DEFAULT_TIMEOUT
 from ddtrace.internal.constants import SAMPLING_DECISION_TRACE_TAG_KEY
 from ddtrace.internal.constants import W3C_TRACESTATE_ORIGIN_KEY
+from ddtrace.internal.constants import W3C_TRACESTATE_PARENT_ID_KEY
 from ddtrace.internal.constants import W3C_TRACESTATE_SAMPLING_PRIORITY_KEY
 from ddtrace.internal.http import HTTPConnection
 from ddtrace.internal.http import HTTPSConnection
@@ -203,6 +204,16 @@ def w3c_encode_tag(args):
     tag_val = pattern.sub(replacement, tag_val)
     # replace = with ~ if it wasn't already replaced by the regex
     return tag_val.replace("=", "~")
+
+
+def w3c_tracestate_add_p(tracestate, span_id):
+    # Adds last datadog parent_id to tracestate. This tag is used to reconnect a trace with non-datadog spans
+    p_member = "{}:{:016x}".format(W3C_TRACESTATE_PARENT_ID_KEY, span_id)
+    if "dd=" in tracestate:
+        return tracestate.replace("dd=", f"dd={p_member};")
+    elif tracestate:
+        return f"dd={p_member},{tracestate}"
+    return f"dd={p_member}"
 
 
 class Response(object):

commit 656c538f19cbd8e5bee3079d93c6d99791ca8038

fix(tracing): fix context cloning (#8656)
Currently `Context._is_remote` is set to `True` on ALL child spans. This
is incorrect.

FIX: When copying a Context object from a parent span to a child span
ensure is_remote is set to `False`!

This regression was introduced in:
https://github.com/DataDog/dd-trace-py/commit/90a3e3fed47763abf64cb4c04c6a32f5c64076cb.
This bug is unreleased and does not require a release note.

`isRemote` is an OpenTelemetry concept and it's expected behavior is
described here:
https://opentelemetry.io/docs/specs/otel/trace/api/#isremote

## Checklist

- [x] Change(s) are motivated and described in the PR description
- [x] Testing strategy is described if automated tests are not included
in the PR
- [x] Risks are described (performance impact, potential for breakage,
maintainability)
- [x] Change is maintainable (easy to change, telemetry, documentation)
- [x] [Library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
are followed or label `changelog/no-changelog` is set
- [x] Documentation is included (in-code, generated user docs, [public
corp docs](https://github.com/DataDog/documentation/))
- [x] Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
- [x] If this PR changes the public interface, I've notified
`@DataDog/apm-tees`.
- [x] If change touches code that signs or publishes builds or packages,
or handles credentials of any kind, I've requested a review from
`@DataDog/security-design-and-guidance`.

## Reviewer Checklist

- [x] Title is accurate
- [x] All changes are related to the pull request's stated goal
- [x] Description motivates each change
- [x] Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes
- [x] Testing strategy adequately addresses listed risks
- [x] Change is maintainable (easy to change, telemetry, documentation)

ddtrace/_trace/context.py
@@ -116,7 +116,7 @@ class Context(object):
             lock=self._lock,
             baggage=self._baggage,
             span_links=self._span_links,
-            is_remote=self._is_remote,
+            is_remote=False,
         )
 
     def _update_tags(self, span):

tests/tracer/test_context.py
@@ -86,8 +86,8 @@ def test_traceparent_basic():
     "context",
     [
         Context(),
-        Context(trace_id=123, span_id=321),
-        Context(trace_id=123, span_id=321, dd_origin="synthetics", sampling_priority=2),
+        Context(trace_id=123, span_id=321, is_remote=True),
+        Context(trace_id=123, span_id=321, dd_origin="synthetics", sampling_priority=2, is_remote=False),
         Context(trace_id=123, span_id=321, meta={"meta": "value"}, metrics={"metric": 4.556}),
         Context(
             trace_id=123,
@@ -363,3 +363,21 @@ def test_tracestate(context, expected_tracestate):
 def test_dd_origin_character_set(ctx, expected_dd_origin):
     # type: (Context,Optional[str]) -> None
     assert ctx.dd_origin == expected_dd_origin
+
+
+def test_is_remote():
+    # type: () -> None
+    """Ensure that the is_remote flag is set to False on all local spans"""
+    # Context._is_remote should be True by default
+    ctx = Context(trace_id=123, span_id=321)
+    assert ctx._is_remote is True
+
+    # Span.context.is_remote should ALWAYS evaluate to False.
+    local_span = Span("span_with_context", context=ctx)
+    local_span.context.trace_id = 123
+    local_span.context.span_id = 321
+    local_span.context._is_remote = False
+
+    # is_remote should be set to False on root spans.
+    root = Span("root")
+    assert root.context._is_remote is False

tests/tracer/test_propagation.py
@@ -454,13 +454,14 @@ def test_last_dd_span_id():
     assert local_root.get_tag(LAST_DD_PARENT_ID_KEY) == "123067aa0ba902a6"
     for span in (root, child1, chunk_root):
         assert span.get_tag(LAST_DD_PARENT_ID_KEY) is None
-    # `p` value in tracestate is set using the current active datadog span
+    # `p` value in tracestate headers is set using the current active datadog span
+    for span in (child1, chunk_root, root, local_root):
+        headers = {}
+        HTTPPropagator.inject(span.context, headers)
+        assert "p:{:016x}".format(span.span_id) in headers["tracestate"]
+    # If a Datadog span is not active, `p` value is set to the last datadog span in the trace
     headers = {}
-    HTTPPropagator.inject(root.context, headers)
-    assert "p:{:016x}".format(root.span_id) in headers["tracestate"]
-    # `p` value is set to the last datadog span in the trace (via non_dd_remote_context's tracestate)
-    headers = {}
-    HTTPPropagator.inject(local_root.context, headers)
+    HTTPPropagator.inject(non_dd_remote_context, headers)
     assert "p:123067aa0ba902a6" in headers["tracestate"]
 
 

tests/tracer/test_tracer.py
@@ -352,6 +352,7 @@ class TracerTestCases(TracerTestCase):
         ctx = self.tracer.current_trace_context()
         assert ctx.trace_id == span.trace_id
         assert ctx.span_id == span.span_id
+        assert ctx._is_remote is False
 
     def test_tracer_current_span(self):
         # the current span is in the local Context()