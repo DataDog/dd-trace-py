340891c352ae8a2e4e53eb1864393de49147f821
lievan <evan.li@datadoqhq.com>
2024-03-20 17:37:32 -0400

    refactor

--- ddtrace/llmobs/decorators.py
+++ ddtrace/llmobs/decorators.py
@@ -25,24 +25,17 @@ def llm(
         @wraps(func)
         def gen_wrapper(*args, **kwargs):
             gen = func(*args, **kwargs)
+            span = None
+            disabled = not LLMObs.enabled or LLMObs._instance is None
 
-            if not LLMObs.enabled or LLMObs._instance is None:
+            if disabled:
                 log.warning("LLMObs.llm() cannot be used while LLMObs is disabled.")
-                for ret in gen:
-                    try:
-                        i = yield ret
-                        if i:
-                            gen.send(i)
-                    except GeneratorExit:
-                        gen.close()
-                        return
-                    except Exception as e:
-                        gen.throw(e)
-                return
+            else:
+                span = LLMObs.llm(
+                    model_name=model_name, model_provider=model_provider, name=span_name, session_id=session_id
+                )
 
-            with LLMObs.llm(
-                model_name=model_name, model_provider=model_provider, name=span_name, session_id=session_id
-            ):
+            try:
                 for ret in gen:
                     try:
                         i = yield ret
@@ -53,6 +46,9 @@ def llm(
                         return
                     except Exception as e:
                         gen.throw(e)
+            finally:
+                if span:
+                    span.finish()
 
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -88,9 +84,15 @@ def llmobs_decorator(operation_kind):
             @wraps(func)
             def gen_wrapper(*args, **kwargs):
                 gen = func(*args, **kwargs)
+                span = None
+                disabled = not LLMObs.enabled or LLMObs._instance is None
 
-                if not LLMObs.enabled or LLMObs._instance is None:
+                if disabled:
                     log.warning("LLMObs.{}() cannot be used while LLMObs is disabled.", operation_kind)
+                else:
+                    traced_operation = getattr(LLMObs, operation_kind, "workflow")
+                    span = traced_operation(name=span_name, session_id=session_id)
+                try:
                     for ret in gen:
                         try:
                             i = yield ret
@@ -101,20 +103,9 @@ def llmobs_decorator(operation_kind):
                             return
                         except Exception as e:
                             gen.throw(e)
-                    return
-
-                traced_operation = getattr(LLMObs, operation_kind, "workflow")
-                with traced_operation(name=span_name, session_id=session_id):
-                    for ret in gen:
-                        try:
-                            i = yield ret
-                            if i:
-                                gen.send(i)
-                        except GeneratorExit:
-                            gen.close()
-                            return
-                        except Exception as e:
-                            gen.throw(e)
+                finally:
+                    if span:
+                        span.finish()
 
             @wraps(func)
             def wrapper(*args, **kwargs):
