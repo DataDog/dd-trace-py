50ff37dd223f0a34a9f3f677b4e077d43e524ff4
ZStriker19 <zach.groves@datadoghq.com>
2024-03-20 17:16:38 -0400

    clean up

--- ddtrace/propagation/_database_monitoring.py
+++ ddtrace/propagation/_database_monitoring.py
@@ -66,7 +66,7 @@ class _DBM_Propagator(object):
         # run sampling before injection to propagate correct sampling priority
         if hasattr(ddtrace, "tracer") and hasattr(ddtrace.tracer, "sample"):
             if dbspan.context.sampling_priority is None:
-                ddtrace.tracer.sample(dbspan.local_root)
+                ddtrace.tracer.sample(dbspan._local_root)
 
         dbm_comment = self._get_dbm_comment(dbspan)
         if dbm_comment is None:
