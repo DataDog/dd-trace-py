d43d5b812002367ce9edc636932d12a8f37b6598
Federico Mon <federico.mon@datadoghq.com>
2024-04-04 12:49:35 +0200

    fix: use items()

--- ddtrace/appsec/_iast/_patch_modules.py
+++ ddtrace/appsec/_iast/_patch_modules.py
@@ -17,7 +17,7 @@ def patch_iast(patch_modules=IAST_PATCH_MODULES):
     # TODO: Devise the correct patching strategy for IAST
     from ddtrace._monkey import _on_import_factory
 
-    for python_module, vuln_modules in patch_modules:
+    for python_module, vuln_modules in patch_modules.items():
         for vuln_module in vuln_modules:
             when_imported(python_module)(
                 _on_import_factory(vuln_module, prefix="ddtrace.appsec._iast.taint_sinks", raise_errors=False)
