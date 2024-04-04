20861a81308c038386bd39d33f61354adf371eb8
Federico Mon <federico.mon@datadoghq.com>
2024-04-04 14:47:11 +0200

    fix patching

--- ddtrace/appsec/_common/taint_sinks/ssrf.py
+++ ddtrace/appsec/_common/taint_sinks/ssrf.py
@@ -30,3 +30,23 @@ def wrapped_open_ED4CF71136E15EBF(original_open_callable, instance, args, kwargs
             call_waf_callback({"SSRF_ADDRESS": url}, crop_trace="wrapped_open_ED4CF71136E15EBF")
             # DEV: Next part of the exploit prevention feature: add block here
     return original_open_callable(*args, **kwargs)
+
+
+def patch():
+    # type: () -> None
+    """Wrap hashing functions.
+    Weak hashing algorithms are those that have been proven to be of high risk, or even completely broken,
+    and thus are not fit for use.
+    """
+    from ddtrace.appsec._iast._patch import set_and_check_module_is_patched
+    from ddtrace.appsec._common_module_patches import try_wrap_function_wrapper
+
+    if not set_and_check_module_is_patched("urllib", default_attr="_datadog_ssrf_patch"):
+        return
+
+    try_wrap_function_wrapper("urllib.request", "OpenerDirector.open", wrapped_open_ED4CF71136E15EBF)
+
+
+def get_version():
+    # type: () -> str
+    return ""
