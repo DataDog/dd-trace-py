a1fe53fcf2d76dc790694437aab54649cf9114c4
Alberto Vara <alberto.vara@datadoghq.com>
2024-04-05 11:53:11 +0200

    feat: enable iast with patch_all

--- tests/appsec/integrations/test_flask_entrypoint_iast_patches.py
+++ tests/appsec/integrations/test_flask_entrypoint_iast_patches.py
@@ -1,20 +1,15 @@
-import dis
-import io
-import sys
-
-from ddtrace import ModuleWatchdog
-from tests.utils import override_env
-from tests.utils import override_global_config
+import pytest
 
 
-def _uninstall_watchdog_and_reload():
-    if len(ModuleWatchdog._instance._pre_exec_module_hooks) > 0:
-        ModuleWatchdog._instance._pre_exec_module_hooks.pop()
-    assert ModuleWatchdog._instance._pre_exec_module_hooks == set()
-
-
+@pytest.mark.subprocess()
 def test_ddtrace_iast_flask_patch():
-    _uninstall_watchdog_and_reload()
+    import dis
+    import io
+    import sys
+
+    from tests.utils import override_env
+    from tests.utils import override_global_config
+
     with override_global_config(dict(_iast_enabled=True)), override_env(
         dict(DD_IAST_ENABLED="true", DD_IAST_REQUEST_SAMPLING="100")
     ):
@@ -31,7 +26,21 @@ def test_ddtrace_iast_flask_patch():
         del sys.modules["tests.appsec.iast.fixtures.entrypoint.app_main_patched"]
 
 
+@pytest.mark.subprocess()
 def test_ddtrace_iast_flask_patch_iast_disabled():
+    import dis
+    import io
+    import sys
+
+    from ddtrace import ModuleWatchdog
+    from tests.utils import override_env
+    from tests.utils import override_global_config
+
+    def _uninstall_watchdog_and_reload():
+        if len(ModuleWatchdog._instance._pre_exec_module_hooks) > 0:
+            ModuleWatchdog._instance._pre_exec_module_hooks.pop()
+        assert ModuleWatchdog._instance._pre_exec_module_hooks == set()
+
     _uninstall_watchdog_and_reload()
     with override_global_config(dict(_iast_enabled=False)), override_env(dict(DD_IAST_ENABLED="false")):
         import tests.appsec.iast.fixtures.entrypoint.app_main_patched as flask_entrypoint
@@ -44,7 +53,21 @@ def test_ddtrace_iast_flask_patch_iast_disabled():
         del sys.modules["tests.appsec.iast.fixtures.entrypoint.app_main_patched"]
 
 
+@pytest.mark.subprocess()
 def test_ddtrace_iast_flask_no_patch():
+    import dis
+    import io
+    import sys
+
+    from ddtrace import ModuleWatchdog
+    from tests.utils import override_env
+    from tests.utils import override_global_config
+
+    def _uninstall_watchdog_and_reload():
+        if len(ModuleWatchdog._instance._pre_exec_module_hooks) > 0:
+            ModuleWatchdog._instance._pre_exec_module_hooks.pop()
+        assert ModuleWatchdog._instance._pre_exec_module_hooks == set()
+
     _uninstall_watchdog_and_reload()
     with override_global_config(dict(_iast_enabled=True)), override_env(
         dict(DD_IAST_ENABLED="true", DD_IAST_REQUEST_SAMPLING="100")
@@ -61,7 +84,21 @@ def test_ddtrace_iast_flask_no_patch():
         del sys.modules["tests.appsec.iast.fixtures.entrypoint.app"]
 
 
+@pytest.mark.subprocess()
 def test_ddtrace_iast_flask_app_create_app_enable_iast_propagation():
+    import dis
+    import io
+    import sys
+
+    from ddtrace import ModuleWatchdog
+    from tests.utils import override_env
+    from tests.utils import override_global_config
+
+    def _uninstall_watchdog_and_reload():
+        if len(ModuleWatchdog._instance._pre_exec_module_hooks) > 0:
+            ModuleWatchdog._instance._pre_exec_module_hooks.pop()
+        assert ModuleWatchdog._instance._pre_exec_module_hooks == set()
+
     _uninstall_watchdog_and_reload()
     with override_global_config(dict(_iast_enabled=True)), override_env(
         dict(DD_IAST_ENABLED="true", DD_IAST_REQUEST_SAMPLING="100")
@@ -79,7 +116,21 @@ def test_ddtrace_iast_flask_app_create_app_enable_iast_propagation():
         del sys.modules["tests.appsec.iast.fixtures.entrypoint.views"]
 
 
+@pytest.mark.subprocess()
 def test_ddtrace_iast_flask_app_create_app_patch_all():
+    import dis
+    import io
+    import sys
+
+    from ddtrace import ModuleWatchdog
+    from tests.utils import override_env
+    from tests.utils import override_global_config
+
+    def _uninstall_watchdog_and_reload():
+        if len(ModuleWatchdog._instance._pre_exec_module_hooks) > 0:
+            ModuleWatchdog._instance._pre_exec_module_hooks.pop()
+        assert ModuleWatchdog._instance._pre_exec_module_hooks == set()
+
     _uninstall_watchdog_and_reload()
     with override_global_config(dict(_iast_enabled=True)), override_env(dict(DD_IAST_ENABLED="true")):
         import tests.appsec.iast.fixtures.entrypoint.app_create_app_patch_all  # noqa: F401
@@ -95,7 +146,21 @@ def test_ddtrace_iast_flask_app_create_app_patch_all():
         del sys.modules["tests.appsec.iast.fixtures.entrypoint.views"]
 
 
+@pytest.mark.subprocess()
 def test_ddtrace_iast_flask_app_create_app_patch_all_enable_iast_propagation():
+    import dis
+    import io
+    import sys
+
+    from ddtrace import ModuleWatchdog
+    from tests.utils import override_env
+    from tests.utils import override_global_config
+
+    def _uninstall_watchdog_and_reload():
+        if len(ModuleWatchdog._instance._pre_exec_module_hooks) > 0:
+            ModuleWatchdog._instance._pre_exec_module_hooks.pop()
+        assert ModuleWatchdog._instance._pre_exec_module_hooks == set()
+
     _uninstall_watchdog_and_reload()
     with override_global_config(dict(_iast_enabled=True)), override_env(dict(DD_IAST_ENABLED="true")):
         import tests.appsec.iast.fixtures.entrypoint.app_create_app_patch_all_enable_iast_propagation  # noqa: F401
@@ -112,7 +177,21 @@ def test_ddtrace_iast_flask_app_create_app_patch_all_enable_iast_propagation():
         del sys.modules["tests.appsec.iast.fixtures.entrypoint.views"]
 
 
+@pytest.mark.subprocess()
 def test_ddtrace_iast_flask_app_create_app_patch_all_enable_iast_propagation_disabled():
+    import dis
+    import io
+    import pytest
+
+    from ddtrace import ModuleWatchdog
+    from tests.utils import override_env
+    from tests.utils import override_global_config
+
+    def _uninstall_watchdog_and_reload():
+        if len(ModuleWatchdog._instance._pre_exec_module_hooks) > 0:
+            ModuleWatchdog._instance._pre_exec_module_hooks.pop()
+        assert ModuleWatchdog._instance._pre_exec_module_hooks == set()
+
     _uninstall_watchdog_and_reload()
     with override_global_config(dict(_iast_enabled=False)), override_env(dict(DD_IAST_ENABLED="false")):
         import tests.appsec.iast.fixtures.entrypoint.app_create_app_patch_all_enable_iast_propagation  # noqa: F401
@@ -124,4 +203,5 @@ def test_ddtrace_iast_flask_app_create_app_patch_all_enable_iast_propagation_dis
         # Should have replaced the binary op with the aspect in add_test:
         assert "(add_aspect)" not in str_output
         assert "BINARY_ADD" in str_output or "BINARY_OP" in str_output
-        assert flask_entrypoint_views.add_test() == []
+        with pytest.raises(ImportError):
+            assert flask_entrypoint_views.add_test() == []
