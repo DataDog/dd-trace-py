324875a7d9a3920355c55848605704864329ee9e
sanchda <sanchda@users.noreply.github.com>
2024-04-05 16:42:02 +0000

    OK

--- ddtrace/profiling/collector/stack.pyx
+++ ddtrace/profiling/collector/stack.pyx
@@ -9,7 +9,7 @@ import attr
 import six
 
 from ddtrace import _threading as ddtrace_threading
-from ddtrace import context  # NOTE: IT IS VERY IMPORTANT THAT THIS IS CHANGED TO ddtrace._trace import context
+from ddtrace._trace import context
 from ddtrace._trace import span as ddspan
 from ddtrace.internal import compat
 from ddtrace.internal.datadog.profiling import ddup
