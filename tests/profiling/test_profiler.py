a14ae8fa002fa4aa1096fc86a6f870ad33d356ad
sanchda <sanchda@users.noreply.github.com>
2024-04-05 17:25:40 +0000

    Fix test again

--- tests/profiling/test_profiler.py
+++ tests/profiling/test_profiler.py
@@ -423,17 +423,17 @@ def test_profiler_ddtrace_deprecation():
 
     with warnings.catch_warnings():
         warnings.simplefilter("error", DeprecationWarning)
-        from ddtrace.profiler import _threading  # noqa:F401
-        from ddtrace.profiler import event  # noqa:F401
-        from ddtrace.profiler import profiler  # noqa:F401
-        from ddtrace.profiler import recorder  # noqa:F401
-        from ddtrace.profiler import scheduler  # noqa:F401
-        from ddtrace.profiler.collector import _lock  # noqa:F401
-        from ddtrace.profiler.collector import _task  # noqa:F401
-        from ddtrace.profiler.collector import _traceback  # noqa:F401
-        from ddtrace.profiler.collector import memalloc  # noqa:F401
-        from ddtrace.profiler.collector import stack  # noqa:F401
-        from ddtrace.profiler.collector import stack_event  # noqa:F401
-        from ddtrace.profiler.exporter import file  # noqa:F401
-        from ddtrace.profiler.exporter import http  # noqa:F401
-        from ddtrace.profiler.exporter import pprof  # noqa:F401
+        from ddtrace.profiling import _threading  # noqa:F401
+        from ddtrace.profiling import event  # noqa:F401
+        from ddtrace.profiling import profiler  # noqa:F401
+        from ddtrace.profiling import recorder  # noqa:F401
+        from ddtrace.profiling import scheduler  # noqa:F401
+        from ddtrace.profiling.collector import _lock  # noqa:F401
+        from ddtrace.profiling.collector import _task  # noqa:F401
+        from ddtrace.profiling.collector import _traceback  # noqa:F401
+        from ddtrace.profiling.collector import memalloc  # noqa:F401
+        from ddtrace.profiling.collector import stack  # noqa:F401
+        from ddtrace.profiling.collector import stack_event  # noqa:F401
+        from ddtrace.profiling.exporter import file  # noqa:F401
+        from ddtrace.profiling.exporter import http  # noqa:F401
+        from ddtrace.profiling.exporter import pprof  # noqa:F401
