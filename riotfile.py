6446029e6b791bc1d943942dd47a9039d11a4d47
Alberto Vara <alberto.vara@datadoghq.com>
2024-04-10 15:36:27 +0200

    chore: fix CI IAST integration flaky tests

--- riotfile.py
+++ riotfile.py
@@ -190,6 +190,7 @@ venv = Venv(
             command="pytest --no-cov tests/appsec/iast_tdd_propagation/",
             pkgs={
                 "flask": "~=3.0",
+                "pycryptodome": latest,
                 "sqlalchemy": "~=2.0.23",
                 "pony": latest,
                 "aiosqlite": latest,
@@ -218,7 +219,6 @@ venv = Venv(
             command="pytest {cmdargs} tests/appsec/integrations/",
             pkgs={
                 "requests": latest,
-                "pycryptodome": latest,
                 "gunicorn": latest,
                 "psycopg2-binary": "~=2.9.9",
             },
