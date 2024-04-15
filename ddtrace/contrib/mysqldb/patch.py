e421343690b5c3fb950eed1b1fe309e49f76e3d0
William Conti <william.conti@datadoghq.com>
2024-04-15 14:47:18 -0400

    force test failures

--- ddtrace/contrib/mysqldb/patch.py
+++ ddtrace/contrib/mysqldb/patch.py
@@ -98,7 +98,7 @@ def _connect(func, instance, args, kwargs):
 
 
 def patch_conn(conn, *args, **kwargs):
-    tags = dict()
+    assert 1 == 0
     tags = {
         t: kwargs[k] if k in kwargs else args[p] for t, (k, p) in KWPOS_BY_TAG.items() if k in kwargs or len(args) > p
     }
