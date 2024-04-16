8fa52a68bbfa8f36ac9f920979b2c63d3fc19840
Alberto Vara <alberto.vara@datadoghq.com>
2024-04-16 17:01:49 +0200

    chore(iast): fix error metrics format

--- ddtrace/appsec/_iast/_taint_tracking/__init__.py
+++ ddtrace/appsec/_iast/_taint_tracking/__init__.py
@@ -94,7 +94,7 @@ def iast_taint_log_error(msg):
 
         stack = inspect.stack()
         frame_info = "\n".join("%s %s" % (frame_info.filename, frame_info.lineno) for frame_info in stack[:7])
-        log_message = "%s:\n%s", msg, frame_info
+        log_message = "%s:\n%s" % (msg, frame_info)
         log.warning(log_message)
         _set_iast_error_metric("IAST propagation error. %s" % msg)
     else:
