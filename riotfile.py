bfdf77eecf1746c833816bf4b8ff145f751cfb87
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-04-19 15:13:06 +0200

    Add requests as testing dependency
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- riotfile.py
+++ riotfile.py
@@ -125,6 +125,9 @@ venv = Venv(
             name="appsec",
             pys=select_pys(),
             command="pytest {cmdargs} tests/appsec/appsec/",
+            pkgs={
+                "requests": latest,
+            },
         ),
         Venv(
             name="appsec_iast",
