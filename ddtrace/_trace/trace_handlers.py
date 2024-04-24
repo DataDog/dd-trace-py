5bfef7356b596d0a438af2329293582a1ef13475
Emmett Butler <emmett.butler321@gmail.com>
2024-04-24 08:41:57 -0700

    merge oppsie

--- ddtrace/_trace/trace_handlers.py
+++ ddtrace/_trace/trace_handlers.py
@@ -643,9 +643,7 @@ def _on_botocore_patched_bedrock_api_call_success(ctx, reqid, latency, input_tok
     span.set_tag_str("bedrock.response.id", reqid)
     span.set_tag_str("bedrock.response.duration", latency)
     span.set_tag_str("bedrock.usage.prompt_tokens", input_token_count)
-
-
-span.set_tag_str("bedrock.usage.completion_tokens", output_token_count)
+    span.set_tag_str("bedrock.usage.completion_tokens", output_token_count)
 
 
 def _on_botocore_bedrock_process_response(
