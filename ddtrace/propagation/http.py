71b38a2dce06c0b6a0a79a343cb18bf59275f4e0
Munir Abdinur <munir_abdinur@hotmail.com>
2024-05-06 17:07:36 -0400

    chore(w3c): ensure last datadog parent id is always set

--- ddtrace/propagation/http.py
+++ ddtrace/propagation/http.py
@@ -921,6 +921,8 @@ class HTTPPropagator(object):
                 ts = _extract_header_value(_POSSIBLE_HTTP_HEADER_TRACESTATE, normalized_headers)
                 if ts:
                     primary_context._meta[W3C_TRACESTATE_KEY] = ts
+                    # W3C Phase 3: Ensure the last datadog parent id is always set on the primary context
+                    primary_context._meta[LAST_DD_PARENT_ID_KEY] = context._meta[LAST_DD_PARENT_ID_KEY]
         primary_context._span_links = links
         return primary_context
 
