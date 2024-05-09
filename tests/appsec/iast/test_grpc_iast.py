85d156645727f0106fd117cf15f68bbd398780cb
Federico Mon <federico.mon@datadoghq.com>
2024-05-09 18:02:34 +0200

    regression test

--- tests/appsec/iast/test_grpc_iast.py
+++ tests/appsec/iast/test_grpc_iast.py
@@ -1,5 +1,6 @@
 import grpc
 from grpc._grpcio_metadata import __version__ as _GRPC_VERSION
+import mock
 
 from tests.contrib.grpc.common import GrpcBaseTestCase
 from tests.contrib.grpc.hello_pb2 import HelloRequest
@@ -72,3 +73,21 @@ class GrpcTestIASTCase(GrpcBaseTestCase):
                     res = stub1.SayHelloLast(requests_iterator)
                     assert hasattr(res, "message")
                     _check_test_range(res.message)
+
+    def test_taint_iast_patching_import_error(self):
+        with mock.patch.dict("sys.modules", {"google._upb._message": None}), override_env({"DD_IAST_ENABLED": "True"}):
+            from collections import UserDict
+
+            from ddtrace.appsec._handlers import _custom_protobuf_getattribute
+
+            def __saved_getattr(self, name):
+                return self
+
+            class MyUserDict(UserDict):
+                pass
+
+            setattr(MyUserDict, "__saved_getattr", __saved_getattr)
+            original_dict = {"apple": 1, "banana": 2}
+            mutable_mapping = MyUserDict(original_dict)
+
+            _custom_protobuf_getattribute(mutable_mapping, "apple")
