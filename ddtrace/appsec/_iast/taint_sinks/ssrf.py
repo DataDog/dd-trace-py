18a0ac37ecc1f3f6a11b35a4d2ec8d271edb1398
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-05-10 21:51:05 +0200

    fmt
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/appsec/_iast/taint_sinks/ssrf.py
+++ ddtrace/appsec/_iast/taint_sinks/ssrf.py
@@ -1,10 +1,9 @@
 from typing import Callable
 
-from ddtrace.internal.utils import get_argument_value, ArgumentError
-
-from ddtrace.internal.utils.importlib import func_name
-
 from ddtrace.internal.logger import get_logger
+from ddtrace.internal.utils import ArgumentError
+from ddtrace.internal.utils import get_argument_value
+from ddtrace.internal.utils.importlib import func_name
 
 from ..._constants import IAST_SPAN_TAGS
 from .. import oce
