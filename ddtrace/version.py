05826a7d20c9a7b78570323a544e6ef707d7ab36
Brett Langdon <brett.langdon@datadoghq.com>
2024-05-10 12:33:26 -0400

    attempt at fixing import issue

--- ddtrace/version.py
+++ ddtrace/version.py
@@ -1,7 +1,7 @@
 def get_version():
     # type: () -> str
     try:
-        from ._version import version
+        from ddtrace._version import version
 
         return version
     except ImportError:
