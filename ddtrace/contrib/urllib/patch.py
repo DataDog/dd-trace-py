6c023c1d5f1eb7eddf727ffdd6e4509288f2db25
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-05-10 16:14:09 +0200

    Add telemetry metric instrumented
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/contrib/urllib/patch.py
+++ ddtrace/contrib/urllib/patch.py
@@ -1,6 +1,9 @@
 import urllib.request
 
 from ddtrace.appsec._common_module_patches import wrapped_request_D8CB81E472AF98A2 as _wrap_open
+from ddtrace.appsec._iast._metrics import _set_metric_iast_instrumented_sink
+from ddtrace.appsec._iast.constants import VULN_SSRF
+from ddtrace.settings.asm import config as asm_config
 from ddtrace.vendor.wrapt import wrap_function_wrapper as _w
 
 from ..trace_utils import unwrap as _u
@@ -18,6 +21,8 @@ def patch():
     urllib.request.__datadog_patch = True
 
     _w("urllib.request", "urlopen", _wrap_open)
+    if asm_config._iast_enabled:
+        _set_metric_iast_instrumented_sink(VULN_SSRF)
 
 
 def unpatch():
