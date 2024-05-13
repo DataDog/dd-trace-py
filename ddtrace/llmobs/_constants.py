8b7be5cf03afe12ff09b5dcc616de31936c710e7
lievan <evan.li@datadoqhq.com>
2024-05-13 15:20:01 -0400

    lint

--- ddtrace/llmobs/_constants.py
+++ ddtrace/llmobs/_constants.py
@@ -17,4 +17,6 @@ OUTPUT_DOCUMENTS = "_ml_obs.meta.output.documents"
 OUTPUT_MESSAGES = "_ml_obs.meta.output.messages"
 OUTPUT_VALUE = "_ml_obs.meta.output.value"
 
-SPAN_START_WHILE_DISABLED_WARNING = "Span started while LLMObs is disabled. Traces will not be sent to LLM Observability."
+SPAN_START_WHILE_DISABLED_WARNING = (
+    "Span started while LLMObs is disabled." "Traces will not be sent to LLM Observability."
+)
