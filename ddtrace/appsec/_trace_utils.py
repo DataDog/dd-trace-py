ecf8cdda5e5a5cfa7710adb1a99b94bb88183710
Federico Mon <federico.mon@datadoghq.com>
2024-05-13 13:28:11 +0200

    set appsec propagation tag

--- ddtrace/appsec/_trace_utils.py
+++ ddtrace/appsec/_trace_utils.py
@@ -26,6 +26,8 @@ def _asm_manual_keep(span: Span) -> None:
     span.set_tag(constants.MANUAL_KEEP_KEY)
     # set decision maker to ASM = -5
     span.set_tag_str(SAMPLING_DECISION_TRACE_TAG_KEY, "-%d" % SamplingMechanism.APPSEC)
+    # set appsec propagation tag
+    span.set_tag_str("_dd.p.appsec", "1")
 
 
 def _track_user_login_common(
