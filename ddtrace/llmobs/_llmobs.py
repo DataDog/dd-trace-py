735d9c22c16e35bf00a77d6d6e875a7f812fb64e
lievan <evan.li@datadoqhq.com>
2024-05-14 07:47:56 -0400

    try catch patch all integrations

--- ddtrace/llmobs/_llmobs.py
+++ ddtrace/llmobs/_llmobs.py
@@ -162,8 +162,10 @@ class LLMObs(Service):
             "bedrock": lambda: patch(botocore=True),
         }
         if not integrations:
-            print("hi")
-            patch(botocore=True, langchain=True, openai=True)
+            try:
+                patch(botocore=True, langchain=True, openai=True)
+            except Exception:
+                log.warning("couldn't patch all LLMObs integrations", exc_info=True)
         else:
             for integration in integrations:
                 integration = integration.lower()
