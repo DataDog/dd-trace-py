cc0861f78f8d617496b50b8dd2cf2e1bdc914169
Joey Zhao <5253430+joeyzhao2018@users.noreply.github.com>
2024-05-15 12:25:44 -0400

    link

--- tests/contrib/asyncio/test_lazyimport.py
+++ tests/contrib/asyncio/test_lazyimport.py
@@ -1,6 +1,6 @@
-import pytest
 import ddtrace.auto  # noqa: F401,I001
 from ddtrace import tracer  # noqa: I001
+import pytest # noqa: I001
 
 
 @pytest.mark.subprocess()
@@ -8,7 +8,7 @@ def test_lazy_import():
     assert tracer.current_trace_context() is None
     span = tracer.trace("itsatest", service="test", resource="resource", span_type="http")
     assert tracer.current_trace_context() is not None
-    import asyncio  # noqa: EF401
+    import asyncio  # noqa: F401
 
     assert tracer.current_trace_context() is not None
     span.finish()
