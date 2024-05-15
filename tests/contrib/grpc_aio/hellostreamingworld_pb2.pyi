eb1abc720a753d641f9262fa54215f6c55eda478
William Conti <william.conti@datadoghq.com>
2024-05-15 14:58:08 -0400

    fix lint again

--- tests/contrib/grpc_aio/hellostreamingworld_pb2.pyi
+++ tests/contrib/grpc_aio/hellostreamingworld_pb2.pyi
@@ -4,6 +4,7 @@ from typing import Optional as _Optional
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 
+
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class HelloReply(_message.Message):
