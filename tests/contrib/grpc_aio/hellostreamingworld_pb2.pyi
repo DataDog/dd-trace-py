23120f52563b15d511cd59f60fff3d0faa63a3c9
William Conti <william.conti@datadoghq.com>
2024-05-16 12:42:24 -0400

    plz work

--- tests/contrib/grpc_aio/hellostreamingworld_pb2.pyi
+++ tests/contrib/grpc_aio/hellostreamingworld_pb2.pyi
@@ -2,21 +2,22 @@
 from typing import ClassVar as _ClassVar
 from typing import Optional as _Optional
 
-from google.protobuf import descriptor as _descriptor
-from google.protobuf import message as _message
+from ddtrace.internal.compat import PYTHON_VERSION_INFO
 
-DESCRIPTOR: _descriptor.FileDescriptor
+if PYTHON_VERSION_INFO > (3, 6):
+    from google.protobuf import descriptor as _descriptor
+    from google.protobuf import message as _message
 
-class HelloReply(_message.Message):
-    __slots__ = ["message"]
-    MESSAGE_FIELD_NUMBER: _ClassVar[int]
-    message: str
-    def __init__(self, message: _Optional[str] = ...) -> None: ...
-
-class HelloRequest(_message.Message):
-    __slots__ = ["name", "num_greetings"]
-    NAME_FIELD_NUMBER: _ClassVar[int]
-    NUM_GREETINGS_FIELD_NUMBER: _ClassVar[int]
-    name: str
-    num_greetings: str
-    def __init__(self, name: _Optional[str] = ..., num_greetings: _Optional[str] = ...) -> None: ...
+    DESCRIPTOR: _descriptor.FileDescriptor
+    class HelloReply(_message.Message):
+        __slots__ = ["message"]
+        MESSAGE_FIELD_NUMBER: _ClassVar[int]
+        message: str
+        def __init__(self, message: _Optional[str] = ...) -> None: ...
+    class HelloRequest(_message.Message):
+        __slots__ = ["name", "num_greetings"]
+        NAME_FIELD_NUMBER: _ClassVar[int]
+        NUM_GREETINGS_FIELD_NUMBER: _ClassVar[int]
+        name: str
+        num_greetings: str
+        def __init__(self, name: _Optional[str] = ..., num_greetings: _Optional[str] = ...) -> None: ...
