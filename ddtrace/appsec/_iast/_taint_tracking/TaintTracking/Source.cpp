11cb950aee7cf5451b9e78a2cc2eff8715d4c4ae
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-05-16 08:43:10 +0200

    clang-format
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/appsec/_iast/_taint_tracking/TaintTracking/Source.cpp
+++ ddtrace/appsec/_iast/_taint_tracking/TaintTracking/Source.cpp
@@ -14,15 +14,13 @@ Source::Source(string name, string value, const OriginType origin)
   : name(std::move(name))
   , value(std::move(value))
   , origin(origin)
-{
-}
+{}
 
 Source::Source(int name, string value, const OriginType origin)
   : name(origin_to_str(OriginType{ name }))
   , value(std::move(value))
   , origin(origin)
-{
-}
+{}
 
 string
 Source::toString() const
