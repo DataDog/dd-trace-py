c5fe6cabebb8d2322ce04dcf21b270daa080013a
William Conti <william.conti@datadoghq.com>
2024-05-16 09:07:42 -0400

    turn isort off for .pyi file

--- tests/contrib/grpc_aio/hellostreamingworld_pb2.pyi
+++ tests/contrib/grpc_aio/hellostreamingworld_pb2.pyi
@@ -1,9 +1,9 @@
+# isort: off
 from typing import ClassVar as _ClassVar
 from typing import Optional as _Optional
 
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
