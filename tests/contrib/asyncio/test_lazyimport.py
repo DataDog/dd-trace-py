44f0416bf6f136e90dcc1a66ae93e932b54b42b0
Brett Langdon <brett.langdon@datadoghq.com>
2024-05-20 15:13:16 -0400

    fix lazyimport test

--- tests/contrib/asyncio/test_lazyimport.py
+++ tests/contrib/asyncio/test_lazyimport.py
@@ -6,13 +6,13 @@ def test_lazy_import():
     import ddtrace.auto  # noqa: F401,I001
     from ddtrace import tracer  # noqa: I001
 
-    assert tracer.current_trace_context() is None
+    assert tracer.context_provider.active() is None
     span = tracer.trace("itsatest", service="test", resource="resource", span_type="http")
-    assert tracer.current_trace_context() is span
+    assert tracer.context_provider.active() is span
 
     # Importing asyncio after starting a trace does not remove the current active span
     import asyncio  # noqa: F401
 
-    assert tracer.current_trace_context() is span
+    assert tracer.context_provider.active() is span
     span.finish()
-    assert tracer.current_trace_context() is None
+    assert tracer.context_provider.active() is None
