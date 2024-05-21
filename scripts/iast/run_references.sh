14cdbd51e82ed3a18fdfb570d24a3d9f44872f52
Alberto Vara <alberto.vara@datadoghq.com>
2024-05-21 13:52:16 +0200

    chore(iast): add local debug scripts to find leaks

--- scripts/iast/run_references.sh
+++ scripts/iast/run_references.sh
@@ -1,15 +1,3 @@
-export PATH=$PATH:$PWD
-export PYTHONPATH=$PYTHONPATH:$PWD
-export PYTHONMALLOC=malloc
-export DD_COMPILE_DEBUG=true
-export DD_TRACE_ENABLED=true
-export DD_IAST_ENABLED=true
-export _DD_IAST_DEBUG=true
-export DD_IAST_REQUEST_SAMPLING=100
-export _DD_APPSEC_DEDUPLICATION_ENABLED=false
-export DD_INSTRUMENTATION_TELEMETRY_ENABLED=false
-export DD_REMOTE_CONFIGURATION_ENABLED=false
-
 # python3.11 setup.py build_ext --inplace
 python3.11 -m pip install -r scripts/iast/requirements.txt
 python3.11 -m ddtrace.commands.ddtrace_run python3.11 scripts/iast/test_references.py
--- scripts/iast/test_leak_functions.py
+++ scripts/iast/test_leak_functions.py
@@ -10,7 +10,7 @@ def test_main():
         rounds = int(sys.argv[1])
     except ValueError:
         rounds = 1
-
+    print("Test %d rounds" % rounds)
     for i in range(rounds):
         try:
             test_doit()
@@ -20,6 +20,7 @@ def test_main():
         if i % 10000 == 0:
             print("Round %d Max RSS: " % i, end="")
             print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024)
+    print("Round %d Max RSS: " % i, end="")
     print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024)
 
 
