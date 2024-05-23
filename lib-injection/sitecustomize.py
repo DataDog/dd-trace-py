69c2c237268e8ac1774fa44077bfb6c6fe5cb4c3
ZStriker19 <zach.groves@datadoghq.com>
2024-05-22 23:38:19 -0400

    modifications for updated metrics, make python 2.7 compatible

--- lib-injection/sitecustomize.py
+++ lib-injection/sitecustomize.py
@@ -2,6 +2,8 @@
 This module when included on the PYTHONPATH will update the PYTHONPATH to point to a directory
 containing the ddtrace package compatible with the current Python version and platform.
 """
+from __future__ import print_function  # noqa: E402
+
 import json
 import os
 import platform
@@ -30,11 +32,11 @@ allow_unsupported_integrations = os.environ.get("DD_TRACE_ALLOW_UNSUPPORTED_SSI_
 installed_packages = pkg_resources.working_set
 installed_packages = {pkg.key: pkg.version for pkg in installed_packages}
 
-python_runtime = sys.implementation.name
+python_runtime = platform.python_implementation().lower()
 python_version = ".".join(str(i) for i in sys.version_info[:2])
 
 
-def create_count_metric(metric, tags):
+def create_count_metric(metric, tags={}):
     return {
         "metric": metric,
         "tags": tags,
@@ -63,15 +65,15 @@ def gen_telemetry_payload(data):
 
 def send_telemetry(event):
     event_json = json.dumps(event)
-    # remove this stuff once rust implementation is done
-    env_vars = os.environ.copy()  # Start with a copy of the current environment
-    env_vars["DD_INSTRUMENT_SERVICE_WITH_APM"] = "false"
-    subprocess.run(
-        ["python3", os.path.join(os.path.dirname(os.path.abspath(__file__)), "send_telemetry.py")],
-        env=env_vars,
-        text=True,
-        input=event_json,
+    print(event_json)
+    p = subprocess.Popen(
+        ["/home/kyle_verhoog_datadoghq_com/dd_telemetry", str(os.getpid())],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
     )
+    p.communicate(input=event_json)
 
 
 debug_mode = os.environ.get("DD_TRACE_DEBUG", "").lower() in ("true", "1", "t")
@@ -83,7 +85,6 @@ def _get_clib():
 
     If GNU is not detected then returns MUSL.
     """
-    import platform
 
     libc, version = platform.libc_ver()
     if libc == "glibc":
@@ -91,12 +92,13 @@ def _get_clib():
     return "musl"
 
 
-def _log(msg, *args, level="info"):
+def _log(msg, *args, **kwargs):
     """Log a message to stderr.
 
     This function is provided instead of built-in Python logging since we can't rely on any logger
     being configured.
     """
+    level = kwargs.get("level", "info")
     if debug_mode:
         asctime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         msg = "[%s] [%s] datadog.autoinstrumentation(pid: %d): " % (asctime, level.upper(), os.getpid()) + msg % args
@@ -104,9 +106,12 @@ def _log(msg, *args, level="info"):
 
 
 def _inject():
+    skip_instr_events = []
+    runtime_incomp_event = False
+    integration_incomp_event = False
     try:
         import ddtrace
-    except ModuleNotFoundError:
+    except ImportError:
         _log("user-installed ddtrace not found, configuring application to use injection site-packages")
 
         platform = "manylinux2014" if _get_clib() == "gnu" else "musllinux_1_1"
@@ -123,45 +128,63 @@ def _inject():
             if package_name in pkgs_allow_list:
                 # TODO: this checking code will have to be more intelligent in the future
                 if package_version < pkgs_allow_list[package_name]:
-                    incompatible_packages[package_name] = {
-                        "version_in_use": package_version,
-                        "required_version": pkgs_allow_list[package_name],
-                    }
+                    incompatible_packages[package_name] = package_version
+                    # incompatible_packages[package_name] = {
+                    #     "version_in_use": package_version,
+                    #     "required_version": pkgs_allow_list[package_name],
+                    # }
         if incompatible_packages:
-            _log(f"Found incompatible packages: {incompatible_packages}.", level="debug")
+            _log("Found incompatible packages: %s." % incompatible_packages, level="debug")
             if not allow_unsupported_integrations:
                 _log("Aborting dd-trace-py instrumentation.", level="debug")
 
-                event = gen_telemetry_payload(
-                    create_count_metric(
-                        "bootstrap.skipped",
-                        {
-                            **incompatible_packages,
-                            "reason": "integration",
-                        },
+                integration_incomp_event = True
+
+                for key, value in incompatible_packages.items():
+                    event_skipped_integration = gen_telemetry_payload(
+                        create_count_metric(
+                            "bootstrap.skipped.integration",
+                            {
+                                "integration_name": key,
+                                "integration_version": value,
+                                # need to add to doc if we want this
+                                "required_version": pkgs_allow_list[key],
+                            },
+                        )
                     )
-                )
-                send_telemetry(event)
-                return
+                    skip_instr_events.append(event_skipped_integration)
+
             else:
                 _log(
                     "DD_TRACE_ALLOW_UNSUPPORTED_SSI_INTEGRATIONS set to True, allowing unsupported integrations.",
                     level="debug",
                 )
         if python_version not in runtimes_allow_list.get(python_runtime, []):
-            _log(f"Found incompatible runtime: {python_runtime} {python_version}.", level="debug")
+            _log("Found incompatible runtime: %s %s." % (python_runtime, python_version), level="debug")
             if not allow_unsupported_runtimes:
                 _log("Aborting dd-trace-py instrumentation.", level="debug")
 
-                event = gen_telemetry_payload(
-                    create_count_metric("bootstrap.skipped", {"reason": "runtime"})
-                )  # noqa: F841
-                send_telemetry(event)
+                runtime_incomp_event = True
+
+                event_skipped_runtime = gen_telemetry_payload(create_count_metric("bootstrap.skipped.runtime"))
+                skip_instr_events.append(event_skipped_runtime)
             else:
                 _log(
                     "DD_TRACE_ALLOW_UNSUPPORTED_SSI_RUNTIMES set to True, allowing unsupported runtimes.",
                     level="debug",
                 )
+        if skip_instr_events:
+            for event in skip_instr_events:
+                send_telemetry(event)
+            skip_event = gen_telemetry_payload(
+                create_count_metric(
+                    "bootstrap.skipped",
+                    {"runtime_skip": runtime_incomp_event, "integration_skip": integration_incomp_event},
+                )
+            )
+            send_telemetry(skip_event)
+
+            return
 
         site_pkgs_path = os.path.join(pkgs_path, "site-packages-ddtrace-py%s-%s" % (python_version, platform))
         _log("site-packages path is %r" % site_pkgs_path, level="debug")
@@ -205,14 +228,16 @@ def _inject():
                 _log("successfully configured ddtrace package, python path is %r" % os.environ["PYTHONPATH"])
                 event = gen_telemetry_payload(create_count_metric("bootstrap.completed", {}))
                 send_telemetry(event)
-            except BaseException as e:
+            except Exception as e:
                 # maybe switch errortype since error will have too high over cardinality
                 event = gen_telemetry_payload(create_count_metric("bootstrap.error", {"error": str(e)}))
                 send_telemetry(event)
                 _log("failed to load ddtrace.bootstrap.sitecustomize: %s" % e, level="error")
                 return
     else:
-        _log(f"user-installed ddtrace found: {ddtrace.__version__}, aborting site-packages injection", level="warning")
+        _log(
+            "user-installed ddtrace found: %s, aborting site-packages injection" % ddtrace.__version__, level="warning"
+        )
 
 
 _inject()
