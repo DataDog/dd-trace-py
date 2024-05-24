c6e43664d5328ffe06f4de48a7dc942e759de7ac
Christophe Papazian <christophe.papazian@datadoghq.com>
2024-05-24 10:15:35 +0200

    add exploit prevention capabilities

--- ddtrace/appsec/_capabilities.py
+++ ddtrace/appsec/_capabilities.py
@@ -29,6 +29,14 @@ class Flags(enum.IntFlag):
     ASM_CUSTOM_BLOCKING_RESPONSE = 1 << 9
     ASM_TRUSTED_IPS = 1 << 10
     ASM_API_SECURITY_SAMPLE_RATE = 1 << 11
+    ASM_RASP_SQLI = 1 << 21
+    ASM_RASP_LFI = 1 << 22
+    ASM_RASP_SSRF = 1 << 23
+    ASM_RASP_SHI = 1 << 24
+    ASM_RASP_XXE = 1 << 25
+    ASM_RASP_RCE = 1 << 26
+    ASM_RASP_NOSQLI = 1 << 27
+    ASM_RASP_XSS = 1 << 28
 
 
 _ALL_ASM_BLOCKING = (
@@ -43,6 +51,8 @@ _ALL_ASM_BLOCKING = (
     | Flags.ASM_CUSTOM_BLOCKING_RESPONSE
 )
 
+_ALL_RASP = Flags.ASM_RASP_LFI | Flags.ASM_RASP_SSRF
+
 
 def _rc_capabilities(test_tracer: Optional[ddtrace.Tracer] = None) -> Flags:
     tracer = ddtrace.tracer if test_tracer is None else test_tracer
@@ -52,6 +62,8 @@ def _rc_capabilities(test_tracer: Optional[ddtrace.Tracer] = None) -> Flags:
             value |= Flags.ASM_ACTIVATION
         if tracer._appsec_processor and _appsec_rc_file_is_not_static():
             value |= _ALL_ASM_BLOCKING
+            if asm_config._ep_enabled:
+                value |= _ALL_RASP
         if asm_config._api_security_enabled:
             value |= Flags.ASM_API_SECURITY_SAMPLE_RATE
     return value
