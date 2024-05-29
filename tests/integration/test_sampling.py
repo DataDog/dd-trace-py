960f408ca68eb864e6519442a752b1c094fb35c7
Munir Abdinur <munir_abdinur@hotmail.com>
2024-05-29 13:07:32 -0400

    add regression test

--- tests/integration/test_sampling.py
+++ tests/integration/test_sampling.py
@@ -297,3 +297,47 @@ def test_extended_sampling_float_special_case_match_star(writer, tracer):
     tracer.configure(sampler=sampler, writer=writer)
     with tracer.trace(name="should_send") as span:
         span.set_tag("tag", 20.1)
+
+
+def test_rate_limiter_on_spans(tracer):
+    """
+    Ensure that the rate limiter is applied to spans
+    """
+    tracer.configure(sampler=DatadogSampler(rate_limit=10))
+    spans = []
+    # Generate 10 spans with the start and finish time in same second
+    for x in range(10):
+        start_time = x / 10
+        span = tracer.trace(name=f"span {start_time}")
+        span.start = start_time
+        span.finish(1 - start_time)
+        spans.append(span)
+    # Generate 11th span in the same second
+    dropped_span = tracer.trace(name=f"span {start_time}")
+    dropped_span.start = 0.8
+    dropped_span.finish(0.9)
+    # Spans are sampled on flush
+    tracer.flush()
+    # Since the rate limiter is set to 10, first ten spans should be kept
+    for span in spans:
+        assert span.context.sampling_priority > 0
+    # 11th span should be dropped
+    assert dropped_span.context.sampling_priority < 0
+
+
+def test_rate_limiter_on_long_running_spans(tracer):
+    """
+    Ensure that the rate limiter is applied correctly to long spans
+    """
+    tracer.configure(sampler=DatadogSampler(rate_limit=5))
+
+    span_m30 = tracer.trace(name="march 30")
+    span_m30.start = 1622347257  # Mar 30 2021
+    span_m30.finish(1617333414)  # April 2 2021
+
+    span_m29 = tracer.trace(name="march 29")
+    span_m29.start = 1616999414  # Mar 29 2021
+    span_m29.finish(1617333414)  # April 2 2021
+
+    assert span_m29.context.sampling_priority > 0
+    assert span_m30.context.sampling_priority > 0
