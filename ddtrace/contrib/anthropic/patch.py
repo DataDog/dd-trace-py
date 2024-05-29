3c50035de891bb6197776e2f287b5aff8b14e377
William Conti <william.conti@datadoghq.com>
2024-05-28 20:21:52 -0400

    initial commit

---
+++ ddtrace/contrib/anthropic/patch.py
@@ -0,0 +1,290 @@
+import json
+import os
+import sys
+from typing import Any
+from typing import Iterable
+from typing import Optional
+from typing import Union
+
+import anthropic
+from pydantic import SecretStr
+
+from ddtrace import config
+from ddtrace.contrib.trace_utils import unwrap
+from ddtrace.contrib.trace_utils import with_traced_module
+from ddtrace.contrib.trace_utils import wrap
+from ddtrace.internal.logger import get_logger
+from ddtrace.internal.utils import get_argument_value
+from ddtrace.internal.utils.version import parse_version
+from ddtrace.llmobs._integrations import AnthropicIntegration
+from ddtrace.pin import Pin
+
+
+log = get_logger(__name__)
+
+
+def get_version():
+    # type: () -> str
+    return getattr(anthropic, "__version__", "")
+
+
+ANTHROPIC_VERSION = parse_version(get_version())
+BASE_MODULE = anthropic
+BASE_MODULE_NAME = getattr(anthropic, "__name__", "anthropic")
+
+
+config._add(
+    "anthropic",
+    {
+        "span_prompt_completion_sample_rate": float(os.getenv("DD_ANTHROPIC_SPAN_PROMPT_COMPLETION_SAMPLE_RATE", 1.0)),
+        "span_char_limit": int(os.getenv("DD_ANTHROPIC_SPAN_CHAR_LIMIT", 128)),
+    },
+)
+
+
+def _extract_model_name(instance: Any) -> Optional[str]:
+    """Extract model name or ID from llm instance."""
+    for attr in "model":
+        if hasattr(instance, attr):
+            return getattr(instance, attr)
+    return None
+
+
+def _format_api_key(api_key: Union[str, SecretStr]) -> str:
+    """Obfuscate a given LLM provider API key by returning the last four characters."""
+    if hasattr(api_key, "get_secret_value"):
+        api_key = api_key.get_secret_value()
+
+    if not api_key or len(api_key) < 4:
+        return ""
+    return "...%s" % api_key[-4:]
+
+
+def _extract_api_key(instance: Any) -> str:
+    """
+    Extract and format LLM-provider API key from instance.
+    Note that langchain's LLM/ChatModel/Embeddings interfaces do not have a
+    standard attribute name for storing the provider-specific API key, so make a
+    best effort here by checking for attributes that end with `api_key/api_token`.
+    """
+    api_key_attrs = [a for a in dir(instance) if a.endswith(("api_token", "api_key"))]
+    if api_key_attrs and hasattr(instance, str(api_key_attrs[0])):
+        api_key = getattr(instance, api_key_attrs[0], None)
+        if api_key:
+            return _format_api_key(api_key)
+    return ""
+
+
+# def _tag_anthropic_token_usage(
+#     span: Span, llm_output: Dict[str, Any], propagated_cost: int = 0, propagate: bool = False
+# ) -> None:
+#     """
+#     Extract token usage from llm_output, tag on span.
+#     Calculate the total cost for each LLM/chat_model, then propagate those values up the trace so that
+#     the root span will store the total token_usage/cost of all of its descendants.
+#     """
+#     for token_type in ("input", "output"):
+#         current_metric_value = span.get_metric("anthropic.tokens.%s_tokens" % token_type) or 0
+#         metric_value = llm_output["token_usage"].get("%s_tokens" % token_type, 0)
+#         span.set_metric("anthropic.tokens.%s_tokens" % token_type, current_metric_value + metric_value)
+#     total_cost = span.get_metric(TOTAL_COST) or 0
+#     if not propagate:
+#         try:
+#             completion_cost = get_anthropic_token_cost_for_model(
+#                 span.get_tag(MODEL),
+#                 span.get_metric(COMPLETION_TOKENS),
+#                 is_completion=True,
+#             )
+#             prompt_cost = get_anthropic_token_cost_for_model(span.get_tag(MODEL), span.get_metric(PROMPT_TOKENS))
+#             total_cost = completion_cost + prompt_cost
+#         except ValueError:
+#             # If not in Anthropic's model catalog, the above helpers will raise a ValueError.
+#             log.debug("Cannot calculate token/cost as the model is not in Anthropic's model catalog.")
+#     span.set_metric(TOTAL_COST, propagated_cost + total_cost)
+#     if span._parent is not None:
+#         _tag_anthropic_token_usage(span._parent, llm_output,
+# propagated_cost=propagated_cost + total_cost, propagate=True)
+
+
+@with_traced_module
+def traced_chat_model_generate(anthropic, pin, func, instance, args, kwargs):
+    chat_messages = get_argument_value(args, kwargs, 0, "messages")
+    integration = anthropic._datadog_integration
+
+    span = integration.trace(
+        pin,
+        "%s.%s" % (instance.__module__, instance.__class__.__name__),
+        submit_to_llmobs=True,
+        interface_type="chat_model",
+        provider="anthropic",
+        model=_extract_model_name(instance),
+        api_key=_extract_api_key(instance),
+    )
+    breakpoint()
+    chat_completions = None
+    try:
+        for message_set_idx, message_set in enumerate(chat_messages):
+            if isinstance(message_set.get("content", None), str):
+                span.set_tag_str(
+                    "anthropic.request.messages.%d.0.content" % (message_set_idx),
+                    integration.trunc(message_set.get("content", "")),
+                )
+                span.set_tag_str(
+                    "anthropic.request.messages.%d.0.message_type" % (message_set_idx),
+                    "text",
+                )
+            elif isinstance(message_set.get("content", None), Iterable):
+                for message_idx, message in enumerate(message_set.get("content", [])):
+                    if integration.is_pc_sampled_span(span):
+                        if message.get("type", None) == "text":
+                            span.set_tag_str(
+                                "anthropic.request.messages.%d.%d.content" % (message_set_idx, message_idx),
+                                integration.trunc(str(message.get("text", ""))),
+                            )
+                        elif message.get("type", None) == "image":
+                            span.set_tag_str(
+                                "anthropic.request.messages.%d.%d.content" % (message_set_idx, message_idx),
+                                integration.trunc(json.dumps(message.get("source", ""))),
+                            )
+
+                    span.set_tag_str(
+                        "anthropic.request.messages.%d.%d.message_type" % (message_set_idx, message_idx),
+                        message.get("type", "text"),
+                    )
+        for param, val in kwargs.items():
+            if param != "messages":
+                if isinstance(val, dict):
+                    for k, v in val.items():
+                        span.set_tag_str("anthropic.request.parameters.%s.%s" % (param, k), str(v))
+                else:
+                    span.set_tag_str("anthropic.request.parameters.%s" % (param), str(val))
+        breakpoint()
+        chat_completions = func(*args, **kwargs)
+
+        #     _tag_anthropic_token_usage(span, chat_completions.llm_output)
+        #     integration.record_usage(span, chat_completions.llm_output)
+
+        for idx, chat_completion in enumerate(chat_completions.content):
+            if integration.is_pc_sampled_span(span):
+                span.set_tag_str(
+                    "anthropic.response.completions.%d.content" % (idx),
+                    integration.trunc(chat_completion.text),
+                )
+            span.set_tag_str(
+                "anthropic.response.completions.%d.message_type" % (idx),  # TO-DO: change to .type?
+                chat_completion.type,
+            )
+    except Exception:
+        span.set_exc_info(*sys.exc_info())
+        raise
+    finally:
+        # if integration.is_pc_sampled_llmobs(span):
+        integration.llmobs_set_tags(span, chat_messages, chat_completions, err=bool(span.error))
+        span.finish()
+        # integration.metric(span, "dist", "request.duration", span.duration_ns)
+    return chat_completions
+
+
+def patch():
+    if getattr(anthropic, "_datadog_patch", False):
+        return
+
+    anthropic._datadog_patch = True
+    breakpoint()
+    Pin().onto(anthropic)
+    integration = AnthropicIntegration(integration_config=config.anthropic)
+    anthropic._datadog_integration = integration
+
+    wrap("anthropic", "resources.messages.Messages.create", traced_chat_model_generate(anthropic))
+    # wrap("langchain", "chat_models.base.BaseChatModel.agenerate", traced_chat_model_agenerate(langchain))
+
+    # if _is_iast_enabled():
+    #     from ddtrace.appsec._iast._metrics import _set_iast_error_metric
+
+    #     def wrap_output_parser(module, parser):
+    #         # Ensure not double patched
+    #         if not isinstance(deep_getattr(module, "%s.parse" % parser), wrapt.ObjectProxy):
+    #             wrap(module, "%s.parse" % parser, taint_parser_output)
+
+    #     try:
+    #         with_agent_output_parser(wrap_output_parser)
+    #     except Exception as e:
+    #         _set_iast_error_metric("IAST propagation error. langchain wrap_output_parser. {}".format(e))
+
+
+def unpatch():
+    if not getattr(anthropic, "_datadog_patch", False):
+        return
+
+    anthropic._datadog_patch = False
+
+    unwrap(anthropic.resources.messages.Messages, "create")
+    # unwrap(langchain.chat_models.base.BaseChatModel, "agenerate")
+
+    delattr(anthropic, "_datadog_integration")
+
+
+def taint_outputs(instance, inputs, outputs):
+    from ddtrace.appsec._iast._metrics import _set_iast_error_metric
+    from ddtrace.appsec._iast._taint_tracking import get_tainted_ranges
+    from ddtrace.appsec._iast._taint_tracking import taint_pyobject
+
+    try:
+        ranges = None
+        for key in filter(lambda x: x in inputs, instance.input_keys):
+            input_val = inputs.get(key)
+            if input_val:
+                ranges = get_tainted_ranges(input_val)
+                if ranges:
+                    break
+
+        if ranges:
+            source = ranges[0].source
+            for key in filter(lambda x: x in outputs, instance.output_keys):
+                output_value = outputs[key]
+                outputs[key] = taint_pyobject(output_value, source.name, source.value, source.origin)
+    except Exception as e:
+        _set_iast_error_metric("IAST propagation error. langchain taint_outputs. {}".format(e))
+
+
+# def taint_parser_output(func, instance, args, kwargs):
+#     from ddtrace.appsec._iast._metrics import _set_iast_error_metric
+#     from ddtrace.appsec._iast._taint_tracking import get_tainted_ranges
+#     from ddtrace.appsec._iast._taint_tracking import taint_pyobject
+
+#     result = func(*args, **kwargs)
+#     try:
+#         try:
+#             from langchain_core.agents import AgentAction
+#             from langchain_core.agents import AgentFinish
+#         except ImportError:
+#             from langchain.agents import AgentAction
+#             from langchain.agents import AgentFinish
+#         ranges = get_tainted_ranges(args[0])
+#         if ranges:
+#             source = ranges[0].source
+#             if isinstance(result, AgentAction):
+#                 result.tool_input = taint_pyobject(result.tool_input, source.name, source.value, source.origin)
+#             elif isinstance(result, AgentFinish) and "output" in result.return_values:
+#                 values = result.return_values
+#                 values["output"] = taint_pyobject(values["output"], source.name, source.value, source.origin)
+#     except Exception as e:
+#         _set_iast_error_metric("IAST propagation error. langchain taint_parser_output. {}".format(e))
+
+#     return result
+
+
+# def with_agent_output_parser(f):
+#     import langchain.agents
+
+#     queue = [(langchain.agents, agent_output_parser_classes)]
+
+#     while len(queue) > 0:
+#         module, current = queue.pop(0)
+#         if isinstance(current, str):
+#             if hasattr(module, current):
+#                 f(module, current)
+#         elif isinstance(current, dict):
+#             for name, value in current.items():
+#                 if hasattr(module, name):
+#                     queue.append((getattr(module, name), value))
---
+++ ddtrace/llmobs/_integrations/anthropic.py
@@ -0,0 +1,106 @@
+import json
+from typing import Any
+from typing import Dict
+from typing import Iterable
+from typing import Optional
+
+from ddtrace._trace.span import Span
+from ddtrace.internal.logger import get_logger
+from ddtrace.llmobs._constants import INPUT_MESSAGES
+from ddtrace.llmobs._constants import METADATA
+from ddtrace.llmobs._constants import MODEL_NAME
+from ddtrace.llmobs._constants import OUTPUT_MESSAGES
+from ddtrace.llmobs._constants import SPAN_KIND
+
+from .base import BaseLLMIntegration
+
+
+log = get_logger(__name__)
+
+
+class AnthropicIntegration(BaseLLMIntegration):
+    _integration_name = "anthropic"
+
+    def llmobs_set_tags(
+        self,
+        span: Span,
+        input_messages: Optional[Dict[str, Any]] = None,
+        formatted_response: Optional[Dict[str, Any]] = None,
+        err: bool = False,
+    ) -> None:
+        """Extract prompt/response tags from a completion and set them as temporary "_ml_obs.*" tags."""
+        # if not self.llmobs_enabled:
+        #     return
+        parameters = {"temperature": float(span.get_tag("anthropic.request.parameters.temperature") or 0.0)}
+        max_tokens = int(span.get_tag("anthropic.request.parameters.max_tokens") or 0)
+        if max_tokens:
+            parameters["max_tokens"] = max_tokens
+        input_messages = self._extract_input_message(input_messages)
+
+        span.set_tag_str(SPAN_KIND, "llm")
+        span.set_tag_str(MODEL_NAME, span.get_tag("anthropic.request.model") or "")
+        span.set_tag_str(INPUT_MESSAGES, json.dumps(input_messages))
+        span.set_tag_str(METADATA, json.dumps(parameters))
+        if err or formatted_response is None:
+            span.set_tag_str(OUTPUT_MESSAGES, json.dumps([{"content": ""}]))
+        else:
+            output_messages = self._extract_output_message(formatted_response)
+            span.set_tag_str(OUTPUT_MESSAGES, json.dumps(output_messages))
+
+    @staticmethod
+    def _extract_input_message(messages):
+        """Extract input messages from the stored prompt.
+        Anthropic allows for messages and multiple texts in a message, which requires some special casing.
+        """
+        if not isinstance(messages, Iterable):
+            log.warning("Anthropic input must be a list of messages.")
+
+        input_messages = []
+        for message in messages:
+            content = message.get("content", None)
+            role = message.get("role", None)
+
+            # TO-DO: do we want to truncate these inputs ?
+
+            if not role or not content:
+                log.warning("Anthropic input message must have content and role.")
+
+            if isinstance(content, str):
+                input_messages.append({"content": content, "role": role})
+
+            elif isinstance(content, list):
+                for entry in content:
+                    if entry.get("type") == "text":
+                        input_messages.append({"content": entry.get("text", ""), "role": role})
+                    elif entry.get("type") == "image":
+                        # Store a placeholder for potentially enormous binary image data.
+                        input_messages.append({"content": "([IMAGE DETECTED])", "role": role})
+                    else:
+                        input_messages.append({"content": entry, "role": role})
+
+        return input_messages
+
+    @staticmethod
+    def _extract_output_message(formatted_response):
+        """Extract output messages from the stored response."""
+        # TO-DO: do we want to truncate these outputs ?
+
+        output_messages = []
+        if isinstance(formatted_response.content, str):
+            return [{"content": formatted_response.content}]
+        if isinstance(formatted_response.content, list):
+            for response in formatted_response.content:
+                if isinstance(response.text, str):
+                    output_messages.append({"content": response.text})
+
+    # def record_usage(self, span: Span, usage: Dict[str, Any]) -> None:
+    #     if not usage or self.metrics_enabled is False:
+    #         return
+    #     for token_type in ("prompt", "completion", "total"):
+    #         num_tokens = usage.get("token_usage", {}).get(token_type + "_tokens")
+    #         if not num_tokens:
+    #             continue
+    #         self.metric(span, "dist", "tokens.%s" % token_type, num_tokens)
+    #     total_cost = span.get_metric(TOTAL_COST)
+    #     if total_cost:
+    #         self.metric(span, "incr", "tokens.total_cost", total_cost)
