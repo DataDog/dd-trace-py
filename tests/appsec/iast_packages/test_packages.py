7201794e11a5927608c7c5b9db50818fe741bce1
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-06-04 10:31:07 +0200

    Update the PACKAGES to the ones in the actual PR
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- tests/appsec/iast_packages/test_packages.py
+++ tests/appsec/iast_packages/test_packages.py
@@ -139,7 +139,38 @@ class PackageForTesting:
 # wheel, importlib-metadata and pip is discarded because they are package to build projects
 # colorama and awscli are terminal commands
 PACKAGES = [
+    PackageForTesting("asn1crypto", "1.5.1", "", "", "", test_e2e=False, import_module_to_validate="asn1crypto.core"),
+    PackageForTesting(
+        "attrs",
+        "23.2.0",
+        "Bruce Dickinson",
+        {"age": 65, "name": "Bruce Dickinson"},
+        "",
+        import_module_to_validate="attr.validators",
+    ),
+    PackageForTesting(
+        "azure-core",
+        "1.30.1",
+        "",
+        "",
+        "",
+        test_e2e=False,
+        import_name="azure",
+        import_module_to_validate="azure.core.settings",
+    ),
     PackageForTesting("beautifulsoup4", "4.12.3", "<html></html>", "", "", import_name="bs4"),
+    PackageForTesting(
+        "boto3",
+        "1.34.110",
+        "",
+        "",
+        "",
+        test_e2e=False,
+        extras=[("pyopenssl", "24.1.0")],
+        import_module_to_validate="boto3.session",
+    ),
+    PackageForTesting("botocore", "1.34.110", "", "", "", test_e2e=False),
+    PackageForTesting("cffi", "1.16.0", "", "", "", import_module_to_validate="cffi.model"),
     PackageForTesting(
         "certifi", "2024.2.2", "", "The path to the CA bundle is", "", import_module_to_validate="certifi.core"
     ),
@@ -151,6 +182,31 @@ PACKAGES = [
         "",
         import_name="charset_normalizer",
         import_module_to_validate="charset_normalizer.api",
+    ),
+    PackageForTesting("click", "8.1.7", "", "", "", test_e2e=False, import_module_to_validate="click.core"),
+    PackageForTesting(
+        "cryptography",
+        "42.0.7",
+        "This is a secret message.",
+        "This is a secret message.",
+        "",
+        import_module_to_validate="cryptography.fernet",
+    ),
+    PackageForTesting("distlib", "0.3.8", "", "", "", test_e2e=False, import_module_to_validate="distlib.util"),
+    PackageForTesting(
+        "exceptiongroup", "1.2.1", "", "", "", test_e2e=False, import_module_to_validate="exceptiongroup._formatting"
+    ),
+    PackageForTesting("filelock", "3.14.0", "", "", "", test_e2e=False, import_module_to_validate="filelock._api"),
+    PackageForTesting("flask", "2.3.3", "", "", "", test_e2e=False, import_module_to_validate="flask.app"),
+    PackageForTesting("fsspec", "2024.5.0", "", "/", ""),
+    PackageForTesting(
+        "google-api-core",
+        "2.19.0",
+        "",
+        "",
+        "",
+        import_name="google",
+        import_module_to_validate="google.auth.transport.grpc",
     ),
     PackageForTesting(
         "google-api-python-client",
@@ -170,6 +226,67 @@ PACKAGES = [
         "xn--eckwd4c7c.xn--zckzah",
         import_module_to_validate="idna.codec",
     ),
+    PackageForTesting(
+        "importlib-resources",
+        "6.4.0",
+        "",
+        "",
+        "",
+        test_e2e=False,
+        import_name="importlib_resources",
+        skip_python_version=[(3, 8)],
+        import_module_to_validate="importlib_resources.readers",
+    ),
+    PackageForTesting("isodate", "0.6.1", "", "", "", test_e2e=False, import_module_to_validate="isodate.duration"),
+    PackageForTesting(
+        "itsdangerous", "2.2.0", "", "", "", test_e2e=False, import_module_to_validate="itsdangerous.serializer"
+    ),
+    PackageForTesting("jinja2", "3.1.4", "", "", "", test_e2e=False, import_module_to_validate="jinja2.compiler"),
+    PackageForTesting("jmespath", "1.0.1", "", "Seattle", "", import_module_to_validate="jmespath.functions"),
+    # jsonschema fails for Python 3.8
+    #        except KeyError:
+    # >           raise exceptions.NoSuchResource(ref=uri) from None
+    # E           referencing.exceptions.NoSuchResource: 'http://json-schema.org/draft-03/schema#'
+    PackageForTesting(
+        "jsonschema",
+        "4.22.0",
+        "Bruce Dickinson",
+        {
+            "data": {"age": 65, "name": "Bruce Dickinson"},
+            "schema": {
+                "properties": {"age": {"type": "number"}, "name": {"type": "string"}},
+                "required": ["name", "age"],
+                "type": "object",
+            },
+            "validation": "successful",
+        },
+        "",
+        skip_python_version=[(3, 8)],
+    ),
+    PackageForTesting("markupsafe", "2.1.5", "", "", "", test_e2e=False),
+    PackageForTesting(
+        "lxml",
+        "5.2.2",
+        "",
+        "",
+        "",
+        test_e2e=False,
+        import_name="lxml.etree",
+        import_module_to_validate="lxml.doctestcompare",
+    ),
+    PackageForTesting(
+        "more-itertools",
+        "10.2.0",
+        "",
+        "",
+        "",
+        test_e2e=False,
+        import_name="more_itertools",
+        import_module_to_validate="more_itertools.more",
+    ),
+    PackageForTesting(
+        "multidict", "6.0.5", "", "", "", test_e2e=False, import_module_to_validate="multidict._multidict_py"
+    ),
     # Python 3.12 fails in all steps with "import error" when import numpy
     PackageForTesting(
         "numpy",
@@ -180,6 +297,45 @@ PACKAGES = [
         skip_python_version=[(3, 12)],
         import_module_to_validate="numpy.core._internal",
     ),
+    PackageForTesting("oauthlib", "3.2.2", "", "", "", test_e2e=False, import_module_to_validate="oauthlib.common"),
+    PackageForTesting("openpyxl", "3.1.2", "", "", "", test_e2e=False, import_module_to_validate="openpyxl.chart.axis"),
+    PackageForTesting(
+        "packaging",
+        "24.0",
+        "",
+        {"is_version_valid": True, "requirement": "example-package>=1.0.0", "specifier": ">=1.0.0", "version": "1.2.3"},
+        "",
+    ),
+    # Pandas dropped Python 3.8 support in pandas>2.0.3
+    PackageForTesting("pandas", "2.2.2", "", "", "", test_e2e=False, skip_python_version=[(3, 8)]),
+    # TODO: Test import fails with AttributeError: 'FormattedValue' object has no attribute 'values'
+    # PackageForTesting("pillow", "10.3.0", "", "", "", test_e2e=False, import_name="PIL.Image"),
+    PackageForTesting(
+        "platformdirs", "4.2.2", "", "", "", test_e2e=False, import_module_to_validate="platformdirs.unix"
+    ),
+    PackageForTesting("pluggy", "1.5.0", "", "", "", test_e2e=False, import_module_to_validate="pluggy._hooks"),
+    PackageForTesting(
+        "pyasn1",
+        "0.6.0",
+        "Bruce Dickinson",
+        {"decoded_age": 65, "decoded_name": "Bruce Dickinson"},
+        "",
+        import_module_to_validate="pyasn1.codec.native.decoder",
+    ),
+    PackageForTesting("pycparser", "2.22", "", "", ""),
+    PackageForTesting("pydantic", "2.7.1", "", "", "", test_e2e=False),
+    PackageForTesting(
+        "pydantic-core",
+        "2.18.2",
+        "",
+        "",
+        "",
+        test_e2e=False,
+        import_name="pydantic_core",
+        import_module_to_validate="pydantic_core.core_schema",
+    ),
+    # TODO: patching Pytest fails: ImportError: cannot import name 'Dir' from '_pytest.main'
+    # PackageForTesting("pytest", "8.2.1", "", "", "", test_e2e=False),
     PackageForTesting(
         "python-dateutil",
         "2.8.2",
@@ -189,6 +345,7 @@ PACKAGES = [
         import_name="dateutil",
         import_module_to_validate="dateutil.relativedelta",
     ),
+    PackageForTesting("pytz", "2024.1", "", "", "", test_e2e=False),
     PackageForTesting(
         "PyYAML",
         "6.0.1",
@@ -206,6 +363,25 @@ PACKAGES = [
         "",
     ),
     PackageForTesting(
+        "rsa",
+        "4.9",
+        "Bruce Dickinson",
+        {"decrypted_message": "Bruce Dickinson", "message": "Bruce Dickinson"},
+        "",
+        import_module_to_validate="rsa.pkcs1",
+    ),
+    PackageForTesting(
+        "sqlalchemy",
+        "2.0.30",
+        "Bruce Dickinson",
+        {"age": 65, "id": 1, "name": "Bruce Dickinson"},
+        "",
+        import_module_to_validate="sqlalchemy.orm.session",
+    ),
+    PackageForTesting(
+        "s3fs", "2024.5.0", "", "", "", extras=[("pyopenssl", "24.1.0")], import_module_to_validate="s3fs.core"
+    ),
+    PackageForTesting(
         "s3transfer",
         "0.10.1",
         "",
@@ -213,21 +389,43 @@ PACKAGES = [
         "",
         extras=[("boto3", "1.34.110")],
     ),
+    # TODO: Test import fails with
+    #   AttributeError: partially initialized module 'setuptools' has no
+    #   attribute 'dist' (most likely due to a circular import)
+    PackageForTesting(
+        "setuptools",
+        "70.0.0",
+        "",
+        {"description": "An example package", "name": "example_package"},
+        "",
+        test_import=False,
+    ),
     PackageForTesting("six", "1.16.0", "", "We're in Python 3", ""),
+    # TODO: Test import fails with AttributeError: 'FormattedValue' object has no attribute 'values'
+    # PackageForTesting("soupsieve", "2.5", "", "", "", test_e2e=False,
+    # import_module_to_validate="soupsieve.css_match"),
+    PackageForTesting("tomli", "2.0.1", "", "", "", test_e2e=False, import_module_to_validate="tomli._parser"),
+    PackageForTesting("tomlkit", "0.12.5", "", "", "", test_e2e=False, import_module_to_validate="tomlkit.items"),
+    PackageForTesting("tqdm", "4.66.4", "", "", "", test_e2e=False, import_module_to_validate="tqdm.std"),
+    # Python 3.8 and 3.9 fial with ImportError: cannot import name 'get_host' from 'urllib3.util.url'
     PackageForTesting(
         "urllib3",
         "2.1.0",
         "https://www.datadoghq.com/",
         ["https", None, "www.datadoghq.com", None, "/", None, None],
         "www.datadoghq.com",
+        skip_python_version=[(3, 8), (3, 9)],
     ),
-    PackageForTesting("setuptools", "70.0.0", "", {"description": "An example package", "name": "example_package"}, ""),
-    PackageForTesting("cryptography", "42.0.7", "", "", "", test_e2e=False),
-    PackageForTesting("fsspec", "2024.5.0", "", "", "", test_e2e=False, test_import=False),
-    PackageForTesting("boto3", "1.34.110", "", "", "", test_e2e=False, test_import=False),
-    # Python 3.8 fails in test_packages_patched_import with
-    # TypeError: '>' not supported between instances of 'int' and 'object'
-    # TODO: try to fix it
+    PackageForTesting(
+        "virtualenv", "20.26.2", "", "", "", test_e2e=False, import_module_to_validate="virtualenv.activation.activator"
+    ),
+    # TODO: Test import fails with AttributeError: 'FormattedValue' object has no attribute 'values'
+    # PackageForTesting("werkzeug", "3.0.3", "", "", "", test_e2e=False, import_module_to_validate="werkzeug.http"),
+    PackageForTesting("yarl", "1.9.4", "", "", "", test_e2e=False, import_module_to_validate="yarl._url"),
+    PackageForTesting("zipp", "3.18.2", "", "", "", test_e2e=False),
+    # PENDING TO TEST
+    # TODO: Python 3.8 fails in test_packages_patched_import with
+    #   TypeError: '>' not supported between instances of 'int' and 'object'
     PackageForTesting(
         "typing-extensions",
         "4.11.0",
@@ -238,99 +436,35 @@ PACKAGES = [
         test_e2e=False,
         skip_python_version=[(3, 8)],
     ),
-    PackageForTesting("botocore", "1.34.110", "", "", "", test_e2e=False),
-    PackageForTesting("packaging", "24.0", "", "", "", test_e2e=False),
-    # PackageForTesting("cffi", "1.16.0", "", "", "", test_e2e=False),
     PackageForTesting(
         "aiobotocore", "2.13.0", "", "", "", test_e2e=False, test_import=False, import_name="aiobotocore.session"
     ),
-    PackageForTesting("s3fs", "2024.5.0", "", "", "", test_e2e=False, test_import=False),
-    PackageForTesting("google-api-core", "2.19.0", "", "", "", test_e2e=False, import_name="google"),
-    PackageForTesting("pycparser", "2.22", "", "", "", test_e2e=False),
-    # Pandas dropped Python 3.8 support in pandas>2.0.3
-    PackageForTesting("pandas", "2.2.2", "", "", "", test_e2e=False, skip_python_version=[(3, 8)]),
-    PackageForTesting("zipp", "3.18.2", "", "", "", test_e2e=False),
-    PackageForTesting("attrs", "23.2.0", "", "", "", test_e2e=False),
-    PackageForTesting("pyasn1", "0.6.0", "", "", "", test_e2e=False),
-    PackageForTesting("rsa", "4.9", "", "", "", test_e2e=False),
-    # protobuf fails for all python versions with No module named 'protobuf
-    # PackageForTesting("protobuf", "5.26.1", "", "", "", test_e2e=False),
-    PackageForTesting("jmespath", "1.0.1", "", "", "", test_e2e=False),
-    PackageForTesting("click", "8.1.7", "", "", "", test_e2e=False),
-    PackageForTesting("pydantic", "2.7.1", "", "", "", test_e2e=False),
-    PackageForTesting("pytz", "2024.1", "", "", "", test_e2e=False),
-    PackageForTesting("markupsafe", "2.1.5", "", "", "", test_e2e=False),
-    PackageForTesting("jinja2", "3.1.4", "", "", "", test_e2e=False),
-    PackageForTesting("platformdirs", "4.2.2", "", "", "", test_e2e=False),
     PackageForTesting("pyjwt", "2.8.0", "", "", "", test_e2e=False, import_name="jwt"),
-    PackageForTesting("tomli", "2.0.1", "", "", "", test_e2e=False),
-    PackageForTesting("filelock", "3.14.0", "", "", "", test_e2e=False),
     PackageForTesting("wrapt", "1.16.0", "", "", "", test_e2e=False),
     PackageForTesting("cachetools", "5.3.3", "", "", "", test_e2e=False),
-    PackageForTesting("pluggy", "1.5.0", "", "", "", test_e2e=False),
-    PackageForTesting("virtualenv", "20.26.2", "", "", "", test_e2e=False),
-    # docutils dropped Python 3.8 support in pandas> 1.10.10.21.2
+    # docutils dropped Python 3.8 support in docutils > 1.10.10.21.2
     PackageForTesting("docutils", "0.21.2", "", "", "", test_e2e=False, skip_python_version=[(3, 8)]),
     PackageForTesting("pyarrow", "16.1.0", "", "", "", test_e2e=False),
-    PackageForTesting("exceptiongroup", "1.2.1", "", "", "", test_e2e=False),
-    # jsonschema fails for Python 3.8
-    #        except KeyError:
-    # >           raise exceptions.NoSuchResource(ref=uri) from None
-    # E           referencing.exceptions.NoSuchResource: 'http://json-schema.org/draft-03/schema#'
-    PackageForTesting("jsonschema", "4.22.0", "", "", "", test_e2e=False, skip_python_version=[(3, 8)]),
     PackageForTesting("requests-oauthlib", "2.0.0", "", "", "", test_e2e=False, import_name="requests_oauthlib"),
     PackageForTesting("pyparsing", "3.1.2", "", "", "", test_e2e=False),
-    PackageForTesting("pytest", "8.2.1", "", "", "", test_e2e=False),
-    PackageForTesting("oauthlib", "3.2.2", "", "", "", test_e2e=False),
-    PackageForTesting("sqlalchemy", "2.0.30", "", "", "", test_e2e=False),
     PackageForTesting("aiohttp", "3.9.5", "", "", "", test_e2e=False),
-    # scipy dropped Python 3.8 support in pandas> 1.10.1
+    # scipy dropped Python 3.8 support in scipy > 1.10.1
     PackageForTesting(
         "scipy", "1.13.0", "", "", "", test_e2e=False, import_name="scipy.special", skip_python_version=[(3, 8)]
     ),
-    PackageForTesting("isodate", "0.6.1", "", "", "", test_e2e=False),
-    PackageForTesting("multidict", "6.0.5", "", "", "", test_e2e=False),
     PackageForTesting("iniconfig", "2.0.0", "", "", "", test_e2e=False),
     PackageForTesting("psutil", "5.9.8", "", "", "", test_e2e=False),
-    PackageForTesting("soupsieve", "2.5", "", "", "", test_e2e=False),
-    PackageForTesting("yarl", "1.9.4", "", "", "", test_e2e=False),
     PackageForTesting("frozenlist", "1.4.1", "", "", "", test_e2e=False),
     PackageForTesting("aiosignal", "1.3.1", "", "", "", test_e2e=False),
-    PackageForTesting("werkzeug", "3.0.3", "", "", "", test_e2e=False),
-    PackageForTesting("pillow", "10.3.0", "", "", "", test_e2e=False, import_name="PIL.Image"),
-    PackageForTesting("tqdm", "4.66.4", "", "", "", test_e2e=False),
     PackageForTesting("pygments", "2.18.0", "", "", "", test_e2e=False),
     PackageForTesting("grpcio", "1.64.0", "", "", "", test_e2e=False, import_name="grpc"),
-    PackageForTesting("greenlet", "3.0.3", "", "", "", test_e2e=False),
     PackageForTesting("pyopenssl", "24.1.0", "", "", "", test_e2e=False, import_name="OpenSSL.SSL"),
-    PackageForTesting("flask", "3.0.3", "", "", "", test_e2e=False),
     PackageForTesting("decorator", "5.1.1", "", "", "", test_e2e=False),
-    PackageForTesting("pydantic-core", "2.18.2", "", "", "", test_e2e=False, import_name="pydantic_core"),
-    PackageForTesting("lxml", "5.2.2", "", "", "", test_e2e=False, import_name="lxml.etree"),
     PackageForTesting("requests-toolbelt", "1.0.0", "", "", "", test_e2e=False, import_name="requests_toolbelt"),
-    PackageForTesting("openpyxl", "3.1.2", "", "", "", test_e2e=False),
-    PackageForTesting("tzdata", "2024.1", "", "", "", test_e2e=False),
-    PackageForTesting(
-        "importlib-resources",
-        "6.4.0",
-        "",
-        "",
-        "",
-        test_e2e=False,
-        import_name="importlib_resources",
-        skip_python_version=[(3, 8)],
-    ),
-    PackageForTesting("asn1crypto", "1.5.1", "", "", "", test_e2e=False),
-    PackageForTesting("coverage", "7.5.1", "", "", "", test_e2e=False),
-    PackageForTesting("azure-core", "1.30.1", "", "", "", test_e2e=False, import_name="azure"),
-    PackageForTesting("distlib", "0.3.8", "", "", "", test_e2e=False),
-    PackageForTesting("tomlkit", "0.12.5", "", "", "", test_e2e=False),
     PackageForTesting("pynacl", "1.5.0", "", "", "", test_e2e=False, import_name="nacl.utils"),
-    PackageForTesting("itsdangerous", "2.2.0", "", "", "", test_e2e=False),
     PackageForTesting("annotated-types", "0.7.0", "", "", "", test_e2e=False, import_name="annotated_types"),
-    PackageForTesting("sniffio", "1.3.1", "", "", "", test_e2e=False),
-    PackageForTesting("more-itertools", "10.2.0", "", "", "", test_e2e=False, import_name="more_itertools"),
 ]
+
 
 # Use this function if you want to test one or a filter number of package for debug proposes
 # SKIP_FUNCTION = lambda package: package.name == "pynacl"  # noqa: E731
