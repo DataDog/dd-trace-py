bf137c680b3d176b33bb936f15e527ff004091e1
Emmett Butler <emmett.butler321@gmail.com>
2024-06-05 06:57:52 -0700

    proper expected tags list

--- tests/lib-injection/dd-lib-python-init-test-django-unsupported-python/validate_telemetry.py
+++ tests/lib-injection/dd-lib-python-init-test-django-unsupported-python/validate_telemetry.py
@@ -7,4 +7,4 @@ parsed = json.loads(sys.stdin.read())
 print(parsed)
 assert len(parsed["points"]) == 2
 assert {"name": "library_entrypoint.abort", "tags": ["reason:incompatible_runtime"]} in parsed["points"]
-assert {"name": "library_entrypoint.abort.runtime"} in parsed["points"]
+assert {"name": "library_entrypoint.abort.runtime", "tags": []} in parsed["points"]
