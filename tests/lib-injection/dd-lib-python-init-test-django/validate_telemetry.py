2cb4cceed228cd664faebc75e0a6a24efd90701b
Emmett Butler <emmett.butler321@gmail.com>
2024-06-05 10:57:03 -0700

    pls

--- tests/lib-injection/dd-lib-python-init-test-django/validate_telemetry.py
+++ tests/lib-injection/dd-lib-python-init-test-django/validate_telemetry.py
@@ -2,9 +2,9 @@ import json
 import sys
 
 
-in = sys.stdin.read()
-print(in)
-parsed = json.loads(in)
+in_text = sys.stdin.read()
+print(in_text)
+parsed = json.loads(in_text)
 print(parsed)
 assert len(parsed["points"]) == 1
 assert parsed["points"][0] == {"name": "library_entrypoint.complete", "tags": ["injection_forced:false"]}
