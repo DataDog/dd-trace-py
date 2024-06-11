40e0e1484f9aec3c0ad8e94d5b47dbdc1ec76628
Zachary Groves <32471391+ZStriker19@users.noreply.github.com>
2024-06-11 10:08:52 -0400

    feat(onboarding): early exit conditions in lib-injection (#9323)
    This pull request adds "guardrails" to the "library injection" process.
    These are early exit conditions from the instrumentation process
    intended to avoid sending any traces when undefined behavior is likely.
    The code makes this determination on the basis of software versions
    present in the application environment, both of Python packages and the
    Python runtime itself.
    
    The biggest risk here is that instrumentation is disabled when it's not
    intended to be. I think existing tests in `tests/lib-injection` cover
    this pretty well. There's a new test added that verifies instrumentation
    was cancelled when an unsupported package version is present.
    
    Contains changes from https://github.com/DataDog/dd-trace-py/pull/9418
    Related RFC: "[RFC] One Step Guardrails"
    
    - [x] minimum package version checks
    - [x] Testing
    - [x] replace envvars with inject_force
    - [x] figure out what to use instead of pkg_resources
    - [x] replace local file path with `DD_TELEMETRY_FORWARDER_PATH`
    - [x] Change(s) are motivated and described in the PR description
    - [x] Testing strategy is described if automated tests are not included
    in the PR
    - [x] Risks are described (performance impact, potential for breakage,
    maintainability)
    - [x] Change is maintainable (easy to change, telemetry, documentation)
    - [x] [Library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    are followed or label `changelog/no-changelog` is set
    - [x] Documentation is included (in-code, generated user docs, [public
    corp docs](https://github.com/DataDog/documentation/))
    - [x] Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    - [x] If this PR changes the public interface, I've notified
    `@DataDog/apm-tees`.
    
    - [x] Title is accurate
    - [x] All changes are related to the pull request's stated goal
    - [x] Description motivates each change
    - [x] Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - [x] Testing strategy adequately addresses listed risks
    - [x] Change is maintainable (easy to change, telemetry, documentation)
    - [x] Release note makes sense to a user of the library
    - [x] Author has acknowledged and discussed the performance implications
    of this PR as reported in the benchmarks PR comment
    - [x] Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    
    ---------
    
    Co-authored-by: Emmett Butler <723615+emmettbutler@users.noreply.github.com>
    Co-authored-by: Emmett Butler <emmett.butler321@gmail.com>
    (cherry picked from commit 0c38e09db92cceedc8affff4a217357fc2a1bcac)

--- lib-injection/copy-lib.sh
+++ lib-injection/copy-lib.sh
@@ -3,4 +3,5 @@
 # This script is used by the admission controller to install the library from the
 # init container into the application container.
 cp sitecustomize.py "$1/sitecustomize.py"
+cp min_compatible_versions.csv "$1/min_compatible_versions.csv"
 cp -r ddtrace_pkgs "$1/ddtrace_pkgs"
