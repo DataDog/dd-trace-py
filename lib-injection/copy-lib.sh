ea13466504338b54550827371068a54fd5a7721a
Emmett Butler <723615+emmettbutler@users.noreply.github.com>
2024-06-11 08:04:32 -0700

    test(lib-injection): test telemetry (#9477)
    This change adds automated testing of the instrumentation telemetry
    emitted by the library injection workflow. It takes the approach of
    setting `DD_TELEMETRY_FORWARDER_PATH` to point to a simple bash script
    that writes to a file, then checking the contents of that file against
    per-test expectations about specific telemetry events.
    
    I am planning on adding the following data to telemetry events emitted
    by ddtrace itself in a separate pull request:
    - whether autoinjection was used
    - whether the force envvar was set
    - whether autoinjection actually installed anything
    
    ## Checklist
    
    - [x] Change(s) are motivated and described in the PR description
    - [x] Testing strategy is described if automated tests are not included
    in the PR
    - [x] Risks are described (performance impact, potential for breakage,
    maintainability)
    - [x] Change is maintainable (easy to change, telemetry, documentation)
    - [x] [Library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    are followed or label `changelog/no-changelog` is set
    - [x] Documentation is included (in-code, generated user docs, [public
    corp docs](https://github.com/DataDog/documentation/))
    - [x] Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    - [x] If this PR changes the public interface, I've notified
    `@DataDog/apm-tees`.
    
    ---------
    
    Co-authored-by: ZStriker19 <zach.groves@datadoghq.com>
    Co-authored-by: Zachary Groves <32471391+ZStriker19@users.noreply.github.com>

--- lib-injection/copy-lib.sh
+++ lib-injection/copy-lib.sh
@@ -4,4 +4,5 @@
 # init container into the application container.
 cp sitecustomize.py "$1/sitecustomize.py"
 cp min_compatible_versions.csv "$1/min_compatible_versions.csv"
+cp telemetry-forwarder.sh "$1/telemetry-forwarder.sh"
 cp -r ddtrace_pkgs "$1/ddtrace_pkgs"
