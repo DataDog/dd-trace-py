67dc3fccd457bead20b8b89ecc926969ca38ad34
Yun Kim <35776586+Yun-Kim@users.noreply.github.com>
2024-06-12 11:58:27 -0400

    fix(langchain): support langchain v0.2 (#9495)
    Fixes #9368.
    
    Langchain has released v0.2, which now lists `langchain_community` as an
    optional dependency. However in langchain v0.1, `langchain_community`
    was a required dependency, and used by our langchain integration to
    patch embeddings and vectorstores and calculate token information. This
    means that for langchain v2, users that do not have
    `langchain_community` installed will run into patch failures/errors as
    we try to patch something that does not exist.
    
    This PR fixes the issue by now only patching langchain community if it
    is available on the user's environment.
    
    This means, moving forward, if a user does not have
    `langchain_community` installed and runs langchain versions >= 1.0, the
    langchain integration no longer:
    - Calculates and tags cost metrics `langchain.tokens.total_cost` or
    dogstatsd metrics for openai-related traces (as it depends on the
    `langchain_community.get_openai_token_cost_for_model()` helper)
    - Patches embeddings calls (as embeddings are dependent on
    `langchain_community` modules) with the exception of OpenAI embeddings
    calls (which we patch only if `langchain_openai` is available)
    - Patches vectorstores similarity search calls (as vectorstores are
    dependent on `langchain_community` modules) with the exception of
    pinecone vectorstore similarity search calls (which we patch only if
    `langchain_pinecone` is available)
    
    Existing tests dependent on `langchain_community` code have been
    modified to be skipped if `langchain_community` does not exist. We've
    also added a new riot venv that does not include `langchain_community`
    to ensure all other functionality remains the same.
    
    - [x] Testing strategy is described if automated tests are not included
    in the PR
    - [x] Risks are described (performance impact, potential for breakage,
    maintainability)
    - [x] Change is maintainable (easy to change, telemetry, documentation)
    - [x] [Library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    are followed or label `changelog/no-changelog` is set
    - [x] Documentation is included (in-code, generated user docs, [public
    corp docs](https://github.com/DataDog/documentation/))
    - [x] Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    - [x] If this PR changes the public interface, I've notified
    `@DataDog/apm-tees`.
    
    - [x] Title is accurate
    - [x] All changes are related to the pull request's stated goal
    - [x] Description motivates each change
    - [x] Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - [x] Testing strategy adequately addresses listed risks
    - [x] Change is maintainable (easy to change, telemetry, documentation)
    - [x] Release note makes sense to a user of the library
    - [x] Author has acknowledged and discussed the performance implications
    of this PR as reported in the benchmarks PR comment
    - [x] Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

--- tests/contrib/langchain/test_langchain_patch.py
+++ tests/contrib/langchain/test_langchain_patch.py
@@ -3,7 +3,7 @@ from ddtrace.contrib.langchain import patch
 from ddtrace.contrib.langchain import unpatch
 from ddtrace.contrib.langchain.constants import text_embedding_models
 from ddtrace.contrib.langchain.constants import vectorstore_classes
-from ddtrace.contrib.langchain.patch import SHOULD_PATCH_LANGCHAIN_COMMUNITY
+from ddtrace.contrib.langchain.patch import PATCH_LANGCHAIN_V0
 from tests.contrib.patch import PatchTestCase
 
 
@@ -15,8 +15,20 @@ class TestLangchainPatch(PatchTestCase.Base):
     __get_version__ = get_version
 
     def assert_module_patched(self, langchain):
-        if SHOULD_PATCH_LANGCHAIN_COMMUNITY:
-            import langchain_community as gated_langchain
+        if PATCH_LANGCHAIN_V0:
+            gated_langchain = langchain
+            self.assert_wrapped(langchain.llms.base.BaseLLM.generate)
+            self.assert_wrapped(langchain.llms.base.BaseLLM.agenerate)
+            self.assert_wrapped(langchain.chat_models.base.BaseChatModel.generate)
+            self.assert_wrapped(langchain.chat_models.base.BaseChatModel.agenerate)
+            self.assert_wrapped(langchain.chains.base.Chain.__call__)
+            self.assert_wrapped(langchain.chains.base.Chain.acall)
+        else:
+            try:
+                import langchain_community as gated_langchain
+            except ImportError:
+                gated_langchain = None
+
             import langchain_core
             import langchain_openai
             import langchain_pinecone
@@ -33,15 +45,9 @@ class TestLangchainPatch(PatchTestCase.Base):
             self.assert_wrapped(langchain_core.runnables.base.RunnableSequence.abatch)
             self.assert_wrapped(langchain_openai.OpenAIEmbeddings.embed_documents)
             self.assert_wrapped(langchain_pinecone.PineconeVectorStore.similarity_search)
-        else:
-            gated_langchain = langchain
-            self.assert_wrapped(langchain.llms.base.BaseLLM.generate)
-            self.assert_wrapped(langchain.llms.base.BaseLLM.agenerate)
-            self.assert_wrapped(langchain.chat_models.base.BaseChatModel.generate)
-            self.assert_wrapped(langchain.chat_models.base.BaseChatModel.agenerate)
-            self.assert_wrapped(langchain.chains.base.Chain.__call__)
-            self.assert_wrapped(langchain.chains.base.Chain.acall)
 
+        if not gated_langchain:
+            return
         for text_embedding_model in text_embedding_models:
             embedding_model = getattr(gated_langchain.embeddings, text_embedding_model, None)
             if embedding_model:
@@ -53,12 +59,27 @@ class TestLangchainPatch(PatchTestCase.Base):
                 self.assert_wrapped(vectorstore_interface.similarity_search)
 
     def assert_not_module_patched(self, langchain):
-        if SHOULD_PATCH_LANGCHAIN_COMMUNITY:
+        if PATCH_LANGCHAIN_V0:
+            from langchain import embeddings  # noqa: F401
+            from langchain import vectorstores  # noqa: F401
+
+            gated_langchain = langchain
+            self.assert_not_wrapped(langchain.llms.base.BaseLLM.generate)
+            self.assert_not_wrapped(langchain.llms.base.BaseLLM.agenerate)
+            self.assert_not_wrapped(langchain.chat_models.base.BaseChatModel.generate)
+            self.assert_not_wrapped(langchain.chat_models.base.BaseChatModel.agenerate)
+            self.assert_not_wrapped(langchain.chains.base.Chain.__call__)
+            self.assert_not_wrapped(langchain.chains.base.Chain.acall)
+        else:
             from langchain import chains  # noqa: F401
             from langchain.chains import base  # noqa: F401
-            import langchain_community as gated_langchain
-            from langchain_community import embeddings  # noqa: F401
-            from langchain_community import vectorstores  # noqa: F401
+
+            try:
+                import langchain_community as gated_langchain
+                from langchain_community import embeddings  # noqa: F401
+                from langchain_community import vectorstores  # noqa: F401
+            except ImportError:
+                gated_langchain = None
             import langchain_core
             import langchain_openai
             import langchain_pinecone
@@ -75,18 +96,9 @@ class TestLangchainPatch(PatchTestCase.Base):
             self.assert_not_wrapped(langchain_core.runnables.base.RunnableSequence.abatch)
             self.assert_not_wrapped(langchain_openai.OpenAIEmbeddings.embed_documents)
             self.assert_not_wrapped(langchain_pinecone.PineconeVectorStore.similarity_search)
-        else:
-            from langchain import embeddings  # noqa: F401
-            from langchain import vectorstores  # noqa: F401
 
-            gated_langchain = langchain
-            self.assert_not_wrapped(langchain.llms.base.BaseLLM.generate)
-            self.assert_not_wrapped(langchain.llms.base.BaseLLM.agenerate)
-            self.assert_not_wrapped(langchain.chat_models.base.BaseChatModel.generate)
-            self.assert_not_wrapped(langchain.chat_models.base.BaseChatModel.agenerate)
-            self.assert_not_wrapped(langchain.chains.base.Chain.__call__)
-            self.assert_not_wrapped(langchain.chains.base.Chain.acall)
-
+        if not gated_langchain:
+            return
         for text_embedding_model in text_embedding_models:
             embedding_model = getattr(gated_langchain.embeddings, text_embedding_model, None)
             if embedding_model:
@@ -98,9 +110,21 @@ class TestLangchainPatch(PatchTestCase.Base):
                 self.assert_not_wrapped(vectorstore_interface.similarity_search)
 
     def assert_not_module_double_patched(self, langchain):
-        if SHOULD_PATCH_LANGCHAIN_COMMUNITY:
+        if PATCH_LANGCHAIN_V0:
+            gated_langchain = langchain
+            self.assert_not_double_wrapped(langchain.llms.base.BaseLLM.generate)
+            self.assert_not_double_wrapped(langchain.llms.base.BaseLLM.agenerate)
+            self.assert_not_double_wrapped(langchain.chat_models.base.BaseChatModel.generate)
+            self.assert_not_double_wrapped(langchain.chat_models.base.BaseChatModel.agenerate)
+            self.assert_not_double_wrapped(langchain.chains.base.Chain.__call__)
+            self.assert_not_double_wrapped(langchain.chains.base.Chain.acall)
+        else:
             from langchain.chains import base  # noqa: F401
-            import langchain_community as gated_langchain
+
+            try:
+                import langchain_community as gated_langchain
+            except ImportError:
+                gated_langchain = None
             import langchain_core
             import langchain_openai
             import langchain_pinecone
@@ -117,15 +141,9 @@ class TestLangchainPatch(PatchTestCase.Base):
             self.assert_not_double_wrapped(langchain_core.runnables.base.RunnableSequence.abatch)
             self.assert_not_double_wrapped(langchain_openai.OpenAIEmbeddings.embed_documents)
             self.assert_not_double_wrapped(langchain_pinecone.PineconeVectorStore.similarity_search)
-        else:
-            gated_langchain = langchain
-            self.assert_not_double_wrapped(langchain.llms.base.BaseLLM.generate)
-            self.assert_not_double_wrapped(langchain.llms.base.BaseLLM.agenerate)
-            self.assert_not_double_wrapped(langchain.chat_models.base.BaseChatModel.generate)
-            self.assert_not_double_wrapped(langchain.chat_models.base.BaseChatModel.agenerate)
-            self.assert_not_double_wrapped(langchain.chains.base.Chain.__call__)
-            self.assert_not_double_wrapped(langchain.chains.base.Chain.acall)
 
+        if not gated_langchain:
+            return
         for text_embedding_model in text_embedding_models:
             embedding_model = getattr(gated_langchain.embeddings, text_embedding_model, None)
             if embedding_model:
