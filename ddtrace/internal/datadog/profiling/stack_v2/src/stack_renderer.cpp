406cfd4183c666a164cb65aee779d702535bd799
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-06-12 18:12:52 +0000

    fix typos

--- ddtrace/internal/datadog/profiling/stack_v2/src/stack_renderer.cpp
+++ ddtrace/internal/datadog/profiling/stack_v2/src/stack_renderer.cpp
@@ -38,6 +38,7 @@ StackRenderer::render_thread_begin(PyThreadState* tstate,
     stashed_native_id = native_id;
     stashed_thread_name = std::string(name);
     stashed_wall_time_us = wall_time_us;
+    stashed_now_time_ns = now_ns;
 }
 
 void
@@ -63,6 +64,11 @@ StackRenderer::render_task_begin(std::string_view name)
                              stashed_thread_name);
         ddup_push_walltime(sample, 1000 * stashed_wall_time_us, 1);
         ddup_push_cputime(sample, 1000 * stashed_cpu_time_us, 1); // initialized to 0, so possibly a no-op
+
+        // If we have a nonzero timestamp, push that too
+        if (stashed_now_time_ns != 0) {
+            ddup_push_monotonic_ns(sample, stashed_now_time_ns);
+        }
     }
 
     ddup_push_task_name(sample, name);
