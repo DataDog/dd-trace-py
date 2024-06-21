9a0ac8580cc484448c27fedce6eba2ce38566ac4
sanchda <sanchda@users.noreply.github.com>
2024-06-21 12:48:47 +0000

    Shuffle build assets around

--- ddtrace/internal/datadog/profiling/dd_wrapper/include/receiver_interface.h
+++ ddtrace/internal/datadog/profiling/dd_wrapper/include/receiver_interface.h
@@ -1,26 +1,11 @@
 #pragma once
 
 #include <stdbool.h>
-#include <unistd.h>
 
-#include "datadog/profiling.h"
-
-bool
-crashtracker_receiver_entry()
-{
-    // Assumes that this will be called only in the receiver binary, which is a
-    // fresh process
-    ddog_prof_CrashtrackerResult new_result = ddog_prof_Crashtracker_receiver_entry_point();
-    if (new_result.tag != DDOG_PROF_CRASHTRACKER_RESULT_OK) {
-        ddog_CharSlice message = ddog_Error_message(&new_result.err);
-
-        //`write` may not write what we want it to write, but there's nothing we can do about it,
-        // so ignore the return
-        int n = write(STDERR_FILENO, message.ptr, message.len);
-        (void)n;
-
-        ddog_Error_drop(&new_result.err);
-        return false;
-    }
-    return true;
-}
+// clang-format off
+// (why does clang-format try to split the decl into a multiline?)
+#ifdef __cplusplus
+extern "C"
+#endif
+bool crashtracker_receiver_entry();
+// clang-format on
