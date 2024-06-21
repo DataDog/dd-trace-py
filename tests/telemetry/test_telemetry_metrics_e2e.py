629c5819057d87b7186a2909d7c37cd70a0d7852
Munir Abdinur <munir_abdinur@hotmail.com>
2024-06-21 00:47:34 -0400

    get_events metrics

--- tests/telemetry/test_telemetry_metrics_e2e.py
+++ tests/telemetry/test_telemetry_metrics_e2e.py
@@ -74,7 +74,7 @@ def parse_payload(data):
 
 def test_telemetry_metrics_enabled_on_gunicorn_child_process(test_agent_session):
     token = "tests.telemetry.test_telemetry_metrics_e2e.test_telemetry_metrics_enabled_on_gunicorn_child_process"
-    initial_event_count = len(test_agent_session.get_events())
+    initial_event_count = len(test_agent_session.get_events("generate-metrics"))
     with gunicorn_server(telemetry_metrics_enabled="true", token=token) as context:
         _, gunicorn_client = context
 
@@ -86,9 +86,8 @@ def test_telemetry_metrics_enabled_on_gunicorn_child_process(test_agent_session)
         response = gunicorn_client.get("/count_metric")
         assert response.status_code == 200
 
-    events = test_agent_session.get_events()
-    assert len(events) > initial_event_count
-    metrics = list(filter(lambda event: event["request_type"] == "generate-metrics", events))
+    metrics = test_agent_session.get_events("generate-metrics")
+    assert len(metrics) > initial_event_count
     assert len(metrics) == 1
     assert metrics[0]["payload"]["series"][0]["metric"] == "test_metric"
     assert metrics[0]["payload"]["series"][0]["points"][0][1] == 5
@@ -103,9 +102,8 @@ for _ in range(10):
 """
     _, stderr, status, _ = ddtrace_run_python_code_in_subprocess(code, env=get_default_telemetry_env())
     assert status == 0, stderr
-    events = test_agent_session.get_events()
-
-    metrics = get_metrics_from_events(events)
+    metrics_events = test_agent_session.get_events("generate-metrics")
+    metrics = get_metrics_from_events(metrics_events)
     assert len(metrics) == 2
     assert metrics[0]["metric"] == "spans_created"
     assert metrics[0]["tags"] == ["integration_name:datadog"]
@@ -128,9 +126,9 @@ for _ in range(9):
     env["DD_TRACE_OTEL_ENABLED"] = "true"
     _, stderr, status, _ = ddtrace_run_python_code_in_subprocess(code, env=env)
     assert status == 0, stderr
-    events = test_agent_session.get_events()
 
-    metrics = get_metrics_from_events(events)
+    metrics_events = test_agent_session.get_events("generate-metrics")
+    metrics = get_metrics_from_events(metrics_events)
     assert len(metrics) == 2
     assert metrics[0]["metric"] == "spans_created"
     assert metrics[0]["tags"] == ["integration_name:otel"]
@@ -151,9 +149,9 @@ for _ in range(9):
 """
     _, stderr, status, _ = ddtrace_run_python_code_in_subprocess(code, env=get_default_telemetry_env())
     assert status == 0, stderr
-    events = test_agent_session.get_events()
 
-    metrics = get_metrics_from_events(events)
+    metrics_events = test_agent_session.get_events("generate-metrics")
+    metrics = get_metrics_from_events(metrics_events)
     assert len(metrics) == 2
     assert metrics[0]["metric"] == "spans_created"
     assert metrics[0]["tags"] == ["integration_name:opentracing"]
@@ -183,8 +181,8 @@ for _ in range(4):
     _, stderr, status, _ = ddtrace_run_python_code_in_subprocess(code, env=env)
     assert status == 0, stderr
 
-    events = test_agent_session.get_events()
-    metrics = get_metrics_from_events(events)
+    metrics_events = test_agent_session.get_events("generate-metrics")
+    metrics = get_metrics_from_events(metrics_events)
     assert len(metrics) == 3
 
     assert metrics[0]["metric"] == "spans_created"
@@ -201,9 +199,7 @@ for _ in range(4):
 def get_metrics_from_events(events):
     metrics = []
     for event in events:
-        if event["request_type"] == "generate-metrics":
-            # Note - this helper function does not aggregate metrics across payloads
-            for series in event["payload"]["series"]:
-                metrics.append(series)
+        for series in event["payload"]["series"]:
+            metrics.append(series)
     metrics.sort(key=lambda x: (x["metric"], x["tags"]), reverse=False)
     return metrics
