c3ab549700bd0f2501b3cf0727a06015934c3a15
sanchda <838104+sanchda@users.noreply.github.com>
2024-06-22 01:00:38 +0000

    More crashtracker tests

--- tests/internal/test_crashtracker.py
+++ tests/internal/test_crashtracker.py
@@ -6,5 +6,17 @@ import pytest
 
 
 @pytest.mark.skipif(not sys.platform.startswith("linux"), reason="Linux only")
-def test_crashtracker():
-    assert False
+@pytest.mark.subprocess()
+def test_crashtracker_loading():
+    try:
+        pass
+    except Exception:
+        pytest.fail("Crashtracker failed to load")
+
+
+@pytest.mark.skipif(not sys.platform.startswith("linux"), reason="Linux only")
+@pytest.mark.subprocess()
+def test_crashtracker_available():
+    import ddtrace.internal.core.crashtracker as crashtracker
+
+    assert crashtracker.is_available
