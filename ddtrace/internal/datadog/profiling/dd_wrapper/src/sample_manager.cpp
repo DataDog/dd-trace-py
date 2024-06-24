7b635aeb8093bfcb80ae542c0afbae0e6b42b8b2
Tae Gyun Kim <taegyun.kim@datadoghq.com>
2024-06-23 22:59:43 -0400

    sample pool

--- ddtrace/internal/datadog/profiling/dd_wrapper/src/sample_manager.cpp
+++ ddtrace/internal/datadog/profiling/dd_wrapper/src/sample_manager.cpp
@@ -27,21 +27,39 @@ Datadog::SampleManager::set_timeline(bool enable)
     Datadog::Sample::set_timeline(enable);
 }
 
-Datadog::Sample*
-Datadog::SampleManager::start_sample()
+Datadog::Sample* Datadog::SampleManager::start_sample()
 {
+    {
+        const std::lock_guard<std::mutex> lock(pool_mutex);
+        if (!sample_pool.empty()) {
+            // Reuse a sample from the pool
+            auto sample = sample_pool.back().release();
+            sample_pool.pop_back();
+            return sample;
+        }
+    }
+    // Create a new sample if the pool is empty
     return new Datadog::Sample(type_mask, max_nframes); // NOLINT(cppcoreguidelines-owning-memory)
 }
 
 void
 Datadog::SampleManager::drop_sample(Datadog::Sample* sample)
 {
-    delete sample; // NOLINT(cppcoreguidelines-owning-memory)
+    // Reset the sample's state
+    sample->clear_buffers();
+
+    {
+        const std::lock_guard<std::mutex> lock(pool_mutex);
+        // Return the sample to the pool
+        sample_pool.emplace_back(sample); // NOLINT(cppcoreguidelines-owning-memory)
+    }
 }
+
 
 void
 Datadog::SampleManager::postfork_child()
 {
+    pool_mutex.unlock();
     Datadog::Sample::postfork_child();
 }
 
