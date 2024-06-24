886e67c4a60dad9f470cc6775eb38951e89878c6
Tae Gyun Kim <taegyun.kim@datadoghq.com>
2024-06-23 22:59:43 -0400

    sample pool

--- ddtrace/internal/datadog/profiling/dd_wrapper/include/sample_manager.hpp
+++ ddtrace/internal/datadog/profiling/dd_wrapper/include/sample_manager.hpp
@@ -18,7 +18,9 @@ class SampleManager
   private:
     static inline unsigned int max_nframes{ g_default_max_nframes };
     static inline SampleType type_mask{ SampleType::All };
-    static inline std::mutex init_mutex{};
+
+    static inline std::mutex pool_mutex{};
+    static inline std::vector<std::unique_ptr<Sample>> sample_pool{};
 
   public:
     // Configuration
