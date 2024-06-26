7cd82baa4e679293cf143cddb216bae1e28ee5bd
Romain Komorn <136473744+romainkomorndatadog@users.noreply.github.com>
2024-06-26 16:50:47 +0100

    Update test_pytest.py
    Fix test formatting.

--- tests/contrib/pytest/test_pytest.py
+++ tests/contrib/pytest/test_pytest.py
@@ -3671,6 +3671,7 @@ class PytestTestCase(TracerTestCase):
                          def wrapper(*args, **kwargs):
                             return func(*args, **kwargs)
                          return wrapper
+
                     @outer_decorator
                     def inner_decorator(func):
                          def wrapper(*args, **kwargs):
@@ -3689,36 +3690,43 @@ class PytestTestCase(TracerTestCase):
                     # this comment is line 2 and if you didn't know that it'd be easy to miscount below
                     from my_decorators import outer_decorator, inner_decorator
                     from unittest.mock import patch
+
                     def local_decorator(func):
                         def wrapper(*args, **kwargs):
                             return func(*args, **kwargs)
                         return wrapper
+
                     def test_one_decorator():  # line 11
                         str1 = "string 1"
                         str2 = "string 2"
                         assert str1 != str2
+
                     @local_decorator  # line 16
                     def test_local_decorated():
                         str1 = "string 1"
                         str2 = "string 2"
                         assert str1 == str2
+
                     @patch("ddtrace.config._potato", "potato")  # line 22
                     def test_patched_undecorated():
                         str1 = "string 1"
                         str2 = "string 2"
                         assert str1 != str2
+
                     @patch("ddtrace.config._potato", "potato")  # line 28
                     @inner_decorator
                     def test_patched_single_decorated():
                         str1 = "string 1"
                         str2 = "string 2"
                         assert str1 == str2
+
                     @patch("ddtrace.config._potato", "potato")  # line 35
                     @outer_decorator
                     def test_patched_double_decorated():
                         str1 = "string 1"
                         str2 = "string 2"
                         assert str1 != str2
+
                     @outer_decorator  # line 42
                     @patch("ddtrace.config._potato", "potato")
                     @local_decorator
