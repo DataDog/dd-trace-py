df7b3e0a69e73f195e132363051e82972501b185
Federico Mon <federico.mon@datadoghq.com>
2024-06-26 09:29:07 +0200

    test: import setuptools first

--- ddtrace/sourcecode/setuptools_auto.py
+++ ddtrace/sourcecode/setuptools_auto.py
@@ -2,9 +2,9 @@ from ddtrace.vendor.wrapt import wrap_function_wrapper as _w
 
 
 try:
-    import distutils.core as distutils_core
+    import setuptools  # noqa: I001
 
-    import setuptools
+    import distutils.core as distutils_core
 except ImportError:
     distutils_core = None  # type: ignore[assignment]
     setuptools = None  # type: ignore[assignment]
