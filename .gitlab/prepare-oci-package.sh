24927f8ce11868f132beb9bd855128ceee2c371d
Laplie Anderson <laplie.anderson@datadoghq.com>
2024-06-28 16:56:20 -0400

    assign python version from wheel

--- .gitlab/prepare-oci-package.sh
+++ .gitlab/prepare-oci-package.sh
@@ -4,6 +4,21 @@ if [ -n "$CI_COMMIT_TAG" ] && [ -z "$PYTHON_PACKAGE_VERSION" ]; then
   PYTHON_PACKAGE_VERSION=${CI_COMMIT_TAG##v}
 fi
 
+if [ -z "$PYTHON_PACKAGE_VERSION" ]; then
+  # Get the version from the filename of the first wheel
+  # wheels have the form:
+  # ddtrace-2.11.0.dev41+g50bf57680-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.wh
+  # the version is everything between the first and second "-"
+  WHEELS_LIST=(../pywheels/*.whl)
+  FIRST_WHEEL=${WHEELS_LIST[1]}
+
+  #everything before -
+  WITHOUT_BEGINNING=${FIRST_WHEEL#*-}
+
+  #strip after -
+  PYTHON_PACKAGE_VERSION=${WITHOUT_BEGINNING%%-*}
+fi
+
 mkdir -p sources
 
 BUILD_DIR=sources
