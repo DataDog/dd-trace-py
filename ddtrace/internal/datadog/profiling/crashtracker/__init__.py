448cb4da397114e2472878f329e4cc253008d36b
sanchda <838104+sanchda@users.noreply.github.com>
2024-07-03 00:24:11 +0000

    Vanity: factor out unnecessary guards

--- ddtrace/internal/datadog/profiling/crashtracker/__init__.py
+++ ddtrace/internal/datadog/profiling/crashtracker/__init__.py
@@ -1,17 +1,13 @@
-# See _crashtracker.pyx or .pyi for a summary of the available interfaces
-import platform
-
-
 is_available = False
 
-if platform.system() == "Linux":
-    try:
-        from ._crashtracker import *  # noqa: F403, F401
 
-        is_available = True
+try:
+    from ._crashtracker import *  # noqa: F403, F401
 
-    except Exception as e:
-        from ddtrace.internal.logger import get_logger
+    is_available = True
 
-        LOG = get_logger(__name__)
-        LOG.warning("Failed to import _crashtracker: %s", e)
+except Exception as e:
+    from ddtrace.internal.logger import get_logger
+
+    LOG = get_logger(__name__)
+    LOG.warning("Failed to import _crashtracker: %s", e)
--- ddtrace/internal/datadog/profiling/ddup/__init__.py
+++ ddtrace/internal/datadog/profiling/ddup/__init__.py
@@ -9,7 +9,5 @@ try:
 except Exception as e:
     from ddtrace.internal.logger import get_logger
 
-    from ._stubs import *  # noqa: F403, F401
-
     LOG = get_logger(__name__)
     LOG.warning("Failed to import _ddup: %s", e)
