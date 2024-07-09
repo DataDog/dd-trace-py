d164f67b0eec302ba2c2d2b05b6da8df5cd3d336
Yun Kim <yun.kim@datadoghq.com>
2024-07-09 13:49:09 -0400

    Add default repr

--- ddtrace/llmobs/_integrations/openai.py
+++ ddtrace/llmobs/_integrations/openai.py
@@ -22,6 +22,7 @@ from ddtrace.llmobs._constants import OUTPUT_VALUE
 from ddtrace.llmobs._constants import SPAN_KIND
 from ddtrace.llmobs._constants import TOTAL_TOKENS_METRIC_KEY
 from ddtrace.llmobs._integrations.base import BaseLLMIntegration
+from ddtrace.llmobs._utils import _unserializable_default_repr
 from ddtrace.llmobs.utils import Document
 from ddtrace.pin import Pin
 
@@ -163,7 +164,7 @@ class OpenAIIntegration(BaseLLMIntegration):
         span.set_tag_str(INPUT_MESSAGES, json.dumps([{"content": str(p)} for p in prompt]))
 
         parameters = {k: v for k, v in kwargs.items() if k not in ("model", "prompt")}
-        span.set_tag_str(METADATA, json.dumps(parameters))
+        span.set_tag_str(METADATA, json.dumps(parameters, default=_unserializable_default_repr))
 
         if err is not None:
             span.set_tag_str(OUTPUT_MESSAGES, json.dumps([{"content": ""}]))
@@ -189,7 +190,7 @@ class OpenAIIntegration(BaseLLMIntegration):
         span.set_tag_str(INPUT_MESSAGES, json.dumps(input_messages))
 
         parameters = {k: v for k, v in kwargs.items() if k not in ("model", "messages", "tools", "functions")}
-        span.set_tag_str(METADATA, json.dumps(parameters))
+        span.set_tag_str(METADATA, json.dumps(parameters, default=_unserializable_default_repr))
 
         if err is not None:
             span.set_tag_str(OUTPUT_MESSAGES, json.dumps([{"content": ""}]))
