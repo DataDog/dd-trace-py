7bac834618dddccd887a61fa86e69ebd35c8f6df
Gordon Hamilton <gordon.hamilton@datadoghq.com>
2024-07-10 11:05:32 -0400

    updating patch file for azure functions integration

--- ddtrace/contrib/azure_functions/patch.py
+++ ddtrace/contrib/azure_functions/patch.py
@@ -1,7 +1,9 @@
 import azure.functions
 from azure.functions import __version__
+from azure.functions.decorators.function_app import Function
 
 from ddtrace import config
+from ddtrace import tracer
 from ddtrace.internal.schema import schematize_service_name
 from ddtrace.internal.wrapping import unwrap
 from ddtrace.internal.wrapping import wrap
@@ -27,16 +29,27 @@ def patch():
     azure.functions._datadog_patch = True
 
     # find function and wrap it
-    wrap(azure.functions.decorators.function_app.Function.__init__, _wrapped_init)
-    pass
+    wrap(Function.get_user_function, _traced_user_function)
 
 
 def unpatch():
     # Undo the monkey patching that patch() did here
     if getattr(azure.functions, "_datadog_patch", False):
-        unwrap(azure.functions.decorators.function_app.Function.__init__, _wrapped_init)
+        unwrap(Function.get_user_function, _traced_user_function)
         azure.functions._datadog_patch = False
 
 
-def _wrapped_init(func, args, kwargs):
-    return func(*args, **kwargs)
+def _traced_user_function(get_user_function, args, kwargs):
+    # create spec for span name and tag values that follows aws_lambda ex: resource name, service, name tags, etc...
+    # find the convention
+    # identify semantics for span name, resources name, operation name and service name
+
+    # resrouce name = function name
+    # service name = should be azure.functions as default
+    # span name =
+    
+    def wrapped_user_func(*wrapped_args, **wrapped_kwargs):
+        with tracer.trace("top-level-span"):
+            return get_user_function(*wrapped_args, **wrapped_kwargs)
+
+    return wrapped_user_func
