8b0da44237dc7d3e1be8ef16956f4d8b065b44f9
Tae Gyun Kim <taegyun.kim@datadoghq.com>
2024-07-10 10:11:01 -0400

    Revert "use boost"
    This reverts commit 3985217a1b11ba4747891059061fc47d8e287229.

--- ddtrace/internal/datadog/profiling/dd_wrapper/src/synchronized_sample_pool.cpp
+++ ddtrace/internal/datadog/profiling/dd_wrapper/src/synchronized_sample_pool.cpp
@@ -1,23 +1,26 @@
-#include "sample.hpp"
 #include "synchronized_sample_pool.hpp"
 
-#include "boost/lockfree/queue.hpp"
+#include "sample.hpp"
 
 namespace Datadog {
 std::optional<Sample*>
 SynchronizedSamplePool::get_sample()
 {
-    Sample* sample = nullptr;
-    if (pool.pop(sample)) {
+    const std::lock_guard<std::mutex> lock(mutex);
+    if (!pool.empty()) {
+        // Reuse a sample from the pool
+        auto sample = pool.back().release();
+        pool.pop_back();
         return sample;
     }
-
     return std::nullopt;
 }
 
 void
 SynchronizedSamplePool::return_sample(Sample* sample)
 {
-    pool.push(sample);
+    const std::lock_guard<std::mutex> lock(mutex);
+    // Return the sample to the pool
+    pool.emplace_back(sample); // NOLINT(cppcoreguidelines-owning-memory)
 }
 } // namespace Datadog
