ace857ff0e9f5694f153764a46290c7b29c20dc2
Yun Kim <yun.kim@datadoghq.com>
2024-07-11 17:09:23 -0400

    Fix test

--- tests/contrib/langchain/test_langchain_community.py
+++ tests/contrib/langchain/test_langchain_community.py
@@ -1265,10 +1265,12 @@ def test_lcel_chain_non_dict_input(langchain_core):
 def test_faiss_vectorstore_retrieval(langchain_community, langchain_openai, request_vcr):
     if langchain_community is None:
         pytest.skip("langchain-community not installed which is required for this test.")
-    with request_vcr.use_cassette("openai_retrieval_embedding.yaml"):
-        faiss = langchain_community.vectorstores.faiss.FAISS.from_texts(
-            ["Quiet Platforms has 7 facilities across the USA"],
-            embedding=langchain_openai.OpenAIEmbeddings(),
-        )
-        retriever = faiss.as_retriever()
-        retriever.invoke("How many platforms does Quiet Platforms have in the US?")
+    with mock.patch("langchain_openai.OpenAIEmbeddings._get_len_safe_embeddings", return_value=[[0.0] * 1536]):
+        with request_vcr.use_cassette("openai_embedding_query.yaml"):
+            faiss = langchain_community.vectorstores.faiss.FAISS.from_texts(
+                ["this is a test query."],
+                embedding=langchain_openai.OpenAIEmbeddings(),
+            )
+            retriever = faiss.as_retriever()
+        with request_vcr.use_cassette("openai_retrieval_embedding.yaml"):
+            retriever.invoke("What was the message of the last test query?")
