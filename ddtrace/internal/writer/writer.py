e4b4a1c62ec2713e029dbbd4150753be829bdd97
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-07-12 12:26:52 -0400

    fix error in writer.py

--- ddtrace/internal/writer/writer.py
+++ ddtrace/internal/writer/writer.py
@@ -161,7 +161,7 @@ class HTTPWriter(periodic.PeriodicService, TraceWriter):
         report_metrics=True,  # type: bool
     ):
         # type: (...) -> None
-        super.__init__()  # type: ignore
+        super().__init__()
 
         if processing_interval is None:
             processing_interval = config._trace_writer_interval_seconds
