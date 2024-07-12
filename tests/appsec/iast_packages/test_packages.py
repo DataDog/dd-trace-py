30bed050e8014ec3a45828b61dcd78ddfa3071df
Federico Mon <federico.mon@datadoghq.com>
2024-07-12 09:27:27 +0200

    add test for google-auth

--- tests/appsec/iast_packages/test_packages.py
+++ tests/appsec/iast_packages/test_packages.py
@@ -240,6 +240,15 @@ PACKAGES = [
     PackageForTesting("flask", "2.3.3", "", "", "", test_e2e=False, import_module_to_validate="flask.app"),
     PackageForTesting("fsspec", "2024.5.0", "", "/", ""),
     PackageForTesting(
+        "google-auth",
+        "2.32.0",
+        "",
+        "",
+        "",
+        import_name="",
+        import_module_to_validate="google.auth.crypt",
+    ),
+    PackageForTesting(
         "google-api-core",
         "2.19.0",
         "",
