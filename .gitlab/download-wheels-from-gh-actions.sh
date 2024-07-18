39db33477cf89e2e8f1060543511d57af9dc376a
Laplie Anderson <laplie.anderson@datadoghq.com>
2024-07-18 18:57:23 -0400

    wait for artifacts to appear

--- .gitlab/download-wheels-from-gh-actions.sh
+++ .gitlab/download-wheels-from-gh-actions.sh
@@ -6,7 +6,26 @@ if [ -z "$CI_COMMIT_SHA" ]; then
   exit 1
 fi
 
-RUN_ID=$(gh run ls --repo DataDog/dd-trace-py --commit=$CI_COMMIT_SHA --workflow=build_deploy.yml --json databaseId --jq "first (.[]) | .databaseId")
+
+timeout=900 # 15 minutes
+start_time=$(date +%s)
+end_time=$((start_time + timeout))
+# Loop for 15 minutes waiting for artifacts to appear in github
+while [ $(date +%s) -lt $end_time ]; do
+  RUN_ID=$(gh run ls --repo DataDog/dd-trace-py --commit=$CI_COMMIT_SHA --workflow=build_deploy.yml --json databaseId --jq "first (.[]) | .databaseId")
+  if [ -n "$RUN_ID" ]; then
+    break;
+  fi
+
+  sleep 20
+done
+
+if [ -z "$RUN_ID" ]; then
+  echo "RUN_ID not found"
+  exit 1
+fi
+
+echo "Found RUN_ID: $RUN_ID"
 
 # wait for run to finish
 gh run watch $RUN_ID --interval 15 --exit-status 1 --repo DataDog/dd-trace-py
