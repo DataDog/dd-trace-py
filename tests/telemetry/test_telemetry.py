DataDog/dd-trace-py

commit 0cdc0831a62ee2575833298c49ff468cb14d51fb

chore(telemetry): improve the maintainability of the telemetry writer (#9680)
Bundled 10 telemetry improvements and refactors into this PR. Each
change is capture by a standalone commit. Merging 10 different PRs to
make telemetry collection slightly better seems cumbersome and not the
best use of CI and everyone's time.

This PR does not introduce any new functionality.

- dc5f75fdd5dd8d0307acf52a71e2871882fa17b5 Refactor global functions and
constants in `../telemetry/writer.py`
- Moves `AGENT_ENDPOINT`, `AGENTLESS_ENDPOINT_V2`, `_get_endpoint_v2`,
and `_agentless_telemetry_url` from a global scope to fields/methods in
the `_TelemetryClient` class. Also updates tests to align with this
change. This allows us to better encapsulate telemetry logic.
- 2490e1d9438ce270248f7b813bfac139deafd501,
baeebfe7a03281593ba29cab7f9886d86362117e Ensures the telemetry writer is
started before all products.
- Also ensures telemetry atexit hooks are executed last (ex: before
tracer, agent writer, profiling). This will allow us to capture more
start up errors and ensure app-started event is sent first and
app-closed event is sent last.
- baeebfe7a03281593ba29cab7f9886d86362117e Ensures telemetry collections
results in little to no performance impact when
`DD_INSTRUMENTATION_TELEMETRY_ENABLED` is `False`.
- With this change telemetry worker thread, atexit hooks and forksafe
hooks will not be enabled/started if telemetry is disabled via envar. If
telemetry is not manually disabled these components will be configured
when `ddtrace` is imported (and not just on ddtrace-run or when the
first telemetry event/log/metric is queued).
- This change also moves all exception hook logic from
`ddtrace.internal.telemetry.__init__` to
`ddtrace.internal.telemrtry.writer.py`. This provides better
encapsulation and resolves some circular import errors.
- df5235087a812abec05a4cccb80b6ddf290ebadd Remove
``ddtrace.internal.telemetry.disable_and_flush(...)`.

ddtrace/internal/telemetry/writer.py
@@ -109,21 +109,6 @@ from .metrics_namespaces import NamespaceMetricType  # noqa:F401
 
 log = get_logger(__name__)
 
-AGENT_ENDPOINT = "telemetry/proxy/api/v2/apmtelemetry"
-AGENTLESS_ENDPOINT_V2 = "api/v2/apmtelemetry"
-
-
-def _get_endpoint_v2(agentless):
-    return AGENTLESS_ENDPOINT_V2 if agentless else AGENT_ENDPOINT
-
-
-def _get_agentless_telemetry_url(site: str):
-    if site == "datad0g.com":
-        return "https://all-http-intake.logs.datad0g.com"
-    if site == "datadoghq.eu":
-        return "https://instrumentation-telemetry-intake.eu1.datadoghq.com"
-    return f"https://instrumentation-telemetry-intake.{site}/"
-
 
 class LogData(dict):
     def __hash__(self):
@@ -139,13 +124,14 @@ class LogData(dict):
 
 
 class _TelemetryClient:
-    def __init__(self, endpoint, agentless=False):
-        # type: (str, bool) -> None
-        self._telemetry_url = _get_agentless_telemetry_url(config._dd_site) if agentless else get_trace_url()
-        self._endpoint = endpoint
+    AGENT_ENDPOINT = "telemetry/proxy/api/v2/apmtelemetry"
+    AGENTLESS_ENDPOINT_V2 = "api/v2/apmtelemetry"
+
+    def __init__(self, agentless):
+        # type: (bool) -> None
+        self._telemetry_url = self.get_host(config._dd_site, agentless)
+        self._endpoint = self.get_endpoint(agentless)
         self._encoder = JSONEncoderV2()
-        self._is_agentless = agentless
-        self._is_disabled = False
 
         self._headers = {
             "Content-Type": "application/json",
@@ -153,11 +139,7 @@ class _TelemetryClient:
             "DD-Client-Library-Version": _pep440_to_semver(),
         }
 
-        if self._is_agentless:
-            if not config._dd_api_key:
-                log.debug("Disabling telemetry: no Datadog API key found in agentless mode")
-                self._is_disabled = True
-                return
+        if agentless and config._dd_api_key:
             self._headers["dd-api-key"] = config._dd_api_key
 
     @property
@@ -166,12 +148,8 @@ class _TelemetryClient:
 
     def send_event(self, request: Dict) -> Optional[httplib.HTTPResponse]:
         """Sends a telemetry request to the trace agent"""
-        if self._is_disabled:
-            return None
-
         resp = None
         conn = None
-        endpoint_str = "Datadog endpoint" if self._is_agentless else "Datadog Agent"
         try:
             rb_json = self._encoder.encode(request)
             headers = self.get_headers(request)
@@ -182,9 +160,9 @@ class _TelemetryClient:
             if resp.status < 300:
                 log.debug("sent %d in %.5fs to %s. response: %s", len(rb_json), sw.elapsed(), self.url, resp.status)
             else:
-                log.debug("failed to send telemetry to the %s at %s. response: %s", endpoint_str, self.url, resp.status)
+                log.debug("failed to send telemetry to %s. response: %s", self.url, resp.status)
         except Exception:
-            log.debug("failed to send telemetry to the %s at %s.", endpoint_str, self.url, exc_info=True)
+            log.debug("failed to send telemetry to %s.", self.url, exc_info=True)
         finally:
             if conn is not None:
                 conn.close()
@@ -199,6 +177,18 @@ class _TelemetryClient:
         headers["DD-Telemetry-API-Version"] = request["api_version"]
         container.update_headers_with_container_info(headers, container.get_container_info())
         return headers
+
+    def get_endpoint(self, agentless: bool) -> str:
+        return self.AGENTLESS_ENDPOINT_V2 if agentless else self.AGENT_ENDPOINT
+
+    def get_host(self, site: str, agentless: bool) -> str:
+        if not agentless:
+            return get_trace_url()
+        elif site == "datad0g.com":
+            return "https://all-http-intake.logs.datad0g.com"
+        elif site == "datadoghq.eu":
+            return "https://instrumentation-telemetry-intake.eu1.datadoghq.com"
+        return f"https://instrumentation-telemetry-intake.{site}/"
 
 
 class TelemetryWriterModuleWatchdog(BaseModuleWatchdog):
@@ -240,6 +230,7 @@ class TelemetryWriter(PeriodicService):
     # of `itertools.count()` which is a CPython implementation detail. The sequence field in telemetry
     # payloads is only used in tests and is not required to process Telemetry events.
     _sequence = itertools.count(1)
+    _ORIGINAL_EXCEPTHOOK = sys.excepthook
 
     def __init__(self, is_periodic=True, agentless=None):
         # type: (bool, Optional[bool]) -> None
@@ -257,24 +248,36 @@ class TelemetryWriter(PeriodicService):
         self._error = (0, "")  # type: Tuple[int, str]
         self._namespace = MetricNamespace()
         self._logs = set()  # type: Set[Dict[str, Any]]
-        self._enabled = config._telemetry_enabled
         self._forked = False  # type: bool
         self._events_queue = []  # type: List[Dict]
         self._configuration_queue = {}  # type: Dict[str, Dict]
         self._lock = forksafe.Lock()  # type: forksafe.ResetObject
         self._imported_dependencies: Dict[str, Distribution] = dict()
 
-        self._is_agentless = config._ci_visibility_agentless_enabled if agentless is None else agentless
-
         self.started = False
-        forksafe.register(self._fork_writer)
 
         # Debug flag that enables payload debug mode.
         self._debug = asbool(os.environ.get("DD_TELEMETRY_DEBUG", "false"))
 
-        self._endpoint = _get_endpoint_v2(self._is_agentless)
+        self._enabled = config._telemetry_enabled
+        agentless = config._ci_visibility_agentless_enabled if agentless is None else agentless
+        if agentless and not config._dd_api_key:
+            log.debug("Disabling telemetry: no Datadog API key found in agentless mode")
+            self._enabled = False
+        self._client = _TelemetryClient(agentless)
 
-        self._client = _TelemetryClient(self._endpoint, self._is_agentless)
+        if self._enabled:
+            # Avoids sending app-started and app-closed events in forked processes
+            forksafe.register(self._fork_writer)
+            # shutdown the telemetry writer when the application exits
+            atexit.register(self.app_shutdown)
+            # Captures unhandled exceptions during application start up
+            self.install_excepthook()
+            # In order to support 3.12, we start the writer upon initialization.
+            # See https://github.com/python/cpython/pull/104826.
+            # Telemetry events will only be sent after the `app-started` is queued.
+            # This will occur when the agent writer starts.
+            self.enable()
 
     def enable(self):
         # type: () -> bool
@@ -308,10 +311,15 @@ class TelemetryWriter(PeriodicService):
         if TelemetryWriterModuleWatchdog.is_installed():
             TelemetryWriterModuleWatchdog.uninstall()
         self.reset_queues()
-        if self._is_periodic and self.status is ServiceStatus.RUNNING:
+        if self._is_running():
             self.stop()
         else:
             self.status = ServiceStatus.STOPPED
+
+    def _is_running(self):
+        # type: () -> bool
+        """Returns True when the telemetry writer worker thread is running"""
+        return self._is_periodic and self._worker is not None and self.status is ServiceStatus.RUNNING
 
     def add_event(self, payload, payload_type):
         # type: (Union[Dict[str, Any], List[Any]], str) -> None
@@ -420,8 +428,6 @@ class TelemetryWriter(PeriodicService):
         #  List of configurations to be collected
 
         self.started = True
-        if register_app_shutdown:
-            atexit.register(self.app_shutdown)
 
         inst_config_id_entry = ("instrumentation_config_id", "", "default")
         if "DD_INSTRUMENTATION_CONFIG_ID" in os.environ:
@@ -777,7 +783,8 @@ class TelemetryWriter(PeriodicService):
             self._client.send_event(telemetry_event)
 
     def app_shutdown(self):
-        self.periodic(force_flush=True, shutting_down=True)
+        if self.started:
+            self.periodic(force_flush=True, shutting_down=True)
         self.disable()
 
     def reset_queues(self):
@@ -806,7 +813,7 @@ class TelemetryWriter(PeriodicService):
         if self.status == ServiceStatus.STOPPED:
             return
 
-        if self._is_periodic:
+        if self._is_running():
             self.stop(join=False)
 
         # Enable writer service in child process to avoid interpreter shutdown
@@ -821,3 +828,47 @@ class TelemetryWriter(PeriodicService):
         super(TelemetryWriter, self)._stop_service(*args, **kwargs)
         if join:
             self.join(timeout=2)
+
+    def _telemetry_excepthook(self, tp, value, root_traceback):
+        if root_traceback is not None:
+            # Get the frame which raised the exception
+            traceback = root_traceback
+            while traceback.tb_next:
+                traceback = traceback.tb_next
+
+            lineno = traceback.tb_frame.f_code.co_firstlineno
+            filename = traceback.tb_frame.f_code.co_filename
+            self.add_error(1, str(value), filename, lineno)
+
+            dir_parts = filename.split(os.path.sep)
+            # Check if exception was raised in the  `ddtrace.contrib` package
+            if "ddtrace" in dir_parts and "contrib" in dir_parts:
+                ddtrace_index = dir_parts.index("ddtrace")
+                contrib_index = dir_parts.index("contrib")
+                # Check if the filename has the following format:
+                # `../ddtrace/contrib/integration_name/..(subpath and/or file)...`
+                if ddtrace_index + 1 == contrib_index and len(dir_parts) - 2 > contrib_index:
+                    integration_name = dir_parts[contrib_index + 1]
+                    self.add_count_metric(
+                        "tracers",
+                        "integration_errors",
+                        1,
+                        (("integration_name", integration_name), ("error_type", tp.__name__)),
+                    )
+                    error_msg = "{}:{} {}".format(filename, lineno, str(value))
+                    self.add_integration(integration_name, True, error_msg=error_msg)
+
+            if self._enabled and not self.started:
+                self._app_started_event(False)
+
+            self.app_shutdown()
+
+        return self._ORIGINAL_EXCEPTHOOK(tp, value, root_traceback)
+
+    def install_excepthook(self):
+        """Install a hook that intercepts unhandled exception and send metrics about them."""
+        sys.excepthook = self._telemetry_excepthook
+
+    def uninstall_excepthook(self):
+        """Uninstall the global tracer except hook."""
+        sys.excepthook = self._ORIGINAL_EXCEPTHOOK

tests/telemetry/test_telemetry.py
@@ -6,11 +6,10 @@ import pytest
 
 def test_enable(test_agent_session, run_python_code_in_subprocess):
     code = """
-from ddtrace.internal.telemetry import telemetry_writer
+import ddtrace # enables telemetry
 from ddtrace.internal.service import ServiceStatus
 
-telemetry_writer.enable()
-
+from ddtrace.internal.telemetry import telemetry_writer
 assert telemetry_writer.status == ServiceStatus.RUNNING
 assert telemetry_writer._worker is not None
 """
@@ -41,13 +40,10 @@ span.finish()
     events = test_agent_session.get_events()
 
     assert len(events) == 5
-    # app-closed is sent after the generate-metrics event. This is because the span aggregator is shutdown after the
-    # the telemetry writer. This is a known limitation of the current implementation. Ideally the app-closed event
-    # would be sent last.
-    assert events[0]["request_type"] == "generate-metrics"
-    assert events[1]["request_type"] == "app-closing"
-    assert events[2]["request_type"] == "app-dependencies-loaded"
-    assert events[3]["request_type"] == "app-integrations-change"
+    assert events[0]["request_type"] == "app-closing"
+    assert events[1]["request_type"] == "app-dependencies-loaded"
+    assert events[2]["request_type"] == "app-integrations-change"
+    assert events[3]["request_type"] == "generate-metrics"
     assert events[4]["request_type"] == "app-started"
 
 
@@ -61,10 +57,10 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 import os
 
+import ddtrace # enables telemetry
 from ddtrace.internal.runtime import get_runtime_id
 from ddtrace.internal.telemetry import telemetry_writer
 
-telemetry_writer.enable()
 telemetry_writer._app_started_event()
 
 if os.fork() == 0:
@@ -104,16 +100,15 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 import os
 
+import ddtrace # enables telemetry
 from ddtrace.internal.runtime import get_runtime_id
-from ddtrace.internal.telemetry import telemetry_writer
-
-telemetry_writer.enable()
 
 if os.fork() > 0:
     # Print the parent process runtime id for validation
     print(get_runtime_id())
 
 # Heartbeat events are only sent if no other events are queued
+from ddtrace.internal.telemetry import telemetry_writer
 telemetry_writer.reset_queues()
 telemetry_writer.periodic(force_flush=True)
     """
@@ -166,12 +161,10 @@ import warnings
 # This process (pid=402) is multi-threaded, use of fork() may lead to deadlocks in the child
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-import ddtrace
+import ddtrace # enables telemetry
 import logging
 import os
 
-logging.basicConfig() # required for python 2.7
-ddtrace.internal.telemetry.telemetry_writer.enable()
 os.fork()
 """
     )
@@ -392,10 +385,9 @@ from ddtrace import tracer
 # Create a span to start the telemetry writer
 tracer.trace("hi").finish()
 
-# Importing ddtrace.internal.telemetry.__init__ creates the telemetry writer. This has a performance cost.
-# We want to avoid this cost when telemetry is disabled.
+# We want to import the telemetry module even when telemetry is disabled.
 import sys
-assert "ddtrace.internal.telemetry" not in sys.modules
+assert "ddtrace.internal.telemetry" in sys.modules
 """
     _, stderr, status, _ = run_python_code_in_subprocess(code, env=env)
 
@@ -404,3 +396,20 @@ assert "ddtrace.internal.telemetry" not in sys.modules
 
     assert status == 0, stderr
     assert stderr == b""
+
+
+# Disable agentless to ensure telemetry is enabled (agentless needs dd-api-key to be set)
+@pytest.mark.subprocess(env={"DD_CIVISIBILITY_AGENTLESS_ENABLED": "0"})
+def test_installed_excepthook():
+    import sys
+
+    # importing ddtrace initializes the telemetry writer and installs the excepthook
+    import ddtrace  # noqa: F401
+
+    assert sys.excepthook.__name__ == "_telemetry_excepthook"
+
+    from ddtrace.internal.telemetry import telemetry_writer
+
+    assert telemetry_writer._enabled is True
+    telemetry_writer.uninstall_excepthook()
+    assert sys.excepthook.__name__ != "_telemetry_excepthook"

ddtrace/internal/writer/writer.py
@@ -19,6 +19,7 @@ from ddtrace.settings.asm import config as asm_config
 from ddtrace.vendor.dogstatsd import DogStatsd
 
 from ...constants import KEEP_SPANS_RATE_KEY
+from ...internal.telemetry import telemetry_writer
 from ...internal.utils.formats import parse_tags_str
 from ...internal.utils.http import Response
 from ...internal.utils.time import StopWatch
@@ -607,13 +608,8 @@ class AgentWriter(HTTPWriter):
     def start(self):
         super(AgentWriter, self).start()
         try:
-            if config._telemetry_enabled:
-                from ...internal import telemetry
-
-                if telemetry.telemetry_writer.started:
-                    return
-
-                telemetry.telemetry_writer._app_started_event()
+            if config._telemetry_enabled and not telemetry_writer.started:
+                telemetry_writer._app_started_event()
 
             # appsec remote config should be enabled/started after the global tracer and configs
             # are initialized

ddtrace/__init__.py
@@ -11,17 +11,18 @@ ModuleWatchdog.install()
 import ddtrace.internal._unpatched  # noqa
 from ._logger import configure_ddtrace_logger
 
-
 # configure ddtrace logger before other modules log
 configure_ddtrace_logger()  # noqa: E402
 
 from .settings import _config as config
 
+# Enable telemetry writer and excepthook as early as possible to ensure we capture any exceptions from initialization
+import ddtrace.internal.telemetry  # noqa: E402
+
 from ._monkey import patch  # noqa: E402
 from ._monkey import patch_all  # noqa: E402
 from .internal.utils.deprecations import DDTraceDeprecationWarning  # noqa: E402
 from .pin import Pin  # noqa: E402
-from .settings import _config as config  # noqa: E402
 from ddtrace._trace.span import Span  # noqa: E402
 from ddtrace._trace.tracer import Tracer  # noqa: E402
 from ddtrace.vendor import debtcollector

ddtrace/_monkey.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING  # noqa:F401
 
 from ddtrace.vendor.wrapt.importer import when_imported
 
+from .internal import telemetry
 from .internal.logger import get_logger
 from .internal.utils import formats
 from .settings import _config as config
@@ -158,8 +159,6 @@ def _on_import_factory(module, prefix="ddtrace.contrib", raise_errors=True, patc
     """Factory to create an import hook for the provided module name"""
 
     def on_import(hook):
-        if config._telemetry_enabled:
-            from .internal import telemetry
         # Import and patch module
         path = "%s.%s" % (prefix, module)
         try:
@@ -169,25 +168,23 @@ def _on_import_factory(module, prefix="ddtrace.contrib", raise_errors=True, patc
                 raise
             error_msg = "failed to import ddtrace module %r when patching on import" % (path,)
             log.error(error_msg, exc_info=True)
-            if config._telemetry_enabled:
-                telemetry.telemetry_writer.add_integration(module, False, PATCH_MODULES.get(module) is True, error_msg)
-                telemetry.telemetry_writer.add_count_metric(
-                    "tracers", "integration_errors", 1, (("integration_name", module), ("error_type", type(e).__name__))
-                )
+            telemetry.telemetry_writer.add_integration(module, False, PATCH_MODULES.get(module) is True, error_msg)
+            telemetry.telemetry_writer.add_count_metric(
+                "tracers", "integration_errors", 1, (("integration_name", module), ("error_type", type(e).__name__))
+            )
         else:
             imported_module.patch()
-            if config._telemetry_enabled:
-                if hasattr(imported_module, "get_versions"):
-                    versions = imported_module.get_versions()
-                    for name, v in versions.items():
-                        telemetry.telemetry_writer.add_integration(
-                            name, True, PATCH_MODULES.get(module) is True, "", version=v
-                        )
-                else:
-                    version = imported_module.get_version()
+            if hasattr(imported_module, "get_versions"):
+                versions = imported_module.get_versions()
+                for name, v in versions.items():
                     telemetry.telemetry_writer.add_integration(
-                        module, True, PATCH_MODULES.get(module) is True, "", version=version
+                        name, True, PATCH_MODULES.get(module) is True, "", version=v
                     )
+            else:
+                version = imported_module.get_version()
+                telemetry.telemetry_writer.add_integration(
+                    module, True, PATCH_MODULES.get(module) is True, "", version=version
+                )
 
             if hasattr(imported_module, "patch_submodules"):
                 imported_module.patch_submodules(patch_indicator)

commit d323bbab450bded455a35969d3a4994210612f07

fix: telemetry exception hooks (#9830)
## Description

When wrapping pre-configured exception hooks the telemetry writer
converts exception hooks from type `function` to type `method `. This
adds the `self` argument to pre-configured exception which raised the
following error: `TypeError: xxxsome_funcxxxx() takes 3 positional
arguments but 4 were given`.

To resolve this issue this PR casts
`TelemetryWriter._ORIGINAL_EXCEPTHOOK` is cast to a static method. This
ensures the telemetry writer does not modify the argument of
preconfigured hooks.

## Impact 

When an application raises an unhandled exception and ddtrace
instrumentation telemetry is enabled:
 - the ddtrace telemetry exception will be executed
- the ddtrace library will fail to run exception hooks that were
configured before ddtrace was imported
- applications will log `TypeError: xxxsome_funcxxxx() takes 3
positional arguments but 4 were given` on application shutdown.


## Motivation 

Resolves the following errors:
https://github.com/DataDog/dd-trace-py/actions/runs/9891512830/job/27322136218#step:5:141

This bug was introduced in:
https://github.com/DataDog/dd-trace-py/commit/0cdc0831a62ee2575833298c49ff468cb14d51fb

## Checklist
- [x] PR author has checked that all the criteria below are met
- The PR description includes an overview of the change
- The PR description articulates the motivation for the change
- The change includes tests OR the PR description describes a testing
strategy
- The PR description notes risks associated with the change, if any
- Newly-added code is easy to change
- The change follows the [library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
- The change includes or references documentation updates if necessary
- Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))

## Reviewer Checklist

ddtrace/internal/telemetry/writer.py
@@ -230,7 +230,7 @@ class TelemetryWriter(PeriodicService):
     # of `itertools.count()` which is a CPython implementation detail. The sequence field in telemetry
     # payloads is only used in tests and is not required to process Telemetry events.
     _sequence = itertools.count(1)
-    _ORIGINAL_EXCEPTHOOK = sys.excepthook
+    _ORIGINAL_EXCEPTHOOK = staticmethod(sys.excepthook)
 
     def __init__(self, is_periodic=True, agentless=None):
         # type: (bool, Optional[bool]) -> None
@@ -863,7 +863,7 @@ class TelemetryWriter(PeriodicService):
 
             self.app_shutdown()
 
-        return self._ORIGINAL_EXCEPTHOOK(tp, value, root_traceback)
+        return TelemetryWriter._ORIGINAL_EXCEPTHOOK(tp, value, root_traceback)
 
     def install_excepthook(self):
         """Install a hook that intercepts unhandled exception and send metrics about them."""
@@ -871,4 +871,4 @@ class TelemetryWriter(PeriodicService):
 
     def uninstall_excepthook(self):
         """Uninstall the global tracer except hook."""
-        sys.excepthook = self._ORIGINAL_EXCEPTHOOK
+        sys.excepthook = TelemetryWriter._ORIGINAL_EXCEPTHOOK

tests/telemetry/test_telemetry.py
@@ -221,8 +221,7 @@ tracer.trace("hello").finish()
     ]["message"]
 
 
-@pytest.mark.skip(reason="We don't have a way to capture unhandled errors in bootstrap before telemetry is loaded")
-def test_app_started_error_unhandled_exception(test_agent_session, run_python_code_in_subprocess):
+def test_app_started_error_unhandled_tracer_exception(test_agent_session, run_python_code_in_subprocess):
     env = os.environ.copy()
     env["DD_SPAN_SAMPLING_RULES"] = "invalid_rules"
 
@@ -243,19 +242,37 @@ def test_app_started_error_unhandled_exception(test_agent_session, run_python_co
     assert "Unable to parse DD_SPAN_SAMPLING_RULES='invalid_rules'" in app_starteds[0]["payload"]["error"]["message"]
 
 
-def test_telemetry_with_raised_exception(test_agent_session, run_python_code_in_subprocess):
-    _, stderr, status, _ = run_python_code_in_subprocess(
-        "import ddtrace; ddtrace.tracer.trace('moon').finish(); raise Exception('bad_code')"
+def test_register_telemetry_excepthook_after_another_hook(test_agent_session, run_python_code_in_subprocess):
+    out, stderr, status, _ = run_python_code_in_subprocess(
+        """
+import sys
+
+old_exc_hook = sys.excepthook
+def pre_ddtrace_exc_hook(exctype, value, traceback):
+    print("pre_ddtrace_exc_hook called")
+    return old_exc_hook(exctype, value, traceback)
+
+sys.excepthook = pre_ddtrace_exc_hook
+
+import ddtrace
+raise Exception('bad_code')
+"""
     )
+    assert b"pre_ddtrace_exc_hook called" in out
     assert status == 1, stderr
     assert b"bad_code" in stderr
     # Regression test for python3.12 support
     assert b"RuntimeError: can't create new thread at interpreter shutdown" not in stderr
+    # Regression test for invalid number of arguments in wrapped exception hook
+    assert b"3 positional arguments but 4 were given" not in stderr
 
     app_starteds = test_agent_session.get_events("app-started")
     assert len(app_starteds) == 1
-    # app-started does not capture exceptions raised in application code
-    assert app_starteds[0]["payload"]["error"]["code"] == 0
+    # app-started captures unhandled exceptions raised in application code
+    assert app_starteds[0]["payload"]["error"]["code"] == 1
+    assert re.search(r"test\.py:\d+:\sbad_code$", app_starteds[0]["payload"]["error"]["message"]), app_starteds[0][
+        "payload"
+    ]["error"]["message"]
 
 
 def test_handled_integration_error(test_agent_session, run_python_code_in_subprocess):