a5b7a184266694ed9cb7609de7211a7acce48356
Laplie Anderson <laplie.anderson@datadoghq.com>
2024-07-19 12:18:20 -0400

    Add some logging

--- .gitlab/download-wheels-from-gh-actions.sh
+++ .gitlab/download-wheels-from-gh-actions.sh
@@ -6,6 +6,8 @@ if [ -z "$CI_COMMIT_SHA" ]; then
   exit 1
 fi
 
+echo "Querying for RUN_ID"
+
 timeout=600 # 10 minutes
 start_time=$(date +%s)
 end_time=$((start_time + timeout))
@@ -25,6 +27,7 @@ if [ -z "$RUN_ID" ]; then
 fi
 
 echo "Found RUN_ID: $RUN_ID"
+echo "Waiting for workflow to finish"
 
 # wait for run to finish
 gh run watch $RUN_ID --interval 45 --exit-status 1 --repo DataDog/dd-trace-py
@@ -32,10 +35,16 @@ gh run watch $RUN_ID --interval 45 --exit-status 1 --repo DataDog/dd-trace-py
 mkdir pywheels
 cd pywheels
 
+echo "Github workflow finished. Downloading wheels"
+
 # download all wheels
 gh run download $RUN_ID --repo DataDog/dd-trace-py
 
 cd ..
 
+echo "Finished downloading wheels. Fixing directory structure"
+
 # Flatten directory structure so all wheels are top level
 find pywheels -type f -exec mv {} pywheels \;
+
+echo "Done"
