b326d146d4ce13d538b3fa2405403eb9b0810f30
Pieter Van Isacker <pieter.vanisacker@lightspeedhq.com>
2024-07-24 00:10:22 +0200

    tests: add first dummy tests

---
+++ tests/contrib/kafka_python_ng/test_kafka_python_ng_patch.py
@@ -0,0 +1,24 @@
+from ddtrace.contrib.kafka.patch import get_version
+from ddtrace.contrib.kafka.patch import patch
+from ddtrace.contrib.kafka.patch import unpatch
+from tests.contrib.patch import PatchTestCase
+
+
+class TestKafkaPythonNgPatch(PatchTestCase.Base):
+    __integration_name__ = "kafka_python_ng"
+    __module_name__ = "kafka"
+    __patch_func__ = patch
+    __unpatch_func__ = unpatch
+    __get_version__ = get_version
+
+    def assert_module_patched(self, kafka):
+        self.assert_wrapped(kafka.KafkaProducer({}).produce)
+        self.assert_wrapped(kafka.KafkaConsumer({"group.id": "group_id"}).poll)
+
+    def assert_not_module_patched(self, kafka):
+        self.assert_not_wrapped(kafka.KafkaProducer({}).produce)
+        self.assert_not_wrapped(kafka.KafkaConsumer({"group.id": "group_id"}).poll)
+
+    def assert_not_module_double_patched(self, kafka):
+        self.assert_not_double_wrapped(kafka.KafkaProducer({}).produce)
+        self.assert_not_double_wrapped(kafka.KafkaConsumer({"group.id": "group_id"}).poll)
