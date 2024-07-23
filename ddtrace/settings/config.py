6aa23848451b52b53cc35cb35789875dc07813b4
Teague Bick <teague.bick@datadoghq.com>
2024-07-23 13:34:48 -0400

    fix backport issue

--- ddtrace/settings/config.py
+++ ddtrace/settings/config.py
@@ -12,7 +12,7 @@ from typing import Tuple  # noqa:F401
 from typing import Union  # noqa:F401
 
 from ddtrace.internal._file_queue import File_Queue
-from ddtrace.internal.serverless import in_azure_function 
+from ddtrace.internal.serverless import in_azure_function
 from ddtrace.internal.serverless import in_gcp_function
 from ddtrace.internal.utils.cache import cachedmethod
 from ddtrace.internal.utils.deprecations import DDTraceDeprecationWarning
