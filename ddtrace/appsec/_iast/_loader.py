b633bdaf365c53c350a8b0dabfef9d2cf7bab6b4
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-07-24 22:06:55 +0200

    retry
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/appsec/_iast/_loader.py
+++ ddtrace/appsec/_iast/_loader.py
@@ -13,8 +13,9 @@ IS_IAST_ENABLED = _is_iast_enabled()
 
 
 def _exec_iast_patched_module(module_watchdog, module):
+    # JJJ
     patched_source = None
-    compiled_code = None
+    # compiled_code = None
     if IS_IAST_ENABLED:
         try:
             module_path, patched_source = astpatch_module(module)
@@ -25,10 +26,11 @@ def _exec_iast_patched_module(module_watchdog, module):
     if patched_source:
         try:
             # Patched source is compiled in order to execute it
-            compiled_code = compile(patched_source, module_path, "exec")
+            # JJJ compiled_code
+            _ = compile(patched_source, module_path, "exec")
         except Exception:
             log.debug("Unexpected exception while compiling patched code", exc_info=True)
-            compiled_code = None
+            # compiled_code = None
 
     # JJJ
     module_watchdog.loader.exec_module(module)
