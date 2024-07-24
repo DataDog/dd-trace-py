729723ac7b751d5a0a3a26aad73488a710990aec
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-07-24 15:10:37 +0200

    force CI rerun
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/appsec/_iast/_loader.py
+++ ddtrace/appsec/_iast/_loader.py
@@ -13,7 +13,6 @@ IS_IAST_ENABLED = _is_iast_enabled()
 
 
 def _exec_iast_patched_module(module_watchdog, module):
-    # JJJ delete comment
     patched_source = None
     compiled_code = None
     if IS_IAST_ENABLED:
