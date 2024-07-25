9f2f9819d3f9a9ab0886c4cfebe7d8f673e85cc9
Munir Abdinur <munir_abdinur@hotmail.com>
2024-07-25 17:12:56 -0400

    test with 1.25 and latest

--- riotfile.py
+++ riotfile.py
@@ -207,7 +207,6 @@ venv = Venv(
                 "protobuf": ">=3",
                 "typing_extensions": latest,
                 "xmltodict": ">=0.12",
-                "opentelemetry-api": ">=1",
                 "opentracing": ">=2.0.0",
                 "bytecode": latest,
             },
@@ -330,8 +329,6 @@ venv = Venv(
                 "flask": "<=2.2.3",
                 "httpretty": "<1.1",
                 "werkzeug": "<2.0",
-                # FIXME: ddtrace does not support the latest versions of opentelemetry-api
-                "opentelemetry-api": "<1.25.0",
                 "pytest-randomly": latest,
                 "markupsafe": "<2.0",
             },
@@ -2355,7 +2352,8 @@ venv = Venv(
             pkgs={
                 "pytest-randomly": latest,
                 "pytest-asyncio": "==0.21.1",
-                "opentelemetry-api": ["~=1.0.0", "~=1.15.0", latest],
+                # Ensure we test against version of opentelemetry-api that broke compatibility with ddtrace
+                "opentelemetry-api": ["~=1.0.0", "~=1.15.0", "~=1.26.0", latest],
                 "opentelemetry-instrumentation-flask": "<=0.37b0",
                 # opentelemetry-instrumentation-flask does not support the latest version of markupsafe
                 "markupsafe": "==2.0.1",
