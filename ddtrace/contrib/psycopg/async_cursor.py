c9d5ff5cd87c85c7798250f5048e69308b6f5c97
quinna-h <quinna.halim@datadoghq.com>
2024-07-26 12:47:31 -0400

    wip

--- ddtrace/contrib/psycopg/async_cursor.py
+++ ddtrace/contrib/psycopg/async_cursor.py
@@ -21,25 +21,6 @@ class Psycopg3TracedAsyncCursor(Psycopg3TracedCursor, dbapi_async.TracedAsyncCur
         # messages will be the same.
         return await self.__wrapped__.__aexit__(exc_type, exc_val, exc_tb)
 
-    async def __aiter__(self):
-        await self._fetch_pipeline()
-        self._check_result_for_fetch()
-
-        def load(pos: int):
-            return self._tx.load_row(pos, self._make_row)
-
-        while True:
-            row = load(self._pos)
-            if row is None:
-                break
-            self._pos += 1
-            yield row
-
-    async def _fetch_pipeline(self) -> None:
-        if self._execmany_returning is not False and not self.pgresult and self._conn._pipeline:
-            async with self._conn.lock:
-                await self._conn.wait(self._conn._pipeline._fetch_gen(flush=True))
-
 
 class Psycopg3FetchTracedAsyncCursor(Psycopg3TracedAsyncCursor, dbapi_async.FetchTracedAsyncCursor):
     """Psycopg3FetchTracedAsyncCursor for psycopg"""
