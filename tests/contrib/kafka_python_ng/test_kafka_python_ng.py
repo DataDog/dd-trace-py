85e6fe1bcb170f084cd7ecd0dee0ec9d7cf5334f
Pieter Van Isacker <pieter.vanisacker@lightspeedhq.com>
2024-07-26 16:13:26 +0200

    tests: all kind of test tweaks

--- tests/contrib/kafka_python_ng/test_kafka_python_ng.py
+++ tests/contrib/kafka_python_ng/test_kafka_python_ng.py
@@ -1,8 +1,10 @@
 import kafka
 from kafka.structs import OffsetAndMetadata
+from kafka.structs import TopicPartition
 import pytest
 
 from ddtrace import Pin
+from ddtrace import Tracer
 from ddtrace.contrib.kafka_python_ng.patch import patch
 from ddtrace.contrib.kafka_python_ng.patch import unpatch
 from ddtrace.filters import TraceFilter
@@ -34,10 +36,58 @@ def kafka_topic(request):
 
     client = kafka.KafkaAdminClient(bootstrap_servers=[BOOTSTRAP_SERVERS])
     try:
+        client.delete_topics([topic_name])
+    except kafka.errors.UnknownTopicOrPartitionError:
+        pass
+
+    client.create_topics([kafka.admin.NewTopic(topic_name, 1, 1)])
+    return topic_name
+
+
+@pytest.fixture()
+def empty_kafka_topic(request):
+    topic_name = request.node.name.replace("[", "_").replace("]", "")
+    client = kafka.KafkaAdminClient(bootstrap_servers=[BOOTSTRAP_SERVERS])
+    client.delete_topics([topic_name])
+
+    client = kafka.KafkaAdminClient(bootstrap_servers=[BOOTSTRAP_SERVERS])
+    try:
         client.create_topics([kafka.admin.NewTopic(topic_name, 1, 1)])
     except kafka.errors.TopicAlreadyExistsError:
         pass
     return topic_name
+
+
+@pytest.fixture
+def dummy_tracer():
+    patch()
+    t = DummyTracer()
+    # disable backoff because it makes these tests less reliable
+    t._writer._send_payload_with_backoff = t._writer._send_payload
+    yield t
+    unpatch()
+
+
+@pytest.fixture
+def should_filter_empty_polls():
+    yield True
+
+
+@pytest.fixture
+def tracer(should_filter_empty_polls):
+    patch()
+    t = Tracer()
+    if should_filter_empty_polls:
+        t.configure(settings={"FILTERS": [KafkaConsumerPollFilter()]})
+    # disable backoff because it makes these tests less reliable
+    t._writer._send_payload_with_backoff = t._writer._send_payload
+
+    try:
+        yield t
+    finally:
+        t.flush()
+        t.shutdown()
+        unpatch()
 
 
 @pytest.fixture
@@ -50,81 +100,72 @@ def producer(tracer):
 @pytest.fixture
 def consumer(tracer, kafka_topic):
     _consumer = kafka.KafkaConsumer(
-        bootstrap_servers=[BOOTSTRAP_SERVERS], auto_offset_reset="earliest", group_id=GROUP_ID, enable_auto_commit=False
+        bootstrap_servers=[BOOTSTRAP_SERVERS], auto_offset_reset="earliest", group_id=GROUP_ID
     )
-    _consumer.subscribe(topics=[kafka_topic])
+
+    tp = TopicPartition(kafka_topic, 0)
+    _consumer.commit({tp: OffsetAndMetadata(0, "")})
     Pin.override(_consumer, tracer=tracer)
-    return _consumer
+    _consumer.subscribe(topics=[kafka_topic])
+    yield _consumer
+    _consumer.close()
 
 
 @pytest.fixture
-def should_filter_empty_polls():
-    yield True
-
-
-@pytest.fixture
-def tracer():
-    patch()
-    t = DummyTracer()
-    # disable backoff because it makes these tests less reliable
-    t._writer._send_payload_with_backoff = t._writer._send_payload
-    if should_filter_empty_polls:
-        t.configure(settings={"FILTERS": [KafkaConsumerPollFilter()]})
-    try:
-        yield t
-    finally:
-        t.flush()
-        t.shutdown()
-        unpatch()
-
-
-@pytest.fixture
-def consumer_without_topic(tracer):
+def non_auto_commit_consumer(tracer, kafka_topic):
     _consumer = kafka.KafkaConsumer(
         bootstrap_servers=[BOOTSTRAP_SERVERS], auto_offset_reset="earliest", group_id=GROUP_ID, enable_auto_commit=False
     )
+
+    tp = TopicPartition(kafka_topic, 0)
+    _consumer.commit({tp: OffsetAndMetadata(0, "")})
     Pin.override(_consumer, tracer=tracer)
-    return _consumer
+    _consumer.subscribe(topics=[kafka_topic])
+    yield _consumer
+    _consumer.close()
 
 
-def test_send_single_server(tracer, producer, kafka_topic):
+def test_send_single_server(dummy_tracer, producer, kafka_topic):
+    Pin.override(producer, tracer=dummy_tracer)
     producer.send(kafka_topic, value=PAYLOAD, key=KEY)
     producer.flush()
 
-    traces = tracer.pop_traces()
+    traces = dummy_tracer.pop_traces()
     assert 1 == len(traces)
     produce_span = traces[0][0]
     assert produce_span.get_tag("messaging.kafka.bootstrap.servers") == BOOTSTRAP_SERVERS
+    Pin.override(producer, tracer=None)
 
 
-def test_send_multiple_servers(tracer, kafka_topic):
+def test_send_multiple_servers(dummy_tracer, kafka_topic):
     producer = kafka.KafkaProducer(bootstrap_servers=[BOOTSTRAP_SERVERS] * 3)
-    Pin.override(producer, tracer=tracer)
+    Pin.override(producer, tracer=dummy_tracer)
     producer.send(kafka_topic, value=PAYLOAD, key=KEY)
     producer.flush()
 
-    traces = tracer.pop_traces()
+    traces = dummy_tracer.pop_traces()
     assert 1 == len(traces)
     produce_span = traces[0][0]
     assert produce_span.get_tag("messaging.kafka.bootstrap.servers") == ",".join([BOOTSTRAP_SERVERS] * 3)
     Pin.override(producer, tracer=None)
 
 
-def test_send_none_key(tracer, producer, kafka_topic):
+def test_send_none_key(dummy_tracer, producer, kafka_topic):
+    Pin.override(producer, tracer=dummy_tracer)
     producer.send(kafka_topic, value=PAYLOAD, key=None)
     producer.flush()
 
-    traces = tracer.pop_traces()
+    traces = dummy_tracer.pop_traces()
     assert 1 == len(traces), "key=None does not cause send() call to raise an exception"
     Pin.override(producer, tracer=None)
 
 
 @pytest.mark.parametrize("tombstone", [False, True])
 @pytest.mark.snapshot(ignores=["metrics.kafka.message_offset"])
-def test_message(producer, consumer, tombstone, kafka_topic):
+def test_message(producer, tombstone, kafka_topic):
     with override_config("kafka", dict(trace_empty_poll_enabled=False)):
         if tombstone:
-            producer.send(kafka_topic, key=KEY)
+            producer.send(kafka_topic, value=None, key=KEY)
         else:
             producer.send(kafka_topic, value=PAYLOAD, key=KEY)
         producer.flush()
@@ -136,50 +177,55 @@ def test_commit_with_poll(producer, consumer, kafka_topic):
         producer.send(kafka_topic, value=PAYLOAD, key=KEY)
         producer.flush()
         result = consumer.poll(100)
+        assert len(result) == 1
         for topic_partition in result:
             for record in result[topic_partition]:
-                consumer.commit({topic_partition: OffsetAndMetadata(record.offset, "")})
+                consumer.commit({topic_partition: OffsetAndMetadata(record.offset + 1, "")})
 
 
-@pytest.mark.snapshot(ignores=["metrics.kafka.message_offset"])
-def test_commit_with_poll_single_message(tracer, producer, consumer, kafka_topic):
+def test_commit_with_poll_single_message(dummy_tracer, producer, consumer, kafka_topic):
+    Pin.override(consumer, tracer=dummy_tracer)
     with override_config("kafka", dict(trace_empty_poll_enabled=False)):
         producer.send(kafka_topic, value=PAYLOAD, key=KEY)
         producer.flush()
         # One message is consumed and one span is generated.
-        result = consumer.poll(timeout_ms=100, max_records=1)
+        result = consumer.poll(timeout_ms=1000, max_records=1)
         assert len(result) == 1
         topic_partition = list(result.keys())[0]
         assert len(result[topic_partition]) == 1
-        consumer.commit({topic_partition: OffsetAndMetadata(result[topic_partition][0].offset, "")})
+        consumer.commit({topic_partition: OffsetAndMetadata(result[topic_partition][0].offset + 1, "")})
 
-    traces = tracer.pop_traces()
-    assert len(traces) == 2
+    traces = dummy_tracer.pop_traces()
+    assert len(traces) == 1
 
-    span = traces[1][0]
+    span = traces[0][0]
     assert span.name == "kafka.consume"
     assert span.get_tag("kafka.received_message") == "True"
+    Pin.override(consumer, tracer=dummy_tracer)
 
 
-@pytest.mark.snapshot(ignores=["metrics.kafka.message_offset"])
-def test_commit_with_poll_with_multiple_messages(tracer, producer, consumer, kafka_topic):
+def test_commit_with_poll_multiple_messages(dummy_tracer, producer, consumer, kafka_topic):
+    Pin.override(consumer, tracer=dummy_tracer)
     with override_config("kafka", dict(trace_empty_poll_enabled=False)):
         producer.send(kafka_topic, value=PAYLOAD, key=KEY)
         producer.send(kafka_topic, value=PAYLOAD, key=KEY)
         producer.flush()
         # Two messages are consumed but only ONE span is generated
-        result = consumer.poll(timeout_ms=100, max_records=2)
+        result = consumer.poll(timeout_ms=1000, max_records=2)
         assert len(result) == 1
         topic_partition = list(result.keys())[0]
         assert len(result[topic_partition]) == 2
-        consumer.commit({topic_partition: OffsetAndMetadata(result[topic_partition][1].offset, "")})
+        consumer.commit({topic_partition: OffsetAndMetadata(result[topic_partition][1].offset + 1, "")})
 
-    traces = tracer.pop_traces()
-    assert len(traces) == 3
+    traces = dummy_tracer.pop_traces()
+    # 1 trace and 1 span
+    assert len(traces) == 1
+    assert len(traces[0]) == 1
 
-    span = traces[2][0]
+    span = traces[0][0]
     assert span.name == "kafka.consume"
     assert span.get_tag("kafka.received_message") == "True"
+    Pin.override(consumer, tracer=None)
 
 
 @pytest.mark.snapshot(ignores=["metrics.kafka.message_offset"])
@@ -189,17 +235,17 @@ def test_async_commit(producer, consumer, kafka_topic):
         producer.flush()
         result = consumer.poll(100)
         topic_partition = list(result.keys())[0]
-        consumer.commit_async({topic_partition: OffsetAndMetadata(result[topic_partition][1].offset, "")})
+        consumer.commit_async({topic_partition: OffsetAndMetadata(result[topic_partition][0].offset, "")})
 
 
 # Empty poll should be traced by default
-def test_traces_empty_poll_by_default(tracer, consumer, kafka_topic):
+def test_traces_empty_poll_by_default(dummy_tracer, consumer, kafka_topic):
+    Pin.override(consumer, tracer=dummy_tracer)
+
     consumer.poll(10.0)
 
-    traces = tracer.pop_traces()
-
+    traces = dummy_tracer.pop_traces()
     empty_poll_span_created = False
-
     for trace in traces:
         for span in trace:
             try:
@@ -211,14 +257,17 @@ def test_traces_empty_poll_by_default(tracer, consumer, kafka_topic):
 
     assert empty_poll_span_created is True
 
+    Pin.override(consumer, tracer=None)
+
 
 # Empty poll should not be traced when disabled
-def test_does_not_trace_empty_poll_when_disabled(tracer, consumer, producer, kafka_topic):
+def test_does_not_trace_empty_poll_when_disabled(dummy_tracer, consumer, producer, kafka_topic):
+    Pin.override(consumer, tracer=dummy_tracer)
     with override_config("kafka", dict(trace_empty_poll_enabled=False)):
         # Test for empty poll
         consumer.poll(10.0)
 
-        traces = tracer.pop_traces()
+        traces = dummy_tracer.pop_traces()
         assert 0 == len(traces)
 
         # Test for non-empty poll right after
@@ -229,7 +278,7 @@ def test_does_not_trace_empty_poll_when_disabled(tracer, consumer, producer, kaf
         while result is None:
             result = consumer.poll(10.0)
 
-        traces = tracer.pop_traces()
+        traces = dummy_tracer.pop_traces()
         non_empty_poll_span_created = False
         for trace in traces:
             for span in trace:
@@ -241,3 +290,4 @@ def test_does_not_trace_empty_poll_when_disabled(tracer, consumer, producer, kaf
                     pass
 
         assert non_empty_poll_span_created is True
+        Pin.override(consumer, tracer=None)
