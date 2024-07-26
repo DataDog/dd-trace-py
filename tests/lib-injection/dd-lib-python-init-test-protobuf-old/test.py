f371b1288a6eaac6709a3b0519edbd96ccf24e4c
sanchda <838104+sanchda@users.noreply.github.com>
2024-07-26 02:51:12 +0000

    Connect to socket

--- tests/lib-injection/dd-lib-python-init-test-protobuf-old/test.py
+++ tests/lib-injection/dd-lib-python-init-test-protobuf-old/test.py
@@ -1,6 +1,13 @@
+import socket
+
 import addressbook_pb2
 
 
+# First, open a socket on port 18080 to allow the fixture to continue
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("localhost", 18080))
+
+# Do some stuff with the protobuf
 person = addressbook_pb2.Person()
 person.id = 1234
 person.name = "John Doe"
@@ -13,3 +20,5 @@ print(person)
 # we just write the string "OK" to a magic file named `mock-telemetry.out` to signal success.
 with open("mock-telemetry.out", "w") as f:
     f.write("OK")
+
+s.close()
