ed4741ca1490dc6f3464ca59c6c9bb2ea1e8e3ba
Emmett Butler <emmett.butler321@gmail.com>
2024-07-29 10:13:38 -0700

    debug

--- scripts/release.py
+++ scripts/release.py
@@ -172,6 +172,7 @@ def generate_release_notes(branch: str) -> str:
 
 def create_release_notes_sections(rn_raw, branch):
     # get anything in unreleased section in case there were updates since the last RC
+    print(rn_raw)
     unreleased = clean_release_notes(rn_raw)
     unreleased = break_into_release_sections(unreleased)
     try:
