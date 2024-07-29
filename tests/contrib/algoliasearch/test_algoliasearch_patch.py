82910fca70db5782cafd5e0b77fd3c3c0d919046
Rachel Yang <rachel.yang@datadoghq.com>
2024-07-29 12:45:21 -0400

    update test files to use private functions

--- tests/contrib/algoliasearch/test_algoliasearch_patch.py
+++ tests/contrib/algoliasearch/test_algoliasearch_patch.py
@@ -3,7 +3,7 @@
 # removed the ``_generated`` suffix from the file name, to prevent the content
 # from being overwritten by future re-generations.
 
-from ddtrace.contrib.algoliasearch import get_version
+from ddtrace.contrib.algoliasearch.patch import _get_version
 from ddtrace.contrib.algoliasearch.patch import patch
 
 
@@ -19,7 +19,7 @@ class TestAlgoliasearchPatch(PatchTestCase.Base):
     __module_name__ = "algoliasearch"
     __patch_func__ = patch
     __unpatch_func__ = unpatch
-    __get_version__ = get_version
+    __get_version__ = _get_version
 
     def assert_module_patched(self, algoliasearch):
         pass
--- tests/contrib/aredis/test_aredis_patch.py
+++ tests/contrib/aredis/test_aredis_patch.py
@@ -3,7 +3,7 @@
 # removed the ``_generated`` suffix from the file name, to prevent the content
 # from being overwritten by future re-generations.
 
-from ddtrace.contrib.aredis import get_version
+from ddtrace.contrib.aredis.patch import _get_version
 from ddtrace.contrib.aredis.patch import patch
 
 
@@ -19,7 +19,7 @@ class TestAredisPatch(PatchTestCase.Base):
     __module_name__ = "aredis"
     __patch_func__ = patch
     __unpatch_func__ = unpatch
-    __get_version__ = get_version
+    __get_version__ = _get_version
 
     def assert_module_patched(self, aredis):
         pass
