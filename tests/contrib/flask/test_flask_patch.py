d6439fbe1be0d2c2ac74f672bee8f615d52cf7d0
Rachel Yang <rachel.yang@datadoghq.com>
2024-07-30 10:49:14 -0400

    fix init and test files to call private methods

--- tests/contrib/flask/test_flask_patch.py
+++ tests/contrib/flask/test_flask_patch.py
@@ -3,7 +3,7 @@
 # removed the ``_generated`` suffix from the file name, to prevent the content
 # from being overwritten by future re-generations.
 
-from ddtrace.contrib.flask import get_version
+from ddtrace.contrib.flask import _get_version
 from ddtrace.contrib.flask.patch import patch
 
 
@@ -19,7 +19,7 @@ class TestFlaskPatch(PatchTestCase.Base):
     __module_name__ = "flask"
     __patch_func__ = patch
     __unpatch_func__ = unpatch
-    __get_version__ = get_version
+    __get_version__ = _get_version
 
     def assert_module_patched(self, flask):
         pass
