b97a2e2f9800a3455f546bd31c3f52750ff40bfd
lievan <evan.li@datadoqhq.com>
2024-07-31 11:27:30 -0400

    refactor test utils

--- tests/llmobs/_utils.py
+++ tests/llmobs/_utils.py
@@ -231,6 +231,76 @@ def _expected_llmobs_eval_metric_event(
     return eval_metric_event
 
 
+def _completion_event():
+    return {
+        "kind": "llm",
+        "span_id": "12345678901",
+        "trace_id": "98765432101",
+        "parent_id": "",
+        "session_id": "98765432101",
+        "name": "completion_span",
+        "tags": ["version:", "env:", "service:", "source:integration"],
+        "start_ns": 1707763310981223236,
+        "duration": 12345678900,
+        "error": 0,
+        "meta": {
+            "span.kind": "llm",
+            "model_name": "ada",
+            "model_provider": "openai",
+            "input": {
+                "messages": [{"content": "who broke enigma?"}],
+                "parameters": {"temperature": 0, "max_tokens": 256},
+            },
+            "output": {
+                "messages": [
+                    {
+                        "content": "\n\nThe Enigma code was broken by a team of codebreakers at Bletchley Park, led by mathematician Alan Turing."  # noqa: E501
+                    }
+                ]
+            },
+        },
+        "metrics": {"input_tokens": 64, "output_tokens": 128, "total_tokens": 192},
+    }
+
+
+def _chat_completion_event():
+    return {
+        "span_id": "12345678902",
+        "trace_id": "98765432102",
+        "parent_id": "",
+        "session_id": "98765432102",
+        "name": "chat_completion_span",
+        "tags": ["version:", "env:", "service:", "source:integration"],
+        "start_ns": 1707763310981223936,
+        "duration": 12345678900,
+        "error": 0,
+        "meta": {
+            "span.kind": "llm",
+            "model_name": "gpt-3.5-turbo",
+            "model_provider": "openai",
+            "input": {
+                "messages": [
+                    {
+                        "role": "system",
+                        "content": "You are an evil dark lord looking for his one ring to rule them all",
+                    },
+                    {"role": "user", "content": "I am a hobbit looking to go to Mordor"},
+                ],
+                "parameters": {"temperature": 0.9, "max_tokens": 256},
+            },
+            "output": {
+                "messages": [
+                    {
+                        "content": "Ah, a bold and foolish hobbit seeking to challenge my dominion in Mordor. Very well, little creature, I shall play along. But know that I am always watching, and your quest will not go unnoticed",  # noqa: E501
+                        "role": "assistant",
+                    },
+                ]
+            },
+        },
+        "metrics": {"input_tokens": 64, "output_tokens": 128, "total_tokens": 192},
+    }
+
+
 def _assert_core_span_context_values_are_expected(
     span_context, span, kind, tags=None, ml_app="test_ml_app", session_id="test_session_id", name="test_span"
 ):
