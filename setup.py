9414097d7e1a53a3c6a373623c164d18462eeb4a
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-07-31 02:23:38 +0000

    pin cython<3

--- setup.py
+++ setup.py
@@ -548,7 +548,7 @@ setup(
         "build_py": LibraryDownloader,
         "clean": CleanLibraries,
     },
-    setup_requires=["setuptools_scm[toml]>=4", "cython", "cmake>=3.24.2,<3.28", "setuptools-rust"],
+    setup_requires=["setuptools_scm[toml]>=4", "cython<3", "cmake>=3.24.2,<3.28", "setuptools-rust"],
     ext_modules=ext_modules
     + cythonize(
         [
