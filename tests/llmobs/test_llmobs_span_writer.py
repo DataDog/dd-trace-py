16bb7d8cd3b84a5991e4818144ab85983d3191cb
Tom Shen <tom.shen@datadoghq.com>
2024-07-31 18:02:13 -0400

    Switch back to datad0g.com and restore bad API key test

--- tests/llmobs/test_llmobs_span_writer.py
+++ tests/llmobs/test_llmobs_span_writer.py
@@ -3,20 +3,22 @@ import time
 import mock
 import pytest
 
-from ddtrace import config
 from ddtrace.llmobs._writer import LLMObsSpanWriter
 from tests.llmobs._utils import _chat_completion_event
 from tests.llmobs._utils import _completion_event
+from tests.utils import override_global_config
 
 
-INTAKE_URL = "https://llmobs-intake.%s" % config._dd_site
+DATADOG_SITE = "datad0g.com"
+INTAKE_URL = "https://llmobs-intake.%s" % DATADOG_SITE
 INTAKE_ENDPOINT = "%s/api/v2/llmobs" % INTAKE_URL
 
 
 def test_writer_start(mock_writer_logs):
-    llmobs_span_writer = LLMObsSpanWriter(is_agentless=True, interval=1000, timeout=1)
-    llmobs_span_writer.start()
-    mock_writer_logs.debug.assert_has_calls([mock.call("started %r to %r", "LLMObsSpanWriter", INTAKE_URL)])
+    with override_global_config(dict(_dd_api_key="foobar.baz", _dd_site=DATADOG_SITE)):
+        llmobs_span_writer = LLMObsSpanWriter(is_agentless=True, interval=1000, timeout=1)
+        llmobs_span_writer.start()
+        mock_writer_logs.debug.assert_has_calls([mock.call("started %r to %r", "LLMObsSpanWriter", INTAKE_URL)])
 
 
 def test_buffer_limit(mock_writer_logs):
@@ -30,52 +32,72 @@ def test_buffer_limit(mock_writer_logs):
 
 @pytest.mark.vcr_logs
 def test_send_completion_event(mock_writer_logs):
-    llmobs_span_writer = LLMObsSpanWriter(is_agentless=True, interval=1, timeout=1)
-    llmobs_span_writer.start()
-    llmobs_span_writer.enqueue(_completion_event())
-    llmobs_span_writer.periodic()
-    mock_writer_logs.debug.assert_has_calls([mock.call("encode %d LLMObs span events to be sent", 1)])
+    with override_global_config(dict(_dd_site=DATADOG_SITE, _dd_api_key="foobar.baz",)):
+        llmobs_span_writer = LLMObsSpanWriter(is_agentless=True, interval=1, timeout=1)
+        llmobs_span_writer.start()
+        llmobs_span_writer.enqueue(_completion_event())
+        llmobs_span_writer.periodic()
+        mock_writer_logs.debug.assert_has_calls([mock.call("encode %d LLMObs span events to be sent", 1)])
 
 
 @pytest.mark.vcr_logs
 def test_send_chat_completion_event(mock_writer_logs):
-    llmobs_span_writer = LLMObsSpanWriter(is_agentless=True, interval=1, timeout=1)
-    llmobs_span_writer.start()
-    llmobs_span_writer.enqueue(_chat_completion_event())
-    llmobs_span_writer.periodic()
-    mock_writer_logs.debug.assert_has_calls([mock.call("encode %d LLMObs span events to be sent", 1)])
+    with override_global_config(dict(_dd_site=DATADOG_SITE, _dd_api_key="foobar.baz",)):
+        llmobs_span_writer = LLMObsSpanWriter(is_agentless=True, interval=1, timeout=1)
+        llmobs_span_writer.start()
+        llmobs_span_writer.enqueue(_chat_completion_event())
+        llmobs_span_writer.periodic()
+        mock_writer_logs.debug.assert_has_calls([mock.call("encode %d LLMObs span events to be sent", 1)])
+
+
+@pytest.mark.vcr_logs
+def test_send_completion_bad_api_key(mock_writer_logs):
+    with override_global_config(dict(_dd_site=DATADOG_SITE, _dd_api_key="<bad-api-key>")):
+        llmobs_span_writer = LLMObsSpanWriter(is_agentless=True, interval=1, timeout=1)
+        llmobs_span_writer.start()
+        llmobs_span_writer.enqueue(_completion_event())
+        llmobs_span_writer.periodic()
+        mock_writer_logs.error.assert_called_with(
+            "failed to send traces to intake at %s: HTTP error status %s, reason %s",
+            1,
+            403,
+            b'{"errors":[{"status":"403","title":"Forbidden","detail":"API key is invalid"}]}',
+        )
 
 
 @pytest.mark.vcr_logs
 def test_send_timed_events(mock_writer_logs):
-    llmobs_span_writer = LLMObsSpanWriter(is_agentless=True, interval=0.01, timeout=1)
-    llmobs_span_writer.start()
-    mock_writer_logs.reset_mock()
+    with override_global_config(dict(_dd_site=DATADOG_SITE, _dd_api_key="foobar.baz",)):
+        llmobs_span_writer = LLMObsSpanWriter(is_agentless=True, interval=0.01, timeout=1)
+        llmobs_span_writer.start()
+        mock_writer_logs.reset_mock()
 
-    llmobs_span_writer.enqueue(_completion_event())
-    time.sleep(0.1)
-    mock_writer_logs.debug.assert_has_calls([mock.call("encode %d LLMObs span events to be sent", 1)])
-    mock_writer_logs.reset_mock()
-    llmobs_span_writer.enqueue(_chat_completion_event())
-    time.sleep(0.1)
-    mock_writer_logs.debug.assert_has_calls([mock.call("encode %d LLMObs span events to be sent", 1)])
+        llmobs_span_writer.enqueue(_completion_event())
+        time.sleep(0.1)
+        mock_writer_logs.debug.assert_has_calls([mock.call("encode %d LLMObs span events to be sent", 1)])
+        mock_writer_logs.reset_mock()
+        llmobs_span_writer.enqueue(_chat_completion_event())
+        time.sleep(0.1)
+        mock_writer_logs.debug.assert_has_calls([mock.call("encode %d LLMObs span events to be sent", 1)])
 
 
 @pytest.mark.vcr_logs
 def test_send_multiple_events(mock_writer_logs):
-    llmobs_span_writer = LLMObsSpanWriter(is_agentless=True, interval=0.01, timeout=1)
-    llmobs_span_writer.start()
-    mock_writer_logs.reset_mock()
+    with override_global_config(dict(_dd_site=DATADOG_SITE, _dd_api_key="foobar.baz",)):
+        llmobs_span_writer = LLMObsSpanWriter(is_agentless=True, interval=0.01, timeout=1)
+        llmobs_span_writer.start()
+        mock_writer_logs.reset_mock()
 
-    llmobs_span_writer.enqueue(_completion_event())
-    llmobs_span_writer.enqueue(_chat_completion_event())
-    time.sleep(0.1)
-    mock_writer_logs.debug.assert_has_calls([mock.call("encode %d LLMObs span events to be sent", 2)])
+        llmobs_span_writer.enqueue(_completion_event())
+        llmobs_span_writer.enqueue(_chat_completion_event())
+        time.sleep(0.1)
+        mock_writer_logs.debug.assert_has_calls([mock.call("encode %d LLMObs span events to be sent", 2)])
 
 
 def test_send_on_exit(mock_writer_logs, run_python_code_in_subprocess):
-    out, err, status, pid = run_python_code_in_subprocess(
-        """
+    with override_global_config(dict(_dd_site=DATADOG_SITE, _dd_api_key="foobar.baz",)):
+        out, err, status, pid = run_python_code_in_subprocess(
+            """
 import atexit
 import os
 import time
@@ -91,7 +113,7 @@ llmobs_span_writer = LLMObsSpanWriter(is_agentless=True, interval=0.01, timeout=
 llmobs_span_writer.start()
 llmobs_span_writer.enqueue(_completion_event())
 """,
-    )
-    assert status == 0, err
-    assert out == b""
-    assert err == b""
+        )
+        assert status == 0, err
+        assert out == b""
+        assert err == b""
