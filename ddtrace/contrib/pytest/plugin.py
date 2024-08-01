57598d47db1d53983bd4a472b438c7a556463dcb
Romain Komorn <136473744+romainkomorndatadog@users.noreply.github.com>
2024-08-01 11:00:50 +0100

    fix(ci_visibility): don't rely on git in pytest plugin (#9989)
    Fixes an issue introduced by #9586 and reported in #9981 where the
    pytest plugin would crash if the `git` binary was absent.
    
    The workspace is instead grabbed from the `CIVisibility` service's tags
    (via a new getter classmethod).
    
    In order for the variable to persist through to the
    `pytest_terminal_summary` hook, it is moved from being stashed on the
    pytest `session` object to the nested `config` object (which itself is
    passed to `pytest_terminal_summary`).
    
    For improved testability, a function to get the location of the `git`
    binary's passed, using `@cached()` to avoid re-fetching the path each
    time we call `git`.
    
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    - [ ] Reviewer has checked that all the criteria below are met
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    
    ---------
    
    Co-authored-by: erikayasuda <153395705+erikayasuda@users.noreply.github.com>
    (cherry picked from commit c460441c0c43d40b5d12e647ce60086b2f8f493f)

--- ddtrace/contrib/pytest/plugin.py
+++ ddtrace/contrib/pytest/plugin.py
@@ -37,7 +37,6 @@ from ddtrace.contrib.pytest.constants import XFAIL_REASON
 from ddtrace.contrib.unittest import unpatch as unpatch_unittest
 from ddtrace.ext import SpanTypes
 from ddtrace.ext import test
-from ddtrace.ext.git import extract_workspace_path
 from ddtrace.internal.ci_visibility import CIVisibility as _CIVisibility
 from ddtrace.internal.ci_visibility.constants import EVENT_TYPE as _EVENT_TYPE
 from ddtrace.internal.ci_visibility.constants import ITR_CORRELATION_ID_TAG_NAME
@@ -462,11 +461,8 @@ def pytest_sessionstart(session):
         log.debug("CI Visibility enabled - starting test session")
         global _global_skipped_elements
         _global_skipped_elements = 0
-        try:
-            workspace_path = extract_workspace_path()
-        except ValueError:
-            log.debug("Couldn't extract workspace path from git, reverting to config rootdir")
-            workspace_path = session.config.rootdir
+
+        workspace_path = _CIVisibility.get_workspace_path() or session.config.rootdir
 
         session._dd_workspace_path = workspace_path
 
