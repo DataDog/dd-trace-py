e13e5976c72d82e4c2ec7900299cc94c0273b8d1
Rachel Yang <rachel.yang@datadoghq.com>
2024-08-05 12:42:55 -0400

    Update __init__.py

--- ddtrace/contrib/algoliasearch/__init__.py
+++ ddtrace/contrib/algoliasearch/__init__.py
@@ -29,7 +29,7 @@ required_modules = ["algoliasearch", "algoliasearch.version"]
 
 with require_modules(required_modules) as missing_modules:
     if not missing_modules:
-        # Required to allow users to import from `ddtrace.contrib.aiohttp.patch` directly
+        # Required to allow users to import from `ddtrace.contrib.algoliasearch.patch` directly
         from . import patch as _  # noqa: F401, I001
 
         from ..internal.algoliasearch.patch import get_version
