4478dfb54e9cea32c8dbb891b94f1b64a38ae633
Rachel Yang <rachel.yang@datadoghq.com>
2024-08-05 12:43:29 -0400

    Update __init__.py

--- ddtrace/contrib/aredis/__init__.py
+++ ddtrace/contrib/aredis/__init__.py
@@ -73,7 +73,7 @@ required_modules = ["aredis", "aredis.client"]
 
 with require_modules(required_modules) as missing_modules:
     if not missing_modules:
-        # Required to allow users to import from `ddtrace.contrib.aiohttp.patch` directly
+        # Required to allow users to import from `ddtrace.contrib.aredis.patch` directly
         from . import patch as _  # noqa: F401, I001
 
         from ..internal.aredis.patch import get_version
