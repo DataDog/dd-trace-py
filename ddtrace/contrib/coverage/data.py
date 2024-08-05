4edee1f39d41e74a276557d6d4b444d519f4e15f
Rachel Yang <rachel.yang@datadoghq.com>
2024-08-05 15:39:01 -0400

    coverage/data issues

--- ddtrace/contrib/coverage/data.py
+++ ddtrace/contrib/coverage/data.py
@@ -1,15 +1,7 @@
-from ddtrace.internal.utils.deprecations import DDTraceDeprecationWarning
-from ddtrace.vendor.debtcollector import deprecate
-
-from ..internal.coverage.data import *  # noqa: F401,F403
+from copy import copy
+import sys
 
 
-def __getattr__(name):
-    deprecate(
-        ("%s.%s is deprecated" % (__name__, name)),
-        category=DDTraceDeprecationWarning,
-    )
+_coverage_data = {}
 
-    if name in globals():
-        return globals()[name]
-    raise AttributeError("%s has no attribute %s", __name__, name)
+_original_sys_argv_command = copy(sys.argv)
