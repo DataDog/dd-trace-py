5cbfc5250f1308bab1dfaaf0509cd747417f20cf
Rachel Yang <rachel.yang@datadoghq.com>
2024-08-05 16:25:13 -0400

    imports

--- ddtrace/contrib/coverage/utils.py
+++ ddtrace/contrib/coverage/utils.py
@@ -1,7 +1,7 @@
 from ddtrace.internal.utils.deprecations import DDTraceDeprecationWarning
 from ddtrace.vendor.debtcollector import deprecate
 
-from ..internal.coverage.utils import *  # noqa: F401,F403
+from ddtrace.contrib.internal.coverage.utils import *  # noqa: F401,F403
 
 
 def __getattr__(name):
