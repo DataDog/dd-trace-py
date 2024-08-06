42a70f81393c2780e1a6aefb362ba5a4db93349e
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-08-06 19:47:33 +0000

    use file endpoint

--- ddtrace/internal/datadog/profiling/dd_wrapper/src/uploader_builder.cpp
+++ ddtrace/internal/datadog/profiling/dd_wrapper/src/uploader_builder.cpp
@@ -159,11 +159,8 @@ Datadog::UploaderBuilder::build()
     }
 
     // If we're here, the tags are good, so we can initialize the exporter
-    ddog_prof_Exporter_NewResult res = ddog_prof_Exporter_new(to_slice("dd-trace-py"),
-                                                              to_slice(profiler_version),
-                                                              to_slice(family),
-                                                              &tags,
-                                                              endpoint);
+    ddog_prof_Exporter_NewResult res =
+      ddog_prof_Exporter_new(to_slice("dd-trace-py"), to_slice(profiler_version), to_slice(family), &tags, endpoint);
     ddog_Vec_Tag_drop(tags);
 
     auto ddog_exporter_result = Datadog::get_newexporter_result(res);
