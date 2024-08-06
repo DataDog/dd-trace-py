64c4bcad816d84cb6a4e4b36fdf47e0bfea840a8
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-08-06 17:59:39 +0200

    Update
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- benchmarks/appsec_iast_aspects/scenario.py
+++ benchmarks/appsec_iast_aspects/scenario.py
@@ -17,18 +17,17 @@ class IAST_Aspects(bm.Scenario):
         self.function_name = "do_re_sub"
         self.args = ["foobar", "o", "a", 1]
 
-        module_unpatched = importlib.import_module(self.mod_original_name)
-
-        with override_env({"DD_IAST_ENABLED": "True"}):
-            from tests.appsec.iast.aspects.conftest import _iast_patched_module
-
-            module_patched = _iast_patched_module(self.mod_original_name)
-
         def _(loops):
             for _ in range(loops):
                 if self.iast_enabled:
+                    with override_env({"DD_IAST_ENABLED": "True"}):
+                        from tests.appsec.iast.aspects.conftest import _iast_patched_module
+
+                        module_patched = _iast_patched_module(self.mod_original_name)
+
                     getattr(module_patched, self.function_name)(*self.args)
                 else:
+                    module_unpatched = importlib.import_module(self.mod_original_name)
                     getattr(module_unpatched, self.function_name)(*self.args)
 
         yield _
