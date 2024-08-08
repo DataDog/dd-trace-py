7de55ce2c03b50236ca4458cf55919229b687b59
yahya-mouman <yahya.mouman@datadoghq.com>
2024-08-08 11:10:07 +0200

    Update assertions

--- tests/contrib/langchain/test_langchain_llmobs.py
+++ tests/contrib/langchain/test_langchain_llmobs.py
@@ -150,12 +150,27 @@ class BaseTestLLMObsLangchain:
         return mock_tracer.pop_traces()[0]
 
     @classmethod
-    def _similarity_search(cls, vector_db, prompt, k, mock_tracer, cassette_name):
+    def _similarity_search(cls, pinecone, pinecone_vector_store, embedding_model, query, k, mock_tracer, cassette_name):
         LLMObs.enable(ml_app=cls.ml_app, integrations_enabled=False, _tracer=mock_tracer)
         with get_request_vcr(subdirectory_name=cls.cassette_subdirectory_name).use_cassette(cassette_name):
-            vector_db.similarity_search(prompt, k)
+            if PINECONE_VERSION <= (2, 2, 4):
+                pinecone.init(
+                    api_key=os.getenv("PINECONE_API_KEY", "<not-a-real-key>"),
+                    environment=os.getenv("PINECONE_ENV", "<not-a-real-env>"),
+                )
+                index = pinecone.Index(index_name="langchain-retrieval")
+            else:
+                # Pinecone 2.2.5+ moved init and other methods to a Pinecone class instance
+                pc = pinecone.Pinecone(
+                    api_key=os.getenv("PINECONE_API_KEY", "<not-a-real-key>"),
+                )
+                index = pc.Index(name="langchain-retrieval")
+
+            vector_db = pinecone_vector_store(index, embedding_model, "text")
+            vector_db.similarity_search(query, k)
+
         LLMObs.disable()
-        return mock_tracer.pop_traces()[0][0]
+        return mock_tracer.pop_traces()[0]
 
 
 @pytest.mark.skipif(LANGCHAIN_VERSION >= (0, 1), reason="These tests are for langchain < 0.1.0")
@@ -395,27 +410,70 @@ class TestLLMObsLangchain(BaseTestLLMObsLangchain):
             )
         )
 
-    def test_llmobs_similarity_search(self, langchain, pinecone, mock_llmobs_span_writer, mock_tracer):
-        if pinecone is None:
-            pytest.skip("pinecone not installed which is required for this test.")
-        pinecone.init(api_key=os.getenv("PINECONE_API_KEY", "<not-a-real-key>"))
-        index = pinecone.Index("langchain-retrieval")
-        embedding = langchain.embeddings.OpenAIEmbeddings()
-        vectorstore = langchain.vectorstores.Pinecone(index, embedding, "text")
+    def test_llmobs_similarity_search(self, langchain, mock_llmobs_span_writer, mock_tracer):
+        import pinecone
+
+        embedding_model = langchain.embeddings.OpenAIEmbeddings(model="text-embedding-ada-002")
         cassette_name = (
             "openai_pinecone_similarity_search_39.yaml" if PY39 else "openai_pinecone_similarity_search.yaml"
         )
-        vectorstore.similarity_search("Who was Alan Turing?", 1, mock_tracer, cassette_name)
-        trace = self._similarity_search(vectorstore, "Who was Alan Turing?", 1, mock_tracer, cassette_name)
-        assert mock_llmobs_span_writer.enqueue.call_count == 1
-        span = trace[0] if isinstance(trace, list) else trace
+        with mock.patch("langchain.embeddings.OpenAIEmbeddings._get_len_safe_embeddings", return_value=[[0.0] * 1536]):
+            trace = self._similarity_search(
+                pinecone=pinecone,
+                pinecone_vector_store=langchain.vectorstores.Pinecone,
+                embedding_model=embedding_model.embed_query,
+                query="Who was Alan Turing?",
+                k=1,
+                mock_tracer=mock_tracer,
+                cassette_name=cassette_name,
+            )
         mock_llmobs_span_writer.enqueue.assert_called_with(
-            _expected_llmobs_non_llm_span_event(
-                span,
+            _expected_llmobs_llm_span_event(
+                trace[0],
                 span_kind="retrieval",
+                model_provider="openai",
                 input_value="Who was Alan Turing?",
-                output_value=[],
-                tags={"ml_app": ""},
+                output_documents=[
+                    {
+                        "text": "A brilliant mathematician and cryptographer Alan was to become the "
+                        "founder of modern-day computer science and artificial intelligence; "
+                        "designing a machine at Bletchley Park to break secret Enigma encrypted "
+                        "messages used by the Nazi German war machine to protect sensitive "
+                        "commercial, diplomatic and military communications during World War 2. "
+                        "Thus, Turing made the single biggest contribution to the Allied victory "
+                        "in the war against Nazi Germany, possibly saving the lives of an "
+                        "estimated 2 million people, through his effort in shortening World War "
+                        "II.\\n\\nIn 2013, almost 60 years later, Turing received a posthumous "
+                        "Royal Pardon from Queen Elizabeth II. Today, the \\u201cTuring law\\u201d "
+                        "grants an automatic pardon to men who died before the law came into "
+                        "force, making it possible for living convicted gay men to seek pardons "
+                        "for offences now no longer on the statute book.\\n\\nAlas, "
+                        "Turing accidentally or otherwise lost his life in 1954, having been "
+                        "subjected by a British court to chemical castration, thus avoiding a "
+                        "custodial sentence. He is known to have ended his life at the age of 41 "
+                        "years, by eating an apple laced with cyanide.\\n\\nCareer \\nTuring was "
+                        "one of the people who worked on the first computers. He created the "
+                        "theoretical  Turing machine in 1936. The machine was imaginary, "
+                        "but it included the idea of a computer program.\\n\\nTuring was "
+                        "interested in artificial intelligence. He proposed the Turing test, "
+                        'to say when a machine could be called \\"intelligent\\". A computer '
+                        'could be said to \\"think\\" if a human talking with it could not tell '
+                        'it was a machine."'
+                    }
+                ],
+                tags={"ml_app": "langchain_test"},
+                integration="langchain",
+            )
+        )
+        mock_llmobs_span_writer.enqueue.assert_called_with(
+            _expected_llmobs_llm_span_event(
+                trace[1],
+                span_kind="embedding",
+                model_name=embedding_model.model,
+                model_provider="openai",
+                input_documents=[{"text": "Who was Alan Turing?"}],
+                output_value="[1 embedding(s) returned with size 1536]",
+                tags={"ml_app": "langchain_test"},
                 integration="langchain",
             )
         )
@@ -657,28 +715,72 @@ class TestLLMObsLangchainCommunity(BaseTestLLMObsLangchain):
             )
         )
 
-    def test_llmobs_similarity_search(self, langchain_openai, langchain_pinecone, mock_llmobs_span_writer, mock_tracer):
+    def test_llmobs_similarity_search(
+        self, langchain_openai, langchain_pinecone, mock_llmobs_span_writer, mock_tracer
+    ):
+        import pinecone
+
         if langchain_pinecone is None:
             pytest.skip("langchain_pinecone not installed which is required for this test.")
-        embedding = langchain_openai.OpenAIEmbeddings(model="text-embedding-ada-002")
-        index_name = "langchain-retrieval"
-        vectorstore = langchain_pinecone.PineconeVectorStore(
-            index_name=index_name,
-            embedding=embedding,
-        )
-        cassette_name = (
-            "openai_pinecone_similarity_search_39.yaml" if PY39 else "openai_pinecone_similarity_search.yaml"
-        )
-        trace = self._similarity_search(vectorstore, "Who was Alan Turing?", 1, mock_tracer, cassette_name)
-        assert mock_llmobs_span_writer.enqueue.call_count == 1
-        span = trace[0] if isinstance(trace, list) else trace
+        embedding_model = langchain_openai.OpenAIEmbeddings(model="text-embedding-ada-002")
+        cassette_name = "openai_pinecone_similarity_search.yaml"
+        with mock.patch("langchain_openai.OpenAIEmbeddings._get_len_safe_embeddings", return_value=[[0.0] * 1536]):
+            trace = self._similarity_search(
+                pinecone=pinecone,
+                pinecone_vector_store=langchain_pinecone.PineconeVectorStore,
+                embedding_model=embedding_model,
+                query="Who was Alan Turing?",
+                k=1,
+                mock_tracer=mock_tracer,
+                cassette_name=cassette_name,
+            )
         mock_llmobs_span_writer.enqueue.assert_called_with(
-            _expected_llmobs_non_llm_span_event(
-                span,
+            _expected_llmobs_llm_span_event(
+                trace[0],
                 span_kind="retrieval",
+                model_provider="openai",
                 input_value="Who was Alan Turing?",
-                output_value=[],
-                tags={"ml_app": ""},
+                output_documents=[
+                    {
+                        "text": "A brilliant mathematician and cryptographer Alan was to become the "
+                        "founder of modern-day computer science and artificial intelligence; "
+                        "designing a machine at Bletchley Park to break secret Enigma encrypted "
+                        "messages used by the Nazi German war machine to protect sensitive "
+                        "commercial, diplomatic and military communications during World War 2. "
+                        "Thus, Turing made the single biggest contribution to the Allied victory "
+                        "in the war against Nazi Germany, possibly saving the lives of an "
+                        "estimated 2 million people, through his effort in shortening World War "
+                        "II.\\n\\nIn 2013, almost 60 years later, Turing received a posthumous "
+                        "Royal Pardon from Queen Elizabeth II. Today, the \\u201cTuring law\\u201d "
+                        "grants an automatic pardon to men who died before the law came into "
+                        "force, making it possible for living convicted gay men to seek pardons "
+                        "for offences now no longer on the statute book.\\n\\nAlas, "
+                        "Turing accidentally or otherwise lost his life in 1954, having been "
+                        "subjected by a British court to chemical castration, thus avoiding a "
+                        "custodial sentence. He is known to have ended his life at the age of 41 "
+                        "years, by eating an apple laced with cyanide.\\n\\nCareer \\nTuring was "
+                        "one of the people who worked on the first computers. He created the "
+                        "theoretical  Turing machine in 1936. The machine was imaginary, "
+                        "but it included the idea of a computer program.\\n\\nTuring was "
+                        "interested in artificial intelligence. He proposed the Turing test, "
+                        'to say when a machine could be called \\"intelligent\\". A computer '
+                        'could be said to \\"think\\" if a human talking with it could not tell '
+                        'it was a machine."'
+                    }
+                ],
+                tags={"ml_app": "langchain_test"},
+                integration="langchain",
+            )
+        )
+        mock_llmobs_span_writer.enqueue.assert_called_with(
+            _expected_llmobs_llm_span_event(
+                trace[1],
+                span_kind="embedding",
+                model_name=embedding_model.model,
+                model_provider="openai",
+                input_documents=[{"text": "Who was Alan Turing?"}],
+                output_value="[1 embedding(s) returned with size 1536]",
+                tags={"ml_app": "langchain_test"},
                 integration="langchain",
             )
         )
