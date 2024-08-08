9b5fbea70eef741ef03791dce11eb99d64621ed4
Pieter Van Isacker <pieter.vanisacker@lightspeedhq.com>
2024-08-08 12:37:22 +0200

    fix: add basic support for DSM
    CircleCI:aiokafka

--- ddtrace/internal/datastreams/__init__.py
+++ ddtrace/internal/datastreams/__init__.py
@@ -3,13 +3,15 @@ from ddtrace import config
 from ...internal.utils.importlib import require_modules
 
 
-required_modules = ["confluent_kafka", "botocore", "kombu"]
+required_modules = ["confluent_kafka", "botocore", "kombu", "aiokafka"]
 _processor = None
 
 if config._data_streams_enabled:
     with require_modules(required_modules) as missing_modules:
         if "confluent_kafka" not in missing_modules:
             from . import kafka  # noqa:F401
+        if "aiokafka" not in missing_modules:
+            from . import aiokafka  # noqa:F401
         if "botocore" not in missing_modules:
             from . import botocore  # noqa:F401
         if "kombu" not in missing_modules:
