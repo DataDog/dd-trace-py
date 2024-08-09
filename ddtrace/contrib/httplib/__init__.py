8a29882011d518713bfd875d402f96dd39a0705e
Rachel Yang <rachel.yang@datadoghq.com>
2024-08-09 13:41:35 -0400

    chore(integrations): move httplib,httpx,jinja2,kafka,kombu to internal

--- ddtrace/contrib/httplib/__init__.py
+++ ddtrace/contrib/httplib/__init__.py
@@ -58,9 +58,10 @@ The integration can be configured per instance::
 
 :ref:`Headers tracing <http-headers-tracing>` is supported for this integration.
 """
-from .patch import get_version
-from .patch import patch
-from .patch import unpatch
+
+from ..internal.httplib.patch import get_version
+from ..internal.httplib.patch import patch
+from ..internal.httplib.patch import unpatch
 
 
 __all__ = ["patch", "unpatch", "get_version"]
