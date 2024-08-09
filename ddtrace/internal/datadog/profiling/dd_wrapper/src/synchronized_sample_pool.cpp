90158c9b9edb9e9bc8c0a50860680fdbe9db8248
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-08-09 14:13:20 -0400

    Apply suggestions from code review
    Co-authored-by: David Sanchez <838104+sanchda@users.noreply.github.com>

--- ddtrace/internal/datadog/profiling/dd_wrapper/src/synchronized_sample_pool.cpp
+++ ddtrace/internal/datadog/profiling/dd_wrapper/src/synchronized_sample_pool.cpp
@@ -12,7 +12,7 @@ namespace Datadog {
 void
 sample_delete_fn(void* sample)
 {
-    delete static_cast<Sample*>(sample);
+    delete static_cast<Sample*>(sample); // NOLINT(cppcoreguidelines-owning-memory)
 }
 
 SynchronizedSamplePool::SynchronizedSamplePool()
@@ -46,27 +46,26 @@ SynchronizedSamplePool::SynchronizedSamplePool()
 std::optional<Sample*>
 SynchronizedSamplePool::get_sample()
 {
-    if (pool == nullptr) {
-        // It's actually ok to call ddog_ArrayQueue_* methods with a nullptr,
-        // they will return an error result, but we already have printed out
-        // an error message in the constructor, so check for nullptr here to
-        // avoid spamming the error message.
-        return std::nullopt;
+    std::optional<Sample*> result = std::nullopt;
+
+    // It's actually ok to call ddog_ArrayQueue_* methods with a nullptr,
+    // they will return an error result, but we already have printed out
+    // an error message in the constructor, so check for nullptr here to
+    // avoid spamming the error message.
+    if (pool != nullptr) {
+        ddog_ArrayQueue_PopResult pop_result = ddog_ArrayQueue_pop(pool.get());
+
+        if (pop_result.tag == DDOG_ARRAY_QUEUE_POP_RESULT_OK) {
+            result = static_cast<Sample*>(pop_result.ok);
+        } else if (pop_result.tag == DDOG_ARRAY_QUEUE_POP_RESULT_ERR) {
+            auto err = pop_result.err;
+            std::string errmsg = err_to_msg(&err, "Failed to get sample from pool");
+            std::cerr << errmsg << std::endl;
+            ddog_Error_drop(&err);
+        }
     }
-    ddog_ArrayQueue_PopResult pop_result = ddog_ArrayQueue_pop(pool.get());
-    if (pop_result.tag == DDOG_ARRAY_QUEUE_POP_RESULT_OK) {
-        return static_cast<Sample*>(pop_result.ok);
-    } else if (pop_result.tag == DDOG_ARRAY_QUEUE_POP_RESULT_EMPTY) {
-        return std::nullopt;
-    } else if (pop_result.tag == DDOG_ARRAY_QUEUE_POP_RESULT_ERR) {
-        auto err = pop_result.err;
-        std::string errmsg = err_to_msg(&err, "Failed to get sample from pool");
-        std::cerr << errmsg << std::endl;
-        ddog_Error_drop(&err);
-        return std::nullopt;
-    }
-    // To silence the warning about missing return statement
-    return std::nullopt;
+
+    return result;
 }
 
 std::optional<Sample*>
