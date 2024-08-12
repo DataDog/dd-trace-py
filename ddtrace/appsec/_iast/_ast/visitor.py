c6c19aff2ef1d2b710b373d35ed9c5ec09484e52
Federico Mon <federico.mon@datadoghq.com>
2024-08-12 17:49:02 +0200

    perf: add ``sqlalchemy.sql.compiler:_requires_quotes`` to fine-grained ast exclusion list

--- ddtrace/appsec/_iast/_ast/visitor.py
+++ ddtrace/appsec/_iast/_ast/visitor.py
@@ -106,6 +106,7 @@ _ASPECTS_SPEC: Dict[Text, Any] = {
             "": ("",),
         },
         "django.utils.html": {"": ("format_html", "format_html_join")},
+        "sqlalchemy.sql.compiler": {"": ("_requires_quotes",)},
     },
     # This is a set since all functions will be replaced by taint_sink_functions
     "taint_sinks": {
