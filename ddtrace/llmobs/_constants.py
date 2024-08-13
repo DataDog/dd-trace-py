d7203df0d6407316d46e8edefd3e8e6be30d72d0
Tom Shen <2065456+tomshen@users.noreply.github.com>
2024-08-13 17:52:11 -0400

    feat(llmobs): truncate LLMObs span events that are >1MB in size (#10148)
    Events that are >1MB are not supported by the event platform, and will
    be truncated automatically in ways that make them no longer processable
    by the LLMObs event processor. This change introduces truncation for
    span events that exceed that size limitation.
    
    Truncation is performed by dropping the input and output fields.

--- ddtrace/llmobs/_constants.py
+++ ddtrace/llmobs/_constants.py
@@ -34,7 +34,11 @@ EVP_PROXY_AGENT_BASE_PATH = "evp_proxy/v2"
 EVP_PROXY_AGENT_ENDPOINT = "{}/api/v2/llmobs".format(EVP_PROXY_AGENT_BASE_PATH)
 EVP_SUBDOMAIN_HEADER_NAME = "X-Datadog-EVP-Subdomain"
 EVP_SUBDOMAIN_HEADER_VALUE = "llmobs-intake"
-EVP_PAYLOAD_SIZE_LIMIT = 5 << 20  # 5MB
+EVP_PAYLOAD_SIZE_LIMIT = 5 << 20  # 5MB (actual limit is 5.1MB)
+EVP_EVENT_SIZE_LIMIT = (1 << 20) - 1024  # 999KB (actual limit is 1MB)
 
 AGENTLESS_BASE_URL = "https://llmobs-intake"
 AGENTLESS_ENDPOINT = "api/v2/llmobs"
+
+DROPPED_IO_TAG = "dropped_io:1"
+DROPPED_VALUE_TEXT = "[This value has been dropped because this span's size exceeds the 1MB size limit.]"
