487971819c56b00cf1b8003eecaaafe5ce80881a
William Conti <58711692+wconti27@users.noreply.github.com>
2024-08-14 11:23:15 -0400

    fix(django): recreate response stream to prevent reading from empty response body [backport 2.11] (#10207)
    Fixes customer escalation. Customer was using the [Spyne
    library](https://github.com/arskom/spyne) with Django and making SOAP
    requests towards it. The problem was found to be that Django integration
    was reading the response body to set some span tags. However, later in
    the callstack, Spyne also tries to read the response body (now empty
    since the stream was fully read). This PR copies/recreates the response
    body to prevent this error.
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

--- ddtrace/contrib/django/utils.py
+++ ddtrace/contrib/django/utils.py
@@ -1,3 +1,4 @@
+import io
 import json
 from typing import Any  # noqa:F401
 from typing import Dict  # noqa:F401
@@ -284,6 +285,19 @@ def _extract_body(request):
         except Exception:
             log.debug("Failed to parse request body", exc_info=True)
         return req_body
+
+
+def _remake_body(request):
+    # some libs that utilize django (Spyne) require the body stream to be unread or else will throw errors
+    # see: https://github.com/arskom/spyne/blob/f105ec2f41495485fef1211fe73394231b3f76e5/spyne/server/wsgi.py#L538
+    if request.method in _BODY_METHODS:
+        try:
+            unread_body = io.BytesIO(request._body)
+            if unread_body.seekable():
+                unread_body.seek(0)
+                request.META["wsgi.input"] = unread_body
+        except Exception:
+            log.debug("Failed to remake Django request body", exc_info=True)
 
 
 def _get_request_headers(request):
