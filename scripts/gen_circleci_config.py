3f88c81d5f65a9369987adc8a9a6cc8ccb008228
brettlangdon <me@brett.is>
2024-08-14 10:55:30 -0400

    chore(ci): move build_docs job to GitHub Actions

--- scripts/gen_circleci_config.py
+++ scripts/gen_circleci_config.py
@@ -88,16 +88,6 @@ def gen_pre_checks(template: dict) -> None:
     )
 
 
-def gen_build_docs(template: dict) -> None:
-    """Include the docs build step if the docs have changed."""
-    from needs_testrun import pr_matches_patterns
-
-    if pr_matches_patterns(
-        {"docker", "docs/*", "ddtrace/*", "scripts/docs", "releasenotes/*", "benchmarks/README.rst"}
-    ):
-        template["workflows"]["test"]["jobs"].append({"build_docs": template["requires_pre_check"]})
-
-
 def gen_slotscheck(template: dict) -> None:
     """Include the slotscheck if the Python source has changed."""
     from needs_testrun import pr_matches_patterns
