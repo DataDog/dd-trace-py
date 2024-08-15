717da8842317b6a804665a9f44d9a83ea134de49
brettlangdon <me@brett.is>
2024-08-15 09:14:56 -0400

    fix repair script to get rid of linux wheels

--- .gitlab/repair_wheel_linux.sh
+++ .gitlab/repair_wheel_linux.sh
@@ -2,21 +2,20 @@
 set -e
 wheelhouse="${1:-wheelhouse/}"
 
-rm -rf tempwheelhouse/
-mkdir tempwheelhouse/
-
+# First repair the wheels
+# This will convert linux_{arch}.whl to manylinux_{arch}.whl, and the delete the
+# original linux_{arch}.whl
 for w in $(ls wheelhouse/*.whl);
 do
     echo "Repairing $w"
     unzip -l "$w" | grep -E '(\.so)|(\*.c)|(\*.cpp)|(\*.cc)|(\*.h)|(\*.hpp)|(\*.pyx)'
-    auditwheel repair -w tempwheelhouse/ "$w"
+    auditwheel repair -w wheelhouse/ "$w"
+    rm "$w"
 done
 
-for new_wheel in tempwheelhouse/*.whl; do
-    echo "Removing source files from $new_wheel"
-    python3.12 scripts/zip_filter.py "$new_wheel" \*.c \*.cpp \*.cc \*.h \*.hpp \*.pyx
-    unzip -l "$new_wheel" | grep -E '(\.so)|(\*.c)|(\*.cpp)|(\*.cc)|(\*.h)|(\*.hpp)|(\*.pyx)'
-    mv "$new_wheel" wheelhouse/
+# Strip any source files from the manylinux wheels
+for w in $(ls wheelhouse/*.whl); do
+    echo "Removing source files from $w"
+    python3.12 scripts/zip_filter.py "$w" \*.c \*.cpp \*.cc \*.h \*.hpp \*.pyx
+    unzip -l "$w" | grep -E '(\.so)|(\*.c)|(\*.cpp)|(\*.cc)|(\*.h)|(\*.hpp)|(\*.pyx)'
 done
-
-rm -rf tempwheelhouse/
