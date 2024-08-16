b5f1b8123d3369dd8cd895527fcab5b9023e53af
brettlangdon <me@brett.is>
2024-08-16 11:02:33 -0400

    refactoring

--- .gitlab/download-dependency-wheels.sh
+++ .gitlab/download-dependency-wheels.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -eo pipefail
 
+PYTHON_EXE=${1:-python3}
+
 if [ -z "$CI_COMMIT_SHA" ]; then
   echo "Error: CI_COMMIT_SHA was not provided"
   exit 1
 fi
 
-python3 -m pip install -U "pip>=22.0"
-python3 -m pip install packaging
+$PYTHON_EXE -m pip install -U "pip>=22.0"
+$PYTHON_EXE -m pip install packaging
 
 mkdir wheelhouse-dep
 
@@ -18,9 +20,7 @@ export PYTHONUNBUFFERED=TRUE
 ../lib-injection/dl_wheels.py \
     --python-version=$PYTHON_VERSION \
     --local-ddtrace \
-    --arch x86_64 \
-    --arch aarch64 \
-    --platform musllinux_1_2 \
-    --platform manylinux2014 \
+    --arch $ARCH \
+    --platform $PLATFORM \
     --output-dir ../wheelhouse-dep \
     --verbose
