61657bc3f4d5cd98127a3ac16bfb78a6809fffcb
Federico Mon <federico.mon@datadoghq.com>
2024-08-21 10:23:17 +0200

    add re.Match to tainteable objects

--- ddtrace/appsec/_iast/_taint_tracking/Utils/StringUtils.h
+++ ddtrace/appsec/_iast/_taint_tracking/Utils/StringUtils.h
@@ -14,6 +14,18 @@ get_unique_id(const PyObject* str)
     return reinterpret_cast<uintptr_t>(str);
 }
 
+inline static bool PyReMatch_Check(const PyObject* obj)
+{
+    PyObject* re_module = PyImport_ImportModule("re");
+    PyTypeObject* match_type = (PyTypeObject*)PyObject_GetAttrString(re_module, "Match");
+    bool res = PyObject_IsInstance(obj, match_type);
+    // Alternatively:
+    // bool res = PyType_IsSubtype(Py_TYPE(obj), match_type);
+    Py_DECREF(re_module);
+    Py_DECREF(match_type);
+    return res;
+}
+
 bool
 is_notinterned_notfasttainted_unicode(const PyObject* objptr);
 
@@ -26,7 +38,7 @@ is_text(const PyObject* pyptr)
     if (!pyptr)
         return false;
 
-    return PyUnicode_Check(pyptr) || PyBytes_Check(pyptr) || PyByteArray_Check(pyptr);
+    return PyUnicode_Check(pyptr) || PyBytes_Check(pyptr) || PyByteArray_Check(pyptr) || PyReMatch_Check(pyptr);
 }
 
 string
