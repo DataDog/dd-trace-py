3a88bd80297c5ebb43765202ba6f6a170852bf21
Juanjo Alvarez Martinez <juanjo.alvarezmartinez@datadoghq.com>
2024-08-21 15:12:12 +0200

    chore: move index_aspect to native only (#10258)
    ## Description
    
    - Move the `index_aspect` implementation to be 100% native. This reduces
    the overhead from 162% to 78%.
    - Add a new `has_pyerr_as_string` helper that will return a non empty
    `std::string` if there is a Python error loaded, thus avoiding having to
    do two calls for checking and retrieving.
    - Some const correctness changes.
    - Fix some duplicated code on `GenericUtils.cpp` from the previous PR.
    
    
    ## Checklist
    - [X] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    
    ---------
    
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
    Co-authored-by: Federico Mon <federico.mon@datadoghq.com>

--- ddtrace/appsec/_iast/_taint_tracking/Utils/GenericUtils.cpp
+++ ddtrace/appsec/_iast/_taint_tracking/Utils/GenericUtils.cpp
@@ -3,7 +3,7 @@
 #include "GenericUtils.h"
 
 bool
-asbool(py::object value)
+asbool(const py::object& value)
 {
     if (value.is_none()) {
         return false;
@@ -12,7 +12,7 @@ asbool(py::object value)
         return value.cast<bool>();
     }
     if (py::isinstance<py::str>(value)) {
-        std::string str_value = value.cast<std::string>();
+        auto str_value = value.cast<std::string>();
         std::transform(str_value.begin(), str_value.end(), str_value.begin(), ::tolower);
         return str_value == "true" || str_value == "1";
     }
@@ -25,7 +25,7 @@ asbool(const char* value)
     if (value == nullptr) {
         return false;
     }
-    py::object debug_value_py = py::str(value);
+    const py::object debug_value_py = py::str(value);
     return asbool(debug_value_py);
 }
 
@@ -36,27 +36,45 @@ iast_taint_log_error(const std::string& msg)
         if (!is_iast_debug_enabled()) {
             return;
         }
-
-        py::object inspect = py::module::import("inspect");
-        py::list stack = inspect.attr("stack")();
         std::string frame_info;
+        try {
+            const py::module inspect = py::module::import("inspect");
+            const py::list stack = inspect.attr("stack")();
 
-        for (size_t i = 0; i < std::min(stack.size(), static_cast<size_t>(7)); ++i) {
-            py::object frame = stack[i];
-            py::object frame_info_obj = frame.attr("frame");
-            std::string filename = py::str(frame_info_obj.attr("f_code").attr("co_filename"));
-            int lineno = py::int_(frame_info_obj.attr("f_lineno"));
-            frame_info += filename + ", " + std::to_string(lineno) + "\n";
+            for (size_t i = 0; i < std::min(stack.size(), static_cast<size_t>(7)); ++i) {
+                py::object frame = stack[i];
+                py::object frame_info_obj = frame.attr("frame");
+                std::string filename = py::str(frame_info_obj.attr("f_code").attr("co_filename"));
+                const int lineno = py::int_(frame_info_obj.attr("f_lineno"));
+                frame_info += filename + ", " + std::to_string(lineno) + "\n";
+            }
+        } catch (const py::error_already_set& e) {
+            cerr << "ddtrace: error in iast_taint_log_error trying to retrieve file and line: " << e.what() << "\n";
+            PyErr_Clear(); // Clear the error state
+            frame_info = "(unkown file)";
         }
 
-        auto log = get_python_logger();
+        const auto log = get_python_logger();
         log.attr("debug")(msg + ": " + frame_info);
 
-        py::module metrics = py::module::import("ddtrace.appsec._iast._metrics");
+        const py::module metrics = py::module::import("ddtrace.appsec._iast._metrics");
         metrics.attr("_set_iast_error_metric")("IAST propagation error. " + msg);
 
     } catch (const py::error_already_set& e) {
+        if (!e.trace().is_none()) {
+
+            PyObject *type, *value, *tb;
+            PyErr_Fetch(&type, &value, &tb);
+            PyErr_NormalizeException(&type, &value, &tb);
+            if (value) {
+                std::cerr << "Exception value: " << py::str(value).cast<std::string>() << "\n";
+            }
+            if (tb) {
+                std::cerr << "Traceback:\n" << py::str(tb).cast<std::string>() << "\n";
+            }
+        }
         cerr << "ddtrace: error when trying to log an IAST native error: " << e.what() << "\n";
+        PyErr_Clear(); // Clear the error state
     } catch (const std::exception& e) {
         cerr << "ddtrace: error when trying to log an IAST native error: " << e.what() << "\n";
     } catch (...) {
