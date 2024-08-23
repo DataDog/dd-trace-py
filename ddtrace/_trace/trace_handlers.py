d6cfa19387aaeea70698f35f32f26d9e2de6229a
chris.agocs <chris.agocs@datadoghq.com>
2024-08-23 13:51:01 -0700

    wtf why can't we import botocore?

--- ddtrace/_trace/trace_handlers.py
+++ ddtrace/_trace/trace_handlers.py
@@ -13,7 +13,8 @@ from ddtrace._trace.span import Span
 from ddtrace._trace.utils import extract_DD_context_from_messages
 from ddtrace._trace.utils import set_botocore_patched_api_call_span_tags as set_patched_api_call_span_tags
 from ddtrace._trace.utils import set_botocore_response_metadata_tags
-from ddtrace.contrib.internal.botocore.services.stepfunctions import BOTOCORE_STEPFUNCTIONS_INPUT_KEY
+
+# from ddtrace.contrib.internal.botocore.services.stepfunctions import BOTOCORE_STEPFUNCTIONS_INPUT_KEY
 from ddtrace.constants import ANALYTICS_SAMPLE_RATE_KEY
 from ddtrace.constants import SPAN_KIND
 from ddtrace.constants import SPAN_MEASURED_KEY
@@ -38,6 +39,9 @@ from ddtrace.internal.utils import http as http_utils
 from ddtrace.propagation.http import HTTPPropagator
 
 log = get_logger(__name__)
+
+
+BOTOCORE_STEPFUNCTIONS_INPUT_KEY = "botocore_stepfunctions_input"
 
 
 class _TracedIterable(wrapt.ObjectProxy):
