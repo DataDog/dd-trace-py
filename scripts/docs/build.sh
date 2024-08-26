70d07431775348d21df86d01558523eff057c729
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-08-26 15:45:49 -0400

    can build docs on mac too

--- scripts/docs/build.sh
+++ scripts/docs/build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -eux
 
+if ! (pip list --editable | grep -q ddtrace); then
+  CMAKE_BUILD_PARALLEL_LEVEL=$(nproc) pip install -v -e .
+fi
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  export PYENCHANT_LIBRARY_PATH=/opt/homebrew/lib/libenchant-2.dylib
+fi
+
 reno lint
 sphinx-build -vvv -W -b spelling docs docs/_build/html
 sphinx-build -vvv -W -b html docs docs/_build/html
