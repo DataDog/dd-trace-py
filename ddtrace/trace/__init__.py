f35bc2c977d078c3430b5fba4952c04cea1b0593
Munir Abdinur <munir.abdinur@datadoghq.com>
2024-08-27 14:44:03 -0700

    Update __init__.py

--- ddtrace/trace/__init__.py
+++ ddtrace/trace/__init__.py
@@ -1,7 +1,16 @@
+from ddtrace import tracer # init the global tracer in this module 
 from ddtrace._trace.context import Context
+from ddtrace._trace.span import Span
+from ddtrace._trace.tracer import Tracer
+from ddtrace._trace.tracer import Tracer
+from ddtrace.pin import Pin # move pin to ddtrace._trace
 
 
 # TODO: Move `ddtrace.Pin`, `ddtrace.Tracer`, `ddtrace.Span`, and `ddtrace.tracer` to this module
 __all__ = [
     "Context",
+    "Pin",
+    "Tracer",
+    "tracer",
+    "Span",
 ]
