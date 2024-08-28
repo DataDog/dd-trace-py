c4a03688d031ce4589d154e1120efbcd7dcb568d
Romain Komorn <romain.komorn@datadoghq.com>
2024-08-28 13:37:10 +0100

    rename ci_visilbity -> test_visibility

--- ddtrace/ext/test_visibility/_utils.py
+++ ddtrace/ext/test_visibility/_utils.py
@@ -34,39 +34,39 @@ def _catch_and_log_exceptions(func):
 def _get_item_tag(item_id: TestVisibilityItemId, tag_name: str) -> Any:
     log.debug("Getting tag for item %s: %s", item_id, tag_name)
     tag_value = core.dispatch_with_results(
-        "ci_visibility.item.get_tag", (_TestVisibilityAPIBase.GetTagArgs(item_id, tag_name),)
+        "test_visibility.item.get_tag", (_TestVisibilityAPIBase.GetTagArgs(item_id, tag_name),)
     ).tag_value.value
     return tag_value
 
 
 def _set_item_tag(item_id: TestVisibilityItemId, tag_name: str, tag_value: Any, recurse: bool = False):
     log.debug("Setting tag for item %s: %s=%s", item_id, tag_name, tag_value)
-    core.dispatch("ci_visibility.item.set_tag", (_TestVisibilityAPIBase.SetTagArgs(item_id, tag_name, tag_value),))
+    core.dispatch("test_visibility.item.set_tag", (_TestVisibilityAPIBase.SetTagArgs(item_id, tag_name, tag_value),))
 
 
 def _set_item_tags(item_id: TestVisibilityItemId, tags: Dict[str, Any], recurse: bool = False):
     log.debug("Setting tags for item %s: %s", item_id, tags)
-    core.dispatch("ci_visibility.item.set_tags", (_TestVisibilityAPIBase.SetTagsArgs(item_id, tags),))
+    core.dispatch("test_visibility.item.set_tags", (_TestVisibilityAPIBase.SetTagsArgs(item_id, tags),))
 
 
 def _delete_item_tag(item_id: TestVisibilityItemId, tag_name: str, recurse: bool = False):
     log.debug("Deleting tag for item %s: %s", item_id, tag_name)
-    core.dispatch("ci_visibility.item.delete_tag", (_TestVisibilityAPIBase.DeleteTagArgs(item_id, tag_name),))
+    core.dispatch("test_visibility.item.delete_tag", (_TestVisibilityAPIBase.DeleteTagArgs(item_id, tag_name),))
 
 
 def _delete_item_tags(item_id: TestVisibilityItemId, tag_names: List[str], recurse: bool = False):
     log.debug("Deleting tags for item %s: %s", item_id, tag_names)
-    core.dispatch("ci_visibility.item.delete_tags", (_TestVisibilityAPIBase.DeleteTagsArgs(item_id, tag_names),))
+    core.dispatch("test_visibility.item.delete_tags", (_TestVisibilityAPIBase.DeleteTagsArgs(item_id, tag_names),))
 
 
 def _get_item_span(item_id: TestVisibilityItemId) -> Span:
     log.debug("Getting span for item %s", item_id)
-    span: Span = core.dispatch_with_results("ci_visibility.item.get_span", (item_id,)).span.value
+    span: Span = core.dispatch_with_results("test_visibility.item.get_span", (item_id,)).span.value
     return span
 
 
 def _is_item_finished(item_id: TestVisibilityItemId) -> bool:
     log.debug("Checking if item %s is finished", item_id)
-    _is_finished = bool(core.dispatch_with_results("ci_visibility.item.is_finished", (item_id,)).is_finished.value)
+    _is_finished = bool(core.dispatch_with_results("test_visibility.item.is_finished", (item_id,)).is_finished.value)
     log.debug("Item %s is finished: %s", item_id, _is_finished)
     return _is_finished
