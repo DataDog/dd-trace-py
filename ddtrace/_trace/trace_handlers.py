DataDog/dd-trace-py

commit 3480c5f07ac8f241fe2b7231c520f976fb59d7bb

chore(botocore): refactor kinesis module (#8937)
This change refactors the kinesis module to use the core API instead of
calling start_span directly in at least one instance.

## Checklist

- [x] Change(s) are motivated and described in the PR description
- [x] Testing strategy is described if automated tests are not included
in the PR
- [x] Risks are described (performance impact, potential for breakage,
maintainability)
- [x] Change is maintainable (easy to change, telemetry, documentation)
- [x] [Library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
are followed or label `changelog/no-changelog` is set
- [x] Documentation is included (in-code, generated user docs, [public
corp docs](https://github.com/DataDog/documentation/))
- [x] Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
- [x] If this PR changes the public interface, I've notified
`@DataDog/apm-tees`.

## Reviewer Checklist

- [x] Title is accurate
- [x] All changes are related to the pull request's stated goal
- [x] Description motivates each change
- [x] Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes
- [x] Testing strategy adequately addresses listed risks
- [x] Change is maintainable (easy to change, telemetry, documentation)
- [x] Release note makes sense to a user of the library
- [x] Author has acknowledged and discussed the performance implications
of this PR as reported in the benchmarks PR comment
- [x] Backport labels are set in a manner that is consistent with the
[release branch maintenance
policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

ddtrace/_trace/trace_handlers.py
@@ -88,7 +88,7 @@ class _TracedIterable(wrapt.ObjectProxy):
 
 def _get_parameters_for_new_span_directly_from_context(ctx: core.ExecutionContext) -> Dict[str, str]:
     span_kwargs = {}
-    for parameter_name in {"span_type", "resource", "service"}:
+    for parameter_name in {"span_type", "resource", "service", "child_of", "activate"}:
         parameter_value = ctx.get_item(parameter_name, traverse=False)
         if parameter_value:
             span_kwargs[parameter_name] = parameter_value
@@ -97,6 +97,7 @@ def _get_parameters_for_new_span_directly_from_context(ctx: core.ExecutionContex
 
 def _start_span(ctx: core.ExecutionContext, call_trace: bool = True, **kwargs) -> Span:
     span_kwargs = _get_parameters_for_new_span_directly_from_context(ctx)
+    call_trace = ctx.get_item("call_trace", call_trace)
     tracer = (ctx.get_item("middleware") or ctx["pin"]).tracer
     distributed_headers_config = ctx.get_item("distributed_headers_config")
     if distributed_headers_config:
@@ -545,14 +546,21 @@ def _on_django_after_request_headers_post(
 
 def _on_botocore_patched_api_call_started(ctx):
     callback = ctx.get_item("context_started_callback")
+    span = ctx.get_item(ctx.get_item("call_key"))
     callback(
-        ctx.get_item(ctx.get_item("call_key")),
+        span,
         ctx.get_item("instance"),
         ctx.get_item("args"),
         ctx.get_item("params"),
         ctx.get_item("endpoint_name"),
         ctx.get_item("operation"),
     )
+
+    # we need this since we may have ran the wrapped operation before starting the span
+    # we need to ensure the span start time is correct
+    start_ns = ctx.get_item("start_ns")
+    if start_ns is not None and ctx.get_item("func_run"):
+        span.start_ns = start_ns
 
 
 def _on_botocore_patched_api_call_exception(ctx, response, exception_type, set_response_metadata_tags):
@@ -568,8 +576,7 @@ def _on_botocore_patched_api_call_exception(ctx, response, exception_type, set_r
 
 
 def _on_botocore_patched_api_call_success(ctx, response, set_response_metadata_tags):
-    span = ctx.get_item("instrumented_api_call")
-    set_response_metadata_tags(span, response)
+    set_response_metadata_tags(ctx.get_item(ctx.get_item("call_key")), response)
 
 
 def _on_botocore_trace_context_injection_prepared(
@@ -617,8 +624,11 @@ def listen():
     core.on("django.after_request_headers.post", _on_django_after_request_headers_post)
     core.on("botocore.patched_api_call.exception", _on_botocore_patched_api_call_exception)
     core.on("botocore.patched_api_call.success", _on_botocore_patched_api_call_success)
+    core.on("botocore.patched_kinesis_api_call.success", _on_botocore_patched_api_call_success)
+    core.on("botocore.patched_kinesis_api_call.exception", _on_botocore_patched_api_call_exception)
     core.on("botocore.prep_context_injection.post", _on_botocore_trace_context_injection_prepared)
     core.on("botocore.patched_api_call.started", _on_botocore_patched_api_call_started)
+    core.on("botocore.patched_kinesis_api_call.started", _on_botocore_patched_api_call_started)
 
     for context_name in (
         "flask.call",
@@ -632,6 +642,7 @@ def listen():
         "django.func.wrapped",
         "botocore.instrumented_api_call",
         "botocore.instrumented_lib_function",
+        "botocore.patched_kinesis_api_call",
     ):
         core.on(f"context.started.start_span.{context_name}", _start_span)
 

ddtrace/contrib/botocore/services/stepfunctions.py
@@ -55,9 +55,6 @@ def inject_trace_to_stepfunction_input(params, span):
         if isinstance(input_obj, dict):
             input_obj["_datadog"] = {}
             HTTPPropagator.inject(span.context, input_obj["_datadog"])
-            input_json = json.dumps(input_obj)
-
-            params["input"] = input_json
             return
         else:
             log.warning("Unable to inject context. The StepFunction input was not a dict.")

ddtrace/contrib/botocore/services/kinesis.py
@@ -14,7 +14,6 @@ from ddtrace.internal import core
 from ddtrace.internal.schema.span_attribute_schema import SpanDirection
 
 from ....ext import SpanTypes
-from ....ext import http
 from ....internal.compat import time_ns
 from ....internal.logger import get_logger
 from ....internal.schema import schematize_cloud_messaging_operation
@@ -149,29 +148,35 @@ def patched_kinesis_api_call(original_func, instance, args, kwargs, function_var
         - func_run_err: There was an error when calling the `getRecords` function
     """
     if (func_run and message_received) or config.botocore.empty_poll_enabled or not func_run or func_run_err:
-        with pin.tracer.start_span(
-            trace_operation,
+        with core.context_with_data(
+            "botocore.patched_kinesis_api_call",
+            instance=instance,
+            args=args,
+            params=params,
+            endpoint_name=endpoint_name,
+            operation=operation,
             service=schematize_service_name("{}.{}".format(pin.service, endpoint_name)),
+            call_trace=False,
+            context_started_callback=set_patched_api_call_span_tags,
+            pin=pin,
+            span_name=trace_operation,
             span_type=SpanTypes.HTTP,
             child_of=child_of if child_of is not None else pin.tracer.context_provider.active(),
             activate=True,
-        ) as span:
-            set_patched_api_call_span_tags(span, instance, args, params, endpoint_name, operation)
+            func_run=func_run,
+            start_ns=start_ns,
+            call_key="patched_kinesis_api_call",
+        ) as ctx, ctx.get_item(ctx.get_item("call_key")) as span:
+            core.dispatch("botocore.patched_kinesis_api_call.started", [ctx])
 
-            # we need this since we may have ran the wrapped operation before starting the span
-            # we need to ensure the span start time is correct
-            if start_ns is not None and func_run:
-                span.start_ns = start_ns
-
-            if config.botocore["distributed_tracing"]:
+            if config.botocore["distributed_tracing"] or config._data_streams_enabled:
                 try_inject_DD_context(
-                    endpoint_name, operation, params, span, trace_operation, inject_trace_context=True
-                )
-
-            # we still want to inject DSM context (if DSM is enabled) even if distributed tracing is disabled
-            elif not config.botocore["distributed_tracing"] and config._data_streams_enabled:
-                try_inject_DD_context(
-                    endpoint_name, operation, params, span, trace_operation, inject_trace_context=False
+                    endpoint_name,
+                    operation,
+                    params,
+                    span,
+                    trace_operation,
+                    inject_trace_context=bool(config.botocore["distributed_tracing"]),
                 )
 
             try:
@@ -184,18 +189,14 @@ def patched_kinesis_api_call(original_func, instance, args, kwargs, function_var
                 if func_run_err:
                     raise func_run_err
 
-                set_response_metadata_tags(span, result)
+                core.dispatch("botocore.patched_kinesis_api_call.success", [ctx, result, set_response_metadata_tags])
                 return result
 
             except botocore.exceptions.ClientError as e:
-                # `ClientError.response` contains the result, so we can still grab response metadata
-                set_response_metadata_tags(span, e.response)
-
-                # If we have a status code, and the status code is not an error,
-                #   then ignore the exception being raised
-                status_code = span.get_tag(http.STATUS_CODE)
-                if status_code and not config.botocore.operations[span.resource].is_error_code(int(status_code)):
-                    span._ignore_exception(botocore.exceptions.ClientError)
+                core.dispatch(
+                    "botocore.patched_kinesis_api_call.exception",
+                    [ctx, e.response, botocore.exceptions.ClientError, set_response_metadata_tags],
+                )
                 raise
     # return results in the case that we ran the function, but no records were returned and empty
     # poll spans are disabled

commit c2822c511c120573f98ac32457effa1f3b3768c8

fix(botocore): inject trace context into stepfunction start_execution input (#10262)
In #7514 , I added a feature to patch start_execution calls to AWS
stepfunctions and inject trace context into the input object. That
feature suffered a regression in #8937 , and the whole stepfunctions
integration was refactored to use the Core API in #9005 . This PR
re-adds the injection of trace context into the start_execution input,
and adds a few unit tests to detect the same regression in the future.


## Checklist
- [x] PR author has checked that all the criteria below are met
- The PR description includes an overview of the change
- The PR description articulates the motivation for the change
- The change includes tests OR the PR description describes a testing
strategy
- The PR description notes risks associated with the change, if any
- Newly-added code is easy to change
- The change follows the [library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
- The change includes or references documentation updates if necessary
- Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))

## Reviewer Checklist
- [x] Reviewer has checked that all the criteria below are met 
- Title is accurate
- All changes are related to the pull request's stated goal
- Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes
- Testing strategy adequately addresses listed risks
- Newly-added code is easy to change
- Release note makes sense to a user of the library
- If necessary, author has acknowledged and discussed the performance
implications of this PR as reported in the benchmarks PR comment
- Backport labels are set in a manner that is consistent with the
[release branch maintenance
policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

ddtrace/_trace/trace_handlers.py
@@ -17,6 +17,7 @@ from ddtrace.constants import ANALYTICS_SAMPLE_RATE_KEY
 from ddtrace.constants import SPAN_KIND
 from ddtrace.constants import SPAN_MEASURED_KEY
 from ddtrace.contrib import trace_utils
+from ddtrace.contrib.internal.botocore.constants import BOTOCORE_STEPFUNCTIONS_INPUT_KEY
 from ddtrace.contrib.trace_utils import _get_request_header_user_agent
 from ddtrace.contrib.trace_utils import _set_url_tag
 from ddtrace.ext import SpanKind
@@ -651,6 +652,12 @@ def _on_botocore_update_messages(ctx, span, _, trace_data, __, message=None):
     HTTPPropagator.inject(context, trace_data)
 
 
+def _on_botocore_patched_stepfunctions_update_input(ctx, span, _, trace_data, __):
+    context = span.context if span else ctx[ctx["call_key"]].context
+    HTTPPropagator.inject(context, trace_data["_datadog"])
+    ctx.set_item(BOTOCORE_STEPFUNCTIONS_INPUT_KEY, trace_data)
+
+
 def _on_botocore_patched_bedrock_api_call_started(ctx, request_params):
     span = ctx[ctx["call_key"]]
     integration = ctx["bedrock_integration"]
@@ -790,7 +797,7 @@ def listen():
     core.on("botocore.sqs_sns.update_messages", _on_botocore_update_messages)
     core.on("botocore.patched_stepfunctions_api_call.started", _on_botocore_patched_api_call_started)
     core.on("botocore.patched_stepfunctions_api_call.exception", _on_botocore_patched_api_call_exception)
-    core.on("botocore.stepfunctions.update_messages", _on_botocore_update_messages)
+    core.on("botocore.stepfunctions.update_input", _on_botocore_patched_stepfunctions_update_input)
     core.on("botocore.eventbridge.update_messages", _on_botocore_update_messages)
     core.on("botocore.client_context.update_messages", _on_botocore_update_messages)
     core.on("botocore.patched_bedrock_api_call.started", _on_botocore_patched_bedrock_api_call_started)

ddtrace/contrib/internal/botocore/constants.py
@@ -1,0 +1,1 @@
+BOTOCORE_STEPFUNCTIONS_INPUT_KEY = "botocore_stepfunctions_input"

ddtrace/contrib/internal/botocore/services/stepfunctions.py
@@ -5,6 +5,7 @@ from typing import Dict
 import botocore.exceptions
 
 from ddtrace import config
+from ddtrace.contrib.internal.botocore.constants import BOTOCORE_STEPFUNCTIONS_INPUT_KEY
 from ddtrace.contrib.trace_utils import ext_service
 from ddtrace.ext import SpanTypes
 from ddtrace.internal import core
@@ -34,8 +35,11 @@ def update_stepfunction_input(ctx: core.ExecutionContext, params: Any) -> None:
         return
 
     input_obj["_datadog"] = {}
-
-    core.dispatch("botocore.stepfunctions.update_input", [ctx, None, None, input_obj["_datadog"], None])
+    core.dispatch("botocore.stepfunctions.update_input", [ctx, None, None, input_obj, None])
+    updated_input_obj = ctx.get_item(BOTOCORE_STEPFUNCTIONS_INPUT_KEY)
+    if updated_input_obj:
+        input_json_str = json.dumps(updated_input_obj)
+        params["input"] = input_json_str
 
 
 def patched_stepfunction_api_call(original_func, instance, args, kwargs: Dict, function_vars: Dict):

releasenotes/notes/fix-botocore-stepfunction-patching-9993630697956278.yaml
@@ -1,0 +1,4 @@
+fixes:
+  - |
+    botocore: This fix resolves a regression where trace context was not being injected into the input of Stepfunction
+      start_execution commands. This re-enables distributed tracing when a Python service invokes a properly instrumented Step Function.

tests/contrib/botocore/test.py
@@ -1046,9 +1046,11 @@ class BotocoreTest(TracerTestCase):
             roleArn="arn:aws:iam::012345678901:role/DummyRole",
         )
         Pin(service=self.TEST_SERVICE, tracer=self.tracer).onto(sf)
-        sf.start_execution(
-            stateMachineArn="arn:aws:states:us-west-2:000000000000:stateMachine:lincoln", input='{"baz":1}'
-        )
+        start_execution_dict = {
+            "stateMachineArn": "arn:aws:states:us-west-2:000000000000:stateMachine:lincoln",
+            "input": '{"baz": 1}',
+        }
+        sf.start_execution(**start_execution_dict)
         # I've tried to find a way to make Moto show me the input to the execution, but can't get that to work.
         spans = self.get_spans()
         assert spans

tests/contrib/botocore/test_stepfunctions.py
@@ -1,0 +1,99 @@
+import json
+
+from ddtrace import Pin
+from ddtrace.contrib.internal.botocore.services.stepfunctions import update_stepfunction_input
+from ddtrace.ext import SpanTypes
+from ddtrace.internal import core
+
+
+def test_update_stepfunction_input():
+    params = {
+        "stateMachineArn": "arn:aws:states:us-east-1:425362996713:stateMachine:agocs_inner_state_machine",
+        "input": "{}",
+    }
+    pin = Pin()
+    with core.context_with_data(
+        "botocore.patched_stepfunctions_api_call",
+        span_name="states.command",
+        service="aws.states",
+        span_type=SpanTypes.HTTP,
+        call_key="patched_stepfunctions_api_call",
+        instance=None,
+        args=(
+            "StartExecution",
+            {
+                "stateMachineArn": "arn:aws:states:us-east-1:425362996713:stateMachine:agocs_inner_state_machine",
+                "input": "{}",
+            },
+        ),
+        params=params,
+        endpoint_name="states",
+        operation="StartExecution",
+        pin=pin,
+    ) as ctx:
+        update_stepfunction_input(ctx, params)
+        assert params["input"]
+        input_obj = json.loads(params["input"])
+        assert "_datadog" in input_obj
+        assert "x-datadog-trace-id" in input_obj["_datadog"]
+        assert "x-datadog-parent-id" in input_obj["_datadog"]
+        assert "x-datadog-tags" in input_obj["_datadog"]
+        assert "_dd.p.tid" in input_obj["_datadog"]["x-datadog-tags"]
+
+
+def test_update_stepfunction_input_does_not_mutate_non_dict_input():
+    params = {
+        "stateMachineArn": "arn:aws:states:us-east-1:425362996713:stateMachine:agocs_inner_state_machine",
+        "input": "hello",
+    }
+    pin = Pin()
+    with core.context_with_data(
+        "botocore.patched_stepfunctions_api_call",
+        span_name="states.command",
+        service="aws.states",
+        span_type=SpanTypes.HTTP,
+        call_key="patched_stepfunctions_api_call",
+        instance=None,
+        args=(
+            "StartExecution",
+            {
+                "stateMachineArn": "arn:aws:states:us-east-1:425362996713:stateMachine:agocs_inner_state_machine",
+                "input": "hello",
+            },
+        ),
+        params=params,
+        endpoint_name="states",
+        operation="StartExecution",
+        pin=pin,
+    ) as ctx:
+        update_stepfunction_input(ctx, params)
+        assert params["input"]
+        assert params["input"] == "hello"
+
+    params = {
+        "stateMachineArn": "arn:aws:states:us-east-1:425362996713:stateMachine:agocs_inner_state_machine",
+        "input": "[1, 2, 3]",
+    }
+    pin = Pin()
+    with core.context_with_data(
+        "botocore.patched_stepfunctions_api_call",
+        span_name="states.command",
+        service="aws.states",
+        span_type=SpanTypes.HTTP,
+        call_key="patched_stepfunctions_api_call",
+        instance=None,
+        args=(
+            "StartExecution",
+            {
+                "stateMachineArn": "arn:aws:states:us-east-1:425362996713:stateMachine:agocs_inner_state_machine",
+                "input": "[1, 2, 3]",
+            },
+        ),
+        params=params,
+        endpoint_name="states",
+        operation="StartExecution",
+        pin=pin,
+    ) as ctx:
+        update_stepfunction_input(ctx, params)
+        assert params["input"]
+        assert params["input"] == "[1, 2, 3]"