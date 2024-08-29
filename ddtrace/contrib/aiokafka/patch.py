fe956e5f86aa7198bf27c07b2cdda789f3c94168
Pieter Van Isacker <pieter.vanisacker@lightspeedhq.com>
2024-08-29 12:43:04 +0200

    fix: make distributed tracing work

--- ddtrace/contrib/aiokafka/patch.py
+++ ddtrace/contrib/aiokafka/patch.py
@@ -75,22 +75,19 @@ def bootstrap_servers(instance):
         return ",".join(client._bootstrap_servers)
 
 
-def create_send_span_tags(instance, args, kwargs):
-    topic = get_argument_value(args, kwargs, 0, "topic")
+def create_send_span_tags(topic, partition, key, value, bootstrap_servers):
     tags = {
         COMPONENT: config.aiokafka.integration_name,
         SPAN_KIND: SpanKind.PRODUCER,
         TOPIC: topic,
-        HOST_LIST: bootstrap_servers(instance),
+        HOST_LIST: bootstrap_servers,
         MESSAGING_SYSTEM: SERVICE,
     }
-    partition = get_argument_value(args, kwargs, 3, "partition", True) or None
     if partition:
         tags[PARTITION] = partition
-    key = get_argument_value(args, kwargs, 2, "key", True) or None
     if key:
         tags[MESSAGE_KEY] = key.decode("utf-8")
-    value = get_argument_value(args, kwargs, 1, "value", True) or None
+
     tags[TOMBSTONE] = str(value is None).lower()
     return tags
 
@@ -100,6 +97,13 @@ async def traced_send_and_wait(func, instance, args, kwargs):
     if not pin or not pin.enabled():
         return await func(*args, **kwargs)
 
+    topic = get_argument_value(args, kwargs, 0, "topic")
+    value = get_argument_value(args, kwargs, 1, "value", True) or None
+    headers = get_argument_value(args, kwargs, 6, "headers", True) or []
+    partition = get_argument_value(args, kwargs, 3, "partition", True) or None
+    key = get_argument_value(args, kwargs, 2, "key", True) or None
+    servers = bootstrap_servers(instance)
+
     with core.context_with_data(
         "aiokafka.send_and_wait",
         parent=None,
@@ -107,12 +111,11 @@ async def traced_send_and_wait(func, instance, args, kwargs):
         span_type=SpanTypes.WORKER,
         call_key="instrumented_send_and_wait",
         service=trace_utils.ext_service(pin, config.aiokafka),
-        tags=create_send_span_tags(instance, args, kwargs),
+        tags=create_send_span_tags(topic, partition, key, value, servers),
         pin=pin,
     ) as ctx, ctx[ctx["call_key"]] as span:
         if config.aiokafka.distributed_tracing_enabled:
             # inject headers with Datadog tags:
-            headers = get_argument_value(args, kwargs, 6, "headers", True) or []
             tracing_headers = {}
             HTTPPropagator.inject(span.context, tracing_headers)
             headers = merge_headers_to_aiokafka(tracing_headers, headers)
@@ -125,6 +128,13 @@ def merge_headers_to_aiokafka(http_headers, aiokafka_headers):
     for key, value in http_headers.items():
         aiokafka_headers.append((key, value.encode("utf-8")))
     return aiokafka_headers
+
+
+def convert_headers_for_distributed_tracing(aiokafka_headers):
+    dd_headers = {}
+    for header in aiokafka_headers:
+        dd_headers[header[0]] = header[1].decode("utf-8")
+    return dd_headers
 
 
 def create_get_span_tags(instance, args, kwargs):
@@ -143,23 +153,27 @@ async def traced_getone(func, instance, args, kwargs):
     if not pin or not pin.enabled():
         return await func(*args, **kwargs)
 
-    with core.context_with_data(
-        "aiokafka.getone",
-        parent=None,
-        span_name=schematize_messaging_operation(CONSUME, provider="kafka", direction=SpanDirection.INBOUND),
-        span_type=SpanTypes.WORKER,
-        service=trace_utils.ext_service(pin, config.aiokafka),
-        call_key="instrumented_getone",
-        tags=create_get_span_tags(instance, args, kwargs),
-        pin=pin,
-    ) as ctx, ctx[ctx["call_key"]]:
-        err = None
-        result = None
-        try:
-            result = await func(*args, **kwargs)
-        except Exception as e:
-            err = e
-            raise err
-        finally:
+    err = None
+    result = None
+    child_of = None
+    try:
+        result = await func(*args, **kwargs)
+        if config.aiokafka.distributed_tracing_enabled and result.headers:
+            child_of = HTTPPropagator.extract(convert_headers_for_distributed_tracing(result.headers))
+    except Exception as e:
+        err = e
+        raise err
+    finally:
+        with core.context_with_data(
+            "aiokafka.getone",
+            call_trace=False,
+            span_name=schematize_messaging_operation(CONSUME, provider="kafka", direction=SpanDirection.INBOUND),
+            span_type=SpanTypes.WORKER,
+            service=trace_utils.ext_service(pin, config.aiokafka),
+            call_key="instrumented_getone",
+            distributed_context=child_of,
+            tags=create_get_span_tags(instance, args, kwargs),
+            pin=pin,
+        ) as ctx, ctx[ctx["call_key"]]:
             core.dispatch("aiokafka.getone.message", (ctx, result, err))
         return result
