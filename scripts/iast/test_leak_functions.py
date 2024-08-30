bc219e47e4ea77ef5aac6c7b950f8b73fdd3cded
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-08-30 18:47:52 +0200

    Add new IAST memory leak test
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- scripts/iast/test_leak_functions.py
+++ scripts/iast/test_leak_functions.py
@@ -1,46 +1,46 @@
-import ddtrace.auto  # noqa: F401  # isort: skip
+import argparse
 import resource
 import sys
 
-from mod_leak_functions import test_doit
-from ddtrace.appsec._iast._taint_tracking import create_context, is_pyobject_tainted, reset_context
-import argparse
+from tests.appsec.iast.aspects.conftest import _iast_patched_module
+from tests.utils import override_env
+
+
+with override_env({"DD_IAST_ENABLED": "True"}):
+    from ddtrace.appsec._iast._taint_tracking import create_context
+    from ddtrace.appsec._iast._taint_tracking import is_pyobject_tainted
+    from ddtrace.appsec._iast._taint_tracking import reset_context
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Memory leak test script.")
-    parser.add_argument("--mode", choices=["ci", "console"], default="console", help="Mode of operation.")
     parser.add_argument("--iterations", type=int, default=100000, help="Number of iterations.")
     parser.add_argument(
-        "--failPercent", type=float, default=2.0, help="Failure threshold for memory increase percentage."
+        "--fail_percent", type=float, default=2.0, help="Failure threshold for memory increase percentage."
     )
-    parser.add_argument("--printEvery", type=int, help="Print status every N iterations.")
-    parser.add_argument(
-        "--graph", type=lambda x: (str(x).lower() == "true"), default=True, help="Enable ASCII graph output."
-    )
-
-    args = parser.parse_args()
-
-    # Set default for printEvery if not provided
-    if args.printEvery is None:
-        args.printEvery = 1000 if args.mode == "ci" else 250
-
-    return args
+    parser.add_argument("--print_every", type=int, default=250, help="Print status every N iterations.")
+    return parser.parse_args()
 
 
-def test_iast_leaks():
-    args = parse_arguments()
-
+def test_iast_leaks(iterations: int, fail_percent: float, print_every: int):
+    if iterations < 100000:
+        print(
+            "Warning: running with %d iterations. At least 100.000 are recommended to stabilize the RSS info"
+            % iterations
+        )
     try:
-        rss_list = []
-        half_iterations = args.iterations // 2
-        print("Test %d iterations" % args.iterations)
+        half_iterations = iterations // 2
+        print("Test %d iterations" % iterations)
         current_rss = 0
         half_rss = 0
 
-        for i in range(args.iterations):
+        mod = _iast_patched_module("scripts.iast.mod_leak_functions")
+        test_doit = mod.test_doit
+
+        for i in range(iterations):
             create_context()
             result = test_doit()  # noqa: F841
+            assert result == "notainted_HIROOT1234-HIROOT123_notainted"  # noqa: F841
             assert is_pyobject_tainted(result)
             reset_context()
 
@@ -48,29 +48,25 @@ def test_iast_leaks():
                 half_rss = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024
 
             current_rss = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024
-            if args.graph:
-                rss_list.append(int(current_rss))
 
-            if i % args.printEvery == 0:
+            if i % print_every == 0:
                 print(f"Round {i} Max RSS: {current_rss}")
 
         final_rss = current_rss
 
-        print(f"Round {args.iterations} Max RSS: {final_rss}")
-
-        if args.graph:
-            # TODO: write an ascii graph
-            pass
+        print(f"Round {iterations} Max RSS: {final_rss}")
 
         percent_increase = ((final_rss - half_rss) / half_rss) * 100
-        if percent_increase > args.failPercent:
+        if percent_increase > fail_percent:
             print(
-                f"Failed: memory increase from half-point ({half_iterations} iterations) is {percent_increase:.2f}% which is greater than {args.failPercent}%"
+                f"Failed: memory increase from half-point ({half_iterations} iterations) is "
+                "{percent_increase:.2f}% which is greater than {fail_percent}%"
             )
             return 1
         else:
             print(
-                f"Success: memory increase is {percent_increase:.2f}% from half-point ({half_iterations} iterations) which is less than {args.failPercent}%"
+                f"Success: memory increase is {percent_increase:.2f}% from half-point ({half_iterations} "
+                "iterations) which is less than {fail_percent}%"
             )
             return 0
 
@@ -79,4 +75,6 @@ def test_iast_leaks():
 
 
 if __name__ == "__main__":
-    sys.exit(test_iast_leaks())
+    args = parse_arguments()
+    with override_env({"DD_IAST_ENABLED": "True"}):
+        sys.exit(test_iast_leaks(args.iterations, args.fail_percent, args.print_every))
