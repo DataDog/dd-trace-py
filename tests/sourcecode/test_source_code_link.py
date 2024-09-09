6fed1ee33172ecd50d9aeceef127e610e1bef802
Emmett Butler <emmett.butler321@gmail.com>
2024-09-09 13:11:28 -0700

    fix expectation

--- tests/sourcecode/test_source_code_link.py
+++ tests/sourcecode/test_source_code_link.py
@@ -7,5 +7,7 @@ def test_get_source_code_link():
     source_code_link = get_source_code_link()
     reporsitory_url, commit_hash = source_code_link.split("#")
 
-    assert reporsitory_url == "https://github.com/DataDog/dd-trace-py"
+    # can be github.com or gitlab.ddbuild.io
+    assert reporsitory_url.startswith("https://git")
+    assert reporsitory_url.endswith("/DataDog/dd-trace-py")
     assert re.match(r"\b[a-f0-9]{5,40}\b", commit_hash) is not None
