206bfb75e833250e4450dc016169333a958d2fa0
brettlangdon <me@brett.is>
2024-09-09 13:45:03 -0400

    fix conflicts

--- tests/contrib/config.py
+++ tests/contrib/config.py
@@ -55,7 +55,7 @@ MARIADB_CONFIG = {
 }
 
 REDIS_CONFIG = {
-    "host": os.getenv("TEST_REDIS_HOST", "127.0.0.1"),
+    "host": os.getenv("TEST_REDIS_HOST", "localhost"),
     "port": int(os.getenv("TEST_REDIS_PORT", 6379)),
 }
 
