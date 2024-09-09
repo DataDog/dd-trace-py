4344a6ef91b5f6b2ecff77479d023134479a3e0a
Yun Kim <yun.kim@datadoghq.com>
2024-09-09 17:06:14 -0400

    Docs

--- ddtrace/contrib/google_generativeai/__init__.py
+++ ddtrace/contrib/google_generativeai/__init__.py
@@ -1,42 +1,39 @@
 """
-The Anthropic integration instruments the Anthropic Python library to traces for requests made to the models for messages.
+The Gemini integration instruments the Google Gemini Python API to traces for requests made to Google models.
 
-All traces submitted from the Anthropic integration are tagged by:
+All traces submitted from the Gemini integration are tagged by:
 
 - ``service``, ``env``, ``version``: see the `Unified Service Tagging docs <https://docs.datadoghq.com/getting_started/tagging/unified_service_tagging>`_.
-- ``anthropic.request.model``: Anthropic model used in the request.
-- ``anthropic.request.api_key``: Anthropic API key used to make the request (obfuscated to match the Anthropic UI representation ``sk-...XXXX`` where ``XXXX`` is the last 4 digits of the key).
-- ``anthropic.request.parameters``: Parameters used in anthropic package call.
+- ``genai.request.model``: Google model used in the request.
+- ``genai.request.api_key``: Google Gemini API key used to make the request (obfuscated to match the Google AI Studio UI representation ``...XXXX`` where ``XXXX`` is the last 4 digits of the key).
 
 
 (beta) Prompt and Completion Sampling
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Prompt texts and completion content for the ``Messages.create`` endpoint are collected in span tags with a default sampling rate of ``1.0``.
+Prompt texts and completion content for the ``generateContent`` endpoint are collected in span tags with a default sampling rate of ``1.0``.
 These tags will have truncation applied if the text exceeds the configured character limit.
 
 
 Enabling
 ~~~~~~~~
 
-The Anthropic integration is enabled automatically when you use
+The Gemini integration is enabled automatically when you use
 :ref:`ddtrace-run<ddtracerun>` or :ref:`import ddtrace.auto<ddtraceauto>`.
 
-Note that these commands also enable the ``httpx`` integration which traces HTTP requests from the Anthropic library.
-
-Alternatively, use :func:`patch() <ddtrace.patch>` to manually enable the Anthropic integration::
+Alternatively, use :func:`patch() <ddtrace.patch>` to manually enable the Gemini integration::
 
     from ddtrace import config, patch
 
-    patch(anthropic=True)
+    patch(google_generativeai=True)
 
 
 Global Configuration
 ~~~~~~~~~~~~~~~~~~~~
 
-.. py:data:: ddtrace.config.anthropic["service"]
+.. py:data:: ddtrace.config.google_generativeai["service"]
 
-   The service name reported by default for Anthropic requests.
+   The service name reported by default for Gemini requests.
 
    Alternatively, you can set this option with the ``DD_SERVICE`` or ``DD_ANTHROPIC_SERVICE`` environment
    variables.
@@ -44,26 +41,26 @@ Global Configuration
    Default: ``DD_SERVICE``
 
 
-.. py:data:: (beta) ddtrace.config.anthropic["span_char_limit"]
+.. py:data:: (beta) ddtrace.config.google_generativeai["span_char_limit"]
 
    Configure the maximum number of characters for the following data within span tags:
 
-   - Message inputs and completions
+   - Text inputs and completions
 
    Text exceeding the maximum number of characters is truncated to the character limit
    and has ``...`` appended to the end.
 
-   Alternatively, you can set this option with the ``DD_ANTHROPIC_SPAN_CHAR_LIMIT`` environment
+   Alternatively, you can set this option with the ``DD_GOOGLE_GENERATIVEAI_SPAN_CHAR_LIMIT`` environment
    variable.
 
    Default: ``128``
 
 
-.. py:data:: (beta) ddtrace.config.anthropic["span_prompt_completion_sample_rate"]
+.. py:data:: (beta) ddtrace.config.google_generativeai["span_prompt_completion_sample_rate"]
 
    Configure the sample rate for the collection of prompts and completions as span tags.
 
-   Alternatively, you can set this option with the ``DD_ANTHROPIC_SPAN_PROMPT_COMPLETION_SAMPLE_RATE`` environment
+   Alternatively, you can set this option with the ``DD_GOOGLE_GENERATIVEAI_SPAN_PROMPT_COMPLETION_SAMPLE_RATE`` environment
    variable.
 
    Default: ``1.0``
@@ -72,14 +69,15 @@ Global Configuration
 Instance Configuration
 ~~~~~~~~~~~~~~~~~~~~~~
 
-To configure the Anthropic integration on a per-instance basis use the
+To configure the Gemini integration on a per-instance basis use the
 ``Pin`` API::
 
-    import anthropic
+    import google.generativeai as genai
     from ddtrace import Pin, config
 
-    Pin.override(anthropic, service="my-anthropic-service")
+    Pin.override(genai, service="my-gemini-service")
 """  # noqa: E501
+
 from ...internal.utils.importlib import require_modules
 
 
