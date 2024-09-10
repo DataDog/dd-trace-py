db3e90c9c929118e6315a37f97ab4ac87ba9326e
Federico Mon <federico.mon@datadoghq.com>
2024-09-10 12:38:17 +0200

    Revert "revert-me: force asm standalone system-tests failures"
    This reverts commit eabec6ab098ae0711a4a1faa0a2cc2022e1f3593.

--- ddtrace/_trace/tracer.py
+++ ddtrace/_trace/tracer.py
@@ -296,7 +296,7 @@ class Tracer(object):
         config._subscribe(["_tracing_enabled"], self._on_global_config_update)
 
     def _maybe_opt_out(self):
-        self._apm_opt_out = False and self._asm_enabled and self._appsec_standalone_enabled
+        self._apm_opt_out = self._asm_enabled and self._appsec_standalone_enabled
         if self._apm_opt_out:
             # If ASM is enabled but tracing is disabled,
             # we need to set the rate limiting to 1 trace per minute
