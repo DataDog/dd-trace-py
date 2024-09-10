8994198f7316602b110cb71fed139321351e82d9
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-09-10 15:31:45 +0000

    fix flaky test

--- tests/profiling-v2/collector/test_threading_asyncio.py
+++ tests/profiling-v2/collector/test_threading_asyncio.py
@@ -1,18 +1,27 @@
-import os
-import threading
-
-from ddtrace.internal.datadog.profiling import ddup
-from ddtrace.profiling.collector import threading as collector_threading
-from tests.profiling.collector import _asyncio_compat
-from tests.profiling.collector import pprof_utils
-from tests.profiling.collector.lock_utils import get_lock_linenos
-from tests.profiling.collector.lock_utils import init_linenos
+import pytest
 
 
-init_linenos(__file__)
-
-
+@pytest.mark.subprocess(
+    env=dict(
+        DD_PROFILING_CAPTURE_PCT="100",
+        DD_PROFILING_OUTPUT_PPROF="/tmp/asyncio_lock_acquire_events",
+        DD_PROFILING_FILE_PATH=__file__,
+    ),
+    err=None,
+)
 def test_lock_acquire_events():
+
+    import os
+    import threading
+
+    from ddtrace.profiling import profiler
+    from tests.profiling.collector import _asyncio_compat
+    from tests.profiling.collector import pprof_utils
+    from tests.profiling.collector.lock_utils import get_lock_linenos
+    from tests.profiling.collector.lock_utils import init_linenos
+
+    init_linenos(os.environ["DD_PROFILING_FILE_PATH"])
+
     # Tests that lock acquire/release events from asyncio threads are captured
     # correctly. See test_asyncio.py for asyncio.Lock tests.
     async def _lock():
@@ -26,27 +35,21 @@ def test_lock_acquire_events():
         _asyncio_compat.run(_lock())
         lock.release()  # !RELEASE! test_lock_acquire_events_2
 
-    output_prefix = "/tmp/asyncio_lock_acquire_events"
+    # start a complete profiler so asyncio policy is setup
+    p = profiler.Profiler()
+    p.start()
+    t = threading.Thread(target=asyncio_run, name="foobar")
+    t.start()
+    t.join()
+    p.stop()
 
-    ddup.config(
-        env="test",
-        service="test_lock_acquire_events",
-        version="my_version",
-        output_filename=output_prefix,
-    )
-    ddup.start()
-
-    with collector_threading.ThreadingLockCollector(None, capture_pct=100):
-        t = threading.Thread(target=asyncio_run, name="foobar")
-        t.start()
-        t.join()
-
-    ddup.upload()
+    expected_filename = "test_threading_asyncio.py"
+    output_filename = os.environ["DD_PROFILING_OUTPUT_PPROF"] + "." + str(os.getpid())
 
     linenos_1 = get_lock_linenos("test_lock_acquire_events_1")
     linenos_2 = get_lock_linenos("test_lock_acquire_events_2")
 
-    profile = pprof_utils.parse_profile(output_prefix + "." + str(os.getpid()))
+    profile = pprof_utils.parse_profile(output_filename)
 
     task_name_regex = r"Task-\d+$"
 
@@ -55,32 +58,34 @@ def test_lock_acquire_events():
         expected_acquire_events=[
             pprof_utils.LockAcquireEvent(
                 caller_name="asyncio_run",
-                filename=os.path.basename(__file__),
+                filename=expected_filename,
                 linenos=linenos_2,
                 lock_name="lock",
             ),
             pprof_utils.LockAcquireEvent(
                 caller_name="_lock",
-                filename=os.path.basename(__file__),
+                filename=expected_filename,
                 linenos=linenos_1,
                 lock_name="lock",
                 task_name=task_name_regex,
+                thread_name="foobar",
             ),
         ],
         expected_release_events=[
             pprof_utils.LockReleaseEvent(
                 caller_name="asyncio_run",
-                filename=os.path.basename(__file__),
+                filename=expected_filename,
                 linenos=linenos_2,
                 lock_name="lock",
                 thread_name="foobar",
             ),
             pprof_utils.LockReleaseEvent(
                 caller_name="_lock",
-                filename=os.path.basename(__file__),
+                filename=expected_filename,
                 linenos=linenos_1,
                 lock_name="lock",
                 task_name=task_name_regex,
+                thread_name="foobar",
             ),
         ],
     )
