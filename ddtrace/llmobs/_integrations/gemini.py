eecda470782e6bd5373b1b0c6027d1fcc6894510
Yun Kim <yun.kim@datadoghq.com>
2024-09-11 15:26:06 -0400

    typing

--- ddtrace/llmobs/_integrations/gemini.py
+++ ddtrace/llmobs/_integrations/gemini.py
@@ -12,7 +12,7 @@ class GeminiIntegration(BaseLLMIntegration):
     def _set_base_span_tags(
         self, span: Span, provider: Optional[str] = None, model: Optional[str] = None, **kwargs: Dict[str, Any]
     ) -> None:
-        if provider:
-            span.set_tag_str("google_generativeai.request.model", model)
-        if model:
-            span.set_tag_str("google_generativeai.request.provider", provider)
+        if provider is not None:
+            span.set_tag_str("google_generativeai.request.provider", str(provider))
+        if model is not None:
+            span.set_tag_str("google_generativeai.request.model", str(model))
