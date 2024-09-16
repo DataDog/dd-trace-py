c0ae8770d33043005855086c9cc2bb7a45bcb72e
Sam Brenner <sam.brenner@datadoghq.com>
2024-09-16 13:06:49 -0400

    lint

--- tests/contrib/langchain/test_langchain_community.py
+++ tests/contrib/langchain/test_langchain_community.py
@@ -1397,9 +1397,11 @@ def test_streamed_json_output_parser(langchain, langchain_core, langchain_openai
     parser = langchain_core.output_parsers.JsonOutputParser()
 
     chain = model | parser
-    inp = ('output a list of the country france their population in JSON format. '
+    inp = (
+        "output a list of the country france their population in JSON format. "
         'Use a dict with an outer key of "countries" which contains a list of countries. '
-        'Each country should have the key `name` and `population`')
+        "Each country should have the key `name` and `population`"
+    )
 
     messages = [
         langchain.schema.SystemMessage(content="You know everything about the world."),
