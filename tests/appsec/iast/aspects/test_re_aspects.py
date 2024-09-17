6ff28d2a8a747630e83cd09aa0fecf48a1af578a
Federico Mon <federico.mon@datadoghq.com>
2024-09-17 18:12:33 +0200

    ruff

--- tests/appsec/iast/aspects/test_re_aspects.py
+++ tests/appsec/iast/aspects/test_re_aspects.py
@@ -7,7 +7,6 @@ from ddtrace.appsec._iast._taint_tracking import TaintRange
 from ddtrace.appsec._iast._taint_tracking import get_tainted_ranges
 from ddtrace.appsec._iast._taint_tracking import is_pyobject_tainted
 from ddtrace.appsec._iast._taint_tracking import taint_pyobject
-
 from ddtrace.appsec._iast._taint_tracking.aspects import index_aspect
 from ddtrace.appsec._iast._taint_tracking.aspects import re_expand_aspect
 from ddtrace.appsec._iast._taint_tracking.aspects import re_findall_aspect
