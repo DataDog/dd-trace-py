a4f24b64133f43e195d8408205eaea05e512ef09
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-09-18 03:12:07 +0000

    also print thread name

--- ddtrace/profiling/collector/stack.pyx
+++ ddtrace/profiling/collector/stack.pyx
@@ -453,6 +453,7 @@ class _ThreadSpanLinks(_thread_span_links_base):
         :return: A set with the active spans.
         """
         active_span = self.get_object(thread_id)
+
         if active_span is not None:
             if not active_span.finished:
                 print("Found active_span with span_id: ", active_span.span_id)
@@ -460,7 +461,8 @@ class _ThreadSpanLinks(_thread_span_links_base):
                 print("Found finished_span with span_id: ", active_span.span_id)
             return active_span
         else:
-            print("failed to find active_span with thread_id: ", thread_id)
+            thread_name = _threading.get_thread_name(thread_id)
+            print("failed to find active_span with thread_id: ", thread_id, " and thread_name: ", thread_name)
         return None
 
 
