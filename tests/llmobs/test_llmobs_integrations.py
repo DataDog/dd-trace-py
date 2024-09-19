e3fd4413354ea9fbc5b23b39e2cd7e4a433225de
Yun Kim <yun.kim@datadoghq.com>
2024-09-19 13:03:02 -0400

    Add mock api key

--- tests/llmobs/test_llmobs_integrations.py
+++ tests/llmobs/test_llmobs_integrations.py
@@ -20,6 +20,7 @@ def ddtrace_global_config():
     with mock.patch("ddtrace.llmobs._integrations.base.config") as mock_global_config:
         mock_global_config._llmobs_agentless_enabled = False
         mock_global_config._llmobs_sample_rate = 1.0
+        mock_global_config._dd_api_key = "<not-a-real-key>"
         yield mock_global_config
 
 
