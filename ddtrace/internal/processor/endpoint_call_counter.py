ad3539e127ce371f22b43a5d4cf701de4bb627e1
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-09-19 02:39:07 +0000

    change type

--- ddtrace/internal/processor/endpoint_call_counter.py
+++ ddtrace/internal/processor/endpoint_call_counter.py
@@ -15,7 +15,12 @@ EndpointCountsType = typing.Dict[str, int]
 @dataclass(eq=False)
 class EndpointCallCounterProcessor(SpanProcessor):
     endpoint_counts: EndpointCountsType = field(default_factory=dict, init=False, repr=False, compare=False)
-    span_id_to_endpoint: typing.Dict[int, str] = field(default_factory=dict, init=False, repr=False, compare=False)
+    # Mapping from endpoint to list of span IDs, here we use mapping from
+    # endpoint to span_ids instead of mapping from span_id to endpoint to
+    # avoid creating a new string object for each span id.
+    endpoint_to_span_ids: typing.Dict[str, typing.List[int]] = field(
+        default_factory=dict, init=False, repr=False, compare=False
+    )
     _endpoint_counts_lock: typing.ContextManager = field(
         default_factory=forksafe.Lock, init=False, repr=False, compare=False
     )
@@ -38,12 +43,14 @@ class EndpointCallCounterProcessor(SpanProcessor):
             span_id = span.span_id
             with self._endpoint_counts_lock:
                 self.endpoint_counts[resource] = self.endpoint_counts.get(resource, 0) + 1
-                self.span_id_to_endpoint[span_id] = resource
+                if resource not in self.endpoint_to_span_ids:
+                    self.endpoint_to_span_ids[resource] = []
+                self.endpoint_to_span_ids[resource].append(span_id)
 
-    def reset(self) -> typing.Tuple[EndpointCountsType, typing.Dict[int, str]]:
+    def reset(self) -> typing.Tuple[EndpointCountsType, typing.Dict[str, typing.List[int]]]:
         with self._endpoint_counts_lock:
             counts = self.endpoint_counts
             self.endpoint_counts = {}
-            span_ids = self.span_id_to_endpoint
-            self.span_id_to_endpoint = {}
+            span_ids = self.endpoint_to_span_ids
+            self.endpoint_to_span_ids = {}
             return counts, span_ids
