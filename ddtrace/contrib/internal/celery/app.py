7988da883bf3cae305d453500e5b5093ce21d7d4
wan <wan.tsui@datadoghq.com>
2024-09-20 13:30:07 -0400

    Fix typo.

--- ddtrace/contrib/internal/celery/app.py
+++ ddtrace/contrib/internal/celery/app.py
@@ -114,7 +114,7 @@ def _traced_apply_async_function(integration_config, fn_name, resource_fn=None):
     Example: before_task_publish starts the span while after_task_publish closes the span.
     If an exception occurs anywhere inside Celery or its dependencies, this can interrupt the
     closing signals.
-    The purpse of _traced_apply_async_function is to close the spans even if one of the closing
+    The purpose of _traced_apply_async_function is to close the spans even if one of the closing
     signals don't get called over the course of the apply_task lifecycle.
     This is done by fetching the stored span and closing it if it hasn't already been closed by a
     closing signal.
