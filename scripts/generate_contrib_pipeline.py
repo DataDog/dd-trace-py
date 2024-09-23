3d47fc39f2d6981e8820e15cb3bc60bedcdf1c64
erikayasuda <erika.yasuda@datadoghq.com>
2024-09-23 17:13:32 -0400

    generate an artifact dummy

--- scripts/generate_contrib_pipeline.py
+++ scripts/generate_contrib_pipeline.py
@@ -1,1 +1,16 @@
-print("testing contrib pipeline generation")
+from pathlib import Path
+import sys
+
+from ruamel.yaml import YAML
+
+
+ROOT = Path(__file__).parents[1]
+CONFIG_TEMPLATE_FILE = ROOT / ".gitlab" / "tests" / "contrib.yml"
+CONFIG_GEN_FILE = ROOT / ".gitlab" / "config.gen.yml"
+
+has_error = False
+with YAML(output=CONFIG_GEN_FILE) as yaml:
+    config = yaml.load(CONFIG_TEMPLATE_FILE)
+    yaml.dump(config)
+
+    sys.exit(has_error)
