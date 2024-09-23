a5499ec69cce72e54ff50c4ac085fb9bcdbdb9b3
erikayasuda <erika.yasuda@datadoghq.com>
2024-09-23 16:46:47 -0400

    dummy pipeline gen job

--- scripts/generate_contrib_pipeline.py
+++ scripts/generate_contrib_pipeline.py
@@ -1,11 +1,1 @@
-from pathlib import Path
-
-from ruamel.yaml import YAML
-
-
-ROOT = Path(__file__).parents[1]
-CONFIG_TEMPLATE_FILE = ROOT / ".gitlab" / "tests" / "contrib.yml"
-CONFIG_GEN_FILE = ROOT / ".gitlab" / "config.gen.yml"
-
-with YAML(output=CONFIG_GEN_FILE) as yaml:
-    config = yaml.load(CONFIG_TEMPLATE_FILE)
+print("testing contrib pipeline generation")
