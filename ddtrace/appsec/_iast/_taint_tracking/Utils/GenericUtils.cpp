e237d910d9ac42d20c3edd105ce435f67ebb7f79
Alberto Vara <alberto.vara@datadoghq.com>
2024-09-24 10:59:52 +0200

    feat(iast): report telemetry log error (#10740)
    Always report a telemetry log error when an IAST propagation error
    raises, regardless of whether the `_DD_IAST_DEBUG` environment variable
    is enabled or not.
    
    - Fix some side effects of aspect functions in join_aspect, ljust_aspect
    - Fix `format_aspect` "if not..." side-effect
    - Fix `format_map_aspect` not BuiltinFunctionType
    - Early fail in `decode/encode_aspect`
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [ ] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

--- ddtrace/appsec/_iast/_taint_tracking/Utils/GenericUtils.cpp
+++ ddtrace/appsec/_iast/_taint_tracking/Utils/GenericUtils.cpp
@@ -36,6 +36,7 @@ asbool(const char* value)
 void
 iast_taint_log_error(const std::string& msg)
 {
+    safe_import("ddtrace.appsec._iast._metrics", "_set_iast_error_metric")("[IAST] Propagation error. " + msg);
     try {
         if (!is_iast_debug_enabled()) {
             return;
@@ -62,9 +63,7 @@ iast_taint_log_error(const std::string& msg)
         }
 
         const auto log = get_python_logger();
-        log.attr("debug")(msg + ": " + frame_info);
-
-        safe_import("ddtrace.appsec._iast._metrics", "_set_iast_error_metric")("IAST propagation error. " + msg);
+        log.attr("debug")("[IAST] Propagation error. " + msg + ": " + frame_info);
 
     } catch (const py::error_already_set& e) {
         if (!e.trace().is_none()) {
