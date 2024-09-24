27fc21e97c75cbbd25c9d710d2b3abf54f315f46
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-09-24 12:54:25 -0400

    fix(profiling): enable endpoint profiling using libdatadog exporter

--- ddtrace/internal/datadog/profiling/dd_wrapper/src/uploader.cpp
+++ ddtrace/internal/datadog/profiling/dd_wrapper/src/uploader.cpp
@@ -49,7 +49,7 @@ Datadog::Uploader::upload(ddog_prof_Profile& profile)
                                                       { .ptr = &file, .len = 1 },
                                                       &tags,
                                                       nullptr,
-                                                      nullptr,
+                                                      encoded->endpoints_stats,
                                                       nullptr,
                                                       max_timeout_ms);
     ddog_prof_EncodedProfile_drop(encoded);
