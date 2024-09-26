3249ab05a32e6395d5f0a0ff406912cb42dca8de
Taegyun Kim <k.taegyun@gmail.com>
2024-09-26 10:26:34 -0400

    Discard changes to ddtrace/internal/datadog/profiling/build_standalone.sh

--- ddtrace/internal/datadog/profiling/build_standalone.sh
+++ ddtrace/internal/datadog/profiling/build_standalone.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -euo pipefail
 
 ### Useful globals
@@ -75,7 +75,7 @@ cmake_args=(
   -DLIB_INSTALL_DIR=$(realpath $MY_DIR)/lib
   -DPython3_INCLUDE_DIRS=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
   -DPython3_EXECUTABLE=$(which python3)
-  -DPython3_LIBRARIES=$(python3 -c "import sysconfig, os; print(os.path.join(sysconfig.get_config_var('LIBDIR'), sysconfig.get_config_var('INSTSONAME')))")
+  -DPython3_LIBRARY=$(python3 -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
 )
 
 # Initial build targets; no matter what, dd_wrapper is the base dependency, so it's always built
