4f5e578f1b387df99094bdfc09fc5f97ec39900b
sanchda <838104+sanchda@users.noreply.github.com>
2024-09-26 14:00:45 +0000

    Initial

--- ddtrace/internal/datadog/profiling/cmake/FindLibdatadog.cmake
+++ ddtrace/internal/datadog/profiling/cmake/FindLibdatadog.cmake
@@ -12,11 +12,8 @@ set(Datadog_BUILD_DIR ${CMAKE_BINARY_DIR}/libdatadog)
 set(Datadog_ROOT ${Datadog_BUILD_DIR}/libdatadog-${TAG_LIBDATADOG})
 
 message(STATUS "${CMAKE_CURRENT_LIST_DIR}/tools/fetch_libdatadog.sh ${TAG_LIBDATADOG} ${Datadog_ROOT}")
-execute_process(
-  COMMAND "${CMAKE_CURRENT_LIST_DIR}/tools/fetch_libdatadog.sh" ${TAG_LIBDATADOG} ${Datadog_ROOT}
-  WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-  COMMAND_ERROR_IS_FATAL ANY
-)
+execute_process(COMMAND "${CMAKE_CURRENT_LIST_DIR}/tools/fetch_libdatadog.sh" ${TAG_LIBDATADOG} ${Datadog_ROOT}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR} COMMAND_ERROR_IS_FATAL ANY)
 
 set(Datadog_DIR "${Datadog_ROOT}/cmake")
 
