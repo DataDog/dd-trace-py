177e7f391f349abe3a6cd33d650b6cabda505edd
lievan <evan.li@datadoqhq.com>
2024-09-27 16:00:14 -0400

    change parsing error msg

--- ddtrace/llmobs/_evaluators/sampler.py
+++ ddtrace/llmobs/_evaluators/sampler.py
@@ -60,9 +60,9 @@ class EvaluatorRunnerSampler:
         rules = []
         sampling_rules_str = os.getenv(self.SAMPLING_RULES_ENV_VAR)
 
-        def parsing_failed_because(msg: str, maybe_throw_this: Exception):
+        def parsing_failed_because(msg, maybe_throw_this):
             if config._raise:
-                raise maybe_throw_this
+                raise maybe_throw_this(msg)
             logger.warning(msg, exc_info=True)
 
         if not sampling_rules_str:
@@ -71,18 +71,18 @@ class EvaluatorRunnerSampler:
             json_rules = json.loads(sampling_rules_str)
         except JSONDecodeError:
             reason = "Failed to parse evaluator sampling rules of: `{}`".format(sampling_rules_str)
-            parsing_failed_because(reason, ValueError(reason))
+            parsing_failed_because(reason, ValueError)
             return []
 
         if not isinstance(json_rules, list):
-            msg = "Evaluator sampling rules must be a list of dictionaries"
-            parsing_failed_because(reason, ValueError(reason))
+            reason = "Evaluator sampling rules must be a list of dictionaries"
+            parsing_failed_because(reason, ValueError)
             return []
 
         for rule in json_rules:
             if "sample_rate" not in rule:
                 reason = "No sample_rate provided for sampling rule: {}".format(json.dumps(rule))
-                parsing_failed_because(reason, KeyError(reason))
+                parsing_failed_because(reason, KeyError)
                 continue
             sample_rate = float(rule["sample_rate"])
             span_name = rule.get("name", SamplingRule.NO_RULE)
