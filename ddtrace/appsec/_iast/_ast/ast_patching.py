DataDog/dd-trace-py

commit 111519b15d6b822f7d8405b5937f9901b995449c

feat(iast): ast deny list (#10540)
Update the deny list of IAST AST patching. The objective of this PR is
to improve the django startup applications

## Checklist
- [x] PR author has checked that all the criteria below are met
- The PR description includes an overview of the change
- The PR description articulates the motivation for the change
- The change includes tests OR the PR description describes a testing
strategy
- The PR description notes risks associated with the change, if any
- Newly-added code is easy to change
- The change follows the [library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
- The change includes or references documentation updates if necessary
- Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))

## Reviewer Checklist
- [x] Reviewer has checked that all the criteria below are met 
- Title is accurate
- All changes are related to the pull request's stated goal
- Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes
- Testing strategy adequately addresses listed risks
- Newly-added code is easy to change
- Release note makes sense to a user of the library
- If necessary, author has acknowledged and discussed the performance
implications of this PR as reported in the benchmarks PR comment
- Backport labels are set in a manner that is consistent with the
[release branch maintenance
policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

ddtrace/appsec/_iast/_ast/ast_patching.py
@@ -26,15 +26,270 @@ IAST_ALLOWLIST: Tuple[Text, ...] = ("tests.appsec.iast",)
 IAST_DENYLIST: Tuple[Text, ...] = (
     "flask",
     "werkzeug",
+    "aiohttp._helpers",
+    "aiohttp._http_parser",
+    "aiohttp._http_writer",
+    "aiohttp._websocket",
+    "aiohttp.log",
+    "aiohttp.tcp_helpers",
+    "asyncio.base_events",
+    "asyncio.base_futures",
+    "asyncio.base_subprocess",
+    "asyncio.base_tasks",
+    "asyncio.constants",
+    "asyncio.coroutines",
+    "asyncio.events",
+    "asyncio.exceptions",
+    "asyncio.futures",
+    "asyncio.locks",
+    "asyncio.log",
+    "asyncio.protocols",
+    "asyncio.queues",
+    "asyncio.runners",
+    "asyncio.selector_events",
+    "asyncio.staggered",
+    "asyncio.subprocess",
+    "asyncio.tasks",
+    "asyncio.threads",
+    "asyncio.transports",
+    "asyncio.trsock",
+    "asyncio.unix_events",
+    "attr._config",
+    "attr._next_gen",
+    "attr.filters",
+    "attr.setters",
+    "backports",
+    "boto3.docs.docstring",
+    "boto3.s3",
+    "botocore.docs.bcdoc",
+    "botocore.retries",
+    "botocore.vendored.requests",
+    "brotli",
+    "brotlicffi",
+    "cchardet",
+    "certifi",
+    "cffi",
+    "chardet.big5freq",
+    "chardet.big5prober",
+    "chardet.charsetgroupprober",
+    "chardet.cp949prober",
+    "chardet.enums",
+    "chardet.escsm",
+    "chardet.eucjpprober",
+    "chardet.euckrfreq",
+    "chardet.euckrprober",
+    "chardet.euctwfreq",
+    "chardet.euctwprober",
+    "chardet.gb2312freq",
+    "chardet.gb2312prober",
+    "chardet.hebrewprober",
+    "chardet.jisfreq",
+    "chardet.langbulgarianmodel",
+    "chardet.langgreekmodel",
+    "chardet.langhebrewmodel",
+    "chardet.langrussianmodel",
+    "chardet.langthaimodel",
+    "chardet.langturkishmodel",
+    "chardet.mbcsgroupprober",
+    "chardet.mbcssm",
+    "chardet.sbcharsetprober",
+    "chardet.sbcsgroupprober",
+    "charset_normalizer",
+    "charset_normalizer.assets",
+    "charset_normalizer.constant",
+    "charset_normalizer.version",
+    "click",
+    "cmath",
+    "colorama",
+    "concurrent.futures",
+    "concurrent.futures._base",
+    "concurrent.futures.thread",
+    "configparser",
+    "coreschema",
+    "crispy_forms",
+    "dateutil",
+    "defusedxml",
+    "difflib",
+    "dill.info",
+    "dill.settings",
+    "django.apps.config",
+    "django.apps.registry",
+    "django.conf",
+    "django.contrib.admin.actions",
+    "django.contrib.admin.admin",
+    "django.contrib.admin.apps",
+    "django.contrib.admin.checks",
+    "django.contrib.admin.decorators",
+    "django.contrib.admin.exceptions",
+    "django.contrib.admin.helpers",
+    "django.contrib.admin.image_formats",
+    "django.contrib.admin.options",
+    "django.contrib.admin.sites",
+    "django.contrib.admin.templatetags",
+    "django.contrib.admin.templatetags.admin_list",
+    "django.contrib.admin.templatetags.admin_modify",
+    "django.contrib.admin.templatetags.admin_urls",
+    "django.contrib.admin.templatetags.base",
+    "django.contrib.admin.templatetags.log",
+    "django.contrib.admin.views.autocomplete",
+    "django.contrib.admin.views.decorators",
+    "django.contrib.admin.views.main",
+    "django.contrib.admin.wagtail_hooks",
+    "django.contrib.admin.widgets",
+    "django.contrib.admindocs.utils",
+    "django.contrib.admindocs.views",
+    "django.contrib.auth.admin",
+    "django.contrib.auth.apps",
+    "django.contrib.auth.backends",
+    "django.contrib.auth.base_user",
+    "django.contrib.auth.checks",
+    "django.contrib.auth.context_processors",
+    "django.contrib.auth.decorators",
+    "django.contrib.auth.hashers",
+    "django.contrib.auth.image_formats",
+    "django.contrib.auth.management",
+    "django.contrib.auth.middleware",
+    "django.contrib.auth.password_validation",
+    "django.contrib.auth.signals",
+    "django.contrib.auth.templatetags",
+    "django.contrib.auth.validators",
+    "django.contrib.auth.wagtail_hooks",
+    "django.contrib.contenttypes.admin",
+    "django.contrib.contenttypes.apps",
+    "django.contrib.contenttypes.checks",
+    "django.contrib.contenttypes.fields",
+    "django.contrib.contenttypes.forms",
+    "django.contrib.contenttypes.image_formats",
+    "django.contrib.contenttypes.management",
+    "django.contrib.contenttypes.models",
+    "django.contrib.contenttypes.templatetags",
+    "django.contrib.contenttypes.views",
+    "django.contrib.contenttypes.wagtail_hooks",
+    "django.contrib.humanize.templatetags",
+    "django.contrib.humanize.templatetags.humanize",
+    "django.contrib.messages.admin",
+    "django.contrib.messages.api",
+    "django.contrib.messages.apps",
+    "django.contrib.messages.constants",
+    "django.contrib.messages.context_processors",
+    "django.contrib.messages.image_formats",
+    "django.contrib.messages.middleware",
+    "django.contrib.messages.storage",
+    "django.contrib.messages.storage.base",
+    "django.contrib.messages.storage.cookie",
+    "django.contrib.messages.storage.fallback",
+    "django.contrib.messages.storage.session",
+    "django.contrib.messages.templatetags",
+    "django.contrib.messages.utils",
+    "django.contrib.messages.wagtail_hooks",
+    "django.contrib.sessions.admin",
+    "django.contrib.sessions.apps",
+    "django.contrib.sessions.backends",
+    "django.contrib.sessions.backends.base",
+    "django.contrib.sessions.base_session",
+    "django.contrib.sessions.exceptions",
+    "django.contrib.sessions.image_formats",
+    "django.contrib.sessions.middleware",
+    "django.contrib.sessions.templatetags",
+    "django.contrib.sessions.wagtail_hooks",
+    "django.contrib.sites",
+    "django.contrib.staticfiles.admin",
+    "django.contrib.staticfiles.apps",
+    "django.contrib.staticfiles.checks",
+    "django.contrib.staticfiles.finders",
+    "django.contrib.staticfiles.image_formats",
+    "django.contrib.staticfiles.models",
+    "django.contrib.staticfiles.storage",
+    "django.contrib.staticfiles.templatetags",
+    "django.contrib.staticfiles.utils",
+    "django.contrib.staticfiles.wagtail_hooks",
+    "django.core.cache.backends",
+    "django.core.cache.backends.base",
+    "django.core.cache.backends.filebased",
+    "django.core.cache.backends.locmem",
+    "django.core.cache.utils",
+    "django.core.checks.async_checks",
+    "django.core.checks.caches",
+    "django.core.checks.compatibility",
+    "django.core.checks.compatibility.django_4_0",
+    "django.core.checks.database",
+    "django.core.checks.files",
+    "django.core.checks.messages",
+    "django.core.checks.model_checks",
+    "django.core.checks.registry",
+    "django.core.checks.security",
+    "django.core.checks.security.base",
+    "django.core.checks.security.csrf",
+    "django.core.checks.security.sessions",
+    "django.core.checks.templates",
+    "django.core.checks.translation",
+    "django.core.checks.urls",
+    "django.core.exceptions",
+    "django.core.mail",
+    "django.core.management.base",
+    "django.core.management.color",
+    "django.core.management.sql",
+    "django.core.paginator",
+    "django.core.signing",
+    "django.core.validators",
+    "django.dispatch.dispatcher",
+    "django.template.autoreload",
+    "django.template.backends",
+    "django.template.backends.base",
+    "django.template.backends.django",
+    "django.template.base",
+    "django.template.context",
+    "django.template.context_processors",
+    "django.template.defaultfilters",
+    "django.template.defaulttags",
+    "django.template.engine",
+    "django.template.exceptions",
+    "django.template.library",
+    "django.template.loader",
+    "django.template.loader_tags",
+    "django.template.loaders",
+    "django.template.loaders.app_directories",
+    "django.template.loaders.base",
+    "django.template.loaders.cached",
+    "django.template.loaders.filesystem",
+    "django.template.response",
+    "django.template.smartif",
+    "django.template.utils",
+    "django.templatetags",
+    "django.templatetags.cache",
+    "django.templatetags.i18n",
+    "django.templatetags.l10n",
+    "django.templatetags.static",
+    "django.templatetags.tz",
+    "django.test",
+    "django.urls.base",
+    "django.urls.conf",
+    "django.urls.converters",
+    "django.urls.exceptions",
+    "django.urls.resolvers",
+    "django.urls.utils",
+    "django.utils",
+    "django_filters.compat",
+    "django_filters.conf",
+    "django_filters.constants",
+    "django_filters.exceptions",
+    "django_filters.fields",
+    "django_filters.filters",
+    "django_filters.filterset",
+    "django_filters.rest_framework",
+    "django_filters.rest_framework.backends",
+    "django_filters.rest_framework.filters",
+    "django_filters.rest_framework.filterset",
+    "django_filters.utils",
+    "django_filters.widgets",
     "crypto",  # This module is patched by the IAST patch methods, propagation is not needed
     "deprecated",
     "api_pb2",  # Patching crashes with these auto-generated modules, propagation is not needed
-    "api_pb2_grpc",  # ditto
+    "api_pb2_grpc",  # Patching crashes with these auto-generated modules, propagation is not needed
     "asyncpg.pgproto",
     "blinker",
     "bytecode",
     "cattrs",
-    "click",
     "ddsketch",
     "ddtrace",
     "encodings",  # this package is used to load encodings when a module is imported, propagation is not needed
@@ -42,14 +297,11 @@ IAST_DENYLIST: Tuple[Text, ...] = (
     "envier",
     "exceptiongroup",
     "freezegun",  # Testing utilities for time manipulation
-    "hypothesis",
+    "hypothesis",  # Testing utilities
     "importlib_metadata",
     "inspect",  # this package is used to get the stack frames, propagation is not needed
     "itsdangerous",
     "moto",  # used for mocking AWS, propagation is not needed
-    "moto[all]",
-    "moto[ec2]",
-    "moto[s3]",
     "opentelemetry-api",
     "packaging",
     "pip",
@@ -67,13 +319,12 @@ IAST_DENYLIST: Tuple[Text, ...] = (
     "urlpatterns_reverse.tests",  # assertRaises eat exceptions in native code, so we don't call the original function
     "wrapt",
     "zipp",
-    ## This is a workaround for Sanic failures:
+    # This is a workaround for Sanic failures:
     "websocket",
     "h11",
     "aioquic",
     "httptools",
     "sniffio",
-    "py",
     "sanic",
     "rich",
     "httpx",

commit 7e8eb0b8aa9ece412d258459631da1d9d2df82c5

chore(iast): memory leak in pypika and pydantic (#10846)
Introduced in https://github.com/DataDog/dd-trace-py/pull/10540

We patch the problem, now we must figure out where is the error in
pydantic and pypika packages

## Checklist
- [x] PR author has checked that all the criteria below are met
- The PR description includes an overview of the change
- The PR description articulates the motivation for the change
- The change includes tests OR the PR description describes a testing
strategy
- The PR description notes risks associated with the change, if any
- Newly-added code is easy to change
- The change follows the [library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
- The change includes or references documentation updates if necessary
- Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))

## Reviewer Checklist
- [x] Reviewer has checked that all the criteria below are met 
- Title is accurate
- All changes are related to the pull request's stated goal
- Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes
- Testing strategy adequately addresses listed risks
- Newly-added code is easy to change
- Release note makes sense to a user of the library
- If necessary, author has acknowledged and discussed the performance
implications of this PR as reported in the benchmarks PR comment
- Backport labels are set in a manner that is consistent with the
[release branch maintenance
policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

---------

Co-authored-by: Juanjo Alvarez Martinez <juanjo.alvarezmartinez@datadoghq.com>

ddtrace/appsec/_iast/_ast/ast_patching.py
@@ -300,6 +300,11 @@ IAST_DENYLIST: Tuple[Text, ...] = (
     "uvicorn.",
     "anyio.",
     "httpcore.",
+    "pypika.",
+    "pydantic.",
+    "pydantic_core.",
+    "pydantic_settings.",
+    "tomli.",
 )
 
 