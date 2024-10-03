1391a210d80f3d9fab95f6d3c0750d646f38fc79
David Sanchez <838104+sanchda@users.noreply.github.com>
2024-10-03 12:26:03 -0500

    Update profiler.py
    Fixup for backport

--- ddtrace/profiling/profiler.py
+++ ddtrace/profiling/profiler.py
@@ -254,7 +254,7 @@ class _ProfilerInstance(service.Service):
 
                 # If this instance of ddtrace was injected, then do not enable profiling, since that will load
                 # protobuf, breaking some environments.
-                if profiling_config._injected:
+                if config._injected:
                     LOG.error("Profiling failures occurred in an injected instance of ddtrace, disabling profiling")
                     return []
 
