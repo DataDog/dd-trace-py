ccbb60bb86e1f77fa42e1b0bfc9801b18e25750c
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-10-04 17:30:26 +0200

    fmt
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/appsec/_iast/_taint_tracking/tests/test_common.hpp
+++ ddtrace/appsec/_iast/_taint_tracking/tests/test_common.hpp
@@ -29,12 +29,10 @@ class PyEnvWithContext : public ::testing::Test
         initializer.reset();
         py::finalize_interpreter();
     }
-public:
-    PyObject* StringToPyObjectStr(const string& ob)
-    {
-        return PyUnicode_FromString(ob.c_str());
-    }
-    string PyObjectStrToString(PyObject*  ob)
+
+  public:
+    PyObject* StringToPyObjectStr(const string& ob) { return PyUnicode_FromString(ob.c_str()); }
+    string PyObjectStrToString(PyObject* ob)
     {
         PyObject* utf8_str = PyUnicode_AsEncodedString(ob, "utf-8", "strict");
         const char* res_data = PyBytes_AsString(utf8_str);
@@ -42,11 +40,8 @@ public:
         Py_DecRef(utf8_str);
         return res_string;
     }
-    PyObject* StringToPyObjectBytes(const string& ob)
-    {
-        return PyBytes_FromString(ob.c_str());
-    }
-    string PyObjectBytesToString(PyObject*  ob)
+    PyObject* StringToPyObjectBytes(const string& ob) { return PyBytes_FromString(ob.c_str()); }
+    string PyObjectBytesToString(PyObject* ob)
     {
         const char* res_data = PyBytes_AsString(ob);
         std::string res_string(res_data);
