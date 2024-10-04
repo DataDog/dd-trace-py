59e7d30e0183921876b1f951310921b93d389cd9
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2024-10-04 11:43:52 +0100

    chore(di): trigger probes
    We implement trigger probes. These allows triggering the capture of
    debug information along a trace, ensuring all the relevant probes are
    also triggered.

--- tests/debugging/origin/test_span.py
+++ tests/debugging/origin/test_span.py
@@ -1,10 +1,21 @@
 from pathlib import Path
+import typing as t
 
 import ddtrace
 from ddtrace.debugging._origin.span import SpanCodeOriginProcessor
+from ddtrace.debugging._session import Session
 from ddtrace.ext import SpanTypes
 from ddtrace.internal import core
+from tests.debugging.mocking import MockLogsIntakeUploaderV1
 from tests.utils import TracerTestCase
+
+
+class MockSpanCodeOriginProcessor(SpanCodeOriginProcessor):
+    __uploader__ = MockLogsIntakeUploaderV1
+
+    @classmethod
+    def get_uploader(cls) -> MockLogsIntakeUploaderV1:
+        return t.cast(MockLogsIntakeUploaderV1, cls.__uploader__._instance)
 
 
 class SpanProbeTestCase(TracerTestCase):
@@ -13,13 +24,13 @@ class SpanProbeTestCase(TracerTestCase):
         self.backup_tracer = ddtrace.tracer
         ddtrace.tracer = self.tracer
 
-        SpanCodeOriginProcessor.enable()
+        MockSpanCodeOriginProcessor.enable()
 
     def tearDown(self):
         ddtrace.tracer = self.backup_tracer
         super(SpanProbeTestCase, self).tearDown()
 
-        SpanCodeOriginProcessor.disable()
+        MockSpanCodeOriginProcessor.disable()
         core.reset_listeners(event_id="service_entrypoint.patch")
 
     def test_span_origin(self):
@@ -54,3 +65,41 @@ class SpanProbeTestCase(TracerTestCase):
         assert _exit.get_tag("_dd.code_origin.type") == "exit"
         assert _exit.get_tag("_dd.code_origin.frames.2.file") == str(Path(__file__).resolve())
         assert _exit.get_tag("_dd.code_origin.frames.2.line") == str(self.test_span_origin.__code__.co_firstlineno)
+
+    def test_span_origin_session(self):
+        def entry_call():
+            pass
+
+        core.dispatch("service_entrypoint.patch", (entry_call,))
+
+        with self.tracer.trace("entry"):
+            # Emulate a trigger probe
+            Session(ident="test", level=2).link_to_trace()
+            entry_call()
+            with self.tracer.trace("middle"):
+                with self.tracer.trace("exit", span_type=SpanTypes.HTTP):
+                    pass
+
+        self.assert_span_count(3)
+        entry, middle, _exit = self.get_spans()
+        payloads = MockSpanCodeOriginProcessor.get_uploader().wait_for_payloads()
+        snapshot_ids = {p["debugger"]["snapshot"]["id"] for p in payloads}
+
+        assert len(payloads) == len(snapshot_ids)
+
+        entry_snapshot_id = entry.get_tag("_dd.code_origin.frames.0.snapshot_id")
+        assert entry.get_tag("_dd.code_origin.type") == "entry"
+        assert entry_snapshot_id in snapshot_ids
+
+        # Check that we don't have span location tags on the middle span
+        assert middle.get_tag("_dd.code_origin.frames.0.snapshot_id") is None
+
+        # Check that we have all the snapshots for the exit span
+        assert _exit.get_tag("_dd.code_origin.type") == "exit"
+        snapshot_ids_from_span_tags = {_exit.get_tag(f"_dd.code_origin.frames.{_}.snapshot_id") for _ in range(8)}
+        snapshot_ids_from_span_tags.discard(None)
+        assert snapshot_ids_from_span_tags < snapshot_ids
+
+        # Check that we have complete data
+        snapshot_ids_from_span_tags.add(entry_snapshot_id)
+        assert snapshot_ids_from_span_tags == snapshot_ids
--- tests/submod/traced_stuff.py
+++ tests/submod/traced_stuff.py
@@ -1,4 +1,7 @@
 # -*- encoding: utf-8 -*-
+from ddtrace.ext import SpanTypes
+
+
 def inner():
     return 42
 
@@ -6,3 +9,22 @@ def inner():
 def traceme():
     cake = "🍰"  # noqa
     return 42 + inner()
+
+
+def exit_call(tracer):
+    with tracer.trace("exit", span_type=SpanTypes.HTTP):
+        return 42
+
+
+def middle(tracer):
+    with tracer.trace("middle"):
+        return exit_call(tracer)
+
+
+def entrypoint(tracer):
+    return middle(tracer)
+
+
+def traced_entrypoint(tracer):
+    with tracer.trace("entry"):
+        return entrypoint(tracer)
