951049cb239fad6f13183ca35bca521ac5672e51
Federico Mon <federico.mon@datadoghq.com>
2024-10-08 19:11:53 +0200

    also for other flasks

--- tests/appsec/iast_tdd_propagation/flask_taint_sinks_app.py
+++ tests/appsec/iast_tdd_propagation/flask_taint_sinks_app.py
@@ -1,9 +1,12 @@
+import os
+
 from flask_taint_sinks_views import create_app
 
 from ddtrace import auto  # noqa: F401
 
+port = int(os.getenv("FLASK_RUN_PORT", 8000))
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=False, port=8000)
+    app.run(debug=False, port=port)
