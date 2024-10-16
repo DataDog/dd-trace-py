8ac5dfcebf6db6f407909a5e6a6088531910b06b
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-10-16 18:11:58 +0200

    mypy
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/appsec/_iast/_taint_tracking/__init__.py
+++ ddtrace/appsec/_iast/_taint_tracking/__init__.py
@@ -2,7 +2,7 @@ from io import BytesIO
 from io import StringIO
 import itertools
 from typing import Any
-from typing import Iterable
+from typing import Sequence
 from typing import Tuple
 
 from ddtrace.internal._unpatched import _threading as threading
@@ -13,7 +13,6 @@ from .._metrics import _set_iast_error_metric
 from .._metrics import _set_metric_iast_executed_source
 from .._utils import _is_iast_debug_enabled
 from .._utils import _is_python_version_supported
-
 
 log = get_logger(__name__)
 
@@ -205,6 +204,7 @@ def get_tainted_ranges(pyobject: Any) -> Tuple:
 if _is_iast_debug_enabled():
     TAINTED_FRAMES = []
 
+
     def trace_calls_and_returns(frame, event, arg):
         co = frame.f_code
         func_name = co.co_name
@@ -237,9 +237,9 @@ if _is_iast_debug_enabled():
                 log.debug("Return from %s on line %d of %s, return value: %s", func_name, line_no, filename, arg)
                 if isinstance(arg, (str, bytes, bytearray, BytesIO, StringIO, list, tuple, dict)):
                     if (
-                        (isinstance(arg, (str, bytes, bytearray, BytesIO, StringIO)) and is_pyobject_tainted(arg))
-                        or (isinstance(arg, (list, tuple)) and any([is_pyobject_tainted(x) for x in arg]))
-                        or (isinstance(arg, dict) and any([is_pyobject_tainted(x) for x in arg.values()]))
+                            (isinstance(arg, (str, bytes, bytearray, BytesIO, StringIO)) and is_pyobject_tainted(arg))
+                            or (isinstance(arg, (list, tuple)) and any([is_pyobject_tainted(x) for x in arg]))
+                            or (isinstance(arg, dict) and any([is_pyobject_tainted(x) for x in arg.values()]))
                     ):
                         log.debug("Return value is tainted")
                     else:
@@ -247,10 +247,11 @@ if _is_iast_debug_enabled():
                 log.debug("-----")
         return
 
+
     threading.settrace(trace_calls_and_returns)
 
 
-def copy_ranges_to_string(s: str, ranges: Iterable[TaintRange]) -> str:
+def copy_ranges_to_string(s: str, ranges: Sequence[TaintRange]) -> str:
     for r in ranges:
         if s in r.source.value:
             s = _taint_pyobject_base(
@@ -270,7 +271,7 @@ def copy_ranges_to_string(s: str, ranges: Iterable[TaintRange]) -> str:
 
 # Given a list of ranges, try to match them with the iterable and return a new iterable with a new range applied that
 # matched the original one Source. If no range matches, take the Source from the first one.
-def copy_ranges_to_iterable_with_strings(iterable: Iterable[str], ranges: Iterable[TaintRange]) -> Iterable[str]:
+def copy_ranges_to_iterable_with_strings(iterable: Sequence[str], ranges: Sequence[TaintRange]) -> Sequence[str]:
     iterable_type = type(iterable)
 
     new_result = []
