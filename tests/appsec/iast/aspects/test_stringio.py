56b3ad8fd12856b38732d9c7c3cfb0dcfa1cb289
Federico Mon <federico.mon@datadoghq.com>
2024-10-17 18:14:44 +0200

    improve test and skip it for now

--- tests/appsec/iast/aspects/test_stringio.py
+++ tests/appsec/iast/aspects/test_stringio.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import pytest
 
 from ddtrace.appsec._common_module_patches import patch_common_modules
 from ddtrace.appsec._iast._taint_tracking import OriginType
@@ -26,10 +27,11 @@ def test_stringio_aspect_read():
         assert len(ranges) == 1
 
 
+@pytest.mark.skip("TODO: APPSEC-55319")
 def test_stringio_aspect_read_with_offset():
     with override_global_config(dict(_iast_enabled=True)):
         patch_common_modules()
-        not_tainted = "foobaz"
+        not_tainted = "foobazbazfoo"
         tainted = taint_pyobject(
             pyobject="foobar",
             source_name="test_stringio_read_aspect_tainted_string",
@@ -38,9 +40,13 @@ def test_stringio_aspect_read_with_offset():
         )
         added = add_aspect(not_tainted, tainted)
         sio = stringio_aspect(None, 0, added)
-        val = sio.read(1)
-        # TODO: Jira: APPSEC-55319
-        # If the StringIO() and read() aspects were perfect, this should not be tainted
-        assert is_pyobject_tainted(val)
+        val = sio.read(10)
+        # If the StringIO() and read() aspects were perfect, `val` would not be tainted
+        assert not is_pyobject_tainted(val)
         ranges = get_tainted_ranges(val)
+        assert len(ranges) == 0
+
+        val_tainted = sio.read(5)
+        assert is_pyobject_tainted(val_tainted)
+        ranges = get_tainted_ranges(val_tainted)
         assert len(ranges) == 1
