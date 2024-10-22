d5eee27885ba1e89f68fefbe9f23e8f6c5ac87a1
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-10-22 17:42:57 +0000

    pass test_gunicorn.py

--- tests/profiling_v2/test_gunicorn.py
+++ tests/profiling_v2/test_gunicorn.py
@@ -7,8 +7,7 @@ import time
 
 import pytest
 
-from . import utils
-
+from tests.profiling.collector import pprof_utils
 
 # gunicorn is not available on Windows
 if sys.platform == "win32":
@@ -21,7 +20,7 @@ def _run_gunicorn(*args):
     cmd = (
         ["ddtrace-run", "gunicorn", "--bind", "127.0.0.1:7643", "--chdir", os.path.dirname(__file__)]
         + list(args)
-        + ["gunicorn-app:app"]
+        + ["tests.profiling.gunicorn-app:app"]
     )
     return subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
@@ -31,6 +30,7 @@ def gunicorn(monkeypatch):
     # Do not ignore profiler so we have samples in the output pprof
     monkeypatch.setenv("DD_PROFILING_IGNORE_PROFILER", "0")
     monkeypatch.setenv("DD_PROFILING_ENABLED", "1")
+    monkeypatch.setenv("DD_PROFILING_STACK_V2_ENABLED", "1")
 
     yield _run_gunicorn
 
@@ -38,14 +38,6 @@ def gunicorn(monkeypatch):
 def _get_worker_pids(stdout):
     # type: (str) -> list[int]
     return [int(_) for _ in re.findall(r"Booting worker with pid: (\d+)", stdout)]
-
-
-def test_gunicorn_tests_can_work():
-    # type: () -> None
-    from ddtrace.settings.profiling import config as profiler_config
-
-    assert profiler_config._force_legacy_exporter
-    assert not profiler_config.export.libdd_enabled
 
 
 def _test_gunicorn(gunicorn, tmp_path, monkeypatch, *args):
@@ -63,9 +55,9 @@ def _test_gunicorn(gunicorn, tmp_path, monkeypatch, *args):
     assert proc.wait() == 0, output
     assert "module 'threading' has no attribute '_active'" not in output, output
 
-    utils.check_pprof_file("%s.%d.1" % (filename, proc.pid))
-    for pid in worker_pids:
-        utils.check_pprof_file("%s.%d.1" % (filename, pid))
+    profile = pprof_utils.parse_profile("%s.%d" % (filename, proc.pid))
+    samples = pprof_utils.get_samples_with_value_type(profile, "cpu-time")
+    assert len(samples) > 0
 
 
 def test_gunicorn(gunicorn, tmp_path, monkeypatch):
