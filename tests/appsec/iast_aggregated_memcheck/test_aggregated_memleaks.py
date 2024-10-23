50148411495330ae83ad06e4ef2c31ef725eb3b6
Alberto Vara <alberto.vara@datadoghq.com>
2024-10-23 10:16:07 +0200

    chore(ci): iast, less iterations

--- tests/appsec/iast_aggregated_memcheck/test_aggregated_memleaks.py
+++ tests/appsec/iast_aggregated_memcheck/test_aggregated_memleaks.py
@@ -8,5 +8,5 @@ async def test_aggregated_leaks():
     with override_global_config(dict(_iast_enabled=True, _deduplication_enabled=False, _iast_request_sampling=100.0)):
         from scripts.iast.leak_functions import iast_leaks
 
-        result = await iast_leaks(60000, 0.3, 500) == 0
+        result = await iast_leaks(60000, 0.2, 500) == 0
         assert result
