fcad298ad1c0eee97c86a8901f90043dc1e27901
Alberto Vara <alberto.vara@datadoghq.com>
2024-10-23 15:52:46 +0200

    chore(ci): fix django snapshot timeouts

--- tests/contrib/django/test_django_snapshots.py
+++ tests/contrib/django/test_django_snapshots.py
@@ -40,7 +40,13 @@ def daphne_client(django_asgi, additional_env=None):
 
     # ddtrace-run uses execl which replaces the process but the webserver process itself might spawn new processes.
     # Right now it doesn't but it's possible that it might in the future (ex. uwsgi).
-    cmd = ["ddtrace-run", "daphne", "-p", str(SERVER_PORT), "tests.contrib.django.asgi:%s" % django_asgi]
+    cmd = [
+        "ddtrace.commands.ddtrace_run",
+        "daphne",
+        "-p",
+        str(SERVER_PORT),
+        "tests.contrib.django.asgi:%s" % django_asgi,
+    ]
     proc = subprocess.Popen(
         cmd,
         stdout=subprocess.PIPE,
