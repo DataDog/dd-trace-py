0dc56a07eff23b5a27168e3be6531aa93b2fdfad
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-10-24 18:21:27 +0200

    improved
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/appsec/ddprof.py
+++ ddtrace/appsec/ddprof.py
@@ -1,19 +1,21 @@
 import os
 import ctypes
+import platform
 from ctypes import c_int
-
-from ddtrace.internal.logger import get_logger
-
-log = get_logger(__name__)
 
 
 class PyDDProfException(Exception):
     pass
 
 
-ddprof_lib_path = os.path.join(os.path.dirname(__file__), "ddprof_lib", "lib", "libdd_profiling.so")
+if platform.system() != "Linux":
+    raise PyDDProfException("this module is only supported on Linux")
+
+ddprof_lib_path = os.path.join(os.path.dirname(__file__), "ddprof_lib", "libdd_profiling.so")
 if not os.path.exists(ddprof_lib_path):
-    raise PyDDProfException(f"ddprof library not found at {ddprof_lib_path}")
+    raise PyDDProfException(
+        f"ddprof library not found at {ddprof_lib_path}. Please switch to ddtrace/appsec/ddprof_lib/ and run ./download_ddprof.sh"
+    )
 
 for required_env in {"DD_ENV", "DD_SERVICE", "DD_API_KEY"}:
     if required_env not in os.environ:
