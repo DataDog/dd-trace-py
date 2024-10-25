739504a61d75252eca6450eac2f063de04a942ee
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-10-25 09:57:08 +0200

    fmt
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/appsec/_ddprof.py
+++ ddtrace/appsec/_ddprof.py
@@ -1,7 +1,7 @@
-import os
 import ctypes
-import platform
 from ctypes import c_int
+import os
+import platform
 
 
 class PyDDProfException(Exception):
@@ -14,7 +14,8 @@ if platform.system() != "Linux":
 ddprof_lib_path = os.path.join(os.path.dirname(__file__), "ddprof_lib", "libdd_profiling.so")
 if not os.path.exists(ddprof_lib_path):
     raise PyDDProfException(
-        f"ddprof library not found at {ddprof_lib_path}. Please switch to ddtrace/appsec/ddprof_lib/ and run ./download_ddprof.sh"
+        f"ddprof library not found at {ddprof_lib_path}. "
+        "Please switch to ddtrace/appsec/ddprof_lib/ and run ./download_ddprof.sh"
     )
 
 for required_env in {"DD_ENV", "DD_SERVICE", "DD_API_KEY"}:
@@ -33,7 +34,7 @@ ddprof.ddprof_stop_profiling.restype = None
 def start_profiling():
     result = ddprof.ddprof_start_profiling()
     if result == -1:
-        raise PyDDProfException(f"Failed to start profiling: error code -1")
+        raise PyDDProfException("Failed to start profiling: error code -1")
     return result
 
 
