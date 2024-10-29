4dee5d2ff1b6fe3285da5c6d9d6b7703be4b98dd
quinna-h <quinna.halim@datadoghq.com>
2024-10-29 10:37:22 -0400

    test increase delay in django server

--- tests/contrib/django/test_django_snapshots.py
+++ tests/contrib/django/test_django_snapshots.py
@@ -66,7 +66,7 @@ def daphne_client(django_asgi, additional_env=None):
     # Wait for the server to start up
     try:
         print("Waiting for server to start")
-        client.wait(max_tries=120, delay=0.2, initial_wait=2.0)
+        client.wait(max_tries=120, delay=1.0, initial_wait=2.0)
         print("Server started")
     except Exception:
         raise AssertionError(
