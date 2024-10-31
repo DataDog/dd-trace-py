fb99370549f456738d77b80ea6fb6be6387c664f
Romain Komorn <136473744+romainkomorndatadog@users.noreply.github.com>
2024-10-31 16:22:36 +0000

    chore(ci_visibility): add Auto Test Retries to pytest plugin v2 (#11241)
    Introduces support for Auto Test Retries to the v2 `pytest` plugin.
    
    This retries tests that have failed their first attempt (except for
    tests that have failed setup or teardown, which are not retried at all).
    Failed tests that pass retries result in the session passing (with
    `pytest` exiting with code 0). Test that fail all retries cause the
    session to fail (exiting with code 1).
    
    If any tests were retried, a section is added to the final summary
    report, showing the results:
    ```
    ======================= Datadog Auto Test Retries ========================
    _________________________________ FAILED _________________________________
    FAILED tests/test_some_fail.py::test_func_fails_all_retries
    _________________________________ PASSED _________________________________
    PASSED tests/test_all_pass.py::TestAllPass::test_method_succeeds_on_5th_retry
    PASSED tests/test_all_pass.py::TestAllPass::test_method_succeeds_on_1st_retry
    PASSED tests/test_all_pass.py::test_func_succeeds_on_3rd
    PASSED tests/test_all_pass.py::test_func_succeeds_first_retry
    PASSED tests/test_some_fail.py::TestAllPass::test_method_succeeds_on_5th_retry
    PASSED tests/test_some_fail.py::TestAllPass::test_method_succeeds_on_1st_retry
    PASSED tests/test_some_fail.py::test_func_succeeds_on_3rd
    PASSED tests/test_some_fail.py::test_func_succeeds_first_retry
    ___________________ Datadog Auto Test Retries summary ____________________
             1 failed, 8 passed (total attempts: 26 failed, 8 passed)
    ==========================================================================
    ```
    
    Other changes in this PR:
    - Some refactoring to DRY `pytest` retry logic 
    - Introduction of API to "stash" data on Test Visibility objects 
    - Tweak to ATR logic to make successfully-retried test not affect
    suite-level-or-above status
    - Tweak to telemetry to add `is_retry` tag to `test`-level
    `event_finished` metric
    
    Finally, no release note is added as the feature is still unreleased.
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

--- ddtrace/contrib/pytest/_retry_utils.py
+++ ddtrace/contrib/pytest/_retry_utils.py
@@ -3,9 +3,14 @@ from dataclasses import dataclass
 import typing as t
 
 import _pytest
+from _pytest.logging import caplog_handler_key
+from _pytest.logging import caplog_records_key
 from _pytest.runner import CallInfo
 import pytest
 
+from ddtrace.contrib.pytest._utils import _TestOutcome
+from ddtrace.ext.test_visibility.api import TestExcInfo
+from ddtrace.ext.test_visibility.api import TestStatus
 from ddtrace.internal import core
 
 
@@ -30,9 +35,63 @@ def set_retry_num(nodeid: str, retry_num: int):
         yield
 
 
-def _efd_get_attempt_string(nodeid) -> str:
+def _get_retry_attempt_string(nodeid) -> str:
     retry_number = get_retry_num(nodeid)
     return "ATTEMPT {} ".format(retry_number) if retry_number is not None else "INITIAL ATTEMPT "
+
+
+def _get_outcome_from_retry(
+    item: pytest.Item,
+    outcomes: RetryOutcomes,
+) -> _TestOutcome:
+    _outcome_status: t.Optional[TestStatus] = None
+    _outcome_skip_reason: t.Optional[str] = None
+    _outcome_exc_info: t.Optional[TestExcInfo] = None
+
+    # _initrequest() needs to be called first because the test has already executed once
+    item._initrequest()
+
+    # Setup
+    setup_call, setup_report = _retry_run_when(item, "setup", outcomes)
+    if setup_report.outcome == outcomes.FAILED:
+        _outcome_status = TestStatus.FAIL
+        if setup_call.excinfo is not None:
+            _outcome_exc_info = TestExcInfo(setup_call.excinfo.type, setup_call.excinfo.value, setup_call.excinfo.tb)
+            item.stash[caplog_records_key] = {}
+            item.stash[caplog_handler_key] = {}
+    if setup_report.outcome == outcomes.SKIPPED:
+        _outcome_status = TestStatus.SKIP
+
+    # Call
+    if setup_report.outcome == outcomes.PASSED:
+        call_call, call_report = _retry_run_when(item, "call", outcomes)
+        if call_report.outcome == outcomes.FAILED:
+            _outcome_status = TestStatus.FAIL
+            if call_call.excinfo is not None:
+                _outcome_exc_info = TestExcInfo(call_call.excinfo.type, call_call.excinfo.value, call_call.excinfo.tb)
+                item.stash[caplog_records_key] = {}
+                item.stash[caplog_handler_key] = {}
+        elif call_report.outcome == outcomes.SKIPPED:
+            _outcome_status = TestStatus.SKIP
+        elif call_report.outcome == outcomes.PASSED:
+            _outcome_status = TestStatus.PASS
+
+    # Teardown does not happen if setup skipped
+    if not setup_report.skipped:
+        teardown_call, teardown_report = _retry_run_when(item, "teardown", outcomes)
+        # Only override the outcome if the teardown failed, otherwise defer to either setup or call outcome
+        if teardown_report.outcome == outcomes.FAILED:
+            _outcome_status = TestStatus.FAIL
+            if teardown_call.excinfo is not None:
+                _outcome_exc_info = TestExcInfo(
+                    teardown_call.excinfo.type, teardown_call.excinfo.value, teardown_call.excinfo.tb
+                )
+                item.stash[caplog_records_key] = {}
+                item.stash[caplog_handler_key] = {}
+
+    item._initrequest()
+
+    return _TestOutcome(status=_outcome_status, skip_reason=_outcome_skip_reason, exc_info=_outcome_exc_info)
 
 
 def _retry_run_when(item, when, outcomes: RetryOutcomes) -> t.Tuple[CallInfo, _pytest.reports.TestReport]:
