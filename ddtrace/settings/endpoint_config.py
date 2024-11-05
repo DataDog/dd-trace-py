045e5eb1e73bd2353d2bc6d33496c45ede7a1db3
Federico Mon <federico.mon@datadoghq.com>
2024-11-05 12:32:08 +0100

    chore(asm): configuration endpoint as private env var (#11136)
    (cherry picked from commit 06c97522e7abad8d04f9ba06d726a88326744cb6)

---
+++ ddtrace/settings/endpoint_config.py
@@ -0,0 +1,45 @@
+"""
+This module contains the logic to configure ddtrace products from a configuration endpoint.
+The configuration endpoint is a URL that returns a JSON object with the configuration for the products.
+It takes precedence over environment variables and configuration files.
+"""
+import json
+import os
+from urllib import parse
+
+from ddtrace.constants import CONFIG_ENDPOINT_ENV
+from ddtrace.internal.logger import get_logger
+from ddtrace.internal.utils.http import connector
+
+
+log = get_logger(__name__)
+
+
+def fetch_config_from_endpoint() -> dict:
+    """
+    Fetch the configuration from the configuration endpoint.
+    """
+    config_endpoint = os.getenv(CONFIG_ENDPOINT_ENV, None)
+
+    if config_endpoint is None:
+        log.debug("Configuration endpoint not set. Skipping fetching configuration.")
+        return {}
+
+    try:
+        parsed_url = parse.urlparse(config_endpoint)
+        connect = connector(config_endpoint)
+        with connect() as conn:
+            conn.request("GET", parsed_url.path or "/")
+            response = conn.getresponse()
+            if not (200 <= response.status < 300):
+                log.error("Failed to fetch configuration from endpoint, status code: %d", response.status)
+                return {}
+
+            data = response.read().decode("utf-8")
+            log.debug("Fetched configuration from endpoint: %s", data)
+            return json.loads(data)
+
+    except (json.JSONDecodeError, Exception):
+        log.error("Failed to fetch configuration from endpoint:", exc_info=True)
+
+    return {}
