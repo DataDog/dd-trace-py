87db7260901b616b7e6dce25c10daf656b391938
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-11-05 16:39:31 -0500

    update gunicorn test

--- tests/profiling_v2/test_gunicorn.py
+++ tests/profiling_v2/test_gunicorn.py
@@ -6,6 +6,7 @@ import sys
 import time
 
 import pytest
+import requests
 
 from tests.profiling.collector import pprof_utils
 
@@ -28,8 +29,7 @@ def _run_gunicorn(*args):
 
 @pytest.fixture
 def gunicorn(monkeypatch):
-    # Do not ignore profiler so we have samples in the output pprof
-    monkeypatch.setenv("DD_PROFILING_IGNORE_PROFILER", "0")
+    monkeypatch.setenv("DD_PROFILING_IGNORE_PROFILER", "1")
     monkeypatch.setenv("DD_PROFILING_ENABLED", "1")
     # This was needed for the gunicorn process to start and print worker startup
     # messages. Without this, the test can't find the worker PIDs.
@@ -47,20 +47,46 @@ def _test_gunicorn(gunicorn, tmp_path, monkeypatch, *args):
     # type: (...) -> None
     filename = str(tmp_path / "gunicorn.pprof")
     monkeypatch.setenv("DD_PROFILING_OUTPUT_PPROF", filename)
+    print(filename)
 
-    proc = gunicorn("-w", "3", *args)
+    proc = gunicorn("-w", "1", *args)
+
     time.sleep(3)
-    proc.terminate()
+
+    response = None
+    try:
+        response = requests.get("http://127.0.0.1:7643")
+    except Exception as e:
+        print(e)
+    finally:
+        proc.terminate()
+
+    if response:
+        print(response.status_code, response.text)
+
     output = proc.stdout.read().decode()
     worker_pids = _get_worker_pids(output)
 
-    assert len(worker_pids) == 3, output
+    print(worker_pids)
+    for line in output.splitlines():
+        print(line)
+
+    assert len(worker_pids) == 1, output
     assert proc.wait() == 0, output
     assert "module 'threading' has no attribute '_active'" not in output, output
 
-    profile = pprof_utils.parse_profile("%s.%d" % (filename, proc.pid))
-    samples = pprof_utils.get_samples_with_value_type(profile, "cpu-time")
-    assert len(samples) > 0
+    for pid in worker_pids:
+        print("%s.%d" % (filename, pid))
+        profile = pprof_utils.parse_profile("%s.%d" % (filename, pid))
+        samples = pprof_utils.get_samples_with_value_type(profile, "cpu-time")
+        assert len(samples) > 0
+
+        pprof_utils.assert_has_samples(
+            profile,
+            expected_samples=pprof_utils.StackEvent(
+                locations=[pprof_utils.StackLocation(function_name="fib", filename="gunicorn-app.py", line_no=8)]
+            ),
+        )
 
 
 def test_gunicorn(gunicorn, tmp_path, monkeypatch):
