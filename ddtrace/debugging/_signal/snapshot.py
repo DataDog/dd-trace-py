DataDog/dd-trace-py

commit 3748d6938bbe141d1424db10594221e84afb2ef0

refactor(di): encapsulate common signal code (#11153)
We encapsulate common signal boilerplate code to simplify the coding of
signals and ensure homogeneous behaviour among them.

## Checklist
- [x] PR author has checked that all the criteria below are met
- The PR description includes an overview of the change
- The PR description articulates the motivation for the change
- The change includes tests OR the PR description describes a testing
strategy
- The PR description notes risks associated with the change, if any
- Newly-added code is easy to change
- The change follows the [library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
- The change includes or references documentation updates if necessary
- Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))

## Reviewer Checklist
- [x] Reviewer has checked that all the criteria below are met 
- Title is accurate
- All changes are related to the pull request's stated goal
- Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes
- Testing strategy adequately addresses listed risks
- Newly-added code is easy to change
- Release note makes sense to a user of the library
- If necessary, author has acknowledged and discussed the performance
implications of this PR as reported in the benchmarks PR comment
- Backport labels are set in a manner that is consistent with the
[release branch maintenance
policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

ddtrace/debugging/_signal/snapshot.py
@@ -1,4 +1,3 @@
-from collections import ChainMap
 from dataclasses import dataclass
 from dataclasses import field
 from itertools import chain
@@ -18,10 +17,7 @@ from ddtrace.debugging._probe.model import CaptureLimits
 from ddtrace.debugging._probe.model import FunctionLocationMixin
 from ddtrace.debugging._probe.model import LineLocationMixin
 from ddtrace.debugging._probe.model import LiteralTemplateSegment
-from ddtrace.debugging._probe.model import LogFunctionProbe
-from ddtrace.debugging._probe.model import LogLineProbe
 from ddtrace.debugging._probe.model import LogProbeMixin
-from ddtrace.debugging._probe.model import ProbeEvaluateTimingForMethod
 from ddtrace.debugging._probe.model import TemplateSegment
 from ddtrace.debugging._redaction import REDACTED_PLACEHOLDER
 from ddtrace.debugging._redaction import DDRedactedExpressionError
@@ -31,10 +27,8 @@ from ddtrace.debugging._safety import get_locals
 from ddtrace.debugging._signal import utils
 from ddtrace.debugging._signal.model import EvaluationError
 from ddtrace.debugging._signal.model import LogSignal
-from ddtrace.debugging._signal.model import SignalState
 from ddtrace.debugging._signal.utils import serialize
 from ddtrace.internal.compat import ExcInfoType
-from ddtrace.internal.rate_limiter import RateLimitExceeded
 from ddtrace.internal.utils.time import HourGlass
 
 
@@ -126,92 +120,25 @@ class Snapshot(LogSignal):
         probe = cast(LogProbeMixin, self.probe)
         self._message = "".join([self._eval_segment(s, _locals) for s in probe.segments])
 
-    def enter(self):
-        if not isinstance(self.probe, LogFunctionProbe):
-            return
-
-        probe = self.probe
+    def _do(self, retval, exc_info, scope):
+        probe = cast(LogProbeMixin, self.probe)
         frame = self.frame
 
-        if probe.evaluate_at == ProbeEvaluateTimingForMethod.EXIT:
-            return
+        self._eval_message(scope)
 
-        scope = ChainMap(self.args, frame.f_globals)
+        self._stack = utils.capture_stack(self.frame)
 
-        if not self._eval_condition(scope):
-            return
+        return _capture_context(frame, exc_info, retval=retval, limits=probe.limits) if probe.take_snapshot else None
 
-        if probe.limiter.limit() is RateLimitExceeded:
-            self.state = SignalState.SKIP_RATE
-            return
+    def enter(self, scope: Mapping[str, Any]) -> None:
+        self.entry_capture = self._do(_NOTSET, (None, None, None), scope)
 
-        if probe.take_snapshot:
-            self.entry_capture = _capture_context(frame, (None, None, None), limits=probe.limits)
-
-        if probe.evaluate_at == ProbeEvaluateTimingForMethod.ENTER:
-            self._eval_message(scope)
-            self.state = SignalState.DONE
-
-    def exit(self, retval, exc_info, duration):
-        if not isinstance(self.probe, LogFunctionProbe):
-            return
-
-        probe = self.probe
-        full_scope = self.get_full_scope(retval, exc_info, duration)
-
-        if probe.evaluate_at == ProbeEvaluateTimingForMethod.EXIT:
-            if not self._eval_condition(full_scope):
-                return
-            if probe.limiter.limit() is RateLimitExceeded:
-                self.state = SignalState.SKIP_RATE
-                return
-        elif self.state not in {SignalState.NONE, SignalState.DONE}:
-            return
-
-        if probe.take_snapshot:
-            self.return_capture = _capture_context(self.frame, exc_info, retval=retval, limits=probe.limits)
-
+    def exit(self, retval, exc_info, duration, scope) -> None:
         self.duration = duration
-        self.state = SignalState.DONE
-        if probe.evaluate_at != ProbeEvaluateTimingForMethod.ENTER:
-            self._eval_message(full_scope)
+        self.return_capture = self._do(retval, exc_info, scope)
 
-        stack = utils.capture_stack(self.frame)
-
-        # Fix the line number of the top frame. This might have been mangled by
-        # the instrumented exception handling of function probes.
-        tb = exc_info[2]
-        while tb is not None:
-            frame = tb.tb_frame
-            if frame == self.frame:
-                stack[0]["lineNumber"] = tb.tb_lineno
-                break
-            tb = tb.tb_next
-
-        self._stack = stack
-
-    def line(self):
-        if not isinstance(self.probe, LogLineProbe):
-            return
-
-        frame = self.frame
-        probe = self.probe
-
-        if not self._eval_condition(frame.f_locals):
-            return
-
-        if probe.take_snapshot:
-            if probe.limiter.limit() is RateLimitExceeded:
-                self.state = SignalState.SKIP_RATE
-                return
-
-            self.line_capture = _capture_context(frame, sys.exc_info(), limits=probe.limits)
-
-        self._eval_message(ChainMap(frame.f_locals, frame.f_globals))
-
-        self._stack = utils.capture_stack(frame)
-
-        self.state = SignalState.DONE
+    def line(self, scope) -> None:
+        self.line_capture = self._do(_NOTSET, sys.exc_info(), scope)
 
     @property
     def message(self) -> Optional[str]:

tests/debugging/test_debugger.py
@@ -12,7 +12,7 @@ from ddtrace.constants import ORIGIN_KEY
 from ddtrace.debugging._debugger import DebuggerWrappingContext
 from ddtrace.debugging._probe.model import DDExpression
 from ddtrace.debugging._probe.model import MetricProbeKind
-from ddtrace.debugging._probe.model import ProbeEvaluateTimingForMethod
+from ddtrace.debugging._probe.model import ProbeEvalTiming
 from ddtrace.debugging._probe.model import SpanDecoration
 from ddtrace.debugging._probe.model import SpanDecorationTag
 from ddtrace.debugging._probe.model import SpanDecorationTargetSpan
@@ -20,6 +20,7 @@ from ddtrace.debugging._probe.registry import _get_probe_location
 from ddtrace.debugging._redaction import REDACTED_PLACEHOLDER as REDACTED
 from ddtrace.debugging._redaction import dd_compile_redacted as dd_compile
 from ddtrace.debugging._signal.model import SignalState
+from ddtrace.debugging._signal.snapshot import _EMPTY_CAPTURED_CONTEXT
 from ddtrace.debugging._signal.tracing import SPAN_NAME
 from ddtrace.debugging._signal.utils import redacted_value
 from ddtrace.internal.remoteconfig.worker import remoteconfig_poller
@@ -181,10 +182,7 @@ def test_debugger_function_probe_on_instance_method():
     assert snapshot_data["stack"][0]["fileName"].endswith("stuff.py")
     assert snapshot_data["stack"][0]["function"] == "instancestuff"
 
-    entry_capture = snapshot_data["captures"]["entry"]
-    assert set(entry_capture["arguments"].keys()) == {"self", "bar"}
-    assert entry_capture["locals"] == {}
-    assert entry_capture["throwable"] is None
+    assert snapshot_data["captures"]["entry"] == _EMPTY_CAPTURED_CONTEXT
 
     return_capture = snapshot_data["captures"]["return"]
     assert set(return_capture["arguments"].keys()) == {"self", "bar"}
@@ -776,7 +774,7 @@ def test_debugger_condition_eval_error_get_reported_once():
         assert "'foo'" == evaluationErrors[0]["message"]
 
 
-def test_debugger_function_probe_eval_on_enter():
+def test_debugger_function_probe_eval_on_entry():
     from tests.submod.stuff import mutator
 
     with debugger() as d:
@@ -785,7 +783,7 @@ def test_debugger_function_probe_eval_on_enter():
                 probe_id="enter-probe",
                 module="tests.submod.stuff",
                 func_qname="mutator",
-                evaluate_at=ProbeEvaluateTimingForMethod.ENTER,
+                evaluate_at=ProbeEvalTiming.ENTRY,
                 condition=DDExpression(
                     dsl="not(contains(arg,42))", callable=dd_compile({"not": {"contains": [{"ref": "arg"}, 42]}})
                 ),
@@ -824,7 +822,7 @@ def test_debugger_function_probe_eval_on_exit():
                 probe_id="exit-probe",
                 module="tests.submod.stuff",
                 func_qname="mutator",
-                evaluate_at=ProbeEvaluateTimingForMethod.EXIT,
+                evaluate_at=ProbeEvalTiming.EXIT,
                 condition=DDExpression(dsl="contains(arg,42)", callable=dd_compile({"contains": [{"ref": "arg"}, 42]})),
             )
         )
@@ -873,7 +871,7 @@ def test_debugger_lambda_fuction_access_locals():
             assert snapshot, d.test_queue
 
 
-def test_debugger_log_live_probe_generate_messages():
+def test_debugger_log_line_probe_generate_messages():
     from tests.submod.stuff import Stuff
 
     with debugger(upload_flush_interval=float("inf")) as d:
@@ -1027,7 +1025,7 @@ class SpanProbeTestCase(TracerTestCase):
                     probe_id="span-decoration",
                     module="tests.submod.stuff",
                     func_qname="mutator",
-                    evaluate_at=ProbeEvaluateTimingForMethod.EXIT,
+                    evaluate_at=ProbeEvalTiming.EXIT,
                     target_span=SpanDecorationTargetSpan.ACTIVE,
                     decorations=[
                         SpanDecoration(
@@ -1142,7 +1140,7 @@ def test_debugger_redacted_identifiers():
                 probe_id="function-probe",
                 module="tests.submod.stuff",
                 func_qname="sensitive_stuff",
-                evaluate_at=ProbeEvaluateTimingForMethod.EXIT,
+                evaluate_at=ProbeEvalTiming.EXIT,
             ),
         )
 
@@ -1222,7 +1220,7 @@ def test_debugger_exception_conditional_function_probe():
             probe_id="probe-instance-method",
             module="tests.submod.stuff",
             func_qname="throwexcstuff",
-            evaluate_at=ProbeEvaluateTimingForMethod.EXIT,
+            evaluate_at=ProbeEvalTiming.EXIT,
             condition=DDExpression(
                 dsl="expr.__class__.__name__ == 'Exception'",
                 callable=dd_compile(

ddtrace/debugging/_debugger.py
@@ -45,7 +45,6 @@ from ddtrace.debugging._probe.remoteconfig import ProbePollerEventType
 from ddtrace.debugging._probe.remoteconfig import ProbeRCAdapter
 from ddtrace.debugging._probe.status import ProbeStatusLogger
 from ddtrace.debugging._signal.collector import SignalCollector
-from ddtrace.debugging._signal.collector import SignalContext
 from ddtrace.debugging._signal.metric_sample import MetricSample
 from ddtrace.debugging._signal.model import Signal
 from ddtrace.debugging._signal.model import SignalState
@@ -170,7 +169,7 @@ class DebuggerWrappingContext(WrappingContext):
     def has_probes(self) -> bool:
         return bool(self.probes)
 
-    def _open_contexts(self) -> None:
+    def _open_signals(self) -> None:
         frame = self.__frame__
         assert frame is not None  # nosec
 
@@ -184,7 +183,7 @@ class DebuggerWrappingContext(WrappingContext):
         for p in self.probes.values():
             (context_creators if p.__context_creator__ else context_consumers).append(p)
 
-        contexts: Deque[SignalContext] = deque()
+        signals: Deque[Signal] = deque()
 
         # Trigger the context creators first, so that the new context can be
         # consumed by the consumers.
@@ -225,22 +224,32 @@ class DebuggerWrappingContext(WrappingContext):
                 log.error("Unsupported probe type: %s", type(probe))
                 continue
 
-            contexts.append(self._collector.attach(signal))
+            try:
+                signal.do_enter()
+            except Exception:
+                log.exception("Failed to enter signal %r", signal)
+                continue
+            signals.append(signal)
 
         # Save state on the wrapping context
         self.set("start_time", compat.monotonic_ns())
-        self.set("contexts", contexts)
+        self.set("signals", signals)
 
-    def _close_contexts(self, retval=None, exc_info=(None, None, None)) -> None:
+    def _close_signals(self, retval=None, exc_info=(None, None, None)) -> None:
         end_time = compat.monotonic_ns()
-        contexts = self.get("contexts")
-        while contexts:
-            # Open probe signal contexts are ordered, with those that have
-            # created new tracing context first. We need to finalise them in
-            # reverse order, so we pop them from the end of the queue (LIFO).
-            context = contexts.pop()
-            context.exit(retval, exc_info, end_time - self.get("start_time"))
-            signal = context.signal
+        signals = cast(Deque[Signal], self.get("signals"))
+        while signals:
+            # Open probe signals are ordered, with those that have created new
+            # tracing context first. We need to finalize them in reverse order,
+            # so we pop them from the end of the queue (LIFO).
+            signal = signals.pop()
+            try:
+                signal.do_exit(retval, exc_info, end_time - self.get("start_time"))
+            except Exception:
+                log.exception("Failed to exit signal %r", signal)
+                continue
+
+            self._collector.push(signal)
             if signal.state is SignalState.DONE:
                 self._probe_registry.set_emitting(signal.probe)
 
@@ -248,7 +257,7 @@ class DebuggerWrappingContext(WrappingContext):
         super().__enter__()
 
         try:
-            self._open_contexts()
+            self._open_signals()
         except Exception:
             log.exception("Failed to open debugging contexts")
 
@@ -256,7 +265,7 @@ class DebuggerWrappingContext(WrappingContext):
 
     def __return__(self, value: T) -> T:
         try:
-            self._close_contexts(retval=value)
+            self._close_signals(retval=value)
         except Exception:
             log.exception("Failed to close debugging contexts from return")
         return super().__return__(value)
@@ -265,7 +274,7 @@ class DebuggerWrappingContext(WrappingContext):
         self, exc_type: Optional[Type[BaseException]], exc_val: Optional[BaseException], exc_tb: Optional[TracebackType]
     ) -> None:
         try:
-            self._close_contexts(exc_info=(exc_type, exc_val, exc_tb))
+            self._close_signals(exc_info=(exc_type, exc_val, exc_tb))
         except Exception:
             log.exception("Failed to close debugging contexts from exception block")
         super().__exit__(exc_type, exc_val, exc_tb)
@@ -412,13 +421,13 @@ class Debugger(Service):
                 log.error("Unsupported probe type: %r", type(probe))
                 return
 
-            signal.line()
-
-            log.debug("[%s][P: %s] Debugger. Report signal %s", os.getpid(), os.getppid(), signal)
-            self.__uploader__.get_collector().push(signal)
+            signal.do_line()
 
             if signal.state is SignalState.DONE:
                 self._probe_registry.set_emitting(probe)
+
+            log.debug("[%s][P: %s] Debugger. Report signal %s", os.getpid(), os.getppid(), signal)
+            self.__uploader__.get_collector().push(signal)
 
         except Exception:
             log.error("Failed to execute probe hook", exc_info=True)

ddtrace/debugging/_exception/replay.py
@@ -188,7 +188,11 @@ class SpanExceptionHandler:
                         )
 
                         # Capture
-                        snapshot.line()
+                        try:
+                            snapshot.do_line()
+                        except Exception:
+                            log.exception("Error capturing exception replay snapshot %r", snapshot)
+                            continue
 
                         # Collect
                         self.__uploader__.get_collector().push(snapshot)

ddtrace/debugging/_expressions.py
@@ -32,6 +32,7 @@ from typing import Any
 from typing import Callable
 from typing import Dict
 from typing import List
+from typing import Mapping
 from typing import Optional
 from typing import Tuple
 from typing import Union
@@ -345,7 +346,7 @@ class DDCompiler:
             self._compile_direct_predicate(ast) or self._compile_arg_predicate(ast) or self._compile_value_source(ast)
         )
 
-    def compile(self, ast: DDASTType) -> Callable[[Dict[str, Any]], Any]:
+    def compile(self, ast: DDASTType) -> Callable[[Mapping[str, Any]], Any]:
         return self._make_function(ast, ("_locals",), "<expr>")
 
 
@@ -375,24 +376,24 @@ class DDExpression:
     __compiler__ = dd_compile
 
     dsl: str
-    callable: Callable[[Dict[str, Any]], Any]
+    callable: Callable[[Mapping[str, Any]], Any]
 
-    def eval(self, _locals):
+    def eval(self, scope: Mapping[str, Any]) -> Any:
         try:
-            return self.callable(_locals)
+            return self.callable(scope)
         except Exception as e:
             raise DDExpressionEvaluationError(self.dsl, e) from e
 
-    def __call__(self, _locals):
-        return self.eval(_locals)
+    def __call__(self, scope: Mapping[str, Any]) -> Any:
+        return self.eval(scope)
 
     @classmethod
-    def on_compiler_error(cls, dsl: str, exc: Exception) -> Callable[[Dict[str, Any]], Any]:
+    def on_compiler_error(cls, dsl: str, exc: Exception) -> Callable[[Mapping[str, Any]], Any]:
         log.error("Cannot compile expression: %s", dsl, exc_info=True)
         return _invalid_expression
 
     @classmethod
-    def compile(cls, expr: Dict[str, Any]) -> "DDExpression":
+    def compile(cls, expr: Mapping[str, Any]) -> "DDExpression":
         ast = expr["json"]
         dsl = expr["dsl"]
 

ddtrace/debugging/_origin/span.py
@@ -19,10 +19,10 @@ from ddtrace.debugging._probe.model import DEFAULT_CAPTURE_LIMITS
 from ddtrace.debugging._probe.model import LiteralTemplateSegment
 from ddtrace.debugging._probe.model import LogFunctionProbe
 from ddtrace.debugging._probe.model import LogLineProbe
-from ddtrace.debugging._probe.model import ProbeEvaluateTimingForMethod
-from ddtrace.debugging._signal.collector import SignalContext
+from ddtrace.debugging._probe.model import ProbeEvalTiming
 
 # from ddtrace.debugging._signal.snapshot import Snapshot
+from ddtrace.debugging._signal.model import Signal
 from ddtrace.ext import EXIT_SPAN_TYPES
 from ddtrace.internal import compat
 from ddtrace.internal import core
@@ -63,7 +63,7 @@ class EntrySpanProbe(LogFunctionProbe):
             tags={},
             module=module,
             func_qname=function,
-            evaluate_at=ProbeEvaluateTimingForMethod.ENTER,
+            evaluate_at=ProbeEvalTiming.ENTRY,
             template=message,
             segments=[LiteralTemplateSegment(message)],
             take_snapshot=True,
@@ -174,21 +174,21 @@ class EntrySpanWrappingContext(WrappingContext):
 
         return self
 
-    def _close_context(self, retval=None, exc_info=(None, None, None)):
+    def _close_signal(self, retval=None, exc_info=(None, None, None)):
         try:
-            context: SignalContext = self.get("context")
+            signal: Signal = t.cast(Signal, self.get("signal"))
         except KeyError:
             # No snapshot was created
             return
 
-        context.exit(retval, exc_info, compat.monotonic_ns() - self.get("start_time"))
+        signal.do_exit(retval, exc_info, compat.monotonic_ns() - self.get("start_time"))
 
     def __return__(self, retval):
-        self._close_context(retval=retval)
+        self._close_signal(retval=retval)
         return super().__return__(retval)
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self._close_context(exc_info=(exc_type, exc_value, traceback))
+        self._close_signal(exc_info=(exc_type, exc_value, traceback))
         super().__exit__(exc_type, exc_value, traceback)
 
 

ddtrace/debugging/_probe/model.py
@@ -156,9 +156,9 @@ class LineLocationMixin(ProbeLocationMixin):
         return (maybe_stringify(self.resolved_source_file), self.line)
 
 
-class ProbeEvaluateTimingForMethod(str, Enum):
+class ProbeEvalTiming(str, Enum):
     DEFAULT = "DEFAULT"
-    ENTER = "ENTER"
+    ENTRY = "ENTRY"
     EXIT = "EXIT"
 
 
@@ -166,10 +166,14 @@ class ProbeEvaluateTimingForMethod(str, Enum):
 class FunctionLocationMixin(ProbeLocationMixin):
     module: str = field(compare=False)
     func_qname: str = field(compare=False)
-    evaluate_at: ProbeEvaluateTimingForMethod
 
     def location(self):
         return (self.module, self.func_qname)
+
+
+@dataclass
+class TimingMixin(AbstractProbeMixIn):
+    evaluate_at: ProbeEvalTiming
 
 
 class MetricProbeKind(str, Enum):
@@ -192,7 +196,7 @@ class MetricLineProbe(Probe, LineLocationMixin, MetricProbeMixin, ProbeCondition
 
 
 @dataclass
-class MetricFunctionProbe(Probe, FunctionLocationMixin, MetricProbeMixin, ProbeConditionMixin):
+class MetricFunctionProbe(Probe, FunctionLocationMixin, TimingMixin, MetricProbeMixin, ProbeConditionMixin):
     pass
 
 
@@ -245,7 +249,7 @@ class LogLineProbe(Probe, LineLocationMixin, LogProbeMixin, ProbeConditionMixin,
 
 
 @dataclass
-class LogFunctionProbe(Probe, FunctionLocationMixin, LogProbeMixin, ProbeConditionMixin, RateLimitMixin):
+class LogFunctionProbe(Probe, FunctionLocationMixin, TimingMixin, LogProbeMixin, ProbeConditionMixin, RateLimitMixin):
     pass
 
 
@@ -288,7 +292,7 @@ class SpanDecorationLineProbe(Probe, LineLocationMixin, SpanDecorationMixin):
 
 
 @dataclass
-class SpanDecorationFunctionProbe(Probe, FunctionLocationMixin, SpanDecorationMixin):
+class SpanDecorationFunctionProbe(Probe, FunctionLocationMixin, TimingMixin, SpanDecorationMixin):
     pass
 
 

ddtrace/debugging/_probe/remoteconfig.py
@@ -24,7 +24,7 @@ from ddtrace.debugging._probe.model import LogLineProbe
 from ddtrace.debugging._probe.model import MetricFunctionProbe
 from ddtrace.debugging._probe.model import MetricLineProbe
 from ddtrace.debugging._probe.model import Probe
-from ddtrace.debugging._probe.model import ProbeEvaluateTimingForMethod
+from ddtrace.debugging._probe.model import ProbeEvalTiming
 from ddtrace.debugging._probe.model import ProbeType
 from ddtrace.debugging._probe.model import SpanDecoration
 from ddtrace.debugging._probe.model import SpanDecorationFunctionProbe
@@ -33,6 +33,7 @@ from ddtrace.debugging._probe.model import SpanDecorationTag
 from ddtrace.debugging._probe.model import SpanFunctionProbe
 from ddtrace.debugging._probe.model import StringTemplate
 from ddtrace.debugging._probe.model import TemplateSegment
+from ddtrace.debugging._probe.model import TimingMixin
 from ddtrace.debugging._probe.status import ProbeStatusLogger
 from ddtrace.debugging._redaction import DDRedactedExpression
 from ddtrace.internal.logger import get_logger
@@ -103,7 +104,8 @@ class ProbeFactory(object):
 
         args["module"] = where.get("type") or where["typeName"]
         args["func_qname"] = where.get("method") or where["methodName"]
-        args["evaluate_at"] = ProbeEvaluateTimingForMethod[attribs.get("evaluateAt", "DEFAULT")]
+        if isinstance(cls.__function_class__, TimingMixin):
+            args["evaluate_at"] = ProbeEvalTiming[attribs.get("evaluateAt", "DEFAULT")]
 
         return cls.__function_class__(**args)
 

commit ba89ba3b0ad1eb3c37ac84761e4bb79fb982e8a1

chore(di): fix top of stack in function snapshots
We fix a regression introduced with #11153 whereby the top of the stack
might have incorrect line number information

ddtrace/debugging/_signal/snapshot.py
@@ -137,6 +137,17 @@ class Snapshot(LogSignal):
         self.duration = duration
         self.return_capture = self._do(retval, exc_info, scope)
 
+        # Fix the line number of the top frame. This might have been mangled by
+        # the instrumented exception handling of function probes.
+        assert self._stack is not None  # nosec B101
+        tb = exc_info[2]
+        while tb is not None:
+            frame = tb.tb_frame
+            if frame == self.frame:
+                self._stack[0]["lineNumber"] = tb.tb_lineno
+                break
+            tb = tb.tb_next
+
     def line(self, scope) -> None:
         self.line_capture = self._do(_NOTSET, sys.exc_info(), scope)
 

tests/debugging/test_debugger.py
@@ -207,6 +207,7 @@ def test_debugger_function_probe_on_function_with_exception():
     snapshot_data = snapshot["debugger"]["snapshot"]
     assert snapshot_data["stack"][0]["fileName"].endswith("stuff.py")
     assert snapshot_data["stack"][0]["function"] == "throwexcstuff"
+    assert snapshot_data["stack"][0]["lineNumber"] == 110
 
     entry_capture = snapshot_data["captures"]["entry"]
     assert entry_capture["arguments"] == {}