ff03dee96fcbeb4716682118401bda4a32467122
David Sanchez <838104+sanchda@users.noreply.github.com>
2024-11-06 10:13:32 -0600

    Update ddtrace/internal/datadog/profiling/dd_wrapper/include/constants.hpp
    Co-authored-by: Brett Langdon <brett.langdon@datadoghq.com>

--- ddtrace/internal/datadog/profiling/dd_wrapper/include/constants.hpp
+++ ddtrace/internal/datadog/profiling/dd_wrapper/include/constants.hpp
@@ -11,7 +11,7 @@ constexpr unsigned int g_default_max_nframes = 64;
 // their stacks may be silently truncated, which is unfortunate.
 constexpr unsigned int g_backend_max_nframes = 512;
 
-// Maximum amount of time, in seconds, to wait for crashtracker signal handler
+// Maximum amount of time, in milliseconds, to wait for crashtracker signal handler
 constexpr uint64_t g_crashtracker_timeout_ms = 5000;
 
 // Default value for the max number of samples to keep in the SynchronizedSamplePool
