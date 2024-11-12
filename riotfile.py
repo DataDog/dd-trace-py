f6fde4ddb948d96ea5e9d1e89856762a11a72cae
Romain Komorn <romain.komorn@datadoghq.com>
2024-11-12 14:22:08 +0000

    only instrument user code and don't instrument coverage module

--- riotfile.py
+++ riotfile.py
@@ -1713,7 +1713,7 @@ venv = Venv(
         ),
         Venv(
             name="pytest-benchmark",
-            command="pytest {cmdargs} --no-cov tests/contrib/pytest_benchmark/",
+            command="pytest {cmdargs} --no-ddtrace --no-cov tests/contrib/pytest_benchmark/",
             pkgs={
                 "msgpack": latest,
                 "pytest-randomly": latest,
