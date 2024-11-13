50db32d75421fcd581e5edb598a7f3e5a93bf6aa
Romain Komorn <romain.komorn@datadoghq.com>
2024-11-13 09:44:11 +0000

    force output and env

--- riotfile.py
+++ riotfile.py
@@ -106,6 +106,7 @@ venv = Venv(
         "DD_PATCH_MODULES": "unittest:false",
         "CMAKE_BUILD_PARALLEL_LEVEL": "12",
         "_DD_CIVISIBILITY_USE_PYTEST_V2": "true",
+        "DD_ENV": "itrtest",
     },
     venvs=[
         Venv(
