0ba0cda64e96a115c04a1ea04a25bb14a1d400df
wantsui <wan.tsui@datadoghq.com>
2024-11-15 14:25:55 -0500

    Update tests/utils.py
    Co-authored-by: Quinna Halim <quinna.halim@datadoghq.com>

--- tests/utils.py
+++ tests/utils.py
@@ -1341,9 +1341,7 @@ def _should_skip(condition=None, until: int = None):
         until = dt.datetime.fromtimestamp(until)
     if until and dt.datetime.now(dt.timezone.utc).replace(tzinfo=None) < until.replace(tzinfo=None):
         return True
-    if condition is not None and condition:
-        return True
-    return False
+return condition is not None and condition
 
 
 def flaky(until: int = None, condition: bool = None, reason: str = None):
