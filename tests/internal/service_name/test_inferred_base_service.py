7297779d1553b616e42bdc34279ba97eba4258de
William Conti <58711692+wconti27@users.noreply.github.com>
2024-11-19 15:17:14 -0500

    chore(tracing): fix inferred base service for pytest command ran with --ddtrace optional argument (#11394)
    # Motivation
    
    Add special case for `--ddtrace` pytest argument, that was causing CI tests to have a different service name than local testing. CI testing adds the `--ddtrace` arg to the command. Previously the code would skip any args that started with `-` as well as the following arg, but that should not be the case for this arg.

--- tests/internal/service_name/test_inferred_base_service.py
+++ tests/internal/service_name/test_inferred_base_service.py
@@ -20,6 +20,9 @@ def mock_file_system():
         (base_path / "venv" / "bin" / "python3.11").mkdir(parents=True)
         (base_path / "venv" / "bin" / "gunicorn").mkdir(parents=True)
 
+        # add a test dir
+        (base_path / "tests" / "contrib" / "aiohttp").mkdir(parents=True)
+
         (base_path / "modules" / "m1" / "first" / "nice" / "package").mkdir(parents=True)
         (base_path / "modules" / "m2").mkdir(parents=True)
         (base_path / "modules" / "no" / "python_files").mkdir(parents=True)
@@ -40,6 +43,11 @@ def mock_file_system():
         (base_path / "apps" / "app1" / "__main__.py").touch()
         (base_path / "apps" / "app2" / "cmd" / "run.py").touch()
         (base_path / "apps" / "app2" / "setup.py").touch()
+
+        (base_path / "tests" / "contrib" / "aiohttp" / "test.py").touch()
+        (base_path / "tests" / "contrib" / "aiohttp" / "__init__.py").touch()
+        (base_path / "tests" / "contrib" / "__init__.py").touch()
+        (base_path / "tests" / "__init__.py").touch()
 
         # Additional edge cases
         (base_path / "modules" / "no" / "python_files" / "here.txt").touch()  # Module with no subdirectories
@@ -69,6 +77,9 @@ def mock_file_system():
         ("python3.12 apps/app2/cmd/run.py", "app2"),
         ("python -m m1.first.nice.package", "m1.first.nice.package"),
         ("python -m http.server 8000", "http.server"),
+        # pytest
+        ("pytest tests/contrib/aiohttp", "tests.contrib.aiohttp"),
+        ("pytest --ddtrace tests/contrib/aiohttp", "tests.contrib.aiohttp"),
     ],
 )
 def test_python_detector(cmd, expected, mock_file_system):
