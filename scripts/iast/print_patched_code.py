d30ea54f2c3790024e35cfac9ab9f4d593c6fcb9
Federico Mon <federico.mon@datadoghq.com>
2024-11-20 19:25:17 +0100

    add tool to debug iast patched code

---
+++ scripts/iast/print_patched_code.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import sys
+from typing import Text
+
+
+def _get_patched_code(module_path: Text, module_name: Text) -> str:
+    """
+    Print the patched code to stdout, for debugging purposes.
+    """
+    import astunparse
+
+    from ddtrace.appsec._iast._ast.ast_patching import get_encoding
+    from ddtrace.appsec._iast._ast.ast_patching import visit_ast
+
+    with open(module_path, "r", encoding=get_encoding(module_path)) as source_file:
+        source_text = source_file.read()
+
+        new_source = visit_ast(
+            source_text,
+            module_path,
+            module_name=module_name,
+        )
+
+        # If no modifications are done,
+        # visit_ast returns None
+        if not new_source:
+            return ""
+
+        new_code = astunparse.unparse(new_source)
+        return new_code
+
+
+if __name__ == "__main__":
+    MODULE_PATH = sys.argv[1]
+    print(_get_patched_code(MODULE_PATH, "MOCK_MODULE_NAME"))
