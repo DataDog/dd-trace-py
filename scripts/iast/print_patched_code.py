bfa015ce338af4fa304b58130fcaab48a5ad74b0
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-11-21 13:29:11 +0100

    new version
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- scripts/iast/print_patched_code.py
+++ scripts/iast/print_patched_code.py
@@ -1,40 +1,56 @@
+import os
+
 #!/usr/bin/env python3
 """
-Debug script to print the patched code of a Python file.
+Debug script to print the patched code of a Python file, optionally with syntax highlighting (with pygments) and code formatting (with black).
 Usage: PYTHONPATH=$PYTHONPATH:~/.../dd-trace-py/ python -m scripts.iast.print_patched_code /path/to/your/python-file.py
 """
 
 import sys
 from typing import Text
+import sys
+
+try:
+    from pygments import highlight
+    from pygments.lexers import PythonLexer
+    from pygments.formatters import TerminalFormatter
+
+    colorize = True
+except ImportError:
+    colorize = False
+
+try:
+    import black
+
+    format = True
+except ImportError:
+    format = False
+
+from ddtrace.appsec._iast._ast.ast_patching import astpatch_module
 
 
 def _get_patched_code(module_path: Text, module_name: Text) -> str:
     """
     Print the patched code to stdout, for debugging purposes.
+    Tips: have pygments and black installed for a nicer output.
     """
     import astunparse
 
-    from ddtrace.appsec._iast._ast.ast_patching import get_encoding
-    from ddtrace.appsec._iast._ast.ast_patching import visit_ast
-
-    with open(module_path, "r", encoding=get_encoding(module_path)) as source_file:
-        source_text = source_file.read()
-
-        new_source = visit_ast(
-            source_text,
-            module_path,
-            module_name=module_name,
-        )
-
-        # If no modifications are done,
-        # visit_ast returns None
-        if not new_source:
-            return ""
-
-        new_code = astunparse.unparse(new_source)
-        return new_code
+    module_dir = os.path.dirname(module_path)
+    sys.path.append(module_dir)
+    _, new_ast = astpatch_module(__import__(module_name))
+    return astunparse.unparse(new_ast)
 
 
 if __name__ == "__main__":
     MODULE_PATH = sys.argv[1]
-    print(_get_patched_code(MODULE_PATH, "MOCK_MODULE_NAME"))
+    MODULE_NAME = sys.argv[2]
+    code = _get_patched_code(MODULE_PATH, MODULE_NAME)
+
+    if format:
+        code = black.format_str(code, mode=black.FileMode())
+
+    if colorize:
+        code = highlight(code, PythonLexer(), TerminalFormatter())
+
+    print(code)
