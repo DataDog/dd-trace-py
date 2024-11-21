5be207643f9cb6ce81613428fb3c986ca4e9e160
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2024-11-21 13:37:16 +0100

    remove unneded arg
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- scripts/iast/print_patched_code.py
+++ scripts/iast/print_patched_code.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+Debug script to print the patched code of a Python file, optionally colorized (if you have pygments) and formatted (if you have black).
+Usage: PYTHONPATH=$PYTHONPATH:~/.../dd-trace-py/ python -m scripts.iast.print_patched_code /path/to/your/python_file.py
+"""
+
 import os
 
 #!/usr/bin/env python3
@@ -29,23 +36,19 @@ except ImportError:
 from ddtrace.appsec._iast._ast.ast_patching import astpatch_module
 
 
-def _get_patched_code(module_path: Text, module_name: Text) -> str:
-    """
-    Print the patched code to stdout, for debugging purposes.
-    Tips: have pygments and black installed for a nicer output.
-    """
+def _get_patched_code(module_path: Text) -> str:
     import astunparse
 
     module_dir = os.path.dirname(module_path)
     sys.path.append(module_dir)
+    module_name = os.path.splitext(os.path.basename(module_path))[0]
     _, new_ast = astpatch_module(__import__(module_name))
     return astunparse.unparse(new_ast)
 
 
 if __name__ == "__main__":
     MODULE_PATH = sys.argv[1]
-    MODULE_NAME = sys.argv[2]
-    code = _get_patched_code(MODULE_PATH, MODULE_NAME)
+    code = _get_patched_code(MODULE_PATH)
 
     if format:
         code = black.format_str(code, mode=black.FileMode())
