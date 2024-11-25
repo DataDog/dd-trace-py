de538d82169209b2a11f82d428a81610978c0b2d
Christophe Papazian <114495376+christophe-papazian@users.noreply.github.com>
2024-11-25 17:40:35 +0100

    fix(asm): add global states to ensure patching once (#11522)
    Ensure common patches for SCA and Exploit Prevention are loaded..
    - only once
    - only if exploit prevention is active or sca is active
    
    Changes:
    - factorize load_common_modules logic in ddtrace.appsec
    - boolean state for patch_common_module and enable_iast_propagation to
    ensure they are only called once.
    - ensure it's loaded after one click activation
    - ensure it's properly loaded in unit tests if required
    - add some failsafe for iast in wrap_open for importerror
    - update an iast test to reflect that common_modules is loaded in the
    test by default.
    
    APPSEC-55997
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    
    (cherry picked from commit 81824b8ba43c88164d4e709097c7772a9846150f)

--- tests/utils.py
+++ tests/utils.py
@@ -23,6 +23,7 @@ from ddtrace._trace.span import Span
 from ddtrace.constants import SPAN_MEASURED_KEY
 from ddtrace.ext import http
 from ddtrace.internal import agent
+from ddtrace.internal import core
 from ddtrace.internal.ci_visibility.writer import CIVisibilityWriter
 from ddtrace.internal.compat import httplib
 from ddtrace.internal.compat import parse
@@ -181,6 +182,7 @@ def override_global_config(values):
     # If ddtrace.settings.asm.config has changed, check _asm_can_be_enabled again
     ddtrace.settings.asm.config._eval_asm_can_be_enabled()
     try:
+        core.dispatch("test.config.override")
         yield
     finally:
         # Reset all to their original values
