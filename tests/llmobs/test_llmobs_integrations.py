11ea7ff7f0185b87a8ae07704053e1b1818e64bc
Nicole Cybul <nicole.cybul@datadoghq.com>
2024-11-26 09:21:49 -0500

    update tests with new history argument

--- tests/llmobs/test_llmobs_integrations.py
+++ tests/llmobs/test_llmobs_integrations.py
@@ -198,15 +198,15 @@ def test_llmobs_set_tags(mock_llmobs, mock_log, mock_integration_config):
     span = DummyTracer().trace("Dummy span", service="dummy_service")
     integration = BaseLLMIntegration(mock_integration_config)
     integration._llmobs_set_tags = mock.Mock()
-    integration.llmobs_set_tags(span, args=[], kwargs={}, response="response", operation="operation")
-    integration._llmobs_set_tags.assert_called_once_with(span, [], {}, "response", "operation")
+    integration.llmobs_set_tags(span, args=[], kwargs={}, response="response", operation="operation", history=[])
+    integration._llmobs_set_tags.assert_called_once_with(span, [], {}, "response", "operation", [])
 
     integration._llmobs_set_tags = mock.Mock(side_effect=AttributeError("Mocked Exception during _llmobs_set_tags()"))
     integration.llmobs_set_tags(
-        span, args=[1, 2, 3], kwargs={"a": 123}, response=[{"content": "hello"}], operation="operation"
+        span, args=[1, 2, 3], kwargs={"a": 123}, response=[{"content": "hello"}], operation="operation", history=[]
     )
     integration._llmobs_set_tags.assert_called_once_with(
-        span, [1, 2, 3], {"a": 123}, [{"content": "hello"}], "operation"
+        span, [1, 2, 3], {"a": 123}, [{"content": "hello"}], "operation", []
     )
     mock_log.error.assert_called_once_with(
         "Error extracting LLMObs fields for span %s, likely due to malformed data", span, exc_info=True
