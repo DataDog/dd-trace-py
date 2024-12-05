6ff4b9cbd3f07119e7332230e1475f53fba4f3f8
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-12-05 16:46:55 +0000

    delete as not used

--- ddtrace/internal/datadog/profiling/dd_wrapper/test/test_forking.cpp
+++ ddtrace/internal/datadog/profiling/dd_wrapper/test/test_forking.cpp
@@ -83,50 +83,6 @@ sample_in_threads_and_fork(unsigned int num_threads, unsigned int sleep_time_ns)
     std::exit(0);
 }
 
-// Really try to break things with many forks
-void
-fork_stress_test(unsigned int num_threads, unsigned int sleep_time_ns, unsigned int num_children)
-{
-    configure("my_test_service", "my_test_env", "0.0.1", "https://127.0.0.1:9126", "cpython", "3.10.6", "3.100", 256);
-    std::atomic<bool> done(false);
-    std::vector<std::thread> threads;
-    std::vector<unsigned int> ids;
-    for (unsigned int i = 0; i < num_threads; i++) {
-        ids.push_back(i);
-    }
-
-    // ddup is configured, launch threads
-    launch_samplers(ids, sleep_time_ns, threads, done);
-
-    std::vector<pid_t> children;
-    pthread_t thread;
-    if (pthread_create(&thread, nullptr, upload_in_thread, nullptr) != 0) {
-        std::cout << "failed to create thread" << std::endl;
-        std::exit(1);
-    }
-    while (num_children > 0) {
-        pid_t pid = fork();
-        if (pid == 0) {
-            // Child
-            profile_in_child(num_threads, 500e3, done); // Child profiles for 500ms
-        }
-        children.push_back(pid);
-        num_children--;
-    }
-
-    // Parent
-    int status;
-    done.store(true);
-    ddup_upload();
-    for (pid_t pid : children) {
-        waitpid(pid, &status, 0);
-        if (!is_exit_normal(status)) {
-            std::exit(1);
-        }
-    }
-    std::exit(0);
-}
-
 TEST(ForkDeathTest, SampleInThreadsAndForkNormal)
 {
     // Same memory leak as before--whatever
