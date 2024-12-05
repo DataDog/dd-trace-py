0aecc54856d48fbe4543f214eb48d4d3bbc8b124
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-12-05 18:31:29 +0000

    use simplistic shared vector protected by mutex

--- ddtrace/internal/datadog/profiling/dd_wrapper/include/synchronized_sample_pool.hpp
+++ ddtrace/internal/datadog/profiling/dd_wrapper/include/synchronized_sample_pool.hpp
@@ -12,33 +12,14 @@ namespace Datadog {
 class SynchronizedSamplePool
 {
   private:
-    moodycamel::ConcurrentQueue<Sample*> pool;
-    std::atomic<size_t> capacity;
+    std::mutex mtx;
+    std::vector<std::unique_ptr<Sample>> pool;
+    size_t capacity;
 
   public:
     SynchronizedSamplePool(size_t _capacity)
+      : capacity(_capacity)
     {
-        capacity.store(_capacity);
-        pool = moodycamel::ConcurrentQueue<Sample*>(_capacity);
-    }
-
-    ~SynchronizedSamplePool()
-    {
-        // number of successive calls to try_dequeue that returned false
-        std::atomic<size_t> cnt{ 0 };
-
-        while (cnt.load() < capacity.load()) {
-            Sample* sample = nullptr;
-            if (pool.try_dequeue(sample) && sample != nullptr) {
-                delete sample;
-                cnt.store(0);
-            } else {
-                cnt.fetch_add(1);
-                // Explicitly yield to make sure that other threads trying to
-                // push to the pool can proceed.
-                std::this_thread::yield();
-            }
-        }
     }
 
     std::optional<Sample*> take_sample();
--- ddtrace/internal/datadog/profiling/dd_wrapper/src/synchronized_sample_pool.cpp
+++ ddtrace/internal/datadog/profiling/dd_wrapper/src/synchronized_sample_pool.cpp
@@ -9,25 +9,26 @@ namespace Datadog {
 std::optional<Sample*>
 SynchronizedSamplePool::take_sample()
 {
-    Sample* sample = nullptr;
-    pool.try_dequeue(sample);
-
-    if (sample == nullptr) {
-        return std::nullopt;
+    const std::lock_guard<std::mutex> lock(mtx);
+    if (!pool.empty()) {
+        // Reuse a sample from the pool
+        auto sample = pool.back().release();
+        pool.pop_back();
+        return sample;
     }
-
-    return sample;
+    return std::nullopt;
 }
 
 std::optional<Sample*>
 SynchronizedSamplePool::return_sample(Sample* sample)
 {
+    const std::lock_guard<std::mutex> lock(mtx);
     // We don't want the pool to grow without a bound, so check the size and
     // discard the sample if it's larger than capacity.
-    if (capacity.load() <= pool.size_approx()) {
+    if (capacity <= pool.size()) {
         return sample;
     } else {
-        pool.enqueue(sample);
+        pool.emplace_back(sample);
         return std::nullopt;
     }
 }
