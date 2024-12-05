6545921612657c312b3105f5c2e6a5f13246dc1e
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-12-05 20:44:46 +0000

    remove tag mutex

--- ddtrace/internal/datadog/profiling/dd_wrapper/src/uploader_builder.cpp
+++ ddtrace/internal/datadog/profiling/dd_wrapper/src/uploader_builder.cpp
@@ -77,7 +77,6 @@ Datadog::UploaderBuilder::set_tag(std::string_view _key, std::string_view _val)
 {
 
     if (!_key.empty() && !_val.empty()) {
-        const std::lock_guard<std::mutex> lock(tag_mutex);
         user_tags[std::string(_key)] = std::string(_val);
     }
 }
