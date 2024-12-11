93cc7c9cac96bb054e0d39f81934d2c87bb333ab
Christophe Papazian <christophe.papazian@datadoghq.com>
2024-12-11 17:16:37 +0100

    add visibility to assert error

--- tests/appsec/integrations/test_flask_entrypoint_iast_patches.py
+++ tests/appsec/integrations/test_flask_entrypoint_iast_patches.py
@@ -22,7 +22,7 @@ def test_ddtrace_iast_flask_patch():
         str_output = dis_output.getvalue()
         # Should have replaced the binary op with the aspect in add_test:
         assert "(add_aspect)" in str_output
-        assert "BINARY_ADD" in str_output or "BINARY_OP" not in str_output
+        assert "BINARY_ADD" in str_output or "BINARY_OP" not in str_output, str_output
         # Should have replaced the app.run() with a pass:
         # assert "Disassembly of run" not in str_output, str_output
         del sys.modules["tests.appsec.iast.fixtures.entrypoint.app_main_patched"]
