18069f09f998d1d6249d764b1b1b6ea211a05a6f
Peter Griggs <peter.griggs@datadoghq.com>
2024-12-11 11:37:53 -0500

    ci(profiling): use Github Actions GPU runner for pytorch workflow.

--- tests/profiling/test_main.py
+++ tests/profiling/test_main.py
@@ -43,15 +43,6 @@ def test_call_script_pytorch_gpu(monkeypatch):
     stdout, stderr, exitcode, pid = call_program(
         sys.executable, os.path.join(os.path.dirname(__file__), "simple_program_pytorch_gpu.py")
     )
-    assert exitcode == 0, (stdout, stderr)
-
-
-@pytest.mark.skipif(not os.getenv("DD_PROFILING_PYTORCH_ENABLED", False), reason="Not testing pytorch CPU")
-def test_call_script_pytorch_cpu(monkeypatch):
-    monkeypatch.setenv("DD_PROFILING_API_TIMEOUT", "0.1")
-    stdout, stderr, exitcode, pid = call_program(
-        sys.executable, os.path.join(os.path.dirname(__file__), "simple_program_pytorch_cpu.py")
-    )
     print("stdout:", stdout.decode())
     print("stderr:", stderr.decode())
     assert exitcode == 0, (stdout, stderr)
