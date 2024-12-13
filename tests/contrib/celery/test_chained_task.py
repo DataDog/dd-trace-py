6945ed0ed60d72adc0ed90b4d644a4cfa7765ead
wan <wan.tsui@datadoghq.com>
2024-12-13 14:53:57 -0500

    Adjust regex for older versions of python.

--- tests/contrib/celery/test_chained_task.py
+++ tests/contrib/celery/test_chained_task.py
@@ -51,5 +51,5 @@ def test_task_chain_task_call_task():
     worker_logs = worker_process.stderr.read()
 
     # Check that the root span was created with one of the Celery specific tags, such as celery.correlation_id
-    pattern_match = r"resource=\'tests.contrib.celery.tasks.fn_a\' type=\'worker\' .* tags=.*correlation_id.*"
+    pattern_match = r"resource=\\'tests.contrib.celery.tasks.fn_a\\' type=\\'worker\\' .* tags=.*correlation_id.*"
     assert re.search(pattern_match, str(worker_logs)) is not None
