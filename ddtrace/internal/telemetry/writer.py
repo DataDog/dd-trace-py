8f70e242f878dc605b794e73469f5363bbe043fc
Munir Abdinur <munir.abdinur@datadoghq.com>
2024-12-16 16:22:57 -0500

    fix(telemetry): avoids stopping periodic threads in telemetry writer atexit hooks (#11708)
    Resolves: https://github.com/DataDog/dd-trace-py/issues/11622
    
    ## Checklist
    - [ ] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [ ] Reviewer has checked that all the criteria below are met
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    
    (cherry picked from commit 56bdd61173b2e66fc884362b88db95834064cebe)

--- ddtrace/internal/telemetry/writer.py
+++ ddtrace/internal/telemetry/writer.py
@@ -181,7 +181,6 @@ class TelemetryWriter(PeriodicService):
         self._forked = False  # type: bool
         self._events_queue = []  # type: List[Dict]
         self._configuration_queue = {}  # type: Dict[str, Dict]
-        self._lock = forksafe.Lock()  # type: forksafe.ResetObject
         self._imported_dependencies: Dict[str, str] = dict()
         self._product_enablement = {product.value: False for product in TELEMETRY_APM_PRODUCT}
         self._send_product_change_updates = False
@@ -245,10 +244,6 @@ class TelemetryWriter(PeriodicService):
         """
         self._enabled = False
         self.reset_queues()
-        if self._is_running():
-            self.stop()
-        else:
-            self.status = ServiceStatus.STOPPED
 
     def enable_agentless_client(self, enabled=True):
         # type: (bool) -> None
@@ -297,7 +292,7 @@ class TelemetryWriter(PeriodicService):
         :param bool auto_enabled: True if module is enabled in _monkey.PATCH_MODULES
         """
         # Integrations can be patched before the telemetry writer is enabled.
-        with self._lock:
+        with self._service_lock:
             if integration_name not in self._integrations_queue:
                 self._integrations_queue[integration_name] = {"name": integration_name}
 
@@ -382,20 +377,20 @@ class TelemetryWriter(PeriodicService):
     def _flush_integrations_queue(self):
         # type: () -> List[Dict]
         """Flushes and returns a list of all queued integrations"""
-        with self._lock:
+        with self._service_lock:
             integrations = list(self._integrations_queue.values())
             self._integrations_queue = dict()
         return integrations
 
     def _flush_new_imported_dependencies(self) -> Set[str]:
-        with self._lock:
+        with self._service_lock:
             new_deps = modules.get_newly_imported_modules()
         return new_deps
 
     def _flush_configuration_queue(self):
         # type: () -> List[Dict]
         """Flushes and returns a list of all queued configurations"""
-        with self._lock:
+        with self._service_lock:
             configurations = list(self._configuration_queue.values())
             self._configuration_queue = {}
         return configurations
@@ -414,7 +409,7 @@ class TelemetryWriter(PeriodicService):
         if not _TelemetryConfig.DEPENDENCY_COLLECTION or not self._enabled:
             return
 
-        with self._lock:
+        with self._service_lock:
             packages = update_imported_dependencies(self._imported_dependencies, newly_imported_deps)
 
         if packages:
@@ -451,7 +446,7 @@ class TelemetryWriter(PeriodicService):
             self._send_product_change_updates = True
 
     def remove_configuration(self, configuration_name):
-        with self._lock:
+        with self._service_lock:
             del self._configuration_queue[configuration_name]
 
     def add_configuration(self, configuration_name, configuration_value, origin="unknown"):
@@ -465,7 +460,7 @@ class TelemetryWriter(PeriodicService):
             # convert unsupported types to strings
             configuration_value = str(configuration_value)
 
-        with self._lock:
+        with self._service_lock:
             self._configuration_queue[configuration_name] = {
                 "name": configuration_name,
                 "origin": origin,
@@ -475,7 +470,7 @@ class TelemetryWriter(PeriodicService):
     def add_configurations(self, configuration_list):
         # type: (List[Tuple[str, Union[bool, float, str], str]]) -> None
         """Creates and queues a list of configurations"""
-        with self._lock:
+        with self._service_lock:
             for name, value, _origin in configuration_list:
                 self._configuration_queue[name] = {
                     "name": name,
@@ -566,7 +561,7 @@ class TelemetryWriter(PeriodicService):
 
     def _flush_log_metrics(self):
         # type () -> Set[Metric]
-        with self._lock:
+        with self._service_lock:
             log_metrics = self._logs
             self._logs = set()
         return log_metrics
@@ -652,7 +647,7 @@ class TelemetryWriter(PeriodicService):
     def _flush_events_queue(self):
         # type: () -> List[Dict]
         """Flushes and returns a list of all telemtery event"""
-        with self._lock:
+        with self._service_lock:
             events = self._events_queue
             self._events_queue = []
         return events
@@ -668,7 +663,6 @@ class TelemetryWriter(PeriodicService):
 
         if self._is_running():
             self.stop(join=False)
-
         # Enable writer service in child process to avoid interpreter shutdown
         # error in Python 3.12
         self.enable()
