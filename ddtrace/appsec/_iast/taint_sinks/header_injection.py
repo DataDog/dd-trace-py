bd07128ef48ad9b32f7c108304c65c48b1a7e83d
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2025-01-07 17:45:49 +0100

    dont use inner function
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/appsec/_iast/taint_sinks/header_injection.py
+++ ddtrace/appsec/_iast/taint_sinks/header_injection.py
@@ -112,36 +112,37 @@ class HeaderInjection(VulnerabilityBase):
     vulnerability_type = VULN_HEADER_INJECTION
 
 
-def _iast_report_header_injection(headers_or_args) -> None:
+def _process_header(headers_args):
     from ddtrace.appsec._iast._taint_tracking.aspects import add_aspect
 
-    def process_header(headers_args):
-        if len(headers_args) != 2:
+    if len(headers_args) != 2:
+        return
+
+    header_name, header_value = headers_args
+    if header_name is None:
+        return
+
+    for header_to_exclude in HEADER_INJECTION_EXCLUSIONS:
+        header_name_lower = header_name.lower()
+        if header_name_lower == header_to_exclude or header_name_lower.startswith(header_to_exclude):
             return
 
-        header_name, header_value = headers_args
-        if header_name is None:
-            return
+    increment_iast_span_metric(IAST_SPAN_TAGS.TELEMETRY_EXECUTED_SINK, HeaderInjection.vulnerability_type)
+    _set_metric_iast_executed_sink(HeaderInjection.vulnerability_type)
 
-        for header_to_exclude in HEADER_INJECTION_EXCLUSIONS:
-            header_name_lower = header_name.lower()
-            if header_name_lower == header_to_exclude or header_name_lower.startswith(header_to_exclude):
-                return
+    if is_iast_request_enabled() and HeaderInjection.has_quota():
+        if is_pyobject_tainted(header_name) or is_pyobject_tainted(header_value):
+            header_evidence = add_aspect(add_aspect(header_name, HEADER_NAME_VALUE_SEPARATOR), header_value)
+            HeaderInjection.report(evidence_value=header_evidence)
 
-        increment_iast_span_metric(IAST_SPAN_TAGS.TELEMETRY_EXECUTED_SINK, HeaderInjection.vulnerability_type)
-        _set_metric_iast_executed_sink(HeaderInjection.vulnerability_type)
 
-        if is_iast_request_enabled() and HeaderInjection.has_quota():
-            if is_pyobject_tainted(header_name) or is_pyobject_tainted(header_value):
-                header_evidence = add_aspect(add_aspect(header_name, HEADER_NAME_VALUE_SEPARATOR), header_value)
-                HeaderInjection.report(evidence_value=header_evidence)
-
+def _iast_report_header_injection(headers_or_args) -> None:
     if headers_or_args and isinstance(headers_or_args[0], typing.Mapping):
         # ({header_name: header_value}, {header_name: header_value}, ...), used by FastAPI Response constructor
         # when used with Response(..., headers={...})
         for headers_dict in headers_or_args:
             for header_name, header_value in headers_dict.items():
-                process_header((header_name, header_value))
+                _process_header((header_name, header_value))
     else:
         # (header_name, header_value), used in other cases
-        process_header(headers_or_args)
+        _process_header(headers_or_args)
