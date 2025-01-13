358a167c7f53849f54063227a27a3ef00af1955f
kyle <kyle@verhoog.ca>
2025-01-13 14:22:23 -0500

    chore(llmobs): make tests runnable outside riot (#11797)
    The tests shouldn't be dependent on whether they're run from riot or
    from plain ol' pytest directly.

--- tests/llmobs/test_llmobs_eval_metric_writer.py
+++ tests/llmobs/test_llmobs_eval_metric_writer.py
@@ -146,7 +146,7 @@ def test_send_multiple_events(mock_writer_logs):
 
 def test_send_on_exit(mock_writer_logs, run_python_code_in_subprocess):
     env = os.environ.copy()
-    pypath = [os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))]
+    pypath = [os.path.dirname(os.path.dirname(os.path.dirname(__file__)))]
     if "PYTHONPATH" in env:
         pypath.append(env["PYTHONPATH"])
     env.update(
