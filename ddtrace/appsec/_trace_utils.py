d47926ddbf930321d1d2d9e9e739f55704ee5fca
Christophe Papazian <christophe.papazian@datadoghq.com>
2025-01-14 11:25:11 +0100

    add usr.login

--- ddtrace/appsec/_trace_utils.py
+++ ddtrace/appsec/_trace_utils.py
@@ -71,6 +71,7 @@ def _track_user_login_common(
                 span.set_tag_str("%s.%s" % (tag_metadata_prefix, k), str(v))
 
         if login:
+            span.set_tag_str(f"{APPSEC.USER_LOGIN_EVENT_PREFIX_PUBLIC}.{success_str}.usr.login", login)
             span.set_tag_str("%s.login" % tag_prefix, login)
 
         if email:
@@ -316,6 +317,7 @@ def _on_django_login(
                 session_key = getattr(request, "session_key", None)
                 track_user_login_success_event(
                     pin.tracer,
+                    login=user_extra.get("login", None),
                     user_id=user_id,
                     session_id=session_key,
                     propagate=True,
