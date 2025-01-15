837afa97f3695f07e368b3b02a28b43eb0a5ca1b
Munir Abdinur <munir_abdinur@hotmail.com>
2025-01-15 13:06:04 -0500

    chore(tracing): deprecates FilterRequestsOnUrl

--- ddtrace/trace/__init__.py
+++ ddtrace/trace/__init__.py
@@ -1,8 +1,7 @@
 from ddtrace._trace.context import Context
-from ddtrace._trace.filters import FilterRequestsOnUrl
 from ddtrace._trace.filters import TraceFilter
 from ddtrace._trace.pin import Pin
 
 
 # TODO: Move `ddtrace.Tracer`, `ddtrace.Span`, and `ddtrace.tracer` to this module
-__all__ = ["Context", "Pin", "TraceFilter", "FilterRequestsOnUrl"]
+__all__ = ["Context", "Pin", "TraceFilter"]
