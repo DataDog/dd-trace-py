35eeb9a32424129a8a5e4ddb80cc76bccce42185
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2025-01-16 10:59:08 +0000

    chore: introduce APM_TRACING RC product
    We introduce the APM_TRACING remote configuration product that allows
    dispatching remote configuration to the library for remote enablement/
    configuration of library components and features.

--- ddtrace/debugging/_products/dynamic_instrumentation.py
+++ ddtrace/debugging/_products/dynamic_instrumentation.py
@@ -1,3 +1,5 @@
+import enum
+
 from ddtrace.settings.dynamic_instrumentation import config
 
 
@@ -5,14 +7,23 @@ requires = ["remote-configuration"]
 
 
 def post_preload():
-    pass
+    from ddtrace.debugging._debugger import Debugger
+
+    # We need to install this on start-up because if DI gets enabled remotely
+    # we won't be able to capture many of the code objects from the modules
+    # that are already loaded.
+    Debugger.__watchdog__.install()
+
+
+def _start():
+    from ddtrace.debugging import DynamicInstrumentation
+
+    DynamicInstrumentation.enable()
 
 
 def start():
     if config.enabled:
-        from ddtrace.debugging import DynamicInstrumentation
-
-        DynamicInstrumentation.enable()
+        _start()
 
 
 def restart(join=False):
@@ -29,3 +40,22 @@ def stop(join=False):
 
 def at_exit(join=False):
     stop(join=join)
+
+
+class APMCapabilities(enum.IntFlag):
+    APM_TRACING_ENABLE_DYNAMIC_INSTRUMENTATION = 1 << 38
+
+
+def apm_tracing_rc(lib_config):
+    enabled = lib_config.get("dynamic_instrumentation_enabled")
+    try:
+        if enabled is not None:  # and config.spec.enabled.full_name not in config.source:
+            if (config.spec.enabled.full_name not in config.source or config.enabled) and enabled:
+                _start()
+            else:
+                stop()
+    except Exception:
+        from traceback import print_exc
+
+        print_exc()
+        raise
