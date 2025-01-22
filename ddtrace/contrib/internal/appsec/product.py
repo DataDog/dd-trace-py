3b7f4d52d93719ebd1aa3a7a63fd290129cf18e4
Federico Mon <federico.mon@datadoghq.com>
2025-01-22 15:46:30 +0100

    refactor preload to use appsec and iast with product interface

---
+++ ddtrace/contrib/internal/appsec/product.py
@@ -0,0 +1,28 @@
+from ddtrace import config
+from ddtrace.settings.asm import config as asm_config
+
+
+def post_preload():
+    pass
+
+
+def start():
+    if asm_config._asm_enabled or config._remote_config_enabled:
+        from ddtrace.appsec._remoteconfiguration import enable_appsec_rc
+
+        enable_appsec_rc()
+
+
+def restart(join=False):
+    if asm_config._asm_enabled or config._remote_config_enabled:
+        from ddtrace.appsec._remoteconfiguration import enable_appsec_rc
+
+        enable_appsec_rc()
+
+
+def stop(join=False):
+    pass
+
+
+def at_exit(join=False):
+    pass
