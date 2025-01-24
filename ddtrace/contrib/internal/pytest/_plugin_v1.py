7ebf96af1bdcd0336c4c93b1e6a5e479ff74fa07
Vítor De Araújo <vitor.dearaujo@datadoghq.com>
2025-01-24 15:46:24 +0000

    remove comment

--- ddtrace/contrib/internal/pytest/_plugin_v1.py
+++ ddtrace/contrib/internal/pytest/_plugin_v1.py
@@ -454,7 +454,6 @@ def pytest_configure(config):
     deprecate(
         "this version of the pytest ddtrace plugin is deprecated",
         message="remove _DD_PYTEST_USE_LEGACY_PLUGIN from your environment to use the currently supported version.",
-        # removal_version="3.0.0",
         category=DDTraceDeprecationWarning,
     )
     unpatch_unittest()
