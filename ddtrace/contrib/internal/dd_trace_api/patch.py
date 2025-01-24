ffc79e511e94cf1d41dd95b809105fdd0d9c18fc
Emmett Butler <emmett.butler321@gmail.com>
2025-01-24 09:41:12 -0800

    linting

--- ddtrace/contrib/internal/dd_trace_api/patch.py
+++ ddtrace/contrib/internal/dd_trace_api/patch.py
@@ -1,19 +1,8 @@
-import os
 from sys import addaudithook
 
 import dd_trace_api
-from wrapt import wrap_function_wrapper as _w
 
 import ddtrace
-from ddtrace import config
-from ddtrace.appsec._common_module_patches import wrapped_request_D8CB81E472AF98A2 as _wrap_request
-from ddtrace.appsec._iast._metrics import _set_metric_iast_instrumented_sink
-from ddtrace.appsec._iast.constants import VULN_SSRF
-from ddtrace.contrib.internal.trace_utils import unwrap as _u
-from ddtrace.internal.schema import schematize_service_name
-from ddtrace.internal.utils.formats import asbool
-from ddtrace.settings.asm import config as asm_config
-from ddtrace.trace import Pin
 
 
 _DD_HOOK_PREFIX = "dd.hooks."
