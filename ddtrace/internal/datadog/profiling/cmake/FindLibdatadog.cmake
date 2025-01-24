7e383a7a621269ac25e23ee4d397561ab5a61080
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-01-24 11:03:43 -0500

    hack to download dd for windows

--- ddtrace/internal/datadog/profiling/cmake/FindLibdatadog.cmake
+++ ddtrace/internal/datadog/profiling/cmake/FindLibdatadog.cmake
@@ -27,17 +27,23 @@ if(NOT DEFINED DD_CHECKSUMS)
         "4540ffb8ccb671550a39ba79226117086582c1eaf9714180a9e26bd6bb175860 libdatadog-aarch64-apple-darwin.tar.gz"
         "31bceab4f56873b03b3728760d30e3abc493d32ca8fdc9e1f2ec2147ef4d5424 libdatadog-aarch64-unknown-linux-gnu.tar.gz"
         "530348c4b02cc7096de2231476ec12db82e2cc6de12a87e5b28af47ea73d4e56 libdatadog-x86_64-alpine-linux-musl.tar.gz"
-        "5073ffc657bc4698f8bdd4935475734577bfb18c54dcbebc4f7d8c7595626e52 libdatadog-x86_64-unknown-linux-gnu.tar.gz")
+        "5073ffc657bc4698f8bdd4935475734577bfb18c54dcbebc4f7d8c7595626e52 libdatadog-x86_64-unknown-linux-gnu.tar.gz"
+        "6c47eac43f6a6a60c96027de5a9155d45fc231ed6fa75258dd515605e90ad7700a549d2fb0fc65cbde9c90d765036b5201fd44f37bfeb563029381ae2a1c8117"
+        "libdatadog-x64-windows.zip"
+        "e07b20c09af5fba309646995b7f2c4da7b2681579d78976d163abd635f79fc9647e4e7f9393268d5ab7e936295e4155add243406cd1ab1caa31f9126aff8d955"
+        "libdatadog-x86-windows.zip")
 endif()
 
 # Determine platform-specific tarball name in a way that conforms to the libdatadog naming scheme in Github releases
 if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64|arm64")
     set(DD_ARCH "aarch64")
-elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64|amd64")
+elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64|amd64|AMD64")
     set(DD_ARCH "x86_64")
 else()
     message(FATAL_ERROR "Unsupported architecture: ${CMAKE_SYSTEM_PROCESSOR}")
 endif()
+
+set(DD_EXT "tar.gz")
 
 if(APPLE)
     set(DD_PLATFORM "apple-darwin")
@@ -52,11 +58,20 @@ elseif(UNIX)
     else()
         set(DD_PLATFORM "unknown-linux-gnu")
     endif()
+elseif(WIN32)
+    # WIN32 is True when it's Windows, including Win64
+    set(DD_PLATFORM "windows")
+    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+        set(DD_ARCH "x64")
+    else()
+        set(DD_ARCH "x86")
+    endif()
+    set(DD_EXT "zip")
 else()
     message(FATAL_ERROR "Unsupported operating system")
 endif()
 
-set(DD_TARBALL "libdatadog-${DD_ARCH}-${DD_PLATFORM}.tar.gz")
+set(DD_TARBALL "libdatadog-${DD_ARCH}-${DD_PLATFORM}.{DD_EXT}")
 
 # Make sure we can get the checksum for the tarball
 foreach(ENTRY IN LISTS DD_CHECKSUMS)
@@ -68,6 +83,34 @@ endforeach()
 
 if(NOT DEFINED DD_HASH)
     message(FATAL_ERROR "Could not find checksum for ${DD_TARBALL}")
+endif()
+
+if(WIN32)
+    # Use FetchContent to download and extract the library
+    FetchContent_Declare(
+        libdatadog
+        URL "https://github.com/DataDog/libdatadog/releases/download/${TAG_LIBDATADOG}/${DD_TARBALL}" DOWNLOAD_DIR
+            "${FETCHCONTENT_DOWNLOADS_DIR}" SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/libdatadog-src")
+else()
+
+    # Clean up any existing downloads if they exist
+    set(TARBALL_PATH "${FETCHCONTENT_DOWNLOADS_DIR}/${DD_TARBALL}")
+    if(EXISTS "${TARBALL_PATH}")
+        file(SHA256 "${TARBALL_PATH}" EXISTING_HASH)
+        if(NOT EXISTING_HASH STREQUAL DD_HASH)
+            file(REMOVE "${TARBALL_PATH}")
+            # Also remove the subbuild directory to force a fresh download
+            file(REMOVE_RECURSE "${CMAKE_CURRENT_BINARY_DIR}/_deps/libdatadog-subbuild")
+        endif()
+    endif()
+
+    # Use FetchContent to download and extract the library
+    FetchContent_Declare(
+        libdatadog
+        URL "https://github.com/DataDog/libdatadog/releases/download/${TAG_LIBDATADOG}/${DD_TARBALL}"
+        URL_HASH SHA256=${DD_HASH}
+        DOWNLOAD_DIR "${FETCHCONTENT_DOWNLOADS_DIR}" SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/libdatadog-src")
+
 endif()
 
 # Clean up any existing downloads if they exist
