1f067a50c49f8db9a95fc8cc6b6a75d3455d6dae
Nick Ripley <nick.ripley@datadoghq.com>
2025-01-24 15:51:08 +0000

    fix(profiling): fix SystemError when collecting memory profiler events
    We added locking to the memory profiler to address crashes. These locks
    are mostly "try" locks, meaning we bail out if we can't acquire them
    right away. This was done defensively to mitigate the possibility of
    deadlock until we fully understood why the locks are needed and could
    guarantee their correctness. But as a result of using try locks, the
    iter_events function in particular can fail if the memory profiler lock
    is contended when it tries to collect profiling events. The function
    then returns NULL, leading to SystemError exceptions because we don't
    set an error.
    
    Even if we set an error, returning NULL isn't the right thing to do.
    It'll basically mean we wait until the next profile iteration, still
    accumulating events in the same buffer, and try again to upload the
    events. So we're going to get multiple iteration's worth of events. The
    right thing to do is take the lock unconditionally in iter_events. This
    is safe because the only thing we're guarding is allocating a new sample
    buffer, which doesn't call back into the python object allocator, and
    rotating out the pointer to the allocation buffer.
    
    Fixes #11831

--- ddtrace/profiling/collector/_memalloc.c
+++ ddtrace/profiling/collector/_memalloc.c
@@ -394,20 +394,18 @@ iterevents_new(PyTypeObject* type, PyObject* Py_UNUSED(args), PyObject* Py_UNUSE
     }
 
     IterEventsState* iestate = (IterEventsState*)type->tp_alloc(type, 0);
-    if (!iestate)
+    if (!iestate) {
+        PyErr_SetString(PyExc_RuntimeError, "failed to allocate IterEventsState");
         return NULL;
+    }
 
     memalloc_assert_gil();
 
     /* reset the current traceback list */
-    if (memlock_trylock(&g_memalloc_lock)) {
-        iestate->alloc_tracker = global_alloc_tracker;
-        global_alloc_tracker = alloc_tracker_new();
-        memlock_unlock(&g_memalloc_lock);
-    } else {
-        Py_TYPE(iestate)->tp_free(iestate);
-        return NULL;
-    }
+    memlock_lock(&g_memalloc_lock);
+    iestate->alloc_tracker = global_alloc_tracker;
+    global_alloc_tracker = alloc_tracker_new();
+    memlock_unlock(&g_memalloc_lock);
     iestate->seq_index = 0;
 
     PyObject* iter_and_count = PyTuple_New(3);
