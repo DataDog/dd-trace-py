b1db4dca39ca098ee7780edb102a8dff95d7ed59
Emmett Butler <emmett.butler321@gmail.com>
2025-01-24 10:47:21 -0800

    only hook once

--- ddtrace/contrib/internal/dd_trace_api/patch.py
+++ ddtrace/contrib/internal/dd_trace_api/patch.py
@@ -15,6 +15,11 @@ def _patched_start_span(*args, **kwargs):
     _CURRENT_SPAN = _TRACER.start_span(*args, **kwargs)
 
 
+def _patched_trace(*args, **kwargs):
+    global _CURRENT_SPAN
+    _CURRENT_SPAN = _TRACER.trace(*args, **kwargs)
+
+
 def _patched_span_enter(*args, **kwargs):
     _CURRENT_SPAN.__enter__(*args, **kwargs)
 
@@ -29,6 +34,7 @@ def _patched_span_finish(*args, **kwargs):
 
 _HANDLERS = {
     "Tracer.start_span": _patched_start_span,
+    "Tracer.trace": _patched_trace,
     "Span.__enter__": _patched_span_enter,
     "Span.__exit__": _patched_span_exit,
     "Span.finish": _patched_span_finish,
@@ -55,7 +61,9 @@ def patch(tracer=None):
     dd_trace_api.__datadog_patch = True
     global _TRACER
     _TRACER = tracer
-    addaudithook(_hook)
+    if not getattr(dd_trace_api, "__dd_has_audit_hook", False):
+        addaudithook(_hook)
+    dd_trace_api.__dd_has_audit_hook = True
 
 
 def unpatch():
--- tests/contrib/dd_trace_api/test_integration.py
+++ tests/contrib/dd_trace_api/test_integration.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import dd_trace_api
 
 from ddtrace import Span as dd_span_class
@@ -15,12 +17,23 @@ class DDTraceAPITestCase(TracerTestCase):
         super(DDTraceAPITestCase, self).tearDown()
         unpatch()
 
-    def test_start_span(self):
-        with dd_trace_api.tracer.Tracer().start_span("web.request") as span:
-            assert not isinstance(span, dd_span_class), "Returned span object should be a stub"
-            assert not hasattr(span, "span_id"), "Returned span stub should not support read operations"
+    def _assert_span_stub(self, stub: Any):
+        assert not isinstance(stub, dd_span_class), "Returned span object should be a stub"
+        assert not hasattr(stub, "span_id"), "Returned span stub should not support read operations"
+
+    def _assert_real_span(self):
         spans = self.pop_spans()
         assert len(spans) == 1
         generated_span = spans[0]
         assert isinstance(generated_span, dd_span_class), "Generated span is a real span"
         assert hasattr(generated_span, "span_id"), "Generated span should support read operations"
+
+    def test_start_span(self):
+        with dd_trace_api.tracer.Tracer().start_span("web.request") as span:
+            self._assert_span_stub(span)
+        self._assert_real_span()
+
+    def test_trace(self):
+        with dd_trace_api.tracer.Tracer().trace("web.request") as span:
+            self._assert_span_stub(span)
+        self._assert_real_span()
