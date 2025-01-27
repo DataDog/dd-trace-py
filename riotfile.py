e30b2782ede8072b65e08519709183e0493b2ba5
Yun Kim <yun.kim@datadoghq.com>
2025-01-27 17:06:41 -0500

    Drop openai v0 testing and patching

--- riotfile.py
+++ riotfile.py
@@ -2493,17 +2493,6 @@ venv = Venv(
             },
             venvs=[
                 Venv(
-                    # openai[embeddings] broken install with sklearn was never fixed on 0.26
-                    # https://github.com/openai/openai-python/issues/210
-                    pys="3.7",
-                    env={"SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL": "True"},
-                    pkgs={
-                        "openai": "==0.26.5",
-                        "scikit-learn": "==1.0.2",
-                        "pillow": "==9.5.0",
-                    },
-                ),
-                Venv(
                     pys="3.7",
                     pkgs={
                         "openai[datalib]": "==1.30.1",
