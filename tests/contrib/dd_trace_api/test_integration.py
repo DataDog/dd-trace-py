026c06dbf1d80ebcd46c57c5cf30ca35cb98c7f3
Emmett Butler <emmett.butler321@gmail.com>
2025-01-27 09:47:49 -0800

    use singleton tracer

--- tests/contrib/dd_trace_api/test_integration.py
+++ tests/contrib/dd_trace_api/test_integration.py
@@ -31,30 +31,31 @@ class DDTraceAPITestCase(TracerTestCase):
         assert isinstance(generated_span, dd_span_class), "Generated span is a real span"
         assert hasattr(generated_span, "span_id"), "Generated span should support read operations"
 
+    def test_tracer_singleton(self):
+        assert isinstance(dd_trace_api.tracer, dd_trace_api.Tracer), "Tracer stub should be exposed as a singleton"
+
     def test_start_span(self):
-        with dd_trace_api.tracer.Tracer().start_span("web.request") as span:
+        with dd_trace_api.tracer.start_span("web.request") as span:
             self._assert_span_stub(span)
         self._assert_real_spans()
 
     def test_trace(self):
-        with dd_trace_api.tracer.Tracer().trace("web.request") as span:
+        with dd_trace_api.tracer.trace("web.request") as span:
             self._assert_span_stub(span)
         self._assert_real_spans()
 
     def test_current_span(self):
-        tracer = dd_trace_api.tracer.Tracer()
-        with tracer.trace("web.request"):
-            span = tracer.current_span()
+        with dd_trace_api.tracer.trace("web.request"):
+            span = dd_trace_api.tracer.current_span()
             self._assert_span_stub(span)
         self._assert_real_spans()
 
     def test_current_root_span(self):
-        tracer = dd_trace_api.tracer.Tracer()
-        with tracer.trace("web.request"):
-            span = tracer.current_root_span()
+        with dd_trace_api.tracer.trace("web.request"):
+            span = dd_trace_api.tracer.current_root_span()
             self._assert_span_stub(span)
-            with tracer.trace("web.other.request"):
-                root_from_nested = tracer.current_root_span()
+            with dd_trace_api.tracer.trace("web.other.request"):
+                root_from_nested = dd_trace_api.tracer.current_root_span()
                 self._assert_span_stub(root_from_nested)
                 assert span._real_span == root_from_nested._real_span
         self._assert_real_spans(2)
