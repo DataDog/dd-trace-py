9df77968204730cc2f3c6cdf608de400d82f90a5
Munir Abdinur <munir.abdinur@datadoghq.com>
2025-01-28 18:04:24 -0500

    lint

--- ddtrace/contrib/__init__.py
+++ ddtrace/contrib/__init__.py
@@ -1,3 +1,3 @@
 # Importing trace handers has to side effect of registering integration level
 # handlers. This is necessary to use the Core API in integrations.
-from ddtrace._trace import trace_handlers as _
+from ddtrace._trace import trace_handlers as _  # noqa: F401
