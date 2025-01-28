3d12d54a133c97165871a2beba6fb66ed6f672d9
Munir Abdinur <munir_abdinur@hotmail.com>
2025-01-28 16:56:46 -0500

    make sure trace handlers are registered

---
+++ ddtrace/contrib/__init__.py
@@ -0,0 +1,3 @@
+# Importing trace handers has to side effect of registering integration level
+# handlers. This is necessary to use the Core API in integrations.
+from ddtrace._trace import trace_handlers as _
