86d69dd964f5207bcbd821652a5d3c23f2c556f5
Emmett Butler <emmett.butler321@gmail.com>
2025-01-28 11:09:24 -0800

    simplify

--- ddtrace/contrib/internal/dd_trace_api/patch.py
+++ ddtrace/contrib/internal/dd_trace_api/patch.py
@@ -11,10 +11,9 @@ import ddtrace
 
 
 _DD_HOOK_PREFIX = "dd.hooks."
-_STATE = {"tracer": ddtrace.tracer}
+_TRACER_KEY = "Tracer"
+_STATE = {_TRACER_KEY: ddtrace.tracer}
 _STUB_TO_SPAN = weakref.WeakKeyDictionary()
-SELF_KEY = "stub_self"
-REAL_SPAN_KEY = "real_span"
 
 
 def _proxy_span_arguments(args: List, kwargs: Dict) -> Tuple[List, Dict]:
@@ -36,57 +35,23 @@ def _proxy_span_arguments(args: List, kwargs: Dict) -> Tuple[List, Dict]:
 
 def _patched(method_of, fn_name):
     def _inner(state_shared_with_api, *args, **kwargs):
-        operand_stub = state_shared_with_api.get(SELF_KEY)
-        if operand_stub:
-            operand = _STUB_TO_SPAN[operand_stub]
-        else:
-            operand = _STATE[method_of]
+        retval_from_api = state_shared_with_api.get("api_return_value")
+        operand_stub = state_shared_with_api.get("stub_self")
         args, kwargs = _proxy_span_arguments(args, kwargs)
-        retval = getattr(operand, fn_name)(*args, **kwargs)
-        api_return_value = state_shared_with_api.get("api_return_value")
-        if isinstance(api_return_value, dd_trace_api.Span):
-            _STUB_TO_SPAN[api_return_value] = retval
+        retval_from_impl = getattr(_STUB_TO_SPAN[operand_stub] if operand_stub else _STATE[method_of], fn_name)(
+            *args, **kwargs
+        )
+        if isinstance(retval_from_api, dd_trace_api.Span):
+            _STUB_TO_SPAN[retval_from_api] = retval_from_impl
 
     return _inner
 
 
-_HANDLERS = {
-    "Tracer.flush": None,
-    "Tracer.shutdown": None,
-    "Tracer.set_tags": None,
-    "Tracer.start_span": _patched("tracer", "start_span"),
-    "Tracer.trace": _patched("tracer", "trace"),
-    "Tracer.current_span": _patched("tracer", "current_span"),
-    "Tracer.current_root_span": _patched("tracer", "current_root_span"),
-    "Span.__enter__": None,
-    "Span.__exit__": None,
-    "Span.finish": None,
-    "Span.finish_with_ancestors": None,
-    "Span.set_exc_info": None,
-    "Span.set_link": None,
-    "Span.set_traceback": None,
-    "Span.link_span": None,
-    "Span.set_tags": None,
-    "Span.set_tag": None,
-    "Span.set_tag_str": None,
-    "Span.set_struct_tag": None,
-}
-
-
 def _hook(name, hook_args):
-    if not dd_trace_api.__datadog_patch:
+    if not dd_trace_api.__datadog_patch or not name.startswith(_DD_HOOK_PREFIX):
         return
-    if name.startswith(_DD_HOOK_PREFIX):
-        name_suffix = ".".join(name.split(".")[2:])
-        if name_suffix not in _HANDLERS:
-            return
-        kwargs = hook_args[0][1]
-        args = hook_args[0][0]
-        state_shared_with_api, args = args[0], args[1:]
-        handler = _HANDLERS[name_suffix]
-        if handler is None:
-            handler = _patched(*(name_suffix.split(".")))
-        handler(state_shared_with_api, *args, **kwargs)
+    args = hook_args[0][0]
+    _patched(*(name.replace(_DD_HOOK_PREFIX, "").split(".")))(args[0], *args[1:], **hook_args[0][1])
 
 
 def get_version() -> str:
@@ -97,7 +62,7 @@ def patch(tracer=None):
     if getattr(dd_trace_api, "__datadog_patch", False):
         return
     dd_trace_api.__datadog_patch = True
-    _STATE["tracer"] = tracer
+    _STATE[_TRACER_KEY] = tracer
     if not getattr(dd_trace_api, "__dd_has_audit_hook", False):
         addaudithook(_hook)
     dd_trace_api.__dd_has_audit_hook = True
