89a88aabb4782a233b4fb389bdfcb5d57731b448
lievan <evan.li@datadoqhq.com>
2025-01-29 10:53:59 -0500

    merge instead of overwrite

--- tests/llmobs/test_llmobs_service.py
+++ tests/llmobs/test_llmobs_service.py
@@ -379,11 +379,24 @@ def test_annotate_metadata(llmobs):
         assert span._get_ctx_item(METADATA) == {"temperature": 0.5, "max_tokens": 20, "top_k": 10, "n": 3}
 
 
+def test_annotate_metadata_updates(llmobs):
+    with llmobs.llm(model_name="test_model", name="test_llm_call", model_provider="test_provider") as span:
+        llmobs.annotate(span=span, metadata={"temperature": 0.5, "max_tokens": 20, "top_k": 10, "n": 3})
+        llmobs.annotate(span=span, metadata={"temperature": 1, "logit_bias": [{"1": 2}]})
+        assert span._get_ctx_item(METADATA) == {
+            "temperature": 1,
+            "max_tokens": 20,
+            "top_k": 10,
+            "n": 3,
+            "logit_bias": [{"1": 2}],
+        }
+
+
 def test_annotate_metadata_wrong_type_raises_warning(llmobs, mock_llmobs_logs):
     with llmobs.llm(model_name="test_model", name="test_llm_call", model_provider="test_provider") as span:
         llmobs.annotate(span=span, metadata="wrong_metadata")
         assert span._get_ctx_item(METADATA) is None
-        mock_llmobs_logs.warning.assert_called_once_with("metadata must be a dictionary of string key-value pairs.")
+        mock_llmobs_logs.warning.assert_called_once_with("metadata must be a dictionary")
         mock_llmobs_logs.reset_mock()
 
 
@@ -398,7 +411,7 @@ def test_annotate_tag_wrong_type(llmobs, mock_llmobs_logs):
         llmobs.annotate(span=span, tags=12345)
         assert span._get_ctx_item(TAGS) is None
         mock_llmobs_logs.warning.assert_called_once_with(
-            "span_tags must be a dictionary of string key - primitive value pairs."
+            "span tags must be a dictionary of string key - primitive value pairs."
         )
 
 
@@ -643,6 +656,13 @@ def test_annotate_metrics(llmobs):
     with llmobs.llm(model_name="test_model") as span:
         llmobs.annotate(span=span, metrics={"input_tokens": 10, "output_tokens": 20, "total_tokens": 30})
         assert span._get_ctx_item(METRICS) == {"input_tokens": 10, "output_tokens": 20, "total_tokens": 30}
+
+
+def test_annotate_metrics_updates(llmobs):
+    with llmobs.llm(model_name="test_model") as span:
+        llmobs.annotate(span=span, metrics={"input_tokens": 10, "output_tokens": 20})
+        llmobs.annotate(span=span, metrics={"input_tokens": 20, "total_tokens": 40})
+        assert span._get_ctx_item(METRICS) == {"input_tokens": 20, "output_tokens": 20, "total_tokens": 40}
 
 
 def test_annotate_metrics_wrong_type(llmobs, mock_llmobs_logs):
