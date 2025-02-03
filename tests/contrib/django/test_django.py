bd791342c95cf7217223d92441384c6d1609242d
ZStriker19 <zach.groves@datadoghq.com>
2025-02-03 17:30:16 -0500

    change for django string tests

--- tests/contrib/django/test_django.py
+++ tests/contrib/django/test_django.py
@@ -2018,7 +2018,8 @@ def test_django_use_handler_resource_format_env(client, test_spans):
                 "python",
                 "-c",
                 (
-                    "from ddtrace import config, patch_all; patch_all(); "
+                    "from ddtrace import config "
+                    "from ddtrace._monkey import _patch_all; _patch_all(); "
                     "import django; "
                     "assert config.django.use_handler_resource_format; print('Test success')"
                 ),
@@ -2037,7 +2038,8 @@ def test_django_use_handler_with_url_name_resource_format_env(client, test_spans
                 "python",
                 "-c",
                 (
-                    "from ddtrace import config, patch_all; patch_all(); "
+                    "from ddtrace import config "
+                    "from ddtrace._monkey import _patch_all; _patch_all(); "
                     "import django; "
                     "assert config.django.use_handler_with_url_name_resource_format; print('Test success')"
                 ),
@@ -2117,7 +2119,8 @@ def test_django_use_legacy_resource_format_env(client, test_spans):
                 "python",
                 "-c",
                 (
-                    "from ddtrace import config, patch_all; patch_all(); "
+                    "from ddtrace import config "
+                    "from ddtrace._monkey import _patch_all; _patch_all(); "
                     "import django; "
                     "assert config.django.use_legacy_resource_format; print('Test success')"
                 ),
