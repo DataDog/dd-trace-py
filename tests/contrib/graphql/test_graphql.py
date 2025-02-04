3cfc20c7b948d1798a1c3c1bdcbc16f726e128b9
quinna-h <quinna.halim@datadoghq.com>
2025-02-04 12:05:58 -0500

    update snapshot decorator arguments

--- tests/contrib/graphql/test_graphql.py
+++ tests/contrib/graphql/test_graphql.py
@@ -82,7 +82,7 @@ def resolve_fail(root, info):
     return undefined_var.property
 
 
-@snapshot(ignores=["meta.error.type", "meta.error.message", "meta.events"])
+@snapshot(ignores=["meta.error.type", "meta.error.message", "meta.error.stack", "meta.events"])
 def test_graphql_fail(enable_graphql_resolvers):
     query = """
     query {
