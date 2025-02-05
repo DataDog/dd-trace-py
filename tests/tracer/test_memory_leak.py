29c1ef0b8faebcf014a35d4858bcb31b21674790
Munir Abdinur <munir_abdinur@hotmail.com>
2025-02-04 20:10:31 -0500

    fix fmt

--- tests/tracer/test_memory_leak.py
+++ tests/tracer/test_memory_leak.py
@@ -1,17 +1,13 @@
 """
 Variety of test cases ensuring that ddtrace does not leak memory.
 """
-from typing import TYPE_CHECKING
 from weakref import WeakValueDictionary
 
 import pytest
 
+from ddtrace.trace import Span
+from ddtrace.trace import Tracer
 from tests.utils import DummyTracer
-
-
-if TYPE_CHECKING:  # pragma: no cover
-    from ddtrace.trace import Span  # noqa:F401
-    from ddtrace.trace import Tracer  # noqa:F401
 
 
 @pytest.fixture
@@ -19,8 +15,7 @@ def tracer() -> DummyTracer:
     return DummyTracer()
 
 
-def trace(weakdict: WeakValueDictionary, tracer: Tracer, *args, **kwargs):
-    # type: (...) -> Span
+def trace(weakdict: WeakValueDictionary, tracer: Tracer, *args, **kwargs) -> Span:
     """Return a span created from ``tracer`` and add it to the given weak
     dictionary.
 
