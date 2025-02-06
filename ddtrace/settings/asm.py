8620f4c8c6083d220fd81ced265c4b1e5aeae696
Emmett Butler <emmett.butler321@gmail.com>
2025-02-06 12:21:31 -0800

    merge conflict

--- ddtrace/settings/asm.py
+++ ddtrace/settings/asm.py
@@ -218,7 +218,7 @@ class ASMConfig(Env):
     _bypass_instrumentation_for_waf = False
 
     # IAST supported on python 3.6 to 3.13 and never on windows
-    _iast_supported: bool = ((3, 8, 0) <= sys.version_info < (3, 14, 0)) and not (
+    _iast_supported: bool = ((3, 6, 0) <= sys.version_info < (3, 14, 0)) and not (
         sys.platform.startswith("win") or sys.platform.startswith("cygwin")
     )
 
