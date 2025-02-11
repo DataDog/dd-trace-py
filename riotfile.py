90ac2bfcf63e930459e2f11bd7e8514fa9ed240f
brettlangdon <me@brett.is>
2025-02-11 17:50:32 -0500

    zope pure python

--- riotfile.py
+++ riotfile.py
@@ -2756,6 +2756,7 @@ venv = Venv(
                 Venv(
                     pys=["3.8"],
                     pkgs={"greenlet": "==3.1.0"},
+                    env={"PURE_PYTHON": "1"},
                 ),
                 # Python 3.9+
                 Venv(
