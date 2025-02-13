1c9f8fcc7349bad7693809718d990396306b8bf5
Sam Brenner <106700075+sabrenner@users.noreply.github.com>
2025-02-13 06:28:13 -0500

    chore(llmobs): enable langgraph for enabled integrations (#12315)
    Adds `langgraph` for auto-patching when `LLMObs.enable(...)` is called.
    It is still further gated behind environment variable.

--- ddtrace/llmobs/_llmobs.py
+++ ddtrace/llmobs/_llmobs.py
@@ -81,6 +81,7 @@ SUPPORTED_LLMOBS_INTEGRATIONS = {
     "langchain": "langchain",
     "google_generativeai": "google_generativeai",
     "vertexai": "vertexai",
+    "langgraph": "langgraph",
 }
 
 
