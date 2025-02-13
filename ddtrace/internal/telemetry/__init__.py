e30214e84738fecf661d07c1fdf3102d49f926cd
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-02-13 14:51:58 -0800

    Create noop telemetry writer when telemetry disabled.

--- ddtrace/internal/telemetry/__init__.py
+++ ddtrace/internal/telemetry/__init__.py
@@ -3,7 +3,6 @@ Instrumentation Telemetry API.
 This is normally started automatically when ``ddtrace`` is imported. It can be disabled by setting
 ``DD_INSTRUMENTATION_TELEMETRY_ENABLED`` variable to ``False``.
 """
-
 import os
 import typing as t
 
@@ -21,8 +20,15 @@ from ddtrace.settings._otel_remapper import parse_otel_env
 
 log = get_logger(__name__)
 
+if os.environ.get("DD_INSTRUMENTATION_TELEMETRY_ENABLED", "true").lower() in ("true", "1"):
+    from .writer import TelemetryWriter
 
-telemetry_writer = TelemetryWriter()  # type: TelemetryWriter
+    telemetry_writer = TelemetryWriter()  # type: TelemetryWriter
+
+else:
+    from .noop_writer import NoOpTelemetryWriter
+
+    telemetry_writer = NoOpTelemetryWriter()
 
 __all__ = ["telemetry_writer"]
 
---
+++ ddtrace/internal/telemetry/noop_writer.py
@@ -0,0 +1,7 @@
+class NoOpTelemetryWriter(object):
+
+    def __getattr__(self, name):
+        return self.noop
+
+    def noop(self, *args, **kwargs):
+        pass
