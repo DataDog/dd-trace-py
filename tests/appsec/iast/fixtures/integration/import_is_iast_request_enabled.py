5cc54320c1230a7c340006bf559ccec50bdb6ec7
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2025-02-13 17:41:45 +0100

    fix
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- tests/appsec/iast/fixtures/integration/import_is_iast_request_enabled.py
+++ tests/appsec/iast/fixtures/integration/import_is_iast_request_enabled.py
@@ -4,7 +4,6 @@ import ddtrace
 
 
 source_code = inspect.getsource(ddtrace.appsec._common_module_patches.is_iast_request_enabled)
-# Split the source code into lines
 lines = source_code.split("\n")
 body = "\n".join(lines[1:]).strip()
 is_body_return_false = body == "return False"
