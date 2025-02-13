e8ea27f371c75408a6f944931278c03241ee2a3b
wan <wan.tsui@datadoghq.com>
2025-02-13 16:06:34 -0500

    Fix memory leak changes for 2.21 branch.

--- ddtrace/contrib/internal/aiohttp/middlewares.py
+++ ddtrace/contrib/internal/aiohttp/middlewares.py
@@ -63,39 +63,20 @@ async def trace_middleware(app, handler):
         if (config._analytics_enabled and analytics_enabled is not False) or analytics_enabled is True:
             request_span.set_tag(_ANALYTICS_SAMPLE_RATE_KEY, app[CONFIG_KEY].get("analytics_sample_rate", True))
 
-        with core.context_with_data(
-            "aiohttp.request",
-            span_name=schematize_url_operation("aiohttp.request", protocol="http", direction=SpanDirection.INBOUND),
-            span_type=SpanTypes.WEB,
-            service=service,
-            tags={},
-            tracer=tracer,
-            distributed_headers=request.headers,
-            distributed_headers_config=config.aiohttp,
-            distributed_headers_config_override=app[CONFIG_KEY]["distributed_tracing_enabled"],
-            headers_case_sensitive=True,
-            analytics_enabled=analytics_enabled,
-            analytics_sample_rate=app[CONFIG_KEY].get("analytics_sample_rate", True),
-        ) as ctx:
-            req_span = ctx.span
-
-            ctx.set_item("req_span", req_span)
-            core.dispatch("web.request.start", (ctx, config.aiohttp))
-
-            # attach the context and the root span to the request; the Context
-            # may be freely used by the application code
-            request[REQUEST_CONTEXT_KEY] = req_span.context
-            request[REQUEST_SPAN_KEY] = req_span
-            request[REQUEST_CONFIG_KEY] = app[CONFIG_KEY]
-            try:
-                response = await handler(request)
-                if not config.aiohttp["disable_stream_timing_for_mem_leak"]:
-                    if isinstance(response, web.StreamResponse):
-                        request.task.add_done_callback(lambda _: finish_request_span(request, response))
-                return response
-            except Exception:
-                req_span.set_traceback()
-                raise
+        # attach the context and the root span to the request; the Context
+        # may be freely used by the application code
+        request[REQUEST_CONTEXT_KEY] = request_span.context
+        request[REQUEST_SPAN_KEY] = request_span
+        request[REQUEST_CONFIG_KEY] = app[CONFIG_KEY]
+        try:
+            response = await handler(request)
+            if not config.aiohttp["disable_stream_timing_for_mem_leak"]:
+                if isinstance(response, web.StreamResponse):
+                    request.task.add_done_callback(lambda _: finish_request_span(request, response))
+            return response
+        except Exception:
+            request_span.set_traceback()
+            raise
 
     return attach_context
 
