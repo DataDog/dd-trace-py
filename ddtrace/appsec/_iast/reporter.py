3cf96a374d71d1aebe29cddf09085a16b0fcfd72
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2025-02-13 12:03:57 +0100

    checkpoint
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/appsec/_iast/reporter.py
+++ ddtrace/appsec/_iast/reporter.py
@@ -66,8 +66,8 @@ class Location(NotNoneDictable):
     spanId: int = dataclasses.field(compare=False, hash=False, repr=False)
     path: Optional[str] = None
     line: Optional[int] = None
-    function: Optional[str] = dataclasses.field(compare=False, hash=False, repr=True, default='')
-    class_name: Optional[str] = dataclasses.field(compare=False, hash=False, repr=True, default='')
+    function: Optional[str] = dataclasses.field(compare=False, hash=False, repr=False, default='')
+    class_name: Optional[str] = dataclasses.field(compare=False, hash=False, repr=False, default='')
 
     def __repr__(self):
         # return f"Location(path='{self.path}', line={self.line}, function='{self.function}', class_name='{self.class_name})"
