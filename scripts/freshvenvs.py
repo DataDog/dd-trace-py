5b9d0eea35ad6f7e62a87090218b952802e08d32
quinna-h <quinna.halim@datadoghq.com>
2025-02-13 19:28:16 -0500

    update

--- scripts/freshvenvs.py
+++ scripts/freshvenvs.py
@@ -80,27 +80,21 @@ def _get_integrated_modules() -> typing.Set[str]:
     """Get all modules that have contribs implemented for them"""
     all_required_modules = set()
     for item in CONTRIB_ROOT.iterdir():
-        # print(f"DEBUG: Found path: {item}")
         if not os.path.isdir(item):
-            # print(f"DEBUG: Skipping {item} - not a directory")
             continue
 
         patch_filepath = item / "patch.py"
-        # print(f"DEBUG: Looking for patch file at: {patch_filepath}")
 
         if os.path.isfile(patch_filepath):
-            # print(f"DEBUG: Found patch.py in: {item}")
             with open(patch_filepath, "r") as patchfile:
                 patch_content = patchfile.read()
 
             # Check if this patch file has a get_version function
             if "def get_version()" in patch_content:
                 module_name = item.name
-                # print(f"DEBUG: Found get_version() in {module_name}")
                 all_required_modules.add(module_name)
 
 
-    # print(f"DEBUG: Final all_required_modules: {all_required_modules}")
     return all_required_modules
 
 
@@ -246,33 +240,24 @@ def _get_version_bounds(packages) -> dict:
     return bounds
 
 def output_outdated_packages(all_required_packages, envs, bounds):
-    # print("DEBUG: Starting output_outdated_packages")
-    # print(f"DEBUG: all_required_packages = {all_required_packages}")
-    # print(f"DEBUG: envs = {envs}")
     outdated_packages = []
 
     for package in all_required_packages:
         earliest, latest = _get_version_extremes(package)
         bounds[package] = (earliest, latest)
-        # print(f"DEBUG: For package {package}, earliest={earliest}, latest={latest}")
 
     all_used_versions = defaultdict(set)
     for env in envs:
         versions_used = _get_package_versions_from(env, all_required_packages)
-        # print(f"DEBUG: For env {env}, versions_used = {versions_used}")
         for pkg, version in versions_used:
             all_used_versions[pkg].add(version)
 
-    # print(f"DEBUG: all_used_versions = {dict(all_used_versions)}")
 
     for package in all_required_packages:
         ordered = sorted([Version(v) for v in all_used_versions[package]], reverse=True)
-        # print(f"DEBUG: For package {package}, ordered versions = {ordered}")
         if not ordered:
-            # print(f"DEBUG: Skipping {package} - no versions found")
             continue
         if not _versions_fully_cover_bounds(bounds[package], ordered):
-            # print(f"{package} is out of date")
             outdated_packages.append(package)
 
     print(" ".join(outdated_packages))
