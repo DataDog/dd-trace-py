0276fd2afc06bc889f67652d37273ac7ea2bc088
Jonathan Chavez <jonathan.chavez@datadoghq.com>
2025-02-20 14:04:50 -0500

    change imports

--- ddtrace/llmobs/experimentation/__init__.py
+++ ddtrace/llmobs/experimentation/__init__.py
@@ -3,9 +3,9 @@ LLM Datasets and Experiments.
 """
 from ._experiments import Dataset
 from ._experiments import Experiment
-from ._experiments import experiment_task
+from ._experiments import task
 from ._experiments import evaluator
 from ._experiments import init
 
 
-__all__ = ["Dataset", "Experiment", "experiment_task", "evaluator", "init"]
+__all__ = ["Dataset", "Experiment", "task", "evaluator", "init"]
