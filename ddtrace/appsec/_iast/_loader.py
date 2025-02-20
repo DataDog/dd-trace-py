db7a337091bd501f83dbdf5d644d253d43ce8f29
Alberto Vara <alberto.vara@datadoghq.com>
2025-02-20 16:33:48 +0100

    chore(iast): AST execution limiter

--- ddtrace/appsec/_iast/_loader.py
+++ ddtrace/appsec/_iast/_loader.py
@@ -1,20 +1,56 @@
 #!/usr/bin/env python3
 
+import functools
+import time
+
 from ddtrace.internal.logger import get_logger
 from ddtrace.settings.asm import config as asm_config
 
 from ._ast.ast_patching import astpatch_module
+from ._taint_tracking._errors import iast_log_error
 
 
 log = get_logger(__name__)
 
-IS_IAST_ENABLED = asm_config._iast_enabled
+TOTAL_TIME = 0
+LIMIT_TIME = 1.0
+FUNC_BLOCKED = False
 
 
+def execution_limiter():
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            global TOTAL_TIME
+            global FUNC_BLOCKED
+            if FUNC_BLOCKED:
+                log.debug("[IAST] Skipping execution, the time limit has been exceeded.")
+                return None
+
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            elapsed = time.time() - start_time
+            TOTAL_TIME += elapsed
+
+            if TOTAL_TIME > LIMIT_TIME:
+                from . import disable_iast_propagation
+
+                disable_iast_propagation()
+                iast_log_error(f"Execution time exceeded {LIMIT_TIME} seconds. Blocking {func.__name__} permanently.")
+                FUNC_BLOCKED = True
+
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+@execution_limiter()
 def _exec_iast_patched_module(module_watchdog, module):
     patched_ast = None
     compiled_code = None
-    if IS_IAST_ENABLED:
+    if asm_config._iast_enabled:
         try:
             module_path, patched_ast = astpatch_module(module)
         except Exception:
