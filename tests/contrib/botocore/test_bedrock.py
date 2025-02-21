131d0f118d56deb812d27a95e5c1ef49cab7d0ab
erikayasuda <153395705+erikayasuda@users.noreply.github.com>
2025-02-21 11:01:15 -0500

    ci(flaky): mark tests as flaky for 30 days (#12396)
    Marking the following tests as flaky for 30 days. The tests will be
    deleted if flakiness is not resolved by then.
    - `tests/contrib/asyncpg/test_asyncpg.py:test_connect`
    - `tests/contrib/botocore/test_bedrock.py:test_anthropic_message_invoke`
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [ ] Reviewer has checked that all the criteria below are met
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    
    (cherry picked from commit 2b96d0cbced30a438b98ef2104126de7bfdc2a63)

--- tests/contrib/botocore/test_bedrock.py
+++ tests/contrib/botocore/test_bedrock.py
@@ -179,6 +179,7 @@ def test_anthropic_invoke(bedrock_client, request_vcr):
 
 
 @pytest.mark.snapshot
+@flaky(until=1742428860, reason="Did not receive expected traces: 'bedrock-runtime.command'")
 def test_anthropic_message_invoke(bedrock_client, request_vcr):
     body, model = json.dumps(_REQUEST_BODIES["anthropic_message"]), _MODELS["anthropic_message"]
     model = "us." + model
@@ -259,6 +260,7 @@ def test_anthropic_message_invoke_stream(bedrock_client, request_vcr):
 
 
 @pytest.mark.snapshot
+@flaky(until=1742428860, reason="Did not receive expected traces: 'bedrock-runtime.command'")
 def test_cohere_invoke_stream_single_output(bedrock_client, request_vcr):
     body = json.dumps(
         {
