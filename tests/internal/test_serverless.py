8c4d62db54ad26c6e838cdf4849fbb929c157f09
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-02-27 11:33:18 -0800

    ci(serverless): force reimport of modules during serverless test (#11888)
    Commit
    https://github.com/DataDog/dd-trace-py/commit/12655eebaa93ec7eb9ce6858ddf5e07ae4e4dae7
    removed the `@pytest.mark.subprocess()` from the serverless slow imports
    test. This effectively rendered the test useless as `ddtrace` and all
    required modules had already been imported in the parent process.
    
    Adding the subprocess mark back will ensure that the sys modules are
    fresh. We must still go through and delete entries because
    `@pytest.mark.subprocess()` relies on `multiprocessing`, so it must be
    removed so as to ensure it can be reimported if needed.
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

--- tests/internal/test_serverless.py
+++ tests/internal/test_serverless.py
@@ -1,4 +1,5 @@
 import mock
+import pytest
 
 from ddtrace.internal.serverless import in_azure_function
 from ddtrace.internal.serverless import in_gcp_function
@@ -78,47 +79,67 @@ def test_not_azure_function_consumption_plan():
     assert in_azure_function() is False
 
 
-def test_slow_imports():
+standard_blocklist = [
+    "ddtrace.appsec._api_security.api_manager",
+    "ddtrace.appsec._iast._ast.ast_patching",
+    "ddtrace.internal.telemetry.telemetry_writer",
+    "email.mime.application",
+    "email.mime.multipart",
+    "logging.handlers",
+    "multiprocessing",
+    "importlib_metadata",
+    # These modules must not be imported because their source files are
+    # specifically removed from the serverless python layer.
+    # See https://github.com/DataDog/datadog-lambda-python/blob/main/Dockerfile
+    "ddtrace.appsec._iast._taint_tracking._native",
+    "ddtrace.appsec._iast._stacktrace",
+    "ddtrace.internal.datadog.profiling.libdd_wrapper",
+    "ddtrace.internal.datadog.profiling.ddup._ddup",
+    "ddtrace.internal.datadog.profiling.stack_v2._stack_v2",
+]
+expanded_blocklist = standard_blocklist + [
+    "importlib.metadata",
+]
+
+
+@pytest.mark.parametrize(
+    "package,blocklist",
+    [
+        ("ddtrace", expanded_blocklist),
+        ("ddtrace.contrib.internal.aws_lambda", expanded_blocklist),
+        ("ddtrace.contrib.internal.psycopg", expanded_blocklist),
+        # requests imports urlib3 which imports importlib.metadata
+        ("ddtrace.contrib.requests", standard_blocklist),
+    ],
+)
+def test_slow_imports(package, blocklist, run_python_code_in_subprocess):
     # We should lazy load certain modules to avoid slowing down the startup
     # time when running in a serverless environment.  This test will fail if
     # any of those modules are imported during the import of ddtrace.
-    import os
-    import sys
 
-    os.environ.update(
-        {
-            "AWS_LAMBDA_FUNCTION_NAME": "foobar",
-            "DD_INSTRUMENTATION_TELEMETRY_ENABLED": "False",
-            "DD_API_SECURITY_ENABLED": "False",
-        }
-    )
+    env = {
+        "AWS_LAMBDA_FUNCTION_NAME": "foobar",
+        "DD_INSTRUMENTATION_TELEMETRY_ENABLED": "False",
+        "DD_API_SECURITY_ENABLED": "False",
+    }
 
-    blocklist = [
-        "ddtrace.appsec._api_security.api_manager",
-        "ddtrace.appsec._iast._ast.ast_patching",
-        "ddtrace.internal.telemetry.telemetry_writer",
-        "email.mime.application",
-        "email.mime.multipart",
-        "logging.handlers",
-        "multiprocessing",
-        "importlib.metadata",
-        "importlib_metadata",
-    ]
+    code = f"""
+import sys
 
-    class BlockListFinder:
-        def find_spec(self, fullname, *args):
-            for lib in blocklist:
-                if fullname == lib:
-                    raise ImportError(f"module {fullname} was imported!")
-            return None
+blocklist = {blocklist}
 
-    try:
-        sys.meta_path.insert(0, BlockListFinder())
+class BlockListFinder:
+    def find_spec(self, fullname, *args):
+        if fullname in blocklist:
+            raise ImportError(f"module {{fullname}} was imported!")
+        return None
 
-        import ddtrace
-        import ddtrace.contrib.internal.aws_lambda  # noqa:F401
-        import ddtrace.contrib.internal.psycopg  # noqa:F401
+sys.meta_path = [BlockListFinder()] + sys.meta_path
 
-    finally:
-        if isinstance(sys.meta_path[0], BlockListFinder):
-            sys.meta_path = sys.meta_path[1:]
+import {package}
+"""
+
+    stderr, stdout, status, _ = run_python_code_in_subprocess(code, env=env)
+    assert stdout.decode() == ""
+    assert stderr.decode() == ""
+    assert status == 0
