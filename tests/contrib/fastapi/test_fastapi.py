8a33892080ef9283331b3982c1eee8cbeeec362f
quinna-h <quinna.halim@datadoghq.com>
2025-03-03 15:35:09 -0600

    remove flaky

--- tests/contrib/fastapi/test_fastapi.py
+++ tests/contrib/fastapi/test_fastapi.py
@@ -558,7 +558,6 @@ def test_dont_trace_websocket_by_default(client, test_spans):
         assert len(spans) <= initial_event_count
 
 
-# @flaky(1741975978)
 # Ignoring span link attributes until values are
 # normalized: https://github.com/DataDog/dd-apm-test-agent/issues/154
 @snapshot(ignores=["meta._dd.span_links"])
