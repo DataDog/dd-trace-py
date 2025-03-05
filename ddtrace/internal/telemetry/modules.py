1e6cf4c848b5ae4c3602cd7b0533a1f528c448b9
Christophe Papazian <christophe.papazian@datadoghq.com>
2025-03-05 14:17:10 +0100

    properly update module set

--- ddtrace/internal/telemetry/modules.py
+++ ddtrace/internal/telemetry/modules.py
@@ -5,5 +5,5 @@ from typing import Set
 def get_newly_imported_modules(already_imported: Set[str]) -> Set[str]:
     latest_modules = set(sys.modules.keys())
     new_modules = latest_modules - already_imported
-    already_imported = latest_modules
+    already_imported.update(latest_modules)
     return new_modules
