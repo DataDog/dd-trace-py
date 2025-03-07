f5f2e7b9103faa07f0b016d8b697a1140a659b59
Yun Kim <35776586+Yun-Kim@users.noreply.github.com>
2025-03-07 16:04:04 -0500

    chore(llmobs): add span.finished telemetry metric (#12614)
    [MLOB-2216]
    This PR adds a `span.created` telemetry metric, which includes the tags
    (`decorator`, `integration`, `is_root_span`, `has_session_id`,
    `model_provider`).
    
    We added this in addition to `span.start` because right before finishing
    was the only place we had access to all of the above tags. Otherwise
    `autoinstrumented` was the only feasible tag we were guaranteed to have
    at span start time. We kept `span.start` as a metric to be able to count
    unfinished spans.
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    
    
    [MLOB-2216]:
    https://datadoghq.atlassian.net/browse/MLOB-2216?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- ddtrace/llmobs/_telemetry.py
+++ ddtrace/llmobs/_telemetry.py
@@ -2,6 +2,13 @@ import time
 
 from ddtrace.internal.telemetry import telemetry_writer
 from ddtrace.internal.telemetry.constants import TELEMETRY_NAMESPACE
+from ddtrace.llmobs._constants import DECORATOR
+from ddtrace.llmobs._constants import INTEGRATION
+from ddtrace.llmobs._constants import PARENT_ID_KEY
+from ddtrace.llmobs._constants import ROOT_PARENT_ID
+from ddtrace.llmobs._constants import SESSION_ID
+from ddtrace.llmobs._constants import SPAN_KIND
+from ddtrace.trace import Span
 
 
 def record_llmobs_enabled(error, agentless_enabled, site, start_ns):
@@ -14,4 +21,35 @@ def record_llmobs_enabled(error, agentless_enabled, site, start_ns):
     )
     telemetry_writer.add_count_metric(
         namespace=TELEMETRY_NAMESPACE.MLOBS, name="llmobs.enabled", value=1, tags=tuple(tags)
+    )
+
+
+def record_span_started():
+    telemetry_writer.add_count_metric(namespace=TELEMETRY_NAMESPACE.MLOBS, name="span.start", value=1)
+
+
+def record_span_created(span: Span):
+    is_root_span = span._get_ctx_item(PARENT_ID_KEY) != ROOT_PARENT_ID
+    has_session_id = span._get_ctx_item(SESSION_ID) is not None
+    integration = span._get_ctx_item(INTEGRATION)
+    autoinstrumented = integration is not None
+    decorator = span._get_ctx_item(DECORATOR) is True
+    span_kind = span._get_ctx_item(SPAN_KIND)
+    model_provider = span._get_ctx_item("model_provider")
+
+    tags = [
+        ("autoinstrumented", str(autoinstrumented)),
+        ("has_session_id", str(has_session_id)),
+        ("is_root_span", str(is_root_span)),
+        ("span_kind", span_kind or "N/A"),
+        ("error", str(span.error)),
+    ]
+    if autoinstrumented:
+        tags.append(("integration", integration))
+    else:
+        tags.append(("decorator", str(decorator)))
+    if model_provider:
+        tags.append(("model_provider", model_provider))
+    telemetry_writer.add_count_metric(
+        namespace=TELEMETRY_NAMESPACE.MLOBS, name="span.finished", value=1, tags=tuple(tags)
     )
