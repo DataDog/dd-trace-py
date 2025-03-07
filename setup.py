448a16ec77fd1a9419a99b54709e565c4b7bd4b2
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2025-03-07 11:54:22 +0100

    fix
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- setup.py
+++ setup.py
@@ -346,7 +346,7 @@ class CMakeBuild(build_ext):
         else:
             super().build_extension(ext)
 
-        if COMPILE_MODE == "release":
+        if COMPILE_MODE.lower() == "release":
             try:
                 self.try_strip_symbols(self.get_ext_fullpath(ext.name))
             except Exception as e:
