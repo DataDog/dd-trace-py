e7d7eaabeede9f8c097b7ca222e6a6b85dba54a1
Jessica Gamio <jessica.gamio@datadoghq.com>
2025-03-07 19:06:55 -0800

    replaced os.env with _get_config in rq integration

--- ddtrace/contrib/internal/rq/patch.py
+++ ddtrace/contrib/internal/rq/patch.py
@@ -1,5 +1,3 @@
-import os
-
 from ddtrace import config
 from ddtrace.constants import SPAN_KIND
 from ddtrace.internal import core
@@ -13,13 +11,16 @@ from ddtrace.trace import Pin
 
 from ....ext import SpanKind
 from ....ext import SpanTypes
+from ....settings._config import _get_config
 from ... import trace_utils
 
 
 config._add(
     "rq",
     dict(
-        distributed_tracing_enabled=asbool(os.environ.get("DD_RQ_DISTRIBUTED_TRACING_ENABLED", True)),
+        distributed_tracing_enabled=asbool(
+            _get_config("DD_RQ_DISTRIBUTED_TRACING_ENABLED", None, report_telemetry=False)
+        ),
         _default_service=schematize_service_name("rq"),
     ),
 )
@@ -27,7 +28,9 @@ config._add(
 config._add(
     "rq_worker",
     dict(
-        distributed_tracing_enabled=asbool(os.environ.get("DD_RQ_DISTRIBUTED_TRACING_ENABLED", True)),
+        distributed_tracing_enabled=asbool(
+            _get_config("DD_RQ_DISTRIBUTED_TRACING_ENABLED", None, report_telemetry=False)
+        ),
         _default_service=schematize_service_name("rq-worker"),
     ),
 )
