a7e5ea169c411a9eaef3960312aa19aeab23e5a7
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-03-11 20:59:00 +0000

    also handle the case from crashtracker_exe

--- ddtrace/commands/crashtracker_exe.py
+++ ddtrace/commands/crashtracker_exe.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from ddtrace.internal.native._native import crashtracker_receiver
+try:
+    from ddtrace.internal.native._native import crashtracker_receiver
+except ImportError:
+    def crashtracker_receiver():
+        print("Crashtracker receiver not available.")
 
 
 def main():
