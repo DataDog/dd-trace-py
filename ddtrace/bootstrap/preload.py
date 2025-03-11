f261985b7001813f566b9a6948574e86dea2d1fb
Mohammad Islam <mohammad.islam@datadoghq.com>
2025-03-11 15:08:21 -0400

    removed unused import

--- ddtrace/bootstrap/preload.py
+++ ddtrace/bootstrap/preload.py
@@ -16,7 +16,6 @@ from ddtrace.internal.utils.formats import asbool  # noqa:F401
 from ddtrace.internal.utils.formats import parse_tags_str  # noqa:F401
 from ddtrace.settings.crashtracker import config as crashtracker_config
 from ddtrace.trace import tracer
-from ddtrace.internal.utils.deprecations import DDTraceDeprecationWarning
 from ddtrace.vendor.debtcollector import deprecate
 
 import typing as t
@@ -105,7 +104,7 @@ if asbool(os.getenv("DD_TRACE_ENABLED", default=True)):
 
 if "DD_TRACE_GLOBAL_TAGS" in os.environ:
     env_tags = os.getenv("DD_TRACE_GLOBAL_TAGS")
-    if (env_tags is not None):
+    if env_tags is not None:
         deprecate(
             "DD_TRACE_GLOBAL_TAGS is deprecated",
             message="DD_TRACE_GLOBAL_TAGS is deprecated, please migrate to using DD_TAGS instead",
@@ -113,6 +112,7 @@ if "DD_TRACE_GLOBAL_TAGS" in os.environ:
         )
 
     tracer.set_tags(parse_tags_str(env_tags))
+
 
 @register_post_preload
 def _():
