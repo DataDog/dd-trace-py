615fcb897115003543bba1b0aad03d9a1cf01120
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-03-13 13:08:43 +0000

    cmake format

--- ddtrace/internal/datadog/profiling/cmake/FindLibdatadog.cmake
+++ ddtrace/internal/datadog/profiling/cmake/FindLibdatadog.cmake
@@ -52,8 +52,7 @@ endif()
 
 # Run the build
 execute_process(
-    COMMAND cargo run --bin release --features profiling --profile ${CARGO_PROFILE} -- --out
-            ${LIBDD_OUTPUT_FOLDER}
+    COMMAND cargo run --bin release --features profiling --profile ${CARGO_PROFILE} -- --out ${LIBDD_OUTPUT_FOLDER}
     WORKING_DIRECTORY "${libdatadog_SOURCE_DIR}"
     RESULT_VARIABLE CARGO_RESULT)
 if(NOT CARGO_RESULT EQUAL 0)
