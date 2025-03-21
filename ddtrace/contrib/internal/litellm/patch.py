a01b4159ba56096861495c5a1bd47f1e433cb8e6
Nicole Cybul <nicole.cybul@datadoghq.com>
2025-03-21 14:06:40 -0400

    remove provider tagging for now

--- ddtrace/contrib/internal/litellm/patch.py
+++ ddtrace/contrib/internal/litellm/patch.py
@@ -9,7 +9,6 @@ from ddtrace.contrib.trace_utils import unwrap
 from ddtrace.contrib.trace_utils import with_traced_module
 from ddtrace.contrib.trace_utils import wrap
 from ddtrace.contrib.internal.litellm.utils import tag_request
-from ddtrace.contrib.internal.litellm.utils import tag_model_and_provider
 from ddtrace.llmobs._integrations import LiteLLMIntegration
 from ddtrace.trace import Pin
 from ddtrace.internal.utils import get_argument_value
@@ -32,9 +31,11 @@ def get_version():
 def _create_span(litellm, pin, func, instance, args, kwargs):
     """Helper function to create and configure a traced span."""
     integration = litellm._datadog_integration
+    model = get_argument_value(args, kwargs, 0, "model", None)
     span = integration.trace(
         pin,
         "litellm.%s" % func.__name__,
+        model=model,
         submit_to_llmobs=False,
     )
     return span
@@ -42,7 +43,6 @@ def _create_span(litellm, pin, func, instance, args, kwargs):
 
 @with_traced_module
 def traced_completion(litellm, pin, func, instance, args, kwargs):
-    requested_model = get_argument_value(args, kwargs, 0, "model", None)
     span = _create_span(litellm, pin, func, instance, args, kwargs)
     tag_request(span, kwargs)
     try:
@@ -51,14 +51,11 @@ def traced_completion(litellm, pin, func, instance, args, kwargs):
         span.set_exc_info(*sys.exc_info())
         raise
     finally:
-        # tag model and provider
-        tag_model_and_provider(litellm, span, requested_model)
         span.finish()
 
 
 @with_traced_module
 async def traced_acompletion(litellm, pin, func, instance, args, kwargs):
-    requested_model = get_argument_value(args, kwargs, 0, "model", None)
     span = _create_span(litellm, pin, func, instance, args, kwargs)
     tag_request(span, kwargs)
     try:
@@ -67,19 +64,7 @@ async def traced_acompletion(litellm, pin, func, instance, args, kwargs):
         span.set_exc_info(*sys.exc_info())
         raise
     finally:
-        # tag model and provider
-        tag_model_and_provider(litellm, span, requested_model)
         span.finish()
-
-
-@with_traced_module
-def traced_get_llm_provider(litellm, pin, func, instance, args, kwargs):
-    requested_model = get_argument_value(args, kwargs, 0, "model", None)
-    integration = litellm._datadog_integration
-    model, custom_llm_provider, dynamic_api_key, api_base = func(*args, **kwargs)
-    # Store the provider information in the integration
-    integration._provider_map[requested_model] = custom_llm_provider
-    return model, custom_llm_provider, dynamic_api_key, api_base
 
 
 def patch():
@@ -96,7 +81,6 @@ def patch():
     wrap("litellm", "acompletion", traced_acompletion(litellm))
     wrap("litellm", "text_completion", traced_completion(litellm))
     wrap("litellm", "atext_completion", traced_acompletion(litellm))
-    wrap("litellm", "get_llm_provider", traced_get_llm_provider(litellm))
 
 
 def unpatch():
@@ -109,6 +93,5 @@ def unpatch():
     unwrap(litellm, "acompletion")
     unwrap(litellm, "text_completion")
     unwrap(litellm, "atext_completion")
-    unwrap(litellm, "get_llm_provider")
 
     delattr(litellm, "_datadog_integration")
