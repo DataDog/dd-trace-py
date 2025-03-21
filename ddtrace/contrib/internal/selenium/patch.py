6c853f58e7e664972a628e7519ea64cbffa8c1d3
Federico Mon <federico.mon@datadoghq.com>
2025-03-21 15:07:55 +0100

    switch to DDConfig, fix defaults and types

--- ddtrace/contrib/internal/selenium/patch.py
+++ ddtrace/contrib/internal/selenium/patch.py
@@ -26,23 +26,9 @@ if (window.DD_RUM && window.DD_RUM.stopSession) {
 }
 """
 
-_DEFAULT_FLUSH_SLEEP_MS = 500
-
 
 def _get_flush_sleep_ms() -> int:
-    env_flush_sleep_ms = TestOptEnv.CIVisibilityEnv.rum_flush_wait_millis
-    if env_flush_sleep_ms is None:
-        return _DEFAULT_FLUSH_SLEEP_MS
-
-    try:
-        return int(env_flush_sleep_ms)
-    except Exception:  # noqa E722
-        log.warning(
-            "Could not convert DD_CIVISIBILITY_RUM_FLUSH_WAIT_MILLIS value %s to int, using default: %s",
-            env_flush_sleep_ms,
-            _DEFAULT_FLUSH_SLEEP_MS,
-        )
-        return _DEFAULT_FLUSH_SLEEP_MS
+    return TestOptEnv.CIVisibilityEnv.rum_flush_wait_millis
 
 
 config._add(
--- ddtrace/internal/ci_visibility/settings.py
+++ ddtrace/internal/ci_visibility/settings.py
@@ -1,19 +1,18 @@
-from envier import Env
-
 from ddtrace.internal.ci_visibility.constants import AGENTLESS_DEFAULT_SITE
+from ddtrace.settings._core import DDConfig
 
 
 DEFAULT_TIMEOUT = 15
 DEFAULT_ITR_SKIPPABLE_TIMEOUT = 20
 
 
-class CIEnv(Env):
+class CIEnv(DDConfig):
     """Class encapsulating CI environment variables and their access."""
 
     # Prefix for all CI environment variables
-    __prefix__ = "CI"
+    __prefix__ = "ci"
 
-    class DDEnv(Env):
+    class DDEnv(DDConfig):
         """Class encapsulating CI DD environment variables and their access.
 
         These are internal environment variables primarily used for testing
@@ -21,54 +20,54 @@ class CIEnv(Env):
         """
 
         # Prefix for CI_DD environment variables
-        __prefix__ = "DD"
+        __prefix__ = "dd"
 
         # Agent URL
-        agent_url = Env.var(str, "agent_url", private=True, default="", help="Internal agent URL used for testing")
+        agent_url = DDConfig.v(str, "agent_url", private=True, default="", help="Internal agent URL used for testing")
 
         # Authentication
-        api_key = Env.var(str, "api_key", private=True, default=None, help="Internal API key used for testing")
+        api_key = DDConfig.v(str, "api_key", private=True, default="", help="Internal API key used for testing")
 
         # Env
-        env = Env.var(str, "env", private=True, default=None, help="Environment name for CI/CD pipelines")
+        env = DDConfig.v(str, "env", private=True, default="", help="Environment name for CI/CD pipelines")
 
 
-class TestOptEnv(Env):
+class TestOptEnv(DDConfig):
     """Class encapsulating Test Optimization environment variables and their access."""
 
     # Prefix for all environment variables
-    __prefix__ = "DD"
+    __prefix__ = "dd"
 
     # Used in recorder.py
-    api_key = Env.var(str, "api_key", default=None)
-    site = Env.var(str, "site", default=AGENTLESS_DEFAULT_SITE)
+    api_key = DDConfig.v(str, "api_key", default="")
+    site = DDConfig.v(str, "site", default=AGENTLESS_DEFAULT_SITE)
 
-    class CIVisibilityEnv(Env):
+    class CIVisibilityEnv(DDConfig):
         """Class encapsulating CI Visibility environment variables and their access."""
 
         # Prefix for CI Visibility environment variables
-        __prefix__ = "CIVISIBILITY"
+        __prefix__ = "civisibility"
 
         # ITR-related settings
-        itr_suite_mode = Env.var(bool, "itr_suite_mode", private=True, default=False)
-        itr_force_enable_coverage = Env.var(bool, "itr_force_enable_coverage", default=False, private=True)
-        itr_prevent_test_skipping = Env.var(bool, "itr_prevent_test_skipping", default=False, private=True)
+        itr_suite_mode = DDConfig.v(bool, "itr_suite_mode", private=True, default=False)
+        itr_force_enable_coverage = DDConfig.v(bool, "itr_force_enable_coverage", default=False, private=True)
+        itr_prevent_test_skipping = DDConfig.v(bool, "itr_prevent_test_skipping", default=False, private=True)
 
         # Flaky test retries
-        flaky_retry_enabled = Env.var(bool, "flaky_retry_enabled", default=True)
-        flaky_retry_count = Env.var(int, "flaky_retry_count", default=5)
-        total_flaky_retry_count = Env.var(int, "total_flaky_retry_count", default=1000)
+        flaky_retry_enabled = DDConfig.v(bool, "flaky_retry_enabled", default=True)
+        flaky_retry_count = DDConfig.v(int, "flaky_retry_count", default=5)
+        total_flaky_retry_count = DDConfig.v(int, "total_flaky_retry_count", default=1000)
 
         # Test management
-        test_management_enabled = Env.var(bool, "test_management_enabled", default=True)
-        test_management_attempt_to_fix_retries = Env.var(int, "test_management_attempt_to_fix_retries", default=20)
+        test_management_enabled = DDConfig.v(bool, "test_management_enabled", default=True)
+        test_management_attempt_to_fix_retries = DDConfig.v(int, "test_management_attempt_to_fix_retries", default=20)
 
         # Context provider
-        use_ci_context_provider = Env.var(bool, "use_ci_context_provider", private=True, default=False)
-        auto_instrumentation_provider = Env.var(str, "auto_instrumentation_provider", default=None)
+        use_ci_context_provider = DDConfig.v(bool, "use_ci_context_provider", private=True, default=False)
+        auto_instrumentation_provider = DDConfig.v(bool, "auto_instrumentation_provider", default=False)
 
         # UI testing
-        rum_flush_wait_millis = Env.var(str, "rum_flush_wait_millis", default=None)
+        rum_flush_wait_millis = DDConfig.v(int, "rum_flush_wait_millis", default=500)
 
         # Unittest settings
-        unittest_strict_naming = Env.var(bool, "unittest_strict_naming", default=True)
+        unittest_strict_naming = DDConfig.v(bool, "unittest_strict_naming", default=True)
