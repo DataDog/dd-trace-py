9f63ceeb0318257a3f13fa590562f95656e3a56b
Nicole Cybul <nicole.cybul@datadoghq.com>
2025-03-21 16:02:14 -0400

    remove unnecessary bedrock credentials in tests

--- tests/contrib/litellm/test_litellm.py
+++ tests/contrib/litellm/test_litellm.py
@@ -81,13 +81,6 @@ async def test_litellm_atext_completion(litellm, request_vcr, stream, n):
 @pytest.mark.parametrize("model", ["vertex_ai/gemini-pro", "anthropic/claude-3-5-sonnet-20240620"])
 @pytest.mark.snapshot(token="tests.contrib.litellm.test_litellm.test_litellm_completion_different_models")
 def test_litellm_completion_different_models(litellm, request_vcr, model):
-    aws_access_key_id = ""
-    aws_secret_access_key = ""
-    aws_region_name = ""
-    if model == "bedrock/amazon.titan-text-lite-v1":
-        aws_access_key_id = "ASIAWYLNJGWWOJPUYN45"
-        aws_secret_access_key = "z99X8m2gwAHrXGH8Owd0gzHq5ndHahRW0nEU8xbu"
-        aws_region_name = "us-east-1"
     with request_vcr.use_cassette(f"completion_{model}.yaml"):
         messages = [{"content": "Hey, what is up?", "role": "user"}]
         litellm.completion(
@@ -95,7 +88,4 @@ def test_litellm_completion_different_models(litellm, request_vcr, model):
             messages=messages,
             stream=False,
             n=1,
-            aws_access_key_id=aws_access_key_id,
-            aws_secret_access_key=aws_secret_access_key,
-            aws_region_name=aws_region_name,
         )
