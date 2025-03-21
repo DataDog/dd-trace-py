5faa428b854e0070d801b0620f39bafc195a7f2e
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-03-21 20:11:19 +0000

    chore(profiling): enable native extensions for x86-64

--- setup.py
+++ setup.py
@@ -647,9 +647,7 @@ if not IS_PYSTON:
             CMakeExtension("ddtrace.appsec._iast._taint_tracking._native", source_dir=IAST_DIR, optional=False)
         )
 
-    if (
-        platform.system() == "Linux" or (platform.system() == "Darwin" and platform.machine() == "arm64")
-    ) and is_64_bit_python():
+    if (platform.system() == "Linux" or platform.system() == "Darwin") and is_64_bit_python():
         ext_modules.append(
             CMakeExtension(
                 "ddtrace.internal.datadog.profiling.ddup._ddup",
