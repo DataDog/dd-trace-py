a066c99f2d0bfb98c6ccb1cf6d22d2af5c89cc44
Nicole Cybul <nicole.cybul@datadoghq.com>
2025-03-23 14:25:53 -0400

    add documentation

--- ddtrace/contrib/internal/litellm/utils.py
+++ ddtrace/contrib/internal/litellm/utils.py
@@ -1,3 +1,3 @@
 def tag_request(span, kwargs):
     if "metadata" in kwargs and "headers" in kwargs["metadata"] and "host" in kwargs["metadata"]["headers"]:
-        span.set_tag_str("litellm.host", kwargs["metadata"]["headers"]["host"])
+        span.set_tag_str("litellm.request.host", kwargs["metadata"]["headers"]["host"])
--- ddtrace/llmobs/_integrations/litellm.py
+++ ddtrace/llmobs/_integrations/litellm.py
@@ -8,12 +8,9 @@ from ddtrace.llmobs._integrations.base import BaseLLMIntegration
 
 class LiteLLMIntegration(BaseLLMIntegration):
     _integration_name = "litellm"
-    _provider_map = {}
 
     def _set_base_span_tags(
-        self, span: Span, provider: Optional[str] = None, model: Optional[str] = None, **kwargs: Dict[str, Any]
+        self, span: Span, model: Optional[str] = None, **kwargs: Dict[str, Any]
     ) -> None:
-        if provider is not None:
-            span.set_tag_str("litellm.request.provider", provider)
         if model is not None:
             span.set_tag_str("litellm.request.model", model)
