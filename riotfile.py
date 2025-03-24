2b5294dc8742e47c7491d2e483b47b71fdd0edf0
erikayasuda <erika.yasuda@datadoghq.com>
2025-03-24 13:07:04 -0400

    more pinning

--- riotfile.py
+++ riotfile.py
@@ -122,7 +122,6 @@ venv = Venv(
             pkgs={
                 "ruamel.yaml": latest,
                 "lxml": latest,
-                "setuptools": "77.0.3",
             },
         ),
         Venv(
