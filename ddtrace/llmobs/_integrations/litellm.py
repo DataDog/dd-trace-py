2ee62ab302bfe89db9d6cb87848face59e5ebb33
Nicole Cybul <nicole.cybul@datadoghq.com>
2025-03-24 10:40:01 -0400

    style fixes

--- ddtrace/llmobs/_integrations/litellm.py
+++ ddtrace/llmobs/_integrations/litellm.py
@@ -9,8 +9,6 @@ from ddtrace.llmobs._integrations.base import BaseLLMIntegration
 class LiteLLMIntegration(BaseLLMIntegration):
     _integration_name = "litellm"
 
-    def _set_base_span_tags(
-        self, span: Span, model: Optional[str] = None, **kwargs: Dict[str, Any]
-    ) -> None:
+    def _set_base_span_tags(self, span: Span, model: Optional[str] = None, **kwargs: Dict[str, Any]) -> None:
         if model is not None:
             span.set_tag_str("litellm.request.model", model)
