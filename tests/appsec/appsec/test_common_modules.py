206e97eb2f89e5360cd5b4dc0ff1dc6dd8bd0726
Alberto Vara <alberto.vara@datadoghq.com>
2025-03-28 16:25:47 +0100

    fix: implement __deepcopy__ for wrapped builtin functions in ASM/IAST

--- tests/appsec/appsec/test_common_modules.py
+++ tests/appsec/appsec/test_common_modules.py
@@ -1,9 +1,15 @@
+import builtins
 import copy
 import types
 
+import pytest
 from wrapt import FunctionWrapper
+from wrapt import wrap_object
 
-import ddtrace.appsec._common_module_patches as cmp
+from ddtrace.appsec._common_module_patches import DataDogFunctionWrapper
+from ddtrace.appsec._common_module_patches import patch_common_modules
+from ddtrace.appsec._common_module_patches import try_wrap_function_wrapper
+from ddtrace.appsec._common_module_patches import unpatch_common_modules
 
 
 def test_patch_read():
@@ -19,13 +25,106 @@ def test_patch_read():
 def test_patch_read_enabled():
     original_open = open
     try:
-        cmp.patch_common_modules()
+        patch_common_modules()
         copy_open = copy.deepcopy(open)
 
-        assert type(open) == cmp.DataDogFunctionWrapper
-        assert isinstance(copy_open, cmp.DataDogFunctionWrapper)
-        assert isinstance(open, cmp.DataDogFunctionWrapper)
+        assert type(open) == DataDogFunctionWrapper
+        assert isinstance(copy_open, DataDogFunctionWrapper)
+        assert isinstance(open, DataDogFunctionWrapper)
         assert hasattr(open, "__wrapped__")
         assert open.__wrapped__ is original_open
     finally:
-        cmp.unpatch_common_modules()
+        unpatch_common_modules()
+
+
+@pytest.mark.parametrize(
+    "builtin_function_name",
+    [
+        "all",
+        "anext",
+        "any",
+        "ascii",
+        "bin",
+        "bool",
+        "breakpoint",
+        "bytearray",
+        "bytes",
+        "callable",
+        "chr",
+        "classmethod",
+        "compile",
+        "complex",
+        "copyright",
+        "credits",
+        "delattr",
+        "dict",
+        "dir",
+        "divmod",
+        "enumerate",
+        "eval",
+        "exec",
+        "exit",
+        "filter",
+        "float",
+        "format",
+        "frozenset",
+        "getattr",
+        "globals",
+        "hasattr",
+        "hash",
+        "help",
+        "hex",
+        "id",
+        "input",
+        "int",
+        "isinstance",
+        "issubclass",
+        "iter",
+        "len",
+        "license",
+        "list",
+        "locals",
+        "map",
+        "max",
+        "memoryview",
+        "min",
+        "next",
+        "object",
+        "oct",
+        "open",
+        "ord",
+        "pow",
+        "print",
+        "property",
+        "quit",
+        "range",
+        "repr",
+        "reversed",
+        "round",
+        "set",
+        "setattr",
+        "slice",
+        "sorted",
+        "staticmethod",
+        "str",
+        "sum",
+        "super",
+        "tuple",
+        "type",
+        "vars",
+        "zip",
+    ],
+)
+def test_other_builtin_functions(builtin_function_name):
+    def dummywrapper(callable, instance, args, kwargs):
+        return callable(*args, **kwargs)
+
+    try_wrap_function_wrapper("builtins", builtin_function_name, dummywrapper)
+
+    original_func = getattr(builtins, builtin_function_name)
+    copy_func = copy.deepcopy(original_func)
+
+    assert type(original_func) == DataDogFunctionWrapper
+    assert isinstance(copy_func, DataDogFunctionWrapper)
+    assert isinstance(original_func, DataDogFunctionWrapper)
+    assert hasattr(original_func, "__wrapped__")
