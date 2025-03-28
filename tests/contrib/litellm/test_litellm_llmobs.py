60830c4ed05427efd752a67fc97e19d8b75a89a6
Nicole Cybul <nicole.cybul@datadoghq.com>
2025-03-28 11:06:47 -0400

    add more tests

--- tests/contrib/litellm/test_litellm_llmobs.py
+++ tests/contrib/litellm/test_litellm_llmobs.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tests.contrib.litellm.utils import get_cassette_name, consume_stream
+from tests.contrib.litellm.utils import async_consume_stream, get_cassette_name, consume_stream, parse_response
 from tests.llmobs._utils import _expected_llmobs_llm_span_event
 
 
@@ -8,8 +8,19 @@ from tests.llmobs._utils import _expected_llmobs_llm_span_event
     "ddtrace_global_config", [dict(_llmobs_enabled=True, _llmobs_sample_rate=1.0, _llmobs_ml_app="<ml-app-name>")]
 )
 class TestLLMObsLiteLLM:
-    # TODO: need to behind the scenes extract token usage from the stream so that it can still be reported even if not returned in the response
-    @pytest.mark.parametrize("stream,n,include_usage", [(True, 1, True), (True, 2, True), (False, 1, True), (False, 2, True), (True, 1, False), (True, 2, False), (False, 1, False), (False, 2, False)])
+    @pytest.mark.parametrize(
+        "stream,n,include_usage",
+        [
+            (True, 1, True),
+            (True, 2, True),
+            (False, 1, True),
+            (False, 2, True),
+            (True, 1, False),
+            (True, 2, False),
+            (False, 1, False),
+            (False, 2, False),
+        ],
+    )
     def test_completion(self, litellm, request_vcr, mock_llmobs_writer, mock_tracer, stream, n, include_usage):
         with request_vcr.use_cassette(get_cassette_name(stream, n, include_usage)):
             messages = [{"content": "Hey, what is up?", "role": "user"}]
@@ -23,12 +34,8 @@ class TestLLMObsLiteLLM:
             if stream:
                 output_messages, token_metrics = consume_stream(resp, n)
             else:
-                output_messages = [{"content": message.message.content, "role": message.message.role} for message in resp.choices]
-                token_metrics = {
-                    "input_tokens": resp.usage.prompt_tokens,
-                    "output_tokens": resp.usage.completion_tokens,
-                    "total_tokens": resp.usage.total_tokens,
-                }
+                output_messages, token_metrics = parse_response(resp)
+
         span = mock_tracer.pop_traces()[0][0]
         assert mock_llmobs_writer.enqueue.call_count == 1
         mock_llmobs_writer.enqueue.assert_called_with(
@@ -44,4 +51,133 @@ class TestLLMObsLiteLLM:
             )
         )
 
+    @pytest.mark.parametrize(
+        "stream,n,include_usage",
+        [
+            (True, 1, True),
+            (True, 2, True),
+            (False, 1, True),
+            (False, 2, True),
+            (True, 1, False),
+            (True, 2, False),
+            (False, 1, False),
+            (False, 2, False),
+        ],
+    )
+    async def test_acompletion(self, litellm, request_vcr, mock_llmobs_writer, mock_tracer, stream, n, include_usage):
+        with request_vcr.use_cassette(get_cassette_name(stream, n, include_usage)):
+            messages = [{"content": "Hey, what is up?", "role": "user"}]
+            resp = await litellm.acompletion(
+                model="gpt-3.5-turbo",
+                messages=messages,
+                stream=stream,
+                n=n,
+                stream_options={"include_usage": include_usage},
+            )
+            if stream:
+                output_messages, token_metrics = await async_consume_stream(resp, n)
+            else:
+                output_messages, token_metrics = parse_response(resp)
 
+        span = mock_tracer.pop_traces()[0][0]
+        assert mock_llmobs_writer.enqueue.call_count == 1
+        mock_llmobs_writer.enqueue.assert_called_with(
+            _expected_llmobs_llm_span_event(
+                span,
+                model_name="gpt-3.5-turbo",
+                model_provider="openai",
+                input_messages=messages,
+                output_messages=output_messages,
+                metadata={"stream": stream, "n": n, "stream_options": {"include_usage": include_usage}},
+                token_metrics=token_metrics,
+                tags={"ml_app": "<ml-app-name>", "service": "tests.contrib.litellm"},
+            )
+        )
+
+    @pytest.mark.parametrize(
+        "stream,n,include_usage",
+        [
+            (True, 1, True),
+            (True, 2, True),
+            (False, 1, True),
+            (False, 2, True),
+            (True, 1, False),
+            (True, 2, False),
+            (False, 1, False),
+            (False, 2, False),
+        ],
+    )
+    def test_text_completion(self, litellm, request_vcr, mock_llmobs_writer, mock_tracer, stream, n, include_usage):
+        with request_vcr.use_cassette(get_cassette_name(stream, n, include_usage)):
+            prompt = "Hey, what is up?"
+            resp = litellm.text_completion(
+                model="gpt-3.5-turbo",
+                prompt=prompt,
+                stream=stream,
+                n=n,
+                stream_options={"include_usage": include_usage},
+            )
+            if stream:
+                output_messages, token_metrics = consume_stream(resp, n, is_completion=True)
+            else:
+                output_messages, token_metrics = parse_response(resp, is_completion=True)
+
+        span = mock_tracer.pop_traces()[0][0]
+        assert mock_llmobs_writer.enqueue.call_count == 1
+        mock_llmobs_writer.enqueue.assert_called_with(
+            _expected_llmobs_llm_span_event(
+                span,
+                model_name="gpt-3.5-turbo",
+                model_provider="openai",
+                input_messages=[{"content": prompt}],
+                output_messages=output_messages,
+                metadata={"stream": stream, "n": n, "stream_options": {"include_usage": include_usage}},
+                token_metrics=token_metrics,
+                tags={"ml_app": "<ml-app-name>", "service": "tests.contrib.litellm"},
+            )
+        )
+
+    @pytest.mark.parametrize(
+        "stream,n,include_usage",
+        [
+            (True, 1, True),
+            (True, 2, True),
+            (False, 1, True),
+            (False, 2, True),
+            (True, 1, False),
+            (True, 2, False),
+            (False, 1, False),
+            (False, 2, False),
+        ],
+    )
+    async def test_atext_completion(
+        self, litellm, request_vcr, mock_llmobs_writer, mock_tracer, stream, n, include_usage
+    ):
+        with request_vcr.use_cassette(get_cassette_name(stream, n, include_usage)):
+            prompt = "Hey, what is up?"
+            resp = await litellm.atext_completion(
+                model="gpt-3.5-turbo",
+                prompt=prompt,
+                stream=stream,
+                n=n,
+                stream_options={"include_usage": include_usage},
+            )
+            if stream:
+                output_messages, token_metrics = await async_consume_stream(resp, n, is_completion=True)
+            else:
+                output_messages, token_metrics = parse_response(resp, is_completion=True)
+
+        span = mock_tracer.pop_traces()[0][0]
+        assert mock_llmobs_writer.enqueue.call_count == 1
+        mock_llmobs_writer.enqueue.assert_called_with(
+            _expected_llmobs_llm_span_event(
+                span,
+                model_name="gpt-3.5-turbo",
+                model_provider="openai",
+                input_messages=[{"content": prompt}],
+                output_messages=output_messages,
+                metadata={"stream": stream, "n": n, "stream_options": {"include_usage": include_usage}},
+                token_metrics=token_metrics,
+                tags={"ml_app": "<ml-app-name>", "service": "tests.contrib.litellm"},
+            )
+        )
--- tests/contrib/litellm/utils.py
+++ tests/contrib/litellm/utils.py
@@ -32,22 +32,55 @@ def get_cassette_name(stream, n, include_usage=True):
     return "completion" + stream_suffix + choice_suffix + usage_suffix + CASETTE_EXTENSION
 
 
-def consume_stream(resp, n):
-    output_messages = [{"content": "", "role": ""} for _ in range(n)]
+def consume_stream(resp, n, is_completion=False):
+    output_messages = [{"content": ""} for _ in range(n)]
     token_metrics = {}
     role = None
     for chunk in resp:
-        for choice in chunk["choices"]:
-            content = choice["delta"]["content"] or ""
-            output_messages[choice.index]["content"] += content
-            if not output_messages[choice.index]["role"]:
-                role = choice["delta"]["role"] or role
-                output_messages[choice.index]["role"] = role
+        role = extract_output_from_chunk(chunk, output_messages, token_metrics, role, is_completion)
+    return output_messages, token_metrics
 
-        if "usage" in chunk:
-            token_metrics = {
+
+async def async_consume_stream(resp, n, is_completion=False):
+    output_messages = [{"content": ""} for _ in range(n)]
+    token_metrics = {}
+    role = None
+    async for chunk in resp:
+        role = extract_output_from_chunk(chunk, output_messages, token_metrics, role, is_completion)
+    return output_messages, token_metrics
+
+
+def extract_output_from_chunk(chunk, output_messages, token_metrics, role, is_completion=False):
+    for choice in chunk["choices"]:
+        content = choice["text"] if is_completion else choice["delta"]["content"]
+        content = content or ""
+        output_messages[choice.index]["content"] += content
+        if "role" not in output_messages[choice.index] and (choice.get("delta", {}).get("role") or role):
+            role = choice.get("delta", {}).get("role") or role
+            output_messages[choice.index]["role"] = role
+
+    if "usage" in chunk and chunk["usage"]:
+        token_metrics.update(
+            {
                 "input_tokens": chunk["usage"]["prompt_tokens"],
                 "output_tokens": chunk["usage"]["completion_tokens"],
                 "total_tokens": chunk["usage"]["total_tokens"],
             }
+        )
+
+    return role
+
+
+def parse_response(resp, is_completion=False):
+    output_messages = []
+    for choice in resp.choices:
+        message = {"content": choice.text if is_completion else choice.message.content}
+        if choice.get("role", None) or choice.get("message", {}).get("role", None):
+            message["role"] = choice["role"] if is_completion else choice["message"]["role"]
+        output_messages.append(message)
+    token_metrics = {
+        "input_tokens": resp.usage.prompt_tokens,
+        "output_tokens": resp.usage.completion_tokens,
+        "total_tokens": resp.usage.total_tokens,
+    }
     return output_messages, token_metrics
