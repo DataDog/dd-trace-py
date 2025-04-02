338b1aa239a5dcd4bbe9bea77184c5e3e6989634
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-04-02 14:36:30 -0400

    update test

--- tests/internal/test_packages.py
+++ tests/internal/test_packages.py
@@ -52,14 +52,6 @@ def test_get_distributions():
             importlib_pkgs.add("typing-extensions")
         elif pkg.name == "pkgutil_resolve_name" and "pkgutil-resolve-name" in pkg_resources_ws:
             importlib_pkgs.add("pkgutil-resolve-name")
-        elif pkg.name == "importlib_metadata" and "importlib-metadata" in pkg_resources_ws:
-            importlib_pkgs.add("importlib-metadata")
-        elif pkg.name == "importlib-metadata" and "importlib_metadata" in pkg_resources_ws:
-            importlib_pkgs.add("importlib_metadata")
-        elif pkg.name == "importlib-resources" and "importlib_resources" in pkg_resources_ws:
-            importlib_pkgs.add("importlib_resources")
-        elif pkg.name == "importlib_resources" and "importlib-resources" in pkg_resources_ws:
-            importlib_pkgs.add("importlib-resources")
         else:
             importlib_pkgs.add(pkg.name)
 
