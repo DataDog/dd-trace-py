1a200b43b41d3236e8dbdba1c7a1e3d286d5f377
lievan <evan.li@datadoqhq.com>
2025-04-03 16:14:07 -0400

    another reset:

--- tests/llmobs/conftest.py
+++ tests/llmobs/conftest.py
@@ -103,6 +103,7 @@ def mock_evaluator_logs():
 def mock_evaluator_sampler_logs():
     with mock.patch("ddtrace.llmobs._evaluators.sampler.logger") as m:
         yield m
+        m.reset_mock()
 
 
 @pytest.fixture
