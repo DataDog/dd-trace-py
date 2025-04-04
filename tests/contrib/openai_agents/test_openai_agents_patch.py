e2480fb2fd90bfb9ba55c597feeee601b2d3edc8
lievan <evan.li@datadoqhq.com>
2025-04-04 15:19:38 -0400

    skip

--- tests/contrib/openai_agents/test_openai_agents_patch.py
+++ tests/contrib/openai_agents/test_openai_agents_patch.py
@@ -1,3 +1,5 @@
+import pytest
+
 from ddtrace.contrib.internal.openai_agents.patch import get_version
 from ddtrace.contrib.internal.openai_agents.patch import patch
 from ddtrace.contrib.internal.openai_agents.patch import unpatch
@@ -10,6 +12,11 @@ class TestAgentsPatch(PatchTestCase.Base):
     __patch_func__ = patch
     __unpatch_func__ = unpatch
     __get_version__ = get_version
+
+    @pytest.mark.skip(reason="Not applicable for openai_agents")
+    def test_ddtrace_run_patch_on_import(self):
+        # This overrides the parent method and skips it
+        pass
 
     def assert_module_patched(self, agents):
         pass
