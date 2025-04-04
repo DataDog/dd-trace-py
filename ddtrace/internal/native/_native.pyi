cd8ddbe0974347f2e77e1fa2cab95beac188c940
vianney <vianney.ruhlmann@datadoghq.com>
2025-04-04 16:35:18 +0200

    chore(native): Update type definitions and exports

--- ddtrace/internal/native/_native.pyi
+++ ddtrace/internal/native/_native.pyi
@@ -111,10 +111,11 @@ class TraceExporter:
         """
         ...
 
-    def send(self, data: bytes) -> None:
+    def send(self, data: bytes, trace_count: int) -> str:
         """
         Send a trace payload to the Agent.
         :param data: The msgpack encoded trace payload to send.
+        :param trace_count: The number of traces in the data payload.
         """
         ...
 
@@ -129,87 +130,120 @@ class TraceExporterBuilder:
         """
         ...
 
-    def set_hostname(self, hostname: str) -> None:
+    def set_hostname(self, hostname: str) -> TraceExporterBuilder:
         """
         Set the hostname of the TraceExporter.
         :param hostname: The hostname to set for the TraceExporter.
         """
         ...
 
-    def set_url(self, url: str) -> None:
+    def set_url(self, url: str) -> TraceExporterBuilder:
         """
         Set the agent url of the TraceExporter.
         :param url: The URL of the agent to send traces to.
         """
         ...
 
-    def set_dogstatsd_url(self, url: str) -> None:
+    def set_dogstatsd_url(self, url: str) -> TraceExporterBuilder:
         """
         Set the DogStatsD URL of the TraceExporter.
         :param url: The URL of the DogStatsD endpoint.
         """
         ...
 
-    def set_env(self, env: str) -> None:
+    def set_env(self, env: str) -> TraceExporterBuilder:
         """
         Set the env of the TraceExporter.
         :param env: The environment name (e.g., 'prod', 'staging', 'dev').
         """
         ...
 
-    def set_app_version(self, version: str) -> None:
+    def set_app_version(self, version: str) -> TraceExporterBuilder:
         """
         Set the app version of the TraceExporter.
         :param version: The version string of the application.
         """
         ...
 
-    def set_git_commit_sha(self, git_commit_sha: str) -> None:
+    def set_git_commit_sha(self, git_commit_sha: str) -> TraceExporterBuilder:
         """
         Set the git commit sha of the TraceExporter.
         :param git_commit_sha: The git commit SHA of the current code version.
         """
         ...
 
-    def set_tracer_version(self, version: str) -> None:
+    def set_tracer_version(self, version: str) -> TraceExporterBuilder:
         """
         Set the tracer version of the TraceExporter.
         :param version: The version string of the tracer.
         """
         ...
 
-    def set_language(self, language: str) -> None:
+    def set_language(self, language: str) -> TraceExporterBuilder:
         """
         Set the language of the TraceExporter.
         :param language: The programming language being traced (e.g., 'python').
         """
         ...
 
-    def set_language_version(self, version: str) -> None:
+    def set_language_version(self, version: str) -> TraceExporterBuilder:
         """
         Set the language version of the TraceExporter.
         :param version: The version string of the programming language.
         """
         ...
 
-    def set_language_interpreter_vendor(self, vendor: str) -> None:
+    def set_language_interpreter(self, interpreter: str) -> TraceExporterBuilder:
+        """
+        Set the language interpreter of the TraceExporter.
+        :param vendor: The language interpreter.
+        """
+        ...
+
+    def set_language_interpreter_vendor(self, vendor: str) -> TraceExporterBuilder:
         """
         Set the language interpreter vendor of the TraceExporter.
-        :param vendor: The vendor of the language interpreter (e.g., 'CPython').
+        :param vendor: The vendor of the language interpreter.
         """
         ...
 
-    def set_input_format(self, format: TraceFormat) -> None:
+    def set_input_format(self, format: str) -> TraceExporterBuilder:
         """
         Set the input format for the trace data.
-        :param format: The format to use for input traces (v04 or v05).
+        :param format: The format to use for input traces (supported values are "v0.4" and "v0.5").
+        :raises ValueError: If format is not a supported value.
         """
         ...
 
-    def set_output_format(self, format: TraceFormat) -> None:
+    def set_output_format(self, format: str) -> TraceExporterBuilder:
         """
         Set the output format for the trace data.
-        :param format: The format to use for output traces (v04 or v05).
+        :param format: The format to use for output traces (supported values are "v0.4" and "v0.5").
+        :raises ValueError: If format is not a supported value.
+        """
+        ...
+
+    def set_client_computed_top_level(self) -> TraceExporterBuilder:
+        """
+        Set the header indicating the tracer has computed the top-level tag
+        """
+        ...
+
+    def enable_stats(self, bucket_size_ns: int) -> TraceExporterBuilder:
+        """
+        Enable stats computation in the TraceExporter
+        :param bucket_size_ns: The size of stats bucket in nanoseconds.
+        """
+        ...
+
+    def enable_telemetry(
+        self,
+        heartbeat: int,
+        runtime_id: str,
+    ) -> TraceExporterBuilder:
+        """
+        Enable stats computation in the TraceExporter
+        :param bucket_size_ns: The size of stats bucket in nanoseconds.
         """
         ...
 
@@ -222,10 +256,56 @@ class TraceExporterBuilder:
         """
         ...
 
-class TraceFormat:
+class AgentError(Exception):
     """
-    TraceFormat is an enum of possible formats for traces.
+    Raised when there is an error in agent response processing.
     """
 
-    V04 = "v04"
-    V05 = "v05"
+    ...
+
+class BuilderError(Exception):
+    """
+    Raised when there is an error in the TraceExporterBuilder configuration.
+    """
+
+    ...
+
+class DeserializationError(Exception):
+    """
+    Raised when there is an error deserializing trace payload.
+    """
+
+    ...
+
+class IoError(Exception):
+    """
+    Raised when there is an I/O error during trace processing.
+    """
+
+    ...
+
+class NetworkError(Exception):
+    """
+    Raised when there is a network-related error during trace processing.
+    """
+
+    ...
+
+class RequestError(Exception):
+    """
+    Raised when the agent responds with an error code.
+    """
+
+    ...
+
+class SerializationError(Exception):
+    """
+    Raised when there is an error serializing trace payload.
+    """
+
+    ...
+
+class EncodingNotSupportedError(Exception):
+    """
+    Raised when the agent return a 404 or 415 error code, indicating the encoding format is not supported.
+    """
