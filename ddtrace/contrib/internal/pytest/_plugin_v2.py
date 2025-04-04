c8144b9dccfed75c3b25cdeb421f87244f0def6c
Vítor De Araújo <vitor.dearaujo@datadoghq.com>
2025-04-04 15:55:47 +0000

    moar imports

--- ddtrace/contrib/internal/pytest/_plugin_v2.py
+++ ddtrace/contrib/internal/pytest/_plugin_v2.py
@@ -15,8 +15,6 @@ from ddtrace.contrib.internal.coverage.patch import run_coverage_report
 from ddtrace.contrib.internal.coverage.utils import _is_coverage_invoked_by_coverage_run
 from ddtrace.contrib.internal.coverage.utils import _is_coverage_patched
 from ddtrace.contrib.internal.pytest._benchmark_utils import _set_benchmark_data_from_item
-from ddtrace.contrib.internal.pytest._plugin_v1 import _extract_reason
-from ddtrace.contrib.internal.pytest._plugin_v1 import _is_pytest_cov_enabled
 from ddtrace.contrib.internal.pytest._types import _pytest_report_teststatus_return_type
 from ddtrace.contrib.internal.pytest._types import pytest_CallInfo
 from ddtrace.contrib.internal.pytest._types import pytest_Config
@@ -742,3 +740,20 @@ def pytest_ddtrace_get_item_test_name(item):
     """Extract name from item, prepending class if desired"""
     names = _get_names_from_item(item)
     return names.test
+
+
+def _extract_reason(call):
+    if call.excinfo is not None:
+        return call.excinfo.value
+
+
+def _is_pytest_cov_enabled(config) -> bool:
+    if not config.pluginmanager.get_plugin("pytest_cov"):
+        return False
+    cov_option = config.getoption("--cov", default=False)
+    nocov_option = config.getoption("--no-cov", default=False)
+    if nocov_option is True:
+        return False
+    if isinstance(cov_option, list) and cov_option == [True] and not nocov_option:
+        return True
+    return cov_option
