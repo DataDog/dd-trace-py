97508bfae8548d93f463778c562301c4eb99d72e
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-04-07 18:56:16 -0400

    remove debug print

--- ddtrace/internal/datadog/profiling/dd_wrapper/src/code_provenance.cpp
+++ ddtrace/internal/datadog/profiling/dd_wrapper/src/code_provenance.cpp
@@ -1,9 +1,8 @@
 #include "code_provenance.hpp"
 
-#include <iostream>
 #include <string>
 #include <string_view>
-#include <unistd.h>
+
 namespace Datadog {
 
 std::string_view
@@ -15,9 +14,7 @@ Datadog::CodeProvenance::get_json_str()
 void
 Datadog::CodeProvenance::set_json_str(std::string_view _json_str)
 {
-    std::cout << "set_json_str on " << getpid() << std::endl;
     this->json_str = _json_str;
-    std::cout << this->json_str << std::endl;
 }
 
 }
