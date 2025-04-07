295e4c9f182678de8772b3fbc5f81df5c267ab2e
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-04-07 17:05:25 -0400

    remove circular import

--- ddtrace/profiling/code_provenance.py
+++ ddtrace/profiling/code_provenance.py
@@ -4,7 +4,6 @@ import sysconfig
 from typing import List
 
 from ddtrace.internal.packages import get_distributions
-from ddtrace.settings.profiling import config
 
 
 class Library:
@@ -27,9 +26,6 @@ class Library:
 
 class CodeProvenance:
     def __init__(self):
-        if not self.enabled():
-            return
-
         self.libraries = []
         self.libraries.append(
             Library(
@@ -43,13 +39,7 @@ class CodeProvenance:
         for dist in get_distributions():
             self.libraries.append(Library(kind="library", name=dist.name, version=dist.version, paths=[dist.paths]))
 
-    def enabled(self):
-        return config.code_provenance
-
     def to_dict(self):
-        if not self.enabled():
-            return {}
-
         return {"v1": [lib.to_dict() for lib in self.libraries]}
 
 
