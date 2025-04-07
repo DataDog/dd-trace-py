f953be1c98de916d1657b1eee159ebd2b28d4036
Yun Kim <yun.kim@datadoghq.com>
2025-04-07 17:55:28 -0400

    rm unused code snippet in docs

--- ddtrace/contrib/_crewai.py
+++ ddtrace/contrib/_crewai.py
@@ -37,7 +37,6 @@ To configure the CrewAI integration on a per-instance basis use the
 ``Pin`` API::
 
     import crewai
-    from ddtrace import config
     from ddtrace.trace import Pin
 
     Pin.override(crewai, service="my-crewai-service")
