8d5d24b77a8fe0cd90458e1566cb5667b782359a
Nicole Cybul <nicole.cybul@datadoghq.com>
2025-04-08 10:17:12 -0400

    consume streams for apm tests

--- tests/contrib/litellm/test_litellm.py
+++ tests/contrib/litellm/test_litellm.py
@@ -33,12 +33,16 @@ def test_litellm_completion(litellm, snapshot_context, request_vcr, stream, n):
     with snapshot_context(token="tests.contrib.litellm.test_litellm.test_litellm_completion"):
         with request_vcr.use_cassette(get_cassette_name(stream, n)):
             messages = [{"content": "Hey, what is up?", "role": "user"}]
-            litellm.completion(
+            resp = litellm.completion(
                 model="gpt-3.5-turbo",
                 messages=messages,
                 stream=stream,
                 n=n,
             )
+            if stream:
+                for _ in resp:
+                    pass
+
 
 
 @pytest.mark.parametrize("stream,n", [(True, 1), (True, 2), (False, 1), (False, 2)])
@@ -46,36 +50,44 @@ async def test_litellm_acompletion(litellm, snapshot_context, request_vcr, strea
     with snapshot_context(token="tests.contrib.litellm.test_litellm.test_litellm_completion", ignores=["resource"]):
         with request_vcr.use_cassette(get_cassette_name(stream, n)):
             messages = [{"content": "Hey, what is up?", "role": "user"}]
-            await litellm.acompletion(
+            resp = await litellm.acompletion(
                 model="gpt-3.5-turbo",
                 messages=messages,
                 stream=stream,
                 n=n,
             )
-
-
+            if stream:
+                async for _ in resp:
+                    pass
+            
 @pytest.mark.parametrize("stream,n", [(True, 1), (True, 2), (False, 1), (False, 2)])
 def test_litellm_text_completion(litellm, snapshot_context, request_vcr, stream, n):
     with snapshot_context(token="tests.contrib.litellm.test_litellm.test_litellm_completion", ignores=["resource"]):
         with request_vcr.use_cassette(get_cassette_name(stream, n)):
-            litellm.text_completion(
+            resp = litellm.text_completion(
                 model="gpt-3.5-turbo",
                 prompt="Hello world",
                 stream=stream,
                 n=n,
             )
+            if stream:
+                for _ in resp:
+                    pass
 
 
 @pytest.mark.parametrize("stream,n", [(True, 1), (True, 2), (False, 1), (False, 2)])
 async def test_litellm_atext_completion(litellm, snapshot_context, request_vcr, stream, n):
     with snapshot_context(token="tests.contrib.litellm.test_litellm.test_litellm_completion", ignores=["resource"]):
         with request_vcr.use_cassette(get_cassette_name(stream, n)):
-            await litellm.atext_completion(
+            resp = await litellm.atext_completion(
                 model="gpt-3.5-turbo",
                 prompt="Hello world",
                 stream=stream,
                 n=n,
             )
+            if stream:
+                async for _ in resp:
+                    pass
 
 
 @pytest.mark.parametrize("model", ["command-r", "anthropic/claude-3-5-sonnet-20240620"])
