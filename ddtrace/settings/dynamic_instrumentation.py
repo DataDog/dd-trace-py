e83568b5e8234f58a43c31da79d10f2a6884fa63
Alexander S. <alexander.seleznyov@datadoghq.com>
2025-04-08 15:24:48 +0300

    default float
    Co-authored-by: Gabriele N. Tornetta <P403n1x87@users.noreply.github.com>

--- ddtrace/settings/dynamic_instrumentation.py
+++ ddtrace/settings/dynamic_instrumentation.py
@@ -97,7 +97,7 @@ class DynamicInstrumentationConfig(DDConfig):
     diagnostics_interval = DDConfig.v(
         float,
         "diagnostics.interval",
-        default=3600,  # 1 hour
+        default=3600.0,  # 1 hour
         help_type="Integer",
         help="Interval in seconds for periodically emitting probe diagnostic messages",
     )
