012ca1bae363170245e896de6841bbf1d29194b1
Alberto Vara <alberto.vara@datadoghq.com>
2025-04-09 10:18:54 +0200

    chore(iast): fix iast span metrics [backport 2.21] (#13087)
    backport https://github.com/DataDog/dd-trace-py/pull/13075 to 2.21
    
    Partial backport of #13046 to align the functions parameters.
    
    
    (cherry picked from commit 9c8c6d0eb66c67ae261329c0f098dd32b6e67b21)
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

--- tests/appsec/iast/test_taint_utils.py
+++ tests/appsec/iast/test_taint_utils.py
@@ -198,19 +198,18 @@ def test_checked_tainted_args(iast_context_defaults):
 
     # Returns False: Untainted first argument
     assert not check_tainted_dbapi_args(
-        args=(untainted_arg,), kwargs=None, tracer=None, integration_name="sqlite", method=cursor.execute
+        args=(untainted_arg,), kwargs=None, integration_name="sqlite", method=cursor.execute
     )
 
     # Returns False: Untainted first argument
     assert not check_tainted_dbapi_args(
-        args=(untainted_arg, tainted_arg), kwargs=None, tracer=None, integration_name="sqlite", method=cursor.execute
+        args=(untainted_arg, tainted_arg), kwargs=None, integration_name="sqlite", method=cursor.execute
     )
 
     # Returns False: Integration name not in list
     assert not check_tainted_dbapi_args(
         args=(tainted_arg,),
         kwargs=None,
-        tracer=None,
         integration_name="nosqlite",
         method=cursor.execute,
     )
@@ -219,24 +218,23 @@ def test_checked_tainted_args(iast_context_defaults):
     assert not check_tainted_dbapi_args(
         args=(tainted_arg,),
         kwargs=None,
-        tracer=None,
         integration_name="sqlite",
         method=cursor.executemany,
     )
 
     # Returns True:
     assert check_tainted_dbapi_args(
-        args=(tainted_arg, untainted_arg), kwargs=None, tracer=None, integration_name="sqlite", method=cursor.execute
+        args=(tainted_arg, untainted_arg), kwargs=None, integration_name="sqlite", method=cursor.execute
     )
 
     # Returns True:
     assert check_tainted_dbapi_args(
-        args=(tainted_arg, untainted_arg), kwargs=None, tracer=None, integration_name="mysql", method=cursor.execute
+        args=(tainted_arg, untainted_arg), kwargs=None, integration_name="mysql", method=cursor.execute
     )
 
     # Returns True:
     assert check_tainted_dbapi_args(
-        args=(tainted_arg, untainted_arg), kwargs=None, tracer=None, integration_name="psycopg", method=cursor.execute
+        args=(tainted_arg, untainted_arg), kwargs=None, integration_name="psycopg", method=cursor.execute
     )
 
 
