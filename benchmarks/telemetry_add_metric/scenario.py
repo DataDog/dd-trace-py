1c5324f6e62c879bc44c450435cfc786fa2f8ac4
wan <wan.tsui@datadoghq.com>
2025-04-11 17:21:46 -0400

    Use latest benchmarks.

--- benchmarks/telemetry_add_metric/scenario.py
+++ benchmarks/telemetry_add_metric/scenario.py
@@ -4,13 +4,16 @@ from ddtrace.internal.telemetry.metrics_namespaces import MetricNamespace
 from ddtrace.internal.telemetry.constants import TELEMETRY_NAMESPACE
 from ddtrace.internal.telemetry.metrics import CountMetric
 
+
 class TelemetryAddMetric(Scenario):
     """
     This scenario checks to see if there's an impact on sending metrics via instrumentation telemetry
     """
+
     runs: int
     flush: bool
     batches: int
+
     def run(self):
         def _(loops):
             for _ in range(loops):
@@ -18,8 +21,13 @@ class TelemetryAddMetric(Scenario):
                 metricnamespace = MetricNamespace()
                 for i in range(self.runs):
                     for j in range(self.batches):
-                        metricnamespace.add_metric(CountMetric, TELEMETRY_NAMESPACE.TRACERS, str(j), 10, tags=(("integration_name","somevalue"),))
-
+                        metricnamespace.add_metric(
+                            CountMetric,
+                            TELEMETRY_NAMESPACE.TRACERS,
+                            str(j),
+                            10,
+                            tags=(("integration_name", "somevalue"),),
+                        )
 
                 # Make flush optional
                 if self.flush:
