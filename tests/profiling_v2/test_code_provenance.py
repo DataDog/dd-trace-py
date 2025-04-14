06d6cec2f37fddae7dce799f6ddf515c5a883378
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-04-14 09:46:57 -0400

    update test

--- tests/profiling_v2/test_code_provenance.py
+++ tests/profiling_v2/test_code_provenance.py
@@ -1,13 +1,56 @@
 import json
+import sys
+import sysconfig
+
+import jsonschema
+import pytest
 
 from ddtrace.internal.datadog.profiling.code_provenance import json_str_to_export
+
+# Copied from RFC
+SCHEMA = {
+    "$schema": "http://json-schema.org/draft-07/schema",
+    "type": "object",
+    "required": ["v1"],
+    "properties": {
+        "v1": {
+            "type": "array",
+            "additionalItems": True,
+            "items": {
+                "anyOf": [
+                    {
+                        "type": "object",
+                        "required": ["kind", "name", "version", "paths"],
+                        "properties": {
+                            "kind": {"type": "string"},
+                            "name": {"type": "string"},
+                            "version": {"type": "string"},
+                            "paths": {
+                                "type": "array",
+                                "additionalItems": True,
+                                "items": {"anyOf": [{"type": "string"}]},
+                            },
+                        },
+                        "additionalProperties": True,
+                    }
+                ]
+            },
+        }
+    },
+    "additionalProperties": True,
+}
 
 
 def is_valid_json(s: str) -> bool:
     try:
-        json.loads(s)
+        json_obj = json.loads(s)
+        jsonschema.validate(json_obj, SCHEMA)
         return True
     except json.JSONDecodeError:
+        print(f"Invalid JSON: {s}")
+        return False
+    except jsonschema.exceptions.ValidationError as e:
+        print(f"Validation error: {e.message}")
         return False
 
 
@@ -16,3 +59,48 @@ class TestCodeProvenance:
         # End to end test to ensure that the output is valid JSON
         json_str = json_str_to_export()
         assert is_valid_json(json_str)
+
+    def test_valid_json_but_invalid_schema(self):
+        # Just a sanity check to ensure that jsonschema is working as expected
+        json_obj = {
+            "v1": [
+                {
+                    "kind": "test",
+                    "name": "test",
+                    "version": 1,
+                    "paths": ["test"],
+                }
+            ]
+        }
+        assert not is_valid_json(json.dumps(json_obj))
+
+    @pytest.mark.skipif(sys.platform == "win32", reason="Unix only")
+    def test_lib_paths_are_absolute(self):
+        json_str = json_str_to_export()
+        json_obj = json.loads(json_str)
+
+        site_packages_path = sysconfig.get_path("purelib")
+        for item in json_obj["v1"]:
+            if item["name"] == "stdlib":
+                # See below test_stdlib_paths
+                continue
+            for path in item["paths"]:
+                assert path.startswith("/") and path.startswith(site_packages_path)
+
+    @pytest.mark.skipif(sys.version_info < (3, 10), reason="Python 3.10+ only")
+    def test_stdlib_paths(self):
+        json_str = json_str_to_export()
+        json_obj = json.loads(json_str)
+
+        # Check that the obj has stdlib
+        stdlib = [item for item in json_obj["v1"] if item["name"] == "stdlib"]
+        assert len(stdlib) == 1
+        stdlib_item = stdlib[0]
+        stdlib_paths = stdlib_item["paths"]
+
+        for path in stdlib_paths:
+            # Note: our test Python environments don't seem to have frozen modules
+            # in the stdlib
+            if path.startswith("<frozen "):
+                continue
+            assert path == sysconfig.get_path("stdlib")
