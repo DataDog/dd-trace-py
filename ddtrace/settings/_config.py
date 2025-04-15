4e2687b00da15b96c46b024f27449e376deee71a
Rachel Yang <rachel.yang@datadoghq.com>
2025-04-15 15:14:55 -0400

    adding config

--- ddtrace/settings/_config.py
+++ ddtrace/settings/_config.py
@@ -650,6 +650,7 @@ class Config(object):
         self._lib_was_injected = False
         self._inject_was_attempted = _get_config("_DD_INJECT_WAS_ATTEMPTED", False, asbool)
         self._inferred_proxy_services_enabled = _get_config("DD_TRACE_INFERRED_PROXY_SERVICES_ENABLED", False, asbool)
+        self._baggage_tag_keys = _get_config("DD_TRACE_BAGGAGE_TAG_KEYS", "usr.id,account.id,session.id")
 
     def __getattr__(self, name) -> Any:
         if name in self._config:
