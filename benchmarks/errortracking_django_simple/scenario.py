3d0cc7d8fe66449af182fc4dfd55eaa0be4cb701
Louis Tricot <louis.tricot@datadoghq.com>
2025-04-16 11:58:08 +0200

    chore: add a django benchmark

---
+++ benchmarks/errortracking_django_simple/scenario.py
@@ -0,0 +1,33 @@
+import os
+
+import bm
+
+
+class DjangoSimple(bm.Scenario):
+    tracer_enabled: bool
+    errortracking_enabled: str
+    path: str
+
+    def run(self):
+        os.environ["DJANGO_SETTINGS_MODULE"] = "app"
+
+        if self.errortracking_enabled:
+            os.environ.update({"DD_ERROR_TRACKING_HANDLED_ERRORS_ENABLED": self.errortracking_enabled})
+
+
+        # This will not work with gevent workers as the gevent hub has not been
+        # initialized when this hook is called.
+        if self.tracer_enabled:
+            import ddtrace.auto  # noqa:F401
+
+        import django
+        from django.test import Client
+
+        django.setup()
+        client = Client()
+
+        def _(loops):
+            for _ in range(loops):
+                client.get(self.path)
+
+        yield _
