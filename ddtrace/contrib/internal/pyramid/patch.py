474fd1b5b52d837ee41fadc385c846365cd97a2f
Jessica Gamio <jessica.gamio@datadoghq.com>
2025-04-21 10:20:33 -0700

    fixed import order with hatch

--- ddtrace/contrib/internal/pyramid/patch.py
+++ ddtrace/contrib/internal/pyramid/patch.py
@@ -4,8 +4,8 @@ import wrapt
 
 from ddtrace import config
 from ddtrace.internal.utils.formats import asbool
-
 from ddtrace.settings._config import _get_config
+
 from .constants import SETTINGS_ANALYTICS_ENABLED
 from .constants import SETTINGS_ANALYTICS_SAMPLE_RATE
 from .constants import SETTINGS_DISTRIBUTED_TRACING
