f74d6cdeb2c42c57652113f515984ef1d50ca439
Alberto Vara <alberto.vara@datadoghq.com>
2025-04-25 11:16:13 +0200

    chore(iast): add more check more cmdi tests

--- ddtrace/appsec/_iast/_ast/iastpatch.c
+++ ddtrace/appsec/_iast/_ast/iastpatch.c
@@ -17,9 +17,10 @@ static char** cached_packages = NULL;
 static size_t cached_packages_count = 0;
 
 /* Static Lists */
-static const char* static_allowlist[] = { "jinja2.",   "pygments.",   "multipart.", "sqlalchemy.", "python_multipart.",
-                                          "attrs.",    "jsonschema.", "s3fs.",      "mysql.",      "pymysql.",
-                                          "markupsafe" };
+static const char* static_allowlist[] = {
+    "jinja2.",     "pygments.", "multipart.", "sqlalchemy.", "python_multipart.", "attrs.",
+    "jsonschema.", "s3fs.",     "mysql.",     "pymysql.",    "markupsafe.",       "werkzeug.utils."
+};
 static const size_t static_allowlist_count = sizeof(static_allowlist) / sizeof(static_allowlist[0]);
 
 static const char* static_denylist[] = { "django.apps.config.",
