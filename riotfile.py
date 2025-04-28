2ca981c6a455c1832b5ab80784e932e4e42870a5
Nicole Cybul <nicole.cybul@datadoghq.com>
2025-04-28 14:51:44 -0400

    instrument base process llm request

--- riotfile.py
+++ riotfile.py
@@ -2572,6 +2572,13 @@ venv = Venv(
                 "pytest-asyncio": latest,
                 "botocore": latest,
                 "boto3": latest,
+                "fastapi": latest,
+                "backoff": latest,
+                "orjson": latest,
+                "apscheduler": latest,
+                "cryptography": latest,
+                "python-multipart": latest,
+                "email-validator": latest,
             },
         ),
         Venv(
