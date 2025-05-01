99006fad40caf2c7cf5c3cd2458c94f3f11cb5b4
Munir Abdinur <munir_abdinur@hotmail.com>
2025-05-01 11:46:46 -0400

    make default none

--- ddtrace/settings/_config.py
+++ ddtrace/settings/_config.py
@@ -647,7 +647,7 @@ class Config(object):
 
         self._inject_force = _get_config("DD_INJECT_FORCE", False, asbool)
         self._lib_was_injected = False
-        self._inject_enabled = _get_config("DD_INJECTION_ENABLED", "none")
+        self._inject_enabled = _get_config("DD_INJECTION_ENABLED")
         self._inferred_proxy_services_enabled = _get_config("DD_TRACE_INFERRED_PROXY_SERVICES_ENABLED", False, asbool)
 
     def __getattr__(self, name) -> Any:
