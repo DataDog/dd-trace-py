69d9346966e6ba187eaa4bcd5f4261c74beb3b89
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-05-02 14:37:55 -0400

    fix tests

--- tests/telemetry/test_writer.py
+++ tests/telemetry/test_writer.py
@@ -437,6 +437,7 @@ import ddtrace.settings.exception_replay
         {"name": "DD_PROFILING_SAMPLE_POOL_CAPACITY", "origin": "default", "value": 4},
         {"name": "DD_PROFILING_STACK_ENABLED", "origin": "env_var", "value": False},
         {"name": "DD_PROFILING_STACK_V2_ENABLED", "origin": "default", "value": True},
+        {"name": "DD_PROFILING_STACK_V2_MAX_FRAMES", "origin": "default", "value": 256},
         {"name": "DD_PROFILING_TAGS", "origin": "default", "value": ""},
         {"name": "DD_PROFILING_TIMELINE_ENABLED", "origin": "default", "value": False},
         {"name": "DD_PROFILING_UPLOAD_INTERVAL", "origin": "env_var", "value": 10.0},
