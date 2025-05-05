6ebfca49d582d8f85e1dc092ebb60514298c57a7
Xinyuan Guo <xinyuan.guo@datadoghq.com>
2025-05-05 15:35:19 -0400

    clean up

--- ddtrace/contrib/internal/openai/utils.py
+++ ddtrace/contrib/internal/openai/utils.py
@@ -176,8 +176,6 @@ class TracedOpenAIResponseStream(BaseTracedOpenAIStream):
             for chunk in self.__wrapped__:
                 if chunk.type == "response.completed":
                     handle_response_tools(chunk.response, self._dd_span)
-                    print("Sync chunk response is", chunk.response)
-                    # Set response attributes
                     if hasattr(chunk.response, "id"):
                         self._dd_span.set_tag("openai.response.id", chunk.response.id)
                     if hasattr(chunk.response, "created_at"):
@@ -236,7 +234,6 @@ class TracedOpenAIAsyncResponseStream(BaseTracedOpenAIStream):
                     if hasattr(chunk.response, "model"):
                         self._dd_span.set_tag("openai.response.model", chunk.response.model)
                     handle_response_tools(chunk.response, self._dd_span)
-                    print("Async chunk response is", chunk.response)
                     self._dd_integration.record_usage(self._dd_span, chunk.response.usage)
                     self._dd_span.finish()
                 yield chunk
@@ -291,7 +288,6 @@ def handle_response_tools(resp, span):
 
         if tool_dict:
             response_tools.append(tool_dict)
-        print("response tools are", response_tools)
     if response_tools != []:
         span.set_tag("openai.response.tools", response_tools)
 
