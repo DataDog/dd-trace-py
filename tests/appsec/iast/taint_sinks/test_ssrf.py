ef0d3151d231426517b52a997e2607b1d41f9436
Alberto Vara <alberto.vara@datadoghq.com>
2025-05-08 13:25:41 +0200

    chore(iast): stackId should report inside vulnerability location (#13358)
    stackId should report inside vulnerability location, following this
    system test PR:
    https://github.com/DataDog/system-tests/pull/4607
    
    Tests confirmed locally:
    
    ![image](https://github.com/user-attachments/assets/c7c22cb0-4a89-4366-999a-d157df95653d)
    
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

--- tests/appsec/iast/taint_sinks/test_ssrf.py
+++ tests/appsec/iast/taint_sinks/test_ssrf.py
@@ -55,6 +55,7 @@ def _check_report(tainted_path, label):
     assert vulnerability["location"]["line"] == line
     assert vulnerability["location"]["method"] == label
     assert "class" not in vulnerability["location"]
+    assert type(vulnerability["location"]["spanId"]) is int
     assert vulnerability["hash"] == hash_value
 
 
