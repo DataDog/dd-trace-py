bd39290ebaf4d3a130f68171d19ca96c7f1467c3
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-05-13 14:32:58 -0400

    turn off profiling for 32bit linux

--- ddtrace/profiling/bootstrap/sitecustomize.py
+++ ddtrace/profiling/bootstrap/sitecustomize.py
@@ -1,5 +1,9 @@
 # -*- encoding: utf-8 -*-
 """Bootstrapping code that is run when using `ddtrace.profiling.auto`."""
+
+import platform
+import sys
+
 from ddtrace.profiling import bootstrap
 from ddtrace.profiling import profiler
 
@@ -12,4 +16,13 @@ def start_profiler():
     bootstrap.profiler.start()
 
 
-start_profiler()
+# Check if it's linux with 32bit python
+if platform.system() == "Linux" and not (sys.maxsize > (1 << 32)):
+    print(
+        "The Datadog Profiler is not supported on 32-bit Linux systems. "
+        "To use the profiler, please upgrade to a 64-bit Linux system. "
+        "If you believe this is an error or need assistance, please report it at "
+        "https://github.com/DataDog/dd-trace-py/issues"
+    )
+else:
+    start_profiler()
