224593b34723ab88f9f5ebb0c3d7810c61cc00ff
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2025-05-13 15:19:09 +0200

    fix
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- riotfile.py
+++ riotfile.py
@@ -1103,7 +1103,7 @@ venv = Venv(
                 "PYDONTWRITEBYTECODE": "1",
                 "PYTHONUNBUFFERED": "1",
             },
-            command="bash tests/appsec/integrations/pygoat_tests/run_pygoat.sh",
+            command="bash tests/appsec/integrations/pygoat_tests/run_pygoat.sh tests/appsec/integrations/pygoat_tests/",
         ),
         Venv(
             name="pynamodb",
