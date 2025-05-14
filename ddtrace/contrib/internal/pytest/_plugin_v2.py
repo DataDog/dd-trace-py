aa08f361f05124e6160ecbdbde7b9cbb203050a6
Vítor De Araújo <vitor.dearaujo@datadoghq.com>
2025-05-14 14:24:56 +0000

    bump the version

--- ddtrace/contrib/internal/pytest/_plugin_v2.py
+++ ddtrace/contrib/internal/pytest/_plugin_v2.py
@@ -280,7 +280,7 @@ def pytest_sessionstart(session: pytest.Session) -> None:
             test_impact_analysis="1" if _pytest_version_supports_itr() else None,
             test_management_quarantine="1",
             test_management_disable="1",
-            test_management_attempt_to_fix="2" if _pytest_version_supports_attempt_to_fix() else None,
+            test_management_attempt_to_fix="4" if _pytest_version_supports_attempt_to_fix() else None,
         )
 
         InternalTestSession.discover(
