e9cb745f34ed747f8d49824172c190790f5bfecc
Sam Brenner <sam.brenner@datadoghq.com>
2025-05-15 09:22:33 -0400

    cleanup

--- ddtrace/llmobs/_integrations/langgraph.py
+++ ddtrace/llmobs/_integrations/langgraph.py
@@ -2,6 +2,8 @@ from typing import Any
 from typing import Dict
 from typing import List
 from typing import Optional
+from typing import Tuple
+from typing import Union
 
 from ddtrace.internal.utils import get_argument_value
 from ddtrace.internal.utils.formats import format_trace_id
@@ -85,14 +87,16 @@ class LangGraphIntegration(BaseLLMIntegration):
             finished_task_name_ids = finished_task_names_to_ids.setdefault(task.name, [])
             finished_task_name_ids.append(task_id)
 
-        seen_pregel_tasks: set[int] = set()  # set of pregel send object ids
+        seen_pregel_tasks_writes: set[int] = set()  # set of pregel send object ids
         for task_id, task in next_tasks.items():
             queued_node = self._graph_nodes_by_task_id.setdefault(task_id, {})
             queued_node["name"] = getattr(task, "name", "")
 
-            self._link_task_to_parent(task_id, task, finished_tasks, finished_task_names_to_ids, seen_pregel_tasks)
+            self._link_task_to_parent(
+                task_id, task, finished_tasks, finished_task_names_to_ids, seen_pregel_tasks_writes
+            )
 
-    def _handle_finished_graph(self, graph_span: Span, finished_tasks, is_subgraph_node):
+    def _handle_finished_graph(self, graph_span: Span, finished_tasks: dict[str, Any], is_subgraph_node: bool):
         """Create the span links for a finished pregel graph from all finished tasks as the graph span's outputs.
         Generate the output-to-output span links for the last nodes in a pregel graph.
         If the graph isn't a subgraph, add a span link from the graph span to the calling LLMObs parent span.
@@ -118,10 +122,17 @@ class LangGraphIntegration(BaseLLMIntegration):
             graph_caller_span._set_ctx_item(SPAN_LINKS, graph_caller_span_links + span_links)
         return
 
-    def _link_task_to_parent(self, task_id, task, finished_tasks, finished_task_names_to_ids, seen_pregel_tasks):
+    def _link_task_to_parent(
+        self,
+        task_id: str,
+        task,
+        finished_tasks: dict[str, Any],
+        finished_task_names_to_ids: dict[str, list[str]],
+        seen_pregel_tasks_writes: set[int],
+    ):
         """Create the span links for a queued task from its triggering parent tasks."""
         trigger_node_ids = _get_task_trigger_ids_from_finished_tasks(
-            task, finished_tasks, finished_task_names_to_ids, seen_pregel_tasks
+            task, finished_tasks, finished_task_names_to_ids, seen_pregel_tasks_writes
         )
 
         for node_id in trigger_node_ids:
@@ -145,27 +156,39 @@ class LangGraphIntegration(BaseLLMIntegration):
 
 
 def _get_task_trigger_ids_from_finished_tasks(
-    task, finished_tasks: dict[str, Any], finished_task_names_to_ids: dict[str, list[str]], seen_pregel_tasks: set[str]
+    task,
+    finished_tasks: dict[str, Any],
+    finished_task_names_to_ids: dict[str, list[str]],
+    seen_pregel_tasks_writes: set[int],
 ):
+    """
+    Get the set of task ids that are responsible for triggering the queued task.
+
+    This logic handles legacy formatting of triggers (pre langgraph 0.3.22), and beyond.
+    Older versions of langgraph would include a list of either task names or task relationships in their task config
+    (ie, task_triggers=["a", "b", ...], task_triggers=["a:b", "c:d", ...], etc.) that provide a possible mapping into
+    the `finished_task_names_to_ids` dictionary.
+
+    Newer versions of langgraph triggers look more like the internal private channel names, ie
+    for task `a`, its one and only trigger might be `("branch:to:a",)`.
+    In this case, we need to look for all the finished tasks whose writes include that channel name.
+
+    Since only one node (aside from PREGEL_PUSH via Send) can be triggered per tick, it is safe to assume all finished
+    nodes that have a given branch trigger in their writes are responsible for triggering the queued task.
+
+    In the case of a pregel push, we assume that all pushed tasks are queued in order of the nodes added in the finished_tasks
+    dictionary. We look for the first PREGEL_TASK write in the finished_tasks that has not already been marked
+    as seen in the `seen_pregel_tasks` set (representing that a node queued via send has already been recorded) with
+    span linkage.
+    """
     task_config = getattr(task, "config", {})
     task_triggers = task_config.get("metadata", {}).get("langgraph_triggers", [])
 
-    # breakpoint()
-
-    # legacy handling for langgraph task triggers
-    # will be of the form: ["a", "b", etc.]
-    # early langgraph versions do not have concept of
-    # pregel sends/pushes
+    # attempt to handle legacy node names
     trigger_ids = []
     for trigger in task_triggers:
         if isinstance(trigger, str) and (task_ids := finished_task_names_to_ids.get(_extract_parent(trigger))):
             trigger_ids.extend(task_ids)
-    # trigger_ids = [
-    #     task_id
-    #     for trigger in task_triggers
-    #     if isinstance(trigger, str)
-    #     if (task_id := finished_task_names_to_ids.get(_extract_parent(trigger))) is not None
-    # ]
 
     if trigger_ids:
         return trigger_ids
@@ -176,7 +199,7 @@ def _get_task_trigger_ids_from_finished_tasks(
         return []
     elif trigger_branch == PREGEL_PUSH:
         for finished_task_id, finished_task in finished_tasks.items():
-            if _has_pregel_push_for_task(task, finished_task, seen_pregel_tasks):
+            if _has_pregel_push_for_task(task, finished_task, seen_pregel_tasks_writes):
                 return [finished_task_id]
         return []
     else:
@@ -187,7 +210,16 @@ def _get_task_trigger_ids_from_finished_tasks(
         ]
 
 
-def _find_trigger(triggers: tuple[str]):
+def _find_trigger(triggers: Union[Tuple[str, ...], List[str]]) -> Optional[str]:
+    """
+    Finds the trigger branch from a list or tuple of triggers.
+
+    This function is used when there are no conventional triggers, and the only trigger used are
+    branch triggers for channel writes and reads, or pregel pushes.
+
+    If a pregel push is found without finding a branch trigger, we return that it is a
+    pregel push, or otherwise no trigger is found.
+    """
     has_pregel_push = False
     for trigger in triggers:
         if trigger.startswith("branch:to"):
@@ -198,18 +230,31 @@ def _find_trigger(triggers: tuple[str]):
     return None
 
 
-def _has_branch_to(task, branch_to):
-    for write in task.writes:
+def _has_branch_to(finished_task, branch_to: str) -> bool:
+    """
+    Checks if the task writes to the given branch_to.
+    """
+    task_writes: list[tuple[str, ...]] = getattr(finished_task, "writes", [])
+    for write in task_writes:
         if branch_to in write:
             return True
     return False
 
 
-def _has_pregel_push_for_task(task, finished_task, seen_pregel_tasks: set):
+def _has_pregel_push_for_task(task, finished_task, seen_pregel_tasks_writes: set[int]) -> bool:
+    """
+    Checks if the task writes has an unmarked pregel push for a given task name.
+    A pregel push (marked by PREGEL_TASKS from langgraph for task writes) has an arg which is the
+    Send object (something like `Send(node="a", arg=...)`).
+
+    This assumes (referenced in `_get_task_trigger_ids_from_finished_tasks`) that all pregel tasks are queued in order
+    of the nodes added in the finished_tasks dictionary (so that the first task whose trigger is a PREGEL_PUSH
+    is from the first finished task that has a PREGEL_TASK write).
+    """
     for branch, arg in finished_task.writes:
-        if branch == PREGEL_TASKS and id(arg) not in seen_pregel_tasks:
-            seen_pregel_tasks.add(id(arg))
-            return arg.node == task.name  # TODO(sabrenner) check if this is correct
+        if branch == PREGEL_TASKS and id(arg) not in seen_pregel_tasks_writes:
+            seen_pregel_tasks_writes.add(id(arg))
+            return arg.node == task.name
     return False
 
 
@@ -228,7 +273,7 @@ def _extract_parent(trigger: str) -> str:
     return split[1]
 
 
-def _default_span_link(span: Span):
+def _default_span_link(span: Span) -> dict:
     """
     Create a default input-to-input span link for a given span, if there are no
     referenced spans that represent the causal link. In this case, we assume
@@ -241,7 +286,7 @@ def _default_span_link(span: Span):
     }
 
 
-def _is_subgraph(graph_span):
+def _is_subgraph(graph_span: Span) -> bool:
     """Helper to denote whether the LangGraph graph this span represents is a sub-graph or a standalone graph.
     Note that this only considers if this graph is nested in the execution of a larger graph,
     not whether this graph is represented as a single node in the larger graph
