1fcc7a0cb14ad41a7ee186fa860921b398aaa7de
William Conti <william.conti@datadoghq.com>
2025-05-21 15:42:48 +0200

    fix lint

--- scripts/freshvenvs.py
+++ scripts/freshvenvs.py
@@ -14,6 +14,7 @@ from typing import Optional
 from packaging.version import Version
 from pip import _internal
 
+
 # add project root to path to import riotfile
 sys.path.append(str(pathlib.Path(__file__).parent.parent.resolve()))
 
