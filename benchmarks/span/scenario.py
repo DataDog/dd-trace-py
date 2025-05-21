8e3455f745b22d50323e873d68f89baf8c7eefdf
Zach Montoya <zach.montoya@datadoghq.com>
2025-05-21 11:46:16 +0200

    Update span benchmark so that it's testing the same operations as tested by otel_span and otel_sdk_span

--- benchmarks/span/scenario.py
+++ benchmarks/span/scenario.py
@@ -2,6 +2,7 @@ from bm import Scenario
 import bm.utils as utils
 
 from ddtrace import config
+from ddtrace.constants import ERROR_MSG
 from ddtrace.trace import tracer
 
 
@@ -29,6 +30,14 @@ class Span(Scenario):
         finishspan = self.finishspan
         config._128_bit_trace_id_enabled = self.traceid128
         config._telemetry_enabled = config._telemetry_metrics_enabled = self.telemetry
+        add_event = self.add_event
+        add_link = self.add_link
+        get_context = self.get_context
+        is_recording = self.is_recording
+        record_exception = self.record_exception
+        set_status = self.set_status
+        update_name = self.update_name
+
         # Recreate span processors and configure global tracer to avoid sending traces to the agent
         utils.drop_traces(tracer)
         utils.drop_telemetry_events()
@@ -41,6 +50,23 @@ class Span(Scenario):
                         s.set_tags(tags)
                     if setmetrics:
                         s.set_metrics(metrics)
+                    if add_event:
+                        s._add_event("test.event", test_attributes)
+                    if add_link:
+                        s.set_link(trace_id=1, span_id=2)
+                    if get_context:
+                        result = s.context._is_remote
+                    if is_recording:
+                        result = not s.finished
+                    if record_exception:
+                        s.record_exception(test_exception)
+                    if set_status:
+                        # Perform the equivalent operation for
+                        # test_status = OtelStatus(OtelStatusCode.ERROR, "something went wrong")
+                        s.error = 1
+                        s.set_tag(ERROR_MSG, "something went wrong")
+                    if update_name:
+                        s.resource = "test.renamed." + str(i)
                     if finishspan:
                         s.finish()
 
