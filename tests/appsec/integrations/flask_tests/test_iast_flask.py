52b5100c1a727c1db90b14a9fabf9d7d09064825
Alberto Vara <alberto.vara@datadoghq.com>
2025-05-27 10:21:19 +0200

    feat(python): enabling iast unvalidated redirect

--- tests/appsec/integrations/flask_tests/test_iast_flask.py
+++ tests/appsec/integrations/flask_tests/test_iast_flask.py
@@ -1745,8 +1745,6 @@ Lorem Ipsum Foobar
             assert vulnerability["evidence"] == {
                 "valueParts": [{"source": 0, "value": "http://localhost:8080/malicious"}]
             }
-            assert vulnerability["location"].get("stackId") == "1", f"Wrong Vulnerability stackId {vulnerability}"
-            assert "class" not in vulnerability["location"]
 
     def test_flask_xss_concat(self):
         @self.app.route("/xss/concat/", methods=["GET"])
