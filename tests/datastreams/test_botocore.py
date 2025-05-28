134f7298b2c70bd62f45a89ec50c0fb90a4b2547
Michael Zhao <michael.zhao@datadoghq.com>
2025-05-28 13:51:49 -0400

    remove obvious  comments

--- tests/datastreams/test_botocore.py
+++ tests/datastreams/test_botocore.py
@@ -5,18 +5,15 @@ from ddtrace.internal.datastreams.botocore import get_datastreams_context
 
 
 class TestGetDatastreamsContext:
-    """Test the get_datastreams_context function with various message formats."""
 
     def test_sqs_to_lambda_format_with_datadog_context(self):
         """Test SQS -> Lambda format with _datadog messageAttributes."""
-        # Sample trace context data
         trace_context = {
             "x-datadog-trace-id": "123456789",
             "x-datadog-parent-id": "987654321",
             "x-datadog-sampling-priority": "1"
         }
 
-        # Lambda event record with SQS message format
         lambda_record = {
             "messageId": "059f36b4-87a3-44ab-83d2-661975830a7d",
             "receiptHandle": "AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...",
@@ -47,10 +44,8 @@ class TestGetDatastreamsContext:
             "awsRegion": "us-east-2"
         }
 
-        # Test the function
         result = get_datastreams_context(lambda_record)
 
-        # Assertions
         assert result is not None
         assert result == trace_context
         assert result["x-datadog-trace-id"] == "123456789"
