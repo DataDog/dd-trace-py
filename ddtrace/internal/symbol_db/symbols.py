DataDog/dd-trace-py

commit e6c8d4d646c860ec7b33ededf7bbaf8ff1bcdb17

chore(symdb): add shallow mode (#12957)
We add the shallow mode to Symbol DB to upload only top-level module
scopes when DI is not enabled.

## Checklist
- [ ] PR author has checked that all the criteria below are met
- The PR description includes an overview of the change
- The PR description articulates the motivation for the change
- The change includes tests OR the PR description describes a testing
strategy
- The PR description notes risks associated with the change, if any
- Newly-added code is easy to change
- The change follows the [library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
- The change includes or references documentation updates if necessary
- Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))

## Reviewer Checklist
- [ ] Reviewer has checked that all the criteria below are met 
- Title is accurate
- All changes are related to the pull request's stated goal
- Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes
- Testing strategy adequately addresses listed risks
- Newly-added code is easy to change
- Release note makes sense to a user of the library
- If necessary, author has acknowledged and discussed the performance
implications of this PR as reported in the benchmarks PR comment
- Backport labels are set in a manner that is consistent with the
[release branch maintenance
policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

ddtrace/internal/symbol_db/symbols.py
@@ -166,12 +166,12 @@ class Scope:
 
     @singledispatchmethod
     @classmethod
-    def _get_from(cls, _: t.Any, data: ScopeData) -> t.Optional["Scope"]:
+    def _get_from(cls, _: t.Any, data: ScopeData, recursive: bool = True) -> t.Optional["Scope"]:
         return None
 
     @_get_from.register
     @classmethod
-    def _(cls, module: ModuleType, data: ScopeData):
+    def _(cls, module: ModuleType, data: ScopeData, recursive: bool = True):
         if module in data.seen:
             return None
         data.seen.add(module)
@@ -183,27 +183,28 @@ class Scope:
         symbols = []
         scopes = []
 
-        for alias, child in object.__getattribute__(module, "__dict__").items():
-            if _isinstance(child, ModuleType):
-                # We don't want to traverse other modules.
-                continue
+        if recursive:
+            for alias, child in object.__getattribute__(module, "__dict__").items():
+                if _isinstance(child, ModuleType):
+                    # We don't want to traverse other modules.
+                    continue
 
-            try:
-                if _isinstance(child, FunctionType):
-                    child = undecorated(child, alias, module_origin)
-                scope = Scope._get_from(child, data)
-                if scope is not None:
-                    scopes.append(scope)
-                elif not callable(child):
-                    symbols.append(
-                        Symbol(
-                            symbol_type=SymbolType.STATIC_FIELD,
-                            name=alias,
-                            line=0,
+                try:
+                    if _isinstance(child, FunctionType):
+                        child = undecorated(child, alias, module_origin)
+                    scope = Scope._get_from(child, data)
+                    if scope is not None:
+                        scopes.append(scope)
+                    elif not callable(child):
+                        symbols.append(
+                            Symbol(
+                                symbol_type=SymbolType.STATIC_FIELD,
+                                name=alias,
+                                line=0,
+                            )
                         )
-                    )
-            except Exception:
-                log.debug("Cannot get child scope %r for module %s", child, module.__name__, exc_info=True)
+                except Exception:
+                    log.debug("Cannot get child scope %r for module %s", child, module.__name__, exc_info=True)
 
         source_git_hash = sha1()  # nosec B324
         source_git_hash.update(f"blob {module_origin.stat().st_size}\0".encode())
@@ -224,7 +225,7 @@ class Scope:
 
     @_get_from.register
     @classmethod
-    def _(cls, obj: type, data: ScopeData):
+    def _(cls, obj: type, data: ScopeData, recursive: bool = True):
         if obj in data.seen:
             return None
         data.seen.add(obj)
@@ -314,7 +315,7 @@ class Scope:
 
     @_get_from.register
     @classmethod
-    def _(cls, code: CodeType, data: ScopeData):
+    def _(cls, code: CodeType, data: ScopeData, recursive: bool = True):
         # DEV: A code object with a mutable probe is currently not hashable, so
         # we cannot put it directly into the set.
         code_id = f"code-{id(code)}"
@@ -347,7 +348,7 @@ class Scope:
 
     @_get_from.register
     @classmethod
-    def _(cls, f: FunctionType, data: ScopeData):
+    def _(cls, f: FunctionType, data: ScopeData, recursive: bool = True):
         if f in data.seen:
             return None
         data.seen.add(f)
@@ -395,7 +396,7 @@ class Scope:
 
     @_get_from.register
     @classmethod
-    def _(cls, method: classmethod, data: ScopeData):
+    def _(cls, method: classmethod, data: ScopeData, recursive: bool = True):
         scope = cls._get_from(method.__func__, data)
 
         if scope is not None:
@@ -405,7 +406,7 @@ class Scope:
 
     @_get_from.register
     @classmethod
-    def _(cls, method: staticmethod, data: ScopeData):
+    def _(cls, method: staticmethod, data: ScopeData, recursive: bool = True):
         scope = cls._get_from(method.__func__, data)
 
         if scope is not None:
@@ -415,7 +416,7 @@ class Scope:
 
     @_get_from.register
     @classmethod
-    def _(cls, pr: property, data: ScopeData):
+    def _(cls, pr: property, data: ScopeData, recursive: bool = True):
         if pr.fget in data.seen:
             return None
         data.seen.add(pr.fget)
@@ -447,7 +448,7 @@ class Scope:
     # TODO: support for singledispatch
 
     @classmethod
-    def from_module(cls, module: ModuleType) -> "Scope":
+    def from_module(cls, module: ModuleType, recursive: bool = True) -> "Scope":
         """Get the scope of a module.
 
         The module must have an origin.
@@ -456,7 +457,7 @@ class Scope:
         if module_origin is None:
             raise ValueError(f"Cannot get scope of module with no origin '{module.__name__}'")
 
-        return t.cast(Scope, cls._get_from(module, ScopeData(module_origin, set())))
+        return t.cast(Scope, cls._get_from(module, ScopeData(module_origin, set()), recursive))
 
 
 class ScopeContext:
@@ -545,8 +546,10 @@ def is_module_included(module: ModuleType) -> bool:
 
 
 class SymbolDatabaseUploader(BaseModuleWatchdog):
-    __scope_limit__ = 400
-    __file_number_limit__ = 10000
+    __scope_limit__: int = 400
+    __file_number_limit__: int = 10000
+
+    shallow: bool = True
 
     def __init__(self) -> None:
         super().__init__()
@@ -560,6 +563,8 @@ class SymbolDatabaseUploader(BaseModuleWatchdog):
     def _process_unseen_loaded_modules(self) -> None:
         # Look for all the modules that are already imported when this is
         # installed and upload the symbols that are marked for inclusion.
+        recursive = not self.shallow
+
         context = ScopeContext()
         for name, module in list(sys.modules.items()):
             if self._processed_files_count >= self.__file_number_limit__:
@@ -583,7 +588,7 @@ class SymbolDatabaseUploader(BaseModuleWatchdog):
                 continue
 
             try:
-                scope = Scope.from_module(module)
+                scope = Scope.from_module(module, recursive)
             except Exception:
                 log.debug("Cannot get symbol scope for module %s", module.__name__, exc_info=True)
                 continue
@@ -625,7 +630,7 @@ class SymbolDatabaseUploader(BaseModuleWatchdog):
             log.debug("[PID %d] SymDB: Excluding imported module %s from symbol database", os.getpid(), module.__name__)
             return
 
-        scope = Scope.from_module(module)
+        scope = Scope.from_module(module, recursive=not self.shallow)
         if scope is not None:
             self._upload_context(ScopeContext([scope]))
             self._processed_files_count += 1
@@ -660,3 +665,8 @@ class SymbolDatabaseUploader(BaseModuleWatchdog):
             log.exception(
                 "[PID %d] SymDB: Failed to upload symbols context with %d scopes", os.getpid(), len(context._scopes)
             )
+
+    @classmethod
+    def install(cls, shallow=True):
+        cls._shallow = shallow
+        return super().install()

ddtrace/debugging/_debugger.py
@@ -43,6 +43,7 @@ from ddtrace.debugging._signal.model import Signal
 from ddtrace.debugging._signal.model import SignalState
 from ddtrace.debugging._uploader import LogsIntakeUploaderV1
 from ddtrace.debugging._uploader import UploaderProduct
+from ddtrace.internal import core
 from ddtrace.internal.logger import get_logger
 from ddtrace.internal.metrics import Metrics
 from ddtrace.internal.module import ModuleHookType
@@ -287,6 +288,8 @@ class Debugger(Service):
         register_post_run_module_hook(cls._on_run_module)
 
         log.debug("%s enabled", cls.__name__)
+
+        core.dispatch("dynamic-instrumentation.enabled")
 
     @classmethod
     def disable(cls, join: bool = True) -> None:

ddtrace/internal/products.py
@@ -52,8 +52,9 @@ class ProductManager:
 
     def __init__(self) -> None:
         self._products: t.Optional[t.List[t.Tuple[str, Product]]] = None  # Topologically sorted products
-
         self._failed: t.Set[str] = set()
+
+    def _load_products(self) -> None:
         for product_plugin in entry_points(group="ddtrace.products"):
             name = product_plugin.name
             log.debug("Discovered product plugin '%s'", name)
@@ -192,6 +193,8 @@ class ProductManager:
         atexit.register(self.exit_products)
 
     def run_protocol(self) -> None:
+        self._load_products()
+
         # uWSGI support
         try:
             check_uwsgi(worker_callback=forksafe.ddtrace_after_in_child)

ddtrace/internal/symbol_db/__init__.py
@@ -1,3 +1,6 @@
+from functools import partial
+
+from ddtrace.internal import core
 from ddtrace.internal.remoteconfig.worker import remoteconfig_poller
 from ddtrace.internal.symbol_db.remoteconfig import SymbolDatabaseAdapter
 from ddtrace.settings.symbol_db import config as symdb_config
@@ -8,10 +11,25 @@ def bootstrap():
         # Force the upload of symbols, ignoring RCM instructions.
         from ddtrace.internal.symbol_db.symbols import SymbolDatabaseUploader
 
-        SymbolDatabaseUploader.install()
+        SymbolDatabaseUploader.install(shallow=False)
     else:
         # Start the RCM subscriber to determine if and when to upload symbols.
         remoteconfig_poller.register("LIVE_DEBUGGING_SYMBOL_DB", SymbolDatabaseAdapter())
+
+    @partial(core.on, "dynamic-instrumentation.enabled")
+    def _():
+        if not symdb_config.enabled:
+            return
+
+        from ddtrace.internal.symbol_db.symbols import SymbolDatabaseUploader
+
+        # If dynamic instrumentation is enabled, we need to restart Symbol DB
+        # to ensure that we recursively collect all symbols from modules.
+        # To avoid tracking the status of all loaded modules, we keep Symbol DB
+        # installed, even if DI is disabled.
+        if SymbolDatabaseUploader.is_installed() and SymbolDatabaseUploader.shallow:
+            SymbolDatabaseUploader.uninstall()
+            SymbolDatabaseUploader.install(shallow=False)
 
 
 def restart():

ddtrace/internal/symbol_db/remoteconfig.py
@@ -3,6 +3,7 @@ import typing as t
 
 from ddtrace.internal.forksafe import has_forked
 from ddtrace.internal.logger import get_logger
+from ddtrace.internal.products import manager as product_manager
 from ddtrace.internal.remoteconfig import Payload
 from ddtrace.internal.remoteconfig._connectors import PublisherSubscriberConnector
 from ddtrace.internal.remoteconfig._publishers import RemoteConfigPublisher
@@ -14,6 +15,16 @@ from ddtrace.internal.symbol_db.symbols import SymbolDatabaseUploader
 
 
 log = get_logger(__name__)
+
+
+def is_di_enabled() -> bool:
+    if (di_product := product_manager.__products__.get("dynamic-instrumentation")) is None:
+        return False
+
+    if (di_config := getattr(di_product, "config", None)) is None:
+        return False
+
+    return di_config.enabled
 
 
 def _rc_callback(data: t.List[Payload], test_tracer=None):
@@ -46,7 +57,7 @@ def _rc_callback(data: t.List[Payload], test_tracer=None):
             log.debug("[PID %d] SymDB: Symbol DB RCM enablement signal received", os.getpid())
             if not SymbolDatabaseUploader.is_installed():
                 try:
-                    SymbolDatabaseUploader.install()
+                    SymbolDatabaseUploader.install(shallow=not is_di_enabled())
                     log.debug("[PID %d] SymDB: Symbol DB uploader installed", os.getpid())
                 except Exception:
                     log.error("[PID %d] SymDB: Failed to install Symbol DB uploader", os.getpid(), exc_info=True)

commit 58bd69c5e0273349ebecc09c0d382a4e742ef2a5

fix(symdb): switch `_shallow` to `shallow` in install (#13522)
This fixes a bug introduced by
https://github.com/DataDog/dd-trace-py/pull/12957 where the shallow mode
for SymDB was introduced.

## Checklist
- [x] PR author has checked that all the criteria below are met
- The PR description includes an overview of the change
- The PR description articulates the motivation for the change
- The change includes tests OR the PR description describes a testing
strategy
- The PR description notes risks associated with the change, if any
- Newly-added code is easy to change
- The change follows the [library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
- The change includes or references documentation updates if necessary
- Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))

## Reviewer Checklist
- [x] Reviewer has checked that all the criteria below are met 
- Title is accurate
- All changes are related to the pull request's stated goal
- Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes
- Testing strategy adequately addresses listed risks
- Newly-added code is easy to change
- Release note makes sense to a user of the library
- If necessary, author has acknowledged and discussed the performance
implications of this PR as reported in the benchmarks PR comment
- Backport labels are set in a manner that is consistent with the
[release branch maintenance
policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

ddtrace/internal/symbol_db/symbols.py
@@ -668,5 +668,5 @@ class SymbolDatabaseUploader(BaseModuleWatchdog):
 
     @classmethod
     def install(cls, shallow=True):
-        cls._shallow = shallow
+        cls.shallow = shallow
         return super().install()

releasenotes/notes/fix-symdb-uploads-cfff7f4d568ff882.yaml
@@ -1,0 +1,5 @@
+---
+fixes:
+  - |
+    dynamic instrumentation: fixes an issue where only module scopes were being uploaded,
+    preventing method probes from being created.

tests/internal/symbol_db/test_symbols.py
@@ -270,7 +270,8 @@ def test_symbols_force_upload():
 
     SymbolDatabaseUploader._upload_context = staticmethod(_upload_context)
 
-    SymbolDatabaseUploader.install()
+    SymbolDatabaseUploader.install(shallow=False)
+    assert SymbolDatabaseUploader.shallow is False
 
     def get_scope(contexts, name):
         for context in (_.to_json() for _ in contexts):