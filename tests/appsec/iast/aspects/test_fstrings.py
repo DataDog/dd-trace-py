1905f57b63a0fb93ca03d0debf8a326eac6516aa
Alberto Vara <alberto.vara@datadoghq.com>
2025-06-05 11:47:18 +0200

    chore(iast): fix fstring log message

--- tests/appsec/iast/aspects/test_fstrings.py
+++ tests/appsec/iast/aspects/test_fstrings.py
@@ -11,7 +11,6 @@ from ddtrace.appsec._iast._taint_tracking import OriginType
 from ddtrace.appsec._iast._taint_tracking import as_formatted_evidence
 from ddtrace.appsec._iast._taint_tracking._taint_objects import taint_pyobject
 from ddtrace.appsec._iast._taint_tracking._taint_objects_base import is_pyobject_tainted
-from tests.appsec.iast.aspects.aspect_utils import BaseReplacement
 from tests.appsec.iast.aspects.aspect_utils import create_taint_range_with_format
 from tests.appsec.iast.iast_utils import CustomStr
 from tests.appsec.iast.iast_utils import _iast_patched_module
@@ -154,7 +153,6 @@ def test_int_fstring_zero_padding_integers(integers_to_test):
     ),
 )
 def test_int_fstring_zero_padding_text(text, spec):
-    print(f"{text}: {spec}")
     with pytest.raises(ValueError) as excinfo:
         f"{text:{spec}}"
     assert str(excinfo.value) == f"Unknown format code '{spec}' for object of type 'str'"
@@ -164,91 +162,97 @@ def test_int_fstring_zero_padding_text(text, spec):
     assert str(excinfo.value) == f"Unknown format code '{spec}' for object of type 'str'"
 
 
-class TestOperatorsReplacement(BaseReplacement):
-    def test_string_build_string_tainted(self):
-        string_input = "foo"
-        result = mod_py3.do_fmt_value(string_input)  # pylint: disable=no-member
-        assert result == "foo     bar"
+def test_string_build_string_tainted():
+    string_input = "foo"
+    result = mod_py3.do_fmt_value(string_input)  # pylint: disable=no-member
+    assert result == "foo     bar"
 
-        string_input = create_taint_range_with_format(":+-foo-+:")
-        result = mod_py3.do_fmt_value(string_input)  # pylint: disable=no-member
-        assert result == "foo     bar"
-        assert as_formatted_evidence(result) == ":+-foo-+:     bar"
+    string_input = create_taint_range_with_format(":+-foo-+:")
+    result = mod_py3.do_fmt_value(string_input)  # pylint: disable=no-member
+    assert result == "foo     bar"
+    assert as_formatted_evidence(result) == ":+-foo-+:     bar"
 
-    def test_string_fstring_tainted(self):
-        string_input = "foo"
-        result = mod_py3.do_repr_fstring(string_input)
-        assert result == "'foo'"
 
-        string_input = create_taint_range_with_format(":+-foo-+:")
+def test_string_fstring_tainted():
+    string_input = "foo"
+    result = mod_py3.do_repr_fstring(string_input)
+    assert result == "'foo'"
 
-        result = mod_py3.do_repr_fstring(string_input)  # pylint: disable=no-member
-        assert as_formatted_evidence(result) == "':+-foo-+:'"
+    string_input = create_taint_range_with_format(":+-foo-+:")
 
-    def test_string_fstring_with_format_tainted(self):
-        string_input = "foo"
-        result = mod_py3.do_repr_fstring_with_format(string_input)
-        assert result == "'foo'     "
+    result = mod_py3.do_repr_fstring(string_input)  # pylint: disable=no-member
+    assert as_formatted_evidence(result) == "':+-foo-+:'"
 
-        string_input = create_taint_range_with_format(":+-foo-+:")
 
-        result = mod_py3.do_repr_fstring_with_format(string_input)  # pylint: disable=no-member
-        assert as_formatted_evidence(result) == "':+-foo-+:'     "
+def test_string_fstring_with_format_tainted():
+    string_input = "foo"
+    result = mod_py3.do_repr_fstring_with_format(string_input)
+    assert result == "'foo'     "
 
-    def test_string_fstring_repr_str_twice_tainted(self):
-        string_input = "foo"
+    string_input = create_taint_range_with_format(":+-foo-+:")
 
-        result = mod_py3.do_repr_fstring_twice(string_input)  # pylint: disable=no-member
-        assert result == "'foo' 'foo'"
+    result = mod_py3.do_repr_fstring_with_format(string_input)  # pylint: disable=no-member
+    assert as_formatted_evidence(result) == "':+-foo-+:'     "
 
-        string_input = create_taint_range_with_format(":+-foo-+:")
 
-        result = mod_py3.do_repr_fstring_twice(string_input)  # pylint: disable=no-member
-        assert result == "'foo' 'foo'"
-        assert as_formatted_evidence(result) == "':+-foo-+:' ':+-foo-+:'"
+def test_string_fstring_repr_str_twice_tainted():
+    string_input = "foo"
 
-    def test_string_fstring_repr_object_twice_tainted(self):
-        string_input = "foo"
-        result = mod.MyObject(string_input)
-        assert repr(result) == "foo a"
+    result = mod_py3.do_repr_fstring_twice(string_input)  # pylint: disable=no-member
+    assert result == "'foo' 'foo'"
 
-        result = mod_py3.do_repr_fstring_twice(result)  # pylint: disable=no-member
-        assert result == "foo a foo a"
+    string_input = create_taint_range_with_format(":+-foo-+:")
 
-        string_input = create_taint_range_with_format(":+-foo-+:")
-        obj = mod.MyObject(string_input)  # pylint: disable=no-member
+    result = mod_py3.do_repr_fstring_twice(string_input)  # pylint: disable=no-member
+    assert result == "'foo' 'foo'"
+    assert as_formatted_evidence(result) == "':+-foo-+:' ':+-foo-+:'"
 
-        result = mod_py3.do_repr_fstring_twice(obj)  # pylint: disable=no-member
-        assert result == "foo a foo a"
-        assert as_formatted_evidence(result) == ":+-foo-+: a :+-foo-+: a"
 
-    def test_string_fstring_twice_different_objects_tainted(self):
-        string_input = create_taint_range_with_format(":+-foo-+:")
-        obj = mod.MyObject(string_input)  # pylint: disable=no-member
-        obj2 = mod.MyObject(string_input)  # pylint: disable=no-member
+def test_string_fstring_repr_object_twice_tainted():
+    string_input = "foo"
+    result = mod.MyObject(string_input)
+    assert repr(result) == "foo a"
 
-        result = mod_py3.do_repr_fstring_twice_different_objects(obj, obj2)  # pylint: disable=no-member
-        assert result == "foo a foo a"
-        assert as_formatted_evidence(result) == ":+-foo-+: a :+-foo-+: a"
+    result = mod_py3.do_repr_fstring_twice(result)  # pylint: disable=no-member
+    assert result == "foo a foo a"
 
-    def test_string_fstring_twice_different_objects_tainted_twice(self):
-        string_input = create_taint_range_with_format(":+-foo-+:")
-        obj = mod.MyObject(string_input)  # pylint: disable=no-member
+    string_input = create_taint_range_with_format(":+-foo-+:")
+    obj = mod.MyObject(string_input)  # pylint: disable=no-member
 
-        result = mod_py3.do_repr_fstring_with_format_twice(obj)  # pylint: disable=no-member
-        assert result == "foo a      foo a      "
-        assert as_formatted_evidence(result) == ":+-foo-+: a      :+-foo-+: a      "
+    result = mod_py3.do_repr_fstring_twice(obj)  # pylint: disable=no-member
+    assert result == "foo a foo a"
+    assert as_formatted_evidence(result) == ":+-foo-+: a :+-foo-+: a"
 
-    @pytest.mark.parametrize(
-        "function",
-        [
-            mod_py3.do_repr_fstring_with_expression1,
-            mod_py3.do_repr_fstring_with_expression2,
-            mod_py3.do_repr_fstring_with_expression3,
-            mod_py3.do_repr_fstring_with_expression4,
-            mod_py3.do_repr_fstring_with_expression5,
-        ],
-    )
-    def test_string_fstring_non_string(self, function):
-        result = function()  # pylint: disable=no-member
-        assert result == "Hello world, True!"
+
+def test_string_fstring_twice_different_objects_tainted():
+    string_input = create_taint_range_with_format(":+-foo-+:")
+    obj = mod.MyObject(string_input)  # pylint: disable=no-member
+    obj2 = mod.MyObject(string_input)  # pylint: disable=no-member
+
+    result = mod_py3.do_repr_fstring_twice_different_objects(obj, obj2)  # pylint: disable=no-member
+    assert result == "foo a foo a"
+    assert as_formatted_evidence(result) == ":+-foo-+: a :+-foo-+: a"
+
+
+def test_string_fstring_twice_different_objects_tainted_twice():
+    string_input = create_taint_range_with_format(":+-foo-+:")
+    obj = mod.MyObject(string_input)  # pylint: disable=no-member
+
+    result = mod_py3.do_repr_fstring_with_format_twice(obj)  # pylint: disable=no-member
+    assert result == "foo a      foo a      "
+    assert as_formatted_evidence(result) == ":+-foo-+: a      :+-foo-+: a      "
+
+
+@pytest.mark.parametrize(
+    "function",
+    [
+        mod_py3.do_repr_fstring_with_expression1,
+        mod_py3.do_repr_fstring_with_expression2,
+        mod_py3.do_repr_fstring_with_expression3,
+        mod_py3.do_repr_fstring_with_expression4,
+        mod_py3.do_repr_fstring_with_expression5,
+    ],
+)
+def test_string_fstring_non_string(function):
+    result = function()  # pylint: disable=no-member
+    assert result == "Hello world, True!"
