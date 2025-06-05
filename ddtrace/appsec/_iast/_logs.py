e8ec2d995a9c0bd8a598c4d90ea2971819edcc54
Alberto Vara <alberto.vara@datadoghq.com>
2025-06-05 14:55:20 +0200

    chore(iast): fix fstring log message

--- ddtrace/appsec/_iast/_logs.py
+++ ddtrace/appsec/_iast/_logs.py
@@ -1,3 +1,5 @@
+import inspect
+
 from ddtrace.appsec._iast._metrics import _set_iast_error_metric
 from ddtrace.appsec._iast._utils import _is_iast_debug_enabled
 from ddtrace.internal.logger import get_logger
@@ -43,7 +45,8 @@ def iast_propagation_error_log(msg):
 
 
 def iast_error(msg, default_prefix="iast::"):
-    p_msg = default_prefix + msg
     if _is_iast_debug_enabled():
-        log.debug(p_msg, exc_info=True)
-    _set_iast_error_metric(p_msg)
+        stack = inspect.stack()
+        frame_info = "\n".join("%s %s" % (frame_info.filename, frame_info.lineno) for frame_info in stack[:7])
+        log.debug("%s%s:\n%s", default_prefix, msg, frame_info)
+        _set_iast_error_metric(f"{default_prefix}. {msg}")
