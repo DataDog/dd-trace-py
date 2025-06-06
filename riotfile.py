f829b4247787644ee9ae45fe245db3d19d48c7a5
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2025-06-06 15:03:09 +0200

    move freezegun from hatch to riot
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- riotfile.py
+++ riotfile.py
@@ -3030,5 +3030,20 @@ venv = Venv(
                 ),
             ],
         ),
+        Venv(
+            name="freezegun",
+            command="pytest tests/contrib/freezegun {cmdargs}",
+            pkgs={
+                "pytest-randomly": latest,
+            },
+            venvs=[
+                Venv(
+                    pys=["3.10", "3.12"],
+                    pkgs={
+                        "freezegun": ["~=1.3.0", "~=1.5.0"],
+                    },
+                ),
+            ],
+        ),
     ],
 )
