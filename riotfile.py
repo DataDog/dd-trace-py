a3a08ab162ac55d599cbf19da7e4942f9f4d72f4
William Conti <william.conti@datadoghq.com>
2025-06-09 18:25:27 -0400

    update version ranges

--- riotfile.py
+++ riotfile.py
@@ -565,16 +565,17 @@ venv = Venv(
                 # Celery 4.3 wants Kombu >= 4.4 and Redis >= 3.2
                 # Split into <3.8 and >=3.8 to pin importlib_metadata dependency for kombu
                 #     # celery added support for Python 3.9 in 4.x
-                #     pys=select_pys(min_version="3.8", max_version="3.9"),
-                #     pkgs={
-                #         "pytest": "~=4.0",
-                #         "celery": [
-                #             "latest",  # most recent 4.x
-                #         ],
-                #         "redis": "~=3.5",
-                #         "kombu": "~=4.4",
-                #     },
-                # ),
+                Venv(
+                    pys=select_pys(min_version="3.8", max_version="3.9"),
+                    pkgs={
+                        "pytest": "~=4.0",
+                        "celery": [
+                            "~=4.4.0",  # most recent 4.x
+                        ],
+                        "redis": "~=3.5",
+                        "kombu": "~=4.4",
+                    },
+                ),
                 # Celery 5.x wants Python 3.6+
                 # Split into <3.8 and >=3.8 to pin importlib_metadata dependency for kombu
                 Venv(
@@ -616,6 +617,7 @@ venv = Venv(
                     pys=select_pys(max_version="3.10"),
                     pkgs={
                         "cherrypy": [
+                            "~=17.0.0",
                             ">=17,<18",
                         ],
                         "more_itertools": "<8.11.0",
@@ -795,7 +797,7 @@ venv = Venv(
                     pys=select_pys(min_version="3.8", max_version="3.11"),
                     pkgs={
                         "sqlalchemy": "~=1.2.18",
-                        "django": "==2.2.1",
+                        "django": "~=2.2.0",
                     },
                 ),
                 Venv(
@@ -811,6 +813,10 @@ venv = Venv(
             name="dramatiq",
             command="pytest {cmdargs} tests/contrib/dramatiq",
             venvs=[
+                Venv(
+                    pys=select_pys(min_version="3.8", max_version="3.9"),
+                    pkgs={"dramatiq": "~=1.10.0", "pytest": latest, "redis": latest},
+                ),
                 Venv(
                     pys=select_pys(),
                     pkgs={"dramatiq": latest, "pytest": latest, "redis": latest},
@@ -1029,7 +1035,7 @@ venv = Venv(
             command="pytest {cmdargs} tests/contrib/mako",
             pys=select_pys(),
             pkgs={
-                "mako": ["~=1.1.0", latest],
+                "mako": ["~=1.0.0", latest],
                 "pytest-randomly": latest,
             },
         ),
@@ -1087,17 +1093,25 @@ venv = Venv(
             },
             venvs=[
                 Venv(
-                    pkgs={"psycopg": [latest]},
                     venvs=[
+                        Venv(
+                            pys=select_pys(min_version="3.8", max_version="3.9"),
+                            pkgs={
+                                "psycopg": "~=3.0.0",
+                                "pytest-asyncio": "==0.21.1",
+                            },
+                        ),
                         Venv(
                             pys=select_pys(min_version="3.8", max_version="3.11"),
                             pkgs={
+                                "psycopg": latest,
                                 "pytest-asyncio": "==0.21.1",
                             },
                         ),
                         Venv(
                             pys=select_pys(min_version="3.12", max_version="3.12"),
                             pkgs={
+                                "psycopg": latest,
                                 "pytest-asyncio": "==0.23.7",
                             },
                         ),
@@ -1148,7 +1162,7 @@ venv = Venv(
                 Venv(
                     pys=select_pys(min_version="3.8", max_version="3.11"),
                     pkgs={
-                        "pynamodb": ["~=5.0", "~=5.3", "<6.0"],
+                        "pynamodb": ["~=5.0.0", "~=5.3", "<6.0"],
                         "moto": ">=1.0,<2.0",
                         "cfn-lint": "~=0.53.1",
                         "Jinja2": "~=2.10.0",
@@ -1245,7 +1259,7 @@ venv = Venv(
                     pys="3.8",
                     pkgs={
                         "requests": [
-                            "~=2.22.0",
+                            "~=2.23.0",
                             latest,
                         ],
                     },
@@ -1333,12 +1347,12 @@ venv = Venv(
             venvs=[
                 Venv(
                     pys="3.8",
-                    pkgs={"mongoengine": ["~=0.23", latest]},
+                    pkgs={"mongoengine": ["~=0.23.0", latest]},
                 ),
                 Venv(
                     # mongoengine added support for Python 3.9/3.10 in 0.24
                     pys=select_pys(min_version="3.9"),
-                    pkgs={"mongoengine": ["~=0.24", latest]},
+                    pkgs={"mongoengine": ["~=0.24.0", "~=0.24", latest]},
                 ),
             ],
         ),
@@ -1518,7 +1532,7 @@ venv = Venv(
                     venvs=[
                         Venv(
                             pkgs={
-                                "pytest": ["~=6.0"],
+                                "pytest": ["~=6.0.0", "~=6.0"],
                                 "pytest-cov": "==2.9.0",
                             },
                         ),
@@ -2214,7 +2228,7 @@ venv = Venv(
                 # our test_asyncpg.py uses `yield` in an async function and is not compatible with Python 3.5
                 Venv(
                     pys="3.8",
-                    pkgs={"asyncpg": ["~=0.23", latest]},
+                    pkgs={"asyncpg": ["~=0.22.0", latest]},
                 ),
                 Venv(
                     # asyncpg added support for Python 3.9 in 0.22
@@ -2677,7 +2691,7 @@ venv = Venv(
             pys=select_pys(min_version="3.9", max_version="3.12"),
             pkgs={
                 "pytest-asyncio": latest,
-                "google-generativeai": [latest],
+                "google-generativeai": ["~=0.7.0", latest],
                 "pillow": latest,
                 "google-ai-generativelanguage": [latest],
                 "vertexai": [latest],
@@ -2700,7 +2714,7 @@ venv = Venv(
             pkgs={
                 "pytest-asyncio": latest,
                 "openai": latest,
-                "crewai": [">=0.102.0", "~=0.121.0"],
+                "crewai": ["~=0.102.0", "~=0.121.0"],
                 "vcrpy": "==7.0.0",
             },
         ),
