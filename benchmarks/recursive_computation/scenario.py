f342a9abc96e4211e329597b34af56d923630b2b
Alysha <alysha.wang@datadoghq.com>
2025-06-09 17:39:15 -0400

    style fixes

--- benchmarks/recursive_computation/scenario.py
+++ benchmarks/recursive_computation/scenario.py
@@ -21,17 +21,17 @@ class RecursiveComputation(bm.Scenario):
     def cpu_intensive_computation(self, depth: int) -> int:
         limit = 1000 + (depth * 100)
         primes = []
-        
+
         for num in range(2, limit):
             is_prime = True
-            for i in range(2, int(num ** 0.5) + 1):
+            for i in range(2, int(num**0.5) + 1):
                 if num % i == 0:
                     is_prime = False
                     break
-            
+
             if is_prime:
                 primes.append(num)
-        
+
         return len(primes)
 
     def recursive_traced_computation(self, depth: int = 0) -> int:
@@ -40,18 +40,18 @@ class RecursiveComputation(bm.Scenario):
             span.set_tag("recursion.max_depth", self.max_depth)
             span.set_tag("profiler.enabled", self.profiler_enabled)
             span.set_tag("component", "recursive_computation")
-            
+
             start_time = time.time()
             result = self.cpu_intensive_computation(depth)
             compute_time = time.time() - start_time
-            
+
             span.set_metric("computation.time_ms", compute_time * 1000)
             span.set_metric("computation.result", result)
-            
+
             for i in range(self.nspans):
                 with tracer.trace(f"computation.span_{i}"):
                     math.sqrt(i + depth)
-            
+
             if depth < self.max_depth:
                 child_result = self.recursive_traced_computation(depth + 1)
                 span.set_metric("child.result", child_result)
@@ -59,15 +59,17 @@ class RecursiveComputation(bm.Scenario):
             elif self.enable_sleep:
                 span.set_tag("action", "sleep_at_max_depth")
                 time.sleep(self.sleep_duration)
-            
+
             span.set_metric("final.result", result)
             return result
 
     def run(self) -> Generator[Callable[[int], None], None, None]:
         if self.profiler_enabled:
-            os.environ.update({
-                "DD_PROFILING_ENABLED": "1", 
-            })
+            os.environ.update(
+                {
+                    "DD_PROFILING_ENABLED": "1",
+                }
+            )
             import ddtrace.profiling.auto
 
         utils.drop_traces(tracer)
@@ -77,4 +79,4 @@ class RecursiveComputation(bm.Scenario):
             for _ in range(loops):
                 self.recursive_traced_computation()
 
-        yield _ 
+        yield _
