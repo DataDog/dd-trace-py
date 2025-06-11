9f36e6300a6491d9d492a30670bbffae8bf57482
brettlangdon <brett.langdon@datadoghq.com>
2025-06-11 15:33:27 -0400

    refactor extra services tests to use subprocesses

--- tests/internal/service_name/test_extra_services_names.py
+++ tests/internal/service_name/test_extra_services_names.py
@@ -1,42 +1,41 @@
-import random
-import re
-import threading
-import time
+import os
 
 import pytest
 
+
+@pytest.mark.parametrize("child_services", [1, 20])
+def test_config_extra_service_names(child_services, run_python_code_in_subprocess):
+    code = f"""
+import ddtrace.auto
 import ddtrace
 
+import re
+import os
+import sys
+import time
 
-MAX_NAMES = 64
+children = []
+for i in range(10):
+    pid = os.fork()
+    if pid == 0:
+        # Child process
+        print(ddtrace.config._extra_services_queue)
+        for c in range({child_services}):
+            ddtrace.config._add_extra_service(f"extra_service_{{i}}_{{c}}")
+        sys.exit(0)
+    else:
+        # Parent process
+        children.append(pid)
 
+for pid in children:
+    os.waitpid(pid, 0)
 
-@pytest.mark.parametrize("nb_service", [2, 16, 64, 256])
-def test_service_name(nb_service):
-    ddtrace.config._extra_services = set()
+extra_services = ddtrace.config._get_extra_services()
+assert len(extra_services) == min(10 * {child_services}, 64), extra_services
+assert all(re.match(r"extra_service_\\d+_\\d+", service) for service in extra_services)
+"""
 
-    def write_in_subprocess(id_nb):
-        time.sleep(random.random())
-        ddtrace.config._add_extra_service(f"extra_service_{id_nb}")
-
-    default_remote_config_enabled = ddtrace.config._remote_config_enabled
-    ddtrace.config._remote_config_enabled = True
-    if ddtrace.config._extra_services_queue is None:
-        import ddtrace.internal._file_queue as file_queue
-
-        ddtrace.config._extra_services_queue = file_queue.File_Queue()
-
-    threads = [threading.Thread(target=write_in_subprocess, args=(i,)) for i in range(nb_service)]
-    for thread in threads:
-        thread.start()
-    for thread in threads:
-        thread.join()
-
-    extra_services = ddtrace.config._get_extra_services()
-    assert len(extra_services) == min(nb_service, MAX_NAMES)
-    assert all(re.match(r"extra_service_\d+", service) for service in extra_services)
-
-    ddtrace.config._remote_config_enabled = default_remote_config_enabled
-    if not default_remote_config_enabled:
-        ddtrace.config._extra_services_queue = None
-    ddtrace.config._extra_services = set()
+    env = os.environ.copy()
+    env["DD_REMOTE_CONFIGURATION_ENABLED"] = "true"
+    stdout, stderr, status, _ = run_python_code_in_subprocess(code, env=env)
+    assert status == 0, (stdout, stderr, status)
