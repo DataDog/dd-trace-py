fb12080e3df4c98571d035b93b47031f06b8ca68
Alberto Vara <alberto.vara@datadoghq.com>
2025-06-11 10:07:17 +0200

    fix(appsec): fix subprocess errors

--- riotfile.py
+++ riotfile.py
@@ -2824,7 +2824,7 @@ venv = Venv(
         ),
         Venv(
             name="subprocess",
-            command="pytest {cmdargs}  --no-ddtrace --no-cov tests/contrib/subprocess",
+            command="pytest {cmdargs} --no-cov tests/contrib/subprocess",
             pkgs={
                 "pytest-randomly": latest,
             },
