76286907842c22745371bd31a25ef09233fe9acc
Michael Zhao <michael.zhao@datadoghq.com>
2025-06-11 20:29:05 -0400

    documentation

--- ddtrace/data_streams.py
+++ ddtrace/data_streams.py
@@ -8,7 +8,7 @@ def set_consume_checkpoint(typ, source, carrier_get, manual_checkpoint=True):
         Examples include kafka, kinesis, sns etc. (str)
     :param source: The source of data. This can be a topic, exchange or stream name. (str)
     :param carrier_get: A function used to extract context from the carrier (function (str) -> str)
-    :param manual_checkpoint: Whether this is a manual checkpoint. (bool)
+    :param manual_checkpoint: Whether this checkpoint was manually set. Keep true if manually instrumenting. (bool)
 
     :returns DataStreamsCtx | None
     """
