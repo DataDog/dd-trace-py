769cdb983545536c02b8442b491dc485d0d3f2b4
maxzhangdd <max.zhang@datadoghq.com>
2025-06-16 15:28:45 -0400

    Update ddtrace/llmobs/_integrations/google_genai.py
    Co-authored-by: ncybul <124532568+ncybul@users.noreply.github.com>

--- ddtrace/llmobs/_integrations/google_genai.py
+++ ddtrace/llmobs/_integrations/google_genai.py
@@ -6,13 +6,6 @@ from ddtrace._trace.span import Span
 from ddtrace.llmobs._integrations.base import BaseLLMIntegration
 
 
-# from ddtrace.llmobs._constants import INPUT_MESSAGES
-# from ddtrace.llmobs._constants import METADATA
-# from ddtrace.llmobs._constants import METRICS
-# from ddtrace.llmobs._constants import MODEL_NAME
-# from ddtrace.llmobs._constants import MODEL_PROVIDER
-# from ddtrace.llmobs._constants import OUTPUT_MESSAGES
-# from ddtrace.llmobs._constants import SPAN_KIND
 
 
 class GoogleGenAIIntegration(BaseLLMIntegration):
