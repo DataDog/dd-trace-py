93f7660d46e875e01a7cc864794de05144ec8468
Alberto Vara <alberto.vara@datadoghq.com>
2025-06-18 15:43:07 +0200

    chore(ci): remove duplicated functions (#13706)
    Cherry pick some refactor that I made in security controls:
    https://github.com/DataDog/dd-trace-py/pull/13655
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

--- tests/appsec/integrations/django_tests/test_iast_django.py
+++ tests/appsec/integrations/django_tests/test_iast_django.py
@@ -16,6 +16,7 @@ from ddtrace.appsec._iast.constants import VULN_STACKTRACE_LEAK
 from ddtrace.appsec._iast.constants import VULN_UNVALIDATED_REDIRECT
 from ddtrace.settings.asm import config as asm_config
 from tests.appsec.iast.iast_utils import get_line_and_hash
+from tests.appsec.integrations.django_tests.utils import _aux_appsec_get_root_span
 from tests.utils import override_global_config
 
 
@@ -26,35 +27,6 @@ def get_iast_stack_trace(root_span):
     appsec_traces = root_span.get_struct_tag(STACK_TRACE.TAG) or {}
     stacks = appsec_traces.get("vulnerability", [])
     return stacks
-
-
-def _aux_appsec_get_root_span(
-    client,
-    iast_span,
-    tracer,
-    payload=None,
-    url="/",
-    content_type="text/plain",
-    headers=None,
-    cookies=None,
-):
-    if cookies is None:
-        cookies = {}
-    # Hack: need to pass an argument to configure so that the processors are recreated
-    tracer._recreate()
-    # Set cookies
-    client.cookies.load(cookies)
-    if payload is None:
-        if headers:
-            response = client.get(url, **headers)
-        else:
-            response = client.get(url)
-    else:
-        if headers:
-            response = client.post(url, payload, content_type=content_type, **headers)
-        else:
-            response = client.post(url, payload, content_type=content_type)
-    return iast_span.spans[0], response
 
 
 def _aux_appsec_get_root_span_with_exception(
