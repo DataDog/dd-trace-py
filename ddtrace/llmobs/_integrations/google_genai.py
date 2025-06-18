373d2ab2875ce1863aefa5cc190de4d7c6e2b9f5
Max Zhang <max.zhang@datadoghq.com>
2025-06-18 15:00:56 -0400

    incremental progress on llmobs

--- ddtrace/llmobs/_integrations/google_genai.py
+++ ddtrace/llmobs/_integrations/google_genai.py
@@ -10,7 +10,8 @@ from ddtrace.llmobs._constants import MODEL_NAME
 from ddtrace.llmobs._constants import MODEL_PROVIDER
 from ddtrace.llmobs._constants import OUTPUT_MESSAGES
 from ddtrace.llmobs._constants import SPAN_KIND
-
+from ddtrace.llmobs._integrations.utils import llmobs_get_metadata_google
+from ddtrace.llmobs._utils import _get_attr
 from ddtrace._trace.span import Span
 from ddtrace.llmobs._integrations.base import BaseLLMIntegration
 
@@ -34,8 +35,10 @@ class GoogleGenAIIntegration(BaseLLMIntegration):
         response: Optional[Any] = None,
         operation: str = "",
     ) -> None:
-         response = instance = kwargs.get("response", None)
-         metadata =  llmobs_get_metadata_google(kwargs, instance)
+         response = kwargs.get("response", None)
+         # TODO: make metadata actually return something
+         metadata =  llmobs_get_metadata_google(kwargs, response)
+         print("metadata: ", metadata)
          input_messages = None
          output_messages = None
          metrics = None
@@ -45,7 +48,7 @@ class GoogleGenAIIntegration(BaseLLMIntegration):
                 SPAN_KIND: "llm",
                 MODEL_NAME: span.get_tag("google_genai.request.model") or "",
                 MODEL_PROVIDER: span.get_tag("google_genai.request.provider") or "",
-                # METADATA: metadata,
+                METADATA: metadata,
                 # INPUT_MESSAGES: input_messages,
                 # OUTPUT_MESSAGES: output_messages,
                 # METRICS: get_llmobs_metrics_tags("vertexai", span),
