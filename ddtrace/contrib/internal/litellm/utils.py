2b412da23b88ae66b0ea16539727218fe65bda64
Nicole Cybul <nicole.cybul@datadoghq.com>
2025-06-18 13:20:33 -0400

    add in bounds check before appending chunk to streamed chunks

--- ddtrace/contrib/internal/litellm/utils.py
+++ ddtrace/contrib/internal/litellm/utils.py
@@ -128,7 +128,10 @@ def _loop_handler(chunk, streamed_chunks):
     When handling a streamed chat/completion response, this function is called for each chunk in the streamed response.
     """
     for choice in chunk.choices:
-        streamed_chunks[choice.index].append(choice)
+        if len(streamed_chunks) > choice.index:
+            streamed_chunks[choice.index].append(choice)
+        else:
+            log.warning("Choice index is out of range; choice index: %s, streamed_chunks length: %s", choice.index, len(streamed_chunks), exc_info=True)
     if getattr(chunk, "usage", None):
         streamed_chunks[0].insert(0, chunk)
 
