af5999b791bcfce94ed3f79a3e76d0e5f414864c
Xinyuan Guo <xinyuan.guo@datadoghq.com>
2025-06-20 15:05:03 -0400

    Update riotfile.py

--- riotfile.py
+++ riotfile.py
@@ -2440,7 +2440,7 @@ venv = Venv(
                 Venv(
                     pys=select_pys(min_version="3.8"),
                     pkgs={
-                        "openai": "~=1.76.2",
+                        "openai": [latest, "~=1.76.2"],
                         "tiktoken": latest,
                         "pillow": latest,
                     },
