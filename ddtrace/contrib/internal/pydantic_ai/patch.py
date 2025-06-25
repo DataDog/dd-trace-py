6ef188d33cdc4ede197602b22e1912de04372f07
Nicole Cybul <nicole.cybul@datadoghq.com>
2025-06-25 10:12:39 -0400

    trace tool runs

--- ddtrace/contrib/internal/pydantic_ai/patch.py
+++ ddtrace/contrib/internal/pydantic_ai/patch.py
@@ -1,6 +1,7 @@
 import functools
 
 from ddtrace import config
+from ddtrace.internal.utils import get_argument_value
 from ddtrace.llmobs._integrations.base import BaseLLMIntegration
 from ddtrace.contrib.internal.trace_utils import unwrap
 from ddtrace.llmobs._constants import SPAN_KIND
@@ -32,19 +33,13 @@ async def traced_agent_run(pydantic_ai, pin, func, instance, args, kwargs):
         return await func(*args, **kwargs)
 
 
-@with_traced_module
-def traced_agent_register_tool(pydantic_ai, pin, func, instance, args, kwargs):
+@with_traced_module_async
+async def traced_tool_run(pydantic_ai, pin, func, instance, args, kwargs):
     integration = pydantic_ai._datadog_integration
+    with integration.trace(pin, "Pydantic Tool", submit_to_llmobs=False) as span:
+        span.name = instance.name
+        return await func(*args, **kwargs)
 
-    tool_func = args[0]
-
-    @functools.wraps(tool_func)
-    async def wrapped_tool(*args, **kwargs):
-        with integration.trace(pin, "Pydantic Tool", submit_to_llmobs=False) as span:
-            span.name = tool_func.__name__
-            return await tool_func(*args, **kwargs)
-
-    return func(wrapped_tool)
 
 def patch():
     import pydantic_ai
@@ -53,14 +48,14 @@ def patch():
     Pin().onto(pydantic_ai)
 
     wrap(pydantic_ai, "agent.Agent.run", traced_agent_run(pydantic_ai))
-    wrap(pydantic_ai, "agent.Agent.tool", traced_agent_register_tool(pydantic_ai))
+    wrap(pydantic_ai, "tools.Tool.run", traced_tool_run(pydantic_ai))
 
 
 def unpatch():
     import pydantic_ai
 
     unwrap(pydantic_ai.agent.Agent, "run")
-    unwrap(pydantic_ai.agent.Agent, "tool")
+    unwrap(pydantic_ai.tools.Tool, "run")
 
     del pydantic_ai._datadog_integration
     Pin().remove_from(pydantic_ai)
