038061c23609aa8d7ad310cc70cbb3ece6bbf447
Nicole Cybul <nicole.cybul@datadoghq.com>
2025-06-26 09:42:17 -0400

    remove unneeded cassettes and reuse existing one

--- tests/contrib/pydantic_ai/test_pydantic_ai.py
+++ tests/contrib/pydantic_ai/test_pydantic_ai.py
@@ -1,13 +1,13 @@
 async def test_agent_run(pydantic_ai, snapshot_context, request_vcr):
     with snapshot_context(token="tests.contrib.pydantic_ai.test_pydantic_ai.test_agent_run"):
-        with request_vcr.use_cassette("agent_run.yaml"):
+        with request_vcr.use_cassette("agent_iter.yaml"):
             agent = pydantic_ai.Agent(model="gpt-4o", name="test_agent")
             await agent.run("Hello, world!")
 
 
 def test_agent_run_sync(pydantic_ai, snapshot_context, request_vcr):
     with snapshot_context(token="tests.contrib.pydantic_ai.test_pydantic_ai.test_agent_run"):
-        with request_vcr.use_cassette("agent_run_sync.yaml"):
+        with request_vcr.use_cassette("agent_iter.yaml"):
             agent = pydantic_ai.Agent(model="gpt-4o", name="test_agent")
             agent.run_sync("Hello, world!")
 
