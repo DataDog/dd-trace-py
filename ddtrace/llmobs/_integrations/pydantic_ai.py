0a36d5d14893654ea3d42662ebdaf31207eb3849
Nicole Cybul <nicole.cybul@datadoghq.com>
2025-06-26 10:03:32 -0400

    use standard system property to get model provider and parse it if needed

--- ddtrace/llmobs/_integrations/pydantic_ai.py
+++ ddtrace/llmobs/_integrations/pydantic_ai.py
@@ -3,6 +3,11 @@ from typing import Optional
 from ddtrace.llmobs._integrations.base import BaseLLMIntegration
 from ddtrace.trace import Span
 
+# in some cases, PydanticAI uses a different provider name than what we expect
+PYDANTIC_AI_SYSTEM_TO_PROVIDER = {
+    "google-gla": "google",
+    "google-vertex": "google",
+}
 
 class PydanticAIIntegration(BaseLLMIntegration):
     _integration_name = "pydantic_ai"
@@ -10,8 +15,7 @@ class PydanticAIIntegration(BaseLLMIntegration):
     def _set_base_span_tags(self, span: Span, model: Optional[str] = None, **kwargs) -> None:
         if model:
             span.set_tag("pydantic_ai.request.model", getattr(model, "model_name", ""))
-            provider = getattr(model, "_provider", None)
             system = getattr(model, "system", None)
-            # different model providers have different model classes and ways of accessing the provider name
-            if provider or system:
-                span.set_tag("pydantic_ai.request.provider", getattr(provider, "name", "") or system)
+            if system:
+                system = PYDANTIC_AI_SYSTEM_TO_PROVIDER.get(system, system)
+                span.set_tag("pydantic_ai.request.provider", system)
