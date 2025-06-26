c7803e973c5b0286df7712800e02ab48c7968bd9
Zach Montoya <zach.montoya@datadoghq.com>
2025-06-25 17:17:13 -0700

    Add the configuration flag DD_TRACE_OTEL_METRICS_ENABLED to enable the new behaviors

--- ddtrace/settings/_config.py
+++ ddtrace/settings/_config.py
@@ -626,7 +626,8 @@ class Config(object):
             "DD_CIVISIBILITY_EARLY_FLAKE_DETECTION_ENABLED", True, asbool
         )
         self._otel_enabled = _get_config("DD_TRACE_OTEL_ENABLED", False, asbool, "OTEL_SDK_DISABLED")
-        if self._otel_enabled:
+        self._otel_metrics_enabled = _get_config("DD_TRACE_OTEL_METRICS_ENABLED", False, asbool, "OTEL_SDK_DISABLED")
+        if self._otel_enabled or self._otel_metrics_enabled:
             # Replaces the default otel api runtime context with DDRuntimeContext
             # https://github.com/open-telemetry/opentelemetry-python/blob/v1.16.0/opentelemetry-api/src/opentelemetry/context/__init__.py#L53
             os.environ["OTEL_PYTHON_CONTEXT"] = "ddcontextvars_context"
