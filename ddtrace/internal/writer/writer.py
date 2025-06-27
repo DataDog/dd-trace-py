eb4ef130460a91088c33e8245fa4cb214a08e61e
vianney <vianney.ruhlmann@datadoghq.com>
2025-06-27 16:45:14 +0200

    feat(native_writer): recreate writer on downgrade

--- ddtrace/internal/writer/writer.py
+++ ddtrace/internal/writer/writer.py
@@ -791,6 +791,7 @@ class NativeWriter(periodic.PeriodicService, TraceWriter):
                 client.ENDPOINT,
                 response.status,
             )
+            self.recreate()
         else:
             log.error(
                 "unsupported endpoint '%s': received response %s from intake (%s)",
