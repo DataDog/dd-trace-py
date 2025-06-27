ffc877f56311cdd716770e1abb279602ff908df7
Duncan Harvey <duncan.harvey@datadoghq.com>
2025-06-27 16:35:35 -0400

    add messaging attributes to azure function service bus consumer spans

--- riotfile.py
+++ riotfile.py
@@ -2913,7 +2913,7 @@ venv = Venv(
             command="pytest {cmdargs} tests/contrib/azure_functions",
             pys=select_pys(min_version="3.8", max_version="3.11"),
             pkgs={
-                "azure.functions": ["~=1.20.0", latest],
+                "azure.functions": ["~=1.10.1", latest],
                 "azure.servicebus": latest,
                 "requests": latest,
             },
