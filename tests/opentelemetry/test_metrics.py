f311392c668d41a78293e6772d72cef5a022baf9
Zach Montoya <zach.montoya@datadoghq.com>
2025-07-02 11:29:14 -0700

    Revert "Update OTel metrics tests to remove skip logic since we support all versions of OpenTelemetry >=1.0"
    This reverts commit 59cd9ce55d55c6e451e500b2e594ee96ce9c204c.

--- tests/opentelemetry/test_metrics.py
+++ tests/opentelemetry/test_metrics.py
@@ -8,13 +8,18 @@ OTEL_VERSION = tuple(int(x) for x in version.__version__.split(".")[:3])
 
 
 def skipif(
-    exporter_installed: bool = False, exporter_not_installed: bool = False):
+    exporter_installed: bool = False, exporter_not_installed: bool = False, unsupported_otel_version: bool = False
+):
     """
     Returns a pytest skip marker based on OpenTelemetry version and exporter installation.
     Parameters:
     - exporter_installed: If True, skip tests that require OpenTelemetry exporters.
     - exporter_not_installed: If True, skip tests that do not require OpenTelemetry exporters.
+    - unsupported_otel_version: If True, skip tests that require OpenTelemetry version 1.0 or higher.
     """
+    if unsupported_otel_version and OTEL_VERSION < (1, 0):
+        return pytest.mark.skipif(True, reason="OpenTelemetry version 1.0 or higher is required for these tests")
+
     has_exporter = os.getenv("SDK_EXPORTER_INSTALLED", "").lower() in ("true", "1")
     if exporter_installed and has_exporter:
         return pytest.mark.skipif(True, reason="Tests not compatible with the opentelemetry exporters")
@@ -23,7 +28,7 @@ def skipif(
     return pytest.mark.skipif(False, reason="No skip condition met for OpenTelemetry logs exporter tests")
 
 
-@skipif(exporter_installed=True)
+@skipif(exporter_installed=True, unsupported_otel_version=True)
 def test_otel_metrics_sdk_not_installed_by_default():
     """
     Test that the OpenTelemetry metrics exporter can be set up correctly.
@@ -38,7 +43,7 @@ def test_otel_metrics_sdk_not_installed_by_default():
         from opentelemetry.sdk.resources import Resource  # noqa: F401
 
 
-@skipif(exporter_not_installed=True)
+@skipif(exporter_not_installed=True, unsupported_otel_version=True)
 @pytest.mark.subprocess()
 def test_otel_metrics_exporter_installed():
     """
@@ -66,7 +71,7 @@ def test_otel_metrics_exporter_installed():
         pytest.fail("OTLPMetricExporter for HTTP/protobuf should be available")
 
 
-@skipif(exporter_not_installed=True)
+@skipif(exporter_not_installed=True, unsupported_otel_version=True)
 @pytest.mark.subprocess(ddtrace_run=True, env={"DD_TRACE_OTEL_METRICS_ENABLED": "true"})
 def test_otel_metrics_enabled():
     """
@@ -78,7 +83,7 @@ def test_otel_metrics_enabled():
     assert meter_provider, f"OpenTelemetry metrics exporter should be configured automatically."
 
 
-@skipif(exporter_not_installed=True)
+@skipif(exporter_not_installed=True, unsupported_otel_version=True)
 @pytest.mark.subprocess(ddtrace_run=True, parametrize={"DD_TRACE_OTEL_METRICS_ENABLED": [None, "false"]})
 def test_otel_metrics_disabled_and_unset():
     """
