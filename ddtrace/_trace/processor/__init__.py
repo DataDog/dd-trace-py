dcf2746e3bf6c28e84bf6f46019219e682f69929
Brett Langdon <brett.langdon@datadoghq.com>
2025-07-10 09:54:48 -0400

    chore(tracer): add abstract base class for AgentWriterInterface (#13876)
    Preparing the code for a future where we have multiple implementations
    of the AgentWriter.
    
    I grabbed some minor adjacent type hinting stuff as well.
    
    Split from #13071 
    
    We expect no major changes from this change. The only interesting change
    is the `agent_url` -> `intake_url` change which this is an
    internal/private class.
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    
    ---------
    
    Co-authored-by: Emmett Butler <723615+emmettbutler@users.noreply.github.com>

--- ddtrace/_trace/processor/__init__.py
+++ ddtrace/_trace/processor/__init__.py
@@ -37,6 +37,7 @@ from ddtrace.internal.telemetry.constants import TELEMETRY_NAMESPACE
 from ddtrace.internal.utils.http import verify_url
 from ddtrace.internal.writer import AgentResponse
 from ddtrace.internal.writer import AgentWriter
+from ddtrace.internal.writer import AgentWriterInterface
 from ddtrace.internal.writer import LogWriter
 from ddtrace.internal.writer import TraceWriter
 from ddtrace.settings._agent import config as agent_config
@@ -296,7 +297,7 @@ class SpanAggregator(SpanProcessor):
         else:
             verify_url(agent_config.trace_agent_url)
             self.writer = AgentWriter(
-                agent_url=agent_config.trace_agent_url,
+                intake_url=agent_config.trace_agent_url,
                 dogstatsd=get_dogstatsd_client(agent_config.dogstatsd_url),
                 sync_mode=SpanAggregator._use_sync_mode(),
                 headers={"Datadog-Client-Computed-Stats": "yes"}
@@ -528,7 +529,7 @@ class SpanAggregator(SpanProcessor):
             # Stopping them before that will raise a ServiceStatusError.
             pass
 
-        if isinstance(self.writer, AgentWriter) and appsec_enabled:
+        if isinstance(self.writer, AgentWriterInterface) and appsec_enabled:
             # Ensure AppSec metadata is encoded by setting the API version to v0.4.
             self.writer._api_version = "v0.4"
         # Re-create the writer to ensure it is consistent with updated configurations (ex: api_version)
