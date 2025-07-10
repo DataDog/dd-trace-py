3634b120aea8d758f701e0a774467fdee6a4b239
Christophe Papazian <christophe.papazian@datadoghq.com>
2025-07-10 11:27:36 +0200

    fix flask_threats in riot

--- riotfile.py
+++ riotfile.py
@@ -3504,6 +3504,7 @@ venv = Venv(
             ],
         ),
         Venv(
+            name="appsec_threats_flask",
             command="pytest tests/appsec/contrib_appsec/test_flask.py {cmdargs}",
             pys=["3.8", "3.9", "3.10", "3.11", "3.13"],
             pkgs={
