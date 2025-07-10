cef8e69caebf6beeb3f5bd2cf4639287a0702319
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2025-07-10 18:50:54 +0200

    fmt
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- riotfile.py
+++ riotfile.py
@@ -5,10 +5,8 @@ from typing import Tuple  # noqa
 
 from riot import Venv
 
-
 logger = logging.getLogger(__name__)
 latest = ""
-
 
 SUPPORTED_PYTHON_VERSIONS: List[Tuple[int, int]] = [
     (3, 8),
@@ -95,7 +93,6 @@ _appsec_threats_iast_variants = [
         },
     ),
 ]
-
 
 venv = Venv(
     pkgs={
@@ -3119,7 +3116,8 @@ venv = Venv(
         Venv(
             name="profile",
             # NB riot commands that use this Venv must include --pass-env to work properly
-            command="python -m tests.profiling.run pytest -v --no-cov --capture=no --benchmark-disable {cmdargs} tests/profiling",  # noqa: E501
+            command="python -m tests.profiling.run pytest -v --no-cov --capture=no --benchmark-disable {cmdargs} tests/profiling",
+            # noqa: E501
             env={
                 "DD_PROFILING_ENABLE_ASSERTS": "1",
                 "DD_PROFILING_STACK_V2_ENABLED": "0",
@@ -3215,7 +3213,8 @@ venv = Venv(
         Venv(
             name="profile-v2",
             # NB riot commands that use this Venv must include --pass-env to work properly
-            command="python -m tests.profiling.run pytest -v --no-cov --capture=no --benchmark-disable {cmdargs} tests/profiling_v2",  # noqa: E501
+            command="python -m tests.profiling.run pytest -v --no-cov --capture=no --benchmark-disable {cmdargs} tests/profiling_v2",
+            # noqa: E501
             env={
                 "DD_PROFILING_ENABLE_ASSERTS": "1",
                 "CPUCOUNT": "12",
