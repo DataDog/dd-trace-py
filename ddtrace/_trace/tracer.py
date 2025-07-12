DataDog/dd-trace-py

commit 911cf0f631ad07580d27c7fa185a7c4d25d5d9f6

chore: remove make_async_decorator from compat.py (#13480)
[APMAPI-1159](https://datadoghq.atlassian.net/browse/APMAPI-1159) Since
ddtrace>=3.0 no longer supports python<=3.7, removing code that was
introduced to support older python versions.

## Checklist
- [X] PR author has checked that all the criteria below are met
- The PR description includes an overview of the change
- The PR description articulates the motivation for the change
- The change includes tests OR the PR description describes a testing
strategy
- The PR description notes risks associated with the change, if any
- Newly-added code is easy to change
- The change follows the [library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
- The change includes or references documentation updates if necessary
- Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))

## Reviewer Checklist
- [x] Reviewer has checked that all the criteria below are met 
- Title is accurate
- All changes are related to the pull request's stated goal
- Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes
- Testing strategy adequately addresses listed risks
- Newly-added code is easy to change
- Release note makes sense to a user of the library
- If necessary, author has acknowledged and discussed the performance
implications of this PR as reported in the benchmarks PR comment
- Backport labels are set in a manner that is consistent with the
[release branch maintenance
policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)


[APMAPI-1159]:
https://datadoghq.atlassian.net/browse/APMAPI-1159?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

ddtrace/_trace/tracer.py
@@ -7,12 +7,12 @@ import os
 from os import getpid
 from threading import RLock
 from typing import TYPE_CHECKING
+from typing import Any
 from typing import Callable
 from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Tuple
-from typing import TypeVar
 from typing import Union
 
 from ddtrace._hooks import Hooks
@@ -30,7 +30,6 @@ from ddtrace.constants import ENV_KEY
 from ddtrace.constants import PID
 from ddtrace.constants import VERSION_KEY
 from ddtrace.internal import atexit
-from ddtrace.internal import compat
 from ddtrace.internal import debug
 from ddtrace.internal import forksafe
 from ddtrace.internal import hostname
@@ -61,7 +60,7 @@ from ddtrace.version import get_version
 log = get_logger(__name__)
 
 
-AnyCallable = TypeVar("AnyCallable", bound=Callable)
+AnyCallable = Callable[..., Any]
 
 if TYPE_CHECKING:
     from ddtrace.appsec._processor import AppSecSpanProcessor
@@ -828,18 +827,12 @@ class Tracer(object):
             # right decorator; this initial check ensures that the
             # evaluation is done only once for each @tracer.wrap
             if iscoroutinefunction(f):
-                # call the async factory that creates a tracing decorator capable
-                # to await the coroutine execution before finishing the span. This
-                # code is used for compatibility reasons to prevent Syntax errors
-                # in Python 2
-                func_wrapper = compat.make_async_decorator(
-                    self,
-                    f,
-                    span_name,
-                    service=service,
-                    resource=resource,
-                    span_type=span_type,
-                )
+                # create an async wrapper that awaits the coroutine and traces it
+                @functools.wraps(f)
+                async def func_wrapper(*args, **kwargs):
+                    with self.trace(span_name, service=service, resource=resource, span_type=span_type):
+                        return await f(*args, **kwargs)
+
             else:
 
                 @functools.wraps(f)

ddtrace/internal/compat.py
@@ -1,4 +1,3 @@
-import functools
 import ipaddress
 import sys
 from types import TracebackType
@@ -45,27 +44,6 @@ def is_integer(obj: Any) -> bool:
     # >>> isinstance(True, int)
     # True
     return isinstance(obj, int) and not isinstance(obj, bool)
-
-
-def make_async_decorator(tracer, coro, *params, **kw_params):
-    """
-    Decorator factory that creates an asynchronous wrapper that yields
-    a coroutine result. This factory is required to handle Python 2
-    compatibilities.
-
-    :param object tracer: the tracer instance that is used
-    :param function f: the coroutine that must be executed
-    :param tuple params: arguments given to the Tracer.trace()
-    :param dict kw_params: keyword arguments given to the Tracer.trace()
-    """
-
-    @functools.wraps(coro)
-    async def func_wrapper(*args, **kwargs):
-        with tracer.trace(*params, **kw_params):
-            result = await coro(*args, **kwargs)
-            return result
-
-    return func_wrapper
 
 
 # DEV: There is `six.u()` which does something similar, but doesn't have the guard around `hasattr(s, 'decode')`

commit 65c54f6f6ecdadbe8a74fa4d83ebf3aefee594f3

fix(tracing): ensure tracer.wrap preserves decorated function’s type (#13906)
This change reverts a regression introduced in
https://github.com/DataDog/dd-trace-py/pull/13480 where using
`@tracer.wrap()` causes type checkers to infer the return type of
decorated functions as `Any` instead of the actual return type.

Addresses https://github.com/DataDog/dd-trace-py/issues/13637

## Checklist
- [x] PR author has checked that all the criteria below are met
- The PR description includes an overview of the change
- The PR description articulates the motivation for the change
- The change includes tests OR the PR description describes a testing
strategy
- The PR description notes risks associated with the change, if any
- Newly-added code is easy to change
- The change follows the [library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
- The change includes or references documentation updates if necessary
- Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))

## Reviewer Checklist
- [x] Reviewer has checked that all the criteria below are met 
- Title is accurate
- All changes are related to the pull request's stated goal
- Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes
- Testing strategy adequately addresses listed risks
- Newly-added code is easy to change
- Release note makes sense to a user of the library
- If necessary, author has acknowledged and discussed the performance
implications of this PR as reported in the benchmarks PR comment
- Backport labels are set in a manner that is consistent with the
[release branch maintenance
policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

---------

Co-authored-by: Rachel Yang <rachel.yang@datadoghq.com>
Co-authored-by: Brett Langdon <brett.langdon@datadoghq.com>

ddtrace/_trace/tracer.py
@@ -8,13 +8,14 @@ import os
 from os import getpid
 from threading import RLock
 from typing import TYPE_CHECKING
-from typing import Any
 from typing import Callable
 from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Tuple
+from typing import TypeVar
 from typing import Union
+from typing import cast
 
 from ddtrace._hooks import Hooks
 from ddtrace._trace.context import Context
@@ -66,7 +67,7 @@ from ddtrace.version import get_version
 log = get_logger(__name__)
 
 
-AnyCallable = Callable[..., Any]
+AnyCallable = TypeVar("AnyCallable", bound=Callable)
 
 if TYPE_CHECKING:
     from ddtrace.appsec._processor import AppSecSpanProcessor
@@ -784,7 +785,7 @@ class Tracer(object):
                 return_value = yield from f(*args, **kwargs)
                 return return_value
 
-        return func_wrapper
+        return cast(AnyCallable, func_wrapper)
 
     def _wrap_generator_async(
         self,
@@ -802,7 +803,7 @@ class Tracer(object):
                 async for value in f(*args, **kwargs):
                     yield value
 
-        return func_wrapper
+        return cast(AnyCallable, func_wrapper)
 
     def wrap(
         self,

releasenotes/notes/fix-tracer-wrap-return-type-c9436d6861451dab.yaml
@@ -1,0 +1,4 @@
+---
+fixes:
+  - |
+    tracing: This fix resolves an issue where the ``@tracer.wrap()`` decorator failed to preserve the decorated function's return type, returning ``Any`` instead of the original return type.