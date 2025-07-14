c8d14316f64b3d9ff95eaaa752fdd70db0455338
Yun Kim <35776586+Yun-Kim@users.noreply.github.com>
2025-07-14 18:53:44 -0400

    chore(llmobs): add support for creating experiment (#13955)
    [MLOB-3275] 
    
    Adds support for creating Experiments to Datadog. This isn't a
    user-facing change, but allows the LLMObs service/writer to send a POST
    request to LLMObs experiments to create a new experiment object.
    
    Also includes minor type hints, as well as changing the LLMObs service
    instance to be stored on each Experiment object instead of the LLMObs
    class. Lastly adds tags as an Experiment attribute.
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    
    
    [MLOB-3275]:
    https://datadoghq.atlassian.net/browse/MLOB-3275?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- tests/llmobs/test_experiments.py
+++ tests/llmobs/test_experiments.py
@@ -145,7 +145,7 @@ def test_experiment_invalid_evaluator_signature_raises(llmobs, test_dataset):
         llmobs.experiment("test_experiment", dummy_task, test_dataset, [my_evaluator_missing_output])
 
 
-def test_experiment_create(llmobs, test_dataset):
+def test_experiment_init(llmobs, test_dataset):
     exp = llmobs.experiment(
         "test_experiment",
         dummy_task,
@@ -158,6 +158,11 @@ def test_experiment_create(llmobs, test_dataset):
     assert exp._task == dummy_task
     assert exp._dataset == test_dataset
     assert exp._evaluators == [dummy_evaluator]
+    assert exp._project_name == "test-project"
+    assert exp._description == "lorem ipsum"
+    assert exp._project_id is None
+    assert exp._run_name is None
+    assert exp._id is None
 
 
 def test_experiment_create_no_project_name_raises(llmobs, test_dataset):
@@ -166,3 +171,18 @@ def test_experiment_create_no_project_name_raises(llmobs, test_dataset):
     with pytest.raises(ValueError, match="project_name must be provided for the experiment"):
         llmobs.experiment("test_experiment", dummy_task, test_dataset, [dummy_evaluator], project_name=None)
     llmobs._project_name = project_name  # reset to original value for other tests
+
+
+def test_experiment_create(llmobs, test_dataset):
+    exp = llmobs.experiment(
+        "test_experiment",
+        dummy_task,
+        test_dataset,
+        [dummy_evaluator],
+        project_name="test-project",
+    )
+    exp_id, exp_run_name = llmobs._instance._create_experiment(
+        exp.name, exp._dataset._id, "test-project", exp._dataset._version, exp._config
+    )
+    assert exp_id is not None
+    assert exp_run_name.startswith("test_experiment")
