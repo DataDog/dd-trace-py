f4126372ff618f3d0daa9be026fa1aeed98f653f
Alberto Vara <alberto.vara@datadoghq.com>
2025-07-14 16:08:21 +0200

    fix(iast): ast patching error in mysqlsh

---
+++ tests/appsec/iast/_ast/test_corner_cases.py
@@ -0,0 +1,13 @@
+import pytest
+
+from tests.appsec.iast.iast_utils import _iast_patched_module
+
+
+mod = _iast_patched_module("tests.appsec.iast._ast.fixtures.del_variables_at_runtime")
+
+
+def test_string_proper_method_called():
+    """Fixed in ddtrace/appsec/_iast/_loader.py except TypeError:"""
+    with pytest.raises(TypeError):
+        mod = _iast_patched_module("tests.appsec.iast._ast.fixtures.del_variables_at_runtime")
+        assert mod
