a705cfc4b05b2b6911d8bc6a4428cf06fd52159a
brettlangdon <brett.langdon@datadoghq.com>
2025-07-15 12:04:24 -0400

    fix linting

--- ddtrace/_trace/processor/__init__.py
+++ ddtrace/_trace/processor/__init__.py
@@ -25,10 +25,10 @@ from ddtrace.internal.logger import get_logger
 from ddtrace.internal.sampling import SpanSamplingRule
 from ddtrace.internal.sampling import get_span_sampling_rules
 from ddtrace.internal.sampling import is_single_span_sampled
+from ddtrace.internal.service import ServiceStatusError
 from ddtrace.internal.telemetry.constants import TELEMETRY_LOG_LEVEL
 from ddtrace.internal.telemetry.constants import TELEMETRY_NAMESPACE
 from ddtrace.internal.writer import AgentResponse
-from ddtrace.internal.writer import AgentWriterInterface
 from ddtrace.internal.writer import create_trace_writer
 from ddtrace.settings._config import config
 from ddtrace.settings.asm import config as asm_config
