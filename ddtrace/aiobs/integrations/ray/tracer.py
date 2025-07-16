f9d8a435dcf536825f8f2d63955d63eb091e6a88
Imran Hendley <imran.hendley@datadoghq.com>
2025-07-16 13:37:56 -0400

    Add tracing hook for Ray ML framework

---
+++ ddtrace/aiobs/integrations/ray/tracer.py
@@ -0,0 +1,25 @@
+import os
+os.environ["DD_TRACE_OTEL_ENABLED"] = "true"
+
+from opentelemetry.trace import set_tracer_provider
+from ddtrace.opentelemetry import TracerProvider
+from ddtrace.trace import tracer
+from ddtrace.trace import TraceFilter
+
+
+class FilterShmilter(TraceFilter):
+    def process_trace(self, trace):
+        for span in trace:
+            span.span_type = "ray." + span.name
+            span.name = "ray.job"
+            span.service = os.environ.get("DD_SERVICE", "unspecified-ray-job")
+            span.set_metric("_dd.djm.enabled", 1)
+            span.set_metric("_dd.filter.kept", 1)
+            span.set_metric("_dd.measured", 1)
+            span.set_metric("_sampling_priority_v1", 2)
+        return trace
+
+
+def setup_tracing() -> None:
+    tracer.configure(trace_processors=[FilterShmilter()])
+    set_tracer_provider(TracerProvider())
