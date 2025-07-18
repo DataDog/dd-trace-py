9c8bb72f19ad53c2475381e6a484867b0e81d9e6
Munir Abdinur <munir_abdinur@hotmail.com>
2025-07-18 15:26:35 -0400

    fix failing test

--- tests/internal/test_module.py
+++ tests/internal/test_module.py
@@ -564,6 +564,7 @@ def test_public_modules_in_ddtrace_contrib():
     # The following modules contain attributes that are exposed to ddtrace users. All other modules in ddtrace.contrib
     # are internal.
     assert public_modules == {
+        'ddtrace.contrib.__init__',
         "ddtrace.contrib.trace_utils",
         "ddtrace.contrib.celery",
         "ddtrace.contrib.tornado",
@@ -571,6 +572,7 @@ def test_public_modules_in_ddtrace_contrib():
         "ddtrace.contrib.asgi",
         "ddtrace.contrib.bottle",
         "ddtrace.contrib.flask_cache",
+        'ddtrace.contrib.integration_registry.__init__',
         "ddtrace.contrib.integration_registry.mappings",
         "ddtrace.contrib.integration_registry.utils",
         "ddtrace.contrib.aiohttp",
