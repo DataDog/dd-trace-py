87af5879ad8cc32ecc9f61ba157a557428fabbf7
Jacob Simpher <jacob.simpher@datadoghq.com>
2025-07-18 13:28:25 -0400

    remove check to removed tag

--- ddtrace/contrib/internal/botocore/services/bedrock.py
+++ ddtrace/contrib/internal/botocore/services/bedrock.py
@@ -331,7 +331,10 @@ def _extract_streamed_response(ctx: core.ExecutionContext, streamed_body: List[D
         elif provider == _COHERE:
             if "is_finished" in streamed_body[0]:  # streamed response
                 if "index" in streamed_body[0]:  # n >= 2
-                    num_generations = int(ctx.get_item("num_generations") or 0)
+                    if ctx.get_item("llmobs.request_params") is not None:
+                        num_generations = int(ctx.get_item("llmobs.request_params")["n"] or 0)
+                    else:
+                        num_generations = 0
                     text = [
                         "".join([chunk["text"] for chunk in streamed_body[:-1] if chunk["index"] == i])
                         for i in range(num_generations)
