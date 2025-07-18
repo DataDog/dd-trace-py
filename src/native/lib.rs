cfc4e5e10bd8346301cac1f093b05c81a3587b53
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2025-07-18 17:31:44 +0200

    Add dummy function to test if .lib is generated.

--- src/native/lib.rs
+++ src/native/lib.rs
@@ -5,6 +5,10 @@ mod library_config;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
+/// Dummy function to check if imported lib is generated on windows builds.
+#[no_mangle]
+pub extern "C" fn ddtrace_force_export_for_windows() {}
+
 #[pymodule]
 fn _native(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<ddsketch::DDSketchPy>()?;
