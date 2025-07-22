7e95207631e2fe893987622b9735599ce2310cf4
Federico Mon <federico.mon@datadoghq.com>
2025-07-22 17:53:28 +0200

    fix bad merge

--- ddtrace/internal/writer/writer.py
+++ ddtrace/internal/writer/writer.py
@@ -419,26 +419,6 @@ class HTTPWriter(periodic.PeriodicService, TraceWriter):
                 self._metrics_dist("encoder.dropped.traces", n_traces)
                 return
 
-        for payload in encoded_traces:
-            encoded_data, n_traces = payload
-            self._flush_single_payload(encoded_data, n_traces, client=client, raise_exc=raise_exc)
-
-    def _flush_single_payload(
-        self, encoded: Optional[bytes], n_traces: int, client: WriterClientBase, raise_exc: bool = False
-    ) -> None:
-        if encoded is None:
-            return
-
-        # Should gzip the payload if intake accepts it
-        if self._intake_accepts_gzip:
-            original_size = len(encoded)
-            # Replace the value to send with the gzipped the value
-            encoded = gzip.compress(encoded, compresslevel=6)
-            log.debug("Original size in bytes: %s, Compressed size: %s", original_size, len(encoded))
-
-            # And add the header
-            self._headers["Content-Encoding"] = "gzip"
-
         try:
             self._send_payload_with_backoff(encoded, n_traces, client)
         except Exception:
