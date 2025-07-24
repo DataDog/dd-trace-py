2b27c98166abe04121d05634bcbc77190e3f9ca3
Imran Hendley <imran.hendley@datadoghq.com>
2025-07-24 13:45:38 -0400

    add ray tracing hook with ddtrace.auto import

---
+++ ddtrace/contrib/internal/ray/tracer.py
@@ -0,0 +1,26 @@
+import os
+
+
+os.environ["DD_TRACE_OTEL_ENABLED"] = "true"
+
+
+import ddtrace.auto
+from ddtrace.trace import TraceFilter
+from ddtrace.trace import tracer
+
+
+class RayTraceFilter(TraceFilter):
+    def process_trace(self, trace):
+        for span in trace:
+            span.span_type = "ray." + span.name
+            span.name = "ray.job"
+            span.service=os.environ.get("DD_SERVICE", "unspecified-ray-job")
+            span.set_metric("_dd.djm.enabled", 1)
+            span.set_metric("_dd.filter.kept", 1)
+            span.set_metric("_dd.measured", 1)
+            span.set_metric("_sampling_priority_v1", 2)
+        return trace
+
+
+def setup_tracing() -> None:
+    tracer.configure(trace_processors=[RayTraceFilter()])
