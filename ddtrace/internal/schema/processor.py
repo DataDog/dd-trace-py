4e0ef0989bda40dcd538b59a8f7d74b2b1baef8f
Zarir Hamza <zarir.hamza@datadoghq.com>
2025-07-24 16:50:40 -0400

    improve SLO

--- ddtrace/internal/schema/processor.py
+++ ddtrace/internal/schema/processor.py
@@ -9,11 +9,13 @@ from . import schematize_service_name
 class BaseServiceProcessor(TraceProcessor):
     def __init__(self):
         self._global_service = schematize_service_name((config.service or "").lower())
+        self._in_aws_lambda = in_aws_lambda()
 
+    
     def process_trace(self, trace):
         # AWS Lambda spans receive unhelpful base_service value of runtime
         # Remove base_service to prevent service overrides in Lambda spans
-        if not trace or in_aws_lambda():
+        if not trace or self._in_aws_lambda:
             return trace
 
         traces_to_process = filter(
