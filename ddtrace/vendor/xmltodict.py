027016ed061104e44b7057bcf525d45f5f3a812d
Christophe Papazian <christophe.papazian@datadoghq.com>
2025-07-25 11:50:45 +0200

    simplifying the parser for default settings and adding unit test

--- ddtrace/vendor/xmltodict.py
+++ ddtrace/vendor/xmltodict.py
@@ -15,115 +15,56 @@ class ParsingInterrupted(Exception):
 
 class _DictSAXHandler:
     def __init__(
-        self,
-        item_depth=0,
-        item_callback=lambda *args: True,
-        xml_attribs=True,
-        attr_prefix="@",
-        cdata_key="#text",
-        force_cdata=False,
-        cdata_separator="",
-        postprocessor=None,
-        dict_constructor=dict,
-        strip_whitespace=True,
-        namespace_separator=":",
-        namespaces=None,
-        force_list=None,
-        comment_key="#comment",
+        self
     ):
         self.path = []
         self.stack = []
         self.data = []
         self.item = None
-        self.item_depth = item_depth
-        self.xml_attribs = xml_attribs
-        self.item_callback = item_callback
-        self.attr_prefix = attr_prefix
-        self.cdata_key = cdata_key
-        self.force_cdata = force_cdata
-        self.cdata_separator = cdata_separator
-        self.postprocessor = postprocessor
-        self.dict_constructor = dict_constructor
-        self.strip_whitespace = strip_whitespace
-        self.namespace_separator = namespace_separator
-        self.namespaces = namespaces
-        self.namespace_declarations = dict_constructor()
-        self.force_list = force_list
-        self.comment_key = comment_key
-
-    def _build_name(self, full_name):
-        if self.namespaces is None:
-            return full_name
-        i = full_name.rfind(self.namespace_separator)
-        if i == -1:
-            return full_name
-        namespace, name = full_name[:i], full_name[i + 1 :]
-        try:
-            short_namespace = self.namespaces[namespace]
-        except KeyError:
-            short_namespace = namespace
-        if not short_namespace:
-            return name
-        else:
-            return self.namespace_separator.join((short_namespace, name))
-
-    def _attrs_to_dict(self, attrs):
-        if isinstance(attrs, dict):
-            return attrs
-        return self.dict_constructor(zip(attrs[0::2], attrs[1::2]))
+        self.attr_prefix = "@"
+        self.cdata_key = "#text"
+        self.cdata_separator = ""
+        self.namespace_separator = ":"
+        self.namespace_declarations = {}
+        self.comment_key = "#comment"
 
     def startNamespaceDecl(self, prefix, uri):
         self.namespace_declarations[prefix or ""] = uri
 
     def startElement(self, full_name, attrs):
-        name = self._build_name(full_name)
-        attrs = self._attrs_to_dict(attrs)
+        if not isinstance(attrs, dict):
+            attrs = dict(zip(attrs[0::2], attrs[1::2]))
         if attrs and self.namespace_declarations:
             attrs["xmlns"] = self.namespace_declarations
-            self.namespace_declarations = self.dict_constructor()
-        self.path.append((name, attrs or None))
-        if len(self.path) >= self.item_depth:
-            self.stack.append((self.item, self.data))
-            if self.xml_attribs:
-                attr_entries = []
-                for key, value in attrs.items():
-                    key = self.attr_prefix + self._build_name(key)
-                    if self.postprocessor:
-                        entry = self.postprocessor(self.path, key, value)
-                    else:
-                        entry = (key, value)
-                    if entry:
-                        attr_entries.append(entry)
-                attrs = self.dict_constructor(attr_entries)
-            else:
-                attrs = None
-            self.item = attrs or None
-            self.data = []
+            self.namespace_declarations = {}
+        self.path.append((full_name, attrs or None))
+        self.stack.append((self.item, self.data))
+        attr_entries = []
+        for key, value in attrs.items():
+            key = self.attr_prefix + key
+            attr_entries.append((key, value))
+        attrs = dict(attr_entries)
+        self.item = attrs or None
+        self.data = []
 
     def endElement(self, full_name):
-        name = self._build_name(full_name)
-        if len(self.path) == self.item_depth:
+        if len(self.path) == 0:
             item = self.item
             if item is None:
                 item = None if not self.data else self.cdata_separator.join(self.data)
 
-            should_continue = self.item_callback(self.path, item)
-            if not should_continue:
-                raise ParsingInterrupted
         if self.stack:
             data = None if not self.data else self.cdata_separator.join(self.data)
             item = self.item
             self.item, self.data = self.stack.pop()
-            if self.strip_whitespace and data:
+            if data:
                 data = data.strip() or None
-            if data and self.force_cdata and item is None:
-                item = self.dict_constructor()
             if item is not None:
                 if data:
                     self.push_data(item, self.cdata_key, data)
-                self.item = self.push_data(self.item, name, item)
+                self.item = self.push_data(self.item, full_name, item)
             else:
-                self.item = self.push_data(self.item, name, data)
+                self.item = self.push_data(self.item, full_name, data)
         else:
             self.item = None
             self.data = []
@@ -135,19 +76,9 @@ class _DictSAXHandler:
         else:
             self.data.append(data)
 
-    def comments(self, data):
-        if self.strip_whitespace:
-            data = data.strip()
-        self.item = self.push_data(self.item, self.comment_key, data)
-
     def push_data(self, item, key, data):
-        if self.postprocessor is not None:
-            result = self.postprocessor(self.path, key, data)
-            if result is None:
-                return item
-            key, data = result
         if item is None:
-            item = self.dict_constructor()
+            item = {}
         try:
             value = item[key]
             if isinstance(value, list):
@@ -155,26 +86,14 @@ class _DictSAXHandler:
             else:
                 item[key] = [value, data]
         except KeyError:
-            if self._should_force_list(key, data):
-                item[key] = [data]
-            else:
                 item[key] = data
         return item
 
-    def _should_force_list(self, key, value):
-        if not self.force_list:
-            return False
-        if isinstance(self.force_list, bool):
-            return self.force_list
-        try:
-            return key in self.force_list
-        except TypeError:
-            return self.force_list(self.path[:-1], key, value)
 
 
 def parse(xml_input: Union[bytes, str]):
     """Parse the given XML input and convert it into a dictionary."""
-    handler = _DictSAXHandler(namespace_separator=":")
+    handler = _DictSAXHandler()
     encoding = None
     if isinstance(xml_input, str):
         encoding = "utf-8"
