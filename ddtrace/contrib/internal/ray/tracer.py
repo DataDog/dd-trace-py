2b30a149f7f647856538eb649807c916858c68be
Imran Hendley <imran.hendley@datadoghq.com>
2025-07-25 13:37:26 -0400

    style

--- ddtrace/contrib/internal/ray/tracer.py
+++ ddtrace/contrib/internal/ray/tracer.py
@@ -10,7 +10,7 @@ class RayTraceFilter(TraceFilter):
         for span in trace:
             span.span_type = "ray." + span.name
             span.name = "ray.job"
-            span.service=os.environ.get("DD_SERVICE", "unspecified-ray-job")
+            span.service = os.environ.get("DD_SERVICE", "unspecified-ray-job")
             span.set_metric("_dd.djm.enabled", 1)
             span.set_metric("_dd.filter.kept", 1)
             span.set_metric("_dd.measured", 1)
