ddbc5ccd6c491c77014d9ae30d48cf451e8c8542
Munir Abdinur <munir_abdinur@hotmail.com>
2025-07-28 10:41:23 -0400

    perf(span_aggregator): move span processing outside of lock

--- ddtrace/_trace/processor/__init__.py
+++ ddtrace/_trace/processor/__init__.py
@@ -27,7 +27,6 @@ from ddtrace.internal.sampling import SpanSamplingRule
 from ddtrace.internal.sampling import get_span_sampling_rules
 from ddtrace.internal.sampling import is_single_span_sampled
 from ddtrace.internal.service import ServiceStatusError
-from ddtrace.internal.telemetry.constants import TELEMETRY_LOG_LEVEL
 from ddtrace.internal.telemetry.constants import TELEMETRY_NAMESPACE
 from ddtrace.internal.writer import AgentResponse
 from ddtrace.internal.writer import create_trace_writer
@@ -166,7 +165,6 @@ class TraceSamplingProcessor(TraceProcessor):
                     # When any span is marked as keep by a single span sampling
                     # decision then we still send all and only those spans.
                     single_spans = [_ for _ in trace if is_single_span_sampled(_)]
-
                     return single_spans or None
 
             # single span sampling rules are applied after trace sampling
@@ -207,6 +205,16 @@ class TopLevelSpanProcessor(SpanProcessor):
         # DEV: Update span after finished to avoid race condition
         if span._is_top_level:
             span.set_metric("_dd.top_level", 1)
+
+
+class ServiceNameProcessor(TraceProcessor):
+    """Processor that adds the service name to the globalconfig."""
+
+    def process_trace(self, trace: List[Span]) -> Optional[List[Span]]:
+        for span in trace:
+            if span.service:
+                config._add_extra_service(span.service)
+        return trace
 
 
 class TraceTagsProcessor(TraceProcessor):
@@ -277,6 +285,7 @@ class SpanAggregator(SpanProcessor):
         self.tags_processor = TraceTagsProcessor()
         self.dd_processors = dd_processors or []
         self.user_processors = user_processors or []
+        self.service_name_processor = ServiceNameProcessor()
         self.writer = create_trace_writer(response_callback=self._agent_response_callback)
         # Initialize the trace buffer and lock
         self._traces: DefaultDict[int, _Trace] = defaultdict(lambda: _Trace())
@@ -294,6 +303,7 @@ class SpanAggregator(SpanProcessor):
             f"{self.__class__.__name__}("
             f"{self.partial_flush_enabled}, "
             f"{self.partial_flush_min_spans}, "
+            f"{self.service_name_processor},"
             f"{self.sampling_processor},"
             f"{self.tags_processor},"
             f"{self.dd_processors}, "
@@ -310,80 +320,96 @@ class SpanAggregator(SpanProcessor):
 
             self._span_metrics["spans_created"][integration_name] += 1
             self._queue_span_count_metrics("spans_created", "integration_name")
+            log.debug(
+                "Span name='%s' id='%d' started and was added to the span aggregator. "
+                "Trace: %d: %d/%d spans finished",
+                span.name,
+                span.span_id,
+                span.trace_id,
+                trace.num_finished,
+                len(trace.spans),
+            )
 
     def on_span_finish(self, span: Span) -> None:
+        # Aqcuire lock to get finished and update trace.spans
         with self._lock:
             integration_name = span._meta.get(COMPONENT, span._span_api)
             self._span_metrics["spans_finished"][integration_name] += 1
 
-            # Calling finish on a span that we did not see the start for
-            # DEV: This can occur if the SpanAggregator is recreated while there is a span in progress
-            #      e.g. `tracer.configure()` is called after starting a span
             if span.trace_id not in self._traces:
-                log_msg = "finished span not connected to a trace"
-                telemetry.telemetry_writer.add_log(TELEMETRY_LOG_LEVEL.ERROR, log_msg)
-                log.debug("%s: %s", log_msg, span)
+                log.debug(
+                    "Span '%s' was started before tracing was enabled. "
+                    "Trace '%d' not found in the span aggregator. This span will be dropped.",
+                    span.name,
+                    span.trace_id,
+                )
                 return
 
             trace = self._traces[span.trace_id]
             trace.num_finished += 1
+
             should_partial_flush = self.partial_flush_enabled and trace.num_finished >= self.partial_flush_min_spans
-            if trace.num_finished == len(trace.spans) or should_partial_flush:
-                trace_spans = trace.spans
-                trace.spans = []
-                if trace.num_finished < len(trace_spans):
-                    finished = []
-                    for s in trace_spans:
-                        if s.finished:
-                            finished.append(s)
-                        else:
-                            trace.spans.append(s)
-                else:
-                    finished = trace_spans
-
-                num_finished = len(finished)
-                trace.num_finished -= num_finished
-                if trace.num_finished != 0:
-                    log_msg = "unexpected finished span count"
-                    telemetry.telemetry_writer.add_log(TELEMETRY_LOG_LEVEL.ERROR, log_msg)
-                    log.debug("%s (%s) for span %s", log_msg, num_finished, span)
-                    trace.num_finished = 0
-
-                # If we have removed all spans from this trace, then delete the trace from the traces dict
-                if len(trace.spans) == 0:
-                    del self._traces[span.trace_id]
-
-                # No spans to process, return early
-                if not finished:
-                    return
-
-                # Set partial flush tag on the first span
-                if should_partial_flush:
-                    log.debug("Partially flushing %d spans for trace %d", num_finished, span.trace_id)
-                    finished[0].set_metric("_dd.py.partial_flush", num_finished)
-
-                spans: Optional[List[Span]] = finished
-                for tp in chain(
-                    self.dd_processors, self.user_processors, [self.sampling_processor, self.tags_processor]
-                ):
-                    try:
-                        if spans is None:
-                            return
-                        spans = tp.process_trace(spans)
-                    except Exception:
-                        log.error("error applying processor %r", tp, exc_info=True)
-
-                self._queue_span_count_metrics("spans_finished", "integration_name")
-                if spans is not None:
-                    for span in spans:
-                        if span.service:
-                            # report extra service name as it may have been set after the span creation by the customer
-                            config._add_extra_service(span.service)
-                self.writer.write(spans)
+            is_trace_complete = trace.num_finished >= len(trace.spans)
+            if not is_trace_complete and not should_partial_flush:
                 return
 
-            log.debug("trace %d has %d spans, %d finished", span.trace_id, len(trace.spans), trace.num_finished)
-            return None
+            if not is_trace_complete:
+                finished = [s for s in trace.spans if s.finished]
+                if not finished:
+                    log.warning(
+                        "Programming Error: No finished spans to process for trace %d."
+                        "Please open a Github issue at https://github.com/DataDog/dd-trace-py/issues",
+                        span.trace_id,
+                    )
+                    return
+                trace.spans[:] = [s for s in trace.spans if not s.finished]  # In-place update
+                trace.num_finished = 0
+                log.debug(
+                    "Partial flush: %d finished, %d unfinished spans for trace %d",
+                    len(finished),
+                    len(trace.spans),
+                    span.trace_id,
+                )
+            else:
+                finished = trace.spans
+                del self._traces[span.trace_id]
+                log.debug("Complete trace: processing remaining %d spans for trace %d", len(finished), span.trace_id)
+
+        if should_partial_flush and finished:
+            # FIXME(munir): should_partial_flush should return false if all the spans in the trace are finished.
+            # For example if partial flushing min spans is 10 and the trace has 10 spans, the trace should
+            # not have a partial flush metric. This trace was processed in its entirety.
+            finished[0].set_metric("_dd.py.partial_flush", len(finished))
+
+        # perf: Process spans outside of the span aggregator lock
+        spans = finished
+        for tp in chain(
+            self.dd_processors,
+            self.user_processors,
+            [self.sampling_processor, self.tags_processor, self.service_name_processor],
+        ):
+            try:
+                initial_count = len(spans)
+                spans = tp.process_trace(spans) or []
+                if len(spans) != initial_count:
+                    log.debug(
+                        "Processor %s: %d → %d spans for trace %d",
+                        type(tp).__name__,
+                        initial_count,
+                        len(spans),
+                        span.trace_id,
+                    )
+                if not spans:
+                    return
+            except Exception:
+                log.error("error applying processor %r to trace %d", tp, span.trace_id, exc_info=True)
+
+        with self._lock:
+            self._queue_span_count_metrics("spans_finished", "integration_name")
+
+        if spans:
+            log.debug("Encoding %d spans for trace %d (local root: %s)", len(spans), span.trace_id, spans[0].name)
+            self.writer.write(spans)
 
     def _agent_response_callback(self, resp: AgentResponse) -> None:
         """Handle the response from the agent.
