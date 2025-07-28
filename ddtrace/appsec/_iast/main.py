0382a7fe989240480d355c53ff2dfb36d50d0783
Alberto Vara <alberto.vara@datadoghq.com>
2025-07-28 14:17:09 +0200

    force unload importlib if iast is enabled

--- ddtrace/appsec/_iast/main.py
+++ ddtrace/appsec/_iast/main.py
@@ -31,19 +31,7 @@ from ddtrace.appsec._iast.secure_marks.validators import ssrf_validator
 from ddtrace.appsec._iast.secure_marks.validators import unvalidated_redirect_validator
 
 
-IAST_PATCH = {
-    "code_injection": True,
-    "command_injection": True,
-    "header_injection": True,
-    "insecure_cookie": True,
-    "unvalidated_redirect": True,
-    "weak_cipher": True,
-    "weak_hash": True,
-    "xss": True,
-}
-
-
-def patch_iast(patch_modules=IAST_PATCH):
+def patch_iast():
     """Patch security-sensitive functions (sink points) for IAST analysis.
 
     This function implements the core IAST patching mechanism by:
@@ -63,21 +51,25 @@ def patch_iast(patch_modules=IAST_PATCH):
         are patched when they are first imported. This allows for lazy loading of
         security instrumentation.
     """
-    import importlib
+    from ddtrace.appsec._iast._patches.json_tainting import patch as json_tainting_patch
+    from ddtrace.appsec._iast.taint_sinks.code_injection import patch as code_injection_patch
+    from ddtrace.appsec._iast.taint_sinks.command_injection import patch as command_injection_patch
+    from ddtrace.appsec._iast.taint_sinks.header_injection import patch as header_injection_patch
+    from ddtrace.appsec._iast.taint_sinks.insecure_cookie import patch as insecure_cookie_patch
+    from ddtrace.appsec._iast.taint_sinks.unvalidated_redirect import patch as unvalidated_redirect_patch
+    from ddtrace.appsec._iast.taint_sinks.weak_cipher import patch as weak_cipher_patch
+    from ddtrace.appsec._iast.taint_sinks.weak_hash import patch as weak_hash_patch
+    from ddtrace.appsec._iast.taint_sinks.xss import patch as xss_patch
 
-    for module_name in (m for m, e in patch_modules.items() if e):
-        try:
-            # Import the taint sink module
-            module_path = f"ddtrace.appsec._iast.taint_sinks.{module_name}"
-            module = importlib.import_module(module_path)
-
-            # Check if the module has a patch function and call it
-            if hasattr(module, "patch") and callable(getattr(module, "patch")):
-                module.patch()
-        except ImportError:
-            pass
-    module = importlib.import_module("ddtrace.appsec._iast._patches.json_tainting")
-    module.patch()
+    code_injection_patch()
+    command_injection_patch()
+    header_injection_patch()
+    insecure_cookie_patch()
+    unvalidated_redirect_patch()
+    weak_cipher_patch()
+    weak_hash_patch()
+    xss_patch()
+    json_tainting_patch()
 
     iast_funcs = WrapFunctonsForIAST()
 
--- ddtrace/bootstrap/sitecustomize.py
+++ ddtrace/bootstrap/sitecustomize.py
@@ -43,17 +43,23 @@ if "gevent" in sys.modules or "gevent.monkey" in sys.modules:
 
 
 def cleanup_loaded_modules():
+    from ddtrace.settings.asm import config as asm_config
+
     def drop(module_name):
         # type: (str) -> None
-        del sys.modules[module_name]
+        try:
+            del sys.modules[module_name]
+        except KeyError:
+            pass
 
     MODULES_REQUIRING_CLEANUP = ("gevent",)
     do_cleanup = os.getenv("DD_UNLOAD_MODULES_FROM_SITECUSTOMIZE", default="auto").lower()
     if do_cleanup == "auto":
         do_cleanup = any(is_module_installed(m) for m in MODULES_REQUIRING_CLEANUP)
 
-    if not asbool(do_cleanup):
-        return
+    if not asm_config._iast_enabled:
+        if not asbool(do_cleanup):
+            return
 
     # Unload all the modules that we have imported, except for the ddtrace one.
     # NB: this means that every `import threading` anywhere in `ddtrace/` code
@@ -94,6 +100,9 @@ def cleanup_loaded_modules():
             # submodule makes use of threading so it is critical to unload when
             # gevent is used.
             "concurrent.futures",
+            "importlib_metadata",
+            "importlib.metadata",
+            "importlib",
         ]
     )
     for u in UNLOAD_MODULES:
