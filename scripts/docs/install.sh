d49a23d34e84aec6a0de2238e9767c8056af40a4
brettlangdon <brett.langdon@datadoghq.com>
2025-07-28 14:33:29 -0400

    ci: install libenchant in base image

--- scripts/docs/install.sh
+++ scripts/docs/install.sh
@@ -5,15 +5,8 @@ set -ex
 if [[ "${READTHEDOCS}" = "True" ]]; then
     # We skip here because we do not check spelling in RTD
     echo "Skipping install"
-else
-  if [[ "$(uname)" == "Darwin" ]]; then
-    brew install enchant
-  else
-    export DEBIAN_FRONTEND="noninteractive"
-    export DEBCONF_NOWARNINGS="yes"
+elif [[ "$(uname)" == "Darwin" ]]; then
+  brew install enchant
 
-    apt-get -qq update
-    apt-get -qy install --no-install-recommends libenchant-2-dev >/dev/null ||
-        apt-get -qy install --no-install-recommends libenchant-dev >/dev/null
-  fi
+# libenchant is already installed in our base dev image docker/Dockerfile
 fi
