be190efd1cae3734fc382124de9bcbb096aa363f
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-07-30 16:31:42 -0400

    intentionally break cache for stack_v2

--- ddtrace/internal/datadog/profiling/stack_v2/include/constants.hpp
+++ ddtrace/internal/datadog/profiling/stack_v2/include/constants.hpp
@@ -7,8 +7,8 @@ constexpr unsigned int g_default_sampling_period_us = 10000; // 100 Hz
 constexpr unsigned int g_min_sampling_period_us = g_default_sampling_period_us / 2;
 constexpr unsigned int g_max_sampling_period_us = 1000000;       // 1 seconds
 constexpr unsigned int g_adaptive_sampling_interval_us = 250000; // 250 ms
-constexpr double g_default_sampling_period_s = g_default_sampling_period_us / 1e6;
 constexpr double g_target_overhead = 0.01; // 1% overhead
+constexpr double g_default_sampling_period_s = g_default_sampling_period_us / 1e6;
 
 // Echion maintains a cache of frames--the size of this cache is specified up-front.
 constexpr unsigned int g_default_echion_frame_cache_size = 1024;
