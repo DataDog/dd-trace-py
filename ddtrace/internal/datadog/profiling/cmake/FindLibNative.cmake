83c9176e4396f0ba22828b2d3b2843ac914762f6
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-08-01 17:49:52 -0400

    format

--- ddtrace/internal/datadog/profiling/cmake/FindLibNative.cmake
+++ ddtrace/internal/datadog/profiling/cmake/FindLibNative.cmake
@@ -3,8 +3,7 @@ find_package(Python3)
 if(DEFINED NATIVE_EXTENSION_LOCATION)
     set(SOURCE_LIB_DIR ${NATIVE_EXTENSION_LOCATION})
 else()
-    set(SOURCE_LIB_DIR
-        ${CMAKE_SOURCE_DIR}/../../../../../src/native/target/release)
+    set(SOURCE_LIB_DIR ${CMAKE_SOURCE_DIR}/../../../../../src/native/target/release)
 endif()
 
 if(DEFINED EXTENSION_SUFFIX)
@@ -16,8 +15,7 @@ endif()
 message(WARNING "SOURCE_LIB_DIR: ${SOURCE_LIB_DIR}")
 message(WARNING "LIBRARY_NAME: ${LIBRARY_NAME}")
 
-set(SOURCE_INCLUDE_DIR
-    ${CMAKE_SOURCE_DIR}/../../../../../src/native/target/include)
+set(SOURCE_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/../../../../../src/native/target/include)
 
 set(DEST_LIB_DIR ${CMAKE_CURRENT_BINARY_DIR})
 set(DEST_INCLUDE_DIR ${DEST_LIB_DIR}/include)
