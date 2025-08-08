00f237f9bca7d17e0c1ec75f21a6cd87ea9819d0
Imran Hendley <imran.hendley@datadoghq.com>
2025-08-08 16:37:53 -0400

    add ray log handler

--- ddtrace/contrib/internal/ray/tracer.py
+++ ddtrace/contrib/internal/ray/tracer.py
@@ -1,11 +1,17 @@
+import logging
+import os
+
 from ddtrace import config
 import ddtrace.auto  # noqa: F401
 from ddtrace.constants import _DJM_ENABLED_KEY
 from ddtrace.constants import _FILTER_KEPT_KEY
 from ddtrace.constants import _SAMPLING_PRIORITY_KEY
 from ddtrace.constants import _SPAN_MEASURED_KEY
+from ddtrace.opentelemetry import TracerProvider
 from ddtrace.trace import TraceFilter
 from ddtrace.trace import tracer
+from opentelemetry.trace import set_tracer_provider
+from pythonjsonlogger import jsonlogger
 
 
 RAY_JOB_ID_TAG_KEY = "ray.job_id"
@@ -27,5 +33,21 @@ class RayTraceFilter(TraceFilter):
         return trace
 
 
+class CustomJsonFormatter(jsonlogger.JsonFormatter):  # pyright: ignore[reportPrivateImportUsage]
+        def add_fields(self, log_record, record, message_dict):
+            super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+
+            log_record['service'] = os.environ.get("DD_SERVICE", "unspecified-ray-job")
+
+
 def setup_tracing() -> None:
     tracer.configure(trace_processors=[RayTraceFilter()])
+    set_tracer_provider(TracerProvider())
+
+    for name in logging.root.manager.loggerDict.keys():
+        logger = logging.getLogger(name)
+        if name.startswith("ray"):
+            log_handler = logging.StreamHandler()
+            formatter = CustomJsonFormatter()
+            log_handler.setFormatter(formatter)
+            logger.addHandler(log_handler)
