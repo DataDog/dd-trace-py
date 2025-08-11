DataDog/dd-trace-py

commit d1bce1cd140e25c2b13ba5c4ad297899d0c95c09

chore(ci_visibility): test-level skipping support for itr (#14115)

ddtrace/internal/ci_visibility/api/_suite.py
@@ -4,6 +4,7 @@ from typing import List
 from typing import Optional
 
 from ddtrace.ext import test
+from ddtrace.ext.test_visibility._constants import ITR_SKIPPING_LEVEL
 from ddtrace.ext.test_visibility._test_visibility_base import TestId
 from ddtrace.ext.test_visibility._test_visibility_base import TestSuiteId
 from ddtrace.ext.test_visibility.status import TestSourceFileInfo
@@ -68,7 +69,9 @@ class TestVisibilitySuite(TestVisibilityParentItem[TestId, TestVisibilityTest], 
                 )
                 return
 
-        self.count_itr_skipped()
+        # Only count for suite-level skipping mode, not test-level
+        if self._session_settings.itr_test_skipping_level == ITR_SKIPPING_LEVEL.SUITE:
+            self.count_itr_skipped()
         self.mark_itr_skipped()
         self.finish()
 
@@ -82,7 +85,12 @@ class TestVisibilitySuite(TestVisibilityParentItem[TestId, TestVisibilityTest], 
         """Set suite-level tags based on ITR enablement status"""
         super()._set_itr_tags(itr_enabled)
 
-        if itr_enabled and self._session_settings.itr_correlation_id:
+        # Only set correlation ID on suites when in suite-level skipping mode
+        if (
+            itr_enabled
+            and self._session_settings.itr_correlation_id
+            and self._session_settings.itr_test_skipping_level == ITR_SKIPPING_LEVEL.SUITE
+        ):
             self.set_tag(ITR_CORRELATION_ID_TAG_NAME, self._session_settings.itr_correlation_id)
 
     def _telemetry_record_event_created(self):

ddtrace/internal/ci_visibility/api/_test.py
@@ -19,6 +19,7 @@ from ddtrace.internal.ci_visibility.api._base import TestVisibilityItemBase
 from ddtrace.internal.ci_visibility.api._base import TestVisibilitySessionSettings
 from ddtrace.internal.ci_visibility.api._coverage_data import TestVisibilityCoverageData
 from ddtrace.internal.ci_visibility.constants import BENCHMARK
+from ddtrace.internal.ci_visibility.constants import ITR_CORRELATION_ID_TAG_NAME
 from ddtrace.internal.ci_visibility.constants import RETRY_REASON
 from ddtrace.internal.ci_visibility.constants import TEST
 from ddtrace.internal.ci_visibility.constants import TEST_ATTEMPT_TO_FIX_PASSED
@@ -232,12 +233,13 @@ class TestVisibilityTest(TestVisibilityChildItem[TestId], TestVisibilityItemBase
 
         When skipping at the suite level, the counting only happens when suites are finished as ITR-skipped.
         """
-        if self._session_settings.itr_test_skipping_level == ITR_SKIPPING_LEVEL.TEST and self.parent is not None:
+        if self.parent is not None:
             self.parent.count_itr_skipped()
 
     def finish_itr_skipped(self) -> None:
         log.debug("Finishing Test Visibility test %s with ITR skipped", self)
-        self.count_itr_skipped()
+        if self._session_settings.itr_test_skipping_level == ITR_SKIPPING_LEVEL.TEST:
+            self.count_itr_skipped()
         self.mark_itr_skipped()
         self.finish_test(TestStatus.SKIP)
 
@@ -286,6 +288,18 @@ class TestVisibilityTest(TestVisibilityChildItem[TestId], TestVisibilityItemBase
 
     def has_failed_all_retries(self):
         return bool(self.get_tag(TEST_HAS_FAILED_ALL_RETRIES))
+
+    def _set_itr_tags(self, itr_enabled: bool) -> None:
+        """Set test-level ITR tags"""
+        super()._set_itr_tags(itr_enabled)
+
+        # Only set correlation ID on tests when in test-level skipping mode
+        if (
+            itr_enabled
+            and self._session_settings.itr_correlation_id
+            and self._session_settings.itr_test_skipping_level == ITR_SKIPPING_LEVEL.TEST
+        ):
+            self.set_tag(ITR_CORRELATION_ID_TAG_NAME, self._session_settings.itr_correlation_id)
 
     #
     # EFD (Early Flake Detection) functionality

ddtrace/contrib/internal/pytest/_plugin_v2.py
@@ -51,6 +51,7 @@ from ddtrace.ext.test_visibility.api import disable_test_visibility
 from ddtrace.ext.test_visibility.api import enable_test_visibility
 from ddtrace.ext.test_visibility.api import is_test_visibility_enabled
 from ddtrace.internal.ci_visibility.constants import SKIPPED_BY_ITR_REASON
+from ddtrace.internal.ci_visibility.service_registry import require_ci_visibility_service
 from ddtrace.internal.ci_visibility.telemetry.coverage import COVERAGE_LIBRARY
 from ddtrace.internal.ci_visibility.telemetry.coverage import record_code_coverage_empty
 from ddtrace.internal.ci_visibility.telemetry.coverage import record_code_coverage_finished
@@ -132,9 +133,21 @@ def _handle_itr_should_skip(item, test_id) -> bool:
 
     suite_id = test_id.parent_id
 
-    item_is_unskippable = InternalTestSuite.is_itr_unskippable(suite_id) or InternalTest.is_attempt_to_fix(test_id)
+    # Check what ITR skipping level is configured
+    ci_visibility_service = require_ci_visibility_service()
+    is_suite_skipping_mode = ci_visibility_service._suite_skipping_mode
 
-    if InternalTestSuite.is_itr_skippable(suite_id):
+    # Determine if the item should be skipped based on the skipping level
+    if is_suite_skipping_mode:
+        # Suite-level skipping: check if the suite is skippable
+        should_skip = InternalTestSuite.is_itr_skippable(suite_id)
+        item_is_unskippable = InternalTestSuite.is_itr_unskippable(suite_id) or InternalTest.is_attempt_to_fix(test_id)
+    else:
+        # Test-level skipping: check if the individual test is skippable
+        should_skip = InternalTest.is_itr_skippable(test_id)
+        item_is_unskippable = InternalTest.is_itr_unskippable(test_id) or InternalTest.is_attempt_to_fix(test_id)
+
+    if should_skip:
         if item_is_unskippable:
             # Marking the test as forced run also applies to its hierarchy
             InternalTest.mark_itr_forced_run(test_id)
@@ -243,7 +256,11 @@ def _pytest_load_initial_conftests_pre_yield(early_config, parser, args):
         return
 
     try:
-        dd_config.test_visibility.itr_skipping_level = ITR_SKIPPING_LEVEL.SUITE
+        dd_config.test_visibility.itr_skipping_level = (
+            ITR_SKIPPING_LEVEL.SUITE
+            if asbool(os.getenv("_DD_CIVISIBILITY_ITR_SUITE_MODE", True))
+            else ITR_SKIPPING_LEVEL.TEST
+        )
         enable_test_visibility(config=dd_config.pytest)
         if InternalTestSession.should_collect_coverage():
             workspace_path = InternalTestSession.get_workspace_path()

ddtrace/ext/test_visibility/__init__.py
@@ -13,9 +13,11 @@ from ddtrace.internal.utils.formats import asbool
 def _get_default_test_visibility_contrib_config() -> t.Dict[str, t.Any]:
     return dict(
         _default_service="default_test_visibility_service",
-        itr_skipping_level=ITR_SKIPPING_LEVEL.SUITE
-        if asbool(os.getenv("_DD_CIVISIBILITY_ITR_SUITE_MODE"))
-        else ITR_SKIPPING_LEVEL.TEST,
+        itr_skipping_level=(
+            ITR_SKIPPING_LEVEL.SUITE
+            if asbool(os.getenv("_DD_CIVISIBILITY_ITR_SUITE_MODE", True))
+            else ITR_SKIPPING_LEVEL.TEST
+        ),
         _itr_skipping_ignore_parameters=False,
     )
 

tests/ci_visibility/api/fake_runner_all_itr_skip_test_level.py
@@ -93,5 +93,8 @@ def main():
 
 if __name__ == "__main__":
     freeze_support()
+    import os
+
+    os.environ["_DD_CIVISIBILITY_ITR_SUITE_MODE"] = "0"
     with mock.patch("ddtrace.internal.ci_visibility.CIVisibility.is_itr_enabled", return_value=True):
         main()

commit 410e991847db94ea495ef52eba95f6216e897fa2

chore(ci_visibility): itr skipped count fix (#14257)
CI Visibility: Fix for a bug introduced in
https://github.com/DataDog/dd-trace-py/pull/14115 where session tags
would not have the proper count of tests skipped by ITR, counting the
suite number instead when ITR is configured at suite level skipping.

Regression tests added for both suite level skipping and test level
skipping.

## Checklist
- [x] PR author has checked that all the criteria below are met
- The PR description includes an overview of the change
- The PR description articulates the motivation for the change
- The change includes tests OR the PR description describes a testing
strategy
- The PR description notes risks associated with the change, if any
- Newly-added code is easy to change
- The change follows the [library release note
guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
- The change includes or references documentation updates if necessary
- Backport labels are set (if
[applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))

## Reviewer Checklist
- [x] Reviewer has checked that all the criteria below are met 
- Title is accurate
- All changes are related to the pull request's stated goal
- Avoids breaking
[API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
changes
- Testing strategy adequately addresses listed risks
- Newly-added code is easy to change
- Release note makes sense to a user of the library
- If necessary, author has acknowledged and discussed the performance
implications of this PR as reported in the benchmarks PR comment
- Backport labels are set in a manner that is consistent with the
[release branch maintenance
policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

ddtrace/internal/ci_visibility/api/_suite.py
@@ -69,9 +69,6 @@ class TestVisibilitySuite(TestVisibilityParentItem[TestId, TestVisibilityTest], 
                 )
                 return
 
-        # Only count for suite-level skipping mode, not test-level
-        if self._session_settings.itr_test_skipping_level == ITR_SKIPPING_LEVEL.SUITE:
-            self.count_itr_skipped()
         self.mark_itr_skipped()
         self.finish()
 

ddtrace/internal/ci_visibility/api/_test.py
@@ -260,8 +260,7 @@ class TestVisibilityTest(TestVisibilityChildItem[TestId], TestVisibilityItemBase
 
     def finish_itr_skipped(self) -> None:
         log.debug("Finishing Test Visibility test %s with ITR skipped", self)
-        if self._session_settings.itr_test_skipping_level == ITR_SKIPPING_LEVEL.TEST:
-            self.count_itr_skipped()
+        self.count_itr_skipped()
         self.mark_itr_skipped()
         self.finish_test(TestStatus.SKIP)
 

tests/contrib/pytest/test_pytest.py
@@ -1429,8 +1429,12 @@ class PytestTestCase(PytestTestCaseBase):
         os.chdir(str(package_a_dir))
         with open("test_a.py", "w+") as fd:
             fd.write(
-                """def test_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_ok():
+                    assert True
+                """
+                )
             )
         self.testdir.chdir()
         self.inline_run("--ddtrace")
@@ -1456,15 +1460,23 @@ class PytestTestCase(PytestTestCaseBase):
         os.chdir(str(package_a_dir))
         with open("test_a.py", "w+") as fd:
             fd.write(
-                """def test_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_ok():
+                    assert True
+                """
+                )
             )
         package_b_dir = self.testdir.mkpydir("test_package_b")
         os.chdir(str(package_b_dir))
         with open("test_b.py", "w+") as fd:
             fd.write(
-                """def test_not_ok():
-                assert 0"""
+                textwrap.dedent(
+                    """
+                def test_not_ok():
+                    assert 0
+                """
+                )
             )
         self.testdir.chdir()
         self.inline_run("--ddtrace")
@@ -1494,12 +1506,20 @@ class PytestTestCase(PytestTestCaseBase):
         os.chdir(str(package_a_dir))
         with open("test_a.py", "w+") as fd:
             fd.write(
-                "def test_ok():\n\tassert True\n"
-                "class TestClassOuter:\n"
-                "\tclass TestClassInner:\n"
-                "\t\tdef test_class_inner(self):\n\t\t\tassert True\n"
-                "\tdef test_class_outer(self):\n\t\tassert True\n"
-                "def test_after_class():\n\tassert True"
+                textwrap.dedent(
+                    """
+                def test_ok():
+                    assert True
+                class TestClassOuter:
+                    class TestClassInner:
+                        def test_class_inner(self):
+                            assert True
+                    def test_class_outer(self):
+                        assert True
+                def test_after_class():
+                    assert True
+                """
+                )
             )
         self.testdir.chdir()
         rec = self.inline_run("--ddtrace")
@@ -1519,15 +1539,23 @@ class PytestTestCase(PytestTestCaseBase):
         os.chdir(str(package_a_dir))
         with open("test_a.py", "w+") as fd:
             fd.write(
-                """def test_not_ok():
-                assert 0"""
+                textwrap.dedent(
+                    """
+                def test_not_ok():
+                    assert 0
+                """
+                )
             )
         package_b_dir = self.testdir.mkpydir("test_package_b")
         os.chdir(str(package_b_dir))
         with open("test_b.py", "w+") as fd:
             fd.write(
-                """def test_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_ok():
+                    assert True
+                """
+                )
             )
         self.testdir.chdir()
         self.inline_run("--ignore=test_package_a", "--ddtrace")
@@ -2061,8 +2089,12 @@ class PytestTestCase(PytestTestCaseBase):
             pass
         with open("test_inner_abc.py", "w+") as fd:
             fd.write(
-                """def test_inner_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_inner_ok():
+                    assert True
+                """
+                )
             )
         with open("test_inner_class_abc.py", "w+") as fd:
             fd.write(
@@ -2155,8 +2187,12 @@ class PytestTestCase(PytestTestCaseBase):
         os.chdir(str(package_outer_dir))
         with open("test_outer_abc.py", "w+") as fd:
             fd.write(
-                """def test_outer_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_outer_ok():
+                    assert True
+                """
+                )
             )
         os.mkdir("test_inner_package")
         os.chdir("test_inner_package")
@@ -2164,8 +2200,12 @@ class PytestTestCase(PytestTestCaseBase):
             pass
         with open("test_inner_abc.py", "w+") as fd:
             fd.write(
-                """def test_inner_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_inner_ok():
+                    assert True
+                """
+                )
             )
         self.testdir.chdir()
 
@@ -2247,8 +2287,12 @@ class PytestTestCase(PytestTestCaseBase):
         os.chdir(str(package_outer_dir))
         with open("test_outer_abc.py", "w+") as fd:
             fd.write(
-                """def test_outer_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_outer_ok():
+                    assert True
+                """
+                )
             )
         os.mkdir("test_inner_package")
         os.chdir("test_inner_package")
@@ -2256,8 +2300,12 @@ class PytestTestCase(PytestTestCaseBase):
             pass
         with open("test_inner_abc.py", "w+") as fd:
             fd.write(
-                """def test_inner_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_inner_ok():
+                    assert True
+                """
+                )
             )
         self.testdir.chdir()
         with mock.patch(
@@ -2306,8 +2354,12 @@ class PytestTestCase(PytestTestCaseBase):
         os.chdir(str(package_outer_dir))
         with open("test_outer_abc.py", "w+") as fd:
             fd.write(
-                """def test_outer_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_outer_ok():
+                    assert True
+                """
+                )
             )
         os.mkdir("test_inner_package")
         os.chdir("test_inner_package")
@@ -2315,8 +2367,12 @@ class PytestTestCase(PytestTestCaseBase):
             pass
         with open("test_inner_abc.py", "w+") as fd:
             fd.write(
-                """def test_inner_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_inner_ok():
+                    assert True
+                """
+                )
             )
         self.testdir.chdir()
         with mock.patch(
@@ -2373,8 +2429,12 @@ class PytestTestCase(PytestTestCaseBase):
         os.chdir(str(package_outer_dir))
         with open("test_outer_abc.py", "w+") as fd:
             fd.write(
-                """def test_outer_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_outer_ok():
+                    assert True
+                """
+                )
             )
         os.mkdir("test_inner_package")
         os.chdir("test_inner_package")
@@ -2382,8 +2442,12 @@ class PytestTestCase(PytestTestCaseBase):
             pass
         with open("test_inner_abc.py", "w+") as fd:
             fd.write(
-                """def test_inner_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_inner_ok():
+                    assert True
+                """
+                )
             )
         self.testdir.chdir()
         with mock.patch(
@@ -2436,6 +2500,168 @@ class PytestTestCase(PytestTestCaseBase):
         for skipped_test_span in skipped_test_spans:
             assert skipped_test_span.get_tag("test.skipped_by_itr") == "true"
 
+    def test_pytest_test_level_skipping_counts_tests_not_suites(self):
+        """
+        Regression test for test level skipping count bug.
+
+        When ITR is enabled at suite level and suites are skipped, the `itr.tests_skipping.count` tag
+        should count the number of tests that were skipped (contained within those suites).
+
+        This test creates 2 suites with multiple tests each (4 tests total), expects all suites to be
+        skipped, and verifies that the count reflects the number of tests (4), not suites (2).
+        """
+        package_outer_dir = self.testdir.mkpydir("test_outer_package")
+        os.chdir(str(package_outer_dir))
+        with open("test_outer_abc.py", "w+") as fd:
+            fd.write(
+                textwrap.dedent(
+                    """
+                def test_outer_1():
+                    assert True
+
+                def test_outer_2():
+                    assert True
+                """
+                )
+            )
+        os.mkdir("test_inner_package")
+        os.chdir("test_inner_package")
+        with open("__init__.py", "w+"):
+            pass
+        with open("test_inner_abc.py", "w+") as fd:
+            fd.write(
+                textwrap.dedent(
+                    """
+                def test_inner_1():
+                    assert True
+
+                def test_inner_2():
+                    assert True
+                """
+                )
+            )
+        self.testdir.chdir()
+
+        with mock.patch(
+            "ddtrace.internal.ci_visibility.recorder.CIVisibility.test_skipping_enabled",
+            return_value=True,
+        ), mock.patch(
+            "ddtrace.internal.ci_visibility.recorder.CIVisibility.is_itr_enabled",
+            return_value=True,
+        ), mock.patch(
+            "ddtrace.internal.ci_visibility.recorder.CIVisibility._fetch_tests_to_skip"
+        ), mock.patch(
+            "ddtrace.internal.ci_visibility.recorder.CIVisibility.is_item_itr_skippable", return_value=True
+        ), mock.patch(
+            "ddtrace.internal.ci_visibility.recorder.ddconfig",
+            _get_default_civisibility_ddconfig(ITR_SKIPPING_LEVEL.TEST),
+        ):
+            self.inline_run("--ddtrace")
+
+        spans = self.pop_spans()
+        assert len(spans) == 9  # 1 session + 2 modules + 2 suites + 4 tests
+
+        # Verify session span tags
+        session_span = _get_spans_from_list(spans, "session")[0]
+        assert session_span.get_tag("test.itr.tests_skipping.enabled") == "true"
+        assert session_span.get_tag("test.itr.tests_skipping.tests_skipped") == "true"
+        assert session_span.get_tag("_dd.ci.itr.tests_skipped") == "true"
+        assert session_span.get_tag("test.itr.tests_skipping.type") == "test"
+
+        # This is the regression test: should count tests (4), not suites (2)
+        expected_test_count = 4  # 4 individual tests were skipped
+        actual_count = session_span.get_metric("test.itr.tests_skipping.count")
+        assert (
+            actual_count == expected_test_count
+        ), f"Expected {expected_test_count} tests skipped but got {actual_count}"
+
+        # Verify all test spans were skipped by ITR
+        skipped_test_spans = [x for x in spans if x.get_tag("test.status") == "skip" and x.get_tag("type") == "test"]
+        assert len(skipped_test_spans) == 4
+        for skipped_test_span in skipped_test_spans:
+            assert skipped_test_span.get_tag("test.skipped_by_itr") == "true"
+
+    def test_pytest_suite_level_skipping_counts_tests_not_suites(self):
+        """
+        Regression test for suite level skipping count bug.
+
+        When ITR is enabled at suite level and suites are skipped, the `itr.tests_skipping.count` tag
+        should count the number of tests that were skipped (contained within those suites).
+
+        This test creates 2 suites with multiple tests each (4 tests total), expects all suites to be
+        skipped, and verifies that the count reflects the number of tests (4), not suites (2).
+        """
+        package_outer_dir = self.testdir.mkpydir("test_outer_package")
+        os.chdir(str(package_outer_dir))
+        with open("test_outer_abc.py", "w+") as fd:
+            fd.write(
+                textwrap.dedent(
+                    """
+                def test_outer_1():
+                    assert True
+
+                def test_outer_2():
+                    assert True
+                """
+                )
+            )
+        os.mkdir("test_inner_package")
+        os.chdir("test_inner_package")
+        with open("__init__.py", "w+"):
+            pass
+        with open("test_inner_abc.py", "w+") as fd:
+            fd.write(
+                textwrap.dedent(
+                    """
+                def test_inner_1():
+                    assert True
+
+                def test_inner_2():
+                    assert True
+                """
+                )
+            )
+        self.testdir.chdir()
+
+        with mock.patch(
+            "ddtrace.internal.ci_visibility.recorder.CIVisibility.test_skipping_enabled",
+            return_value=True,
+        ), mock.patch(
+            "ddtrace.internal.ci_visibility.recorder.CIVisibility.is_itr_enabled",
+            return_value=True,
+        ), mock.patch(
+            "ddtrace.internal.ci_visibility.recorder.CIVisibility._fetch_tests_to_skip"
+        ), mock.patch(
+            "ddtrace.internal.ci_visibility.recorder.CIVisibility.is_item_itr_skippable", return_value=True
+        ), mock.patch(
+            "ddtrace.internal.ci_visibility.recorder.ddconfig",
+            _get_default_civisibility_ddconfig(ITR_SKIPPING_LEVEL.SUITE),
+        ):
+            self.inline_run("--ddtrace")
+
+        spans = self.pop_spans()
+        assert len(spans) == 9  # 1 session + 2 modules + 2 suites + 4 tests
+
+        # Verify session span tags
+        session_span = _get_spans_from_list(spans, "session")[0]
+        assert session_span.get_tag("test.itr.tests_skipping.enabled") == "true"
+        assert session_span.get_tag("test.itr.tests_skipping.tests_skipped") == "true"
+        assert session_span.get_tag("_dd.ci.itr.tests_skipped") == "true"
+        assert session_span.get_tag("test.itr.tests_skipping.type") == "suite"
+
+        # This is the regression test: should count tests (4), not suites (2)
+        expected_test_count = 4  # 4 individual tests were skipped
+        actual_count = session_span.get_metric("test.itr.tests_skipping.count")
+        assert (
+            actual_count == expected_test_count
+        ), f"Expected {expected_test_count} tests skipped but got {actual_count}"
+
+        # Verify all test spans were skipped by ITR
+        skipped_test_spans = [x for x in spans if x.get_tag("test.status") == "skip" and x.get_tag("type") == "test"]
+        assert len(skipped_test_spans) == 4
+        for skipped_test_span in skipped_test_spans:
+            assert skipped_test_span.get_tag("test.skipped_by_itr") == "true"
+
     def test_pytest_skip_none_test_suites(self):
         """
         Test that running pytest on two nested packages with 1 test each. It should generate
@@ -2446,8 +2672,12 @@ class PytestTestCase(PytestTestCaseBase):
         os.chdir(str(package_outer_dir))
         with open("test_outer_abc.py", "w+") as fd:
             fd.write(
-                """def test_outer_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_outer_ok():
+                    assert True
+                """
+                )
             )
         os.mkdir("test_inner_package")
         os.chdir("test_inner_package")
@@ -2455,8 +2685,12 @@ class PytestTestCase(PytestTestCaseBase):
             pass
         with open("test_inner_abc.py", "w+") as fd:
             fd.write(
-                """def test_inner_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_inner_ok():
+                    assert True
+                """
+                )
             )
         self.testdir.chdir()
         with mock.patch(
@@ -2505,8 +2739,12 @@ class PytestTestCase(PytestTestCaseBase):
         os.chdir(str(package_outer_dir))
         with open("test_outer_abc.py", "w+") as fd:
             fd.write(
-                """def test_outer_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_outer_ok():
+                    assert True
+                """
+                )
             )
         os.mkdir("test_inner_package")
         os.chdir("test_inner_package")
@@ -2514,8 +2752,12 @@ class PytestTestCase(PytestTestCaseBase):
             pass
         with open("test_inner_abc.py", "w+") as fd:
             fd.write(
-                """def test_inner_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_inner_ok():
+                    assert True
+                """
+                )
             )
         self.testdir.chdir()
         with mock.patch("ddtrace.internal.ci_visibility.recorder.CIVisibility._fetch_tests_to_skip"):
@@ -2550,8 +2792,12 @@ class PytestTestCase(PytestTestCaseBase):
         os.chdir(str(package_outer_dir))
         with open("test_outer_abc.py", "w+") as fd:
             fd.write(
-                """def test_outer_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_outer_ok():
+                    assert True
+                """
+                )
             )
         os.mkdir("test_inner_package")
         os.chdir("test_inner_package")
@@ -2559,8 +2805,12 @@ class PytestTestCase(PytestTestCaseBase):
             pass
         with open("test_inner_abc.py", "w+") as fd:
             fd.write(
-                """def test_inner_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_inner_ok():
+                    assert True
+                """
+                )
             )
         self.testdir.chdir()
 
@@ -2614,8 +2864,12 @@ class PytestTestCase(PytestTestCaseBase):
         os.chdir(str(package_outer_dir))
         with open("test_outer_abc.py", "w+") as fd:
             fd.write(
-                """def test_outer_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_outer_ok():
+                    assert True
+                """
+                )
             )
         os.mkdir("test_inner_package")
         os.chdir("test_inner_package")
@@ -2623,8 +2877,12 @@ class PytestTestCase(PytestTestCaseBase):
             pass
         with open("test_inner_abc.py", "w+") as fd:
             fd.write(
-                """def test_inner_ok():
-                assert True"""
+                textwrap.dedent(
+                    """
+                def test_inner_ok():
+                    assert True
+                    """
+                )
             )
         self.testdir.chdir()
 