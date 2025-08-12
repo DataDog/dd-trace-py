5934a94844caf461cfcbd9976a76a99b67c862c4
Yun Kim <35776586+Yun-Kim@users.noreply.github.com>
2025-08-12 12:55:39 -0400

    feat(crewai): support tracing crewAI flows (#14082)
    [MLOB-2806]
    
    This PR adds support for APM and LLMObs tracing of CrewAI flows,
    including the overall flow execution and individual start/listener
    method execution, as well as span linking of which methods triggered
    other methods in the flow (includes support for linking conditional
    AND/OR and router method triggers).
    
    (Due to some type hinting issues, I've removed `CrewAIIntegration`
    import from `llmobs/_integrations/__init__.py`. Python 3.8 has issues
    with subscripting WeakKeyDictionary, but CrewAI (and also LangGraph)
    only run on Python 3.9/3.10+ so we didn't know this was an issue until
    now)
    
    Note (assist for reading test assertions): span linking/event tests in
    `test_crewai_llmobs.py` are based on a complex flow that looks like
    this:
    <img width="974" height="627" alt="Screenshot 2025-07-30 at 4 23 59 PM"
    src="https://github.com/user-attachments/assets/c784fe1b-6574-4735-a632-ec63a245ffc9"
    />
    
    Additional note: tested
    [manually](https://dd.datad0g.com/llm/traces?query=%40ml_app%3Aml-app%20%40event_type%3Aspan%20%40parent_id%3Aundefined&agg_m=count&agg_m_source=base&agg_t=count&fromUser=false&llmPanels=%5B%7B%22t%22%3A%22sampleDetailPanel%22%2C%22rEID%22%3A%22AwAAAZhc-Jpa5Z3YfgAAABhBWmhjLUpwYUFBRHBkZlpaLTBvekFBQUEAAAAkMDE5ODVjZjktYTNkYS00ZGEzLWE3NGYtOTQ1NmI4YWU2Mjg1AAAAfQ%22%7D%5D&spanId=14559885504901134809&start=1753820708729&end=1753907108729&paused=false)
    and verified that crews are compatible with being run inside flows (as
    is a documented use case in CrewAI):
    <img width="707" height="685" alt="Screenshot 2025-07-30 at 4 25 48 PM"
    src="https://github.com/user-attachments/assets/cc694c63-e043-4d36-a58d-5d65b31616cc"
    />
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    
    
    [MLOB-2806]:
    https://datadoghq.atlassian.net/browse/MLOB-2806?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- ddtrace/contrib/internal/crewai/patch.py
+++ ddtrace/contrib/internal/crewai/patch.py
@@ -7,12 +7,17 @@ from ddtrace import config
 from ddtrace.contrib.internal.trace_utils import unwrap
 from ddtrace.contrib.internal.trace_utils import with_traced_module
 from ddtrace.contrib.internal.trace_utils import wrap
-from ddtrace.llmobs._integrations import CrewAIIntegration
+from ddtrace.internal.logger import get_logger
+from ddtrace.internal.utils import get_argument_value
+from ddtrace.llmobs._integrations.crewai import CrewAIIntegration
 from ddtrace.trace import Pin
 
 
 def get_version() -> str:
     return getattr(crewai, "__version__", "")
+
+
+logger = get_logger(__name__)
 
 
 config._add("crewai", {})
@@ -24,7 +29,7 @@ def _supported_versions() -> Dict[str, str]:
 
 @with_traced_module
 def traced_kickoff(crewai, pin, func, instance, args, kwargs):
-    integration = crewai._datadog_integration
+    integration: CrewAIIntegration = crewai._datadog_integration
     result = None
     instance_id = getattr(instance, "id", "")
     planning_enabled = getattr(instance, "planning", False)
@@ -43,7 +48,7 @@ def traced_kickoff(crewai, pin, func, instance, args, kwargs):
         span.set_exc_info(*sys.exc_info())
         raise
     finally:
-        kwargs["instance"] = instance
+        kwargs["_dd.instance"] = instance
         integration.llmobs_set_tags(span, args=args, kwargs=kwargs, response=result, operation="crew")
         span.finish()
     return result
@@ -51,7 +56,7 @@ def traced_kickoff(crewai, pin, func, instance, args, kwargs):
 
 @with_traced_module
 def traced_task_execute(crewai, pin, func, instance, args, kwargs):
-    integration = crewai._datadog_integration
+    integration: CrewAIIntegration = crewai._datadog_integration
     result = None
     span = integration.trace(
         pin,
@@ -70,7 +75,7 @@ def traced_task_execute(crewai, pin, func, instance, args, kwargs):
     finally:
         if getattr(instance, "_ddtrace_ctx", None):
             delattr(instance, "_ddtrace_ctx")
-        kwargs["instance"] = instance
+        kwargs["_dd.instance"] = instance
         integration.llmobs_set_tags(span, args=args, kwargs=kwargs, response=result, operation="task")
         span.finish()
     return result
@@ -78,7 +83,7 @@ def traced_task_execute(crewai, pin, func, instance, args, kwargs):
 
 @with_traced_module
 def traced_task_execute_async(crewai, pin, func, instance, args, kwargs):
-    integration = crewai._datadog_integration
+    integration: CrewAIIntegration = crewai._datadog_integration
     _ddtrace_ctx = integration._get_current_ctx(pin)
     setattr(instance, "_ddtrace_ctx", _ddtrace_ctx)
     return func(*args, **kwargs)
@@ -86,7 +91,7 @@ def traced_task_execute_async(crewai, pin, func, instance, args, kwargs):
 
 @with_traced_module
 def traced_task_get_context(crewai, pin, func, instance, args, kwargs):
-    integration = crewai._datadog_integration
+    integration: CrewAIIntegration = crewai._datadog_integration
     span = pin.tracer.current_span()
     result = func(*args, **kwargs)
     integration._llmobs_set_span_link_on_task(span, args, kwargs)
@@ -95,7 +100,7 @@ def traced_task_get_context(crewai, pin, func, instance, args, kwargs):
 
 @with_traced_module
 def traced_agent_execute(crewai, pin, func, instance, args, kwargs):
-    integration = crewai._datadog_integration
+    integration: CrewAIIntegration = crewai._datadog_integration
     result = None
     span = integration.trace(
         pin, "CrewAI Agent", span_name=getattr(instance, "role", ""), operation="agent", submit_to_llmobs=True
@@ -106,7 +111,7 @@ def traced_agent_execute(crewai, pin, func, instance, args, kwargs):
         span.set_exc_info(*sys.exc_info())
         raise
     finally:
-        kwargs["instance"] = instance
+        kwargs["_dd.instance"] = instance
         integration.llmobs_set_tags(span, args=args, kwargs=kwargs, response=result, operation="agent")
         span.finish()
     return result
@@ -114,7 +119,7 @@ def traced_agent_execute(crewai, pin, func, instance, args, kwargs):
 
 @with_traced_module
 def traced_tool_run(crewai, pin, func, instance, args, kwargs):
-    integration = crewai._datadog_integration
+    integration: CrewAIIntegration = crewai._datadog_integration
     result = None
     span = integration.trace(
         pin, "CrewAI Tool", span_name=getattr(instance, "name", ""), operation="tool", submit_to_llmobs=True
@@ -125,9 +130,53 @@ def traced_tool_run(crewai, pin, func, instance, args, kwargs):
         span.set_exc_info(*sys.exc_info())
         raise
     finally:
-        kwargs["instance"] = instance
+        kwargs["_dd.instance"] = instance
         integration.llmobs_set_tags(span, args=args, kwargs=kwargs, response=result, operation="tool")
         span.finish()
+    return result
+
+
+@with_traced_module
+async def traced_flow_kickoff(crewai, pin, func, instance, args, kwargs):
+    integration: CrewAIIntegration = crewai._datadog_integration
+    span_name = getattr(type(instance), "__name__", "CrewAI Flow")
+    with integration.trace(pin, "CrewAI Flow", span_name=span_name, operation="flow", submit_to_llmobs=True) as span:
+        result = await func(*args, **kwargs)
+        integration.llmobs_set_tags(span, args=args, kwargs=kwargs, response=result, operation="flow")
+        return result
+
+
+@with_traced_module
+async def traced_flow_method(crewai, pin, func, instance, args, kwargs):
+    integration: CrewAIIntegration = crewai._datadog_integration
+    span_name = get_argument_value(args, kwargs, 0, "method_name", optional=True) or "Flow Method"
+    with integration.trace(
+        pin,
+        "CrewAI Flow Method",
+        span_name=span_name,
+        operation="flow_method",
+        submit_to_llmobs=True,
+        flow_instance=instance,
+    ) as span:
+        flow_state = getattr(instance, "state", {})
+        initial_flow_state = {}
+        if isinstance(flow_state, dict):
+            initial_flow_state = {**flow_state}
+        elif hasattr(flow_state, "model_dump"):
+            initial_flow_state = flow_state.model_dump()
+        result = await func(*args, **kwargs)
+        kwargs["_dd.instance"] = instance
+        kwargs["_dd.initial_flow_state"] = initial_flow_state
+        integration.llmobs_set_tags(span, args=args, kwargs=kwargs, response=result, operation="flow_method")
+        return result
+
+
+@with_traced_module
+def patched_find_triggered_methods(crewai, pin, func, instance, args, kwargs):
+    integration: CrewAIIntegration = crewai._datadog_integration
+    result = func(*args, **kwargs)
+    current_span = pin.tracer.current_span()
+    integration.llmobs_set_span_links_on_flow(current_span, args, kwargs, instance)
     return result
 
 
@@ -138,15 +187,21 @@ def patch():
     crewai._datadog_patch = True
 
     Pin().onto(crewai)
-    integration = CrewAIIntegration(integration_config=config.crewai)
+    integration: CrewAIIntegration = CrewAIIntegration(integration_config=config.crewai)
     crewai._datadog_integration = integration
 
     wrap(crewai, "Crew.kickoff", traced_kickoff(crewai))
-    wrap(crewai, "Crew._get_context", traced_task_get_context(crewai))
-    wrap(crewai, "Task._execute_core", traced_task_execute(crewai))
     wrap(crewai, "Task.execute_async", traced_task_execute_async(crewai))
     wrap(crewai, "Agent.execute_task", traced_agent_execute(crewai))
     wrap(crewai.tools.structured_tool, "CrewStructuredTool.invoke", traced_tool_run(crewai))
+    wrap(crewai, "Flow.kickoff_async", traced_flow_kickoff(crewai))
+    try:
+        wrap(crewai, "Crew._get_context", traced_task_get_context(crewai))
+        wrap(crewai, "Task._execute_core", traced_task_execute(crewai))
+        wrap(crewai, "Flow._execute_method", traced_flow_method(crewai))
+        wrap(crewai, "Flow._find_triggered_methods", patched_find_triggered_methods(crewai))
+    except AttributeError:
+        logger.warning("Failed to patch internal CrewAI methods.")
 
 
 def unpatch():
@@ -156,10 +211,16 @@ def unpatch():
     crewai._datadog_patch = False
 
     unwrap(crewai.Crew, "kickoff")
-    unwrap(crewai.Crew, "_get_context")
-    unwrap(crewai.Task, "_execute_core")
     unwrap(crewai.Task, "execute_async")
     unwrap(crewai.Agent, "execute_task")
     unwrap(crewai.tools.structured_tool.CrewStructuredTool, "invoke")
+    unwrap(crewai.Flow, "kickoff_async")
+    try:
+        unwrap(crewai.Crew, "_get_context")
+        unwrap(crewai.Task, "_execute_core")
+        unwrap(crewai.Flow, "_execute_method")
+        unwrap(crewai.Flow, "_find_triggered_methods")
+    except AttributeError:
+        pass
 
     delattr(crewai, "_datadog_integration")
