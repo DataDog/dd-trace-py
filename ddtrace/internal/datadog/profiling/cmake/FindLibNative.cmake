17d57ec3eb8a1d8e53e6fdb0c9a730b314c20e37
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-08-13 13:10:04 -0400

    require NATIVE_EXTENSION_LOCATION to be set

--- ddtrace/internal/datadog/profiling/cmake/FindLibNative.cmake
+++ ddtrace/internal/datadog/profiling/cmake/FindLibNative.cmake
@@ -3,11 +3,16 @@ find_package(Python3)
 if(DEFINED NATIVE_EXTENSION_LOCATION)
     set(LIB_FILE_LOCATION ${NATIVE_EXTENSION_LOCATION})
 else()
-    set(LIB_FILE_LOCATION ${CMAKE_SOURCE_DIR}/../../../../../src/native/target/release)
+    message(
+        FATAL_ERROR
+            "NATIVE_EXTENSION_LOCATION is not set. Use `python setup.py` or ddtrace/internal/datadog/profiling/build_standalone.sh"
+            "to build profiling native extensions.")
 endif()
 
 message(WARNING "LIB_FILE_LOCATION: ${LIB_FILE_LOCATION}")
 
+# We expect the native extension to be built and installed the headers in the following directory. It is configured in
+# setup.py by setting CARGO_TARGET_DIR environment variable.
 set(SOURCE_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/../../../../../src/native/target/include)
 
 set(DEST_LIB_DIR ${CMAKE_CURRENT_BINARY_DIR})
