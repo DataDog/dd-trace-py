3d5e1b8f7989b9323fcc837083009b75ecf00af3
Jacob Simpher <jacob.simpher@datadoghq.com>
2025-08-14 16:30:27 -0400

    remove double patch

--- ddtrace/contrib/internal/langchain/patch.py
+++ ddtrace/contrib/internal/langchain/patch.py
@@ -596,40 +596,6 @@ async def traced_llm_ainvoke(langchain, pin, func, instance, args, kwargs):
     return response
 
 
-@with_traced_module
-def traced_chat_model_invoke(langchain, pin, func, instance, args, kwargs):
-    """
-    Wrapper for BaseChatModel.invoke() method to handle prompt template metadata transfer.
-
-    BaseLLM.invoke() wraps BaseLLM.generate(), converting .invoke()'s input (often a PromptValue
-    with templating information) into a string.
-    While most tagging happens in the .generate() wrapper, we need to enter here to capture
-    that templating information before it is consumed.
-    Since child spans may need to read the tagged data, we must tag before calling the wrapped function.
-    """
-    integration: LangChainIntegration = langchain._datadog_integration
-    if integration.llmobs_enabled is False:
-        return func(*args, **kwargs)
-
-    integration.handle_llm_invoke(instance, args, kwargs)
-    response = func(*args, **kwargs)
-    return response
-
-
-@with_traced_module
-async def traced_chat_model_ainvoke(langchain, pin, func, instance, args, kwargs):
-    """
-    async version of above traced_chat_model_invoke
-    """
-    integration: LangChainIntegration = langchain._datadog_integration
-    if integration.llmobs_enabled is False:
-        return await func(*args, **kwargs)
-
-    integration.handle_llm_invoke(instance, args, kwargs)
-    response = await func(*args, **kwargs)
-    return response
-
-
 def _patch_embeddings_and_vectorstores():
     """
     Text embedding models override two abstract base methods instead of super calls,
@@ -738,16 +704,6 @@ def patch():
         "langchain_core",
         "language_models.chat_models.BaseChatModel.agenerate",
         traced_chat_model_agenerate(langchain),
-    )
-    wrap(
-        "langchain_core",
-        "language_models.chat_models.BaseChatModel.invoke",
-        traced_chat_model_invoke(langchain),
-    )
-    wrap(
-        "langchain_core",
-        "language_models.chat_models.BaseChatModel.ainvoke",
-        traced_chat_model_ainvoke(langchain),
     )
     wrap(
         "langchain_core",
