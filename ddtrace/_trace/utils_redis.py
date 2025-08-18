73debae209cccae7d5bf1a2e372dda0c61305dc4
Emmett Butler <emmett.butler321@gmail.com>
2025-08-18 10:59:07 -0700

    unused imports

--- ddtrace/_trace/utils_redis.py
+++ ddtrace/_trace/utils_redis.py
@@ -1,25 +1,8 @@
-from contextlib import contextmanager
-from typing import List
-from typing import Optional
-
-from ddtrace.constants import _SPAN_MEASURED_KEY
-from ddtrace.constants import SPAN_KIND
-from ddtrace.contrib import trace_utils
-from ddtrace.ext import SpanKind
-from ddtrace.ext import SpanTypes
 from ddtrace.ext import db
 from ddtrace.ext import redis as redisx
-from ddtrace.internal import core
-from ddtrace.internal.constants import COMPONENT
-from ddtrace.internal.schema import schematize_cache_operation
-from ddtrace.internal.utils.formats import stringify_cache_args
-
-
-format_command_args = stringify_cache_args
 
 
 def _extract_conn_tags(conn_kwargs):
-    """Transform redis conn info into dogtrace metas"""
     try:
         conn_tags = {
             net.TARGET_HOST: conn_kwargs["host"],
