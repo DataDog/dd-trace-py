4aa34f6a0368e9a274c8954f2ce2e0b2aa8c0cc4
Vítor De Araújo <vitor.dearaujo@datadoghq.com>
2025-08-19 09:41:31 +0000

    chore(ci_visibility): ensure exception dictionary is cleared after each test (#14344)
    The solution in PR #14328 was incomplete. This PR ensures all exceptions
    are cleared up at the end of each test.
    
    I'm not adding a release note because this is a continuation of #14328,
    which already has a release note.
    
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    - [x] Reviewer has checked that all the criteria below are met
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)
    
    (cherry picked from commit a5aee01df71e0b4a515e3ccf2c3a00735d0cb1ab)

--- tests/contrib/pytest/test_pytest.py
+++ tests/contrib/pytest/test_pytest.py
@@ -9,6 +9,8 @@ import pytest
 import ddtrace
 from ddtrace.constants import _SAMPLING_PRIORITY_KEY
 from ddtrace.constants import ERROR_MSG
+from ddtrace.contrib.internal.pytest._utils import excinfo_by_report
+from ddtrace.contrib.internal.pytest._utils import reports_by_item
 from ddtrace.contrib.internal.pytest.constants import XFAIL_REASON
 from ddtrace.contrib.internal.pytest.patch import get_version
 from ddtrace.contrib.internal.pytest.plugin import is_enabled
@@ -4274,6 +4276,21 @@ class PytestTestCase(PytestTestCaseBase):
         result = self.subprocess_run(file_name)
         assert "I/O operation on closed file" not in result.stderr.str()
         assert result.ret == 0
+
+    def test_pytest_clears_excinfo_dict_after_use(self):
+        reports_by_item_count_before = len(reports_by_item)
+        excinfo_by_report_count_before = len(excinfo_by_report)
+
+        self.testdir.makepyfile(
+            """
+            def test_one():
+                assert False
+            """
+        )
+
+        self.inline_run("--ddtrace")
+        assert len(reports_by_item) == reports_by_item_count_before
+        assert len(excinfo_by_report) == excinfo_by_report_count_before
 
 
 def test_pytest_coverage_data_format_handling_none_value():
