b67f975e35e60403bc84871199b18f71c28e0234
Vítor De Araújo <vitor.dearaujo@datadoghq.com>
2025-08-19 10:24:15 +0000

    chore(ci_visibility): replace weak dicts with regular dicts

--- ddtrace/contrib/internal/pytest/_utils.py
+++ ddtrace/contrib/internal/pytest/_utils.py
@@ -253,5 +253,5 @@ def get_user_property(report, key, default=None):
     return default
 
 
-excinfo_by_report = weakref.WeakKeyDictionary()
-reports_by_item = weakref.WeakKeyDictionary()
+excinfo_by_report = {}
+reports_by_item = {}
