0833a18f82e120ca8a71699fd286d1234b623a94
Emmett Butler <723615+emmettbutler@users.noreply.github.com>
2025-08-20 13:08:12 -0700

    Update ddtrace/_trace/trace_handlers.py
    Co-authored-by: Brett Langdon <brett.langdon@datadoghq.com>

--- ddtrace/_trace/trace_handlers.py
+++ ddtrace/_trace/trace_handlers.py
@@ -785,8 +785,8 @@ def _on_redis_execute_pipeline(ctx: core.ExecutionContext, pin, config_integrati
     span.set_tag_str(SPAN_KIND, SpanKind.CLIENT)
     span.set_tag_str(COMPONENT, config_integration.integration_name)
     span.set_tag_str(db.SYSTEM, redisx.APP)
-    # PERF: avoid setting via Span.set_tag
-    span.set_metric(_SPAN_MEASURED_KEY, 1)
+    # PERF: avoid extra overhead from checks in Span.set_metric
+    span._metrics[_SPAN_MEASURED_KEY] = 1
     if query is not None:
         span_name = schematize_cache_operation(redisx.RAWCMD, cache_provider=redisx.APP)  # type: ignore[operator]
         span.set_tag_str(span_name, query)
