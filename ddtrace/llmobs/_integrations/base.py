0d8cee776d9c7a6a1102f5f37db6b0af55332a41
Xinyuan Guo <xinyuan.guo@datadoghq.com>
2025-08-21 10:53:31 -0400

    add back comment

--- ddtrace/llmobs/_integrations/base.py
+++ ddtrace/llmobs/_integrations/base.py
@@ -66,7 +66,7 @@ class BaseLLMIntegration:
             span_type=SpanTypes.LLM if (submit_to_llmobs and self.llmobs_enabled) else None,
         )
         log.debug("Creating LLM span with type %s", span.span_type)
-
+        # determine if the span represents a proxy request
         base_url = self._get_base_url(**kwargs)
         if self._is_instrumented_proxy_url(base_url):
             span._set_ctx_item(PROXY_REQUEST, True)
