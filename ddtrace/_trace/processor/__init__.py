8eb2c47c01114ae354ee2b0c69d84aa2161edbf1
Munir Abdinur <munir.abdinur@datadoghq.com>
2025-08-21 15:42:20 -0400

    fmt

--- ddtrace/_trace/processor/__init__.py
+++ ddtrace/_trace/processor/__init__.py
@@ -478,8 +478,10 @@ class SpanAggregator(SpanProcessor):
         This method is typically used after a process fork or during runtime reconfiguration.
         Arguments that are None will not override existing values.
         """
-        # Flush any remaining spans in the writer's queue before reconfiguring.
-        self.writer.flush_queue()
+        if not reset_buffer:
+            # Flush any encoded spans in the writer's buffer. This operation ensures encoded spans
+            # are not dropped when the writer is recreated. This operation should not be handled after a fork. 
+            self.writer.flush_queue()
         # Re-create the writer to ensure it is consistent with updated configurations (ex: api_version)
         self.writer = self.writer.recreate(appsec_enabled=appsec_enabled)
 
