b3c2e4adc4646407871e79ccb59c85a49bfaf98d
Christophe Papazian <christophe.papazian@datadoghq.com>
2025-08-22 17:03:17 +0200

    simplification

--- tests/tracer/test_trace_utils.py
+++ tests/tracer/test_trace_utils.py
@@ -644,8 +644,6 @@ ALL_IP_HEADERS = (
 ALL_TESTS = [
     ["", dict(ALL_IP_HEADERS[-1 : -i - 2 : -1]), ALL_IP_HEADERS[-1 - i][1]] for i in range(len(ALL_IP_HEADERS))
 ]
-# x-forwarded is now ignored so we fall back to forwarded
-ALL_TESTS[5][2] = "5.5.5.5"
 
 
 @pytest.mark.parametrize(
