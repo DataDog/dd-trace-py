449e8aa82f6ba9794b1ba3568b867bac057b28a6
Alberto Vara <alberto.vara@datadoghq.com>
2025-08-26 09:21:20 +0200

    performance

--- ddtrace/appsec/_iast/_utils.py
+++ ddtrace/appsec/_iast/_utils.py
@@ -4,7 +4,7 @@ from ddtrace.appsec._iast._taint_tracking import num_objects_tainted
 from ddtrace.settings.asm import config as asm_config
 
 
-PROPAGATION_DEBUG = None
+PROPAGATION_DEBUG = asm_config._iast_propagation_debug
 
 
 def _get_source_index(sources: List, source) -> int:
@@ -21,9 +21,6 @@ def _is_iast_debug_enabled():
 
 
 def _is_iast_propagation_debug_enabled():
-    global PROPAGATION_DEBUG
-    if PROPAGATION_DEBUG is None:
-        PROPAGATION_DEBUG = asm_config._iast_propagation_debug
     return PROPAGATION_DEBUG
 
 
