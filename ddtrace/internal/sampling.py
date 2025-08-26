4ae16145da7af51b6bef00bb6b71003a8b579b1d
Munir Abdinur <munir.abdinur@datadoghq.com>
2025-08-26 17:42:10 -0400

    fix typo

--- ddtrace/internal/sampling.py
+++ ddtrace/internal/sampling.py
@@ -15,7 +15,7 @@ from ddtrace.constants import _SINGLE_SPAN_SAMPLING_MECHANISM
 from ddtrace.constants import _SINGLE_SPAN_SAMPLING_RATE
 from ddtrace.internal.constants import _KEEP_PRIORITY_INDEX
 from ddtrace.internal.constants import _REJECT_PRIORITY_INDEX
-from ddtrace.internal.constants import KNUETH_SAMPLE_RATE_KEY
+from ddtrace.internal.constants import KNUTH_SAMPLE_RATE_KEY
 from ddtrace.internal.constants import MAX_UINT_64BITS
 from ddtrace.internal.constants import SAMPLING_DECISION_TRACE_TAG_KEY
 from ddtrace.internal.constants import SAMPLING_HASH_MODULO
