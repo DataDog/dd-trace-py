c27b4fb98c421753543f52a3cad8f1ccc001576b
vianney <vianney.ruhlmann@datadoghq.com>
2025-08-27 14:22:54 +0200

    style(tests): fix styling

--- tests/integration/test_sampling.py
+++ tests/integration/test_sampling.py
@@ -378,7 +378,7 @@ def test_rate_limiter_on_long_running_spans(tracer):
         "DD_TRACE_SAMPLING_RULES": '[{"sample_rate": 0, "service": "animals"}]',
         "DD_SPAN_SAMPLING_RULES": '[{"service":"animals", "name":"monkey", "sample_rate":1}]',
     },
-    parametrize={"DD_TRACE_COMPUTE_STATS": ["false", "true"], "_DD_TRACE_WRITER_NATIVE": ["false", "true"] },
+    parametrize={"DD_TRACE_COMPUTE_STATS": ["false", "true"], "_DD_TRACE_WRITER_NATIVE": ["false", "true"]},
 )
 def test_single_span_and_trace_sampling_match_non_root_span():
     """Validates that a single span sampling rule applied to a non-root span does not
@@ -386,12 +386,13 @@ def test_single_span_and_trace_sampling_match_non_root_span():
     """
     # FIXME: When stats computation is enabled the native writer does not drop unsampled spans and does not
     # send stats to the testagent (and maybe even the real datadog agent).
-    from ddtrace.trace import tracer
     from ddtrace import config
+    from ddtrace.trace import tracer
 
     # NativeWriter needs to fetch agent info before client-side stats can be enabled
     if config._trace_compute_stats and config._trace_writer_native:
         import time
+
         time.sleep(1)
 
     with tracer.trace("non_monkey") as root:
@@ -423,7 +424,7 @@ def test_single_span_and_trace_sampling_match_non_root_span():
         "DD_TRACE_SAMPLING_RULES": '[{"sample_rate": 0, "service": "animals"}]',
         "DD_SPAN_SAMPLING_RULES": '[{"service":"animals", "name":"monkey", "sample_rate":1}]',
     },
-    parametrize={"DD_TRACE_COMPUTE_STATS": ["false", "true"],"_DD_TRACE_WRITER_NATIVE": ["false", "true"] },
+    parametrize={"DD_TRACE_COMPUTE_STATS": ["false", "true"], "_DD_TRACE_WRITER_NATIVE": ["false", "true"]},
 )
 def test_single_span_and_trace_sampling_match_root_span_partial_flushing():
     """Validates that a single span sampling rule applied to a root span does not
@@ -431,12 +432,13 @@ def test_single_span_and_trace_sampling_match_root_span_partial_flushing():
     """
     # FIXME: When stats computation is enabled the native writer does not drop unsampled spans and does not
     # send stats to the testagent (and maybe even the real datadog agent).
-    from ddtrace.trace import tracer
     from ddtrace import config
+    from ddtrace.trace import tracer
 
     # NativeWriter needs to fetch agent info before client-side stats can be enabled
     if config._trace_writer_native:
         import time
+
         time.sleep(1)
 
     with tracer.trace("monkey") as root:
