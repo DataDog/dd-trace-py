6c7dbfdfb1234230c15a89d7570e989ac6cc8ccb
Nicole Cybul <nicole.cybul@datadoghq.com>
2025-09-04 12:57:12 -0400

    run black

--- ddtrace/llmobs/_llmobs.py
+++ ddtrace/llmobs/_llmobs.py
@@ -515,7 +515,9 @@ class LLMObs(Service):
 
         core.reset_listeners(DISPATCH_ON_GUARDRAIL_SPAN_START, self._guardrail_link_tracker.on_guardrail_span_start)
         core.reset_listeners(DISPATCH_ON_LLM_SPAN_FINISH, self._guardrail_link_tracker.on_llm_span_finish)
-        core.reset_listeners(DISPATCH_ON_OPENAI_AGENT_SPAN_FINISH, self._guardrail_link_tracker.on_openai_agent_span_finish)
+        core.reset_listeners(
+            DISPATCH_ON_OPENAI_AGENT_SPAN_FINISH, self._guardrail_link_tracker.on_openai_agent_span_finish
+        )
 
         forksafe.unregister(self._child_after_fork)
 
@@ -632,7 +634,9 @@ class LLMObs(Service):
 
             core.on(DISPATCH_ON_GUARDRAIL_SPAN_START, cls._instance._guardrail_link_tracker.on_guardrail_span_start)
             core.on(DISPATCH_ON_LLM_SPAN_FINISH, cls._instance._guardrail_link_tracker.on_llm_span_finish)
-            core.on(DISPATCH_ON_OPENAI_AGENT_SPAN_FINISH, cls._instance._guardrail_link_tracker.on_openai_agent_span_finish)
+            core.on(
+                DISPATCH_ON_OPENAI_AGENT_SPAN_FINISH, cls._instance._guardrail_link_tracker.on_openai_agent_span_finish
+            )
 
             atexit.register(cls.disable)
             telemetry_writer.product_activated(TELEMETRY_APM_PRODUCT.LLMOBS, True)
