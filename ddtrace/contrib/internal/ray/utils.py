78e1f6edf362eba8e5a1e870f18655708ff02545
Louis Tricot <louis.tricot@datadoghq.com>
2025-09-04 14:45:19 +0200

    fix: ci

--- ddtrace/contrib/internal/ray/utils.py
+++ ddtrace/contrib/internal/ray/utils.py
@@ -10,16 +10,6 @@ from ddtrace.propagation.http import _TraceContext
 import ray
 from ray.runtime_context import get_runtime_context
 
-
-def in_ray_job():
-    # type: () -> bool
-    """Returns whether we are in a ray environemt.
-    This is accomplished by checking if the _RAY_SUBMISSION_ID environment variable is defined
-    which means a job has been submitted and is traced
-    """
-    return bool(os.environ.get("_RAY_SUBMISSION_ID", False))
-
-
 def _inject_dd_trace_ctx_kwarg(method: Callable) -> Signature:
     old_sig = inspect.signature(method)
     if "_dd_trace_ctx" in old_sig.parameters:
