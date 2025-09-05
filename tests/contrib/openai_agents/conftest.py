bc343f19a65c3cecb59dc031f150e52bc1fd2c62
ncybul <124532568+ncybul@users.noreply.github.com>
2025-09-05 12:47:29 -0400

    chore(llmobs): [MLOB-3828] link input and output guardrail spans for openai agents (#14488)
    This PR implements span linking for Open AI Agents guardrail spans. 
    
    For input guardrails, a span link is created from the guardrail span to
    the first LLM span which is a sibling of the input guardrail (share the
    same llmobs parent). This implementation prevents a link from the
    guardrail span to the openai_agents.response span from being created
    while still linking the guardrail to the Triage Agent (LLM) span.
    
    
    <img width="708" height="264" alt="image"
    src="https://github.com/user-attachments/assets/35df597a-9d27-464e-b302-9b241e2b4f4d"
    />
    
    
    For output guardrails, a span link is created from the last finished LLM
    span (for a given agent run) to the output guardrail span.
    
    
    <img width="2766" height="178" alt="image"
    src="https://github.com/user-attachments/assets/35773ad9-d512-4be5-8db9-8a559ab29047"
    />
    
    
    With this implementation, we get a trace like the following which better
    represents the guardrail's functionality of checking the user input to
    the first agent and the final output of the last agent.
    
    <img width="488" height="1296" alt="image"
    src="https://github.com/user-attachments/assets/4aac3564-d9bb-491e-9155-5847cec1f8ce"
    />
    
    This PR also removes the experimental `_record_object` method from the
    LLMObs service since it is not being used anywhere.
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

--- tests/contrib/openai_agents/conftest.py
+++ tests/contrib/openai_agents/conftest.py
@@ -5,6 +5,7 @@ from agents import GuardrailFunctionOutput
 from agents import WebSearchTool
 from agents import function_tool
 from agents import input_guardrail
+from agents import output_guardrail
 from openai.types.responses.web_search_tool_param import UserLocation
 import pytest
 import vcr
@@ -120,14 +121,26 @@ def weather_agent(web_search):
 
 
 @input_guardrail
-async def simple_guardrail(
+async def simple_input_guardrail(
     context,
     agent,
     inp,
 ):
     return GuardrailFunctionOutput(
         output_info="dummy",
-        tripwire_triggered="safe" not in inp,
+        tripwire_triggered=False,
+    )
+
+
+@output_guardrail
+async def simple_output_guardrail(
+    context,
+    agent,
+    inp,
+):
+    return GuardrailFunctionOutput(
+        output_info="dummy",
+        tripwire_triggered=False,
     )
 
 
@@ -135,9 +148,11 @@ async def simple_guardrail(
 def simple_agent_with_guardrail():
     """An agent with addition tools and a guardrail"""
     yield Agent(
-        name="Simple Agent",
+        name="Simple Agent with Guardrails",
         instructions="You are a helpful assistant specialized in addition calculations.",
-        input_guardrails=[simple_guardrail],
+        input_guardrails=[simple_input_guardrail],
+        output_guardrails=[simple_output_guardrail],
+        tools=[add],
         model="gpt-4o",
     )
 
