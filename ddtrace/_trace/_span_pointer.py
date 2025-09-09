1cf7bbf16ab12ab21f0ed1e125faa2b71b05eef1
Joey Zhao <5253430+joeyzhao2018@users.noreply.github.com>
2025-09-09 13:36:44 -0400

    fix span pointer __repr__

--- ddtrace/_trace/_span_pointer.py
+++ ddtrace/_trace/_span_pointer.py
@@ -64,7 +64,7 @@ class _SpanPointer(SpanLink):
     def __repr__(self):
         return (
             f"SpanPointer(trace_id={self.trace_id}, span_id={self.span_id}, kind={self.kind}, "
-            f"direction={self.direction}, hash={self.hash}, attributes={self.attributes})"
+            f"direction={self.attributes.get('ptr.dir')}, hash={self.attributes.get('ptr.hash')}, attributes={self.attributes})"
         )
 
 
