887aba91e58bfe7ceb59f3708b140420d8c92c86
Jessica Gamio <jessica.gamio@datadoghq.com>
2025-09-08 20:29:06 -0700

    fixed errors post testing
    Signed-off-by: jessicagamio <jessica.gamio@datadoghq.com>

--- ddtrace/contrib/internal/dramatiq/patch.py
+++ ddtrace/contrib/internal/dramatiq/patch.py
@@ -61,7 +61,7 @@ def _traced_send_with_options_function(integration_config: Config) -> Callable[[
     ) -> Callable[[Any], Any]:
         with core.context_with_data(
             "dramatiq.Actor.send_with_options",
-            span_name=dramatiq.Actor.send_with_option
+            span_name=dramatiq.Actor.send_with_option,
             pin=pin,
             span_type=SpanTypes.WORKER,
             service=trace_utils.ext_service(pin=None, int_config=integration_config),
@@ -71,20 +71,8 @@ def _traced_send_with_options_function(integration_config: Config) -> Callable[[
                     "actor.name": instance.actor_name,
                     "actor.options": instance.options,                
                 }
-
-        # with tracer.trace(
-        #     "dramatiq.Actor.send_with_options",
-        #     span_type=SpanTypes.WORKER,
-        #     service=trace_utils.ext_service(pin=None, int_config=integration_config),
-        # ) as span:
-        #     span.set_tags(
-        #         {
-        #             SPAN_KIND: SpanKind.PRODUCER,
-        #             "actor.name": instance.actor_name,
-        #             "actor.options": instance.options,
-        #         }
             )
 
-            return func(*args, **kwargs)
+                return func(*args, **kwargs)
 
     return _traced_send_with_options
