1b92919b80dc187dce9f0d79172acc7eefb00cfe
Christophe Papazian <114495376+christophe-papazian@users.noreply.github.com>
2025-09-12 18:21:19 +0200

    feat(aap): add support for api10 on requests package (#14556)
    Extends the api10 downstream request analysis to the requests package.
    This requires some auto-instrumentation of urllib3
    
    (following https://github.com/DataDog/dd-trace-py/pull/14481)
    
    Adding tests and this will be also tested e2e on system tests.
    
    APPSEC-58715
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

--- riotfile.py
+++ riotfile.py
@@ -3520,6 +3520,7 @@ venv = Venv(
                 "AGENT_VERSION": "testagent",
                 "DD_REMOTE_CONFIGURATION_ENABLED": "true",
                 "DD_API_SECURITY_SAMPLE_DELAY": "0",
+                "DD_PATCH_MODULES": "unittest:false,urllib3:true",
             },
             venvs=[
                 Venv(
@@ -3573,6 +3574,7 @@ venv = Venv(
                 "AGENT_VERSION": "testagent",
                 "DD_REMOTE_CONFIGURATION_ENABLED": "true",
                 "DD_API_SECURITY_SAMPLE_DELAY": "0",
+                "DD_PATCH_MODULES": "unittest:false,urllib3:true",
             },
             venvs=[
                 Venv(
@@ -3623,6 +3625,7 @@ venv = Venv(
                 "DD_REMOTE_CONFIGURATION_ENABLED": "true",
                 "DD_IAST_DEDUPLICATION_ENABLED": "false",
                 "DD_API_SECURITY_SAMPLE_DELAY": "0",
+                "DD_PATCH_MODULES": "unittest:false,urllib3:true",
             },
             venvs=[
                 Venv(
