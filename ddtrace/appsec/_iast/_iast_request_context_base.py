b529db93026ecad42863f6b954f3e86911e4d05a
Alberto Vara <alberto.vara@datadoghq.com>
2025-09-17 16:34:29 +0200

    feat(iast): reduce weak hash false positives (#14606)
    This PR reduces the number of false positives for weak hash usage. It
    automatically discards all `md5` or `sha1` functions that have the
    parameter `usedforsecurity=False`.
    This PR addresses the following open issue:
    [https://github.com/DataDog/dd-trace-py/issues/14190](https://github.com/DataDog/dd-trace-py/issues/14190)
    
    
    ## Checklist
    - [x] PR author has checked that all the criteria below are met
    - The PR description includes an overview of the change
    - The PR description articulates the motivation for the change
    - The change includes tests OR the PR description describes a testing
    strategy
    - The PR description notes risks associated with the change, if any
    - Newly-added code is easy to change
    - The change follows the [library release note
    guidelines](https://ddtrace.readthedocs.io/en/stable/releasenotes.html)
    - The change includes or references documentation updates if necessary
    - Backport labels are set (if
    [applicable](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting))
    
    ## Reviewer Checklist
    - [x] Reviewer has checked that all the criteria below are met 
    - Title is accurate
    - All changes are related to the pull request's stated goal
    - Avoids breaking
    [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces)
    changes
    - Testing strategy adequately addresses listed risks
    - Newly-added code is easy to change
    - Release note makes sense to a user of the library
    - If necessary, author has acknowledged and discussed the performance
    implications of this PR as reported in the benchmarks PR comment
    - Backport labels are set in a manner that is consistent with the
    [release branch maintenance
    policy](https://ddtrace.readthedocs.io/en/latest/contributing.html#backporting)

--- ddtrace/appsec/_iast/_iast_request_context_base.py
+++ ddtrace/appsec/_iast/_iast_request_context_base.py
@@ -121,3 +121,29 @@ def _num_objects_tainted_in_request() -> int:
     if context_id is not None:
         return debug_num_tainted_objects(context_id)
     return 0
+
+
+def get_hash_object_tracking_len():
+    env = _get_iast_env()
+    if env:
+        return len(env.iast_hash_object_tracking)
+    return 0
+
+
+def get_hash_object_tracking(obj):
+    env = _get_iast_env()
+    if env:
+        return env.iast_hash_object_tracking.get(id(obj), False)
+    return False
+
+
+def set_hash_object_tracking(result, value):
+    env = _get_iast_env()
+    if env:
+        env.iast_hash_object_tracking[id(result)] = value
+
+
+def clear_hash_object_tracking():
+    env = _get_iast_env()
+    if env:
+        env.iast_hash_object_tracking.clear()
