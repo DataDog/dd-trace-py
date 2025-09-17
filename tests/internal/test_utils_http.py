1c7fc258923aa205c5b82fb064c1ad2bbf50e491
Emmett Butler <emmett.butler321@gmail.com>
2025-09-17 12:57:51 -0700

    skip a test that doesnt have a working 3.14 alternative

--- tests/internal/test_utils_http.py
+++ tests/internal/test_utils_http.py
@@ -1,9 +1,14 @@
 import httpretty
 import pytest
 
+from ddtrace.internal.compat import PYTHON_VERSION_INFO
 from ddtrace.internal.utils.http import connector
 
 
+@pytest.mark.skipif(
+    PYTHON_VERSION_INFO >= (3, 14),
+    reason="httpretty doesn't work with Python 3.14 and there is no apparent replacement that works with http.client",
+)
 @pytest.mark.parametrize("scheme", ["http", "https"])
 def test_connector(scheme):
     with httpretty.enabled():
