004943bb26d6496854e12b7c49c56b047209da9a
Emmett Butler <emmett.butler321@gmail.com>
2025-09-22 11:00:24 -0700

    update some other riot envs to actually test the new pytest-asyncio

--- riotfile.py
+++ riotfile.py
@@ -2197,9 +2197,15 @@ venv = Venv(
                     },
                 ),
                 Venv(
-                    pys=select_pys(min_version="3.8"),
+                    pys=select_pys(min_version="3.8", max_version="3.12"),
                     pkgs={
                         "pytest-asyncio": ["==0.23.7"],
+                    },
+                ),
+                Venv(
+                    pys=select_pys(min_version="3.13"),
+                    pkgs={
+                        "pytest-asyncio": [">=1.0.0"],
                     },
                 ),
             ],
@@ -2631,11 +2637,23 @@ venv = Venv(
         Venv(
             name="asyncio",
             command="pytest {cmdargs} tests/contrib/asyncio",
-            pys=select_pys(),
             pkgs={
                 "pytest-randomly": latest,
-                "pytest-asyncio": "==0.21.1",
             },
+            venvs=[
+                Venv(
+                    pys=select_pys(max_version="3.12"),
+                    pkgs={
+                        "pytest-asyncio": "==0.21.1",
+                    },
+                ),
+                Venv(
+                    pys=select_pys(min_version="3.13"),
+                    pkgs={
+                        "pytest-asyncio": ">=1.0.0",
+                    },
+                ),
+            ],
         ),
         Venv(
             name="openai",
