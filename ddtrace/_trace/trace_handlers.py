d3007840c765b83b571e7abf0dc5047cf7f07185
Jessica Gamio <jessica.gamio@datadoghq.com>
2025-09-22 20:27:43 -0700

    added dramatiq integration to trace_handlers.py
    Signed-off-by: jessicagamio <jessica.gamio@datadoghq.com>

--- ddtrace/_trace/trace_handlers.py
+++ ddtrace/_trace/trace_handlers.py
@@ -1258,6 +1258,7 @@ def listen():
         "azure.servicebus.patched_producer_schedule",
         "azure.servicebus.patched_producer_send",
         "psycopg.patched_connect",
+        "dramatiq.Actor.send_with_options",
     ):
         core.on(f"context.started.{context_name}", _start_span)
 
