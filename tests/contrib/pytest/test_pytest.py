a113a049c57b3507ecb1e9348e8a6d5879c42a6b
Vítor De Araújo <vitor.dearaujo@datadoghq.com>
2025-09-29 13:49:21 +0000

    fix most tests

--- tests/contrib/pytest/test_pytest.py
+++ tests/contrib/pytest/test_pytest.py
@@ -1743,8 +1743,8 @@ class PytestTestCase(PytestTestCaseBase):
         first_tag_data = _get_span_coverage_data(first_test_span, True)
         assert len(first_tag_data) == 2
         assert sorted(first_tag_data.keys()) == ["/lib_fn.py", "/test_cov.py"]
-        assert first_tag_data["/lib_fn.py"] == [(2, 2)]
-        assert first_tag_data["/test_cov.py"] == [(4, 5)]
+        assert first_tag_data["/lib_fn.py"] == [(1, 2)]
+        assert first_tag_data["/test_cov.py"] == [(1, 1), (3, 5), (7, 7)]
 
         second_test_span = spans[1]
         assert second_test_span.get_tag("type") == "test"
@@ -1753,8 +1753,8 @@ class PytestTestCase(PytestTestCaseBase):
         second_tag_data = _get_span_coverage_data(second_test_span, True)
         assert len(second_tag_data) == 2
         assert sorted(second_tag_data.keys()) == ["/ret_false.py", "/test_cov.py"]
-        assert second_tag_data["/ret_false.py"] == [(2, 2)]
-        assert second_tag_data["/test_cov.py"] == [(8, 9)]
+        assert second_tag_data["/ret_false.py"] == [(1, 2)]
+        assert second_tag_data["/test_cov.py"] == [(1, 1), (3, 3), (7, 9)]
 
     @pytest.mark.skipif(
         not _PYTEST_SUPPORTS_ITR,
@@ -1833,8 +1833,8 @@ class PytestTestCase(PytestTestCaseBase):
         second_tag_data = _get_span_coverage_data(second_test_span, True)
         assert len(second_tag_data) == 2
         assert sorted(second_tag_data.keys()) == ["/test_cov.py", "/test_ret_false.py"]
-        assert second_tag_data["/test_ret_false.py"] == [(2, 2)]
-        assert second_tag_data["/test_cov.py"] == [(8, 9)]
+        assert second_tag_data["/test_ret_false.py"] == [(1, 2)]
+        assert second_tag_data["/test_cov.py"] == [(1, 1), (3, 3), (7, 9)]
 
     @pytest.mark.skipif(
         not _PYTEST_SUPPORTS_ITR,
@@ -1922,8 +1922,8 @@ class PytestTestCase(PytestTestCaseBase):
         second_tag_data = _get_span_coverage_data(second_test_span, True)
         assert len(second_tag_data) == 2
         assert sorted(second_tag_data.keys()) == ["/test_cov.py", "/test_ret_false.py"]
-        assert second_tag_data["/test_ret_false.py"] == [(2, 2)]
-        assert second_tag_data["/test_cov.py"] == [(9, 10)]
+        assert second_tag_data["/test_ret_false.py"] == [(1, 2)]
+        assert second_tag_data["/test_cov.py"] == [(1, 1), (3, 4), (8, 10), (12, 15), (17, 18), (22, 25), (27, 28)]
 
         third_test_span = spans[2]
         assert third_test_span.get_tag("test.name") == "test_skipif_mark_false"
@@ -1931,8 +1931,8 @@ class PytestTestCase(PytestTestCaseBase):
         third_tag_data = _get_span_coverage_data(third_test_span, True)
         assert len(third_tag_data) == 2
         assert sorted(third_tag_data.keys()) == ["/test_cov.py", "/test_ret_false.py"]
-        assert third_tag_data["/test_cov.py"] == [(19, 20)]
-        assert third_tag_data["/test_ret_false.py"] == [(2, 2)]
+        assert third_tag_data["/test_cov.py"] == [(1, 1), (3, 4), (8, 8), (12, 15), (17, 20), (22, 25), (27, 28)]
+        assert third_tag_data["/test_ret_false.py"] == [(1, 2)]
 
         fourth_test_span = spans[3]
         assert fourth_test_span.get_tag("test.name") == "test_skipif_mark_true"
@@ -2002,7 +2002,7 @@ class PytestTestCase(PytestTestCaseBase):
         first_tag_data = _get_span_coverage_data(first_test_span, True)
         assert len(first_tag_data) == 1
         assert sorted(first_tag_data.keys()) == ["/test_cov.py"]
-        assert first_tag_data["/test_cov.py"] == [(4, 5)]
+        assert first_tag_data["/test_cov.py"] == [(1, 1), (3, 5), (9, 9)]
 
         second_test_span = spans[1]
         assert second_test_span.get_tag("type") == "test"
@@ -2011,8 +2011,8 @@ class PytestTestCase(PytestTestCaseBase):
         second_tag_data = _get_span_coverage_data(second_test_span, True)
         assert len(second_tag_data) == 2
         assert sorted(second_tag_data.keys()) == ["/test_cov.py", "/test_ret_false.py"]
-        assert second_tag_data["/test_ret_false.py"] == [(2, 2)]
-        assert second_tag_data["/test_cov.py"] == [(10, 11)]
+        assert second_tag_data["/test_ret_false.py"] == [(1, 2)]
+        assert second_tag_data["/test_cov.py"] == [(1, 1), (3, 3), (9, 11)]
 
     def test_pytest_will_report_git_metadata(self):
         py_file = self.testdir.makepyfile(
