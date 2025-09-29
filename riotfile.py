d94849f3579f4d898649c742fb128b4fb163812d
Vlad Scherbich <vlad.scherbich@datadoghq.com>
2025-09-29 10:39:03 -0400

    [dd-trace-py] Fix  run commands for 'profile-v2' and 'profile-v2-memalloc' Venv

--- riotfile.py
+++ riotfile.py
@@ -3470,7 +3470,7 @@ venv = Venv(
         Venv(
             name="profile-v2",
             # NB riot commands that use this Venv must include --pass-env to work properly
-            command="python -m tests.profiling.run pytest -v --no-cov --capture=no --benchmark-disable --ignore='tests/profile_v2/test_memalloc.py' {cmdargs} tests/profiling_v2",  # noqa: E501
+            command="python -m tests.profiling.run pytest -v --no-cov --capture=no --benchmark-disable --ignore='tests/profiling_v2/collector/test_memalloc.py' {cmdargs} tests/profiling_v2",  # noqa: E501
             env={
                 "DD_PROFILING_ENABLE_ASSERTS": "1",
                 "CPUCOUNT": "12",
@@ -3569,9 +3569,10 @@ venv = Venv(
                 ),
                 Venv(
                     name="profile-v2-memalloc",
-                    command="python -m pytest {cmdargs} tests/profiling_v2/collector/test_memalloc.py",
+                    command="python -m tests.profiling.run pytest -v --no-cov --capture=no --benchmark-disable {cmdargs} tests/profiling_v2/collector/test_memalloc.py",  # noqa: E501
                     pys=select_pys(),
                     venvs=[
+                        # standard allocators
                         Venv(
                             env={
                                 "PYTHONMALLOC": ["pymalloc", "malloc", "malloc_debug", "pymalloc_debug"],
