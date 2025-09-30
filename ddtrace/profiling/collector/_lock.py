3151b51ced2cddf164bc5c54d99132292a49b5a8
Vlad Scherbich <vlad.scherbich@datadoghq.com>
2025-09-30 12:50:13 -0400

    [dd-trace-py][Lock profiler] ruff-ruff

--- ddtrace/profiling/collector/_lock.py
+++ ddtrace/profiling/collector/_lock.py
@@ -6,15 +6,13 @@ import os.path
 import sys
 import time
 import types
-from typing import (  # noqa: I001
-    Any,
-    Callable,
-    Dict,
-    List,
-    Optional,
-    Tuple,
-    Type,
-)
+from typing import Any
+from typing import Callable
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Tuple
+from typing import Type
 
 import wrapt
 
