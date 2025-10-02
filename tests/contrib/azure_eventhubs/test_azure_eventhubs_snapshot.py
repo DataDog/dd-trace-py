0106da6644fce7e15388fe9bcaa307ea67f85ee7
Duncan Harvey <duncan.harvey@datadoghq.com>
2025-10-02 11:19:32 -0400

    add error test for eventhubs

--- tests/contrib/azure_eventhubs/test_azure_eventhubs_snapshot.py
+++ tests/contrib/azure_eventhubs/test_azure_eventhubs_snapshot.py
@@ -2,14 +2,19 @@ import itertools
 import os
 from pathlib import Path
 
+from azure.eventhub import EventData
+from azure.eventhub import EventHubProducerClient
 import pytest
 
 from ddtrace.contrib.internal.azure_eventhubs.patch import patch
 from ddtrace.contrib.internal.azure_eventhubs.patch import unpatch
 
 
+CONNECTION_STRING = "Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;"
+EVENTHUB_NAME = "eh1"
+
 # Ignoring span link attributes until values are normalized: https://github.com/DataDog/dd-apm-test-agent/issues/154
-SNAPSHOT_IGNORES = ["meta.messaging.message_id", "meta._dd.span_links"]
+SNAPSHOT_IGNORES = ["meta.messaging.message_id", "meta._dd.span_links", "meta.error.stack"]
 
 METHODS = ["send_event", "send_batch"]
 BUFFERED_OPTIONS = [False, True]
@@ -82,3 +87,18 @@ async def test_producer(ddtrace_run_python_code_in_subprocess, env_vars):
 
     assert status == 0, (err.decode(), out.decode())
     assert err == b"", err.decode()
+
+
+@pytest.mark.snapshot(ignores=SNAPSHOT_IGNORES)
+def test_producer_error():
+    producer_client = EventHubProducerClient.from_connection_string(
+        conn_str=CONNECTION_STRING, eventhub_name=EVENTHUB_NAME
+    )
+
+    try:
+        # send_batch does not accept a single EventData
+        producer_client.send_batch(EventData(body='{"body":"test message"}'))
+    except TypeError as e:
+        assert str(e) == "'EventData' object is not iterable"
+    finally:
+        producer_client.close()
