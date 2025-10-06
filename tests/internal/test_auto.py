f629d163900f0baaaf4e722369c53fdc58ec0f5f
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2025-10-06 14:38:37 +0100

    chore: initialise HTTP client when cloning modules
    A recent attempt to reduce start-up time by lazy-loading the HTTP client
    modules resulted in a potential gevent support regression whereby the
    lazily created HTTP client could end up making requests via the
    gevent-monkey-patched socket module. This introduces the possibility of
    spurious deadlock during the processing of requests. We prevent this by
    force-importing the HTTP clients on bootstrap to ensure that we grab
    references to an original copy of the socket module. This prevents our
    HTTP clients from calling into gevent when uploading payloads to the
    agent, preventing the potential deadlocks.

--- tests/internal/test_auto.py
+++ tests/internal/test_auto.py
@@ -1,18 +1,34 @@
-import sys
-
 import pytest
 
 
-@pytest.mark.skipif(sys.version_info < (3, 12, 5), reason="Python < 3.12.5 eagerly loads the threading module")
+# DEV: This test must pass ALWAYS. If this test fails, it means that something
+# needs to be fixed somewhere. Please DO NOT skip this test under any
+# circumstance!
 @pytest.mark.subprocess(
     env=dict(
         DD_UNLOAD_MODULES_FROM_SITECUSTOMIZE="true",
-        DD_REMOTE_CONFIGURATION_ENABLED="1",
-    )
+    ),
+    parametrize={
+        "DD_REMOTE_CONFIGURATION_ENABLED": ("1", "0"),
+    },
 )
 def test_auto():
+    import os
     import sys
 
     import ddtrace.auto  # noqa:F401
 
     assert "threading" not in sys.modules
+
+    if os.getenv("DD_REMOTE_CONFIGURATION_ENABLED") == "0":
+        # When unloading modules we must have the HTTP clients imported already
+        assert "ddtrace.internal.http" in sys.modules
+
+        # emulate socket patching (e.g. by gevent)
+        import socket  # noqa:F401
+
+        socket.create_connection = None
+
+        from ddtrace.internal.http import HTTPConnection  # noqa:F401
+
+        assert HTTPConnection("localhost")._create_connection is not None
