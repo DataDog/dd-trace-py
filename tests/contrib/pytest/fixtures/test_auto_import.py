78091e63efa8d7166a80493817cf57013d3ab293
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2025-10-10 11:28:41 +0200

    Add missing files
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

---
+++ tests/contrib/pytest/fixtures/test_auto_import.py
@@ -0,0 +1,15 @@
+"""Test script to verify ddtrace.auto behavior with pytest.ini."""
+import os
+import sys
+
+sitecustomize_imported = 'ddtrace.bootstrap.sitecustomize' in sys.modules
+
+with open('auto_import_result.txt', 'w') as f:
+    f.write('SITECUSTOMIZE_IMPORTED={}'.format(sitecustomize_imported))
+
+print(f"SITECUSTOMIZE_IMPORTED={sitecustomize_imported}")
+print(f"PYTHONPATH={os.environ.get('PYTHONPATH', '')}")
+print(f"sys.path={sys.path}")
+print(f"sys.modules keys: {list(k for k in sys.modules.keys() if 'ddtrace' in k or 'sitecustomize' in k)}")
+
+assert not sitecustomize_imported, "sitecustomize was imported when it shouldn't have been"
