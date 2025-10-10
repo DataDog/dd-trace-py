59699c1febe24b72bc7338a3c23668659ec1a27a
brettlangdon <brett.langdon@datadoghq.com>
2025-10-10 13:51:54 -0400

    better type hinting

--- ddtrace/_trace/processor/__init__.pyi
+++ ddtrace/_trace/processor/__init__.pyi
@@ -1,6 +1,8 @@
-from typing import List, Optional
+from threading import RLock
+from typing import Dict, List, Optional
 
 from ddtrace._trace.span import Span
+from ddtrace.internal.writer.writer import TraceWriter
 
 class TraceProcessor:
     def process_trace(self, trace: List[Span]) -> Optional[List[Span]]: ...
@@ -16,4 +18,26 @@ class TraceSamplingProcessor(TraceProcessor): ...
 class TopLevelSpanProcessor(SpanProcessor): ...
 class ServiceNameProcessor(TraceProcessor): ...
 class TraceTagsProcessor(TraceProcessor): ...
-class SpanAggregator(SpanProcessor): ...
+
+class SpanAggregator(SpanProcessor):
+    partial_flush_enabled: bool
+    partial_flush_min_spans: int
+    sampling_processor: TraceSamplingProcessor
+    tags_processor: TraceTagsProcessor
+    service_name_processor: ServiceNameProcessor
+    dd_processors: List[TraceProcessor]
+    user_processors: List[TraceProcessor]
+    writer: TraceWriter
+    _traces: Dict[int, List[Span]]
+    _spans_created: Dict[str, int]
+    _spans_finished: Dict[str, int]
+    _lock: RLock
+    _total_spans_finished: int
+
+    def __init__(
+        self,
+        partial_flush_enabled: bool,
+        partial_flush_min_spans: int,
+        dd_processors: Optional[List[TraceProcessor]] = None,
+        user_processors: Optional[List[TraceProcessor]] = None,
+    ) -> None: ...
