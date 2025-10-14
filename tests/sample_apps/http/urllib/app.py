4241c49c58c279475d42ad48af0bb4ebfc94451c
Louis Tricot <louis.tricot@datadoghq.com>
2025-10-14 11:30:13 +0200

    remove urllib

--- tests/sample_apps/http/urllib/app.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-from ddtrace import patch
-
-patch(urllib=True)
-
-def send_request(method: str, url: str):
-    req = urllib.request.Request(url, method=method)
-    with urllib.request.urlopen(req, timeout=10.0) as response:
-        print(f"{method} {url}: {response.status}")
