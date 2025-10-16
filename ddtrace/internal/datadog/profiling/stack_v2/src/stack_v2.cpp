54a3a0ea3500face85d091f30c71acf2bcdeafd9
Gabriele N. Tornetta <P403n1x87@users.noreply.github.com>
2025-10-16 18:30:46 +0100

    fix(profiling): release the GIL for internal mutation (#14852)
    ## Description
    
    We make the stack v2 sampler release the GIL when performing internal
    state mutation that can require acquiring other internal locks. This
    gives the chance to other threads to make progress while the tracking
    operations wait on an internal mutex.

--- ddtrace/internal/datadog/profiling/stack_v2/src/stack_v2.cpp
+++ ddtrace/internal/datadog/profiling/stack_v2/src/stack_v2.cpp
@@ -73,7 +73,10 @@ stack_v2_thread_register(PyObject* self, PyObject* args)
         return NULL;
     }
 
+    Py_BEGIN_ALLOW_THREADS;
     Sampler::get().register_thread(id, native_id, name);
+    Py_END_ALLOW_THREADS;
+
     Py_RETURN_NONE;
 }
 
@@ -87,8 +90,11 @@ stack_v2_thread_unregister(PyObject* self, PyObject* args)
         return NULL;
     }
 
+    Py_BEGIN_ALLOW_THREADS;
     Sampler::get().unregister_thread(id);
     ThreadSpanLinks::get_instance().unlink_span(id);
+    Py_END_ALLOW_THREADS;
+
     Py_RETURN_NONE;
 }
 
@@ -122,7 +128,9 @@ _stack_v2_link_span(PyObject* self, PyObject* args, PyObject* kwargs)
         span_type = empty_string.c_str();
     }
 
+    Py_BEGIN_ALLOW_THREADS;
     ThreadSpanLinks::get_instance().link_span(thread_id, span_id, local_root_span_id, std::string(span_type));
+    Py_END_ALLOW_THREADS;
 
     Py_RETURN_NONE;
 }
@@ -140,7 +148,9 @@ stack_v2_track_asyncio_loop(PyObject* self, PyObject* args)
         return NULL;
     }
 
+    Py_BEGIN_ALLOW_THREADS;
     Sampler::get().track_asyncio_loop(thread_id, loop);
+    Py_END_ALLOW_THREADS;
 
     Py_RETURN_NONE;
 }
@@ -172,7 +182,9 @@ stack_v2_link_tasks(PyObject* self, PyObject* args)
         return NULL;
     }
 
+    Py_BEGIN_ALLOW_THREADS;
     Sampler::get().link_tasks(parent, child);
+    Py_END_ALLOW_THREADS;
 
     Py_RETURN_NONE;
 }
@@ -211,7 +223,9 @@ track_greenlet(PyObject* Py_UNUSED(m), PyObject* args)
         return NULL;
     }
 
+    Py_BEGIN_ALLOW_THREADS;
     Sampler::get().track_greenlet(greenlet_id, greenlet_name, frame);
+    Py_END_ALLOW_THREADS;
 
     Py_RETURN_NONE;
 }
@@ -223,7 +237,9 @@ untrack_greenlet(PyObject* Py_UNUSED(m), PyObject* args)
     if (!PyArg_ParseTuple(args, "l", &greenlet_id))
         return NULL;
 
+    Py_BEGIN_ALLOW_THREADS;
     Sampler::get().untrack_greenlet(greenlet_id);
+    Py_END_ALLOW_THREADS;
 
     Py_RETURN_NONE;
 }
@@ -236,7 +252,9 @@ link_greenlets(PyObject* Py_UNUSED(m), PyObject* args)
     if (!PyArg_ParseTuple(args, "ll", &child, &parent))
         return NULL;
 
+    Py_BEGIN_ALLOW_THREADS;
     Sampler::get().link_greenlets(parent, child);
+    Py_END_ALLOW_THREADS;
 
     Py_RETURN_NONE;
 }
@@ -250,7 +268,9 @@ update_greenlet_frame(PyObject* Py_UNUSED(m), PyObject* args)
     if (!PyArg_ParseTuple(args, "lO", &greenlet_id, &frame))
         return NULL;
 
+    Py_BEGIN_ALLOW_THREADS;
     Sampler::get().update_greenlet_frame(greenlet_id, frame);
+    Py_END_ALLOW_THREADS;
 
     Py_RETURN_NONE;
 }
