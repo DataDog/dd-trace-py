ec94439de01615005d29712562553b54bafe4884
Thomas Kowalski <thomas.kowalski@datadoghq.com>
2025-10-20 15:39:22 +0200

    profiling: remove unneeded files from echion

--- ddtrace/internal/datadog/profiling/echion/tests/target_where.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-
-
-def main():
-    sleep(2)
-
-
-if __name__ == "__main__":
-    main()
--- ddtrace/internal/datadog/profiling/echion/tests/test_asyncio_deadlock.py
+++
@@ -1,6 +0,0 @@
-from tests.utils import run_target
-
-
-def test_asyncio_deadlock():
-    result, data = run_target("target_async_deadlock")
-    assert result.returncode == 0, result.stderr.decode()
