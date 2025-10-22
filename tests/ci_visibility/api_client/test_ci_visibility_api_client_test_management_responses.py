2c18cccb63ac5078274007122cf8271b8f26395f
Vítor De Araújo <vitor.dearaujo@datadoghq.com>
2025-10-22 12:55:28 +0000

    fix some tests

--- tests/ci_visibility/api_client/test_ci_visibility_api_client_test_management_responses.py
+++ tests/ci_visibility/api_client/test_ci_visibility_api_client_test_management_responses.py
@@ -119,6 +119,8 @@ class TestTestVisibilityAPIClientTestManagementResponses(TestTestVisibilityAPICl
     def test_api_client_test_management_tests_errors(self, do_request_side_effect):
         """Tests that the client correctly handles errors in the Test Management test API response"""
         client = self._get_test_client()
-        with mock.patch.object(client, "_do_request", side_effect=[do_request_side_effect]):
+        with mock.patch.object(client, "_do_request", side_effect=do_request_side_effect), mock.patch(
+            "ddtrace.internal.utils.retry.sleep"
+        ):
             settings = client.fetch_test_management_tests(read_from_cache=False)
             assert settings is None
