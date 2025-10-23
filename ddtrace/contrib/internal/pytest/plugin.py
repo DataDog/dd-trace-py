6706409021458e40ca2485ce1ce08cd8470ee3db
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2025-10-23 11:25:12 +0100

    fix(pytest): disable module cloning
    We prevent module cloning from triggering in pytest sessions as it is
    likely to happen at the wrong time, potentially causing module import
    issues.

--- ddtrace/contrib/internal/pytest/plugin.py
+++ ddtrace/contrib/internal/pytest/plugin.py
@@ -120,6 +120,12 @@ def pytest_addoption(parser):
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "dd_tags(**kwargs): add tags to current span")
+
+    # Disable module cloning in test runs
+    import ddtrace.bootstrap.cloning as cloning
+
+    cloning.enabled = False
+
     if is_enabled(config):
         _disable_telemetry_dependency_collection()
         _versioned_pytest_configure(config)
