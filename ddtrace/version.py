22cb85c860bb574622cd29733cbbf7b8d05f20dd
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2025-10-23 10:23:09 +0200

    Hardcode 3.14.5 version for this branch
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/version.py
+++ ddtrace/version.py
@@ -1,13 +1,3 @@
 def get_version() -> str:
-    try:
-        from ._version import version
-
-        return version
-    except ImportError:
-        from importlib.metadata import version as ilm_version
-
-        try:
-            return ilm_version("ddtrace")
-        except ModuleNotFoundError:
-            # package is not installed
-            return "dev"
+    # Hardcoded version for this branch
+    return "3.14.5"
