c3f7622cfdcb1033b6e6f4acac530b7415f238ff
Munir Abdinur <munir.abdinur@datadoghq.com>
2025-10-23 12:38:58 -0400

    Update tests/integration/test_integration.py
    Co-authored-by: erikayasuda <153395705+erikayasuda@users.noreply.github.com>

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -809,7 +809,7 @@ def test_logging_during_tracer_init_succeeds_when_debug_logging_and_logs_injecti
 
     assert (
         b"[dd.service=ddtrace_subprocess_dir dd.env= dd.version= dd.trace_id=0 dd.span_id=0]" not in err
-    ), "stderr should contain debug output when DD_TRACE_DEBUG is set"
+    ), "stderr should not contain debug output when DD_TRACE_DEBUG is set"
 
     assert b"KeyError: 'dd.service'" not in err, "stderr should not contain any exception logs"
     assert (
