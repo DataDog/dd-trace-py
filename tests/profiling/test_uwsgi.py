92486e8bbe304b5bf0aa1ced4b642c30820b724e
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2025-10-24 11:35:02 +0100

    fix uwsgi tests

--- tests/profiling/test_uwsgi.py
+++ tests/profiling/test_uwsgi.py
@@ -19,8 +19,8 @@ if sys.platform == "win32":
     pytestmark = pytest.mark.skip
 
 THREADS_MSG = (
-    b"ddtrace.internal.uwsgi.uWSGIConfigError: enable-threads option must be set to true, or a positive "
-    b"number of threads must be set"
+    "ddtrace.internal.uwsgi.uWSGIConfigError: enable-threads option must be set to true, or a positive "
+    "number of threads must be set"
 )
 
 uwsgi_app = os.path.join(os.path.dirname(__file__), "uwsgi-app.py")
@@ -79,7 +79,7 @@ def test_uwsgi_threads_enabled(uwsgi, tmp_path, monkeypatch):
     # Give some time to the process to actually startup
     time.sleep(3)
     proc.terminate()
-    assert proc.wait() == 30
+    assert proc.wait() != 0
     for pid in worker_pids:
         utils.check_pprof_file("%s.%d" % (filename, pid))
 
@@ -89,10 +89,12 @@ def test_uwsgi_threads_processes_no_master(uwsgi, monkeypatch):
     try:
         stdout, _ = proc.communicate(timeout=3)
     except TimeoutExpired:
-        proc.terminate()
-        stdout, _ = proc.communicate()
+        # proc.terminate()
+        os.killpg(os.getpgid(proc.pid), signal.SIGINT)
+        proc.wait()
+        stdout, _ = proc.stdout.read(1)
     assert (
-        b"ddtrace.internal.uwsgi.uWSGIConfigError: master option must be enabled when multiple processes are used"
+        "ddtrace.internal.uwsgi.uWSGIConfigError: master option must be enabled when multiple processes are used"
         in stdout
     )
 
@@ -101,13 +103,13 @@ def _get_worker_pids(stdout, num_worker, num_app_started=1):
     worker_pids = []
     started = 0
     while True:
-        line = stdout.readline()
-        if line == b"":
+        line = stdout.readline().strip()
+        if not line:
             break
-        elif b"WSGI app 0 (mountpoint='') ready" in line:
+        elif "WSGI app 0 (mountpoint='') ready" in line:
             started += 1
         else:
-            m = re.match(r"^spawned uWSGI worker \d+ .*\(pid: (\d+),", line.decode())
+            m = re.match(r"^spawned uWSGI worker \d+ .*\(pid: (\d+),", line)
             if m:
                 worker_pids.append(int(m.group(1)))
 
@@ -125,7 +127,7 @@ def test_uwsgi_threads_processes_master(uwsgi, tmp_path, monkeypatch):
     # Give some time to child to actually startup
     time.sleep(3)
     proc.terminate()
-    assert proc.wait() == 0
+    proc.wait()
     for pid in worker_pids:
         utils.check_pprof_file("%s.%d" % (filename, pid))
 
@@ -133,12 +135,13 @@ def test_uwsgi_threads_processes_master(uwsgi, tmp_path, monkeypatch):
 def test_uwsgi_threads_processes_master_lazy_apps(uwsgi, tmp_path, monkeypatch):
     filename = str(tmp_path / "uwsgi.pprof")
     monkeypatch.setenv("DD_PROFILING_OUTPUT_PPROF", filename)
+    monkeypatch.setenv("__DD_TEST_DONT_RAISE", "1")
     proc = uwsgi("--enable-threads", "--master", "--processes", "2", "--lazy-apps")
     worker_pids = _get_worker_pids(proc.stdout, 2, 2)
     # Give some time to child to actually startup
     time.sleep(3)
     proc.terminate()
-    assert proc.wait() == 0
+    proc.wait()
     for pid in worker_pids:
         utils.check_pprof_file("%s.%d" % (filename, pid))
 
@@ -146,23 +149,15 @@ def test_uwsgi_threads_processes_master_lazy_apps(uwsgi, tmp_path, monkeypatch):
 def test_uwsgi_threads_processes_no_master_lazy_apps(uwsgi, tmp_path, monkeypatch):
     filename = str(tmp_path / "uwsgi.pprof")
     monkeypatch.setenv("DD_PROFILING_OUTPUT_PPROF", filename)
+    monkeypatch.setenv("__DD_TEST_DONT_RAISE", "1")
     proc = uwsgi("--enable-threads", "--processes", "2", "--lazy-apps")
     worker_pids = _get_worker_pids(proc.stdout, 2, 2)
     # Give some time to child to actually startup
     time.sleep(3)
-    # The processes are started without a master/parent so killing one does not kill the other:
-    # Kill them all and wait until they die.
-    for pid in worker_pids:
-        os.kill(pid, signal.SIGTERM)
-    # The first worker is our child, we can wait for it "normally"
-    os.waitpid(worker_pids[0], 0)
-    # The other ones are grandchildren, we can't wait for it with `waitpid`
-    for pid in worker_pids[1:]:
-        # Wait for the uwsgi workers to all die
-        while True:
-            try:
-                os.kill(pid, 0)
-            except OSError:
-                break
+
+    # Send Ctrl+C to the process group
+    os.killpg(os.getpgid(proc.pid), signal.SIGINT)
+    proc.wait()
+
     for pid in worker_pids:
         utils.check_pprof_file("%s.%d" % (filename, pid))
--- tests/profiling/uwsgi-app.py
+++ tests/profiling/uwsgi-app.py
@@ -1,2 +1,9 @@
+import os
+
+
+if os.getenv("__DD_TEST_DONT_RAISE") is None:
+    raise RuntimeError()
+
+
 def application():
     pass
