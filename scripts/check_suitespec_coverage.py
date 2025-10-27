d0cb6196e3bac28c0a68bac19d1424ef6ceaaba1
Emmett Butler <emmett.butler321@gmail.com>
2025-10-27 14:51:12 -0400

    undo versioning changes

--- scripts/check_suitespec_coverage.py
+++ scripts/check_suitespec_coverage.py
@@ -26,7 +26,6 @@ SPEC_PATTERNS = {_ for suite in spec.get_suites() for _ in spec.get_patterns(sui
 IGNORE_PATTERNS.add("**/*.md")
 # The aioredis integration is deprecated and untested
 IGNORE_PATTERNS.add("ddtrace/contrib/aioredis/*")
-IGNORE_PATTERNS.add("ddtrace/_version.py")
 
 
 def owners(path: str) -> str:
