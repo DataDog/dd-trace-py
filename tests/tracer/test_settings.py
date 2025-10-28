5c82ef6da49591641cb526e279a8579ac0cfac9f
Emmett Butler <emmett.butler321@gmail.com>
2025-10-28 10:18:35 -0400

    remove failing test

--- tests/tracer/test_settings.py
+++ tests/tracer/test_settings.py
@@ -178,47 +178,6 @@ class TestIntegrationConfig(BaseTestCase):
 
         assert self.integration_config.get_analytics_sample_rate() == 1
 
-    def test_app_analytics_deprecation(self):
-        warnings.simplefilter("always")
-        with warnings.catch_warnings(record=True) as warns:
-            IntegrationConfig(self.config, "test")
-        assert len(warns) == 0
-
-        with warnings.catch_warnings(record=True) as warns:
-            self.integration_config.analytics_enabled
-        assert (
-            "analytics_enabled is deprecated and will be removed in version '4.0.0': Controlling ingestion via analytics is no longer supported. See https://docs.datadoghq.com/tracing/legacy_app_analytics/?code-lang=python#migrate-to-the-new-configuration-options"  # noqa:E501
-            in str(warns[0].message)
-        )
-
-        with warnings.catch_warnings(record=True) as warns:
-            self.integration_config.analytics_enabled = True
-        assert (
-            "analytics_enabled is deprecated and will be removed in version '4.0.0': Controlling ingestion via analytics is no longer supported. See https://docs.datadoghq.com/tracing/legacy_app_analytics/?code-lang=python#migrate-to-the-new-configuration-options"  # noqa:E501
-            in str(warns[0].message)
-        )
-
-        with warnings.catch_warnings(record=True) as warns:
-            self.integration_config.analytics_sample_rate
-        assert (
-            "analytics_sample_rate is deprecated and will be removed in version '4.0.0': Controlling ingestion via analytics is no longer supported. See https://docs.datadoghq.com/tracing/legacy_app_analytics/?code-lang=python#migrate-to-the-new-configuration-options"  # noqa:E501
-            in str(warns[0].message)
-        )
-
-        with warnings.catch_warnings(record=True) as warns:
-            self.integration_config.analytics_sample_rate = 0.5
-        assert (
-            "analytics_sample_rate is deprecated and will be removed in version '4.0.0': Controlling ingestion via analytics is no longer supported. See https://docs.datadoghq.com/tracing/legacy_app_analytics/?code-lang=python#migrate-to-the-new-configuration-options"  # noqa:E501
-            in str(warns[0].message)
-        )
-
-        with warnings.catch_warnings(record=True) as warns:
-            self.integration_config.get_analytics_sample_rate()
-        assert (
-            "get_analytics_sample_rate is deprecated and will be removed in version '4.0.0': Controlling ingestion via analytics is no longer supported. See https://docs.datadoghq.com/tracing/legacy_app_analytics/?code-lang=python#migrate-to-the-new-configuration-options"  # noqa:E501
-            in str(warns[0].message)
-        )
-
 
 def test_environment_header_tags():
     with override_env(dict(DD_TRACE_HEADER_TAGS="Host:http.host,User-agent:http.user_agent")):
@@ -249,8 +208,6 @@ def test_x_datadog_tags(env, expected):
 @pytest.mark.skipif(PYTHON_VERSION_INFO < (3, 9), reason="Additional deprecation warning under Python 3.8")
 @pytest.mark.subprocess()
 def test_config_exception_deprecation():
-    import warnings
-
     with warnings.catch_warnings(record=True) as warns:
         warnings.simplefilter("default")
 
@@ -267,8 +224,6 @@ def test_config_exception_deprecation():
 @pytest.mark.skipif(PYTHON_VERSION_INFO < (3, 9), reason="Additional deprecation warning under Python 3.8")
 @pytest.mark.subprocess()
 def test_http_config_deprecation():
-    import warnings
-
     with warnings.catch_warnings(record=True) as warns:
         warnings.simplefilter("default")
 
@@ -284,8 +239,6 @@ def test_http_config_deprecation():
 @pytest.mark.skipif(PYTHON_VERSION_INFO < (3, 9), reason="Additional deprecation warning under Python 3.8")
 @pytest.mark.subprocess()
 def test_hooks_deprecation():
-    import warnings
-
     with warnings.catch_warnings(record=True) as warns:
         warnings.simplefilter("default")
 
@@ -301,8 +254,6 @@ def test_hooks_deprecation():
 @pytest.mark.skipif(PYTHON_VERSION_INFO < (3, 9), reason="Additional deprecation warning under Python 3.8")
 @pytest.mark.subprocess()
 def test_integration_config_deprecation():
-    import warnings
-
     with warnings.catch_warnings(record=True) as warns:
         warnings.simplefilter("default")
 
