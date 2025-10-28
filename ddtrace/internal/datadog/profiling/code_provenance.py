99008381a305bb47204fccf1efb98bd4337256e0
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-10-28 15:09:29 -0400

    perf(profiling): initialize code provenance on import

--- ddtrace/internal/datadog/profiling/code_provenance.py
+++ ddtrace/internal/datadog/profiling/code_provenance.py
@@ -103,6 +103,10 @@ class CodeProvenance:
         return {"v1": [lib.to_dict() for lib in self.libraries]}
 
 
+# Initialize once at import to avoid reinitializing this in every child process.
+# This can add up and show up as a significant overhead.
+_code_provenance_instance = CodeProvenance()
+
+
 def json_str_to_export() -> str:
-    cp = CodeProvenance()
-    return json.dumps(cp.to_dict())
+    return json.dumps(_code_provenance_instance.to_dict())
