c598a5fcb457a96830cf994889ed5f41be8ef9a7
brettlangdon <brett.langdon@datadoghq.com>
2025-10-31 10:51:56 -0400

    chore(ci): remove build_docs cache

--- scripts/gen_gitlab_config.py
+++ scripts/gen_gitlab_config.py
@@ -190,33 +190,23 @@ def gen_build_docs() -> None:
 
     if pr_matches_patterns(
         {
-            "docker*",
             "docs/*",
             "ddtrace/*",
             "scripts/docs/*",
-            "releasenotes/*",
             "benchmarks/README.rst",
             ".readthedocs.yml",
         }
     ):
-        date_str = datetime.datetime.now().strftime("%Y-%m")
-
         with TESTS_GEN.open("a") as f:
             print("build_docs:", file=f)
             print("  extends: .testrunner", file=f)
             print("  stage: core", file=f)
             print("  needs:", file=f)
             print("    - prechecks", file=f)
-            print("  variables:", file=f)
-            print("    PIP_CACHE_DIR: '${CI_PROJECT_DIR}/.cache/pip'", file=f)
             print("  script:", file=f)
             print("    - |", file=f)
             print("      hatch run docs:build", file=f)
             print("      mkdir -p /tmp/docs", file=f)
-            print("  cache:", file=f)
-            print(f"    key: build_docs-pip-cache-{date_str}", file=f)
-            print("    paths:", file=f)
-            print("      - .cache", file=f)
             print("  artifacts:", file=f)
             print("    paths:", file=f)
             print("      - 'docs/'", file=f)
