3e9c7df6fd281a0f542e5c8ed3a3c14eda9d5656
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-11-05 14:08:45 -0500

    feat(profiling): dont enable v1 profiler (#15108)
    ## Description
    
    <!-- Provide an overview of the change and motivation for the change -->
    
    ## Testing
    
    <!-- Describe your testing strategy or note what tests are included -->
    
    ## Risks
    
    <!-- Note any risks associated with this change, or "None" if no risks
    -->
    
    ## Additional Notes
    
    <!-- Any other information that would be helpful for reviewers -->
    
    Co-authored-by: Emmett Butler <723615+emmettbutler@users.noreply.github.com>

--- ddtrace/internal/settings/profiling.py
+++ ddtrace/internal/settings/profiling.py
@@ -85,31 +85,6 @@ def _parse_profiling_enabled(raw: str) -> bool:
     return False
 
 
-def _parse_v2_enabled(raw: str) -> bool:
-    if sys.version_info >= (3, 14):
-        return False
-
-    # Parse the boolean value
-    raw_lc = raw.lower()
-    enabled = raw_lc in ("1", "true", "yes", "on")
-
-    # Warn if user explicitly disabled v2 profiler (v1 is deprecated)
-    if raw_lc in ("false", "0", "no", "off"):
-        from ddtrace.internal.utils.deprecations import DDTraceDeprecationWarning
-        from ddtrace.vendor.debtcollector import deprecate
-
-        deprecate(
-            "Setting DD_PROFILING_STACK_V2_ENABLED=false is deprecated",
-            message="The v1 stack profiler is deprecated and will be removed in a future version. "
-            "Please migrate to the v2 stack profiler by removing DD_PROFILING_STACK_V2_ENABLED=false "
-            "or setting it to true.",
-            category=DDTraceDeprecationWarning,
-            removal_version="4.0.0",
-        )
-
-    return enabled
-
-
 def _parse_api_timeout_ms(raw: str) -> int:
     # Check if the deprecated DD_PROFILING_API_TIMEOUT is set (in seconds)
     deprecated_timeout = os.environ.get("DD_PROFILING_API_TIMEOUT")
@@ -308,7 +283,6 @@ class ProfilingConfigStack(DDConfig):
     _v2_enabled = DDConfig.v(
         bool,
         "v2_enabled",
-        parser=_parse_v2_enabled,
         # Not yet supported on 3.14
         default=sys.version_info < (3, 14),
         help_type="Boolean",
@@ -441,9 +415,11 @@ if config.stack.v2_enabled and not stack_v2_is_available:
     logger.warning("Failed to load stack_v2 module (%s), falling back to v1 stack sampler", msg)
     telemetry_writer.add_log(
         TELEMETRY_LOG_LEVEL.ERROR,
-        "Failed to load stack_v2 module (%s), falling back to v1 stack sampler" % msg,
+        "Failed to load stack_v2 module (%s), disabling profiling" % msg,
     )
     config.stack.v2_enabled = False
+    config.stack.enabled = False
+    config.enabled = False
 
 # Enrich tags with git metadata and DD_TAGS
 config.tags = _enrich_tags(config.tags)
