98786d48c7d95cc8b3b84c606bac62febaa5e18c
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-11-05 14:49:46 -0500

    use unpatched lock

--- ddtrace/internal/forksafe.py
+++ ddtrace/internal/forksafe.py
@@ -5,11 +5,12 @@ An API to provide fork-safe functions.
 import functools
 import logging
 import os
-import threading
 import typing
 import weakref
 
 import wrapt
+
+from ddtrace.internal._unpatched import _threading as unpatched_threading
 
 
 log = logging.getLogger(__name__)
@@ -138,13 +139,13 @@ class ResetObject(wrapt.ObjectProxy, typing.Generic[_T]):
         self.__wrapped__ = self._self_wrapped_class()
 
 
-def Lock() -> threading.Lock:
-    return ResetObject(threading.Lock)  # type: ignore
+def Lock() -> unpatched_threading.Lock:
+    return ResetObject(unpatched_threading.Lock)  # type: ignore
 
 
-def RLock() -> threading.RLock:
-    return ResetObject(threading.RLock)  # type: ignore
+def RLock() -> unpatched_threading.RLock:
+    return ResetObject(unpatched_threading.RLock)  # type: ignore
 
 
-def Event() -> threading.Event:
-    return ResetObject(threading.Event)  # type: ignore
+def Event() -> unpatched_threading.Event:
+    return ResetObject(unpatched_threading.Event)  # type: ignore
