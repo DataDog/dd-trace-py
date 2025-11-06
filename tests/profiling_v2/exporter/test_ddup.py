4ce9048279caf8bd8bbc0cfd259d18ee2fa8b12c
Louis Tricot <louis.tricot@datadoghq.com>
2025-11-06 12:08:06 +0100

    chore(profiler): add process_tags to profiler tags

--- tests/profiling_v2/exporter/test_ddup.py
+++ tests/profiling_v2/exporter/test_ddup.py
@@ -62,3 +62,63 @@ def test_tags_propagated():
     # Profiler could add tags, so check that tags is a superset of config.tags
     for k, v in config.tags.items():
         assert tags[k] == v
+
+
+@pytest.mark.subprocess()
+def test_process_tags_deactivated():
+    import sys
+    from unittest import mock
+
+    sys.modules["ddtrace.internal.datadog.profiling.ddup"] = mock.Mock()
+
+    from ddtrace.profiling.profiler import Profiler  # noqa: I001
+    from ddtrace.internal.datadog.profiling import ddup
+
+    # When Profiler is instantiated and libdd is enabled, it should call ddup.config
+    Profiler()
+
+    ddup.config.assert_called()
+
+    tags = ddup.config.call_args.kwargs["tags"]
+
+    assert "process_tags" not in tags
+
+
+@pytest.mark.subprocess(
+    env=dict(
+        DD_EXPERIMENTAL_PROPAGATE_PROCESS_TAGS_ENABLED="true",
+    )
+)
+def test_process_tags_activated():
+    import sys
+    from unittest import mock
+
+    sys.modules["ddtrace.internal.datadog.profiling.ddup"] = mock.Mock()
+
+    from ddtrace.profiling.profiler import Profiler  # noqa: I001
+    from ddtrace.internal.datadog.profiling import ddup
+    from ddtrace.internal.process_tags import _process_tag_reload
+
+    from ddtrace.internal.process_tags.constants import ENTRYPOINT_BASEDIR_TAG
+    from ddtrace.internal.process_tags.constants import ENTRYPOINT_NAME_TAG
+    from ddtrace.internal.process_tags.constants import ENTRYPOINT_TYPE_SCRIPT
+    from ddtrace.internal.process_tags.constants import ENTRYPOINT_TYPE_TAG
+    from ddtrace.internal.process_tags.constants import ENTRYPOINT_WORKDIR_TAG
+
+    with mock.patch("sys.argv", ["/path/to/test_script.py"]), mock.patch("os.getcwd", return_value="/path/to/workdir"):
+        _process_tag_reload()
+
+        # When Profiler is instantiated and libdd is enabled, it should call ddup.config
+        Profiler()
+
+        ddup.config.assert_called()
+
+        tags = ddup.config.call_args.kwargs["tags"]
+
+        assert "process_tags" in tags
+        process_tags = dict(tag.split(":", 1) for tag in tags["process_tags"].split(","))
+
+        assert process_tags[ENTRYPOINT_BASEDIR_TAG] == "to"
+        assert process_tags[ENTRYPOINT_NAME_TAG] == "test_script"
+        assert process_tags[ENTRYPOINT_TYPE_TAG] == ENTRYPOINT_TYPE_SCRIPT
+        assert process_tags[ENTRYPOINT_WORKDIR_TAG] == "workdir"
