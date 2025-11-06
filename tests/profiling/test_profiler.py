907193d8e90e40b1e8517ca91bfb1625547ac2e5
Louis Tricot <louis.tricot@datadoghq.com>
2025-11-06 12:08:06 +0100

    chore(profiler): add process_tags to profiler tags

--- tests/profiling/test_profiler.py
+++ tests/profiling/test_profiler.py
@@ -5,12 +5,19 @@ from unittest import mock
 import pytest
 
 import ddtrace
+from ddtrace.internal.process_tags import _process_tag_reload
+from ddtrace.internal.process_tags.constants import ENTRYPOINT_BASEDIR_TAG
+from ddtrace.internal.process_tags.constants import ENTRYPOINT_NAME_TAG
+from ddtrace.internal.process_tags.constants import ENTRYPOINT_TYPE_SCRIPT
+from ddtrace.internal.process_tags.constants import ENTRYPOINT_TYPE_TAG
+from ddtrace.internal.process_tags.constants import ENTRYPOINT_WORKDIR_TAG
 from ddtrace.profiling import collector
 from ddtrace.profiling import profiler
 from ddtrace.profiling import scheduler
 from ddtrace.profiling.collector import asyncio
 from ddtrace.profiling.collector import stack
 from ddtrace.profiling.collector import threading
+from ddtrace.settings._config import config
 
 
 def test_status():
@@ -144,3 +151,31 @@ def test_profiler_serverless(monkeypatch):
     p = profiler.Profiler()
     assert isinstance(p._scheduler, scheduler.ServerlessScheduler)
     assert p.tags["functionname"] == "foobar"
+
+
+def test_process_tags_deactivated():
+    p = profiler.Profiler(tags={})
+    assert "process_tags" not in p.tags
+
+
+def test_process_tags_activated():
+    # type: (...) -> None
+    with mock.patch("sys.argv", ["/path/to/test_script.py"]), mock.patch("os.getcwd", return_value="/path/to/workdir"):
+        try:
+            config._process_tags_enabled = True
+            _process_tag_reload()
+
+            # Pass explicit tags dict to avoid mutating shared profiling_config.tags
+            p = profiler.Profiler(tags={})
+
+            # Verify that process_tags are in the profiler tags
+            assert "process_tags" in p.tags
+            process_tags = dict(tag.split(":", 1) for tag in p.tags["process_tags"].split(","))
+
+            assert process_tags[ENTRYPOINT_BASEDIR_TAG] == "to"
+            assert process_tags[ENTRYPOINT_NAME_TAG] == "test_script"
+            assert process_tags[ENTRYPOINT_TYPE_TAG] == ENTRYPOINT_TYPE_SCRIPT
+            assert process_tags[ENTRYPOINT_WORKDIR_TAG] == "workdir"
+        finally:
+            config._process_tags_enabled = False
+            _process_tag_reload()
