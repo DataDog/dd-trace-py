c412a40961762880427ab07c7112db8ac415be49
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-11-10 17:28:05 -0500

    format

--- ddtrace/profiling/profiler.py
+++ ddtrace/profiling/profiler.py
@@ -258,7 +258,9 @@ class _ProfilerInstance(service.Service):
 
         self._build_default_exporters()
 
-        scheduler_class = scheduler.ServerlessScheduler if self._lambda_function_name else scheduler.Scheduler  # type: (Type[Union[scheduler.Scheduler, scheduler.ServerlessScheduler]])
+        scheduler_class = (
+            scheduler.ServerlessScheduler if self._lambda_function_name else scheduler.Scheduler
+        )  # type: (Type[Union[scheduler.Scheduler, scheduler.ServerlessScheduler]])
 
         self._scheduler = scheduler_class(
             before_flush=self._collectors_snapshot,
