fd3cca6e55b1d580fda41f9bed05656c0a599cb5
Daniel Schwartz-Narbonne <daniel.schwartznarbonne@datadoghq.com>
2025-11-11 12:18:34 -0500

    setup.py type annotations

--- setup.py
+++ setup.py
@@ -702,8 +702,9 @@ class CustomBuildExt(build_ext):
         else:
             # For the memalloc extension, dynamically add libdd_wrapper to extra_objects
             if ext.name == "ddtrace.profiling.collector._memalloc" and CURRENT_OS in ("Linux", "Darwin"):
-                dd_wrapper_suffix = sysconfig.get_config_var("EXT_SUFFIX")
+                dd_wrapper_suffix: t.Optional[str] = sysconfig.get_config_var("EXT_SUFFIX")
 
+                wrapper_dir: Path
                 if IS_EDITABLE or getattr(self, "inplace", False):
                     # Editable build: use source directory
                     wrapper_dir = Path(__file__).parent / "ddtrace" / "internal" / "datadog" / "profiling"
@@ -713,9 +714,9 @@ class CustomBuildExt(build_ext):
                         Path(__file__).parent / Path(self.build_lib) / "ddtrace" / "internal" / "datadog" / "profiling"
                     )
 
-                wrapper_path = wrapper_dir / f"libdd_wrapper{dd_wrapper_suffix}"
+                wrapper_path: Path = wrapper_dir / f"libdd_wrapper{dd_wrapper_suffix}"
                 if wrapper_path.exists():
-                    wrapper_path_str = str(wrapper_path)
+                    wrapper_path_str: str = str(wrapper_path)
                     if wrapper_path_str not in ext.extra_objects:
                         ext.extra_objects.append(wrapper_path_str)
 
