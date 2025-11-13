b521da39789d24e02fdf9345e2d87dd01a2f4f10
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-11-13 00:55:44 -0500

    minimize diff

--- tests/profiling_v2/test_uwsgi.py
+++ tests/profiling_v2/test_uwsgi.py
@@ -26,7 +26,9 @@ uwsgi_app = os.path.join(os.path.dirname(__file__), "..", "profiling", "uwsgi-ap
 
 
 @pytest.fixture
-def uwsgi(tmp_path):
+def uwsgi(monkeypatch, tmp_path):
+    # Do not ignore profiler so we have samples in the output pprof
+    monkeypatch.setenv("DD_PROFILING_IGNORE_PROFILER", "0")
     # Do not use pytest tmpdir fixtures which generate directories longer than allowed for a socket file name
     socket_name = str(tmp_path / "uwsgi.sock")
     import os
