6097ce954d25b8fe1a3892de50ceb999a0d8ccfb
brettlangdon <brett.langdon@datadoghq.com>
2025-11-13 09:55:20 -0500

    don't try to use gha artifacts

--- .gitlab/benchmarks/steps/build-baseline.sh
+++ .gitlab/benchmarks/steps/build-baseline.sh
@@ -4,6 +4,7 @@ set -e -o pipefail
 # If we have a tag (e.g. v2.21.1), then use the PyPI published wheel
 if [[ -n "${BASELINE_TAG}" ]];
 then
+  echo "Downloading wheel for tag ${BASELINE_TAG} from PyPI"
   python3.9 -m pip download --no-deps "ddtrace==${BASELINE_TAG:1}"
   exit 0
 fi
@@ -13,18 +14,14 @@ fi
 if [[ -n "${BASELINE_COMMIT_SHA}" ]];
 then
   # Try to download the wheel from the public S3 bucket if we have one
-  .gitlab/scripts/get-pipelines-for-ref.sh "${BASELINE_COMMIT_SHA}" || true
+  PIPELINE_ID=$(.gitlab/scripts/get-pipelines-for-ref.sh "${BASELINE_COMMIT_SHA}" | jq -r 'if length > 0 then .[0].id else empty end')
 
-  exit 1
-  # CI_COMMIT_SHA="${BASELINE_COMMIT_SHA}" .gitlab/download-wheels-from-gh-actions.sh || true
-
-  # If we have a ./pywheels/ for cp39 manylinux x86_64 use that wheel
-  # if [ -d "./pywheels" ] && ls ./pywheels/ddtrace-*cp39*manylinux* | grep -q "x86_64";
-  # then
-  #   echo "Using prebuilt wheel from GitHub Actions for baseline commit ${BASELINE_COMMIT_SHA}"
-  #   cp ./pywheels/ddtrace-*cp39*manylinux*x86_64.whl ./
-  #   exit 0
-  # fi
+  if [[ -n "${PIPELINE_ID}" ]];
+  then
+    echo "Downloading prebuilt wheel for ${BASELINE_BRANCH}:${BASELINE_COMMIT_SHA} from pipeline ${PIPELINE_ID}"
+    python3.9 -m pip download --no-deps --index-url "https://dd-trace-py-builds.s3.amazonaws.com/${PIPELINE_ID}/" "ddtrace"
+    exit 0
+  fi
 fi
 
 # Otherwise, build from source
--- .gitlab/scripts/get-pipelines-for-ref.sh
+++ .gitlab/scripts/get-pipelines-for-ref.sh
@@ -6,13 +6,12 @@ set -euo pipefail
 #
 # Example:
 #   ./get-pipelines-for-ref.sh 3dbf82a
-#   ./get-pipelines-for-ref.sh v2.1.0
 #
 # Environment:
 #   GITLAB_TOKEN (required)
 
-GITLAB_API_URL=${GITLAB_API_URL:"https://gitlab.ddbuild.io/api/v4"}
-GITLAB_PROJECT_ID=${GITLAB_PROJECT_ID:"358"}
+GITLAB_API_URL=${GITLAB_API_URL:-"https://gitlab.ddbuild.io/api/v4"}
+GITLAB_PROJECT_ID=${GITLAB_PROJECT_ID:-"358"}
 
 if [[ $# -ne 1 ]]; then
   echo "usage: $0 <commit-sha-or-tag>" >&2
@@ -26,6 +25,6 @@ fi
 
 REF="$1"
 
-curl --fail --silent --show-error \
+curl --show-error --silent --fail \
   --header "PRIVATE-TOKEN: ${GITLAB_TOKEN}" \
-  "${GITLAB_API_URL}/projects/${GITLAB_PROJECT_ID}/pipelines?ref=${REF}"
+  "${GITLAB_API_URL}/projects/${GITLAB_PROJECT_ID}/pipelines?sha=${REF}"
