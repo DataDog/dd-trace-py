e17694171b7da8b9cffbf45c545f38cf62958117
brettlangdon <brett.langdon@datadoghq.com>
2025-11-17 12:07:38 -0500

    fix package version and add verification

--- ddtrace/version.py
+++ ddtrace/version.py
@@ -19,5 +19,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "4.1.0dev"
+__version__ = version = "4.1.0.dev0"
 __version_tuple__ = version_tuple = (4, 1, 0, "dev0")
