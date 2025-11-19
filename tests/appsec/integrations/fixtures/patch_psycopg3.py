62b2c7ee261c738450819d770e85a54fa3a865d4
Alberto Vara <alberto.vara@datadoghq.com>
2025-11-19 15:21:35 +0100

    ci(iast): add psycopg3 tests

--- tests/appsec/integrations/fixtures/patch_psycopg3.py
+++ tests/appsec/integrations/fixtures/patch_psycopg3.py
@@ -11,6 +11,6 @@ def adapt_list(obj_list):
             items.append(str(item).lower())
         else:
             items.append(str(item))
-    
+
     array_str = "{" + ",".join(items) + "}"
     return b"r-" + array_str.encode()
