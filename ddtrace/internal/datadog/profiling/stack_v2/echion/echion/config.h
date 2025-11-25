c595d4db66d2f29843f559d837a2a26872a7f6e5
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-11-24 23:22:57 -0500

    remove

--- ddtrace/internal/datadog/profiling/stack_v2/echion/echion/config.h
+++ ddtrace/internal/datadog/profiling/stack_v2/echion/echion/config.h
@@ -4,12 +4,5 @@
 
 #pragma once
 
-// CPU Time mode
-inline int cpu = 1;
-
-// For cpu time mode, Echion only unwinds threads that're running by default.
-// Set this to false to unwind all threads.
-inline bool ignore_non_running_threads = false;
-
 // Maximum number of frames to unwind
 inline unsigned int max_frames = 2048;
