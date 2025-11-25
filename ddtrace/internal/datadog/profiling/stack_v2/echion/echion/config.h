f4f1833ef44c0cb23064cf428c991249bb5eb413
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-11-25 09:07:39 -0500

    chore(profiling): remove config variables that are no longer needed (#15410)
    ## Description
    
    In `sampler.cpp` we've been setting echion's `cpu` and
    `ignore_non_running_threads` to following values.
    
    `cpu`: `true`
    `ignore_non_running_threads`: `false`
    
    Now that we no longer need to keep these as we put echion source code in
    dd-trace-py, we can simply assume such values and remove dead code.
    
    ## Testing
    
    <!-- Describe your testing strategy or note what tests are included -->
    
    ## Risks
    
    <!-- Note any risks associated with this change, or "None" if no risks
    -->
    
    ## Additional Notes
    
    <!-- Any other information that would be helpful for reviewers -->

--- ddtrace/internal/datadog/profiling/stack_v2/echion/echion/config.h
+++ ddtrace/internal/datadog/profiling/stack_v2/echion/echion/config.h
@@ -4,26 +4,5 @@
 
 #pragma once
 
-// CPU Time mode
-inline int cpu = 0;
-
-// For cpu time mode, Echion only unwinds threads that're running by default.
-// Set this to false to unwind all threads.
-inline bool ignore_non_running_threads = true;
-
 // Maximum number of frames to unwind
 inline unsigned int max_frames = 2048;
-
-// ----------------------------------------------------------------------------
-inline void
-_set_cpu(int new_cpu)
-{
-    cpu = new_cpu;
-}
-
-// ----------------------------------------------------------------------------
-inline void
-_set_ignore_non_running_threads(bool new_ignore_non_running_threads)
-{
-    ignore_non_running_threads = new_ignore_non_running_threads;
-}
