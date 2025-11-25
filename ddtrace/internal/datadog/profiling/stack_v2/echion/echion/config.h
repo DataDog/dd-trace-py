31d59f9dba59f545971fb5c8338e79e47dac1846
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-11-24 23:21:04 -0500

    chore(profiling): remove config variables that are no longer needed

--- ddtrace/internal/datadog/profiling/stack_v2/echion/echion/config.h
+++ ddtrace/internal/datadog/profiling/stack_v2/echion/echion/config.h
@@ -5,25 +5,11 @@
 #pragma once
 
 // CPU Time mode
-inline int cpu = 0;
+inline int cpu = 1;
 
 // For cpu time mode, Echion only unwinds threads that're running by default.
 // Set this to false to unwind all threads.
-inline bool ignore_non_running_threads = true;
+inline bool ignore_non_running_threads = false;
 
 // Maximum number of frames to unwind
 inline unsigned int max_frames = 2048;
-
-// ----------------------------------------------------------------------------
-inline void
-_set_cpu(int new_cpu)
-{
-    cpu = new_cpu;
-}
-
-// ----------------------------------------------------------------------------
-inline void
-_set_ignore_non_running_threads(bool new_ignore_non_running_threads)
-{
-    ignore_non_running_threads = new_ignore_non_running_threads;
-}
