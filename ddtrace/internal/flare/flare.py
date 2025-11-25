750871480f83dbccd5be0c2bd5925edd7932b51d
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2025-11-25 09:45:24 +0100

    Remove get_connection after merge
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- ddtrace/internal/flare/flare.py
+++ ddtrace/internal/flare/flare.py
@@ -17,7 +17,6 @@ from ddtrace.internal.flare.json_formatter import StructuredJSONFormatter
 from ddtrace.internal.logger import get_logger
 from ddtrace.internal.native._native import register_tracer_flare as native_flare  # type: ignore
 from ddtrace.internal.native._native import logger as native_logger
-from ddtrace.internal.utils.http import get_connection
 
 
 TRACER_FLARE_DIRECTORY = "tracer_flare"
