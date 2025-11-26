74951d2556398d86d65d73218b3df44a39d92ea5
Thomas Kowalski <thomas.kowalski@datadoghq.com>
2025-11-26 18:49:06 +0100

    test(profiling): temporarily mark flaky tests as xfail

--- tests/profiling/collector/test_generators.py
+++ tests/profiling/collector/test_generators.py
@@ -1,6 +1,7 @@
 import pytest
 
 
+@pytest.mark.xfail(reason="Temporarily marking new tests as xfail until we make them non-flaky")
 @pytest.mark.subprocess(
     env=dict(
         DD_PROFILING_OUTPUT_PPROF="/tmp/test_generators",
