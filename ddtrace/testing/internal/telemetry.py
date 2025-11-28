dcacb5a235c2e80b736e2a06db66eb9923a7a43b
Vítor De Araújo <vitor.dearaujo@datadoghq.com>
2025-11-28 16:26:09 +0000

    actually send telemetry

--- ddtrace/testing/internal/telemetry.py
+++ ddtrace/testing/internal/telemetry.py
@@ -5,6 +5,9 @@ from enum import Enum
 import logging
 import typing as t
 
+from ddtrace.internal.telemetry import telemetry_writer
+from ddtrace.internal.telemetry.constants import TELEMETRY_NAMESPACE
+
 
 if t.TYPE_CHECKING:
     from ddtrace.testing.internal.http import BackendConnectorSetup
@@ -26,10 +29,10 @@ class TelemetryAPI:
     def __init__(self, connector_setup: BackendConnectorSetup) -> None:
         # DEV: In a beautiful world, this would set up a backend connector to the telemetry endpoint.
         # Currently we rely on ddtrace's telemetry infrastructure, so we don't have to do anything here.
-        pass
+        self.writer = telemetry_writer
 
     def request_metrics(
-        self, count: t.Optional[str], duration: t.Optional[str], response_bytes: t.Optional[str], error: t.Optional[str]
+        self, count: str, duration: str, response_bytes: t.Optional[str], error: str
     ) -> TelemetryAPIRequestMetrics:
         return TelemetryAPIRequestMetrics(
             telemetry_api=self, count=count, duration=duration, response_bytes=response_bytes, error=error
@@ -39,12 +42,36 @@ class TelemetryAPI:
 @dataclasses.dataclass
 class TelemetryAPIRequestMetrics:
     telemetry_api: TelemetryAPI
-    count: t.Optional[str]
-    duration: t.Optional[str]
+    count: str
+    duration: str
     response_bytes: t.Optional[str]
-    error: t.Optional[str]
+    error: str
 
     def record_request(
         self, seconds: float, response_bytes: t.Optional[int], compressed_response: bool, error: t.Optional[ErrorType]
     ) -> None:
-        log.debug("Recording request: %s %s %s %s %s", self.count, seconds, response_bytes, compressed_response, error)
+        log.debug(
+            "Recording Test Optimization telemetry for %s: %s %s %s %s",
+            self.count,
+            seconds,
+            response_bytes,
+            compressed_response,
+            error,
+        )
+        self.telemetry_api.writer.add_count_metric(TELEMETRY_NAMESPACE.CIVISIBILITY, self.count, 1)
+        self.telemetry_api.writer.add_distribution_metric(TELEMETRY_NAMESPACE.CIVISIBILITY, self.duration, seconds)
+        if response_bytes is not None and self.response_bytes is not None:
+            # We don't always want to record response bytes (for settings requests), so assume that no metric name
+            # means we don't want to record it.
+            self.telemetry_api.writer.add_distribution_metric(
+                TELEMETRY_NAMESPACE.CIVISIBILITY, self.response_bytes, response_bytes
+            )
+
+        if error is not None:
+            self.record_error(error)
+
+    def record_error(self, error: ErrorType) -> None:
+        log.debug("Recording Test Optimization request error telemetry: %s", error)
+        self.telemetry_api.writer.add_count_metric(
+            TELEMETRY_NAMESPACE.CIVISIBILITY, self.error, 1, (("error_type", error),)
+        )
