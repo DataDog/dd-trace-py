21e4e98dee894bac6f684918bd9f4432e0fc25f0
Munir Abdinur <munir.abdinur@datadoghq.com>
2025-12-06 00:07:09 -0800

    Update build.sh
    Co-authored-by: Brett Langdon <brett.langdon@datadoghq.com>

--- scripts/docs/build.sh
+++ scripts/docs/build.sh
@@ -5,8 +5,8 @@ if [[ "${READTHEDOCS:-}" = "True" ]]; then
   echo "Skipping spelling check in RTD"
 else
   if [[ "$(uname)" == "Darwin" ]]; then
-    export PYENCHANT_LIBRARY_PATH=/opt/homebrew/lib/libenchant-2.dylib
     brew install enchant
+    export PYENCHANT_LIBRARY_PATH=$(brew --prefix enchant)/lib/libenchant-2.dylib
   fi
   sphinx-build -vvv -W -b spelling docs docs/_build/spelling
 fi
