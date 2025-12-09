ec1788e79a3f0fd9583beddbb4424bb5075a7f33
gary-huang <garyhuang@hotmail.ca>
2025-12-08 22:35:13 -0500

    tests and fmt

--- ddtrace/llmobs/_experiment.py
+++ ddtrace/llmobs/_experiment.py
@@ -23,8 +23,8 @@ from ddtrace.constants import ERROR_STACK
 from ddtrace.constants import ERROR_TYPE
 from ddtrace.internal.logger import get_logger
 from ddtrace.llmobs._constants import DD_SITES_NEEDING_APP_SUBDOMAIN
-from ddtrace.llmobs._constants import EXPERIMENT_RECORD_METADATA
 from ddtrace.llmobs._constants import EXPERIMENT_EXPECTED_OUTPUT
+from ddtrace.llmobs._constants import EXPERIMENT_RECORD_METADATA
 from ddtrace.llmobs._utils import convert_tags_dict_to_list
 from ddtrace.llmobs._utils import safe_json
 from ddtrace.version import __version__
