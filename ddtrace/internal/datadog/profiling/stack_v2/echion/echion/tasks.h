7f3be8aaefbe7cbd5bd530c045e1db64b0ee1880
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-12-10 15:26:13 -0500

    use 1<<16

--- ddtrace/internal/datadog/profiling/stack_v2/echion/echion/tasks.h
+++ ddtrace/internal/datadog/profiling/stack_v2/echion/echion/tasks.h
@@ -313,7 +313,7 @@ get_tasks_from_linked_list(uintptr_t head_addr, PyObject* loop, std::vector<Task
     uintptr_t current_node_addr = head_addr;          // Address of current node
 
     // Copied from CPython's _remote_debugging_module.c: MAX_ITERATIONS
-    const size_t MAX_ITERATIONS = 2 << 15;
+    const size_t MAX_ITERATIONS = 1 << 16;
     size_t iteration_count = 0;
 
     // Iterate over linked-list. The linked list is circular, so we stop
