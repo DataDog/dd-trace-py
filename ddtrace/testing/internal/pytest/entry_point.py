9ec266fd243fd5ac4213b2e7666a3eebc5095dc3
Federico Mon <federico.mon@datadoghq.com>
2025-12-18 10:49:17 +0100

    safely remove env var

--- ddtrace/testing/internal/pytest/entry_point.py
+++ ddtrace/testing/internal/pytest/entry_point.py
@@ -6,6 +6,6 @@ if os.environ.get("_DD_PYTEST_USE_NEW_PLUGIN", "").lower() in ("1", "true") or o
 ).lower() in ("1", "true"):
     from ddtrace.testing.internal.pytest.plugin import *  # noqa: F403
 
-    del os.environ["_DD_PYTEST_USE_NEW_PLUGIN"]
+    os.environ.pop("_DD_PYTEST_USE_NEW_PLUGIN", None)
 else:
     from ddtrace.contrib.internal.pytest.plugin import *  # noqa: F403
