ff67c68b3bc582e369efc57baa22b252edf0712b
Vítor De Araújo <vitor.dearaujo@datadoghq.com>
2025-12-19 16:02:56 +0000

    chore(ci_visibility): mock subprocess execution in git upload metadata tests

--- tests/ci_visibility/test_ci_visibility.py
+++ tests/ci_visibility/test_ci_visibility.py
@@ -942,6 +942,16 @@ class TestUploadGitMetadata:
             yield
 
     @pytest.fixture(scope="function", autouse=True)
+    def mock_process(self):
+        """Run subprocess routine synchronously within current process to avoid test flakiness."""
+        def _process(target, args, kwargs):
+            target(*args, **kwargs)
+            return mock.Mock(exit_code=0)
+
+        with mock.patch("ddtrace.internal.ci_visibility.git_client.Process", side_effect=_process):
+            yield
+
+    @pytest.fixture(scope="function", autouse=True)
     def _test_context_manager(self):
         with mock.patch(
             "ddtrace.ext.ci.tags",
