cb85af730d82e6efbe8b29084f978d1e8e72ee64
Rob Carlan <rob.carlan@datadoghq.com>
2025-12-19 11:54:50 -0500

    test

--- tests/contrib/kafka/test_kafka_dsm.py
+++ tests/contrib/kafka/test_kafka_dsm.py
@@ -222,14 +222,15 @@ def test_data_streams_kafka_offset_monitoring_auto_commit(dsm_processor, consume
     assert len(buckets) == 1
     assert list(buckets.values())[0].latest_produce_offsets[PartitionKey(kafka_topic, 0)] > 0
 
-    # Auto commit is enabled so we want to wait for the commit event to fire
-    time.sleep(1)
+    consumer.commit(asynchronous=False)
+
     first_offset = consumer.committed([TopicPartition(kafka_topic, 0)])[0].offset
-    if first_offset:
-        assert (
-            list(buckets.values())[0].latest_commit_offsets[ConsumerPartitionKey("test_group", kafka_topic, 0)]
-            == first_offset
-        )
+
+    assert first_offset == _message.offset() + 1
+    assert (
+        list(buckets.values())[0].latest_commit_offsets[ConsumerPartitionKey("test_group", kafka_topic, 0)]
+        == first_offset
+    )
 
 
 def test_data_streams_kafka_produce_api_compatibility(dsm_processor, consumer, producer, empty_kafka_topic):
