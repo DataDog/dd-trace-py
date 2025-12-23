fbf811accc2b8515d6b95c5c72eda9e0c691e3f5
Emmett Butler <emmett.butler321@gmail.com>
2025-12-23 09:12:23 -0800

    no github credentials or app IDs in code

--- scripts/codeql_scan.sh
+++ scripts/codeql_scan.sh
@@ -8,9 +8,7 @@ export PATH=$PATH:/usr/local/go/bin
 
 # Clone Code Scanning repository to download custom CodeQL packs from.
 git config --global url."https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.ddbuild.io/DataDog/".insteadOf "https://github.com/DataDog/"
-set +x # Disable command echoing to prevent token leakage
 git clone https://github.com/DataDog/codescanning.git --depth 1 --single-branch --branch=main /tmp/codescanning
-set -x # Re-enable command echoing
 
 dd-octo-sts debug --scope DataDog/dd-trace-py --policy codeql || true
 
@@ -19,3 +17,9 @@ $CODEQL database create "$CODEQL_DB" $DB_CONFIGS
 
 # Run queries for each supported ecosystem and save results to intermediate SARIF files.
 $CODEQL database analyze "$CODEQL_DB" "$PYTHON_CUSTOM_QLPACK" $SCAN_CONFIGS --sarif-category="python" --output="/tmp/python.sarif"
+
+set +x # Disable command echoing to prevent token leakage
+export GITHUB_TOKEN="$(DD_TRACE_ENABLED=false dd-octo-sts token --scope DataDog/dd-trace-py --policy codeql)"
+set -x # Re-enable command echoing
+cd /tmp/codescanning && go build -o codescanning_binary && chmod +x codescanning_binary
+./codescanning_binary -upload_sarif=true -scan_started_time="${CI_JOB_STARTED_AT}"
