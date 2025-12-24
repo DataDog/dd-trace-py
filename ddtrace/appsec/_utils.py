da4c0420c5f7ea8eca6141ba7277c26680dba055
erikayasuda <erika.yasuda@datadoghq.com>
2025-12-24 13:20:54 -0800

    Revert "ci(appsec): fix subprocess flakyness [backport 4.1] (#15728)"
    This reverts commit 8dd9c03206bcd352411701d605f2427edea31177.

--- ddtrace/appsec/_utils.py
+++ ddtrace/appsec/_utils.py
@@ -409,22 +409,13 @@ def unpatching_popen():
     os.close = unpatched_close
     original_popen = subprocess.Popen
     subprocess.Popen = unpatched_Popen
-    # Save the original bypass flag value
-    original_bypass_flag = asm_config._bypass_instrumentation_for_waf
     asm_config._bypass_instrumentation_for_waf = True
     try:
         yield
     finally:
         subprocess.Popen = original_popen
         os.close = original_os_close
-        # In tests, restore the original value to avoid corrupting test configurations
-        # In production, force to False to ensure instrumentation is re-enabled
-        if asm_config._is_testing_instrumentation_for_waf:
-            # If it was already True, restore it (likely a test scenario)
-            asm_config._bypass_instrumentation_for_waf = original_bypass_flag
-        else:
-            # If it was False, keep it False (normal production scenario)
-            asm_config._bypass_instrumentation_for_waf = False
+        asm_config._bypass_instrumentation_for_waf = False
 
 
 def is_inferred_span(span: Span) -> bool:
