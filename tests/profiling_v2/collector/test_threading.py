bbc7b476ffcb3fa3deb9a516aec517ce5da61ba4
Vlad Scherbich <vlad.scherbich@datadoghq.com>
2026-01-08 17:32:15 -0500

    fix(profiling): enable sub-classing wrapped lock types by custom lock implementations [backport 3.19] (#15895)
    ## Description
    
    This PR back-ports a bug fix to v3.19: #15604
    
    ## Testing
    * CI

--- tests/profiling_v2/collector/test_threading.py
+++ tests/profiling_v2/collector/test_threading.py
@@ -20,6 +20,7 @@ from ddtrace import ext
 from ddtrace._trace.span import Span
 from ddtrace._trace.tracer import Tracer
 from ddtrace.internal.datadog.profiling import ddup
+from ddtrace.profiling.collector._lock import _LockAllocatorWrapper as LockAllocatorWrapper
 from ddtrace.profiling.collector.threading import ThreadingLockCollector
 from ddtrace.profiling.collector.threading import ThreadingRLockCollector
 from tests.profiling.collector import pprof_utils
@@ -127,8 +128,6 @@ def test_lock_repr(
 
 
 def test_patch():
-    from ddtrace.profiling.collector._lock import _LockAllocatorWrapper
-
     lock = threading.Lock
     collector = ThreadingLockCollector()
     collector.start()
@@ -136,7 +135,7 @@ def test_patch():
     # After patching, threading.Lock is replaced with our wrapper
     # The old reference (lock) points to the original builtin Lock class
     assert lock != threading.Lock  # They're different after patching
-    assert isinstance(threading.Lock, _LockAllocatorWrapper)  # threading.Lock is now wrapped
+    assert isinstance(threading.Lock, LockAllocatorWrapper)  # threading.Lock is now wrapped
     assert callable(threading.Lock)  # and it's callable
     collector.stop()
     # After stopping, everything is restored
