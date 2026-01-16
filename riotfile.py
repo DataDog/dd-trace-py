fc0d39510464d443d19baf1a5ae18ead79f4e131
Federico Mon <federico.mon@datadoghq.com>
2026-01-16 11:01:06 +0100

    use new plugin for all

--- riotfile.py
+++ riotfile.py
@@ -111,6 +111,7 @@ venv = Venv(
     },
     env={
         "_DD_CIVISIBILITY_USE_CI_CONTEXT_PROVIDER": "1",
+        "DD_PYTEST_USE_NEW_PLUGIN": "true",
         "DD_TESTING_RAISE": "1",
         "DD_REMOTE_CONFIGURATION_ENABLED": "false",
         "DD_INJECTION_ENABLED": "1",
@@ -425,7 +426,6 @@ venv = Venv(
                 "django": latest,
             },
             env={
-                "DD_PYTEST_USE_NEW_PLUGIN": "true",
                 "DD_TRACE_DEBUG": "1",
             },
             venvs=[
