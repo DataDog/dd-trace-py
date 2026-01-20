78cf2997ff95b2e7171019adf4905d631c347cac
Emmett Butler <723615+emmettbutler@users.noreply.github.com>
2026-01-20 08:02:50 -0800

    ci: unpin direct dependencies in nightly runs [APMLP-628] (#16034)
    This change updates the `build_base_venvs` job to install pre-release
    versions of direct ddtrace dependencies when run in the context of the
    nightly workflow. This combined with proper alerting on the nightly
    workflow will increase the amount of lead time we get when future
    versions of dependencies would break compatibility with ddtrace.
    
    Requirements:
    * Choose the newest* version of each dependency, whether or not it is a
    prerelease
    * The chosen version must conform to the PEP 508 specification of that
    dependency in `pyproject.toml` including version bounds
    
    It would be great if pip had a built-in way to include prerelease
    versions in package searches irrespective of the dependency specifier,
    but I don't think it does. I've experimentally verified that to get
    prerelease versions of dependencies listed in pyproject.toml that also
    match the specified conditions, the `PIP_PRE` option must be used *and*
    the specifier has to include a prerelease marker (`rc` or `dev`). To
    implement this, this change takes the approach of modifying
    `pyproject.toml` to include these markers before calling `riot
    generate`.
    
    --------------------------------------------
    
    `pip freeze` output from a [test
    run](https://gitlab.ddbuild.io/DataDog/apm-reliability/dd-trace-py/-/jobs/1354307272)
    of `build_base_venvs` (note that `wrapt` installed a prerelease version
    still within the bounds given in the `pyproject.toml`):
    ```
    bytecode==0.17.0
    # Editable Git install with no remote (ddtrace==4.3.0.dev0)
    -e /go/src/github.com/DataDog/apm-reliability/dd-trace-py
    envier==0.6.1
    importlib_metadata==8.7.1
    opentelemetry-api==1.39.1
    typing_extensions==4.15.0
    wrapt==2.1.0.dev1
    zipp==3.23.0
    ```
    
    ---------
    
    Co-authored-by: Brett Langdon <brett.langdon@datadoghq.com>

---
+++ scripts/allow_prerelease_dependencies.py
@@ -0,0 +1,84 @@
+import os
+
+import tomllib
+
+
+PROJECT_FILENAME = "pyproject.toml"
+PRERELEASE_MARKER = "rc"
+
+
+def _add_prerelease_marker(dependency_specifier: str) -> str:
+    """
+    The `dependency_specifier` parameter is expected to be PEP 508-compliant.
+    This function does not handle all possible PEP 508 strings, only the ones that are
+    used at the time of writing in `pyproject.toml`, and maybe some others.
+
+    >>> _add_prerelease_marker("bytecode>=0.17.0,<1; python_version>='3.14.0'")
+    "bytecode>=0.17.0,<1; python_version>='3.14.0'"
+    >>> _add_prerelease_marker('envier~=0.6.1')
+    "envier~=0.6.1rc99"
+    >>> _add_prerelease_marker('opentelemetry-api>=1,<2')
+    "opentelemetry-api>=1,<2rc99"
+    >>> _add_prerelease_marker('wrapt>=1,<3')
+    "wrapt>=1,<3rc99"
+    """
+    dependency_parts: list[str] = dependency_specifier.split(";")
+    version_bounds: list[str] = dependency_parts[0].split(",")
+    if ">" in version_bounds[-1]:
+        version_bounds[-1] += f"{PRERELEASE_MARKER}0"
+    else:
+        version_bounds[-1] += f"{PRERELEASE_MARKER}99"
+    dependency_parts[0] = ",".join(version_bounds)
+    return ";".join(dependency_parts)
+
+
+def _replace_dependency_strings(input_: list[str], new_specifiers: list[str]) -> list[str]:
+    """
+    Return a copy of the input list with the strings representing project dependencies replaced with the
+    item from `new_specifiers` at the matching index.
+
+    `input_` is assumed to be a list of lines from a pyproject.toml file
+    `new_specifiers` is a list of PEP 508 dependency specifiers
+
+    This index-maintenance approach is chosen to avoid dependence on a third-party toml-writing library.
+    """
+    input_copy: list[str] = []
+    replace_at_idx = -1
+    for line in input_:
+        if len(new_specifiers) > replace_at_idx >= 0:
+            input_copy.append(f'    "{new_specifiers[replace_at_idx]}",\n')
+            replace_at_idx += 1
+        else:
+            input_copy.append(line)
+        if line.startswith("dependencies = ["):
+            replace_at_idx += 1
+    return input_copy
+
+
+def update_dependencies_to_allow_prereleases():
+    """
+    Updates the pyproject.toml file in-place, adding pre-release markers like "rc0"
+    to the libraries listed in the `dependencies` block. Combined with the `PIP_PRE`
+    environment variable configuration, this tells pip, and thus riot, to include
+    pre-release versions of dependencies in its package search.
+    """
+    updated_specifiers: list[str] = []
+
+    with open(PROJECT_FILENAME, "r") as f:
+        project_file_lines: list[str] = f.readlines()
+
+    with open(PROJECT_FILENAME, "rb") as f:
+        project_file_data: dict = tomllib.load(f)
+
+    for dependency_specifier in project_file_data["project"]["dependencies"]:
+        updated_specifiers.append(_add_prerelease_marker(dependency_specifier))
+
+    updated_project_file_lines = _replace_dependency_strings(project_file_lines, updated_specifiers)
+
+    os.remove(PROJECT_FILENAME)
+    with open(PROJECT_FILENAME, "w") as f:
+        f.writelines(updated_project_file_lines)
+
+
+if __name__ == "__main__":
+    update_dependencies_to_allow_prereleases()
