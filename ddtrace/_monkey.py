37efc8c94552e80cd32728bbf53b6caec0b2fd4c
Pablo Martínez Bernardo <pablo.martinezbernardo@datadoghq.com>
2026-01-26 12:03:04 +0100

    Initial impl

--- ddtrace/_monkey.py
+++ ddtrace/_monkey.py
@@ -102,6 +102,7 @@ PATCH_MODULES = {
     "asyncpg": True,
     "aws_lambda": True,  # patch only in AWS Lambda environments
     "azure_eventhubs": True,
+    "azure_durable_functions": True,
     "azure_functions": True,
     "azure_servicebus": True,
     "tornado": False,
@@ -159,6 +160,7 @@ _MODULES_FOR_CONTRIB = {
     "vertica": ("vertica_python",),
     "aws_lambda": ("datadog_lambda",),
     "azure_eventhubs": ("azure.eventhub",),
+    "azure_durable_functions": ("azure.durable_functions",),
     "azure_functions": ("azure.functions",),
     "azure_servicebus": ("azure.servicebus",),
     "httplib": ("http.client",),
