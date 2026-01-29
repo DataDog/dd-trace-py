693108b8bdb80c1cf27e5d2d0fca469f047a7470
Alexandre Choura <alexandre.choura@datadoghq.com>
2026-01-29 14:58:47 +0100

    Remove app_key parameter from PromptManager and associated header logic

--- ddtrace/llmobs/_llmobs.py
+++ ddtrace/llmobs/_llmobs.py
@@ -1435,7 +1435,6 @@ class LLMObs(Service):
 
         return PromptManager(
             api_key=api_key,
-            app_key=cls._app_key,
             ml_app=ml_app,
             endpoint_override=endpoint_override,
             cache_ttl=cache_ttl,
