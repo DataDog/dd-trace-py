374fa5591927a74253c0a789de2d91fd3554accf
Taegyun Kim <taegyun.kim@datadoghq.com>
2026-01-30 15:03:19 -0500

    need this back for django

--- riotfile.py
+++ riotfile.py
@@ -933,6 +933,8 @@ venv = Venv(
             env={
                 "DD_CIVISIBILITY_ITR_ENABLED": "0",
                 "DD_IAST_REQUEST_SAMPLING": "100",  # Override default 30% to analyze all IAST requests
+                # TODO: Remove once pkg_resources warnings are no longer emitted from this internal module
+                "PYTHONWARNINGS": "ignore::UserWarning:ddtrace.internal.module",
             },
             venvs=[
                 Venv(
