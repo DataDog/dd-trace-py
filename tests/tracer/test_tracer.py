c714f85f931705f96297b6646440a11bcc5a3416
Munir <munir.abdinur@datadoghq.com>
2026-02-05 09:33:59 -0500

    mock properties directly

--- tests/tracer/test_tracer.py
+++ tests/tracer/test_tracer.py
@@ -735,7 +735,7 @@ def test_tracer_shutdown_timeout():
 
 @pytest.mark.subprocess(
     err=b"Spans started after the tracer has been shut down will not be sent to the Datadog Agent: "
-    b"<Span(id=1,trace_id=1,parent_id=None,name=span_created_after_shutdown)>.\n",
+    b"<Span(id=2,trace_id=1,parent_id=None,name=span_created_after_shutdown)>.\n",
     parametrize={"USE_START_SPAN": ["true", "false"]},
 )
 def test_tracer_shutdown():
@@ -743,6 +743,7 @@ def test_tracer_shutdown():
 
     import mock
 
+    from ddtrace._trace.span import Span
     from ddtrace.trace import tracer as t
 
     t.shutdown()
@@ -754,9 +755,11 @@ def test_tracer_shutdown():
 
     with (
         mock.patch.object(t._span_aggregator.writer, "write") as mock_write,
-        mock.patch("ddtrace._trace.span._rand64bits", return_value=1),
-        mock.patch("ddtrace._trace.span._rand128bits", return_value=1),
+        mock.patch.object(Span, "trace_id", new_callable=mock.PropertyMock) as mock_trace_id,
+        mock.patch.object(Span, "span_id", new_callable=mock.PropertyMock) as mock_span_id,
     ):
+        mock_trace_id.return_value = 1
+        mock_span_id.return_value = 2
         create_span("span_created_after_shutdown").finish()
 
     mock_write.assert_not_called()
