36b07d461e50c911f1ac1bccf4496a9da7f0a0c1
Pablo Martínez Bernardo <pablo.martinezbernardo@datadoghq.com>
2026-02-05 14:37:34 +0100

    Remove dfapp tests from azure functions tests

--- tests/contrib/azure_functions/test_azure_functions_patch.py
+++ tests/contrib/azure_functions/test_azure_functions_patch.py
@@ -16,23 +16,11 @@ class TestAzureFunctionsPatch(PatchTestCase.Base):
     __unpatch_func__ = unpatch
     __get_version__ = get_version
 
-    @staticmethod
-    def _get_dfapp():
-        from azure.durable_functions.decorators import durable_app
-
-        return durable_app.DFApp
-
     def assert_module_patched(self, azure_functions):
         self.assert_wrapped(azure_functions.FunctionApp.get_functions)
-        dfapp = self._get_dfapp()
-        self.assert_wrapped(dfapp.get_functions)
 
     def assert_not_module_patched(self, azure_functions):
         self.assert_not_wrapped(azure_functions.FunctionApp.get_functions)
-        dfapp = self._get_dfapp()
-        self.assert_not_wrapped(dfapp.get_functions)
 
     def assert_not_module_double_patched(self, azure_functions):
         self.assert_not_double_wrapped(azure_functions.FunctionApp.get_functions)
-        dfapp = self._get_dfapp()
-        self.assert_not_double_wrapped(dfapp.get_functions)
