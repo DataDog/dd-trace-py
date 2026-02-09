cfd4fd953cc28d0e8769dd07e299265e03eb6589
Pablo Martínez Bernardo <pablo.martinezbernardo@datadoghq.com>
2026-02-09 09:48:16 +0100

    Extract patched_get_functions to common utils

--- ddtrace/contrib/internal/azure_durable_functions/patch.py
+++ ddtrace/contrib/internal/azure_durable_functions/patch.py
@@ -8,21 +8,9 @@ try:
 except Exception:
     durable_functions = None
 
-from ddtrace import config
 from ddtrace._trace.pin import Pin
-from ddtrace.contrib.internal.azure_functions.utils import create_context
-from ddtrace.contrib.internal.azure_functions.utils import wrap_function_with_tracing
+from ddtrace.contrib.internal.azure_functions.utils import patched_get_functions
 from ddtrace.contrib.internal.trace_utils import unwrap as _u
-from ddtrace.ext import SpanKind
-
-
-_DURABLE_ACTIVITY_TRIGGER = "activityTrigger"
-_DURABLE_ENTITY_TRIGGER = "entityTrigger"
-_DURABLE_ORCHESTRATION_TRIGGER = "orchestrationTrigger"
-_DURABLE_ACTIVITY_TRIGGER_NAME = "Activity"
-_DURABLE_ENTITY_TRIGGER_NAME = "Entity"
-_DURABLE_ACTIVITY_CONTEXT = "azure.durable_functions.patched_activity"
-_DURABLE_ENTITY_CONTEXT = "azure.durable_functions.patched_entity"
 
 
 def get_version() -> str:
@@ -59,51 +47,8 @@ def _patch_dfapp():
     if not hasattr(durable_app, "DFApp"):
         return
 
-    from ddtrace.contrib.internal.azure_functions.patch import _patched_get_functions
-
     Pin().onto(durable_app.DFApp)
-    _w("azure.durable_functions", "DFApp.get_functions", _patched_get_functions)
-
-
-def wrap_durable_functions(pin, functions):
-    if durable_functions is None or not getattr(durable_functions, "_datadog_patch", False):
-        return
-
-    for function in functions:
-        trigger = function.get_trigger()
-        if not trigger:
-            continue
-
-        trigger_type = trigger.get_binding_name()
-        if trigger_type == _DURABLE_ORCHESTRATION_TRIGGER:
-            continue
-
-        if trigger_type == _DURABLE_ACTIVITY_TRIGGER:
-            trigger_name = _DURABLE_ACTIVITY_TRIGGER_NAME
-            context_name = _DURABLE_ACTIVITY_CONTEXT
-        elif trigger_type == _DURABLE_ENTITY_TRIGGER:
-            trigger_name = _DURABLE_ENTITY_TRIGGER_NAME
-            context_name = _DURABLE_ENTITY_CONTEXT
-        else:
-            continue
-
-        function_name = function.get_function_name()
-        func = function.get_user_function()
-        function._func = _wrap_durable_trigger(pin, func, function_name, trigger_name, context_name)
-
-
-def _wrap_durable_trigger(pin, func, function_name, trigger_type, context_name):
-    def context_factory(kwargs):
-        resource_name = f"{trigger_type} {function_name}"
-        return create_context(context_name, pin, resource_name)
-
-    def pre_dispatch(ctx, kwargs):
-        return (
-            "azure.durable_functions.trigger_call_modifier",
-            (ctx, config.azure_functions, function_name, trigger_type, SpanKind.INTERNAL),
-        )
-
-    return wrap_function_with_tracing(func, context_factory, pre_dispatch=pre_dispatch)
+    _w("azure.durable_functions", "DFApp.get_functions", patched_get_functions)
 
 
 def unpatch():
