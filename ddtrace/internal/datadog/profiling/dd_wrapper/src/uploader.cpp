0b440a341e508b19826b171b8207183e5a0d47f3
Thomas Kowalski <thomas.kowalski@datadoghq.com>
2026-02-12 15:25:26 +0100

    re-run

--- ddtrace/internal/datadog/profiling/dd_wrapper/src/uploader.cpp
+++ ddtrace/internal/datadog/profiling/dd_wrapper/src/uploader.cpp
@@ -130,7 +130,7 @@ Datadog::Uploader::upload_unlocked()
         ddog_CancellationToken_drop(&current_cancel);
     }
 
-    // re-run
+    // re-run 2
     auto res = ddog_prof_Exporter_send_blocking(&ddog_exporter,
                                                 &encoded_profile,
                                                 files_to_compress,
