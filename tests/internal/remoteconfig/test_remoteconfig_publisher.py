4718cf03847e5d19352d834aef7bca968a7be409
Gabriele N. Tornetta <P403n1x87@users.noreply.github.com>
2026-02-16 20:01:33 +0000

    chore(rc): remove preprocess (#16513)
    ## Description
    
    We drop the preprocess support from the remote configuration client implementation since it's no longer needed after the single-thread refactor.
    
    Co-authored-by: gabriele.tornetta <gabriele.tornetta@datadoghq.com>

--- tests/internal/remoteconfig/test_remoteconfig_publisher.py
+++ tests/internal/remoteconfig/test_remoteconfig_publisher.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-import mock
-
 from ddtrace.internal.remoteconfig import ConfigMetadata
 from ddtrace.internal.remoteconfig import Payload
 from ddtrace.internal.remoteconfig._publishers import RemoteConfigPublisher
@@ -8,31 +6,24 @@ from tests.internal.remoteconfig.utils import MockConnector
 
 
 def test_remoteconfig_publisher_dispatch():
-    mock_preprocess_results = mock.MagicMock()
-    mock_preprocess_results.return_value = [{"config": "parsed_data"}]
     mock_connector = MockConnector({"example": "data"})
 
-    publisher = RemoteConfigPublisher(mock_connector, mock_preprocess_results)
+    publisher = RemoteConfigPublisher(mock_connector)
     config = {"id": "1", "product_name": "MOCK", "sha256_hash": "sha256_hash", "length": 5, "tuf_version": 35}
     publisher.append({"config": "data"}, "path", config)
     publisher.dispatch()
 
-    if publisher._preprocess_results_func:
-        mock_preprocess_results.assert_called_once_with(
-            [
-                Payload(
-                    metadata=ConfigMetadata(
-                        id="1",
-                        product_name="MOCK",
-                        sha256_hash="sha256_hash",
-                        length=5,
-                        tuf_version=35,
-                        apply_state=1,
-                        apply_error=None,
-                    ),
-                    path="path",
-                    content={"config": "data"},
-                )
-            ],
-        )
-    assert mock_connector.data == [{"config": "parsed_data"}]
+    expected_payload = Payload(
+        metadata=ConfigMetadata(
+            id="1",
+            product_name="MOCK",
+            sha256_hash="sha256_hash",
+            length=5,
+            tuf_version=35,
+            apply_state=1,
+            apply_error=None,
+        ),
+        path="path",
+        content={"config": "data"},
+    )
+    assert mock_connector.data == [expected_payload]
