9832b014756bef2e2078ed54d2a1ad2c0ab7b1a6
Gabriele N. Tornetta <P403n1x87@users.noreply.github.com>
2026-02-18 19:00:55 +0000

    chore(debugger): improve exception typing (#16555)
    ## Description
    
    We improve the typing of exception information arguments and checks.
    
    ## Additional Notes
    
    Changes assisted by Claude Code
    
    Co-authored-by: gabriele.tornetta <gabriele.tornetta@datadoghq.com>

--- ddtrace/debugging/_origin/span.py
+++ ddtrace/debugging/_origin/span.py
@@ -17,6 +17,8 @@ from ddtrace.debugging._session import Session
 from ddtrace.debugging._signal.snapshot import Snapshot
 from ddtrace.debugging._uploader import SignalUploader
 from ddtrace.debugging._uploader import UploaderProduct
+from ddtrace.internal.compat import NO_EXCEPTION
+from ddtrace.internal.compat import ExcInfoType
 from ddtrace.internal.logger import get_logger
 from ddtrace.internal.safety import _isinstance
 from ddtrace.internal.threads import Lock
@@ -118,7 +120,7 @@ class EntrySpanWrappingContext(LazyWrappingContext):
 
         return self
 
-    def _close_signal(self, retval: t.Any = None, exc_info: tuple[t.Any, t.Any, t.Any] = (None, None, None)) -> None:
+    def _close_signal(self, retval: t.Any = None, exc_info: ExcInfoType = NO_EXCEPTION) -> None:
         if not self.__enabled__:
             return
 
@@ -156,12 +158,17 @@ class EntrySpanWrappingContext(LazyWrappingContext):
             if (collector := self.uploader.get_collector()) is not None:
                 collector.push(snapshot)
 
-    def __return__(self, retval):
+    def __return__(self, retval: t.Any) -> t.Any:
         self._close_signal(retval=retval)
         return super().__return__(retval)
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        self._close_signal(exc_info=(exc_type, exc_value, traceback))
+    def __exit__(
+        self,
+        exc_type: t.Optional[type[BaseException]],
+        exc_value: t.Optional[BaseException],
+        traceback: t.Optional[t.Any],
+    ) -> None:
+        self._close_signal(exc_info=t.cast(ExcInfoType, (exc_type, exc_value, traceback)))
         super().__exit__(exc_type, exc_value, traceback)
 
 
