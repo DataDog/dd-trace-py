260afc6c7af535b3a50acf89dab331d7477c9abb
Edouard Schweisguth <edouard.schweisguth@datadoghq.com>
2026-02-25 10:15:37 +0100

    hmm pull the thing we just build? (not a fan but, not sure what can be done otherwise?)

--- .gitlab/scripts/fuzz_infra.py
+++ .gitlab/scripts/fuzz_infra.py
@@ -138,6 +138,7 @@ def sign_image(config: Config, metadata_file: str) -> None:
 
 def extract_manifest(config: Config) -> list[FuzzBinary]:
     """Pull the manifest from the built image and parse it into FuzzBinary entries."""
+    run_command(["docker", "pull", config.full_image_ref])
     result = run_command(
         ["docker", "run", "--rm", config.full_image_ref, "cat", MANIFEST_PATH],
     )
