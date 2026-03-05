b5af4b4976fdef14ab3401fa340f32f5b1540482
Vlad Scherbich <vlad.scherbich@datadoghq.com>
2026-03-05 17:56:08 -0500

    more improves

--- hooks/scripts/run-fmt.sh
+++ hooks/scripts/run-fmt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-staged_files=$(git diff --staged --name-only HEAD --diff-filter=ACMR | grep -E '\.(py|pyx|pxd)$' | tr '\n' ' ')
+staged_files=$(git diff --staged --name-only HEAD --diff-filter=ACMR | grep -E '\.py$' | tr '\n' ' ')
 if [ -n "$staged_files" ]; then
     hatch -v run lint:fmt -- $staged_files
 else
-    echo 'hatch lint:fmt skipped: No Python/Cython files were found in `git diff --staged`'
+    echo 'hatch lint:fmt skipped: No Python files were found in `git diff --staged`'
 fi
--- hooks/scripts/run-mypy.sh
+++ hooks/scripts/run-mypy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-staged_files=$(git diff --staged --name-only HEAD --diff-filter=ACMR | grep -E '\.py$')
+staged_files=$(git diff --staged --name-only HEAD --diff-filter=ACMR | grep -E '\.py$' | tr '\n' ' ')
 if [ -n "$staged_files" ]; then
-    hatch -v run lint:typing
+    hatch -v run lint:typing -- $staged_files
 else
     echo 'Run mypy skipped: No Python files were found in `git diff --staged`'
 fi
