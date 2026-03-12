69e5206abb29c79e9e1bbe95cdc1b47ef3f178ae
Emmett Butler <emmett.butler321@gmail.com>
2026-03-12 13:06:53 -0700

    set svc src tag in trace subscriber

--- ddtrace/_trace/subscribers/_base.py
+++ ddtrace/_trace/subscribers/_base.py
@@ -67,6 +67,8 @@ def _start_span(ctx: core.ExecutionContext[TracingEventType]) -> Span:
     if event.measured:
         span.set_metric(_SPAN_MEASURED_KEY, 1)
 
+    maybe_set_service_source_tag(span, integration_config or dict())
+
     ctx.span = span
 
     if config._inferred_proxy_services_enabled:
