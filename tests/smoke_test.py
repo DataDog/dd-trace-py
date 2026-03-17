f8019f59cd059a15ed1a4df159ffafb736b479fc
Florentin Labelle <florentin.labelle@datadoghq.com>
2026-03-17 09:40:19 +0100

    chore(aap): format

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -68,6 +68,7 @@ if __name__ == "__main__":
         print("Running WAF module load test...")
         # Proceed with the WAF module load test
         import ddtrace.appsec._ddwaf as _ddwaf
+
         assert _ddwaf.is_available, f"WAF module failed to load: {_ddwaf.failure_msg}"
         assert module.loaded
         print("WAF module load test completed successfully")
