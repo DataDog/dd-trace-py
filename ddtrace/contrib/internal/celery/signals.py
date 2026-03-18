9b88f33aff6420642cf010db4c82b40cb4a2ad66
Emmett Butler <emmett.butler321@gmail.com>
2026-03-18 06:44:54 -0700

    update svc src tag name to match system-tests and rfc

--- ddtrace/contrib/internal/celery/signals.py
+++ ddtrace/contrib/internal/celery/signals.py
@@ -56,7 +56,7 @@ def trace_prerun(*args, **kwargs):
     span = tracer.trace(c.WORKER_ROOT_SPAN, service=service, resource=task.name, span_type=SpanTypes.WORKER)
     # Use inline check for worker default service
     if span.service == config.celery["_default_service_worker"]:
-        span.set_tag("_dd.svc.src", "celery")
+        span.set_tag("_dd.svc_src", "celery")
 
     # set span.kind to the type of request being performed
     span._set_attribute(SPAN_KIND, SpanKind.CONSUMER)
@@ -132,7 +132,7 @@ def trace_before_publish(*args, **kwargs):
     span = tracer.trace(c.PRODUCER_ROOT_SPAN, service=service, resource=task_name)
     # Use inline check for producer default service
     if span.service == config.celery["_default_service_producer"]:
-        span.set_tag("_dd.svc.src", "celery")
+        span.set_tag("_dd.svc_src", "celery")
 
     # Store an item called "task span" in case after_task_publish doesn't get called
     core.set_item("task_span", span)
