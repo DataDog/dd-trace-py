e9ec134347a5d74a1d4e56702b22f311d3460e62
Florentin Labelle <florentin.labelle@datadoghq.com>
2026-03-20 09:40:42 +0100

    chore(appsec): move httpx integration to core API with events

--- ddtrace/contrib/_events/http_client.py
+++ ddtrace/contrib/_events/http_client.py
@@ -1,13 +1,17 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any
+from typing import Callable
+from typing import Mapping
 from typing import MutableMapping
 from typing import Optional
 from typing import Protocol
+from typing import Sequence
+from typing import Union
 
 from ddtrace._trace.events import TracingEvent
 from ddtrace.ext import SpanKind
 from ddtrace.ext import SpanTypes
+from ddtrace.internal.core.events import Event
 from ddtrace.internal.core.events import event_field
 from ddtrace.internal.logger import get_logger
 from ddtrace.internal.schema import schematize_url_operation
@@ -18,20 +22,37 @@ from ddtrace.internal.settings.integration import IntegrationConfig
 log = get_logger(__name__)
 
 
+JsonType = Union[None, bool, int, float, str, Sequence["JsonType"], Mapping[str, "JsonType"]]
+
+
 class _HttpClientResponse(Protocol):
     headers: MutableMapping[str, str]
     status_code: int
 
-    def json(self) -> Any: ...
+    def json(self) -> JsonType: ...
 
 
 class HttpClientEvents(Enum):
     HTTP_REQUEST = "http.client.request"
     HTTPX_REQUEST = "httpx.request"
+    HTTPX_SINGLE_REQUEST = "httpx.single.request"
 
 
 @dataclass
-class HttpClientRequestEvent(TracingEvent):
+class HttpClientBaseEvent(Event):
+    url: str = event_field()
+    request_method: str = event_field()
+    request_headers: MutableMapping[str, str] = event_field()
+    response_headers: MutableMapping[str, str] = event_field(default_factory=dict)
+    response_status_code: Optional[int] = event_field(default=None)
+
+    def set_response(self, response: _HttpClientResponse) -> None:
+        self.response_status_code = response.status_code
+        self.response_headers = response.headers
+
+
+@dataclass
+class HttpClientRequestEvent(HttpClientBaseEvent, TracingEvent):
     """HTTP client request event"""
 
     event_name = HttpClientEvents.HTTP_REQUEST.value
@@ -40,14 +61,11 @@ class HttpClientRequestEvent(TracingEvent):
     span_type = SpanTypes.HTTP
 
     http_operation: str = event_field()
-    url: str = event_field()
     query: str = event_field()
     target_host: Optional[str] = event_field()
-    request_method: str = event_field()
-    request_headers: MutableMapping[str, str] = event_field()
-    response_headers: MutableMapping[str, str] = event_field(default_factory=dict)
-    response_status_code: Optional[int] = event_field(default=None)
     config: "IntegrationConfig" = event_field()
+
+    response: Optional[_HttpClientResponse] = event_field(default=None)
 
     def __post_init__(self):
         self.span_name = schematize_url_operation(
@@ -55,6 +73,17 @@ class HttpClientRequestEvent(TracingEvent):
         )
 
     def set_response(self, response: _HttpClientResponse) -> None:
+        super().set_response(response)
         self.response = response
-        self.response_status_code = response.status_code
-        self.response_headers = response.headers
+
+
+@dataclass
+class HttpClientSendEvent(HttpClientBaseEvent, Event):
+    """HTTP client send event
+
+    This represents individual requests in a single http client call.
+    Examples are managed auth flows and redirect requests.
+    """
+
+    event_name = HttpClientEvents.HTTPX_SINGLE_REQUEST.value
+    request_body: Callable[[], Union[str, bytes]] = event_field()
