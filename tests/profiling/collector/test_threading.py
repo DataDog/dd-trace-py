fc94be7700afc1388c89ec5d87786048e48f0582
Vlad Scherbich <vlad.scherbich@datadoghq.com>
2026-03-20 11:59:50 -0400

    fix subprocess tests

--- tests/profiling/collector/test_threading.py
+++ tests/profiling/collector/test_threading.py
@@ -2176,6 +2176,8 @@ def test_exclude_modules_empty_is_backward_compatible() -> None:
 
     assert not os.environ.get("DD_PROFILING_LOCK_EXCLUDE_MODULES")
 
+    from tests.profiling.collector.test_utils import init_ddup
+
     init_ddup("test_exclude_modules_empty")
 
     with ThreadingLockCollector(capture_pct=100):
