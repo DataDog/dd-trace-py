a3042feb6d55a36b8e5632e26036f609bff17202
Yun Kim <yun.kim@datadoghq.com>
2026-03-30 16:43:22 -0400

    prompt typing

--- ddtrace/llmobs/_utils.py
+++ ddtrace/llmobs/_utils.py
@@ -514,8 +514,13 @@ def _annotate_llmobs_span_data(
         if input_documents is not None:
             meta[LLMOBS_STRUCT.INPUT][LLMOBS_STRUCT.DOCUMENTS] = input_documents
         if prompt is not None:
+<<<<<<< Updated upstream
             existing_prompt = meta[LLMOBS_STRUCT.INPUT].setdefault(LLMOBS_STRUCT.PROMPT, {})
             existing_prompt.update(prompt)
+=======
+            existing_prompt = meta[LLMOBS_STRUCT.INPUT].setdefault(LLMOBS_STRUCT.PROMPT, cast(Prompt, {}))
+            existing_prompt.update(cast(Prompt, prompt))
+>>>>>>> Stashed changes
             span._set_ctx_item(INPUT_PROMPT, existing_prompt)
         if output_messages is not None:
             meta[LLMOBS_STRUCT.OUTPUT][LLMOBS_STRUCT.MESSAGES] = output_messages
