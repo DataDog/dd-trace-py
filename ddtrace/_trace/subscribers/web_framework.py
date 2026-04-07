e5ba8e456f0bf5dcb40abff77f348f4c8b55acb5
Louis Tricot <louis.tricot@datadoghq.com>
2026-04-07 15:53:20 +0200

    chore(web_framework): create events and migrate pyramid/bottle/aiohttp

---
+++ ddtrace/_trace/subscribers/web_framework.py
@@ -0,0 +1,61 @@
+from types import TracebackType
+from typing import Optional
+
+from ddtrace._trace.span import Span
+from ddtrace._trace.subscribers._base import TracingSubscriber
+from ddtrace._trace.trace_handlers import _set_inferred_proxy_tags
+from ddtrace.contrib._events.web_framework import WebFrameworkEvents
+from ddtrace.contrib._events.web_framework import WebFrameworkRequestEvent
+from ddtrace.contrib.internal import trace_utils
+from ddtrace.ext import http
+from ddtrace.internal import core
+
+
+class WebFrameworkRequestSubscriber(TracingSubscriber):
+    """Shared tracing logic for web framework integrations."""
+
+    event_names = (WebFrameworkEvents.WEB_REQUEST.value,)
+
+    @classmethod
+    def on_started(cls, ctx: core.ExecutionContext) -> None:
+        event: WebFrameworkRequestEvent = ctx.event
+
+        if event.allow_default_resource:
+            event.set_resource = True
+
+    @classmethod
+    def on_ended(
+        cls,
+        ctx: core.ExecutionContext,
+        exc_info: tuple[Optional[type], Optional[BaseException], Optional[TracebackType]],
+    ) -> None:
+        event: WebFrameworkRequestEvent = ctx.event
+        status_code = event.response_status_code
+        method = event.request_method
+        res_headers = event.response_headers
+
+        span: Span = ctx.span
+
+        # event.resource can be updated at span finish time
+        if event.resource:
+            span.resource = event.resource
+        elif event.set_resource and status_code is not None:
+            span.resource = f"{method} {status_code}"
+
+        trace_utils.set_http_meta(
+            span=span,
+            integration_config=event.integration_config,
+            method=method,
+            url=event.request_url,
+            status_code=status_code,
+            request_headers=event.request_headers,
+            response_headers=dict(res_headers) if res_headers is not None else None,
+            route=event.request_route,
+        )
+
+        if event.trace_query_string and event.query is not None:
+            span._set_attribute(http.QUERY_STRING, event.query)
+
+        _set_inferred_proxy_tags(span, status_code)
+        for tk, tv in core.get_item("additional_tags", default=dict()).items():
+            span._set_attribute(tk, tv)
