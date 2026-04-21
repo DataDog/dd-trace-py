cca6edd0b354ace84f090dcc97d298ca0bbb9ed4
Vlad Scherbich <vlad.scherbich@datadoghq.com>
2026-04-21 08:12:24 -0400

    frozenset -> set

--- ddtrace/internal/settings/profiling.pyi
+++ ddtrace/internal/settings/profiling.pyi
@@ -37,7 +37,7 @@ class ProfilingConfigStack(DDConfig):
 class ProfilingConfigLock(DDConfig):
     enabled: bool
     name_inspect_dir: bool
-    exclude_modules: frozenset[str]
+    exclude_modules: set[str]
 
 class ProfilingConfigMemory(DDConfig):
     enabled: bool
