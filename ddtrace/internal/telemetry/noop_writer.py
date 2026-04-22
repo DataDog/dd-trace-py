82f7cb7f193bcd951e96b4d4af41691273739a21
Emmett Butler <emmett.butler321@gmail.com>
2026-04-22 09:55:19 -0700

    allow nooptelemetrywriter to handle kwargs

--- ddtrace/internal/telemetry/noop_writer.py
+++ ddtrace/internal/telemetry/noop_writer.py
@@ -1,4 +1,7 @@
 class NoOpTelemetryWriter(object):
+    def __init__(self, *args, **kwargs):
+        super(NoOpTelemetryWriter, self).__init__()
+
     def __getattr__(self, name):
         return self.noop
 
