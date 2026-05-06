ec1588838cf6087952ae5eb74ecbada27e993053
Rithika Narayan <rithika.narayan@datadoghq.com>
2026-05-06 17:48:05 -0400

    linting

--- ddtrace/ext/db.py
+++ ddtrace/ext/db.py
@@ -4,4 +4,4 @@ USER = "db.user"  # the user connecting to the db
 SYSTEM = "db.system"  # the database's DBMS name (e.g. postgresql for pgsql)
 ROWCOUNT = "db.row_count"  # the rowcount of a query
 SYSTEM = "db.system"  # the database's DBMS name (e.g. postgresql for pgsql)
-STATUS_CODE = "db.response.status_code" # the database's response status code
+STATUS_CODE = "db.response.status_code"  # the database's response status code
