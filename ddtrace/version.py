d52373cce632eec9f78d178462d40815622aab25
Emmett Butler <723615+emmettbutler@users.noreply.github.com>
2026-05-07 12:07:20 -0700

    Update ddtrace/version.py
    Co-authored-by: Brett Langdon <brett.langdon@datadoghq.com>

--- ddtrace/version.py
+++ ddtrace/version.py
@@ -10,5 +10,5 @@ __version__: str
 try:
     distributions = importlib.metadata.packages_distributions().get(__package__ or __name__)
     __version__ = importlib.metadata.version(distributions[0] if distributions else "ddtrace")
-except importlib.metadata.PackageNotFoundError:
+except Exception:
     __version__ = "0.0.0"
