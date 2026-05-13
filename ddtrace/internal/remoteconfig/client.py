fabad07079696a4418d3ea1694dd52c8ac6b0812
brettlangdon <brett.langdon@datadoghq.com>
2026-05-13 12:18:23 -0400

    fix rc typing

--- ddtrace/internal/remoteconfig/client.py
+++ ddtrace/internal/remoteconfig/client.py
@@ -11,6 +11,7 @@ from typing import Iterable
 from typing import Mapping
 from typing import Optional
 from typing import Sequence
+from typing import TypedDict
 import uuid
 
 import ddtrace
@@ -201,6 +202,12 @@ AppliedConfigType = dict[str, ConfigMetadata]
 TargetsType = dict[str, ConfigMetadata]
 
 
+class CachedTargetFile(TypedDict):
+    path: str
+    length: Optional[int]
+    hashes: list[dict[str, str]]
+
+
 class RemoteConfigClient:
     """
     The Remote Configuration client regularly checks for updates on the agent
@@ -244,7 +251,7 @@ class RemoteConfigClient:
         if p_tags_list := process_tags.process_tags_list:
             self._client_tracer["process_tags"] = p_tags_list
 
-        self.cached_target_files: list[AppliedConfigType] = []
+        self.cached_target_files: list[CachedTargetFile] = []
 
         # Product callbacks for single subscriber architecture
         self._product_callbacks: dict[str, RCCallback] = {}
