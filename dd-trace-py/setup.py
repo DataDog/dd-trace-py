77871714e32817e7137b49aba4ad7310217e29f8
Emmett Butler <emmett.butler321@gmail.com>
2026-05-19 11:45:23 -0700

    dynamic dependencies

--- dd-trace-py/setup.py
+++ dd-trace-py/setup.py
@@ -1720,6 +1720,11 @@ if os.getenv("DD_CYTHONIZE", "1").lower() in ("1", "yes", "on", "true"):
             cache=True,
         )
 
+if os.getenv("CI_COMMIT_TAG") is not None:
+    ddtrace_internal_spec = f""
+else:
+    ddtrace_internal_spec = f" @ file://{HERE / '..'}/pywheels/ddtrace_internal-0.0.0-py3-none-any.whl"
+
 PACKAGE_NAME = f"ddtrace{WHEEL_FLAVOR}"
 if PACKAGE_NAME != "ddtrace":
     subprocess.run(["sed", "-i", "-e", f's/^name = ".*"/name = "{PACKAGE_NAME}"/g', "pyproject.toml"])
@@ -1739,6 +1744,17 @@ setup(
             "ddtrace.internal.datadog.profiling.ddup.test*",
         ]
     ),
+    install_requires=[
+        "bytecode>=0.17.0,<1; python_version>='3.14.0'",
+        "bytecode>=0.16.0,<1; python_version>='3.13.0'",
+        "bytecode>=0.15.1,<1; python_version~='3.12.0'",
+        "bytecode>=0.14.0,<1; python_version~='3.11.0'",
+        "bytecode>=0.13.0,<1; python_version<'3.11'",
+        f"ddtrace-internal{ddtrace_internal_spec}",
+        "envier~=0.6.1",
+        "opentelemetry-api>=1,<2",
+        "wrapt>=1,<3",
+    ],
     include_package_data=False,
     package_data={
         # Type stubs and markers for all packages
