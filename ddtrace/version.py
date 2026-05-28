373c558ed5c894aaab260e4a1cf76941f2afb70d
Brett Langdon <brett.langdon@datadoghq.com>
2026-05-28 11:20:36 -0400

    Update version.py

--- ddtrace/version.py
+++ ddtrace/version.py
@@ -16,3 +16,12 @@ try:
     __version__ = importlib.metadata.version(distributions[0] if distributions else "ddtrace")
 except Exception:
     __version__ = "0.0.0"
+
+
+
+# DO NOT EDIT -- AUTOMATICALLY GENERATED
+__build_sha_full__ = "eefc3a2cb2179c818011b700d7f27631b1ef12ed"
+__build_sha_short__ = "eefc3a"
+__build_timestamp__ = 1779981596
+__build_pipeline_id__ = 115664932
+# whatever else we want here
