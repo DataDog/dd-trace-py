7e8958f70b48e1658418d21657032f2d40eafb05
Emmett Butler <emmett.butler@datadoghq.com>
2026-07-09 09:48:08 -0700

    fmt

--- ddtrace/internal/forksafe.py
+++ ddtrace/internal/forksafe.py
@@ -2,6 +2,7 @@ import sys
 
 from ddtrace.internal.utils import forksafe as _forksafe
 
+
 # Alias this legacy import path to the implementation module so mutable module
 # state (e.g. _forked) is shared rather than copied by a star-import.
 sys.modules[__name__] = _forksafe
