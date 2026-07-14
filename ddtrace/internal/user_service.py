8a24787d44fba7b38f76c37c2e53237a1dc4bb8a
Emmett Butler <emmett.butler@datadoghq.com>
2026-07-14 12:59:42 -0700

    dont rely on reexport

--- ddtrace/internal/user_service.py
+++ ddtrace/internal/user_service.py
@@ -1,11 +1,11 @@
-from ddtrace.internal.telemetry import get_config as _get_config
+from ddtrace.internal.getconfig import get_config
 from ddtrace.internal.utils.formats import parse_tags_str
 
 
 def is_user_provided_service() -> bool:
-    service = _get_config("DD_SERVICE")
+    service = get_config("DD_SERVICE")
     if not service:
-        service = parse_tags_str(_get_config("DD_TAGS")).get("service")
+        service = parse_tags_str(get_config("DD_TAGS")).get("service")
     if not service:
-        service = _get_config("OTEL_SERVICE_NAME")
+        service = get_config("OTEL_SERVICE_NAME")
     return service is not None
