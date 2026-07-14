b6ff13d8036863c3d42e4a7dfa60860cb76fee52
Emmett Butler <emmett.butler@datadoghq.com>
2026-07-14 11:10:17 -0700

    handle none

--- ddtrace/internal/user_service.py
+++ ddtrace/internal/user_service.py
@@ -5,7 +5,7 @@ from ddtrace.internal.utils.formats import parse_tags_str
 def is_user_provided_service() -> bool:
     service = _get_config("DD_SERVICE")
     if not service:
-        service = parse_tags_str(_get_config("DD_TAGS"))["service"]
+        service = parse_tags_str(_get_config("DD_TAGS")).get("service")
     if not service:
         service = _get_config("OTEL_SERVICE_NAME")
     return service is not None
