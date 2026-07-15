38fbe9dcbab1b73f1fe41578ab601743afb3b74d
Emmett Butler <emmett.butler@datadoghq.com>
2026-07-15 07:23:05 -0700

    cache to avoid recomputation on repeated calls

--- ddtrace/internal/user_service.py
+++ ddtrace/internal/user_service.py
@@ -1,11 +1,15 @@
 from ddtrace.internal.getconfig import get_config
+from ddtrace.internal.utils.cache import cached
 from ddtrace.internal.utils.formats import parse_tags_str
 
 
+@cached()
 def is_user_provided_service() -> bool:
-    service = get_config(
-        "DD_SERVICE",
-        parse_tags_str(get_config("DD_TAGS", otel_env="OTEL_RESOURCE_ATTRIBUTES")).get("service"),
-        otel_env="OTEL_SERVICE_NAME",
+    return (
+        get_config(
+            "DD_SERVICE",
+            parse_tags_str(get_config("DD_TAGS", otel_env="OTEL_RESOURCE_ATTRIBUTES")).get("service"),
+            otel_env="OTEL_SERVICE_NAME",
+        )
+        is not None
     )
-    return service is not None
