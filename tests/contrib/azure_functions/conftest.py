cbc44174ac86d996599d7d4015124f8080b8d33e
Rithika Narayan <rithika.narayan@datadoghq.com>
2026-07-16 14:59:48 -0400

    address timeout DD_EKKQ9I

--- tests/contrib/azure_functions/conftest.py
+++ tests/contrib/azure_functions/conftest.py
@@ -1,6 +1,7 @@
 import os
 import signal
 import subprocess
+import tempfile
 import time
 
 import pytest
@@ -10,6 +11,12 @@ from tests.webclient import Client
 
 AZURE_FUNCTIONS_PORT = 7071
 AZURE_FUNCTION_APP_DIR = os.path.join(os.path.dirname(__file__), "azure_function_app")
+
+
+def _read_log(log_file):
+    # type: (object) -> str
+    log_file.seek(0)
+    return log_file.read().decode("utf-8", errors="replace")
 
 
 def _start_azure_functions_server(extra_env=None):
@@ -22,12 +29,16 @@ def _start_azure_functions_server(extra_env=None):
     env["AZURE_FUNCTIONS_TEST_PORT"] = str(port)
     env["DD_TRACE_STATS_COMPUTATION_ENABLED"] = "False"  # disable stats computation to avoid potential flakes in tests
 
+    # Use temp files instead of PIPE to capture logs without deadlocking when func is verbose.
+    stdout_log = tempfile.TemporaryFile(mode="w+b")
+    stderr_log = tempfile.TemporaryFile(mode="w+b")
+
     # webservers might exec or fork into another process, so we need to os.setsid() to create a process group
     # (all of which will listen to signals sent to the parent) so that we can kill the whole application.
     proc = subprocess.Popen(
         ["func", "start", "--port", str(port)],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
+        stdout=stdout_log,
+        stderr=stderr_log,
         close_fds=True,
         env=env,
         preexec_fn=os.setsid,
@@ -37,8 +48,8 @@ def _start_azure_functions_server(extra_env=None):
     try:
         client.wait(delay=0.5)
     except Exception:
-        stdout = proc.stdout.read()
-        stderr = proc.stderr.read()
+        stdout = _read_log(stdout_log)
+        stderr = _read_log(stderr_log)
         os.killpg(proc.pid, signal.SIGKILL)
         proc.wait()
         raise TimeoutError(
@@ -58,7 +69,7 @@ def azure_functions_server():
 
 
 @pytest.fixture
-def azure_functions_client(request, azure_functions_server):
+def azure_functions_client(request):
     env_vars = getattr(request, "param", {})
 
     if env_vars:
@@ -71,7 +82,8 @@ def azure_functions_client(request, azure_functions_server):
             proc.wait()
         return
 
-    client = Client("http://0.0.0.0:%d" % azure_functions_server)
+    port = request.getfixturevalue("azure_functions_server")
+    client = Client("http://0.0.0.0:%d" % port)
     yield client
     # At this point the traces have been sent to the test agent
     # but the test agent hasn't necessarily finished processing
