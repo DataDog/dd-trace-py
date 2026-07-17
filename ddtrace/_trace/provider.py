b01e327f1e8b71e899ccd31a6d8dd777759a9c3d
Florentin Labelle <florentin.labelle@datadoghq.com>
2026-07-17 15:14:07 +0200

    inline stuff

--- ddtrace/_trace/provider.py
+++ ddtrace/_trace/provider.py
@@ -39,12 +39,6 @@ if sys.platform == "linux":
     from ddtrace.internal.native._native import detach_otel_thread_context
     from ddtrace.internal.native._native import update_otel_thread_context
 
-    def _update_otel_thread_context(ctx: Optional[ActiveTrace]) -> None:
-        if type(ctx) is Span:
-            update_otel_thread_context(ctx, ctx._local_root)
-        else:
-            detach_otel_thread_context()
-
 
 class BaseContextProvider(metaclass=abc.ABCMeta):
     """
@@ -95,7 +89,12 @@ class DefaultContextProvider(BaseContextProvider):
         def activate(self, ctx: Optional[ActiveTrace]) -> None:
             """Makes the given context active in the current execution."""
             _activate_contextvar(ctx)
-            _update_otel_thread_context(ctx)
+
+            if type(ctx) is Span:
+                update_otel_thread_context(ctx, ctx._local_root)
+            else:
+                detach_otel_thread_context()
+
             super(DefaultContextProvider, self).activate(ctx)
     else:
 
