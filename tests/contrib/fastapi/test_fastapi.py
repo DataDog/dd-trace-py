cbcf512b2e0f2c757f7327d64c2afdba6440854b
Emmett Butler <emmett.butler@datadoghq.com>
2026-07-20 14:13:49 -0700

    lint

--- tests/contrib/fastapi/test_fastapi.py
+++ tests/contrib/fastapi/test_fastapi.py
@@ -13,7 +13,6 @@ from ddtrace.contrib.internal.starlette.patch import patch as patch_starlette
 from ddtrace.contrib.internal.starlette.patch import unpatch as unpatch_starlette
 from ddtrace.internal.utils.version import parse_version
 from ddtrace.propagation import http as http_propagation
-from tests.conftest import DEFAULT_DDTRACE_SUBPROCESS_TEST_SERVICE_NAME
 from tests.tracer.utils_inferred_spans.test_helpers import assert_web_and_inferred_aws_api_gateway_span_data
 from tests.utils import override_config
 from tests.utils import override_global_config
