a74c1b4d8b69a64b11121f1c028a90ed568d73f0
Emmett Butler <emmett.butler@datadoghq.com>
2026-07-22 11:04:55 -0700

    set svc_src if no override is occurring

--- ddtrace/_trace/processor/__init__.py
+++ ddtrace/_trace/processor/__init__.py
@@ -383,9 +383,9 @@ class SpanAggregator(SpanProcessor):
         # Acquire lock to get finished and update trace.spans
         with self._lock:
             integration_name = span._get_str_attribute(COMPONENT) or span._span_api
-            existing_service_source = span.get_tag(_SERVICE_SOURCE)
+            existing_service_source = span._get_str_attribute(_SERVICE_SOURCE)
             if span.service in config._integration_default_services or span.service == config._inferred_base_service:
-                if existing_service_source and integration_name != span._span_api:
+                if not existing_service_source or (existing_service_source and integration_name != span._span_api):
                     span.set_tag(_SERVICE_SOURCE, integration_name)
             self._span_metrics["spans_finished"][integration_name] += 1
 
