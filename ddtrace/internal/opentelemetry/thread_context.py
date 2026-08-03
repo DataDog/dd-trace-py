bc6249269b9c39ae92cf8da45df58b9c13a9241a
Florentin Labelle <florentin.labelle@datadoghq.com>
2026-08-03 11:40:08 +0200

    feat(tracing): listen for Python context switches

--- ddtrace/internal/opentelemetry/thread_context.py
+++ ddtrace/internal/opentelemetry/thread_context.py
@@ -17,20 +17,19 @@ class TracerProtocol(Protocol):
 
 
 _ContextActivationListener = Callable[[BaseContextProvider, Optional[Union[Context, Span]]], None]
+_ContextSwitchListener = Callable[[], None]
+_ThreadContextListeners = tuple[_ContextActivationListener, _ContextSwitchListener]
 
 
 if sys.platform == "linux":
     from ddtrace.internal.native._native import detach_otel_thread_context
     from ddtrace.internal.native._native import update_otel_thread_context
 
-    def register_otel_thread_context_listener(tracer: TracerProtocol) -> Optional[_ContextActivationListener]:
+    def register_otel_thread_context_listener(tracer: TracerProtocol) -> Optional[_ThreadContextListeners]:
         if not config._otel_thread_context_enabled:
             return None
 
-        def _sync_otel_thread_context(provider: BaseContextProvider, ctx: Optional[Union[Context, Span]]) -> None:
-            if provider is not tracer.context_provider:
-                return
-
+        def _sync_otel_thread_context(ctx: Optional[Union[Context, Span]]) -> None:
             if type(ctx) is Span:
                 sampling_priority = ctx._local_root.context.sampling_priority
                 trace_flags = 1 if sampling_priority is not None and sampling_priority > 0 else 0
@@ -38,10 +37,18 @@ if sys.platform == "linux":
             else:
                 detach_otel_thread_context()
 
-        core.on("ddtrace.context_provider.activate", _sync_otel_thread_context)
-        return _sync_otel_thread_context
+        def _sync_active_otel_thread_context() -> None:
+            _sync_otel_thread_context(tracer.context_provider.active())
+
+        def _on_context_provider_activate(provider: BaseContextProvider, ctx: Optional[Union[Context, Span]]) -> None:
+            if provider is tracer.context_provider:
+                _sync_otel_thread_context(ctx)
+
+        core.on("ddtrace.context_provider.activate", _on_context_provider_activate)
+        core.on("python.context.switch", _sync_active_otel_thread_context)
+        return _on_context_provider_activate, _sync_active_otel_thread_context
 
 else:
 
-    def register_otel_thread_context_listener(tracer: TracerProtocol) -> Optional[_ContextActivationListener]:
+    def register_otel_thread_context_listener(tracer: TracerProtocol) -> Optional[_ThreadContextListeners]:
         return None
