b4d8e503e2019ecd544fd67fba3b6d79d7b57a95
Taegyun Kim <taegyun.kim@datadoghq.com>
2026-08-04 19:19:26 +0000

    test(profiling): reproduce sampler native shutdown race

---
+++ ddtrace/internal/datadog/profiling/stack/test/test_native_shutdown.cpp
@@ -0,0 +1,53 @@
+#include "sampler.hpp"
+#include "../../dd_wrapper/test/test_utils.hpp"
+#include <gtest/gtest.h>
+
+#include <Python.h>
+
+#include <chrono>
+#include <cstdlib>
+#include <thread>
+
+namespace {
+
+void
+keep_process_alive_after_profiler_teardown()
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+}
+
+[[noreturn]] void
+exit_without_stopping_sampler()
+{
+    Py_Initialize();
+
+    // Register this before profiler state and the sampler so it runs after their
+    // native exit handlers and widens the use-after-free window.
+    std::atexit(keep_process_alive_after_profiler_teardown);
+
+    configure("shutdown-test", "test", "1.0", "http://127.0.0.1:8126", "cpython", "test", "test", 64);
+
+    auto& sampler = Datadog::Sampler::get();
+    sampler.set_interval(0.001);
+    if (!sampler.start()) {
+        std::exit(2);
+    }
+
+    for (size_t i = 0; i < 1000 && !sampler.is_running(); i++) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+    if (!sampler.is_running()) {
+        std::exit(3);
+    }
+
+    // Embedders such as uWSGI can begin native process teardown without first
+    // invoking the profiler's Python shutdown handler.
+    std::exit(0);
+}
+
+}
+
+TEST(NativeShutdownDeathTest, ExitWithoutStoppingSampler)
+{
+    EXPECT_EXIT(exit_without_stopping_sampler(), ::testing::ExitedWithCode(0), "");
+}
