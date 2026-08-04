164801371c7958966a10fb00a812700798b85686
Gyuheon Oh <gyuheon.oh@datadoghq.com>
2026-08-04 16:14:39 -0400

    clanker macos ci issue fix

--- .gitlab/scripts/multi-os-tests.sh
+++ .gitlab/scripts/multi-os-tests.sh
@@ -15,11 +15,19 @@ export PATH="$HOME/.local/bin:$PATH"
 export TMPDIR=$(mktemp -d)
 cd "$TMPDIR"
 export WHEEL_TAG="cp${PYTHON_VERSION//./}"
+echo "Contents of $CI_PROJECT_DIR/pywheels:"
+ls -la "$CI_PROJECT_DIR/pywheels" 2>&1 || echo "(pywheels directory does not exist)"
 echo "Installing Python $PYTHON_VERSION and dependencies in $TMPDIR..."
 uv python install $PYTHON_VERSION
 uv venv --python $PYTHON_VERSION .venv
-WHEEL_PATH="$CI_PROJECT_DIR/pywheels/ddtrace*${WHEEL_TAG}*${WHEEL_PATTERN}"
-uv pip install --python $PYTHON_VERSION -r "$CI_PROJECT_DIR/.gitlab/requirements/multi-os-tests.txt" $WHEEL_PATH
+shopt -s nullglob
+WHEEL_MATCHES=("$CI_PROJECT_DIR"/pywheels/ddtrace*"${WHEEL_TAG}"*${WHEEL_PATTERN})
+shopt -u nullglob
+if [ "${#WHEEL_MATCHES[@]}" -ne 1 ]; then
+  echo "Expected exactly one wheel matching ddtrace*${WHEEL_TAG}*${WHEEL_PATTERN} in $CI_PROJECT_DIR/pywheels, found ${#WHEEL_MATCHES[@]}: ${WHEEL_MATCHES[*]}"
+  exit 1
+fi
+uv pip install --python $PYTHON_VERSION -r "$CI_PROJECT_DIR/.gitlab/requirements/multi-os-tests.txt" "${WHEEL_MATCHES[0]}"
 
 # Run tests
 export PATH="$HOME/.local/bin:$PATH"
