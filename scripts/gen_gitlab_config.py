66d705d878b7b6dffd3f42c4b5b6df63ac739309
Federico Mon <federico.mon@datadoghq.com>
2026-08-06 10:10:07 +0200

    change approach, move logic to riotfile

--- scripts/gen_gitlab_config.py
+++ scripts/gen_gitlab_config.py
@@ -32,20 +32,6 @@ BENCHMARK_CLASS_REGEX = r"class ([A-Za-z]+)\((bm\.)?Scenario(.+)?\)\:"
 BENCHMARK_SCENARIO_REGEX = re.compile(" +- name: ([a-z0-9]+)-.+")
 
 
-def _python_version_tuple(version: str) -> tuple[int, ...]:
-    return tuple(int(part) for part in version.split("."))
-
-
-def _get_itr_python_versions(python_versions: t.Optional[set[str]], min_python_version: str) -> str:
-    versions = python_versions or set(ALL_PYTHON_VERSIONS)
-    min_version = _python_version_tuple(min_python_version)
-    return " ".join(
-        version
-        for version in sorted(versions, key=_python_version_tuple)
-        if _python_version_tuple(version) >= min_version
-    )
-
-
 def _get_bool_env(name: str) -> str:
     """Return "true"/"false" for a boolean environment variable.
 
@@ -90,7 +76,6 @@ class JobSpec:
     skip_pip_cache: bool = False
     itr_enabled: bool = True
     itr_test_skipping_enabled: bool = False
-    itr_min_python_version: str = "3.12"
 
     python_versions: t.Optional[set[str]] = None
 
@@ -158,9 +143,6 @@ class JobSpec:
         suite_name = env["SUITE_NAME"]
         env["DD_TRACE_PY_ENABLE_ITR_FOR_JOB"] = "true" if self.itr_enabled else "false"
         env["DD_TRACE_PY_ENABLE_ITR_TEST_SKIPPING_FOR_JOB"] = "true" if self.itr_test_skipping_enabled else "false"
-        env["DD_TRACE_PY_ITR_PYTHON_VERSIONS"] = _get_itr_python_versions(
-            self.python_versions, self.itr_min_python_version
-        )
         env["PIP_CACHE_DIR"] = "${CI_PROJECT_DIR}/.cache/pip"
         env["PIP_CACHE_KEY"] = (
             subprocess.check_output([".gitlab/scripts/get-riot-pip-cache-key.sh", suite_name]).decode().strip()
