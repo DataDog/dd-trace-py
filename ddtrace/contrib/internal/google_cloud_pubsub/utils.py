ec1d9b7e96bc097e7975bd4ac0532dc3ee72261c
Louis Tricot <louis.tricot@datadoghq.com>
2026-08-25 17:32:36 +0200

    feat(config): add a new propagation_as_span_links configuration

--- ddtrace/contrib/internal/google_cloud_pubsub/utils.py
+++ ddtrace/contrib/internal/google_cloud_pubsub/utils.py
@@ -1,8 +1,29 @@
 """Utilities for Google Cloud Pub/Sub instrumentation."""
 
+from ddtrace.internal.settings import env
 from ddtrace.internal.settings._config import _get_config
 from ddtrace.internal.settings._config import config
+from ddtrace.internal.utils.deprecations import DDTraceDeprecationWarning
+from ddtrace.internal.utils.deprecations import deprecate
 from ddtrace.internal.utils.formats import asbool
+
+
+_DEPRECATED_SPAN_LINKS_ENV = "DD_GOOGLE_CLOUD_PUBSUB_PROPAGATION_AS_SPAN_LINKS"
+
+
+def _propagation_as_span_links_enabled() -> bool:
+    if "google_cloud_pubsub" in config._propagation_as_span_links:
+        return True
+    if _DEPRECATED_SPAN_LINKS_ENV in env:
+        deprecate(
+            f"{_DEPRECATED_SPAN_LINKS_ENV} is deprecated",
+            message="Use DD_TRACE_PROPAGATION_AS_SPAN_LINKS with a comma-separated list of "
+            "integration names (e.g. 'google_cloud_pubsub,kafka') instead.",
+            removal_version="5.0.0",
+            category=DDTraceDeprecationWarning,
+        )
+        return asbool(_get_config(_DEPRECATED_SPAN_LINKS_ENV, default=False))
+    return False
 
 
 def ensure_config_registered() -> None:
@@ -17,9 +38,7 @@ def ensure_config_registered() -> None:
         "google_cloud_pubsub",
         dict(
             distributed_tracing_enabled=asbool(_get_config("DD_GOOGLE_CLOUD_PUBSUB_PROPAGATION_ENABLED", default=True)),
-            propagation_as_span_links=asbool(
-                _get_config("DD_GOOGLE_CLOUD_PUBSUB_PROPAGATION_AS_SPAN_LINKS", default=False)
-            ),
+            propagation_as_span_links=_propagation_as_span_links_enabled(),
         ),
     )
 
