c058f99a4ada473421643aa3c9f4d8585e13fa2b
Rithika Narayan <rithika.narayan@datadoghq.com>
2026-09-03 16:19:33 -0700

    Reducing overhead of version.py

--- ddtrace/version.py
+++ ddtrace/version.py
@@ -8,11 +8,10 @@ __all__ = ["__version__"]
 __version__: str
 
 try:
-    distributions = importlib.metadata.packages_distributions().get(__package__ or __name__)
+    __version__ = importlib.metadata.version("ddtrace")
 except Exception:
-    distributions = None
-
-try:
-    __version__ = importlib.metadata.version(distributions[0] if distributions else "ddtrace")
-except Exception:
-    __version__ = "0.0.0"
+    try:
+        distributions = importlib.metadata.packages_distributions().get(__package__ or __name__)
+        __version__ = importlib.metadata.version(distributions[0] if distributions else "ddtrace")
+    except Exception:
+        __version__ = "0.0.0"
