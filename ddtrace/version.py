85e2aebcf1458307cc5b5eff6a921944fc9ca076
Rithika Narayan <rithika.narayan@datadoghq.com>
2026-09-08 09:29:34 -0700

    fix indent'

--- ddtrace/version.py
+++ ddtrace/version.py
@@ -3,7 +3,7 @@
 import importlib.metadata
 
 
-__all__ = ["__version__", *sorted(_LAZY_EXPORTS)]
+__all__ = ["__version__"]
 
 __version__: str
 
@@ -11,7 +11,7 @@ try:
     __version__ = importlib.metadata.version("ddtrace")
 except Exception:
     try:
-       _distributions = importlib.metadata.packages_distributions().get(__package__ or __name__)
+        _distributions = importlib.metadata.packages_distributions().get(__package__ or __name__)
         __version__ = importlib.metadata.version(_distributions[0] if _distributions else "ddtrace")
     except Exception:
         __version__ = "0.0.0"
