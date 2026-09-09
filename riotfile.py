ed6d72f9ac5b3c89863445f4b046667712a90848
Crystal Luc-Magloire <crystal.magloire@datadoghq.com>
2026-09-09 12:29:11 -0400

    ci: fill Python test coverage through 3.14

--- riotfile.py
+++ riotfile.py
@@ -946,7 +946,7 @@ venv = Venv(
                         "python -m pytest {cmdargs} --ignore='tests/contrib/bottle/test_autopatch.py' "
                         "tests/contrib/bottle/"
                     ),
-                    pys="3.14",
+                    pys=select_pys(min_version="3.10", max_version="3.14"),
                     pkgs={"bottle": latest},
                 ),
                 Venv(
@@ -962,7 +962,7 @@ venv = Venv(
                         "python tests/ddtrace_run.py python -m pytest {cmdargs} tests/contrib/bottle/test_autopatch.py"
                     ),
                     env={"DD_SERVICE": "bottle-app"},
-                    pys="3.14",
+                    pys=select_pys(min_version="3.10", max_version="3.14"),
                     pkgs={"bottle": latest},
                 ),
             ],
@@ -1672,7 +1672,7 @@ venv = Venv(
                     pkgs={"moto": "==5.2.3"},
                 ),
                 Venv(
-                    pys="3.14",
+                    pys=select_pys(min_version="3.13", max_version="3.14"),
                     pkgs={"moto": "==5.2.3", "pynamodb": "<6.0"},
                 ),
             ],
@@ -2723,7 +2723,7 @@ venv = Venv(
                     pys="3.10",
                     pkgs={"yaaredis": latest},
                 ),
-                Venv(pys="3.14", pkgs={"yaaredis": latest}),
+                Venv(pys=select_pys(min_version="3.11", max_version="3.14"), pkgs={"yaaredis": latest}),
             ],
         ),
         Venv(
@@ -2789,7 +2789,7 @@ venv = Venv(
                     },
                 ),
                 Venv(
-                    pys="3.14",
+                    pys=select_pys(min_version="3.13", max_version="3.14"),
                     pkgs={
                         "sanic": latest,
                         "sanic-testing": latest,
@@ -3398,7 +3398,7 @@ venv = Venv(
                         "torch": ["~=2.8.0", "~=2.9.0", "~=2.10.0", "~=2.11.0", "~=2.12.0", latest],
                     },
                 ),
-                Venv(pys="3.14", pkgs={"torch": latest}),
+                Venv(pys=select_pys(min_version="3.13", max_version="3.14"), pkgs={"torch": latest}),
             ],
         ),
         Venv(
