fd8b2923584f6ab08ce09437a3570ef0475a34e6
Taegyun Kim <taegyun.kim@datadoghq.com>
2026-09-09 13:16:05 -0400

    chore(profiling): inline Python header setup

--- ddtrace/internal/datadog/profiling/stack/include/stack_renderer.hpp
+++ ddtrace/internal/datadog/profiling/stack/include/stack_renderer.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
-#include "python_headers.hpp"
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#if PY_VERSION_HEX >= 0x030c0000
+// https://github.com/python/cpython/issues/108216#issuecomment-1696565797
+#undef _PyGC_FINALIZED
+#endif
 
 #include <cstdint>
 #include <memory>
