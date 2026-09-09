c9ac42da02994dbd2b1bb4f1c9ed639bc129b830
Louis Tricot <louis.tricot@datadoghq.com>
2026-09-09 14:03:29 +0200

    chore(aiobotocore): remove Pin

--- ddtrace/contrib/internal/aiobotocore/patch.py
+++ ddtrace/contrib/internal/aiobotocore/patch.py
@@ -2,7 +2,6 @@ import aiobotocore.client
 import wrapt
 
 from ddtrace import config
-from ddtrace._trace.pin import Pin
 
 # AIDEV-NOTE: _http_propagation_suppressed is the shared seam telling the
 # aiohttp-layer subscriber to skip its own injection during AWS calls; _wrapped_api_call
@@ -80,7 +79,6 @@ def patch():
     aiobotocore.client._datadog_patch = True
 
     wrapt.wrap_function_wrapper("aiobotocore.client", "AioBaseClient._make_api_call", _wrapped_api_call)
-    Pin().onto(aiobotocore.client.AioBaseClient)
 
 
 def unpatch():
@@ -93,7 +91,7 @@ def unpatch():
 
 
 class WrappedClientResponseContentProxy(wrapt.ObjectProxy):
-    def __init__(self, body, pin, parent_span):
+    def __init__(self, body, parent_span):
         super(WrappedClientResponseContentProxy, self).__init__(body)
         self._self_parent_span = parent_span
 
@@ -129,11 +127,6 @@ class WrappedClientResponseContentProxy(wrapt.ObjectProxy):
 
 
 async def _wrapped_api_call(original_func, instance, args, kwargs):
-    pin = Pin.get_from(instance)
-    if not pin or not pin.enabled():
-        result = await original_func(*args, **kwargs)
-        return result
-
     endpoint_name = deep_getattr(instance, "_endpoint._endpoint_prefix")
 
     fallback_service = config._get_service(default="aws.{}".format(endpoint_name))
@@ -145,7 +138,7 @@ async def _wrapped_api_call(original_func, instance, args, kwargs):
     ) as span:
         set_service_and_source(
             span,
-            ext_service(pin, config.aiobotocore, default=schematize_service_name(fallback_service)),
+            ext_service(None, config.aiobotocore, default=schematize_service_name(fallback_service)),
             config.aiobotocore,
         )
         span._set_attribute(COMPONENT, config.aiobotocore.integration_name)
@@ -208,7 +201,7 @@ async def _wrapped_api_call(original_func, instance, args, kwargs):
 
         # ClientResponseContentProxy removed in aiobotocore 2.3.x: https://github.com/aio-libs/aiobotocore/pull/934/
         if hasattr(body, "ClientResponseContentProxy") and isinstance(body, ClientResponseContentProxy):
-            result["Body"] = WrappedClientResponseContentProxy(body, pin, span)
+            result["Body"] = WrappedClientResponseContentProxy(body, span)
 
         response_meta = result["ResponseMetadata"]
         response_headers = response_meta["HTTPHeaders"]
