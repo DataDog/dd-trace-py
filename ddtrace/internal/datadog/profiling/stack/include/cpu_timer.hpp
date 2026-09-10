a2f72c0ecb84318271847465f116db39b40e6715
Taegyun Kim <taegyun.kim@datadoghq.com>
2026-09-10 20:07:43 +0000

    fix(profiling): update CPU timer Python include

--- ddtrace/internal/datadog/profiling/stack/include/cpu_timer.hpp
+++ ddtrace/internal/datadog/profiling/stack/include/cpu_timer.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
-#include <cstdint>
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
 
-#include "python_headers.hpp"
+#if PY_VERSION_HEX >= 0x030c0000
+// https://github.com/python/cpython/issues/108216#issuecomment-1696565797
+#undef _PyGC_FINALIZED
+#endif
+
+#include <cstdint>
 
 #include "echion/timing.h"
 
