56574ac2405df0b0679c5ac0661af8c40bed5f67
Vlad Scherbich <vlad.scherbich@datadoghq.com>
2026-09-10 14:53:53 -0400

    fix: satisfy ruff I001 blank line after imports in py315 degrade test

--- tests/internal/test_compat_version_bounds.py
+++ tests/internal/test_compat_version_bounds.py
@@ -22,6 +22,7 @@ from ddtrace.internal.compat import PYTHON_VERSION_INFO
 from ddtrace.internal.compat import is_at_least_py
 from ddtrace.internal.compat import is_at_most_py
 
+
 _RUNNING_VERSION: str = f"{PYTHON_VERSION_INFO[0]}.{PYTHON_VERSION_INFO[1]}"
 _UNSUPPORTED_MSG: str = f"This version of CPython is not supported yet: {_RUNNING_VERSION}"
 
