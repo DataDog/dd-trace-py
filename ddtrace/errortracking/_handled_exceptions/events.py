40b0e7a4ab933d84a377c50422eb5b1b1788bd77
Louis Tricot <louis.tricot@datadoghq.com>
2026-09-15 15:41:08 +0200

    remove product dependency

--- ddtrace/errortracking/_handled_exceptions/events.py
+++ ddtrace/errortracking/_handled_exceptions/events.py
@@ -1,9 +1,30 @@
 import typing as t
 from typing import NamedTuple
 
-from ddtrace._trace.span import Span
 from ddtrace.internal.constants import COLLECTOR_MAX_SIZE_PER_SPAN
 from ddtrace.internal.constants import SPAN_EVENTS_HAS_EXCEPTION
+
+
+class _SpanProtocol(t.Protocol):
+    """Structural interface for the span members this module relies on.
+
+    Lets error-tracking storage helpers type-annotate spans without importing the
+    concrete ddtrace._trace.span.Span class, which would create a
+    product:errortracking -> product:tracing dependency.
+    """
+
+    span_id: int
+
+    def _set_attribute(self, key: str, value: t.Union[str, int, float]) -> None: ...
+
+    def _add_event(
+        self,
+        name: str,
+        attributes: t.Optional[t.Mapping[str, t.Any]] = None,
+        time_unix_nano: t.Optional[int] = None,
+    ) -> None: ...
+
+    def _add_on_finish_exception_callback(self, callback: t.Callable[["_SpanProtocol"], None]) -> None: ...
 
 
 class SpanEventData(NamedTuple):
@@ -15,7 +36,7 @@ class SpanEventData(NamedTuple):
 _span_exception_events: dict[int, dict[int, tuple[Exception, SpanEventData]]] = {}
 
 
-def add_span_events(span: Span) -> None:
+def add_span_events(span: _SpanProtocol) -> None:
     """
     If the same error is handled/rethrown multiple times, report only one span event.
     Store handled exceptions until the span finishes so they can be deduplicated.
@@ -29,14 +50,14 @@ def add_span_events(span: Span) -> None:
     clear_exception_events(span.span_id)
 
 
-def on_span_exception(span: Span, _exc_msg: object, exc_val: BaseException, _exc_tb: object) -> None:
+def on_span_exception(span: _SpanProtocol, _exc_msg: object, exc_val: BaseException, _exc_tb: object) -> None:
     exception_events = get_exception_events(span.span_id)
     exc_id = id(exc_val)
     if exception_events and exc_id in exception_events:
         del exception_events[exc_id]
 
 
-def capture_exception_event(span: Span, exc: Exception, event: SpanEventData) -> None:
+def capture_exception_event(span: _SpanProtocol, exc: Exception, event: SpanEventData) -> None:
     span_id = span.span_id
     events_dict = _span_exception_events.setdefault(span_id, {})
     if not events_dict:
