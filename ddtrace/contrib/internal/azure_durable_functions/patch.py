0af64c65192ca5134f97c6722c55689774b4d32a
jcstorms1 <jordan.storms@datadoghq.com>
2026-09-16 09:59:02 -0400

    fix(azure): limit durable functions support to 1.x

--- ddtrace/contrib/internal/azure_durable_functions/patch.py
+++ ddtrace/contrib/internal/azure_durable_functions/patch.py
@@ -73,7 +73,7 @@ def get_version() -> str:
 
 
 def _supported_versions() -> dict[str, str]:
-    return {"azure.durable_functions": ">=1.2.1"}
+    return {"azure.durable_functions": ">=1.2.1,<2.0.0"}
 
 
 def patch():
