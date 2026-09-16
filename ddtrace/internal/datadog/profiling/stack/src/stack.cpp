d2a8c7867bac6bf8ab3d749b113fde2ddef1d379
Taegyun Kim <taegyun.kim@datadoghq.com>
2026-09-16 13:27:21 +0000

    refactor(profiling): defer native tracking queries

--- ddtrace/internal/datadog/profiling/stack/src/stack.cpp
+++ ddtrace/internal/datadog/profiling/stack/src/stack.cpp
@@ -426,19 +426,6 @@ stack_track_asyncio_loop(PyObject* self, PyObject* args)
 }
 
 static PyObject*
-stack_is_asyncio_loop_registered(PyObject* self, PyObject* args)
-{
-    (void)self;
-    uint64_t thread_id;
-
-    if (!PyArg_ParseTuple(args, "K", &thread_id)) {
-        return nullptr;
-    }
-
-    return PyBool_FromLong(Sampler::get().is_asyncio_loop_registered(static_cast<uintptr_t>(thread_id)));
-}
-
-static PyObject*
 stack_init_asyncio(PyObject* self, PyObject* args)
 {
     (void)self;
@@ -684,19 +671,6 @@ track_greenlet(PyObject* Py_UNUSED(m), PyObject* args)
     Py_END_ALLOW_THREADS;
 
     Py_RETURN_NONE;
-}
-
-static PyObject*
-is_greenlet_tracked(PyObject* Py_UNUSED(m), PyObject* args)
-{
-    uint64_t greenlet_id;
-    if (!PyArg_ParseTuple(args, "K", &greenlet_id))
-        return nullptr;
-
-    if (Sampler::get().is_greenlet_tracked(static_cast<uintptr_t>(greenlet_id))) {
-        Py_RETURN_TRUE;
-    }
-    Py_RETURN_FALSE;
 }
 
 static PyObject*
@@ -1262,16 +1236,11 @@ static PyMethodDef stack_methods[] = {
       "Clear the originating asyncio task for the current (executor worker) thread" },
     // asyncio task support
     { "track_asyncio_loop", stack_track_asyncio_loop, METH_VARARGS, "Track an asyncio loop for a registered thread" },
-    { "is_asyncio_loop_registered",
-      stack_is_asyncio_loop_registered,
-      METH_VARARGS,
-      "Return whether a thread has a tracked running asyncio loop" },
     { "init_asyncio", stack_init_asyncio, METH_VARARGS, "Initialise asyncio tracking" },
     { "link_tasks", stack_link_tasks, METH_VARARGS, "Link two tasks" },
     { "weak_link_tasks", stack_weak_link_tasks, METH_VARARGS, "Weakly link two tasks" },
     // greenlet support
     { "track_greenlet", track_greenlet, METH_VARARGS, "Map a greenlet with its identifier" },
-    { "is_greenlet_tracked", is_greenlet_tracked, METH_VARARGS, "Return whether a greenlet is tracked" },
     { "untrack_greenlet", untrack_greenlet, METH_VARARGS, "Untrack a terminated greenlet" },
     { "link_greenlets", link_greenlets, METH_VARARGS, "Link two greenlets" },
     { "record_greenlet_switch", record_greenlet_switch, METH_VARARGS, "Record a greenlet context switch" },
