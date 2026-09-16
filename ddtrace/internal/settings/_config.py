15082285cda9c4e09142985fbc2b5c27ad49124c
Vlad Scherbich <vlad.scherbich@datadoghq.com>
2026-09-15 21:43:24 -0400

    chore: fix pre-existing mypy has-type errors blocking profiling commits

--- ddtrace/internal/settings/_config.py
+++ ddtrace/internal/settings/_config.py
@@ -427,6 +427,10 @@ class Config(object):
     available and can be updated by users.
     """
 
+    service: Optional[str]
+    _data_streams_enabled: bool
+    _model_lab_enabled: bool
+
     class _HTTPServerConfig(object):
         _error_statuses: str = _get_config("DD_TRACE_HTTP_SERVER_ERROR_STATUSES", "500-599")
         _error_ranges: list[tuple[int, int]] = get_error_ranges(_error_statuses)
