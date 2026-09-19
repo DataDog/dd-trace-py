e76f184a233df359d44664e1c9dd3c95879452d4
Daniel Schwartz-Narbonne <daniel.schwartznarbonne@datadoghq.com>
2026-09-19 01:24:55 +0000

    fix memalloc_heap call

--- ddtrace/profiling/collector/_memalloc_heap.cpp
+++ ddtrace/profiling/collector/_memalloc_heap.cpp
@@ -300,9 +300,11 @@ heap_tracker_t::export_heap_no_cpython()
         tb->sample.export_sample();
     }
 
-    auto& stats = Datadog::Sample::profile_borrow().stats();
-    stats.set_heap_tracker_size(allocs_m.size());
-    stats.set_heap_tracker_cap_drops(cap_drops);
+    auto borrow = Datadog::Sample::profile_borrow();
+    if (borrow.has_value()) {
+        borrow->stats.set_heap_tracker_size(allocs_m.size());
+        borrow->stats.set_heap_tracker_cap_drops(cap_drops);
+    }
 }
 
 void
