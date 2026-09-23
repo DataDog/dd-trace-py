c9fec231a34f2ddb68950df09e4d4ecb681cb5cb
Emmett Butler <emmett.butler@datadoghq.com>
2026-09-23 08:20:52 -0700

    undo

--- ddtrace/_monkey.py
+++ ddtrace/_monkey.py
@@ -23,7 +23,6 @@ from .internal.utils.deprecations import DDTraceDeprecationWarning  # noqa: E402
 
 log = get_logger(__name__)
 
-
 # Default set of modules to automatically patch or not
 PATCH_MODULES = {
     "aiokafka": True,
